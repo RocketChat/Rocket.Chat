fcb7e60fa06b3145c21ae181f4662fb76ed2e530
Gabriel Engel <gabriel.engel@fgsys.com>
2015-05-24 12:46:48 -0300

    Adding pm2 and building scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+	echo " Example of use: source build.sh production"
+fi
+
+if [ $1 ]; then
+	source ./build-info.sh
+	export METEOR_SETTINGS=$(cat settings.$1.json)
+	meteor build --server rocket.chat --directory /var/www/rocket.chat
+	cd /var/www/rocket.chat/bundle/programs/server
+	npm install
+	cd /var/www/rocket.chat
+	pm2 startOrRestart /var/www/rocket.chat/current/pm2.$1.json
+fi
