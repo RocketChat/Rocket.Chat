1a031f08897a416a7f3029ce4fa35ce9e15b005b
Rafael Caferati <rafael.caferati@konecty.com>
2015-06-01 16:01:32 +0200

    fixed autogrow bug
    fixed autogrow.js div replication bug; added facabook/twitter metas;
    added description meta; removed flex-tab from homepage.

--- client/lib/textarea-autogrow.js
+++ client/lib/textarea-autogrow.js
@@ -1,76 +1,77 @@
-(function($)
-{
-    /**
-    * Auto-growing textareas; technique ripped from Facebook
-    *
-    *
-    * http://github.com/jaz303/jquery-grab-bag/tree/master/javascripts/jquery.autogrow-textarea.js
-    */
-    $.fn.autogrow = function(options)
-{
-    return this.filter('textarea').each(function()
-{
-    var self         = this;
-    var $self        = $(self);
-    var minHeight    = $self.height();
-    var noFlickerPad = $self.hasClass('autogrow-short') ? 0 : parseInt($self.css('lineHeight')) || 0;
-    var settings = $.extend({
-        preGrowCallback: null,
-        postGrowCallback: null
-    }, options );
+(function($) {
+	/**
+	 * Auto-growing textareas; technique ripped from Facebook
+	 *
+	 *
+	 * http://github.com/jaz303/jquery-grab-bag/tree/master/javascripts/jquery.autogrow-textarea.js
+	 */
+	$.fn.autogrow = function(options) {
+		return this.filter('textarea').each(function() {
+			var self = this;
+			var $self = $(self);
+			var minHeight = $self.height();
+			var noFlickerPad = $self.hasClass('autogrow-short') ? 0 : parseInt($self.css('lineHeight')) || 0;
+			var settings = $.extend({
+				preGrowCallback: null,
+				postGrowCallback: null
+			}, options);
 
-    var shadow = $('<div></div>').css({
-        position:    'absolute',
-        top:         -10000,
-        left:        -10000,
-        width:       $self.width(),
-        fontSize:    $self.css('fontSize'),
-        fontFamily:  $self.css('fontFamily'),
-        fontWeight:  $self.css('fontWeight'),
-        lineHeight:  $self.css('lineHeight'),
-        resize:      'none',
-        'word-wrap': 'break-word'
-    }).appendTo(document.body);
+			var shadow = $("div.autogrow-shadow");
+			if(!shadow.length){
+				shadow = $('<div></div>').addClass("autogrow-shadow").appendTo(document.body);
+			}
 
-    var update = function(event)
-{
-    var times = function(string, number)
-{
-    for (var i=0, r=''; i<number; i++) r += string;
-    return r;
-};
+			shadow.css({
+				width: $self.width(),
+				fontSize: $self.css('fontSize'),
+				fontFamily: $self.css('fontFamily'),
+				fontWeight: $self.css('fontWeight'),
+				lineHeight: $self.css('lineHeight'),
+				resize: 'none',
+				wordWrap: 'break-word'
+			});
 
-var val = self.value.replace(/</g, '&lt;')
-.replace(/>/g, '&gt;')
-.replace(/&/g, '&amp;')
-.replace(/\n$/, '<br/>&nbsp;')
-.replace(/\n/g, '<br/>')
-.replace(/ {2,}/g, function(space){ return times('&nbsp;', space.length - 1) + ' ' });
+			var update = function(event) {
+				var times = function(string, number) {
+					for (var i = 0, r = ''; i < number; i++) r += string;
+					return r;
+				};
 
-// Did enter get pressed?  Resize in this keydown event so that the flicker doesn't occur.
-if (event && event.data && event.data.event === 'keydown' && event.keyCode === 13 && event.shiftKey) {
-    val += '<br />';
-}
+				var val = self.value.replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;')
+					.replace(/&/g, '&amp;')
+					.replace(/\n$/, '<br/>&nbsp;')
+					.replace(/\n/g, '<br/>')
+					.replace(/ {2,}/g, function(space) {
+						return times('&nbsp;', space.length - 1) + ' '
+					});
 
-shadow.css('width', $self.width());
-shadow.html(val + (noFlickerPad === 0 ? '...' : '')); // Append '...' to resize pre-emptively.
+				// Did enter get pressed?  Resize in this keydown event so that the flicker doesn't occur.
+				if (event && event.data && event.data.event === 'keydown' && event.keyCode === 13 && event.shiftKey) {
+					val += '<br />';
+				}
 
-var newHeight=Math.max(shadow.height() + noFlickerPad, minHeight);
-if(settings.preGrowCallback!=null){
-    newHeight=settings.preGrowCallback($self,shadow,newHeight,minHeight);
-}
+				shadow.css('width', $self.width());
+				shadow.html(val + (noFlickerPad === 0 ? '...' : '')); // Append '...' to resize pre-emptively.
 
-$self.height(newHeight);
+				var newHeight = Math.max(shadow.height() + noFlickerPad, minHeight);
+				if (settings.preGrowCallback != null) {
+					newHeight = settings.preGrowCallback($self, shadow, newHeight, minHeight);
+				}
 
-if(settings.postGrowCallback!=null){
-    settings.postGrowCallback($self);
-}
-}
+				$self.height(newHeight);
 
-$self.change(update).keyup(update).keydown({event:'keydown'},update);
-$(window).resize(update);
+				if (settings.postGrowCallback != null) {
+					settings.postGrowCallback($self);
+				}
+			}
 
-update();
-});
-};
+			$self.change(update).keyup(update).keydown({
+				event: 'keydown'
+			}, update);
+			$(window).resize(update);
+
+			update();
+		});
+	};
 })(jQuery);
