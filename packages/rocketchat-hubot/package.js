d1d6017f91d7ad3df7cf702d034193e2c8f1aee1
Gabriel Engel <gabriel.engel@fgsys.com>
2015-06-12 06:09:45 -0300

    adding embedded hubot

--- packages/rocketchat-hubot/package.js
+++ packages/rocketchat-hubot/package.js
@@ -8,15 +8,24 @@ Package.describe({
 Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
-	api.use(['coffeescript']);
+	api.use([
+		'coffeescript',
+		'rocketchat:lib@0.0.1'
+	]);
 
 	api.addFiles('hubot.coffee', ['server']);
 
 	api.export('Hubot', ['server']);
+	api.export('HubotScripts', ['server']);
+	api.export('RocketBot', ['server']);
+	api.export('RocketBotReceiver', ['server']);
+	api.export('RocketChatAdapter', ['server']);
+
 });
 
 Npm.depends({
 	"coffee-script": "1.9.3",
+	"codex-blackboard-hubot-scripts": "https://github.com/cscott/codex-blackboard-hubot-scripts/tarball/f57c178a2faee9b36d07a7905c29093b9824e0b0",
 	"hubot": "2.13.1"
 });
 
