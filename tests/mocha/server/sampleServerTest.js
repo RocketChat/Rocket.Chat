ca6345a430a051a1707faa821b94fcc2098d24ef
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-06-28 23:22:16 -0300

    Add sample tests

---
+++ tests/mocha/server/sampleServerTest.js
@@ -0,0 +1,9 @@
+if (!(typeof MochaWeb === 'undefined')){
+  MochaWeb.testOnly(function(){
+    describe("Server initialization", function(){
+      it("should have a Meteor version defined", function(){
+        chai.assert(Meteor.release);
+      });
+    });
+  });
+}
