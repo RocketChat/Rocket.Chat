e1245532abbc04bb4da67217d2b23e2f30b86a9a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-07-07 10:10:05 -0300

    Add confirm dialog for webrtc call

--- packages/rocketchat-webrtc/webrtc.js
+++ packages/rocketchat-webrtc/webrtc.js
@@ -12,6 +12,7 @@ webrtc = {
 	stream: stream,
 	send: function(data) {
 		data.to = webrtc.to;
+		data.from = Meteor.user().username;
 		stream.emit('send', data);
 	},
 	stop: function(sendEvent) {
@@ -36,7 +37,7 @@ function onError() {
 }
 
 // run start(true) to initiate a call
-webrtc.start = function (isCaller) {
+webrtc.start = function (isCaller, fromUsername) {
 	webrtc.pc = new RTCPeerConnection(webrtc.config);
 
 	webrtc.pc.ondatachannel = function() {webrtc.log('ondatachannel', arguments)}
@@ -80,23 +81,45 @@ webrtc.start = function (isCaller) {
 		}
 	}
 
-	// get the local stream, show it in the local video element and send it
-	navigator.getUserMedia({ "audio": true, "video": true }, function (stream) {
-		webrtc.onSelfUrl(URL.createObjectURL(stream));
+	var getUserMedia = function() {
+		// get the local stream, show it in the local video element and send it
+		navigator.getUserMedia({ "audio": true, "video": true }, function (stream) {
+			webrtc.onSelfUrl(URL.createObjectURL(stream));
 
-		webrtc.pc.addStream(stream);
+			webrtc.pc.addStream(stream);
 
-		if (isCaller) {
-			webrtc.pc.createOffer(gotDescription, onError);
-		} else {
-			webrtc.pc.createAnswer(gotDescription, onError);
-		}
+			if (isCaller) {
+				webrtc.pc.createOffer(gotDescription, onError);
+			} else {
+				webrtc.pc.createAnswer(gotDescription, onError);
+			}
 
-		function gotDescription(desc) {
-			webrtc.pc.setLocalDescription(desc, function() {}, onError);
-			webrtc.send({ "sdp": desc.toJSON(), cid: webrtc.cid });
-		}
-	}, function() {});
+			function gotDescription(desc) {
+				webrtc.pc.setLocalDescription(desc, function() {}, onError);
+				webrtc.send({ "sdp": desc.toJSON(), cid: webrtc.cid });
+			}
+		}, function() {});
+	}
+
+	if (isCaller) {
+		getUserMedia();
+	} else {
+		swal({
+			title: "Video call from "+fromUsername,
+			text: "Do you want to accept?",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Yes",
+			cancelButtonText: "No"
+		}, function(isConfirm){
+			if (isConfirm) {
+				getUserMedia();
+			} else {
+				webrtc.stop();
+			}
+		});
+	}
 }
 
 stream.on(Meteor.userId(), function(data) {
@@ -107,7 +130,7 @@ stream.on(Meteor.userId(), function(data) {
 	}
 
 	if (!webrtc.pc)
-		webrtc.start(false);
+		webrtc.start(false, data.from);
 
 	if (data.sdp) {
 		webrtc.pc.setRemoteDescription(new RTCSessionDescription(data.sdp));
