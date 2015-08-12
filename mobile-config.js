8604a74ded33c471bd7578edaf204591a05b7b4d
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-08-12 01:04:51 -0300

    Back base version of mobile to 0.0.2

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@
 // the entire section is optional.
 App.info({
 	id: 'com.konecty.rocket.chat',
-	version: '0.1.1',
+	version: '0.0.2',
 	name: 'Rocket.Chat',
 	description: 'Rocket.Chat',
 	author: 'Rocket.Chat Development Group',
