fca9bafdb78fc0b82b80aeec13428b9852b4b364
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-08-12 02:23:58 -0300

    Set ios build to 7.0

--- build-ios.sh
+++ build-ios.sh
@@ -3,4 +3,5 @@
 rm -rf .meteor/local/cordova-build
 rm -rf ../Rocket.Chat-build
 meteor build ../Rocket.Chat-build --server https://rocket.chat
+sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET[[:space:]]=[[:space:]]6\.0/IPHONEOS_DEPLOYMENT_TARGET = 7\.0/g' .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj/project.pbxproj
 open .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj
