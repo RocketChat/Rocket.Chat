419578369b4b623e72905ef10e08571b4aca68b4
Gabriel Engel <gabriel.engel@fgsys.com>
2015-08-13 11:54:50 -0300

    moving website to a different project

--- build-ios.sh
+++ build-ios.sh
@@ -2,6 +2,6 @@
 
 rm -rf .meteor/local/cordova-build
 rm -rf ../Rocket.Chat-build
-meteor build ../Rocket.Chat-build --server https://rocket.chat
+meteor build ../Rocket.Chat-build --server https://demo.rocket.chat
 sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET[[:space:]]=[[:space:]]6\.0/IPHONEOS_DEPLOYMENT_TARGET = 7\.0/g' .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj/project.pbxproj
 open .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj
