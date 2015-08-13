5f7c2264936950c8011b72b3f5b880e7f53773f0
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-08-13 18:04:42 -0300

    Fix path of come commands in build-ios.sh

--- build-ios.sh
+++ build-ios.sh
@@ -6,14 +6,14 @@ meteor build ../Rocket.Chat-build --server https://demo.rocket.chat
 sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET[[:space:]]=[[:space:]]6\.0/IPHONEOS_DEPLOYMENT_TARGET = 8\.0/g' .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj/project.pbxproj
 
 # Set some plist attr for new xcode compile
-/usr/libexec/PlistBuddy -c "Add :NSAppTransportSecurity dict" Rocket.Chat-Info.plist
-/usr/libexec/PlistBuddy -c "Add :NSAppTransportSecurity:NSAllowsArbitraryLoads bool YES" Rocket.Chat-Info.plist
-/usr/libexec/PlistBuddy -c "Add :LSApplicationQueriesSchemes array" Rocket.Chat-Info.plist
-/usr/libexec/PlistBuddy -c "Add :LSApplicationQueriesSchemes:0 string 'fbauth'" Rocket.Chat-Info.plist
+/usr/libexec/PlistBuddy -c "Add :NSAppTransportSecurity dict" .meteor/local/cordova-build/platforms/ios/Rocket.Chat/Rocket.Chat-Info.plist
+/usr/libexec/PlistBuddy -c "Add :NSAppTransportSecurity:NSAllowsArbitraryLoads bool YES" .meteor/local/cordova-build/platforms/ios/Rocket.Chat/Rocket.Chat-Info.plist
+/usr/libexec/PlistBuddy -c "Add :LSApplicationQueriesSchemes array" .meteor/local/cordova-build/platforms/ios/Rocket.Chat/Rocket.Chat-Info.plist
+/usr/libexec/PlistBuddy -c "Add :LSApplicationQueriesSchemes:0 string 'fbauth'" .meteor/local/cordova-build/platforms/ios/Rocket.Chat/Rocket.Chat-Info.plist
 
 # Set buildnumber as cur date in seconds
 DATE=$(date +%s)
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $DATE" Rocket.Chat-Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $DATE" .meteor/local/cordova-build/platforms/ios/Rocket.Chat/Rocket.Chat-Info.plist
 
 open .meteor/local/cordova-build/platforms/ios/Rocket.Chat.xcodeproj
 echo '- Change provisioning profiles'
