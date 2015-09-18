8dedb22795faa1637a2bdb6f29432dc623d80d95
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-09-18 17:50:52 -0300

    Replace all ChatMessage.findOne

--- packages/rocketchat-lib/package.js
+++ packages/rocketchat-lib/package.js
@@ -71,6 +71,7 @@ Package.onUse(function(api) {
 	api.addFiles('server/models/Users.coffee', 'server');
 	api.addFiles('server/models/Subscriptions.coffee', 'server');
 	api.addFiles('server/models/Rooms.coffee', 'server');
+	api.addFiles('server/models/Messages.coffee', 'server');
 
 	// TAPi18n -- needs to be added last
 	api.addFiles(tapi18nFiles, ["client", "server"]);
