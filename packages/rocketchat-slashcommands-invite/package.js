9cdeb6b54e6234c1bae3019dcf0e568d971154e6
Marcelo Schmidt <marcelo.schmidt@konecty.com>
2015-10-02 13:26:10 -0300

    Added language files

--- packages/rocketchat-slashcommands-invite/package.js
+++ packages/rocketchat-slashcommands-invite/package.js
@@ -15,6 +15,19 @@ Package.onUse(function(api) {
 	]);
 
 	api.addFiles('invite.coffee');
+
+	// TAPi18n
+	api.use('templating', 'client');
+	var _ = Npm.require('underscore');
+	var fs = Npm.require('fs');
+	tapi18nFiles = _.compact(_.map(fs.readdirSync('packages/rocketchat-slashcommands-invite/i18n'), function(filename) {
+		if (fs.statSync('packages/rocketchat-slashcommands-invite/i18n/' + filename).size > 16) {
+			return 'i18n/' + filename;
+		}
+	}));
+	api.use('tap:i18n@1.6.1', ['client', 'server']);
+	api.imply('tap:i18n');
+	api.addFiles(tapi18nFiles, ['client', 'server']);
 });
 
 Package.onTest(function(api) {
