0d20322b57f0d39e7f89749e8f6fb7fbf5f6e5c6
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-10-05 14:46:10 -0300

    Move all styles to inside the package

---
+++ packages/rocketchat-theme-colors/client.coffee
@@ -0,0 +1,9 @@
+Meteor.startup ->
+	el = $('#theme-colors')[0]
+
+	connected = Meteor.status().connected
+	Tracker.autorun ->
+		if connected is false and Meteor.status().connected is true
+			el.href = el.href.replace(/\?.*$/, '') + '?_dc=' + Random.id()
+			console.log el.href
+		connected = Meteor.status().connected
--- packages/rocketchat-theme-colors/package.js
+++ packages/rocketchat-theme-colors/package.js
@@ -10,11 +10,41 @@ Package.onUse(function(api) {
 
 	api.use('coffeescript');
 	api.use('webapp');
+	api.use('less@2.5.0_3');
 
+	api.addFiles('variables.coffee', 'server');
 	api.addFiles('server.coffee', 'server');
+	api.addFiles('client.coffee', 'client');
 
-	api.addFiles('assets/lesshat.import.less', 'server', {isAsset: true});
-	api.addFiles('assets/colors.less', 'server', {isAsset: true});
+	// api.addFiles('assets/stylesheets/global/_variables.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_colors.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_emojione.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_fonts.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_keyframes.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_lesshat.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_preloader.import.less', 'client');
+	// api.addFiles('assets/stylesheets/utils/_reset.import.less', 'client');
+	// api.addFiles('assets/stylesheets/animation.css', 'client');
+	// api.addFiles('assets/stylesheets/base.less', 'client');
+	// api.addFiles('assets/stylesheets/fontello.css', 'client');
+	// api.addFiles('assets/stylesheets/rtl.less', 'client');
+	// api.addFiles('assets/stylesheets/swipebox.min.css', 'client');
+
+	api.addAssets('assets/stylesheets/global/_variables.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_colors.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_emojione.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_fonts.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_keyframes.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_lesshat.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_preloader.import.less', 'server');
+	api.addAssets('assets/stylesheets/utils/_reset.import.less', 'server');
+	api.addAssets('assets/stylesheets/animation.css', 'server');
+	api.addAssets('assets/stylesheets/base.less', 'server');
+	api.addAssets('assets/stylesheets/fontello.css', 'server');
+	api.addAssets('assets/stylesheets/rtl.less', 'server');
+	api.addAssets('assets/stylesheets/swipebox.min.css', 'server');
+
+	api.addAssets('assets/colors.less', 'server');
 
 	api.export('less', 'server');
 	api.export('getText', 'server');
