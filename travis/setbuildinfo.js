e9e9179da6493ce89db124be3b3c5b37ab41d8e7
S. Li <sli@makawave.com>
2015-10-11 01:53:42 -0500

    add buildinfo and revision number support

---
+++ travis/setbuildinfo.js
@@ -0,0 +1,43 @@
+var BUILD_INFO_PATH = '../public/buildinfo/buildinfo.txt';
+var PACKAGES_PATH = '../.meteor/packages';
+var BUILD_PATH = '../../build';
+var LineByLineReader = require('line-by-line'),
+    mkdirp = require('mkdirp'),
+	fs = require('fs'),
+    lr = new LineByLineReader(BUILD_INFO_PATH);
+
+var firstline = "";
+
+
+if (process.env.TRAVIS_JOB_ID)  {
+
+	var transformVersion = function (firstline) {
+	 var versions = firstline.split(".");
+
+	 return versions[0] + '.' + versions[1] + '.' + process.env.TRAVIS_JOB_ID + '\n';
+	};
+
+
+
+	lr.on('error', function (err) {
+    	// 'err' contains error object
+	});
+
+	lr.on('line', function (line) {
+  		if (firstline == "")
+    		firstline = line;
+
+	});
+
+	lr.on('end', function () {
+
+	     var packages = fs.readFileSync(PACKAGES_PATH);
+	     var verinfo = transformVersion(firstline);
+	     var content = verinfo + packages;
+	     mkdirp.sync(BUILD_PATH);
+	     fs.writeFileSync(BUILD_PATH + "/version.txt", verinfo);
+	     fs.writeFileSync(BUILD_INFO_PATH, content);
+   
+	});
+
+}
