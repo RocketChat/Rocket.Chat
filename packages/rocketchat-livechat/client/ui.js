1d80dfbca25a82f8a9669b5f3f1eb5a25243a359
Diego Sampaio <chinello@gmail.com>
2015-10-23 15:10:54 +0200

    fixing livechat route

--- packages/rocketchat-livechat/client/ui.js
+++ packages/rocketchat-livechat/client/ui.js
@@ -1,4 +1,9 @@
 Meteor.startup(function() {
-	RocketChat.roomTypes.add('livechat', ['livechat-agent', 'livechat-manager']);
+	RocketChat.roomTypes.addType('livechat', ['livechat-agent', 'livechat-manager']);
+	RocketChat.roomTypes.setRoute('l', 'live', function(sub) {
+		console.log('livechat route ->',sub);
+		return { name: sub.name };
+	});
+
 	AccountBox.addOption({ name: 'Livechat', icon: 'icon-chat-empty', class: 'livechat-manager', roles: ['livechat-manager'] });
 });
