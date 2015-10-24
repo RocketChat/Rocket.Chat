f1df23b7d71174ee66333343af1a6812ea500c4d
Gabriel Engel <gabriel.engel@fgsys.com>
2015-10-24 13:03:04 +0200

    removed temp package after merging with original

--- packages/rocketchat-webrtc-ib/adapter.js
+++
@@ -1,5 +0,0 @@
-window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
-window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
