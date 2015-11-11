4e1da3fa8d920e6b604c51bbf74a0cfd21e0cd1c
Diego Sampaio <chinello@gmail.com>
2015-11-11 18:01:03 -0200

    room icons

--- packages/rocketchat-livechat/client/ui.js
+++ packages/rocketchat-livechat/client/ui.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
 	RocketChat.roomTypes.addType('livechat', ['livechat-agent', 'livechat-manager']);
+	RocketChat.roomTypes.setIcon('l', 'icon-chat-empty');
 	RocketChat.roomTypes.setRoute('l', 'live', function(sub) {
-		console.log('livechat route ->',sub);
 		return { name: sub.name };
 	});
 
