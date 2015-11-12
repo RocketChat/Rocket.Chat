a8040a38613013db9398c2644d00d30612a0c05f
Diego Sampaio <chinello@gmail.com>
2015-11-12 00:01:21 -0200

    use correct settings for livechat app

--- packages/rocketchat-livechat/livechat.js
+++ packages/rocketchat-livechat/livechat.js
@@ -10,14 +10,7 @@ WebApp.connectHandlers.use('/livechat/', (req, res, next) => {
 		<head>
 			<link rel="stylesheet" type="text/css" class="__meteor-css__" href="/packages/rocketchat_livechat/public/livechat.css?_dc=${Autoupdate.autoupdateVersion}">
 			<script type="text/javascript">
-				__meteor_runtime_config__ = {
-					"meteorRelease": "METEOR@1.1.0.2",
-					"ROOT_URL": "${Meteor.absoluteUrl()}",
-					"ROOT_URL_PATH_PREFIX": "",
-					"autoupdateVersion": "${Autoupdate.autoupdateVersion}",
-					"autoupdateVersionRefreshable": "${Autoupdate.autoupdateVersionRefreshable}",
-					"autoupdateVersionCordova": "${Autoupdate.autoupdateVersionCordova}"
-				};
+				__meteor_runtime_config__ = ${JSON.stringify(__meteor_runtime_config__)};
 			</script>
 			<script type="text/javascript" src="/packages/rocketchat_livechat/public/livechat.js?_dc=${Autoupdate.autoupdateVersion}"></script>
 
