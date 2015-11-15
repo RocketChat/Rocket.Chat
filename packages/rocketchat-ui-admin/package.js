6c7a9814f04492d109cd6f1b76be30ab3347adaa
Rafael Caferati <rafael@caferati.me>
2015-11-15 22:56:32 +0100

    implemented rocketchat-ui-flextab, rocketchat-ui-master and renamed ui-message package

--- packages/rocketchat-ui-admin/package.js
+++ packages/rocketchat-ui-admin/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'rocketchat:rocketchat-ui-admin',
-	version: '0.0.1',
+	version: '0.1.0',
 	// Brief, one-line summary of the package.
 	summary: '',
 	// URL to the Git repository containing the source code for this package.
