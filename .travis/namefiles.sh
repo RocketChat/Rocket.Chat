f72141c24ae673fde929fe4759d004b16cfe6ac9
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-01 06:57:40 -0200

    minor typo fixes

--- .travis/namefiles.sh
+++ .travis/namefiles.sh
@@ -1,5 +1,4 @@
 cd ../../build
-FILENAME=rocket.chat-`git describe --abbrev=0 --tags`."$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH".tgz
-mv Rocket.Chat.tar.gz  "$FILENAME"
+FILENAME=rocket.chat-"$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH".tgz
+smv Rocket.Chat.tar.gz  "$FILENAME"
 ln -s  "$FILENAME" "$TRAVIS_BRANCH.rocket.chat-v.latest.tgz"
-
