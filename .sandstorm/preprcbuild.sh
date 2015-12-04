8d8881ab56b1a323b3437844196f23afd3d37cf7
SingLi <Sing-Li@users.noreply.github.com>
2015-12-04 06:53:10 -0500

    fix typo

--- .sandstorm/preprcbuild.sh
+++ .sandstorm/preprcbuild.sh
@@ -16,4 +16,5 @@ ln -s meteor-spk-0.1.8 meteor-spk
 cp -a /bin/bash /opt/meteor-spk/meteor-spk.deps/bin/
 cp -a /lib/x86_64-linux-gnu/libncurses.so.* /opt/meteor-spk/meteor-spk.deps/lib/x86_64-linux-gnu/
 cp -a /lib/x86_64-linux-gnu/libtinfo.so.* /opt/meteor-spk/meteor-spk.deps/lib/x86_64-linux-gnu/
+cd $TRAVIS_BUILD_DIR
 cp -r . /opt/app
