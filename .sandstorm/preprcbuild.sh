f2ce52195498ef9cbbf1f742545a4784d3e25697
S. Li <sli@makawave.com>
2015-12-04 07:27:43 -0500

    fix PATH env var

--- .sandstorm/preprcbuild.sh
+++ .sandstorm/preprcbuild.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-export SANDSTORM_VERSION=$(curl -f "https://install.sandstorm.io/dev?from=0&type=install")
 cd /tmp
 curl https://dl.sandstorm.io/sandstorm-$SANDSTORM_VERSION.tar.xz | tar -xJf -
-export PATH=$PATH:${PWD}/sandstorm-$SANDSTORM_VERSION/bin
 sudo mkdir -p /home/vagrant
 sudo chown -R travis /home/vagrant
 sudo mkdir -p /opt
