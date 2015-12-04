a17b982098938a38651b387a60dd6d4f99a21e96
SingLi <Sing-Li@users.noreply.github.com>
2015-12-04 05:57:16 -0500

    fix buildrc.sh
    escape subst

--- .sandstorm/buildrc.sh
+++ .sandstorm/buildrc.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 spk init -p3000 -- nothing
-export SANDSTORM_ID=$(grep '\sid =' sandstorm-pkgdef.capnp)
+export SANDSTORM_ID="$(grep '\sid =' sandstorm-pkgdef.capnp)"
 cd $TRAVIS_BUILD_DIR
 export METEOR_WAREHOUSE_DIR="${METEOR_WAREHOUSE_DIR:-$HOME/.meteor}"
 export METEOR_DEV_BUNDLE=$(dirname $(readlink -f "$METEOR_WAREHOUSE_DIR/meteor"))/dev_bundle
