8366b234e7c4c965af6d48ace84e54718bc1ba12
Diego Sampaio <chinello@gmail.com>
2015-12-04 17:23:37 -0200

    cleanup

--- packages/rocketchat-livechat/client/ui.js
+++ packages/rocketchat-livechat/client/ui.js
@@ -17,25 +17,6 @@ RocketChat.roomTypes.add('l', 5, {
 	permissions: ['view-l-room']
 });
 
-// AccountBox.addItem({
-// 	name: 'Livechat',
-// 	icon: 'icon-chat-empty',
-// 	class: 'livechat-manager',
-// 	route: {
-// 		name: 'livechat-manager',
-// 		path: '/livechat-manager',
-// 		action(params, queryParams) {
-// 			Session.set('openedRoom');
-// 			BlazeLayout.render('main', {
-// 				center: 'pageContainer',
-// 				pageTitle: t('Livechat_Manager'),
-// 				pageTemplate: 'livechatManager'
-// 			});
-// 		}
-// 	},
-// 	permissions: ['view-livechat-manager']
-// });
-
 AccountBox.addItem({
 	name: 'Livechat',
 	icon: 'icon-chat-empty',
