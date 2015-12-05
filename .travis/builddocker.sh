1dbf8cb7a925a24d7bd7ed76a060e4a0a255f5f8
S. Li <sli@makawave.com>
2015-12-04 23:06:17 -0500

    retrigger docker builds

--- .travis/builddocker.sh
+++ .travis/builddocker.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 
-set -euo pipefail
+set -eu
 
 if [ -z ${TRAVIS_TAG+x} ];
-then "\"curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Branch', 'source_name': '$TRAVIS_BRANCH', 'docker_tag': '$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/\"";
-else "\"curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Tag', 'source_name': '$TRAVIS_TAG', 'docker_tag': '$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/\"";
+then "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Branch', 'source_name': '$TRAVIS_BRANCH', 'docker_tag': '$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
+else "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Tag', 'source_name': '$TRAVIS_TAG', 'docker_tag': '$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
 fi
 
