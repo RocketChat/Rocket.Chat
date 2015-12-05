073df1ae450e3a1cf8ffa610d1deccaafaaecda6
S. Li <sli@makawave.com>
2015-12-04 21:31:39 -0500

    versions.txt has been removed, fix tag to restore docker image builds

--- .travis/builddocker.sh
+++ .travis/builddocker.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
 if [ -z ${TRAVIS_TAG+x} ];
-then "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Branch', 'source_name': \'$TRAVIS_BRANCH\', 'docker_tag': \'`cat ../../build/version.txt`\'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
-else "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Tag', 'source_name': \'$TRAVIS_TAG\', 'docker_tag': \'`cat ../../build/version.txt`\'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
+then "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Branch', 'source_name': \'$TRAVIS_BRANCH\', 'docker_tag': \'$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH\'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
+else "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Tag', 'source_name': \'$TRAVIS_TAG\', 'docker_tag': \'$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH\'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
 fi
 
