2048fc5c04fa003fa64728a2b8e81f519cfff210
SingLi <Sing-Li@users.noreply.github.com>
2015-12-04 21:49:28 -0500

    fix unsupported option

--- .travis/builddocker.sh
+++ .travis/builddocker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
-
+set -eu
 
 if [ -z ${TRAVIS_TAG+x} ];
 then "curl -H \"Content-Type: application/json\" --data \"{'source_type': 'Branch', 'source_name': \'$TRAVIS_BRANCH\', 'docker_tag': \'$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH\'}\" -X POST https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/";
