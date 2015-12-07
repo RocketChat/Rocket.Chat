1ad3feafddc87bf79a1bb4cfd96740a7546527e8
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-07 21:41:38 -0200

    simplifying deploy scripts

--- .travis/namefiles.sh
+++ .travis/namefiles.sh
@@ -1,4 +1,4 @@
-cd ../../build
-FILENAME=rocket.chat-"$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH".tgz
-mv Rocket.Chat.tar.gz  "$FILENAME"
-ln -s  "$FILENAME" "$TRAVIS_BRANCH.rocket.chat-v.latest.tgz"
+cd $TRAVIS_BUILD_DIR
+export TAG=$(git describe --abbrev=0 --tags)
+ln -s /tmp/build/Rocket.Chat.tar.gz "/tmp/deploy/$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH.tgz"
+ln -s /tmp/build/Rocket.Chat.tar.gz "/tmp/deploy/$TRAVIS_BRANCH.rocket.chat-v.latest.tgz"
