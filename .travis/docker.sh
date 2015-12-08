b1dee3a51d2dabc9bd64a276a515c53bac1432c4
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-08 01:05:02 -0200

    remove spaces

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -5,13 +5,13 @@ IFS=$'\n\t'
 CURL_HEADER="Content-Type: application/json"
 CURL_URL="https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/"
 
-if [ "$TRAVIS_TAG" ]; then
-  CURL_DATA='{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'", "docker_tag": "'$TRAVIS_TAG'"}';
+if ["$TRAVIS_TAG" ]; then
+  CURL_DATA='{"source_type":"Tag","source_name":"'$TRAVIS_TAG'","docker_tag":"'$TRAVIS_TAG'"}';
 else
-  if [ "$TRAVIS_BRANCH" = "master" ]; then
-    CURL_DATA='{"source_type": "Branch", "source_name": "master", "docker_tag": "latest"}';
+  if ["$TRAVIS_BRANCH" ="master" ]; then
+    CURL_DATA='{"source_type":"Branch","source_name":"master","docker_tag":"latest"}';
   else
-    CURL_DATA='{"source_type": "Branch", "source_name": "'$TRAVIS_BRANCH'", "docker_tag": "'$TRAVIS_BRANCH'"}';
+    CURL_DATA='{"source_type":"Branch","source_name":"'$TRAVIS_BRANCH'","docker_tag":"'$TRAVIS_BRANCH'"}';
   fi
 fi
 
