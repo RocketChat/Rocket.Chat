d7e7948ddec8b8a295e4abbd8a042de780fe3fcf
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-08 00:45:03 -0200

    trying to fix docker hub triggers

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -2,22 +2,18 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# TRAVIS_TAG='v0.7'
-# TAG="v0.7"
-# TRAVIS_BUILD_NUMBER="1234"
-# TRAVIS_BRANCH="develop"
-# PUSHTOKEN="secret"
-
 CURL_HEADER="Content-Type: application/json"
 CURL_URL="https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/"
 
-if [ -z ${TRAVIS_TAG+x} ];
-then
-CURL_DATA='{"source_type": "Branch", "source_name": "'$TRAVIS_BRANCH'", "docker_tag": "'$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH'"}';
+if [ "$TRAVIS_TAG" ]; then
+  CURL_DATA='{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'", "docker_tag": "'$TRAVIS_TAG'"}';
 else
-CURL_DATA='{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'", "docker_tag": "'$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH.$TRAVIS_TAG'"}';
+  if [ "$TRAVIS_BRANCH" = "master" ]; then
+    CURL_DATA='{"source_type": "Branch", "source_name": "master", "docker_tag": "latest"}';
+  else
+    CURL_DATA='{"source_type": "Branch", "source_name": "'$TRAVIS_BRANCH'", "docker_tag": "'$TRAVIS_BRANCH'"}';
+  fi
 fi
 
-curl -H "${CURL_HEADER}" --data "${CURL_DATA}" -X POST "${CURL_URL}"
-
-
+echo -H "${CURL_HEADER}" --data "${CURL_DATA}" -X POST $CURL_URL
+curl -H "${CURL_HEADER}" --data "${CURL_DATA}" -X POST $CURL_URL
