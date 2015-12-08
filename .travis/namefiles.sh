98aa6f92f587b6b0ec2bc25364abd371c913cd3a
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-07 22:11:45 -0200

    removing sh

--- .travis/namefiles.sh
+++ .travis/namefiles.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 cd $TRAVIS_BUILD_DIR
 export TAG=$(git describe --abbrev=0 --tags)
 ln -s /tmp/build/Rocket.Chat.tar.gz "/tmp/deploy/$TAG.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH.tgz"
