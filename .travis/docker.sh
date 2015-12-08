956a124942e243cbef032fd94bdb389a29847a97
Gabriel Engel <gabriel.engel@fgsys.com>
2015-12-08 01:16:16 -0200

    trying to fix curl

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -6,13 +6,14 @@ CURL_HEADER="Content-Type: application/json"
 CURL_URL="https://registry.hub.docker.com/u/rocketchat/rocket.chat/trigger/$PUSHTOKEN/"
 
 if ["$TRAVIS_TAG" ]; then
-  CURL_DATA='{"source_type":"Tag","source_name":"'$TRAVIS_TAG'","docker_tag":"'$TRAVIS_TAG'"}';
+  CURL_DATA='{"source_type":"Tag","source_name":"$TRAVIS_TAG","docker_tag":"$TRAVIS_TAG"}';
 else
   if ["$TRAVIS_BRANCH" ="master" ]; then
     CURL_DATA='{"source_type":"Branch","source_name":"master","docker_tag":"latest"}';
   else
-    CURL_DATA='{"source_type":"Branch","source_name":"'$TRAVIS_BRANCH'","docker_tag":"'$TRAVIS_BRANCH'"}';
+    CURL_DATA='{"source_type":"Branch","source_name":"$TRAVIS_BRANCH","docker_tag":"$TRAVIS_BRANCH"}';
   fi
 fi
 
+echo -H '"'$CURL_HEADER'"' --data "'"$CURL_DATA"'" -X POST CURL_URL
 curl -H '"'$CURL_HEADER'"' --data "'"$CURL_DATA"'" -X POST $CURL_URL
