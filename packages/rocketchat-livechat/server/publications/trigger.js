0ed5e60955a88ef70189e24d9ccbd61fef1eb8d1
Diego Sampaio <chinello@gmail.com>
2015-12-15 10:09:09 -0200

    code cleanup

--- packages/rocketchat-livechat/server/publications/trigger.js
+++ packages/rocketchat-livechat/server/publications/trigger.js
@@ -1,13 +1,3 @@
 Meteor.publish('livechat:trigger', function() {
-	// if (!this.userId) {
-	// 	throw new Meteor.Error('not-authorized');
-	// }
-
-	// if (!RocketChat.authz.hasPermission(this.userId, 'view-livechat-manager')) {
-	// 	throw new Meteor.Error('not-authorized');
-	// }
-
-	// console.log('[publish] livechat:trigger -> '.green, 'arguments:', arguments);
-
 	return RocketChat.models.LivechatTrigger.find();
 });
