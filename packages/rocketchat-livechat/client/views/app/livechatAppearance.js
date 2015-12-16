1f0b4bc92279adcdc0d75085aec22ed4c198f8a5
Diego Sampaio <chinello@gmail.com>
2015-12-16 12:08:55 -0200

    livechat appearance preview

--- packages/rocketchat-livechat/client/views/app/livechatAppearance.js
+++ packages/rocketchat-livechat/client/views/app/livechatAppearance.js
@@ -1,8 +1,11 @@
 Template.livechatAppearance.helpers({
+	previewState () {
+		return Template.instance().previewState.get();
+	},
 	sampleData () {
 		return {
 			color: RocketChat.settings.get('Livechat_title_color'),
-			title: 'TITLE',
+			title: RocketChat.settings.get('Livechat_title'),
 			messages: [
 				{
 					_id: Random.id(),
@@ -11,9 +14,70 @@ Template.livechatAppearance.helpers({
 					},
 					time: moment(this.ts).format('HH:mm'),
 					date: moment(this.ts).format('LL'),
-					body: 'Testing'
+					body: 'Hello',
+					sequential: null
+				},
+				{
+					_id: Random.id(),
+					u: {
+						username: 'rocketchat-agent'
+					},
+					time: moment(this.ts).format('HH:mm'),
+					date: moment(this.ts).format('LL'),
+					body: 'Hey, what can I help you with?',
+					sequential: null
+				},
+				{
+					_id: Random.id(),
+					u: {
+						username: 'guest'
+					},
+					time: moment(this.ts).format('HH:mm'),
+					date: moment(this.ts).format('LL'),
+					body: 'I\'m looking for informations about your product.',
+					sequential: null
+				},
+				{
+					_id: Random.id(),
+					u: {
+						username: 'rocketchat-agent'
+					},
+					time: moment(this.ts).format('HH:mm'),
+					date: moment(this.ts).format('LL'),
+					body: 'Our product is open source, you can do what you want with it! =D',
+					sequential: null
+				},
+				{
+					_id: Random.id(),
+					u: {
+						username: 'guest'
+					},
+					time: moment(this.ts).format('HH:mm'),
+					date: moment(this.ts).format('LL'),
+					body: 'Yay, thanks. That\'s awesome.',
+					sequential: null
+				},
+				{
+					_id: Random.id(),
+					u: {
+						username: 'rocketchat-agent'
+					},
+					time: moment(this.ts).format('HH:mm'),
+					date: moment(this.ts).format('LL'),
+					body: 'You\'re welcome.',
+					sequential: null
 				}
 			]
 		};
 	}
 });
+
+Template.livechatAppearance.onCreated(function() {
+	this.previewState = new ReactiveVar('opened');
+});
+
+Template.livechatAppearance.events({
+	'change .preview-mode' (e, instance) {
+		instance.previewState.set(e.currentTarget.value);
+	}
+})
