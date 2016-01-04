138c0c3d3960d06404ea687f3122969a8890fa2c
Diego Sampaio <chinello@gmail.com>
2016-01-04 18:00:31 -0200

    show correct info from visitor

--- packages/rocketchat-livechat/client/views/app/tabbar/visitorInfo.js
+++ packages/rocketchat-livechat/client/views/app/tabbar/visitorInfo.js
@@ -1,23 +1,25 @@
 Template.visitorInfo.helpers({
 	user() {
-		var user = Meteor.users.findOne({ username: 'guest-54' });
-		// user.ip = user.ip;
+		var room = ChatRoom.findOne(Session.get('openedRoom'));
 
-		if (user && user.userAgent) {
-			var ua = new UAParser();
-			ua.setUA(user.userAgent);
+		if (room && room.v && room.v.token) {
+			var user = Meteor.users.findOne({ "profile.token": room.v.token });
+			if (user && user.userAgent) {
+				var ua = new UAParser();
+				ua.setUA(user.userAgent);
 
-			user.os = ua.getOS().name + ' ' + ua.getOS().version;
-			if (['Mac OS', 'iOS'].indexOf(ua.getOS().name) !== -1) {
-				user.osIcon = 'icon-apple';
-			} else {
-				user.osIcon = 'icon-' +  ua.getOS().name.toLowerCase();
+				user.os = ua.getOS().name + ' ' + ua.getOS().version;
+				if (['Mac OS', 'iOS'].indexOf(ua.getOS().name) !== -1) {
+					user.osIcon = 'icon-apple';
+				} else {
+					user.osIcon = 'icon-' +  ua.getOS().name.toLowerCase();
+				}
+				user.browser = ua.getBrowser().name + ' ' + ua.getBrowser().version;
+				user.browserIcon = 'icon-' + ua.getBrowser().name.toLowerCase();
 			}
-			user.browser = ua.getBrowser().name + ' ' + ua.getBrowser().version;
-			user.browserIcon = 'icon-' + ua.getBrowser().name.toLowerCase();
-		}
 
-		return user;
+			return user;
+		}
 	}
 });
 
