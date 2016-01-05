66b51cbc450bc998c0143c590e4bc23b0f1b81ab
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-01-05 16:58:13 -0200

    Init style of oauth popup

--- packages/rocketchat-oauth2-server-config/package.js
+++ packages/rocketchat-oauth2-server-config/package.js
@@ -9,6 +9,8 @@ Package.onUse(function(api) {
 
 	api.use('webapp');
 	api.use('coffeescript');
+	api.use('rocketchat:lib@0.0.1');
+	api.use('rocketchat:theme');
 	api.use('rocketchat:oauth2-server');
 
 	api.use('templating', 'client');
@@ -18,4 +20,7 @@ Package.onUse(function(api) {
 
 	api.addFiles('client/oauth2-client.html', 'client');
 	api.addFiles('client/oauth2-client.coffee', 'client');
+
+	api.addAssets('client/stylesheets/oauth2.less', 'server');
+	api.addFiles('client/stylesheets/load.coffee', 'server');
 });
