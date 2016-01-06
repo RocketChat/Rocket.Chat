2c379d34e2e0e2247f35d07360cc6b866e644b19
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-01-06 19:39:04 -0200

    Fix some package dependencies

--- packages/rocketchat-ui-sidenav/package.js
+++ packages/rocketchat-ui-sidenav/package.js
@@ -18,7 +18,8 @@ Package.onUse(function(api) {
 		'templating',
 		'coffeescript',
 		'underscore',
-		'rocketchat:lib@0.0.1'
+		'rocketchat:lib@0.0.1',
+		'rocketchat:ui'
 	]);
 
 	api.addFiles('side-nav/accountBox.html', 'client');
