8643c83505cbae7cbe3205945ab74e2fa4245301
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-01-30 21:21:33 -0200

    Create script to build livechat on windows

--- packages/rocketchat-livechat/plugin/build-livechat.js
+++ packages/rocketchat-livechat/plugin/build-livechat.js
@@ -2,8 +2,12 @@ var fs = Npm.require('fs');
 var path = Npm.require('path');
 var shell = Npm.require('shelljs');
 
-var curPath = path.resolve('.')
+var curPath = path.resolve('.');
 var packagePath = path.join(path.resolve('.'), 'packages', 'rocketchat-livechat');
 var pluginPath = path.join(packagePath, 'plugin');
 
-shell.exec('sh '+pluginPath+'/build.sh');
+if (process.platform === 'win32') {
+	shell.exec(pluginPath+'/build.bat');
+} else {
+	shell.exec('sh '+pluginPath+'/build.sh');
+}
