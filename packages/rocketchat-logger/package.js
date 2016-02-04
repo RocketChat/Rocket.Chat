dde78bace0374f3c64dd3076bbf8791d584ed55a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-02-04 19:51:12 -0200

    Init view to show server logs on client side

--- packages/rocketchat-logger/package.js
+++ packages/rocketchat-logger/package.js
@@ -10,13 +10,19 @@ Package.onUse(function(api) {
 
 	api.use('coffeescript');
 	api.use('underscore');
+	api.use('random');
 	api.use('logging');
 	api.use('nooitaf:colors');
 	api.use('raix:eventemitter');
-	api.use('templating', 'client', {weak: true});
+	api.use('templating', 'client');
+	api.use('kadira:flow-router', 'client');
 
 	api.addFiles('ansispan.js', 'client');
 	api.addFiles('logger.coffee', 'client');
+	api.addFiles('client/viewLogs.coffee', 'client');
+	api.addFiles('client/views/viewLogs.html', 'client');
+	api.addFiles('client/views/viewLogs.coffee', 'client');
+
 	api.addFiles('server.coffee', 'server');
 
 	api.export('Logger');
