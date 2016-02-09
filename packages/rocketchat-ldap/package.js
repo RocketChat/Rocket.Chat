3c7c6c2e5ae66aa4d227b6646c773de78bfd2fae
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-02-08 22:27:30 -0200

    Remove i18n files from ldap package

--- packages/rocketchat-ldap/package.js
+++ packages/rocketchat-ldap/package.js
@@ -9,19 +9,9 @@ Npm.depends({
   ldapjs: "1.0.0",
 });
 
-// Loads all i18n.json files into tapi18nFiles
-var _ = Npm.require('underscore');
-var fs = Npm.require('fs');
-tapi18nFiles = _.compact(_.map(fs.readdirSync('packages/rocketchat-ldap/i18n'), function(filename) {
-    if (fs.statSync('packages/rocketchat-ldap/i18n/' + filename).size > 16) {
-        return 'i18n/' + filename;
-    }
-}));
-
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
 
-  // Commom
   api.use('rocketchat:logger');
   api.use('rocketchat:lib');
   api.use('tap:i18n');
@@ -29,28 +19,17 @@ Package.onUse(function(api) {
   api.use('coffeescript');
   api.use('ecmascript');
   api.use('sha');
-  // Client
+
   api.use('templating', 'client');
-  // Server
+
   api.use('accounts-base', 'server');
   api.use('accounts-password', 'server');
 
+  api.addFiles('client/loginHelper.js', 'client');
 
-  // Common
-  // TAP
-  api.addFiles('package-tap.i18n');
-
-  // Client
-  api.addFiles('ldap_client.js', 'client');
-  // Server
-  api.addFiles('ldap_server.js', 'server');
   api.addFiles('server/ldap.js', 'server');
-  api.addFiles('config_server.coffee', 'server');
-
-
-  api.addFiles(tapi18nFiles);
+  api.addFiles('server/loginHandler.js', 'server');
+  api.addFiles('server/settings.coffee', 'server');
 
   api.export('LDAP', 'server');
-  api.export('LDAP_DEFAULTS', 'server');
-  api.export('MeteorWrapperLdapjs');
 });
