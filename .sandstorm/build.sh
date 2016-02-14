c4c97041060b5301c287c84affd8f36d22e1e523
Gabriel Engel <gabriel.engel@fgsys.com>
2016-02-14 20:31:02 -0200

    rolling back to working version of sandstorm

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -7,10 +7,10 @@ set -euvo pipefail
 METEOR_WAREHOUSE_DIR="${METEOR_WAREHOUSE_DIR:-$HOME/.meteor}"
 METEOR_DEV_BUNDLE=$(dirname $(readlink -f "$METEOR_WAREHOUSE_DIR/meteor"))/dev_bundle
 
-cd ~/opt/app
-meteor build --directory ~/vagrant/
-(cd ~/vagrant/bundle/programs/server && "$METEOR_DEV_BUNDLE/bin/npm" install)
+cd /opt/app
+meteor build --directory /home/vagrant/
+(cd /home/vagrant/bundle/programs/server && "$METEOR_DEV_BUNDLE/bin/npm" install)
 
 # Copy our launcher script into the bundle so the grain can start up.
-mkdir -p ~/vagrant/bundle/opt/app/.sandstorm/
-cp ~/opt/app/.sandstorm/launcher.sh ~/vagrant/bundle/opt/app/.sandstorm/
+mkdir -p /home/vagrant/bundle/opt/app/.sandstorm/
+cp /opt/app/.sandstorm/launcher.sh /home/vagrant/bundle/opt/app/.sandstorm/
