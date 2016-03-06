fd02b426fac2eae11c536882c893f038f337f926
Diego Sampaio <chinello@gmail.com>
2016-03-05 21:40:12 -0300

    moving files to expose functions

---
+++ packages/rocketchat-lib/lib/fileUploadRestrictions.js
@@ -0,0 +1,28 @@
+RocketChat.fileUploadMediaWhiteList = function() {
+	var mediaTypeWhiteList = RocketChat.settings.get('FileUpload_MediaTypeWhiteList');
+
+	console.log('mediaTypeWhiteList ->',mediaTypeWhiteList);
+	if (!mediaTypeWhiteList) {
+		return;
+	}
+	return _.map(mediaTypeWhiteList.split(','), function(item) {
+		return item.trim();
+	});
+};
+
+RocketChat.fileUploadIsValidContentType = function(type) {
+	var list, wildCardGlob, wildcards;
+	list = RocketChat.fileUploadMediaWhiteList();
+	if (!list || _.contains(list, type)) {
+		return true;
+	} else {
+		wildCardGlob = '/*';
+		wildcards = _.filter(list, function(item) {
+			return item.indexOf(wildCardGlob) > 0;
+		});
+		if (_.contains(wildcards, type.replace(/(\/.*)$/, wildCardGlob))) {
+			return true;
+		}
+	}
+	return false;
+};
