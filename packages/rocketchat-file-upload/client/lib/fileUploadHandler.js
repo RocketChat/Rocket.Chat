abf6dbe1b4233931c221a501de68556862421f5c
Diego Sampaio <chinello@gmail.com>
2016-03-09 08:53:49 -0300

    using the name of storage type for upload objects

--- packages/rocketchat-file-upload/client/lib/fileUploadHandler.js
+++ packages/rocketchat-file-upload/client/lib/fileUploadHandler.js
@@ -1,8 +1,7 @@
 fileUploadHandler = (meta, file, data) => {
 	var storageType = RocketChat.settings.get('FileUpload_Storage_Type');
-	if (storageType === 'GridFS') {
-		return new FileUpload.GridFS(meta, file, data);
-	} else if (storageType === 'AmazonS3') {
-		return new FileUpload.S3(meta, file, data);
+
+	if (FileUpload[storageType] !== undefined) {
+		return new FileUpload[storageType](meta, file, data);
 	}
 };
