4ddf5f1793109b04bbc24ade8c49e8f2db3bce28
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-03-10 17:24:18 -0300

    New implementation of OTR

--- packages/rocketchat-otr/client/rocketchat.otr.js
+++ packages/rocketchat-otr/client/rocketchat.otr.js
@@ -42,12 +42,10 @@ Meteor.startup(function() {
 	});
 
 	RocketChat.promises.add('onClientBeforeSendMessage', function(message) {
-		if (message.rid && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].aesReady.get()) {
-			return RocketChat.OTR.instancesByRoomId[message.rid].encryptAES(message.msg)
-			.then((params) => {
-				[msg, iv] = params;
+		if (message.rid && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].established.get()) {
+			return RocketChat.OTR.instancesByRoomId[message.rid].encrypt(message.msg)
+			.then((msg) => {
 				message.msg = msg;
-				message.iv = iv;
 				return message;
 			});
 		} else {
@@ -56,8 +54,8 @@ Meteor.startup(function() {
 	}, RocketChat.promises.priority.HIGH);
 
 	RocketChat.promises.add('onClientMessageReceived', function(message) {
-		if (message.rid && message.iv && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].aesReady.get()) {
-			return RocketChat.OTR.instancesByRoomId[message.rid].decryptAES(message.msg, message.iv)
+		if (message.rid && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].established.get()) {
+			return RocketChat.OTR.instancesByRoomId[message.rid].decrypt(message.msg, message.iv)
 			.then((msg) => {
 				message.msg = msg;
 				return message;
