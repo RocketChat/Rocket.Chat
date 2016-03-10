c42f189a3c1e1a010f35cd7c3902bf063b8ee5c0
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-03-10 00:19:54 -0300

    Add OTR to startup, so it doesn't load before settings

--- packages/rocketchat-otr/client/rocketchat.otr.js
+++ packages/rocketchat-otr/client/rocketchat.otr.js
@@ -1,34 +1,34 @@
-RocketChat.OTR = new (class {
-	constructor() {
-		this.enabled = !!(window.crypto && window.crypto.subtle) && RocketChat.settings.get('OTR_Enable');
-		this.instancesByRoomId = {};
-	}
-
-	getInstanceByRoomId(roomId) {
-		var enabled, subscription;
-		subscription = ChatSubscription.findOne({
-			rid: roomId
-		});
-		if (!subscription) {
-			return;
-		}
-		enabled = false;
-		switch (subscription.t) {
-			case 'd':
-				enabled = RocketChat.settings.get('OTR_Enabled');
-				break;
-		}
-		if (enabled === false) {
-			return;
-		}
-		if (this.instancesByRoomId[roomId] == null) {
-			this.instancesByRoomId[roomId] = new RocketChat.OTR.Room(Meteor.userId(), roomId);
-		}
-		return this.instancesByRoomId[roomId];
-	}
-})();
-
 Meteor.startup(function() {
+	RocketChat.OTR = new (class {
+		constructor() {
+			this.enabled = !!(window.crypto && window.crypto.subtle) && RocketChat.settings.get('OTR_Enable');
+			this.instancesByRoomId = {};
+		}
+
+		getInstanceByRoomId(roomId) {
+			var enabled, subscription;
+			subscription = ChatSubscription.findOne({
+				rid: roomId
+			});
+			if (!subscription) {
+				return;
+			}
+			enabled = false;
+			switch (subscription.t) {
+				case 'd':
+					enabled = RocketChat.settings.get('OTR_Enabled');
+					break;
+			}
+			if (enabled === false) {
+				return;
+			}
+			if (this.instancesByRoomId[roomId] == null) {
+				this.instancesByRoomId[roomId] = new RocketChat.OTR.Room(Meteor.userId(), roomId);
+			}
+			return this.instancesByRoomId[roomId];
+		}
+	})();
+
 	RocketChat.Notifications.onUser('otr', (type, data) => {
 		if (!data.roomId || !data.userId || data.userId === Meteor.userId()) {
 			return;
@@ -36,6 +36,7 @@ Meteor.startup(function() {
 			RocketChat.OTR.getInstanceByRoomId(data.roomId).onUserStream(type, data);
 		}
 	});
+
 	RocketChat.promises.add('onClientBeforeSendMessage', function(message) {
 		if (message.rid && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].aesReady.get()) {
 			return RocketChat.OTR.instancesByRoomId[message.rid].encryptAES(message.msg)
@@ -49,7 +50,6 @@ Meteor.startup(function() {
 			return Promise.resolve(message);
 		}
 	}, RocketChat.promises.priority.HIGH);
-
 
 	RocketChat.promises.add('onClientMessageReceived', function(message) {
 		if (message.rid && message.iv && RocketChat.OTR.instancesByRoomId && RocketChat.OTR.instancesByRoomId[message.rid] && RocketChat.OTR.instancesByRoomId[message.rid].aesReady.get()) {
