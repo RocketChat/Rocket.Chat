5ce588e1ec295a8d1a9be535f931a1de0715684b
Diego Sampaio <chinello@gmail.com>
2016-03-14 13:06:03 -0300

    Better rendering of message attachment text

---
+++ packages/rocketchat-ui-message/message/renderMessageBody.js
@@ -0,0 +1,24 @@
+/* global renderMessageBody:true */
+renderMessageBody = function(msg) {
+	var message;
+
+	msg.html = msg.msg;
+
+	if (_.trim(msg.html) !== '') {
+		msg.html = _.escapeHTML(msg.html);
+	}
+
+	message = RocketChat.callbacks.run('renderMessage', msg);
+
+	if (message.tokens && message.tokens.length > 0) {
+		for (var i = 0, len = message.tokens.length; i < len; i++) {
+			let token = message.tokens[i];
+			token.text = token.text.replace(/([^\$])(\$[^\$])/gm, '$1$$$2');
+			message.html = message.html.replace(token.token, token.text);
+		}
+	}
+
+	msg.html = message.html.replace(/\n/gm, '<br/>');
+
+	return msg.html;
+};
