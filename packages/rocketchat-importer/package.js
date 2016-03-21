0d7bc9a3a1c178d07205768653057a7ca18251f4
Gabriel Engel <gabriel.engel@gmail.com>
2016-03-21 20:00:26 -0300

    code standard compliance

--- packages/rocketchat-importer/package.js
+++ packages/rocketchat-importer/package.js
@@ -52,21 +52,18 @@ Package.onUse(function(api) {
 	// TAPi18n
 	var _ = Npm.require('underscore');
 	var fs = Npm.require('fs');
-	tapi18nFiles = _.compact(_.map(fs.readdirSync('packages/rocketchat-importer/i18n'), function(filename) {
+	var tapi18nFiles = _.compact(_.map(fs.readdirSync('packages/rocketchat-importer/i18n'), function(filename) {
 		if (fs.statSync('packages/rocketchat-importer/i18n/' + filename).size > 16) {
 			return 'i18n/' + filename;
 		}
 	}));
-	api.use('tap:i18n@1.6.1', ['client', 'server']);
+	api.use('tap:i18n', ['client', 'server']);
 	api.imply('tap:i18n');
 	api.addFiles(tapi18nFiles, ['client', 'server']);
 
 	api.export('Importer');
 });
 
-Package.onTest(function(api) {
-});
-
 Npm.depends({
-	"adm-zip": "0.4.7"
+	'adm-zip': '0.4.7'
 });
