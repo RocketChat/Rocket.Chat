9c6a34c74b678531f5e02c8ce094c35f3c566df5
Diego Sampaio <chinello@gmail.com>
2016-03-22 17:09:38 -0300

    New reactions package

---
+++ packages/rocketchat-reactions/client/init.js
@@ -0,0 +1,48 @@
+Template.room.events({
+	'click .add-reaction'(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		const data = Blaze.getData(event.currentTarget);
+
+		RocketChat.EmojiPicker.open(event.currentTarget, (emoji) => {
+			Meteor.call('setReaction', ':' + emoji + ':', data._arguments[1]._id);
+		});
+	},
+
+	'click .reactions > li:not(.add-reaction)'(event) {
+		event.preventDefault();
+		const data = Blaze.getData(event.currentTarget);
+		Meteor.call('setReaction', $(event.currentTarget).data('emoji'), data._arguments[1]._id, () => {
+			RocketChat.tooltip.hide();
+		});
+	},
+
+	'mouseenter .reactions > li:not(.add-reaction)'(event) {
+		event.stopPropagation();
+		RocketChat.tooltip.showElement($(event.currentTarget).find('.people').get(0), event.currentTarget);
+	},
+
+	'mouseleave .reactions > li:not(.add-reaction)'(event) {
+		event.stopPropagation();
+		RocketChat.tooltip.hide();
+	}
+});
+
+Meteor.startup(function() {
+	RocketChat.MessageAction.addButton({
+		id: 'reaction-message',
+		icon: 'icon-people-plus',
+		i18nLabel: 'Reaction',
+		action(event) {
+			const data = Blaze.getData(event.currentTarget);
+
+			RocketChat.EmojiPicker.open(event.currentTarget, (emoji) => {
+				Meteor.call('setReaction', ':' + emoji + ':', data._arguments[1]._id);
+			});
+		},
+		validation() {
+			return true;
+		},
+		order: 11
+	});
+});
