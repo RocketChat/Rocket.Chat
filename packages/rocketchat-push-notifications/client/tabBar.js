f3d75e5180b56723bad667b401bee05a907de525
Diego Sampaio <chinello@gmail.com>
2016-03-23 14:29:12 -0300

    Fix many icon related things

--- packages/rocketchat-push-notifications/client/tabBar.js
+++ packages/rocketchat-push-notifications/client/tabBar.js
@@ -3,7 +3,7 @@ Meteor.startup(function() {
 		groups: ['channel', 'privategroup', 'directmessage'],
 		id: 'push-notifications',
 		i18nTitle: 'Push_notifications',
-		icon: 'icon-bell',
+		icon: 'icon-bell-alt',
 		template: 'pushNotificationsFlexTab',
 		order: 2
 	})
