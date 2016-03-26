3962f7bcd446a3049fc79b483e5fa90243035e7f
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-03-26 14:32:19 -0300

    More jsHint fixes

--- packages/meteor-accounts-saml/saml_client.js
+++ packages/meteor-accounts-saml/saml_client.js
@@ -5,18 +5,19 @@ if (!Accounts.saml) {
 Accounts.saml.initiateLogin = function (options, callback, dimensions) {
 	// default dimensions that worked well for facebook and google
 	var popup = openCenteredPopup(
-		Meteor.absoluteUrl("_saml/authorize/" + options.provider + "/" + options.credentialToken), (dimensions && dimensions.width) || 650, (dimensions && dimensions.height) || 500);
+		Meteor.absoluteUrl('_saml/authorize/' + options.provider + '/' + options.credentialToken), (dimensions && dimensions.width) || 650, (dimensions && dimensions.height) || 500);
 
 	var checkPopupOpen = setInterval(function () {
+		var popupClosed;
 		try {
 			// Fix for #328 - added a second test criteria (popup.closed === undefined)
 			// to humour this Android quirk:
 			// http://code.google.com/p/android/issues/detail?id=21061
-			var popupClosed = popup.closed || popup.closed === undefined;
+			popupClosed = popup.closed || popup.closed === undefined;
 		} catch (e) {
 			// For some unknown reason, IE9 (and others?) sometimes (when
-			// the popup closes too quickly?) throws "SCRIPT16386: No such
-			// interface supported" when trying to read 'popup.closed'. Try
+			// the popup closes too quickly?) throws 'SCRIPT16386: No such
+			// interface supported' when trying to read 'popup.closed'. Try
 			// again in 100ms.
 			return;
 		}
@@ -44,8 +45,9 @@ var openCenteredPopup = function (url, width, height) {
 		',left=' + left + ',top=' + top + ',scrollbars=yes');
 
 	var newwindow = window.open(url, 'Login', features);
-	if (newwindow.focus)
+	if (newwindow.focus) {
 		newwindow.focus();
+	}
 	return newwindow;
 };
 
@@ -54,7 +56,7 @@ Meteor.loginWithSaml = function (options, callback) {
 	var credentialToken = Random.id();
 	options.credentialToken = credentialToken;
 
-	Accounts.saml.initiateLogin(options, function (error, result) {
+	Accounts.saml.initiateLogin(options, function (/*error, result*/) {
 		Accounts.callLoginMethod({
 			methodArguments: [{
 				saml: true,
@@ -65,12 +67,12 @@ Meteor.loginWithSaml = function (options, callback) {
 	});
 };
 
-Meteor.logoutWithSaml = function (options, callback) {
-	//Accounts.saml.idpInitiatedSLO(options, callback); 
-	Meteor.call("samlLogout", options.provider, function (err, result) {
-		console.log("LOC " + result);
+Meteor.logoutWithSaml = function (options/*, callback*/) {
+	//Accounts.saml.idpInitiatedSLO(options, callback);
+	Meteor.call('samlLogout', options.provider, function (err, result) {
+		console.log('LOC ' + result);
 		// A nasty bounce: 'result' has the SAML LogoutRequest but we need a proper 302 to redirected from the server.
-		//window.location.replace(Meteor.absoluteUrl("_saml/sloRedirect/" + options.provider + "/?redirect="+result));
-		window.location.replace(Meteor.absoluteUrl("_saml/sloRedirect/" + options.provider + "/?redirect="+encodeURIComponent(result)));
+		//window.location.replace(Meteor.absoluteUrl('_saml/sloRedirect/' + options.provider + '/?redirect='+result));
+		window.location.replace(Meteor.absoluteUrl('_saml/sloRedirect/' + options.provider + '/?redirect='+encodeURIComponent(result)));
 	});
 };
