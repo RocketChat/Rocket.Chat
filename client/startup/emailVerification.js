251fe55757dda932c3467f20c0c2f2998eff34a8
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-03-30 17:35:39 -0300

    Fix #2634. Admins are warned if they have not verified their e-mail and e-mail verification is true.

---
+++ client/startup/emailVerification.js
@@ -0,0 +1,8 @@
+Meteor.startup(function() {
+	Tracker.autorun(function() {
+		if (Meteor.user() && Meteor.user().emails && Meteor.user().emails[0] && Meteor.user().emails[0].verified !== true && RocketChat.settings.get('Accounts_EmailVerification') === true && !Session.get('Accounts_EmailVerification_Warning')) {
+			toastr.warning(TAPi18n.__('You_have_not_verified_your_email'));
+			Session.set('Accounts_EmailVerification_Warning', true);
+		}
+	})
+});
