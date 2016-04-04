737e2ebea5f1e1c3b29d002533748c3b64e56e2c
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-04-04 17:27:25 -0300

    Fixed migration; Fix #2712

--- server/startup/migrations/v37.js
+++ server/startup/migrations/v37.js
@@ -2,13 +2,14 @@ RocketChat.Migrations.add({
 	version: 37,
 	up: function() {
 		if (RocketChat && RocketChat.models && RocketChat.models.Settings) {
-			var footer = RocketChat.models.Settings.findOne('Layout_Sidenav_Footer');
+			var footer = RocketChat.models.Settings.findOne({ _id: 'Layout_Sidenav_Footer' });
+			console.log(footer);
 
 			// Replace footer octicons with icons
 			if (footer && footer.value !== '') {
 				var footerValue = footer.value.replace('octicon octicon-pencil', 'icon-pencil');
-				footerValue = footer.replace('octicon octicon-heart', 'icon-heart');
-				footerValue = footer.replace('octicon octicon-mark-github', 'icon-github-circled');
+				footerValue = footerValue.replace('octicon octicon-heart', 'icon-heart');
+				footerValue = footerValue.replace('octicon octicon-mark-github', 'icon-github-circled');
 				RocketChat.models.Settings.update({ _id: 'Layout_Sidenav_Footer' }, { $set: { value: footerValue, packageValue: footerValue } });
 			}
 		}
