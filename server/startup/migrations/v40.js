dacc9ed04dd25b0317fbe88da08834087716a8c2
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-04-08 14:43:40 -0300

    Add option on custom oauth to send access token in headers or in payload

---
+++ server/startup/migrations/v40.js
@@ -0,0 +1,39 @@
+RocketChat.Migrations.add({
+	version: 40,
+	up: function() {
+		RocketChat.models.Settings.find({ _id: /Accounts_OAuth_Custom_/, i18nLabel: 'Accounts_OAuth_Custom_Enable' }).forEach(function(customOauth) {
+			var parts = customOauth._id.split('_');
+			var name = parts[3];
+			var id = 'Accounts_OAuth_Custom_' + name + '_token_sent_via';
+			if (!RocketChat.models.Settings.findOne( { _id: id } )) {
+				RocketChat.models.Settings.insert({
+					'_id': id,
+					'type': 'select',
+					'group': 'OAuth',
+					'section': 'Custom OAuth: ' +  name,
+					'i18nLabel': 'Accounts_OAuth_Custom_Token_Sent_Via',
+					'persistent': true,
+					'values': [
+					{
+						'key': 'header',
+						'i18nLabel': 'Header'
+					},
+					{
+						'key': 'payload',
+						'i18nLabel': 'Payload'
+					}
+					],
+					'packageValue': 'payload',
+					'valueSource': 'packageValue',
+					'ts': new Date(),
+					'hidden': false,
+					'blocked': false,
+					'sorter': 255,
+					'i18nDescription': 'Accounts_OAuth_Custom_' + name + '_token_sent_via_Description',
+					'createdAt': new Date(),
+					'value': 'payload'
+				});
+			}
+		});
+	}
+});
