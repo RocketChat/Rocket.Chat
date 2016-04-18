35971ab71134843587c01019f5d06908c14a2ada
Marcelo Schmidt <marcelo.schmidt@rocket.chat>
2016-04-18 11:25:02 -0300

    Auto-translate all languages (#2927)
    * Run each language and wait for 100s (google rate)
    
    * Auto-translate all languages
    
    * Codacy compliance

--- private/node_scripts/compare_langs.js
+++ private/node_scripts/compare_langs.js
@@ -1,9 +1,9 @@
 var async = require('async');
 var fs = require('fs');
 var _ = require('underscore');
-var googleTranslate = require('google-translate')('AIzaSyCrXsIW7UrXrx2_VDYWwo5prYk5DIo-SlM');
+var googleTranslate = require('google-translate')('insert google api key here');
 
-var path = '../rocket.chat/packages/rocketchat-lib/i18n/';
+var path = '../../packages/rocketchat-lib/i18n/';
 var enContents = fs.readFileSync(path + 'en.i18n.json', 'utf-8');
 var en = JSON.parse(enContents);
 
@@ -12,23 +12,24 @@ googleTranslate.getSupportedLanguages(function(err, langs) {
 		console.log(err);
 		return;
 	}
-	files = fs.readdirSync(path);
-	async.each(files, function(file, callback) {
-		if (file === 'en.i18n.json') return callback();
+	var files = fs.readdirSync(path);
+	async.eachSeries(files, function(file, callback) {
+		if (file === 'en.i18n.json') { return callback(); }
 
 		var lang = file.replace('.i18n.json', '');
 		var destContents = fs.readFileSync(path + file, 'utf-8');
 		var destJson = JSON.parse(destContents);
-
 		var toTranslate = {};
 		var newContent = {};
 
-		for (key in en) {
-			if (destJson[key]) {
-				newContent[key] = destJson[key];
-			} else {
-				newContent[key] = '';
-				toTranslate[key] = en[key];
+		for (var key in en) {
+			if (en.hasOwnProperty(key)) {
+				if (destJson[key]) {
+					newContent[key] = destJson[key];
+				} else {
+					newContent[key] = '';
+					toTranslate[key] = en[key];
+				}
 			}
 		}
 
@@ -48,18 +49,23 @@ googleTranslate.getSupportedLanguages(function(err, langs) {
 		}
 
 		if (langs.indexOf(lang) !== -1 && toTranslateLang.length > 1) {
+			console.log(lang, toTranslateLang.length);
 			googleTranslate.translate(toTranslateLang, 'en', lang, function(err, translations) {
 				if (err) {
 					console.log(lang, err);
 				} else {
-					for (key in translations) {
-						newContent[invertToTranslate[translations[key].originalText]] = translations[key].translatedText;
+					for (var key in translations) {
+						if (translations.hasOwnProperty(key)) {
+							newContent[invertToTranslate[translations[key].originalText]] = translations[key].translatedText;
+						}
 					}
 					var newJsonString = JSON.stringify(newContent, null, '  ').replace(/": "/g, '" : "');
 					fs.writeFileSync(path + file, newJsonString, 'utf8');
-					return callback();
+					setTimeout(function() { return callback(); }, 1000);
 				}
 			});
+		} else {
+			return callback();
 		}
 	});
 });
