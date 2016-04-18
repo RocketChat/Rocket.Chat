4c196644d26f88c5d93922b8ae2a30ade4fe5c98
Diego Sampaio <chinello@gmail.com>
2016-04-18 15:25:19 -0300

    Add support to livechat send and receive messages via SMS

--- packages/rocketchat-livechat/server/lib/getNextAgent.js
+++
@@ -1,9 +0,0 @@
-/* exported getNextAgent */
-
-this.getNextAgent = function(department) {
-	if (department) {
-		return RocketChat.models.LivechatDepartmentAgents.getNextAgentForDepartment(department);
-	} else {
-		return RocketChat.models.Users.getNextAgent();
-	}
-};
--- packages/rocketchat-livechat/server/models/Users.js
+++ packages/rocketchat-livechat/server/models/Users.js
@@ -137,3 +137,37 @@ RocketChat.models.Users.updateLivechatDataByToken = function(token, key, value) 
 	return this.upsert(query, update);
 };
 
+/**
+ * Find a visitor by their phone number
+ * @return {object} User from db
+ */
+RocketChat.models.Users.findOneVisitorByPhone = function(phone) {
+	const query = {
+		'profile.phones.number': phone
+	};
+
+	return this.findOne(query);
+};
+
+/**
+ * Get the next visitor name
+ * @return {string} The next visitor name
+ */
+RocketChat.models.Users.getNextVisitorUsername = function() {
+	const settingsRaw = RocketChat.models.Settings.model.rawCollection();
+	const findAndModify = Meteor.wrapAsync(settingsRaw.findAndModify, settingsRaw);
+
+	const query = {
+		_id: 'Livechat_guest_count'
+	};
+
+	const update = {
+		$inc: {
+			value: 1
+		}
+	};
+
+	const livechatCount = findAndModify(query, null, update);
+
+	return 'guest-' + (livechatCount.value + 1);
+};
