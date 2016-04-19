649adaf587a0b8b4329946ed45888ecea5a8fbcb
Diego Sampaio <chinello@gmail.com>
2016-04-18 21:26:20 -0300

    Livechat SMS support (#2939)
    * Add package to send SMS
    
    * Add support to livechat send and receive messages via SMS
    
    * Fix livechat typing indicator and first message after trigger
    
    * rename migration number.

---
+++ packages/rocketchat-livechat/server/sendMessageBySMS.js
@@ -0,0 +1,37 @@
+RocketChat.callbacks.add('afterSaveMessage', function(message, room) {
+	// skips this callback if the message was edited
+	if (message.editedAt) {
+		return message;
+	}
+
+	if (!RocketChat.SMS.enabled) {
+		return message;
+	}
+
+	// only send the sms by SMS if it is a livechat room with SMS set to true
+	if (typeof room.t === 'undefined' || room.t !== 'l' || !room.sms || !room.v || !room.v.token) {
+		return message;
+	}
+
+	// if the message has a token, it was sent from the visitor, so ignore it
+	if (message.token) {
+		return message;
+	}
+
+	const SMSService = RocketChat.SMS.getService(RocketChat.settings.get('SMS_Service'));
+
+	if (!SMSService) {
+		return message;
+	}
+
+	const visitor = RocketChat.models.Users.getVisitorByToken(room.v.token);
+
+	if (!visitor || !visitor.profile || !visitor.profile.phones || visitor.profile.phones.length === 0) {
+		return message;
+	}
+
+	SMSService.send(visitor.profile.phones[0].number, message.msg);
+
+	return message;
+
+}, RocketChat.callbacks.priority.LOW);
