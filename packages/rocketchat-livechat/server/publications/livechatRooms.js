1ffc5c0dbc41ba9aee5e9fb392bfc80656e54cbc
Diego Sampaio <chinello@gmail.com>
2016-04-20 14:08:40 -0300

    Return publication error correctly

--- packages/rocketchat-livechat/server/publications/livechatRooms.js
+++ packages/rocketchat-livechat/server/publications/livechatRooms.js
@@ -1,10 +1,10 @@
 Meteor.publish('livechat:rooms', function(offset = 0, limit = 20) {
 	if (!this.userId) {
-		throw new Meteor.Error('not-authorized');
+		return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:rooms' }));
 	}
 
 	if (!RocketChat.authz.hasPermission(this.userId, 'view-livechat-rooms')) {
-		throw new Meteor.Error('not-authorized');
+		return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:rooms' }));
 	}
 
 	return RocketChat.models.Rooms.findLivechat(offset, limit);
