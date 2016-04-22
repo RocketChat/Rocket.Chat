18290d0d3d101b8d33f170d220584932b667f29e
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-04-22 10:30:32 -0300

    Error: livechat publishes

--- packages/rocketchat-livechat/server/publications/visitorInfo.js
+++ packages/rocketchat-livechat/server/publications/visitorInfo.js
@@ -1,10 +1,10 @@
 Meteor.publish('livechat:visitorInfo', function(roomId) {
 	if (!this.userId) {
-		throw new Meteor.Error('not-authorized');
+		return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:visitorInfo' }));
 	}
 
-	if (!RocketChat.authz.hasPermission(this.userId, 'view-l-room')) {
-		throw new Meteor.Error('not-authorized');
+	if (!RocketChat.authz.hasPermission(this.userId, 'view-livechat-rooms')) {
+		return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:visitorInfo' }));
 	}
 
 	var room = RocketChat.models.Rooms.findOneById(roomId);
