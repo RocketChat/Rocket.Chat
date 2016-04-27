685fdb65e4d39abe20684e99ce134d686a0fc21d
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-04-27 08:44:07 -0300

    Fix migration number

--- server/startup/migrations/v049.js
+++ server/startup/migrations/v049.js
@@ -1,5 +1,5 @@
 RocketChat.Migrations.add({
-	version: 48,
+	version: 49,
 	up: function() {
 		if (RocketChat && RocketChat.models && RocketChat.models.Users && RocketChat.models.Users.update) {
 			RocketChat.models.Users.update({ avatarOrigin: { $exists: false } }, { $set: { avatarOrigin: 'initials' } }, { multi: true });
