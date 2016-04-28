8a789b78ab732e662e6d2edb2dbca8eb8ee18b26
Diego Sampaio <chinello@gmail.com>
2016-04-28 09:16:49 -0300

    Add an external for livechat received messages

---
+++ packages/rocketchat-livechat/client/views/app/tabbar/externalSearch.js
@@ -0,0 +1,23 @@
+Template.externalSearch.helpers({
+	messages() {
+		console.log('messages helper');
+		return RocketChat.models.LivechatExternalMessage.findByRoomId(this.rid, { ts: 1 });
+	}
+});
+
+Template.externalSearch.events({
+	'click a.pick-message'(event, instance) {
+		event.preventDefault();
+
+		$('#chat-window-' + instance.roomId + ' .input-message').val(this.msg).focus();
+	}
+});
+
+Template.externalSearch.onCreated(function() {
+	this.roomId = null;
+	// console.log('externalSearch.this ->',this);
+	this.autorun(() => {
+		this.roomId = Template.currentData().rid;
+		this.subscribe('livechat:externalMessages', Template.currentData().rid);
+	});
+});
--- packages/rocketchat-livechat/config.js
+++ packages/rocketchat-livechat/config.js
@@ -5,4 +5,28 @@ Meteor.startup(function() {
 	RocketChat.settings.add('Livechat_enabled', false, { type: 'boolean', group: 'Livechat', public: true });
 	RocketChat.settings.add('Livechat_registration_form', true, { type: 'boolean', group: 'Livechat', public: true, i18nLabel: 'Show_preregistration_form' });
 	RocketChat.settings.add('Livechat_guest_count', 1, { type: 'int', group: 'Livechat' });
+
+	RocketChat.settings.add('Livechat_Knowledge_Enabled', false, {
+		type: 'boolean',
+		group: 'Livechat',
+		section: 'Knowledge Base',
+		public: true,
+		i18nLabel: 'Knowledge_Enabled'
+	});
+
+	RocketChat.settings.add('Livechat_Knowledge_Apiai_Key', '', {
+		type: 'string',
+		group: 'Livechat',
+		section: 'Knowledge Base',
+		public: true,
+		i18nLabel: 'Apiai_Key'
+	});
+
+	RocketChat.settings.add('Livechat_Knowledge_Apiai_Language', 'en', {
+		type: 'string',
+		group: 'Livechat',
+		section: 'Knowledge Base',
+		public: true,
+		i18nLabel: 'Apiai_Language'
+	});
 });
