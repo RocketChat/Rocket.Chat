1d5711a8d40f631bd4ef7397214dc0a65d67b331
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-04-29 08:59:10 -0300

    Use new placholders.js for sending mail through Mailer

--- packages/rocketchat-lib/lib/placeholders.js
+++ packages/rocketchat-lib/lib/placeholders.js
@@ -2,11 +2,11 @@ RocketChat.placeholders = {};
 
 RocketChat.placeholders.replace = function(str, data) {
 	if (!str) {
-		return "";
+		return '';
 	}
 
-	str = str.replace(/\[Site_Name\]/g, RocketChat.settings.get("Site_Name") || '');
-	str = str.replace(/\[Site_URL\]/g, RocketChat.settings.get("Site_Url") || '');
+	str = str.replace(/\[Site_Name\]/g, RocketChat.settings.get('Site_Name') || '');
+	str = str.replace(/\[Site_URL\]/g, RocketChat.settings.get('Site_Url') || '');
 
 	if (data) {
 		str = str.replace(/\[name\]/g, data.name || '');
@@ -14,7 +14,14 @@ RocketChat.placeholders.replace = function(str, data) {
 		str = str.replace(/\[lname\]/g, _.strRightBack(data.name, ' ') || '');
 		str = str.replace(/\[email\]/g, data.email || '');
 		str = str.replace(/\[password\]/g, data.password || '');
+
+		if (data.unsubscribe) {
+			str = str.replace(/\[unsubscribe\]/g, data.unsubscribe);
+		}
 	}
+
+	str = str.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
+
 
 	return str;
 };
