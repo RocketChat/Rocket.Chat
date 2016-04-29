a55665bc772664283c189114d024afc26f66b047
Christian Fusco <christian.fusco@mymail.champlain.edu>
2016-04-29 05:18:10 -0700

    Emoji's by themselves appear 3x as large. (#3072)
    * Added the ability to create larger emojis by editing sprites.css.
    Changed renderMessageBody.js so that if a user's message contained only an emoji, it appears 3x as large.
    
    * Fixed spacing and equality issues.
    
    * Update renderMessageBody.js
    
    Cosmetic fixes

--- packages/rocketchat-ui-message/message/renderMessageBody.js
+++ packages/rocketchat-ui-message/message/renderMessageBody.js
@@ -22,5 +22,46 @@ renderMessageBody = function(msg) {
 
 	msg.html = message.html.replace(/\n/gm, '<br/>');
 
+	if (singleEmojiCheck(msg)) {
+		msg.html = (msg.html).replace('emojione ', 'big_emojione ');
+	}
+
 	return msg.html;
 };
+
+/**
+ *Checks to see if a message contains only an emoji span
+ *@para {msg} some html message
+ *@return {false} the html message either doesn't 
+ *contain an emoji span, or contains other characters
+ */
+singleEmojiCheck = function(msg) {
+	var str = msg.html;
+	str = str.trim();
+
+	//checks to make sure it's a span
+	if (!str.includes('<') || !str.includes('>')) {
+		return false;
+	}
+
+	if (str.charAt(0) !== '<' || str.charAt(str.length - 1) !== '>') {
+		return false;
+	}
+
+	if (str.indexOf('>') === str.lastIndexOf('>')) {
+		return false;
+	}
+
+	//checks to make sure it's the only span
+	str = str.substring(str.indexOf('<') + 1, str.lastIndexOf('>') - 1);
+	if (!str.includes('<') || !str.includes('>')) {
+		return false;
+	}
+
+	if (str.indexOf('<') < str.lastIndexOf('>')) {
+		return false;
+	}
+
+	return true;
+};
+
