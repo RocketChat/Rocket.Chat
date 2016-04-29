bf2b0ac82df5ab7ecacf3b4868515ff5c999d28c
ForReal <Christian.Fusco@Mymail.Champlain.Edu>
2016-04-28 20:12:13 -0700

    Fixed spacing and equality issues.

--- packages/rocketchat-ui-message/message/renderMessageBody.js
+++ packages/rocketchat-ui-message/message/renderMessageBody.js
@@ -21,9 +21,10 @@ renderMessageBody = function(msg) {
 	}
 
 	msg.html = message.html.replace(/\n/gm, '<br/>');
-	
-	if(singleEmojiCheck(msg))
-		msg.html = (msg.html).replace("emojione ", "big_emojione ");
+
+	if (singleEmojiCheck(msg)) {
+		msg.html = (msg.html).replace('emojione ', 'big_emojione ');
+	}
 
 	return msg.html;
 };
@@ -33,23 +34,32 @@ renderMessageBody = function(msg) {
  *@para {msg} some html message
  *@return {false} the html message either doesn't contain an emoji span, or contains other characters
  */
-singleEmojiCheck = function(msg){
+singleEmojiCheck = function(msg) {
 	var str = msg.html;
 	str = str.trim();
+
 	//checks to make sure it's a span
-	if(!str.includes("<") || !str.includes(">"))
+	if (!str.includes('<') || !str.includes('>')) {
 		return false;
-	if(str.charAt(0) != '<' || str.charAt(str.length - 1) != '>')
+	}
+
+	if (str.charAt(0) !== '<' || str.charAt(str.length - 1) !== '>') {
 		return false;
-	if(str.indexOf(">") == str.lastIndexOf(">"))
+	}
+
+	if (str.indexOf('>') === str.lastIndexOf('>')) {
 		return false;
+	}
 
 	//checks to make sure it's the only span
-	str = str.substring(str.indexOf("<") + 1, str.lastIndexOf(">") - 1);
-	if(!str.includes("<") || !str.includes(">"))
+	str = str.substring(str.indexOf('<') + 1, str.lastIndexOf('>') - 1);
+	if (!str.includes('<') || !str.includes('>')) {
 		return false;
-	if(str.indexOf("<") < str.lastIndexOf(">"))
+	}
+
+	if (str.indexOf('<') < str.lastIndexOf('>')){
 		return false;
+	}
 
 	return true;
 };
