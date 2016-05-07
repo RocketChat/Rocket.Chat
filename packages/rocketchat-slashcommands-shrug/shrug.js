21eed23984791d90ff48b168b65d3b97d6bb74ab
Aaron Ogle <aaron@geekgonecrazy.com>
2016-05-07 01:10:30 -0500

    Add /shrug command

---
+++ packages/rocketchat-slashcommands-shrug/shrug.js
@@ -0,0 +1,20 @@
+/*
+* Shrug is a named function that will replace /shrug commands
+* @param {Object} message - The message object
+*/
+
+
+function Shrug (command, params, item) {
+	if (command === "shrug") {
+		var msg;
+
+		msg = item;
+		msg.msg = params + ' ¯\\_(ツ)_/¯';
+		Meteor.call('sendMessage', msg);
+	}
+}
+
+RocketChat.slashCommands.add('shrug', Shrug, {
+	description: 'Slash_Shrug_Description',
+	params: 'your message (optional)'
+});
