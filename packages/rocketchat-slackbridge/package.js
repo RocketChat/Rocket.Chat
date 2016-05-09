9c776fc176f97c95b0531e87ffd72eeed29bfa4f
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-05-09 12:40:10 -0300

    Add logger, connects to bridge and listen to events

--- packages/rocketchat-slackbridge/package.js
+++ packages/rocketchat-slackbridge/package.js
@@ -10,7 +10,9 @@ Package.onUse(function(api) {
 	api.versionsFrom('1.2.1');
 	api.use('ecmascript');
 	api.use('rocketchat:lib');
+	api.use('rocketchat:logger');
 
+	api.addFiles('logger.js', 'server');
 	api.addFiles('settings.js', 'server');
 	api.addFiles('slackbridge.js', 'server');
 });
