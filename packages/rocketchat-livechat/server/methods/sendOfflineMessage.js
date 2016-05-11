2ac131b2c289ffc20fe86dcc128f522fb3c44f8e
Diego Sampaio <chinello@gmail.com>
2016-05-11 10:42:59 -0300

    Add replyTo and more descriptive 'from' to livechat offline email

--- packages/rocketchat-livechat/server/methods/sendOfflineMessage.js
+++ packages/rocketchat-livechat/server/methods/sendOfflineMessage.js
@@ -3,16 +3,27 @@ Meteor.methods({
 		const header = RocketChat.placeholders.replace(RocketChat.settings.get('Email_Header') || '');
 		const footer = RocketChat.placeholders.replace(RocketChat.settings.get('Email_Footer') || '');
 
+		const message = (data.message + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
+
 		const html = `
 			<h1>New livechat message</h1>
 			<p><strong>Visitor name:</strong> ${data.name}</p>
 			<p><strong>Visitor email:</strong> ${data.email}</p>
-			<p><strong>Message:</strong> ${data.message}</p>`;
+			<p><strong>Message:</strong><br>${message}</p>`;
+
+		let fromEmail = RocketChat.settings.get('From_Email').match(/\b[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}\b/i);
+
+		if (fromEmail) {
+			fromEmail = fromEmail[0];
+		} else {
+			fromEmail = RocketChat.settings.get('From_Email');
+		}
 
 		Meteor.defer(() => {
 			Email.send({
 				to: RocketChat.settings.get('Livechat_offline_email'),
-				from: RocketChat.settings.get('From_Email'),
+				from: `${data.name} - ${data.email} <${fromEmail}>`,
+				replyTo: `${data.name} <${data.email}>`,
 				subject: 'New livechat offline message',
 				html: header + html + footer
 			});
