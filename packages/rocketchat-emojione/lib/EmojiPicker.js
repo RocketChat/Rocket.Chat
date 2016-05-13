3311f468a7d3d7e3eef7e00d9b4bb23b310ca081
André Rivotti Casimiro <acasimiro@cyngn.com>
2016-05-13 03:44:14 +0100

    improved ux for 360 width media
    * side-nav: hide footer
    * fixed-title:
      * room icon smaller and bolder
      * room-title smaller and bolder
      * room-topic in a different line, smaller and with italic
      * favorite moved to the far rigth and increased in size
      * burguer smaller and closer to the edge
    * flex-tab-bar now slides up and down
    * flex-tab-bar now horizontally after fixed-tile
    * message: reduce the space around the button
    * message-form:
      * removed the space around the message box
      * emoji icon move to the left side of the box
      * attach moved to the right side of the box
      * change the behaviour of the buttons based on the message box's text:
        * if empty, show attach and mic
        * if higher than 0, hide attach and mic and show send
      * hide tooltips
      * hide user typing
    * emoji-picker:
      * now appears bellow the message-form
      * search box removed

--- packages/rocketchat-emojione/lib/EmojiPicker.js
+++ packages/rocketchat-emojione/lib/EmojiPicker.js
@@ -52,6 +52,9 @@ RocketChat.EmojiPicker = {
 		return this.recent;
 	},
 	setPosition() {
+		if (window.matchMedia('all and (min-width: 360px)').matches)
+			return $('.emoji-picker')
+
 		let sourcePos = $(this.source).offset();
 		let left = (sourcePos.left - this.width + 65);
 		let top = (sourcePos.top - this.height - 5);
@@ -88,6 +91,9 @@ RocketChat.EmojiPicker = {
 		this.opened = true;
 	},
 	close() {
+		const mainContent = $('.main-content');
+		mainContent.removeClass('emoji-picking');
+
 		$('.emoji-picker').removeClass('show');
 		this.opened = false;
 	},
