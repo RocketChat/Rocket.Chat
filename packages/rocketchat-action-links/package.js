328d9e254bb2de3c422464a866fc88c1153fc485
Daniel Bressan <Daniel Bressan>
2016-05-25 22:27:13 +0100

    initial add of Action Links

---
+++ packages/rocketchat-action-links/package.js
@@ -0,0 +1,24 @@
+Package.describe({
+	name: 'rocketchat:action-links',
+	version: '0.0.1',
+	summary: 'Add custom actions that call functions',
+	git: '',
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0');
+
+	api.use('ecmascript');
+	api.use('templating');
+	api.use('rocketchat:lib');
+	api.use('rocketchat:theme');
+	api.use('rocketchat:ui');
+
+	api.addFiles('client/init.js', 'client');
+	api.addAssets('client/stylesheets/actionLinks.less', 'server');
+	api.addFiles('loadStylesheets.js', 'server');
+	
+	api.addFiles('server/registerActionLinkFuncts.js', 'server');
+	api.addFiles('server/actionLinkHandler.js', 'server');
+
+});
