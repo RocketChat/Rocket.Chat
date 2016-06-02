69edf0b3a46156d18a6db70980a0fe31c8b5f744
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-06-02 13:39:26 -0300

    Removed test package; Added error handling to client side; Changed variable names to make it more clear;

--- packages/rocketchat-action-links/client/init.js
+++ packages/rocketchat-action-links/client/init.js
@@ -3,9 +3,13 @@ Template.room.events({
 		event.preventDefault();
 		event.stopPropagation();
 		const data = Blaze.getData(event.currentTarget);
-		// console.log("passing fuctindex....", event);
 
-		// TODO, better way to grab stored HTML meta data?
-		Meteor.call('actionLinkHandler', event.currentTarget.childNodes[1].name, data._arguments[1], Meteor.userId());
-	},
+		if (data && data._arguments && data._arguments[1] && data._arguments[1]._id) {
+			Meteor.call('actionLinkHandler', event.currentTarget.childNodes[1].name, data._arguments[1]._id, (err) => {
+				if (err) {
+					handleError(err);
+				}
+			});
+		}
+	}
 });
