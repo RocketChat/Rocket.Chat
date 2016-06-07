5b43f6229f743054a4d666d9c8c2f2ca2a9d1f80
Aaron Ogle <aaron@geekgonecrazy.com>
2016-06-06 22:52:30 -0500

    Basic working jitsi implementation

--- packages/rocketchat-videobridge/client/views/videoFlexTab.js
+++ packages/rocketchat-videobridge/client/views/videoFlexTab.js
@@ -1,6 +1,14 @@
 Template.videoFlexTab.helpers({
-	videoUrl() {
-		return "https://jitsi.rocket.chat/rocketchat";
+	videoAvaliable() {
+		return true;
+	},
+
+	videoActive() {
+		return false;
+	},
+
+	callInProgress() {
+		return false;
 	}
 
 });
@@ -10,8 +18,45 @@ Template.videoFlexTab.onCreated(function() {
 	this.timeout = null;
 	this.autorun(() => {
 
-});
+	});
 
 });
 
+Template.videoFlexTab.events({
+	'click .start-video-call': () => {
+		var domain = "meet.jit.si"; // Need to get from config
+		var room = "a124124124124124125125125"; // Need to calc from instance id and room id
+		var width = 500;
+		var height = 300;
 
+		var configOverwrite = {};
+		var interfaceConfigOverwrite = {
+
+		};
+		var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite);
+
+		api.executeCommand('displayName', [Meteor.user().name]);
+	},
+
+	'click .start-audio-call': () => {
+		var domain = "meet.jit.si"; // Need to get from config
+		var room = "a124124124124124125125125"; // Need to calc from instance id and room id
+		var width = 500;
+		var height = 300;
+
+		var configOverwrite = {};
+		var interfaceConfigOverwrite = {
+
+		};
+		var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite);
+
+		api.executeCommand('displayName', [Meteor.user().name]);
+	},
+
+	'click .join-video-call': () => {
+	},
+
+	'click .join-audio-call': () => {
+	}
+
+});
