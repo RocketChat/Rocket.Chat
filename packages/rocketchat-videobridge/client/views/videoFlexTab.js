142ac676f3f71220e6a4f3780eb1a2457b4c6479
Aaron Ogle <aaron@geekgonecrazy.com>
2016-06-07 14:05:46 -0500

    changed JitsiMeet ssl flag.  Flag now means no SSL.  So its going to use ssl by default

--- packages/rocketchat-videobridge/client/views/videoFlexTab.js
+++ packages/rocketchat-videobridge/client/views/videoFlexTab.js
@@ -10,17 +10,17 @@ Template.videoFlexTab.onCreated(function() {
 	});
 
 	// Opening a PR so we can do this via the https://meet.jit.si/external_api.js
-	$.getScript( "https://cdn.rawgit.com/geekgonecrazy/jitsi-meet/master/external_api.js" )
+	$.getScript( 'https://cdn.rawgit.com/geekgonecrazy/jitsi-meet/master/external_api.js' )
 		.done(function( script, textStatus ) {
-			var domain = "meet.jit.si"; // Need to get from settings
-			var room = "a124124124124124125125126"; // Need to calc from instance id and room id
+			var domain = 'meet.jit.si'; // Need to get from settings
+			var room = 'a124124124124124125125126'; // Need to calc from instance id and room id
 			var width = 500;
 			var height = 500;
 
 			var configOverwrite = {};
 			var interfaceConfigOverwrite = {};
 
-			var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite, true);
+			var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite, false);
 
 			// This for sure needs to be an onReady of some sort instead
 			setTimeout(() => {
