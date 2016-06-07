ba2ef6077bc761fa6e226f1d249fe5bad9734162
Aaron Ogle <aaron@geekgonecrazy.com>
2016-06-06 23:34:18 -0500

    Try UI that auto starts call on tab open

--- packages/rocketchat-videobridge/client/views/videoFlexTab.js
+++ packages/rocketchat-videobridge/client/views/videoFlexTab.js
@@ -1,15 +1,4 @@
 Template.videoFlexTab.helpers({
-	videoAvaliable() {
-		return true;
-	},
-
-	videoActive() {
-		return false;
-	},
-
-	callInProgress() {
-		return false;
-	}
 
 });
 
@@ -20,43 +9,27 @@ Template.videoFlexTab.onCreated(function() {
 
 	});
 
+	// Opening a PR so we can do this via the https://meet.jit.si/external_api.js
+	$.getScript( "https://cdn.rawgit.com/geekgonecrazy/jitsi-meet/master/external_api.js" )
+		.done(function( script, textStatus ) {
+			var domain = "meet.jit.si"; // Need to get from config
+			var room = "a124124124124124125125125"; // Need to calc from instance id and room id
+			var width = 500;
+			var height = 500;
+
+			var configOverwrite = {};
+			var interfaceConfigOverwrite = {};
+
+			var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite, true);
+
+			api.executeCommand('displayName', [Meteor.user().name]);
+		})
+		.fail(function( jqxhr, settings, exception ) {
+			// Show an error
+		});
+
 });
 
 Template.videoFlexTab.events({
-	'click .start-video-call': () => {
-		var domain = "meet.jit.si"; // Need to get from config
-		var room = "a124124124124124125125125"; // Need to calc from instance id and room id
-		var width = 500;
-		var height = 300;
-
-		var configOverwrite = {};
-		var interfaceConfigOverwrite = {
-
-		};
-		var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite, true);
-		
-		api.executeCommand('displayName', [Meteor.user().name]);
-	},
-
-	'click .start-audio-call': () => {
-		var domain = "meet.jit.si"; // Need to get from config
-		var room = "a124124124124124125125125"; // Need to calc from instance id and room id
-		var width = 500;
-		var height = 300;
-
-		var configOverwrite = {};
-		var interfaceConfigOverwrite = {
-
-		};
-		var api = new JitsiMeetExternalAPI(domain, room, width, height, document.getElementById('videoContainer'), configOverwrite, interfaceConfigOverwrite, true);
-
-		api.executeCommand('displayName', [Meteor.user().name]);
-	},
-
-	'click .join-video-call': () => {
-	},
-
-	'click .join-audio-call': () => {
-	}
 
 });
