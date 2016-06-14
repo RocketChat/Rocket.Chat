a3ac1975cf650489e0f5ceb79328c576aef9c8ef
Flavio Grossi <flaviogrossi@gmail.com>
2016-06-14 16:40:43 +0200

    use single quotes for all strings

--- packages/rocketchat-ui-vrecord/package.js
+++ packages/rocketchat-ui-vrecord/package.js
@@ -1,8 +1,8 @@
 Package.describe({
-	"name": "rocketchat:ui-vrecord",
-	"version": "0.0.1",
-	"description": "Video upload with on the fly recording",
-	"documentation": "README.md"
+	'name': 'rocketchat:ui-vrecord',
+	'version': '0.0.1',
+	'description': 'Video upload with on the fly recording',
+	'documentation': 'README.md'
 });
 
 Package.onUse(function(api) {
