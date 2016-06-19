96d8e1baebcf601439fe06a516a72808d542ac1f
Deepak Kothandan <deepak.kothandan@valmano.de>
2016-06-19 02:10:43 +0200

    added the slash command /open - to create a new channel

---
+++ packages/rocketchat-slashcommands-open/client.js
@@ -0,0 +1,4 @@
+RocketChat.slashCommands.add('open', null, {
+	description: TAPi18n.__('Create_A_New_Channel'),
+	params: 'channel'
+});
---
+++ packages/rocketchat-slashcommands-open/server.js
@@ -0,0 +1,29 @@
+function Open(command, params, item) {
+	var channel, room, user;
+	if (command !== 'open' || !Match.test(params, String)) {
+		return;
+	}
+	channel = params.trim();
+	if (channel === '') {
+		return;
+	}
+
+	user = Meteor.users.findOne(Meteor.userId());
+	room = RocketChat.models.Rooms.findOneByName(channel);
+	if (room != null) {
+		RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
+			_id: Random.id(),
+			rid: item.rid,
+			ts: new Date,
+			msg: TAPi18n.__('Channel_already_exist', {
+				postProcess: 'sprintf',
+				sprintf: [channel]
+			}, user.language)
+		});
+		return;
+	}
+	Meteor.call('createChannel', channel, []);
+	return Open;
+}
+
+RocketChat.slashCommands.add('open', Open);
