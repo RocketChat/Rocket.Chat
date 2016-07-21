45b3c033eb0fbb9f83c248c252164c48d131847b
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-07-21 18:39:46 -0300

    Add getUsersOfRoom

---
+++ server/methods/getUsersOfRoom.js
@@ -0,0 +1,14 @@
+Meteor.methods({
+	getUsersOfRoom(roomId) {
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'getUsersOfRoom' });
+		}
+
+		const room = Meteor.call('canAccessRoom', roomId, Meteor.userId());
+		if (!room) {
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'getUsersOfRoom' });
+		}
+
+		return RocketChat.cache.Rooms.findByIndex('_id', roomId).usernames;
+	}
+});
