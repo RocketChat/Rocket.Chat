59d2f64ad5765bb773e88af3b443fb2ffc6ecdbe
Isaac Roca <iroca@icon.cat>
2016-07-23 11:29:38 +0200

    Less spartan expiry time for Amazon S3 download links.

--- packages/rocketchat-file-upload/server/config/configFileUploadAmazonS3.js
+++ packages/rocketchat-file-upload/server/config/configFileUploadAmazonS3.js
@@ -8,7 +8,7 @@ var generateURL = function(file) {
 		return;
 	}
 	let resourceURL = '/' + file.s3.bucket + '/' + file.s3.path + file._id;
-	let expires = parseInt(new Date().getTime() / 1000) + 60;
+	let expires = parseInt(new Date().getTime() / 1000) + 600;
 	let StringToSign = 'GET\n\n\n' + expires +'\n'+resourceURL;
 	let signature = crypto.createHmac('sha1', S3secretKey).update(new Buffer(StringToSign, 'utf-8')).digest('base64');
 	return file.url + '?AWSAccessKeyId='+encodeURIComponent(S3accessKey)+'&Expires='+expires+'&Signature='+encodeURIComponent(signature);
