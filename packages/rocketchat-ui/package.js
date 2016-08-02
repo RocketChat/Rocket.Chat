ab1fb46478ae272d7ddbbca00e3ce63c1eb721ae
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-08-02 18:46:32 -0300

    Improve spotlight

--- packages/rocketchat-ui/package.js
+++ packages/rocketchat-ui/package.js
@@ -94,6 +94,7 @@ Package.onUse(function(api) {
 	api.addFiles('views/app/secretURL.html', 'client');
 	api.addFiles('views/app/userSearch.html', 'client');
 	api.addFiles('views/app/spotlight/spotlight.html', 'client');
+	api.addFiles('views/app/spotlight/spotlightTemplate.html', 'client');
 	api.addFiles('views/app/videoCall/videoButtons.html', 'client');
 	api.addFiles('views/app/videoCall/videoCall.html', 'client');
 
@@ -109,6 +110,7 @@ Package.onUse(function(api) {
 	api.addFiles('views/app/secretURL.coffee', 'client');
 	api.addFiles('views/app/spotlight/mobileMessageMenu.coffee', 'client');
 	api.addFiles('views/app/spotlight/spotlight.coffee', 'client');
+	api.addFiles('views/app/spotlight/spotlightTemplate.js', 'client');
 	api.addFiles('views/app/videoCall/videoButtons.coffee', 'client');
 	api.addFiles('views/app/videoCall/videoCall.coffee', 'client');
 });
