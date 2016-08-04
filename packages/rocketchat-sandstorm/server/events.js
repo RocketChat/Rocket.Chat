7b39935d57116771386d40131bc953ca8d634613
Jason Paryani <github@jparyani.com>
2016-08-03 17:36:57 -0700

    Move common Sandstorm functions out to a lib.js

--- packages/rocketchat-sandstorm/server/events.js
+++ packages/rocketchat-sandstorm/server/events.js
@@ -1,36 +1,9 @@
-RocketChat.Sandstorm = {};
-
 RocketChat.Sandstorm.notify = function() {};
 
 if (process.env.SANDSTORM === '1') {
-	var Future = Npm.require('fibers/future');
-	var Capnp = Npm.require('capnp');
-	var SandstormHttpBridge = Npm.require('sandstorm/sandstorm-http-bridge.capnp').SandstormHttpBridge;
-
-	var capnpConnection = null;
-	var httpBridge = null;
-
-	var getHttpBridge = function() {
-		if (!httpBridge) {
-			capnpConnection = Capnp.connect('unix:/tmp/sandstorm-api');
-			httpBridge = capnpConnection.restore(null, SandstormHttpBridge);
-		}
-		return httpBridge;
-	};
-
 	var ACTIVITY_TYPES = {
 		'message': 0,
 		'privateMessage': 1
-	};
-
-	var promiseToFuture = function(promise) {
-		const result = new Future();
-		promise.then(result.return.bind(result), result.throw.bind(result));
-		return result;
-	};
-
-	var waitPromise = function(promise) {
-		return promiseToFuture(promise).wait();
 	};
 
 	RocketChat.Sandstorm.notify = function(message, userIds, caption, type) {
