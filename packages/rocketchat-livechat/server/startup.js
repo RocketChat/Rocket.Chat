a40317890e70b1094e042171de6b9f12b433422b
Diego Sampaio <chinello@gmail.com>
2016-08-08 11:35:09 -0300

    Initial livechat fix

--- packages/rocketchat-livechat/server/startup.js
+++ packages/rocketchat-livechat/server/startup.js
@@ -1,26 +1,31 @@
 Meteor.startup(() => {
-	RocketChat.roomTypes.setPublish('l', (code) => {
-		// CACHE: can we stop using publications here?
-		return RocketChat.models.Rooms.findLivechatByCode(code, {
-			name: 1,
-			t: 1,
-			cl: 1,
-			u: 1,
-			label: 1,
-			usernames: 1,
-			v: 1,
-			livechatData: 1,
-			topic: 1,
-			tags: 1,
-			sms: 1,
-			code: 1,
-			open: 1
-		});
+	RocketChat.roomTypes.setRoomFind('l', (code) => {
+
+		return RocketChat.cache.Rooms.findByIndex('t,code', ['l', code]).fetch();
+
+
+		// // CACHE: can we stop using publications here?
+		// return RocketChat.models.Rooms.findLivechatByCode(code, {
+		// 	name: 1,
+		// 	t: 1,
+		// 	cl: 1,
+		// 	u: 1,
+		// 	label: 1,
+		// 	usernames: 1,
+		// 	v: 1,
+		// 	livechatData: 1,
+		// 	topic: 1,
+		// 	tags: 1,
+		// 	sms: 1,
+		// 	code: 1,
+		// 	open: 1
+		// });
 	});
 
-	RocketChat.authz.addRoomAccessValidator(function(room, user) {
-		return room.t === 'l' && RocketChat.authz.hasPermission(user._id, 'view-livechat-rooms');
-	});
+	// RocketChat.authz.addRoomAccessValidator(function(room, user) {
+	// 	console.log('addRoomAccessValidator ->', RocketChat.authz.hasPermission(user._id, 'view-livechat-rooms'), room, user);
+	// 	return room.t === 'l' && RocketChat.authz.hasPermission(user._id, 'view-livechat-rooms');
+	// });
 
 	RocketChat.callbacks.add('beforeLeaveRoom', function(user, room) {
 		if (room.t !== 'l') {
