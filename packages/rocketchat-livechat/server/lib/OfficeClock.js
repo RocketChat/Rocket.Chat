fb0870078462bc96a3bf282f18a079e3822fd8a1
Alec Troemel <alectroemel@hotmail.com>
2016-08-16 16:29:49 -0500

    more fixes

--- packages/rocketchat-livechat/server/lib/OfficeClock.js
+++ packages/rocketchat-livechat/server/lib/OfficeClock.js
@@ -1,8 +1,8 @@
-// Every minute check if office closed 
+// Every minute check if office closed
 Meteor.setInterval(function() {
 	if (RocketChat.settings.get('Livechat_enable_office_hours')) {
 		if (RocketChat.models.LivechatOfficeHour.isOpeningTime()) {
-			RocketChat.models.Users.openOffice();	
+			RocketChat.models.Users.openOffice();
 		}
 		else if (RocketChat.models.LivechatOfficeHour.isClosingTime()) {
 			RocketChat.models.Users.closeOffice();
