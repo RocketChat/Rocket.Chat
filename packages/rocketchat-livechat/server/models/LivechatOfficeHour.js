78ad40f21900a42fe598c94b730a837a494ec35f
Alec Troemel <alectroemel@hotmail.com>
2016-08-17 14:14:42 -0500

    fixed lint errors

--- packages/rocketchat-livechat/server/models/LivechatOfficeHour.js
+++ packages/rocketchat-livechat/server/models/LivechatOfficeHour.js
@@ -8,19 +8,22 @@ class LivechatOfficeHour extends RocketChat.models._Base {
 		this.tryEnsureIndex({ 'finish': 1 }); // the closing hours of the office
 		this.tryEnsureIndex({ 'open': 1 }); // whether or not the offices are open on this day
 
-		// if there is nothing in the collection, add defaults 
-		if (this.find().count() == 0) {
-			this.insert({"day" : "Monday", "start" : "08:00", "finish" : "20:00", "code" : 1, "open" : true });
-			this.insert({"day" : "Tuesday", "start" : "08:00", "finish" : "20:00", "code" : 2, "open" : true });
-			this.insert({"day" : "Wednesday", "start" : "08:00", "finish" : "20:00", "code" : 3, "open" : true });
-			this.insert({"day" : "Thursday", "start" : "08:00", "finish" : "20:00", "code" : 4, "open" : true });
-			this.insert({"day" : "Friday", "start" : "08:00", "finish" : "20:00", "code" : 5, "open" : true });
-			this.insert({"day" : "Saturday", "start" : "08:00", "finish" : "20:00", "code" : 6, "open" : false });
-			this.insert({"day" : "Sunday", "start" : "08:00", "finish" : "20:00", "code" : 0, "open" : false });
-			
-		}	
+		// if there is nothing in the collection, add defaults
+		if (this.find().count() === 0) {
+			this.insert({'day' : 'Monday', 'start' : '08:00', 'finish' : '20:00', 'code' : 1, 'open' : true });
+			this.insert({'day' : 'Tuesday', 'start' : '08:00', 'finish' : '20:00', 'code' : 2, 'open' : true });
+			this.insert({'day' : 'Wednesday', 'start' : '08:00', 'finish' : '20:00', 'code' : 3, 'open' : true });
+			this.insert({'day' : 'Thursday', 'start' : '08:00', 'finish' : '20:00', 'code' : 4, 'open' : true });
+			this.insert({'day' : 'Friday', 'start' : '08:00', 'finish' : '20:00', 'code' : 5, 'open' : true });
+			this.insert({'day' : 'Saturday', 'start' : '08:00', 'finish' : '20:00', 'code' : 6, 'open' : false });
+			this.insert({'day' : 'Sunday', 'start' : '08:00', 'finish' : '20:00', 'code' : 0, 'open' : false });
+
+		}
 	}
 
+	/*
+	 * update the given days start and finish times and whether the office is open on that day
+	 */
 	updateHours(day, newStart, newFinish, newOpen) {
 		this.update({
 			'day': day
@@ -33,6 +36,10 @@ class LivechatOfficeHour extends RocketChat.models._Base {
 		});
 	}
 
+	/*
+	 * Check if the current server time (utc) is within the office hours of that day
+	 * returns true or false
+	 */
 	isNowWithinHours() {
 		// get current time on server in utc
 		// var ct = moment().utc();
@@ -45,22 +52,22 @@ class LivechatOfficeHour extends RocketChat.models._Base {
 		}
 
 		// check if offices are open today
-		if (todaysOfficeHours.open == false) {
+		if (todaysOfficeHours.open === false) {
 			return false;
 		}
 
 		var start = moment.utc(todaysOfficeHours.day + ':' + todaysOfficeHours.start, 'dddd:HH:mm');
 		var finish = moment.utc(todaysOfficeHours.day + ':' + todaysOfficeHours.finish, 'dddd:HH:mm');
-		
+
 		// console.log(finish.isBefore(start));
 		if (finish.isBefore(start)) {
 			// finish.day(finish.day()+1);
-			finish.add(1,'days');
+			finish.add(1, 'days');
 		}
 
 		var result = currentTime.isBetween(start, finish);
 
-		// inBetween  check 
+		// inBetween  check
 		return result;
 	}
 
@@ -75,7 +82,7 @@ class LivechatOfficeHour extends RocketChat.models._Base {
 		}
 
 		// check if offices are open today
-		if (todaysOfficeHours.open == false) {
+		if (todaysOfficeHours.open === false) {
 			return false;
 		}
 
