7e42466d538d22359c876b844bd306591ee0ee28
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-08-27 16:07:21 -0300

    New and unique loading

--- packages/rocketchat-ui-master/server/inject.js
+++ packages/rocketchat-ui-master/server/inject.js
@@ -2,12 +2,8 @@
 
 Inject.rawBody('page-loading', `
 	<div id="initial-page-loading" class="page-loading">
-		<div class="spinner">
-			<div class="rect1"></div>
-			<div class="rect2"></div>
-			<div class="rect3"></div>
-			<div class="rect4"></div>
-			<div class="rect5"></div>
+		<div class="loading">
+			<i class="icon-spinner animate-pulse"></i>
 		</div>
 	</div>`
 );
