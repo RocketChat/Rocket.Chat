c89a7872b7991065b1fd7f21dbab679063b83834
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-08-29 17:14:43 -0300

    Refactoring

--- packages/rocketchat-slackbridge/slashcommand/slackbridge_import.server.js
+++ packages/rocketchat-slackbridge/slashcommand/slackbridge_import.server.js
@@ -16,30 +16,42 @@ function SlackBridgeImport(command, params, item) {
 		}, user.language)
 	});
 
-	RocketChat.SlackBridge.importMessages(item.rid, err => {
-		if (err) {
-			RocketChat.Notifications.notifyUser(user._id, 'message', {
-				_id: Random.id(),
-				rid: item.rid,
-				ts: new Date(),
-				msg: TAPi18n.__('SlackBridge_got_an_error_while_importing_your_messages_at_s__s_', {
-					postProcess: 'sprintf',
-					sprintf: [channel, err.message]
-				}, user.language)
-			});
-		} else {
-			RocketChat.Notifications.notifyUser(user._id, 'message', {
-				_id: Random.id(),
-				rid: item.rid,
-				ts: new Date(),
-				msg: TAPi18n.__('SlackBridge_has_finished_importing_your_messages_at_s_', {
-					postProcess: 'sprintf',
-					sprintf: [channel]
-				}, user.language)
-			});
-		}
-	});
-
+	try {
+		RocketChat.SlackBridge.importMessages(item.rid, err => {
+			if (err) {
+				RocketChat.Notifications.notifyUser(user._id, 'message', {
+					_id: Random.id(),
+					rid: item.rid,
+					ts: new Date(),
+					msg: TAPi18n.__('SlackBridge_got_an_error_while_importing_your_messages_at_s__s_', {
+						postProcess: 'sprintf',
+						sprintf: [channel, err.message]
+					}, user.language)
+				});
+			} else {
+				RocketChat.Notifications.notifyUser(user._id, 'message', {
+					_id: Random.id(),
+					rid: item.rid,
+					ts: new Date(),
+					msg: TAPi18n.__('SlackBridge_has_finished_importing_your_messages_at_s_', {
+						postProcess: 'sprintf',
+						sprintf: [channel]
+					}, user.language)
+				});
+			}
+		});
+	} catch (error) {
+		RocketChat.Notifications.notifyUser(user._id, 'message', {
+			_id: Random.id(),
+			rid: item.rid,
+			ts: new Date(),
+			msg: TAPi18n.__('SlackBridge_got_an_error_while_importing_your_messages_at_s__s_', {
+				postProcess: 'sprintf',
+				sprintf: [channel, error.message]
+			}, user.language)
+		});
+		throw error;
+	}
 	return SlackBridgeImport;
 }
 
