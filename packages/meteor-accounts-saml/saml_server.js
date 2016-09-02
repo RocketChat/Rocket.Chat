40fa0c4a03548756a7b880a3a0f3b20e6246b308
Jan Baier <jan.baier@amagical.net>
2016-09-02 13:06:20 +0200

    Accept username from SAML response
    Closes #4177

--- packages/meteor-accounts-saml/saml_server.js
+++ packages/meteor-accounts-saml/saml_server.js
@@ -110,6 +110,10 @@ Accounts.registerLoginHandler(function(loginRequest) {
 				if (username) {
 					newUser.username = username;
 				}
+			} else {
+				if (loginResult.profile.username) {
+					newUser.username = loginResult.profile.username;	
+				}
 			}
 
 			var userId = Accounts.insertUserDoc({}, newUser);
