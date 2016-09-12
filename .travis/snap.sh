4a492a62ce970fd1bda4bc615ac76b77e906fb16
Aaron Ogle <aaron@geekgonecrazy.com>
2016-09-12 11:36:53 -0500

    Decrypt macroon for snapcraft

--- .travis/snap.sh
+++ .travis/snap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -x
 
-mkdir -p "$HOME/.config/snapcraft"
+if [ -z "$SNAPCRAFT_SECRET" ]; then
+    exit 0
+fi
 
-echo "$SNAPCRAFT_CONFIG" > "$HOME/.config/snapcraft/snapcraft.cfg"
+mkdir -p ".encrypted"
+if [ ! -e ".encrypted/snapcraft.cfg.enc" ]; then
+    echo "Seeding a new macaroon."
+    echo "$SNAPCRAFT_CONFIG" > ".encrypted/snapcraft.cfg.enc"
+fi
+
+mkdir -p "$HOME/.config/snapcraft"
+openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET -d -in ".encrypted/snapcraft.cfg.enc" -out "$HOME/.config/snapcraft/snapcraft.cfg"
 
 if [[ $TRAVIS_TAG ]]
  then
@@ -16,4 +25,7 @@ fi
 
 docker run -v $HOME:/root -v $SNAP_FOLDER:/cwd snapcore/snapcraft sh -c 'cd /cwd; snapcraft'
 docker run -v $HOME:/root -v $SNAP_FOLDER:/cwd -e CHANNEL=$CHANNEL snapcore/snapcraft sh -c "cd /cwd; snapcraft push *.snap --release $CHANNEL"
+
+openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET -out ".encrypted/snapcraft.cfg.enc" < "$HOME/.config/snapcraft/snapcraft.cfg"
+rm -f "$HOME/.config/snapcraft/snapcraft.cfg"
 
