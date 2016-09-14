83f8ff79d585a73794eb2b3ba62de1a49305028b
Alex Brazier <aejbrazier@gmail.com>
2016-09-14 21:06:01 +0100

    Make travis happy

--- server/startup/migrations/v061.js
+++ server/startup/migrations/v061.js
@@ -1,9 +1,9 @@
 RocketChat.Migrations.add({
 	version: 61,
 	up: function() {
-        RocketChat.models.Users.find({}).forEach((user) => {
-            RocketChat.models.Messages.updateAllNamesByUserId(user._id, user.name);
-            RocketChat.models.Subscriptions.setRealNameForDirectRoomsWithUsername(user.username, user.name);
-        });
+		RocketChat.models.Users.find({}).forEach((user) => {
+			RocketChat.models.Messages.updateAllNamesByUserId(user._id, user.name);
+			RocketChat.models.Subscriptions.setRealNameForDirectRoomsWithUsername(user.username, user.name);
+		});
 	}
 });
