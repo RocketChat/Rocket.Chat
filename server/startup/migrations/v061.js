40508b656d713fe04d16e383fe105f0d5438fbc1
Alex Brazier <aejbrazier@gmail.com>
2016-09-14 20:31:19 +0100

    Improve messages migration

--- server/startup/migrations/v061.js
+++ server/startup/migrations/v061.js
@@ -1,14 +1,8 @@
 RocketChat.Migrations.add({
 	version: 61,
 	up: function() {
-        let users = RocketChat.models.Users.find({}, { name: 1 }).fetch().reduce((o, v, i) => {
-            o[v._id] = v.name;
-            return o;
-        }, {});
-
-        RocketChat.models.Messages.find({}).forEach((message) => {
-            let name = users[message.u._id];
-            RocketChat.models.Messages.update({_id: message._id}, {$set: { 'u.name': name }});
+        RocketChat.models.Users.find({}).forEach((user) => {
+            RocketChat.models.Messages.update({'u._id': user._id}, {$set: { 'u.name': user.name }}, {multi: true});
         });
 	}
 });
