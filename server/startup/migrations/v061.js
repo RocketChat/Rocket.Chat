4ba5ab882e322ffee5d39e4783376de989d9ceda
Alex Brazier <aejbrazier@gmail.com>
2016-09-14 20:56:38 +0100

    Update subscriptions in migration

--- server/startup/migrations/v061.js
+++ server/startup/migrations/v061.js
@@ -2,7 +2,8 @@ RocketChat.Migrations.add({
 	version: 61,
 	up: function() {
         RocketChat.models.Users.find({}).forEach((user) => {
-            RocketChat.models.Messages.update({'u._id': user._id}, {$set: { 'u.name': user.name }}, {multi: true});
+            RocketChat.models.Messages.updateAllNamesByUserId(user._id, user.name);
+            RocketChat.models.Subscriptions.setRealNameForDirectRoomsWithUsername(user.username, user.name);
         });
 	}
 });
