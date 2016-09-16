6d87e021c4bd07c5bc213de418da03cd500c6670
Eduard Dumitru <alex_dorn@mail.com>
2016-09-16 15:58:53 -0400

    Modify for new livechat feature

--- packages/rocketchat-livechat/app/client/views/livechatWindow.js
+++ packages/rocketchat-livechat/app/client/views/livechatWindow.js
@@ -37,7 +37,19 @@ Template.livechatWindow.helpers({
 			offlineUnavailableMessage: Livechat.offlineUnavailableMessage.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2'),
 			displayOfflineForm: Livechat.displayOfflineForm
 		};
-	}
+	},
+  usersTyping() {
+    room_typing = MsgTyping.get(visitor.getRoom());
+    users = _.keys(room_typing.users);
+    if (room_typing.input_text == "")
+      return "";
+    input_text = " is Typing";
+
+    if (users.length ==  0)
+      return "";
+    if (users.length == 1)
+      return users[0] + input_text;
+  }
 });
 
 Template.livechatWindow.events({
