fac823a50652c814b1e0feacb5dc4b208b38c328
Alex Brazier <aejbrazier@gmail.com>
2016-09-16 20:28:19 +0100

    Add rejectUnauthorized to crowd login

--- packages/rocketchat-crowd/server/settings.js
+++ packages/rocketchat-crowd/server/settings.js
@@ -3,6 +3,7 @@ Meteor.startup(function() {
 		const enableQuery = {_id: 'CROWD_Enable', value: true};
 		this.add('CROWD_Enable', false, { type: 'boolean', public: true, i18nLabel: 'Enabled' });
 		this.add('CROWD_URL', '', { type: 'string', enableQuery: enableQuery, i18nLabel: 'URL' });
+		this.add('CROWD_Reject_Unauthorized', true, { type: 'boolean', enableQuery: enableQuery });
 		this.add('CROWD_APP_USERNAME', '', { type: 'string', enableQuery: enableQuery, i18nLabel: 'Username' });
 		this.add('CROWD_APP_PASSWORD', '', { type: 'string', enableQuery: enableQuery, i18nLabel: 'Password' });
 		this.add('CROWD_Sync_User_Data', false, { type: 'boolean', enableQuery: enableQuery, i18nLabel: 'Sync_Users' });
