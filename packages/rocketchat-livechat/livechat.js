5a6d57af69f125ce098f44ec64593752c492ea4a
Diego Sampaio <chinello@gmail.com>
2016-09-19 14:20:34 -0300

    Move livechat bundled assets to root app

--- packages/rocketchat-livechat/livechat.js
+++ packages/rocketchat-livechat/livechat.js
@@ -3,25 +3,28 @@
 WebApp = Package.webapp.WebApp;
 const Autoupdate = Package.autoupdate.Autoupdate;
 
-WebApp.connectHandlers.use('/livechat/', (req, res/*, next*/) => {
+WebApp.connectHandlers.use('/livechat/', Meteor.bindEnvironment((req, res, next) => {
+	if (req.url !== '/') {
+		return next();
+	}
 	res.setHeader('content-type', 'text/html; charset=utf-8');
 
 	const head = Assets.getText('public/head.html');
 
 	const html = `<html>
 		<head>
-			<link rel="stylesheet" type="text/css" class="__meteor-css__" href="/packages/rocketchat_livechat/public/livechat.css?_dc=${Autoupdate.autoupdateVersion}">
+			<link rel="stylesheet" type="text/css" class="__meteor-css__" href="/livechat/livechat.css?_dc=${Autoupdate.autoupdateVersion}">
 			<script type="text/javascript">
 				__meteor_runtime_config__ = ${JSON.stringify(__meteor_runtime_config__)};
 			</script>
-			<script type="text/javascript" src="/packages/rocketchat_livechat/public/livechat.jsi?_dc=${Autoupdate.autoupdateVersion}"></script>
 
 			${head}
 		</head>
 		<body>
+			<script type="text/javascript" src="/livechat/livechat.js?_dc=${Autoupdate.autoupdateVersion}"></script>
 		</body>
 	</html>`;
 
 	res.write(html);
 	res.end();
-});
+}));
