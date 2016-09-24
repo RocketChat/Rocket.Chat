245a52a8a54ac8bce2451caa4deb8830a543931e
Gabriel Engel <gabriel.engel@gmail.com>
2016-09-24 15:37:27 -0300

    improving travis cache

--- packages/rocketchat-livechat/plugin/build.sh
+++ packages/rocketchat-livechat/plugin/build.sh
@@ -1,19 +1,19 @@
 export METEOR_PRETTY_OUTPUT=0
+export LIVECHAT_DIR="../../../public/livechat"
+export BUILD_DIR="../build"
+export BUNDLE_DIR="../build/bundle/programs/web.browser"
+
 cd packages/rocketchat-livechat/app
 meteor npm install
-meteor build --directory .meteor/build/
-
-LIVECHAT_DIR="../../../public/livechat"
-BUILD_DIR=".meteor/build/bundle/programs/web.browser"
-
-rm -rf ../public/*
-mkdir -p ../public/
+meteor build --directory $BUILD_DIR
 
 rm -rf $LIVECHAT_DIR
 mkdir -p $LIVECHAT_DIR
+cp $BUNDLE_DIR/*.css $LIVECHAT_DIR/livechat.css
+cp $BUNDLE_DIR/*.js $LIVECHAT_DIR/livechat.js
 
-cp $BUILD_DIR/*.css $LIVECHAT_DIR/livechat.css
-cp $BUILD_DIR/*.js $LIVECHAT_DIR/livechat.js
-cp $BUILD_DIR/head.html ../public/head.html
+rm -rf ../public
+mkdir -p ../public
+cp $BUNDLE_DIR/head.html ../public/head.html
 
-rm -rf .meteor/build/
+rm -rf $BUILD_DIR
