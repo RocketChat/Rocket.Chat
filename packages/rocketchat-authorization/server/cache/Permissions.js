83ae805940472884517ee791a038705605ce4f58
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-09-30 18:15:08 -0300

    Move Permissions to new settings format

--- packages/rocketchat-authorization/server/cache/Permissions.js
+++
@@ -1,9 +0,0 @@
-/* eslint new-cap: 0 */
-
-RocketChat.cache.Permissions = new (class CachePermission extends RocketChat.cache._Base {
-	constructor() {
-		super('Permissions');
-	}
-});
-
-RocketChat.cache.Permissions.load();
