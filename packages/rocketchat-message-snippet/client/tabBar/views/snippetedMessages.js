0c9dba7868f1cba40fe9418c9d23acc417195ae9
Jean-Loup Adde <jean-loup.adde@eu.clara.net>
2016-10-06 13:44:15 +0100

    Move client tabs into tabBar folder.

--- packages/rocketchat-message-snippet/client/tabBar/views/snippetedMessages.js
+++ packages/rocketchat-message-snippet/client/tabBar/views/snippetedMessages.js
@@ -18,13 +18,9 @@ Template.snippetedMessages.onCreated(function() {
 	this.limit = new ReactiveVar(50);
 	let self = this;
 	this.autorun(function() {
-		console.log("Inside the autorun funciton.");
 		let data = Template.currentData();
-		console.log("self.limit: " + self.limit.get());
 		self.subscribe("snippetedMessages", data.rid, self.limit.get(), function() {
-			console.log("Bonjour");
 			if (SnippetedMessage.find({rid: data.rid}).count() < self.limit.get()) {
-				console.log("ADIO HASGORE");
 				return self.hasMore.set(false);
 			}
 		});
