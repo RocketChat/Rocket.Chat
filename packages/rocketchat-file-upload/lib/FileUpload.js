d516cea8e46f377d3b1125269f2e059337a5bc9b
Martin Schoeler <martin.schoeler@rocket.chat>
2016-11-07 09:50:25 -0200

    added option to remove the filesize restriction
    if max file size is set to -1 it will remove the  size restriction

--- packages/rocketchat-file-upload/lib/FileUpload.js
+++ packages/rocketchat-file-upload/lib/FileUpload.js
@@ -18,11 +18,13 @@ FileUpload = {
 			return false;
 		}
 
-		if (file.size > maxFileSize) {
-			const reason = TAPi18n.__('File_exceeds_allowed_size_of_bytes', {
-				size: filesize(maxFileSize)
-			}, user.language);
-			throw new Meteor.Error('error-file-too-large', reason);
+		if (maxFileSize !== -1) {
+			if (file.size > maxFileSize) {
+				const reason = TAPi18n.__('File_exceeds_allowed_size_of_bytes', {
+					size: filesize(maxFileSize)
+				}, user.language);
+				throw new Meteor.Error('error-file-too-large', reason);
+			}
 		}
 
 		if (!RocketChat.fileUploadIsValidContentType(file.type)) {
