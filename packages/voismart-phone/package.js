19262ed5491650a246e5f2943b7120c5da4ec566
matteo brancaleoni <mbrancaleoni@voismart.it>
2016-11-10 11:48:54 +0100

    Use Temasys AdapterJS and make it global, to properly wrap functions

--- packages/voismart-phone/package.js
+++ packages/voismart-phone/package.js
@@ -22,7 +22,6 @@ Package.onUse(function(api) {
 
 	api.addFiles([
             'client/tabBar.coffee',
-            'client/3rdparty/adapter-latest.js',
             'client/3rdparty/verto/jquery.json.min.js',
             'client/3rdparty/verto/jquery.jsonrpcclient.js',
             'client/3rdparty/verto/jquery.FSRTC.js',
