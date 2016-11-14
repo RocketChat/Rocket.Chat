c3ccdcc4414e2bda63898531b4d4740d07f16c68
Martin Schoeler <martin.schoeler@rocket.chat>
2016-11-14 16:35:19 -0200

    added option to disable file uploads in direct messages

--- packages/rocketchat-file-upload/lib/FileUpload.js
+++ packages/rocketchat-file-upload/lib/FileUpload.js
@@ -13,9 +13,15 @@ FileUpload = {
 
 		const user = Meteor.user();
 		const room = RocketChat.models.Rooms.findOneById(file.rid);
+		const directMessageAllow = RocketChat.settings.get('FileUpload_Enabled_Direct');
 
 		if (RocketChat.authz.canAccessRoom(room, user) !== true) {
 			return false;
+		}
+
+		if (!directMessageAllow && room.t === "d") {
+			const reason = TAPi18n.__('File_not_allowed_direct_messages', user.language);
+			throw new Meteor.Error('error-invalid-file-type', reason);
 		}
 
 		if (file.size > maxFileSize) {
