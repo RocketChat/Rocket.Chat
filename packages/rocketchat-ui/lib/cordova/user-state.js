4a6950e9f36c734d07b225f5e7f6c649c0a5bb1d
Flavio Grossi <flaviogrossi@gmail.com>
2016-11-22 15:31:10 +0100

    lower disconnect timeout and disable away on cordova
    The Meteor.disconnect() timeout cannot be too high or it may be not run because
    the application is already sleeping.
    
    setAway was commented because having a small time interval frame with own
    status set to away will interfere with call receveing.

--- packages/rocketchat-ui/lib/cordova/user-state.js
+++ packages/rocketchat-ui/lib/cordova/user-state.js
@@ -3,14 +3,14 @@
 var timer = undefined;
 if (Meteor.isCordova) {
 	document.addEventListener('pause', () => {
-		UserPresence.setAway();
+		// UserPresence.setAway();
 		readMessage.disable();
 
 		//Only disconnect after one minute of being in the background
 		timer = setTimeout(() => {
 			Meteor.disconnect();
 			timer = undefined;
-		}, 60000);
+		}, 10000);
 	}, true);
 
 	document.addEventListener('resume', () => {
