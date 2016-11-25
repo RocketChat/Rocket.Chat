81da71de37accf83c789881605259f20385b51b4
Bradley Hilton <graywolf336@gmail.com>
2016-11-25 16:51:57 -0600

    Add a warning about the hipchat enterprise with a link to the documentation

--- packages/rocketchat-importer-hipchat-enterprise/main.js
+++ packages/rocketchat-importer-hipchat-enterprise/main.js
@@ -2,5 +2,9 @@
 
 Importer.addImporter('hipchatenterprise', Importer.HipChatEnterprise, {
 	name: 'HipChat Enterprise',
+	warnings: [{
+		text: 'Importer_HipChatEnterprise_Information',
+		href: 'https://rocket.chat/docs/administrator-guides/import/hipchat/enterprise/'
+	}],
 	fileTypeRegex: new RegExp('application\/.*?gzip')
 });
