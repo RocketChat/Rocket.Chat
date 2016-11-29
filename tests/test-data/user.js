0d81b7ba58aead2903d3c3220c8f420fd36a035e
Martin Schoeler <martin.schoeler@rocket.chat>
2016-11-29 12:23:07 -0200

    fixes issues with first users in clear databases

--- tests/test-data/user.js
+++ tests/test-data/user.js
@@ -1,3 +1,7 @@
 export const username = 'user.test.'+Date.now();
 export const email = username+'@rocket.chat';
 export const password = 'rocket.chat';
+
+export const adminUsername = 'admin.test';
+export const adminEmail = adminUsername+'@rocket.chat';
+export const adminPassword = 'rocket.cat';
