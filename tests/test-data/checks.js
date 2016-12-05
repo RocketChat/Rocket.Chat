50678e5d2dbc2b1a40de6546a69cc158074ae812
Martin Schoeler <martin.schoeler@rocket.chat>
2016-12-05 15:38:31 -0200

    added a channel creation test

--- tests/test-data/checks.js
+++ tests/test-data/checks.js
@@ -2,6 +2,21 @@ import loginPage from '../pageobjects/login.page';
 import mainContent from '../pageobjects/main-content.page';
 import sideNav from '../pageobjects/side-nav.page';
 
+export var publicChannelCreated = false;
+export var privateChannelCreated = false;
+export var directMessageCreated = false;
+
+export function setPublicChannelCreated(status) {
+	publicChannelCreated = status;
+}
+
+export function setPrivateChannelCreated(status) {
+	privateChannelCreated = status;
+}
+
+export function setDirectMessageCreated(status) {
+	directMessageCreated = status;
+}
 
 export function checkIfUserIsValid(username, email, password) {
 	if (!sideNav.accountBoxUserName.isVisible()) {
