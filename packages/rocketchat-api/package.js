6542ea11f0bdfbf57037e71a7ef29f5c7b185ea9
Bradley Hilton <graywolf336@gmail.com>
2016-12-08 12:06:09 -0600

    Fix the chat.postMessage not returning any data about the sent message

--- packages/rocketchat-api/package.js
+++ packages/rocketchat-api/package.js
@@ -16,6 +16,7 @@ Package.onUse(function(api) {
 
 	api.addFiles('server/api.coffee', 'server');
 	api.addFiles('server/v1/channels.js', 'server');
+	api.addFiles('server/v1/chat.js', 'server');
 	api.addFiles('server/v1/groups.js', 'server');
 	api.addFiles('server/v1/im.js', 'server');
 	api.addFiles('server/v1/users.js', 'server');
--- packages/rocketchat-api/server/v1/chat.js
+++ packages/rocketchat-api/server/v1/chat.js
@@ -1,9 +1,49 @@
 /* global processWebhookMessage */
+RocketChat.API.v1.addRoute('chat.delete', { authRequired: true }, {
+	post: function() {
+		try {
+			check(this.bodyParams, {
+				msgId: String,
+				roomId: String,
+				asUser: Match.Maybe(Boolean)
+			});
+
+			const msg = RocketChat.models.Messages.findOneById(this.bodyParams.msgId, { fields: { u: 1, rid: 1 }});
+
+			if (!msg) {
+				return RocketChat.API.v1.failure(`No message found with the id of "${this.bodyParams.msgId}".`);
+			}
+
+			if (this.bodyParams.roomId !== msg.rid) {
+				return RocketChat.API.v1.failure('The room id provided does not match where the message is from.');
+			}
+
+			Meteor.runAsUser(this.bodyParams.asUser ? msg.u._id : this.userId, () => {
+				Meteor.call('deleteMessage', { _id: msg._id });
+			});
+
+			return RocketChat.API.v1.success({
+				_id: msg._id,
+				ts: Date.now()
+			});
+		} catch (e) {
+			return RocketChat.API.v1.failure(e.name + ': ' + e.message);
+		}
+	}
+});
+
 RocketChat.API.v1.addRoute('chat.postMessage', { authRequired: true }, {
 	post: function() {
 		try {
+			if (!this.bodyParams.attachments) {
+				check(this.bodyParams, {
+					channel: String,
+					text: String
+				});
+			}
+
 			//TODO: Completely rewrite this? Seems too "magical"
-			const messageReturn = processWebhookMessage(this.bodyParams, this.user);
+			const messageReturn = processWebhookMessage(this.bodyParams, this.user)[0];
 
 			if (!messageReturn) {
 				return RocketChat.API.v1.failure('unknown-error');
@@ -15,7 +55,7 @@ RocketChat.API.v1.addRoute('chat.postMessage', { authRequired: true }, {
 				message: messageReturn.message
 			});
 		} catch (e) {
-			return RocketChat.API.v1.failure(e.error);
+			return RocketChat.API.v1.failure(e.name + ': ' + e.message);
 		}
 	}
 });
