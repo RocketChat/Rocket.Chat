7ee19cf5e793c1bba5f7b01a035e05caf65eb39d
Bradley Hilton <graywolf336@gmail.com>
2016-12-12 14:18:45 -0600

    Show the reaction button on the action buttons. Also, fix a bug that would allow you to react to private messages only

--- packages/rocketchat-reactions/client/init.js
+++ packages/rocketchat-reactions/client/init.js
@@ -57,9 +57,11 @@ Meteor.startup(function() {
 			let room = RocketChat.models.Rooms.findOne({ _id: message.rid });
 			let user = Meteor.user();
 
-			if (Array.isArray(room.muted) && room.muted.indexOf(user.username) !== -1) {
+			if (Array.isArray(room.muted) && room.muted.indexOf(user.username) !== -1 && !room.reactWhenReadOnly) {
 				return false;
 			} else if (!RocketChat.models.Subscriptions.findOne({ rid: message.rid })) {
+				return false;
+			} else if (message.private) {
 				return false;
 			}
 
