379e4cf05ce04b21ce8888b400971effbf6ec82a
Diego Sampaio <chinello@gmail.com>
2016-12-16 13:49:02 -0200

    Change livechat widget state on popout

--- packages/rocketchat-livechat/assets/rocket-livechat.js
+++ packages/rocketchat-livechat/assets/rocket-livechat.js
@@ -6,16 +6,6 @@
 	var hookQueue = [];
 	var ready = false;
 
-	var closeWidget = function() {
-		widget.dataset.state = 'closed';
-		widget.style.height = '30px';
-	};
-
-	var openWidget = function() {
-		widget.dataset.state = 'opened';
-		widget.style.height = '300px';
-	};
-
 	// hooks
 	var callHook = function(action, params) {
 		if (!ready) {
@@ -27,6 +17,18 @@
 			args: params
 		};
 		iframe.contentWindow.postMessage(data, '*');
+	};
+
+	var closeWidget = function() {
+		widget.dataset.state = 'closed';
+		widget.style.height = '30px';
+		callHook('widgetClosed');
+	};
+
+	var openWidget = function() {
+		widget.dataset.state = 'opened';
+		widget.style.height = '300px';
+		callHook('widgetOpened');
 	};
 
 	var api = {
@@ -42,10 +44,8 @@
 		toggleWindow: function(/*forceClose*/) {
 			if (widget.dataset.state === 'closed') {
 				openWidget();
-				callHook('widgetOpened');
 			} else {
 				closeWidget();
-				callHook('widgetClosed');
 			}
 		},
 		openPopout: function() {
