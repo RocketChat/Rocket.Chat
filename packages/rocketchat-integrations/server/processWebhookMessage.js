afcf012be83dc58658118fd41b2bed9cb4170f06
Bradley Hilton <graywolf336@gmail.com>
2016-12-16 05:59:57 -0600

    Several changes to allow for easier Integrations from others.
    * getFullUserData now accepts a limit of -1 which will retrieve all of the users, but you only see what you have permission to.
    * processWebhookMessage now accepts a roomId for rooms or user, that way integrations don't have to know the name of the channel.
    * Fixed the chat.postMessage being broke for messages that contain more than channel and text...whoops!

--- packages/rocketchat-integrations/server/processWebhookMessage.js
+++ packages/rocketchat-integrations/server/processWebhookMessage.js
@@ -1,5 +1,47 @@
+function retrieveRoomInfo({ userId, channel, ignoreEmpty=false }) {
+	const room = RocketChat.models.Rooms.findOneByIdOrName(channel);
+	if (!_.isObject(room) && !ignoreEmpty) {
+		throw new Meteor.Error('invalid-channel');
+	}
+
+	if (room && room.t === 'c') {
+		Meteor.runAsUser(userId, function() {
+			return Meteor.call('joinRoom', room._id);
+		});
+	}
+
+	return room;
+}
+
+function retrieveDirectMessageInfo({ userId, channel }) {
+	const roomUser = RocketChat.models.Users.findOne({
+		$or: [
+			{
+				_id: channel
+			}, {
+				username: channel
+			}
+		]
+	}) || {};
+
+	const rid = [userId, roomUser._id].sort().join('');
+	let room = RocketChat.models.Rooms.findOneById({$in: [rid, channel]});
+	if (!_.isObject(roomUser) && !_.isObject(room)) {
+		throw new Meteor.Error('invalid-channel');
+	}
+
+	if (!room) {
+		Meteor.runAsUser(userId, function() {
+			Meteor.call('createDirectMessage', roomUser.username);
+			room = RocketChat.models.Rooms.findOneById(rid);
+		});
+	}
+
+	return room;
+}
+
 this.processWebhookMessage = function(messageObj, user, defaultValues) {
-	var attachment, channel, channels, channelType, i, len, message, ref, rid, room, roomUser, ret;
+	var attachment, channel, channels, channelType, i, len, message, ref, room, ret;
 	ret = [];
 
 	if (!defaultValues) {
@@ -11,7 +53,7 @@ this.processWebhookMessage = function(messageObj, user, defaultValues) {
 		};
 	}
 
-	channel = messageObj.channel || defaultValues.channel;
+	channel = messageObj.channel || messageObj.roomId || defaultValues.channel;
 
 	channels = [].concat(channel);
 
@@ -22,41 +64,26 @@ this.processWebhookMessage = function(messageObj, user, defaultValues) {
 
 		switch (channelType) {
 			case '#':
-				room = RocketChat.models.Rooms.findOneByIdOrName(channel);
-				if (!_.isObject(room)) {
-					throw new Meteor.Error('invalid-channel');
-				}
-				rid = room._id;
-				if (room.t === 'c') {
-					Meteor.runAsUser(user._id, function() {
-						return Meteor.call('joinRoom', room._id);
-					});
-				}
+				room = retrieveRoomInfo({ userId: user._id, channel });
 				break;
 			case '@':
-				roomUser = RocketChat.models.Users.findOne({
-					$or: [
-						{
-							_id: channel
-						}, {
-							username: channel
-						}
-					]
-				}) || {};
-				rid = [user._id, roomUser._id].sort().join('');
-				room = RocketChat.models.Rooms.findOneById({$in: [rid, channel]});
-				if (!_.isObject(roomUser) && !_.isObject(room)) {
-					throw new Meteor.Error('invalid-channel');
-				}
-				if (!room) {
-					Meteor.runAsUser(user._id, function() {
-						Meteor.call('createDirectMessage', roomUser.username);
-						room = RocketChat.models.Rooms.findOneById(rid);
-					});
-				}
+				room = retrieveDirectMessageInfo({ userId: user._id, channel });
 				break;
 			default:
-				throw new Meteor.Error('invalid-channel-type');
+				//Try to find the room by id or name if they didn't include the prefix.
+				room = retrieveRoomInfo({ userId: user._id, channel: channelType + channel, ignoreEmpty: true });
+				if (room) {
+					break;
+				}
+
+				//We didn't get a room, let's try finding direct messages
+				room = retrieveDirectMessageInfo({ userId: user._id, channel: channelType + channel });
+				if (room) {
+					break;
+				}
+
+				//No room, so throw an error
+				throw new Meteor.Error('invalid-channel');
 		}
 
 		if (messageObj.attachments && !_.isArray(messageObj.attachments)) {
