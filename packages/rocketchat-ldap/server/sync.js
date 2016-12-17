07b22b7391eab57113e5bd722256bec6aab4823c
Gabriel Engel <gabriel.engel@gmail.com>
2016-12-17 00:49:55 -0200

    fix lint

--- packages/rocketchat-ldap/server/sync.js
+++ packages/rocketchat-ldap/server/sync.js
@@ -221,8 +221,7 @@ sync = function sync() {
 
 				if (!user) {
 					addLdapUser(ldapUser, username);
-				}
-				else if (user.ldap !== true && RocketChat.settings.get('LDAP_Merge_Existing_Users') === true) {
+				} else if (user.ldap !== true && RocketChat.settings.get('LDAP_Merge_Existing_Users') === true) {
 					syncUserData(user, ldapUser);
 				}
 			});
