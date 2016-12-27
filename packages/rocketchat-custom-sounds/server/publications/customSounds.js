2bf498c8d61270534d6bd8ca9960fd4e8479f2c9
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-12-27 17:57:04 -0200

    Custom Sounds admin sections

---
+++ packages/rocketchat-custom-sounds/server/publications/customSounds.js
@@ -0,0 +1,24 @@
+Meteor.publish('customSounds', function(filter, limit) {
+	if (!this.userId) {
+		return this.ready();
+	}
+
+	let fields = {
+		name: 1
+	};
+
+	filter = s.trim(filter);
+
+	let options = {
+		fields,
+		limit,
+		sort: { name: 1 }
+	};
+
+	if (filter) {
+		let filterReg = new RegExp(s.escapeRegExp(filter), 'i');
+		return RocketChat.models.CustomSounds.findByName(filterReg, options);
+	}
+
+	return RocketChat.models.CustomSounds.find({}, options);
+});
