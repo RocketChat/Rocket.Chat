19d2461193e8c41476e1dfff4eb3e1bbce9b1196
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2016-12-29 10:04:58 -0200

    Revert migration 77

--- server/startup/migrations/v077.js
+++ server/startup/migrations/v077.js
@@ -1,11 +1,23 @@
 RocketChat.Migrations.add({
 	version: 77,
 	up: function() {
-		if (RocketChat && RocketChat.models && RocketChat.models.Users) {
-			RocketChat.models.Users.update({ newMessageNotification: false }, { $set: { newMessageNotification: 'none' } }, { multi: true });
-			RocketChat.models.Users.update({ newMessageNotification: true }, { $unset: { newMessageNotification: 1 } }, { multi: true });
-			RocketChat.models.Users.update({ newRoomNotification: false }, { $set: { newRoomNotification: 'none' } }, { multi: true });
-			RocketChat.models.Users.update({ newRoomNotification: true }, { $unset: { newRoomNotification: 1 } }, { multi: true });
+		if (RocketChat && RocketChat.models && RocketChat.models.Rooms) {
+			RocketChat.models.Rooms.find({
+				t: 'l',
+				'v._id': { $exists: true },
+				'v.username': { $exists: false }
+			}, { fields: { 'v._id': 1 } }).forEach(function(room) {
+				const user = RocketChat.models.Users.findOne({ _id: room.v._id }, { username: 1 });
+
+				RocketChat.models.Rooms.update({
+					_id: room._id
+				}, {
+					$set: {
+						'v.username': user.username
+					}
+				});
+			});
 		}
 	}
 });
+
