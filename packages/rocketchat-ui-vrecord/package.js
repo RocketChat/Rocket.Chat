e228e4f671ec3e984292f1d179283d3823517676
Karl Prieb <karl@prieb.me>
2017-01-04 20:00:45 -0200

    remove transition mixin/move all less files to build

--- packages/rocketchat-ui-vrecord/package.js
+++ packages/rocketchat-ui-vrecord/package.js
@@ -18,12 +18,11 @@ Package.onUse(function(api) {
 	]);
 
 
-	api.addAssets('client/vrecord.less', 'server');
+	api.addFiles('client/vrecord.less', 'client');
 
 	api.addFiles('client/vrecord.html', 'client');
 	api.addFiles('client/vrecord.coffee', 'client');
 	api.addFiles('client/VRecDialog.coffee', 'client');
 
 	api.addFiles('server/settings.coffee', 'server');
-	api.addFiles('loadStylesheet.coffee', 'server');
 });
