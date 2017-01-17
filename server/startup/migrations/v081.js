b7c60ae264521ba4e8daafed371f235e82dbd75b
Bradley Hilton <graywolf336@craftyn.com>
2017-01-17 17:20:54 -0200

    Bump the migration number and fix the whitespacing in the json

--- server/startup/migrations/v081.js
+++ server/startup/migrations/v081.js
@@ -1,5 +1,5 @@
 RocketChat.Migrations.add({
-	version: 80,
+	version: 81,
 	up: function() {
 		RocketChat.models.Rooms.find().forEach((room) => {
 			RocketChat.models.Rooms.incMsgCountById(room._id, RocketChat.models.Messages.find({ rid: room._id, t: { $exists: true }}).count());
