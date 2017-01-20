844ed9190e85cae9aff14219de10084df0e6a83d
Bradley Hilton <graywolf336@craftyn.com>
2017-01-20 18:35:03 -0200

    Move the migration to a newer version.

--- server/startup/migrations/v082.js
+++ server/startup/migrations/v082.js
@@ -1,5 +1,5 @@
 RocketChat.Migrations.add({
-	version: 81,
+	version: 82,
 	up: function() {
 		RocketChat.models.Rooms.find().forEach((room) => {
 			RocketChat.models.Rooms.incMsgCountById(room._id, RocketChat.models.Messages.find({ rid: room._id, t: { $exists: true }}).count());
