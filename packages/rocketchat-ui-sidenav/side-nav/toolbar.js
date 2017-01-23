c1fe7b80a252d2f48e191ecceb99c567536d460e
Karl Prieb <karl@prieb.me>
2017-01-23 17:24:02 -0200

    toolbar search

--- packages/rocketchat-ui-sidenav/side-nav/toolbar.js
+++ packages/rocketchat-ui-sidenav/side-nav/toolbar.js
@@ -1,12 +1,15 @@
-this.toolbarSearch = {
+const toolbarSearch = {
 	clear: () => {
-		$('.toolbar--search').val('');
+		$('.toolbar-search__input').val('');
+		console.log('clear');
 	},
 	focus: () => {
-		$('.toolbar--search').val('');
-		$('.toolbar--search').focus();
+		$('.toolbar-search__input').val('');
+		$('.toolbar-search__input').focus();
 	}
 };
+
+this.toolbarSearch = toolbarSearch;
 
 const getFromServer = (filter, usernames, records, cb) => {
 	Meteor.call('spotlight', filter, usernames, (err, results) => {
@@ -55,8 +58,8 @@ Template.toolbar.helpers({
 		const config = {
 			cls: 'search-results-list',
 			collection: RocketChat.models.Subscriptions,
-			template: 'spotlightTemplate',
-      		input: '.toolbar--search',
+			template: 'toolbarSearchList',
+      		input: '.toolbar-search__input',
 			getFilter: function(collection, filter, cb) {
 				const resultsFromClient = collection.find({name: new RegExp((RegExp.escape(filter)), 'i'), rid: {$ne: Session.get('openedRoom')}}, {limit: 10, sort: {unread: -1, ls: -1}}).fetch();
 
@@ -77,7 +80,7 @@ Template.toolbar.helpers({
 				const doc = _.findWhere(records, {_id: _id});
 
 				RocketChat.roomTypes.openRouteLink(doc.t, doc, FlowRouter.current().queryParams);
-				toolbarSearch.hide();
+				toolbarSearch.clear();
       		}
 		};
 
@@ -85,74 +88,35 @@ Template.toolbar.helpers({
 	}
 });
 
-// Template.toolbar.events({
-// 	'keyup .toolbar--search, change .toolbar--search': (e, t) => {
-// 		const searchVal = $(e.currentTarget).val().trim();
+Template.toolbar.events({
+	'click .toolbar-search__icon--cancel': () => {
+		$('.toolbar-search__input').trigger({
+			type: 'keyup',
+			which: 27
+		});
+		toolbarSearch.clear();
+	},
+	'keydown .toolbar-search__input': (event) => {
+		if (event.which === 13) {
+			$('.toolbar-search__input').trigger({
+				type: 'keyup',
+				which: 27
+			});
+			toolbarSearch.clear();
+		}
+	}
+});
 
-// 		if (!$('.toolbar').hasClass('shortcut')) {
-// 			if (searchVal === '') {
-// 				$('.toolbar').removeClass('active');
-// 				t.resultsList.set([]);
-// 				return false;
-// 			}
-// 		}
+Template.toolbarSearchList.helpers({
+	icon() {
+		return RocketChat.roomTypes.getIcon(this.t);
+	},
 
-// 		$('.toolbar').addClass('active');
-
-// 		const resultsFromClient = RocketChat.models.Subscriptions
-// 			.find({name: new RegExp((RegExp.escape(searchVal)), 'i'), rid: {$ne: Session.get('openedRoom')}}, {limit: 10, sort: {unread: -1, ls: -1}}).fetch();
-
-// 		t.resultsList.set(resultsFromClient);
-
-
-// 		const resultsFromClientLength = resultsFromClient.length;
-// 		const usernamesFromClient = [Meteor.user().username];
-
-// 		for (let i = 0; i < resultsFromClientLength; i++) {
-// 			if (resultsFromClient[i].t === 'd') {
-// 				usernamesFromClient.push(resultsFromClient[i].name);
-// 			}
-// 		}
-
-// 		t.getFromServer(searchVal, usernamesFromClient);
-// 	}
-// });
-
-// Template.toolbar.onCreated(function() {
-// 	this.resultsList = new ReactiveVar([]);
-// 	this.getFromServer = _.throttle((filter, usernames) => {
-// 		Meteor.call('spotlight', filter, usernames, (err, results) => {
-// 			if (err) {
-// 				console.log(err);
-// 				return false;
-// 			}
-
-// 			const resultsFromServer = [];
-// 			const usersLength = results.users.length;
-// 			const roomsLength = results.rooms.length;
-
-// 			if (usersLength) {
-// 				for (let i = 0; i < usersLength; i++) {
-// 					resultsFromServer.push({
-// 						_id: results.users[i]._id,
-// 						t: 'd',
-// 						name: results.users[i].username
-// 					});
-// 				}
-// 			}
-
-// 			if (roomsLength) {
-// 				for (let i = 0; i < roomsLength; i++) {
-// 					resultsFromServer.push({
-// 						_id: results.rooms[i]._id,
-// 						t: results.rooms[i].t,
-// 						name: results.rooms[i].name
-// 					});
-// 				}
-// 			}
-
-// 			this.resultsList.set(this.resultsList.get().concat(resultsFromServer));
-// 			console.log(this.resultsList.get());
-// 		});
-// 	}, 500);
-// });
+	userStatus() {
+		if (this.t === 'd') {
+			return 'status-' + (Session.get(`user_${this.name}_status`) || 'offline');
+		} else {
+			return 'status-' + (RocketChat.roomTypes.getUserStatus(this.t, this.rid || this._id) || 'offline');
+		}
+	}
+});
