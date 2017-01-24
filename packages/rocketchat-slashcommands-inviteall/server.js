94c009b04ec8b4ff614f8f810e144a55a84e60f2
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-01-24 20:34:37 -0200

    splitted the slash command /invite-all into /invite-all-to and /invite-all-from

--- packages/rocketchat-slashcommands-inviteall/server.js
+++ packages/rocketchat-slashcommands-inviteall/server.js
@@ -2,49 +2,70 @@
  * Invite is a named function that will replace /invite commands
  * @param {Object} message - The message object
  */
-function InviteAll(command, params, item) {
-	if (command !== 'invite-all' || !Match.test(params, String)) {
-		return;
-	}
-	let regexp = /#([\d-_\w]+)/g;
-	let [, channel] = regexp.exec(params.trim());
-	if (!channel) {
-		return;
-	}
-	let users = RocketChat.models.Rooms.findOneById(item.rid).usernames || [];
-	let currentUser = Meteor.users.findOne(Meteor.userId());
 
-	if (users.length > RocketChat.settings.get('API_User_Limit')) {
-    return RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
-      _id: Random.id(),
-      rid: item.rid,
-      ts: new Date(),
-      msg: TAPi18n.__('error-user-limit-exceeded', null, currentUser.language)
-    });		
-	}
+function InviteAll(type) {
 
-	let room = RocketChat.models.Rooms.findOneByName(channel);
-	if (!room) {
-		return Meteor.call('createChannel', channel, users);
-	}
+	return function InviteAll(command, params, item) {
+		// if (/invite\-all/.test(command) || !Match.test(params, String)) {
+		// 	return;
+		// }
+		let regexp = /#([\d-_\w]+)/g,
+			[, channel] = regexp.exec(params.trim());
 
-	users.forEach(function(user) {
-		try {
-			Meteor.call('addUserToRoom', {
-				rid: room._id,
-				username: user
-			});
-		} catch (e) {
-			let msg = e.error === 'cant-invite-for-direct-room' ? 'Cannot_invite_users_to_direct_rooms' : e.error;
-			RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
+		if (!channel) {
+			return;
+		}
+
+		let currentUser = Meteor.users.findOne(Meteor.userId());
+		let baseChannel = type === 'to' ? RocketChat.models.Rooms.findOneById(item.rid) : RocketChat.models.Rooms.findOneByName(channel);
+		let targetChannel = type === 'from' ? RocketChat.models.Rooms.findOneById(item.rid) : RocketChat.models.Rooms.findOneByName(channel);
+
+		if (!baseChannel) {
+			return RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
 				_id: Random.id(),
 				rid: item.rid,
 				ts: new Date(),
-				msg: TAPi18n.__(msg, null, currentUser.language)
+				msg: TAPi18n.__('Channel_doesnt_exist', {
+					postProcess: 'sprintf',
+					sprintf: [channel]
+				}, currentUser.language)
 			});
 		}
-	});
+
+		let users = baseChannel.usernames || [];
+
+		if (users.length > RocketChat.settings.get('API_User_Limit')) {
+			return RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
+				_id: Random.id(),
+				rid: item.rid,
+				ts: new Date(),
+				msg: TAPi18n.__('error-user-limit-exceeded', null, currentUser.language)
+			});
+		}
+
+		if (!targetChannel) {
+			return Meteor.call('createChannel', channel, users);
+		}
+
+		users.forEach(function(user) {
+			try {
+				Meteor.call('addUserToRoom', {
+					rid: targetChannel._id,
+					username: user
+				});
+			} catch (e) {
+				let msg = e.error === 'cant-invite-for-direct-room' ? 'Cannot_invite_users_to_direct_rooms' : e.error;
+				RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
+					_id: Random.id(),
+					rid: item.rid,
+					ts: new Date(),
+					msg: TAPi18n.__(msg, null, currentUser.language)
+				});
+			}
+		});
+	}
 }
 
-RocketChat.slashCommands.add('invite-all', InviteAll);
+RocketChat.slashCommands.add('invite-all-to', InviteAll('to'));
+RocketChat.slashCommands.add('invite-all-from', InviteAll('from'));
 module.exports = InviteAll;
