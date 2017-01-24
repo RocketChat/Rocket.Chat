621b499fc0f88a4d92b3683688b9cce025f0bbd4
Bradley Hilton <graywolf336@craftyn.com>
2017-01-24 18:52:41 -0200

    Move the stats migration to v83

--- server/startup/migrations/v082.js
+++
@@ -1,8 +0,0 @@
-RocketChat.Migrations.add({
-	version: 82,
-	up: function() {
-		RocketChat.models.Rooms.find().forEach((room) => {
-			RocketChat.models.Rooms.incMsgCountById(room._id, RocketChat.models.Messages.find({ rid: room._id, t: { $exists: true }}).count());
-		});
-	}
-});
