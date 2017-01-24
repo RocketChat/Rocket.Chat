1fabcfa241ca0ba9e9a2e8e1cc34b32ae73b651a
S. Li <sli@makawave.com>
2017-01-24 00:33:41 -0500

    fix eslint complaints

--- packages/rocketchat-api/server/default/info.js
+++ packages/rocketchat-api/server/default/info.js
@@ -1,7 +1,3 @@
-var client = require('prom-client');
- 
-var defaultMetrics = client.defaultMetrics;
-
 RocketChat.API.default.addRoute('info', { authRequired: false }, {
 	get: function() {
 		return RocketChat.Info;
