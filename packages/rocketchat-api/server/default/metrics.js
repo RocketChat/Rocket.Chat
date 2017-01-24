1c05d4f32b661b80a32ed682bb6557b814efc8d0
S. Li <sli@makawave.com>
2017-01-24 00:33:41 -0500

    fix eslint complaints

--- packages/rocketchat-api/server/default/metrics.js
+++ packages/rocketchat-api/server/default/metrics.js
@@ -1,5 +1,8 @@
 RocketChat.API.default.addRoute('metrics', { authRequired: false }, {
-	get: function() {
-    return { headers: { 'Content-Type': 'text/plain' }, body: RocketChat.promclient.register.metrics()};
-}
+	get:	function() {
+		return {
+			headers: { 'Content-Type': 'text/plain' },
+			body: RocketChat.promclient.register.metrics()
+		};
+	}
 });
