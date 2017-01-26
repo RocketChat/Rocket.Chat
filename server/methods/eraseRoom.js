721cea765ddb47b12c1e514840c6e9fb4fce1865
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-01-26 18:46:49 -0200

    Convert /server/methods to JS

---
+++ server/methods/eraseRoom.js
@@ -0,0 +1,30 @@
+Meteor.methods({
+	eraseRoom(rid) {
+		check(rid, String);
+
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
+				method: 'eraseRoom'
+			});
+		}
+
+		const fromId = Meteor.userId();
+		const room = RocketChat.models.Rooms.findOneById(rid);
+
+		if (!room) {
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', {
+				method: 'eraseRoom'
+			});
+		}
+
+		if (RocketChat.authz.hasPermission(fromId, `delete-${room.t}`, rid)) {
+			RocketChat.models.Messages.removeByRoomId(rid);
+			RocketChat.models.Subscriptions.removeByRoomId(rid);
+			return RocketChat.models.Rooms.removeById(rid);
+		} else {
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', {
+				method: 'eraseRoom'
+			});
+		}
+	}
+});
