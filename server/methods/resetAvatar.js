f5c8134578fe949f0c936141ae1db12a8980e49a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-01-26 20:34:32 -0200

    Fix ESLint

--- server/methods/resetAvatar.js
+++ server/methods/resetAvatar.js
@@ -1,29 +1,30 @@
 Meteor.methods({
-  resetAvatar() {
-    var user;
-    if (!Meteor.userId()) {
-      throw new Meteor.Error('error-invalid-user', 'Invalid user', {
-        method: 'resetAvatar'
-      });
-    }
-    if (!RocketChat.settings.get("Accounts_AllowUserAvatarChange")) {
-      throw new Meteor.Error('error-not-allowed', 'Not allowed', {
-        method: 'resetAvatar'
-      });
-    }
-    user = Meteor.user();
-    RocketChatFileAvatarInstance.deleteFile(user.username + ".jpg");
-    RocketChat.models.Users.unsetAvatarOrigin(user._id);
-    RocketChat.Notifications.notifyLogged('updateAvatar', {
-      username: user.username
-    });
-  }
+	resetAvatar() {
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
+				method: 'resetAvatar'
+			});
+		}
+
+		if (!RocketChat.settings.get('Accounts_AllowUserAvatarChange')) {
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', {
+				method: 'resetAvatar'
+			});
+		}
+
+		const user = Meteor.user();
+		RocketChatFileAvatarInstance.deleteFile(`${user.username}.jpg`);
+		RocketChat.models.Users.unsetAvatarOrigin(user._id);
+		RocketChat.Notifications.notifyLogged('updateAvatar', {
+			username: user.username
+		});
+	}
 });
 
 DDPRateLimiter.addRule({
-  type: 'method',
-  name: 'resetAvatar',
-  userId() {
-    return true;
-  }
+	type: 'method',
+	name: 'resetAvatar',
+	userId() {
+		return true;
+	}
 }, 1, 60000);
