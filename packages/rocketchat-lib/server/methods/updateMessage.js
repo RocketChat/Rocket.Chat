b570ead43af0cd294877ade86c118850c5f80fc9
Kiran Rao <kiran_rao@outlook.com>
2017-01-28 16:31:55 -0500

    Convert lib server methods to JS

---
+++ packages/rocketchat-lib/server/methods/updateMessage.js
@@ -0,0 +1,36 @@
+Meteor.methods({
+	updateMessage(message) {
+
+		check(message, Match.ObjectIncluding({_id:String}));
+
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'updateMessage' });
+		}
+
+		const originalMessage = RocketChat.models.Messages.findOneById(message._id);
+
+		if (!originalMessage && !originalMessage._id) {
+			return;
+		}
+
+		const hasPermission = RocketChat.authz.hasPermission(Meteor.userId(), 'edit-message', message.rid);
+		const editAllowed = RocketChat.settings.get('Message_AllowEditing');
+		const editOwn = originalMessage && originalMessage.u && (originalMessage.u._id === Meteor.userId());
+
+		if (!hasPermission && (!editAllowed || !editOwn)) {
+			throw new Meteor.Error('error-action-not-allowed', 'Message editing not allowed', { method: 'updateMessage', action: 'Message_editing' });
+		}
+
+		const blockEditInMinutes = RocketChat.settings.get('Message_AllowEditing_BlockEditInMinutes');
+		if ((blockEditInMinutes != null) && (blockEditInMinutes !== 0)) {
+			let currentTsDiff, msgTs;
+			if (originalMessage.ts != null) { msgTs = moment(originalMessage.ts); }
+			if (msgTs != null) { currentTsDiff = moment().diff(msgTs, 'minutes'); }
+			if (currentTsDiff > blockEditInMinutes) {
+				throw new Meteor.Error('error-message-editing-blocked', 'Message editing is blocked', { method: 'updateMessage' });
+			}
+		}
+
+		return RocketChat.updateMessage(message, Meteor.user());
+	}
+});
