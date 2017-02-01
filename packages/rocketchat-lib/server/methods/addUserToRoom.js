b1fd68d8b41a3600634298905e035ed390669a3c
timkinnane <tim@nestedcode.com>
2017-02-01 13:57:18 +1100

    Refactor addUser/s methods - remove coffescript

--- packages/rocketchat-lib/server/methods/addUserToRoom.js
+++ packages/rocketchat-lib/server/methods/addUserToRoom.js
@@ -1,36 +1,8 @@
 Meteor.methods({
-	addUserToRoom(data) {
-		if (!Meteor.userId()) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'addUserToRoom' });
-		}
-
-		if (!Match.test(data && data.rid, String)) {
-			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'addUserToRoom' });
-		}
-
-		if (!Match.test(data && data.username, String)) {
-			throw new Meteor.Error('error-invalid-username', 'Invalid username', { method: 'addUserToRoom' });
-		}
-
-		const room = RocketChat.models.Rooms.findOneById(data.rid);
-
-		if (room.usernames.indexOf(Meteor.user().username) === -1) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'addUserToRoom' });
-		}
-
-		const fromId = Meteor.userId();
-		if (!RocketChat.authz.hasPermission(fromId, 'add-user-to-room', room._id)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'addUserToRoom' });
-		}
-
-		if (room.t === 'd') {
-			throw new Meteor.Error('error-cant-invite-for-direct-room', 'Can\'t invite user to direct rooms', { method: 'addUserToRoom' });
-		}
-
-
-		const newUser = RocketChat.models.Users.findOneByUsername(data.username);
-		RocketChat.addUserToRoom(data.rid, newUser, Meteor.user());
-
-		return true;
+	addUserToRoom: function(data) {
+		return Meteor.call('addUsersToRoom', {
+			rid: data.rid,
+			users: [ data.username ]
+		});
 	}
 });
