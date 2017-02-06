61d9d265b72cc5c557e63ea540a38edcce9b35af
Bradley Hilton <graywolf336@craftyn.com>
2017-02-06 18:29:50 -0200

    Add the missing file for the IntegrationHistory

---
+++ packages/rocketchat-integrations/server/models/IntegrationHistory.js
@@ -0,0 +1,25 @@
+RocketChat.models.IntegrationHistory = new class IntegrationHistory extends RocketChat.models._Base {
+	constructor() {
+		super('integration_history');
+	}
+
+	findByType(type, options) {
+		if (type !== 'outgoing-webhook' || type !== 'incoming-webhook') {
+			throw new Meteor.Error('invalid-integration-type');
+		}
+
+		return this.find({ type }, options);
+	}
+
+	findByIntegrationId(id, options) {
+		return this.find({ 'integration._id': id }, options);
+	}
+
+	findByEventName(event, options) {
+		return this.find({ event }, options);
+	}
+
+	findFailed(options) {
+		return this.find({ error: true }, options);
+	}
+};
