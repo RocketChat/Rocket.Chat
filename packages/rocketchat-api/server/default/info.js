bdde1ff5c2fae732531ba8770d473a777321a23a
Jared Hilton <jaredhilton@me.com>
2017-02-07 12:15:54 -0600

    Fix 3651; get user role with user-id in headers

--- packages/rocketchat-api/server/default/info.js
+++ packages/rocketchat-api/server/default/info.js
@@ -1,5 +1,13 @@
 RocketChat.API.default.addRoute('info', { authRequired: false }, {
 	get: function() {
-		return RocketChat.Info;
+		if (this.request.headers['x-user-id'] != null && RocketChat.authz.hasRole(this.request.headers['x-user-id'], 'admin')) {
+			return RocketChat.Info;
+		}
+
+		return RocketChat.API.v1.success({
+			info: {
+				"version": RocketChat.Info.version
+			}
+		});
 	}
 });
