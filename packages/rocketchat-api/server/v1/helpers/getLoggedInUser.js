60ba24db843aa3531278736367149fc66ff76815
jaredhilton <jaredhilton@me.com>
2017-02-08 13:35:19 -0600

    Updates getLoggedInUser to get user by token

--- packages/rocketchat-api/server/v1/helpers/getLoggedInUser.js
+++ packages/rocketchat-api/server/v1/helpers/getLoggedInUser.js
@@ -1,9 +1,31 @@
 RocketChat.API.v1.helperMethods.set('getLoggedInUser', function _getLoggedInUser() {
+	let token;
 	let user;
 
-	if (this.request.headers['x-user-id'] && this.request.headers['x-auth-token']) {
-		user = RocketChat.models.Users.findOneById(this.request.headers['x-user-id']);
+	if (this.request.headers['x-auth-token']) {
+		token = Accounts._hashLoginToken(this.request.headers['x-auth-token']);
+		user = RocketChat.models.Users.findOne({'services.resume.loginTokens.hashedToken': token});
 	}
 
 	return user;
 });
+
+// const auth = function _auth () {
+// 	const invalidResults = [undefined, null, false];
+// 	return {
+// 		token: 'services.resume.loginTokens.hashedToken',
+// 		user: function(headerId, headerToken) {
+
+
+// 			let token;
+// 			if (headerToken) {
+// 				token = Accounts._hashLoginToken(headerToken);
+// 			}
+
+// 			return {
+// 				userId: headerId,
+// 				token
+// 			};
+// 		}
+// 	};
+// }
