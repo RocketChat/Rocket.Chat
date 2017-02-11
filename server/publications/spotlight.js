6735af1a46207e19fc0c612919e971e102b3d3ea
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-02-11 10:39:45 -0200

    Make sportlight work for anonymous

--- server/publications/spotlight.js
+++ server/publications/spotlight.js
@@ -5,23 +5,39 @@ Meteor.methods({
 			rooms: []
 		};
 
-		if (this.userId == null) {
-			return result;
-		}
+		const roomOptions = {
+			limit: 5,
+			fields: {
+				t: 1,
+				name: 1
+			},
+			sort: {
+				name: 1
+			}
+		};
+
+		const userOptions = {
+			limit: 5,
+			fields: {
+				username: 1,
+				status: 1
+			},
+			sort: {
+				username: 1
+			}
+		};
 
 		const regex = new RegExp(s.trim(s.escapeRegExp(text)), 'i');
 
+		if (this.userId == null) {
+			if (RocketChat.settings.get('Accounts_AllowAnonymousAccess') === true) {
+				result.rooms = RocketChat.models.Rooms.findByNameAndType(regex, 'c', roomOptions).fetch();
+			}
+			return result;
+		}
+
 		if (type.users === true && RocketChat.authz.hasPermission(this.userId, 'view-d-room')) {
-			result.users = RocketChat.models.Users.findByActiveUsersUsernameExcept(text, usernames, {
-				limit: 5,
-				fields: {
-					username: 1,
-					status: 1
-				},
-				sort: {
-					username: 1
-				}
-			}).fetch();
+			result.users = RocketChat.models.Users.findByActiveUsersUsernameExcept(text, usernames, userOptions).fetch();
 		}
 
 		if (type.rooms === true && RocketChat.authz.hasPermission(this.userId, 'view-c-room')) {
@@ -29,16 +45,7 @@ Meteor.methods({
 				username: 1
 			}).username;
 
-			result.rooms = RocketChat.models.Rooms.findByNameAndTypeNotContainingUsername(regex, 'c', username, {
-				limit: 5,
-				fields: {
-					t: 1,
-					name: 1
-				},
-				sort: {
-					name: 1
-				}
-			}).fetch();
+			result.rooms = RocketChat.models.Rooms.findByNameAndTypeNotContainingUsername(regex, 'c', username, roomOptions).fetch();
 		}
 		return result;
 	}
