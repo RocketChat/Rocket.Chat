ebddc5c9e5582599284d9eebe5ca49fa65747953
Yuntao Zhou <zhouyuntao@magictavern.com>
2017-02-18 12:44:47 +0800

    optimize code style

--- packages/rocketchat-slashcommand-gif/gif-server.js
+++ packages/rocketchat-slashcommand-gif/gif-server.js
@@ -6,31 +6,29 @@
 
 function Gif(command, params, item) {
 	if (command === 'gif') {
-		let msg;
-		let url = 'https://rightgif.com/search/web';
+		const url = 'https://rightgif.com/search/web';
 
-		msg = item;
-		msg.msg = '';
+		item.msg = '';
 		HTTP.post(url, {
 			headers: {
-				"content-type": "application/json"
+				'content-type': 'application/json'
 			},
 			data: {
 				text: params
 			}
 		}, function(err, response) {
-			if (err || response.statusCode != 200 || response.data.url === 'https://rightgif.com/public/images/error.gif') {
-				let currentUser = Meteor.users.findOne(Meteor.userId());
+			if (err || response.statusCode !== 200 || response.data.url === 'https://rightgif.com/public/images/error.gif') {
+				const currentUser = Meteor.users.findOne(Meteor.userId());
 				RocketChat.Notifications.notifyUser(Meteor.userId(), 'message', {
 					_id: Random.id(),
-					rid: msg.rid,
+					rid: item.rid,
 					ts: new Date(),
 					msg: TAPi18n.__('Slash_Gif_Failed', null, currentUser.language)
 				});
 			} else {
-				msg.attachments = [ {title: params, image_url: response.data.url} ];
-				msg.groupable = false;
-				Meteor.call('sendMessage', msg);
+				item.attachments = [ {title: params, image_url: response.data.url} ];
+				item.groupable = false;
+				Meteor.call('sendMessage', item);
 			}
 		});
 	}
