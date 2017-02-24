14c5676e3ddb32b404a2404d4b92fbe84d46aa4d
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2017-02-24 17:30:26 -0300

    Translate other items like attchment description

--- packages/rocketchat-autotranslate/server/autotranslate.js
+++ packages/rocketchat-autotranslate/server/autotranslate.js
@@ -10,6 +10,8 @@ class AutoTranslate {
 	tokenize(message) {
 		message = this.tokenizeEmojis(message);
 		message = this.tokenizeCode(message);
+		message = this.tokenizeURLs(message);
+		message = this.tokenizeMentions(message);
 		return message;
 	}
 
@@ -25,6 +27,51 @@ class AutoTranslate {
 				text: match
 			});
 			return token;
+		});
+
+		return message;
+	}
+
+	tokenizeURLs(message) {
+		if (!message.tokens || !Array.isArray(message.tokens)) {
+			message.tokens = [];
+		}
+		let count = message.tokens.length;
+
+		const schemes = RocketChat.settings.get('Markdown_SupportSchemesForLink').split(',').join('|');
+
+		// Support ![alt text](http://image url) and [text](http://link)
+		message.msg = message.msg.replace(new RegExp(`(!?\\[)([^\\]]+)(\\]\\((?:${schemes}):\\/\\/[^\\)]+\\))`, 'gm'), function(match, pre, text, post) {
+			const pretoken = `<i class=notranslate>{${count++}}</i>`;
+			message.tokens.push({
+				token: pretoken,
+				text: pre
+			});
+
+			const posttoken = `<i class=notranslate>{${count++}}</i>`;
+			message.tokens.push({
+				token: posttoken,
+				text: post
+			});
+
+			return pretoken + text + posttoken;
+		});
+
+		// Support <http://link|Text>
+		message.msg = message.msg.replace(new RegExp(`((?:<|&lt;)(?:${schemes}):\\/\\/[^\\|]+\\|)(.+?)(?=>|&gt;)((?:>|&gt;))`, 'gm'), function(match, pre, text, post) {
+			const pretoken = `<i class=notranslate>{${count++}}</i>`;
+			message.tokens.push({
+				token: pretoken,
+				text: pre
+			});
+
+			const posttoken = `<i class=notranslate>{${count++}}</i>`;
+			message.tokens.push({
+				token: posttoken,
+				text: post
+			});
+
+			return pretoken + text + posttoken;
 		});
 
 		return message;
@@ -55,6 +102,41 @@ class AutoTranslate {
 		return message;
 	}
 
+	tokenizeMentions(message) {
+		if (!message.tokens || !Array.isArray(message.tokens)) {
+			message.tokens = [];
+		}
+		let count = message.tokens.length;
+
+		if (message.mentions && message.mentions.length > 0) {
+			message.mentions.forEach(mention => {
+				message.msg = message.msg.replace(new RegExp(`(@${mention.username})`, 'gm'), match => {
+					const token = `<i class=notranslate>{${count++}}</i>`;
+					message.tokens.push({
+						token: token,
+						text: match
+					});
+					return token;
+				});
+			});
+		}
+
+		if (message.channels && message.channels.length > 0) {
+			message.channels.forEach(channel => {
+				message.msg = message.msg.replace(new RegExp(`(#${channel.name})`, 'gm'), match => {
+					const token = `<i class=notranslate>{${count++}}</i>`;
+					message.tokens.push({
+						token: token,
+						text: match
+					});
+					return token;
+				});
+			});
+		}
+
+		return message;
+	}
+
 	deTokenize(message) {
 		if (message.tokens && message.tokens.length > 0) {
 			for (const {token, text, noHtml} of message.tokens) {
@@ -65,28 +147,55 @@ class AutoTranslate {
 	}
 
 	translateMessage(message, room) {
-		if (this.enabled && this.apiKey && message.msg) {
-			Meteor.defer(() => {
-				const translations = {};
-				const targetLanguages = RocketChat.models.Subscriptions.getAutoTranslateLanguagesByRoomAndNotUser(room._id, message.u && message.u._id);
+		if (this.enabled && this.apiKey) {
+			const targetLanguages = RocketChat.models.Subscriptions.getAutoTranslateLanguagesByRoomAndNotUser(room._id, message.u && message.u._id);
+			if (message.msg) {
+				Meteor.defer(() => {
+					const translations = {};
+					let targetMessage = Object.assign({}, message);
 
-				message.html = s.escapeHTML(String(message.msg));
-				message = this.tokenize(message);
+					targetMessage.html = s.escapeHTML(String(targetMessage.msg));
+					targetMessage = this.tokenize(targetMessage);
 
-				let msgs = message.msg.split('\n');
-				msgs = msgs.map(msg => encodeURIComponent(msg));
-				const query = `q=${msgs.join('&q=')}`;
-				targetLanguages.forEach(language => {
-					const result = HTTP.get('https://translation.googleapis.com/language/translate/v2', { params: { key: this.apiKey, target: language }, query: query });
-					if (result.statusCode === 200 && result.data && result.data.data && result.data.data.translations && Array.isArray(result.data.data.translations) && result.data.data.translations.length > 0) {
-						const txt = result.data.data.translations.map(translation => translation.translatedText).join('\n');
-						translations[language] = this.deTokenize(Object.assign({}, message, { msg: txt }));
+					let msgs = targetMessage.msg.split('\n');
+					msgs = msgs.map(msg => encodeURIComponent(msg));
+					const query = `q=${msgs.join('&q=')}`;
+					targetLanguages.forEach(language => {
+						const result = HTTP.get('https://translation.googleapis.com/language/translate/v2', { params: { key: this.apiKey, target: language }, query: query });
+						if (result.statusCode === 200 && result.data && result.data.data && result.data.data.translations && Array.isArray(result.data.data.translations) && result.data.data.translations.length > 0) {
+							const txt = result.data.data.translations.map(translation => translation.translatedText).join('\n');
+							translations[language] = this.deTokenize(Object.assign({}, targetMessage, { msg: txt }));
+						}
+					});
+					if (!_.isEmpty(translations)) {
+						RocketChat.models.Messages.addTranslations(message._id, translations);
 					}
 				});
-				if (!_.isEmpty(translations)) {
-					RocketChat.models.Messages.addTranslations(message._id, translations);
-				}
-			});
+			}
+
+			if (message.attachments && message.attachments.length > 0) {
+				Meteor.defer(() => {
+					for (const index in message.attachments) {
+						if (message.attachments.hasOwnProperty(index)) {
+							const attachment = message.attachments[index];
+							const translations = {};
+							if (attachment.description) {
+								const query = `q=${encodeURIComponent(attachment.description)}`;
+								targetLanguages.forEach(language => {
+									const result = HTTP.get('https://translation.googleapis.com/language/translate/v2', { params: { key: this.apiKey, target: language }, query: query });
+									if (result.statusCode === 200 && result.data && result.data.data && result.data.data.translations && Array.isArray(result.data.data.translations) && result.data.data.translations.length > 0) {
+										const txt = result.data.data.translations.map(translation => translation.translatedText).join('\n');
+										translations[language] = txt;
+									}
+								});
+								if (!_.isEmpty(translations)) {
+									RocketChat.models.Messages.addAttachmentDescriptionTranslations(message._id, index, translations);
+								}
+							}
+						}
+					}
+				});
+			}
 		}
 		return message;
 	}
