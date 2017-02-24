a12757d2eb5cdf0e98be8830579dc76fac72ec54
github@beimir.net <nRj6GnqdFO88wg8upvY2>
2017-02-24 11:50:25 +0100

    Fix external messages - merged into development by RC with 93735a6a2f3c3bff37ac1ab1c09ffb37a53c833c

--- packages/rocketchat-livechat/server/models/LivechatExternalMessage.js
+++ packages/rocketchat-livechat/server/models/LivechatExternalMessage.js
@@ -1,11 +1,9 @@
 class LivechatExternalMessage extends RocketChat.models._Base {
 	constructor() {
 		super('livechat_external_message');
-		try {
-			this.model = new Mongo.Collection('rocketchat_livechat_external_message');
-		}
-		catch(e){
-			console.log('I do not get why this is getting called multiple times');
+
+		if (Meteor.isClient) {
+				this._initModel('livechat_external_message');
 		}
 	}
 
