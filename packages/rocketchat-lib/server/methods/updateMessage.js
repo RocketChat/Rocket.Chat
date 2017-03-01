198dcecc90656439bb48a24c4d31303d308ef307
Martin Schoeler <martin.schoeler@rocket.chat>
2017-03-01 14:56:55 -0300

    added the file description edit

--- packages/rocketchat-lib/server/methods/updateMessage.js
+++ packages/rocketchat-lib/server/methods/updateMessage.js
@@ -34,7 +34,7 @@ Meteor.methods({
 		}
 
 		if (originalMessage.attachments) {
-			if (originalMessage.attachments.description) {
+			if (originalMessage.attachments[0].description !== undefined) {
 				message.attachments = originalMessage.attachments;
 				message.attachments[0].description = message.msg;
 				message.msg = originalMessage.msg;
