6f2e9815a01c6bd168cf0331cda9338d94aea546
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-03-05 15:46:52 -0300

    Allows proxy OAuth requests

---
+++ packages/rocketchat-lib/server/oauth/proxy.js
@@ -0,0 +1,11 @@
+/* globals OAuth */
+
+OAuth._redirectUri = _.wrap(OAuth._redirectUri, function(func, serviceName, ...args) {
+	const proxy = RocketChat.settings.get('Accounts_OAuth_Proxy_services').replace(/\s/g, '').split(',');
+	if (proxy.includes(serviceName)) {
+		return RocketChat.settings.get('Accounts_OAuth_Proxy_host')+'/oauth_redirect';
+	} else {
+		return func(serviceName, ...args);
+	}
+
+});
