d4cc9049c5679de9fe8dd12c91635569a34263f6
Martin Schoeler <martin.schoeler@rocket.chat>
2017-03-07 15:03:09 -0300

    userInfo.coffee to js

--- packages/rocketchat-ui-flextab/package.js
+++ packages/rocketchat-ui-flextab/package.js
@@ -32,5 +32,5 @@ Package.onUse(function(api) {
 	api.addFiles('client/tabs/messageSearch.coffee', 'client');
 	api.addFiles('client/tabs/uploadedFilesList.coffee', 'client');
 	api.addFiles('client/tabs/userEdit.coffee', 'client');
-	api.addFiles('client/tabs/userInfo.coffee', 'client');
+	api.addFiles('client/tabs/userInfo.js', 'client');
 });
