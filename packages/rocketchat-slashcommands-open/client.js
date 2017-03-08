9ce48396de1d7b051ae3aed9ba990db6b86d8d2f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-03-08 11:48:56 -0300

    slashcommand /open now works event if you never talked with the user

--- packages/rocketchat-slashcommands-open/client.js
+++ packages/rocketchat-slashcommands-open/client.js
@@ -1,29 +1,43 @@
-function Open(command, params/*, item*/) {
+function Open(command, params /*, item*/) {
+	const dict = {
+		'#': 'c',
+		'@': 'd'
+	};
 	var room, subscription, type;
+
 	if (command !== 'open' || !Match.test(params, String)) {
 		return;
 	}
+
 	room = params.trim();
-	if (room.indexOf('#') !== -1) {
-		type = 'c';
-	}
-	if (room.indexOf('@') !== -1) {
-		type = 'd';
-	}
-	room = room.replace('#', '');
-	room = room.replace('@', '');
+	type = dict[room[0]];
+	room = room.replace(/#|@/, '');
 
 	var query = {
 		name: room
 	};
+
 	if (type) {
 		query['t'] = type;
 	}
+
 	subscription = ChatSubscription.findOne(query);
 
 	if (subscription) {
 		RocketChat.roomTypes.openRouteLink(subscription.t, subscription, FlowRouter.current().queryParams);
 	}
+
+	if (type && type !== 'd') {
+		return;
+	}
+	return Meteor.call('createDirectMessage', room, function(err) {
+		if (err) {
+			return;
+		}
+		subscription = RocketChat.models.Subscriptions.findOne(query);
+		RocketChat.roomTypes.openRouteLink(subscription.t, subscription, FlowRouter.current().queryParams);
+	});
+
 }
 
 RocketChat.slashCommands.add('open', Open, {
