da8a9ba4ca99f5e7018c86f25eeb0d4b0e03f256
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2017-03-08 19:51:08 -0300

    Adds google vision settings and callback

---
+++ packages/rocketchat-google-vision/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+	name: 'rocketchat:google-vision',
+	version: '0.0.1',
+	summary: 'Rocket.Chat Google Vision Integration',
+	git: ''
+});
+
+Npm.depends({
+	'@google-cloud/storage': '0.7.0',
+	'@google-cloud/vision': '0.9.0'
+});
+
+Package.onUse(function(api) {
+	api.use([
+		'ecmascript',
+		'less',
+		'rocketchat:lib'
+	]);
+
+	api.addFiles(['server/settings.js', 'server/googlevision.js'], 'server');
+});
