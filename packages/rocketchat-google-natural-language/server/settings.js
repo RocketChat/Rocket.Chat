1e1936d24e83fe199a013b96e3de568158410d9f
Diego Sampaio <chinello@gmail.com>
2017-03-09 09:01:39 -0300

    Add translations and instructions

--- packages/rocketchat-google-natural-language/server/settings.js
+++ packages/rocketchat-google-natural-language/server/settings.js
@@ -2,17 +2,19 @@ Meteor.startup(function() {
 	RocketChat.settings.add('GoogleNaturalLanguage_Enabled', false, {
 		type: 'boolean',
 		group: 'Message',
-		section: 'GoogleNaturalLanguage',
-		public: true
+		section: 'Google Natural Language',
+		public: true,
+		i18nLabel: 'Enabled'
 	});
 	RocketChat.settings.add('GoogleNaturalLanguage_ServiceAccount', '', {
 		type: 'string',
 		group: 'Message',
-		section: 'GoogleNaturalLanguage',
+		section: 'Google Natural Language',
 		multiline: true,
 		enableQuery: {
 			_id: 'GoogleNaturalLanguage_Enabled',
 			value: true
-		}
+		},
+		i18nLabel: 'Service_account_key'
 	});
 });
