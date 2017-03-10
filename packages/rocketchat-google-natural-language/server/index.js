480a9612e5f6557e08719840acc8de8f953fd9db
Diego Sampaio <chinello@gmail.com>
2017-03-10 11:39:57 -0300

    Refactor callbacks and isolate code better

--- packages/rocketchat-google-natural-language/server/index.js
+++ packages/rocketchat-google-natural-language/server/index.js
@@ -1,21 +1,41 @@
 import './settings.js';
 import './models/Rooms.js';
 
-const getMessageSentiment = function(message) {
-	const googleLanguage = Npm.require('@google-cloud/language');
+const googleLanguage = Npm.require('@google-cloud/language');
 
-	const languageClient = googleLanguage({
-		credentials: JSON.parse(RocketChat.settings.get('GoogleNaturalLanguage_ServiceAccount'))
-	});
+let languageClient;
 
-	const result = Meteor.wrapAsync(languageClient.detectSentiment, languageClient)(message.msg);
-
-	if (result) {
-		// message.sentiment = result;
-		RocketChat.models.Rooms.setSentiment(message.rid, result);
+RocketChat.settings.get('GoogleNaturalLanguage_ServiceAccount', (key, value) => {
+	if (value) {
+		try {
+			languageClient = googleLanguage({
+				credentials: JSON.parse(value)
+			});
+		} catch (e) {
+			languageClient = null;
+			console.error('Error parsing Google Natural Language credential.', e);
+		}
 	}
+});
+
+const setRoomSentiment = function(message) {
+	if (!languageClient) {
+		return;
+	}
+
+	languageClient.detectSentiment(message.msg, Meteor.bindEnvironment((error, result) => {
+		if (!error) {
+			RocketChat.models.Rooms.setSentiment(message.rid, result);
+		}
+	}));
 
 	return message;
 };
 
-RocketChat.callbacks.add('afterSaveMessage', getMessageSentiment, RocketChat.callbacks.priority.MEDIUM, 'GoogleNaturalLanguage');
+RocketChat.settings.get('GoogleNaturalLanguage_Enabled', (key, value) => {
+	if (value) {
+		RocketChat.callbacks.add('afterSaveMessage', setRoomSentiment, RocketChat.callbacks.priority.MEDIUM, 'GoogleNaturalLanguage');
+	} else {
+		RocketChat.callbacks.remove('afterSaveMessage', 'GoogleNaturalLanguage');
+	}
+});
--- packages/rocketchat-ui/client/views/app/room.coffee
+++ packages/rocketchat-ui/client/views/app/room.coffee
@@ -14,16 +14,6 @@ userCanDrop = (_id) ->
 	return !RocketChat.roomTypes.readOnly _id, Meteor.user()
 
 Template.room.helpers
-	sentimentSmile: ->
-		room = ChatRoom.findOne this._id, fields: sentiment: 1
-
-		if room.sentiment >= 0.3
-			return ':)'
-		else if room.sentiment >= -0.3
-			return ':|'
-		else if room.sentiment < -0.3
-			return ':('
-
 	isTranslated: ->
 		sub = ChatSubscription.findOne { rid: this._id }, { fields: { autoTranslate: 1, autoTranslateLanguage: 1 } }
 		return RocketChat.settings.get('AutoTranslate_Enabled') and sub?.autoTranslate is true and sub.autoTranslateLanguage?
