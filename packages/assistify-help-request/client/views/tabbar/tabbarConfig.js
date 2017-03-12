c1e2b5eab3815d181a162941a6687be11e77cbcb
github@beimir.net <nRj6GnqdFO88wg8upvY2>
2017-03-12 19:27:17 +0100

    Fix tabbar for requests and expertise

--- packages/assistify-help-request/client/views/tabbar/tabbarConfig.js
+++ packages/assistify-help-request/client/views/tabbar/tabbarConfig.js
@@ -6,6 +6,10 @@
 
 RocketChat.TabBar.addGroup('starred-messages', ['request', 'expertise']);
 RocketChat.TabBar.addGroup('push-notifications', ['request', 'expertise']);
+RocketChat.TabBar.addGroup('channel-settings', ['request', 'expertise']);
+RocketChat.TabBar.addGroup('members-list', ['request', 'expertise']);
+RocketChat.TabBar.addGroup('message-search',['request', 'expertise']);
+RocketChat.TabBar.addGroup('uploaded-files-list',['request', 'expertise']);
 
 RocketChat.TabBar.addButton({
 	groups: ['request', 'expertise', 'live'],
@@ -14,40 +18,4 @@ RocketChat.TabBar.addButton({
 	icon: 'icon-lightbulb',
 	template: 'dbsAI_externalSearch',
 	order: 0
-});
-
-RocketChat.TabBar.addButton({
-	groups: ['request', 'expertise'],
-	id: 'channel-settings',
-	i18nTitle: 'Room_Info',
-	icon: 'icon-info-circled',
-	template: 'channelSettings',
-	order: 1
-});
-
-RocketChat.TabBar.addButton({
-	groups: ['request', 'expertise'],
-	id: 'members-list',
-	i18nTitle: 'Members_List',
-	icon: 'icon-users',
-	template: 'membersList',
-	order: 3
-});
-
-RocketChat.TabBar.addButton({
-	groups: ['request', 'expertise'],
-	id: 'message-search',
-	i18nTitle: 'Search',
-	icon: 'icon-search',
-	template: 'messageSearch',
-	order: 10
-});
-
-RocketChat.TabBar.addButton({
-	groups: ['request', 'expertise'],
-	id: 'uploaded-files-list',
-	i18nTitle: 'Room_uploaded_file_list',
-	icon: 'icon-attach',
-	template: 'uploadedFilesList',
-	order: 30
 });
