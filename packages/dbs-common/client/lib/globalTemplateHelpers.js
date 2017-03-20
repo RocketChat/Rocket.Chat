46202214b61d1bd47b72f3dd543b20967b93debf
github@beimir.net <nRj6GnqdFO88wg8upvY2>
2017-03-20 07:57:51 +0100

    Remove last reisebuddy reference

--- packages/dbs-common/client/lib/globalTemplateHelpers.js
+++ packages/dbs-common/client/lib/globalTemplateHelpers.js
@@ -12,8 +12,6 @@ Template.registerHelper('add', (a, b) => a + b);
 
 Template.registerHelper('text', (i18n_alias) => t(i18n_alias));
 
-Template.registerHelper('isReisebuddy', () => !!RocketChat.settings.get('Reisebuddy_active'));
-
 Template.registerHelper('formatDateMilliseconds', (val) => new _dbs.Duration(val).toHHMMSS());
 
 Template.registerHelper('templateExists', (val) => !!Template[val]);
