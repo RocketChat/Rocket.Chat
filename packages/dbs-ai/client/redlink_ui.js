30d0c22c6067182b5b8fe6004a7d2c45e83bf830
github@beimir.net <nRj6GnqdFO88wg8upvY2>
2017-03-22 18:38:07 +0100

    Small livechat cleanups - Remove rocket.chat livechat-callback - Remove one of the two redundant lightbulb icons in the tabbar

--- packages/dbs-ai/client/redlink_ui.js
+++ packages/dbs-ai/client/redlink_ui.js
@@ -2,7 +2,7 @@ RocketChat.TabBar.removeButton('external-search');
 
 RocketChat.TabBar.addButton({
 	groups: ['live', 'channel'],
-	id: 'external-search',
+	id: 'dbsai',
 	i18nTitle: 'Knowledge_Base',
 	icon: 'icon-lightbulb',
 	template: 'dbsAI_externalSearch',
