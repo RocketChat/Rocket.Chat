40d74477718839b9f3b79543cf01bbf0363e8a07
github@beimir.net <nRj6GnqdFO88wg8upvY2>
2017-03-24 09:39:41 +0100

    Close room along with helprequest

--- packages/assistify-help-request/server/methods/closeHelpRequest.js
+++ packages/assistify-help-request/server/methods/closeHelpRequest.js
@@ -3,7 +3,16 @@ Meteor.methods({
 		const room = RocketChat.models.Rooms.findOneByIdOrName(roomId);
 		if(room.helpRequestId) {
 			RocketChat.models.HelpRequests.close(room.helpRequestId, closingProps);
-
+			const user = Meteor.user();
+			const now = new Date();
+			RocketChat.models.Rooms.closeByRoomId(room._id, {
+				user: {
+					_id: user._id,
+					username: user.username
+				},
+				closedAt: now,
+				chatDuration: (now.getTime() - room.ts) / 1000
+			});
 			// delete subscriptions in order to make the room disappear from the user's clients
 			const nonOwners = RocketChat.models.Subscriptions.findByRoomIdAndNotUserId(roomId, room.u._id).fetch();
 			nonOwners.forEach((nonOwner)=>{
