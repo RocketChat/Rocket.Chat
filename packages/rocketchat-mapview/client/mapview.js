c963535b68cde63725e947fb77042c7865e24fc6
Martin Schoeler <martin.schoeler@rocket.chat>
2017-03-24 14:25:47 -0300

    fix call class as a function

--- packages/rocketchat-mapview/client/mapview.js
+++ packages/rocketchat-mapview/client/mapview.js
@@ -3,28 +3,27 @@
  * @param {Object} message - The message object
  */
 
-class MapView {
-	constructor(message) {
+
+function MapView(message) {
 
 		// get MapView settings
-		const mv_googlekey = RocketChat.settings.get('MapView_GMapsAPIKey');
+	const mv_googlekey = RocketChat.settings.get('MapView_GMapsAPIKey');
 
-		if (message.location) {
+	if (message.location) {
 
 			// GeoJSON is reversed - ie. [lng, lat]
-			const longitude = message.location.coordinates[0];
-			const latitude = message.location.coordinates[1];
+		const longitude = message.location.coordinates[0];
+		const latitude = message.location.coordinates[1];
 
 			// confirm we have an api key set, and generate the html required for the mapview
-			if (mv_googlekey && mv_googlekey.length) {
-				message.html = `<a href="https://maps.google.com/maps?daddr=${latitude},${longitude}" target="_blank"><img src="https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=250x250&markers=color:gray%7Clabel:%7C${latitude},${longitude}&key=${mv_googlekey}" /></a>`;
-			} else {
-				message.html = `<a href="https://maps.google.com/maps?daddr=${latitude},${longitude}" target="_blank">${TAPi18n.__('Shared_Location')}</a>`;
-			}
+		if (mv_googlekey && mv_googlekey.length) {
+			message.html = `<a href="https://maps.google.com/maps?daddr=${latitude},${longitude}" target="_blank"><img src="https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=250x250&markers=color:gray%7Clabel:%7C${latitude},${longitude}&key=${mv_googlekey}" /></a>`;
+		} else {
+			message.html = `<a href="https://maps.google.com/maps?daddr=${latitude},${longitude}" target="_blank">${TAPi18n.__('Shared_Location')}</a>`;
 		}
-
-		return message;
 	}
+
+	return message;
 }
 
 RocketChat.callbacks.add('renderMessage', MapView, RocketChat.callbacks.priority.HIGH);
