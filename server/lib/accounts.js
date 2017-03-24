fba26875d12bbea955a9d322d7a705ddf1425e05
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-03-24 16:00:27 -0300

    Integrate login with 2FA TOTP

--- server/lib/accounts.js
+++ server/lib/accounts.js
@@ -164,17 +164,7 @@ Accounts.validateLoginAttempt(function(login) {
 		}
 	}
 
-	if (login.type === 'password' && login.user.services && login.user.services.totp && login.user.services.totp.enabled === true) {
-		const { totp } = login.methodArguments[0];
-		if (totp && totp.code) {
-			//TODO Verify OTP
-			if (totp.code !== '123') {
-				throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
-			}
-		} else {
-			throw new Meteor.Error('totp-required', 'TOTP Required');
-		}
-	}
+	login = RocketChat.callbacks.run('onValidateLogin', login);
 
 	RocketChat.models.Users.updateLastLoginById(login.user._id);
 	Meteor.defer(function() {
