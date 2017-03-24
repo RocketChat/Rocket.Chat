3fa77183c908db28aa40997f63d823d72e806059
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-03-24 16:51:45 -0300

    Fix ESLint

--- packages/rocketchat-2fa/package.js
+++ packages/rocketchat-2fa/package.js
@@ -1,12 +1,8 @@
 Package.describe({
 	name: 'rocketchat:2fa',
 	version: '0.0.1',
-	// Brief, one-line summary of the package.
 	summary: '',
-	// URL to the Git repository containing the source code for this package.
 	git: '',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
 });
 
@@ -16,16 +12,13 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-	// api.versionsFrom('1.4.3.1');
-	// api.use('ecmascript');
-	// api.mainModule('rocketchat-2fa.js');
-
 	api.use([
 		'accounts-base',
 		'ecmascript',
 		'templating',
 		'rocketchat:lib'
 	]);
+
 	api.addFiles('client/accountSecurity.html', 'client');
 	api.addFiles('client/accountSecurity.js', 'client');
 	api.addFiles('client/TOTPPassword.js', 'client');
