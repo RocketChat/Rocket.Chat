f1c8d46078866e8d3f88456455bd8f6e76a8c5ea
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-03-24 16:00:27 -0300

    Integrate login with 2FA TOTP

--- packages/rocketchat-2fa/server/loginHandler.js
+++ packages/rocketchat-2fa/server/loginHandler.js
@@ -1,7 +1,29 @@
+import speakeasy from 'speakeasy';
+
 Accounts.registerLoginHandler('totp', function(options) {
 	if (!options.totp || !options.totp.code) {
 		return;
 	}
 
 	return Accounts._runLoginHandlers(this, options.totp.login);
+});
+
+RocketChat.callbacks.add('onValidateLogin', (login) => {
+	if (login.type === 'password' && login.user.services && login.user.services.totp && login.user.services.totp.enabled === true) {
+		const { totp } = login.methodArguments[0];
+
+		if (!totp || !totp.code) {
+			throw new Meteor.Error('totp-required', 'TOTP Required');
+		}
+
+		const verified = speakeasy.totp.verify({
+			secret: login.user.services.totp.secret,
+			encoding: 'base32',
+			token: totp.code
+		});
+
+		if (verified !== true) {
+			throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
+		}
+	}
 });
