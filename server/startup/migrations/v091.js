3e8d9801b46f1adf452ed737e92ef99a61645ddd
Alex Brazier <aejbrazier.apps@gmail.com>
2017-03-27 18:19:40 +0100

    Fix eslint

--- server/startup/migrations/v091.js
+++ server/startup/migrations/v091.js
@@ -1,6 +1,6 @@
 RocketChat.Migrations.add({
 	version: 91,
-	up: function() {
+	up() {
 		RocketChat.models.Users.find({}, {username: 1, name: 1}).forEach((user) => {
 			RocketChat.models.Messages.updateAllNamesByUserId(user._id, user.name);
 			RocketChat.models.Subscriptions.setRealNameForDirectRoomsWithUsername(user.username, user.name);
