5d5453e36026ce1fb6d35c794a22741c2a623b56
Martin Schoeler <martin.schoeler@rocket.chat>
2017-03-30 16:35:27 -0300

    fix self calling functions

--- packages/rocketchat-sharedsecret/sharedsecret.js
+++ packages/rocketchat-sharedsecret/sharedsecret.js
@@ -2,105 +2,87 @@
 const SharedSecret = [];
 
 if (Meteor.isServer) {
-	const EncryptMessage = (() => {
-		function EncryptMessage(message) {
+	const EncryptMessage = ((message) => {
+		const currentUser = Meteor.user()._id;
+		if (message) {
+			const currentRoomId = message.rid;
+			if (SharedSecret && SharedSecret[currentUser] && SharedSecret[currentUser][currentRoomId]) {
+				const currentSecret = SharedSecret[currentUser][currentRoomId];
+				const encrypted = CryptoJS.AES.encrypt(message.msg, currentSecret);
+				message.msg = encrypted.toString();
+				if (message.urls) {
+					const messageUrls = message.urls;
+					messageUrls.forEach((i) => {
+						const urls = messageUrls[i];
+						urls.url = CryptoJS.AES.encrypt(urls.url, currentSecret).toString();
+					});
+				}
+				message.encrypted = true;
+			}
+		}
+		return message;
+	})();
+
+	const HandleSlashCommand = ((command, params, item) => {
+		if (command === 'setsecretkey') {
 			const currentUser = Meteor.user()._id;
-			if (message) {
-				const currentRoomId = message.rid;
-				if (SharedSecret && SharedSecret[currentUser] && SharedSecret[currentUser][currentRoomId]) {
-					const currentSecret = SharedSecret[currentUser][currentRoomId];
-					const encrypted = CryptoJS.AES.encrypt(message.msg, currentSecret);
-					message.msg = encrypted.toString();
-					if (message.urls) {
-						const messageUrls = message.urls;
-						messageUrls.forEach((i) => {
-							const urls = messageUrls[i];
-							urls.url = CryptoJS.AES.encrypt(urls.url, currentSecret).toString();
-						});
-					}
-					message.encrypted = true;
-				}
-				return message;
+			const currentRoomId = item.rid;
+			let secret = params;
+			if (secret === 'off') {
+				secret = null;
+			}
+			if (SharedSecret && SharedSecret[currentUser]) {
+				SharedSecret[currentUser][currentRoomId] = secret;
+			} else {
+				SharedSecret[currentUser] = [];
+				SharedSecret[currentUser][currentRoomId] = secret;
 			}
 		}
-
-		return EncryptMessage;
-
-	});
-	const HandleSlashCommand = (() => {
-		function HandleSlashCommand(command, params, item) {
-			if (command === 'setsecretkey') {
-				const currentUser = Meteor.user()._id;
-				const currentRoomId = item.rid;
-				let secret = params;
-				if (secret === 'off') {
-					secret = null;
-				}
-				if (SharedSecret && SharedSecret[currentUser]) {
-					SharedSecret[currentUser][currentRoomId] = secret;
-				} else {
-					SharedSecret[currentUser] = [];
-					SharedSecret[currentUser][currentRoomId] = secret;
-				}
-			}
-		}
-
-		return HandleSlashCommand;
-
-	});
+	})();
 	RocketChat.callbacks.add('beforeSaveMessage', EncryptMessage, 9999, 'sharedsecret-encrypt-message');
 	RocketChat.slashCommands.add('setsecretkey', HandleSlashCommand);
 }
 
 if (Meteor.isClient) {
-	const DecryptMessage = (() => {
-		function DecryptMessage(message) {
-			if (message && message.encrypted) {
-				const currentRoomId = message.rid;
-				const currentSecret = localStorage.getItem(`rocket.chat.sharedSecretKey.${ currentRoomId }`);
-				if (currentSecret) {
-					const decrypted = CryptoJS.AES.decrypt(message.msg, currentSecret).toString(CryptoJS.enc.Utf8);
-					if (decrypted === '') {
-						message.msg = '~ encrypted message ~';
-						message.html = '~ encrypted message ~';
-					} else {
-						const lockImage = 'images/lock8.png';
-						message.msg = `<img src=${ lockImage } style='width:8px;height:9px;'></img> ${ decrypted }`;
-						message.html = `<img src=${ lockImage } style='width:8px;height:9px;'></img> ${ decrypted }`;
-					}
-					if (message.urls) {
-						const messageUrls = message.urls;
-						messageUrls.forEach((i) => {
-							const urls = messageUrls[i];
-							urls.url = CryptoJS.AES.decrypt(urls.url, currentSecret).toString(CryptoJS.enc.Utf8);
-						});
-					}
-				} else {
+	const DecryptMessage = ((message) => {
+		if (message && message.encrypted) {
+			const currentRoomId = message.rid;
+			const currentSecret = localStorage.getItem(`rocket.chat.sharedSecretKey.${ currentRoomId }`);
+			if (currentSecret) {
+				const decrypted = CryptoJS.AES.decrypt(message.msg, currentSecret).toString(CryptoJS.enc.Utf8);
+				if (decrypted === '') {
 					message.msg = '~ encrypted message ~';
 					message.html = '~ encrypted message ~';
+				} else {
+					const lockImage = 'images/lock8.png';
+					message.msg = `<img src=${ lockImage } style='width:8px;height:9px;'></img> ${ decrypted }`;
+					message.html = `<img src=${ lockImage } style='width:8px;height:9px;'></img> ${ decrypted }`;
 				}
-			}
-			return message;
-		}
-
-		return DecryptMessage;
-
-	});
-	HandleSlashCommand = (() => {
-		function HandleSlashCommand(command, params, item) {
-			if (command === 'setsecretkey') {
-				let secret = params;
-				if (secret === 'off') {
-					secret = null;
+				if (message.urls) {
+					const messageUrls = message.urls;
+					messageUrls.forEach((i) => {
+						const urls = messageUrls[i];
+						urls.url = CryptoJS.AES.decrypt(urls.url, currentSecret).toString(CryptoJS.enc.Utf8);
+					});
 				}
-				const currentRoomId = item.rid;
-				localStorage.setItem(`rocket.chat.sharedSecretKey.${ currentRoomId }`, secret);
+			} else {
+				message.msg = '~ encrypted message ~';
+				message.html = '~ encrypted message ~';
 			}
 		}
+		return message;
+	})();
 
-		return HandleSlashCommand;
-
-	});
+	const HandleSlashCommand = ((command, params, item) => {
+		if (command === 'setsecretkey') {
+			let secret = params;
+			if (secret === 'off') {
+				secret = null;
+			}
+			const currentRoomId = item.rid;
+			localStorage.setItem(`rocket.chat.sharedSecretKey.${ currentRoomId }`, secret);
+		}
+	})();
 	RocketChat.callbacks.add('renderMessage', DecryptMessage, -9999, 'sharedsecret-decrypt-message');
 	RocketChat.slashCommands.add('setsecretkey', HandleSlashCommand);
 }
