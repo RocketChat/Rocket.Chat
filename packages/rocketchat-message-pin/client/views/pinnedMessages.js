8063714c25526c52aaa89a98ca0d7817a2ecd248
Martin Schoeler <martin.schoeler@rocket.chat>
2017-04-03 16:42:30 -0300

    fix not saved file

--- packages/rocketchat-message-pin/client/views/pinnedMessages.js
+++ packages/rocketchat-message-pin/client/views/pinnedMessages.js
@@ -31,35 +31,34 @@ Template.pinnedMessages.helpers({
 Template.pinnedMessages.onCreated(function() {
 	this.hasMore = new ReactiveVar(true);
 	this.limit = new ReactiveVar(50);
-	return this.autorun((function(_this) {
+	return this.autorun(() => {
 		return function() {
 			const data = Template.currentData();
-			return _this.subscribe('pinnedMessages', data.rid, _this.limit.get(), function() {
+			return this.subscribe('pinnedMessages', data.rid, this.limit.get(), function() {
 				if (PinnedMessage.find({
 					rid: data.rid
-				}).count() < _this.limit.get()) {
-					return _this.hasMore.set(false);
+				}).count() < this.limit.get()) {
+					return this.hasMore.set(false);
 				}
 			});
 		};
-	})(this));
+	});
 });
 
 Template.pinnedMessages.events({
 	'click .message-cog'(e, t) {
-		let actions, dropDown, el, message, message_id;
 		e.stopPropagation();
 		e.preventDefault();
-		message_id = $(e.currentTarget).closest('.message').attr('id');
+		const message_id = $(e.currentTarget).closest('.message').attr('id');
 		RocketChat.MessageAction.hideDropDown();
 		t.$(`\#${ message_id } .message-dropdown`).remove();
-		message = PinnedMessage.findOne(message_id);
-		actions = RocketChat.MessageAction.getButtons(message, 'pinned');
-		el = Blaze.toHTMLWithData(Template.messageDropdown, {
+		const message = PinnedMessage.findOne(message_id);
+		const actions = RocketChat.MessageAction.getButtons(message, 'pinned');
+		const el = Blaze.toHTMLWithData(Template.messageDropdown, {
 			actions
 		});
 		t.$(`\#${ message_id } .message-cog-container`).append(el);
-		dropDown = t.$(`\#${ message_id } .message-dropdown`);
+		const dropDown = t.$(`\#${ message_id } .message-dropdown`);
 		return dropDown.show();
 	},
 	'scroll .content': _.throttle(function(e, instance) {
