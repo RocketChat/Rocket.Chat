8b195c97b01ee822e1e592e427fd5a991a73ff18
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-04-04 18:34:35 -0300

    Only process messages to set user’s name when necessary

--- packages/rocketchat-lib/server/methods/getChannelHistory.js
+++ packages/rocketchat-lib/server/methods/getChannelHistory.js
@@ -49,9 +49,11 @@ Meteor.methods({
 			records = RocketChat.models.Messages.findVisibleByRoomIdBetweenTimestamps(rid, oldest, latest, options).fetch();
 		}
 
+		const UI_Use_Real_Name = RocketChat.settings.get('UI_Use_Real_Name') === true;
+
 		const messages = _.map(records, (message) => {
 			message.starred = _.findWhere(message.starred, { _id: fromUserId });
-			if (message.u && message.u._id) {
+			if (message.u && message.u._id && UI_Use_Real_Name) {
 				const user = RocketChat.models.Users.findOneById(message.u._id);
 				message.u.name = user && user.name;
 			}
