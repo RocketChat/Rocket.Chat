5e7c7b3c9037098af57db0b49a93900ff243c16e
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-04-06 14:39:56 -0300

    phase 2

--- packages/rocketchat-mentions/tests/client.tests.js
+++ packages/rocketchat-mentions/tests/client.tests.js
@@ -1,131 +1,222 @@
 /* eslint-env mocha */
 import assert from 'assert';
 
-global.RocketChat = typeof global.RocketChat === 'undefined' ? {} : global.RocketChat;
+import Mentions from '../Mentions';
+const mention = new Mentions({
+	pattern: '[0-9a-zA-Z-_.]+',
+	me: () => 'me'
+});
 
-const confs = {
-	'UTF8_Names_Validation' : '[0-9a-zA-Z-_.]+'
-};
-
-global.RocketChat.settings = {
-	get(key) {
-		return confs[key];
-	}
-};
-
-const {msgMentionRegex, msgChannelRegex, replaceUsers, replaceChannels} = require('../mentions').default(RocketChat);
-
-describe('Testing regex\'s', function() {
-	describe(`msgMentionRegex: ${ msgMentionRegex }`, function() {
-		const str1 = '@rocket.cat';
-		it(`one user '${ str1 }'`, function functionName() {
-			const result = str1.match(msgMentionRegex)|| [];
-			assert.equal(result.length, 1);
+describe('Mention', function() {
+	describe('get pattern', () => {
+		const regexp = '[0-9a-zA-Z-_.]+';
+		describe('by function', function functionName() {
+			before(() => mention.pattern = () => regexp);
+			it(`should be equal to ${ regexp }`, ()=> {
+				assert.equal(regexp, mention.pattern);
+			});
 		});
-
-		it('two users \'@rocket.cat @all\'', function functionName() {
-			const result = '@rocket.cat @all'.match(msgMentionRegex)|| [];
-			assert.equal(result.length, 2);
+		describe('by const', function functionName() {
+			before(() => mention.pattern = regexp);
+			it(`should be equal to ${ regexp }`, ()=> {
+				assert.equal(regexp, mention.pattern);
+			});
 		});
-
-		const strTest3 = 'hello @rocket.cat';
-		it(`text before user '${ strTest3 }'`, function functionName() {
-			const result = strTest3.match(msgMentionRegex)|| [];
-			assert.equal(result.length, 1);
+	});
+	describe('get me', () => {
+		const me = 'me';
+		describe('by function', function functionName() {
+			before(() => mention.me = () => me);
+			it(`should be equal to ${ me }`, ()=> {
+				assert.equal(me, mention.me);
+			});
 		});
-
-		const strTest4 = '@rocket.cat, hello';
-		it(`text after user '${ strTest4 }'`, function functionName() {
-			const result = strTest4.match(msgMentionRegex)|| [];
-			assert.equal(result.length, 1);
+		describe('by const', function functionName() {
+			before(() => mention.me = me);
+			it(`should be equal to ${ me }`, ()=> {
+				assert.equal(me, mention.me);
+			});
 		});
-
-		const strTest5 = '@rocket.cat and @user';
-		it(`text between users '${ strTest5 }'`, function functionName() {
-			const result = strTest5.match(msgMentionRegex)|| [];
-			assert.equal(result.length, 2);
+	});
+	describe('getUserMentions', function functionName() {
+		describe('for simple text, no mentions', () => {
+			const result = [];
+			[
+				'#rocket.cat',
+				'hello rocket.cat how are you?'
+			]
+			.forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getUserMentions(text));
+				});
+			});
 		});
-
+		describe('for one user', () => {
+			const result = ['@rocket.cat'];
+			[
+				'@rocket.cat',
+				' @rocket.cat ',
+				'hello @rocket.cat',
+				'hello,@rocket.cat',
+				'@rocket.cat, hello',
+				'@rocket.cat,hello',
+				'hello @rocket.cat how are you?'
+			]
+			.forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getUserMentions(text));
+				});
+			});
+			it.skip('shoud return without the "." from "@rocket.cat."', () => {
+				assert.deepEqual(result, mention.getUserMentions('@rocket.cat.'));
+			});
+			it.skip('shoud return without the "_" from "@rocket.cat_"', () => {
+				assert.deepEqual(result, mention.getUserMentions('@rocket.cat_'));
+			});
+			it.skip('shoud return without the "-" from "@rocket.cat."', () => {
+				assert.deepEqual(result, mention.getUserMentions('@rocket.cat-'));
+			});
+		});
+		describe('for two users', () => {
+			const result = ['@rocket.cat', '@all'];
+			[
+				'@rocket.cat @all',
+				' @rocket.cat @all ',
+				'hello @rocket.cat and @all',
+				'@rocket.cat, hello @all',
+				'hello @rocket.cat and @all how are you?'
+			]
+			.forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getUserMentions(text));
+				});
+			});
+		});
 	});
 
-	describe(`msgChannelRegex: ${ msgChannelRegex }`, function() {
-		const str1 = '#general';
-		it(`one channel '${ str1 }'`, function functionName() {
-			const result = str1.match(msgChannelRegex)|| [];
-			assert.equal(result.length, 1);
+	describe('getChannelMentions', function functionName() {
+		describe('for simple text, no mentions', () => {
+			const result = [];
+			[
+				'@rocket.cat',
+				'hello rocket.cat how are you?'
+			]
+			.forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getChannelMentions(text));
+				});
+			});
 		});
-		const str2 = '#general #private';
-		it(`two channels '${ str2 }'`, function functionName() {
-			const result = str2.match(msgChannelRegex)|| [];
-			assert.equal(result.length, 2);
+		describe('for one channel', () => {
+			const result = ['#general'];
+			[
+				'#general',
+				' #general ',
+				'hello #general',
+				'#general, hello',
+				'hello #general, how are you?'
+			].forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getChannelMentions(text));
+				});
+			});
+			it.skip('shoud return without the "." from "#general."', () => {
+				assert.deepEqual(result, mention.getUserMentions('#general.'));
+			});
+			it.skip('shoud return without the "_" from "#general_"', () => {
+				assert.deepEqual(result, mention.getUserMentions('#general_'));
+			});
+			it.skip('shoud return without the "-" from "#general."', () => {
+				assert.deepEqual(result, mention.getUserMentions('#general-'));
+			});
 		});
-		const strTest3 = 'hello #general';
-		it(`text before channels '${ strTest3 }'`, function functionName() {
-			const result = strTest3.match(msgChannelRegex)|| [];
-			assert.equal(result.length, 1);
-		});
-		const strTest4 = '#general, hello';
-		it(`text after channel '${ strTest4 }'`, function functionName() {
-			const result = strTest4.match(msgChannelRegex)|| [];
-			assert.equal(result.length, 1);
+		describe('for two channels', () => {
+			const result = ['#general', '#other'];
+			[
+				'#general #other',
+				' #general #other',
+				'hello #general and #other',
+				'#general, hello #other',
+				'hello #general #other, how are you?'
+			].forEach(text => {
+				it(`should return "${ JSON.stringify(result) }" from "${ text }"`, () => {
+					assert.deepEqual(result, mention.getChannelMentions(text));
+				});
+			});
 		});
 
-		const strTest5 = '#general and #private';
-		it(`text between channels '${ strTest5 }'`, function functionName() {
-			const result = strTest5.match(msgChannelRegex)|| [];
-			assert.equal(result.length, 2);
-		});
 	});
 
 });
-
-describe('Testing replace functions', function() {
-	const message = {
-		mentions:[{username:'rocket.cat'}, {username:'admin'}],
-		channels: [{name: 'general'}]
-	};
-	const me = 'admin';
-	describe('function replaceChannels', function() {
-		const str1 = '#general';
-		it(`an public existent channel '${ str1 }'`, function() {
-			const result = str1.replace(msgChannelRegex, (match, mention, name) => replaceChannels.apply(this, [match, mention, name, message]));
-			assert.equal(result, `<a class="mention-link" data-channel="${ str1.replace('#', '') }">${ str1 }</a>`);
-		});
-
-		const str2 = '#private';
-		it(`an private or non existent channel '${ str2 }'`, function() {
-			const result = str2.replace(msgChannelRegex, (match, mention, name) => replaceChannels.apply(this, [match, mention, name, message]));
-			assert.equal(result, str2);
-		});
-	});
-	describe('function replaceUsers', function() {
-		const str1 = '@unknow.user';
-		it(`an unknow user '${ str1 }'`, function() {
-			const result = str1.replace(msgMentionRegex, (match, mention, username) => replaceUsers.apply(this, [match, mention, username, message, me]));
-			assert.equal(result, str1);
+const message = {
+	mentions:[{username:'rocket.cat'}, {username:'admin'}, {username: 'me'}],
+	channels: [{name: 'general'}, {name: 'rocket.cat'}]
+};
+describe('replace methods', function() {
+	describe('replaceUsers', () => {
+		it('shoud render for @all', () => {
+			const result = mention.replaceUsers('@all', message, 'me');
+			assert.equal('<a class="mention-link mention-link-me mention-link-all background-attention-color">@all</a>', result);
 		});
 		const str2 = '@rocket.cat';
-		it(`a know user '${ str2 }'`, function() {
-			const result = str2.replace(msgMentionRegex, (match, mention, username) => replaceUsers.apply(this, [match, mention, username, message, me]));
+		it(`shoud render for ${ str2 }`, () => {
+			const result = mention.replaceUsers('@rocket.cat', message, 'me');
 			assert.equal(result, `<a class="mention-link " data-username="${ str2.replace('@', '') }">${ str2 }</a>`);
 		});
 
-		const str3 = '@all';
-		it(`'${ str3 }'`, function() {
-			const result = str3.replace(msgMentionRegex, (match, mention, username) => replaceUsers.apply(this, [match, mention, username, message, me]));
-			assert.equal(result, `<a class="mention-link mention-link-me mention-link-all background-attention-color">${ str3 }</a>`);
+		it(`shoud render for "hello ${ str2 }"`, () => {
+			const result = mention.replaceUsers(`hello ${ str2 }`, message, 'me');
+			assert.equal(result, `hello <a class="mention-link " data-username="${ str2.replace('@', '') }">${ str2 }</a>`);
+		});
+		it('shoud render for unknow/private user "hello @unknow"', () => {
+			const result = mention.replaceUsers('hello @unknow', message, 'me');
+			assert.equal(result, 'hello @unknow');
+		});
+		it('shoud render for me', () => {
+			const result = mention.replaceUsers('hello @me', message, 'me');
+			assert.equal(result, 'hello <a class="mention-link mention-link-me background-primary-action-color" data-username="me">@me</a>');
+		});
+	});
+	describe('replaceChannels', () => {
+		it('shoud render for #general', () => {
+			const result = mention.replaceChannels('#general', message, 'me');
+			assert.equal('<a class="mention-link" data-channel="general">#general</a>', result);
+		});
+		const str2 = '#rocket.cat';
+		it(`shoud render for ${ str2 }`, () => {
+			const result = mention.replaceChannels(str2, message, 'me');
+			assert.equal(result, `<a class="mention-link" data-channel="${ str2.replace('#', '') }">${ str2 }</a>`);
+		});
+		it(`shoud render for "hello ${ str2 }"`, () => {
+			const result = mention.replaceChannels(`hello ${ str2 }`, message, 'me');
+			assert.equal(result, `hello <a class="mention-link" data-channel="${ str2.replace('#', '') }">${ str2 }</a>`);
+		});
+		it('shoud render for unknow/private channel "hello #unknow"', () => {
+			const result = mention.replaceChannels('hello #unknow', message, 'me');
+			assert.equal(result, 'hello #unknow');
+		});
+	});
+	describe('parse all', () => {
+		it('shoud render for #general', () => {
+			message.html = '#general';
+			const result = mention.parse(message, 'me');
+			assert.equal('<a class="mention-link" data-channel="general">#general</a>', result.html);
+		});
+		it('shoud render for "#general and @rocket.cat', () => {
+			message.html = '#general and @rocket.cat';
+			const result = mention.parse(message, 'me');
+			assert.equal('<a class="mention-link" data-channel="general">#general</a> and <a class="mention-link " data-username="rocket.cat">@rocket.cat</a>', result.html);
+		});
+		it('shoud render for "', () => {
+			message.html = '';
+			const result = mention.parse(message, 'me');
+			assert.equal('', result.html);
+		});
+		it('shoud render for "simple text', () => {
+			message.html = 'simple text';
+			const result = mention.parse(message, 'me');
+			assert.equal('simple text', result.html);
 		});
 
-		const str4 = '@here';
-		it(`'${ str4 }'`, function() {
-			const result = str4.replace(msgMentionRegex, (match, mention, username) => replaceUsers.apply(this, [match, mention, username, message, me]));
-			assert.equal(result, `<a class="mention-link mention-link-me mention-link-all background-attention-color">${ str4 }</a>`);
-		});
-
-		const str5 = '@admin';
-		it(`me '${ str5 }'`, function() {
-			const result = str5.replace(msgMentionRegex, (match, mention, username) => replaceUsers.apply(this, [match, mention, username, message, me]));
-			assert.equal(result, `<a class="mention-link mention-link-me background-primary-action-color" data-username="${ str5.replace('@', '') }">${ str5 }</a>`);
-		});
 	});
 });
