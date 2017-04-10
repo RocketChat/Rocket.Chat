60a14ea65d91d082b98031c1127c0b9a2fd37a70
Diego Sampaio <chinello@gmail.com>
2017-04-10 15:17:58 -0300

    Add monitoring package instead of kadira

--- packages/kadira/lib/profiler/client.js
+++
@@ -1,9 +0,0 @@
-// For just making a notice
-// meteorhacks:kadira-profiler will override this method to add
-// actual functionality
-Kadira.profileCpu = function profileCpu() {
-  var message =
-    "Please install meteorhacks:kadira-profiler" +
-    " to take a CPU profile.";
-  console.log(message);
-};
--- packages/kadira/tests/_helpers/globals.js
+++
@@ -1,3 +0,0 @@
-MethodStore = [];
-TestData = new Meteor.Collection('tinytest-data');
-TestData2 = new Meteor.Collection('tinytest-data-2');
