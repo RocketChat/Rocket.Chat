2e055efde7e53d63665e80d435795a8ba853424b
Diego Sampaio <chinello@gmail.com>
2017-04-10 15:17:58 -0300

    Add monitoring package instead of kadira

--- packages/kadira/lib/models/0model.js
+++
@@ -1,9 +0,0 @@
-KadiraModel = function() {
-
-};
-
-KadiraModel.prototype._getDateId = function(timestamp) {
-  var remainder = timestamp % (1000 * 60);
-  var dateId = timestamp - remainder;
-  return dateId;
-};
