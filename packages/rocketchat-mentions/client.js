1bdeafda5aa06bddd7cd1487fd35f72fc184f106
Alex Brazier <aejbrazier.apps@gmail.com>
2017-04-14 00:30:54 +0100

    Show full name in mentions

--- packages/rocketchat-mentions/client.js
+++ packages/rocketchat-mentions/client.js
@@ -17,10 +17,12 @@ function MentionsClient(message) {
 		if (['all', 'here'].includes(username)) {
 			return match.replace(mention, `<a class="mention-link mention-link-me mention-link-all background-attention-color">${ mention }</a>`);
 		}
-		if (message.temp == null && _.findWhere(message.mentions, {username}) == null) {
+		const mentionObj = _.findWhere(message.mentions, {username});
+		if (message.temp == null && mentionObj == null) {
 			return match;
 		}
-		return match.replace(mention, `<a class="mention-link ${ username === me ? 'mention-link-me background-primary-action-color':'' }" data-username="${ username }">${ mention }</a>`);
+		const name = RocketChat.settings.get('UI_Use_Real_Name') && mentionObj && mentionObj.name;
+		return match.replace(mention, `<a class="mention-link ${ username === me ? 'mention-link-me background-primary-action-color':'' }" data-username="${ username }" title="${ name ? username : '' }">${ name || mention }</a>`);
 	});
 
 	const msgChannelRegex = new RegExp(`(?:^|\\s|\\n)(#(${ RocketChat.settings.get('UTF8_Names_Validation') }))[:.,\s]?`, 'g');
