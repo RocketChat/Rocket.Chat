7acd73a4ebf279ddc8b7a7775091cb102f306622
Martin Schoeler <martin.schoeler@rocket.chat>
2017-04-20 11:36:43 -0300

    Fix Reviews

--- packages/rocketchat-oembed/client/oembedAudioWidget.js
+++ packages/rocketchat-oembed/client/oembedAudioWidget.js
@@ -1,10 +1,10 @@
 Template.oembedAudioWidget.helpers({
 	collapsed() {
-		const user = Meteor.user();
 		if (this.collapsed) {
 			return this.collapsed;
 		} else {
-			return (user && user.settings && user.settings.preferences && user.settings.preferences.collapseMediaByDefault);
+			const user = Meteor.user();
+			return user && user.settings && user.settings.preferences && user.settings.preferences.collapseMediaByDefault === true;
 		}
 	}
 });
--- packages/rocketchat-oembed/client/oembedFrameWidget.js
+++ packages/rocketchat-oembed/client/oembedFrameWidget.js
@@ -1,10 +1,10 @@
 Template.oembedFrameWidget.helpers({
 	collapsed() {
-		const user = Meteor.user();
 		if (this.collapsed) {
 			return this.collapsed;
 		} else {
-			return (user && user.settings && user.settings.preferences && user.settings.preferences.collapseMediaByDefault) === true;
+			const user = Meteor.user();
+			return user && user.settings && user.settings.preferences && user.settings.preferences.collapseMediaByDefault === true;
 		}
 	}
 });
