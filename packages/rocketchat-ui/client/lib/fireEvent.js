d84897247f3e0335fc1d5931945f6e450c9d9279
Diego Sampaio <chinello@gmail.com>
2017-04-25 17:34:48 -0300

    Wait for iframe setting to start sending postMessages

--- packages/rocketchat-ui/client/lib/fireEvent.js
+++ packages/rocketchat-ui/client/lib/fireEvent.js
@@ -1,10 +1,16 @@
-window.fireGlobalEvent = (eventName, params) => {
-	window.dispatchEvent(new CustomEvent(eventName, {detail: params}));
-
-	if (RocketChat.settings.get('Iframe_Integration_send_enable') === true) {
-		parent.postMessage({
-			eventName,
-			data: params
-		}, RocketChat.settings.get('Iframe_Integration_send_target_origin'));
-	}
+window.fireGlobalEvent = function _fireGlobalEvent(eventName, params) {
+	Tracker.autorun((computation) => {
+		const enabled = RocketChat.settings.get('Iframe_Integration_send_enable');
+		if (enabled === undefined) {
+			return;
+		}
+		computation.stop();
+		if (enabled) {
+			window.dispatchEvent(new CustomEvent(eventName, {detail: params}));
+			parent.postMessage({
+				eventName,
+				data: params
+			}, RocketChat.settings.get('Iframe_Integration_send_target_origin'));
+		}
+	});
 };
