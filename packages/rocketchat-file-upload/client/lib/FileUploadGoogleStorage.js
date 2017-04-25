0365447b2d98b287be71c13ea42cece2a8318159
Diego Sampaio <chinello@gmail.com>
2017-04-25 09:38:03 -0300

    Add Google Cloud Storage support to avatars

--- packages/rocketchat-file-upload/client/lib/FileUploadGoogleStorage.js
+++ packages/rocketchat-file-upload/client/lib/FileUploadGoogleStorage.js
@@ -1,36 +1,19 @@
 /* globals FileUpload, FileUploadBase, Slingshot */
 
 FileUpload.GoogleCloudStorage = class FileUploadGoogleCloudStorage extends FileUploadBase {
-	constructor(meta, file) {
+	constructor(directive, meta, file) {
 		super(meta, file);
-		this.uploader = new Slingshot.Upload('rocketchat-uploads-gs', { rid: meta.rid });
+		this.uploader = new Slingshot.Upload(directive, { rid: meta.rid });
 	}
 
-	start() {
+	start(callback) {
 		this.uploader.send(this.file, (error, downloadUrl) => {
 			if (this.computation) {
 				this.computation.stop();
 			}
 
 			if (error) {
-				let uploading = Session.get('uploading');
-				if (!Array.isArray(uploading)) {
-					uploading = [];
-				}
-
-				const item = _.findWhere(uploading, { id: this.id });
-
-				if (_.isObject(item)) {
-					item.error = error.error;
-					item.percentage = 0;
-				} else {
-					uploading.push({
-						error: error.error,
-						percentage: 0
-					});
-				}
-
-				Session.set('uploading', uploading);
+				return callback.call(this, error);
 			} else {
 				const file = _.pick(this.meta, 'type', 'size', 'name', 'identify', 'description');
 				file._id = downloadUrl.substr(downloadUrl.lastIndexOf('/') + 1);
@@ -38,13 +21,7 @@ FileUpload.GoogleCloudStorage = class FileUploadGoogleCloudStorage extends FileU
 
 				Meteor.call('sendFileMessage', this.meta.rid, 'googleCloudStorage', file, () => {
 					Meteor.setTimeout(() => {
-						const uploading = Session.get('uploading');
-						if (uploading !== null) {
-							const item = _.findWhere(uploading, {
-								id: this.id
-							});
-							return Session.set('uploading', _.without(uploading, item));
-						}
+						callback.call(this, null, file);
 					}, 2000);
 				});
 			}
