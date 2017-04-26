7f3d9d8bc8913a063d614e41185c1a8a4944be5b
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-04-26 14:51:30 -0300

    createToken just receive userId

--- packages/rocketchat-lib/server/methods/createToken.js
+++ packages/rocketchat-lib/server/methods/createToken.js
@@ -1,12 +1,12 @@
 Meteor.methods({
-	createToken({user}) {
-		if (Meteor.userId() !== user._id && !RocketChat.authz.hasPermission(Meteor.userId(), 'user-generate-access-token')) {
+	createToken(userId) {
+		if (Meteor.userId() !== userId && !RocketChat.authz.hasPermission(Meteor.userId(), 'user-generate-access-token')) {
 			throw new Meteor.Error('error-not-authorized', 'Not authorized', { method: 'createToken' });
 		}
 		const token = Accounts._generateStampedLoginToken();
-		Accounts._insertLoginToken(user._id, token);
+		Accounts._insertLoginToken(userId, token);
 		return {
-			userId: user._id,
+			userId,
 			authToken: token.token
 		};
 	}
