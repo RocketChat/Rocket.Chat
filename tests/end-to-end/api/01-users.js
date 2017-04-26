e0b584c9acad4b3321d5291a538877df1164d08f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-04-26 18:12:34 -0300

    createToken - tested if the generated token is valid

--- tests/end-to-end/api/01-users.js
+++ tests/end-to-end/api/01-users.js
@@ -13,134 +13,134 @@ describe('Users', function() {
 
 	it('/users.create', (done) => {
 		request.post(api('users.create'))
-			.set(credentials)
-			.send({
-				email: apiEmail,
-				name: apiUsername,
-				username: apiUsername,
-				password,
-				active: true,
-				roles: ['user'],
-				joinDefaultChannels: true,
-				verified:true
-			})
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.deep.property('user.username', apiUsername);
-				expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
-				expect(res.body).to.have.deep.property('user.active', true);
-				expect(res.body).to.have.deep.property('user.name', apiUsername);
-				targetUser._id = res.body.user._id;
-			})
-			.end(done);
+		.set(credentials)
+		.send({
+			email: apiEmail,
+			name: apiUsername,
+			username: apiUsername,
+			password,
+			active: true,
+			roles: ['user'],
+			joinDefaultChannels: true,
+			verified:true
+		})
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.deep.property('user.username', apiUsername);
+			expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
+			expect(res.body).to.have.deep.property('user.active', true);
+			expect(res.body).to.have.deep.property('user.name', apiUsername);
+			targetUser._id = res.body.user._id;
+		})
+		.end(done);
 	});
 
 	it('/users.info', (done) => {
 		request.get(api('users.info'))
-			.set(credentials)
-			.query({
-				userId: targetUser._id
-			})
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.deep.property('user.username', apiUsername);
-				expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
-				expect(res.body).to.have.deep.property('user.active', true);
-				expect(res.body).to.have.deep.property('user.name', apiUsername);
-			})
-			.end(done);
+		.set(credentials)
+		.query({
+			userId: targetUser._id
+		})
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.deep.property('user.username', apiUsername);
+			expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
+			expect(res.body).to.have.deep.property('user.active', true);
+			expect(res.body).to.have.deep.property('user.name', apiUsername);
+		})
+		.end(done);
 	});
 
 	it('/users.getPresence', (done) => {
 		request.get(api('users.getPresence'))
-			.set(credentials)
-			.query({
-				userId: targetUser._id
-			})
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.deep.property('presence', 'offline');
-			})
-			.end(done);
+		.set(credentials)
+		.query({
+			userId: targetUser._id
+		})
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.deep.property('presence', 'offline');
+		})
+		.end(done);
 	});
 
 	it('/users.list', (done) => {
 		request.get(api('users.list'))
-			.set(credentials)
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.property('count');
-				expect(res.body).to.have.property('total');
-			})
-			.end(done);
+		.set(credentials)
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.property('count');
+			expect(res.body).to.have.property('total');
+		})
+		.end(done);
 	});
 
 	it.skip('/users.list', (done) => {
-	//filtering user list
+		//filtering user list
 		request.get(api('users.list'))
-			.set(credentials)
-			.query({
-				name: { '$regex': 'g' }
-			})
-			.field('username', 1)
-			.sort('createdAt', -1)
-			.expect(log)
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.property('count');
-				expect(res.body).to.have.property('total');
-			})
-			.end(done);
+		.set(credentials)
+		.query({
+			name: { '$regex': 'g' }
+		})
+		.field('username', 1)
+		.sort('createdAt', -1)
+		.expect(log)
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.property('count');
+			expect(res.body).to.have.property('total');
+		})
+		.end(done);
 	});
 
 
 
 	it.skip('/users.setAvatar', (done) => {
 		request.post(api('users.setAvatar'))
-			.set(credentials)
-			.attach('avatarUrl', imgURL)
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-			})
-			.end(done);
+		.set(credentials)
+		.attach('avatarUrl', imgURL)
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+		})
+		.end(done);
 	});
 
 	it('/users.update', (done) => {
 		request.post(api('users.update'))
-			.set(credentials)
-			.send({
-				userId: targetUser._id,
-				data :{
-					email: apiEmail,
-					name: `edited${ apiUsername }`,
-					username: `edited${ apiUsername }`,
-					password,
-					active: true,
-					roles: ['user']
-				}
-			})
-			.expect('Content-Type', 'application/json')
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).to.have.property('success', true);
-				expect(res.body).to.have.deep.property('user.username', `edited${ apiUsername }`);
-				expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
-				expect(res.body).to.have.deep.property('user.active', true);
-				expect(res.body).to.have.deep.property('user.name', `edited${ apiUsername }`);
-			})
-			.end(done);
+		.set(credentials)
+		.send({
+			userId: targetUser._id,
+			data :{
+				email: apiEmail,
+				name: `edited${ apiUsername }`,
+				username: `edited${ apiUsername }`,
+				password,
+				active: true,
+				roles: ['user']
+			}
+		})
+		.expect('Content-Type', 'application/json')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body).to.have.property('success', true);
+			expect(res.body).to.have.deep.property('user.username', `edited${ apiUsername }`);
+			expect(res.body).to.have.deep.property('user.emails[0].address', apiEmail);
+			expect(res.body).to.have.deep.property('user.active', true);
+			expect(res.body).to.have.deep.property('user.name', `edited${ apiUsername }`);
+		})
+		.end(done);
 	});
 
 	describe('/users.createToken', () => {
@@ -149,29 +149,29 @@ describe('Users', function() {
 			const username = `user.test.${ Date.now() }`;
 			const email = `${ username }@rocket.chat`;
 			request.post(api('users.create'))
-				.set(credentials)
-				.send({ email, name: username, username, password })
-				.end((err, res) => {
-					user = res.body.user;
-					done();
-				});
+			.set(credentials)
+			.send({ email, name: username, username, password })
+			.end((err, res) => {
+				user = res.body.user;
+				done();
+			});
 		});
 
 		let userCredentials;
 		beforeEach((done) => {
 			request.post(api('login'))
-				.send({
-					user: user.username,
-					password
-				})
-				.expect('Content-Type', 'application/json')
-				.expect(200)
-				.expect((res) => {
-					userCredentials = {};
-					userCredentials['X-Auth-Token'] = res.body.data.authToken;
-					userCredentials['X-User-Id'] = res.body.data.userId;
-				})
-				.end(done);
+			.send({
+				user: user.username,
+				password
+			})
+			.expect('Content-Type', 'application/json')
+			.expect(200)
+			.expect((res) => {
+				userCredentials = {};
+				userCredentials['X-Auth-Token'] = res.body.data.authToken;
+				userCredentials['X-User-Id'] = res.body.data.userId;
+			})
+			.end(done);
 		});
 		afterEach(done => {
 			request.post(api('users.delete')).set(credentials).send({
@@ -245,6 +245,24 @@ describe('Users', function() {
 					expect(res.body).to.have.property('message');
 				})
 				.end(done);
+			});
+		});
+
+		describe('Testing if the returned token is valid', (done) => {
+			it('should return 200', (done) => {
+				return request.post(api('users.createToken'))
+				.set(credentials)
+				.send({ username: user.username })
+				.expect('Content-Type', 'application/json')
+				.end((err, res) => {
+					return err ? done () : request.get(api('me'))
+					.set({ 'X-Auth-Token': `${ res.body.data.authToken }`, 'X-User-Id': res.body.data.userId })
+					.expect(200)
+					.expect((res) => {
+						expect(res.body).to.have.property('success', true);
+					})
+					.end(done);
+				});
 			});
 		});
 	});
