7e59f7648ca5f442bbdf6e60382ffd7b0a40cf90
Nonthawat Srichad <nonthz@gmail.com>
2017-04-30 09:12:51 +0700

    Added mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,7 @@
+App.configurePlugin('cordova-plugin-googleplus', {
+  REVERSED_CLIENT_ID: 'com.googleusercontent.apps.XXXXXX'
+});
+
+App.configurePlugin('phonegap-plugin-push', {
+  SENDER_ID: 'XXXXXXX'
+});
