5c1aeb05ebdc0db3fe636c6bbed9b437fac70a58
Nonthawat Srichad <nonthz@gmail.com>
2017-05-08 09:37:02 +0700

    Implemented set Slack file id to RocketChat Messages model

--- packages/rocketchat-slackbridge/server/models/Messages.js
+++ packages/rocketchat-slackbridge/server/models/Messages.js
@@ -3,3 +3,28 @@ RocketChat.models.Messages.findOneBySlackFileId = function(slackFileID) {
 		'file.imported_id': slackFileID
 	});
 };
+
+RocketChat.models.Messages.setSlackFileId = function(_id, slackFileID) {
+	const updateObj = {
+		'file.imported_id': slackFileID
+	};
+
+	return this.update({ _id: _id }, { $set: updateObj });
+};
+
+RocketChat.models.Messages.setSlackTs = function(_id, slackTs) {
+	const updateObj = {
+		slackTs: slackTs
+	};
+
+	return this.update({ _id: _id }, { $set: updateObj });
+};
+
+RocketChat.models.Messages.setSlackFileIdAndSlackTs = function(_id, slackFileID, slackTs) {
+	const updateObj = {
+		'file.imported_id': slackFileID,
+		slackTs: slackTs
+	};
+
+	return this.update({ _id: _id }, { $set: updateObj });
+};
