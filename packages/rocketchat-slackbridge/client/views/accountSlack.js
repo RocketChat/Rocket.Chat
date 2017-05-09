713030bc726b0d67de180a40e680fddc3add4c0a
Nonthawat Srichad <nonthz@gmail.com>
2017-05-09 11:52:51 +0700

    Added Slack account config page

---
+++ packages/rocketchat-slackbridge/client/views/accountSlack.js
@@ -0,0 +1,18 @@
+
+Template.accountSlack.helpers({
+	hasSlackAccount() {
+		const user = Meteor.user();
+		return (user && user.settings && user.settings.slack);
+	},
+	slackAccessToken() {
+		const user = Meteor.user();
+		return user.settings.slack.access_token;
+	},
+	authorizationUrl() {
+		const client_id = RocketChat.settings.get('SlackBridge_Client_ID');
+		const scopes = RocketChat.settings.get('SlackBridge_OAuth_Scopes');
+		const redirect_uri = RocketChat.settings.get('SlackBridge_OAuth_Redirect_Url');
+
+		return `https://slack.com/oauth/authorize?scope=${ scopes }&client_id=${ client_id }&redirect_uri=${ redirect_uri }`;
+	}
+});
