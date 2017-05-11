f21f0fb5507c19b99e82d7e24a240b4144327d26
Martin Schoeler <martin.schoeler@rocket.chat>
2017-05-11 12:48:34 -0300

    fix reviews

--- packages/rocketchat-ui-account/client/avatar/avatar.js
+++ packages/rocketchat-ui-account/client/avatar/avatar.js
@@ -1,7 +1,7 @@
 Template.avatar.helpers({
 	imageUrl() {
 		let username = this.username;
-		if ((username == null) && (this.userId != null)) {
+		if (username == null && this.userId != null) {
 			const user = Meteor.users.findOne(this.userId);
 			username = user && user.username;
 		}
