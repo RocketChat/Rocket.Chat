f8c8633b38ac79695c396ab17735ef69ed2d43a4
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-05-10 23:20:26 -0300

    Fix versions and add build number to develop build

--- .travis/setartname.sh
+++ .travis/setartname.sh
@@ -1,1 +1,6 @@
-export ARTIFACT_NAME="$(meteor npm run version --silent)"
+if [[ $TRAVIS_BRANCH ]]
+ then
+  export ARTIFACT_NAME="$(meteor npm run version --silent).$TRAVIS_BUILD_NUMBER"
+else
+  export ARTIFACT_NAME="$(meteor npm run version --silent)"
+fi
