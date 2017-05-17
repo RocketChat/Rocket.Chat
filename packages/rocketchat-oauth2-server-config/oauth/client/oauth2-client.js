a1856b9bbb689213c645db127590ecdba3010fb0
Martin Schoeler <martin.schoeler@rocket.chat>
2017-05-17 18:47:11 -0300

    wip

---
+++ packages/rocketchat-oauth2-server-config/oauth/client/oauth2-client.js
@@ -0,0 +1,59 @@
+// @ChatOAuthApps = new Mongo.Collection 'rocketchat_oauth_apps'
+/*globals ChatOAuthApps */
+FlowRouter.route('/oauth/authorize', {
+	action(params, queryParams) {
+		BlazeLayout.render('main', {
+			center: 'authorize',
+			modal: true,
+			client_id: queryParams.client_id,
+			redirect_uri: queryParams.redirect_uri,
+			response_type: queryParams.response_type,
+			state: queryParams.state
+		});
+	}
+});
+
+FlowRouter.route('/oauth/error/:error', {
+	action(params) {
+		BlazeLayout.render('main', {
+			center: 'oauth404',
+			modal: true,
+			error: params.error
+		});
+	}
+});
+
+Template.authorize.onCreated(function() {
+	this.subscribe('authorizedOAuth');
+	this.subscribe('oauthClient', this.data.client_id());
+});
+
+Template.authorize.helpers({
+	getToken() {
+		return localStorage.getItem('Meteor.loginToken');
+	},
+	getClient() {
+		return ChatOAuthApps.findOne();
+	}
+});
+
+Template.authorize.events({
+	'click #logout-oauth'() {
+		return Meteor.logout();
+	},
+	'click #cancel-oauth'() {
+		return window.close();
+	}
+});
+
+Template.authorize.onRendered(function() {
+	this.autorun((c) => {
+		const user = Meteor.user();
+		if (user && user.oauth && user.oauth.authorizedClients) {
+			if (user.oauth.authorizedClients.indexOf(this.data.client_id() > -1)) {
+				c.stop();
+				$('button[type=submit]').click();
+			}
+		}
+	});
+});
