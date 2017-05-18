f3266819c953d3b728d5f94e9193d1af5c3d81b0
Karl Prieb <karlprieb@gmail.com>
2017-05-18 16:55:45 -0300

    roomlist

--- packages/rocketchat-ui-sidenav/client/roomList.js
+++ packages/rocketchat-ui-sidenav/client/roomList.js
@@ -1,15 +1,33 @@
 Template.roomList.helpers({
 	rooms() {
+		if (this.unread) {
+			return Template.instance().unreadRooms;
+		}
+
+		if (this.anonymous) {
+			return RocketChat.models.Rooms.find({t: 'c'}, { sort: { name: 1 } });
+		}
+
+		const user = Meteor.user();
+		const favoritesEnabled = RocketChat.settings.get('Favorite_Rooms');
+
 		const query = {
-			t: { $in: [this.identifier]},
 			open: true
 		};
 
-		if (RocketChat.settings.get('Favorite_Rooms')) {
-			query.f = { $ne: true };
+		if (this.identifier) {
+			if (this.isCombined) {
+				query.t = { $in: ['c', 'p']};
+			} else {
+				query.t = { $in: [this.identifier] };
+			}
+
+			query.f = { $ne: favoritesEnabled };
+		} else {
+			query.f = favoritesEnabled;
 		}
 
-		if (Meteor.user() && Meteor.user().settings && Meteor.user().settings.preferences && Meteor.user().settings.preferences.unreadRoomsMode) {
+		if (user && user.settings && user.settings.preferences && user.settings.preferences.unreadRoomsMode) {
 			query.$or = [
 				{ alert: { $ne: true } },
 				{ hideUnreadStatus: true }
@@ -19,14 +37,60 @@ Template.roomList.helpers({
 		return ChatSubscription.find(query, { sort: { 't': 1, 'name': 1 }});
 	},
 
-	favoritesCount() {
-		return ChatSubscription.find({ f: true }).count();
+	isActiveFavorite() {
+		if (!this.identifier && !this.unread) {
+			return ChatSubscription.find({ f: true }).count();
+		}
+
+		return true;
+	},
+
+	isLivechat() {
+		return this.identifier === 'l';
+	},
+
+	unreadClass(room) {
+		let classes = '';
+
+		if (room.unread) {
+			const user = Meteor.user();
+			classes += 'unread-rooms-mode';
+
+			if ((user && user.settings && user.settings.preferences && user.settings.preferences.unreadRoomsMode) && (Template.instance().unreadRooms.count())) {
+				classes += ' has-unread';
+			}
+		}
+
+		return classes;
+	},
+
+	hasMoreButton(room) {
+		return room.identifier === 'c' || room.identifier === 'p' || room.anonymous;
 	}
 });
 
 Template.roomList.events({
-	'click .more-channels'() {
-		SideNav.setFlex('listChannelsFlex');
+	'click .more-channels'(e, t) {
+		if (t.data.identifier === 'p') {
+			SideNav.setFlex('listPrivateGroupsFlex');
+		} else if (t.data.isCombined) {
+			SideNav.setFlex('listCombinedFlex');
+		} else {
+			SideNav.setFlex('listChannelsFlex');
+		}
+
 		return SideNav.openFlex();
 	}
+});
+
+Template.roomList.onCreated(function() {
+	this.autorun(() => {
+		const query = {
+			alert: true,
+			open: true,
+			hideUnreadStatus: { $ne: true }
+		};
+
+		return this.unreadRooms = ChatSubscription.find(query, { sort: { 't': 1, 'name': 1 }});
+	});
 });
