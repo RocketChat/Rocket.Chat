55dc4a032847c9f8d9520178a3650f454a990588
Martin Schoeler <martin.schoeler@rocket.chat>
2017-05-19 12:52:10 -0300

    try to fix the test

--- tests/end-to-end/ui/06-messaging.js
+++ tests/end-to-end/ui/06-messaging.js
@@ -192,7 +192,7 @@ function messageActionsTest() {
 
 				it('delete the message', () => {
 					mainContent.selectAction('delete');
-					mainContent.popupFileConfirmBtn.waitForVisible(3000);
+					mainContent.popupFileConfirmBtn.waitForVisible(7000);
 					mainContent.popupFileConfirmBtn.click();
 					Global.sweetAlertOverlay.waitForVisible(3000, true);
 				});
