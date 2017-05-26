e231f0fa6f7e42cfb1774dcae75ecdadcaf82f60
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-05-26 11:54:52 -0300

    Add only options Uploads and Avatars to client

---
+++ packages/rocketchat-file-upload/server/config/configStore.js
@@ -0,0 +1,13 @@
+/* globals UploadFS */
+
+const configStore = _.debounce(() => {
+	const store = RocketChat.settings.get('FileUpload_Storage_Type');
+
+	if (store) {
+		console.log('Setting default file store to', store);
+		UploadFS.getStores().Avatars = UploadFS.getStore(`${ store }:Avatars`);
+		UploadFS.getStores().Uploads = UploadFS.getStore(`${ store }:Uploads`);
+	}
+}, 500);
+
+RocketChat.settings.get(/^FileUpload_/, configStore);
