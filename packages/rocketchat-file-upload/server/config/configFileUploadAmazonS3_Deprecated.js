fac26cce9fab7c8474132fe58214b99e60d73583
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-05-27 17:04:59 -0300

    Uploads: Remove uncessary code

--- packages/rocketchat-file-upload/server/config/configFileUploadAmazonS3_Deprecated.js
+++ packages/rocketchat-file-upload/server/config/configFileUploadAmazonS3_Deprecated.js
@@ -1,61 +1,4 @@
-/* globals Slingshot, FileUpload, AWS */
-import { FileUploadClass } from '../lib/FileUpload';
-import AWS4 from '../lib/AWS4.js';
-
-let S3accessKey;
-let S3secretKey;
-let S3expiryTimeSpan;
-
-const generateURL = function(file) {
-	if (!file || !file.s3) {
-		return;
-	}
-
-	const credential = {
-		accessKeyId: S3accessKey,
-		secretKey: S3secretKey
-	};
-
-	const req = {
-		bucket: file.s3.bucket,
-		region: file.s3.region,
-		path: `/${ file.s3.path }${ file._id }`,
-		url: file.url,
-		expire: Math.max(5, S3expiryTimeSpan)
-	};
-
-	const queryString = AWS4.sign(req, credential);
-
-	return `${ file.url }?${ queryString }`;
-};
-
-const getFile = function(file, req, res) {
-	const fileUrl = generateURL(file);
-
-	if (fileUrl) {
-		res.setHeader('Location', fileUrl);
-		res.writeHead(302);
-	}
-	res.end();
-};
-
-const deleteFile = function(file) {
-	const s3 = new AWS.S3();
-	const request = s3.deleteObject({
-		Bucket: file.s3.bucket,
-		Key: file.s3.path + file._id
-	});
-	request.send();
-};
-
-// DEPRECATED: backwards compatibility (remove)
-new FileUploadClass({
-	name: 's3',
-	model: 'Uploads',
-
-	get: getFile,
-	delete: deleteFile
-});
+/* globals Slingshot, FileUpload */
 
 function createDirective(directiveName, { key, bucket, accessKey, secretKey, region, acl, cdn, bucketUrl}) {
 	if (Slingshot._directives[directiveName]) {
@@ -92,48 +35,6 @@ function createDirective(directiveName, { key, bucket, accessKey, secretKey, reg
 }
 
 const configureSlingshot = _.debounce(() => {
-	const directives = [
-		{
-			name: 'rocketchat-uploads',
-			key(file, metaContext) {
-				const path = `${ RocketChat.hostname }/${ metaContext.rid }/${ this.userId }/`;
-
-				const upload = {
-					rid: metaContext.rid,
-					s3: {
-						bucket: RocketChat.settings.get('FileUpload_S3_Bucket'),
-						region: RocketChat.settings.get('FileUpload_S3_Region'),
-						path
-					}
-				};
-				const fileId = RocketChat.models.Uploads.insertFileInit(this.userId, 'AmazonS3:Uploads', file, upload);
-
-				return path + fileId;
-			}
-		},
-		{
-			name: 'rocketchat-avatars',
-			key(file/*, metaContext*/) {
-				const path = `${ RocketChat.hostname }/avatars/`;
-
-				const user = RocketChat.models.Users.findOneById(this.userId);
-
-				const upload = {
-					username: user && user.username,
-					s3: {
-						bucket: RocketChat.settings.get('FileUpload_S3_Bucket'),
-						region: RocketChat.settings.get('FileUpload_S3_Region'),
-						path
-					}
-				};
-				delete file.name;
-				RocketChat.models.Avatars.insertAvatarFileInit(user.username, this.userId, 'AmazonS3:Avatars', file, upload);
-
-				return path + user.username;
-			}
-		}
-	];
-
 	const type = RocketChat.settings.get('FileUpload_Storage_Type');
 	const bucket = RocketChat.settings.get('FileUpload_S3_Bucket');
 	const acl = RocketChat.settings.get('FileUpload_S3_Acl');
@@ -143,47 +44,36 @@ const configureSlingshot = _.debounce(() => {
 	const region = RocketChat.settings.get('FileUpload_S3_Region');
 	const bucketUrl = RocketChat.settings.get('FileUpload_S3_BucketURL');
 
-	AWS.config.update({
-		accessKeyId: RocketChat.settings.get('FileUpload_S3_AWSAccessKeyId'),
-		secretAccessKey: RocketChat.settings.get('FileUpload_S3_AWSSecretAccessKey')
-	});
+	delete Slingshot._directives['rocketchat-uploads'];
 
 	if (type === 'AmazonS3' && !_.isEmpty(bucket) && !_.isEmpty(accessKey) && !_.isEmpty(secretKey)) {
-		directives.forEach((conf) => {
-			createDirective(conf.name, { key: conf.key, bucket, accessKey, secretKey, region, acl, cdn, bucketUrl});
-		});
-	} else {
-		directives.forEach((conf) => {
-			if (Slingshot._directives[conf.name]) {
-				delete Slingshot._directives[conf.name];
-			}
+		createDirective('rocketchat-uploads', {
+			key(file, metaContext) {
+				const id = Random.id();
+				const path = `${ RocketChat.settings.get('uniqueID') }/uploads/${ metaContext.rid }/${ this.userId }/${ id }`;
+
+				const upload = {
+					_id: id,
+					rid: metaContext.rid,
+					AmazonS3: {
+						path
+					}
+				};
+
+				RocketChat.models.Uploads.insertFileInit(this.userId, 'AmazonS3:Uploads', file, upload);
+
+				return path;
+			},
+			bucket,
+			accessKey,
+			secretKey,
+			region,
+			acl,
+			cdn,
+			bucketUrl
 		});
 	}
 }, 500);
 
 RocketChat.settings.get('FileUpload_Storage_Type', configureSlingshot);
-
-RocketChat.settings.get('FileUpload_S3_Bucket', configureSlingshot);
-
-RocketChat.settings.get('FileUpload_S3_Acl', configureSlingshot);
-
-RocketChat.settings.get('FileUpload_S3_AWSAccessKeyId', function(key, value) {
-	S3accessKey = value;
-	configureSlingshot();
-});
-
-RocketChat.settings.get('FileUpload_S3_AWSSecretAccessKey', function(key, value) {
-	S3secretKey = value;
-	configureSlingshot();
-});
-
-RocketChat.settings.get('FileUpload_S3_URLExpiryTimeSpan', function(key, value) {
-	S3expiryTimeSpan = value;
-	configureSlingshot();
-});
-
-RocketChat.settings.get('FileUpload_S3_CDN', configureSlingshot);
-
-RocketChat.settings.get('FileUpload_S3_Region', configureSlingshot);
-
-RocketChat.settings.get('FileUpload_S3_BucketURL', configureSlingshot);
+RocketChat.settings.get(/^FileUpload_S3_/, configureSlingshot);
