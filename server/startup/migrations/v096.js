c01487afdae32d287928b400119c821b0e15aa2b
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-05-29 16:01:49 -0300

    Uploads: Migrate old avatars

--- server/startup/migrations/v096.js
+++ server/startup/migrations/v096.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 RocketChat.Migrations.add({
 	version: 96,
 	up() {
@@ -59,5 +62,62 @@ RocketChat.Migrations.add({
 		}, {
 			multi: true
 		});
+
+		const avatarOrigins = [
+			'upload',
+			'gravatar',
+			'facebook',
+			'twitter',
+			'github',
+			'google',
+			'url',
+			'gitlab',
+			'linkedin'
+		];
+
+		const avatarsFileStore = FileUpload.getStore('Avatars');
+
+		const oldAvatarGridFS = new RocketChatFile.GridFS({
+			name: 'avatars'
+		});
+
+
+		RocketChat.models.Users.find({avatarOrigin: {$in: avatarOrigins}}, {avatarOrigin: 1, username: 1}).forEach((user) => {
+			const id = `${ user.username }.jpg`;
+			const gridFSAvatar = oldAvatarGridFS.getFileWithReadStream(id);
+
+			if (gridFSAvatar) {
+				const details = {
+					userId: user._id,
+					type: gridFSAvatar.contentType,
+					size: gridFSAvatar.length
+				};
+
+				avatarsFileStore.insert(details, gridFSAvatar.readStream, () => {
+					oldAvatarGridFS.deleteFile(id);
+				});
+			} else {
+				const avatarsPath = RocketChat.settings.get('Accounts_AvatarStorePath');
+				if (avatarsPath && avatarsPath.trim()) {
+					const filePath = path.join(avatarsPath, id);
+					const stat = fs.statSync(filePath);
+					if (stat && stat.isFile()) {
+						const rs = fs.createReadStream(filePath);
+						const details = {
+							userId: user._id,
+							type: 'image/jpeg',
+							size: stat.size
+						};
+
+						avatarsFileStore.insert(details, rs, () => {
+							fs.unlinkSync(filePath);
+						});
+					}
+				}
+			}
+		});
+
+		RocketChat.models.Settings.remove({_id: 'Accounts_AvatarStoreType'});
+		RocketChat.models.Settings.remove({_id: 'Accounts_AvatarStorePath'});
 	}
 });
