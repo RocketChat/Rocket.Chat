b8f0a4e53269d0a7500d2668cce25fff980c6f03
Luis Fernando do Nascimento <luisfn@gmail.com>
2017-05-31 00:16:29 -0300

    Fixing test where field was not found

--- tests/pageobjects/login.page.js
+++ tests/pageobjects/login.page.js
@@ -11,7 +11,7 @@ class LoginPage extends Page {
 	get emailField() { return browser.element('[name=email]'); }
 	get passwordField() { return browser.element('[name=pass]'); }
 	get confirmPasswordField() { return browser.element('[name=confirm-pass]'); }
-	get reasonField() { return browser.element('[reason]'); }
+	get reasonField() { return browser.element('[name=reason]'); }
 	get inputUsername() { return browser.element('form#login-card input#username'); }
 
 	get emailOrUsernameInvalidText() { return browser.element('[name=emailOrUsername]~.input-error'); }
