2ad24e16ff5e8907e0bda1e4a1e8ffa0c92f1674
Aaron Ogle <aaron@geekgonecrazy.com>
2017-06-06 19:54:00 -0500

    Revert "Merge branch 'rc-fix-mobile-avatars' into develop"
    This reverts commit 8d9e3d726a85287a8a9ab40bcba14b0d31ffa4a9, reversing
    changes made to ecc433967a33420f91ef9585f087dcd03ca72768.

--- server/startup/avatar.js
+++ server/startup/avatar.js
@@ -18,8 +18,6 @@ Meteor.startup(function() {
 			let username = decodeURIComponent(match[1]);
 			let file;
 
-			username = username.replace(/\.jpg$/, '');
-
 			if (username[0] !== '@') {
 				if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.sandstorm) {
 					const user = RocketChat.models.Users.findOneByUsername(username);
