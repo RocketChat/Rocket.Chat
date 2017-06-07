2521d35d9e9472bd8481cfe0e8a69a6299e3a668
Gustavo Aguiar <gugahoa@gmail.com>
2017-06-07 11:06:36 -0300

    fix: no need to add bot to permissions

--- packages/rocketchat-livechat/permissions.js
+++ packages/rocketchat-livechat/permissions.js
@@ -10,10 +10,10 @@ Meteor.startup(() => {
 		RocketChat.models.Roles.createOrUpdate('livechat-guest');
 	}
 	if (RocketChat.models && RocketChat.models.Permissions) {
-		RocketChat.models.Permissions.createOrUpdate('view-l-room', ['bot', 'livechat-agent', 'livechat-manager', 'admin']);
+		RocketChat.models.Permissions.createOrUpdate('view-l-room', ['livechat-agent', 'livechat-manager', 'admin']);
 		RocketChat.models.Permissions.createOrUpdate('view-livechat-manager', ['livechat-manager', 'admin']);
 		RocketChat.models.Permissions.createOrUpdate('view-livechat-rooms', ['livechat-manager', 'admin']);
-		RocketChat.models.Permissions.createOrUpdate('close-livechat-room', ['bot', 'livechat-agent', 'livechat-manager', 'admin']);
+		RocketChat.models.Permissions.createOrUpdate('close-livechat-room', ['livechat-agent', 'livechat-manager', 'admin']);
 		RocketChat.models.Permissions.createOrUpdate('close-others-livechat-room', ['livechat-manager', 'admin']);
 		RocketChat.models.Permissions.createOrUpdate('save-others-livechat-room-info', ['livechat-manager']);
 	}
