63f1522a676dbe2d95d780faa9b9983ada887d45
Raj Ved <rasheshved7@gmail.com>
2017-06-09 10:53:32 +0530

    Solved Errors

--- packages/rocketchat-lib/server/models/Messages.js
+++ packages/rocketchat-lib/server/models/Messages.js
@@ -35,15 +35,14 @@ RocketChat.models.Messages = new class extends RocketChat.models._Base {
 		return this.find(query, options);
 	}
 
-	findAllMentions(rid,options){
+	findAllMentions(rid, options) {
 		const query={
 			'mentions.username': { $exists: true },
 			rid
 		};
-		
+
 		return this.find(query, options);
 	}
-
 
 	findVisibleByRoomId(roomId, options) {
 		const query = {
