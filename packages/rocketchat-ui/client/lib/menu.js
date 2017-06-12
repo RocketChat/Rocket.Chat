5f07beab3d91250f93a4ac0fc55aecac49182946
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-06-12 16:18:59 -0300

    close menu on click body

--- packages/rocketchat-ui/client/lib/menu.js
+++ packages/rocketchat-ui/client/lib/menu.js
@@ -3,6 +3,7 @@ const sideNavW = 280;
 this.menu = new class {
 	constructor() {
 		this._onOpen = [];
+		this._open = false;
 		this._onClose = [];
 		this.updateUnreadBars = _.throttle(() => {
 			if (this.list == null) {
@@ -35,8 +36,14 @@ this.menu = new class {
 	}
 	init() {
 		this.mainContent = $('.main-content');
+
 		this.list = $('.rooms-list');
-		Session.set('isMenuOpen', false);
+		this._open = false;
+		Session.set('isMenuOpen', this._open);
+
+		this.mainContent[0].addEventListener('click', _.debounce(() => {
+			this._open && this.close();
+		}, 300));
 	}
 
 	isOpen() {
@@ -53,13 +60,15 @@ this.menu = new class {
 		}
 	}
 	open() {
-		Session.set('isMenuOpen', true);
+		this._open = true;
+		Session.set('isMenuOpen', this._open);
 		this.mainContent && this.mainContent.css('transform', `translateX(${ isRtl(localStorage.getItem('userLanguage'))?'-':'' }${ this.sideNavW }px)`);
 		setTimeout(() => this._onOpen.forEach(fn => fn.apply(this)), 10);
 	}
 
 	close() {
-		Session.set('isMenuOpen', false);
+		this._open = false;
+		Session.set('isMenuOpen', this._open);
 		this.mainContent && this.mainContent .css('transform', 'translateX(0)');
 		setTimeout(() => this._onClose.forEach(fn => fn.apply(this)), 10);
 	}
