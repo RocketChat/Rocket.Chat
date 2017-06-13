480f06bca33030f75109c368f1e1d99b2e97344b
Kenneth Ellis McCall <xellisx@gmail.com>
2017-06-13 15:01:29 -0500

    Tabs

--- packages/rocketchat-markdown/settings.js
+++ packages/rocketchat-markdown/settings.js
@@ -5,7 +5,25 @@ Meteor.startup(() => {
 		section: 'Markdown',
 		public: true
 	});
-
+	RocketChat.settings.add('Markdown_Colors', 'no', {
+		type: 'select',
+		values: [
+			{
+				key: 'no',
+				i18nLabel: 'Markdown_No_Colors'
+			}, {
+				key: 'irc',
+				i18nLabel: 'Markdown_IRC_Colors'
+			}, {
+				key: 'full',
+				i18nLabel: 'Markdown_Full_Colors'
+			}
+		],
+		group: 'Message',
+		section: 'Markdown',
+		public: true,
+		i18nDescription: 'Markdown_ColorSupport_Description'
+    });
 	return RocketChat.settings.add('Markdown_SupportSchemesForLink', 'http,https', {
 		type: 'string',
 		group: 'Message',
@@ -13,4 +31,5 @@ Meteor.startup(() => {
 		public: true,
 		i18nDescription: 'Markdown_SupportSchemesForLink_Description'
 	});
+
 });
