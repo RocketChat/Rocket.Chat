fe3f698aabafe12f6b089065f2cf2ed6cfc2254a
Kenneth Ellis McCall <xellisx@gmail.com>
2017-06-12 23:51:19 -0500

    Markdown colors settings.

--- packages/rocketchat-markdown/markdown.js
+++ packages/rocketchat-markdown/markdown.js
@@ -68,11 +68,19 @@ class MarkdownClass {
 			return `<a href="${ _.escapeHTML(url) }" target="${ _.escapeHTML(target) }">${ _.escapeHTML(title) }</a>`;
 		});
 
-		// IRC Color Support
-		msg = msg.replace(/\{color fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$1;">$2</span>');
-		msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="background-color:$1;">$2</span>');
-		msg = msg.replace(/\{color fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$1; background-color:$2;">$3</span>');
-		msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$2; background-color:$1;">$3</span>');
+        if(RocketChat.settings.get('Markdown_colors') === 'irc') {
+			// IRC Color Support
+			msg = msg.replace(/\{color fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$1;">$2</span>');
+			msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="background-color:$1;">$2</span>');
+			msg = msg.replace(/\{color fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$1; background-color:$2;">$3</span>');
+			msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$2; background-color:$1;">$3</span>');
+        }
+        else if(RocketChat.settigns.get('Markdown_colors') === 'full') {
+			msg = msg.replace(/\{color fg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="color:$1;">$2</span>');
+			msg = msg.replace(/\{color bg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="background-color:$1;">$2</span>');
+			msg = msg.replace(/\{color fg:([#\w]\w+) bg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="color:$1; background-color:$2;">$3</span>');
+			msg = msg.replace(/\{color bg:([#\w]\w+) fg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="color:$2; background-color:$1;">$3</span>');
+		}
 
 		if (typeof window !== 'undefined' && window !== null ? window.rocketDebug : undefined) { console.log('Markdown', msg); }
 
