0b3ed323853cf33a3c3a2422e0c5a6a10c4ed3a2
Kenneth Ellis McCall <xellisx@gmail.com>
2017-06-13 21:43:21 -0500

    Typo.

--- packages/rocketchat-markdown/markdown.js
+++ packages/rocketchat-markdown/markdown.js
@@ -74,7 +74,7 @@ class MarkdownClass {
 			msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="background-color:$1;">$2</span>');
 			msg = msg.replace(/\{color fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$1; background-color:$2;">$3</span>');
 			msg = msg.replace(/\{color bg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray) fg:(White|Black|Blue|Green|Red|Brown|Purple|Orange|Yellow|LightGreen|Cyan|LightCyan|LightBlue|PinkGray|LightGray)\}([^{]+)\{\/color\}/img, '<span style="color:$2; background-color:$1;">$3</span>');
-		} else if (RocketChat.settigns.get('Markdown_colors') === 'full') {
+		} else if (RocketChat.settings.get('Markdown_colors') === 'full') {
 			msg = msg.replace(/\{color fg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="color:$1;">$2</span>');
 			msg = msg.replace(/\{color bg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="background-color:$1;">$2</span>');
 			msg = msg.replace(/\{color fg:([#\w]\w+) bg:([#\w]\w+)\}([^{]+)\{\/color\}/img, '<span style="color:$1; background-color:$2;">$3</span>');
