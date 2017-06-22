ca07c82efe32c3ff043d64caa77b77639b14e0a6
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-06-22 18:07:21 -0300

    Update handleError.js

--- client/lib/handleError.js
+++ client/lib/handleError.js
@@ -3,14 +3,14 @@ this.handleError = function(error, useToastr = true) {
 	if (_.isObject(error.details)) {
 		for (const key in error.details) {
 			if (error.details.hasOwnProperty(key)) {
-				error.details[key] = TAPi18n.__(_.escapeHTML(error.details[key]));
+				error.details[key] = TAPi18n.__(error.details[key]);
 			}
 		}
 	}
 
 	if (useToastr) {
-		return toastr.error(TAPi18n.__(error.error, error.details), error.details && error.details.errorTitle ? TAPi18n.__(error.details.errorTitle) : null);
+		return toastr.error(_.escapeHTML(TAPi18n.__(error.error, error.details)), error.details && error.details.errorTitle ? _.escapeHTML(TAPi18n.__(error.details.errorTitle)) : null);
 	}
 
-	return TAPi18n.__(error.error, error.details);
+	return _.escapeHTML(TAPi18n.__(error.error, error.details));
 };
