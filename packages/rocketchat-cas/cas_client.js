3537d28987d2f1e985952fdb51bb032aec22d7e1
wojtek <w@swtk.info>
2017-07-11 21:19:24 +0200

    Issue #7365: added check for the existence of a parameter in the CAS URL

--- packages/rocketchat-cas/cas_client.js
+++ packages/rocketchat-cas/cas_client.js
@@ -33,7 +33,12 @@ Meteor.loginWithCas = function(options, callback) {
 	}
 
 	const appUrl = Meteor.absoluteUrl().replace(/\/$/, '') + __meteor_runtime_config__.ROOT_URL_PATH_PREFIX;
-	const loginUrl = `${ login_url }?service=${ appUrl }/_cas/${ credentialToken }`;
+	// check if the provided CAS URL already has some parameters
+	if (login_url.split('?').length > 1) {
+		const loginUrl = `${ login_url }&service=${ appUrl }/_cas/${ credentialToken }`;
+	} else {
+		const loginUrl = `${ login_url }?service=${ appUrl }/_cas/${ credentialToken }`;
+	}
 
 	const popup = openCenteredPopup(
 		loginUrl,
