b400c39b559d0329ad8a9ea3767a7ce1fdcc94e6
pkgodara <pkgodara.choudhary@gmail.com>
2017-07-13 21:28:54 +0530

    fix class definition, config, onload

--- packages/rocketchat-lib/server/startup/settingsOnLoadDirectReply.js
+++ packages/rocketchat-lib/server/startup/settingsOnLoadDirectReply.js
@@ -8,31 +8,13 @@ const startEmailIntercepter = _.debounce(Meteor.bindEnvironment(function() {
 				console.log('Disconnecting already running instance...');
 				RocketChat.IMAP.stop(Meteor.bindEnvironment(function() {
 					console.log('Starting new instance......');
-					RocketChat.IMAP = new RocketChat.IMAPIntercepter({
-						user: RocketChat.settings.get('Direct_Reply_Username'),
-						password: RocketChat.settings.get('Direct_Reply_Password'),
-						host: RocketChat.settings.get('Direct_Reply_Host'),
-						port: RocketChat.settings.get('Direct_Reply_Port'),
-						debug: RocketChat.settings.get('Direct_Reply_Debug') ? console.log : false,
-						tls: !RocketChat.settings.get('Direct_Reply_IgnoreTLS'),
-						connTimeout: 30000,
-						keepalive: true
-					});
+					RocketChat.IMAP = new RocketChat.IMAPIntercepter();
 					RocketChat.IMAP.start();
 					return true;
 				}));
 			} else if (!RocketChat.IMAP || !RocketChat.IMAP.isActive()) {
 				console.log('Starting new instance......');
-				RocketChat.IMAP = new RocketChat.IMAPIntercepter({
-					user: RocketChat.settings.get('Direct_Reply_Username'),
-					password: RocketChat.settings.get('Direct_Reply_Password'),
-					host: RocketChat.settings.get('Direct_Reply_Host'),
-					port: RocketChat.settings.get('Direct_Reply_Port'),
-					debug: RocketChat.settings.get('Direct_Reply_Debug') ? console.log : false,
-					tls: !RocketChat.settings.get('Direct_Reply_IgnoreTLS'),
-					connTimeout: 30000,
-					keepalive: true
-				});
+				RocketChat.IMAP = new RocketChat.IMAPIntercepter();
 				RocketChat.IMAP.start();
 				return true;
 			}
@@ -40,40 +22,4 @@ const startEmailIntercepter = _.debounce(Meteor.bindEnvironment(function() {
 	}
 }), 1000);
 
-RocketChat.settings.onload('Direct_Reply_Enable', function() {
-	return startEmailIntercepter();
-});
-
-RocketChat.settings.onload('Direct_Reply_Debug', function() {
-	return startEmailIntercepter();
-});
-
-RocketChat.settings.onload('Direct_Reply_Host', function(key, value) {
-	if (_.isString(value)) {
-		return startEmailIntercepter();
-	}
-});
-
-RocketChat.settings.onload('Direct_Reply_Port', function() {
-	return startEmailIntercepter();
-});
-
-RocketChat.settings.onload('Direct_Reply_Username', function(key, value) {
-	if (_.isString(value)) {
-		return startEmailIntercepter();
-	}
-});
-
-RocketChat.settings.onload('Direct_Reply_Password', function(key, value) {
-	if (_.isString(value)) {
-		return startEmailIntercepter();
-	}
-});
-
-RocketChat.settings.onload('Direct_Reply_Protocol', function() {
-	return startEmailIntercepter();
-});
-
-RocketChat.settings.onload('Direct_Reply_IgnoreTLS', function() {
-	return startEmailIntercepter();
-});
+RocketChat.settings.onload(/^Direct_Reply_.+/, startEmailIntercepter);
