145ec7ca3f598fb5079669bf17cde8566e6868fc
Lindoélio Lázaro <lindoelio@gmail.com>
2017-07-18 07:34:45 -0300

    Internal package for notifications sound volume control UI.

---
+++ packages/rocketchat-slider/rocketchat-slider.js
@@ -0,0 +1,20 @@
+import './rocketchat-slider.scss';
+import './rocketchat-slider.html';
+
+Template.slider.onRendered(function() {
+	const params = this.data;
+
+	const rangeSlider = function() {
+
+		const range = $('#' + params.id);
+		const labelValue = $('#' + params.id + '__value');
+
+		labelValue.html(params.value);
+
+		range.on('input', function() {
+			labelValue.html(this.value);
+		});
+	};
+
+	rangeSlider();
+});
