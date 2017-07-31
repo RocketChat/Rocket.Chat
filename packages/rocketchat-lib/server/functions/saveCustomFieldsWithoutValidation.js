db9cd72e0fe169d41b71e8bdad8348239496d38e
Martin Schoeler <martin.schoeler@rocket.chat>
2017-07-31 15:48:57 -0300

    fix custom fields

--- packages/rocketchat-lib/server/functions/saveCustomFieldsWithoutValidation.js
+++ packages/rocketchat-lib/server/functions/saveCustomFieldsWithoutValidation.js
@@ -1,33 +1,35 @@
 RocketChat.saveCustomFieldsWithoutValidation = function(userId, formData) {
-	if (s.trim(RocketChat.settings.get('Accounts_CustomFields')) !== '') {
-		let customFieldsMeta;
-		try {
-			customFieldsMeta = JSON.parse(RocketChat.settings.get('Accounts_CustomFields'));
-		} catch (e) {
-			throw new Meteor.Error('error-invalid-customfield-json', 'Invalid JSON for Custom Fields');
-		}
-
-		const customFields = formData;
-
-		// for fieldName, field of customFieldsMeta
-		RocketChat.models.Users.setCustomFields(userId, customFields);
-
-		Object.keys(customFields).forEach((fieldName) => {
-			if (!customFieldsMeta[fieldName].modifyRecordField) {
-				return;
-			}
-
-			const modifyRecordField = customFieldsMeta[fieldName].modifyRecordField;
-			const update = {};
-			if (modifyRecordField.array) {
-				update.$addToSet = {};
-				update.$addToSet[modifyRecordField.field] = customFields[fieldName];
-			} else {
-				update.$set = {};
-				update.$set[modifyRecordField.field] = customFields[fieldName];
-			}
-
-			RocketChat.models.Users.update(userId, update);
-		});
+	if (s.trim(RocketChat.settings.get('Accounts_CustomFields')) === '') {
+		return;
 	}
+
+	let customFieldsMeta;
+	try {
+		customFieldsMeta = JSON.parse(RocketChat.settings.get('Accounts_CustomFields'));
+	} catch (e) {
+		throw new Meteor.Error('error-invalid-customfield-json', 'Invalid JSON for Custom Fields');
+	}
+
+	const customFields = Object.keys(customFieldsMeta).filter(fieldName => !customFieldsMeta[fieldName].modifyRecordField).
+			reduce((update, key) => {
+				update[key] = formData[key];
+				return update;
+			}, {});
+
+	RocketChat.models.Users.setCustomFields(userId, customFields);
+	const update = Object.keys(customFieldsMeta).
+			filter(fieldName => customFieldsMeta[fieldName].modifyRecordField).
+			reduce((update, fieldName) => {
+				const modifyRecordField = customFieldsMeta[fieldName].modifyRecordField;
+				if (modifyRecordField.array) {
+					update.$addToSet[modifyRecordField.field] = formData[fieldName];
+				} else {
+					update.$set[modifyRecordField.field] = formData[fieldName];
+				}
+				return update;
+			}, {
+				$addToSet: {},
+				$set: {}
+			});
+	RocketChat.models.Users.update(userId, update);
 };
