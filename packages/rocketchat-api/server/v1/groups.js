549b126ad219ee180d1bd6a5823115e5c8c88359
Bradley Hilton <graywolf336@craftyn.com>
2017-08-17 15:44:41 -0500

    Allow pinning, unpinning, starring, and unstarring via the rest api

--- packages/rocketchat-api/server/v1/groups.js
+++ packages/rocketchat-api/server/v1/groups.js
@@ -1,7 +1,7 @@
 //Returns the private group subscription IF found otherwise it will return the failure of why it didn't. Check the `statusCode` property
 function findPrivateGroupByIdOrName({ params, userId, checkedArchived = true }) {
 	if ((!params.roomId || !params.roomId.trim()) && (!params.roomName || !params.roomName.trim())) {
-		throw new Meteor.Error('error-roomid-param-not-provided', 'The parameter "roomId" or "roomName" is required');
+		throw new Meteor.Error('error-room-param-not-provided', 'The parameter "roomId" or "roomName" is required');
 	}
 
 	let roomSub;
