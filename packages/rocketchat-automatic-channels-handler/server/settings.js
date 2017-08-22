902f5a7114bf76eb4de8f8e4ab6edf9212766003
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-08-22 20:32:08 -0300

    Fix setting group name

--- packages/rocketchat-automatic-channels-handler/server/settings.js
+++ packages/rocketchat-automatic-channels-handler/server/settings.js
@@ -1,4 +1,4 @@
-RocketChat.settings.addGroup('Automatic Channels Settings', function() {
+RocketChat.settings.addGroup('Automatic Channels', function() {
 	this.section('language', function() {
 		this.add('Enable_Language', false, {
 			type: 'boolean'
