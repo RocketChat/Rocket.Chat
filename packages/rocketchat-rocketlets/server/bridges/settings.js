a16d5eaf017a58f7a81b7a70e17339350a97da2a
Bradley Hilton <graywolf336@craftyn.com>
2017-08-22 15:18:41 -0500

    RocketletManager is now loaded.

---
+++ packages/rocketchat-rocketlets/server/bridges/settings.js
@@ -0,0 +1,35 @@
+export class RocketletSettingBridge {
+	constructor(converters) {
+		this.converters = converters;
+	}
+
+	getAll(rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is getting all the settings.`);
+		throw new Error('Method not implemented.');
+	}
+
+	getOneById(id, rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is getting the setting by id ${ id }.`);
+		throw new Error('Method not implemented.');
+	}
+
+	hideGroup(name, rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is hidding the group ${ name }.`);
+		throw new Error('Method not implemented.');
+	}
+
+	hideSetting(id, rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is hidding the setting ${ id }.`);
+		throw new Error('Method not implemented.');
+	}
+
+	isReadableById(id, rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is checking if they can read the setting ${ id }.`);
+		throw new Error('Method not implemented.');
+	}
+
+	updateOne(setting, rocketletId) {
+		console.log(`The Rocketlet ${ rocketletId } is updating the setting ${ setting.id } .`);
+		throw new Error('Method not implemented.');
+	}
+}
--- packages/rocketchat-rocketlets/server/converters/index.js
+++ packages/rocketchat-rocketlets/server/converters/index.js
@@ -1,9 +1,11 @@
 import { RocketletMessagesConverter } from './messages';
 import { RocketletRoomsConverter } from './rooms';
+import { RocketletSettingsConverter } from './settings';
 import { RocketletUsersConverter } from './users';
 
 export {
 	RocketletMessagesConverter,
 	RocketletRoomsConverter,
+	RocketletSettingsConverter,
 	RocketletUsersConverter
 };
