992e1ff236ff34e481dd23e167381daf4f5bf96f
Karl Prieb <karl@prieb.me>
2017-08-28 18:40:39 -0300

    Fix create channel on Firefox :tada:

--- packages/rocketchat-ui/client/views/app/createChannel.js
+++ packages/rocketchat-ui/client/views/app/createChannel.js
@@ -155,6 +155,7 @@ Template.createChannel.events({
 				}
 				return;
 			}
+
 			if (!isPrivate) {
 				RocketChat.callbacks.run('aftercreateCombined', { _id: result.rid, name: result.name });
 			}
