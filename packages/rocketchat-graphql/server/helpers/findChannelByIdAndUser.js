146fe2654aa5579a15235eebee7747f2edcfa24e
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-08-29 11:40:38 +0200

    Use findOneByRoomIdAndUserId instead of custom one

--- packages/rocketchat-graphql/server/helpers/findChannelByIdAndUser.js
+++
@@ -1,8 +0,0 @@
-export function findChannelByIdAndUser({ params, options = {} }) {
-	const sub = RocketChat.models.Subscriptions.findOne({
-		rid: params.roomId,
-		'u._id': params.userId
-	}, options);
-
-	return sub;
-}
