fa703fc65fde7ca5569820feb7df9e33840ca796
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-08-29 13:00:43 +0200

    Update deps in rocketchat:graphql

--- packages/rocketchat-graphql/package.js
+++ packages/rocketchat-graphql/package.js
@@ -20,14 +20,13 @@ Package.onUse(function(api) {
 
 Npm.depends({
 	'@accounts/graphql-api': '0.1.1',
-	'apollo-client': '1.6.0',
-	'cors': '2.8.3',
+	'apollo-server-express': '1.1.2',
+	'cors': '2.8.4',
 	'body-parser': '1.17.2',
-	'express': '4.15.3',
+	'express': '4.15.4',
 	'graphql': '0.10.3',
-	'graphql-server-express': '0.9.0',
 	'graphql-subscriptions': '0.4.4',
-	'graphql-tools': '1.1.0',
-	'merge-graphql-schemas': '1.1.0',
-	'subscriptions-transport-ws': '0.8.1'
+	'graphql-tools': '1.2.2',
+	'merge-graphql-schemas': '1.1.2',
+	'subscriptions-transport-ws': '0.8.2'
 });
