494ffd109bc61e1262a4230fff0a78663a29e850
Bradley Hilton <graywolf336@craftyn.com>
2017-08-31 23:59:45 -0500

    Very basic start of the web interface for the rocketlets, not much done

--- packages/rocketchat-rocketlets/client/orchestrator.js
+++ packages/rocketchat-rocketlets/client/orchestrator.js
@@ -3,10 +3,29 @@ import { RocketletWebsocketReceiver } from './communication';
 class RocketletClientOrchestrator {
 	constructor() {
 		this.ws = new RocketletWebsocketReceiver(this);
+
+		this._addAdminMenuOption();
 	}
 
 	getWsListener() {
 		return this.ws;
+	}
+
+	_addAdminMenuOption() {
+		FlowRouter.route('/admin/rocketlets', {
+			name: 'rocketlets',
+			action() {
+				BlazeLayout.render('main', { center: 'rocketlets' });
+			}
+		});
+
+		RocketChat.AdminBox.addOption({
+			href: 'rocketlets',
+			i18nLabel: 'rocketlets',
+			permissionGranted() {
+				return RocketChat.authz.hasAtLeastOnePermission(['manage-rocketlets']);
+			}
+		});
 	}
 }
 
