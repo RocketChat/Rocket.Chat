6257a2420396e902f2d75768be0c37b55ce7194b
Diego Sampaio <chinello@gmail.com>
2017-09-05 14:45:37 -0300

    Show tokenpass options only for TCA rooms

--- packages/rocketchat-tokenly/client/startup.js
+++ packages/rocketchat-tokenly/client/startup.js
@@ -3,8 +3,14 @@ Meteor.startup(function() {
 		group: ['room'],
 		id: 'tokenly',
 		template: 'channelSettings__tokenly',
-		validation() {
-			return true;
+		validation(data) {
+			if (data && data.rid) {
+				const room = RocketChat.models.Rooms.findOne(data.rid, { fields: { tokenpass: 1 } });
+
+				return room && room.tokenpass;
+			}
+
+			return false;
 		}
 	});
 });
