23c607e8aefae3c559b6e8d679d22ea2d119f6a0
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-09-05 14:10:14 -0300

    Merge pull request #7988 from RocketChat/action=messages
    [FIX] Vertical menu on flex-tab
    # Conflicts:
    #	packages/rocketchat-ui-master/public/icons.svg

--- packages/rocketchat-mentions-flextab/client/actionButton.js
+++ packages/rocketchat-mentions-flextab/client/actionButton.js
@@ -1,12 +1,11 @@
 Meteor.startup(function() {
 	RocketChat.MessageAction.addButton({
 		id: 'jump-to-message',
-		icon: 'right-hand',
+		icon: 'jump',
 		label: 'Jump_to_message',
 		context: ['mentions'],
 		action() {
 			const message = this._arguments[1];
-			RocketChat.MessageAction.hideDropDown();
 			if (window.matchMedia('(max-width: 500px)').matches) {
 				Template.instance().tabBar.close();
 			}
