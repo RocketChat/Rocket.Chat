7251839931830e1c5a8fb03162de5c6bfcf3f834
Vitor Capretz <capretzvitor@gmail.com>
2017-09-14 10:38:32 -0300

    Move mentions files to client/server

--- packages/rocketchat-mentions/package.js
+++ packages/rocketchat-mentions/package.js
@@ -11,7 +11,6 @@ Package.onUse(function(api) {
 		'rocketchat:lib'
 	]);
 
-	api.addFiles('server.js', 'server');
-	api.addFiles('client.js', 'client');
-	// api.('mentions.js', 'client');
+	api.addFiles('server/server.js', 'server');
+	api.addFiles('client/client.js', 'client');
 });
