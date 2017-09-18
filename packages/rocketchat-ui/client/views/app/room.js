1868c13feeb1b2244d21336949a4a68a6decd18f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-09-18 14:06:35 -0300

    perfecr scrollbar
    applied sidenav and room

--- packages/rocketchat-ui/client/views/app/room.js
+++ packages/rocketchat-ui/client/views/app/room.js
@@ -800,6 +800,7 @@ Template.room.onDestroyed(function() {
 });
 
 Template.room.onRendered(function() {
+	$(this.find('.messages-box .wrapper')).perfectScrollbar();
 	const rid = Session.get('openedRoom');
 	if (!window.chatMessages[rid]) {
 		window.chatMessages[rid] = new ChatMessages;
