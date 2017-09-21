a056c438be13e475ed0b224bc13f759568674dad
Vitor Capretz <capretzvitor@gmail.com>
2017-09-21 11:59:46 -0300

    slashcommand-topic: fix files spliting
    as the files now are separeted we don't need to check
    if it's client or server, so we can remove some validations

--- packages/rocketchat-slashcommands-topic/client/topic.js
+++ packages/rocketchat-slashcommands-topic/client/topic.js
@@ -5,19 +5,13 @@
 
 function Topic(command, params, item) {
 	if (command === 'topic') {
-		if (Meteor.isClient && RocketChat.authz.hasAtLeastOnePermission('edit-room', item.rid) || (Meteor.isServer && RocketChat.authz.hasPermission(Meteor.userId(), 'edit-room', item.rid))) {
+		if (RocketChat.authz.hasAtLeastOnePermission('edit-room', item.rid)) {
 			Meteor.call('saveRoomSettings', item.rid, 'roomTopic', params, (err) => {
 				if (err) {
-					if (Meteor.isClient) {
-						return handleError(err);
-					} else {
-						throw err;
-					}
+					return handleError(err);
 				}
 
-				if (Meteor.isClient) {
-					RocketChat.callbacks.run('roomTopicChanged', ChatRoom.findOne(item.rid));
-				}
+				RocketChat.callbacks.run('roomTopicChanged', ChatRoom.findOne(item.rid));
 			});
 		}
 	}
