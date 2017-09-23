8ed7f638497c53bba85022fc0ec04ec900cffc6a
Gabriel Engel <gabriel.engel@gmail.com>
2017-09-22 22:14:33 -0300

    Merge pull request #8257 from RocketChat/hover-color
    [FIX] sidenav colors, hide and leave, create channel on safari

--- packages/rocketchat-ui-sidenav/client/sidebarItem.js
+++ packages/rocketchat-ui-sidenav/client/sidebarItem.js
@@ -1,47 +1,39 @@
 /* globals menu popover */
-
-Template.sidebarItem.helpers({
-	canLeave() {
-		const roomData = Session.get(`roomData${ this.rid }`);
-
-		if (!roomData) { return false; }
-
-		if (((roomData.cl != null) && !roomData.cl) || (roomData.t === 'd')) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-});
-
 Template.sidebarItem.events({
 	'click [data-id], click .sidebar-item__link'() {
 		return menu.close();
 	},
 	'click .sidebar-item__menu'(e) {
-		e.preventDefault();
+		const canLeave = () => {
+			const roomData = Session.get(`roomData${ this.rid }`);
 
+			if (!roomData) { return false; }
+
+			return !(((roomData.cl != null) && !roomData.cl) || (['d', 'l'].includes(roomData.t)));
+		};
+		e.preventDefault();
+		const items = [{
+			icon: 'eye-off',
+			name: t('Hide_room'),
+			type: 'sidebar-item',
+			id: 'hide'
+		}];
+		if (canLeave()) {
+			items.push({
+				icon: 'sign-out',
+				name: t('Leave_room'),
+				type: 'sidebar-item',
+				id: 'leave',
+				modifier: 'error'
+			});
+		}
 		const config = {
 			popoverClass: 'sidebar-item',
 			columns: [
 				{
 					groups: [
 						{
-							items: [
-								{
-									icon: 'eye-off',
-									name: t('Hide_room'),
-									type: 'sidebar-item',
-									id: 'hide'
-								},
-								{
-									icon: 'sign-out',
-									name: t('Leave_room'),
-									type: 'sidebar-item',
-									id: 'leave',
-									modifier: 'error'
-								}
-							]
+							items
 						}
 					]
 				}
