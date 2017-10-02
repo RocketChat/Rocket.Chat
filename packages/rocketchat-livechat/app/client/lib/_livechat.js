d7b5bab0fa10c543e08cebbd4f8c3a60d61b0589
icosamuel <begs1307@gmail.com>
2017-10-02 15:27:42 -0400

    explicitly added the showAgentEmail value to LiveChat obj

--- packages/rocketchat-livechat/app/client/lib/_livechat.js
+++ packages/rocketchat-livechat/app/client/lib/_livechat.js
@@ -26,6 +26,7 @@ this.Livechat = new (class Livechat {
 		this._widgetOpened = new ReactiveVar(false);
 		this._ready = new ReactiveVar(false);
 		this._agent = new ReactiveVar();
+		this._showAgentEmail = new ReactiveVar(true);
 
 		this.stream = new Meteor.Streamer('livechat-room');
 
@@ -107,6 +108,9 @@ this.Livechat = new (class Livechat {
 	get agent() {
 		return this._agent.get();
 	}
+	get showAgentEmail() {
+		return this._showAgentEmail.get();
+	}
 
 	set online(value) {
 		this._online.set(value);
@@ -174,6 +178,9 @@ this.Livechat = new (class Livechat {
 	}
 	set agent(agentData) {
 		this._agent.set(agentData);
+	}
+	set showAgentEmail(value) {
+		this._showAgentEmail.set(value);
 	}
 
 	ready() {
