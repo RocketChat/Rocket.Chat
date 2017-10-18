5581557a3706b65a966f5eda15437347c5b30ce9
Joe Sitton <sittonjoe@gmail.com>
2017-10-18 12:04:26 -0400

    Truncate filename in upload bar if length > 50 chars

--- packages/rocketchat-file-upload/lib/FileUploadBase.js
+++ packages/rocketchat-file-upload/lib/FileUploadBase.js
@@ -27,7 +27,7 @@ FileUploadBase = class FileUploadBase {
 	}
 
 	getFileName() {
-		return this.meta.name;
+		return (this.meta.name.length > 50) ? this.meta.name.substring(0, 50) : this.meta.name;
 	}
 
 	start(callback) {
