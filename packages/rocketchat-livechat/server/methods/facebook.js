9df191e4e663c73d789811da1c4be68c48caee77
Diego Sampaio <chinello@gmail.com>
2017-10-18 16:49:11 -0200

    Add error handling

--- packages/rocketchat-livechat/server/methods/facebook.js
+++ packages/rocketchat-livechat/server/methods/facebook.js
@@ -4,66 +4,70 @@ Meteor.methods({
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'livechat:addAgent' });
 		}
 
-		switch (options.action) {
-			case 'initialState': {
-				return {
-					enabled: RocketChat.settings.get('Livechat_Facebook_Enabled')
-				};
+		try {
+			switch (options.action) {
+				case 'initialState': {
+					return {
+						enabled: RocketChat.settings.get('Livechat_Facebook_Enabled')
+					};
+				}
+
+				case 'enable': {
+					RocketChat.settings.set('Livechat_Facebook_Enabled', true);
+
+					const result = HTTP.call('POST', 'https://omni.rocket.chat/facebook/enable', {
+						headers: {
+							'authorization': `Bearer ${ RocketChat.settings.get('Livechat_Facebook_API_Key') }`,
+							'content-type': 'application/json'
+						},
+						data: {
+							url: RocketChat.settings.get('Site_Url')
+						}
+					});
+					return result.data;
+				}
+
+				case 'disable': {
+					RocketChat.settings.set('Livechat_Facebook_Enabled', false);
+
+					const result = HTTP.call('DELETE', 'https://omni.rocket.chat/facebook/enable', {
+						headers: {
+							'authorization': `Bearer ${ RocketChat.settings.get('Livechat_Facebook_API_Key') }`,
+							'content-type': 'application/json'
+						}
+					});
+					return result.data;
+				}
+
+				case 'list-pages': {
+					const result = HTTP.call('GET', 'https://omni.rocket.chat/facebook/pages', {
+						headers: {
+							'authorization': `Bearer ${ RocketChat.settings.get('Livechat_Facebook_API_Key') }`
+						}
+					});
+					return result.data;
+				}
+
+				case 'subscribe': {
+					const result = HTTP.call('POST', `https://omni.rocket.chat/facebook/page/${ options.page }/subscribe`, {
+						headers: {
+							'authorization': `Bearer ${ RocketChat.settings.get('Livechat_Facebook_API_Key') }`
+						}
+					});
+					return result.data;
+				}
+
+				case 'unsubscribe': {
+					const result = HTTP.call('DELETE', `https://omni.rocket.chat/facebook/page/${ options.page }/subscribe`, {
+						headers: {
+							'authorization': `Bearer ${ RocketChat.settings.get('Livechat_Facebook_API_Key') }`
+						}
+					});
+					return result.data;
+				}
 			}
-
-			case 'enable': {
-				RocketChat.settings.set('Livechat_Facebook_Enabled', true);
-
-				const result = HTTP.call('POST', 'http://localhost:3000/facebook/enable', {
-					headers: {
-						'x-rocketchat-instance': RocketChat.settings.get('Livechat_Facebook_API_Key'),
-						'content-type': 'application/json'
-					},
-					data: {
-						url: RocketChat.settings.get('Site_Url')
-					}
-				});
-				return result.data;
-			}
-
-			case 'disable': {
-				RocketChat.settings.set('Livechat_Facebook_Enabled', false);
-
-				const result = HTTP.call('DELETE', 'http://localhost:3000/facebook/enable', {
-					headers: {
-						'x-rocketchat-instance': RocketChat.settings.get('Livechat_Facebook_API_Key'),
-						'content-type': 'application/json'
-					}
-				});
-				return result.data;
-			}
-
-			case 'list-pages': {
-				const result = HTTP.call('GET', 'http://localhost:3000/facebook/pages', {
-					headers: {
-						'x-rocketchat-instance': RocketChat.settings.get('Livechat_Facebook_API_Key')
-					}
-				});
-				return result.data;
-			}
-
-			case 'subscribe': {
-				const result = HTTP.call('POST', `http://localhost:3000/facebook/page/${ options.page }/subscribe`, {
-					headers: {
-						'x-rocketchat-instance': RocketChat.settings.get('Livechat_Facebook_API_Key')
-					}
-				});
-				return result.data;
-			}
-
-			case 'unsubscribe': {
-				const result = HTTP.call('DELETE', `http://localhost:3000/facebook/page/${ options.page }/subscribe`, {
-					headers: {
-						'x-rocketchat-instance': RocketChat.settings.get('Livechat_Facebook_API_Key')
-					}
-				});
-				return result.data;
-			}
+		} catch (e) {
+			throw new Meteor.Error('integration-error', e.response.data.error.message);
 		}
 	}
 });
