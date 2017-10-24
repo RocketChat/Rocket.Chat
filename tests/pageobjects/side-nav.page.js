b40fcb47af1ed8a600e46e9c5ace4e22acd90a9d
Oliver Jägle <github@beimir.net>
2017-10-24 19:52:17 +0200

    Fix UI-unit-tests

--- tests/pageobjects/side-nav.page.js
+++ tests/pageobjects/side-nav.page.js
@@ -2,44 +2,133 @@ import Page from './Page';
 
 class SideNav extends Page {
 	// New channel
-	get channelType() { return browser.element('.create-channel__content .rc-switch__button'); }
-	get channelReadOnly() { return browser.elements('.create-channel__switches .rc-switch__button').value[1]; }
-	get channelName() { return browser.element('.create-channel__content input[name="name"]'); }
-	get saveChannelBtn() { return browser.element('.create-channel__content [data-button="create"]'); }
+	get channelType() {
+		return browser.element('.create-channel__content .rc-switch__button');
+	}
+
+	get channelReadOnly() {
+		return browser.elements('.create-channel__switches .rc-switch__button').value[1];
+	}
+
+	get channelName() {
+		return browser.element('.create-channel__content input[name="name"]');
+	}
+
+	get saveChannelBtn() {
+		return browser.element('.create-channel__content [data-button="create"]');
+	}
 
 	// Account box
-	get accountBoxUserName() { return browser.element('.sidebar__account-username'); }
-	get accountBoxUserAvatar() { return browser.element('.sidebar__account .avatar-image'); }
-	get accountMenu() { return browser.element('.sidebar__account'); }
-	get popOverContent() { return browser.element('.rc-popover__content'); }
-	get statusOnline() { return browser.element('[data-id="online"]'); }
-	get statusAway() { return browser.element('[data-id="away"]'); }
-	get statusBusy() { return browser.element('[data-id="busy"]'); }
-	get statusOffline() { return browser.element('[data-id="offline"]'); }
-	get account() { return browser.element('[data-id="account"][data-type="open"]'); }
-	get admin() { return browser.element('[data-id="administration"][data-type="open"]'); }
-	get logout() { return browser.element('[data-id="logout"][data-type="open"]'); }
-	get sideNavBar() { return browser.element('.sidebar'); }
+	get accountBoxUserName() {
+		return browser.element('.sidebar__account-username');
+	}
+
+	get accountBoxUserAvatar() {
+		return browser.element('.sidebar__account .avatar-image');
+	}
+
+	get accountMenu() {
+		return browser.element('.sidebar__account');
+	}
+
+	get popOverContent() {
+		return browser.element('.rc-popover__content');
+	}
+
+	get statusOnline() {
+		return browser.element('[data-id="online"]');
+	}
+
+	get statusAway() {
+		return browser.element('[data-id="away"]');
+	}
+
+	get statusBusy() {
+		return browser.element('[data-id="busy"]');
+	}
+
+	get statusOffline() {
+		return browser.element('[data-id="offline"]');
+	}
+
+	get account() {
+		return browser.element('[data-id="account"][data-type="open"]');
+	}
+
+	get admin() {
+		return browser.element('[data-id="administration"][data-type="open"]');
+	}
+
+	get logout() {
+		return browser.element('[data-id="logout"][data-type="open"]');
+	}
+
+	get sideNavBar() {
+		return browser.element('.sidebar');
+	}
 
 	// Toolbar
-	get spotlightSearch() { return browser.element('.toolbar__search input'); }
-	get spotlightSearchPopUp() { return browser.element('.rooms-list__toolbar-search'); }
-	get newChannelBtn() { return browser.element('.toolbar .toolbar__search-create-channel'); }
-	get newChannelIcon() { return browser.element('.toolbar__icon.toolbar__search-create-channel'); }
+	get spotlightSearch() {
+		return browser.element('.toolbar__search input');
+	}
+
+	get spotlightSearchPopUp() {
+		return browser.element('.rooms-list__toolbar-search');
+	}
+
+	get newChannelBtn() {
+		return browser.element('.toolbar .toolbar__search-create-channel');
+	}
+
+	get newChannelIcon() {
+		return browser.element('.toolbar__icon.toolbar__search-create-channel');
+	}
 
 	// Rooms List
-	get general() { return this.getChannelFromList('general'); }
-	get channelLeave() { return browser.element('.leave-room'); }
-	get channelHoverIcon() { return browser.element('.rooms-list > .wrapper > ul [title="general"] .icon-eye-off'); }
-	get moreChannels() { return browser.element('.rooms-list .more-channels'); }
+	get general() {
+		return this.getChannelFromList('general');
+	}
+
+	get channelLeave() {
+		return browser.element('.leave-room');
+	}
+
+	get channelHoverIcon() {
+		return browser.element('.rooms-list > .wrapper > ul [title="general"] .icon-eye-off');
+	}
+
+	get moreChannels() {
+		return browser.element('.rooms-list .more-channels');
+	}
 
 	// Account
-	get preferences() { return browser.element('[href="/account/preferences"]'); }
-	get profile() { return browser.element('[href="/account/profile"]'); }
-	get avatar() { return browser.element('[href="/changeavatar"]'); }
-	get preferencesClose() { return browser.element('.sidebar-flex__close-button[data-action="close"]'); }
+	get preferences() {
+		return browser.element('[href="/account/preferences"]');
+	}
 
-	get burgerBtn() { return browser.element('.burger'); }
+	get profile() {
+		return browser.element('[href="/account/profile"]');
+	}
+
+	get avatar() {
+		return browser.element('[href="/changeavatar"]');
+	}
+
+	get preferencesClose() {
+		return browser.element('.sidebar-flex__close-button[data-action="close"]');
+	}
+
+	get burgerBtn() {
+		return browser.element('.burger');
+	}
+
+	get tabs() {
+		return browser.element('nav.rc-tabs');
+	}
+
+	get createChannelTab() {
+		return browser.element('nav.rc-tabs .rc-tabs__tab-link.createChannel');
+	}
 
 	// Opens a channel via rooms list
 	openChannel(channelName) {
@@ -95,6 +184,14 @@ class SideNav extends Page {
 	createChannel(channelName, isPrivate, /*isReadOnly*/) {
 		this.newChannelBtn.waitForVisible(10000);
 		this.newChannelBtn.click();
+		/*
+		This is to support custom room types: If custom room types are present in the build (most probably on
+		another repo), the channel creation is moved into a tabstrip.
+		The tab needs to be activated before the usual operations are possible
+		 */
+		if (this.tabs) {
+			this.createChannelTab.click();
+		}
 		this.channelName.waitForVisible(10000);
 
 		//workaround for incomplete setvalue bug
