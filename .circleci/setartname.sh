e9193d1eac167bed6ac967ee944dcb7a09f62eee
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-10-28 15:11:48 -0200

    Deploy test

--- .circleci/setartname.sh
+++ .circleci/setartname.sh
@@ -1,6 +1,6 @@
 if [[ $CIRCLE_TAG ]]
  then
-  export ARTIFACT_NAME="$(meteor npm run version --silent)"
+  export ARTIFACT_NAME="$(npm run version --silent)"
 else
-  export ARTIFACT_NAME="$(meteor npm run version --silent).$CIRCLE_BUILD_NUM"
+  export ARTIFACT_NAME="$(npm run version --silent).$CIRCLE_BUILD_NUM"
 fi
