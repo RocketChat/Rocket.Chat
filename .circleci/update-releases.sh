972656850634d919de44a60ea2433eb7583ce9d5
Aaron Ogle <aaron@geekgonecrazy.com>
2017-10-30 11:01:38 -0500

    add s3 redirect on upload of new release

--- .circleci/update-releases.sh
+++ .circleci/update-releases.sh
@@ -3,6 +3,12 @@ set -x
 set -euvo pipefail
 IFS=$'\n\t'
 
-CURL_URL="https://rocket.chat/releases/update"
+if [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+ ]]; then
+    CHANNEL=candidate
+elif [[ $CIRCLE_TAG ]]; then
+    CHANNEL=stable
+else
+    CHANNEL=develop
+fi
 
-curl -X POST "$CURL_URL"
+aws s3api put-object --acl public-read --website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" --bucket download.rocket.chat --key "$CHANNEL"
