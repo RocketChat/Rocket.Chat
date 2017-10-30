d5a25d5f8212aa5bbec2ed0a5503b10c2122ef0e
Aaron Ogle <aaron@geekgonecrazy.com>
2017-10-30 11:21:19 -0500

    create redirect for RC_VERSION-develop

--- .circleci/update-releases.sh
+++ .circleci/update-releases.sh
@@ -9,6 +9,7 @@ elif [[ $CIRCLE_TAG ]]; then
     CHANNEL=stable
 else
     CHANNEL=develop
+    aws s3api put-object --acl public-read --website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" --bucket download.rocket.chat --key "build/rocket.chat-$RC_VERSION.tgz"
 fi
 
 aws s3api put-object --acl public-read --website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" --bucket download.rocket.chat --key "$CHANNEL"
