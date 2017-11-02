4b65199f7d6dc46e1a615a5c3538a245acd04e6d
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-11-02 15:27:04 -0200

    Fix update-release.sh

--- .circleci/update-releases.sh
+++ .circleci/update-releases.sh
@@ -2,12 +2,19 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+ ]]; then
-    CHANNEL=candidate
-elif [[ $CIRCLE_TAG ]]; then
-    CHANNEL=stable
-else
-    CHANNEL=develop
+CHANNEL=develop
+
+if [[ "${CIRCLE_TAG+SET}" = "SET" ]]; then
+    echo 'if'
+    if [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+ ]]; then
+        CHANNEL=candidate
+    elif [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        CHANNEL=stable
+    fi
+fi
+
+if [[ $CHANNEL = "develop" ]]; then
+    RC_VERSION=0.59.0-develop
     aws s3api put-object --acl public-read --website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" --bucket download.rocket.chat --key "build/rocket.chat-$RC_VERSION.tgz"
 fi
 
