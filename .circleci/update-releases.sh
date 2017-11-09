c27c00e77481177a18b7f2c16dea5d678648fbc8
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2017-11-09 04:51:34 -0600

    Update path for S3 redirect in CircleCI (#8819)
    * Update path for s3 redirect
    
    * Add .asc to bump ci
    
    * Add `--force` to npm cache clear.  Required now with new version of npm / node.js

--- .circleci/update-releases.sh
+++ .circleci/update-releases.sh
@@ -5,13 +5,19 @@ IFS=$'\n\t'
 if [[ $RC_RELEASE = "develop" ]]; then
     aws s3api put-object \
     --acl public-read \
-    --website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" \
+    --website-redirect-location "https://download.rocket.chat/build/rocket.chat-$ARTIFACT_NAME.tgz" \
     --bucket download.rocket.chat \
     --key "build/rocket.chat-$RC_VERSION.tgz"
 fi
 
 aws s3api put-object \
 --acl public-read \
---website-redirect-location "https://download.rocket.chat/build/$ARTIFACT_NAME" \
+--website-redirect-location "https://download.rocket.chat/build/rocket.chat-$ARTIFACT_NAME.tgz" \
 --bucket download.rocket.chat \
 --key "$RC_RELEASE"
+
+aws s3api put-object \
+--acl public-read \
+--website-redirect-location "https://download.rocket.chat/build/rocket.chat-$ARTIFACT_NAME.tgz.asc" \
+--bucket download.rocket.chat \
+--key "$RC_RELEASE.asc"
