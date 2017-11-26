dcd51bad7d53c66927cda4b37c3344a78cf93f01
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2017-11-26 13:39:40 +0100

    added smarti script

---
+++ .scripts/smarti.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker pull thomasroehl/smarti:firsttry
+docker run -d -p 8080:8080 thomasroehl/smarti
+
+sleep 2m
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"defaultClient": true, "description": "", "name": "testclient"}' 'http://localhost:8080/client' > out.json
+x=$(grep -Eo '"id":.*?[^\\]",' out.json)
+x=${x:6:25}
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: text/plain' -d '{"queryBuilder":[{"_class":"io.redlink.smarti.model.config.ComponentConfiguration","name":"conversationmlt","displayName":"conversationmlt","type":"conversationmlt","enabled":true,"unbound":false,"pageSize":3,"filter":["support_area"]},{"_class":"io.redlink.smarti.model.config.ComponentConfiguration","name":"conversationsearch","displayName":"conversationsearch","type":"conversationsearch","enabled":true,"unbound":false,"pageSize":3,"filter":["support_area"]}]}' 'http://localhost:8080/client/${x}/config'
