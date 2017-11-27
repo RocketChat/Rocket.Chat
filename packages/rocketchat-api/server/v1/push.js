ad85d18757b4e542a9c4c1a54b8372548132790a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2017-11-27 15:02:55 -0200

    Add API DELETE push.token/:token

---
+++ packages/rocketchat-api/server/v1/push.js
@@ -0,0 +1,22 @@
+/* globals Push */
+
+RocketChat.API.v1.addRoute('push.token/:token', { authRequired: true }, {
+	delete() {
+		const affectedRecords = Push.appCollection.remove({
+			$or: [{
+				apn: this.urlParams._id
+			}, {
+				gcm: this.urlParams._id
+			}],
+			userId: this.userId
+		});
+
+		if (affectedRecords === 0) {
+			return {
+				statusCode: 404,
+			};
+		}
+
+		return RocketChat.API.v1.success();
+	}
+});
