9555c47338565541a555452a4995705648868206
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2017-12-03 23:28:59 +0100

    docker fix for nlp

--- .scripts/smarti.sh
+++ .scripts/smarti.sh
@@ -4,17 +4,7 @@ docker pull thomasroehl/smarti:redlink
 
 #docker run -d --net=host thomasroehl/smarti:firsttry
 
-docker build -t smarti-with-stanfordnlp -<EOF
-FROM redlinkgmbh/smarti
-
-USER root
-ADD [\
-    "https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.6.0/stanford-corenlp-3.6.0.jar", \
-    "https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.6.0/stanford-corenlp-3.6.0-models-german.jar", \
-    "/opt/ext/"]
-RUN chmod -R a+r /opt/ext/
-USER smarti
-EOF
+docker build -t smarti-with-stanfordnlp - < dockerfile
 
 docker run -d --name smarti --net=host smarti-with-stanfordnlp  --security.password=admin
 
