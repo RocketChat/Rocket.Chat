efbf0c854a8c3e041d9233617ba922fde29ede4b
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2017-12-04 09:49:09 +0100

    fix docker

--- .scripts/smarti.sh
+++ .scripts/smarti.sh
@@ -4,7 +4,9 @@ docker pull thomasroehl/smarti:redlink
 
 #docker run -d --net=host thomasroehl/smarti:firsttry
 
-docker build -t smarti-with-stanfordnlp:latest - < .scripts/dockerfile
+pwd
+
+docker build -t smarti-with-stanfordnlp - < .scripts/dockerfile
 
 docker run -d --name smarti --net=host smarti-with-stanfordnlp  --security.password=admin
 
