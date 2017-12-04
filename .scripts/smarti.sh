be311b33128b4352e293f3a8a590b48b3cf3ba8f
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2017-12-04 10:51:51 +0100

    test sh environment

--- .scripts/smarti.sh
+++ .scripts/smarti.sh
@@ -4,8 +4,9 @@ docker pull thomasroehl/smarti:redlink
 
 #docker run -d --net=host thomasroehl/smarti:firsttry
 
-pwd
+ls
 
+ls /tmp/build/
 docker build -t smarti-with-stanfordnlp - < .scripts/dockerfile
 
 docker run -d --name smarti --net=host smarti-with-stanfordnlp  --security.password=admin
