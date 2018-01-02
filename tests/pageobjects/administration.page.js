54f4ee9ecd4e1e145e1f28632679af1ef295e1c7
RationalCoding <thomasmullendesign@gmail.com>
2018-01-02 15:38:28 -0500

    sound on window focus

--- tests/pageobjects/administration.page.js
+++ tests/pageobjects/administration.page.js
@@ -211,6 +211,9 @@ class Administration extends Page {
 	get accountsNewMessageNotification() { return browser.element('[name="Accounts_Default_User_Preferences_newMessageNotification"]'); }
 	get accountsNewMessageNotificationReset() { return browser.element('.reset-setting[data-setting="Accounts_Default_User_Preferences_newMessageNotification"]'); }
 
+	get accountsFocusWindowNotification() { return browser.element('[name="Accounts_Default_User_Preferences_focusWindowNotification"]'); }
+	get accountsFocusWindowNotificationReset() { return browser.element('.reset-setting[data-setting="Accounts_Default_User_Preferences_focusWindowNotification"]'); }
+
 	get accountsNotificationsSoundVolume() { return browser.element('[name="Accounts_Default_User_Preferences_notificationsSoundVolume"]'); }
 	get accountsNotificationsSoundVolumeReset() { return browser.element('.reset-setting[data-setting="Accounts_Default_User_Preferences_notificationsSoundVolume"]'); }
 
