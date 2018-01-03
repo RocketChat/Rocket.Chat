450016cd299217ea0d95a96174c074a699393cfd
Karl Prieb <karl@prieb.me>
2018-01-03 14:53:43 -0200

    check if firefox is < 55

--- packages/rocketchat-ui/client/components/icon.js
+++ packages/rocketchat-ui/client/components/icon.js
@@ -1,5 +1,5 @@
 /* globals isFirefox */
 
-const baseUrl = isFirefox ? () => { FlowRouter.watchPathChange(); return `${ window.location.origin }${ FlowRouter.current().path }`; } : () => '';
+const baseUrl = isFirefox && isFirefox[1] < 55 ? () => `${ window.location.origin }${ FlowRouter.current().path }` : () => '';
 
 Template.icon.helpers({ baseUrl });
