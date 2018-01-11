dbeca9891e9d26b7abc54eb283466a92bd9b6b04
Bradley Hilton <graywolf336@craftyn.com>
2018-01-11 16:24:08 -0600

    Provide the bridge/connector for the storing of the log entries

---
+++ packages/rocketchat-apps/server/storage/logs-storage.js
@@ -0,0 +1,40 @@
+import { AppConsole } from '@rocket.chat/apps-engine/server/logging';
+import { AppLogStorage } from '@rocket.chat/apps-engine/server/storage';
+
+export class AppRealLogsStorage extends AppLogStorage {
+	constructor(model) {
+		super('mongodb');
+		this.db = model;
+	}
+
+	storeEntries(appId, logger) {
+		console.log(appId);
+		return new Promise((resolve, reject) => {
+			const item = AppConsole.toStorageEntry(appId, logger);
+
+			try {
+				console.log(item);
+				const id = this.db.insert(item);
+				console.log(id);
+
+				resolve(this.db.findOneById(id));
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	getEntriesFor(appId) {
+		return new Promise((resolve, reject) => {
+			let docs;
+
+			try {
+				docs = this.db.find({ appId }).fetch();
+			} catch (e) {
+				return reject(e);
+			}
+
+			resolve(docs);
+		});
+	}
+}
