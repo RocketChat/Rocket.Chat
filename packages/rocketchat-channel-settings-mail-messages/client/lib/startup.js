13fd33bd0e6977b1c8fdb835a9e6c7bf005dcb24
Karl Prieb <karl@prieb.me>
2018-01-22 16:27:05 -0200

    remove mail messages from room info

--- packages/rocketchat-channel-settings-mail-messages/client/lib/startup.js
+++ packages/rocketchat-channel-settings-mail-messages/client/lib/startup.js
@@ -1,12 +1,15 @@
 import resetSelection from '../resetSelection';
 Meteor.startup(() => {
-	RocketChat.ChannelSettings.addOption({
-		group: ['room'],
+	RocketChat.TabBar.addButton({
+		groups: ['channel', 'group', 'direct'],
 		id: 'mail-messages',
-		template: 'channelSettingsMailMessages',
-		validation() {
-			return RocketChat.authz.hasAllPermission('mail-messages');
-		}
+		anonymous: true,
+		i18nTitle: 'Mail_Messages',
+		icon: 'mail',
+		template: 'mailMessagesInstructions',
+		order: 10,
+		condition: () => RocketChat.authz.hasAllPermission('mail-messages')
 	});
-	RocketChat.callbacks.add('roomExit', () => resetSelection(false), RocketChat.callbacks.priority.MEDIUM, 'room-exit-mail-messages');
+
+	// RocketChat.callbacks.add('roomExit', () => resetSelection(false), RocketChat.callbacks.priority.MEDIUM, 'room-exit-mail-messages');
 });
