0edebc7c9424682d1d21a21af85d731253afa537
Tim Kinnane <tim@nestedcode.com>
2018-01-30 09:37:25 +1100

    fix(blockstack): Remove auth path overlap

--- packages/rocketchat-blockstack/server/main.js
+++ packages/rocketchat-blockstack/server/main.js
@@ -6,4 +6,4 @@ Meteor.isDevelopment = (process.env.ROOT_URL.indexOf('localhost') != -1)
 // Define path configs for generated files and routes
 Accounts.blockstack.manifestPath = '_blockstack/manifest'
 Accounts.blockstack.redirectPath = '_blockstack/validate'
-Accounts.blockstack.authHost = Meteor.isDevelopment ? 'http://localhost:8888/' : 'https://blockstack.org/auth'
+Accounts.blockstack.authHost = Meteor.isDevelopment ? 'http://localhost:8888/' : 'https://blockstack.org/'
