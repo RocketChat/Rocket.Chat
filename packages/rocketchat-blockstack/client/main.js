3410fbd089700789e45964ca361236531da172f1
Tim Kinnane <tim@nestedcode.com>
2018-01-30 12:00:39 +1100

    fix(temp): Use standard redirect in _blank

--- packages/rocketchat-blockstack/client/main.js
+++ packages/rocketchat-blockstack/client/main.js
@@ -1,5 +1,4 @@
 import blockstack from 'blockstack'
-// import protocolCheck from 'custom-protocol-detection-blockstack'
 import { openPopup } from './popup'
 
 // Let service config set values, but not order of params
@@ -13,16 +12,16 @@ const mergeParams = (defaults, overrides) => {
 // Do signin redirect, with location of manifest
 const redirectToSignIn = (config) => {
   let defaults = {
-    redirectURI: `${window.location.origin}/`,
-    manifestURI: Accounts.blockstack.manifestURI,
-    scopes: ['store_write']
+    redirectURI: `${window.location.origin}/`, // location to redirect user to after sign in approval
+    manifestURI: `${window.location.origin}/manifest.json`, // location of this app's manifest file
+    scopes: ['store_write'] // the permissions this app is requesting
   }
   let params = mergeParams(defaults, config)
   return blockstack.redirectToSignIn(...params)
 }
 
 // Do a custom blockstack redirect through auth services
-const _makeAuthRequest = (config) => {
+const makeAuthRequest = (config) => {
   let defaults = {
     transitPrivateKey: blockstack.generateAndStoreTransitKey(), // hex encoded transit private key
     redirectURI: `${window.location.origin}/`, // location to redirect user to after sign in approval
@@ -36,26 +35,31 @@ const _makeAuthRequest = (config) => {
 }
 
 // Save the auth request for when redirect returns
-// TODO: Not currently used, should do so
-const _saveDataForRedirect = (privateKey, authRequest) => {
+// TODO: Not currently used, should do for popup
+const saveDataForRedirect = (privateKey, authRequest) => {
   Reload._onMigrate('blockstack', () => [true, {authRequest: authRequest}])
   Reload._migrate(null, {immediateMigration: true})
 }
 
+// Send user to Blockstack with auth request
+// TODO: allow serviceConfig.loginStyle == popup
 Meteor.loginWithBlockstack = function(option={}, callback) {
-  const privateKey = blockstack.generateAndStoreTransitKey()
-  const serviceConfig = ServiceConfiguration.configurations.findOne({service: 'blockstack'})
-  const requestParams = Object.assign({ transitPrivateKey: privateKey }, serviceConfig)
-  const authRequest = _makeAuthRequest(requestParams)
-  const httpsURI = `${serviceConfig.blockstackIDHost}auth?authRequest=${authRequest}`
-  // GO!...
-  // _saveDataForRedirect(privateKey, authRequest)
-  // TODO: if serviceConfig.loginStyle == popup else...
-  window.location.assign(httpsURI) // hack, just do it without protocol handler
-  // NB: using smarter protocol detection gets routed to new tab by Rocket.Chat :(
-  // const protocolURI = `blockstack:${authRequest}`
-  // const protocolSuccess = () => console.log('protocol handler detected')
-  // const protocolFail = () => window.location.assign(httpsURI)
-  // const protocolUnsupported = () => window.location.assign(protocolURI)
-  // protocolCheck(protocolURI, protocolFail, protocolSuccess, protocolUnsupported)
+  const serviceConfig = ServiceConfiguration.configurations.findOne({ service: 'blockstack' })
+  /*
+    const privateKey = blockstack.generateAndStoreTransitKey()
+    const requestParams = Object.assign({ transitPrivateKey: privateKey }, serviceConfig)
+    const authRequest = makeAuthRequest(requestParams)
+    const httpsURI = `${serviceConfig.blockstackIDHost}auth?authRequest=${authRequest}`
+    window.location.assign(httpsURI) // hack redirect without protocol handler
+  */
+  redirectToSignIn(serviceConfig)
+  /*
+    // NB: using smarter protocol detection gets routed to new tab by Rocket.Chat :(
+    import protocolCheck from 'custom-protocol-detection-blockstack'
+    const protocolURI = `blockstack:${authRequest}`
+    const protocolSuccess = () => console.log('protocol handler detected')
+    const protocolFail = () => window.location.assign(httpsURI)
+    const protocolUnsupported = () => window.location.assign(protocolURI)
+    protocolCheck(protocolURI, protocolFail, protocolSuccess, protocolUnsupported)
+  */
 }
