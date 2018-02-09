5c7b7da1f93ea5ba603036ebb7b791b2e8f34ce7
Diego Sampaio <chinello@gmail.com>
2018-02-09 11:04:29 -0200

    Improve read receipts modal UI

--- imports/message-read-receipt/server/api/methods/getReadReceipts.js
+++ imports/message-read-receipt/server/api/methods/getReadReceipts.js
@@ -4,7 +4,16 @@ import { ReadReceipt } from '../../lib/ReadReceipt';
 
 Meteor.methods({
 	getReadReceipts({ messageId }) {
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'getReadReceipts' });
+		}
+
 		const message = RocketChat.models.Messages.findOneById(messageId);
+
+		const room = Meteor.call('canAccessRoom', message.rid, Meteor.userId());
+		if (!room) {
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'getReadReceipts' });
+		}
 
 		return ReadReceipt.getReceipts(message);
 	}
