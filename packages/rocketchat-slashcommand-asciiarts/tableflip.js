7ea92d6bdad2787f52f98ae792df40712e48b979
Bradley Hilton <graywolf336@craftyn.com>
2018-02-09 11:46:47 -0600

    Add the asciiarts commands back in for now until the Rocket.Chat Apps are officially released

---
+++ packages/rocketchat-slashcommand-asciiarts/tableflip.js
@@ -0,0 +1,18 @@
+/*
+* Tableflip is a named function that will replace /Tableflip commands
+* @param {Object} message - The message object
+*/
+
+
+function Tableflip(command, params, item) {
+	if (command === 'tableflip') {
+		const msg = item;
+		msg.msg = `${ params } (╯°□°）╯︵ ┻━┻`;
+		Meteor.call('sendMessage', msg);
+	}
+}
+
+RocketChat.slashCommands.add('tableflip', Tableflip, {
+	description: 'Slash_Tableflip_Description',
+	params: 'your_message_optional'
+});
