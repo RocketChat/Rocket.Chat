1a14c395711e003e5d22680d7b5627f5f9d01667
Diego Sampaio <chinello@gmail.com>
2018-02-14 17:18:51 -0200

    Improve set as read logic

--- imports/message-read-receipt/server/lib/ReadReceipt.js
+++ imports/message-read-receipt/server/lib/ReadReceipt.js
@@ -3,7 +3,22 @@ import ModelReadReceipts from '../models/ReadReceipts';
 
 const rawReadReceipts = ModelReadReceipts.model.rawCollection();
 
-// @TODO create a debounced function by roomId, so multiple calls to same roomId runs only once
+// debounced function by roomId, so multiple calls within 2 seconds to same roomId runs only once
+const list = {};
+const debounceByRoomId = function(fn) {
+	return function(roomId, ...args) {
+		clearTimeout(list[roomId]);
+		list[roomId] = setTimeout(() => { console.log('vai rodar'); fn.call(this, roomId, ...args); }, 2000);
+	};
+};
+
+const updateMessages = debounceByRoomId(Meteor.bindEnvironment((roomId) => {
+	// @TODO maybe store firstSubscription in room object so we don't need to call the above update method
+	// if firstSubscription on room didn't change
+	const firstSubscription = RocketChat.models.Subscriptions.getMinimumLastSeenByRoomId(roomId);
+	// console.log('firstSubscription ->', firstSubscription);
+	RocketChat.models.Messages.setAsRead(roomId, firstSubscription.ls);
+}));
 
 export const ReadReceipt = {
 	markMessagesAsRead(roomId, userId, userLastSeen) {
@@ -13,29 +28,34 @@ export const ReadReceipt = {
 
 		const room = RocketChat.models.Rooms.findOneById(roomId, { fields: { lm: 1 } });
 
-		// if users last seen is greater than room's last message, it means the user already have this room marked as read
+		// if users last seen is greadebounceByRoomIdter than room's last message, it means the user already have this room marked as read
 		if (userLastSeen > room.lm) {
 			return;
 		}
 
+		this.storeReadReceipts(RocketChat.models.Messages.findUnreadMessagesByRoomAndDate(roomId, userLastSeen), roomId, userId);
+
+		updateMessages(roomId);
+	},
+
+	markMessageAsReadBySender(message, roomId, userId) {
+		if (!RocketChat.settings.get('Message_Read_Receipt_Enabled')) {
+			return;
+		}
+
+		// this will usually happens if the message sender is the only one on the room
 		const firstSubscription = RocketChat.models.Subscriptions.getMinimumLastSeenByRoomId(roomId);
-		// console.log('userLastSeen ->', userLastSeen);
-		// console.log('firstSubscription ->', firstSubscription);
-		// console.log('room ->', room);
+		if (message.unread && message.ts < firstSubscription.ls) {
+			RocketChat.models.Messages.setAsReadById(message._id, firstSubscription.ls);
+		}
 
-		// last time room was read is already past room's last message, so does nothing everybody have this room already
-		// if (firstSubscription.ls > room.lm) {
-		// 	console.log('already read by everyone');
+		this.storeReadReceipts([{ _id: message._id }], roomId, userId);
+	},
 
-		// 	return;
-		// }
-
-		// @TODO maybe store firstSubscription in room object so we don't need to call the above update method
-		// if firstSubscription on room didn't change
-
+	storeReadReceipts(messages, roomId, userId) {
 		if (RocketChat.settings.get('Message_Read_Receipt_Store_Users')) {
 			const ts = new Date();
-			const receipts = RocketChat.models.Messages.findUnreadMessagesByRoomAndDate(roomId, userLastSeen).map(message => {
+			const receipts = messages.map(message => {
 				return {
 					_id: Random.id(),
 					roomId,
@@ -51,8 +71,6 @@ export const ReadReceipt = {
 				console.error('Error inserting read receipts per user');
 			}
 		}
-
-		RocketChat.models.Messages.setAsRead(roomId, firstSubscription.ls);
 	},
 
 	getReceipts(message) {
