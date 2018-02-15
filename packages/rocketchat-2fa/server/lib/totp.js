bc44d21838f27eb27da98ec8f4beef8e98afdcce
Hudell <brian@hudell.com>
2018-02-15 16:42:11 -0200

    Organized code

--- packages/rocketchat-2fa/server/lib/totp.js
+++ packages/rocketchat-2fa/server/lib/totp.js
@@ -13,42 +13,39 @@ RocketChat.TOTP = {
 	},
 
 	verify({ secret, token, backupTokens, userId }) {
-		let verified;
-		let maxDelta = RocketChat.settings.get('Accounts_TwoFactorAuthentication_MaxDelta');
-
 		// validates a backup code
 		if (token.length === 8 && backupTokens) {
 			const hashedCode = SHA256(token);
 			const usedCode = backupTokens.indexOf(hashedCode);
 
 			if (usedCode !== -1) {
-				verified = true;
-
 				backupTokens.splice(usedCode, 1);
 
 				// mark the code as used (remove it from the list)
 				RocketChat.models.Users.update2FABackupCodesByUserId(userId, backupTokens);
+				return true;
 			}
-		} else {
-			if (maxDelta) {
-				let verifiedDelta = speakeasy.totp.verifyDelta({
-					secret,
-					encoding: 'base32',
-					token,
-					window: maxDelta
-				});
 
-				verified = verifiedDelta !== undefined;
-			} else {
-				verified = speakeasy.totp.verify({
-					secret,
-					encoding: 'base32',
-					token
-				});
-			}
+			return false;
 		}
 
-		return verified;
+		const maxDelta = RocketChat.settings.get('Accounts_TwoFactorAuthentication_MaxDelta');
+		if (maxDelta) {
+			const verifiedDelta = speakeasy.totp.verifyDelta({
+				secret,
+				encoding: 'base32',
+				token,
+				window: maxDelta
+			});
+
+			return verifiedDelta !== undefined;
+		}
+		
+		return speakeasy.totp.verify({
+			secret,
+			encoding: 'base32',
+			token
+		});
 	},
 
 	generateCodes() {
