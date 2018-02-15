c8b18fe28a7efa6b2e8d26a70678cd1044db4d7c
Diego Sampaio <chinello@gmail.com>
2018-02-15 14:58:05 -0200

    Code cleanup

--- packages/rocketchat-ui/client/views/app/room.js
+++ packages/rocketchat-ui/client/views/app/room.js
@@ -718,23 +718,6 @@ Template.room.events({
 			showCancelButton: true,
 			cancelButtonText: t('Close')
 		});
-	},
-	'click .read-receipt'(event) {
-		if (!RocketChat.settings.get('Message_Read_Receipt_Store_Users')) {
-			return;
-		}
-		const data = Blaze.getData(event.currentTarget);
-		const messageId = data && data._arguments && data._arguments[1] && data._arguments[1]._id;
-		modal.open({
-			title: t('Read_receipts'),
-			content: 'readReceipts',
-			data: {
-				messageId
-			},
-			showConfirmButton: true,
-			showCancelButton: false,
-			confirmButtonText: t('Close')
-		});
 	}
 });
 
