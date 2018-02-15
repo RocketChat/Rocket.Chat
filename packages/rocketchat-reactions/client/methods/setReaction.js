3b262c26aef9f27d59dbd1880f12bb7eb5ddf6bb
Marcos Vinicius Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2018-02-15 18:16:09 -0200

    removed uncessary parameter

--- packages/rocketchat-reactions/client/methods/setReaction.js
+++ packages/rocketchat-reactions/client/methods/setReaction.js
@@ -1,12 +1,12 @@
 import _ from 'underscore';
 
 Meteor.methods({
-	setReaction(reaction, messageId, usr) {
+	setReaction(reaction, messageId) {
 		if (!Meteor.userId()) {
 			throw new Meteor.Error(203, 'User_logged_out');
 		}
 
-		const user = usr ? usr : Meteor.user();
+		const user = Meteor.user();
 
 		const message = RocketChat.models.Messages.findOne({ _id: messageId });
 		const room = RocketChat.models.Rooms.findOne({ _id: message.rid });
