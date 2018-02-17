b32b0107fcd37c00d5a04fc73cc5f883e5bc7c45
Karan Bedi <karanbedi0304@gmail.com>
2018-02-17 11:39:10 +0530

    [NEW] Audio Message UI - Skip preview modal

--- packages/rocketchat-ui-message/client/messageBox.js
+++ packages/rocketchat-ui-message/client/messageBox.js
@@ -1,4 +1,4 @@
-/* globals fileUpload KonchatNotification chatMessages popover isRtl AudioRecorder chatMessages*/
+/* globals fileUpload KonchatNotification chatMessages popover isRtl AudioRecorder chatMessages fileUploadHandler*/
 import toastr from 'toastr';
 import moment from 'moment';
 
@@ -488,13 +488,96 @@ Template.messageBox.events({
 
 			AudioRecorder.stop(function(blob) {
 				chatMessages[RocketChat.openedRoom].recording = false;
-				fileUpload([
-					{
-						file: blob,
-						type: 'audio',
-						name: `${ TAPi18n.__('Audio record') }.mp3`
+				const roomId = Session.get('openedRoom');
+				const record = {
+					name: `${ TAPi18n.__('Audio record') }.mp3`,
+					size: blob.size,
+					type: 'audio/mp3',
+					rid: roomId,
+					description: ''
+				}
+				const upload = fileUploadHandler('Uploads', record, blob);
+				let uploading = Session.get('uploading') || [];
+				uploading.push({
+					id: upload.id,
+					name: upload.getFileName(),
+					percentage: 0
+				});
+				Session.set('uploading', uploading);
+				upload.onProgress = function(progress) {
+					uploading = Session.get('uploading');
+
+					const item = _.findWhere(uploading, {id: upload.id});
+					if (item != null) {
+						item.percentage = Math.round(progress * 100) || 0;
+						return Session.set('uploading', uploading);
 					}
-				]);
+				};
+
+				upload.start(function(error, file, storage) {
+					if (error) {
+						let uploading = Session.get('uploading');
+						if (!Array.isArray(uploading)) {
+							uploading = [];
+						}
+
+						const item = _.findWhere(uploading, { id: upload.id });
+
+						if (_.isObject(item)) {
+							item.error = error.message;
+							item.percentage = 0;
+						} else {
+							uploading.push({
+								error: error.error,
+								percentage: 0
+							});
+						}
+
+						Session.set('uploading', uploading);
+						return;
+					}
+
+
+					if (file) {
+						Meteor.call('sendFileMessage', roomId, storage, file, () => {
+							Meteor.setTimeout(() => {
+								const uploading = Session.get('uploading');
+								if (uploading !== null) {
+									const item = _.findWhere(uploading, {
+										id: upload.id
+									});
+									return Session.set('uploading', _.without(uploading, item));
+								}
+							}, 2000);
+						});
+					}
+				});
+
+				Tracker.autorun(function(c) {
+					const cancel = Session.get(`uploading-cancel-${ upload.id }`);
+					if (cancel) {
+						let item;
+						upload.stop();
+						c.stop();
+
+						uploading = Session.get('uploading');
+						if (uploading != null) {
+							item = _.findWhere(uploading, {id: upload.id});
+							if (item != null) {
+								item.percentage = 0;
+							}
+							Session.set('uploading', uploading);
+						}
+
+						return Meteor.setTimeout(function() {
+							uploading = Session.get('uploading');
+							if (uploading != null) {
+								item = _.findWhere(uploading, {id: upload.id});
+								return Session.set('uploading', _.without(uploading, item));
+							}
+						}, 1000);
+					}
+				});
 			});
 			return false;
 		}
