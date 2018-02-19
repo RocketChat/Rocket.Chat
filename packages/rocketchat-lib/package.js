b6b85d45edbacc2fa7730cd8bc2d88cf4d53a14b
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-02-19 10:42:15 -0300

    integration oauth with facebook

--- packages/rocketchat-lib/package.js
+++ packages/rocketchat-lib/package.js
@@ -129,6 +129,7 @@ Package.onUse(function(api) {
 	api.addFiles('server/models/Users.js', 'server');
 
 	api.addFiles('server/oauth/oauth.js', 'server');
+	api.addFiles('server/oauth/facebook.js', 'server');
 	api.addFiles('server/oauth/google.js', 'server');
 	api.addFiles('server/oauth/proxy.js', 'server');
 
