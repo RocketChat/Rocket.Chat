be9bb330ba5d7e6050238627b341530bb44b36d8
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-02-20 00:47:35 -0300

    init

---
+++ packages/rocketchat-ui/client/views/app/alerts.js
@@ -0,0 +1,56 @@
+/* globals alerts isRtl */
+import _ from 'underscore';
+
+import { hide, leave } from 'meteor/rocketchat:lib';
+
+this.alerts = {
+	renderedAlert: null,
+	open(config) {
+		this.close();
+		this.renderedAlert = Blaze.renderWithData(Template.alerts, config, document.body, document.body.querySelector('#rocket-chat'));
+	},
+	close() {
+		if (!this.renderedAlert) {
+			return false;
+		}
+
+		Blaze.remove(this.renderedAlert);
+
+		const activeElement = this.renderedAlert.dataVar.curValue.activeElement;
+		if (activeElement) {
+			$(activeElement).removeClass('active');
+		}
+	}
+};
+
+Template.alerts.helpers({
+	hasAction() {
+		return !!this.action;
+	}
+});
+
+Template.alerts.onRendered(function() {
+	if (this.data.onRendered) {
+		this.data.onRendered();
+	}
+});
+
+Template.alerts.onDestroyed(function() {
+	if (this.data.onDestroyed) {
+		this.data.onDestroyed();
+	}
+});
+
+Template.alerts.events({
+	'click .js-action'(e, instance) {
+		!this.action || this.action.call(this, e, instance.data.data);
+		alerts.close();
+	},
+	'click .js-close'() {
+		alerts.close();
+	}
+});
+
+Template.alerts.helpers({
+	isSafariIos: /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent)
+});
--- packages/rocketchat-ui/package.js
+++ packages/rocketchat-ui/package.js
@@ -87,6 +87,7 @@ Package.onUse(function(api) {
 	api.addFiles('client/views/app/pageContainer.html', 'client');
 	api.addFiles('client/views/app/pageSettingsContainer.html', 'client');
 	api.addFiles('client/views/app/popout.html', 'client');
+	api.addFiles('client/views/app/alerts.html', 'client');
 
 	api.addFiles('client/views/app/privateHistory.html', 'client');
 	api.addFiles('client/views/app/room.html', 'client');
@@ -113,6 +114,7 @@ Package.onUse(function(api) {
 	api.addFiles('client/views/app/secretURL.js', 'client');
 	api.addFiles('client/views/app/videoCall/videoButtons.js', 'client');
 	api.addFiles('client/views/app/videoCall/videoCall.js', 'client');
+	api.addFiles('client/views/app/alerts.js', 'client');
 	api.addFiles('client/views/app/popover.js', 'client');
 	api.addFiles('client/views/app/popout.js', 'client');
 	api.addFiles('client/views/app/modal.js', 'client');
