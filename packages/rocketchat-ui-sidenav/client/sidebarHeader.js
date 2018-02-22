74f86c68f75940b3dcabfe980919cec62cf04e7f
Karl Prieb <karl@prieb.me>
2018-02-22 18:49:15 -0300

    change create channel icon

--- packages/rocketchat-ui-sidenav/client/sidebarHeader.js
+++ packages/rocketchat-ui-sidenav/client/sidebarHeader.js
@@ -143,7 +143,7 @@ const toolbarButtons = (user) => {
 	},
 	{
 		name: t('Create_A_New_Channel'),
-		icon: 'plus',
+		icon: 'edit-roudend',
 		condition: () => RocketChat.authz.hasAtLeastOnePermission(['create-c', 'create-p']),
 		action: () => {
 			menu.close();
