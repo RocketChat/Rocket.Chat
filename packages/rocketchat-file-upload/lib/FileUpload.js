31ead9ad92867f19b00d0e1256a423773ce2fc2e
graywolf336 <graywolf336@craftyn.com>
2018-02-22 15:43:14 -0600

    Allow the filesize to be -1 which means unlimited for #9577

--- packages/rocketchat-file-upload/lib/FileUpload.js
+++ packages/rocketchat-file-upload/lib/FileUpload.js
@@ -30,14 +30,15 @@ FileUpload = {
 			throw new Meteor.Error('error-direct-message-file-upload-not-allowed', reason);
 		}
 
-		if (file.size > maxFileSize) {
+		// -1 maxFileSize means there is no limit
+		if (maxFileSize >= -1 && file.size > maxFileSize) {
 			const reason = TAPi18n.__('File_exceeds_allowed_size_of_bytes', {
 				size: filesize(maxFileSize)
 			}, user.language);
 			throw new Meteor.Error('error-file-too-large', reason);
 		}
 
-		if (parseInt(maxFileSize) > 0) {
+		if (maxFileSize > 0) {
 			if (file.size > maxFileSize) {
 				const reason = TAPi18n.__('File_exceeds_allowed_size_of_bytes', {
 					size: filesize(maxFileSize)
@@ -56,5 +57,9 @@ FileUpload = {
 };
 
 RocketChat.settings.get('FileUpload_MaxFileSize', function(key, value) {
-	maxFileSize = value;
+	try {
+		maxFileSize = parseInt(value);
+	} catch (e) {
+		maxFileSize = 2097152; // the default value
+	}
 });
