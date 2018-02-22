16e96ed9d12e40f3a9644b90429a81d2c140647b
Henrique Magarotto <magarotto@gmail.com>
2018-02-22 15:07:23 -0300

    Added api to enable external system sends visitor message

--- packages/rocketchat-livechat/server/api.js
+++ packages/rocketchat-livechat/server/api.js
@@ -2,3 +2,4 @@ import '../imports/server/rest/departments.js';
 import '../imports/server/rest/facebook.js';
 import '../imports/server/rest/sms.js';
 import '../imports/server/rest/users.js';
+import '../imports/server/rest/messages.js';
