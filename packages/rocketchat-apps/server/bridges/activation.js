b468b06f99e17fa875b6c55ac09b8d80940f30de
graywolf336 <graywolf336@craftyn.com>
2018-02-26 13:03:07 -0600

    Multi-instance servers now support Rocket.Chat Apps and get notified of updates

--- packages/rocketchat-apps/server/bridges/activation.js
+++ packages/rocketchat-apps/server/bridges/activation.js
@@ -4,26 +4,18 @@ export class AppActivationBridge {
 	}
 
 	appAdded(app) {
-		console.log(`The ${ app.getName() } App (${ app.getID() }) has been added.`);
-
 		this.orch.getNotifier().appAdded(app.getID());
 	}
 
 	appUpdated(app) {
-		console.log(`The ${ app.getName() } App (${ app.getID() }) has been updated.`);
-
 		this.orch.getNotifier().appUpdated(app.getID());
 	}
 
 	appRemoved(app) {
-		console.log(`The ${ app.getName() } App (${ app.getID() }) has been removed.`);
-
 		this.orch.getNotifier().appRemoved(app.getID());
 	}
 
 	appStatusChanged(app, status) {
-		console.log(`The ${ app.getName() } App (${ app.getID() }) status has changed: ${ status }`);
-
 		this.orch.getNotifier().appStatusUpdated(app.getID(), status);
 	}
 }
