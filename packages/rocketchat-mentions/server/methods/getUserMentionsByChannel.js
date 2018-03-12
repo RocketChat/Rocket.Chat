665b0f413aa365dbd2fc13e1d2bd236a536c7207
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-03-12 12:26:07 -0300

    added endpoint to retrieve all the mentions of a channel

---
+++ packages/rocketchat-mentions/server/methods/getUserMentionsByChannel.js
@@ -0,0 +1,19 @@
+Meteor.methods({
+	getUserMentionsByChannel({ roomId }) {
+		check(roomId, String);
+
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'getUserMentionsByChannel' });
+		}
+
+		const room = RocketChat.models.Rooms.findOneById(roomId);
+
+		if (!room) {
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'getUserMentionsByChannel' });
+		}
+
+		const user = RocketChat.models.Users.findOneById(Meteor.userId());
+
+		return RocketChat.models.Messages.findVisibleByMentionAndRoomId(user.username, roomId).fetch();
+	}
+});
