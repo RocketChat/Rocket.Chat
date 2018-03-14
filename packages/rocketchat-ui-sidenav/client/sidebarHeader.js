3c24051020fee0e8819e7424f04787685a5a8c9e
Nikhil <nikhil.celtics@gmail.com>
2018-03-14 23:34:02 +0530

    enables admin ui for manage-emoji permission

--- packages/rocketchat-ui-sidenav/client/sidebarHeader.js
+++ packages/rocketchat-ui-sidenav/client/sidebarHeader.js
@@ -153,10 +153,10 @@ const toolbarButtons = (user) => {
 	{
 		name: t('Options'),
 		icon: 'menu',
-		condition: () => AccountBox.getItems().length || RocketChat.authz.hasAtLeastOnePermission(['view-statistics', 'view-room-administration', 'view-user-administration', 'view-privileged-setting' ]),
+		condition: () => AccountBox.getItems().length || RocketChat.authz.hasAtLeastOnePermission(['view-statistics', 'view-room-administration', 'view-user-administration', 'view-privileged-setting', 'manage-emoji' ]),
 		action: (e) => {
 			let adminOption;
-			if (RocketChat.authz.hasAtLeastOnePermission(['view-statistics', 'view-room-administration', 'view-user-administration', 'view-privileged-setting' ])) {
+			if (RocketChat.authz.hasAtLeastOnePermission(['view-statistics', 'view-room-administration', 'view-user-administration', 'view-privileged-setting', 'manage-emoji' ])) {
 				adminOption = {
 					icon: 'customize',
 					name: t('Administration'),
