a46221b0701b4abc52cebeb04783a638cd99143a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-03-15 16:35:03 -0300

    Update addOAuthService.js

--- packages/rocketchat-lib/server/methods/addOAuthService.js
+++ packages/rocketchat-lib/server/methods/addOAuthService.js
@@ -17,7 +17,6 @@ Meteor.methods({
 
 		name = name.toLowerCase().replace(/[^a-z0-9_]/g, '');
 		name = s.capitalize(name);
-
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }`                   , false             , { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Enable', persistent: true });
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-url`               , ''                , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'URL', persistent: true });
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-token_path`        , '/oauth/token'    , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Token_Path', persistent: true });
@@ -33,6 +32,6 @@ Meteor.methods({
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-button_label_color`, '#FFFFFF'         , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Button_Label_Color', persistent: true });
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-button_color`      , '#13679A'         , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Button_Color', persistent: true });
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-username_field`    , ''                , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Username_Field', persistent: true });
-		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-avatar_field`      , ''                , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Avatar_Field', persistent: true });
 		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-merge_users`       , false             , { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Merge_Users', persistent: true });
+		RocketChat.settings.add(`Accounts_OAuth_Custom-${ name }-avatar_field`      , ''                , { type: 'string' , group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Avatar_Field', persistent: true });
 	}});
