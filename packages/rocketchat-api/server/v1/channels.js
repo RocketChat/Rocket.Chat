bdbd6f756792e0bbe62003b2709051b244d59769
Nikhil <nikhil.celtics@gmail.com>
2018-03-20 23:05:21 +0530

    includes private room in find by id function

--- packages/rocketchat-api/server/v1/channels.js
+++ packages/rocketchat-api/server/v1/channels.js
@@ -18,7 +18,7 @@ function findChannelByIdOrName({ params, checkedArchived = true, returnUsernames
 		room = RocketChat.models.Rooms.findOneByName(params.roomName, { fields });
 	}
 
-	if (!room || room.t !== 'c') {
+	if (!room || !(room.t === 'c' || room.t === 'p')) {
 		throw new Meteor.Error('error-room-not-found', 'The required "roomId" or "roomName" param provided does not match any channel');
 	}
 
