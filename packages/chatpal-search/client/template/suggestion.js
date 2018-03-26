abff45ce293c37667d17a9e1d3e08c53d73e4010
Thomas Kurz <th.kurz@gmail.com>
2018-03-26 17:26:50 +0200

    fix code style

--- packages/chatpal-search/client/template/suggestion.js
+++ packages/chatpal-search/client/template/suggestion.js
@@ -1,7 +1,7 @@
 Template.ChatpalSuggestionItemTemplate.onCreated(function() {
 	if (this.data.type === 'link') {
 		this.data.action = () => {
-			console.log("an example for an external link")
+			console.log('an example for an external link');
 		};
 	}
 });
