5e0444be59e4f921f746551e662370274eb09f76
Henrique Magarotto <magarotto@gmail.com>
2018-03-26 22:31:04 -0300

    Added visitors endpoint

--- packages/rocketchat-livechat/server/api.js
+++ packages/rocketchat-livechat/server/api.js
@@ -3,3 +3,4 @@ import '../imports/server/rest/facebook.js';
 import '../imports/server/rest/sms.js';
 import '../imports/server/rest/users.js';
 import '../imports/server/rest/messages.js';
+import '../imports/server/rest/visitors.js';
