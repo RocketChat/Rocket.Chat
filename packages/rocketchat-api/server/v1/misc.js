66a32547d49507e73dc94606b4a81ccfb68c524c
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-04-06 21:17:10 -0300

    Add REST /forgotPassword endpoint and tests

--- packages/rocketchat-api/server/v1/misc.js
+++ packages/rocketchat-api/server/v1/misc.js
@@ -168,3 +168,18 @@ RocketChat.API.v1.addRoute('shield.svg', { authRequired: false }, {
 		};
 	}
 });
+
+RocketChat.API.v1.addRoute('forgotPassword', { authRequired: false }, {
+	post() {
+		const { email } = this.bodyParams;
+		if (!email) {
+			return RocketChat.API.v1.failure('The \'email\' param is required');
+		}
+
+		const emailSent = Meteor.call('sendForgotPasswordEmail', email);
+		if (emailSent) {
+			return RocketChat.API.v1.success();
+		}
+		return RocketChat.API.v1.failure('User not found');
+	}
+});
--- tests/end-to-end/api/00-miscellaneous.js
+++ tests/end-to-end/api/00-miscellaneous.js
@@ -97,5 +97,32 @@ describe('miscellaneous', function() {
 				.end(done);
 		});
 	});
+	describe('/forgotPassword', () => {
+		it('should send email to user (return success), when is a valid email', (done) => {
+			request.post(api('forgotPassword'))
+				.send({
+					email: adminEmail
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				})
+				.end(done);
+		});
+
+		it('should not send email to user(return error), when is a invalid email', (done) => {
+			request.post(api('forgotPassword'))
+				.send({
+					email: 'invalidEmail'
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+				})
+				.end(done);
+		});
+	});
 
 });
