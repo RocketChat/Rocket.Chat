58ee03a8e82fcd400add186d28c0ffe41a0db3b6
Thomas Kurz <th.kurz@gmail.com>
2018-04-15 20:40:36 +0200

    #8381 enable simple caching in order to reduce the number of db calls (fixes https://github.com/RocketChat/Rocket.Chat/pull/10110#discussion_r180751612 )

--- packages/rocketchat-search/server/service/validationService.js
+++ packages/rocketchat-search/server/service/validationService.js
@@ -5,11 +5,23 @@ class ValidationService {
 
 	validateSearchResult(result) {
 
+		const subscriptionCache = {};
+
+		const getSubscription = (rid, uid) => {console.log(rid,subscriptionCache.hasOwnProperty(rid));
+			if (!subscriptionCache.hasOwnProperty(rid)) {
+				subscriptionCache[rid] = Meteor.call('canAccessRoom', rid, uid);
+			}
+
+			return subscriptionCache[rid];
+		};
+
 		const uid = Meteor.userId();
 		//get subscription for message
 		if (result.message) {
 			result.message.docs.forEach((msg) => {
-				const subscription = Meteor.call('canAccessRoom', msg.rid, uid);
+
+				const subscription = getSubscription(msg.rid, uid);
+
 				if (subscription) {
 					msg.r = {name: subscription.name, t: subscription.t};
 					msg.username = subscription.username;
@@ -28,7 +40,7 @@ class ValidationService {
 
 		if (result.room) {
 			result.room.docs.forEach((room) => {
-				const subscription = Meteor.call('canAccessRoom', room._id, uid);
+				const subscription = getSubscription(room._id, uid);
 				if (subscription) {
 					room.valid = true;
 					SearchLogger.debug(`user ${ uid } can access ${ room._id } ( ${ subscription.t === 'd' ? subscription.username : subscription.name } )`);
