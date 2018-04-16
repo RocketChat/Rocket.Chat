a92417c549b3517ad97159b39b4d588f8b1d97b2
Gabriel Delavald <gdelavald@gmail.com>
2018-04-16 11:49:24 -0300

    [NEW] Room Announcements module (#10396)
    [NEW] Add internal API to handle room announcements

--- packages/rocketchat-channel-settings/server/functions/saveRoomAnnouncement.js
+++ packages/rocketchat-channel-settings/server/functions/saveRoomAnnouncement.js
@@ -4,11 +4,16 @@ RocketChat.saveRoomAnnouncement = function(rid, roomAnnouncement, user, sendMess
 	if (!Match.test(rid, String)) {
 		throw new Meteor.Error('invalid-room', 'Invalid room', { function: 'RocketChat.saveRoomAnnouncement' });
 	}
+	const announcementObject = typeof roomAnnouncement === 'object' ?
+		roomAnnouncement :
+		{
+			message: roomAnnouncement
+		};
 
-	roomAnnouncement = s.escapeHTML(roomAnnouncement);
-	const updated = RocketChat.models.Rooms.setAnnouncementById(rid, roomAnnouncement);
+	const escapedMessage = s.escapeHTML(announcementObject.message);
+	const updated = RocketChat.models.Rooms.setAnnouncementById(rid, {...announcementObject, message: escapedMessage});
 	if (updated && sendMessage) {
-		RocketChat.models.Messages.createRoomSettingsChangedWithTypeRoomIdMessageAndUser('room_changed_announcement', rid, roomAnnouncement, user);
+		RocketChat.models.Messages.createRoomSettingsChangedWithTypeRoomIdMessageAndUser('room_changed_announcement', rid, escapedMessage, user);
 	}
 
 	return updated;
