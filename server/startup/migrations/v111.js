b8fb08ac15a63bfeb3f88dcf7c4f5a8632de3af1
Diego Sampaio <chinello@gmail.com>
2018-04-17 15:57:29 -0300

    Changing migration version to 111

--- server/startup/migrations/v111.js
+++ server/startup/migrations/v111.js
@@ -1,6 +1,6 @@
 // Migration to give delete channel, delete group permissions to owner
 RocketChat.Migrations.add({
-	version: 110,
+	version: 111,
 	up() {
 		if (RocketChat.models && RocketChat.models.Permissions) {
 			RocketChat.models.Permissions.update({ _id: 'delete-c' }, { $addToSet: { roles: 'owner' } });
