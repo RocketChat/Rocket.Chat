4b5360fe15bdb8214a094d35258f6ecde86bd0ad
Zhou Yuntao <zhouyuntao@magictavern.com>
2018-04-19 11:57:18 +0800

    fix tests (group limit admin)

--- tests/pageobjects/administration.page.js
+++ tests/pageobjects/administration.page.js
@@ -65,14 +65,14 @@ class Administration extends Page {
 	//settings
 	get buttonSave() { return browser.element('button.save'); }
 
-	get generalButtonExpandGroupLimit() { return browser.element('.section:nth-of-type(2) .button.expand'); }
-	get generalButtonExpandIframe() { return browser.element('.section:nth-of-type(3) .button.expand'); }
-	get generalButtonExpandNotifications() { return browser.element('.section:nth-of-type(4) .button.expand'); }
-	get generalButtonExpandRest() { return browser.element('.section:nth-of-type(5) .button.expand'); }
-	get generalButtonExpandReporting() { return browser.element('.section:nth-of-type(6) .button.expand'); }
-	get generalButtonExpandStreamCast() { return browser.element('.section:nth-of-type(7) .button.expand'); }
-	get generalButtonExpandTranslations() { return browser.element('.section:nth-of-type(8) .button.expand'); }
-	get generalButtonExpandUTF8() { return browser.element('.section:nth-of-type(9) .button.expand'); }
+	get generalButtonExpandGroupLimit() { return browser.element('.section:nth-of-type(3) .button.expand'); }
+	get generalButtonExpandIframe() { return browser.element('.section:nth-of-type(4) .button.expand'); }
+	get generalButtonExpandNotifications() { return browser.element('.section:nth-of-type(5) .button.expand'); }
+	get generalButtonExpandRest() { return browser.element('.section:nth-of-type(6) .button.expand'); }
+	get generalButtonExpandReporting() { return browser.element('.section:nth-of-type(7) .button.expand'); }
+	get generalButtonExpandStreamCast() { return browser.element('.section:nth-of-type(8) .button.expand'); }
+	get generalButtonExpandTranslations() { return browser.element('.section:nth-of-type(9) .button.expand'); }
+	get generalButtonExpandUTF8() { return browser.element('.section:nth-of-type(10) .button.expand'); }
 
 	get generalSiteUrl() { return browser.element('[name="Site_Url"]'); }
 	get generalSiteUrlReset() { return browser.element('.reset-setting[data-setting="Site_Url"]'); }
