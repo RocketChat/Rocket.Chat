54d0b27e356cde4f9345818fa1095855c4aca51b
Karl Prieb <karl@prieb.me>
2018-04-19 20:31:01 -0300

    implements dicts

--- packages/rocketchat-setup-wizard/client/setupWizard.js
+++ packages/rocketchat-setup-wizard/client/setupWizard.js
@@ -1,35 +1,47 @@
 Template.setupWizard.onCreated(function() {
-	this.currentStep = new ReactiveVar(3);
-	this.wizardSettings;
+	this.state = new ReactiveDict();
+	this.wizardSettings = {};
 	Meteor.call('getWizardSettings', (error, result) => {
 		if (result) {
 			this.wizardSettings = result;
 		}
 	});
+
+	const storage = JSON.parse(localStorage.getItem('wizard'));
+	Object.entries(storage).forEach(([key, value]) => {
+		this.state.set(key, value);
+	});
+
+	this.state.set('currentStep', 1);
+
+	Tracker.autorun(() => {
+		const states = this.state.all();
+		states['step1-password'] = '';
+		localStorage.setItem('wizard', JSON.stringify(states));
+	});
 });
 
 Template.setupWizard.events({
-	'click .setup-wizard-button-next'(e, t) {
-		const current = t.currentStep.get();
-
-		t.setupStep.set(current + 1);
+	'click .setup-wizard-forms__footer-next'(e, t) {
+		t.state.set('currentStep', t.state.get('currentStep') + 1);
 	},
 	'click .setup-wizard-button-back'(e, t) {
-		const current = t.currentStep.get();
-
-		t.setupStep.set(current - 1);
+		t.state.set('currentStep', t.state.get('currentStep') - 1);
+	},
+	'input input[data-step]'(e, t) {
+		t.state.set(e.currentTarget.name, e.currentTarget.value);
 	}
 });
 
 Template.setupWizard.helpers({
 	currentStepTemplate() {
-		return `setup-wizard-step${ Template.instance().setupStep.get() }`;
+		return `setup-wizard-step${ Template.instance().state.get('currentStep') }`;
 	},
 	currentStep() {
-		return Template.instance().setupStep.get();
+		return Template.instance().state.get('currentStep');
 	},
 	itemModifier(step) {
-		const current = Template.instance().currentStep.get();
+		const current = Template.instance().state.get('currentStep');
 
 		if (current === step) {
 			return 'setup-wizard-info__steps-item--active';
@@ -37,6 +49,20 @@ Template.setupWizard.helpers({
 
 		if (current > step) {
 			return 'setup-wizard-info__steps-item--past';
+		}
+
+		return '';
+	},
+	getValue(name) {
+		return Template.instance().state.get(name);
+	},
+	isDisabled() {
+		if (Template.instance().state.get('currentStep') === 1) {
+			const state = Template.instance().state.all();
+
+			if (Object.entries(state).filter(e => /step1-/.test(e[0]) && !e[1]).length) {
+				return 'disabled';
+			}
 		}
 
 		return '';
