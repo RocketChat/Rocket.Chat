d5e4e1496992a5df63c14e20475143e74a551ce2
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-04-21 01:27:28 -0300

    fix review

--- packages/rocketchat-livechat/client/startup/notifyUnreadRooms.js
+++ packages/rocketchat-livechat/client/startup/notifyUnreadRooms.js
@@ -1,30 +1,51 @@
+let audio = null;
+
+const stop = audio => {
+	if (!audio) {
+		return;
+	}
+	audio.loop = false;
+	return audio.pause && audio.pause();
+};
+const play = audio => {
+	if (!audio) {
+		return;
+	}
+	audio.loop = true;
+	return audio.play && audio.play();
+};
+
 Meteor.startup(function() {
 	Tracker.autorun(function() {
-		if (RocketChat.settings.get('Livechat_continuous_sound_notification_new_livechat_room')) {
-			const unreadAlertCount = ChatSubscription.find({ t: 'l', open: true, alert: true, unread: 1, disableNotifications: { $ne: true }, audioNotifications: { $ne: 'nothing'} }, { fields: { unread: 1, alert: 1, rid: 1, unreadAlert: 1 } }).count();
-			const user = RocketChat.models.Users.findOne(Meteor.userId(), {
-				fields: {
-					'settings.preferences.newRoomNotification': 1
-				}
-			});
 
-			const newRoomNotification = RocketChat.getUserPreference(user, 'newRoomNotification');
-			const [audio] = $(`audio#${ newRoomNotification }`);
-			if (audio && audio.play) {
-				if (unreadAlertCount > 0) {
-					Meteor.setTimeout(() => {
-						if (audio && audio.play) {
-							audio.loop = true;
-							return audio.play();
-						}
-					}, 1000);
-				} else {
-					if (audio.pause) {
-						audio.pause();
-					}
-					audio.loop = false;
-				}
+
+		const subs = RocketChat.models.Subscriptions.find({ t: 'l', ls : { $exists: 0 } }).count();
+
+		const user = RocketChat.models.Users.findOne(Meteor.userId(), {
+			fields: {
+				'settings.preferences.newRoomNotification': 1
 			}
+		});
+
+		const newRoomNotification = RocketChat.getUserPreference(user, 'newRoomNotification');
+
+		stop(audio);
+
+		if (!RocketChat.settings.get('Livechat_continuous_sound_notification_new_livechat_room')) {
+			return;
 		}
+
+		[audio] = $(`#${ newRoomNotification }`);
+
+		if (!audio || !audio.play || !audio.pause) {
+			return;
+		}
+
+		if (subs === 0) {
+			return;
+		}
+
+		play(audio);
+
 	});
 });
