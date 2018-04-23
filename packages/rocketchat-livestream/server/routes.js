c7c2306a007b4aa7c9d85c2a7b2397672a798b0d
Gabriel Delavald <gdelavald@gmail.com>
2018-04-23 18:25:22 -0300

    Fix broken callback url

--- packages/rocketchat-livestream/server/routes.js
+++ packages/rocketchat-livestream/server/routes.js
@@ -3,9 +3,8 @@ const OAuth2 = google.auth.OAuth2;
 
 RocketChat.API.v1.addRoute('livestream/oauth', {
 	get: function functionName() {
-		const clientAuth = new OAuth2(RocketChat.settings.get('Broadcasting_client_id'), RocketChat.settings.get('Broadcasting_client_secret'), `${ RocketChat.settings.get('Site_Url') }api/v1/livestream/oauth/callback`);
+		const clientAuth = new OAuth2(RocketChat.settings.get('Broadcasting_client_id'), RocketChat.settings.get('Broadcasting_client_secret'), `${ RocketChat.settings.get('Site_Url') }/api/v1/livestream/oauth/callback`.replace(/\/{2}api/g, '/api'));
 		const { userId } = this.queryParams;
-		console.log('userId', userId);
 		const url = clientAuth.generateAuthUrl({
 			access_type: 'offline',
 			scope: ['https://www.googleapis.com/auth/youtube'],
@@ -29,7 +28,7 @@ RocketChat.API.v1.addRoute('livestream/oauth/callback', {
 
 		const { userId } = JSON.parse(state);
 
-		const clientAuth = new OAuth2(RocketChat.settings.get('Broadcasting_client_id'), RocketChat.settings.get('Broadcasting_client_secret'), `${ RocketChat.settings.get('Site_Url') }api/v1/livestream/oauth/callback`);
+		const clientAuth = new OAuth2(RocketChat.settings.get('Broadcasting_client_id'), RocketChat.settings.get('Broadcasting_client_secret'), `${ RocketChat.settings.get('Site_Url') }/api/v1/livestream/oauth/callback`.replace(/\/{2}api/g, '/api'));
 
 		const ret = Meteor.wrapAsync(clientAuth.getToken.bind(clientAuth))(code);
 
