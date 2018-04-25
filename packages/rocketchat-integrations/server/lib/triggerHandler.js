63805b5643fef8c14949ba605bd539980fe4401a
Diego Sampaio <chinello@gmail.com>
2018-04-25 13:57:26 -0300

    Change _.clone object spread

--- packages/rocketchat-integrations/server/lib/triggerHandler.js
+++ packages/rocketchat-integrations/server/lib/triggerHandler.js
@@ -84,7 +84,7 @@ RocketChat.integrations.triggerHandler = new class RocketChatIntegrationHandler 
 		}
 
 		if (data) {
-			history.data = _.clone(data);
+			history.data = { ...data };
 
 			if (data.user) {
 				history.data.user = _.omit(data.user, ['meta', '$loki', 'services']);
