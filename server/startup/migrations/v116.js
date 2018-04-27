56f0b7011dd0373a131d360a7bf736bcb7f12bac
Karl Prieb <karl@prieb.me>
2018-04-27 20:57:13 -0300

    setup wizard setting migration

--- server/startup/migrations/v116.js
+++ server/startup/migrations/v116.js
@@ -5,7 +5,7 @@ RocketChat.Migrations.add({
 			if (RocketChat.models.Settings) {
 				const setting = RocketChat.models.Settings.findOne({ _id: 'Show_Setup_Wizard' });
 				if (!setting) {
-					RocketChat.models.Settings.insert(
+					RocketChat.models.Settings.upsert(
 						{ _id: 'Show_Setup_Wizard' },
 						{ $set: { value: false }}
 					);
