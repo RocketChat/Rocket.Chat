e227f896f26517e50cb019b02d051db58fa9d6ce
Karl Prieb <karl@prieb.me>
2018-04-27 20:14:17 -0300

    new migration for setupwizard

---
+++ server/startup/migrations/v116.js
@@ -0,0 +1,16 @@
+RocketChat.Migrations.add({
+	version: 116,
+	up() {
+		if (RocketChat && RocketChat.models) {
+			if (RocketChat.models.Settings) {
+				const setting = RocketChat.models.Settings.findOne({ _id: 'Show_Setup_Wizard' });
+				if (!setting) {
+					RocketChat.models.Settings.insert(
+						{ _id: 'Show_Setup_Wizard' },
+						{ $set: { value: false }}
+					);
+				}
+			}
+		}
+	}
+});
