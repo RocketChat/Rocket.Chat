6a8174294b43f60fcf93c734d723f47ea3bf120f
Henrique Magarotto <magarotto@gmail.com>
2018-05-02 21:11:59 -0300

    Refactoring queue methods to only select agents for routing

--- packages/rocketchat-livechat/server/lib/QueueMethods.js
+++ packages/rocketchat-livechat/server/lib/QueueMethods.js
@@ -6,7 +6,7 @@ RocketChat.QueueMethods = {
 	 * default method where the agent with the least number
 	 * of open chats is paired with the incoming livechat
 	 */
-	'Least_Amount'(guest, message, roomInfo, agent) {
+	'Least_Amount'(guest, message, room, agent) {
 		if (!agent) {
 			agent = RocketChat.Livechat.getNextAgent(guest.department);
 			if (!agent) {
@@ -14,70 +14,23 @@ RocketChat.QueueMethods = {
 			}
 		}
 
-		const roomCode = RocketChat.models.Rooms.getNextLivechatRoomCode();
-
-		const room = _.extend({
-			_id: message.rid,
-			msgs: 1,
-			lm: new Date(),
-			code: roomCode,
-			label: guest.name || guest.username,
-			// usernames: [agent.username, guest.username],
-			t: 'l',
-			ts: new Date(),
-			v: {
-				_id: guest._id,
-				username: guest.username,
-				token: message.token,
-				status: guest.status || 'online'
-			},
-			servedBy: {
-				_id: agent.agentId,
-				username: agent.username
-			},
-			cl: false,
-			open: true,
-			waitingResponse: true
-		}, roomInfo);
-		const subscriptionData = {
-			rid: message.rid,
-			name: guest.name || guest.username,
-			alert: true,
-			open: true,
-			unread: 1,
-			userMentions: 1,
-			groupMentions: 0,
-			code: roomCode,
-			u: {
-				_id: agent.agentId,
-				username: agent.username
-			},
-			t: 'l',
-			desktopNotifications: 'all',
-			mobilePushNotifications: 'all',
-			emailNotifications: 'all'
-		};
-
-		RocketChat.models.Rooms.insert(room);
-		RocketChat.models.Subscriptions.insert(subscriptionData);
-
+		RocketChat.models.Rooms.changeAgentByRoomId(room._id, agent);
+		RocketChat.models.Subscriptions.subscribeWithRoomAndUser(room, agent);
 		RocketChat.Livechat.stream.emit(room._id, {
 			type: 'agentData',
 			data: RocketChat.models.Users.getAgentInfo(agent.agentId)
 		});
 
-		return room;
+		return agent;
 	},
 	/* Guest Pool Queuing Method:
 	 *
 	 * An incomming livechat is created as an Inquiry
 	 * which is picked up from an agent.
 	 * An Inquiry is visible to all agents (TODO: in the correct department)
-     *
-	 * A room is still created with the initial message, but it is occupied by
-	 * only the client until paired with an agent
+	 *
 	 */
-	'Guest_Pool'(guest, message, roomInfo) {
+	'Guest_Pool'(guest, message, room) {
 		let agents = RocketChat.Livechat.getOnlineAgents(guest.department);
 
 		if (agents.count() === 0 && RocketChat.settings.get('Livechat_guest_pool_with_no_agents')) {
@@ -87,8 +40,6 @@ RocketChat.QueueMethods = {
 		if (agents.count() === 0) {
 			throw new Meteor.Error('no-agent-online', 'Sorry, no online agents');
 		}
-
-		const roomCode = RocketChat.models.Rooms.getNextLivechatRoomCode();
 
 		const agentIds = [];
 
@@ -105,7 +56,7 @@ RocketChat.QueueMethods = {
 			message: message.msg,
 			name: guest.name || guest.username,
 			ts: new Date(),
-			code: roomCode,
+			code: room.code,
 			department: guest.department,
 			agents: agentIds,
 			status: 'open',
@@ -117,29 +68,9 @@ RocketChat.QueueMethods = {
 			},
 			t: 'l'
 		};
-		const room = _.extend({
-			_id: message.rid,
-			msgs: 1,
-			lm: new Date(),
-			code: roomCode,
-			label: guest.name || guest.username,
-			// usernames: [guest.username],
-			t: 'l',
-			ts: new Date(),
-			v: {
-				_id: guest._id,
-				username: guest.username,
-				token: message.token,
-				status: guest.status
-			},
-			cl: false,
-			open: true,
-			waitingResponse: true
-		}, roomInfo);
 		RocketChat.models.LivechatInquiry.insert(inquiry);
-		RocketChat.models.Rooms.insert(room);
 
-		return room;
+		return undefined;
 	},
 	'External'(guest, message, roomInfo, agent) {
 		return this['Least_Amount'](guest, message, roomInfo, agent); // eslint-disable-line
