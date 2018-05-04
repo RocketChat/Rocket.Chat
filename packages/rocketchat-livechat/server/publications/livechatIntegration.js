a356331f93ac262f42a15cb99f5542dda100fd11
Rafael Souza <rafael@rafael.org>
2018-05-04 15:51:16 -0300

    Fixing indentations

--- packages/rocketchat-livechat/server/publications/livechatIntegration.js
+++ packages/rocketchat-livechat/server/publications/livechatIntegration.js
@@ -10,14 +10,14 @@ Meteor.publish('livechat:integration', function() {
 	const self = this;
 
 	const handle = RocketChat.models.Settings.findByIds([
-    'Livechat_webhookUrl',
-    'Livechat_secret_token',
-    'Livechat_webhook_on_close',
-    'Livechat_webhook_on_offline_msg',
-    'Livechat_webhook_on_visitor_message',
-    'Livechat_webhook_on_agent_message',
-    'Livechat_webhook_on_visitor_transfer'
-  ]).observeChanges({
+		'Livechat_webhookUrl',
+		'Livechat_secret_token',
+		'Livechat_webhook_on_close',
+		'Livechat_webhook_on_offline_msg',
+		'Livechat_webhook_on_visitor_message',
+		'Livechat_webhook_on_agent_message',
+		'Livechat_webhook_on_visitor_transfer'
+	]).observeChanges({
 		added(id, fields) {
 			self.added('livechatIntegration', id, fields);
 		},
