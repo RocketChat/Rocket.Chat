5be58e38ca89f931ccd3cb4b4e1af270445c7e8f
Hudell <brian@hudell.com>
2018-05-10 14:17:48 -0300

    Fixed code so that changing settings will take effect immediatelly

--- packages/rocketchat-wordpress/common.js
+++ packages/rocketchat-wordpress/common.js
@@ -1,4 +1,5 @@
 /* globals CustomOAuth */
+import _ from 'underscore';
 
 const config = {
 	serverURL: '',
@@ -12,58 +13,55 @@ const config = {
 
 const WordPress = new CustomOAuth('wordpress', config);
 
-const fillSettings = function() {
-	if (RocketChat.settings.get('API_Wordpress_URL')) {
-		config.serverURL = RocketChat.settings.get('API_Wordpress_URL');
+const fillSettings = _.debounce(() => {
+	config.serverURL = RocketChat.settings.get('API_Wordpress_URL');
 
-		delete config.identityPath;
-		delete config.identityTokenSentVia;
-		delete config.authorizePath;
-		delete config.tokenPath;
-		delete config.scope;
+	delete config.identityPath;
+	delete config.identityTokenSentVia;
+	delete config.authorizePath;
+	delete config.tokenPath;
+	delete config.scope;
 
-		const serverType = RocketChat.settings.get('Accounts_OAuth_Wordpress_server_type');
-		switch (serverType) {
-			case 'custom':
-				if (RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_path')) {
-					config.identityPath = RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_path');
-				}
+	const serverType = RocketChat.settings.get('Accounts_OAuth_Wordpress_server_type');
+	switch (serverType) {
+		case 'custom':
+			if (RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_path')) {
+				config.identityPath = RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_path');
+			}
 
-				if (RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_token_sent_via')) {
-					config.identityTokenSentVia = RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_token_sent_via');
-				}
+			if (RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_token_sent_via')) {
+				config.identityTokenSentVia = RocketChat.settings.get('Accounts_OAuth_Wordpress_identity_token_sent_via');
+			}
 
-				if (RocketChat.settings.get('Accounts_OAuth_Wordpress_token_path')) {
-					config.tokenPath = RocketChat.settings.get('Accounts_OAuth_Wordpress_token_path');
-				}
+			if (RocketChat.settings.get('Accounts_OAuth_Wordpress_token_path')) {
+				config.tokenPath = RocketChat.settings.get('Accounts_OAuth_Wordpress_token_path');
+			}
 
-				if (RocketChat.settings.get('Accounts_OAuth_Wordpress_authorize_path')) {
-					config.authorizePath = RocketChat.settings.get('Accounts_OAuth_Wordpress_authorize_path');
-				}
+			if (RocketChat.settings.get('Accounts_OAuth_Wordpress_authorize_path')) {
+				config.authorizePath = RocketChat.settings.get('Accounts_OAuth_Wordpress_authorize_path');
+			}
 
-				if (RocketChat.settings.get('Accounts_OAuth_Wordpress_scope')) {
-					config.scope = RocketChat.settings.get('Accounts_OAuth_Wordpress_scope');
-				}
-				break;
-			case 'wordpress-com':
-				config.identityPath = '/rest/v1/me';
-				config.identityTokenSentVia = 'header';
-				config.authorizePath = '/oauth2/authorize';
-				config.tokenPath = '/oauth2/token';
-				config.scope = 'auth';
-				break;
-			default:
-				config.identityPath = '/oauth/me';
-				break;
-		}
-
-		return WordPress.configure(config);
+			if (RocketChat.settings.get('Accounts_OAuth_Wordpress_scope')) {
+				config.scope = RocketChat.settings.get('Accounts_OAuth_Wordpress_scope');
+			}
+			break;
+		case 'wordpress-com':
+			config.identityPath = '/rest/v1/me';
+			config.identityTokenSentVia = 'header';
+			config.authorizePath = '/oauth2/authorize';
+			config.tokenPath = '/oauth2/token';
+			config.scope = 'auth';
+			break;
+		default:
+			config.identityPath = '/oauth/me';
+			break;
 	}
-};
+	return WordPress.configure(config);
+}, 1000);
 
 if (Meteor.isServer) {
 	Meteor.startup(function() {
-		return fillSettings();
+		return RocketChat.settings.get(/(API\_Wordpress\_URL)?(Accounts\_OAuth\_Wordpress\_)?/, () => fillSettings());
 	});
 } else {
 	Meteor.startup(function() {
