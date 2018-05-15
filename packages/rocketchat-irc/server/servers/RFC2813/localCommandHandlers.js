cbd54182f12b5551a4e44d1d16c26d9a9cf6d46d
Hudell <brian@hudell.com>
2018-05-15 14:51:37 -0300

    Fixed problems with scope

--- packages/rocketchat-irc/server/servers/RFC2813/localCommandHandlers.js
+++ packages/rocketchat-irc/server/servers/RFC2813/localCommandHandlers.js
@@ -1,73 +1,73 @@
-export default {
-	registerUser: (parameters) => {
-		const { name, profile: { irc: { nick, username } } } = parameters;
+function registerUser(parameters) {
+	const { name, profile: { irc: { nick, username } } } = parameters;
 
-		this.write({
-			prefix: this.config.server.name,
-			command: 'NICK', parameters: [ nick, 1, username, 'irc.rocket.chat', 1, '+i' ],
-			trailer: name
-		});
-	},
+	this.write({
+		prefix: this.config.server.name,
+		command: 'NICK', parameters: [ nick, 1, username, 'irc.rocket.chat', 1, '+i' ],
+		trailer: name
+	});
+}
 
-	joinChannel: (parameters) => {
-		const {
-			room: { name: roomName },
-			user: { profile: { irc: { nick } } }
-		} = parameters;
+function joinChannel(parameters) {
+	const {
+		room: { name: roomName },
+		user: { profile: { irc: { nick } } }
+	} = parameters;
 
-		this.write({
-			prefix: this.config.server.name,
-			command: 'NJOIN', parameters: [ `#${ roomName }` ],
-			trailer: nick
-		});
-	},
+	this.write({
+		prefix: this.config.server.name,
+		command: 'NJOIN', parameters: [ `#${ roomName }` ],
+		trailer: nick
+	});
+}
 
-	joinedChannel: (parameters) => {
-		const {
-			room: { name: roomName },
-			user: { profile: { irc: { nick } } }
-		} = parameters;
+function joinedChannel(parameters) {
+	const {
+		room: { name: roomName },
+		user: { profile: { irc: { nick } } }
+	} = parameters;
 
-		this.write({
-			prefix: nick,
-			command: 'JOIN', parameters: [ `#${ roomName }` ]
-		});
-	},
+	this.write({
+		prefix: nick,
+		command: 'JOIN', parameters: [ `#${ roomName }` ]
+	});
+}
 
-	leftChannel: (parameters) => {
-		const {
-			room: { name: roomName },
-			user: { profile: { irc: { nick } } }
-		} = parameters;
+function leftChannel(parameters) {
+	const {
+		room: { name: roomName },
+		user: { profile: { irc: { nick } } }
+	} = parameters;
 
-		this.write({
-			prefix: nick,
-			command: 'PART', parameters: [ `#${ roomName }` ]
-		});
-	},
+	this.write({
+		prefix: nick,
+		command: 'PART', parameters: [ `#${ roomName }` ]
+	});
+}
 
-	sentMessage: (parameters) => {
-		const {
-			user: { profile: { irc: { nick } } },
-			to,
-			message
-		} = parameters;
+function sentMessage(parameters) {
+	const {
+		user: { profile: { irc: { nick } } },
+		to,
+		message
+	} = parameters;
 
-		this.write({
-			prefix: nick,
-			command: 'PRIVMSG', parameters: [ to ],
-			trailer: message
-		});
-	},
+	this.write({
+		prefix: nick,
+		command: 'PRIVMSG', parameters: [ to ],
+		trailer: message
+	});
+}
 
-	disconnected: (parameters) => {
-		const {
-			user: { profile: { irc: { nick } } }
-		} = parameters;
+function disconnected(parameters) {
+	const {
+		user: { profile: { irc: { nick } } }
+	} = parameters;
 
-		this.write({
-			prefix: nick,
-			command: 'QUIT'
-		});
-	}
-};
+	this.write({
+		prefix: nick,
+		command: 'QUIT'
+	});
+}
+
+export default { registerUser, joinChannel, joinedChannel, leftChannel, sentMessage, disconnected };
