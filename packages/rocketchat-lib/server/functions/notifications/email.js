a914122f966a776e6637fa574b1bb5c54d401935
John Szaszvari <jszaszvari@gmail.com>
2018-05-29 01:08:33 +1000

    Fix formatting

--- packages/rocketchat-lib/server/functions/notifications/email.js
+++ packages/rocketchat-lib/server/functions/notifications/email.js
@@ -81,13 +81,13 @@ function getMessageLink(room, sub) {
 		'text-decoration: none;'
 	].join(' ');
 	const message = TAPi18n.__('Offline_Link_Message');
-    let linkSuffix;
-    if (room.t === 'd') {
-        linkSuffix = `/${ room.username }`;
-    } else if (room.t === 'c') {
-		linkSuffix = `/${ room.name }`;
-    }
-	return `<p style="text-align:center;margin-bottom:8px;"><a style="${ style }" href="${ path }${ linkSuffix }">${ message }</a>`;
+	let linkSuffix;
+	if (room.t === 'd') {
+		messageSuffix = `/${ room.username }`;
+	} else if (room.t === 'c') {
+		messageSuffix = `/${ room.name }`;
+	}
+	return `<p style="text-align:center;margin-bottom:8px;"><a style="${ style }" href="${ path }${ messageSuffix }">${ message }</a>`;
 }
 
 export function sendEmail({ message, user, subscription, room, emailAddress, toAll }) {
