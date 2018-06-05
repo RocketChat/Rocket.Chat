2c836914e448abfe2531c3bf1d5494187cc22137
Karl Prieb <karl@prieb.me>
2018-06-05 18:48:04 -0300

    table component: fixed header

--- packages/rocketchat-ui/client/components/table.js
+++ packages/rocketchat-ui/client/components/table.js
@@ -1,4 +1,7 @@
 Template.table.onRendered(function() {
-	const dummyTr = '<tr class="tr-dummy"></tr>';
+	const dummyTr = '<tr class="table-tr-dummy"></tr>';
 	this.$('tbody').prepend(dummyTr).append(dummyTr);
+	this.$('th').each(function() {
+		$(this).append(`<div class="table-fake-th">${ $(this).html() }</div>`);
+	});
 });
