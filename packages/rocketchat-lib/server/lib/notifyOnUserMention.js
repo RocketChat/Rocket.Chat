3ff136a49941242cfc19dd788fec90ba5b46b203
Roman Prokopczuk <lomboboo@gmail.com>
2018-06-05 14:46:14 +0200

    mention-notification * notifyOnUserMention refactor

--- packages/rocketchat-lib/server/lib/notifyOnUserMention.js
+++ packages/rocketchat-lib/server/lib/notifyOnUserMention.js
@@ -1,5 +1,5 @@
 /**
- * Send notification after User mention and User is not in the room already
+ * Send notification after User have been mentioned and User is not in the room already
  *
  * @param {string} message
  * @param {object} room
@@ -10,12 +10,10 @@
 
 function notifyOnUserMention(message, room, userId) {
 	if (room != null) {
-		const mentionIds = [];
 		const mentionedUsersThatAreNotInTheRoom = [];
 		if (message.mentions != null) {
 			message.mentions.forEach(function(mention) {
 				if (mention._id !== message.u._id) {
-					mentionIds.push(mention._id);
 					const userInTheRoom = RocketChat.models.Subscriptions.findByRoomAndUserId(room._id, mention._id).count();
 
 					if (!userInTheRoom) {
@@ -24,6 +22,7 @@ function notifyOnUserMention(message, room, userId) {
 				}
 			});
 
+			/* If user has permission to invite */
 			let canAddUser = false;
 			if (RocketChat.authz.hasPermission(userId, 'add-user-to-joined-room', room._id)) {
 				canAddUser = true;
@@ -53,15 +52,6 @@ function notifyOnUserMention(message, room, userId) {
 			}
 		}
 	}
-
-	// Update all the room activity tracker fields
-	// This method take so long to execute on gient rooms cuz it will trugger the cache rebuild for the releations of that room
-	RocketChat.models.Rooms.incMsgCountAndSetLastMessageById(message.rid, 1, message.ts, RocketChat.settings.get('Store_Last_Message') && message);
-	// Update all other subscriptions to alert their owners but witout incrementing
-	// the unread counter, as it is only for mentions and direct messages
-	// We now set alert and open properties in two separate update commands. This proved to be more efficient on MongoDB - because it uses a more efficient index.
-	RocketChat.models.Subscriptions.setAlertForRoomIdExcludingUserId(message.rid, message.u._id);
-	RocketChat.models.Subscriptions.setOpenForRoomIdExcludingUserId(message.rid, message.u._id);
 
 	return message;
 }
