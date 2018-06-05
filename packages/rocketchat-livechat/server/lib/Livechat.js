1f3bd477f9b3aad8cced3333714aa7649915f31f
Nikhil <nikhil.celtics@gmail.com>
2018-06-05 19:34:49 +0530

    Merge branch 'develop' into max-msg-size-emoji

--- packages/rocketchat-livechat/server/lib/Livechat.js
+++ packages/rocketchat-livechat/server/lib/Livechat.js
@@ -297,7 +297,7 @@ RocketChat.Livechat = {
 		});
 
 		if (!_.isEmpty(guestData.name)) {
-			return RocketChat.models.Rooms.setLabelByRoomId(roomData._id, guestData.name) && RocketChat.models.Subscriptions.updateNameByRoomId(roomData._id, guestData.name);
+			return RocketChat.models.Rooms.setFnameById(roomData._id, guestData.name) && RocketChat.models.Subscriptions.updateDisplayNameByRoomId(roomData._id, guestData.name);
 		}
 	},
 
@@ -351,7 +351,6 @@ RocketChat.Livechat = {
 				unread: 1,
 				userMentions: 1,
 				groupMentions: 0,
-				code: room.code,
 				u: {
 					_id: agent.agentId,
 					username: agent.username
@@ -410,9 +409,8 @@ RocketChat.Livechat = {
 
 		const postData = {
 			_id: room._id,
-			label: room.label,
+			label: room.fname || room.label, // using same field for compatibility
 			topic: room.topic,
-			code: room.code,
 			createdAt: room.ts,
 			lastMessageAt: room.lm,
 			tags: room.tags,
