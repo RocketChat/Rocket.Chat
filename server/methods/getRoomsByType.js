3295b99517c7771a344208021ca71240293a62f1
Roman Prokopczuk <lomboboo@gmail.com>
2018-06-08 17:35:34 +0200

    hide-all-direct-messages * getRoomByType error name fix

--- server/methods/getRoomsByType.js
+++ server/methods/getRoomsByType.js
@@ -3,7 +3,7 @@ Meteor.methods({
 		check(type, String);
 
 		if (!Meteor.userId()) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'getDirectRoomsByUsername' });
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'getRoomsByType' });
 		}
 
 		return RocketChat.models.Rooms.findByTypeContainingUsername(type, Meteor.user().username).fetch();
