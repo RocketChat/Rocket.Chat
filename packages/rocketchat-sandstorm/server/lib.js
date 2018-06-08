2acaa63bf76f5bfa4b8a92a1e3cf25f9f3ed6421
PeterLee <peterlee0127@gmail.com>
2018-06-08 14:09:41 +0800

    update import capnp

--- packages/rocketchat-sandstorm/server/lib.js
+++ packages/rocketchat-sandstorm/server/lib.js
@@ -5,7 +5,7 @@ import Future from 'fibers/future';
 RocketChat.Sandstorm = {};
 
 if (process.env.SANDSTORM === '1') {
-	import Capnp from 'capnp';
+	const Capnp = require('capnp');
 	const SandstormHttpBridge = Capnp.importSystem('sandstorm/sandstorm-http-bridge.capnp').SandstormHttpBridge;
 
 	let capnpConnection = null;
