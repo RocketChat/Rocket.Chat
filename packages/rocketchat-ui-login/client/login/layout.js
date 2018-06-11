e6e2089fffa358af784adb1196dce0ba48cd4b10
vynmera <39674991+vynmera@users.noreply.github.com>
2018-06-11 21:48:21 +0000

    Address requested changes

--- packages/rocketchat-ui-login/client/login/layout.js
+++ packages/rocketchat-ui-login/client/login/layout.js
@@ -6,7 +6,7 @@ Template.loginLayout.helpers({
 	backgroundUrl() {
 		const asset = RocketChat.settings.get('Assets_background');
 		const prefix = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX || '';
-		if (asset != null) {
+		if (asset && (asset.url || asset.defaultUrl)) {
 			return `${ prefix }/${ asset.url || asset.defaultUrl }`;
 		}
 	}
