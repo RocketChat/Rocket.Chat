f1610a1fa88b8a38ce48655e76c2520ce678a482
Mikael Mello <mikaelmmello@gmail.com>
2018-06-12 19:18:10 -0300

    replyClientCommand checks whether its caller is the target of the command

--- packages/rocketchat-lib/server/methods/replyClientCommand.js
+++ packages/rocketchat-lib/server/methods/replyClientCommand.js
@@ -4,8 +4,17 @@ Meteor.methods({
 		check(response, Object);
 
 		const command = RocketChat.models.ClientCommands.findOne(commandId);
+		const user = Meteor.user();
+
 		if (!command) {
 			throw new Meteor.Error('error-client-command-not-found', `There is no command with id ${ commandId }`, {
+				method: 'replyClientCommand',
+				field: commandId
+			});
+		}
+
+		if (command.u._id !== user._id) {
+			throw new Meteor.Error('error-client-command-not-allowed', `User ${ user.username } is not the target of command with id ${ commandId }`, {
 				method: 'replyClientCommand',
 				field: commandId
 			});
@@ -16,6 +25,5 @@ Meteor.methods({
 				response
 			}
 		});
-
 	}
 });
