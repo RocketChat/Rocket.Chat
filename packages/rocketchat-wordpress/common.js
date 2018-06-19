094f1cab1857bcc593a7216aad166440d1b41993
Pierre <pierre.lehnen@rocket.chat>
2018-06-19 15:03:55 -0300

    Fixed issue were wordpress oauth configuration was not loaded properly

--- packages/rocketchat-wordpress/common.js
+++ packages/rocketchat-wordpress/common.js
@@ -15,6 +15,9 @@ const WordPress = new CustomOAuth('wordpress', config);
 
 const fillSettings = _.debounce(Meteor.bindEnvironment(() => {
 	config.serverURL = RocketChat.settings.get('API_Wordpress_URL');
+	if (!config.serverURL) {
+		return;
+	}
 
 	delete config.identityPath;
 	delete config.identityTokenSentVia;
@@ -58,22 +61,23 @@ const fillSettings = _.debounce(Meteor.bindEnvironment(() => {
 	}
 
 	const result = WordPress.configure(config);
-
-	const enabled = RocketChat.settings.get('Accounts_OAuth_Wordpress');
-	if (enabled) {
-		ServiceConfiguration.configurations.upsert({
-			service: 'wordpress'
-		}, {
-			$set: config
-		});
-	} else {
-		ServiceConfiguration.configurations.remove({
-			service: 'wordpress'
-		});
+	if (Meteor.isServer) {
+		const enabled = RocketChat.settings.get('Accounts_OAuth_Wordpress');
+		if (enabled) {
+			ServiceConfiguration.configurations.upsert({
+				service: 'wordpress'
+			}, {
+				$set: config
+			});
+		} else {
+			ServiceConfiguration.configurations.remove({
+				service: 'wordpress'
+			});
+		}
 	}
 
 	return result;
-}), 1000);
+}), 500);
 
 if (Meteor.isServer) {
 	Meteor.startup(function() {
