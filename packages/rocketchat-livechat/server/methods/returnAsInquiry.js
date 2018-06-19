b75fe05a5c7ebe0ab698c0be533ab38bd87a6bca
Renato Becker <renato.augusto.becker@gmail.com>
2018-06-19 17:48:11 -0300

    Improved the logic returning the livechat room to the queue.

--- packages/rocketchat-livechat/server/methods/returnAsInquiry.js
+++ packages/rocketchat-livechat/server/methods/returnAsInquiry.js
@@ -1,56 +1,9 @@
 Meteor.methods({
 	'livechat:returnAsInquiry'(rid, departmentId) {
-		const room = RocketChat.models.Rooms.findOneById(rid);
-
-		if (!room) {
-			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'livechat:returnAsInquiry' });
+		if (!Meteor.userId() || !RocketChat.authz.hasPermission(Meteor.userId(), 'view-l-room')) {
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'livechat:saveDepartment' });
 		}
 
-		//When an user goes offline, for example, their user is no longer available
-		//In this case, an error is raised, so to avoid this the room user is instantiated
-		let user = Meteor.user();
-		if (!user) {
-			user = RocketChat.models.Users.findOne(room.servedBy._id);
-		}
-
-		if (!(user && user._id && RocketChat.authz.hasPermission(user._id, 'view-l-room'))) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'livechat:returnAsInquiry' });
-		}
-
-		const agentIds = [];
-		//get the agents of the department
-		if (departmentId) {
-			let agents = RocketChat.Livechat.getOnlineAgents(departmentId);
-
-			if (agents.count() === 0 && RocketChat.settings.get('Livechat_guest_pool_with_no_agents')) {
-				agents = RocketChat.Livechat.getAgents(departmentId);
-			}
-
-			if (agents.count() === 0) {
-				return false;
-			}
-
-			agents.forEach((agent) => {
-				agentIds.push(agent.agentId);
-			});
-		}
-
-		//delete agent and room subscription
-		RocketChat.models.Subscriptions.removeByRoomId(rid);
-
-		// remove user from room
-		const username = user.username;
-
-		RocketChat.models.Rooms.removeUsernameById(rid, username);
-
-		// find inquiry corresponding to room
-		const inquiry = RocketChat.models.LivechatInquiry.findOne({rid});
-
-		// mark inquiry as open
-		if (agentIds.length === 0) {
-			return RocketChat.models.LivechatInquiry.openInquiry(inquiry._id);
-		} else {
-			return RocketChat.models.LivechatInquiry.openInquiryWithAgents(inquiry._id, agentIds);
-		}
+		return RocketChat.Livechat.returnRoomAsInquiry(rid, departmentId);
 	}
 });
