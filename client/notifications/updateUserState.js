0829cc1d5105d36e2381b6523c7a776ba469158b
Happy Tobi <happytobi@tscoding.de>
2018-07-01 23:29:04 +0200

    [NEW] Send status to client

--- client/notifications/updateUserState.js
+++ client/notifications/updateUserState.js
@@ -3,6 +3,6 @@
 /* fire user state change globally, to listen on desktop electron client */ 
 Meteor.startup(function() {
 	RocketChat.callbacks.add('userStatusManuallySet', (status) => {
-		fireGlobalEvent('userStatusManuallySet', status);
+		fireGlobalEvent('user-status-manually-set', status);
 	});
 });
