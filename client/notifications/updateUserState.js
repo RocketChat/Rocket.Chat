f86d0fde5c01c9cf7fbe2521ef2a4dade3d565de
Happy Tobi <happytobi@tscoding.de>
2018-07-02 23:55:00 +0200

    [FIX] linting problem

--- client/notifications/updateUserState.js
+++ client/notifications/updateUserState.js
@@ -1,6 +1,6 @@
 /* globals fireGlobalEvent */
 
-/* fire user state change globally, to listen on desktop electron client */ 
+/* fire user state change globally, to listen on desktop electron client */
 Meteor.startup(function() {
 	RocketChat.callbacks.add('userStatusManuallySet', (status) => {
 		fireGlobalEvent('user-status-manually-set', status);
