3ad7746501420a3ce36d3d6529899fa375236a7f
Mikael Mello <mikaelmmello@gmail.com>
2018-07-16 16:50:07 -0300

    Override function called on client-commands stream onStop
    Done to reset data about the client that could be outdated on a new
    connection

--- packages/rocketchat-client-commands/server/startup.js
+++ packages/rocketchat-client-commands/server/startup.js
@@ -4,3 +4,15 @@ this.commandStream = commandStream;
 commandStream.allowWrite('none');
 
 commandStream.allowRead('all');
+
+/**
+ * Overriding function that is called on the onStop() event of the stream
+ * Anytime the account unsubscribes from the CC stream, we reset its data
+ * because it might connect with another client and if this new client
+ * does not set a new customClientData, the currrent data will be outdated
+ */
+const oldRemoveSubscription = commandStream.removeSubscription;
+commandStream.removeSubscription = function(subscription, eventName) {
+	RocketChat.resetCustomClientData(eventName);
+	oldRemoveSubscription.apply(this, arguments);
+};
