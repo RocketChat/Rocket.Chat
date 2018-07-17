8338718f3963f0feae4a9d20dbff29d9a291d5c0
Tasso Evangelista <tasso@tassoevan.me>
2018-07-17 01:18:29 -0300

    Fix the server-side handling of room_files collection

--- server/publications/roomFiles.js
+++ server/publications/roomFiles.js
@@ -7,15 +7,17 @@ Meteor.publish('roomFiles', function(rid, limit = 50) {
 
 	const cursorFileListHandle = RocketChat.models.Uploads.findNotHiddenFilesOfRoom(rid, limit).observeChanges({
 		added(_id, record) {
-			const {username, name} = record.userId ? RocketChat.models.Users.findOneById(record.userId) : {};
-			return pub.added('room_files', _id, {...record, user:{username, name}});
+			const { username, name } = record.userId ? RocketChat.models.Users.findOneById(record.userId) : {};
+			return pub.added('room_files', _id, { ...record, user: { username, name } });
 		},
-		changed(_id, record) {
-			const {username, name} = record.userId ? RocketChat.models.Users.findOneById(record.userId) : {};
-			return pub.changed('room_files', _id, {...record, user:{username, name}});
+		changed(_id, recordChanges) {
+			if (!recordChanges.hasOwnProperty('user') && recordChanges.userId) {
+				recordChanges.user = RocketChat.models.Users.findOneById(recordChanges.userId);
+			}
+			return pub.changed('room_files', _id, recordChanges);
 		},
-		removed(_id, record) {
-			return pub.removed('room_files', _id, record);
+		removed(_id) {
+			return pub.removed('room_files', _id);
 		}
 	});
 
