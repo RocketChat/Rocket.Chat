0306438ecf0a3cc49966c256d3720ede8bc9c2a0
Tasso Evangelista <tasso@tassoevan.me>
2018-07-18 23:44:58 -0300

    [NEW] Room files search form (#11486)
    * Keep only one PhotoSwipe gallery open
    
    * Refactor PhotoSwipe code
    
    * Handle preloading in PhotoSwipe
    
    * Use common fixCordova helper in uploadFilesList
    
    * Fix log helper
    
    * Fix the server-side handling of room_files collection
    
    * Improvements in uploadedFilesList
    
    * Adjust margins
    
    * Add file name filter for uploaded files
    
    * Add search form for uploaded files
    
    * Add missing translation key
    
    * Fix indentation in template
    
    * Add layout changes
    
    * Fix progressive loading in uploaded files list
    
    * Add rel="noopener noreferrer" to links in uploaded files list
    
    * Rename parameter
    
    * Handle image loading for PhotoShipe gallery
    
    * Show images in uploaded files list in PhotoSwipe gallery
    
    * Add escapeCssUrl helper
    
    * Escape thumbnail URL in uploaded files list
    
    * Force download on uploaded files list
    
    * Add a publication for uploaded files list with search text
    
    * Update roomFiles.js
    
    * Update roomFilesWithSearchText.js

---
+++ server/lib/roomFiles.js
@@ -0,0 +1,27 @@
+export const roomFiles = (pub, { rid, searchText, limit = 50 }) => {
+	if (!pub.userId) {
+		return pub.ready();
+	}
+
+	const cursorFileListHandle = RocketChat.models.Uploads.findNotHiddenFilesOfRoom(rid, searchText, limit).observeChanges({
+		added(_id, record) {
+			const { username, name } = record.userId ? RocketChat.models.Users.findOneById(record.userId) : {};
+			return pub.added('room_files', _id, { ...record, user: { username, name } });
+		},
+		changed(_id, recordChanges) {
+			if (!recordChanges.hasOwnProperty('user') && recordChanges.userId) {
+				recordChanges.user = RocketChat.models.Users.findOneById(recordChanges.userId);
+			}
+			return pub.changed('room_files', _id, recordChanges);
+		},
+		removed(_id) {
+			return pub.removed('room_files', _id);
+		}
+	});
+
+	pub.ready();
+
+	return pub.onStop(function() {
+		return cursorFileListHandle.stop();
+	});
+};
