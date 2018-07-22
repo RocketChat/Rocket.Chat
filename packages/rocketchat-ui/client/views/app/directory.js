2c0141102152975fd2ab0bc8d32d25bb4c3e428f
Hiroki Ishiura <ishiura@ja2.so-net.ne.jp>
2018-07-22 19:28:39 +0900

    Fix links in `onTableItemClick` of the directroy page

--- packages/rocketchat-ui/client/views/app/directory.js
+++ packages/rocketchat-ui/client/views/app/directory.js
@@ -113,7 +113,7 @@ Template.directory.helpers({
 				type = 'd';
 				routeConfig = { name: item.username };
 			}
-			FlowRouter.go(RocketChat.roomTypes.getRouteLink(type, routeConfig));
+		    RocketChat.roomTypes.openRouteLink(type, routeConfig);
 		};
 	},
 	isLoading() {
