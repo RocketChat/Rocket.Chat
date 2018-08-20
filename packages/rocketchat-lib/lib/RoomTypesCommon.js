b47fa2c1eba4b3dd3c5789270921a41dae51db52
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-08-20 13:01:24 -0300

    fix some code style

--- packages/rocketchat-lib/lib/RoomTypesCommon.js
+++ packages/rocketchat-lib/lib/RoomTypesCommon.js
@@ -74,23 +74,6 @@ export class RoomTypesCommon {
 		return FlowRouter.path(this.roomTypes[roomType].route.name, routeData);
 	}
 
-	openRouteLink(roomType, subData, queryParams) {
-		if (!this.roomTypes[roomType]) {
-			return false;
-		}
-
-		let routeData = {};
-		if (this.roomTypes[roomType] && this.roomTypes[roomType].route && this.roomTypes[roomType].route.link) {
-			routeData = this.roomTypes[roomType].route.link(subData);
-		} else if (subData && subData.name) {
-			routeData = {
-				name: subData.name,
-			};
-		}
-
-		return FlowRouter.go(this.roomTypes[roomType].route.name, routeData, queryParams);
-	}
-
 	/**
 	 * @param {string} roomType room type (e.g.: c (for channels), d (for direct channels))
 	 * @param {RoomTypeConfig} roomConfig room's type configuration
@@ -99,21 +82,11 @@ export class RoomTypesCommon {
 		return this.roomTypes[roomType];
 	}
 
-	getURL(roomType, subData) {
-		if (!this.roomTypes[roomType]) {
+	getURL(...args) {
+		const path = this.getRouteLink(...args);
+		if (!path) {
 			return false;
 		}
-
-		let routeData = {};
-		if (this.roomTypes[roomType] && this.roomTypes[roomType].route && this.roomTypes[roomType].route.link) {
-			routeData = this.roomTypes[roomType].route.link(subData);
-		} else if (subData && subData.name) {
-			routeData = {
-				name: subData.name,
-			};
-		}
-
-		const url = FlowRouter.url(this.roomTypes[roomType].route.name, routeData);
-		return url.replace(/([^:])\/{2,}/, '$1/');
+		return Meteor.absoluteUrl(path.replace(/^\//, ''));
 	}
 }
