b3dd7cf845ec571e7a07671cca89ee3da9f147d0
Pierre <pierre.lehnen@rocket.chat>
2018-08-24 12:04:14 -0300

    Working on the WeChat OAuth class

--- packages/rocketchat-wechat/common.js
+++ packages/rocketchat-wechat/common.js
@@ -1,28 +1,91 @@
 /* global CustomOAuth */
+import _ from 'underscore';
+
 const config = {
 	serverURL: '',
 	identityPath: '/',
 	scope: '',
+	buttonColor: '#4fc134',
 	addAutopublishFields: {
+		forLoggedInUser: ['services.wechat'],
+		forOtherUsers: ['services.wechat.nickname']
 	}
 };
 
 const WeChat = new CustomOAuth('wechat', config);
 
+const fillSettings = _.debounce(Meteor.bindEnvironment(() => {
+	config.serverURL = RocketChat.settings.get('Accounts_OAuth_WeChat_URL').trim().replace(/\/*$/, '');
+
+	delete config.identityPath;
+	delete config.identityTokenSentVia;
+	delete config.tokenPath;
+	delete config.tokenSentVia;
+	delete config.authorizePath;
+	delete config.scope;
+	delete config.usernameField;
+	delete config.mergeUsers;
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_identity_path')) {
+		config.identityPath = RocketChat.settings.get('Accounts_OAuth_WeChat_identity_path');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_identity_token_sent_via')) {
+		config.identityTokenSentVia = RocketChat.settings.get('Accounts_OAuth_WeChat_identity_token_sent_via');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_token_path')) {
+		config.tokenPath = RocketChat.settings.get('Accounts_OAuth_WeChat_token_path');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_authorize_path')) {
+		config.authorizePath = RocketChat.settings.get('Accounts_OAuth_WeChat_authorize_path');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_scope')) {
+		config.scope = RocketChat.settings.get('Accounts_OAuth_WeChat_scope');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_token_sent_via')) {
+		config.tokenSentVia = RocketChat.settings.get('Accounts_OAuth_WeChat_token_sent_via');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_username_field')) {
+		config.usernameField = RocketChat.settings.get('Accounts_OAuth_WeChat_username_field');
+	}
+
+	if (RocketChat.settings.get('Accounts_OAuth_WeChat_merge_users')) {
+		config.mergeUsers = RocketChat.settings.get('Accounts_OAuth_WeChat_merge_users');
+	}
+
+	const result = WeChat.configure(config);
+	if (Meteor.isServer) {
+		const enabled = RocketChat.settings.get('Accounts_OAuth_WeChat');
+		if (enabled) {
+			ServiceConfiguration.configurations.upsert({
+				service: 'wechat'
+			}, {
+				$set: config
+			});
+		} else {
+			ServiceConfiguration.configurations.remove({
+				service: 'wechat'
+			});
+		}
+	}
+
+	return result;
+
+}), Meteor.isServer ? 1000 : 100);
+
 if (Meteor.isServer) {
 	Meteor.startup(function() {
-		RocketChat.settings.get('Accounts_OAuth_WeChat_URL', function(key, value) {
-			config.serverURL = value.trim().replace(/\/*$/, '');
-			WeChat.configure(config);
-		});
+		return RocketChat.settings.get(/Accounts\_OAuth\_WeChat\_?/, () => fillSettings());
 	});
 } else {
 	Meteor.startup(function() {
-		Tracker.autorun(function() {
-			if (RocketChat.settings.get('Accounts_OAuth_WeChat_URL')) {
-				config.serverURL = RocketChat.settings.get('Accounts_OAuth_WeChat_URL').trim().replace(/\/*$/, '');
-				WeChat.configure(config);
-			}
+		return Tracker.autorun(function() {
+			return fillSettings();
 		});
 	});
 }
--- packages/rocketchat-wechat/startup.js
+++ packages/rocketchat-wechat/startup.js
@@ -11,7 +11,6 @@ RocketChat.settings.addGroup('OAuth', function() {
 		this.add('Accounts_OAuth_WeChat_URL', '', { type: 'string', enableQuery, public: true, persistent });
 		this.add('Accounts_OAuth_WeChat_id', '', { type: 'string', enableQuery, persistent });
 		this.add('Accounts_OAuth_WeChat_secret', '', { type: 'string', enableQuery });
-		this.add('Accounts_OAuth_WeChat_callback_url', '_oauth/wechat', { type: 'relativeUrl', readonly: true, force: true, enableQuery, persistent });
 
 		this.add('Accounts_OAuth_WeChat_token_path', '/oauth/token', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_token_sent_via', 'payload', { type: 'select', persistent, values: [ { key: 'header', i18nLabel: 'Header' }, { key: 'payload', i18nLabel: 'Payload' } ] });
@@ -19,9 +18,7 @@ RocketChat.settings.addGroup('OAuth', function() {
 		this.add('Accounts_OAuth_WeChat_identity_path', '/me', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_authorize_path', '/oauth/authorize', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_scope', 'openid', { type: 'string', persistent });
-		this.add('Accounts_OAuth_WeChat_button_label_text', 'WeChat', { type: 'string', persistent });
-		this.add('Accounts_OAuth_WeChat_button_label_color', '#FFFFFF', { type: 'string', persistent });
-		this.add('Accounts_OAuth_WeChat_button_color', '#13679A', { type: 'string', persistent });
+
 		this.add('Accounts_OAuth_WeChat_username_field', '', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_merge_users', false, { type: 'boolean', persistent });
 	});
