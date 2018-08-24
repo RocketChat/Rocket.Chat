c0c810600232b1124bdc9bb717c8c2c5d1b755ce
Pierre <pierre.lehnen@rocket.chat>
2018-08-24 14:00:58 -0300

    eslint

--- packages/rocketchat-wechat/startup.js
+++ packages/rocketchat-wechat/startup.js
@@ -2,7 +2,7 @@ RocketChat.settings.addGroup('OAuth', function() {
 	this.section('WeChat', function() {
 		const enableQuery = {
 			_id: 'Accounts_OAuth_WeChat',
-			value: true
+			value: true,
 		};
 
 		const persistent = true;
@@ -13,8 +13,8 @@ RocketChat.settings.addGroup('OAuth', function() {
 		this.add('Accounts_OAuth_WeChat_secret', '', { type: 'string', enableQuery });
 
 		this.add('Accounts_OAuth_WeChat_token_path', '/oauth/token', { type: 'string', persistent });
-		this.add('Accounts_OAuth_WeChat_token_sent_via', 'payload', { type: 'select', persistent, values: [ { key: 'header', i18nLabel: 'Header' }, { key: 'payload', i18nLabel: 'Payload' } ] });
-		this.add('Accounts_OAuth_WeChat_identity_token_sent_via', 'default', { type: 'select', persistent, values: [ { key: 'default', i18nLabel: 'Same_As_Token_Sent_Via' }, { key: 'header', i18nLabel: 'Header' }, { key: 'payload', i18nLabel: 'Payload' } ] });
+		this.add('Accounts_OAuth_WeChat_token_sent_via', 'payload', { type: 'select', persistent, values: [{ key: 'header', i18nLabel: 'Header' }, { key: 'payload', i18nLabel: 'Payload' }] });
+		this.add('Accounts_OAuth_WeChat_identity_token_sent_via', 'default', { type: 'select', persistent, values: [{ key: 'default', i18nLabel: 'Same_As_Token_Sent_Via' }, { key: 'header', i18nLabel: 'Header' }, { key: 'payload', i18nLabel: 'Payload' }] });
 		this.add('Accounts_OAuth_WeChat_identity_path', '/me', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_authorize_path', '/oauth/authorize', { type: 'string', persistent });
 		this.add('Accounts_OAuth_WeChat_scope', 'openid', { type: 'string', persistent });
