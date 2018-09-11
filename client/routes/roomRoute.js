2a6fee2912a39c25b4ad19612efa4b6193af1bd6
Oliver Jägle <github@beimir.net>
2018-09-11 04:48:14 +0200

    Include public channels in published rooms

--- client/routes/roomRoute.js
+++ client/routes/roomRoute.js
@@ -2,5 +2,10 @@ FlowRouter.goToRoomById = (roomId) => {
 	const subscription = ChatSubscription.findOne({rid: roomId});
 	if (subscription) {
 		RocketChat.roomTypes.openRouteLink(subscription.t, subscription, FlowRouter.current().queryParams);
+	} else {
+		const room = ChatRoom.findOne({_id: roomId});
+		if (room) {
+			RocketChat.roomTypes.openRouteLink(room.t, room, FlowRouter.current().queryParams);
+		}
 	}
 };
