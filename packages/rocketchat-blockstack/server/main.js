0568371893b9774895e577d47323e2605bb816c7
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-09-18 14:36:53 -0300

    Update main.js

--- packages/rocketchat-blockstack/server/main.js
+++ packages/rocketchat-blockstack/server/main.js
@@ -1,7 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
 import './routes.js';
 import './settings.js';
 import './loginHandler.js';
-
-Meteor.isDevelopment = (process.env.ROOT_URL.indexOf('localhost') !== -1);
