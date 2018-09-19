f38dd61689db966114f06df25edcaab87fb6ce92
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-09-19 13:56:57 -0300

    Change E2E setting’s group name

--- packages/rocketchat-e2e/server/settings.js
+++ packages/rocketchat-e2e/server/settings.js
@@ -1,6 +1,6 @@
 import { RocketChat } from 'meteor/rocketchat:lib';
 
-RocketChat.settings.addGroup('E2E', function() {
+RocketChat.settings.addGroup('E2E Encryption', function() {
 	this.add('E2E_Enable', true, {
 		type: 'boolean',
 		i18nLabel: 'Enabled',
