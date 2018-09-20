3839c373854c823f2955c29779dc3f5b330d9fad
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-09-20 09:28:03 -0300

    Disable E2E by default

--- packages/rocketchat-e2e/server/settings.js
+++ packages/rocketchat-e2e/server/settings.js
@@ -1,7 +1,7 @@
 import { RocketChat } from 'meteor/rocketchat:lib';
 
 RocketChat.settings.addGroup('E2E Encryption', function() {
-	this.add('E2E_Enable', true, {
+	this.add('E2E_Enable', false, {
 		type: 'boolean',
 		i18nLabel: 'Enabled',
 		public: true,
