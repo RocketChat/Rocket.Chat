8da960e637c457718548acd6821c03e681da1972
Rafael Kellermann Streit <rafaelks@me.com>
2018-09-26 11:08:33 -0300

    Fixed favicon in Safari 12.0

--- packages/rocketchat-ui-master/server/inject.js
+++ packages/rocketchat-ui-master/server/inject.js
@@ -58,6 +58,7 @@ if (process.env.DISABLE_ANIMATION || process.env.TEST_MODE === 'true') {
 
 RocketChat.settings.get('Assets_SvgFavicon_Enable', (key, value) => {
 	const standardFavicons = `
+		<link rel="mask-icon" href="assets/safari_pinned.svg" color="#04436a">
 		<link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
 		<link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />`;
 
