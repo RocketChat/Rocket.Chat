1f5c1eebac13d85eb376fdf4b740aee7f457b231
Roman Zharikov <roman.zharikov@oe-solution.net>
2018-10-01 12:20:02 +0200

    ROS-4 revert videoFlexTab for code style

--- packages/rocketchat-videobridge/client/views/videoFlexTab.js
+++ packages/rocketchat-videobridge/client/views/videoFlexTab.js
@@ -11,11 +11,11 @@ Template.videoFlexTab.helpers({
     },
 });
 
-Template.videoFlexTab.onCreated(function () {
+Template.videoFlexTab.onCreated(function() {
     this.tabBar = Template.currentData().tabBar;
 });
 
-Template.videoFlexTab.onRendered(function () {
+Template.videoFlexTab.onRendered(function() {
     this.api = null;
 
     let timeOut = null;
@@ -37,7 +37,7 @@ Template.videoFlexTab.onRendered(function () {
 
         this.tabBar.close();
 
-        RocketChat.TabBar.updateButton('video', {class: ''});
+        RocketChat.TabBar.updateButton('video', { class: '' });
     };
 
     modal.open({
@@ -59,15 +59,8 @@ Template.videoFlexTab.onRendered(function () {
                     const roomId = Session.get('openedRoom');
 
                     const domain = RocketChat.settings.get('Jitsi_Domain');
-                    const uniqueID = RocketChat.settings.get('uniqueID');
+                    const jitsiRoom = RocketChat.settings.get('Jitsi_URL_Room_Prefix') + RocketChat.settings.get('uniqueID') + roomId;
                     const noSsl = !RocketChat.settings.get('Jitsi_SSL');
-
-                    var jitsiRoom = '';
-                    if (typeof uniqueID !== 'undefined') {
-                        jitsiRoom = RocketChat.settings.get('Jitsi_URL_Room_Prefix') + uniqueID + roomId;
-                    } else {
-                        jitsiRoom = RocketChat.settings.get('Jitsi_URL_Room_Prefix') + roomId;
-                    }
 
                     if (jitsiRoomActive !== null && jitsiRoomActive !== jitsiRoom) {
                         jitsiRoomActive = null;
@@ -80,80 +73,61 @@ Template.videoFlexTab.onRendered(function () {
                             clearInterval(timeOut);
                         }
                     } else {
-
-                        console.log('Init conference...');
-
                         jitsiRoomActive = jitsiRoom;
 
-                        RocketChat.TabBar.updateButton('video', {class: 'red'});
+                        RocketChat.TabBar.updateButton('video', { class: 'red' });
 
-                        var api = this.api;
-                        Meteor.call('jitsi:generateAccessToken', function (error, accessToken) {
-                            if (error) {
-                                console.log('ERROR - jitsi:generateAccessToken');
+                        if (RocketChat.settings.get('Jitsi_Open_New_Window') || Meteor.isCordova) {
+                            Meteor.call('jitsi:updateTimeout', roomId);
+
+                            timeOut = Meteor.setInterval(() => Meteor.call('jitsi:updateTimeout', roomId), 10 * 1000);
+                            let newWindow = null;
+                            if (Meteor.isCordova) {
+                                newWindow = window.open(`${ (noSsl ? 'http://' : 'https://') + domain }/${ jitsiRoom }`, '_system');
+                                closePanel();
+                                clearInterval(timeOut);
                             } else {
-
-                                // we need to token authentication for our domain - travelmeet.de
-                                console.log('Your personal access token: ' + accessToken);
-
-                                if (RocketChat.settings.get('Jitsi_Open_New_Window') || Meteor.isCordova) {
-
-                                    console.log('Opening a conference in a NEW tab');
-
-                                    Meteor.call('jitsi:updateTimeout', roomId);
-
-                                    timeOut = Meteor.setInterval(() => Meteor.call('jitsi:updateTimeout', roomId), 10 * 1000);
-                                    let newWindow = null;
-                                    if (Meteor.isCordova) {
-                                        newWindow = window.open(`${ (noSsl ? 'http://' : 'https://') + domain }/${ jitsiRoom }?jwt=${ accessToken }`, '_system');
+                                newWindow = window.open(`${ (noSsl ? 'http://' : 'https://') + domain }/${ jitsiRoom }`, jitsiRoom);
+                                const closeInterval = setInterval(() => {
+                                    if (newWindow.closed !== false) {
                                         closePanel();
+                                        clearInterval(closeInterval);
                                         clearInterval(timeOut);
-                                    } else {
-                                        newWindow = window.open(`${ (noSsl ? 'http://' : 'https://') + domain }/${ jitsiRoom }?jwt=${ accessToken }`, jitsiRoom);
-                                        const closeInterval = setInterval(() => {
-                                            if (newWindow.closed !== false) {
-                                                closePanel();
-                                                clearInterval(closeInterval);
-                                                clearInterval(timeOut);
-                                            }
-                                        }, 300);
                                     }
-                                    if (newWindow) {
-                                        newWindow.focus();
-                                    }
-
-                                    // Lets make sure its loaded before we try to show it.
-                                } else if (typeof JitsiMeetExternalAPI !== 'undefined') {
-
-                                    // Keep it from showing duplicates when re-evaluated on variable change.
-                                    if (!$('[id^=jitsiConference]').length) {
-
-                                        console.log('Opening a conference in THIS tab');
-
-                                        api = new JitsiMeetExternalAPI(domain, jitsiRoom, width, height, this.$('.video-container').get(0), configOverwrite, interfaceConfigOverwrite, noSsl, accessToken);
-
-                                        /*
-                                        * Hack to send after frame is loaded.
-                                        * postMessage converts to events in the jitsi meet iframe.
-                                        * For some reason those aren't working right.
-                                        */
-                                        Meteor.setTimeout(() => {
-                                            api.executeCommand('displayName', [Meteor.user().name]);
-                                        }, 5000);
-
-                                        Meteor.call('jitsi:updateTimeout', roomId);
-
-                                        timeOut = Meteor.setInterval(() => Meteor.call('jitsi:updateTimeout', roomId), 10 * 1000);
-                                    }
-
-                                    // Execute any commands that might be reactive.  Like name changing.
-                                    api && api.executeCommand('displayName', [Meteor.user().name]);
-                                }
+                                }, 300);
                             }
-                        });
+                            if (newWindow) {
+                                newWindow.focus();
+                            }
+
+
+                            // Lets make sure its loaded before we try to show it.
+                        } else if (typeof JitsiMeetExternalAPI !== 'undefined') {
+
+                            // Keep it from showing duplicates when re-evaluated on variable change.
+                            if (!$('[id^=jitsiConference]').length) {
+                                this.api = new JitsiMeetExternalAPI(domain, jitsiRoom, width, height, this.$('.video-container').get(0), configOverwrite, interfaceConfigOverwrite, noSsl);
+
+                                /*
+                                * Hack to send after frame is loaded.
+                                * postMessage converts to events in the jitsi meet iframe.
+                                * For some reason those aren't working right.
+                                */
+                                Meteor.setTimeout(() => {
+                                    this.api.executeCommand('displayName', [Meteor.user().name]);
+                                }, 5000);
+
+                                Meteor.call('jitsi:updateTimeout', roomId);
+
+                                timeOut = Meteor.setInterval(() => Meteor.call('jitsi:updateTimeout', roomId), 10 * 1000);
+                            }
+
+                            // Execute any commands that might be reactive.  Like name changing.
+                            this.api && this.api.executeCommand('displayName', [Meteor.user().name]);
+                        }
                     }
                 } else {
-                    RocketChat.TabBar.updateButton('video', {class: ''});
+                    RocketChat.TabBar.updateButton('video', { class: '' });
 
                     // Clean up and stop updating timeout.
                     if (timeOut) {
