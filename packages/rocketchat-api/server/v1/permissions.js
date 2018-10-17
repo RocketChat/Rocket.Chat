598fca3a50c2c4759666c20bbf8e916142f41ff1
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-10-17 17:18:53 -0300

    removing outdated comment

--- packages/rocketchat-api/server/v1/permissions.js
+++ packages/rocketchat-api/server/v1/permissions.js
@@ -1,10 +1,3 @@
-/**
-	This API returns all permissions that exists
-	on the server, with respective roles.
-
-	Method: GET
-	Route: api/v1/permissions
- */
 RocketChat.API.v1.addRoute('permissions.list', { authRequired: true }, {
 	get() {
 		const result = Meteor.runAsUser(this.userId, () => Meteor.call('permissions/get'));
