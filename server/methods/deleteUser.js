4fb5857a1ae8599a19334a55bc97b1bf72937e37
Tim Kinnane <timkinnane@gmail.com>
2018-10-20 13:33:06 +1100

    Update deleteUser errors to be more semantic
    Two different errors with the same message make it hard to know what's going wrong.
    Errors should be more descriptive for log/debug to describe if the problem is with the user doing the deleting, or the user you're trying to delete.

--- server/methods/deleteUser.js
+++ server/methods/deleteUser.js
@@ -3,7 +3,7 @@ Meteor.methods({
 		check(userId, String);
 
 		if (!Meteor.userId()) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
+			throw new Meteor.Error('error-invalid-user', 'Invalid current user', {
 				method: 'deleteUser',
 			});
 		}
@@ -16,7 +16,7 @@ Meteor.methods({
 
 		const user = RocketChat.models.Users.findOneById(userId);
 		if (!user) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user to delete', {
 				method: 'deleteUser',
 			});
 		}
