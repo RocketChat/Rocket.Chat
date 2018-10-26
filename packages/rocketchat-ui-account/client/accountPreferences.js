e3b4dc805c5ea0aafa851831332ab23a77792b15
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-10-26 10:20:34 -0300

    Remove global variable Reload

--- packages/rocketchat-ui-account/client/accountPreferences.js
+++ packages/rocketchat-ui-account/client/accountPreferences.js
@@ -2,6 +2,7 @@
 import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Tracker } from 'meteor/tracker';
+import { Reload } from 'meteor/reload';
 import _ from 'underscore';
 import s from 'underscore.string';
 import toastr from 'toastr';
