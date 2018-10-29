6388268d1ebf20e52dd7b75374835dc74ced78f9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-10-29 15:39:44 -0300

    [FIX] Manage own integrations permissions check (#12397)
    …ements on permissions
    
    <!-- INSTRUCTION: Your Pull Request name should start with one of the following tags -->
    <!-- [NEW] For new features -->
    <!-- [FIX] For bug fixes -->
    <!-- [BREAK] For pull requests including breaking changes -->
    
    <!-- INSTRUCTION: Inform the issue number that this PR closes, or remove the line below -->
    
    
    <!-- INSTRUCTION: Link to a https://github.com/RocketChat/docs PR with added/updated documentation or an update to the missing/outdated documentation list, see https://rocket.chat/docs/contributing/documentation/  -->
    
    <!-- INSTRUCTION: Tell us more about your PR with screen shots if you can -->

--- packages/rocketchat-lib/lib/roomTypes/direct.js
+++ packages/rocketchat-lib/lib/roomTypes/direct.js
@@ -29,7 +29,7 @@ export class DirectMessageRoomType extends RoomTypeConfig {
 	}
 
 	findRoom(identifier) {
-		if (!RocketChat.authz.hasAtLeastOnePermission('view-d-room')) {
+		if (!RocketChat.authz.hasPermission('view-d-room')) {
 			return null;
 		}
 
