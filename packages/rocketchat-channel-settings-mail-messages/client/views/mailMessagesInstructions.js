59207260707c15d3f4e8345637c39b7d416cc3a8
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2018-11-06 14:36:32 -0200

    Convert rocketchat-channel-settings-mail-messages to main module structure (#12537)

--- packages/rocketchat-channel-settings-mail-messages/client/views/mailMessagesInstructions.js
+++ packages/rocketchat-channel-settings-mail-messages/client/views/mailMessagesInstructions.js
@@ -1,10 +1,12 @@
-/* global Deps */
 import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Blaze } from 'meteor/blaze';
 import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 import { AutoComplete } from 'meteor/mizzao:autocomplete';
+import { RocketChat, handleError } from 'meteor/rocketchat:lib';
+import { t, ChatRoom } from 'meteor/rocketchat:ui';
+import { Deps } from 'meteor/deps';
 import toastr from 'toastr';
 import resetSelection from '../resetSelection';
 
