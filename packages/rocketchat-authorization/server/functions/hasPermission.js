7f36759cc8194dd3a1346d951199b46b8ae7ccad
Diego Sampaio <chinello@gmail.com>
2018-11-06 11:50:53 -0200

    Cache permission find

--- packages/rocketchat-authorization/server/functions/hasPermission.js
+++ packages/rocketchat-authorization/server/functions/hasPermission.js
@@ -1,15 +1,18 @@
 import memoize from 'mem';
-const CACHE_TTL = process.env.TEST_MODE ? 0 : 1000;
+const CACHE_TTL = { maxAge: process.env.TEST_MODE ? 0 : 1000 };
+
+const findPermission = memoize((permissionId) => RocketChat.models.Permissions.findOne(permissionId), CACHE_TTL);
+
 function atLeastOne(userId, permissions = [], scope) {
 	return permissions.some((permissionId) => {
-		const permission = RocketChat.models.Permissions.findOne(permissionId);
+		const permission = findPermission(permissionId);
 		return RocketChat.models.Roles.isUserInRoles(userId, permission.roles, scope);
 	});
 }
 
 function all(userId, permissions = [], scope) {
 	return permissions.every((permissionId) => {
-		const permission = RocketChat.models.Permissions.findOne(permissionId);
+		const permission = findPermission(permissionId);
 		return RocketChat.models.Roles.isUserInRoles(userId, permission.roles, scope);
 	});
 }
@@ -23,16 +26,16 @@ function hasPermission(userId, permissions, scope, strategy) {
 
 RocketChat.authz.hasAllPermission = memoize(function(userId, permissions, scope) {
 	return hasPermission(userId, permissions, scope, all);
-}, { maxAge: CACHE_TTL });
+}, CACHE_TTL);
 
 RocketChat.authz.hasPermission = memoize(function(userId, permissionId, scope) {
 	if (!userId) {
 		return false;
 	}
-	const permission = RocketChat.models.Permissions.findOne(permissionId);
+	const permission = findPermission(permissionId);
 	return RocketChat.models.Roles.isUserInRoles(userId, permission.roles, scope);
-}, { maxAge: CACHE_TTL });
+}, CACHE_TTL);
 
 RocketChat.authz.hasAtLeastOnePermission = memoize(function(userId, permissions, scope) {
 	return hasPermission(userId, permissions, scope, atLeastOne);
-}, { maxAge: CACHE_TTL });
+}, CACHE_TTL);
