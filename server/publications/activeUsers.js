95836b4e0b15e0f4c2c7baf6eadf7d6b33f001d3
Diego Sampaio <chinello@gmail.com>
2018-11-06 17:59:19 -0200

    Send active users via stream

--- server/publications/activeUsers.js
+++ server/publications/activeUsers.js
@@ -1,16 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 
+const findActiveUsers = () => RocketChat.models.Users.findUsersNotOffline({
+	fields: {
+		username: 1,
+		name: 1,
+		status: 1,
+		utcOffset: 1,
+	},
+});
+
 Meteor.publish('activeUsers', function() {
 	if (!this.userId) {
 		return this.ready();
 	}
 
-	return RocketChat.models.Users.findUsersNotOffline({
-		fields: {
-			username: 1,
-			name: 1,
-			status: 1,
-			utcOffset: 1,
-		},
-	});
+	return findActiveUsers();
+});
+
+Meteor.methods({
+	getActiveUsers() {
+		if (!this.userId) {
+			return this.ready();
+		}
+
+		const users = findActiveUsers().fetch();
+
+		return { users };
+	},
 });
