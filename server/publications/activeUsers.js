3cb073fb60e6c9f60fc40211071b0cbc971daa14
Diego Sampaio <chinello@gmail.com>
2018-11-06 21:51:35 -0200

    Change to REST

--- server/publications/activeUsers.js
+++ server/publications/activeUsers.js
@@ -1,30 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
-const findActiveUsers = () => RocketChat.models.Users.findUsersNotOffline({
-	fields: {
-		username: 1,
-		name: 1,
-		status: 1,
-		utcOffset: 1,
-	},
-});
-
 Meteor.publish('activeUsers', function() {
 	if (!this.userId) {
 		return this.ready();
 	}
 
-	return findActiveUsers();
-});
-
-Meteor.methods({
-	getActiveUsers() {
-		if (!this.userId) {
-			return this.ready();
-		}
-
-		const users = findActiveUsers().fetch();
-
-		return { users };
-	},
+	return RocketChat.models.Users.findUsersNotOffline({
+		fields: {
+			username: 1,
+			name: 1,
+			status: 1,
+			utcOffset: 1,
+		},
+	});
 });
