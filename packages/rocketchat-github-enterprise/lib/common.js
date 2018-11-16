9a2f71eb65aafbb4c63861fd57f901c34ff39741
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-11-16 13:38:53 -0200

    Convert rocketchat-github-enterprise to main module structure

--- packages/rocketchat-github-enterprise/lib/common.js
+++ packages/rocketchat-github-enterprise/lib/common.js
@@ -1,6 +1,8 @@
-/* global CustomOAuth */
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
+import { CustomOAuth } from 'meteor/rocketchat:custom-oauth';
+import { RocketChat } from 'meteor/rocketchat:lib';
+console.log(CustomOAuth)
 
 // GitHub Enterprise Server CallBack URL needs to be http(s)://{rocketchat.server}[:port]/_oauth/github_enterprise
 // In RocketChat -> Administration the URL needs to be http(s)://{github.enterprise.server}/
