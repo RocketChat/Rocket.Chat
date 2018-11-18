3b92c1d52f3df31a83814a4b65df62be641e2b5e
Alan Sikora <alansikora@gmail.com>
2018-11-18 10:15:59 -0200

    fixing /invite command

--- packages/rocketchat-slashcommands-invite/server/server.js
+++ packages/rocketchat-slashcommands-invite/server/server.js
@@ -15,7 +15,7 @@ function Invite(command, params, item) {
 	if (command !== 'invite' || !Match.test(params, String)) {
 		return;
 	}
-	const usernames = params.replace(/@/g, '').split(/[\s,]/).filter((a) => a !== '');
+	const usernames = params.replace(/(^@)|( @)/, '').split(/[\s,]/).filter((a) => a !== '');
 	if (usernames.length === 0) {
 		return;
 	}
