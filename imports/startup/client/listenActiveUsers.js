b5d6c83b168b95ba234a4fadf5997cb66e1df1c8
Diego Sampaio <chinello@gmail.com>
2018-11-23 21:59:19 -0200

    Retry if users.actives request fails

--- imports/startup/client/listenActiveUsers.js
+++ imports/startup/client/listenActiveUsers.js
@@ -25,16 +25,22 @@ const updateUser = (user, force = false) => {
 	Meteor.users._collection.insert(user);
 };
 
-Tracker.autorun(() => {
-	if (!Meteor.userId()) {
-		return;
-	}
+let retry = 0;
+function getActiveUsers() {
 	RocketChat.API.v1.get('users.actives')
 		.then((result) => {
 			result.users.forEach((user) => {
 				updateUser(user);
 			});
-		});
+		})
+		.catch(() => setTimeout(getActiveUsers, retry++ * 2000));
+}
+
+Tracker.autorun(() => {
+	if (!Meteor.userId()) {
+		return;
+	}
+	getActiveUsers();
 });
 
 RocketChat.Notifications.onLogged('user-status', (user) => {
