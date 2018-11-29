ec5ae7b5b8d39df9d3c821024a63e41f9e9278ba
Diego Sampaio <chinello@gmail.com>
2018-11-29 00:07:48 -0200

    Fix set status

--- imports/presence/server/src/actions/index.js
+++ imports/presence/server/src/actions/index.js
@@ -1,9 +1,12 @@
 import { newConnection } from './newConnection';
 import { removeConnection } from './removeConnection';
 import { removeLostConnections } from './removeLostConnections';
+import { setStatus, setConnectionStatus } from './setStatus';
 
 export default {
 	newConnection,
 	removeConnection,
 	removeLostConnections,
+	setStatus,
+	setConnectionStatus,
 };
