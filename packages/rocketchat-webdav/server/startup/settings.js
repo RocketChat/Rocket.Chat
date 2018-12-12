054de3e37271e53e2bb129aaefeb457f57a94740
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2018-12-12 17:30:51 -0200

    Convert rocketchat-webdav to main module structure (#12886)
    * Convert rocketchat-webdav to main module structure
    
    * Fix Handlebars import

--- packages/rocketchat-webdav/server/startup/settings.js
+++ packages/rocketchat-webdav/server/startup/settings.js
@@ -1,3 +1,5 @@
+import { RocketChat } from 'meteor/rocketchat:lib';
+
 RocketChat.settings.addGroup('Webdav Integration', function() {
 	this.add('Webdav_Integration_Enabled', false, {
 		type: 'boolean',
