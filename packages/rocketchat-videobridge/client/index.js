b132601c0b09cf17afe7e83ce388ebf6c2825343
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-12-12 14:26:08 -0200

    Remove duplicated import

--- packages/rocketchat-videobridge/client/index.js
+++ packages/rocketchat-videobridge/client/index.js
@@ -2,7 +2,6 @@ import { JitsiMeetExternalAPI } from './public/external_api';
 import './views/videoFlexTab.html';
 import './views/bbbLiveView.html';
 import './views/videoFlexTabBbb.html';
-import './views/videoFlexTabBbb.html';
 import './views/videoFlexTab';
 import './views/videoFlexTabBbb';
 import './tabBar';
