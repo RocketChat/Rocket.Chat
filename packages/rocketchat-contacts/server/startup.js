af17d867a508cfade4ff804ba9bc3e57dc081d3d
Karan Bedi <karanbedi0304@gmail.com>
2018-12-15 20:55:43 +0530

    Add description for sync interval

--- packages/rocketchat-contacts/server/startup.js
+++ packages/rocketchat-contacts/server/startup.js
@@ -8,5 +8,6 @@ RocketChat.settings.addGroup('Contacts', function() {
 	this.add('Contacts_Background_Sync_Interval', 10, {
 		type: 'int',
 		public: true,
+		i18nDescription: 'Contacts_Background_Sync_Interval_Description',
 	});
 });
