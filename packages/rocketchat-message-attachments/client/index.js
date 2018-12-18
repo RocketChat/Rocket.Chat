efcad4b0d9f842b3b07f0dbe4921f0e4d3085d67
Oliver Jägle <github@beimir.net>
2018-12-18 19:58:26 +0100

    Infrastructure for custom renderings of attachment fields

--- packages/rocketchat-message-attachments/client/index.js
+++ packages/rocketchat-message-attachments/client/index.js
@@ -1,2 +1,10 @@
 import './messageAttachment.html';
 import './messageAttachment';
+import './renderField.html';
+import './renderField';
+
+import { registerFieldTemplate } from './renderField';
+
+export {
+	registerFieldTemplate,
+};
