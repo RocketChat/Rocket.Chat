f5bc51531844faca85f1114760c826371093b39d
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-12-20 17:48:27 -0200

    [IMPROVE] Accept Slash Commands via Action Buttons when `msg_in_chat_window: true`

--- packages/rocketchat-ui/client/lib/chatMessages.js
+++ packages/rocketchat-ui/client/lib/chatMessages.js
@@ -259,43 +259,8 @@ ChatMessages = class ChatMessages {
 			this.hasValue.set(false);
 			this.stopTyping(rid);
 
-			// Check if message starts with /command
-			if (msg[0] === '/') {
-				const match = msg.match(/^\/([^\s]+)(?:\s+(.*))?$/m);
-				if (match) {
-					let command;
-					if (RocketChat.slashCommands.commands[match[1]]) {
-						const commandOptions = RocketChat.slashCommands.commands[match[1]];
-						command = match[1];
-						const param = match[2] || '';
-
-						if (!commandOptions.permission || RocketChat.authz.hasAtLeastOnePermission(commandOptions.permission, Session.get('openedRoom'))) {
-							if (commandOptions.clientOnly) {
-								commandOptions.callback(command, param, msgObject);
-							} else {
-								Meteor.call('slashCommand', { cmd: command, params: param, msg: msgObject }, (err, result) => typeof commandOptions.result === 'function' && commandOptions.result(err, result, { cmd: command, params: param, msg: msgObject }));
-							}
-
-							return;
-						}
-					}
-
-					if (!RocketChat.settings.get('Message_AllowUnrecognizedSlashCommand')) {
-						const invalidCommandMsg = {
-							_id: Random.id(),
-							rid,
-							ts: new Date,
-							msg: TAPi18n.__('No_such_command', { command: match[1] }),
-							u: {
-								username: RocketChat.settings.get('InternalHubot_Username'),
-							},
-							private: true,
-						};
-
-						ChatMessage.upsert({ _id: invalidCommandMsg._id }, invalidCommandMsg);
-						return;
-					}
-				}
+			if (this.processSlashCommand(msgObject)) {
+				return;
 			}
 
 			Meteor.call('sendMessage', msgObject);
@@ -317,6 +282,49 @@ ChatMessages = class ChatMessages {
 
 			return this.confirmDeleteMsg(message, done);
 		}
+	}
+
+	processSlashCommand(msgObject) {
+		// Check if message starts with /command
+		if (msgObject.msg[0] === '/') {
+			const match = msgObject.msg.match(/^\/([^\s]+)(?:\s+(.*))?$/m);
+			if (match) {
+				let command;
+				if (RocketChat.slashCommands.commands[match[1]]) {
+					const commandOptions = RocketChat.slashCommands.commands[match[1]];
+					command = match[1];
+					const param = match[2] || '';
+
+					if (!commandOptions.permission || RocketChat.authz.hasAtLeastOnePermission(commandOptions.permission, Session.get('openedRoom'))) {
+						if (commandOptions.clientOnly) {
+							commandOptions.callback(command, param, msgObject);
+						} else {
+							Meteor.call('slashCommand', { cmd: command, params: param, msg: msgObject }, (err, result) => typeof commandOptions.result === 'function' && commandOptions.result(err, result, { cmd: command, params: param, msg: msgObject }));
+						}
+
+						return true;
+					}
+				}
+
+				if (!RocketChat.settings.get('Message_AllowUnrecognizedSlashCommand')) {
+					const invalidCommandMsg = {
+						_id: Random.id(),
+						rid: msgObject.rid,
+						ts: new Date,
+						msg: TAPi18n.__('No_such_command', { command: match[1] }),
+						u: {
+							username: RocketChat.settings.get('InternalHubot_Username'),
+						},
+						private: true,
+					};
+
+					ChatMessage.upsert({ _id: invalidCommandMsg._id }, invalidCommandMsg);
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	confirmDeleteMsg(message, done = function() {}) {
