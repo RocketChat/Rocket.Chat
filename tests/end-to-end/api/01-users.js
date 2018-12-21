25c3a62861aa79c888f100badc63fc108c990cba
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2018-12-20 22:31:34 -0200

    [FIX] Pin and unpin message were not checking permissions (#12739)
    * Fix pin and unpin message without permissions
    
    * Update 05-chat.js
    
    * Fix lint

--- tests/end-to-end/api/01-users.js
+++ tests/end-to-end/api/01-users.js
@@ -12,30 +12,7 @@ import {
 import { adminEmail, preferences, password, adminUsername } from '../../data/user.js';
 import { imgURL } from '../../data/interactions.js';
 import { customFieldText, clearCustomFields, setCustomFields } from '../../data/custom-fields.js';
-
-const updatePermission = (permission, roles) => new Promise((resolve) => {
-	request.post(api('permissions.update'))
-		.set(credentials)
-		.send({ permissions: [{ _id: permission, roles }] })
-		.expect('Content-Type', 'application/json')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body).to.have.property('success', true);
-		})
-		.end(resolve);
-});
-
-const updateSetting = (setting, value) => new Promise((resolve) => {
-	request.post(`/api/v1/settings/${ setting }`)
-		.set(credentials)
-		.send({ value })
-		.expect('Content-Type', 'application/json')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body).to.have.property('success', true);
-		})
-		.end(resolve);
-});
+import { updatePermission, updateSetting } from '../../data/permissions.helper';
 
 describe('[Users]', function() {
 	this.retries(0);
