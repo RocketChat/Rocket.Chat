b55d026a83ec0c7ecfc8035679cc758eb18985e7
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2018-12-27 11:37:35 -0200

    Move statistics Model to rocketchat:models

--- packages/rocketchat-statistics/server/models/Statistics.js
+++ packages/rocketchat-statistics/server/models/Statistics.js
@@ -1,26 +1,4 @@
 import { RocketChat } from 'meteor/rocketchat:lib';
+import { Statistics } from 'meteor/rocketchat:models';
 
-RocketChat.models.Statistics = new class extends RocketChat.models._Base {
-	constructor() {
-		super('statistics');
-
-		this.tryEnsureIndex({ createdAt: 1 });
-	}
-
-	// FIND ONE
-	findOneById(_id, options) {
-		const query = { _id };
-		return this.findOne(query, options);
-	}
-
-	findLast() {
-		const options = {
-			sort: {
-				createdAt: -1,
-			},
-			limit: 1,
-		};
-		const records = this.find({}, options).fetch();
-		return records && records[0];
-	}
-};
+RocketChat.models.Statistics = Statistics;
