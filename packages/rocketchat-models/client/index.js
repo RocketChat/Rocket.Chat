42c1ac9e17b94b35fa37353d1f86399face6c30b
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-01-02 22:04:27 -0200

    Move CachedCollection from lib to models

--- packages/rocketchat-models/client/index.js
+++ packages/rocketchat-models/client/index.js
@@ -2,10 +2,13 @@ import { Base } from './models/_Base';
 import Avatars from './models/Avatars';
 import Uploads from './models/Uploads';
 import UserDataFiles from './models/UserDataFiles';
+import { CachedCollection, CachedCollectionManager } from './models/CachedCollection';
 
 export {
 	Base,
 	Avatars,
 	Uploads,
 	UserDataFiles,
+	CachedCollection,
+	CachedCollectionManager,
 };
