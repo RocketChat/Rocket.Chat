cfcc1136cf6a1159c0324b5ee38ee9b7d10efb9f
Oliver Jägle <github@beimir.net>
2019-01-05 23:35:36 +0100

    Slash command /thread

---
+++ packages/rocketchat-slashcommands-create-thread/server/server.js
@@ -0,0 +1,14 @@
+function CreateThread(command, params, item) {
+	if (command !== 'thread' || !Match.test(params, String)) {
+		return;
+	}
+
+	const openingMessage = params.trim();
+
+	Meteor.call('createThread', item.rid, { msg: openingMessage });
+}
+
+RocketChat.slashCommands.add('thread', CreateThread, {
+	description: 'Thread_slash_command_description',
+	params: 'Thread_slash_command_params',
+});
