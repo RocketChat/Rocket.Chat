5815c5d1dd6f029fd053f40840a9a2f153a3270a
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-01-07 10:58:58 -0200

    Filter users to emit events, only livechat manager or agent

--- packages/rocketchat-livechat/server/agentStatus.js
+++ packages/rocketchat-livechat/server/agentStatus.js
@@ -2,5 +2,7 @@ import { RocketChat } from 'meteor/rocketchat:lib';
 import { UserPresenceMonitor } from 'meteor/konecty:user-presence';
 
 UserPresenceMonitor.onSetUserStatus((user, status) => {
-	RocketChat.Livechat.notifyAgentStatusChanged(user._id, status);
+	if (RocketChat.authz.hasRole(user._id, 'livechat-manager') || RocketChat.authz.hasRole(user._id, 'livechat-agent')) {
+		RocketChat.Livechat.notifyAgentStatusChanged(user._id, status);
+	}
 });
