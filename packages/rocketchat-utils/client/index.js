0b45f4c3a51d63af0a08d15f7b57e49fe6cc4dc2
Douglas Gubert <douglas.gubert@gmail.com>
2019-01-11 13:16:53 -0200

    Improve the way the app detail screen shows support emails

--- packages/rocketchat-utils/client/index.js
+++ packages/rocketchat-utils/client/index.js
@@ -2,6 +2,7 @@ import { t, isRtl } from '../lib/tapi18n';
 import { isChrome, isFirefox } from './lib/browsers';
 import { getDefaultSubscriptionPref } from '../lib/getDefaultSubscriptionPref';
 import { Info } from '../rocketchat.info';
+import { isEmail } from '../lib/isEmail';
 
 export {
 	t,
@@ -10,4 +11,5 @@ export {
 	isFirefox,
 	getDefaultSubscriptionPref,
 	Info,
+	isEmail,
 };
