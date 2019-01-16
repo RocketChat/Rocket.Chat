8b68b3bc8cab74253556a30ac9b48703e6563a5d
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2019-01-16 14:54:01 -0200

    Remove dependency of RocketChat namespace and push-notifications (#13137)
    * Move rocketchat settings to specific package
    
    * WIP: Move models from rocketchat-lib to a specific package (server)
    
    * Move function from rocketchat:lib to rocketchat:utils to use it in rocketchat:models
    
    * Move client models from rocketchat:lib to rocketchat:models
    
    * Fix lint
    
    * Move rocketchat.info from lib to utils
    
    * Remove directly dependency between lib and migrations
    
    * Move statistics Model to rocketchat:models
    
    * Create rocketchat:metrics to be able to depacking rocketchat callbacks
    
    * Move  callbacks to specific package
    
    * Remove unused dependency
    
    * Move rocketchat-notifications to a specific package
    
    * Move rocketchat-promises to a specific package
    
    * remove directly dependency from metrics and models
    
    * Move CachedCollection from lib to models
    
    * Move ui models/collections from ui to models
    
    * Move authorization client/ui models to rocketchat:models to be able to remove lib dependency
    
    * Creation of rocketchat:ui-utils to help decouple rocketchat:lib and rocketchat:authz
    
    * Move some common functions to rocketchat:utils
    
    * Change imports to dynamic imports to avoid directly dependency between some packages
    
    * Move authz models to rocketchat:models
    
    * Remove directly dependency between rocketchat:authz and rocketchat:lib
    
    * Move some functions from rocketchat:lib to rocketchat:utils
    
    * Add functions to settings package
    
    * Convert rocketchat:file-upload to main module structure
    
    * Import FileUpload where it is being used
    
    * Remove FileUpload and fileUploadHandler from globals eslintrc
    
    * Move some  functions to rocketchat:ui-utils
    
    * Remove directly dependency between rocketchat:authorization and rocketchat:ui-utils
    
    * Remove dependency between lazy-load and lib
    
    * Change imports of renderMessageBody from ui-message to ui-utils
    
    * Add import of main ready from ui-utils
    
    * Convert rocketchat-ui-sidenav to main module structure
    
    * Add imports of toolbarSearch from ui-sidenav
    
    * Remove toolbarSearch from eslintrc globals
    
    * Move CachedCollection to a specific package
    
    * Change imports of CachedCollection to new package
    
    * Move some functions to rocketchat:ui-utils
    
    * Remove directly dependency between tooltip and lib
    
    *  Remove directly dependency between settings and metrics
    
    * Move some settings client function from lib to settings
    
    * Convert rocketchat-ui-master to main module structure
    
    * Remove directly dependency between rocketchat:e2e and rocketchat:lib
    
    * Fix wrong import and lint
    
    * Convert rocketchat-webrtc to main module structure
    
    * Fix missing export
    
    * Remove directly dependency between rocketchat:emoji and lib
    
    * Add emoji dependencies inside RocketChat namespace
    
    * Merge branch 'develop' into globals/move-rocketchat-callbacks
    
    * Move some functions to utils
    
    * Fix lint
    
    * Move some ui functions to ui-utils
    
    * Fix import missed objects inside RocketChat namespace
    
    * Fix lint
    
    * Remove rocketchat:ui package dependency of RocketChat namespace
    
    * Remove lib dependency in rocketchat:ui-sidenav
    
    * Remove dependency between lib and ui-vrecord
    
    * Add logger dependency in file-upload
    
    * Convert rocketchat:ui to main module structure
    
    * import variables that was broken due to conversion of rocketchat:ui
    
    * Remove globals variables from eslintrc and add some to the eslintrc of livechat app
    
    * Remove dependency between RocketChat namespace and migrations
    
    * Revert commented test file
    
    * Remove dependency of RocketChat namespace and logger
    
    * Move CustomSounds Model to rocketchat:models
    
    * Remove dependency of RocketChat namespace and custom-sounds
    
    * MOve function to utils
    
    * Remove dependency of RocketChat namespace and push-notifications

---
+++ packages/rocketchat-utils/lib/getUserNotificationPreference.js
@@ -0,0 +1,31 @@
+import { settings } from 'meteor/rocketchat:settings';
+import { Users } from 'meteor/rocketchat:models';
+
+export const getUserNotificationPreference = (user, pref) => {
+	if (typeof user === 'string') {
+		user = Users.findOneById(user);
+	}
+
+	let preferenceKey;
+	switch (pref) {
+		case 'desktop': preferenceKey = 'desktopNotifications'; break;
+		case 'mobile': preferenceKey = 'mobileNotifications'; break;
+		case 'email': preferenceKey = 'emailNotificationMode'; break;
+	}
+
+	if (user && user.settings && user.settings.preferences && user.settings.preferences[preferenceKey] !== 'default') {
+		return {
+			value: user.settings.preferences[preferenceKey],
+			origin: 'user',
+		};
+	}
+	const serverValue = settings.get(`Accounts_Default_User_Preferences_${ preferenceKey }`);
+	if (serverValue) {
+		return {
+			value: serverValue,
+			origin: 'server',
+		};
+	}
+
+	return null;
+};
