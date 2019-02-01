e08ad7e81d5861048666b4119cfac2d5735bc262
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-02-01 15:00:56 -0200

    Remove dependency of RC namespace in rc-mentions

--- packages/rocketchat-mentions/package.js
+++ packages/rocketchat-mentions/package.js
@@ -8,7 +8,10 @@ Package.describe({
 Package.onUse(function(api) {
 	api.use([
 		'ecmascript',
-		'rocketchat:lib',
+		'rocketchat:settings',
+		'rocketchat:callbacks',
+		'rocketchat:notifications',
+		'rocketchat:models',
 	]);
 	api.mainModule('client/index.js', 'client');
 	api.mainModule('server/index.js', 'server');
