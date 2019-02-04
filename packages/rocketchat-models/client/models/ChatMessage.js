b807e99b1568c315faade57d957ddf349ef84e31
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-02-04 18:06:57 -0200

    Remove dependency of RC namespace in rc-reactions

--- packages/rocketchat-models/client/models/ChatMessage.js
+++ packages/rocketchat-models/client/models/ChatMessage.js
@@ -1,3 +1,11 @@
 import { Mongo } from 'meteor/mongo';
 
 export const ChatMessage = new Mongo.Collection(null);
+
+ChatMessage.setReactions = function(messageId, reactions) {
+	return this.update({ _id: messageId }, { $set: { reactions } });
+};
+
+ChatMessage.unsetReactions = function(messageId) {
+	return this.update({ _id: messageId }, { $unset: { reactions: 1 } });
+};
