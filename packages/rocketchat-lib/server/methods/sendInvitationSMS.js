dd17d1ac401e697e529ebe87b08fd6b9c6a89637
erosenthal <eric.rosenthal@viasat.com>
2019-02-07 12:02:49 -0500

    support spanish, 'es', for local language in SMS invites.

--- packages/rocketchat-lib/server/methods/sendInvitationSMS.js
+++ packages/rocketchat-lib/server/methods/sendInvitationSMS.js
@@ -3,7 +3,7 @@ import { TAPi18n } from 'meteor/tap:i18n';
 import { check } from 'meteor/check';
 import _ from 'underscore';
 
-const supportedLanguages = ['en'];
+const supportedLanguages = ['en', 'es'];
 
 Meteor.methods({
 	sendInvitationSMS(phones, language) {
