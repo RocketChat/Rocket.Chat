dd0a5e1d45886bae039d573ba5756949c9349e50
Oliver Jägle <github@beimir.net>
2019-02-08 20:13:38 +0100

    Add more logging for error analysis

--- packages/assistify-ai/client/messageRenderer.js
+++ packages/assistify-ai/client/messageRenderer.js
@@ -8,12 +8,16 @@ import s from 'underscore.string';
  * @augments message.html - the rendered message body
  */
 const highlightRecognizedTerms = function(message) {
+	const { recognizedTerms } = message;
+	console.info('Highlighting Smarti tokens in message', message._id);
 	if (RocketChat.settings.get('Assistify_AI_Smarti_Inline_Highlighting_Enabled')) {
 		let { html } = message;
-		if (message.recognizedTerms) {
-			message.recognizedTerms.forEach((term) => {
+		console.info('Recognized terms', JSON.stringify(recognizedTerms));
+		if (recognizedTerms) {
+			recognizedTerms.forEach((term) => {
 				const regexpFindTerm = `(^|\\b|[\\s\\n\\r\\t.,،'\\\"\\+!?:-])(${ s.escapeRegExp(term) })($|\\b|[\\s\\n\\r\\t.,،'\\\"\\+!?:-])(?![^<]*>|[^<>]*<\\/)`;
 				html = html.replace(new RegExp(regexpFindTerm, 'gmi'), '$1<span class="recognized-term"><span class="text">$2</span></span>$3');
+				console.info('HTML after term', term, html);
 			});
 		}
 		message.html = html;
