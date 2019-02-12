351c44a988be936d05db60076ff7426fe40774d9
Diego Sampaio <chinello@gmail.com>
2019-02-12 14:12:08 -0200

    Fix misspelled language setting name

--- packages/rocketchat-livechat/server/startup.js
+++ packages/rocketchat-livechat/server/startup.js
@@ -21,7 +21,7 @@ Meteor.startup(() => {
 			return user;
 		}
 		throw new Meteor.Error(TAPi18n.__('You_cant_leave_a_livechat_room_Please_use_the_close_button', {
-			lng: user.language || RocketChat.settings.get('language') || 'en',
+			lng: user.language || RocketChat.settings.get('Language') || 'en',
 		}));
 	}, RocketChat.callbacks.priority.LOW, 'cant-leave-room');
 });
