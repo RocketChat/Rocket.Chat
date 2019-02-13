0fe376f22e341e801519c52a31c87decd8ae6278
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-02-13 16:46:52 -0200

    create thread message action function improved

--- packages/assistify-threading/client/createThreadMessageAction.js
+++ packages/assistify-threading/client/createThreadMessageAction.js
@@ -1,48 +1,37 @@
 import { Meteor } from 'meteor/meteor';
 import { RocketChat } from 'meteor/rocketchat:lib';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Tracker } from 'meteor/tracker';
-import { ChatMessage } from 'meteor/rocketchat:models';
+import { call } from 'meteor/rocketchat:lib';
+
+const condition = (rid, uid) => {
+	if (!RocketChat.models.Subscriptions.findOne({ rid })) {
+		return false;
+	}
+	return uid !== Meteor.userId() ? RocketChat.authz.hasPermission('start-thread-other-user') : RocketChat.authz.hasPermission('start-thread');
+};
 
 Meteor.startup(function() {
-	const instance = this;
-	instance.room = new ReactiveVar('');
-	RocketChat.MessageAction.addButton({
-		id: 'start-thread',
-		icon: 'thread',
-		label: 'Thread_start',
-		context: ['message', 'message-mobile'],
-		action() {
-			const message = this._arguments[1];
-			Meteor.call('createThreadFromMessage', message, function(error, result) {
-				if (result) {
-					// remove the hidden message from the UI - the message list is not reactive
-					Tracker.nonreactive(function() {
-						ChatMessage.remove({
-							_id: message._id,
-						});
-					});
+	RocketChat.settings.get('Thread_from_context_menu', (key, value) => {
 
-					// navigate to the newly created room
-					FlowRouter.goToRoomById(result._id);
-				}
-			});
-		},
-		condition(message) {
-			if (RocketChat.models.Subscriptions.findOne({ rid: message.rid }) == null) {
-				return false;
-			}
-			if (RocketChat.settings.get('Thread_from_context_menu') !== 'button') {
-				return false;
-			}
-			if (message.u._id !== Meteor.userId()) {
-				return RocketChat.authz.hasAtLeastOnePermission('start-thread-other-user');
-			} else {
-				return RocketChat.authz.hasAtLeastOnePermission('start-thread');
-			}
-		},
-		order: 0,
-		group: 'menu',
+		if (value !== 'button') {
+			return RocketChat.MessageAction.removeButton('start-thread');
+		}
+
+		RocketChat.MessageAction.addButton({
+			id: 'start-thread',
+			icon: 'thread',
+			label: 'Thread_start',
+			context: ['message', 'message-mobile'],
+			async action() {
+				const [, message] = this._arguments;
+				const { _id } = await call('createThreadFromMessage', message);
+				FlowRouter.goToRoomById(_id);
+			},
+			condition({ rid, u: { _id: uid } }) {
+				return condition(rid, uid);
+			},
+			order: 0,
+			group: 'menu',
+		});
 	});
 });
