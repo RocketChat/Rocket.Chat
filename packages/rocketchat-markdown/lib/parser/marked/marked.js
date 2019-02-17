f4a6246051d1ee350c68b87f9f58ba03258a7888
Tasso Evangelista <tasso@tassoevan.me>
2019-02-17 18:34:43 +0100

    Update packages/rocketchat-markdown/lib/parser/marked/marked.js
    Co-Authored-By: DeviaVir <chase@sillevis.net>

--- packages/rocketchat-markdown/lib/parser/marked/marked.js
+++ packages/rocketchat-markdown/lib/parser/marked/marked.js
@@ -62,7 +62,7 @@ renderer.blockquote = function(quote) {
 const linkRenderer = renderer.link;
 renderer.link = function(href, title, text) {
 	const html = linkRenderer.call(renderer, href, title, text);
-	return html.replace(/^<a /, '<a target="_blank" rel="nofollow" ');
+	return html.replace(/^<a /, '<a target="_blank" rel="nofollow noopener noreferrer" ');
 };
 
 const highlight = function(code, lang) {
