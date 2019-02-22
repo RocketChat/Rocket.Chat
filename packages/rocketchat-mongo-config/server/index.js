562f456c549e1e38f210667f8c2573f76bcfe9cd
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-02-22 16:11:06 -0300

    Remove TLS fix

--- packages/rocketchat-mongo-config/server/index.js
+++ packages/rocketchat-mongo-config/server/index.js
@@ -1,10 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 
-import tls from 'tls';
-// FIX For TLS error see more here https://github.com/RocketChat/Rocket.Chat/issues/9316
-// TODO: Remove after NodeJS fix it, more information https://github.com/nodejs/node/issues/16196 https://github.com/nodejs/node/pull/16853
-tls.DEFAULT_ECDH_CURVE = 'auto';
-
 // Revert change from Meteor 1.6.1 who set ignoreUndefined: true
 // more information https://github.com/meteor/meteor/pull/9444
 let mongoOptions = {
