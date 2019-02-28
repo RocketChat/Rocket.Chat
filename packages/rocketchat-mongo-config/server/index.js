b16fe933346d96daeb506622796321b9d4c3eb4f
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-02-28 14:37:58 -0300

    Fix: Mongo.setConnectionOptions was not being set correctly (#13586)

--- packages/rocketchat-mongo-config/server/index.js
+++ packages/rocketchat-mongo-config/server/index.js
@@ -1,16 +1,8 @@
 import { Mongo } from 'meteor/mongo';
 
-// Revert change from Meteor 1.6.1 who set ignoreUndefined: true
-// more information https://github.com/meteor/meteor/pull/9444
-let mongoOptions = {
-	ignoreUndefined: false,
-};
-
 const mongoOptionStr = process.env.MONGO_OPTIONS;
 if (typeof mongoOptionStr !== 'undefined') {
-	const jsonMongoOptions = JSON.parse(mongoOptionStr);
+	const mongoOptions = JSON.parse(mongoOptionStr);
 
-	mongoOptions = Object.assign({}, mongoOptions, jsonMongoOptions);
+	Mongo.setConnectionOptions(mongoOptions);
 }
-
-Mongo.setConnectionOptions(mongoOptions);
