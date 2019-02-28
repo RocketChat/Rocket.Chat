b52a00a20491eccb6ead3c05cba0b956834cc8ef
Diego Sampaio <chinello@gmail.com>
2019-02-28 18:17:11 -0300

    Add validation to methods

--- packages/rocketchat-federation/server/methods/federationAddUser.js
+++ packages/rocketchat-federation/server/methods/federationAddUser.js
@@ -3,9 +3,18 @@ import { Users } from 'meteor/rocketchat:models';
 
 import { logger } from '../logger';
 import peerClient from '../peerClient';
+import peerServer from '../peerClient';
 
 Meteor.methods({
 	federationAddUser(emailAddress, domainOverride) {
+		if (!Meteor.userId()) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'federationAddUser' });
+		}
+
+		if (!peerServer.enabled) {
+			throw new Meteor.Error('error-federation-disabled', 'Federation disabled', { method: 'federationAddUser' });
+		}
+
 		// Make sure the federated user still exists, and get the unique one, by email address
 		const [federatedUser] = peerClient.findUsers(emailAddress, { domainOverride, emailOnly: true });
 
--- packages/rocketchat-federation/server/methods/federationSearchUsers.js
+++ packages/rocketchat-federation/server/methods/federationSearchUsers.js
@@ -1,10 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+
 import peerClient from '../peerClient';
+import peerServer from '../peerServer';
 
 Meteor.methods({
 	federationSearchUsers(email) {
 		if (!Meteor.userId()) {
-			return [];
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'federationSearchUsers' });
+		}
+
+		if (!peerServer.enabled) {
+			throw new Meteor.Error('error-federation-disabled', 'Federation disabled', { method: 'federationAddUser' });
 		}
 
 		const federatedUsers = peerClient.findUsers(email);
