f933e596cabac589bd38761de450ed20f2847ece
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-03-07 20:05:47 -0300

    useless configs

--- packages/assistify-threading/config.js
+++ packages/assistify-threading/config.js
@@ -4,13 +4,6 @@ import { settings } from 'meteor/rocketchat:settings';
 Meteor.startup(() => {
 	settings.addGroup('Threading', function() {
 		// the channel for which threads are created if none is explicitly chosen
-		this.add('Thread_invitations_threshold', 10, {
-			group: 'Threading',
-			i18nLabel: 'Thread_invitations_threshold',
-			i18nDescription: 'Thread_invitations_threshold_description',
-			type: 'int',
-			public: true,
-		});
 
 		this.add('Thread_from_context_menu', 'button', {
 			group: 'Threading',
@@ -21,16 +14,6 @@ Meteor.startup(() => {
 				{ key: 'none', i18nLabel: 'Threading_context_menu_none' },
 			],
 			public: true,
-		});
-
-
-		// this is a technical counter which allows for generation of unique room names
-		this.add('Thread_Count', 1, {
-			group: 'Threading',
-			i18nLabel: 'Thread_count',
-			type: 'int',
-			public: false,
-			hidden: true,
 		});
 	});
 
