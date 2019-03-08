574b96feb17dc51055817489b26e08032c9fbdce
Diego Sampaio <chinello@gmail.com>
2019-03-07 23:58:05 -0300

    Remove slash command files

--- packages/rocketchat-slashcommands-create-thread/client/client.js
+++
@@ -1,6 +0,0 @@
-import { slashCommands } from 'meteor/rocketchat:utils';
-
-slashCommands.add('thread', null, {
-	description: 'Thread_slash_command_description',
-	params: 'Thread_slash_command_params',
-});
