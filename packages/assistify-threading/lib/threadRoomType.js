d2be600c2a194c5d351908a0f2e7fcd0cb9a8158
Diego Sampaio <chinello@gmail.com>
2019-03-08 00:05:17 -0300

    Add condition to show threads category

--- packages/assistify-threading/lib/threadRoomType.js
+++ packages/assistify-threading/lib/threadRoomType.js
@@ -1,5 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { RoomTypeConfig, roomTypes, getUserPreference } from 'meteor/rocketchat:utils';
 
-import { RoomTypeConfig, roomTypes } from 'meteor/rocketchat:utils';
 export class ThreadRoomType extends RoomTypeConfig {
 	constructor() {
 		super({
@@ -10,6 +11,10 @@ export class ThreadRoomType extends RoomTypeConfig {
 
 		// we need a custom template in order to have a custom query showing the subscriptions to threads
 		this.customTemplate = 'ThreadList';
+	}
+
+	condition() {
+		return getUserPreference(Meteor.userId(), 'sidebarShowThreads');
 	}
 }
 
