542dbe5b354c78d316ea8596ad73dd64073a3838
Aditya <aditya.1998bhardwaj@gmail.com>
2019-03-09 19:31:01 +0530

    [IMPROVE] User thread join message

--- packages/rocketchat-lib/server/functions/addUserToRoom.js
+++ packages/rocketchat-lib/server/functions/addUserToRoom.js
@@ -44,14 +44,14 @@ export const addUserToRoom = function(rid, user, inviter, silenced) {
 					username: inviter.username,
 				},
 			});
-		} else {
-			if(room.prid){
-				Messages.createUserJoinWithRoomIdAndUserThread(rid,user,{ts:now});
+		} 
+		else {
+			if (room.prid) {
+				Messages.createUserJoinWithRoomIdAndUserThread(rid, user, { ts: now });
 			}
-			else{
+			else {
 				Messages.createUserJoinWithRoomIdAndUser(rid, user, { ts: now });
-			}
-			
+			}	
 		}
 	}
 
