5a0ebe8ee9aefa6b4568a8db93d645af0c2bd014
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-03-14 17:51:36 -0300

    Add memoize and improve the code

--- app/utils/server/lib/composeMessageObjectWithUser.js
+++ app/utils/server/lib/composeMessageObjectWithUser.js
@@ -1,28 +1,33 @@
 import { Users } from '/app/models';
 import { settings } from '/app/settings';
+import memoize from 'mem';
+
+const maxAgeInMS = 1000;
 
 const getUserByUsername = (username) => Users.findOneByUsername(username, { fields: { name: 1 } });
-const getNameOfUser = (username) => {
+const getNameOfUser = memoize((username) => {
 	const user = getUserByUsername(username);
 	return user ? user.name : undefined;
-};
+}, { maxAge: maxAgeInMS });
 
 export const composeMessageObjectWithUser = function(message, userId) {
 	if (message) {
 		if (message.starred && Array.isArray(message.starred)) {
 			message.starred = message.starred.filter((star) => star._id === userId);
 		}
-		if (message.u && message.u._id && settings.get('UI_Use_Real_Name')) {
-			message.u.name = getNameOfUser(message.u.username);
-		}
-		if (message.mentions && message.mentions.length && settings.get('UI_Use_Real_Name')) {
-			message.mentions.forEach((mention) => mention.name = getNameOfUser(mention.username));
-		}
-		if (message.reactions && Object.keys(message.reactions).length && settings.get('UI_Use_Real_Name')) {
-			Object.keys(message.reactions).forEach((reaction) => {
-				const names = message.reactions[reaction].usernames.map(getNameOfUser);
-				message.reactions[reaction].names = names;
-			});
+		if (settings.get('UI_Use_Real_Name')) {
+			if (message.u && message.u._id) {
+				message.u.name = getNameOfUser(message.u.username);
+			}
+			if (message.mentions && message.mentions.length) {
+				message.mentions.forEach((mention) => mention.name = getNameOfUser(mention.username));
+			}
+			if (message.reactions && Object.keys(message.reactions).length) {
+				Object.keys(message.reactions).forEach((reaction) => {
+					const names = message.reactions[reaction].usernames.map(getNameOfUser);
+					message.reactions[reaction].names = names;
+				});
+			}
 		}
 	}
 	return message;
