07641dfc23415104348e55743831f50cb0e9fe32
Renato Becker <renato.augusto.becker@gmail.com>
2019-03-14 14:10:20 -0300

    Initial commit providing new Livechat.

--- app/livechat/client/views/app/livechatInstallation.js
+++ app/livechat/client/views/app/livechatInstallation.js
@@ -2,10 +2,10 @@ import { Template } from 'meteor/templating';
 import { settings } from '/app/settings';
 import s from 'underscore.string';
 
-Template.livechatInstallation.helpers({
-	script() {
-		const siteUrl = s.rtrim(settings.get('Site_Url'), '/');
+const siteUrl = s.rtrim(settings.get('Site_Url'), '/');
 
+Template.livechatInstallation.helpers({
+	oldScript() {
 		return `<!-- Start of Rocket.Chat Livechat Script -->
 <script type="text/javascript">
 (function(w, d, s, u) {
@@ -14,6 +14,19 @@ Template.livechatInstallation.helpers({
 	j.async = true; j.src = '${ siteUrl }/packages/rocketchat_livechat/assets/rocketchat-livechat.min.js?_=201702160944';
 	h.parentNode.insertBefore(j, h);
 })(window, document, 'script', '${ siteUrl }/livechat');
+</script>
+<!-- End of Rocket.Chat Livechat Script -->`;
+	},
+
+	script() {
+		return `<!-- Start of Rocket.Chat Livechat Script -->
+<script type="text/javascript">
+(function(w, d, s, u) {
+	w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+	var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+	j.async = true; j.src = '${ siteUrl }/livechat/1.0/rocketchat-livechat.min.js?_=201903270000';
+	h.parentNode.insertBefore(j, h);
+})(window, document, 'script', '${ siteUrl }/livechat?version=1.0');
 </script>
 <!-- End of Rocket.Chat Livechat Script -->`;
 	},
--- packages/rocketchat-livechat/plugin/build.sh
+++ packages/rocketchat-livechat/plugin/build.sh
@@ -2,6 +2,8 @@ export NODE_ENV="production"
 export LIVECHAT_DIR="../../../public/livechat"
 export BUILD_DIR="../build"
 export BUNDLE_DIR="../build/bundle/programs/web.browser.legacy"
+export LIVECHAT_APP_DIR="../../../app/livechat"
+export LATEST_LIVECHAT_VERSION="1.0"
 
 cd packages/rocketchat-livechat/.app
 meteor npm install --production
@@ -12,8 +14,29 @@ mkdir -p $LIVECHAT_DIR
 cp $BUNDLE_DIR/*.css $LIVECHAT_DIR/livechat.css
 cp $BUNDLE_DIR/*.js $LIVECHAT_DIR/livechat.js
 
-rm -rf ../public
-mkdir -p ../public
-cp $BUNDLE_DIR/head.html ../public/head.html
+rm -rf $LIVECHAT_APP_DIR/public
+mkdir $LIVECHAT_APP_DIR/public
 
+cp $BUNDLE_DIR/head.html $LIVECHAT_APP_DIR/public/head.html
 rm -rf $BUILD_DIR
+
+#NEW LIVECHAT#
+echo "Installing new Livechat..."
+cd $LIVECHAT_DIR
+mkdir -p $LATEST_LIVECHAT_VERSION
+cd $LATEST_LIVECHAT_VERSION
+
+curl -sOL https://github.com/RocketChat/Rocket.Chat.Livechat/releases/latest/download/build.tar.gz
+tar -xf build.tar.gz
+rm build.tar.gz
+
+cd $LIVECHAT_APP_DIR/public
+
+# change to lowercase so all injected junk from rocket.chat is not sent: https://github.com/meteorhacks/meteor-inject-initial/blob/master/lib/inject-core.js#L10
+# this is not harmful since doctype is case-insesitive: https://www.w3.org/TR/html5/syntax.html#the-doctype
+sed -i -e "s/<!DOCTYPE/<!doctype/" head.html
+sed -i -e "s/<!DOCTYPE/<!doctype/" $LIVECHAT_DIR/$LATEST_LIVECHAT_VERSION/index.html
+sed -i -e "s/<!DOCTYPE/<!doctype/" $LIVECHAT_DIR/$LATEST_LIVECHAT_VERSION/widget-demo.html
+
+cp $LIVECHAT_DIR/$LATEST_LIVECHAT_VERSION/index.html .
+cp $LIVECHAT_DIR/$LATEST_LIVECHAT_VERSION/widget-demo.html .
