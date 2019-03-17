a60f4d55b8e17480140dfe7e8b36a95d82a3ace8
fliptrail <amishmittal@outlook.com>
2019-03-18 01:41:34 +0530

    Stylizes 404 pageNotFound.html

--- client/routes/pageNotFound.js
+++ client/routes/pageNotFound.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 
+import './pageNotFound.html';
+
 Template.pageNotFound.helpers({
 	errorcode: function () {
 		return '#404';
