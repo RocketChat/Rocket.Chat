8aa90c777bcf5654b19876215dd457e3b0d6a8b3
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-03-18 15:13:03 -0300

    Show threads as section option on popover

--- app/ui-sidenav/client/sortlist.js
+++ app/ui-sidenav/client/sortlist.js
@@ -6,6 +6,10 @@ import { settings } from '../../settings';
 
 const checked = function(prop, field) {
 	const userId = Meteor.userId();
+
+	if (prop === 'sidebarShowThreads') {
+		return getUserPreference(userId, 'sidebarShowThreads');
+	}
 	if (prop === 'sidebarShowFavorites') {
 		return getUserPreference(userId, 'sidebarShowFavorites');
 	}
