44bdead5aaa0cb54edc59dda056dde2bb21863a7
fliptrail <amishmittal@outlook.com>
2019-03-19 23:59:18 +0530

    Adds Previous and home button

--- client/routes/pageNotFound.js
+++ client/routes/pageNotFound.js
@@ -7,3 +7,21 @@ Template.pageNotFound.helpers({
 		return '404';
 	},
 });
+
+Template.pageNotFound.onRendered(function() {
+	const parent = document.querySelector('.page-loading');
+	const child = document.querySelector('.loading-animation');
+	parent.removeChild(child);
+});
+
+Template.pageNotFound.events({
+	'click .page-not-found-button-home'(e) {
+		e.preventDefault();
+		window.location.href = '/';
+	},
+
+	'click .page-not-found-button-previous'(e) {
+		e.preventDefault();
+		window.history.back();
+	},
+});
