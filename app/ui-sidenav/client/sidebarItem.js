c7aba897f8f91e17118a271de14db4a7a4274f03
Kailash Bhalaki <kailashbhalaki@gmail.com>
2019-03-22 02:11:27 +0530

    Don't close sidebar when room menu is clicked.

--- app/ui-sidenav/client/sidebarItem.js
+++ app/ui-sidenav/client/sidebarItem.js
@@ -102,6 +102,7 @@ Template.sidebarItem.events({
 		return menu.close();
 	},
 	'click .sidebar-item__menu'(e) {
+		e.stopPropagation(); // to not close the menu
 		e.preventDefault();
 
 		const canLeave = () => {
