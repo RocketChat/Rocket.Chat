78538200221e6ac30eb50bdcbf230463d6a70550
fliptrail <amishmittal@outlook.com>
2019-03-23 00:49:51 +0530

    User gets feedback when a message has been starred or unstarred

--- app/message-star/client/starMessage.js
+++ app/message-star/client/starMessage.js
@@ -1,17 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { settings } from '../../settings';
 import { ChatMessage, Subscriptions } from '../../models';
+import toastr from 'toastr';
+import { TAPi18n } from 'meteor/tap:i18n';
 
 Meteor.methods({
 	starMessage(message) {
 		if (!Meteor.userId()) {
+			toastr.error(TAPi18n.__('error-starring-message'));
 			return false;
 		}
 		if (Subscriptions.findOne({ rid: message.rid }) == null) {
+			toastr.error(TAPi18n.__('error-starring-message'));
 			return false;
 		}
 		if (!settings.get('Message_AllowStarring')) {
+			toastr.error(TAPi18n.__('error-starring-message'));
 			return false;
+		}
+		if(message.starred) {
+			toastr.success(TAPi18n.__('Message_has_been_starred'));
+		}
+		else{
+			toastr.success(TAPi18n.__('Message_has_been_unstarred'));
 		}
 		return ChatMessage.update({
 			_id: message._id,
