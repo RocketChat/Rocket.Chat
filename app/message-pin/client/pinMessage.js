a85d07c9edb67a1da8fbb574026df370ef7889b9
Ashwani <ashyadavash@gmail.com>
2019-03-23 15:06:58 +0530

    linting issue

--- app/message-pin/client/pinMessage.js
+++ app/message-pin/client/pinMessage.js
@@ -5,25 +5,20 @@ import toastr from 'toastr';
 import { TAPi18n } from 'meteor/tap:i18n';
 
 Meteor.methods({
-	pinMessage(message) 
-	{
-		if (!Meteor.userId()) 
-		{
+	pinMessage(message) {
+		if (!Meteor.userId()) {
 			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		else if (!settings.get('Message_AllowPinning')) 
-		{
+		else if (!settings.get('Message_AllowPinning')) {
 			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		else if (Subscriptions.findOne({ rid: message.rid }) == null) 
-		{
+		else if (Subscriptions.findOne({ rid: message.rid }) == null) {
 			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		else 
-		{
+		else {
 			toastr.success(TAPi18n.__('Message_has_been_pinned'));
 			return ChatMessage.update({
 				_id: message._id,
@@ -34,25 +29,20 @@ Meteor.methods({
 			});
 		}
 	},
-	unpinMessage(message) 
-	{
-		if (!Meteor.userId()) 
-		{
+	unpinMessage(message) {
+		if (!Meteor.userId()) {
 			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		else if (!settings.get('Message_AllowPinning')) 
-		{
+		else if (!settings.get('Message_AllowPinning')) {
 			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		else if (Subscriptions.findOne({ rid: message.rid }) == null) 
-		{
+		else if (Subscriptions.findOne({ rid: message.rid }) == null) {
 			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		else 
-		{
+		else {
 			toastr.success(TAPi18n.__('Message_has_been_unpinned'));
 			return ChatMessage.update({
 				_id: message._id,
