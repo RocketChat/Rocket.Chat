dd50e326ae9577127562fcdb19561889c34fff4f
Ashwani <ashyadavash@gmail.com>
2019-03-23 14:59:09 +0530

    Users get feedback when a message is pinned or unpinned

--- app/message-pin/client/pinMessage.js
+++ app/message-pin/client/pinMessage.js
@@ -1,42 +1,56 @@
 import { Meteor } from 'meteor/meteor';
 import { settings } from '../../settings';
 import { ChatMessage, Subscriptions } from '../../models';
+import toastr from 'toastr';
+import { TAPi18n } from 'meteor/tap:i18n';
 
 Meteor.methods({
 	pinMessage(message) {
 		if (!Meteor.userId()) {
+			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		if (!settings.get('Message_AllowPinning')) {
+		else if (!settings.get('Message_AllowPinning')) {
+			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		if (Subscriptions.findOne({ rid: message.rid }) == null) {
+		else if (Subscriptions.findOne({ rid: message.rid }) == null) {
+			toastr.error(TAPi18n.__('error-pinning-message'));
 			return false;
 		}
-		return ChatMessage.update({
-			_id: message._id,
-		}, {
-			$set: {
-				pinned: true,
-			},
-		});
+		else{
+			toastr.success(TAPi18n.__('Message_has_been_pinned'));
+			return ChatMessage.update({
+				_id: message._id,
+			}, {
+				$set: {
+					pinned: true,
+				},
+			});
+		}
 	},
 	unpinMessage(message) {
 		if (!Meteor.userId()) {
+			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		if (!settings.get('Message_AllowPinning')) {
+		else if (!settings.get('Message_AllowPinning')) {
+			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		if (Subscriptions.findOne({ rid: message.rid }) == null) {
+		else if (Subscriptions.findOne({ rid: message.rid }) == null) {
+			toastr.error(TAPi18n.__('error-unpinning-message'));
 			return false;
 		}
-		return ChatMessage.update({
-			_id: message._id,
-		}, {
-			$set: {
-				pinned: false,
-			},
-		});
+		else{
+			toastr.success(TAPi18n.__('Message_has_been_unpinned'));
+			return ChatMessage.update({
+				_id: message._id,
+			}, {
+				$set: {
+					pinned: false,
+				},
+			});
+		}
 	},
 });
