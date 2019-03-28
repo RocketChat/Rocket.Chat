661edcae485867a3b1abad1e78a06b123e7064b3
Alan Sikora <alansikora@gmail.com>
2019-03-28 14:16:50 -0300

    Better federation file and code organization

---
+++ app/federation/server/methods/searchUsers.js
@@ -0,0 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+
+export function searchUsers(identifier) {
+	if (!Meteor.userId()) {
+		throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'federationSearchUsers' });
+	}
+
+	if (!this.peerClient.enabled) {
+		throw new Meteor.Error('error-federation-disabled', 'Federation disabled', { method: 'federationSearchUsers' });
+	}
+
+	const federatedUsers = this.peerClient.findUsers(identifier);
+
+	if (!federatedUsers.length) {
+		throw new Meteor.Error('federation-user-not-found', `Could not find federated users using "${ identifier }"`);
+	}
+
+	return federatedUsers;
+}
