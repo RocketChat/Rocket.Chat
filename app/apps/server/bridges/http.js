6551891a833339b997db61f32ce15374adb37d14
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-03-29 12:50:08 -0300

    [NEW] Limit App’s HTTP calls to 500ms
    And fix a regresion

--- app/apps/server/bridges/http.js
+++ app/apps/server/bridges/http.js
@@ -1,7 +1,15 @@
 import { HTTP } from 'meteor/http';
 
 export class AppHttpBridge {
+	constructor(orch) {
+		this.orch = orch;
+	}
+
 	async call(info) {
+		if (typeof info.request.timeout !== 'number' || info.request.timeout > 500) {
+			info.request.timeout = 500;
+		}
+
 		if (!info.request.content && typeof info.request.data === 'object') {
 			info.request.content = JSON.stringify(info.request.data);
 		}
@@ -9,7 +17,7 @@ export class AppHttpBridge {
 		this.orch.debugLog(`The App ${ info.appId } is requesting from the outter webs:`, info);
 
 		try {
-			return HTTP.call(info.method, info.url, info.request);
+			return await HTTP.call(info.method, info.url, info.request);
 		} catch (e) {
 			return e.response;
 		}
