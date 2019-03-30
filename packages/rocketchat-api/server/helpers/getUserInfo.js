262496dfe34744ab7ca9769ed2901689f0db4be3
Karan Bedi <karanbedi0304@gmail.com>
2019-03-30 16:10:34 +0100

    Use config setting

--- packages/rocketchat-api/server/helpers/getUserInfo.js
+++ packages/rocketchat-api/server/helpers/getUserInfo.js
@@ -57,8 +57,29 @@ RocketChat.API.helperMethods.set('getUserInfo', function _getUserInfo(user) {
 	};
 
 	me.telephoneNumber = '';
-	if (('services' in user) && ('viasatsso' in user.services) && ('telephoneNumber' in user.services.viasatsso)) {
-		me.telephoneNumber = user.services.viasatsso.telephoneNumber;
+	let phoneFieldName = '';
+
+	RocketChat.settings.get('Contacts_Phone_Custom_Field_Name', function(name, fieldName) {
+		phoneFieldName = fieldName;
+	});
+
+	let phoneFieldArray = [];
+	if (phoneFieldName) {
+		phoneFieldArray = phoneFieldName.split(',');
 	}
+
+	if (phoneFieldArray.length > 0) {
+		let dict = user;
+		for (let i = 0;i < phoneFieldArray.length - 1;i++) {
+			if (phoneFieldArray[i] in dict) {
+				dict = dict[phoneFieldArray[i]];
+			}
+		}
+		const phone = dict[phoneFieldArray[phoneFieldArray.length - 1]];
+		if (phone) {
+			me.telephoneNumber = phone;
+		}
+	}
+
 	return me;
 });
