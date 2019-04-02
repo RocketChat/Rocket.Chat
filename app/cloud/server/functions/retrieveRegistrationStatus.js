b0d71bc9431a26a2b9eb2e256a1d0cd117777bb7
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2019-04-02 11:16:41 -0500

    [FIX] Improve cloud section (#13820)
    * Improve cloud section
    
    * add sync and fix i18n and a few other issues
    
    * set oauth scopes
    
    * add suggested grammar and federation hub scope
    
    * Switch to postform instead of query string to post oauth more securely

--- app/cloud/server/functions/retrieveRegistrationStatus.js
+++ app/cloud/server/functions/retrieveRegistrationStatus.js
@@ -3,9 +3,11 @@ import { Users } from '../../../models';
 
 export function retrieveRegistrationStatus() {
 	const info = {
-		registeredWithWizard: settings.get('Register_Server'),
-		workspaceConnected: (settings.get('Cloud_Workspace_Client_Id')) ? true : false,
+		connectToCloud: settings.get('Register_Server'),
+		workspaceRegistered: (settings.get('Cloud_Workspace_Client_Id')) ? true : false,
 		userAssociated: (settings.get('Cloud_Workspace_Account_Associated')) ? true : false,
+		workspaceId: settings.get('Cloud_Workspace_Id'),
+		uniqueId: settings.get('uniqueID'),
 		token: '',
 		email: '',
 	};
