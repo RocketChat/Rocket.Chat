27be0ec48c87a6f020fba170640bcc1609ac1fb5
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2019-04-05 19:39:19 -0300

    Update app/models/server/models/_BaseDb.js
    Co-Authored-By: rodrigok <rodrigoknascimento@gmail.com>

--- app/models/server/models/_BaseDb.js
+++ app/models/server/models/_BaseDb.js
@@ -47,6 +47,7 @@ export class BaseDb extends EventEmitter {
 					};
 
 					MongoInternals.defaultRemoteCollectionDriver().mongo._oplogHandle.onOplogEntry(query, this.processOplogRecord.bind(this));
+					// Meteor will handle if we have a value https://github.com/meteor/meteor/blob/5dcd0b2eb9c8bf881ffbee98bc4cb7631772c4da/packages/mongo/oplog_tailing.js#L5
 					if (process.env.METEOR_OPLOG_TOO_FAR_BEHIND == null) {
 						MongoInternals.defaultRemoteCollectionDriver().mongo._oplogHandle._defineTooFarBehind(Number.MAX_SAFE_INTEGER);
 					}
