a315eaeae84e7eb2347a14eabf6218a38df75e9d
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-04-06 17:22:04 -0300

    Check permission in rest endpoint

--- app/api/server/v1/users.js
+++ app/api/server/v1/users.js
@@ -256,21 +256,19 @@ API.v1.addRoute('users.setAvatar', { authRequired: true }, {
 			username: Match.Maybe(String),
 		}));
 
+		if (!settings.get('Accounts_AllowUserAvatarChange')) {
+			throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed', {
+				method: 'users.setAvatar',
+			});
+		}
+
 		let user;
 		if (this.isUserFromParams()) {
 			user = Meteor.users.findOne(this.userId);
-		} else if (hasPermission(this.userId, 'edit-other-user-info')) {
+		} else if (hasPermission(this.userId, 'edit-other-user-avatar')) {
 			user = this.getUserFromParams();
 		} else {
 			return API.v1.unauthorized();
-		}
-
-		if (!user.roles.includes('admin')) {
-			if (!settings.get('Accounts_AllowUserAvatarChange')) {
-				throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed', {
-					method: 'users.setAvatar',
-				});
-			}
 		}
 
 		Meteor.runAsUser(user._id, () => {
