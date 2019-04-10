5c5e2e2c0340fef9756f95ce7619a1bee9595e92
mohamedar97 <mohamedraafat060@gmail.com>
2019-04-10 16:37:07 +0000

    adjustments to go back to users list when closing the user info panel

--- app/ui-flextab/client/tabs/userInfo.js
+++ app/ui-flextab/client/tabs/userInfo.js
@@ -222,6 +222,10 @@ Template.userInfo.events({
 	'click .js-close-info'(e, instance) {
 		return instance.clear();
 	},
+	'click .js-close'(e, instance) {
+		return instance.clear();
+	},
+
 	'click .js-back'(e, instance) {
 		return instance.clear();
 	},
