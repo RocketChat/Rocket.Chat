c3ff5a68cea34e6a586aa131757d99efd977a82c
Diego Sampaio <chinello@gmail.com>
2019-04-11 14:43:50 -0300

    Update packages/rocketchat-livechat/.app/client/lib/chatMessages.js

--- packages/rocketchat-livechat/.app/client/lib/chatMessages.js
+++ packages/rocketchat-livechat/.app/client/lib/chatMessages.js
@@ -142,7 +142,7 @@ this.ChatMessages = class ChatMessages {
 			});
 		};
 
-		if ((!visitor.getId()) || (visitor.getDepartment() !== Livechat.department)) {
+		if (!visitor.getId() || visitor.getDepartment() !== Livechat.department) {
 			const guest = {
 				token: visitor.getToken(),
 			};
