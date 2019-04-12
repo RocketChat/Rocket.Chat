9dcdfe4810ab1c33009c787a971dc6953e3ab659
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-04-12 20:20:00 +0200

    [IMPROVE]: Adding check for array

--- app/custom-oauth/server/oauth_helpers.js
+++ app/custom-oauth/server/oauth_helpers.js
@@ -6,23 +6,25 @@ export function mapRolesFromSSO(user, identity, roleClaimName) {
 	if (user && roleClaimName) {
 		// Adding roles
 		if (identity[roleClaimName]) {
-			// loop through all assigned roles and drop if not assigned anymore
-			user[roleClaimName].forEach(function(roleFromOAuthProvider) {
-				if (identity[roleClaimName].length === 0) {
-					removeUserFromRoles(user._id, roleFromOAuthProvider);
-				}
-				if (identity[roleClaimName].indexOf(roleFromOAuthProvider) === -1) {
-					removeUserFromRoles(user._id, roleFromOAuthProvider);
-				}
-			});
-			// loop through all roles from SSO Provider and add them if needed
-			identity[roleClaimName].forEach(function(roleFromOAuthProvider) {
-				if (user[roleClaimName].indexOf(roleFromOAuthProvider) === -1
-					/* filter OpenID pseudo roles */
-					&& roleFromOAuthProvider !== 'offline_access' && roleFromOAuthProvider !== 'uma_authorization') {
-					addUserRoles(user._id, roleFromOAuthProvider);
-				}
-			});
+			if (Array.isArray(identity[roleClaimName])) {
+				// loop through all assigned roles and drop if not assigned anymore
+				user[roleClaimName].forEach(function(roleFromOAuthProvider) {
+					if (identity[roleClaimName].length === 0) {
+						removeUserFromRoles(user._id, roleFromOAuthProvider);
+					}
+					if (identity[roleClaimName].indexOf(roleFromOAuthProvider) === -1) {
+						removeUserFromRoles(user._id, roleFromOAuthProvider);
+					}
+				});
+				// loop through all roles from SSO Provider and add them if needed
+				identity[roleClaimName].forEach(function(roleFromOAuthProvider) {
+					if (user[roleClaimName].indexOf(roleFromOAuthProvider) === -1
+						/* filter OpenID pseudo roles */
+						&& roleFromOAuthProvider !== 'offline_access' && roleFromOAuthProvider !== 'uma_authorization') {
+						addUserRoles(user._id, roleFromOAuthProvider);
+					}
+				});
+			}
 		} else {
 			// else drop all roles
 			user[roleClaimName].forEach(function(roleFromOAuthProvider) {
