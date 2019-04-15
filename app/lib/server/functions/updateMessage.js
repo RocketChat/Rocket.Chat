db3d99585f520e70cd505766063c4a225537698d
Diego Sampaio <chinello@gmail.com>
2019-04-15 10:03:49 -0300

    [FIX] Updating a message from apps if keep history is on (#14129)

--- app/lib/server/functions/updateMessage.js
+++ app/lib/server/functions/updateMessage.js
@@ -29,7 +29,7 @@ export const updateMessage = function(message, user, originalMessage) {
 
 	// If we keep history of edits, insert a new message to store history information
 	if (settings.get('Message_KeepHistory')) {
-		Messages.cloneAndSaveAsHistoryById(message._id);
+		Messages.cloneAndSaveAsHistoryById(message._id, user);
 	}
 
 	message.editedAt = new Date();
