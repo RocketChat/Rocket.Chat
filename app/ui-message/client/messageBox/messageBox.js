36050a53aeace1130be9f2877b21869fd087803e
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2019-04-27 17:25:49 -0300

    [NEW] Update message actions (#14268)
    * Wip: update message actions
    
    * Fix report condition
    
    * Fix discussion icon
    
    * Fix new icons proportions
    
    * Keep follow and unfollow only in threads context
    
    * Change autotranslate buttons to "translate" and "view original"
    
    * Fix message action id
    
    * Fix open reply popup when reply in direct message

--- app/ui-message/client/messageBox/messageBox.js
+++ app/ui-message/client/messageBox/messageBox.js
@@ -98,6 +98,11 @@ Template.messageBox.onCreated(function() {
 });
 
 Template.messageBox.onRendered(function() {
+	const $input = $(this.find('.js-input-message'));
+	$input.on('dataChange', () => {
+		const messages = $input.data('reply') || [];
+		this.replyMessageData.set(messages);
+	});
 	this.autorun(() => {
 		const { rid, subscription } = Template.currentData();
 		const room = Session.get(`roomData${ rid }`);
@@ -156,12 +161,6 @@ Template.messageBox.onRendered(function() {
 
 			const shadow = this.find('.js-input-message-shadow');
 			this.autogrow = setupAutogrow(input, shadow, onResize);
-
-			const $input = $(input);
-			$input.on('dataChange', () => {
-				const messages = $input.data('reply') || [];
-				this.replyMessageData.set(messages);
-			});
 		});
 	});
 });
