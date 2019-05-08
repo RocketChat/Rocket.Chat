20ce36374de28379acc53db15dd4b6c0bcd656e4
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-05-08 14:15:16 -0300

    [FIX] Mentions message missing 'jump to message' action (#14430)
    * fixed context
    
    * threads context

--- app/ui-message/client/message.js
+++ app/ui-message/client/message.js
@@ -374,9 +374,13 @@ Template.message.helpers({
 		}
 		return roomTypes.getIcon(room);
 	},
+	customClass() {
+		const { customClass, msg } = this;
+		return customClass || msg.customClass;
+	},
 	fromSearch() {
-		const { customClass } = this;
-		return customClass === 'search';
+		const { customClass, msg } = this;
+		return [msg.customClass, customClass].includes('search');
 	},
 	actionContext() {
 		const { msg } = this;
