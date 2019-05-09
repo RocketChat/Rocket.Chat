056db23ff587a6697d11d6267b2560d00fc70115
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-05-08 21:25:34 -0300

    Add migration

---
+++ server/startup/migrations/v144.js
@@ -0,0 +1,22 @@
+import { Migrations } from '../../../app/migrations/server';
+import { Sessions } from '../../../app/models/server';
+
+Migrations.add({
+	version: 144,
+	up() {
+		Sessions.remove({
+			type: 'user_daily',
+		});
+
+		Sessions.update({
+			type: 'computed-session',
+		}, {
+			$set: {
+				type: 'session',
+			},
+			unset: {
+				_computedAt: 1,
+			},
+		}, { multi: true });
+	},
+});
