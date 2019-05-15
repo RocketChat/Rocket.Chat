c5efa3b151e6a1a841f907f8fc802d085da6a889
Diego Sampaio <chinello@gmail.com>
2019-05-15 16:04:54 -0300

    Send less data for status changes

--- imports/startup/client/listenActiveUsers.js
+++ imports/startup/client/listenActiveUsers.js
@@ -5,6 +5,13 @@ import { debounce } from 'underscore';
 import { Notifications } from '../../../app/notifications/client';
 import { APIClient } from '../../../app/utils/client';
 
+const STATUS_MAP = [
+	'offline',
+	'online',
+	'away',
+	'busy',
+];
+
 const saveUser = (user, force = false) => {
 	// do not update my own user, my user's status will come from a subscription
 	if (user._id === Meteor.userId()) {
@@ -62,8 +69,8 @@ Tracker.autorun(() => {
 });
 
 Meteor.startup(function() {
-	Notifications.onLogged('user-status', (user) => {
-		saveUser(user, true);
+	Notifications.onLogged('user-status', ([_id, username, status]) => {
+		saveUser({ _id, username, status: STATUS_MAP[status] }, true);
 	});
 
 	Notifications.onLogged('Users:NameChanged', ({ _id, username }) => {
--- imports/users-presence/server/activeUsers.js
+++ imports/users-presence/server/activeUsers.js
@@ -2,6 +2,13 @@ import { UserPresenceEvents } from 'meteor/konecty:user-presence';
 
 import { Notifications } from '../../../app/notifications/server';
 
+const STATUS_MAP = {
+	offline: 0,
+	online: 1,
+	away: 2,
+	busy: 3,
+};
+
 UserPresenceEvents.on('setUserStatus', (user, status/* , statusConnection*/) => {
 	const {
 		_id,
@@ -12,9 +19,9 @@ UserPresenceEvents.on('setUserStatus', (user, status/* , statusConnection*/) => 
 
 	// since this callback can be called by only one instance in the cluster
 	// we need to brodcast the change to all instances
-	Notifications.notifyLogged('user-status', {
+	Notifications.notifyLogged('user-status', [
 		_id,
 		username,
-		status,
-	});
+		STATUS_MAP[status],
+	]);
 });
