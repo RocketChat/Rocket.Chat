627622b6a3477b4486cf7ba1e3905e092b2a6fc3
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-05-24 14:08:50 -0300

    Introduce injectIntoHead and injectIntoBody

--- app/ui-master/server/inject.js
+++ app/ui-master/server/inject.js
@@ -1,10 +1,130 @@
 import { Meteor } from 'meteor/meteor';
 import { Inject } from 'meteor/meteorhacks:inject-initial';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Tracker } from 'meteor/tracker';
 import _ from 'underscore';
 import s from 'underscore.string';
 
 import { Settings } from '../../models';
 import { settings } from '../../settings';
+
+const headInjections = new ReactiveDict();
+
+export const injectIntoHead = (key, value) => {
+	headInjections.set(key, value);
+};
+
+export const injectIntoBody = (key, value) => {
+	Inject.rawBody(key, value);
+};
+
+const applyHeadInjections = (injections) => {
+	if (injections.length === 0) {
+		return (html) => html;
+	}
+
+	const replacementHtml = `${ injections.join('\n').replace(/\$/g, '$$$$') }\n</head>`;
+
+	return (html) => html.replace('</head>', replacementHtml);
+};
+
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		const injections = Object.values(headInjections.all());
+		Inject.rawModHtml('headInjections', applyHeadInjections(injections));
+	});
+
+	injectIntoHead('noreferrer', '<meta name="referrer" content="origin-when-cross-origin" />');
+	injectIntoHead('dynamic', `<script>${ Assets.getText('server/dynamic-css.js') }</script>`);
+
+	if (process.env.DISABLE_ANIMATION || process.env.TEST_MODE === 'true') {
+		injectIntoHead('disable-animation', `
+		<style>
+			body, body * {
+				animation: none !important;
+				transition: none !important;
+			}
+		</style>
+		<script>
+			window.DISABLE_ANIMATION = true;
+		</script>
+		`);
+	}
+
+	settings.get('Assets_SvgFavicon_Enable', (key, value) => {
+		const standardFavicons = `
+			<link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
+			<link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />`;
+
+		if (value) {
+			injectIntoHead(key,
+				`${ standardFavicons }
+				<link rel="icon" sizes="any" type="image/svg+xml" href="assets/favicon.svg" />`);
+		} else {
+			injectIntoHead(key, standardFavicons);
+		}
+	});
+
+	settings.get('theme-color-sidebar-background', (key, value) => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key, `<meta name="msapplication-TileColor" content="${ escapedValue }" />`
+							+ `<meta name="theme-color" content="${ escapedValue }" />`);
+	});
+
+	settings.get('Site_Name', (key, value = 'Rocket.Chat') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key,
+			`<title>${ escapedValue }</title>`
+			+ `<meta name="application-name" content="${ escapedValue }">`
+			+ `<meta name="apple-mobile-web-app-title" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_language', (key, value = '') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key,
+			`<meta http-equiv="content-language" content="${ escapedValue }">`
+			+ `<meta name="language" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_robots', (key, value = '') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key, `<meta name="robots" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_msvalidate01', (key, value = '') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key, `<meta name="msvalidate.01" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_google-site-verification', (key, value = '') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key, `<meta name="google-site-verification" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_fb_app_id', (key, value = '') => {
+		const escapedValue = s.escapeHTML(value);
+		injectIntoHead(key, `<meta property="fb:app_id" content="${ escapedValue }">`);
+	});
+
+	settings.get('Meta_custom', (key, value = '') => {
+		injectIntoHead(key, value);
+	});
+
+	Meteor.defer(() => {
+		let baseUrl;
+		if (__meteor_runtime_config__.ROOT_URL_PATH_PREFIX && __meteor_runtime_config__.ROOT_URL_PATH_PREFIX.trim() !== '') {
+			baseUrl = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX;
+		} else {
+			baseUrl = '/';
+		}
+		if (/\/$/.test(baseUrl) === false) {
+			baseUrl += '/';
+		}
+		injectIntoHead('base', `<base href="${ baseUrl }">`);
+	});
+
+	injectIntoHead('css-theme', '');
+});
 
 const renderDynamicCssList = _.debounce(Meteor.bindEnvironment(() => {
 	// const variables = RocketChat.models.Settings.findOne({_id:'theme-custom-variables'}, {fields: { value: 1}});
@@ -19,7 +139,7 @@ const renderDynamicCssList = _.debounce(Meteor.bindEnvironment(() => {
 		}
 		return `--${ _id.replace('theme-color-', '') }: ${ value };`;
 	}).join('\n');
-	Inject.rawBody('dynamic-variables', `<style id='css-variables'> :root {${ css }}</style>`);
+	injectIntoBody('dynamic-variables', `<style id='css-variables'> :root {${ css }}</style>`);
 }), 500);
 
 renderDynamicCssList();
@@ -32,12 +152,9 @@ Settings.find({ _id: /theme-color-rc/i }, { fields: { value: 1 } }).observe({
 	changed: renderDynamicCssList,
 });
 
-Inject.rawHead('noreferrer', '<meta name="referrer" content="origin-when-cross-origin" />');
-Inject.rawHead('dynamic', `<script>${ Assets.getText('server/dynamic-css.js') }</script>`);
+injectIntoBody('icons', Assets.getText('public/icons.svg'));
 
-Inject.rawBody('icons', Assets.getText('public/icons.svg'));
-
-Inject.rawBody('page-loading-div', `
+injectIntoBody('page-loading-div', `
 <div id="initial-page-loading" class="page-loading">
 	<div class="loading-animation">
 		<div class="bounce bounce1"></div>
@@ -45,40 +162,6 @@ Inject.rawBody('page-loading-div', `
 		<div class="bounce bounce3"></div>
 	</div>
 </div>`);
-
-if (process.env.DISABLE_ANIMATION || process.env.TEST_MODE === 'true') {
-	Inject.rawHead('disable-animation', `
-	<style>
-		body, body * {
-			animation: none !important;
-			transition: none !important;
-		}
-	</style>
-	<script>
-		window.DISABLE_ANIMATION = true;
-	</script>
-	`);
-}
-
-settings.get('Assets_SvgFavicon_Enable', (key, value) => {
-	const standardFavicons = `
-		<link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
-		<link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />`;
-
-	if (value) {
-		Inject.rawHead(key,
-			`${ standardFavicons }
-			<link rel="icon" sizes="any" type="image/svg+xml" href="assets/favicon.svg" />`);
-	} else {
-		Inject.rawHead(key, standardFavicons);
-	}
-});
-
-settings.get('theme-color-sidebar-background', (key, value) => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key, `<meta name="msapplication-TileColor" content="${ escapedValue }" />`
-						+ `<meta name="theme-color" content="${ escapedValue }" />`);
-});
 
 settings.get('Accounts_ForgetUserSessionOnWindowClose', (key, value) => {
 	if (value) {
@@ -95,56 +178,4 @@ settings.get('Accounts_ForgetUserSessionOnWindowClose', (key, value) => {
 	} else {
 		Inject.rawModHtml(key, (html) => html);
 	}
-});
-
-settings.get('Site_Name', (key, value = 'Rocket.Chat') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key,
-		`<title>${ escapedValue }</title>`
-		+ `<meta name="application-name" content="${ escapedValue }">`
-		+ `<meta name="apple-mobile-web-app-title" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_language', (key, value = '') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key,
-		`<meta http-equiv="content-language" content="${ escapedValue }">`
-		+ `<meta name="language" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_robots', (key, value = '') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key, `<meta name="robots" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_msvalidate01', (key, value = '') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key, `<meta name="msvalidate.01" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_google-site-verification', (key, value = '') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key, `<meta name="google-site-verification" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_fb_app_id', (key, value = '') => {
-	const escapedValue = s.escapeHTML(value);
-	Inject.rawHead(key, `<meta property="fb:app_id" content="${ escapedValue }">`);
-});
-
-settings.get('Meta_custom', (key, value = '') => {
-	Inject.rawHead(key, value);
-});
-
-Meteor.defer(() => {
-	let baseUrl;
-	if (__meteor_runtime_config__.ROOT_URL_PATH_PREFIX && __meteor_runtime_config__.ROOT_URL_PATH_PREFIX.trim() !== '') {
-		baseUrl = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX;
-	} else {
-		baseUrl = '/';
-	}
-	if (/\/$/.test(baseUrl) === false) {
-		baseUrl += '/';
-	}
-	Inject.rawHead('base', `<base href="${ baseUrl }">`);
 });
