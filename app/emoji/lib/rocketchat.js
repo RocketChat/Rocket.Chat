a487ac0615a3d81fba125bc4de6c54876cf483b6
Diego Sampaio <chinello@gmail.com>
2019-05-27 10:57:23 -0300

    Handle missing emojis

--- app/emoji/lib/rocketchat.js
+++ app/emoji/lib/rocketchat.js
@@ -1,4 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+
 import { emojioneRender } from '../../emoji-emojione/lib/emojioneRender';
+
+let EmojiPicker;
+const removeFromRecent = (emoji) => {
+	if (!Meteor.isClient) {
+		return;
+	}
+	if (!EmojiPicker) {
+		({ EmojiPicker } = require('../client/lib/EmojiPicker'));
+	}
+	EmojiPicker.removeFromRecent(emoji.replace(/(^:|:$)/g, ''));
+};
 
 export const emoji = {
 	packages: {
@@ -11,6 +24,10 @@ export const emoji = {
 			toneList: {},
 			render: emojioneRender,
 			renderPicker(emojiToRender) {
+				if (!emoji.list[emojiToRender]) {
+					removeFromRecent(emojiToRender);
+					return;
+				}
 				const correctPackage = emoji.list[emojiToRender].emojiPackage;
 				return emoji.packages[correctPackage].renderPicker(emojiToRender);
 			},
