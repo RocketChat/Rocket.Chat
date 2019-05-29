69b29d2a66079d04778e0da52c76f948b205c732
ifantom <ifantom.group@gmail.com>
2019-05-29 22:55:51 +0300

    "Discussions" item in "More" menu not hidden, when Discussions are disabled

--- app/discussion/client/tabBar.js
+++ app/discussion/client/tabBar.js
@@ -1,14 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import { TabBar } from '../../ui-utils/client';
+import { settings } from '../../settings';
 
 Meteor.startup(function() {
-	return TabBar.addButton({
+	TabBar.addButton({
 		groups: ['channel', 'group', 'direct'],
 		id: 'discussions',
 		i18nTitle: 'Discussions',
 		icon: 'discussion',
 		template: 'discussionsTabbar',
 		order: 10,
+		condition: () => settings.get('Discussion_enabled'),
 	});
 });
