9551085aaba71fe4ff44b7ddda6643961469d299
fliptrail <amishmittal@outlook.com>
2019-05-29 23:59:17 +0530

    Access all users in initialize function

--- app/newsfeed/client/index.js
+++
@@ -1,1 +0,0 @@
-console.log('Client side loaded');
--- app/newsfeed/server/initialize.js
+++ app/newsfeed/server/initialize.js
@@ -1,9 +1,16 @@
 // import { Meteor } from 'meteor/meteor';
-//
 // import { settings } from '../../settings';
+// import { Mongo } from 'meteor/mongo';
+
+import { Users } from '../../models';
 
 function initializeNewsfeed() {
-	console.log('Export working');
+	const usersArray = Users.findByUsername().fetch();
+	usersArray.forEach((user) => {
+		if (user.type === 'user') {
+			console.log(user.name);
+		}
+	});
 }
 
 export { initializeNewsfeed };
