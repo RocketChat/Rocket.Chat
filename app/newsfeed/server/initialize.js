a2a16fea1450bd1bb1c56fcdda1dd709bd2edbd2
fliptrail <amishmittal@outlook.com>
2019-05-31 01:10:01 +0530

    Check if newsfeed for a user already exists

--- app/newsfeed/server/initialize.js
+++ app/newsfeed/server/initialize.js
@@ -1,14 +1,16 @@
 // import { Meteor } from 'meteor/meteor';
 // import { settings } from '../../settings';
 // import { Mongo } from 'meteor/mongo';
-
-import { Users } from '../../models';
+import { createRoom } from '../../lib/server/functions';
+import { Users, Rooms } from '../../models';
 
 function initializeNewsfeed() {
 	const usersArray = Users.findByUsername().fetch();
 	usersArray.forEach((user) => {
 		if (user.type === 'user') {
-			console.log(user.name);
+			if (!Rooms.findOneByName(`news_${ user._id }`)) {
+				createRoom('n', `news_${ user._id }`, user.name, [], true, { }, {});
+			}
 		}
 	});
 }
