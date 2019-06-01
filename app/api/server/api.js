b279d676e20646015396ad07349bc4a7dd0a6339
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-05-31 22:03:12 -0300

    Fix wrong setting

--- app/api/server/api.js
+++ app/api/server/api.js
@@ -241,7 +241,7 @@ class APIClass extends Restivus {
 					try {
 						const shouldVerifyRateLimit = rateLimiterDictionary.hasOwnProperty(objectForRateLimitMatch.route)
 							&& (!this.userId || !hasPermission(this.userId, 'api-bypass-rate-limit'))
-							&& (settings.get('API_Enable_Rate_Limiter_Dev') || (process.env.NODE_ENV === 'development' && settings.get('API_Enable_Rate_Limiter_Dev') === true));
+							&& (settings.get('API_Enable_Rate_Limiter') || (process.env.NODE_ENV === 'development' && settings.get('API_Enable_Rate_Limiter_Dev') === true));
 						if (shouldVerifyRateLimit) {
 							rateLimiterDictionary[objectForRateLimitMatch.route].rateLimiter.increment(objectForRateLimitMatch);
 							const attemptResult = rateLimiterDictionary[objectForRateLimitMatch.route].rateLimiter.check(objectForRateLimitMatch);
