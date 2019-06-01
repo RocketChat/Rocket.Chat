d55af08d636c084a1132379d38cfc70c819b6664
fliptrail <amishmittal@outlook.com>
2019-06-01 16:50:58 +0530

    Add deinitialize newsfeed function

--- app/newsfeed/server/deinitialize.js
+++ app/newsfeed/server/deinitialize.js
@@ -1,9 +1,14 @@
 // import { Meteor } from 'meteor/meteor';
 //
 // import { settings } from '../../settings';
+import { deleteRoom } from '../../lib/server/functions';
+import { Rooms } from '../../models';
 
 function deinitializeNewsfeed() {
-	console.log('DeExport working');
+	const roomList = Rooms.findByType('n').fetch();
+	roomList.forEach((room) => {
+		deleteRoom(room._id);
+	});
 }
 
 export { deinitializeNewsfeed };
