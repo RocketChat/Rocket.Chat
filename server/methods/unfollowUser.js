dff42512866c1210c29c8456378c3b6c46631c90
fliptrail <amishmittal@outlook.com>
2019-06-09 02:18:04 +0530

    Update unfollowUser to new scheme

--- server/methods/unfollowUser.js
+++ server/methods/unfollowUser.js
@@ -7,8 +7,24 @@ import { settings } from '../../app/settings';
 Meteor.methods({
 	unfollowUser(username) {
 		if (settings.get('Newsfeed_enabled')) {
-			Users.update(Users.findOneByUsername(username)._id, { $pull: { followers: Meteor.user().username } });
-			Users.update(Users.findOneByUsername(Meteor.user().username)._id, { $pull: { following: username } });
+			// Update followers keys
+			let userObject = Users.findOneByUsername(username);
+			if ('followers' in userObject) {
+				const followersObject = userObject.followers;
+				const { _id } = Users.findOneByUsername(Meteor.user().username);
+				delete followersObject[_id];
+				Users.update(userObject._id, { $set: { followers: followersObject } });
+			}
+
+			// Update following keys
+			userObject = Users.findOneByUsername(Meteor.user().username);
+			if ('following' in userObject) {
+				const followingObject = userObject.following;
+				const { _id } = Users.findOneByUsername(username);
+				delete followingObject[_id];
+				Users.update(userObject._id, { $set: { following: followingObject } });
+			}
+
 			return true;
 		}
 
