08a7168898787580383564228fba34c279a7cc39
fliptrail <amishmittal@outlook.com>
2019-06-09 15:52:12 +0530

    Add to deInitialize newsfeed to remove following and followers from database

--- app/newsfeed/server/deinitialize.js
+++ app/newsfeed/server/deinitialize.js
@@ -2,13 +2,15 @@
 //
 // import { settings } from '../../settings';
 import { deleteRoom } from '../../lib/server/functions';
-import { Rooms } from '../../models';
+import { Rooms, Users } from '../../models';
 
 function deinitializeNewsfeed() {
 	const roomList = Rooms.findByType('n').fetch();
 	roomList.forEach((room) => {
 		deleteRoom(room._id);
 	});
+	Users.update({}, { $unset: { followers: 1 } }, { multi: true });
+	Users.update({}, { $unset: { following: 1 } }, { multi: true });
 }
 
 export { deinitializeNewsfeed };
