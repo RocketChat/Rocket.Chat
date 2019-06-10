9010f355c96bf24ca581fd68e08639db0ffe81c4
fliptrail <amishmittal@outlook.com>
2019-06-10 16:26:05 +0530

    based newsfeed type room on private group

--- app/lib/lib/roomTypes/newsfeed.js
+++ app/lib/lib/roomTypes/newsfeed.js
@@ -1,10 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
-import { openRoom } from '../../../ui-utils';
 import { ChatRoom, ChatSubscription } from '../../../models';
+import { openRoom } from '../../../ui-utils';
 import { settings } from '../../../settings';
-import { hasAtLeastOnePermission } from '../../../authorization';
-import { getUserPreference, RoomTypeConfig, RoomTypeRouteConfig, RoomSettingsEnum, UiTextContext } from '../../../utils';
+import { hasAtLeastOnePermission, hasPermission } from '../../../authorization';
+import { getUserPreference, RoomSettingsEnum, RoomTypeConfig, RoomTypeRouteConfig, UiTextContext, roomTypes } from '../../../utils';
+import { getRoomAvatarURL } from '../../../utils/lib/getRoomAvatarURL';
 import { getAvatarURL } from '../../../utils/lib/getAvatarURL';
 
 export class NewsfeedRoomRoute extends RoomTypeRouteConfig {
@@ -43,6 +44,7 @@ export class NewsfeedRoomType extends RoomTypeConfig {
 			t: 'n',
 			name: identifier,
 		};
+
 		return ChatRoom.findOne(query);
 	}
 
@@ -53,20 +55,13 @@ export class NewsfeedRoomType extends RoomTypeConfig {
 		if (settings.get('UI_Allow_room_names_with_special_chars')) {
 			return roomData.fname || roomData.name;
 		}
+
 		return roomData.name;
 	}
 
 	condition() {
 		const groupByType = getUserPreference(Meteor.userId(), 'sidebarGroupByType');
-		return groupByType && (hasAtLeastOnePermission(['view-c-room', 'view-joined-room']) || settings.get('Accounts_AllowAnonymousRead') === true);
-	}
-
-	showJoinLink(roomId) {
-		return !!ChatRoom.findOne({ _id: roomId, t: 'n' });
-	}
-
-	includeInRoomSearch() {
-		return true;
+		return groupByType && hasPermission('view-p-room');
 	}
 
 	isGroupChat() {
@@ -74,47 +69,44 @@ export class NewsfeedRoomType extends RoomTypeConfig {
 	}
 
 	canAddUser(room) {
-		return hasAtLeastOnePermission(['add-user-to-any-c-room', 'add-user-to-joined-room'], room._id);
+		return hasAtLeastOnePermission(['add-user-to-any-p-room', 'add-user-to-joined-room'], room._id);
 	}
 
 	canSendMessage(roomId) {
-		const room = ChatRoom.findOne({ _id: roomId, t: 'n' }, { fields: { prid: 1 } });
-		if (room.prid) {
-			return true;
-		}
-
 		// TODO: remove duplicated code
 		return ChatSubscription.find({
 			rid: roomId,
 		}).count() > 0;
 	}
 
-	enableMembersListProfile() {
-		return true;
-	}
-
 	allowRoomSettingChange(room, setting) {
 		switch (setting) {
+			case RoomSettingsEnum.JOIN_CODE:
+				return false;
 			case RoomSettingsEnum.BROADCAST:
 				return room.broadcast;
 			case RoomSettingsEnum.READ_ONLY:
 				return !room.broadcast;
 			case RoomSettingsEnum.REACT_WHEN_READ_ONLY:
 				return !room.broadcast && room.ro;
-			case RoomSettingsEnum.E2E:
-				return false;
 			case RoomSettingsEnum.SYSTEM_MESSAGES:
+			case RoomSettingsEnum.E2E:
+				return settings.get('E2E_Enable') === true;
 			default:
 				return true;
 		}
 	}
 
+	enableMembersListProfile() {
+		return true;
+	}
+
 	getUiText(context) {
 		switch (context) {
 			case UiTextContext.HIDE_WARNING:
-				return 'Hide_Room_Warning';
+				return 'Hide_Group_Warning';
 			case UiTextContext.LEAVE_WARNING:
-				return 'Leave_Room_Warning';
+				return 'Leave_Group_Warning';
 			default:
 				return '';
 		}
@@ -123,6 +115,18 @@ export class NewsfeedRoomType extends RoomTypeConfig {
 	getAvatarPath(roomData) {
 		// TODO: change to always get avatar from _id when rooms have avatars
 
-		return getAvatarURL({ username: `@${ this.roomName(roomData) }` });
+		// if room is not a discussion, returns the avatar for its name
+		if (!roomData.prid) {
+			return getAvatarURL({ username: `@${ this.roomName(roomData) }` });
+		}
+
+		// if discussion's parent room is known, get his avatar
+		const proom = ChatRoom.findOne({ _id: roomData.prid }, { reactive: false });
+		if (proom) {
+			return roomTypes.getConfig(proom.t).getAvatarPath(proom);
+		}
+
+		// otherwise gets discussion's avatar via _id
+		return getRoomAvatarURL(roomData.prid);
 	}
 }
