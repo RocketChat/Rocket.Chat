ecb1e73b3d2bbdf8a12aaa2753ecb157b70afdac
Diego Sampaio <chinello@gmail.com>
2019-06-11 16:55:34 -0300

    Clean up

--- app/models/server/raw/Roles.js
+++ app/models/server/raw/Roles.js
@@ -3,15 +3,6 @@ import * as Models from '..';
 import { BaseRaw } from './BaseRaw';
 
 export class RolesRaw extends BaseRaw {
-	// async findUsersInRole(name, scope, options) {
-	// 	const role = await this.findOne({ _id: name });
-	// 	const roleScope = (role && role.scope) || 'Users';
-	// 	console.log('roleScope ->', roleScope);
-	// 	const model = Models[roleScope];
-
-	// 	return model && model.findUsersInRoles && model.findUsersInRoles(name, scope, options);
-	// }
-
 	async isUserInRoles(userId, roles, scope) {
 		roles = [].concat(roles);
 
--- app/models/server/raw/Subscriptions.js
+++ app/models/server/raw/Subscriptions.js
@@ -10,28 +10,6 @@ export class SubscriptionsRaw extends BaseRaw {
 		return this.col.findOne(query, options);
 	}
 
-	// async findUsersInRoles(roles, scope, options) {
-	// 	roles = [].concat(roles);
-
-	// 	const query = {
-	// 		roles: { $in: roles },
-	// 	};
-
-	// 	if (scope) {
-	// 		query.rid = scope;
-	// 	}
-
-	// 	const subscriptions = await this.find(query).toArray();
-
-	// 	const users = subscriptions
-	// 		.filter((sub) => sub.u && sub.u._id)
-	// 		.map((sub) => sub.u._id);
-
-	// 	console.log('users ->', users);
-
-	// 	return Users.find({ _id: { $in: users } }, options);
-	// }
-
 	isUserInRole(userId, roleName, rid) {
 		if (rid == null) {
 			return;
