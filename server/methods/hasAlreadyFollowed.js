15e9fe9b727609ca2912dc3c48903ca09b7e3554
fliptrail <amishmittal@outlook.com>
2019-07-01 01:38:30 +0530

    Update hasAlreadyFollowed for new schema

--- server/methods/hasAlreadyFollowed.js
+++ server/methods/hasAlreadyFollowed.js
@@ -1,16 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 
-import { Users } from '../../app/models';
+import { Users, UserRelations } from '../../app/models';
 import { settings } from '../../app/settings';
 
 Meteor.methods({
 	hasAlreadyFollowed(username) {
-		if (settings.get('Newsfeed_enabled')) {
-			if ('followers' in Users.findOneByUsername(username)) {
-				if (`${ Meteor.user()._id }` in Users.findOneByUsername(username).followers) {
-					return true;
-				}
-			}
+		if (!settings.get('Newsfeed_enabled')) {
+			return false;
+		}
+
+		const { _id: followingId } = Users.findOneByUsername(username, { fields: { _id: 1 } });
+
+		if (UserRelations.find({ following: followingId, follower: Meteor.userId() }, { limit: 1 }).fetch()[0]) {
+			return true;
 		}
 		return false;
 	},
