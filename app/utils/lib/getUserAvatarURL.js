fd44c813d5340dea9e406f4ddcfb8fd96ebade4b
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-07-12 02:07:27 -0300

    [IMPROVEMENT] patch to improve emoji render (#14722)
    * path to improve emoji render
    
    * Apply suggestions from code review
    
    Co-Authored-By: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- app/utils/lib/getUserAvatarURL.js
+++ app/utils/lib/getUserAvatarURL.js
@@ -1,19 +1,40 @@
 import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { getAvatarURL } from './getAvatarURL';
 import { settings } from '../../settings';
 
-export const getUserAvatarURL = function(username) {
-	const externalSource = (settings.get('Accounts_AvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
-	if (externalSource !== '') {
-		return externalSource.replace('{username}', username);
-	}
-	if (username == null) {
-		return;
-	}
-	const key = `avatar_random_${ username }`;
-	const cache = Tracker.nonreactive(() => Session && Session.get(key)); // there is no Session on server
+let externalSource = '';
 
-	return getAvatarURL({ username, cache });
-};
+if (Meteor.isServer) {
+	settings.get('Accounts_AvatarExternalProviderUrl', (key, value = '') => {
+		externalSource = value.trim().replace(/\/$/, '');
+	});
+} else {
+	Tracker.autorun(function() {
+		externalSource = (settings.get('Accounts_AvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
+	});
+}
+
+export const getUserAvatarURL = Meteor.isServer
+	? function(username) {
+		if (username == null) {
+			return;
+		}
+		if (externalSource !== '') {
+			return externalSource.replace('{username}', username);
+		}
+		return getAvatarURL({ username });
+	}
+	: function(username) {
+		if (username == null) {
+			return;
+		}
+		if (externalSource !== '') {
+			return externalSource.replace('{username}', username);
+		}
+		const key = `avatar_random_${ username }`;
+		const cache = Tracker.nonreactive(() => Session.get(key));
+		return getAvatarURL({ username, cache });
+	};
