e7fa1a92dff2306a748e08093ab72274dc6f1b53
Pierre Lehnen <brian@hudell.com>
2019-07-20 13:29:09 -0300

    Removed reactive callback from migration

--- server/startup/migrations/v148.js
+++ server/startup/migrations/v148.js
@@ -5,18 +5,16 @@ import { Settings } from '../../../app/models/server';
 Migrations.add({
 	version: 148,
 	up() {
-		settings.get('Accounts_OAuth_Gitlab', function(key, value) {
-			if (value) {
-				const setting = Settings.findById('Accounts_OAuth_Gitlab_identity_path');
-				const newValue = '/api/v3/user';
+		if (settings.get('Accounts_OAuth_Gitlab')) {
+			const setting = Settings.findById('Accounts_OAuth_Gitlab_identity_path');
+			const newValue = '/api/v3/user';
 
-				if (setting) {
-					Settings.updateValueById('Accounts_OAuth_Gitlab_identity_path', newValue);
-				} else {
-					Settings.createWithIdAndValue('Accounts_OAuth_Gitlab_identity_path', newValue);
-				}
+			if (setting) {
+				Settings.updateValueById('Accounts_OAuth_Gitlab_identity_path', newValue);
+			} else {
+				Settings.createWithIdAndValue('Accounts_OAuth_Gitlab_identity_path', newValue);
 			}
-		});
+		}
 	},
 	down() {
 		// Down migration does not apply in this case
