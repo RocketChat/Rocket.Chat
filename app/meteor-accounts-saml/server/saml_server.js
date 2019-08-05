2ec9aaac4bd9cff13a3e34d6877317a28e03ae90
Maximilian Ruta <mr@xtain.net>
2019-08-05 14:48:41 +0200

    More options for SAML auth for individual organization needs. Fix #6481
    Signed-off-by: Maximilian Ruta <mr@xtain.net>

--- app/meteor-accounts-saml/server/saml_server.js
+++ app/meteor-accounts-saml/server/saml_server.js
@@ -10,6 +10,7 @@ import _ from 'underscore';
 import { SAML } from './saml_utils';
 import { CredentialTokens } from '../../models';
 import { generateUsernameSuggestion } from '../../lib';
+import { _setUsername } from '../../lib/server/functions';
 
 if (!Accounts.saml) {
 	Accounts.saml = {
@@ -94,6 +95,16 @@ Meteor.methods({
 	},
 });
 
+Accounts.normalizeUsername = function(name) {
+	switch (Accounts.saml.settings.usernameNormalize) {
+		case 'Lowercase':
+			name = name.toLowerCase();
+			break;
+	}
+
+	return name;
+};
+
 Accounts.registerLoginHandler(function(loginRequest) {
 	if (!loginRequest.saml || !loginRequest.credentialToken) {
 		return undefined;
@@ -111,8 +122,10 @@ Accounts.registerLoginHandler(function(loginRequest) {
 		};
 	}
 
+	const { emailField, usernameField } = Accounts.saml.settings;
+
 	if (loginResult && loginResult.profile && loginResult.profile.email) {
-		const emailList = Array.isArray(loginResult.profile.email) ? loginResult.profile.email : [loginResult.profile.email];
+		const emailList = Array.isArray(loginResult.profile[emailField]) ? loginResult.profile[emailField] : [loginResult.profile[emailField]];
 		const emailRegex = new RegExp(emailList.map((email) => `^${ RegExp.escape(email) }$`).join('|'), 'i');
 
 		const eduPersonPrincipalName = loginResult.profile.eppn;
@@ -132,12 +145,28 @@ Accounts.registerLoginHandler(function(loginRequest) {
 			}
 		}
 
+		let username;
+		if (loginResult.profile[usernameField]) {
+			username = Accounts.normalizeUsername(loginResult.profile[usernameField]);
+		}
+
 		// If eppn is not exist
 		if (!user) {
-			user = Meteor.users.findOne({
-				'emails.address': emailRegex,
-			});
+			if (Accounts.saml.settings.immutableProperty === 'Username') {
+				user = Meteor.users.findOne({
+					username,
+				});
+			} else {
+				user = Meteor.users.findOne({
+					'emails.address': emailRegex,
+				});
+			}
 		}
+
+		const emails = emailList.map((email) => ({
+			address: email,
+			verified: true,
+		}));
 
 		if (!user) {
 			const newUser = {
@@ -145,19 +174,15 @@ Accounts.registerLoginHandler(function(loginRequest) {
 				active: true,
 				eppn: eduPersonPrincipalName,
 				globalRoles: ['user'],
-				emails: emailList.map((email) => ({
-					address: email,
-					verified: true,
-				})),
+				emails,
 			};
 
 			if (Accounts.saml.settings.generateUsername === true) {
-				const username = generateUsernameSuggestion(newUser);
-				if (username) {
-					newUser.username = username;
-				}
-			} else if (loginResult.profile.username) {
-				newUser.username = loginResult.profile.username;
+				username = generateUsernameSuggestion(newUser);
+			}
+
+			if (username) {
+				newUser.username = username;
 			}
 
 			const userId = Accounts.insertUserDoc({}, newUser);
@@ -196,8 +221,11 @@ Accounts.registerLoginHandler(function(loginRequest) {
 			$set: {
 				// TBD this should be pushed, otherwise we're only able to SSO into a single IDP at a time
 				'services.saml': samlLogin,
+				emails,
 			},
 		});
+
+		_setUsername(user._id, username);
 
 		// Overwrite fullname if needed
 		if (Accounts.saml.settings.nameOverwrite === true) {
