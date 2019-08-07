c509cb4a39b01ff6744a3ee6bbfc39a0628ffda6
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-08-07 15:43:29 -0300

    [FIX] Direct Message names not visible on Admin panel (#15114)

--- app/ui-admin/client/rooms/adminRooms.js
+++ app/ui-admin/client/rooms/adminRooms.js
@@ -12,18 +12,19 @@ import { SideNav, RocketChatTabBar, TabBar } from '../../../ui-utils';
 import { t, roomTypes } from '../../../utils';
 import { hasAllPermission } from '../../../authorization';
 import { ChannelSettings } from '../../../channel-settings';
+import { getAvatarURL } from '../../../utils/lib/getAvatarURL';
 
 export const AdminChatRoom = new Mongo.Collection('rocketchat_room');
 
 Template.adminRooms.helpers({
 	url() {
-		return roomTypes.getConfig(this.t).getAvatarPath(this);
+		return this.t === 'd' ? getAvatarURL({ username: `@${ this.usernames[0] }` }) : roomTypes.getConfig(this.t).getAvatarPath(this);
 	},
 	getIcon() {
 		return roomTypes.getIcon(this);
 	},
 	roomName() {
-		return roomTypes.getRoomName(this.t, this);
+		return this.t === 'd' ? this.usernames.join(' x ') : roomTypes.getRoomName(this.t, this);
 	},
 	searchText() {
 		const instance = Template.instance();
@@ -51,9 +52,6 @@ Template.adminRooms.helpers({
 	roomCount() {
 		const rooms = Template.instance().rooms();
 		return rooms && rooms.count();
-	},
-	name() {
-		return roomTypes.roomTypes[this.t].getDisplayName(this);
 	},
 	type() {
 		return TAPi18n.__(roomTypes.roomTypes[this.t].label);
