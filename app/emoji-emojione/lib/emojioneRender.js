a021937fdecaff6e202fccfed2359e11c59d8801
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-08-08 11:46:50 -0300

    fix emoji

--- app/emoji-emojione/lib/emojioneRender.js
+++ app/emoji-emojione/lib/emojioneRender.js
@@ -1,9 +1,9 @@
-import { toImage, shortnameToImage } from 'emojione';
+import emojione from 'emojione';
 
 export function emojioneRender(message) {
-	return toImage(message);
+	return emojione.toImage(message);
 }
 
 export function emojioneRenderFromShort(message) {
-	return shortnameToImage(message);
+	return emojione.shortnameToImage(message);
 }
