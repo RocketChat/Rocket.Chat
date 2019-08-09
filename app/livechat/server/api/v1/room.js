931634ebe82b19b03949aa267fd5c46f0f8dc8a2
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-08-09 16:31:39 -0300

    Created an endpoint to get all livechat rooms with filters

--- app/livechat/server/api/v1/room.js
+++ app/livechat/server/api/v1/room.js
@@ -7,7 +7,7 @@ import { settings as rcSettings } from '../../../../settings';
 import { Messages, Rooms } from '../../../../models';
 import { hasPermission } from '../../../../authorization/server';
 import { API } from '../../../../api';
-import { findGuest, findRoom, getRoom, settings, findAgent } from '../lib/livechat';
+import { findGuest, findRoom, getRoom, settings, findAgent, findRooms } from '../lib/livechat';
 import { Livechat } from '../../lib/Livechat';
 
 API.v1.addRoute('livechat/room', {
@@ -50,8 +50,39 @@ API.v1.addRoute('livechat/rooms', { authRequired: true }, {
 				return API.v1.unauthorized();
 			}
 			const { offset, count } = this.getPaginationItems();
-			const { sort } = this.parseJsonQuery();
-			const { agents } = this.requestParams();
+			const { sort, fields } = this.parseJsonQuery();
+			const { agents, departmentId, open, tags } = this.requestParams();
+			let { createdAt, customFields, closedAt } = this.requestParams();
+			check(agents, Match.Maybe([String]));
+			check(departmentId, Match.Maybe(String));
+			check(open, Match.Maybe(String));
+			check(tags, Match.Maybe([String]));
+			if (createdAt) {
+				createdAt = JSON.parse(createdAt);
+			}
+			if (closedAt) {
+				closedAt = JSON.parse(closedAt);
+			}
+			if (customFields) {
+				customFields = JSON.parse(customFields);
+			}
+			const { rooms, total } = findRooms({
+				agents,
+				departmentId,
+				open: open === undefined ? undefined : open === 'true',
+				createdAt,
+				closedAt,
+				tags,
+				customFields,
+				options: { offset, count, sort, fields },
+			});
+
+			return API.v1.success({
+				rooms,
+				count: rooms.length,
+				offset,
+				total,
+			});
 		} catch (e) {
 			return API.v1.failure(e);
 		}
