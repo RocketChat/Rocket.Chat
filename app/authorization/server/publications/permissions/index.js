b09fd79cd71e1535bd85cbb2c5395a7f20f7937f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-08-15 14:40:30 -0300

    emiiter

--- app/authorization/server/publications/permissions/index.js
+++ app/authorization/server/publications/permissions/index.js
@@ -1,9 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import Permissions from '../../../../models/server/models/Permissions';
-import Settings from '../../../../models/server/models/Settings';
-import { Notifications } from '../../../../notifications/server';
-import { CONSTANTS } from '../../../lib';
+import './emitter';
 
 Meteor.methods({
 	'permissions/get'(updatedAt) {
@@ -15,33 +13,14 @@ Meteor.methods({
 		if (updatedAt instanceof Date) {
 			return {
 				update: records.filter((record) => record._updatedAt > updatedAt),
-				remove: Permissions.trashFindDeletedAfter(updatedAt, {}, { fields: { _id: 1, _deletedAt: 1 } }).fetch(),
+				remove: Permissions.trashFindDeletedAfter(
+					updatedAt,
+					{},
+					{ fields: { _id: 1, _deletedAt: 1 } }
+				).fetch(),
 			};
 		}
 
 		return records;
 	},
-});
-
-Permissions.on('change', ({ clientAction, id, data }) => {
-	switch (clientAction) {
-		case 'updated':
-		case 'inserted':
-			data = data || Permissions.findOneById(id);
-			break;
-
-		case 'removed':
-			data = { _id: id };
-			break;
-	}
-
-	Notifications.notifyLoggedInThisInstance('permissions-changed', clientAction, data);
-
-	if (data.level && data.level === CONSTANTS.SETTINGS_LEVEL) {
-		// if the permission changes, the effect on the visible settings depends on the role affected.
-		// The selected-settings-based consumers have to react accordingly and either add or remove the
-		// setting from the user's collection
-		const setting = Settings.findOneById(data.settingId);
-		Notifications.notifyLoggedInThisInstance('private-settings-changed', 'auth', setting);
-	}
 });
