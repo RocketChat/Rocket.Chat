f46933bea281f85ece215048ca9783ed69545233
Alan Sikora <alansikora@gmail.com>
2019-08-15 19:11:32 -0300

    added bogus dashboard file for now

---
+++ app/federation/server/methods/dashboard.js
@@ -0,0 +1,8 @@
+export function getStatistics() {
+	const numberOfEvents = 0;
+	const numberOfFederatedUsers = 0;
+	const numberOfActivePeers = 0;
+	const numberOfInactivePeers = 0;
+
+	return { numberOfEvents, numberOfFederatedUsers, numberOfActivePeers, numberOfInactivePeers };
+}
