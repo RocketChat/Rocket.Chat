37731a7ba30f827e9ed6d1eb9c3ab627385f02f9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-08-14 23:16:52 -0300

    Update Setup Wizards pager

--- client/components/setupWizard/Pager.js
+++ client/components/setupWizard/Pager.js
@@ -1,19 +1,17 @@
+import { Button, ButtonGroup } from '@rocket.chat/fuselage';
 import React from 'react';
-import { Button } from '@rocket.chat/fuselage';
 
 import { useTranslation } from '../../hooks/useTranslation';
-
-import './Pager.css';
 
 export function Pager({ disabled, onBackClick, isContinueEnabled = true }) {
 	const t = useTranslation();
 
-	return <footer className='Pager'>
+	return <ButtonGroup align='start'>
 		{onBackClick ? <Button type='button' disabled={disabled} onClick={onBackClick}>
 			{t('Back')}
 		</Button> : null}
 		<Button type='submit' primary disabled={!isContinueEnabled || disabled}>
 			{t('Continue')}
 		</Button>
-	</footer>;
+	</ButtonGroup>;
 }
