0858217065c9da046d87df08a029e0f6ccc16841
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-08-15 14:40:30 -0300

    emiiter

--- app/ui-admin/client/SettingsCachedCollection.js
+++ app/ui-admin/client/SettingsCachedCollection.js
@@ -1,9 +1,5 @@
-import _ from 'underscore';
-
 import { CachedCollection } from '../../ui-cached-collection';
 import { Notifications } from '../../notifications/client';
-import { hasAllPermission, hasAtLeastOnePermission } from '../../authorization/client';
-import { RoomManager } from '../../ui-utils/client';
 
 export class PrivateSettingsCachedCollection extends CachedCollection {
 	constructor() {
@@ -15,24 +11,11 @@ export class PrivateSettingsCachedCollection extends CachedCollection {
 	}
 
 	async setupListener(eventType, eventName) {
-		await super.setupListener(eventType, eventName);
-
 		// private settings also need to listen to a change of authorizations for the setting-based authorizations
-		Notifications[eventType || this.eventType](eventName || this.eventName, async (t, record) => {
-			this.log('record received', t, record);
-			if (t === 'auth') { // TODO remove that
-				if (hasAtLeastOnePermission('view-privileged-setting', 'edit-privileged-setting')
-					|| hasAllPermission([`change-setting-${ record._id }`, 'manage-selected-settings'])) {
-					delete record.$loki;
-					this.collection.upsert({ _id: record._id }, _.omit(record, '_id'));
-				} else {
-					this.collection.remove(record._id);
-					RoomManager.close(record.t + record.name);
-				}
-
-				this.save();
-				await this.sync();
-			}
+		Notifications[eventType || this.eventType](eventName || this.eventName, async (t, { _id, ...record }) => {
+			this.log('record received', t, { _id, ...record });
+			this.collection.upsert({ _id }, record);
+			this.sync();
 		});
 	}
 }
