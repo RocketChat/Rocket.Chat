7d3d3edd6e634c1af7fa165e08be3de5d51a0757
Diego Sampaio <chinello@gmail.com>
2019-08-19 09:41:14 -0300

    Remove REST endpoint /api/v1/emoji-custom

--- tests/end-to-end/api/12-emoji-custom.js
+++ tests/end-to-end/api/12-emoji-custom.js
@@ -8,28 +8,6 @@ describe('[EmojiCustom]', function() {
 	this.retries(0);
 
 	before((done) => getCredentials(done));
-	// DEPRECATED
-	// Will be removed after v1.12.0
-	describe('[/emoji-custom]', () => {
-		it('should return emojis', (done) => {
-			request.get(api('emoji-custom'))
-				.set(credentials)
-				.expect(200)
-				.expect((res) => {
-					expect(res.body).to.have.property('emojis').and.to.be.a('array');
-				})
-				.end(done);
-		});
-		it('should return emojis when use "query" query parameter', (done) => {
-			request.get(api('emoji-custom?query={"_updatedAt": {"$gt": { "$date": "2018-11-27T13:52:01Z" } }}'))
-				.set(credentials)
-				.expect(200)
-				.expect((res) => {
-					expect(res.body).to.have.property('emojis').and.to.be.a('array');
-				})
-				.end(done);
-		});
-	});
 
 	describe('[/emoji-custom.create]', () => {
 		it('should create new custom emoji', (done) => {
