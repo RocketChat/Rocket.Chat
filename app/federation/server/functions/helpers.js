7164a2db7a6c8fec63ac2941fa762bdabab9af5c
Diego Sampaio <chinello@gmail.com>
2019-09-10 00:36:01 -0300

    Add federation back (#15283)

---
+++ app/federation/server/functions/helpers.js
@@ -0,0 +1,62 @@
+import { Settings, Subscriptions, Users } from '../../../models/server';
+
+export const getNameAndDomain = (fullyQualifiedName) => fullyQualifiedName.split('@');
+export const isFullyQualified = (name) => name.indexOf('@') !== -1;
+
+export function updateStatus(status) {
+	Settings.updateValueById('FEDERATION_Status', status);
+}
+
+export function updateEnabled(enabled) {
+	Settings.updateValueById('FEDERATION_Enabled', enabled);
+}
+
+export const isFederated = (resource) => !!resource.federation;
+
+export const hasExternalDomain = ({ federation }) => {
+	// same test as isFederated(room)
+	if (!federation) {
+		return false;
+	}
+
+	return federation.domains
+		.some((domain) => domain !== federation.origin);
+};
+
+export const isLocalUser = ({ federation }, localDomain) =>
+	!federation || federation.origin === localDomain;
+
+export const getFederatedRoomData = (room) => {
+	let hasFederatedUser = false;
+
+	let users = null;
+	let subscriptions = null;
+
+	if (room.t === 'd') {
+		// Check if there is a federated user on this room
+		hasFederatedUser = room.usernames.find((u) => u.indexOf('@') !== -1);
+	} else {
+		// Find all subscriptions of this room
+		subscriptions = Subscriptions.findByRoomIdWhenUsernameExists(room._id).fetch();
+		subscriptions = subscriptions.reduce((acc, s) => {
+			acc[s.u._id] = s;
+
+			return acc;
+		}, {});
+
+		// Get all user ids
+		const userIds = Object.keys(subscriptions);
+
+		// Load all the users
+		users = Users.findUsersWithUsernameByIds(userIds).fetch();
+
+		// Check if there is a federated user on this room
+		hasFederatedUser = users.find((u) => u.username.indexOf('@') !== -1);
+	}
+
+	return {
+		hasFederatedUser,
+		users,
+		subscriptions,
+	};
+};
