d4b87f6a0d7184939f90df6156c5afc6729c33aa
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-09-16 17:48:18 -0300

    Refactor burger template

--- app/ui/client/views/app/burger.js
+++ app/ui/client/views/app/burger.js
@@ -1,31 +1,40 @@
 import { Blaze } from 'meteor/blaze';
 import { Template } from 'meteor/templating';
 
-Template.burger = new Blaze.Template(name, () => null);
-
-Template.burger.onRendered(() => {
+const onRendered = (getComponent) => () => {
 	const instance = Template.instance();
+
 	(async () => {
-		const React = await import('react');
 		const ReactDOM = await import('react-dom');
-		const { Burger } = await import('../../../../../client/components/basic/Burger');
+		instance.ReactDOM = ReactDOM;
+
+		const React = await import('react');
+		const component = await getComponent();
 
 		instance.autorun((computation) => {
 			if (computation.firstRun) {
 				instance.container = instance.firstNode.parentElement;
 			}
 
-			ReactDOM.render(React.createElement(Burger), instance.container);
+			const props = Template.currentData();
+
+			ReactDOM.render(React.createElement(component, props), instance.container);
 		});
 	})();
-});
+};
 
-Template.burger.onDestroyed(() => {
-	const { container } = Template.instance();
-	(async () => {
-		const ReactDOM = await import('react-dom');
-		if (container) {
-			ReactDOM.unmountComponentAtNode(container);
-		}
-	})();
-});
+const onDestroyed = () => {
+	const { container, ReactDOM } = Template.instance();
+	if (container) {
+		ReactDOM.unmountComponentAtNode(container);
+	}
+};
+
+Template.burger = new Blaze.Template(name, () => null);
+
+Template.burger.onRendered(onRendered(async () => {
+	const { Burger } = await import('../../../../../client/components/basic/Burger');
+	return Burger;
+}));
+
+Template.burger.onDestroyed(onDestroyed);
