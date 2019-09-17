b1c1b40d0490ce7a5192dedcdd2e4c9e7a0eacba
marco-aa <marco-aa@local.com>
2019-09-17 22:00:37 +0000

    fixed jslint

--- app/ui-flextab/client/tabs/userInfo.js
+++ app/ui-flextab/client/tabs/userInfo.js
@@ -70,15 +70,15 @@ Template.userInfo.helpers({
 		return customFields;
 	},
 
-        customFieldIsUrl(str) {
-          var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-         '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-         '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-         return !!pattern.test(str);
-        },
+	customFieldIsUrl(str) {
+		const pattern = new RegExp('^(https?:\\/\\/)?' // protocol
+         + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
+         + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+         + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
+         + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
+         + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+		return !!pattern.test(str);
+	},
 
 	name() {
 		const user = Template.instance().user.get();
