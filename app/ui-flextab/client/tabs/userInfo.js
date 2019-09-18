4baf22ceab19375cc44321f790378e1c98a9191f
marco-aa <m.amato@gmx.de>
2019-09-18 10:48:29 +0000

    Added href for userinfo-tab and helper to check if field is url

--- app/ui-flextab/client/tabs/userInfo.js
+++ app/ui-flextab/client/tabs/userInfo.js
@@ -70,6 +70,16 @@ Template.userInfo.helpers({
 		return customFields;
 	},
 
+	customFieldIsUrl(str) {
+		const pattern = new RegExp('^(https?:\\/\\/)?' // protocol
+         + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
+         + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+         + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
+         + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
+         + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+		return !!pattern.test(str);
+	},
+
 	name() {
 		const user = Template.instance().user.get();
 		return user && user.name ? user.name : TAPi18n.__('Unnamed');
