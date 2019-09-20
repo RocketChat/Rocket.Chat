da44a1d2d3af08af172ed41da270c5f51071fcdb
Diego Sampaio <chinello@gmail.com>
2019-09-20 15:57:57 -0300

    Change default value for notifications to all

---
+++ server/startup/migrations/v160.js
@@ -0,0 +1,21 @@
+import { Migrations } from '../../../app/migrations/server';
+import { Settings } from '../../../app/models/server';
+
+Migrations.add({
+	version: 160,
+	up() {
+		const desktopNotifications = Settings.findOne({ _id: 'Accounts_Default_User_Preferences_desktopNotifications' });
+		const mobileNotifications = Settings.findOne({ _id: 'Accounts_Default_User_Preferences_mobileNotifications' });
+
+		if (desktopNotifications && desktopNotifications.value === 'mentions') {
+			Settings.update({ _id: 'Accounts_Default_User_Preferences_desktopNotifications' }, { value: 'all' });
+		}
+
+		if (mobileNotifications && mobileNotifications.value === 'mentions') {
+			Settings.update({ _id: 'Accounts_Default_User_Preferences_mobileNotifications' }, { value: 'all' });
+		}
+	},
+	down() {
+		// Down migration does not apply in this case
+	},
+});
