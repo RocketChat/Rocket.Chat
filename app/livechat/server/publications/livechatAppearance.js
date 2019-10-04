61ad656068632fc67d6b8f6d87048a6fb224a725
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-10-04 15:16:52 -0300

    Replace livechat:apperance pub to REST

--- app/livechat/server/publications/livechatAppearance.js
+++ app/livechat/server/publications/livechatAppearance.js
@@ -4,6 +4,7 @@ import { hasPermission } from '../../../authorization';
 import { Settings } from '../../../models';
 
 Meteor.publish('livechat:appearance', function() {
+	console.warn('The publication "livechat:appearance" is deprecated and will be removed after version v3.0.0');
 	if (!this.userId) {
 		return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:appearance' }));
 	}
