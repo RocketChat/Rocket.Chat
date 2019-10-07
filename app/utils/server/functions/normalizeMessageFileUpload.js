b7d70c1673d07b2bf697090e1c7c62f06f147fd6
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-10-07 09:03:27 -0300

    Use getURL isntead of Meteor.absoluteUrl

--- app/utils/server/functions/normalizeMessageFileUpload.js
+++ app/utils/server/functions/normalizeMessageFileUpload.js
@@ -1,5 +1,4 @@
-import { Meteor } from 'meteor/meteor';
-
+import { getURL } from '../../lib/getURL';
 import { FileUpload } from '../../../file-upload/server';
 import { Uploads } from '../../../models/server';
 
@@ -11,7 +10,7 @@ export const normalizeMessageFileUpload = (message) => {
 			return message;
 		}
 		message.fileUpload = {
-			publicFilePath: Meteor.absoluteUrl(`${ FileUpload.getPath(`${ file._id }/${ encodeURI(file.name) }`).substring(1) }${ jwt ? `?token=${ jwt }` : '' }`),
+			publicFilePath: getURL(`${ FileUpload.getPath(`${ file._id }/${ encodeURI(file.name) }`).substring(1) }${ jwt ? `?token=${ jwt }` : '' }`, { cdn: false, full: true }),
 			type: file.type,
 			size: file.size,
 		};
