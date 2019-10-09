87cb811f2f10b2f6035a02a7b2725daac0bb3760
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-10-09 16:19:57 -0300

    Replace starred messages subscription

--- app/api/server/lib/messages.js
+++ app/api/server/lib/messages.js
@@ -28,3 +28,31 @@ export async function findMentionedMessages({ uid, roomId, pagination: { offset,
 		total,
 	};
 }
+
+export async function findStarredMessages({ uid, roomId, pagination: { offset, count, sort } }) {
+	const room = await Rooms.findOneById(roomId);
+	if (!await canAccessRoomAsync(room, { _id: uid })) {
+		throw new Error('error-not-allowed');
+	}
+	const user = await Users.findOneById(uid, { fields: { username: 1 } });
+	if (!user) {
+		throw new Error('invalid-user');
+	}
+
+	const cursor = await Messages.findStarredByUserAtRoom(uid, roomId, {
+		sort: sort || { ts: -1 },
+		skip: offset,
+		limit: count,
+	});
+
+	const total = await cursor.count();
+
+	const messages = await cursor.toArray();
+
+	return {
+		messages,
+		count: messages.length,
+		offset,
+		total,
+	};
+}
