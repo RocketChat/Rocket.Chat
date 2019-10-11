2a379eeda02319f8dacd7a19839c394486eacb34
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-10-11 16:02:59 -0300

    fix Self-XSS in validation functionality

--- app/ui-admin/client/rooms/adminRoomInfo.js
+++ app/ui-admin/client/rooms/adminRoomInfo.js
@@ -4,6 +4,7 @@ import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
 import toastr from 'toastr';
+import s from 'underscore.string';
 
 import { AdminChatRoom } from './adminRooms';
 import { t, handleError } from '../../../utils';
@@ -163,7 +164,7 @@ Template.adminRoomInfo.onCreated(function() {
 		}
 		if (!nameValidation.test(name)) {
 			toastr.error(t('error-invalid-room-name', {
-				room_name: name,
+				room_name: s.escapeHTML(name),
 			}));
 			return false;
 		}
