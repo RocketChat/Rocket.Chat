ff95cd3d9148b828ae12c62e3e500a846f9bff35
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-10-16 17:41:30 -0300

    Add SetupWizard to Storybook (temp)

---
+++ client/components/setupWizard/Epilogue.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Epilogue } from './Epilogue';
+
+export default {
+	title: 'setupWizard/Epilogue',
+	component: Epilogue,
+};
+
+export const _default = () =>
+	<Epilogue />;
