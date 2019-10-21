8f3fee7cde4c2d38862c07e91d3c057898be1518
Prajval Raval <ravalprajval@gmail.com>
2019-10-21 18:03:30 +0530

    camelCase fix

--- server/methods/alexaApiCall.js
+++ server/methods/alexaApiCall.js
@@ -5,23 +5,23 @@ import { check } from 'meteor/check';
 import { settings } from '../../app/settings';
 
 Meteor.methods({
-	registerAlexaUser(serverurl, servername) {
-		check(serverurl, String);
-		check(servername, String);
+	registerAlexaUser(serverUrl, serverName) {
+		check(serverUrl, String);
+		check(serverName, String);
 
-		const userid = Meteor.userId();
+		const userId = Meteor.userId();
 
-		if (!userid) {
+		if (!userId) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
 				method: 'registerAlexaUser',
 			});
 		}
 
-		const tokenName = `alexa-${ servername }`;
+		const tokenName = `alexa-${ serverName }`;
 		const token = Meteor.call('personalAccessTokens:generateToken', { tokenName });
 
 		const apiUrl = settings.get('Register_Alexa_Enable_Server_Proxy_URL');
-		const body = { serverurl, servername, userid, token };
+		const body = { serverurl: serverUrl, servername: serverName, userid: userId, token };
 		try {
 			const result = HTTP.call('POST', apiUrl, {
 				headers: {
