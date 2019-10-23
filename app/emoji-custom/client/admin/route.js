6cd4bb80d63dcfef55576e8e7d071195aa40f99c
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-10-23 17:08:52 -0300

    Remove unused subscription

--- app/emoji-custom/client/admin/route.js
+++ app/emoji-custom/client/admin/route.js
@@ -4,9 +4,6 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/admin/emoji-custom', {
 	name: 'emoji-custom',
-	subscriptions(/* params, queryParams*/) {
-		this.register('EmojiCustom', Meteor.subscribe('EmojiCustom'));
-	},
 	action(/* params*/) {
 		BlazeLayout.render('main', { center: 'adminEmoji' });
 	},
