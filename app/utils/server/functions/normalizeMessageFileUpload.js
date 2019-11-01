f6ff72957970237da016ddd5326bf0216a056318
Altair J. Fernandes <tatosjb@gmail.com>
2019-11-01 15:52:39 -0300

    Review fix

--- app/utils/server/functions/normalizeMessageFileUpload.js
+++ app/utils/server/functions/normalizeMessageFileUpload.js
@@ -2,13 +2,9 @@ import { getURL } from '../../lib/getURL';
 import { FileUpload } from '../../../file-upload/server';
 import { Uploads } from '../../../models/server';
 
-function userId(message) {
-	return message.u ? message.u._id : message.agentId;
-}
-
 export const normalizeMessageFileUpload = (message) => {
 	if (message.file && !message.fileUpload) {
-		const jwt = FileUpload.generateJWTToFileUrls({ rid: message.rid, userId: userId(message), fileId: message.file._id });
+		const jwt = FileUpload.generateJWTToFileUrls({ rid: message.rid, userId: message.u._id, fileId: message.file._id });
 		const file = Uploads.findOne({ _id: message.file._id });
 		if (!file) {
 			return message;
