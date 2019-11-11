a8ad42b34fb7f8918b7b48e9eb8df4ebfa8a6bc4
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2019-11-11 18:35:35 -0300

    [IMPROVE] Replace livechat:visitorInfo publication by REST (#15639)
    * Replace livechat:visitorInfo publication by REST
    
    * Rename endpoint

--- app/livechat/server/api.js
+++ app/livechat/server/api.js
@@ -9,3 +9,4 @@ import '../imports/server/rest/rooms.js';
 import '../imports/server/rest/appearance.js';
 import '../imports/server/rest/triggers.js';
 import '../imports/server/rest/integrations.js';
+import '../imports/server/rest/visitors.js';
