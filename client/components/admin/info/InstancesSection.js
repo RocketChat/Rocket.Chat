0c61174ec2f0de6806844b029cc905df21176b7e
Diego Sampaio <chinello@gmail.com>
2019-11-12 17:09:35 -0300

    Fix admin instances info (#15772)

--- client/components/admin/info/InstancesSection.js
+++ client/components/admin/info/InstancesSection.js
@@ -17,8 +17,8 @@ export function InstancesSection({ instances }) {
 		{instances.map(({ address, broadcastAuth, currentStatus, instanceRecord }, i) =>
 			<InformationList key={i}>
 				<InformationEntry label={t('Address')}>{address}</InformationEntry>
-				<InformationEntry label={t('Auth')}>{broadcastAuth}</InformationEntry>
-				<InformationEntry label={<>{t('Current_Status')} > {t('Connected')}</>}>{currentStatus.connected}</InformationEntry>
+				<InformationEntry label={t('Auth')}>{broadcastAuth ? 'true' : 'false'}</InformationEntry>
+				<InformationEntry label={<>{t('Current_Status')} > {t('Connected')}</>}>{currentStatus.connected ? 'true' : 'false'}</InformationEntry>
 				<InformationEntry label={<>{t('Current_Status')} > {t('Retry_Count')}</>}>{currentStatus.retryCount}</InformationEntry>
 				<InformationEntry label={<>{t('Current_Status')} > {t('Status')}</>}>{currentStatus.status}</InformationEntry>
 				<InformationEntry label={<>{t('Instance_Record')} > {t('ID')}</>}>{instanceRecord._id}</InformationEntry>
