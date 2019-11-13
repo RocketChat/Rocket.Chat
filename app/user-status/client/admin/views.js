7e539adcf213c196236f89ae4d221d47441f1062
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-11-13 18:10:20 -0300

    remove pages to dynamic

---
+++ app/user-status/client/admin/views.js
@@ -0,0 +1,9 @@
+import './adminUserStatus.html';
+import './adminUserStatus';
+import './adminUserStatusEdit.html';
+import './adminUserStatusInfo.html';
+import './userStatusEdit.html';
+import './userStatusEdit';
+import './userStatusInfo.html';
+import './userStatusInfo';
+import './userStatusPreview.html';
