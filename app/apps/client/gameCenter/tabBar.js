8c72c9f3024591ae5ab9e8313335a3e989b66aac
lolimay <lolimay@lolimay.cn>
2019-11-15 22:05:45 +0800

    Check externalComponents count before showing

--- app/apps/client/gameCenter/tabBar.js
+++ app/apps/client/gameCenter/tabBar.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
+import { APIClient } from '../../../utils';
 import { TabBar } from '../../../ui-utils/client';
 import { settings } from '../../../settings';
 
@@ -8,8 +9,16 @@ import './gameCenter.html';
 
 Meteor.startup(function() {
 	Tracker.autorun(function() {
-		if (settings.get('Apps_Game_Center_enabled')) {
-			return TabBar.addButton({
+		if (!settings.get('Apps_Game_Center_enabled')) {
+			return TabBar.removeButton('gameCenter');
+		}
+
+		APIClient.get('apps/externalComponents').then(({ externalComponents }) => {
+			if (!externalComponents.length) {
+				return TabBar.removeButton('gameCenter');
+			}
+
+			TabBar.addButton({
 				groups: ['channel', 'group', 'direct'],
 				id: 'gameCenter',
 				i18nTitle: 'Game_Center',
@@ -17,7 +26,6 @@ Meteor.startup(function() {
 				template: 'GameCenter',
 				order: -1,
 			});
-		}
-		TabBar.removeButton('gameCenter');
+		});
 	});
 });
