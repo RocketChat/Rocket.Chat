939e6e3fed944deef79bbd0be3b6a6f8743e0aba
Kailash Bhalaki <kailashbhalaki@gmail.com>
2019-11-21 19:54:58 +0530

    Added search bar in the sidebar header

--- app/ui-sidenav/client/sidebarHeader.js
+++ app/ui-sidenav/client/sidebarHeader.js
@@ -78,9 +78,18 @@ const toolbarButtons = (user) => [{
 	},
 },
 {
+	name: t('Search Input'),
+	icon: '',
+	condition: () => isMobile(),
+	action: () => {
+		toolbarSearch.show(false);
+	},
+	searchBar: true,
+},
+{
 	name: t('Directory'),
 	icon: 'globe',
-	condition: () => settings.get('UI_DisplayDirectory'),
+	condition: () => !isMobile() && settings.get('UI_DisplayDirectory'),
 	action: () => {
 		menu.close();
 		FlowRouter.go('directory');
@@ -349,6 +358,12 @@ Template.sidebarHeader.events({
 			e.currentTarget.blur();
 		}
 		return this.action && this.action.apply(this, [e]);
+	},
+	'click #searchBar'() {
+		toolbarSearch.show(false);
+	},
+	'focus #searchBar'() {
+		toolbarSearch.show(false);
 	},
 	'click .sidebar__header .avatar'(e) {
 		if (!(Meteor.userId() == null && settings.get('Accounts_AllowAnonymousRead'))) {
