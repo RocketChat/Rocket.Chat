475e783ac366bc9c678a3d94f57f44617f0ac3d0
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-11-21 18:46:25 -0300

    call trigger view submit

--- app/ui-message/client/blocks/ModalBlock.js
+++ app/ui-message/client/blocks/ModalBlock.js
@@ -1,19 +1,25 @@
 import { Template } from 'meteor/templating';
 
 import './ModalBlock.html';
-// import { triggerAction } from '../ActionManager';
+import { triggerAction } from '../ActionManager';
 
 Template.ModalBlock.events({
 	'submit form'(e) {
 		e.preventDefault();
-		console.log(Object.values(e.target).reduce((obj, e) => {
-			if (!e.getAttribute || !e.name) {
-				return obj;
-			}
-			const block = e.getAttribute('data-block-id') || 'default';
-			obj[block] = obj[block] || {};
-			obj[block][e.name] = e.value;
-			return obj;
-		}, {}));
+
+		triggerAction({
+			type: 'viewSubmit',
+
+			payload: {
+				state: Object.values(e.target).reduce((obj, e) => {
+					if (!e.getAttribute || !e.name) {
+						return obj;
+					}
+					const block = e.getAttribute('data-block-id') || 'default';
+					obj[block] = obj[block] || {};
+					obj[block][e.name] = e.value;
+					return obj;
+				}, {}) },
+		});
 	},
 });
