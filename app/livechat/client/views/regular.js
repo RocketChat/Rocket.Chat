187c606e29199bbb2eaec0255df06b5cdf7aa28d
Renato Becker <renato.augusto.becker@gmail.com>
2019-11-27 22:51:45 -0300

    Fix Livechat duplicated templates error (#15869)

--- app/livechat/client/views/regular.js
+++ app/livechat/client/views/regular.js
@@ -2,7 +2,7 @@ import './app/livechatAutocompleteUser';
 import './app/livechatQueue';
 import './app/livechatReadOnly';
 import './app/livechatNotSubscribed.html';
-import './app/livechatRoomTagSelector.html';
+import './app/livechatRoomTagSelector';
 import './app/tabbar/agentEdit';
 import './app/tabbar/agentInfo';
 import './app/tabbar/externalSearch';
