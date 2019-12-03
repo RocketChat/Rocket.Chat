4e2bf02b9b0391f49286c95b4f31aa421219f314
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-12-03 16:59:05 -0300

    Replace roles publication by REST

--- app/authorization/client/startup.js
+++ app/authorization/client/startup.js
@@ -3,9 +3,14 @@ import { Meteor } from 'meteor/meteor';
 import { hasAtLeastOnePermission } from './hasPermission';
 import { CachedCollectionManager } from '../../ui-cached-collection';
 import { AdminBox } from '../../ui-utils/client/lib/AdminBox';
+import { APIClient } from '../../utils/client';
+import { Roles } from '../../models/client';
 
 Meteor.startup(() => {
-	CachedCollectionManager.onLogin(() => Meteor.subscribe('roles'));
+	CachedCollectionManager.onLogin(async () => {
+		const { roles } = await APIClient.v1.get('roles.list');
+		roles.forEach((role) => Roles.insert(role));
+	});
 
 	AdminBox.addOption({
 		href: 'admin-permissions',
