3c821cbab7ca8fb7e300a29023f6bcca709b120f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-12-06 17:45:47 -0300

    [FIX] Administration UI issues (#15934)

--- client/components/admin/settings/groups/OAuthGroupPage.js
+++ client/components/admin/settings/groups/OAuthGroupPage.js
@@ -1,8 +1,11 @@
 import { Button } from '@rocket.chat/fuselage';
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import toastr from 'toastr';
 import s from 'underscore.string';
 
+import { call } from '../../../../../app/ui-utils/client/lib/callMethod';
+import { modal } from '../../../../../app/ui-utils/client/lib/modal';
 import { RawText } from '../../../basic/RawText';
 import { useTranslation } from '../../../providers/TranslationProvider';
 import { GroupPage } from '../GroupPage';
@@ -19,22 +22,69 @@ export function OAuthGroupPage({ group }) {
 		return Meteor.absoluteUrl(`_oauth/${ id }`);
 	};
 
+	const handleRefreshOAuthServicesButtonClick = () => {
+		toastr.info(t('Refreshing'));
+		call('refreshOAuthService').then(() => {
+			toastr.success(t('Done'));
+		});
+	};
+
+	const handleAddCustomOAuthButtonClick = () => {
+		modal.open({
+			title: t('Add_custom_oauth'),
+			text: t('Give_a_unique_name_for_the_custom_oauth'),
+			type: 'input',
+			showCancelButton: true,
+			closeOnConfirm: true,
+			inputPlaceholder: t('Custom_oauth_unique_name'),
+		}, (inputValue) => {
+			if (inputValue === false) {
+				return false;
+			}
+			if (inputValue === '') {
+				modal.showInputError(t('Name_cant_be_empty'));
+				return false;
+			}
+			call('addOAuthService', inputValue);
+		});
+	};
+
 	return <GroupPage group={group} headerButtons={<>
-		<Button className='refresh-oauth'>{t('Refresh_oauth_services')}</Button>
-		<Button className='add-custom-oauth'>{t('Add_custom_oauth')}</Button>
+		<Button onClick={handleRefreshOAuthServicesButtonClick}>{t('Refresh_oauth_services')}</Button>
+		<Button onClick={handleAddCustomOAuthButtonClick}>{t('Add_custom_oauth')}</Button>
 	</>}>
-		{group.sections.map((sectionName) => (sectionIsCustomOAuth(sectionName)
-			? <Section
-				key={sectionName}
-				groupId={group._id}
-				help={<RawText>{t('Custom_oauth_helper', callbackURL(sectionName))}</RawText>}
-				sectionName={sectionName}
-				solo={solo}
-			>
-				<div className='submit'>
-					<Button cancel className='remove-custom-oauth'>{t('Remove_custom_oauth')}</Button>
-				</div>
-			</Section>
-			: <Section key={sectionName} groupId={group._id} sectionName={sectionName} solo={solo} />))}
+		{group.sections.map((sectionName) => {
+			if (sectionIsCustomOAuth(sectionName)) {
+				const id = s.strRight(sectionName, 'Custom OAuth: ').toLowerCase();
+
+				const handleRemoveCustomOAuthButtonClick = () => {
+					modal.open({
+						title: t('Are_you_sure'),
+						type: 'warning',
+						showCancelButton: true,
+						confirmButtonColor: '#DD6B55',
+						confirmButtonText: t('Yes_delete_it'),
+						cancelButtonText: t('Cancel'),
+						closeOnConfirm: true,
+					}, () => {
+						call('removeOAuthService', id);
+					});
+				};
+
+				return <Section
+					key={sectionName}
+					groupId={group._id}
+					help={<RawText>{t('Custom_oauth_helper', callbackURL(sectionName))}</RawText>}
+					sectionName={sectionName}
+					solo={solo}
+				>
+					<div className='submit'>
+						<Button danger onClick={handleRemoveCustomOAuthButtonClick}>{t('Remove_custom_oauth')}</Button>
+					</div>
+				</Section>;
+			}
+
+			return <Section key={sectionName} groupId={group._id} sectionName={sectionName} solo={solo} />;
+		})}
 	</GroupPage>;
 }
