13e35571215ebb611b6f9f7ae8cdd33916cd0008
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-12-09 19:34:17 -0300

    Replace livechat:visitorHistory publication by REST

--- app/livechat/client/views/app/tabbar/visitorHistory.js
+++ app/livechat/client/views/app/tabbar/visitorHistory.js
@@ -1,27 +1,20 @@
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
-import { Mongo } from 'meteor/mongo';
 import moment from 'moment';
+import _ from 'underscore';
 
-import { ChatRoom } from '../../../../../models';
 import './visitorHistory.html';
+import { APIClient } from '../../../../../utils/client';
 
-const visitorHistory = new Mongo.Collection('visitor_history');
+const ITEMS_COUNT = 50;
 
 Template.visitorHistory.helpers({
 	historyLoaded() {
-		return !Template.instance().loadHistory.ready();
+		return Template.instance().isLoading.get();
 	},
 
 	previousChats() {
-		return visitorHistory.find({
-			_id: { $ne: this.rid },
-			'v._id': Template.instance().visitorId.get(),
-		}, {
-			sort: {
-				ts: -1,
-			},
-		});
+		return Template.instance().history.get();
 	},
 
 	title() {
@@ -38,15 +31,38 @@ Template.visitorHistory.helpers({
 Template.visitorHistory.onCreated(function() {
 	const currentData = Template.currentData();
 	this.visitorId = new ReactiveVar();
+	this.isLoading = new ReactiveVar(true);
+	this.history = new ReactiveVar([]);
+	this.offset = new ReactiveVar(0);
+	this.total = new ReactiveVar(0);
 
-	this.autorun(() => {
-		const room = ChatRoom.findOne({ _id: Template.currentData().rid });
+	this.autorun(async () => {
+		const { room } = await APIClient.v1.get(`rooms.info?roomId=${ currentData.rid }`);
 		if (room) {
 			this.visitorId.set(room.v._id);
 		}
 	});
 
-	if (currentData && currentData.rid) {
-		this.loadHistory = this.subscribe('livechat:visitorHistory', { rid: currentData.rid });
-	}
+	this.autorun(async () => {
+		this.isLoading.set(true);
+		const offset = this.offset.get();
+		if (currentData && currentData.rid) {
+			const { history, total } = await APIClient.v1.get(`livechat/visitors.chatHistory/room/${ currentData.rid }/visitor/${ this.visitorId.get() }?count=${ ITEMS_COUNT }&offset=${ offset }`);
+			this.isLoading.set(false);
+			this.total.set(total);
+			this.history.set(this.history.get().concat(history));
+		}
+	});
+});
+
+Template.visitorHistory.events({
+	'scroll .visitor-scroll': _.throttle(function(e, instance) {
+		if (e.target.scrollTop >= (e.target.scrollHeight - e.target.clientHeight)) {
+			const history = instance.history.get();
+			if (instance.total.get() <= history.length) {
+				return;
+			}
+			return instance.offset.set(instance.offset.get() + ITEMS_COUNT);
+		}
+	}, 200),
 });
--- app/livechat/server/api/lib/visitors.js
+++ app/livechat/server/api/lib/visitors.js
@@ -1,5 +1,5 @@
 import { hasPermissionAsync } from '../../../../authorization/server/functions/hasPermission';
-import { LivechatVisitors, Messages, LivechatRooms } from '../../../../models/server/raw';
+import { LivechatVisitors, Messages, LivechatRooms, Subscriptions } from '../../../../models/server/raw';
 
 export async function findVisitorInfo({ userId, visitorId }) {
 	if (!await hasPermissionAsync(userId, 'view-l-room')) {
@@ -37,6 +37,37 @@ export async function findVisitedPages({ userId, roomId, pagination: { offset, c
 	return {
 		pages,
 		count: pages.length,
+		offset,
+		total,
+	};
+}
+
+export async function findChatHistory({ userId, roomId, visitorId, pagination: { offset, count, sort } }) {
+	if (!await hasPermissionAsync(userId, 'view-l-room')) {
+		throw new Error('error-not-authorized');
+	}
+	const room = await LivechatRooms.findOneById(roomId);
+	if (!room) {
+		throw new Error('invalid-room');
+	}
+	const subscription = Subscriptions.findOneByRoomIdAndUserId(room._id, this.userId, { fields: { _id: 1 } });
+	if (!subscription) {
+		throw new Error('invalid-room');
+	}
+
+	const cursor = await LivechatRooms.findByVisitorId(visitorId, {
+		sort: sort || { ts: -1 },
+		skip: offset,
+		limit: count,
+	});
+
+	const total = await cursor.count();
+
+	const history = await cursor.toArray();
+
+	return {
+		history,
+		count: history.length,
 		offset,
 		total,
 	};
