accb833be634b43dbe14e6fdc97005830ec6080b
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2019-12-09 16:02:27 -0300

    [IMPROVE] Replace livechat:queue subscription (#15612)
    * Replace livechat:queue subscription
    
    * Refactor livechat queue screen
    
    * Fix some style stuff.
    
    * Rename old class name.
    
    * Display the user avatar on the livechat queue list.

--- app/models/server/raw/LivechatRooms.js
+++ app/models/server/raw/LivechatRooms.js
@@ -1,5 +1,97 @@
 import { BaseRaw } from './BaseRaw';
 
 export class LivechatRoomsRaw extends BaseRaw {
-
+	getQueueMetrics({ departmentId, agentId, includeOfflineAgents, options = {} }) {
+		const match = { $match: { t: 'l', open: true, servedBy: { $exists: true } } };
+		const matchUsers = { $match: {} };
+		if (departmentId) {
+			match.$match.departmentId = departmentId;
+		}
+		if (agentId) {
+			matchUsers.$match['user._id'] = agentId;
+		}
+		if (!includeOfflineAgents) {
+			matchUsers.$match['user.status'] = { $ne: 'offline' };
+			matchUsers.$match['user.statusLivechat'] = { $eq: 'available' };
+		}
+		const departmentsLookup = {
+			$lookup: {
+				from: 'rocketchat_livechat_department',
+				localField: 'departmentId',
+				foreignField: '_id',
+				as: 'departments',
+			},
+		};
+		const departmentsUnwind = {
+			$unwind: {
+				path: '$departments',
+				preserveNullAndEmptyArrays: true,
+			},
+		};
+		const departmentsGroup = {
+			$group: {
+				_id: {
+					departmentId: '$departmentId',
+					name: '$departments.name',
+					room: '$$ROOT',
+				},
+			},
+		};
+		const usersLookup = {
+			$lookup: {
+				from: 'users',
+				localField: '_id.room.servedBy._id',
+				foreignField: '_id',
+				as: 'user',
+			},
+		};
+		const usersUnwind = {
+			$unwind: {
+				path: '$user',
+				preserveNullAndEmptyArrays: true,
+			},
+		};
+		const usersGroup = {
+			$group: {
+				_id: {
+					userId: '$user._id',
+					username: '$user.username',
+					status: '$user.status',
+					departmentId: '$_id.departmentId',
+					departmentName: '$_id.name',
+				},
+				chats: { $sum: 1 },
+			},
+		};
+		const project = {
+			$project: {
+				_id: 0,
+				user: {
+					_id: '$_id.userId',
+					username: '$_id.username',
+					status: '$_id.status',
+				},
+				department: {
+					_id: { $ifNull: ['$_id.departmentId', null] },
+					name: { $ifNull: ['$_id.departmentName', null] },
+				},
+				chats: 1,
+			},
+		};
+		const firstParams = [match, departmentsLookup, departmentsUnwind, departmentsGroup, usersLookup, usersUnwind];
+		if (Object.keys(matchUsers.$match)) {
+			firstParams.push(matchUsers);
+		}
+		const params = [...firstParams, usersGroup, project];
+		if (options.offset) {
+			params.push({ $skip: options.offset });
+		}
+		if (options.count) {
+			params.push({ $limit: options.count });
+		}
+		if (options.sort) {
+			params.push({ $sort: { name: 1 } });
+		}
+		return this.col.aggregate(params).toArray();
+	}
 }
