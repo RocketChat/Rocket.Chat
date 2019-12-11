0d9d0c89241c337d96b90737d990b303aaf675ba
Alan Sikora <alansikora@gmail.com>
2019-12-11 08:48:14 -0200

    added headless version of chimp test

---
+++ tests/chimp-config-headless.js
@@ -0,0 +1,14 @@
+// import path from 'path';
+// import {isCI} from '../lib/ci';
+
+const deepExtend = require('deep-extend');
+
+const baseConfig = require('./chimp-config');
+
+module.exports = deepExtend(baseConfig, {
+	webdriverio: {
+		desiredCapabilities: {
+			'goog:chromeOptions': { args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--disable-dev-shm-usage', '--disable-features=VizDisplayCompositor'] }
+		},
+	},
+});
