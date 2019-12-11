26cbfb72b32381347b65305103a92032220cb3aa
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-12-11 17:01:35 -0300

    Move code to respective files

---
+++ app/2fa/server/email/index.js
@@ -0,0 +1,84 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { Accounts } from 'meteor/accounts-base';
+import bcrypt from 'bcrypt';
+
+import { settings } from '../../../settings';
+import * as Mailer from '../../../mailer';
+import { Users } from '../../../models/server';
+
+export function getUserVerifiedEmails(user) {
+	return user.emails.filter(({ verified }) => verified);
+}
+
+export function send2FAEmail({ address, random }) {
+	Mailer.send({
+		to: address,
+		from: settings.get('From_Email'),
+		subject: 'Authentication code',
+		html: `
+			<p>Here is your authentication code:</p>
+			<p style="font-size: 30px;">
+				<b>${ random.replace(/^(\d{3})/, '$1-') }</b>
+			</p>
+			<p>Do not provide this code to anyone.</p>
+			<p>If you didn't try to login in your account please ignore this email.</p>
+		`,
+	});
+}
+
+export function validate(user, codeFromEmail) {
+	Users.removeExpiredEmailCodesOfUserId(user._id);
+
+	const valid = user.emailCode.find(({ code, expire }) => {
+		if (expire < Date.now) {
+			return false;
+		}
+
+		if (bcrypt.compareSync(codeFromEmail, code)) {
+			Users.removeEmailCodeByUserIdAndCode(user._id, code);
+			return true;
+		}
+
+		return false;
+	});
+
+	if (!valid) {
+		throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
+	}
+}
+
+export function generateCodeAndSend(user, emails) {
+	const random = Random._randomString(6, '0123456789');
+
+	const encryptedRandom = bcrypt.hashSync(random, Accounts._bcryptRounds());
+
+	const expire = new Date();
+	// TODO: Add setting to define the expiration range?
+	expire.setHours(expire.getHours() + 1);
+
+	Users.addEmailCodeByUserId(user._id, encryptedRandom, expire);
+
+	for (const { address } of emails) {
+		send2FAEmail({ address, random });
+	}
+
+	throw new Meteor.Error('totp-required', 'TOTP Required');
+}
+
+export function verifyOrSend(user, code) {
+	if (Array.isArray(user.emails)) {
+		const verifiedEmails = getUserVerifiedEmails(user);
+		if (verifiedEmails.length === 0) {
+			return;
+		}
+
+		if (!code) {
+			generateCodeAndSend(user, verifiedEmails);
+		}
+
+		if (Array.isArray(user.emailCode)) {
+			validate(user, code);
+		}
+	}
+}
--- app/2fa/server/loginHandler.js
+++ app/2fa/server/loginHandler.js
@@ -1,16 +1,9 @@
-import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { Random } from 'meteor/random';
-import bcrypt from 'bcrypt';
 
-import { TOTP } from './lib/totp';
 import { settings } from '../../settings';
 import { callbacks } from '../../callbacks';
-import * as Mailer from '../../mailer';
-import { Users } from '../../models/server';
-
-const bcryptHash = Meteor.wrapAsync(bcrypt.hash);
-const bcryptCompare = Meteor.wrapAsync(bcrypt.compare);
+import { verifyOrSend } from './email';
+import { verify } from './totp';
 
 Accounts.registerLoginHandler('totp', function(options) {
 	if (!options.totp || !options.totp.code) {
@@ -21,87 +14,13 @@ Accounts.registerLoginHandler('totp', function(options) {
 });
 
 callbacks.add('onValidateLogin', (login) => {
-	if (!settings.get('Accounts_TwoFactorAuthentication_Enabled')) {
+	if (login.type !== 'password' || !settings.get('Accounts_TwoFactorAuthentication_Enabled')) {
 		return;
 	}
 
-	if (login.type === 'password' && login.user.services && login.user.services.totp && login.user.services.totp.enabled === true) {
-		const { totp } = login.methodArguments[0];
+	const { totp } = login.methodArguments[0];
 
-		if (!totp || !totp.code) {
-			throw new Meteor.Error('totp-required', 'TOTP Required');
-		}
+	verify(login.user, totp && totp.code);
 
-		const verified = TOTP.verify({
-			secret: login.user.services.totp.secret,
-			token: totp.code,
-			userId: login.user._id,
-			backupTokens: login.user.services.totp.hashedBackup,
-		});
-
-		if (verified !== true) {
-			throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
-		}
-	}
-
-	if (Array.isArray(login.user.emails)) {
-		const verifiedEmails = login.user.emails.filter(({ verified }) => verified);
-
-		if (verifiedEmails.length === 0) {
-			return;
-		}
-
-		const { totp } = login.methodArguments[0];
-
-		if (!totp || !totp.code) {
-			const random = Random._randomString(6, '0123456789');
-
-			const encryptedRandom = bcryptHash(random, Accounts._bcryptRounds());
-
-			const expire = new Date();
-			// TODO: Add setting to define the expiration range?
-			expire.setHours(expire.getHours() + 1);
-
-			Users.addEmailCodeByUserId(login.user._id, encryptedRandom, expire);
-
-			for (const { address } of verifiedEmails) {
-				Mailer.send({
-					to: address,
-					from: settings.get('From_Email'),
-					subject: 'Authentication code',
-					html: `
-						<p>Here is your authentication code:</p>
-						<p style="font-size: 30px;">
-							<b>${ random.replace(/^(\d{3})/, '$1-') }</b>
-						</p>
-						<p>Do not provide this code to anyone.</p>
-						<p>If you didn't try to login in your account please ignore this email.</p>
-					`,
-				});
-			}
-
-			throw new Meteor.Error('totp-required', 'TOTP Required');
-		}
-
-		if (Array.isArray(login.user.emailCode)) {
-			Users.removeExpiredEmailCodesOfUserId(login.user._id);
-
-			const valid = login.user.emailCode.find(({ code, expire }) => {
-				if (expire < Date.now) {
-					return false;
-				}
-
-				if (bcryptCompare(totp.code, code)) {
-					Users.removeEmailCodeByUserIdAndCode(login.user._id, code);
-					return true;
-				}
-
-				return false;
-			});
-
-			if (!valid) {
-				throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
-			}
-		}
-	}
+	verifyOrSend(login.user, totp && totp.code);
 }, callbacks.priority.MEDIUM, '2fa');
