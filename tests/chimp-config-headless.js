48712b8a4f2e152fa19accb2f907bb1a176d5da2
Alan Sikora <alansikora@gmail.com>
2019-12-11 08:52:20 -0200

    lint

--- tests/chimp-config-headless.js
+++ tests/chimp-config-headless.js
@@ -8,7 +8,7 @@ const baseConfig = require('./chimp-config');
 module.exports = deepExtend(baseConfig, {
 	webdriverio: {
 		desiredCapabilities: {
-			'goog:chromeOptions': { args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--disable-dev-shm-usage', '--disable-features=VizDisplayCompositor'] }
+			'goog:chromeOptions': { args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--disable-dev-shm-usage', '--disable-features=VizDisplayCompositor'] },
 		},
 	},
 });
