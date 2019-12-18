e90e6f75ed6e60be518e5990ea55f4fba03bf2a1
Eduarda Cunha <eduarda.mcunha@outlook.com>
2019-12-18 15:09:43 -0300

    fix new message popup

--- app/ui/client/views/app/room.js
+++ app/ui/client/views/app/room.js
@@ -1329,7 +1329,7 @@ Template.room.onRendered(function() {
 			}
 		});
 	}
-	callbacks.add('streamMessage', (msg) => {
+	callbacks.add('streamNewMessage', (msg) => {
 		if (rid !== msg.rid || msg.editedAt) {
 			return;
 		}
