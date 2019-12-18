f495a2bc437e8151619f33f36efa63c9c5f9ce0c
Ashwani Yadav <ashyadavash@gmail.com>
2019-12-18 06:06:17 +0530

    Variable name is apperaing instead of it's value

--- app/importer-slack-users/lib/info.js
+++ app/importer-slack-users/lib/info.js
@@ -1,8 +1,9 @@
 import { ImporterInfo } from '../../importer/lib/ImporterInfo';
+import { t } from '../../utils';
 
 export class SlackUsersImporterInfo extends ImporterInfo {
 	constructor() {
-		super('slack-users', 'Slack_Users', 'text/csv', [{
+		super('slack-users', t('Slack_Users'), 'text/csv', [{
 			text: 'Importer_Slack_Users_CSV_Information',
 			href: 'https://rocket.chat/docs/administrator-guides/import/slack/users',
 		}]);
