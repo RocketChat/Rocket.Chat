10bdc7f3b6823c579a5ff5473fe4b9f5e67d1b94
Marcos Spessatto Defendi <marcos.defendi@ulbra.inf.br>
2019-12-19 21:44:12 -0300

    [IMPROVE] Replace oauth publications by REST (#15878)

--- app/models/server/raw/index.js
+++ app/models/server/raw/index.js
@@ -28,6 +28,8 @@ import LivechatExternalMessagesModel from '../models/LivechatExternalMessages';
 import { LivechatExternalMessageRaw } from './LivechatExternalMessages';
 import LivechatVisitorsModel from '../models/LivechatVisitors';
 import { LivechatVisitorsRaw } from './LivechatVisitors';
+import OAuthAppsModel from '../models/OAuthApps';
+import { OAuthAppsRaw } from './OAuthApps';
 import CustomSoundsModel from '../models/CustomSounds';
 import { CustomSoundsRaw } from './CustomSounds';
 import CustomUserStatusModel from '../models/CustomUserStatus';
@@ -50,6 +52,7 @@ export const LivechatRooms = new LivechatRoomsRaw(LivechatRoomsModel.model.rawCo
 export const Messages = new MessagesRaw(MessagesModel.model.rawCollection());
 export const LivechatExternalMessage = new LivechatExternalMessageRaw(LivechatExternalMessagesModel.model.rawCollection());
 export const LivechatVisitors = new LivechatVisitorsRaw(LivechatVisitorsModel.model.rawCollection());
+export const OAuthApps = new OAuthAppsRaw(OAuthAppsModel.model.rawCollection());
 export const CustomSounds = new CustomSoundsRaw(CustomSoundsModel.model.rawCollection());
 export const CustomUserStatus = new CustomUserStatusRaw(CustomUserStatusModel.model.rawCollection());
 export const LivechatAgentActivity = new LivechatAgentActivityRaw(LivechatAgentActivityModel.model.rawCollection());
