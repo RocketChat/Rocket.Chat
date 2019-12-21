d43d0f9aeca277d431f6f080bfb0cb0c19bcbea9
Ashwani Yadav <ashyadavash@gmail.com>
2019-12-22 01:21:44 +0530

    updated requested changes

--- app/importer-slack-users/lib/info.js
+++ app/importer-slack-users/lib/info.js
@@ -1,9 +1,8 @@
 import { ImporterInfo } from '../../importer/lib/ImporterInfo';
-import { t } from '../../utils';
 
 export class SlackUsersImporterInfo extends ImporterInfo {
 	constructor() {
-		super('slack-users', t('Slack_Users'), 'text/csv', [{
+		super('slack-users', 'Slack_Users', 'text/csv', [{
 			text: 'Importer_Slack_Users_CSV_Information',
 			href: 'https://rocket.chat/docs/administrator-guides/import/slack/users',
 		}]);
