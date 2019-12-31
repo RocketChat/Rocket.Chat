02033049740f2c0931b34da3efbb20bb0ffb566f
Diego Sampaio <chinello@gmail.com>
2019-12-31 15:28:50 -0300

    Fix oauth server complexity

--- app/custom-oauth/server/custom_oauth_server.js
+++ app/custom-oauth/server/custom_oauth_server.js
@@ -219,79 +219,119 @@ export class CustomOAuth {
 		});
 	}
 
-	normalizeIdentity(identity) {
-		if (identity) {
-			// Set 'id' to '_id' for any sources that provide it
-			if (identity._id && !identity.id) {
-				identity.id = identity._id;
-			}
+	_normalizeId(identity) {
+		if (identity.id) {
+			return identity.id;
+		}
 
-			// Fix for Reddit
-			if (identity.result) {
-				identity = identity.result;
-			}
+		// Set 'id' to '_id' for any sources that provide it
+		if (identity._id) {
+			return identity._id;
+		}
 
-			// Fix WordPress-like identities having 'ID' instead of 'id'
-			if (identity.ID && !identity.id) {
-				identity.id = identity.ID;
-			}
+		// Fix WordPress-like identities having 'ID' instead of 'id'
+		if (identity.ID) {
+			return identity.ID;
+		}
 
-			// Fix Auth0-like identities having 'user_id' instead of 'id'
-			if (identity.user_id && !identity.id) {
-				identity.id = identity.user_id;
-			}
+		// Fix Auth0-like identities having 'user_id' instead of 'id'
+		if (identity.user_id) {
+			return identity.user_id;
+		}
 
-			if (identity.CharacterID && !identity.id) {
-				identity.id = identity.CharacterID;
-			}
+		if (identity.CharacterID) {
+			return identity.CharacterID;
+		}
 
+		// Fix Dataporten having 'user.userid' instead of 'id'
+		if (identity.user && identity.user.userid) {
+			if (identity.user.userid_sec && identity.user.userid_sec[0]) {
+				return identity.user.userid_sec[0];
+			}
+			return identity.user.userid;
+			// identity.email = identity.user.email;
+		}
+
+		// Fix for Xenforo [BD]API plugin for 'user.user_id; instead of 'id'
+		if (identity.user && identity.user.user_id) {
+			return identity.user.user_id;
+			// identity.email = identity.user.user_email;
+		}
+		// Fix general 'phid' instead of 'id' from phabricator
+		if (identity.phid) {
+			return identity.phid;
+		}
+
+		// Fix Keycloak-like identities having 'sub' instead of 'id'
+		if (identity.sub) {
+			return identity.sub;
+		}
+
+		// Fix OpenShift identities where id is in 'metadata' object
+		if (identity.metadata && identity.metadata.uid) {
+			return identity.metadata.uid;
+			// identity.name = identity.fullName;
+		}
+
+		// Fix general 'userid' instead of 'id' from provider
+		if (identity.userid) {
+			return identity.userid;
+		}
+
+		// Fix Nextcloud provider
+		if (identity.ocs && identity.ocs.data && identity.ocs.data.id) {
+			return identity.ocs.data.id;
+			// identity.name = identity.ocs.data.displayname;
+			// identity.email = identity.ocs.data.email;
+		}
+
+		// // Fix when authenticating from a meteor app with 'emails' field
+		// if (!identity.email && (identity.emails && Array.isArray(identity.emails) && identity.emails.length >= 1)) {
+		// 	identity.email = identity.emails[0].address ? identity.emails[0].address : undefined;
+		// }
+	}
+
+	_normalizeForServices(originalIdentity) {
+		const normalized = {};
+
+		// Fix for Reddit
+		const identity = originalIdentity.result || originalIdentity;
+
+		normalized.id = this._normalizeId(identity);
+
+		if (identity.user) {
 			// Fix Dataporten having 'user.userid' instead of 'id'
-			if (identity.user && identity.user.userid && !identity.id) {
-				if (identity.user.userid_sec && identity.user.userid_sec[0]) {
-					identity.id = identity.user.userid_sec[0];
-				} else {
-					identity.id = identity.user.userid;
-				}
+			if (identity.user.email) {
 				identity.email = identity.user.email;
-			}
 
 			// Fix for Xenforo [BD]API plugin for 'user.user_id; instead of 'id'
-			if (identity.user && identity.user.user_id && !identity.id) {
-				identity.id = identity.user.user_id;
+			} else if (identity.user.user_email) {
 				identity.email = identity.user.user_email;
 			}
-			// Fix general 'phid' instead of 'id' from phabricator
-			if (identity.phid && !identity.id) {
-				identity.id = identity.phid;
-			}
+		}
 
-			// Fix Keycloak-like identities having 'sub' instead of 'id'
-			if (identity.sub && !identity.id) {
-				identity.id = identity.sub;
-			}
+		// Fix OpenShift identities where id is in 'metadata' object
+		if (identity.metadata && identity.metadata.fullName) {
+			identity.name = identity.fullName;
+		}
 
-			// Fix OpenShift identities where id is in 'metadata' object
-			if (!identity.id && identity.metadata && identity.metadata.uid) {
-				identity.id = identity.metadata.uid;
-				identity.name = identity.fullName;
-			}
+		// Fix Nextcloud provider
+		if (identity.ocs && identity.ocs.data && identity.ocs.data.id) {
+			identity.name = identity.ocs.data.displayname;
+			identity.email = identity.ocs.data.email;
+		}
 
-			// Fix general 'userid' instead of 'id' from provider
-			if (identity.userid && !identity.id) {
-				identity.id = identity.userid;
-			}
+		// Fix when authenticating from a meteor app with 'emails' field
+		if (!identity.email && (identity.emails && Array.isArray(identity.emails) && identity.emails.length >= 1)) {
+			identity.email = identity.emails[0].address ? identity.emails[0].address : undefined;
+		}
 
-			// Fix Nextcloud provider
-			if (!identity.id && identity.ocs && identity.ocs.data && identity.ocs.data.id) {
-				identity.id = identity.ocs.data.id;
-				identity.name = identity.ocs.data.displayname;
-				identity.email = identity.ocs.data.email;
-			}
+		return normalized;
+	}
 
-			// Fix when authenticating from a meteor app with 'emails' field
-			if (!identity.email && (identity.emails && Array.isArray(identity.emails) && identity.emails.length >= 1)) {
-				identity.email = identity.emails[0].address ? identity.emails[0].address : undefined;
-			}
+	normalizeIdentity(identity) {
+		if (identity) {
+			identity = this._normalizeForServices(identity);
 		}
 
 		if (this.usernameField) {
