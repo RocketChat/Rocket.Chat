7c44fe9e06d32c8a1f200a6725263da1318daa9e
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2020-01-08 08:38:28 -0300

    [IMPROVE] Status Text form validation (#16121)

--- app/ui-account/client/accountProfile.js
+++ app/ui-account/client/accountProfile.js
@@ -135,7 +135,7 @@ Template.accountProfile.helpers({
 				return;
 			}
 		}
-		if (!avatar && user.name === realname && user.username === username && getUserEmailAddress(user) === email === email && (!password || password !== confirmationPassword)) {
+		if (!avatar && user.name === realname && user.username === username && getUserEmailAddress(user) === email && statusText === user.statusText && !password) {
 			return ret;
 		}
 		if (!validateEmail(email) || !validatePassword(password, confirmationPassword) || (!validateUsername(username) || usernameAvaliable !== true) || !validateName(realname) || !validateStatusMessage(statusText)) {
