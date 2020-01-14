ab0f9fe6b6f86f4a227de9933354c7607893b2e4
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-01-14 14:43:23 -0300

    Added custom emoji page link to emoji picker

--- app/emoji/client/emojiPicker.js
+++ app/emoji/client/emojiPicker.js
@@ -1,6 +1,7 @@
 import _ from 'underscore';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
 
 import { t } from '../../utils/client';
@@ -139,6 +140,12 @@ Template.emojiPicker.events({
 	'click .emoji-picker'(event) {
 		event.stopPropagation();
 		event.preventDefault();
+	},
+	'click #add-custom'(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		FlowRouter.go('/admin/emoji-custom');
+		EmojiPicker.close();
 	},
 	'click .category-link'(event) {
 		event.stopPropagation();
