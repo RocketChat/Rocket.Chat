182341eed444717f50f20121891e4d2e6cb41fb1
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-01-16 13:02:12 -0300

    modal trap focus

--- app/ui-message/client/blocks/MessageBlock.js
+++ app/ui-message/client/blocks/MessageBlock.js
@@ -1,10 +1,14 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useCallback, useLayoutEffect, useMemo } from 'react';
 import { UiKitMessage as uiKitMessage, kitContext, UiKitModal as uiKitModal, messageParser, modalParser } from '@rocket.chat/fuselage-ui-kit';
 import { uiKitText } from '@rocket.chat/ui-kit';
-import { Modal, AnimatedVisibility, ButtonGroup, Button } from '@rocket.chat/fuselage';
+import { Modal, AnimatedVisibility, ButtonGroup, Button, Box } from '@rocket.chat/fuselage';
+import { useUniqueId } from '@rocket.chat/fuselage-hooks';
 
 import { renderMessageBody } from '../../../ui-utils/client';
 import { useReactiveValue } from '../../../../client/hooks/useReactiveValue';
+
+
+const focusableElementsString =	'a[href]:not([tabindex="-1"]), area[href]:not([tabindex="-1"]), input:not([disabled]):not([tabindex="-1"]), select:not([disabled]):not([tabindex="-1"]), textarea:not([disabled]):not([tabindex="-1"]), button:not([disabled]):not([tabindex="-1"]), iframe, object, embed, [tabindex]:not([tabindex="-1"]), [contenteditable]';
 
 messageParser.text = ({ text, type } = {}) => {
 	if (type !== 'mrkdwn') {
@@ -41,6 +45,9 @@ const textParser = uiKitText(new class {
 	}
 }());
 const thumb =	'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==';
+
+// https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/dialog.html
+
 export const modalBlockWithContext = ({
 	view: {
 		title,
@@ -51,26 +58,85 @@ export const modalBlockWithContext = ({
 	onClose,
 	...context
 }) => (props) => {
+	const id = `modal_id_${ useUniqueId() }`;
+
 	const { view } = useReactiveValue(props.data);
 	const ref = useRef();
-	useEffect(() => ref.current && ref.current.querySelector('input').focus(), []);
+
+	// Auto focus
+	useEffect(() => ref.current && ref.current.querySelector(focusableElementsString).focus(), [ref.current]);
+	// save fovus to restore after close
+	const previousFocus = useMemo(() => document.activeElement, []);
+	// restore the focus after the component unmount
+	useEffect(() => () => previousFocus && previousFocus.focus(), []);
+	// Handle Tab, Shift + Tab, Enter and Escape
+	const handleKeyUp = useCallback((event) => {
+		if (event.keyCode === 13) { // ENTER
+			return onSubmit();
+		}
+
+		if (event.keyCode === 27) { // ESC
+			event.stopPropagation();
+			event.preventDefault();
+			onClose();
+			return false;
+		}
+
+		if (event.keyCode === 9) { // TAB
+			const elements = Array.from(ref.current.querySelectorAll(focusableElementsString));
+			const [first] = elements;
+			const last = elements.pop();
+
+			if (!ref.current.contains(document.activeElement)) {
+				return first.focus();
+			}
+
+			if (event.shiftKey) {
+				if (!first || first === document.activeElement) {
+					last.focus();
+					event.stopPropagation();
+					event.preventDefault();
+				}
+				return;
+			}
+
+			if (!last || last === document.activeElement) {
+				first.focus();
+				event.stopPropagation();
+				event.preventDefault();
+			}
+		}
+	}, [onSubmit]);
+	// Clean the events
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyUp);
+		return () => document.removeEventListener('keydown', handleKeyUp);
+	}, handleKeyUp);
+
 	return (
 		<kitContext.Provider value={context}>
-			<AnimatedVisibility visibility={AnimatedVisibility.VISIBLE}>
-				<Modal open ref={ref}>
+			<AnimatedVisibility visibility={AnimatedVisibility.UNHIDING}>
+				<Modal open id={id} ref={ref}>
 					<Modal.Header>
 						{/* <Modal.Thumb url={`api/apps/${ context.appId }/icon`} /> */}
 						<Modal.Thumb url={thumb} />
 						<Modal.Title>{textParser([title])}</Modal.Title>
-						<Modal.Close onClick={onClose} />
+						<Modal.Close tabIndex={-1} onClick={onClose} />
 					</Modal.Header>
-					<Modal.Content>{uiKitModal(view.blocks)}</Modal.Content>
+					<Modal.Content>
+						<Box
+							is='form'
+							method='post'
+							action='#'
+							onSubmit={onSubmit}
+						>
+							{uiKitModal(view.blocks)}
+						</Box>
+					</Modal.Content>
 					<Modal.Footer>
 						<ButtonGroup align='end'>
 							<Button onClick={onClose}>{textParser([close.text])}</Button>
-							<Button primary onClick={onSubmit}>
-								{textParser([submit.text])}
-							</Button>
+							<Button primary onClick={onSubmit}>{textParser([submit.text])}</Button>
 						</ButtonGroup>
 					</Modal.Footer>
 				</Modal>
