1c4df19423a064b54b3b81fef9b803a55499f0ee
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-01-23 03:37:00 +0000

    Add Facebook, whatsapp, twiiter, linkedin, telegram options

--- app/ui-share/client/share.js
+++ app/ui-share/client/share.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 
-import { getShareData } from '../../utils';
+import { getShareData, isMobile } from '../../utils';
 
 function getShareString() {
 	const data = getShareData();
@@ -26,4 +26,25 @@ Template.share.events({
 	'click [data-type="sms"]'() {
 		location.href = `sms:'Pick a contact'?&body=${ getShareString() }`;
 	},
+
+
+	'click [data-type="facebook"]'() {
+		window.open(`https://www.facebook.com/sharer/sharer.php?u=${ getShareString() }`);
+	},
+	'click [data-type="whatsapp"]'() {
+		window.open((isMobile() ? 'whatsapp://send?text=' : 'https://api.whatsapp.com/send?text=') + getShareString());
+	},
+	'click [data-type="twitter"]'() {
+		const { url } = getShareData();
+		window.open(`http://twitter.com/share?text=${ getShareString() }&url=${ url }`);
+	},
+	'click [data-type="linkedin"]'() {
+		const { title, url } = getShareData();
+		window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${ url }&title=${ title }&summary=${ getShareString() }&source=LinkedIn`);
+	},
+	'click [data-type="telegram"]'() {
+		const { url } = getShareData();
+		window.open(isMobile() ? `tg://msg?text=${ getShareString() }` : `https://telegram.me/share/msg?url=${ url }&text=${ getShareString() }`);
+	},
+
 });
