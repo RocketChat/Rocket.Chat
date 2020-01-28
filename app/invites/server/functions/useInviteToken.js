8109b311ac771e6dbd44b8ee2d4a574bc845e952
Diego Sampaio <chinello@gmail.com>
2020-01-28 14:34:55 -0300

    Use model methods

--- app/invites/server/functions/useInviteToken.js
+++ app/invites/server/functions/useInviteToken.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
-import { Invites, Users } from '../../../models';
+import { Invites, Users } from '../../../models/server';
 import { validateInviteToken } from './validateInviteToken';
 import { addUserToRoom } from '../../../lib/server/functions/addUserToRoom';
 
@@ -18,11 +18,7 @@ export const useInviteToken = (userId, token) => {
 	const user = Users.findOneById(userId);
 	Users.updateInviteToken(user._id, token);
 
-	Invites.update(inviteData._id, {
-		$inc: {
-			uses: 1,
-		},
-	});
+	Invites.increaseUsageById(inviteData._id);
 
 	// If the user already has an username, then join the invite room,
 	// If no username is set yet, then the the join will happen on the setUsername method
