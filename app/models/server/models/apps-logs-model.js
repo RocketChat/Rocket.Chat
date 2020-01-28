3532782600f5802cd5c8c94f3ecd555ffe86f2fa
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-01-28 17:33:18 -0300

    Regression: App deletion wasn’t returning the correct information

--- app/models/server/models/apps-logs-model.js
+++ app/models/server/models/apps-logs-model.js
@@ -4,4 +4,9 @@ export class AppsLogsModel extends Base {
 	constructor() {
 		super('apps_logs');
 	}
+
+	// Bypass trash collection
+	remove(query) {
+		return this._db.originals.remove(query);
+	}
 }
