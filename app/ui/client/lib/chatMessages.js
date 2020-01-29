68db246d53169bab9fa6fab9d398bb46488df88d
Ashwani Yadav <ashyadavash@gmail.com>
2020-01-29 17:54:18 +0530

    [BUG][FIX] If InternalHubot_Username is undefined then rocket.cat should be default bot

--- app/ui/client/lib/chatMessages.js
+++ app/ui/client/lib/chatMessages.js
@@ -422,7 +422,7 @@ export class ChatMessages {
 						ts: new Date(),
 						msg: TAPi18n.__('No_such_command', { command: s.escapeHTML(match[1]) }),
 						u: {
-							username: settings.get('InternalHubot_Username'),
+							username: settings.get('InternalHubot_Username') || 'rocket.cat',
 						},
 						private: true,
 					};
