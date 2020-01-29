069bbedd6000d81d43dd489814a419a0b1d3186c
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-01-29 16:14:28 -0300

    [FIX] App removal was moving logs to the trash collection (#16362)
    * [FIX] App removal was moving logs to the trash collection
    
    It may cause a huge delay and memory overload if the app contains a huge history of logs
    
    * Add indexes and expire logs after 30 days
    
    * Add instance id to apps engine logs

--- app/models/server/models/apps-logs-model.js
+++ app/models/server/models/apps-logs-model.js
@@ -3,5 +3,13 @@ import { Base } from './_Base';
 export class AppsLogsModel extends Base {
 	constructor() {
 		super('apps_logs');
+
+		this.tryEnsureIndex({ appId: 1 });
+		this.tryEnsureIndex({ _updatedAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 30 });
+	}
+
+	// Bypass trash collection
+	remove(query) {
+		return this._db.originals.remove(query);
 	}
 }
