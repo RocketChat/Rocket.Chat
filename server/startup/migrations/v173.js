c175a0db476dd15aa74b2b852dbc818c6682f4ff
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-02-03 17:11:20 -0300

    improve migration

--- server/startup/migrations/v173.js
+++ server/startup/migrations/v173.js
@@ -4,11 +4,13 @@ import {
 import {
 	Rooms,
 } from '../../../app/models';
+import { MessageTypes } from '../../../app/lib/server/lib/messageTypes';
 
 
 Migrations.add({
 	version: 173,
 	up() {
-		Rooms.update({ sysMes: { $exists: 1 } }, { $unset: { sysMes: '' } });
+		Rooms.update({ sysMes: true }, { $unset: { sysMes: '' } }, { multi: true });
+		Rooms.update({ sysMes: false }, { $set: MessageTypes.map(({ key }) => key) }, { multi: true });
 	},
 });
