325d7314a0727e1e27f1df5b78bfaaff3a1d1d28
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2020-02-06 14:16:31 -0600

    Fix license missing from manual register handler

--- app/cloud/server/functions/saveRegistrationData.js
+++ app/cloud/server/functions/saveRegistrationData.js
@@ -8,6 +8,7 @@ export function saveRegistrationData({
 	client_secret_expires_at,
 	publicKey,
 	registration_client_uri,
+	licenseData,
 }) {
 	return Promise.all([
 		Settings.updateValueById('Register_Server', true),
@@ -18,5 +19,9 @@ export function saveRegistrationData({
 		Settings.updateValueById('Cloud_Workspace_Client_Secret_Expires_At', client_secret_expires_at),
 		Settings.updateValueById('Cloud_Workspace_PublicKey', publicKey),
 		Settings.updateValueById('Cloud_Workspace_Registration_Client_Uri', registration_client_uri),
-	]);
+		Settings.updateValueById('Cloud_Workspace_License', licenseData.license),
+	]).then((...results) => {
+                callbacks.run('workspaceLicenseChanged', licenseData.license);
+                return results;
+        });
 }
