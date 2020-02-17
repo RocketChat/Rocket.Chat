649109416a933359b3d3656c32a5049eb416e8d6
Diego Sampaio <chinello@gmail.com>
2020-02-17 20:42:42 -0300

    Do not use toArray in model

--- app/models/server/raw/LivechatDepartment.js
+++ app/models/server/raw/LivechatDepartment.js
@@ -3,6 +3,6 @@ import { BaseRaw } from './BaseRaw';
 export class LivechatDepartmentRaw extends BaseRaw {
 	findInIds(departmentsIds, options) {
 		const query = { _id: { $in: departmentsIds } };
-		return this.find(query, options).toArray();
+		return this.find(query, options);
 	}
 }
