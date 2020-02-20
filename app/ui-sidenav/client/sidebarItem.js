7de48f36e617e2169185989349207eb2571aaa77
erosenthal <eric.rosenthal@viasat.com>
2020-02-20 15:34:15 -0500

    bring getConfig function in scope

--- app/ui-sidenav/client/sidebarItem.js
+++ app/ui-sidenav/client/sidebarItem.js
@@ -75,7 +75,7 @@ function setLastMessageTs(instance, ts) {
 	}, 60000);
 }
 
-function getConfig(e) {
+var getConfig = function(e) {
 	const canLeave = () => {
 		const roomData = Session.get(`roomData${ this.rid }`);
 
@@ -221,7 +221,7 @@ Template.sidebarItem.events({
 		e.stopPropagation(); // to not close the menu
 		e.preventDefault();
 
-		const config = getConfig(e);
+		const config = getConfig.call(this, e);
 		popover.open(config);
 	},
 });
