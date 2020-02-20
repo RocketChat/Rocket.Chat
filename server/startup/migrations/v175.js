62478021ab935402bb307a25216c646cad9a7eb3
taiju271 <taiju271@users.noreply.github.com>
2020-02-20 15:28:50 +0900

    Rename migration v174 to v175

--- server/startup/migrations/v175.js
+++ server/startup/migrations/v175.js
@@ -2,7 +2,7 @@ import { Migrations } from '../../../app/migrations';
 import { Settings } from '../../../app/models';
 
 Migrations.add({
-	version: 174,
+	version: 175,
 	up() {
 		Settings.remove({ _id: 'AutoLinker_UrlsRegExp' });
 	},
