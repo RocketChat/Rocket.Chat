1d29b0c5cd3d2313285e6fffcccd19d268f3700e
TheGr8Wonder <ttodd@ttodd.com>
2020-02-26 23:29:02 -0500

    Update startup.js

--- app/ui-clean-history/client/lib/startup.js
+++ app/ui-clean-history/client/lib/startup.js
@@ -15,4 +15,24 @@ Meteor.startup(() => {
 		order: 250,
 		condition: () => hasAllPermission('clean-channel-history', Session.get('openedRoom')),
 	});
+	TabBar.addButton({
+		groups: ['group'],
+		id: 'clean-history',
+		anonymous: true,
+		i18nTitle: 'Prune_Messages',
+		icon: 'eraser',
+		template: 'cleanHistory',
+		order: 250,
+		condition: () => hasAllPermission('clean-group-history', Session.get('openedRoom')),
+	});
+	TabBar.addButton({
+		groups: ['direct'],
+		id: 'clean-history',
+		anonymous: true,
+		i18nTitle: 'Prune_Messages',
+		icon: 'eraser',
+		template: 'cleanHistory',
+		order: 250,
+		condition: () => hasAllPermission('clean-direct-history', Session.get('openedRoom')),
+	});
 });
