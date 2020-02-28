066b221615de180a269b5268c8ed478fd8439db3
TheGr8Wonder <ttodd@ttodd.com>
2020-02-27 20:03:46 -0500

    Fix permissions

--- app/lib/server/methods/cleanRoomHistory.js
+++ app/lib/server/methods/cleanRoomHistory.js
@@ -5,7 +5,7 @@ import { hasPermission } from '../../../authorization';
 import { cleanRoomHistory } from '../functions';
 
 Meteor.methods({
-	cleanRoomHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
+	cleanChannelHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
 		check(roomId, String);
 		check(latest, Date);
 		check(oldest, Date);
@@ -18,21 +18,59 @@ Meteor.methods({
 		const userId = Meteor.userId();
 
 		if (!userId) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanRoomHistory' });
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanChannelHistory' });
 		}
 
 		if (!hasPermission(userId, 'clean-channel-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
-		}
-
-		if (!hasPermission(userId, 'clean-group-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
-		}
-
-		if (!hasPermission(userId, 'clean-direct-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanChannelHistory' });
 		}
 
 		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
 	},
+
+	cleanGroupHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
+		check(roomId, String);
+		check(latest, Date);
+		check(oldest, Date);
+		check(inclusive, Boolean);
+		check(limit, Match.Maybe(Number));
+		check(excludePinned, Match.Maybe(Boolean));
+		check(filesOnly, Match.Maybe(Boolean));
+		check(fromUsers, Match.Maybe([String]));
+
+		const userId = Meteor.userId();
+
+		if (!userId) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanGroupHistory' });
+		}
+
+		if (!hasPermission(userId, 'clean-group-history', roomId)) {
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanGroupHistory' });
+		}
+
+		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
+	},
+
+	cleanDirectHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
+		check(roomId, String);
+		check(latest, Date);
+		check(oldest, Date);
+		check(inclusive, Boolean);
+		check(limit, Match.Maybe(Number));
+		check(excludePinned, Match.Maybe(Boolean));
+		check(filesOnly, Match.Maybe(Boolean));
+		check(fromUsers, Match.Maybe([String]));
+
+		const userId = Meteor.userId();
+		
+		if (!userId) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanDirectHistory' });
+		}
+
+		if (!hasPermission(userId, 'clean-direct-history', roomId)) {
+			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanDirectHistory' });
+		}
+
+		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
+        },
 });
