5afcad397d4a2aec885d50aef8540062905cde45
TheGr8Wonder <ttodd@ttodd.com>
2020-02-28 13:50:38 -0500

    Update cleanRoomHistory.js

--- app/lib/server/methods/cleanRoomHistory.js
+++ app/lib/server/methods/cleanRoomHistory.js
@@ -5,8 +5,9 @@ import { hasPermission } from '../../../authorization';
 import { cleanRoomHistory } from '../functions';
 
 Meteor.methods({
-	cleanChannelHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
+	cleanRoomHistory({ roomId, roomType, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
 		check(roomId, String);
+		check(roomType, String);
 		check(latest, Date);
 		check(oldest, Date);
 		check(inclusive, Boolean);
@@ -21,56 +22,31 @@ Meteor.methods({
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanChannelHistory' });
 		}
 
-		if (!hasPermission(userId, 'clean-channel-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanChannelHistory' });
-		}
+                if (roomType === 'c') {
 
-		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
-	},
+                        if (!hasPermission(userId, 'clean-channel-history', roomId)) {
+                                throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
+                        }
 
-	cleanGroupHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
-		check(roomId, String);
-		check(latest, Date);
-		check(oldest, Date);
-		check(inclusive, Boolean);
-		check(limit, Match.Maybe(Number));
-		check(excludePinned, Match.Maybe(Boolean));
-		check(filesOnly, Match.Maybe(Boolean));
-		check(fromUsers, Match.Maybe([String]));
+                        return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
+                }
 
-		const userId = Meteor.userId();
+                if (roomType === 'p') {
 
-		if (!userId) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanGroupHistory' });
-		}
+                        if (!hasPermission(userId, 'clean-group-history', roomId)) {
+                                throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
+                        }
 
-		if (!hasPermission(userId, 'clean-group-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanGroupHistory' });
-		}
+                        return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
+                }
 
-		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
-	},
+                if (roomType === 'd') {
 
-	cleanDirectHistory({ roomId, latest, oldest, inclusive = true, limit, excludePinned = false, ignoreDiscussion = true, filesOnly = false, fromUsers = [] }) {
-		check(roomId, String);
-		check(latest, Date);
-		check(oldest, Date);
-		check(inclusive, Boolean);
-		check(limit, Match.Maybe(Number));
-		check(excludePinned, Match.Maybe(Boolean));
-		check(filesOnly, Match.Maybe(Boolean));
-		check(fromUsers, Match.Maybe([String]));
+                        if (!hasPermission(userId, 'clean-direct-history', roomId)) {
+                                throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanRoomHistory' });
+                        }
 
-		const userId = Meteor.userId();
-
-		if (!userId) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'cleanDirectHistory' });
-		}
-
-		if (!hasPermission(userId, 'clean-direct-history', roomId)) {
-			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'cleanDirectHistory' });
-		}
-
-		return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
-	},
+                        return cleanRoomHistory({ rid: roomId, latest, oldest, inclusive, limit, excludePinned, ignoreDiscussion, filesOnly, fromUsers });
+                }
+        },
 });
