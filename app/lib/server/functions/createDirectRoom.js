b845c7c39f7508273161cc6c9a055b8779531fef
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-03-02 10:22:07 -0300

    improve name generation

--- app/lib/server/functions/createDirectRoom.js
+++ app/lib/server/functions/createDirectRoom.js
@@ -2,7 +2,7 @@ import { Rooms, Subscriptions } from '../../../models/server';
 import { settings } from '../../../settings/lib/settings';
 import { getDefaultSubscriptionPref } from '../../../utils';
 
-const generate = (fname, name, user, extra) => ({
+const generateSubscription = (fname, name, user, extra) => ({
 	alert: false,
 	unread: 0,
 	userMentions: 0,
@@ -27,12 +27,14 @@ export const createDirectRoom = function(members, roomExtraData = {}, options = 
 		return;
 	}
 
+	const sortedMembers = members.sort((u1, u2) => (u1.name || u1.username).localeCompare(u2.name || u2.username));
+
 	const rid = members.map(({ _id }) => _id).sort().join(''); // TODO provide a better rid heuristic
 
 	const { insertedId } = Rooms.upsert({ _id: rid }, {
 		$setOnInsert: {
 			t: 'd',
-			usernames: members.map(({ username }) => username),
+			usernames: sortedMembers.map(({ username }) => username),
 			usersCount: members.length,
 			msgs: 0,
 			ts: new Date(),
@@ -42,12 +44,12 @@ export const createDirectRoom = function(members, roomExtraData = {}, options = 
 	if (members.length === 1) { // dm to yourself
 		Subscriptions.upsert({ rid, 'u._id': members[0]._id }, {
 			$set: { open: true },
-			$setOnInsert: generate(members[0].name || members[0].username, members[0].username, members[0], { ...options.subscriptionExtra }),
+			$setOnInsert: generateSubscription(members[0].name || members[0].username, members[0].username, members[0], { ...options.subscriptionExtra }),
 		});
 	} else {
 		members.forEach((member) => Subscriptions.upsert({ rid, 'u._id': member._id }, {
 			...options.creator === member._id && { $set: { open: true } },
-			$setOnInsert: generate(getFname(member._id, members), getName(member._id, members), member, { ...options.subscriptionExtra, ...options.creator !== member._id && { open: members.length > 2 } }),
+			$setOnInsert: generateSubscription(getFname(member._id, sortedMembers), getName(member._id, sortedMembers), member, { ...options.subscriptionExtra, ...options.creator !== member._id && { open: members.length > 2 } }),
 		}));
 	}
 
