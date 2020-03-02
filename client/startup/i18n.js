528ff3a90b52f061d215bec49387e4c94d159789
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-03-02 09:40:47 -0300

    Enforce uppercase country on filterLanguage method and change the language list to list with uppercase countries

--- client/startup/i18n.js
+++ client/startup/i18n.js
@@ -21,7 +21,7 @@ Meteor.startup(() => {
 		const regex = /([a-z]{2,3})-([a-z]{2,4})/;
 		const matches = regex.exec(language);
 		if (matches) {
-			return `${ matches[1] }-${ matches[2].toLowerCase() }`;
+			return `${ matches[1] }-${ matches[2].toUpperCase() }`;
 		}
 
 		return language;
@@ -47,10 +47,6 @@ Meteor.startup(() => {
 	});
 
 	const applyLanguage = (language = 'en') => {
-		if (language.includes('-')) {
-			language = language.replace(/-.+/, `-${ language.split('-')[1].toUpperCase() }`);
-		}
-
 		language = filterLanguage(language);
 
 		if (!availableLanguages[language]) {
