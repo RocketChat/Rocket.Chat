82568536adeaa548c7a5da633016991511182f06
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-03-06 14:16:21 -0300

    Embed heat map into UsersByTimeOfTheDaySection

--- ee/app/engagement-dashboard/client/components/UsersTab/UsersByTimeOfTheDaySection.js
+++ ee/app/engagement-dashboard/client/components/UsersTab/UsersByTimeOfTheDaySection.js
@@ -1,11 +1,12 @@
+import { ResponsiveHeatMap } from '@nivo/heatmap';
 import { Box, Flex, Skeleton } from '@rocket.chat/fuselage';
 import moment from 'moment';
 import React, { useMemo } from 'react';
 
 import { useTranslation } from '../../../../../client/contexts/TranslationContext';
-import { HeatMap } from '../data/HeatMap';
 import { Section } from '../Section';
 import { useEndpointData } from '../../hooks/useEndpointData';
+import { monochromaticColors } from '../data/colors';
 
 export function UsersByTimeOfTheDaySection() {
 	const t = useTranslation();
@@ -13,50 +14,87 @@ export function UsersByTimeOfTheDaySection() {
 	const params = useMemo(() => ({ start: moment().subtract(20, 'days').toISOString() }), []);
 	const data = useEndpointData('GET', 'engagement-dashboard/users/users-by-time-of-the-day-in-a-week', params);
 
-	const [keys, values] = useMemo(() => {
+	const [dates, groups] = useMemo(() => {
 		if (!data) {
 			return [];
 		}
 
-		const keysSet = new Set();
+		const points = data.week
+			.map(({ users, hour, day, month, year }) => ({
+				users,
+				hour,
+				date: moment.utc([year, month - 1, day, 0, 0, 0]).valueOf(),
+			}));
 
-		for (const { day, month, year } of data.week) {
-			const key = moment.utc([year, month - 1, day, 0, 0, 0]).toISOString();
-			keysSet.add(key);
-		}
+		const dates = Array.from(points.reduce((set, { date }) => { set.add(date); return set; }, new Set())).sort();
 
-		const keyFormat = keysSet.size === 7 ? 'dddd' : 'L';
-		keysSet.clear();
+		const emptyGroup = dates.reduce((obj, date) => ({ ...obj, [String(date)]: 0 }), {});
 
-		const valuesMap = new Map();
+		const groups = Array.from({ length: 24 });
 
-		for (const { users, hour, day, month, year } of data.week) {
-			const value = valuesMap.get(hour) || { hour };
-			const key = moment.utc([year, month - 1, day, 0, 0, 0]).format(keyFormat);
-			value[key] = users;
-			keysSet.add(key);
-			valuesMap.set(hour, value);
+		for (const { users, hour, date } of points) {
+			const group = groups[hour] || { hour, ...emptyGroup };
+			group[date] = users;
+			groups[hour] = group;
 		}
 
 		return [
-			Array.from(keysSet),
-			Array.from(valuesMap.values()),
+			dates.map(String),
+			groups,
 		];
 	}, [data]);
 
 	console.log(data);
-	console.log(keys);
-	console.log(values);
+	console.log(dates);
+	console.log(groups);
 
 	return <Section title={t('Users by time of day')} filter={null}>
 		<Flex.Container>
 			{data
 				? <Box style={{ height: 696 }}>
-					<HeatMap
-						keys={keys}
-						indexBy='hour'
-						values={values}
-					/>
+					<Flex.Item align='stretch' grow={1} shrink={0}>
+						<Box style={{ position: 'relative' }}>
+							<Box style={{ position: 'absolute', width: '100%', height: '100%' }}>
+								<ResponsiveHeatMap
+									data={groups}
+									indexBy='hour'
+									keys={dates}
+									groupMode='grouped'
+									padding={4}
+									margin={{ left: 32, bottom: 20 }}
+									colors={monochromaticColors}
+									cellOpacity={1}
+									enableLabels={false}
+									axisTop={null}
+									axisRight={null}
+									axisBottom={{
+										tickSize: 0,
+										tickPadding: 5,
+										tickRotation: 0,
+										format: (timestamp) => moment(parseInt(timestamp, 10)).format(dates.length === 7 ? 'dddd' : 'L'),
+									}}
+									axisLeft={{
+										tickSize: 0,
+										tickPadding: 5,
+										tickRotation: 0,
+										format: (hour) => moment().set({ hour, minute: 0, second: 0 }).format('LT'),
+									}}
+									animate={true}
+									motionStiffness={90}
+									motionDamping={15}
+									theme={{
+										font: 'inherit',
+										fontStyle: 'normal',
+										fontWeight: 600,
+										fontSize: 10,
+										lineHeight: 12,
+										letterSpacing: 0.2,
+										color: '#9EA2A8',
+									}}
+								/>
+							</Box>
+						</Box>
+					</Flex.Item>
 				</Box>
 				: <Skeleton variant='rect' height={696} />}
 		</Flex.Container>
