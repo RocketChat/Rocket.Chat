c35519ca0b6163d51fe2589d12b5eee6eb1aa92e
mrsimpson <github@beimir.net>
2020-03-11 22:54:24 +0100

    Don't expose translation API keys to client

--- app/autotranslate/server/settings.js
+++ app/autotranslate/server/settings.js
@@ -33,7 +33,7 @@ Meteor.startup(function() {
 		type: 'string',
 		group: 'Message',
 		section: 'AutoTranslate_Google',
-		public: true,
+		public: false,
 		i18nLabel: 'AutoTranslate_APIKey',
 		enableQuery: [
 			{
@@ -48,7 +48,7 @@ Meteor.startup(function() {
 		type: 'string',
 		group: 'Message',
 		section: 'AutoTranslate_DeepL',
-		public: true,
+		public: false,
 		i18nLabel: 'AutoTranslate_APIKey',
 		enableQuery: [
 			{
@@ -62,7 +62,7 @@ Meteor.startup(function() {
 		type: 'string',
 		group: 'Message',
 		section: 'AutoTranslate_Microsoft',
-		public: true,
+		public: false,
 		i18nLabel: 'AutoTranslate_APIKey',
 		enableQuery: [
 			{
