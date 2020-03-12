5fda4097b0834728e3beec03b1cc6c1eebeba699
subham103 <subhamsahoo4444@gmail.com>
2020-03-13 04:21:19 +0530

    [FIX] Allow badWord filter for Diacritics word

--- app/lib/server/methods/filterBadWords.js
+++ app/lib/server/methods/filterBadWords.js
@@ -7,7 +7,9 @@ callbacks.add('beforeSaveMessage', function(message) {
 	if (settings.get('Message_AllowBadWordsFilter')) {
 		const badWordsList = settings.get('Message_BadWordsFilterList');
 		let options;
-
+		let { msg } = message;
+		// Convert diacritics to english words
+		msg = msg.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 		// Add words to the blacklist
 		if (!!badWordsList && badWordsList.length) {
 			options = {
@@ -15,7 +17,7 @@ callbacks.add('beforeSaveMessage', function(message) {
 			};
 		}
 		const filter = new Filter(options);
-		message.msg = filter.clean(message.msg);
+		message.msg = filter.clean(msg);
 	}
 
 	return message;
