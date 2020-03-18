0c7aaf80f6a26ec5151d3bc21c5af2473de9f6f6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-03-18 01:00:29 -0300

    Add TableSection prototype

---
+++ ee/app/engagement-dashboard/client/components/ChannelsTab/TableSection.js
@@ -0,0 +1,112 @@
+import { Box, Icon, Margins, Pagination, Select, Table } from '@rocket.chat/fuselage';
+import moment from 'moment';
+import React, { useMemo, useState } from 'react';
+
+import { useTranslation } from '../../../../../../client/contexts/TranslationContext';
+import { Growth } from '../data/Growth';
+import { Section } from '../Section';
+import { useEndpointData } from '../../hooks/useEndpointData';
+
+export function TableSection() {
+	const t = useTranslation();
+
+	const periodOptions = useMemo(() => [
+		['last 7 days', t('Last 7 days')],
+		['last 30 days', t('Last 30 days')],
+		['last 90 days', t('Last 90 days')],
+	], [t]);
+
+	const [periodId, setPeriodId] = useState('last 7 days');
+
+	const period = useMemo(() => {
+		switch (periodId) {
+			case 'last 7 days':
+				return {
+					start: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(7, 'days'),
+					end: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(1),
+				};
+
+			case 'last 30 days':
+				return {
+					start: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(30, 'days'),
+					end: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(1),
+				};
+
+			case 'last 90 days':
+				return {
+					start: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(90, 'days'),
+					end: moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).subtract(1),
+				};
+		}
+	}, [periodId]);
+
+	const handlePeriodChange = (periodId) => setPeriodId(periodId);
+
+	const params = useMemo(() => ({
+		start: period.start.toISOString(),
+		end: period.end.toISOString(),
+		offset: 0,
+		count: 10,
+	}), [period]);
+
+	const data = useEndpointData('GET', 'engagement-dashboard/channels/list', params);
+
+	const channels = useMemo(() => {
+		if (!data) {
+			return;
+		}
+
+		return data.channels.map(({
+			room: { t, name, usernames, ts, _updatedAt },
+			messages,
+			diffFromLastWeek,
+		}) => ({
+			t,
+			name: name || usernames.join(' × '),
+			createdAt: ts,
+			updatedAt: _updatedAt,
+			messagesCount: messages,
+			messagesVariation: diffFromLastWeek,
+		}));
+	}, [data]);
+
+	return <Section filter={<Select options={periodOptions} value={periodId} onChange={handlePeriodChange} />}>
+		<Box>
+			<Table>
+				<Table.Head>
+					<Table.Row>
+						<Table.Cell>{t('#')}</Table.Cell>
+						<Table.Cell>{t('Channel')}</Table.Cell>
+						<Table.Cell>{t('Created')}</Table.Cell>
+						<Table.Cell>{t('Last active')}</Table.Cell>
+						<Table.Cell>{t('Messages sent')}</Table.Cell>
+					</Table.Row>
+				</Table.Head>
+				<Table.Body>
+					{channels && channels.map(({ t, name, createdAt, updatedAt, messagesCount, messagesVariation }, i) =>
+						<Table.Row key={i}>
+							<Table.Cell>{i + 1}.</Table.Cell>
+							<Table.Cell>
+								<Margins inlineEnd='x4'>
+									{(t === 'd' && <Icon name='at' />)
+								|| (t === 'c' && <Icon name='lock' />)
+								|| (t === 'p' && <Icon name='hashtag' />)}
+								</Margins>
+								{name}
+							</Table.Cell>
+							<Table.Cell>
+								{moment(createdAt).format('L')}
+							</Table.Cell>
+							<Table.Cell>
+								{moment(updatedAt).format('L')}
+							</Table.Cell>
+							<Table.Cell>
+								{messagesCount} <Growth>{messagesVariation}</Growth>
+							</Table.Cell>
+						</Table.Row>)}
+				</Table.Body>
+			</Table>
+			<Pagination count={500} />
+		</Box>
+	</Section>;
+}
