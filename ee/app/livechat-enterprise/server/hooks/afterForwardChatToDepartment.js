3dfb6184923bc18ab7a0d28e69223bc130933ba9
Renato Becker <renato.augusto.becker@gmail.com>
2020-03-20 20:39:32 -0300

    Add new omnichannel department forwarding callback handler.

---
+++ ee/app/livechat-enterprise/server/hooks/afterForwardChatToDepartment.js
@@ -0,0 +1,24 @@
+import { callbacks } from '../../../../../app/callbacks';
+import LivechatRooms from '../../../../../app/models/server/models/LivechatRooms';
+import LivechatDepartment from '../../../../../app/models/server/models/LivechatDepartment';
+
+callbacks.add('livechat.afterForwardChatToDepartment', ({ rid, newDepartmentId }) => {
+	const room = LivechatRooms.findOneById(rid);
+	if (!room) {
+		return;
+	}
+
+	const department = LivechatDepartment.findOneById(newDepartmentId, { fields: { ancestors: 1 } });
+    if (!department) {
+        return;
+    }
+
+	const { departmentAncestors } = room;
+	const { ancestors } = department;
+	if (!ancestors && !departmentAncestors) {
+		return room;
+	}
+
+	LivechatRooms.updateDepartmentAncestorsById(room._id, ancestors);
+	return LivechatRooms.findOneById(rid);
+}, callbacks.priority.MEDIUM, 'livechat-after-forward-room-to-department');
