dfa8fd3c9b0933bc875b44b53b43f70f45731fac
Renato Becker <renato.augusto.becker@gmail.com>
2020-03-25 19:26:46 -0300

    Regression: Remove deprecated Omnichannel setting used to fetch the queue data through subscription  (#17017)
    * Removed deprecated omnichannel setting,
    
    * Remove unnecessary setting creation.
    
    * Add method to clear the inquiry collection on client-side before set up the stream.

--- app/livechat/client/lib/stream/queueManager.js
+++ app/livechat/client/lib/stream/queueManager.js
@@ -1,24 +1,23 @@
 import { APIClient } from '../../../../utils/client';
-import { getLivechatInquiryCollection } from '../../collections/LivechatInquiry';
+import { LivechatInquiry } from '../../collections/LivechatInquiry';
 import { inquiryDataStream } from './inquiry';
 import { hasRole } from '../../../../authorization/client';
 
 let agentDepartments = [];
-const collection = getLivechatInquiryCollection();
 
 const events = {
 	added: (inquiry) => {
 		delete inquiry.type;
-		collection.insert(inquiry);
+		LivechatInquiry.insert(inquiry);
 	},
 	changed: (inquiry) => {
 		if (inquiry.status !== 'queued' || (inquiry.department && !agentDepartments.includes(inquiry.department))) {
-			return collection.remove(inquiry._id);
+			return LivechatInquiry.remove(inquiry._id);
 		}
 		delete inquiry.type;
-		collection.upsert({ _id: inquiry._id }, inquiry);
+		LivechatInquiry.upsert({ _id: inquiry._id }, inquiry);
 	},
-	removed: (inquiry) => collection.remove(inquiry._id),
+	removed: (inquiry) => LivechatInquiry.remove(inquiry._id),
 };
 
 const updateCollection = (inquiry) => { events[inquiry.type](inquiry); };
@@ -31,7 +30,7 @@ const getInquiriesFromAPI = async (url) => {
 };
 
 const updateInquiries = async (inquiries) => {
-	(inquiries || []).forEach((inquiry) => collection.upsert({ _id: inquiry._id }, inquiry));
+	(inquiries || []).forEach((inquiry) => LivechatInquiry.upsert({ _id: inquiry._id }, inquiry));
 };
 
 const getAgentsDepartments = async (userId) => {
@@ -54,12 +53,14 @@ const removeGlobalListener = () => {
 };
 
 export const initializeLivechatInquiryStream = async (userId) => {
-	collection.remove({});
+	LivechatInquiry.remove({});
+
 	if (agentDepartments.length) {
 		removeDepartmentsListeners(agentDepartments);
 	}
 	removeGlobalListener();
 	await updateInquiries(await getInquiriesFromAPI('livechat/inquiries.queued?sort={"ts": 1}'));
+
 	agentDepartments = (await getAgentsDepartments(userId)).map((department) => department.departmentId);
 	await addListenerForeachDepartment(userId, agentDepartments);
 	if (agentDepartments.length === 0 || hasRole(userId, 'livechat-manager')) {
