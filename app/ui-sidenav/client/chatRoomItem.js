35144deda702b20302b96d56457dbc8790506e96
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-03-27 17:02:25 -0300

    Regression: omnichannel manual queued sidebarlist (#17048)

--- app/ui-sidenav/client/chatRoomItem.js
+++ app/ui-sidenav/client/chatRoomItem.js
@@ -23,7 +23,7 @@ Template.chatRoomItem.helpers({
 		const roomData = {
 			...this,
 			icon: icon !== 'at' && icon,
-			avatar: roomTypes.getConfig(this.t).getAvatarPath(room, this),
+			avatar: roomTypes.getConfig(this.t).getAvatarPath(room || this),
 			username: this.name,
 			route: roomTypes.getRouteLink(this.t, this),
 			name: roomType.roomName(this),
