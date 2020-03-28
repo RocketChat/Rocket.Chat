beb55ff2e523b405f3dd2af4f1b53ed33b74aae2
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-03-28 00:05:34 -0300

    Fix: Last message of Group DMs not showing the sender

--- app/ui-sidenav/client/chatRoomItem.js
+++ app/ui-sidenav/client/chatRoomItem.js
@@ -31,6 +31,9 @@ Template.chatRoomItem.helpers({
 			active: false,
 			archivedClass,
 			status: this.t === 'd' || this.t === 'l',
+			...this.t === 'd' && {
+				isGroupChat: roomType.isGroupChat(room),
+			},
 		};
 		roomData.username = roomData.username || roomData.name;
 
