fbfef0951e7dd208d55ab07cf5d9f1ab54a59953
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-03-28 00:20:13 -0300

    Update chatRoomItem.js

--- app/ui-sidenav/client/chatRoomItem.js
+++ app/ui-sidenav/client/chatRoomItem.js
@@ -31,9 +31,7 @@ Template.chatRoomItem.helpers({
 			active: false,
 			archivedClass,
 			status: this.t === 'd' || this.t === 'l',
-			...this.t === 'd' && {
-				isGroupChat: roomType.isGroupChat(room),
-			},
+			isGroupChat: roomType.isGroupChat(room),
 		};
 		roomData.username = roomData.username || roomData.name;
 
