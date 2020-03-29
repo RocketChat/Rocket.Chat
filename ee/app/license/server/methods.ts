03cb0e1edd1ea228c8c099cffb8559abef383db1
Diego Sampaio <chinello@gmail.com>
2020-03-28 23:07:40 -0300

    Regression: Overwrite model functions on EE only when license applied (#17061)
    * Overwrite model function on EE only license applied
    
    * Convert to typescript
    
    Co-authored-by: Rodrigo Nascimento <rodrigoknascimento@gmail.com>

--- ee/app/license/server/methods.ts
+++ ee/app/license/server/methods.ts
@@ -1,9 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
 import { hasLicense } from './license';
 
 Meteor.methods({
-	'license:hasLicense'(feature) {
+	'license:hasLicense'(feature: string) {
+		check(feature, String);
+
 		return hasLicense(feature);
 	},
 });
