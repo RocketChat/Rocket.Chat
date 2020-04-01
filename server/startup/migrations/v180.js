7557b5ca29c6c18ce00fff819fa32be6f704bc6a
Renato Becker <renato.augusto.becker@gmail.com>
2020-03-31 21:46:47 -0300

    Remove old and closed Omnichannel Inquiries.

--- server/startup/migrations/v180.js
+++ server/startup/migrations/v180.js
@@ -5,8 +5,11 @@ Migrations.add({
 	version: 180,
 	up() {
 		// Remove Old Omnichannel Inquiries related to rooms already closed
-		LivechatInquiry.find({ status: 'queued' }).forEach((inquiry) => {
-			const { rid } = inquiry;
+		LivechatInquiry.find().forEach((inquiry) => {
+			const { rid, status } = inquiry;
+			if (status === 'closed') {
+				return LivechatInquiry.removeByRoomId(rid);
+			}
 
 			const room = LivechatRooms.findOneById(rid);
 			if (!room || room.closedAt) {
