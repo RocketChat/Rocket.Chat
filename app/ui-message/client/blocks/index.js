22bb34e00bc66da86727f7833c4ad1991980372b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-04-04 03:03:30 -0300

    Remove unused modules

--- app/ui-message/client/blocks/index.js
+++ app/ui-message/client/blocks/index.js
@@ -1,7 +1,4 @@
 import { createTemplateForComponent } from '../../../../client/reactAdapters';
-import './styles.css';
-import './TextBlock';
-import './ButtonElement.html';
 
 createTemplateForComponent('ModalBlock', () => import('./ModalBlock'));
 createTemplateForComponent('Blocks', () => import('./MessageBlock'));
