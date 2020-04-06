185131db553ecbc8bdc87c45dbbf7fee52bce155
Diego Sampaio <chinello@gmail.com>
2020-04-06 13:47:29 -0300

    Convert license modules to Set

--- ee/app/license/server/license.ts
+++ ee/app/license/server/license.ts
@@ -6,10 +6,6 @@ import { getBundleModules, isBundle } from './bundles';
 
 const EnterpriseLicenses = new EventEmitter();
 
-export interface IModules {
-	[key: string]: number;
-}
-
 export interface ILicense {
 	url: string;
 	expiry: string;
@@ -27,7 +23,7 @@ class LicenseClass {
 
 	private licenses: IValidLicense[] = [];
 
-	private modules: IModules = {};
+	private modules = new Set<string>();
 
 	_validateExpiration(expiration: string): boolean {
 		return new Date() > new Date(expiration);
@@ -49,7 +45,7 @@ class LicenseClass {
 				: [licenseModule];
 
 			modules.forEach((module) => {
-				this.modules[module] = 1;
+				this.modules.add(module);
 				EnterpriseLicenses.emit(`valid:${ module }`);
 			});
 		});
@@ -79,11 +75,11 @@ class LicenseClass {
 	}
 
 	hasModule(module: string): boolean {
-		return typeof this.modules[module] !== 'undefined';
+		return this.modules.has(module);
 	}
 
 	getModules(): string[] {
-		return Object.keys(this.modules).filter((module) => typeof this.modules[module] !== 'undefined');
+		return [...this.modules];
 	}
 
 	setURL(url: string): void {
