326d565519c6949016e3b428e06849f8bf5ee85b
Diego Sampaio <chinello@gmail.com>
2020-04-06 23:37:37 -0300

    Remove old setting

--- app/lib/server/startup/settings.js
+++ app/lib/server/startup/settings.js
@@ -2864,10 +2864,6 @@ settings.addGroup('Troubleshoot', function() {
 		type: 'boolean',
 		alert: 'Troubleshoot_Disable_Workspace_Sync_Alert',
 	});
-	this.add('Troubleshoot_Use_REST_For_DDP_Calls', false, {
-		type: 'boolean',
-		alert: 'Troubleshoot_Use_REST_For_DDP_Calls_Alert',
-	});
 });
 
 settings.init();
