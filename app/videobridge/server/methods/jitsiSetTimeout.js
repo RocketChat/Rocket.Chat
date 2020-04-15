63ff14e1c0f6a8ce3cbc1b9d59348143f0d399fd
rguerrero <rguerrero@scj.gob.cl>
2020-04-15 14:53:30 -0400

    Añadido traduccion de llamada a es.i18n

--- app/videobridge/server/methods/jitsiSetTimeout.js
+++ app/videobridge/server/methods/jitsiSetTimeout.js
@@ -29,7 +29,8 @@ Meteor.methods({
 		if (!jitsiTimeout || currentTime > jitsiTimeout - CONSTANTS.TIMEOUT / 2) {
 			Rooms.setJitsiTimeout(rid, new Date(currentTime + CONSTANTS.TIMEOUT));
 		}
-
+		console.log(`JitsiTimeout => ${jitsiTimeout}
+					 currentTime => ${currentTime}`);
 		if (!jitsiTimeout || currentTime > jitsiTimeout) {
 			const message = Messages.createWithTypeRoomIdMessageAndUser('jitsi_call_started', rid, '', Meteor.user(), {
 				actionLinks: [
