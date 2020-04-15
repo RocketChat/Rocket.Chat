559b62d9efe11f43e0577f0ae99fa1274d4a9e7d
rguerrero <rguerrero@scj.gob.cl>
2020-04-15 12:31:31 -0400

    Change translate messagetype

--- app/videobridge/lib/messageType.js
+++ app/videobridge/lib/messageType.js
@@ -6,12 +6,9 @@ import { settings } from '../../settings';
 
 Meteor.startup(function() {
 
-	const user =  Meteor.user();
-	const lng = (user && user.language) || settings.get('Language') || 'en';
-
 	MessageTypes.registerType({
 		id: 'jitsi_call_started',
 		system: true,
-		message: TAPi18n.__('Started_a_video_call', { lng }),
+		message: "Started_a_video_call",
 	});
 });
