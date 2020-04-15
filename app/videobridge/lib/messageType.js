c9ec4f19e2d9283569a2f74fe76759bc75eaa8b9
rguerrero <rguerrero@scj.gob.cl>
2020-04-15 12:03:08 -0400

    Translate fix on messageType

--- app/videobridge/lib/messageType.js
+++ app/videobridge/lib/messageType.js
@@ -2,11 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
 
 import { MessageTypes } from '../../ui-utils';
+import { settings } from '../../settings';
 
 Meteor.startup(function() {
+
+	const user =  Meteor.user();
+	const lng = (user && user.language) || settings.get('Language') || 'en';
+
 	MessageTypes.registerType({
 		id: 'jitsi_call_started',
 		system: true,
-		message: TAPi18n.__('Started_a_video_call'),
+		message: TAPi18n.__('Started_a_video_call', { lng }),
 	});
 });
