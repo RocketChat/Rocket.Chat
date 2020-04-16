2d21f595f4cb771c9ce3cef429e3683e32783862
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2020-04-16 10:06:06 -0300

    Not remove caller's data and add more tests

--- app/lib/server/functions/getFullUserData.js
+++ app/lib/server/functions/getFullUserData.js
@@ -70,21 +70,26 @@ const removePasswordInfo = (user) => {
 		delete user.services.email;
 		delete user.services.resume;
 		delete user.services.emailCode;
+		delete user.services.cloud;
+		delete user.services.email2fa;
+		delete user.services.totp;
 	}
 	return user;
 };
 
 export function getFullUserDataByIdOrUsername({ userId, filterId, filterUsername }) {
 	const caller = Users.findOneById(userId, { fields: { username: 1 } });
-	const canViewAllInfo = (userId === filterId || filterUsername === caller.username) || hasPermission(userId, 'view-full-other-user-info');
+	const myself = userId === filterId || filterUsername === caller.username;
+	const canViewAllInfo = myself || hasPermission(userId, 'view-full-other-user-info');
 
 	const fields = getFields(canViewAllInfo);
 
 	const options = {
 		fields,
 	};
+	const user = Users.findOneByIdOrUsername(filterId || filterUsername, options);
 
-	return removePasswordInfo(Users.findOneByIdOrUsername(filterId || filterUsername, options));
+	return myself ? user : removePasswordInfo(user);
 }
 
 export const getFullUserData = function({ userId, filter, limit: l }) {
