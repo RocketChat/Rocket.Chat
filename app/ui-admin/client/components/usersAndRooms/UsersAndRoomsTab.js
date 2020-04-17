00a7273e226d4ba9f2dec4f1201ec67dcb857ba4
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-04-17 12:21:54 -0300

    add new button

--- app/ui-admin/client/components/usersAndRooms/UsersAndRoomsTab.js
+++ app/ui-admin/client/components/usersAndRooms/UsersAndRoomsTab.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Tabs } from '@rocket.chat/fuselage';
+import React, { useCallback } from 'react';
+import { Tabs, ButtonGroup, Button, Icon } from '@rocket.chat/fuselage';
 
 import { Page } from '../../../../../client/components/basic/Page';
 import { useTranslation } from '../../../../../client/contexts/TranslationContext';
@@ -15,12 +15,21 @@ export function UsersAndRoomsTab({ route, tab, children, switchTab, ...props }) 
 	const mobile = useMediaQuery('(max-width: 420px)');
 	const small = useMediaQuery('(max-width: 780px)');
 
-
 	const router = useRoute(route);
+	const handleNew = useCallback(() => router.push({
+		context: 'new',
+	}), [router]);
 
 	return <Page {...props} flexDirection='row'>
 		<Page name='admin-user-and-room'>
-			<Page.Header title={t('Users_and_rooms')} />
+			<Page.Header title={t('Users_and_rooms')}>
+				{ tab === 'users' && <ButtonGroup>
+					<Button small onClick={handleNew} aria-label={t('New')}>
+						<Icon name='plus' />
+					</Button>
+				</ButtonGroup>
+				}
+			</Page.Header>
 			<Tabs>
 				<Tabs.Item selected={tab === 'users'} onClick={switchTab && switchTab.users}>{t('Users')}</Tabs.Item>
 				<Tabs.Item selected={tab === 'rooms'} onClick={switchTab && switchTab.rooms}>{t('Rooms')}</Tabs.Item>
