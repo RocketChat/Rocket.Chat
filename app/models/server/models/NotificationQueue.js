61eb21b00270b65a90ff40bcbd81e0c7e7934d33
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-04-18 23:42:25 -0300

    Add TTL of 2h

--- app/models/server/models/NotificationQueue.js
+++ app/models/server/models/NotificationQueue.js
@@ -4,7 +4,7 @@ export class NotificationQueue extends Base {
 	constructor() {
 		super('notification_queue');
 		this.tryEnsureIndex({ uid: 1 });
-		this.tryEnsureIndex({ ts: 1 });
+		this.tryEnsureIndex({ ts: 1 }, { expireAfterSeconds: 2 * 60 * 60 });
 		this.tryEnsureIndex({ schedule: 1 }, { sparse: true });
 		this.tryEnsureIndex({ sending: 1 }, { sparse: true });
 	}
