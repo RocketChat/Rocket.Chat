b1f343939df30bf83a7f1183c61f63d9324c9e5b
“mariaeduardacunha“ <eduarda.mcunha@outlook.com>
2020-04-20 19:05:14 -0300

    Rewriting JS view-logs functions

--- app/logger/client/components/ViewLogs/index.js
+++ app/logger/client/components/ViewLogs/index.js
@@ -1,12 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { Box } from '@rocket.chat/fuselage';
 
+import { ansispan } from '../../ansispan';
 import { useToastMessageDispatch } from '../../../../../client/contexts/ToastMessagesContext';
 import { useEndpoint } from '../../../../../client/contexts/ServerContext';
 import { useTranslation } from '../../../../../client/contexts/TranslationContext';
 import { Page } from '../../../../../client/components/basic/Page';
-import { ansispan } from '../../ansispan';
+import '../../views/viewLogs.css';
 
 export function ViewLogs() {
 	const [lines, setLines] = useState([]);
@@ -45,24 +46,135 @@ export function ViewLogs() {
 	const t = useTranslation();
 
 	const wrapperRef = useRef();
+	const newLogsRef = useRef();
+	const atBottomRef = useRef();
 
-	// const isAtBottom = useCallback((scrollThreshold) => {
-	// 	if (scrollThreshold == null) {
-	// 		scrollThreshold = 0;
-	// 	}
-	// 	if (wrapper.scrollTop + scrollThreshold >= wrapper.scrollHeight - wrapper.clientHeight) {
-	// 		newLogs.className = 'new-logs not';
-	// 		return true;
-	// 	}
-	// 	return false;
-	// });
+	const isAtBottom = useCallback((scrollThreshold) => {
+		const wrapper = wrapperRef.current;
+		const newLogs = newLogsRef.current;
+
+		if (scrollThreshold == null) {
+			scrollThreshold = 0;
+		}
+		if (wrapper.scrollTop + scrollThreshold >= wrapper.scrollHeight - wrapper.clientHeight) {
+			newLogs.className = 'new-logs not';
+			return true;
+		}
+		return false;
+	}, []);
+
+	const sendToBottom = useCallback(() => {
+		const wrapper = wrapperRef.current;
+		const newLogs = newLogsRef.current;
+
+		wrapper.scrollTop = wrapper.scrollHeight - wrapper.clientHeight;
+		newLogs.className = 'new-logs not';
+	}, []);
+
+	const checkIfScrollIsAtBottom = useCallback(() => {
+		atBottomRef.current = isAtBottom(100);
+	}, [isAtBottom]);
+
+	const sendToBottomIfNecessary = useCallback(() => {
+		if (atBottomRef.current === true && isAtBottom() !== true) {
+			sendToBottom();
+		} else if (atBottomRef.current === false) {
+			newLogsRef.current.className = 'new-logs';
+		}
+	}, [isAtBottom, sendToBottom]);
+
+	useEffect(() => {
+		if (window.MutationObserver) {
+			const observer = new MutationObserver((mutations) => {
+				mutations.forEach(() => sendToBottomIfNecessary());
+			});
+			observer.observe(wrapperRef.current, { childList: true });
+
+			return () => {
+				observer.disconnect();
+			};
+		}
+
+		const handleSubtreeModified = () => {
+			sendToBottomIfNecessary();
+		};
+		wrapperRef.current.addEventListener('DOMSubtreeModified', handleSubtreeModified);
+
+		return () => {
+			wrapperRef.current.removeEventListener('DOMSubtreeModified', handleSubtreeModified);
+		};
+	}, [sendToBottomIfNecessary]);
+
+	useEffect(() => {
+		const handleWindowResize = () => {
+			setTimeout(() => {
+				sendToBottomIfNecessary();
+			}, 100);
+		};
+
+		window.addEventListener('resize', handleWindowResize);
+
+		return () => {
+			window.removeEventListener('resize', handleWindowResize);
+		};
+	}, [sendToBottomIfNecessary]);
+
+	const handleWheel = useCallback(() => {
+		atBottomRef.current = false;
+		setTimeout(() => {
+			checkIfScrollIsAtBottom();
+		}, 100);
+	}, [checkIfScrollIsAtBottom]);
+
+	const handleTouchStart = () => {
+		atBottomRef.current = false;
+	};
+
+	const handleTouchEnd = useCallback(() => {
+		setTimeout(() => {
+			checkIfScrollIsAtBottom();
+		}, 100);
+	}, [checkIfScrollIsAtBottom]);
+
+	const handleScroll = useCallback(() => {
+		atBottomRef.current = false;
+		setTimeout(() => {
+			checkIfScrollIsAtBottom();
+		}, 100);
+	}, [checkIfScrollIsAtBottom]);
+
+	const handleClick = useCallback(() => {
+		atBottomRef.current = true;
+		sendToBottomIfNecessary();
+	}, [sendToBottomIfNecessary]);
 
 	return <Page>
 		<Page.Header title={t('View Logs')} />
 		<Page.Content>
-			<Box ref={wrapperRef} className='view-logs__terminal' margin='none' paddingBlock='8px' paddingInline='10px' flexGrow={1} width='full' height='full' dangerouslySetInnerHTML={{ __html: lines.map(({ string }) => ansispan(string)).join('\n') }}
-				// onMouseWheel={handleHandleWheel}
+			<Box
+				ref={wrapperRef}
+				className='view-logs__terminal'
+				margin='none'
+				paddingBlock='8px'
+				paddingInline='10px'
+				flexGrow={1}
+				width='full'
+				height='full'
+				dangerouslySetInnerHTML={{ __html: lines.sort((a, b) => a.ts - b.ts).map(({ string }) => ansispan(string)).join('\n') }}
+				onClick={handleWheel}
+				onWheel={handleWheel}
+				onTouchStart={handleTouchStart}
+				onTouchEnd={handleTouchEnd}
+				onScroll={handleScroll}
 			/>
+			<div
+				ref={newLogsRef}
+				className='new-logs js-new-logs not color-primary-action-color'
+				onClick={handleClick}
+			>
+				<i className='icon-down-big' />
+				<span>{t('New_logs')}</span>
+			</div>
 		</Page.Content>
 	</Page>;
 }
