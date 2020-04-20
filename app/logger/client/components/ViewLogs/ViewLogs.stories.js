6ea7f63106bb211e7a2b766509ee10176cf5d8d2
“mariaeduardacunha“ <eduarda.mcunha@outlook.com>
2020-04-20 19:06:09 -0300

    Adding decorators on ViewLogs.stories

--- app/logger/client/components/ViewLogs/ViewLogs.stories.js
+++ app/logger/client/components/ViewLogs/ViewLogs.stories.js
@@ -5,11 +5,11 @@ import { ViewLogs } from '.';
 export default {
 	title: 'admin/pages/ViewLogs',
 	component: ViewLogs,
-	// decorators: [
-	//  (storyFn) => <SettingsState>
-	//      {storyFn()}
-	//  </SettingsState>,
-	// ],
+	decorators: [
+		(storyFn) => <div className='rc-old' style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+			{storyFn()}
+		</div>,
+	],
 };
 
 export const _default = () =>
