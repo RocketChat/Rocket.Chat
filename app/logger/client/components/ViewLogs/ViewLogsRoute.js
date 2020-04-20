7dd9ff9b5ab88fc911c93b2957589a5c65a098e6
“mariaeduardacunha“ <eduarda.mcunha@outlook.com>
2020-04-20 19:06:47 -0300

    refatoring canAccess permission

--- app/logger/client/components/ViewLogs/ViewLogsRoute.js
+++ app/logger/client/components/ViewLogs/ViewLogsRoute.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { NotAuthorizedPage } from '../../../../ui-admin/client/components/settings/NotAuthorizedPage';
+import { NotAuthorizedPage } from '../../../../ui-admin/client/components/NotAuthorizedPage';
 import { usePermission } from '../../../../../client/contexts/AuthorizationContext';
 
 import { ViewLogs } from '.';
 
 export default function ViewLogsRoute() {
 	const canAccessMailer = usePermission('view-logs');
-	return canAccessMailer ? <ViewLogs/> : <NotAuthorizedPage/>;
+
+	if (!canAccessMailer) {
+		return <NotAuthorizedPage />;
+	}
+
+	return <ViewLogs />;
 }
