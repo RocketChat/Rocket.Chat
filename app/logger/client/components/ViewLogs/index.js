556f995c720e7fab6aba61d35c6e42351659a662
“mariaeduardacunha“ <eduarda.mcunha@outlook.com>
2020-04-20 15:35:03 -0300

    Tasso changes Co-authored-by: Tasso Evangelista

--- app/logger/client/components/ViewLogs/index.js
+++ app/logger/client/components/ViewLogs/index.js
@@ -1,24 +1,41 @@
 import { Meteor } from 'meteor/meteor';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Box } from '@rocket.chat/fuselage';
 
+import { useToastMessageDispatch } from '../../../../../client/contexts/ToastMessagesContext';
+import { useEndpoint } from '../../../../../client/contexts/ServerContext';
 import { useTranslation } from '../../../../../client/contexts/TranslationContext';
 import { Page } from '../../../../../client/components/basic/Page';
 import { ansispan } from '../../ansispan';
-import { APIClient } from '../../../../utils/client';
 
 export function ViewLogs() {
-	const [stdout, setStdout] = useState([]);
+	const [lines, setLines] = useState([]);
+
+	const dispatchToastMessage = useToastMessageDispatch();
+
+	const getStdoutQueue = useEndpoint('GET', 'stdout.queue');
+
 	useEffect(() => {
-		(async () => {
-			const data = await APIClient.v1.get('stdout.queue');
+		const fetchLines = async () => {
+			try {
+				const { queue } = await getStdoutQueue();
+				setLines(queue);
+			} catch (error) {
+				dispatchToastMessage({ type: 'error', message: error });
+			}
+		};
 
-			setStdout(data.queue);
-		})();
+		fetchLines();
+	}, []);
 
+	useEffect(() => {
 		const stdoutStreamer = new Meteor.Streamer('stdout');
 
-		stdoutStreamer.on('stdout', (item) => stdout.push(item));
+		const handleStdout = (line) => {
+			setLines((lines) => [...lines, line]);
+		};
+
+		stdoutStreamer.on('stdout', handleStdout);
 
 		return () => {
 			stdoutStreamer.removeListener('stdout');
@@ -27,8 +44,25 @@ export function ViewLogs() {
 
 	const t = useTranslation();
 
-	return <Page _id='viewlogs' i18nLabel='ViewLogs'>
-		<Page.Header title={t('View Logs')}></Page.Header>
-		<Box componentClassName='view-logs__terminal' width='full' height='full' dangerouslySetInnerHTML={{ __html: stdout.map(({ string }) => ansispan(string)).join('\n') }} />
+	const wrapperRef = useRef();
+
+	// const isAtBottom = useCallback((scrollThreshold) => {
+	// 	if (scrollThreshold == null) {
+	// 		scrollThreshold = 0;
+	// 	}
+	// 	if (wrapper.scrollTop + scrollThreshold >= wrapper.scrollHeight - wrapper.clientHeight) {
+	// 		newLogs.className = 'new-logs not';
+	// 		return true;
+	// 	}
+	// 	return false;
+	// });
+
+	return <Page>
+		<Page.Header title={t('View Logs')} />
+		<Page.Content>
+			<Box ref={wrapperRef} className='view-logs__terminal' margin='none' paddingBlock='8px' paddingInline='10px' flexGrow={1} width='full' height='full' dangerouslySetInnerHTML={{ __html: lines.map(({ string }) => ansispan(string)).join('\n') }}
+				// onMouseWheel={handleHandleWheel}
+			/>
+		</Page.Content>
 	</Page>;
 }
