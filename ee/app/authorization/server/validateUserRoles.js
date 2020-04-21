fba088833bebb3c7ada4edca55723709b6bdb657
Diego Sampaio <chinello@gmail.com>
2020-04-21 04:51:56 -0300

    Return "first"

--- ee/app/authorization/server/validateUserRoles.js
+++ ee/app/authorization/server/validateUserRoles.js
@@ -8,20 +8,22 @@ export const validateUserRoles = function(userId, userData) {
 		return;
 	}
 
-	if (userData.roles.includes('guest')) {
-		if (userData.roles.length >= 2) {
-			throw new Meteor.Error('error-guests-cant-have-other-roles', "Guest users can't receive any other role", {
-				method: 'insertOrUpdateUser',
-				field: 'Assign_role',
-			});
-		}
+	if (!userData.roles.includes('guest')) {
+		return;
+	}
 
-		const guestCount = Users.getActiveLocalGuestCount(userData._id);
-		if (guestCount >= getMaxGuestUsers()) {
-			throw new Meteor.Error('error-max-guests-number-reached', 'Maximum number of guests reached.', {
-				method: 'insertOrUpdateUser',
-				field: 'Assign_role',
-			});
-		}
+	if (userData.roles.length >= 2) {
+		throw new Meteor.Error('error-guests-cant-have-other-roles', "Guest users can't receive any other role", {
+			method: 'insertOrUpdateUser',
+			field: 'Assign_role',
+		});
+	}
+
+	const guestCount = Users.getActiveLocalGuestCount(userData._id);
+	if (guestCount >= getMaxGuestUsers()) {
+		throw new Meteor.Error('error-max-guests-number-reached', 'Maximum number of guests reached.', {
+			method: 'insertOrUpdateUser',
+			field: 'Assign_role',
+		});
 	}
 };
