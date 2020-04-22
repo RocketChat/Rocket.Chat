04daffedaffbda7a587a99ae46e12eee64e335f1
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-04-22 17:03:51 -0300

    Avatar init

---
+++ client/components/basic/setAvatar/SetAvatar.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TextInput, Box, Icon } from '@rocket.chat/fuselage';
+
+import { SetAvatar } from './SetAvatar';
+
+
+export default {
+	title: 'directory/table',
+	component: SetAvatar,
+	decorators: [(fn) => <div children={fn()} style={{ height: '100vh' }} />],
+};
+
+
+export const _default = () => {
+	// const Search = () => <Box mb='x16' is='form' display='flex' flexDirection='column'>
+	// 	<TextInput placeholder='Search...' addon={<Icon name='magnifier' size='x20'/>}/>
+	// </Box>;
+
+	return <SetAvatar />;
+};
+
+// export const _default = () => {
+// 	const Search = () => <Box mb='x16' is='form' display='flex' flexDirection='column'>
+// 		<TextInput placeholder='Search...' addon={<Icon name='magnifier' size='x20'/>}/>
+// 	</Box>;
+
+
+// 	const header = [
+// 		<Th>Name</Th>,
+// 		<Th>Email</Th>,
+// 		<Th>Data</Th>,
+// 		<Th>Info</Th>,
+// 	];
+// 	return <GenericTable FilterComponent={Search} header={header} />;
+// };
