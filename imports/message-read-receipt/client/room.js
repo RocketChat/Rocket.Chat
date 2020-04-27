345a75ff4c8cbb744fce9fcccedef70a87c45570
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-04-27 11:40:56 -0300

    Read receipt on thread message

--- imports/message-read-receipt/client/room.js
+++ imports/message-read-receipt/client/room.js
@@ -1,29 +1,40 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+
 import { t } from '../../../app/utils';
 import { modal, MessageAction } from '../../../app/ui-utils';
 import { messageArgs } from '../../../app/ui-utils/client/lib/messageArgs';
 import { settings } from '../../../app/settings';
 
-MessageAction.addButton({
-	id: 'receipt-detail',
-	icon: 'info-circled',
-	label: 'Info',
-	context: ['starred', 'message', 'message-mobile'],
-	action() {
-		const { msg: message } = messageArgs(this);
-		modal.open({
-			title: t('Info'),
-			content: 'readReceipts',
-			data: {
-				messageId: message._id,
+
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		const enabled = settings.get('Message_Read_Receipt_Store_Users');
+
+		if (!enabled) {
+			return MessageAction.removeButton('receipt-detail');
+		}
+
+		MessageAction.addButton({
+			id: 'receipt-detail',
+			icon: 'info-circled',
+			label: 'Info',
+			context: ['starred', 'message', 'message-mobile', 'threads'],
+			action() {
+				const { msg: message } = messageArgs(this);
+				modal.open({
+					title: t('Info'),
+					content: 'readReceipts',
+					data: {
+						messageId: message._id,
+					},
+					showConfirmButton: true,
+					showCancelButton: false,
+					confirmButtonText: t('Close'),
+				});
 			},
-			showConfirmButton: true,
-			showCancelButton: false,
-			confirmButtonText: t('Close'),
+			order: 10,
+			group: 'menu',
 		});
-	},
-	condition() {
-		return settings.get('Message_Read_Receipt_Store_Users');
-	},
-	order: 10,
-	group: 'menu',
+	});
 });
