827f674ca422fd5a72f16ea9690d8ab96e475265
Martin Schoeler <martin.schoeler@rocket.chat>
2020-04-29 18:43:10 -0300

    resolve  conflicts

--- app/custom-sounds/client/admin/route.js
+++ app/custom-sounds/client/admin/route.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-
 import { registerAdminRoute } from '../../../../client/admin';
-=======
-import { registerAdminRoute } from '../../../ui-admin/client';
->>>>>>> 5c22a19... Wip on custom sounds refactor
 
 registerAdminRoute('/custom-sounds/:context?', {
 	name: 'custom-sounds',
