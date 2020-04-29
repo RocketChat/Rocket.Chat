942efee6e52f22998e12f663da0b9c290e381a17
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-04-29 17:14:21 -0300

    Update DescriptionList

--- client/admin/info/DescriptionList.js
+++ client/admin/info/DescriptionList.js
@@ -7,7 +7,7 @@ export const DescriptionList = ({ children, title, ...props }) => <>
 	{title && <Box display='flex' justifyContent='flex-end' width='30%' paddingInline='x8'>
 		{title}
 	</Box>}
-	<Table marginBlockEnd='x32' width='full' {...props}>
+	<Table striped marginBlockEnd='x32' width='full' {...props}>
 		<Table.Body>
 			{children}
 		</Table.Body>
@@ -16,7 +16,7 @@ export const DescriptionList = ({ children, title, ...props }) => <>
 
 const Entry = ({ children, label, ...props }) =>
 	<Table.Row {...props}>
-		<Table.Cell is='th' scope='col' width='30%' align='end' color='hint' fontScale='p2' style={style}>{label}</Table.Cell>
+		<Table.Cell is='th' scope='col' width='30%' align='end' color='hint' backgroundColor='surface' fontScale='p2' style={style}>{label}</Table.Cell>
 		<Table.Cell width='70%' align='start' color='default' style={style}>{children}</Table.Cell>
 	</Table.Row>;
 
