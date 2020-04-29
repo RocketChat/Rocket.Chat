13deca739ffe23d83b379c5a3f79e2046f288a32
shubhsherl <shubham.singh094@yahoo.com>
2020-04-29 15:29:29 +0530

    add role cache collection

--- server/main.js
+++ server/main.js
@@ -70,6 +70,7 @@ import './methods/userSetUtcOffset';
 import './publications/messages';
 import './publications/ownUser';
 import './publications/room';
+import './publications/roles';
 import './publications/settings';
 import './publications/spotlight';
 import './publications/subscription';
