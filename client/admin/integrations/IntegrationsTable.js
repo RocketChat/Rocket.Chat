3e6ad23598eed329281dfe0a9308dc4f0fc1899f
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-04-30 20:56:42 -0300

    Using the new ResizeObserver

--- client/admin/integrations/IntegrationsTable.js
+++ client/admin/integrations/IntegrationsTable.js
@@ -1,11 +1,11 @@
 import { Box, Table, TextInput, Icon } from '@rocket.chat/fuselage';
-import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
+import { useDebouncedValue, useResizeObserver } from '@rocket.chat/fuselage-hooks';
 import React, { useMemo, useCallback, useState, useEffect } from 'react';
 
 import { GenericTable, Th } from '../../../app/ui/client/components/GenericTable';
 import { useTranslation } from '../../contexts/TranslationContext';
 import { useRoute } from '../../contexts/RouterContext';
-import { useEndpointData } from '../../hooks/useEndpointData';
+import { useEndpointDataExperimental } from '../../hooks/useEndpointDataExperimental';
 import { useFormatDateAndTime } from '../../hooks/useFormatDateAndTime';
 
 const style = { whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' };
@@ -30,9 +30,17 @@ const useQuery = (params, sort) => useMemo(() => ({
 	...params.current && { offset: params.current },
 }), [JSON.stringify(params), JSON.stringify(sort)]);
 
+const useResizeInlineBreakpoint = (sizes = [], debounceDelay = 0) => {
+	const { ref, borderBoxSize } = useResizeObserver({ debounceDelay });
+	const inlineSize = borderBoxSize ? borderBoxSize.inlineSize : 0;
+	sizes = useMemo(() => sizes.map((current) => (inlineSize ? inlineSize > current : true)), [inlineSize]);
+	return [ref, ...sizes];
+};
+
 export function IntegrationsTable() {
 	const t = useTranslation();
 	const formatDateAndTime = useFormatDateAndTime();
+	const [ref, isBig] = useResizeInlineBreakpoint([700], 200);
 
 	const [params, setParams] = useState({ text: '', current: 0, itemsPerPage: 25 });
 	const [sort, setSort] = useState(['name', 'asc']);
@@ -41,13 +49,13 @@ export function IntegrationsTable() {
 	const debouncedSort = useDebouncedValue(sort, 500);
 	const query = useQuery(debouncedParams, debouncedSort);
 
-	const data = useEndpointData('integrations.list', query) || {};
+	const { data } = useEndpointDataExperimental('integrations.list', query);
 
 	const router = useRoute('admin-integrations');
 
 	const onClick = (_id) => () => router.push({
 		context: 'edit',
-		integrationid: _id,
+		integrationId: _id,
 	});
 
 	const onHeaderClick = (id) => {
@@ -60,26 +68,24 @@ export function IntegrationsTable() {
 		setSort([id, 'asc']);
 	};
 
-	// const mediaQuery = useMediaQuery('(min-width: 1024px)');
-
 	const header = useMemo(() => [
 		<Th key={'name'} direction={sort[1]} active={sort[0] === 'name'} onClick={onHeaderClick} sort='name'>{t('Name')}</Th>,
 		<Th key={'channel'} direction={sort[1]} active={sort[0] === 'channel'} onClick={onHeaderClick} sort='channel'>{t('Post_to')}</Th>,
 		<Th key={'_createdBy'} direction={sort[1]} active={sort[0] === '_createdBy'} onClick={onHeaderClick} sort='_createdBy'>{t('Created_by')}</Th>,
-		<Th key={'_createdAt'} direction={sort[1]} active={sort[0] === '_createdAt'} onClick={onHeaderClick} sort='_createdAt'>{t('Created_at')}</Th>,
+		isBig && <Th key={'_createdAt'} direction={sort[1]} active={sort[0] === '_createdAt'} onClick={onHeaderClick} sort='_createdAt'>{t('Created_at')}</Th>,
 		<Th key={'username'} direction={sort[1]} active={sort[0] === 'username'} onClick={onHeaderClick} sort='username'>{t('Created_as')}</Th>,
-	].filter(Boolean), [sort/* , mediaQuery */]);
+	].filter(Boolean), [sort, isBig]);
 
 	const renderRow = useCallback(({ name, _id, username, _createdAt, _createdBy: { username: createdBy }, channel }) =>
 		<Table.Row key={_id} onKeyDown={onClick(_id)} onClick={onClick(_id)} tabIndex={0} role='link' action qa-user-id={_id}>
 			<Table.Cell style={style}>{name}</Table.Cell>
 			<Table.Cell style={style}>{channel.join(', ')}</Table.Cell>
 			<Table.Cell style={style}>{createdBy}</Table.Cell>
-			<Table.Cell style={style}>{formatDateAndTime(_createdAt)}</Table.Cell>
+			{isBig && <Table.Cell style={style}>{formatDateAndTime(_createdAt)}</Table.Cell>}
 			<Table.Cell style={style}>{username}</Table.Cell>
 		</Table.Row>);
 
-	return <GenericTable FilterComponent={FilterByText} header={header} renderRow={renderRow} results={data.integrations} total={data.total} setParams={setParams} params={params} />;
+	return <GenericTable ref={ref} FilterComponent={FilterByText} header={header} renderRow={renderRow} results={data && data.integrations} total={data && data.total} setParams={setParams} params={params} />;
 }
 
 export default IntegrationsTable;
