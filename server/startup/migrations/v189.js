1b342fe8f6540472b66575a1c54c0e95c1e39e16
Renato Becker <renato.augusto.becker@gmail.com>
2020-05-05 09:39:38 -0300

    Remove unnecessary console.log.

--- server/startup/migrations/v189.js
+++ server/startup/migrations/v189.js
@@ -4,7 +4,6 @@ import { Settings } from '../../../app/models/server';
 Migrations.add({
 	version: 189,
 	up() {
-		console.log('hey');
 		Settings.remove({ _id: 'Livechat_Knowledge_Enabled' });
 		Settings.remove({ _id: 'Livechat_Knowledge_Apiai_Key' });
 		Settings.remove({ _id: 'Livechat_Knowledge_Apiai_Language' });
