b75f19e4c7e0758fabc090488c31b57825a3c5fe
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-05-05 23:24:05 -0300

    Regression: Fix Avatar Url Provider when CDN_PREFIX_ALL is false (#17542)

--- client/providers/AvatarUrlProvider.js
+++ client/providers/AvatarUrlProvider.js
@@ -9,6 +9,6 @@ export function AvatarUrlProvider({ children }) {
 	const cdn = useSetting('CDN_PREFIX_ALL');
 	const cdnPrefix = useSetting('CDN_PREFIX');
 
-	const avatarBase = useMemo(() => ({ baseUrl: `${ cdn && cdnPrefix }${ base }` }), [cdn, cdnPrefix]);
+	const avatarBase = useMemo(() => ({ baseUrl: `${ (cdn && cdnPrefix) || '' }${ base }` }), [cdn, cdnPrefix]);
 	return <Avatar.Context.Provider children={children} value={avatarBase} />;
 }
