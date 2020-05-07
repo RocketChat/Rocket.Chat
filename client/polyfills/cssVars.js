ca6146a0ee4f0dd960d8b6e611f6e6f7d15435c8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-05-07 16:52:31 -0300

    Speed up CSS variables polyfill

--- client/polyfills/cssVars.js
+++ client/polyfills/cssVars.js
@@ -1,117 +1,84 @@
 import _ from 'underscore';
 
+const findDeclarations = (code) => (code.match(/(--[^:; ]+:..*?;)/g) ?? [])
+	.map((declaration) => {
+		const [, name, value] = /(.*?):\s*(.*?)\s*;/.exec(declaration);
+		return [
+			name,
+			value.indexOf('var(') >= 0
+				? (variables) => value.replace(/var\((--.*?)\)/gm, (_, name) => variables[name](variables))
+				: () => value,
+		];
+	});
+
+const replaceReferences = (code, variables) => code.replace(/var\((--.*?)\)/gm, (_, name) => variables[name](variables));
+
+let cssVariablesElement = null;
+const originalCodes = new Map();
+
+const update = _.debounce(() => {
+	const declarations = [].concat(
+		findDeclarations(cssVariablesElement.innerHTML),
+		...Array.from(originalCodes.values(), findDeclarations),
+	);
+
+	const variables = Object.fromEntries(declarations);
+
+	originalCodes.forEach((originalCode, element) => {
+		const patchedCode = replaceReferences(originalCode, variables);
+
+		let patchedElement = element.nextElementSibling;
+		if (!patchedElement || !patchedElement.classList.contains('patched-css-variables')) {
+			patchedElement = document.createElement('style');
+			patchedElement.type = 'text/css';
+			patchedElement.classList.add('patched-css-variables');
+			element.insertAdjacentElement('afterend', patchedElement);
+		}
+
+		const { sheet } = patchedElement;
+		while (sheet.cssRules.length > 0) {
+			sheet.deleteRule(0);
+		}
+		sheet.insertRule(`@media all {${ patchedCode }}`, 0);
+	});
+}, 100);
+
+const findAndPatchFromLinkElements = () => {
+	Array.from(document.querySelectorAll('link[type="text/css"].__meteor-css__')).forEach(async (linkElement) => {
+		const url = linkElement.getAttribute('href');
+		try {
+			const response = await fetch(url);
+			const code = await response.text();
+			originalCodes.set(linkElement, code);
+		} catch (error) {
+			console.warn(error);
+		} finally {
+			update();
+		}
+	});
+};
+
+const waitAndInitialize = () => {
+	if (document.readyState !== 'complete') {
+		requestAnimationFrame(waitAndInitialize);
+		return;
+	}
+
+	cssVariablesElement = document.getElementById('css-variables');
+
+	const cssVariablesElementObserver = new MutationObserver(() => {
+		update();
+	});
+
+	cssVariablesElementObserver.observe(cssVariablesElement, { childList: true });
+
+	findAndPatchFromLinkElements();
+};
+
 (() => {
 	if (window.CSS && window.CSS.supports && window.CSS.supports('(--foo: red)')) {
 		return;
 	}
 
-	const cssVarPoly = {};
-
-	cssVarPoly.init = () => {
-		console.time('cssVarPoly');
-		cssVarPoly.ratifiedVars = {};
-		cssVarPoly.varsByBlock = [];
-		cssVarPoly.oldCSS = [];
-		cssVarPoly.findCSS();
-		cssVarPoly.updateCSS();
-		console.timeEnd('cssVarPoly');
-	};
-
-	cssVarPoly.findCSS = () => {
-		const styleBlocks = Array.from(document.querySelectorAll('#css-variables, link[type="text/css"].__meteor-css__'));
-		styleBlocks.forEach((block, counter) => {
-			if (block.nodeName === 'STYLE') {
-				const theCSS = block.innerHTML;
-				cssVarPoly.findSetters(theCSS, counter);
-				cssVarPoly.oldCSS[counter] = theCSS;
-				return;
-			}
-
-			if (block.nodeName === 'LINK') {
-				const url = block.getAttribute('href');
-				cssVarPoly.oldCSS[counter] = '';
-				cssVarPoly.getLink(url, counter, (counter, request) => {
-					cssVarPoly.findSetters(request.responseText, counter);
-					cssVarPoly.oldCSS[counter] = request.responseText;
-					cssVarPoly.updateCSS();
-				});
-			}
-		});
-	};
-
-	cssVarPoly.findSetters = (theCSS, counter) => {
-		cssVarPoly.varsByBlock[counter] = theCSS.match(/(--[^:; ]+:..*?;)/g);
-	};
-
-	cssVarPoly.updateCSS = _.debounce(() => {
-		cssVarPoly.ratifySetters(cssVarPoly.varsByBlock);
-		cssVarPoly.oldCSS.filter((e) => e).forEach((css, id) => {
-			const newCSS = cssVarPoly.replaceGetters(css, cssVarPoly.ratifiedVars);
-			const el = document.querySelector(`#inserted${ id }`);
-
-			if (el) {
-				el.innerHTML = newCSS;
-			} else {
-				const style = document.createElement('style');
-				style.type = 'text/css';
-				style.innerHTML = newCSS;
-				style.classList.add('inserted');
-				style.id = `inserted${ id }`;
-				document.getElementsByTagName('head')[0].appendChild(style);
-			}
-		});
-	}, 100);
-
-	cssVarPoly.replaceGetters = (oldCSS, varList) => oldCSS.replace(/var\((--.*?)\)/gm, (all, variable) => varList[variable]);
-
-	cssVarPoly.ratifySetters = (varList) => {
-		varList.filter((curVars) => curVars).forEach((curVars) => {
-			curVars.forEach((theVar) => {
-				const matches = theVar.split(/:\s*/);
-				cssVarPoly.ratifiedVars[matches[0]] = matches[1].replace(/;/, '');
-			});
-		});
-		Object.keys(cssVarPoly.ratifiedVars).filter((key) => cssVarPoly.ratifiedVars[key].indexOf('var') > -1).forEach((key) => {
-			cssVarPoly.ratifiedVars[key] = cssVarPoly.ratifiedVars[key].replace(/var\((--.*?)\)/gm, (all, variable) => cssVarPoly.ratifiedVars[variable]);
-		});
-	};
-
-	cssVarPoly.getLink = (url, counter, success) => {
-		const request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.overrideMimeType('text/css;');
-
-		request.onload = () => {
-			if (request.status >= 200 && request.status < 400) {
-				if (typeof success === 'function') {
-					success(counter, request);
-				}
-			} else {
-				console.warn('an error was returned from:', url);
-			}
-		};
-
-		request.onerror = () => {
-			console.warn('we could not get anything from:', url);
-		};
-
-		request.send();
-	};
-
-	const waitForCssVariables = () => {
-		if (document.readyState !== 'complete') {
-			requestAnimationFrame(waitForCssVariables);
-			return;
-		}
-
-		const cssVariablesStyleObserver = new MutationObserver(() => {
-			cssVarPoly.init();
-		});
-
-		cssVariablesStyleObserver.observe(document.querySelector('#css-variables'), { childList: true });
-
-		cssVarPoly.init();
-	};
-
-	waitForCssVariables();
+	waitAndInitialize();
 })();
