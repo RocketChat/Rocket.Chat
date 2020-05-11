9692c4541c541710a074a2ce90a580321ca0e3f0
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-05-11 11:57:49 -0300

    Added Modals

--- client/admin/integrations/edit/EditIncomingWebhook.js
+++ client/admin/integrations/edit/EditIncomingWebhook.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { Field, TextInput, Box, Headline, Skeleton, ToggleSwitch, Icon, TextAreaInput, FieldGroup, Margins, Button } from '@rocket.chat/fuselage';
 
+import { SuccessModal, DeleteWarningModal } from './EditIntegrationsPage';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useEndpointDataExperimental, ENDPOINT_STATES } from '../../../hooks/useEndpointDataExperimental';
 import { useMethod, useAbsoluteUrl } from '../../../contexts/ServerContext';
@@ -42,14 +43,25 @@ function EditIncomingWebhook({ data, setData, onChange, ...props }) {
 	const dispatchToastMessage = useToastMessageDispatch();
 
 	const [newData, setNewData] = useState({});
+	const [modal, setModal] = useState();
 
 	const hasUnsavedChanges = useMemo(() => Object.values(newData).filter((current) => current === null).length < Object.keys(newData).length, [JSON.stringify(newData)]);
 
 	const deleteQuery = useMemo(() => ({ type: 'webhook-incoming', integrationId: data._id }), [data._id]);
-	const deleteIntegration = useEndpointAction('POST', 'integrations.remove', deleteQuery, t('Your_entry_has_been_deleted'));
+	const deleteIntegration = useEndpointAction('POST', 'integrations.remove', deleteQuery);
 	const saveIntegration = useMethod('updateIncomingIntegration');
 
 	const router = useRoute('admin-integrations');
+
+	const handleDeleteIntegration = () => {
+		const closeModal = () => setModal();
+		const onDelete = async () => {
+			const result = await deleteIntegration();
+			if (result.success) { setModal(<SuccessModal onClose={() => { closeModal(); router.push({}); }}/>); }
+		};
+
+		setModal(<DeleteWarningModal onDelete={onDelete} onCancel={closeModal} />);
+	};
 
 	const absoluteUrl = useAbsoluteUrl();
 
@@ -94,120 +106,123 @@ function EditIncomingWebhook({ data, setData, onChange, ...props }) {
 
 	const hilightedExampleJson = useHilightedCode('json', JSON.stringify(exampleData, null, 2));
 
-	return <Page.ScrollableContent pb='x24' mi='neg-x24' is='form' qa-admin-user-edit='form' { ...props }>
-		<Margins block='x16'>
-			<FieldGroup width='x600' alignSelf='center'>
-				<Field>
-					<Field.Label>{t('Enabled')}</Field.Label>
-					<Field.Row>
-						<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center'>
-							<Box mie='x8' color={ enabled ? 'hint' : 'default' }>{t('False')}</Box>
-							<ToggleSwitch checked={enabled} onChange={handleChange('enabled', data.enabled, () => !enabled)} />
-							<Box mis='x8' color={ enabled ? 'default' : 'hint' }>{t('True')}</Box>
-						</Box>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Label>{t('Name_optional')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={name} onChange={handleChange('name', data.name)}/>
-					</Field.Row>
-					<Field.Hint>{t('You_should_name_it_to_easily_manage_your_integrations')}</Field.Hint>
-				</Field>
-				<Field>
-					<Field.Label>{t('Post_to_Channel')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={channel} onChange={handleChange('channel', data.channel.join(', '))} addon={<Icon name='at' size='x20'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('Messages_that_are_sent_to_the_Incoming_WebHook_will_be_posted_here')}</Field.Hint>
-					<Field.Hint dangerouslySetInnerHTML={{ __html: t('Start_with_s_for_user_or_s_for_channel_Eg_s_or_s', '@', '#', '@john', '#general') }} />
-				</Field>
-				<Field>
-					<Field.Label>{t('Post_as')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={username} onChange={handleChange('username', data.username)} addon={<Icon name='user' size='x20'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('Choose_the_username_that_this_integration_will_post_as')}</Field.Hint>
-					<Field.Hint>{t('Should_exists_a_user_with_this_username')}</Field.Hint>
-				</Field>
-				<Field>
-					<Field.Label>{`${ t('Alias') } (${ t('optional') })`}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={alias} onChange={handleChange('alias', data.alias)} addon={<Icon name='edit' size='x20'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('Choose_the_alias_that_will_appear_before_the_username_in_messages')}</Field.Hint>
-				</Field>
-				<Field>
-					<Field.Label>{`${ t('Avatar_URL') } (${ t('optional') })`}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={avatarUrl} onChange={handleChange('avatarUrl', data.avatarUrl)} addon={<Icon name='user-rounded' size='x20' alignSelf='center'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('You_can_change_a_different_avatar_too')}</Field.Hint>
-					<Field.Hint>{t('Should_be_a_URL_of_an_image')}</Field.Hint>
-				</Field>
-				<Field>
-					<Field.Label>{`${ t('Emoji') } (${ t('optional') })`}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={emoji} onChange={handleChange('emoji', data.emoji)} addon={<Icon name='emoji' size='x20' alignSelf='center'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('You_can_use_an_emoji_as_avatar')}</Field.Hint>
-					<Field.Hint dangerouslySetInnerHTML={{ __html: t('Example_s', ':ghost:') }} />
-				</Field>
-				<Field>
-					<Field.Label>{t('Script_Enabled')}</Field.Label>
-					<Field.Row>
-						<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center'>
-							<Box mie='x8' color={ scriptEnabled ? 'hint' : 'default' }>{t('False')}</Box>
-							<ToggleSwitch checked={scriptEnabled} onChange={handleChange('scriptEnabled', data.scriptEnabled, () => !scriptEnabled)} />
-							<Box mis='x8' color={ scriptEnabled ? 'default' : 'hint' }>{t('True')}</Box>
-						</Box>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Label>{t('Script')}</Field.Label>
-					<Field.Row>
-						<TextAreaInput rows={10} flexGrow={1} value={script} onChange={handleChange('script', data.script)} addon={<Icon name='code' size='x20' alignSelf='center'/>}/>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Label>{t('Webhook_URL')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={url} addon={<Icon name='permalink' size='x20'/>}/>
-					</Field.Row>
-					<Field.Hint>{t('Send_your_JSON_payloads_to_this_URL')}</Field.Hint>
-				</Field>
-				<Field>
-					<Field.Label>{t('Token')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={`${ data._id }/${ data.token }`} addon={<Icon name='key' size='x20'/>}/>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Label>{t('Example_payload')}</Field.Label>
-					<Field.Row>
-						<Box fontScale='p1' withRichContent flexGrow={1}>
-							<pre><code dangerouslySetInnerHTML={{ __html: hilightedExampleJson }}></code></pre>
-						</Box>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Label>{t('Curl')}</Field.Label>
-					<Field.Row>
-						<TextInput flexGrow={1} value={curlData} addon={<Icon name='code' size='x20'/>}/>
-					</Field.Row>
-				</Field>
-				<Field>
-					<Field.Row display='flex' flexDirection='column'>
-						<Box display='flex' flexDirection='row' justifyContent='space-between' w='full'>
-							<Margins inlineEnd='x4'>
-								<Button flexGrow={1} type='reset' disabled={!hasUnsavedChanges} onClick={() => setNewData({})}>{t('Reset')}</Button>
-								<Button mie='none' flexGrow={1} disabled={!hasUnsavedChanges} onClick={handleSave}>{t('Save')}</Button>
-							</Margins>
-						</Box>
-						<Button mbs='x4' primary danger w='full' onClick={() => { deleteIntegration(); router.push({}); }}>{t('Delete')}</Button>
-					</Field.Row>
-				</Field>
-			</FieldGroup>
-		</Margins>
-	</Page.ScrollableContent>;
+	return <>
+		<Page.ScrollableContent pb='x24' mi='neg-x24' is='form' qa-admin-user-edit='form' { ...props }>
+			<Margins block='x16'>
+				<FieldGroup width='x600' alignSelf='center'>
+					<Field>
+						<Field.Label>{t('Enabled')}</Field.Label>
+						<Field.Row>
+							<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center'>
+								<Box mie='x8' color={ enabled ? 'hint' : 'default' }>{t('False')}</Box>
+								<ToggleSwitch checked={enabled} onChange={handleChange('enabled', data.enabled, () => !enabled)} />
+								<Box mis='x8' color={ enabled ? 'default' : 'hint' }>{t('True')}</Box>
+							</Box>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Name_optional')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={name} onChange={handleChange('name', data.name)}/>
+						</Field.Row>
+						<Field.Hint>{t('You_should_name_it_to_easily_manage_your_integrations')}</Field.Hint>
+					</Field>
+					<Field>
+						<Field.Label>{t('Post_to_Channel')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={channel} onChange={handleChange('channel', data.channel.join(', '))} addon={<Icon name='at' size='x20'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('Messages_that_are_sent_to_the_Incoming_WebHook_will_be_posted_here')}</Field.Hint>
+						<Field.Hint dangerouslySetInnerHTML={{ __html: t('Start_with_s_for_user_or_s_for_channel_Eg_s_or_s', '@', '#', '@john', '#general') }} />
+					</Field>
+					<Field>
+						<Field.Label>{t('Post_as')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={username} onChange={handleChange('username', data.username)} addon={<Icon name='user' size='x20'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('Choose_the_username_that_this_integration_will_post_as')}</Field.Hint>
+						<Field.Hint>{t('Should_exists_a_user_with_this_username')}</Field.Hint>
+					</Field>
+					<Field>
+						<Field.Label>{`${ t('Alias') } (${ t('optional') })`}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={alias} onChange={handleChange('alias', data.alias)} addon={<Icon name='edit' size='x20'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('Choose_the_alias_that_will_appear_before_the_username_in_messages')}</Field.Hint>
+					</Field>
+					<Field>
+						<Field.Label>{`${ t('Avatar_URL') } (${ t('optional') })`}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={avatarUrl} onChange={handleChange('avatarUrl', data.avatarUrl)} addon={<Icon name='user-rounded' size='x20' alignSelf='center'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('You_can_change_a_different_avatar_too')}</Field.Hint>
+						<Field.Hint>{t('Should_be_a_URL_of_an_image')}</Field.Hint>
+					</Field>
+					<Field>
+						<Field.Label>{`${ t('Emoji') } (${ t('optional') })`}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={emoji} onChange={handleChange('emoji', data.emoji)} addon={<Icon name='emoji' size='x20' alignSelf='center'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('You_can_use_an_emoji_as_avatar')}</Field.Hint>
+						<Field.Hint dangerouslySetInnerHTML={{ __html: t('Example_s', ':ghost:') }} />
+					</Field>
+					<Field>
+						<Field.Label>{t('Script_Enabled')}</Field.Label>
+						<Field.Row>
+							<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center'>
+								<Box mie='x8' color={ scriptEnabled ? 'hint' : 'default' }>{t('False')}</Box>
+								<ToggleSwitch checked={scriptEnabled} onChange={handleChange('scriptEnabled', data.scriptEnabled, () => !scriptEnabled)} />
+								<Box mis='x8' color={ scriptEnabled ? 'default' : 'hint' }>{t('True')}</Box>
+							</Box>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Script')}</Field.Label>
+						<Field.Row>
+							<TextAreaInput rows={10} flexGrow={1} value={script} onChange={handleChange('script', data.script)} addon={<Icon name='code' size='x20' alignSelf='center'/>}/>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Webhook_URL')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={url} addon={<Icon name='permalink' size='x20'/>}/>
+						</Field.Row>
+						<Field.Hint>{t('Send_your_JSON_payloads_to_this_URL')}</Field.Hint>
+					</Field>
+					<Field>
+						<Field.Label>{t('Token')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={`${ data._id }/${ data.token }`} addon={<Icon name='key' size='x20'/>}/>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Example_payload')}</Field.Label>
+						<Field.Row>
+							<Box fontScale='p1' withRichContent flexGrow={1}>
+								<pre><code dangerouslySetInnerHTML={{ __html: hilightedExampleJson }}></code></pre>
+							</Box>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Label>{t('Curl')}</Field.Label>
+						<Field.Row>
+							<TextInput flexGrow={1} value={curlData} addon={<Icon name='code' size='x20'/>}/>
+						</Field.Row>
+					</Field>
+					<Field>
+						<Field.Row display='flex' flexDirection='column'>
+							<Box display='flex' flexDirection='row' justifyContent='space-between' w='full'>
+								<Margins inlineEnd='x4'>
+									<Button flexGrow={1} type='reset' disabled={!hasUnsavedChanges} onClick={() => setNewData({})}>{t('Reset')}</Button>
+									<Button mie='none' flexGrow={1} disabled={!hasUnsavedChanges} onClick={handleSave}>{t('Save')}</Button>
+								</Margins>
+							</Box>
+							<Button mbs='x4' primary danger w='full' onClick={handleDeleteIntegration} >{t('Delete')}</Button>
+						</Field.Row>
+					</Field>
+				</FieldGroup>
+			</Margins>
+		</Page.ScrollableContent>
+		{ modal }
+	</>;
 }
