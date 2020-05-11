74206168f943ab7a5e59d974f2df9fd773d846f3
Douglas Gubert <douglas.gubert@gmail.com>
2020-05-11 00:09:25 -0300

    Fix Fibers errors

--- app/apps/server/storage/storage.js
+++ app/apps/server/storage/storage.js
@@ -6,86 +6,46 @@ export class AppRealStorage extends AppStorage {
 		this.db = data;
 	}
 
-	create(item) {
-		return new Promise((resolve, reject) => {
-			item.createdAt = new Date();
-			item.updatedAt = new Date();
+	async create(item) {
+		item.createdAt = new Date();
+		item.updatedAt = new Date();
 
-			let doc;
+		const doc = this.db.findOne({ $or: [{ id: item.id }, { 'info.nameSlug': item.info.nameSlug }] });
 
-			try {
-				doc = this.db.findOne({ $or: [{ id: item.id }, { 'info.nameSlug': item.info.nameSlug }] });
-			} catch (e) {
-				return reject(e);
-			}
+		if (doc) {
+			throw new Error('App already exists.');
+		}
 
-			if (doc) {
-				return reject(new Error('App already exists.'));
-			}
+		const id = this.db.insert(item);
+		item._id = id;
 
-			try {
-				const id = this.db.insert(item);
-				item._id = id;
+		return item;
+	}
 
-				resolve(item);
-			} catch (e) {
-				reject(e);
-			}
+	async retrieveOne(id) {
+		return this.db.findOne({ $or: [{ _id: id }, { id }] });
+	}
+
+	async retrieveAll() {
+		const docs = this.db.find({}).fetch();
+
+		const items = new Map();
+
+		docs.forEach((i) => items.set(i.id, i));
+
+		return items;
+	}
+
+	async update(item) {
+		return this.db.findAndModify({
+			query: { id: item.id },
+			update: item,
+			new: true,
 		});
 	}
 
-	retrieveOne(id) {
-		return new Promise((resolve, reject) => {
-			let doc;
-
-			try {
-				doc = this.db.findOne({ $or: [{ _id: id }, { id }] });
-			} catch (e) {
-				return reject(e);
-			}
-
-			resolve(doc);
-		});
-	}
-
-	retrieveAll() {
-		return new Promise((resolve, reject) => {
-			let docs;
-
-			try {
-				docs = this.db.find({}).fetch();
-			} catch (e) {
-				return reject(e);
-			}
-
-			const items = new Map();
-
-			docs.forEach((i) => items.set(i.id, i));
-
-			resolve(items);
-		});
-	}
-
-	update(item) {
-		return new Promise((resolve, reject) => {
-			try {
-				this.db.update({ id: item.id }, item);
-				resolve(item.id);
-			} catch (e) {
-				return reject(e);
-			}
-		}).then(this.retrieveOne.bind(this));
-	}
-
-	remove(id) {
-		return new Promise((resolve, reject) => {
-			try {
-				this.db.remove({ id });
-			} catch (e) {
-				return reject(e);
-			}
-
-			resolve({ success: true });
-		});
+	async remove(id) {
+		this.db.remove({ id });
+		return { success: true };
 	}
 }
