e5448157e870267599f317045dddb463d6f57a67
Pierre Lehnen <pierre.lehnen@rocket.chat>
2020-05-13 14:10:30 -0300

    Added missing license checks to LDAP Enterprise code

--- ee/app/ldap-enterprise/server/hooks/beforeSearchAll.js
+++ ee/app/ldap-enterprise/server/hooks/beforeSearchAll.js
@@ -1,13 +1,16 @@
 import { callbacks } from '../../../../../app/callbacks';
+import { onLicense } from '../../../license/server';
 
-callbacks.add('ldap.beforeSearchAll', (searchParams) => {
-	const { options } = searchParams;
+onLicense('ldap-enterprise', () => {
+	callbacks.add('ldap.beforeSearchAll', (searchParams) => {
+		const { options } = searchParams;
 
-	if (!Array.isArray(options.attributes)) {
-		options.attributes = options.attributes ? [options.attributes] : ['*'];
-	}
+		if (!Array.isArray(options.attributes)) {
+			options.attributes = options.attributes ? [options.attributes] : ['*'];
+		}
 
-	options.attributes.push('pwdAccountLockedTime');
+		options.attributes.push('pwdAccountLockedTime');
 
-	return searchParams;
-}, callbacks.priority.MEDIUM, 'ldap-return-attribute-AccountLockedTime');
+		return searchParams;
+	}, callbacks.priority.MEDIUM, 'ldap-return-attribute-AccountLockedTime');
+});
--- ee/app/ldap-enterprise/server/hooks/syncExistentUser.js
+++ ee/app/ldap-enterprise/server/hooks/syncExistentUser.js
@@ -1,12 +1,15 @@
 import { callbacks } from '../../../../../app/callbacks';
 import { logger } from '../../../../../app/ldap/server/sync';
 import { setUserActiveStatus } from '../../../../../app/lib/server/functions/setUserActiveStatus';
+import { onLicense } from '../../../license/server';
 
-callbacks.add('ldap.afterSyncExistentUser', ({ ldapUser, user }) => {
-	const activate = !!ldapUser && !ldapUser.pwdAccountLockedTime;
+onLicense('ldap-enterprise', () => {
+	callbacks.add('ldap.afterSyncExistentUser', ({ ldapUser, user }) => {
+		const activate = !!ldapUser && !ldapUser.pwdAccountLockedTime;
 
-	if (activate !== user.active) {
-		setUserActiveStatus(user._id, activate);
-		logger.info(`${ activate ? 'Activating' : 'Deactivating' } user ${ user.name } (${ user._id })`);
-	}
-}, callbacks.priority.MEDIUM, 'ldap-disable-enable-users');
+		if (activate !== user.active) {
+			setUserActiveStatus(user._id, activate);
+			logger.info(`${ activate ? 'Activating' : 'Deactivating' } user ${ user.name } (${ user._id })`);
+		}
+	}, callbacks.priority.MEDIUM, 'ldap-disable-enable-users');
+});
