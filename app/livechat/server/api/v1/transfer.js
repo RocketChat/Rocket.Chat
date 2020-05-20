9dde4ffc96ba78ac1486dece58e6462fd7c1e7ba
Renato Becker <renato.augusto.becker@gmail.com>
2020-05-20 17:22:12 -0300

    Fixes on the endpoint implementation.

--- app/livechat/server/api/v1/transfer.js
+++ app/livechat/server/api/v1/transfer.js
@@ -7,34 +7,30 @@ import { findLivechatTransferHistory } from '../lib/transfer';
 
 API.v1.addRoute('livechat/transfer.history/:rid', { authRequired: true }, {
 	get() {
-		try {
-			check(this.urlParams, {
-				rid: String,
-			});
+		check(this.urlParams, {
+			rid: String,
+		});
 
-			const { rid } = this.urlParams;
+		const { rid } = this.urlParams;
 
-			const room = LivechatRooms.findOneById(rid);
-			if (!room || room.t !== 'l') {
-				throw new Meteor.Error('invalid-room');
-			}
-
-			const { offset, count } = this.getPaginationItems();
-			const { sort } = this.parseJsonQuery();
-
-			const history = Promise.await(findLivechatTransferHistory({
-				userId: this.userId,
-				rid,
-				pagination: {
-					offset,
-					count,
-					sort,
-				},
-			}));
-
-			return API.v1.success(history);
-		} catch (e) {
-			return API.v1.failure(e);
+		const room = LivechatRooms.findOneById(rid, { _id: 1 });
+		if (!room) {
+			throw new Meteor.Error('invalid-room');
 		}
+
+		const { offset, count } = this.getPaginationItems();
+		const { sort } = this.parseJsonQuery();
+
+		const history = Promise.await(findLivechatTransferHistory({
+			userId: this.userId,
+			rid,
+			pagination: {
+				offset,
+				count,
+				sort,
+			},
+		}));
+
+		return API.v1.success(history);
 	},
 });
