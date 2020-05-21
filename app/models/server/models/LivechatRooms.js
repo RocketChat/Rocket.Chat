eae28e0b86a9c446f66ab6940587091baeec93ec
Renato Becker <renato.augusto.becker@gmail.com>
2020-05-21 01:32:13 -0300

    [NEW][ENTERPRISE] Support Omnichannel conversations auditing (#17692)
    Co-authored-by: Rodrigo Nascimento <rodrigoknascimento@gmail.com>

--- app/models/server/models/LivechatRooms.js
+++ app/models/server/models/LivechatRooms.js
@@ -252,6 +252,16 @@ export class LivechatRooms extends Base {
 		return this.find(query);
 	}
 
+	findByVisitorIdAndAgentId(visitorId, agentId, options) {
+		const query = {
+			t: 'l',
+			...visitorId && { 'v._id': visitorId },
+			...agentId && { 'servedBy._id': agentId },
+		};
+
+		return this.find(query, options);
+	}
+
 	findByVisitorId(visitorId) {
 		const query = {
 			t: 'l',
