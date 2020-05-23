5b4b96ee1f01c59899849fa2345ec8554efd297a
Douglas Gubert <douglas.gubert@gmail.com>
2020-05-23 16:15:04 -0300

    Start changing the AsyncIterator APIs

--- app/apps/server/bridges/rooms.js
+++ app/apps/server/bridges/rooms.js
@@ -93,49 +93,30 @@ export class AppRoomBridge {
 			throw new Error(`Room could not be found by id ${ roomId }`);
 		}
 
-		const orcha = this.orch;
-		const totalCount = await MessagesRaw.countVisibleByRoomId(roomId);
+		const orchestrator = this.orch;
 
 		return {
 			[Symbol.asyncIterator]: function getMessageByRoomIdIterator() {
-				let exhausted = false;
-				let currentIndex = 0;
+				const cursor = MessagesRaw.findVisibleByRoomId(roomId);
 
 				return {
-					async next() {
-						// currentIndex will always be 0 based whereas count is always 1 based.
-						// so we check if it is equal to or greater than the total count
-						// and if so, then we are done with this iteration
-						if (currentIndex >= totalCount || exhausted) {
-							return {
+					next() {
+						if (cursor.isClosed() || !cursor.hasNext()) {
+							return Promise.resolve({
 								value: undefined,
 								done: true,
-							};
+							});
 						}
 
-						// We skip the current index because if it is the first time, then we skip zero
-						// if it is the second time then we skip one, and so on and so forth
-						const options = { limit: 1, sort: { ts: 1 }, skip: currentIndex };
-						const result = await MessagesRaw.findVisibleByRoomId(roomId, options).toArray();
-
-						if (result.length !== 1) {
-							return {
-								value: undefined,
-								done: true,
-							};
-						}
-
-						currentIndex++;
-						return {
-							value: orcha.getConverters().get('messages').convertMessage(result[0]),
+						return cursor.next().then((document) => ({
+							value: orchestrator.getConverters().get('messages').convertMessage(document),
 							done: false,
-						};
+						}));
 					},
 					async throw(e) {
 						throw e;
 					},
 					async return() {
-						exhausted = true;
 						return {
 							done: true,
 						};
