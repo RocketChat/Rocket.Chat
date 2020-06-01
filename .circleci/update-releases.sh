6f041baa25e14f45d43d77698db216c14815d294
Bradley Hilton <graywolf336@craftyn.com>
2020-06-01 16:04:03 -0500

    Fix invalid develop payload to release service (#17799)
    The payload sent to the release service contains an invalid reference to the tag/version and wasn't working when building develop branch.

--- .circleci/update-releases.sh
+++ .circleci/update-releases.sh
@@ -3,7 +3,7 @@ set -euvo pipefail
 IFS=$'\n\t'
 
 curl -H "Content-Type: application/json" -H "X-Update-Token: $UPDATE_TOKEN" -d \
-    "{\"commit\": \"$CIRCLE_SHA1\", \"tag\": \"$CIRCLE_TAG\", \"branch\": \"$CIRCLE_BRANCH\", \"artifactName\": \"$ARTIFACT_NAME\", \"releaseType\": \"$RC_RELEASE\" }" \
+    "{\"commit\": \"$CIRCLE_SHA1\", \"tag\": \"$RC_VERSION\", \"branch\": \"$CIRCLE_BRANCH\", \"artifactName\": \"$ARTIFACT_NAME\", \"releaseType\": \"$RC_RELEASE\" }" \
     https://releases.rocket.chat/update
 
 # Makes build fail if the release isn't there
