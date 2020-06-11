eae6ce3d8d88cd6e5e42015da25a1df25eafeb1d
Alan Sikora <alansikora@gmail.com>
2020-06-11 11:10:01 -0300

    moved migration to 192

---
+++ server/startup/migrations/v192.js
@@ -0,0 +1,33 @@
+import { fork } from 'child_process';
+
+import { Migrations } from '../../../app/migrations/server';
+import { getLocalSrc } from '../../../app/events/server/lib/getLocalSrc';
+import { getFederationDomain } from '../../../app/federation/server/lib/getFederationDomain';
+
+Migrations.add({
+	version: 190,
+	up() {
+		Promise.await(new Promise((resolve, reject) => {
+			const prc = fork(Assets.absoluteFilePath('migrations/v192/v1ToV2.js'), {
+				env: {
+					LOCAL_SRC: getLocalSrc(),
+					FEDERATION_DOMAIN: getFederationDomain(),
+					MONGO_URL: process.env.MONGO_URL,
+				},
+			});
+
+			prc.on('exit', function(code) {
+				console.log(`process exit code ${ code }`);
+
+				if (code === 0) {
+					resolve();
+				} else {
+					reject('Error running external script');
+				}
+			});
+		}));
+	},
+	down() {
+	// Once you go 175 you never go back
+	},
+});
