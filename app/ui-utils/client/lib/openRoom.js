cacc9021715c8daa84290b442470b98f9c11a769
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-06-16 15:08:10 -0300

    Link redirect to threads

--- app/ui-utils/client/lib/openRoom.js
+++ app/ui-utils/client/lib/openRoom.js
@@ -8,7 +8,7 @@ import { Session } from 'meteor/session';
 import mem from 'mem';
 import _ from 'underscore';
 
-import { ChatSubscription, Rooms } from '../../../models';
+import { Messages, ChatSubscription, Rooms } from '../../../models';
 import { settings } from '../../../settings';
 import { callbacks } from '../../../callbacks';
 import { roomTypes } from '../../../utils';
@@ -111,7 +111,15 @@ export const openRoom = async function(type, name) {
 			}
 
 			if (FlowRouter.getQueryParam('msg')) {
-				const msg = { _id: FlowRouter.getQueryParam('msg'), rid: room._id };
+				const messageId = FlowRouter.getQueryParam('msg');
+				const msg = { _id: messageId, rid: room._id };
+
+				const message = Messages.findOne({ ss_id: msg._id }) || (await call('getMessages', [msg._id]))[0];
+
+				if (message && (message.tmid || message.tcount)) {
+					return FlowRouter.setParams({ tab: 'thread', context: message.tmid || message._id });
+				}
+
 				RoomHistoryManager.getSurroundingMessages(msg);
 			}
 
