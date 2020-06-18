e420745573f006e7c821e01ff8234701d01b426f
Diego Sampaio <chinello@gmail.com>
2020-06-18 15:38:50 -0300

    Fix tests

--- app/api/server/v1/video-conference.js
+++ app/api/server/v1/video-conference.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
+import { Rooms } from '../../../models/server';
 import { API } from '../api';
 
 API.v1.addRoute('video-conference/jitsi.update-timeout', { authRequired: true }, {
@@ -7,6 +8,11 @@ API.v1.addRoute('video-conference/jitsi.update-timeout', { authRequired: true },
 		const { roomId } = this.bodyParams;
 		if (!roomId) {
 			return API.v1.failure('The "roomId" parameter is required!');
+		}
+
+		const room = Rooms.findOneById(roomId, { fields: { _id: 1 } });
+		if (!room) {
+			return API.v1.failure('Room does not exist!');
 		}
 
 		const jitsiTimeout = Meteor.runAsUser(this.userId, () => Meteor.call('jitsi:updateTimeout', roomId));
