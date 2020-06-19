69b38ce9562d8c65c928345e98e0d8e65f4bc8af
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-06-19 11:21:07 -0300

    Fix after send message callback

--- app/lib/server/functions/sendMessage.js
+++ app/lib/server/functions/sendMessage.js
@@ -247,7 +247,7 @@ export const sendMessage = function(user, message, room, upsert = false) {
 		Defer other updates as their return is not interesting to the user
 		*/
 		// Execute all callbacks
-		Meteor.defer(() => callbacks.run('afterSaveMessage', message, room, user._id));
+		callbacks.runAsync('afterSaveMessage', message, room, user._id);
 		return message;
 	}
 };
