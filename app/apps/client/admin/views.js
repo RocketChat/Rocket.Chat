d9d4d4b2eb447682b43ab4c844ed9825889a68b8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-06-20 03:12:00 -0300

    Prune obsolete templates

--- app/apps/client/admin/views.js
+++
@@ -1,8 +0,0 @@
-import './modalTemplates/iframeModal.html';
-import './modalTemplates/iframeModal';
-import './apps';
-import './appInstall.html';
-import './appInstall';
-import './appLogs.html';
-import './appLogs';
-import './appManage';
