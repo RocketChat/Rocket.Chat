efe9de70022a19f10d2edc512edc33491c4b3b01
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-06-29 17:16:16 -0300

    Fix exitr-room on livechat

--- app/utils/lib/roomExit.js
+++ app/utils/lib/roomExit.js
@@ -12,7 +12,7 @@ export const roomExit = function() {
 	Tracker.afterFlush(() => {
 		const context = FlowRouter.current();
 
-		if (oldRoute && testIfPathAreEquals(oldRoute.params.name || oldRoute.params.rid, context.params.name || context.params.rid)) {
+		if (oldRoute && testIfPathAreEquals(oldRoute.params.name || oldRoute.params.rid || oldRoute.params.id, context.params.name || context.params.rid || context.params.id)) {
 			return;
 		}
 		// 7370 - Close flex-tab when opening a room on mobile UI
