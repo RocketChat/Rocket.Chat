d9e1b993ab9c326d69705d7fef7f69f6a7a69df1
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-07-03 19:41:39 -0300

    Delete old files

--- app/ui-account/client/index.js
+++ app/ui-account/client/index.js
@@ -1,8 +1,6 @@
 import './accountIntegrations.html';
-import './accountPreferences.html';
 import './avatar/avatar.html';
 import './avatar/prompt.html';
 import './accountIntegrations';
-import './accountPreferences';
 import './avatar/avatar';
 import './avatar/prompt';
