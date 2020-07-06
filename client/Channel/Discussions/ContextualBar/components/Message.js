fe669a481267c65791bda26990c28efc4d69ae04
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-06 15:08:21 -0300

    Discussion stories

--- client/Channel/Discussions/ContextualBar/components/Message.js
+++ client/Channel/Discussions/ContextualBar/components/Message.js
@@ -6,7 +6,7 @@ import RawText from '../../../../components/basic/RawText';
 import * as MessageTemplate from '../../../components/Message';
 
 
-export default React.memo(function Message({ _id, msg, following, username, name, ts, dcount, t = (text) => text, participants, handleFollowButton, unread, mention, all, formatDate = (e) => e, dlm, className = [], ...props }) {
+export default React.memo(function Message({ _id, msg, following, username, name = username, ts, dcount, t = (text) => text, participants, handleFollowButton, unread, mention, all, formatDate = (e) => e, dlm, className = [], ...props }) {
 	return <MessageTemplate.Message {...props} className={className}>
 		<MessageTemplate.Container mb='neg-x2'>
 			<UserAvatar username={username} rcx-message__avatar size='x36'/>
@@ -19,8 +19,8 @@ export default React.memo(function Message({ _id, msg, following, username, name
 			<MessageTemplate.BodyClamp><Icon name='discussion' size='x20' mie='x2'/><RawText>{msg}</RawText></MessageTemplate.BodyClamp>
 			<Box mi='neg-x2' flexDirection='row' display='flex' alignItems='baseline' mbs='x8'>
 				{!dcount && <Box display='flex' alignItems='center' is='span' fontSize='x12' color='neutral-700' fontWeight='600'>{t('No_messages_yet')}</Box>}
-				{ dcount && <Box display='flex' alignItems='center' is='span' fontSize='x12' color='neutral-700' fontWeight='600'><Icon name='discussion' size='x20' mi='x2'/>{dcount}</Box>}
-				{ dcount && <Box display='flex' alignItems='center' is='span' fontSize='x12' color='neutral-700' fontWeight='600' withTruncatedText flexShrink={1} mi='x2'><Icon name='clock' size='x20' mi='x2' /> {formatDate(dlm)} </Box>}
+				{ !!dcount && <Box display='flex' alignItems='center' is='span' fontSize='x12' color='neutral-700' fontWeight='600'><Icon name='discussion' size='x20' mi='x2'/>{dcount}</Box>}
+				{ !!dcount && <Box display='flex' alignItems='center' is='span' fontSize='x12' color='neutral-700' fontWeight='600' withTruncatedText flexShrink={1} mi='x2'><Icon name='clock' size='x20' mi='x2' /> {formatDate(dlm)} </Box>}
 			</Box>
 		</MessageTemplate.Container>
 	</MessageTemplate.Message>;
