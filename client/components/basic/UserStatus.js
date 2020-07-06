2486a8f89307aca6fd5a43d58374b29cb6106f8f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-06 20:46:40 -0300

    User Status Component

---
+++ client/components/basic/UserStatus.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box } from '@rocket.chat/fuselage';
+
+const Base = (props) => <Box size='x12' borderRadius='full' flexShrink={0} {...props}/>;
+
+export const Busy = () => <Base bg='danger-500'/>;
+export const Away = () => <Base bg='warning-600'/>;
+export const Online = () => <Base bg='success-500'/>;
+export const Offline = () => <Base bg='neutral-600'/>;
