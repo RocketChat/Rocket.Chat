589a753e80e0e19887e7dd44e98d3deed75f3425
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-07-06 17:26:51 +0000

    Fix First Time Caching Libs in PWA
    Improve SW Logic
    
    DOn't refetch fonts

--- client/importServiceWorker.js
+++ client/importServiceWorker.js
@@ -7,6 +7,15 @@ Meteor.startup(() => {
 				scope: './',
 			})
 			.then(function(reg) {
+				if (reg.installing) {
+					const sw = reg.installing || reg.waiting;
+					sw.onstatechange = function() {
+						if (sw.state === 'installed') {
+							// SW installed. Reload page.
+							window.location.reload();
+						}
+					};
+				}
 				console.log(`Service worker has been registered for scope: ${ reg.scope }`);
 			});
 	}
