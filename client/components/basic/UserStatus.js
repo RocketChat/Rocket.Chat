814f33c1f785fd0653d11c6d78513e8fe682ea57
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-07 14:51:31 -0300

    getStatus Helper

--- client/components/basic/UserStatus.js
+++ client/components/basic/UserStatus.js
@@ -7,3 +7,17 @@ export const Busy = () => <Base bg='danger-500'/>;
 export const Away = () => <Base bg='warning-600'/>;
 export const Online = () => <Base bg='success-500'/>;
 export const Offline = () => <Base bg='neutral-600'/>;
+
+
+export const getStatus = (status) => {
+	switch (status) {
+		case 'online':
+			return <Online/>;
+		case 'busy':
+			return <Busy/>;
+		case 'away':
+			return <Away/>;
+		default:
+			return <Offline/>;
+	}
+};
