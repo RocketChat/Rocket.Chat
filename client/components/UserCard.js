7b22e1c1c7efc3db21b00470fb9b0daf3690a1b1
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-06 21:24:24 -0300

    Loading State

--- client/components/UserCard.js
+++ client/components/UserCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Tag, Button, Icon } from '@rocket.chat/fuselage';
+import { Box, Tag, Button, Icon, Skeleton } from '@rocket.chat/fuselage';
 
 import { ActionButton } from './basic/Buttons/ActionButton';
 import UserAvatar from './basic/avatar/UserAvatar';
@@ -17,43 +17,60 @@ export const Action = ({ icon }) => <Button small mi='x2'><Icon name={icon} size
 
 export const Info = (props) => <Box mb='x4' is='p' fontSize='p1' fontSize='p1' fontScale='p1' letterSpacing='p1' fontWeight='p1' color='hint' withTruncatedText {...props} />;
 
-const Roles = ({ roles }) =>
-	<Info mi='neg-x2' height='16px'>
-		{roles.map((role, index) => (
-			<Tag
-				pb={0}
-				mi='x2'
-				disabled
-				fontSize='c2'
-				fontSize='c2'
-				fontScale='c2'
-				letterSpacing='c2'
-				fontWeight='c2'
-				key={index}
-			>
-				{role}
-			</Tag>
-		))}
+const Roles = ({ children }) =>
+	<Info rcx-user-card__roles mi='neg-x2' height='16px' display='flex'>
+		{children}
 	</Info>;
 
-const UserCard = ({ name, customStatus, roles = [], bio, status = <Status.Offline/>, actions, localTime }) => <Box rcx-user-card elevation='1' p='x24' display='flex' borderRadius='x2' width='439px' heigth='230px'>
-	<Box><UserAvatar size='x124'/>
+const Role = ({ children }) => <Tag
+	pb={0}
+	mi='x2'
+	disabled
+	fontSize='c2'
+	fontSize='c2'
+	fontScale='c2'
+	letterSpacing='c2'
+	fontWeight='c2'
+	children={children}
+/>;
+
+const UserCardConteiner = (props) => <Box rcx-user-card elevation='1' p='x24' display='flex' borderRadius='x2' width='439px' heigth='230px' {...props}/>;
+const UserCard = ({
+	name = <Skeleton width='100%'/>,
+	customStatus = <Skeleton width='100%'/>,
+	roles = <>
+		<Skeleton width='32%' mi='x2'/>
+		<Skeleton width='32%' mi='x2'/>
+		<Skeleton width='32%' mi='x2'/>
+	</>,
+	bio = <>
+		<Skeleton width='100%'/>
+		<Skeleton width='100%'/>
+		<Skeleton width='100%'/>
+	</>,
+	status = <Status.Offline/>,
+	actions,
+	localTime = <Skeleton width='100%'/>,
+}) => <UserCardConteiner>
+	<Box>
+		<UserAvatar size='x124'/>
 		{ actions && <Box display='flex' mb='x8' align='center' justifyContent='center'>
 			{actions}
 		</Box>}
 	</Box>
 	<Box display='flex' flexDirection='column' flexGrow={1} flexShrink={1} mis='x24' width='1px'>
-		<Box display='flex' alignItems='center' is='p' fontSize='s2' fontSize='s2' fontScale='s2' letterSpacing='s2' fontWeight='s2' color='default' withTruncatedText>{status} <Box mi='x8' withTruncatedText>{name}</Box></Box>
+		<Box display='flex' flexGrow={1} alignItems='center' is='p' fontSize='s2' fontSize='s2' fontScale='s2' letterSpacing='s2' fontWeight='s2' color='default' withTruncatedText>{status} <Box mis='x8' flexGrow={1} withTruncatedText>{name}</Box></Box>
 		<Info>{customStatus}</Info>
-		<Roles roles={roles}/>
+		<Roles>{roles}</Roles>
 		<Info>{localTime}</Info>
 		<Info withTruncatedText={false} style={style} height='60px'>{bio}</Info>
 		<a href='#'>See Full Profile</a>
 	</Box>
 	<Box><ActionButton icon='cross'/></Box>
-</Box>;
+</UserCardConteiner>;
 
 
 export default UserCard;
 
 UserCard.Action = Action;
+UserCard.Role = Role;
--- client/components/UserCard.stories.js
+++ client/components/UserCard.stories.js
@@ -10,7 +10,7 @@ export default {
 const user = {
 	name: 'guilherme.gazzo',
 	customStatus: '🛴 currently working on User Card',
-	roles: ['Admin', 'Rocket.Chat', 'Team'],
+	roles: [<UserCard.Role>Admin</UserCard.Role>, <UserCard.Role>Rocket.Chat</UserCard.Role>, <UserCard.Role>Team</UserCard.Role>],
 	bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus, eros convallis vulputate cursus, nisi neque eleifend libero, eget lacinia justo purus nec est. In at sodales ipsum. Sed lacinia quis purus eget pulvinar. Aenean eu pretium nunc, at aliquam magna. Praesent dignissim, tortor sed volutpat mattis, mauris diam pulvinar leo, porta commodo risus est non purus. Mauris in justo vel lorem ullamcorper hendrerit. Nam est metus, viverra a pellentesque vitae, ornare eget odio. Morbi tempor feugiat mattis. Morbi non felis tempor, aliquam justo sed, sagittis nibh. Mauris consequat ex metus. Praesent sodales sit amet nibh a vulputate. Integer commodo, mi vel bibendum sollicitudin, urna lectus accumsan ante, eget faucibus augue ex id neque. Aenean consectetur, orci a pellentesque mattis, tortor tellus fringilla elit, non ullamcorper risus nunc feugiat risus. Fusce sit amet nisi dapibus turpis commodo placerat. In tortor ante, vehicula sit amet augue et, imperdiet porta sem.',
 	actions: [<UserCard.Action icon='message'/>, <UserCard.Action icon='phone'/>],
 	localTime: 'Local Time: 7:44 AM',
@@ -26,35 +26,35 @@ const largeName = {
 
 const noRoles = {
 	...user,
-	roles: undefined,
+	roles: null,
 };
 
 const noActions = {
 	...user,
-	actions: undefined,
+	actions: null,
 };
 
 const noLocalTime = {
 	...user,
-	localTime: undefined,
+	localTime: null,
 };
 
 const noBio = {
 	...user,
-	bio: undefined,
+	bio: null,
 };
 
 const noBioNoLocalTime = {
 	...user,
-	bio: undefined,
-	localTime: undefined,
+	bio: null,
+	localTime: null,
 };
 
 const noBioNoLocalTimeNoRoles = {
 	...user,
-	bio: undefined,
-	localTime: undefined,
-	roles: undefined,
+	bio: null,
+	localTime: null,
+	roles: null,
 };
 
 
@@ -67,3 +67,5 @@ export const NoLocalTime = () => <UserCard {...noLocalTime} />;
 export const NoBio = () => <UserCard {...noBio} />;
 export const NoBioAndNoLocalTime = () => <UserCard {...noBioNoLocalTime} />;
 export const NoBioNoLocalTimeNoRoles = () => <UserCard {...noBioNoLocalTimeNoRoles} />;
+
+export const Loading = () => <UserCard />;
