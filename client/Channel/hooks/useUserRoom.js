3ab82ca46477946bd9cb5ecceb76b4391fedfd9e
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-07-10 23:31:54 -0300

    Eita

--- client/Channel/hooks/useUserRoom.js
+++ client/Channel/hooks/useUserRoom.js
@@ -1,4 +1,6 @@
+import { useCallback } from 'react';
+
 import { useReactiveValue } from '../../hooks/useReactiveValue';
 import { Rooms } from '../../../app/models/client';
 
-export const useUserRoom = (rid, fields) => useReactiveValue(() => Rooms.findOne({ _id: rid }, { fields }), [rid, fields]);
+export const useUserRoom = (rid, fields) => useReactiveValue(useCallback(() => Rooms.findOne({ _id: rid }, { fields }), [rid, fields]));
--- client/Channel/hooks/useUserSubscription.js
+++ client/Channel/hooks/useUserSubscription.js
@@ -1,4 +1,6 @@
+import { useCallback } from 'react';
+
 import { useReactiveValue } from '../../hooks/useReactiveValue';
 import { Subscriptions } from '../../../app/models/client';
 
-export const useUserSubscription = (rid, fields) => useReactiveValue(() => Subscriptions.findOne({ rid }, { fields }), [rid, fields]);
+export const useUserSubscription = (rid, fields) => useReactiveValue(useCallback(() => Subscriptions.findOne({ rid }, { fields }), [rid, fields]));
