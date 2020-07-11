452931a0dd28cb7de2e6f5f0c283f4b92b3492b3
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-11 00:48:21 -0300

    Rewrite Contextual Bar Discussion List in React (#18127)

--- app/threads/client/components/hooks/useUserSubscription.js
+++
@@ -1,7 +0,0 @@
-import { useCallback } from 'react';
-
-import { useReactiveValue } from '../../../../../client/hooks/useReactiveValue';
-import { Subscriptions } from '../../../../models/client';
-
-export const useUserSubscription = (rid, fields) =>
-	useReactiveValue(useCallback(() => Subscriptions.findOne({ rid }, { fields }), [rid, fields]));
