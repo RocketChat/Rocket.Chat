6320ce06459677665300fd39b9f67689792f407f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-10 23:08:45 -0300

    Update the API of React Hooks using Meteor's reactive system (#18226)

--- app/threads/client/components/hooks/useUserSubscription.js
+++ app/threads/client/components/hooks/useUserSubscription.js
@@ -1,4 +1,7 @@
+import { useCallback } from 'react';
+
 import { useReactiveValue } from '../../../../../client/hooks/useReactiveValue';
 import { Subscriptions } from '../../../../models/client';
 
-export const useUserSubscription = (rid, fields) => useReactiveValue(() => Subscriptions.findOne({ rid }, { fields }), [rid, fields]);
+export const useUserSubscription = (rid, fields) =>
+	useReactiveValue(useCallback(() => Subscriptions.findOne({ rid }, { fields }), [rid, fields]));
