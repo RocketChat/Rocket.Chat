96c90416a01ae11fd4c4b12c283be8c438435627
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-07-14 12:39:11 -0300

    Fix Ghost menu

--- client/channel/UserCard/index.js
+++ client/channel/UserCard/index.js
@@ -63,7 +63,7 @@ const UserCardWithData = ({ username, onClose, target, open, rid }) => {
 
 	const { actions: actionsDefinition, menu: menuOptions } = useUserInfoActionsSpread(useUserInfoActions(user, rid));
 
-	const menu = menuOptions && <Menu flexShrink={0} key='menu' renderItem={({ label: { label, icon }, ...props }) => <Option {...props} label={label} icon={icon} />} options={menuOptions}/>;
+	const menu = menuOptions && <Menu flexShrink={0} mi='x2' key='menu' ghost={false} renderItem={({ label: { label, icon }, ...props }) => <Option {...props} label={label} icon={icon} />} options={menuOptions}/>;
 
 	const actions = useMemo(() => [...actionsDefinition.map(([key, { label, icon, action }]) => <UserCard.Action key={key} aria-label={label} onClick={action} icon={icon}/>), menu].filter(Boolean), [actionsDefinition, menu]);
 
