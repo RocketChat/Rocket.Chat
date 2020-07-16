4dbef830f4394dfb8aad583a0d9138db194d5179
Martin Schoeler <martin.schoeler@rocket.chat>
2020-07-16 11:28:05 -0300

    fix some reviews

--- app/ui-utils/client/lib/reactionListContent.js
+++ app/ui-utils/client/lib/reactionListContent.js
@@ -1,50 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Box, Tag, Divider } from '@rocket.chat/fuselage';
 
 import { useTranslation } from '../../../../client/contexts/TranslationContext';
 import Emoji from '../../../../client/components/basic/Emoji';
 
 export function Reactions(props) {
-	const styleDivider = {
-		marginTop: '8px',
-		marginBottom: '16px',
-	};
-
 	const reactionKeys = useMemo(() => Object.keys(props.reactions), [props.reactions]);
 
 	return <Box>
-		{
-			reactionKeys.map((reaction, i) => <Box key={reaction}><div style={{ display: 'flex' }}><Emoji emojiHandle={reaction}/> {reaction}</div>
-				<Usernames usernames={props.reactions[reaction].usernames}></Usernames> { i !== (reactionKeys.length - 1) && <Divider key={reaction} style={styleDivider}/> } </Box>)
-		}
+		{reactionKeys.map((reaction, i) => <Box key={reaction}>
+			<Box display='flex'>
+				<Emoji emojiHandle={reaction}/> {reaction}
+			</Box>
+			<Usernames usernames={props.reactions[reaction].usernames}></Usernames>
+			{ i !== (reactionKeys.length - 1) && <Divider key={reaction} marginBlockStart='x8' marginBlockEnd='x16'/> }
+		</Box>)}
 	</Box>;
 }
 
 export function Usernames(props) {
-	const styleBox = {
-		paddingTop: '8px',
-	};
-
-	const styleTag = {
-		marginRight: '4px',
-	};
-
-	return <Box style={styleBox}>
-		{ props.usernames.map((user) => <Tag style={styleTag} key={user}>{user}</Tag>)}
+	return <Box paddingBlockStart='8x'>
+		{ props.usernames.map((user) => <Tag marginInlineEnd='4x' key={user}>{user}</Tag>)}
 	</Box>;
 }
 
 export default function ReactionListContent(props) {
 	const t = useTranslation();
 
-	const style = {
-		overflow: 'auto',
-		maxHeight: '450px',
-	};
-
 	const { reactions } = props.data;
 
-	return <Box style={style}>
+	return <Box overflow='auto' maxHeight='480x'>
 		<Box is='h2' fontScale='h1' mb='x8'>{t('Users_reacted')}</Box>
 		<Reactions reactions={reactions} />
 	</Box>;
