65bf475cd1039a1069b9ebb24fe4956282c6fc4a
Diego Sampaio <chinello@gmail.com>
2020-07-20 20:53:39 -0300

    Run migration in parallel batches

--- server/startup/migrations/v199.js
+++ server/startup/migrations/v199.js
@@ -1,35 +1,73 @@
+import { Meteor } from 'meteor/meteor';
+import Future from 'fibers/future';
+
 import { Users, Subscriptions } from '../../../app/models/server/raw';
 import { Migrations } from '../../../app/migrations/server';
 
-Migrations.add({
-	version: 199,
-	up: () => {
-		Promise.await((async () => {
-			const usersCursor = Users.find({}, { fields: { _id: 1 } });
-			const total = await usersCursor.count();
-			let i = 0;
+const batchSize = 5000;
 
-			console.log('Migrating ', total, 'records...');
+async function migrateUserRecords(total, current) {
+	console.log(`Migrating ${ current }/${ total }`);
 
-			for await (const user of usersCursor) {
-				const rooms = await Subscriptions.find({
-					'u._id': user._id,
-					t: { $nin: ['d', 'l'] },
-				}, { rid: 1 }).toArray();
+	const items = await Users.find({ __rooms: { $exists: false } }, { projection: { _id: 1 } }).limit(batchSize).toArray();
 
-				await Users.update({
-					_id: user._id,
-				}, {
+	const actions = [];
+
+	for await (const user of items) {
+		const rooms = await Subscriptions.find({
+			'u._id': user._id,
+			t: { $nin: ['d', 'l'] },
+		}, { projection: { rid: 1 } }).toArray();
+
+		actions.push({
+			updateOne: {
+				filter: { _id: user._id },
+				update: {
 					$set: {
 						__rooms: rooms.map(({ rid }) => rid),
 					},
-				});
+				},
+			},
+		});
+	}
 
-				i++;
-				if (i % 100 === 0 || i === total) {
-					console.log(i, 'of', total, 'records migrated');
-				}
+	if (actions.length === 0) {
+		return;
+	}
+
+	const batch = await Users.col.bulkWrite(actions, { ordered: false });
+	if (actions.length === batchSize) {
+		await batch;
+		return migrateUserRecords(total, current + batchSize);
+	}
+
+	return batch;
+}
+
+Migrations.add({
+	version: 199,
+	up() {
+		const fut = new Future();
+
+		console.log('Changing schema of User records, this may take a long time ...');
+
+		Meteor.setTimeout(async () => {
+			const users = Users.find({ __rooms: { $exists: false } }, { projection: { _id: 1 } });
+			const total = await users.count();
+			await users.close();
+
+			if (total < batchSize * 10) {
+				await migrateUserRecords(total, 0);
+				return fut.return();
 			}
-		})());
+
+			await migrateUserRecords(total, 0);
+
+			fut.return();
+		}, 250);
+
+		fut.wait();
+
+		console.log('Changing schema of User records finished.');
 	},
 });
