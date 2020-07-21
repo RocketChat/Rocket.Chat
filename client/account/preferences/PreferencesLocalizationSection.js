35ef47477b6a3ec72fe4fa18c220084d30eb61a2
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2020-07-21 19:52:05 -0300

    Regression: Preferences crashing when User has no preferences set. (#18341)

--- client/account/preferences/PreferencesLocalizationSection.js
+++ client/account/preferences/PreferencesLocalizationSection.js
@@ -7,7 +7,7 @@ import { useForm } from '../../hooks/useForm';
 
 const PreferencesLocalizationSection = ({ onChange, ...props }) => {
 	const t = useTranslation();
-	const userLanguage = useUserPreference('language');
+	const userLanguage = useUserPreference('language') || '';
 	const languages = useLanguages();
 
 	const languageOptions = useMemo(() => languages.map(({ key, name }) => [key, name]).sort(([a], [b]) => a - b), [languages]);
