7f86529e3930f437a8120fb9fff7520600616a89
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-07-21 18:35:37 -0300

    Apply suggestions from code review

--- client/account/preferences/PreferencesGlobalSection.js
+++ client/account/preferences/PreferencesGlobalSection.js
@@ -8,9 +8,9 @@ import { useForm } from '../../hooks/useForm';
 const PreferencesGlobalSection = ({ onChange, ...props }) => {
 	const t = useTranslation();
 
-	const userDontAskAgainList = useUserPreference('dontAskAgainList') || [];
+	const userDontAskAgainList = useUserPreference('dontAskAgainList');
 
-	const options = useMemo(() => userDontAskAgainList.map(({ action, label }) => [action, label]), [userDontAskAgainList]);
+	const options = useMemo(() => (userDontAskAgainList || []).map(({ action, label }) => [action, label]), [userDontAskAgainList]);
 
 	const selectedOptions = options.map(([action]) => action);
 
