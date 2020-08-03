6af9f6041f3ba612b42f6d8b9e57565dd38c5757
Diego Sampaio <chinello@gmail.com>
2020-08-03 15:00:56 -0300

    Fix discussion's avatar

--- server/routes/avatar/room.js
+++ server/routes/avatar/room.js
@@ -11,19 +11,32 @@ import { Rooms, Avatars } from '../../../app/models/server';
 import { roomTypes } from '../../../app/utils';
 
 
-const getRoom = (roomId) => {
+const getRoomAvatar = (roomId) => {
 	const room = Rooms.findOneById(roomId, { fields: { t: 1, prid: 1, name: 1, fname: 1 } });
-
-	// if it is a discussion, returns the parent room
-	if (room && room.prid) {
-		return Rooms.findOneById(room.prid, { fields: { t: 1, name: 1, fname: 1 } });
+	if (!room) {
+		return {};
 	}
-	return room;
+
+	const file = Avatars.findOneByRoomId(room._id);
+
+	// if it is a discussion that doesn't have it's own avatar, returns the parent's room avatar
+	if (room.prid && !file) {
+		return getRoomAvatar(room.prid);
+	}
+
+	return { room, file };
 };
 
 export const roomAvatar = Meteor.bindEnvironment(function(req, res/* , next*/) {
 	const roomId = decodeURIComponent(req.url.substr(1).replace(/\?.*$/, ''));
-	const file = Avatars.findOneByRoomId(roomId);
+
+	const { room, file } = getRoomAvatar(roomId);
+	if (!room) {
+		res.writeHead(404);
+		res.end();
+		return;
+	}
+
 	const reqModifiedHeader = req.headers['if-modified-since'];
 	if (file) {
 		res.setHeader('Content-Security-Policy', 'default-src \'none\'');
@@ -40,15 +53,6 @@ export const roomAvatar = Meteor.bindEnvironment(function(req, res/* , next*/) {
 		res.setHeader('Content-Length', file.size);
 
 		return FileUpload.get(file, req, res);
-	}
-
-
-	const room = getRoom(roomId);
-
-	if (!room) {
-		res.writeHead(404);
-		res.end();
-		return;
 	}
 
 	const roomName = roomTypes.getConfig(room.t).roomName(room);
