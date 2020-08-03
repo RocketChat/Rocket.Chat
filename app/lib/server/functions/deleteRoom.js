1db35da95c820e0b02b35784c4d5b054e6b1983e
Diego Sampaio <chinello@gmail.com>
2020-08-03 18:03:40 -0300

    Fix deleting avatar when deleting a room

--- app/lib/server/functions/deleteRoom.js
+++ app/lib/server/functions/deleteRoom.js
@@ -7,7 +7,7 @@ export const deleteRoom = function(rid) {
 	Messages.removeByRoomId(rid);
 	callbacks.run('beforeDeleteRoom', rid);
 	Subscriptions.removeByRoomId(rid);
-	FileUpload.getStore('Avatars').deleteById(rid);
+	FileUpload.getStore('Avatars').deleteByRoomId(rid);
 	callbacks.run('afterDeleteRoom', rid);
 	return Rooms.removeById(rid);
 };
