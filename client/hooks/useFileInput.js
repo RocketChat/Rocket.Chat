015efd7544766ddbf1b7ce0c072fe4773709555c
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-08-04 22:45:03 -0300

    Fix useFileInput filetype

--- client/hooks/useFileInput.js
+++ client/hooks/useFileInput.js
@@ -1,13 +1,14 @@
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { useRef, useEffect } from 'react';
 
-export const useFileInput = (onSetFile, fileType = 'image') => {
+export const useFileInput = (onSetFile, fileType = 'image/*') => {
 	const ref = useRef();
 
 	useEffect(() => {
 		const fileInput = document.createElement('input');
 		const formData = new FormData();
 		fileInput.setAttribute('type', 'file');
+		fileInput.setAttribute('accept', fileType);
 		fileInput.setAttribute('style', 'display: none');
 		document.body.appendChild(fileInput);
 
