ab98f03aaeac57ed798f4f1194d9caefadb4bfaf
Martin <martin.schoeler@rocket.chat>
2020-08-06 18:28:06 -0300

    remove old files

--- app/livechat/client/route.js
+++ app/livechat/client/route.js
@@ -43,22 +43,6 @@ AccountBox.addRoute({
 }, livechatManagerRoutes, load);
 
 AccountBox.addRoute({
-	name: 'livechat-managers',
-	path: '/managers',
-	sideNav: 'livechatFlex',
-	i18nPageTitle: 'Livechat_Managers',
-	pageTemplate: 'livechatManagers',
-}, livechatManagerRoutes, load);
-
-AccountBox.addRoute({
-	name: 'livechat-agents',
-	path: '/agents',
-	sideNav: 'livechatFlex',
-	pageTemplate: 'livechatAgents',
-	customContainer: true,
-}, livechatManagerRoutes, load);
-
-AccountBox.addRoute({
 	name: 'livechat-departments',
 	path: '/departments',
 	sideNav: 'livechatFlex',
