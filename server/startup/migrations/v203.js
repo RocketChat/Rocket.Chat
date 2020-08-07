dd3cbab30887ef4599da2c39eb77794f492d57f1
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-08-07 11:04:48 -0300

    Add Migration

---
+++ server/startup/migrations/v203.js
@@ -0,0 +1,13 @@
+import { Migrations } from '../../../app/migrations/server';
+import { Avatars } from '../../../app/models/server';
+
+Migrations.add({
+	version: 203,
+	up() {
+		Avatars.tryDropIndex({ name: 1 });
+		Avatars.tryEnsureIndex({ name: 1 }, { sparse: true });
+
+		Avatars.tryDropIndex({ rid: 1 });
+		Avatars.tryEnsureIndex({ rid: 1 }, { sparse: true });
+	},
+});
