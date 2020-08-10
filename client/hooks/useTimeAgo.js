2df90d672d8c6cf54047b0aaa5f24e3985419f43
Martin <admin@mshost.cz>
2020-08-10 15:30:53 +0100

    [FIX] Follow 12/24h clock preference in threads

--- client/hooks/useTimeAgo.js
+++ client/hooks/useTimeAgo.js
@@ -1,4 +1,22 @@
 import { useCallback } from 'react';
 import moment from 'moment';
 
-export const useTimeAgo = () => useCallback((time) => moment(time).calendar(null, { sameDay: 'LT', lastWeek: 'dddd LT', sameElse: 'LL' }), []);
+import { useUserPreference } from '../contexts/UserContext';
+import { useSetting } from '../contexts/SettingsContext';
+
+export const useTimeAgo = () => {
+	const clockMode = useUserPreference('clockMode', false);
+	const dateFormat = useSetting('Message_DateFormat');
+	const timeFormat = useSetting('Message_TimeFormat');
+
+	return useCallback((time) => {
+		switch (clockMode) {
+			case 1:
+				return moment(time).calendar(null, { sameDay: 'h:mm A', lastWeek: 'dddd h:mm A', sameElse: dateFormat }, []);
+			case 2:
+				return moment(time).calendar(null, { sameDay: 'HH:mm', lastWeek: 'dddd HH:mm', sameElse: dateFormat }, []);
+			default:
+				return moment(time).calendar(null, { sameDay: timeFormat, lastWeek: `dddd ${timeFormat}`, sameElse: dateFormat }, []);
+		}
+	}, [clockMode, dateFormat, timeFormat]);
+};
