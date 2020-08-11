0d0f628d32b6143b6fa5dc1571d9a3cdca341499
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-08-11 18:08:53 -0300

    BusinessHoursTimezone

---
+++ ee/client/omnichannel/additionalForms/BusinessHoursTimeZone.js
@@ -0,0 +1,35 @@
+import React, { useMemo, useState } from 'react';
+import { SelectFiltered, Field } from '@rocket.chat/fuselage';
+import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
+
+import { useTranslation } from '../../../../client/contexts/TranslationContext';
+import { useTimezoneNameList } from '../../../../client/hooks/useTimezoneNameList';
+
+const BusinessHoursTimeZone = ({ onChange, value, className }) => {
+	const t = useTranslation();
+
+	const [timezone, setTimezone] = useState(value);
+
+	const timeZones = useTimezoneNameList();
+
+	const timeZonesOptions = useMemo(() => timeZones.map((name) => [
+		name,
+		t(name),
+	]), [t, timeZones]);
+
+	const handleChange = useMutableCallback((value) => {
+		setTimezone(value);
+		onChange(value);
+	});
+
+	return <Field className={className}>
+		<Field.Label>
+			{t('Timezone')}
+		</Field.Label>
+		<Field.Row>
+			<SelectFiltered options={timeZonesOptions} value={timezone} onChange={handleChange}/>
+		</Field.Row>
+	</Field>;
+};
+
+export default BusinessHoursTimeZone;
--- ee/client/omnichannel/additionalForms/register.js
+++ ee/client/omnichannel/additionalForms/register.js
@@ -3,3 +3,4 @@ import { useMemo, lazy } from 'react';
 import { registerForm } from '../../../../client/omnichannel/additionalForms';
 
 registerForm({ useCustomFieldsAdditionalForm: () => useMemo(() => lazy(() => import('./CustomFieldsAdditionalForm')), []) });
+registerForm({ useBusinessHoursTimeZone: () => useMemo(() => lazy(() => import('./BusinessHoursTimeZone')), []) });
