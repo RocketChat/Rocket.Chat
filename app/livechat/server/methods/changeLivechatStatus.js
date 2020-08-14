9b46e5e245926b5827926894884cae50c78ddc94
Martin <martin.schoeler@rocket.chat>
2020-08-14 18:11:45 -0300

    Add agentId parameter to changeLivechatStatus method

--- app/livechat/server/methods/changeLivechatStatus.js
+++ app/livechat/server/methods/changeLivechatStatus.js
@@ -1,14 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Livechat } from '../lib/Livechat';
+import { hasPermission, hasRole } from '../../../authorization';
+import { Users } from '../../../models';
 
 Meteor.methods({
-	'livechat:changeLivechatStatus'() {
+	'livechat:changeLivechatStatus'(agentId) {
 		if (!Meteor.userId()) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'livechat:changeLivechatStatus' });
 		}
 
-		const user = Meteor.user();
+		let user = Meteor.user();
+
+		if (agentId && hasPermission(Meteor.userId(), 'manage-livechat-agents')) {
+			user = Users.findOneById(agentId);
+			if (!user || !hasRole(agentId, 'livechat-agent')) {
+				throw new Meteor.Error('error-user-is-not-agent', 'User is not a livechat agent', { method: 'livechat:saveAgentInfo' });
+			}
+		}
 
 		const newStatus = user.statusLivechat === 'available' ? 'not-available' : 'available';
 		if (!Livechat.allowAgentChangeServiceStatus(newStatus, user._id)) {
