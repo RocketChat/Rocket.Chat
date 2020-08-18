d1ed662c52f2eea24300f26983014c2d43517ebe
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-08-18 10:05:14 -0300

    Fix review

--- ee/client/omnichannel/additionalForms/MaxChatsPerAgentDisplay.js
+++ ee/client/omnichannel/additionalForms/MaxChatsPerAgentDisplay.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Box } from '@rocket.chat/fuselage';
-// import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 
 import { useTranslation } from '../../../../client/contexts/TranslationContext';
+import UserInfo from '../../../../client/components/basic/UserInfo';
 
 const MaxChatsPerAgentDisplay = ({ data: { livechat: { maxNumberSimultaneousChat = '' } = {} } = {} }) => {
 	const t = useTranslation();
 
-
-	return <Box>
-		<Box fontScale='p2' color='default'>{t('Max_number_of_chats_per_agent')}</Box>
-		<Box mb='x4' is='span' fontSize='p1' fontScale='p1' color='hint' withTruncatedText>	{maxNumberSimultaneousChat} </Box>;
-	</Box>;
+	return <>
+		<UserInfo.Label>{t('Max_number_of_chats_per_agent')}</UserInfo.Label>
+		<UserInfo.Info>{maxNumberSimultaneousChat}</UserInfo.Info>
+	</>;
 };
 
 export default MaxChatsPerAgentDisplay;
