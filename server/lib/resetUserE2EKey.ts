5d7dfc1cabab5ef3af4536850efffa13ae30ebf8
Pierre Lehnen <pierre.lehnen@rocket.chat>
2020-08-25 14:42:38 -0300

    [NEW] Send email notification when an admin resets someone else's E2E key.

--- server/lib/resetUserE2EKey.ts
+++ server/lib/resetUserE2EKey.ts
@@ -1,6 +1,63 @@
+import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
+
 import { Users, Subscriptions } from '../../app/models/server';
+import { settings } from '../../app/settings';
+import * as Mailer from '../../app/mailer';
+
+const sendResetNotitification = function(uid: string): void {
+	const user = Users.findOneById(uid);
+	if (!user) {
+		throw new Meteor.Error('invalid-user');
+	}
+
+	const language = user.language || settings.get('Language') || 'en';
+	const addresses = user.emails?.filter(({ verified }) => verified).map((e) => e.address);
+	if (!addresses?.length) {
+		return;
+	}
+
+	const t = (s: string): string => TAPi18n.__(s, { lng: language });
+	const text = `
+	${ t('Your_e2e_key_has_been_reset') }
+
+	${ t('E2E_Reset_Email_Content') }
+	`;
+	const html = `
+		<p>${ t('Your_e2e_key_has_been_reset') }</p>
+		<p>${ t('E2E_Reset_Email_Content') }</p>
+	`;
+
+	const from = settings.get('From_Email');
+	const subject = t('E2E_key_reset_email');
+
+	for (const address of addresses) {
+		Meteor.defer(() => {
+			try {
+				Mailer.send({
+					to: address,
+					from,
+					subject,
+					replyTo: undefined,
+					headers: undefined,
+					text,
+					html,
+				});
+			} catch (error) {
+				throw new Meteor.Error('error-email-send-failed', `Error trying to send email: ${ error.message }`, {
+					function: 'resetUserE2EEncriptionKey',
+					message: error.message,
+				});
+			}
+		});
+	}
+};
 
 export function resetUserE2EEncriptionKey(uid: string): boolean {
+	if (uid !== Meteor.userId()) {
+		sendResetNotitification(uid);
+	}
+
 	Users.resetE2EKey(uid);
 	Subscriptions.resetUserE2EKey(uid);
 
