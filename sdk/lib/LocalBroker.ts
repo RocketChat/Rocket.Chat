5c4760b87e520d4b6f8ff351027fe43240a56a23
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-09-04 16:20:31 -0300

    Remove delay from broker calls

--- sdk/lib/LocalBroker.ts
+++ sdk/lib/LocalBroker.ts
@@ -1,15 +1,11 @@
 import { IBroker } from '../types/IBroker';
 import { ServiceClass } from '../types/ServiceClass';
 
-const delay = (ms: number): Promise<number> => new Promise((resolve) => setTimeout(resolve, ms));
-
 export class LocalBroker implements IBroker {
 	private methods = new Map<string, Function>();
 
 	async call(method: string, data: any): Promise<any> {
-		await delay(500);
 		const result = this.methods.get(method)?.(...data);
-		await delay(500);
 		return result;
 	}
 
