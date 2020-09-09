6e984a8865df9ef23e7f8a9f79bd95fed5a09fc7
Pierre Lehnen <pierre.lehnen@rocket.chat>
2020-09-08 21:04:07 -0300

    [NEW] Room avatar changed system messages

--- app/channel-settings/client/startup/messageTypes.js
+++ app/channel-settings/client/startup/messageTypes.js
@@ -64,4 +64,15 @@ Meteor.startup(function() {
 			};
 		},
 	});
+
+	MessageTypes.registerType({
+		id: 'room_changed_avatar',
+		system: true,
+		message: 'room_changed_avatar',
+		data(message) {
+			return {
+				user_by: message.u && message.u.username,
+			};
+		},
+	});
 });
