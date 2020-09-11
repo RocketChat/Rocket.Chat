6276e0bf06060b3d2fca3514b59e4d1acbd5832c
Felipe Parreira <felipeparreirafb@gmail.com>
2020-09-11 15:56:20 -0300

    test: fix async to use done

--- tests/end-to-end/api/01-users.js
+++ tests/end-to-end/api/01-users.js
@@ -348,22 +348,22 @@ describe('[Users]', function() {
 				.expect((res) => {
 					expect(res.body).to.have.property('success', true);
 				})
-				.end(done);
-
-			request.get(api('users.info'))
-				.set(credentials)
-				.query({
-					username: 'ufs',
-				})
-				.expect(200)
-				.expect((res) => {
-					expect(res.body).to.have.property('success', true);
-					expect(res.body.user).to.have.property('type', 'user');
-					expect(res.body.user).to.have.property('name', 'testuser');
-					expect(res.body.user).to.have.property('username', 'ufs');
-					expect(res.body.user).to.have.property('active', true);
-				})
-				.end(done);
+				.then(() => {
+					request.get(api('users.info'))
+						.set(credentials)
+						.query({
+							username: 'ufs',
+						})
+						.expect(200)
+						.expect((res) => {
+							expect(res.body).to.have.property('success', true);
+							expect(res.body.user).to.have.property('type', 'user');
+							expect(res.body.user).to.have.property('name', 'testuser');
+							expect(res.body.user).to.have.property('username', 'ufs');
+							expect(res.body.user).to.have.property('active', true);
+						})
+						.end(done);
+				});
 		});
 	});
 	describe('[/users.getPresence]', () => {
