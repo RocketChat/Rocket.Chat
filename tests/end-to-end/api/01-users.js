2c4eeb03bec8e0909290db02a9cd2e7fd07ba0a6
Felipe Parreira <felipeparreirafb@gmail.com>
2020-09-11 14:46:46 -0300

    test(ufs-router): remove expect for header

--- tests/end-to-end/api/01-users.js
+++ tests/end-to-end/api/01-users.js
@@ -344,7 +344,6 @@ describe('[Users]', function() {
 					username: 'ufs',
 					password: '1234',
 				})
-				.expect('Content-Type', 'application/json')
 				.expect(200)
 				.expect((res) => {
 					expect(res.body).to.have.property('success', true);
@@ -355,7 +354,6 @@ describe('[Users]', function() {
 				.query({
 					username: 'ufs',
 				})
-				.expect('Content-Type', 'application/json')
 				.expect(200)
 				.expect((res) => {
 					expect(res.body).to.have.property('success', true);
