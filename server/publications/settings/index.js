73ace9f0d442be2d3bbc82421dbcc4a3b34b4ef6
Diego Sampaio <chinello@gmail.com>
2020-09-14 20:08:37 -0300

    Add settings emitter

--- server/publications/settings/index.js
+++ server/publications/settings/index.js
@@ -4,6 +4,7 @@ import { Settings } from '../../../app/models/server';
 import { hasPermission, hasAtLeastOnePermission } from '../../../app/authorization/server';
 import { getSettingPermissionId } from '../../../app/authorization/lib';
 import { SettingsEvents } from '../../../app/settings/server/functions/settings';
+import './emitter';
 
 Meteor.methods({
 	'public-settings/get'(updatedAt) {
