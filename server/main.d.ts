c2ebc78bf00448a78a6f41fa685385f03e3258e5
pierre-lehnen-rc <55164754+pierre-lehnen-rc@users.noreply.github.com>
2020-09-16 15:22:06 -0300

    [FIX] "Save to WebDav" not working (#18883)

--- server/main.d.ts
+++ server/main.d.ts
@@ -28,7 +28,7 @@ declare module 'meteor/meteor' {
 		interface Error extends globalError {
 			error: string | number;
 			reason?: string;
-			details?: string | undefined;
+			details?: string | undefined | Record<string, string>;
 		}
 
 		const server: any;
