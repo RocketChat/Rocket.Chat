8a9ccdd5102282670060a7f3df5974d3bbf2b4b1
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2020-09-16 23:23:40 -0300

    Read from secondary not working when passing empty array of tags

--- server/database/readSecondaryPreferred.ts
+++ server/database/readSecondaryPreferred.ts
@@ -1,12 +1,19 @@
 import { ReadPreference, Db } from 'mongodb';
 
-export function readSecondaryPreferred(db: Db, tags: any[] = []): ReadPreference {
+export function readSecondaryPreferred(db: Db, tags: any[] = []): ReadPreference | string {
 	const { readPreferenceTags, readPreference } = db.options as any;
 
 	if (tags.length === 0 && Array.isArray(readPreferenceTags) && readPreferenceTags.length) {
 		tags = readPreferenceTags;
 	} else if (tags.length === 0 && readPreference instanceof ReadPreference) {
 		tags = readPreference.tags;
+	}
+
+	// For some reason the new ReadPreference definition requires the tags parameter even not been
+	// required by the code implementation and, for some reason, when passing an empty array it
+	// doesn't ignore the tags and stop using the secondaries.
+	if (!tags.length) {
+		return ReadPreference.SECONDARY_PREFERRED;
 	}
 
 	return new ReadPreference(ReadPreference.SECONDARY_PREFERRED, tags);
