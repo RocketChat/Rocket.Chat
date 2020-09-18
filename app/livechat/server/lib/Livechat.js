d30a05988b12746294c7b981a45265451446230a
Douglas Gubert <douglas.gubert@gmail.com>
2020-09-18 19:27:18 -0300

    Update trigger calls

--- app/livechat/server/lib/Livechat.js
+++ app/livechat/server/lib/Livechat.js
@@ -312,7 +312,7 @@ export const Livechat = {
 		const ret = LivechatVisitors.saveGuestById(_id, updateData);
 
 		Meteor.defer(() => {
-			Apps.getBridges().getListenerBridge().livechatEvent(AppEvents.IPostLivechatGuestSaved, _id);
+			Apps.triggerEvent(AppEvents.IPostLivechatGuestSaved, _id);
 			callbacks.run('livechat.saveGuest', updateData);
 		});
 
@@ -496,7 +496,7 @@ export const Livechat = {
 		}
 
 		Meteor.defer(() => {
-			Apps.getBridges().getListenerBridge().livechatEvent(AppEvents.IPostLivechatRoomSaved, roomData._id);
+			Apps.triggerEvent(AppEvents.IPostLivechatRoomSaved, roomData._id);
 			callbacks.run('livechat.saveRoom', roomData);
 		});
 
