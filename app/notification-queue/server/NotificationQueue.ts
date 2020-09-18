a812caef50b88dbb45e09d156bb1a033f22bd1f0
Pierre Lehnen <pierre.lehnen@rocket.chat>
2020-09-17 23:06:26 -0300

    Removed unused import

--- app/notification-queue/server/NotificationQueue.ts
+++ app/notification-queue/server/NotificationQueue.ts
@@ -5,7 +5,6 @@ import { NotificationQueue, Users } from '../../models/server/raw';
 import { sendEmailFromData } from '../../lib/server/functions/notifications/email';
 import { PushNotification } from '../../push-notifications/server';
 import { IUser } from '../../../definition/IUser';
-import { settings } from '../../settings/server';
 
 const {
 	NOTIFICATIONS_WORKER_TIMEOUT = 2000,
