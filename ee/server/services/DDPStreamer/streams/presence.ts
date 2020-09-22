c9b0507778f549d7108ffa50d96252365cbf3553
Diego Sampaio <chinello@gmail.com>
2020-09-22 17:02:17 -0300

    Add TODOs

--- ee/server/services/DDPStreamer/streams/presence.ts
+++ ee/server/services/DDPStreamer/streams/presence.ts
@@ -3,4 +3,5 @@ import { STREAM_NAMES } from '../constants';
 
 export const userpresence = new Stream(STREAM_NAMES.PRESENCE);
 
+// TODO remove, not used
 userpresence.allowRead('all');
--- ee/server/services/DDPStreamer/streams/room.ts
+++ ee/server/services/DDPStreamer/streams/room.ts
@@ -2,5 +2,8 @@ import { Stream } from '../Streamer';
 import { STREAM_NAMES } from '../constants';
 
 export const streamRoom = new Stream(STREAM_NAMES.NOTIFY_ROOM);
-streamRoom.allowWrite('all');
+
+streamRoom.allowWrite('none');
+
+// TODO canAccessRoom?
 streamRoom.allowRead('all');
