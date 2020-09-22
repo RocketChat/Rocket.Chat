33860deb4f19bb89b2fa4d0f252f50af41f38379
Shiqi Mei <shiqi.mei@rocket.chat>
2020-09-22 11:00:23 +0800

    [NEW][Apps] Add a new upload API (#18955)
    Co-authored-by: Douglas Gubert <douglas.gubert@gmail.com>

--- app/apps/server/bridges/uploads.js
+++ app/apps/server/bridges/uploads.js
@@ -1,4 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+
 import { FileUpload } from '../../../file-upload/server';
+import { determineFileType } from '../../lib/misc/determineFileType';
 
 export class AppUploadBridge {
 	constructor(orch) {
@@ -25,5 +28,22 @@ export class AppUploadBridge {
 				resolve(result);
 			});
 		});
+	}
+
+	createUpload(details, buffer, appId) {
+		this.orch.debugLog(`The App ${ appId } is creating an upload "${ details.name }"`);
+
+		details.type = determineFileType(buffer, details);
+
+		const fileStore = FileUpload.getStore('Uploads');
+		const uploadedFile = fileStore.insertSync(details, buffer);
+
+		if (details.userId) {
+			Meteor.runAsUser(details.userId, () => {
+				Meteor.call('sendFileMessage', details.rid, null, uploadedFile);
+			});
+		}
+
+		return this.orch.getConverters().get('uploads').convertToApp(uploadedFile);
 	}
 }
