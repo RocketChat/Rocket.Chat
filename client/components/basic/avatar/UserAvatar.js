5b33235b07799eddbd77b5c4889b44453dbc3dee
Martin <martin.schoeler@rocket.chat>
2020-09-28 15:16:23 -0300

    add new setting for room avatar providers

--- client/components/basic/avatar/UserAvatar.js
+++ client/components/basic/avatar/UserAvatar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import BaseAvatar from './BaseAvatar';
-import { getUserAvatarURL as getAvatarUrl } from '../../../../app/utils/lib/getUserAvatarURL';
+import { settings } from '../../../../app/settings';
 
 function UserAvatar({ url, username, etag, ...props }) {
 	// NOW, `username` and `etag` props are enough to determine the whole state of
 	// this component, but it must be as performatic as possible as it will be
 	// rendered many times; and some of the state can be derived at the ancestors.
 	// Ideally, it should be a purely visual component.
-	const avatarUrl = getAvatarUrl(username) || url || `/avatar/${ username }${ etag ? `?etag=${ etag }` : '' }`;
+
+	let externalSource = (settings.get('Accounts_AvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
+	externalSource = externalSource !== '' && externalSource.replace('{username}', username);
+
+	const avatarUrl = externalSource || url || `/avatar/${ username }${ etag ? `?etag=${ etag }` : '' }`;
 	return <BaseAvatar url={avatarUrl} title={username} {...props}/>;
 }
 
