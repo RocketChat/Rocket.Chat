fa728b26e38b70cf0a1c7ae764fd8600586ffa0d
Diego Sampaio <chinello@gmail.com>
2020-09-29 18:03:04 -0300

    Add missing typescript module for async_hooks

--- server/main.d.ts
+++ server/main.d.ts
@@ -107,3 +107,21 @@ declare module 'meteor/mongo' {
 		function defaultRemoteCollectionDriver(): RemoteCollectionDriver;
 	}
 }
+
+declare module 'async_hooks' {
+	export class AsyncLocalStorage<T> {
+		disable(): void;
+
+		getStore(): T | undefined;
+
+		run(store: T, callback: (...args: any[]) => void, ...args: any[]): void;
+
+		exit(callback: (...args: any[]) => void, ...args: any[]): void;
+
+		runSyncAndReturn<R>(store: T, callback: (...args: any[]) => R, ...args: any[]): R;
+
+		exitSyncAndReturn<R>(callback: (...args: any[]) => R, ...args: any[]): R;
+
+		enterWith(store: T): void;
+	}
+}
