b1284d7a2e223f2595e0f6b2ec18135d9b66ca3a
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-10-06 00:25:15 -0300

    Presence

--- client/components/sidebar/Chats.js
+++ client/components/sidebar/Chats.js
@@ -81,11 +81,11 @@ const SidebarIcon = ({ room }) => {
 		case 'l':
 			return <Sidebar.Item.Icon name='headset' color={colors[room.v.status]}/>;
 		case 'd':
-			if (room.uids && room.uids.length === 1) {
-				return room.uids && room.uids.length && <Sidebar.Item.Icon><ReactiveUserStatus uid={room.uids[0]} /></Sidebar.Item.Icon>;
-			}
-			if (room.uids && room.uids.length > 1) {
+			if (room.uids && room.uids.length > 2) {
 				return <Sidebar.Item.Icon name='team'/>;
+			}
+			if (room.uids && room.uids.length > 0) {
+				return room.uids && room.uids.length && <Sidebar.Item.Icon><ReactiveUserStatus uid={room.uids[0]} /></Sidebar.Item.Icon>;
 			}
 			return <Sidebar.Item.Icon name={roomTypes.getIcon(room)}/>;
 		default:
