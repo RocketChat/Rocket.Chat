bf8281f462489fc3d572611093e83ac05cf776b8
Diego Sampaio <chinello@gmail.com>
2020-10-08 14:56:43 -0300

    Move mongodb typings augmentation to a different file

--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/interface-name-prefix */
-import 'mongodb';
-
 declare module 'meteor/rocketchat:tap-i18n';
 declare module 'meteor/littledata:synced-cron';
 declare module 'meteor/promise';
@@ -8,9 +5,3 @@ declare module 'meteor/ddp-common';
 declare module 'meteor/routepolicy';
 declare module 'xml-encryption';
 declare module 'webdav';
-
-declare module 'mongodb' {
-	export interface FindOneOptions<T> {
-		awaitData?: boolean;
-	}
-}
