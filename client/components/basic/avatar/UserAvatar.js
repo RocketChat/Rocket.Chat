3eb1f589065cfb82444fd5cd8187b8309b518a9f
Martin <martin.schoeler@rocket.chat>
2020-10-13 16:05:56 -0300

    useSetting

--- client/components/basic/avatar/UserAvatar.js
+++ client/components/basic/avatar/UserAvatar.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import BaseAvatar from './BaseAvatar';
 import { settings } from '../../../../app/settings';
+import { useSetting } from '../../../contexts/SettingsContext';
 
 function UserAvatar({ url, username, etag, ...props }) {
 	// NOW, `username` and `etag` props are enough to determine the whole state of
 	// this component, but it must be as performatic as possible as it will be
 	// rendered many times; and some of the state can be derived at the ancestors.
 	// Ideally, it should be a purely visual component.
+	const externalProviderUrl = useSetting('Accounts_AvatarExternalProviderUrl');
 
-	let externalSource = (settings.get('Accounts_AvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
+	let externalSource = (externalProviderUrl || '').trim().replace(/\/$/, '');
 	externalSource = externalSource !== '' && externalSource.replace('{username}', username);
 
 	const avatarUrl = externalSource || url || `/avatar/${ username }${ etag ? `?etag=${ etag }` : '' }`;
