69691542c3d7d8c0cc036ef59ceedf498b4d1063
Pierre Lehnen <pierre.lehnen@rocket.chat>
2020-10-19 19:14:33 -0300

    Use distincted user list on migration

--- server/startup/migrations/v208.js
+++ server/startup/migrations/v208.js
@@ -1,24 +1,37 @@
+import Future from 'fibers/future';
+
 import { Migrations } from '../../../app/migrations';
-import { Users, Sessions } from '../../../app/models/server';
+import { Users, Sessions } from '../../../app/models/server/raw';
+
+async function migrateSessions(fut) {
+	const userIds = await Sessions.col.distinct('userId', { roles: { $exists: false } });
+	const items = await Users.find({ _id: { $in: userIds } }, { fields: { roles: 1 } });
+	if (!items) {
+		return;
+	}
+	const users = await items.toArray();
+
+	for (const { _id, roles } of users) {
+		Sessions.update({
+			userId: _id,
+		}, {
+			$set: {
+				roles,
+			},
+		}, {
+			multi: true,
+		});
+	}
+
+	fut.return();
+}
+
 
 Migrations.add({
 	version: 208,
 	up() {
-		const users = Users.find({}, {
-			fields: {
-				roles: 1,
-			},
-		});
-		users.forEach(({ _id, roles }) => {
-			Sessions.update({
-				userId: _id,
-			}, {
-				$set: {
-					roles,
-				},
-			}, {
-				multi: true,
-			});
-		});
+		const fut = new Future();
+		migrateSessions(fut);
+		fut.wait();
 	},
 });
