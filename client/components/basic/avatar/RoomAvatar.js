678226ab89fbe7feb08c7c1a81ce58fabcc47803
dougfabris <deefabris@gmail.com>
2020-10-21 11:41:24 -0300

    Fix type and url verification

--- client/components/basic/avatar/RoomAvatar.js
+++ client/components/basic/avatar/RoomAvatar.js
@@ -4,8 +4,8 @@ import { roomTypes } from '../../../../app/utils/client';
 import BaseAvatar from './BaseAvatar';
 
 function RoomAvatar({ room: { type, ...room }, ...rest }) {
-	const { url = roomTypes.getConfig(type).getAvatarPath({ username: room._id, ...room }), ...props } = rest;
-	return <BaseAvatar url={url} {...props}/>;
+	const { url = type && roomTypes.getConfig(type).getAvatarPath({ username: room._id, ...room }), ...props } = rest;
+	return <BaseAvatar url={url || ''} {...props}/>;
 }
 
 export default RoomAvatar;
