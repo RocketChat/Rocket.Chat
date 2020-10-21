39633c52f426faa459e94835633f00a2a54c564a
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-10-21 19:23:59 -0300

    CDN and external Providers

--- client/providers/AvatarUrlProvider.tsx
+++ client/providers/AvatarUrlProvider.tsx
@@ -5,16 +5,22 @@ import { AvatarUrlContext } from '../contexts/AvatarUrlContext';
 import { roomTypes } from '../../app/utils/client';
 
 const AvatarUrlProvider: FC = ({ children }) => {
+	const cdnAvatarUrl = String(useSetting('CDN_PREFIX') || '');
 	const externalProviderUrl = String(useSetting('Accounts_AvatarExternalProviderUrl') || '');
-
+	const getUserPathAvatar = (() => {
+		if(externalProviderUrl) {
+			return (uid: string) => externalProviderUrl.trim().replace(/\/+$/, '').replace('{username}', uid);
+		}
+		if(cdnAvatarUrl){
+			return (uid: string, etag?: string) => `${ cdnAvatarUrl }/avatar/${ uid }${ etag ? `?etag=${ etag }` : '' }`)
+		}
+		return (uid: string, etag?: string) => `/avatar/${ uid }${ etag ? `?etag=${ etag }` : '' }`
+	})();
+	
 	const contextValue = useMemo(() => ({
-		getUserPathAvatar: externalProviderUrl
-			? (uid: string): string => {
-				const externalSource = externalProviderUrl.trim().replace(/\/+$/, '').replace('{username}', uid);
-				return externalSource;
-			} : (uid: string, etag?: string): string => `/avatar/${ uid }${ etag ? `?etag=${ etag }` : '' }`,
+		getUserPathAvatar,
 		getRoomPathAvatar: ({ type, ...room }: any): string => roomTypes.getConfig(type || room.t).getAvatarPath({ username: room._id, ...room }),
-	}), [externalProviderUrl]);
+	}), [externalProviderUrl, cdnAvatarUrl]);
 
 	return <AvatarUrlContext.Provider children={children} value={contextValue} />;
 };
