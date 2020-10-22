2e3c7e0575cd561f8ee2624e8a09eed81377bcc2
Christopher Price <cprice@priceconsultinggroupinc.com>
2020-10-22 14:02:59 -0400

    allow non-admin user who has appropriate permissions in channel to change the avatar, as expected.

--- app/file-upload/server/lib/FileUpload.js
+++ app/file-upload/server/lib/FileUpload.js
@@ -181,8 +181,15 @@ export const FileUpload = {
 		if (settings.get('Accounts_AvatarResize') !== true) {
 			return;
 		}
-		if (Meteor.userId() !== file.userId && !hasPermission(Meteor.userId(), 'edit-other-user-info')) {
-			throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed');
+
+		if (file.rid) {
+			if (!hasPermission(Meteor.userId(), 'edit-room-avatar', file.rid)) {
+				throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed');
+			}
+		} else {
+			if (Meteor.userId() !== file.userId && !hasPermission(Meteor.userId(), 'edit-other-user-info')) {
+				throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed');
+			}
 		}
 
 		const tempFilePath = UploadFS.getTempFilePath(file._id);
