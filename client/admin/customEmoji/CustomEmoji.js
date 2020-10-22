3622ce3e39283a3dcc03911f660260fa1ef039b8
microsoft-sucks <67187266+microsoft-sucks@users.noreply.github.com>
2020-10-22 11:49:57 +0200

    Remove useless import

--- client/admin/customEmoji/CustomEmoji.js
+++ client/admin/customEmoji/CustomEmoji.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { Box, Table } from '@rocket.chat/fuselage';
 
 import FilterByText from '../../components/FilterByText';
