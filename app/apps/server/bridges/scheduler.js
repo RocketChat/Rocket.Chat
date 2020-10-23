090906987d9ac25c56128132d38e85ed0bbb4b82
thassiov <tvmcarvalho@gmail.com>
2020-10-22 22:50:47 -0300

    Add a method to create a job's "real" processor id

--- app/apps/server/bridges/scheduler.js
+++ app/apps/server/bridges/scheduler.js
@@ -8,16 +8,23 @@ export class AppSchedulerBridge {
 
 	async registerProcessor(processor, appId) {
 		this.orch.debugLog(`The App ${ appId } is registering a new job processor`, processor);
-		this.scheduler.define(processor.id, processor.processor);
+		const processorRealId = createProcessorId(processor.id, appId);
+		this.scheduler.define(processorRealId, processor.processor);
 	}
 
 	async scheduleOnce(job, appId) {
 		this.orch.debugLog(`The App ${ appId } is scheduling an onetime job`, job);
-		await this.scheduler.schedule(job.when, job.id, job.data || {});
+		const processorRealId = createProcessorId(job.id, appId);
+		await this.scheduler.schedule(job.when, processorRealId, job.data || {});
 	}
 
 	async scheduleRecurring(job, appId) {
 		this.orch.debugLog(`The App ${ appId } is scheduling a recurring job`, job);
-		await this.scheduler.every(job.cron, job.id, job.data || {});
+		const processorRealId = createProcessorId(job.id, appId);
+		await this.scheduler.every(job.cron, processorRealId, job.data || {});
 	}
+}
+
+function createProcessorId(jobId, appId) {
+	return `${jobId}_${appId}`;
 }
