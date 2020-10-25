87ed6ff153430c3704abcb301e5dd2c6f3dde105
Renato Becker <renato.augusto.becker@gmail.com>
2020-10-25 15:12:08 -0300

    Add Migration.

---
+++ server/startup/migrations/v209.js
@@ -0,0 +1,26 @@
+import { Migrations } from '../../../app/migrations';
+import { LivechatRooms, Subscriptions } from '../../../app/models/server';
+
+Migrations.add({
+	version: 209,
+	up() {
+		Subscriptions.find({
+			t: 'l',
+			department: { $exists: false },
+		}).forEach((subscription) => {
+			const { _id, rid } = subscription;
+			const room = LivechatRooms.findOneById(rid);
+			if (!room && !room.departmentId) {
+				return;
+			}
+
+			Subscriptions.update({
+				_id,
+			}, {
+				$set: {
+					department: room.departmentId,
+				},
+			});
+		});
+	},
+});
