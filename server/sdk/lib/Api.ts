3c22b3462a5c41709a2a706e07491f87f26151cf
Diego Sampaio <chinello@gmail.com>
2020-11-01 19:37:26 -0300

    Create internal services

--- server/sdk/lib/Api.ts
+++ server/sdk/lib/Api.ts
@@ -4,10 +4,21 @@ import { ServiceClass } from '../types/ServiceClass';
 import { EventSignatures } from './Events';
 import { LocalBroker } from './LocalBroker';
 
+const {
+	INTERNAL_SERVICES_ONLY = 'false',
+	SERVICES_ALLOWED = '',
+} = process.env;
+
 export class Api {
 	private services = new Set<ServiceClass>();
 
 	private broker: IBroker = new LocalBroker();
+
+	// wether only internal services are allowed to be registered
+	private internalOnly = ['true', 'yes'].includes(INTERNAL_SERVICES_ONLY.toLowerCase());
+
+	// list of allowed services to run - has precedence over `internalOnly`
+	private allowedList = new Set<string>(SERVICES_ALLOWED?.split(','));
 
 	// set a broker for the API and registers all services in the broker
 	setBroker(broker: IBroker): void {
@@ -29,6 +40,9 @@ export class Api {
 	}
 
 	registerService(instance: ServiceClass): void {
+		if (!this.isServiceAllowed(instance)) {
+			return;
+		}
 		this.services.add(instance);
 
 		if (this.broker) {
@@ -46,5 +60,20 @@ export class Api {
 
 	async broadcast<T extends keyof EventSignatures>(event: T, ...args: Parameters<EventSignatures[T]>): Promise<void> {
 		return this.broker.broadcast(event, ...args);
+	}
+
+	private isServiceAllowed(instance: ServiceClass): boolean {
+
+		// check if the service is in the list of allowed services if the list is not empty
+		if (this.allowedList.size > 0 && !this.allowedList.has(instance.getName())) {
+			return false;
+		}
+
+		// allow only internal services if internalOnly is true
+		if (this.internalOnly && !instance.isInternal()) {
+			return false;
+		}
+
+		return true;
 	}
 }
