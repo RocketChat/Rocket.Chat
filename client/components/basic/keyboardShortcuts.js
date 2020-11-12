2ea752fd16fe637c52911e7a024dbec93f6bcd97
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2020-11-12 20:06:26 -0300

    fix section prop, create ShortcutSection

--- client/components/basic/keyboardShortcuts.js
+++ client/components/basic/keyboardShortcuts.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Box } from '@rocket.chat/fuselage';
+import { Box, Divider } from '@rocket.chat/fuselage';
 
 import { useTranslation } from '../../contexts/TranslationContext';
 import VerticalBar from './VerticalBar';
 
+const ShortcutSection = ({ title, command }) =>
+	<Box is='section' mb='x16'>
+		<Box fontSize='x16' fontWeight='700'>{title}</Box>
+		<Divider />
+		<Box dangerouslySetInnerHTML={{ __html: command }} />
+	</Box>;
 
 const KeyboardShortcuts = () => {
 	const t = useTranslation();
@@ -15,47 +21,39 @@ const KeyboardShortcuts = () => {
 				<VerticalBar.Text>{t('Keyboard_Shortcuts_Title')}</VerticalBar.Text>
 				<VerticalBar.Close />
 			</VerticalBar.Header>
-			<VerticalBar.ScrollableContent className='contextual-bar__content'>
-				<Box is='section' className='section'>
-					<h2>{t('Keyboard_Shortcuts_Open_Channel_Slash_User_Search')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_1') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Mark_all_as_read')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_8') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Edit_Previous_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_2') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Move_To_Beginning_Of_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_3') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Move_To_Beginning_Of_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_4') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Move_To_End_Of_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_5') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_Move_To_End_Of_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_6') }} />
-				</Box>
-				<Box is='section' section>
-					<h2>{t('Keyboard_Shortcuts_New_Line_In_Message')}</h2>
-					<hr />
-					<Box dangerouslySetInnerHTML={{ __html: t('Keyboard_Shortcuts_Keys_7') }} />
-				</Box>
+			<VerticalBar.ScrollableContent>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Open_Channel_Slash_User_Search')}
+					command={t('Keyboard_Shortcuts_Keys_1')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Mark_all_as_read')}
+					command={t('Keyboard_Shortcuts_Keys_8')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Edit_Previous_Message')}
+					command={t('Keyboard_Shortcuts_Keys_2')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Move_To_Beginning_Of_Message')}
+					command={t('Keyboard_Shortcuts_Keys_3')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Move_To_Beginning_Of_Message')}
+					command={t('Keyboard_Shortcuts_Keys_4')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Move_To_End_Of_Message')}
+					command={t('Keyboard_Shortcuts_Keys_5')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_Move_To_End_Of_Message')}
+					command={t('Keyboard_Shortcuts_Keys_6')}
+				/>
+				<ShortcutSection
+					title={t('Keyboard_Shortcuts_New_Line_In_Message')}
+					command={t('Keyboard_Shortcuts_Keys_7')}
+				/>
 			</VerticalBar.ScrollableContent>
 		</>
 	);
