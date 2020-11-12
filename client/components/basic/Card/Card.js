e57cb5dc9bce6781b89de5f86c5f9dee71d8f4b0
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-11-12 13:17:38 -0300

    Fix card

--- client/components/basic/Card/Card.js
+++ client/components/basic/Card/Card.js
@@ -5,7 +5,7 @@ const Title = ({ children }) => <Box mb='x8' fontScale='p2'>{children}</Box>;
 
 const Footer = ({ children }) => <Box mb='x8'>{children}</Box>;
 
-const Body = ({ children }) => <Box mb='x8' display='flex' flexDirection='row'>{children}</Box>;
+const Body = ({ children }) => <Box mb='x8' display='flex' flexDirection='row' flexGrow={1}>{children}</Box>;
 
 const Col = ({ children }) => <Box display='flex' alignSelf='stretch' w='x228' flexDirection='column' fontScale='c1'>{children}</Box>;
 
@@ -15,7 +15,7 @@ const ColTitle = ({ children }) => <Box fontScale='c2' m='none'>{children}</Box>
 
 const CardDivider = () => <Divider width='x1' mi='x28' mb='none' alignSelf='stretch'/>;
 
-const Card = ({ children }) => <Box pi='x16' pb='x8' alignSelf='center' width='fit-content' bg='neutral-100'>{children}</Box>;
+const Card = ({ children, ...props }) => <Box display='flex' flexDirection='column' pi='x16' pb='x8' width='fit-content' bg='neutral-100' {...props}>{children}</Box>;
 
 Object.assign(Col, {
 	Title: ColTitle,
