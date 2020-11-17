f0a33faac9819e90b67c2f762588d71c8a9d6c9e
Douglas Fabris <deefabris@gmail.com>
2020-11-17 20:28:34 -0300

    [IMPROVE] Rewrite Room Info (#19511)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

---
+++ client/components/basic/RoomInfo.stories.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { UserInfo } from './UserInfo';
+import { RoomInfo } from './RoomInfo';
+import VerticalBar from './VerticalBar';
+
+export default {
+	title: 'components/RoomInfo',
+	component: UserInfo,
+};
+
+const room = {
+	name: 'rocketchat-frontend-team',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis nisi vel arcu bibendum vehicula. Integer vitae suscipit libero',
+	announcement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis nisi vel arcu bibendum vehicula. Integer vitae suscipit libero',
+	topic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis nisi vel arcu bibendum vehicula. Integer vitae suscipit libero',
+};
+
+export const Default = () => <VerticalBar>
+	<RoomInfo
+		{...room}
+		icon='lock'
+		onClickHide={alert}
+		onClickLeave={alert}
+		onClickEdit={alert}
+		onClickDelete={alert}
+	/>
+</VerticalBar>;
+
+
+export const Archived = () => <VerticalBar>
+	<RoomInfo
+		{...room}
+		icon='lock'
+		onClickHide={alert}
+		onClickLeave={alert}
+		onClickEdit={alert}
+		onClickDelete={alert}
+		archived
+	/>
+</VerticalBar>;
+
+
+export const Broadcast = () => <VerticalBar>
+	<RoomInfo
+		{...room}
+		icon='lock'
+		onClickHide={alert}
+		onClickLeave={alert}
+		onClickEdit={alert}
+		onClickDelete={alert}
+		broadcast
+	/>
+</VerticalBar>;
