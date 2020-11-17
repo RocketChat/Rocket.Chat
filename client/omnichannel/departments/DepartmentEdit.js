8bbf5784c2cc5151213177693186a30e051252c2
Rafael Ferreira <rafaelblink@gmail.com>
2020-11-17 16:33:52 -0300

    Update client/omnichannel/departments/DepartmentEdit.js
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- client/omnichannel/departments/DepartmentEdit.js
+++ client/omnichannel/departments/DepartmentEdit.js
@@ -296,7 +296,7 @@ export function EditDepartment({ data, id, title, reload }) {
 						<WaitingQueueMessageInput value={waitingQueueMessage} handler={handleWaitingQueueMessage} label={'Waiting_queue_message'} />
 					</Field>}
 					{DepartmentForwarding && <Field>
-						<DepartmentForwarding value={departmentsAllowedToForward} handler={handleDepartmentsAllowedToForward} label={'List_of_departments_for_forward_description'} placeholder='Enter_a_department_name' />
+						<DepartmentForwarding value={departmentsAllowedToForward} handler={handleDepartmentsAllowedToForward} label={'List_of_departments_for_forward'} placeholder='Enter_a_department_name' />
 					</Field>}
 					<Field>
 						<Box display='flex' flexDirection='row'>
