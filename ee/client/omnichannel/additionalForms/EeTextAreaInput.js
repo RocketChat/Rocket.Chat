6d6a2695559a4b12507a9c92949d3e1df3fb1950
Rafael Ferreira <rafaelblink@gmail.com>
2020-11-17 23:38:17 -0300

    [IMPROVE] UI/UX enhancements in department pages following the design system (#19421)
    * [IMPROVEMENTS] Refactoring departments page to follow design system.
    
    * [FIX] Translation of title page and "new" button.
    
    * [IMPROVE] Removing unnecessary margin bottom from EE inputs and department margins from form.
    
    * Update client/omnichannel/departments/DepartmentEdit.js
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- ee/client/omnichannel/additionalForms/EeTextAreaInput.js
+++ ee/client/omnichannel/additionalForms/EeTextAreaInput.js
@@ -7,7 +7,7 @@ import { useTranslation } from '../../../../client/contexts/TranslationContext';
 export const EeTextAreaInput = ({ value, handler, label, placeholder }) => {
 	const t = useTranslation();
 
-	return <Field mbe='x16'>
+	return <Field>
 		<Field.Label>{t(label)}</Field.Label>
 		<Field.Row>
 			<TextAreaInput flexGrow={1} value={value} onChange={handler} placeholder={t(placeholder)} />
