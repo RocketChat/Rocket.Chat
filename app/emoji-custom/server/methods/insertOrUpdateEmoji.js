5b5faa90d36c0899e67822e132bd215a9bc0ef4c
m-shreyansh <shreyanshmeena2401@gmail.com>
2020-11-18 07:40:56 +0530

    making file for custom emoji necessary

--- app/emoji-custom/server/methods/insertOrUpdateEmoji.js
+++ app/emoji-custom/server/methods/insertOrUpdateEmoji.js
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+gimport { Meteor } from 'meteor/meteor';
 import _ from 'underscore';
 import s from 'underscore.string';
 import limax from 'limax';
@@ -62,9 +62,11 @@ Meteor.methods({
 		if (matchingResults.length > 0) {
 			throw new Meteor.Error('Custom_Emoji_Error_Name_Or_Alias_Already_In_Use', 'The custom emoji or one of its aliases is already in use', { method: 'insertOrUpdateEmoji' });
 		}
+
 		if (emojiData.extension===undefined) {
 			throw new Meteor.Error('error-the-field-is-required', 'The custom emoji file is required', { method: 'insertOrUpdateEmoji' });
 		}
+		
 		if (!emojiData._id) {
 			// insert emoji
 			const createEmoji = {
