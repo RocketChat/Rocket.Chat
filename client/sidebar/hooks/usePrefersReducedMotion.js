fff11c38bbc71b1db9fe9e13f4a9b99d05ecf766
Martin <martin.schoeler@rocket.chat>
2020-11-20 22:35:38 -0300

    Forgot to save :P

--- client/sidebar/hooks/usePrefersReducedMotion.js
+++ client/sidebar/hooks/usePrefersReducedMotion.js
@@ -1,7 +1,4 @@
 import { useMediaQueries } from '@rocket.chat/fuselage-hooks';
 
 
-export const usePrefersReducedMotion = () => {
-	console.log(useMediaQueries('(prefers-reduced-motion: reduce)')[0]);
-	return useMediaQueries('(prefers-reduced-motion: reduce)')[0];
-};
+export const usePrefersReducedMotion = () => useMediaQueries('(prefers-reduced-motion: reduce)')[0];
