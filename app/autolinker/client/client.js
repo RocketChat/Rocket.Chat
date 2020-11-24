781006bbd4acce8ebeacc2765cf4c30a7678f91f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-11-24 14:26:02 -0300

    Refactor autolinker initialization

--- app/autolinker/client/client.js
+++ app/autolinker/client/client.js
@@ -9,68 +9,70 @@ import { settings } from '../../settings';
 import { callbacks } from '../../callbacks';
 import { escapeRegExp } from '../../../lib/escapeRegExp';
 
-let config;
+const createAutolinker = (config) =>
+	(message) => {
+		if (!s.trim(message.html)) {
+			return message;
+		}
 
-Tracker.autorun(function() {
-	config = {
-		stripPrefix: settings.get('AutoLinker_StripPrefix'),
-		urls: {
-			schemeMatches: settings.get('AutoLinker_Urls_Scheme'),
-			wwwMatches: settings.get('AutoLinker_Urls_www'),
-			tldMatches: settings.get('AutoLinker_Urls_TLD'),
-		},
-		email: settings.get('AutoLinker_Email'),
-		phone: settings.get('AutoLinker_Phone'),
-		twitter: false,
-		stripTrailingSlash: false,
-	};
-});
+		let msgParts;
+		let regexTokens;
+		if (message.tokens && message.tokens.length) {
+			regexTokens = new RegExp(`(${ (message.tokens || []).map(({ token }) => escapeRegExp(token)) })`, 'g');
+			msgParts = message.html.split(regexTokens);
+		} else {
+			msgParts = [message.html];
+		}
 
-const renderMessage = (message) => {
-	if (!s.trim(message.html)) {
+		message.html = msgParts
+			.map((msgPart) => {
+				if (regexTokens && regexTokens.test(msgPart)) {
+					return msgPart;
+				}
+				return Autolinker.link(msgPart, {
+					...config,
+					replaceFn: (match) => {
+						const token = `=!=${ Random.id() }=!=`;
+						const tag = match.buildTag();
+
+						if (~match.matchedText.indexOf(Meteor.absoluteUrl())) {
+							tag.setAttr('target', '');
+						}
+
+						message.tokens.push({
+							token,
+							text: tag.toAnchorString(),
+						});
+						return token;
+					} });
+			})
+			.join('');
+
 		return message;
-	}
+	};
 
-	let msgParts;
-	let regexTokens;
-	if (message.tokens && message.tokens.length) {
-		regexTokens = new RegExp(`(${ (message.tokens || []).map(({ token }) => escapeRegExp(token)) })`, 'g');
-		msgParts = message.html.split(regexTokens);
-	} else {
-		msgParts = [message.html];
-	}
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		const isEnabled = settings.get('AutoLinker') === true;
 
-	message.html = msgParts
-		.map((msgPart) => {
-			if (regexTokens && regexTokens.test(msgPart)) {
-				return msgPart;
-			}
-			return Autolinker.link(msgPart, {
-				...config,
-				replaceFn: (match) => {
-					const token = `=!=${ Random.id() }=!=`;
-					const tag = match.buildTag();
+		if (!isEnabled) {
+			callbacks.remove('renderMessage', 'autolinker');
+			return;
+		}
 
-					if (~match.matchedText.indexOf(Meteor.absoluteUrl())) {
-						tag.setAttr('target', '');
-					}
+		const config = {
+			stripPrefix: settings.get('AutoLinker_StripPrefix'),
+			urls: {
+				schemeMatches: settings.get('AutoLinker_Urls_Scheme'),
+				wwwMatches: settings.get('AutoLinker_Urls_www'),
+				tldMatches: settings.get('AutoLinker_Urls_TLD'),
+			},
+			email: settings.get('AutoLinker_Email'),
+			phone: settings.get('AutoLinker_Phone'),
+			twitter: false,
+			stripTrailingSlash: false,
+		};
 
-					message.tokens.push({
-						token,
-						text: tag.toAnchorString(),
-					});
-					return token;
-				} });
-		})
-		.join('');
-
-	return message;
-};
-
-Tracker.autorun(function() {
-	if (settings.get('AutoLinker') !== true) {
-		return callbacks.remove('renderMessage', 'autolinker');
-	}
-
-	callbacks.add('renderMessage', renderMessage, callbacks.priority.MEDIUM, 'autolinker');
+		callbacks.add('renderMessage', createAutolinker(config), callbacks.priority.MEDIUM, 'autolinker');
+	});
 });
