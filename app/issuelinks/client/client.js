f9afb86f5bff27d1fbfdb7a618e8f3c504faa173
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-11-25 22:16:10 -0300

    Refactor issuelinks initialization

--- app/issuelinks/client/client.js
+++ app/issuelinks/client/client.js
@@ -1,21 +1,35 @@
-import s from 'underscore.string';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 
 import { settings } from '../../settings';
 import { callbacks } from '../../callbacks';
 
-//
-// IssueLink is a named function that will add issue links
-// @param {Object} message - The message object
-//
-
-function IssueLink(message) {
-	if (s.trim(message.html) && settings.get('IssueLinks_Enabled')) {
-		message.html = message.html.replace(/(?:^|\s|\n)(#[0-9]+)\b/g, function(match, issueNumber) {
-			const url = settings.get('IssueLinks_Template').replace('%s', issueNumber.substring(1));
-			return match.replace(issueNumber, `<a href="${ url }" target="_blank">${ issueNumber }</a>`);
-		});
+const createIssueLinksMessageRenderer = ({ template }) => (message) => {
+	if (!message.html?.trim()) {
+		return message;
 	}
-	return message;
-}
 
-callbacks.add('renderMessage', IssueLink, callbacks.priority.MEDIUM, 'issuelink');
+	message.html = message.html.replace(/(?:^|\s|\n)(#[0-9]+)\b/g, (match, issueNumber) => {
+		const url = template.replace('%s', issueNumber.substring(1));
+		return match.replace(issueNumber, `<a href="${ url }" target="_blank">${ issueNumber }</a>`);
+	});
+
+	return message;
+};
+
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		const isEnabled = settings.get('IssueLinks_Enabled');
+
+		if (!isEnabled) {
+			callbacks.remove('renderMessage', 'issuelink');
+			return;
+		}
+
+		const renderMessage = createIssueLinksMessageRenderer({
+			template: settings.get('IssueLinks_Template'),
+		});
+
+		callbacks.add('renderMessage', renderMessage, callbacks.priority.MEDIUM, 'issuelink');
+	});
+});
