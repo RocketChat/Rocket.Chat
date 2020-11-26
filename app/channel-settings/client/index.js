9dfaed566af546bef4eb6f6b4a1a52999df842c4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-11-26 17:41:01 -0300

    Remove trackSettingsChange module

--- app/channel-settings/client/index.js
+++ app/channel-settings/client/index.js
@@ -1,6 +1,5 @@
 import './startup/messageTypes';
 import './startup/tabBar';
-import './startup/trackSettingsChange';
 import './views/channelSettings.html';
 import './views/channelSettings';
 import './views/Multiselect';
