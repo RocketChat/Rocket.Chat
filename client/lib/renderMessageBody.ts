5cfd4fa8e4d64bf4e30ff3cb0488e374f43e8dca
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-11-27 19:22:37 -0300

    Rearrange renderMessageBody signature

--- client/lib/renderMessageBody.ts
+++ client/lib/renderMessageBody.ts
@@ -2,7 +2,7 @@ import { escapeHTML } from '../../lib/escapeHTML';
 import { callbacks } from '../../app/callbacks/lib/callbacks';
 import { IMessage } from '../../definition/IMessage';
 
-export const renderMessageBody = <T extends IMessage>(message: T & { html?: string }): string => {
+export const renderMessageBody = <T extends Partial<IMessage> & {html?: string}>(message: T): string => {
 	message.html = message.msg?.trim() ? escapeHTML(message.msg.trim()) : '';
 
 	const { tokens, html } = callbacks.run('renderMessage', message);
