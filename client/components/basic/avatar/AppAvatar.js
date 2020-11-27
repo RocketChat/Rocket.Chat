ca37a8df90e76e59ebec3b1c6d8bd1bdee6f196f
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2020-11-27 14:23:36 -0300

    fix AppAvatar margin

--- client/components/basic/avatar/AppAvatar.js
+++ client/components/basic/avatar/AppAvatar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Box } from '@rocket.chat/fuselage';
 
 import BaseAvatar from './BaseAvatar';
 
-const objectFit = { objectFit: 'contain' };
-
 export default function AppAvatar({ iconFileContent, iconFileData, ...props }) {
-	return <BaseAvatar style={objectFit} url={iconFileContent || `data:image/png;base64,${ iconFileData }`} {...props}/>;
+	return (
+		<Box {...props}>
+			<BaseAvatar objectFit url={iconFileContent || `data:image/png;base64,${ iconFileData }`}/>
+		</Box>
+	);
 }
