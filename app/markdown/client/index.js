b27b39299c48c8d1bc193f9e195748b8e23cf2ab
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-11-27 22:08:24 -0300

    Move markdown template helpers

--- app/markdown/client/index.js
+++ app/markdown/client/index.js
@@ -1,14 +1,2 @@
-import { Blaze } from 'meteor/blaze';
-
-import { Markdown } from '../lib/markdown';
-
-Blaze.registerHelper('RocketChatMarkdown', (text) => Markdown.parse(text));
-Blaze.registerHelper('RocketChatMarkdownUnescape', (text) => Markdown.parseNotEscaped(text));
-Blaze.registerHelper('RocketChatMarkdownInline', (text) => {
-	const output = Markdown.parse(text);
-	return output.replace(/^<p>/, '').replace(/<\/p>$/, '');
-});
-
-export { Markdown };
-
+export { Markdown } from '../lib/markdown';
 export { createMarkdownMessageRenderer, createMarkdownNotificationRenderer } from '../lib/markdown';
---
+++ client/templateHelpers/index.ts
@@ -0,0 +1,3 @@
+import './log';
+import './RocketChatMarkdown';
+import './RocketChatMarkdownInline';
