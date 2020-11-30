82eea98e092f4196eed4323a49f086a4d03412ad
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-11-30 09:18:23 -0300

    Direction RTL

--- client/components/basic/ScrollableContentWrapper.js
+++ client/components/basic/ScrollableContentWrapper.js
@@ -2,7 +2,11 @@ import React, { useCallback } from 'react';
 import SimpleBar from 'simplebar-react';
 import 'simplebar/src/simplebar.css';
 
+import { useDir } from '../../hooks/useDir';
+
 const CustomScrollbars = ({ onScroll, forwardedRef, children }) => {
+	const dir = useDir();
+
 	const refSetter = useCallback((scrollbarsRef) => {
 		if (scrollbarsRef) {
 			forwardedRef(scrollbarsRef.view);
@@ -11,10 +15,10 @@ const CustomScrollbars = ({ onScroll, forwardedRef, children }) => {
 		}
 	}, []);
 
-	return <SimpleBar ref={refSetter} style={{ maxHeight: '100%', flexGrow: 1 }} scrollableNodeProps={{ ref: forwardedRef, onScroll }} children={children}/>;
+	return <SimpleBar timeout={500} data-simplebar-direction={dir} direction={dir} ref={refSetter} style={{ direction: dir, maxHeight: '100%', flexGrow: 1 }} scrollableNodeProps={{ ref: forwardedRef, onScroll }} children={children}/>;
 };
 
 
-const ScrollableContentWrapper = React.forwardRef((props, ref) => <CustomScrollbars {...console.log(props)} {...props} forwardedRef={ref} />);
+const ScrollableContentWrapper = React.memo(React.forwardRef((props, ref) => <CustomScrollbars {...props} forwardedRef={ref} />));
 
 export default ScrollableContentWrapper;
