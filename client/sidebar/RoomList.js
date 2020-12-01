4ec647dae57d27ddb221c78941a49836d9951b15
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2020-12-01 11:13:27 -0300

    [FIX] Sidebar UI disappearing (#19725)

--- client/sidebar/RoomList.js
+++ client/sidebar/RoomList.js
@@ -154,7 +154,7 @@ const getMessage = (room, lastMessage, t) => {
 	if (lastMessage.u?.username === room.u?.username) {
 		return `${ t('You') }: ${ normalizeSidebarMessage(lastMessage, t) }`;
 	}
-	if (room.t === 'd' && room.uids.length <= 2) {
+	if (room.t === 'd' && room.uids && room.uids.length <= 2) {
 		return normalizeSidebarMessage(lastMessage, t);
 	}
 	return `${ lastMessage.u.name || lastMessage.u.username }: ${ normalizeSidebarMessage(lastMessage, t) }`;
