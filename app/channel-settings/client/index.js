fd4a69c431434b3daf01663993e872285fbe2338
Douglas Fabris <deefabris@gmail.com>
2020-12-08 01:38:04 -0300

    Regression: RoomInfo folder structure (#19787)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

--- app/channel-settings/client/index.js
+++ app/channel-settings/client/index.js
@@ -1,9 +1,6 @@
 import './startup/messageTypes';
 import './startup/tabBar';
 import './startup/trackSettingsChange';
-import './views/channelSettings.html';
-import './views/channelSettings';
 import './views/Multiselect';
-import './stylesheets/channel-settings.css';
 
 export { ChannelSettings } from './lib/ChannelSettings';
