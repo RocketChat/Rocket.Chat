ef79b8d46c4431f7fe2de8d3c5c96c5a7a0c4e01
Gabriel Thomé <gabrielallan.thome@gmail.com>
2020-12-08 16:52:37 -0300

    refactor group dm update name method

--- app/lib/server/functions/updateGroupDMsName.js
+++ app/lib/server/functions/updateGroupDMsName.js
@@ -3,46 +3,48 @@ import { Rooms, Subscriptions, Users } from '../../../models/server';
 const getFname = (members) => members.map(({ name, username }) => name || username).join(', ');
 const getName = (members) => members.map(({ username }) => username).join(',');
 
-export const updateGroupDMsName = (user) => {
-	if (!user.username) {
-		return;
-	}
-
+function getUsersWhoAreInTheSameGroupDMsAs(user) {
 	const userIds = new Set();
 	const users = new Map();
 
+	// add all users to single array so we can fetch details from them all at once
 	const rooms = Rooms.findGroupDMsByUids(user._id, { fields: { uids: 1 } });
 
 	if (rooms.count() === 0) {
 		return;
 	}
 
-	// add all users to single array so we can fetch details from them all at once
 	rooms.forEach((room) => room.uids.forEach((uid) => uid !== user._id && userIds.add(uid)));
 
 	Users.findByIds([...userIds], { fields: { username: 1, name: 1 } })
 		.forEach((user) => users.set(user._id, user));
+
+	return users;
+}
+
+function sortUsersAlphabetically(u1, u2) {
+	return (u1.name || u1.username).localeCompare(u2.name || u2.username);
+}
+
+export const updateGroupDMsName = (userThatChangedName) => {
+	if (!userThatChangedName.username) {
+		return;
+	}
+
+	const users = getUsersWhoAreInTheSameGroupDMsAs(userThatChangedName);
+	const rooms = Rooms.findGroupDMsByUids(userThatChangedName._id, { fields: { uids: 1 } });
 
 	const getMembers = (uids) => uids.map((uid) => users.get(uid)).filter(Boolean);
 
 	// loop rooms to update the subcriptions from them all
 	rooms.forEach((room) => {
 		const members = getMembers(room.uids);
-		members.push(user);
-		const sortedMembers = members.sort((u1, u2) => (u1.name || u1.username).localeCompare(u2.name || u2.username));
-
-		console.log('members: ', members);
-		console.log('sorted members: ', sortedMembers);
+		members.push(userThatChangedName);
+		const sortedMembers = members.sort(sortUsersAlphabetically);
 
 		const subs = Subscriptions.findByRoomId(room._id, { fields: { _id: 1, 'u._id': 1 } });
-		console.log('subscriptions: ', subs);
 		subs.forEach((sub) => {
 			const otherMembers = sortedMembers.filter(({ _id }) => _id !== sub.u._id);
-
-			console.log('other members: ', otherMembers);
-			console.log('updating names: ', getName(otherMembers));
-			console.log('updating fNames: ', getFname(otherMembers));
-
 			Subscriptions.updateNameAndFnameById(sub._id, getName(otherMembers), getFname(otherMembers));
 		});
 	});
