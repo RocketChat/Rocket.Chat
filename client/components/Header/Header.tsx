0cbfa37c329f5a80a3cd4db98d6642fa0b2d6625
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-12-16 19:18:03 -0300

    Fix Types

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -2,16 +2,16 @@ import React, { FC } from 'react';
 import { Box, Icon, Divider, ButtonGroup, ActionButton, Badge, BadgeProps } from '@rocket.chat/fuselage';
 import { css } from '@rocket.chat/css-in-js';
 
-const Title = (props: any): JSX.Element => <Box color='default' mi='x4' fontScale='s2' withTruncatedText {...props}/>;
-const Subtitle = (props: any): JSX.Element => <Box color='hint' fontScale='p1' withTruncatedText {...props}/>;
+const Title: FC = (props: any) => <Box color='default' mi='x4' fontScale='s2' withTruncatedText {...props}/>;
+const Subtitle: FC = (props: any) => <Box color='hint' fontScale='p1' withTruncatedText {...props}/>;
 
-const Row = (props: any): JSX.Element => <Box alignItems='center' flexShrink={1} flexGrow={1} display='flex' {...props}/>;
+const Row: FC = (props: any) => <Box alignItems='center' flexShrink={1} flexGrow={1} display='flex' {...props}/>;
 
-const HeaderIcon = ({ icon }: { icon: JSX.Element | { name: string; color?: string } | null}): JSX.Element | null => icon && <Box display='flex' flexShrink={0} alignItems='center' size={18} overflow='hidden' justifyContent='center'>{React.isValidElement(icon) ? icon : <Icon size='x18' { ...{ name: (icon as any).name }} />}</Box>;
+const HeaderIcon: FC<{ icon: JSX.Element | { name: string; color?: string } | null}> = ({ icon }) => icon && <Box display='flex' flexShrink={0} alignItems='center' size={18} overflow='hidden' justifyContent='center'>{React.isValidElement(icon) ? icon : <Icon color='info' size='x18' { ...{ name: (icon as any).name }} />}</Box>;
 
-const ToolBox = (props: any): JSX.Element => <ButtonGroup small {...props}/>;
+const ToolBox: FC = (props: any) => <ButtonGroup small {...props}/>;
 
-const ToolBoxAction: any = ({ id, icon, title, action, className, tabId, index, ...props }: any): JSX.Element => <ActionButton
+const ToolBoxAction: FC & { Badge: FC } = ({ id, icon, title, action, className, tabId, index, ...props }: any) => <ActionButton
 	className={className}
 	primary={tabId === id}
 	onClick={action}
@@ -19,7 +19,7 @@ const ToolBoxAction: any = ({ id, icon, title, action, className, tabId, index, 
 	data-toolbox={index}
 	key={id}
 	icon={icon}
-	position={'relative'}
+	position='relative'
 	ghost
 	small
 	overflow='visible'
@@ -28,21 +28,23 @@ const ToolBoxAction: any = ({ id, icon, title, action, className, tabId, index, 
 
 const ToolBoxActionBadge: FC<BadgeProps> = (props) => <Box position='absolute' className={css`top: 0; right: 0; transform: translate(30%, -30%);`}><Badge {...props}/></Box>;
 
-const State = (props: any): JSX.Element => (props.onClick ? <ActionButton ghost mini {...props}/> : <Icon size={16} name={props.icon} {...props}/>);
+const State: FC = (props: any) => (props.onClick ? <ActionButton ghost mini {...props}/> : <Icon size={16} name={props.icon} {...props}/>);
 
-const Content = (props: any): JSX.Element => <Box flexGrow={1} width={1} flexShrink={1} mi='x4' display='flex' justifyContent='center' flexDirection='column' {...props}/>;
+const Content: FC = (props: any) => <Box flexGrow={1} width={1} flexShrink={1} mi='x4' display='flex' justifyContent='center' flexDirection='column' {...props}/>;
 
-const Button = (props: any): JSX.Element => <Box mi='x4' display='flex' alignItems='center' {...props}/>;
-const Avatar = (props: any): JSX.Element => <Button width='x36' {...props}/>;
+const Button: FC = (props: any) => <Box mi='x4' display='flex' alignItems='center' {...props}/>;
+const Avatar: FC = (props: any) => <Button width='x36' {...props}/>;
 
-const HeaderDivider = (): JSX.Element => <Divider { ...{ mbs: 'neg-x2', mbe: 0 } as any} />;
+const HeaderDivider: FC = () => <Divider { ...{ mbs: 'neg-x2', mbe: 0 } as any} />;
 
-const Header = (props: any): JSX.Element => <Box rcx-room-header is='header' height='x64' display='flex' justifyContent='center' flexDirection='column' overflow='hidden' flexShrink={0}>
+const Header: FC & { ToolBoxAction: FC } = (props: any) => <Box rcx-room-header is='header' height='x64' display='flex' justifyContent='center' flexDirection='column' overflow='hidden' flexShrink={0}>
 	<Box mi='neg-x4' height='x64' pi='x24' display='flex' flexGrow={1} justifyContent='center' alignItems='center' overflow='hidden' flexDirection='row' {...props}/>
 	<HeaderDivider/>
 </Box>;
+
+ToolBoxAction.Badge = ToolBoxActionBadge;
 Header.ToolBoxAction = ToolBoxAction;
-Header.ToolBoxAction.Badge = ToolBoxActionBadge;
+
 export default Header;
 
 Object.assign(Content, {
