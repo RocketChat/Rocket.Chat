ef3d37e47bde66225a1d31c2a2cbc2e2301392fd
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-12-16 04:08:19 -0300

    Fix DM

--- client/views/room/contextualBar/RoomFiles/hooks/useFileList.js
+++ client/views/room/contextualBar/RoomFiles/hooks/useFileList.js
@@ -3,7 +3,14 @@ import { useCallback } from 'react';
 import { useDataWithLoadMore } from '../../hooks/useDataWithLoadMore';
 import { useEndpoint } from '../../../../../contexts/ServerContext';
 
+const roomTypes = {
+	c: 'channels.files',
+	l: 'channels.files',
+	d: 'im.files',
+	p: 'groups.files',
+};
+
 export const useFileList = (type, params) => {
-	const method = useEndpoint('GET', type === 'c' ? 'channels.files' : 'groups.files');
+	const method = useEndpoint('GET', roomTypes[type]);
 	return useDataWithLoadMore(useCallback((args) => method(args), [method]), params);
 };
