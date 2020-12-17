6730e7543a5e0e122c23b2bdfb4c7bbb1d7f76d6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-12-17 17:35:17 -0300

    Add bulkUpsertMessages in useMessageList

--- client/views/room/contextualBar/StarredMessages/hooks/useMessageList.ts
+++ client/views/room/contextualBar/StarredMessages/hooks/useMessageList.ts
@@ -4,8 +4,9 @@ import { IMessage } from '../../../../../../definition/IMessage';
 
 type MessageList = {
 	messages: IMessage[];
-	upsertMessage: (payload: IMessage) => void;
-	deleteMessage: (payload: { _id: string }) => void;
+	upsertMessage: (message: IMessage) => void;
+	bulkUpsertMessages: (messages: IMessage[]) => void;
+	deleteMessage: (mid: IMessage['_id']) => void;
 };
 
 type UpsertAction = {
@@ -13,18 +14,22 @@ type UpsertAction = {
 	payload: IMessage;
 };
 
-type DeleteAction = {
-	type: 'delete';
-	payload: { _id: string };
+type BulkUpsertAction = {
+	type: 'bulkUpsert';
+	payload: IMessage[];
 };
 
-  type Action = UpsertAction | DeleteAction;
+type DeleteAction = {
+	type: 'delete';
+	payload: IMessage['_id'];
+};
+
+type Action = UpsertAction | BulkUpsertAction | DeleteAction;
 
 const messageReducer = (state: IMessage[], action: Action): IMessage[] => {
-	const index = state.findIndex((value) => value._id === action.payload?._id);
-
 	switch (action.type) {
-		case 'upsert':
+		case 'upsert': {
+			const index = state.findIndex((value) => value._id === action.payload?._id);
 			if (index > -1) {
 				state[index] = action.payload;
 
@@ -32,12 +37,30 @@ const messageReducer = (state: IMessage[], action: Action): IMessage[] => {
 			}
 
 			return [...state, action.payload];
-		case 'delete':
+		}
+
+		case 'bulkUpsert': {
+			if (action.payload.length === 0) {
+				return state;
+			}
+
+			const map = new Map<IMessage['_id'], IMessage>([
+				...state.map<[IMessage['_id'], IMessage]>((message) => [message._id, message]),
+				...action.payload.map<[IMessage['_id'], IMessage]>((message) => [message._id, message]),
+			]);
+
+			return Array.from(map.values());
+		}
+
+		case 'delete': {
+			const index = state.findIndex((value) => value._id === action.payload);
 			if (index > -1) {
 				state.slice(0, index).concat(state.slice(index + 1));
 			}
 
 			return [...state];
+		}
+
 		default:
 			return state;
 	}
@@ -46,12 +69,25 @@ const messageReducer = (state: IMessage[], action: Action): IMessage[] => {
 export const useMessageList = (): MessageList => {
 	const [messages, changeMessageList] = useReducer(messageReducer, []);
 
-	const upsertMessage = useCallback((payload: IMessage) => changeMessageList({ type: 'upsert', payload }), []);
-	const deleteMessage = useCallback((payload: { _id: string }) => changeMessageList({ type: 'delete', payload }), []);
+	const upsertMessage = useCallback<MessageList['upsertMessage']>((message) => changeMessageList({
+		type: 'upsert',
+		payload: message,
+	}), []);
+
+	const bulkUpsertMessages = useCallback<MessageList['bulkUpsertMessages']>((messages) => changeMessageList({
+		type: 'bulkUpsert',
+		payload: messages,
+	}), []);
+
+	const deleteMessage = useCallback<MessageList['deleteMessage']>((mid) => changeMessageList({
+		type: 'delete',
+		payload: mid,
+	}), []);
 
 	return {
 		messages,
 		upsertMessage,
+		bulkUpsertMessages,
 		deleteMessage,
 	};
 };
