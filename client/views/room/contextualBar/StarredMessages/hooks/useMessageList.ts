107e4f3f7d4fe8514ab55a9bcbe922eb452dc4ff
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-12-18 20:26:22 -0300

    Remove reducer

--- client/views/room/contextualBar/StarredMessages/hooks/useMessageList.ts
+++ client/views/room/contextualBar/StarredMessages/hooks/useMessageList.ts
@@ -13,55 +13,8 @@ type MessageList = {
 	update: () => void;
 };
 
-type UpsertAction = {
-	type: 'upsert';
-	payload: IMessage;
-};
-
-type BulkUpsertAction = {
-	type: 'bulkUpsert';
-	payload: IMessage[];
-};
-
-type DeleteAction = {
-	type: 'delete';
-	payload: IMessage['_id'];
-};
-
-type Action = UpsertAction | BulkUpsertAction | DeleteAction;
-
 const compareMessages = (a: IMessage, b: IMessage): number =>
 	b.ts.getTime() - a.ts.getTime();
-
-export const messageReducer = (state: IMessage[], action: Action): IMessage[] => {
-	switch (action.type) {
-		case 'upsert': {
-			return [
-				action.payload,
-				...state.filter((message) => message._id !== action.payload._id),
-			].sort(compareMessages);
-		}
-
-		case 'bulkUpsert': {
-			if (action.payload.length === 0) {
-				return state;
-			}
-
-			const newIds = action.payload.map((message) => message._id);
-
-			return [
-				...action.payload,
-				...state.filter((message) => !newIds.includes(message._id)),
-			].sort(compareMessages);
-		}
-
-		case 'delete':
-			return state.filter((message) => message._id !== action.payload);
-
-		default:
-			return state;
-	}
-};
 
 export const useMessageList = (): MessageList => {
 	const { resolve, reject, reset, update, ...state } = useAsyncState<IMessage[]>();
