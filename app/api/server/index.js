e3f7f811224c4d2ca135f5a22afbf19ac753d8ef
Gabriel Thomé <gabrielallan.thome@gmail.com>
2020-12-21 14:53:58 -0300

    add endpoints for enterprise license

--- app/api/server/index.js
+++ app/api/server/index.js
@@ -37,5 +37,6 @@ import './v1/webdav';
 import './v1/oauthapps';
 import './v1/custom-sounds';
 import './v1/custom-user-status';
+import './v1/licenses';
 
 export { API, APIClass, defaultRateLimiterOptions } from './api';
