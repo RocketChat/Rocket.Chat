eb4fdebb7bbacecd255168492e2b48c7f1013b7f
Rafael Ferreira <rafaelblink@gmail.com>
2020-12-22 02:57:49 -0300

    [NEW] Omnichannel Contact Center (Directory) (#19931)
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- client/components/FilterByText.tsx
+++ client/components/FilterByText.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, TextInput } from '@rocket.chat/fuselage';
+import { Box, Icon, TextInput, Button } from '@rocket.chat/fuselage';
 import React, { FC, ChangeEvent, FormEvent, memo, useCallback, useEffect, useState } from 'react';
 
 import { useTranslation } from '../contexts/TranslationContext';
@@ -6,11 +6,17 @@ import { useTranslation } from '../contexts/TranslationContext';
 type FilterByTextProps = {
 	placeholder?: string;
 	onChange: (filter: { text: string }) => void;
+	displayButton: boolean;
+	textButton: string;
+	onButtonClick: () => void;
 };
 
 const FilterByText: FC<FilterByTextProps> = ({
 	placeholder,
 	onChange: setFilter,
+	displayButton: display = false,
+	textButton = '',
+	onButtonClick,
 	...props
 }) => {
 	const t = useTranslation();
@@ -29,8 +35,9 @@ const FilterByText: FC<FilterByTextProps> = ({
 		event.preventDefault();
 	}, []);
 
-	return <Box mb='x16' is='form' onSubmit={handleFormSubmit} display='flex' flexDirection='column' {...props}>
+	return <Box mb='x16' is='form' onSubmit={handleFormSubmit} display='flex' flexDirection='row' {...props}>
 		<TextInput placeholder={placeholder ?? t('Search')} addon={<Icon name='magnifier' size='x20'/>} onChange={handleInputChange} value={text} />
+		<Button onClick={onButtonClick} display={display ? 'block' : 'none'} mis='x8' primary>{textButton}</Button>
 	</Box>;
 };
 
