8057bf58549714b38987c94e217293d098d1f87c
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-12-21 21:04:48 -0300

    Fix scrollable

--- client/components/ScrollableContentWrapper.tsx
+++ client/components/ScrollableContentWrapper.tsx
@@ -4,13 +4,19 @@ import 'simplebar/src/simplebar.css';
 
 import { useDir } from '../hooks/useDir';
 
+
+const styleDefault = {
+	maxHeight: '100%', flexGrow: 1,
+};
+
+
 type CustomScrollbarsProps = {
 	onScroll?: Function;
 	ref: React.Ref<unknown>;
-	style: CSSProperties;
+	style?: CSSProperties;
 }
 
-const ScrollableContentWrapper: FC<CustomScrollbarsProps> = React.memo(React.forwardRef(({ onScroll, children, style }, ref) => {
+const ScrollableContentWrapper: FC<CustomScrollbarsProps> = React.memo(React.forwardRef(({ onScroll, children, style = styleDefault }, ref) => {
 	const dir = useDir();
 	return <SimpleBar data-simplebar-direction={dir} direction={dir} style={style} scrollableNodeProps={{ ref, onScroll }} children={children}/>;
 }));
