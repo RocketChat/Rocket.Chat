e33ba5441f54edeba6ac494ec7a38467c13697eb
Rafael Ferreira <rafaelblink@gmail.com>
2020-12-22 02:57:49 -0300

    [NEW] Omnichannel Contact Center (Directory) (#19931)
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- client/components/CustomFieldsForm.js
+++ client/components/CustomFieldsForm.js
@@ -24,7 +24,7 @@ const CustomTextInput = ({ name, required, minLength, maxLength, setState, state
 	}, [state, required, minLength, t]);
 
 	return useMemo(() => <Field className={className}>
-		<Field.Label>{t(name)}</Field.Label>
+		<Field.Label>{t(name)}{required && '*'}</Field.Label>
 		<Field.Row>
 			<TextInput name={name} error={verify} maxLength={maxLength} flexGrow={1} value={state} required={required} onChange={(e) => setState(e.currentTarget.value)}/>
 		</Field.Row>
@@ -38,7 +38,7 @@ const CustomSelect = ({ name, required, options = {}, setState, state, className
 	const verify = useMemo(() => (!state.length && required ? t('Field_required') : ''), [required, state.length, t]);
 
 	return useMemo(() => <Field className={className}>
-		<Field.Label>{t(name)}</Field.Label>
+		<Field.Label>{t(name)}{required && '*'}</Field.Label>
 		<Field.Row>
 			<Select name={name} error={verify} flexGrow={1} value={state} options={mappedOptions} required={required} onChange={(val) => setState(val)}/>
 		</Field.Row>
@@ -66,12 +66,12 @@ const CustomFieldsAssembler = ({ formValues, formHandlers, customFields, ...prop
 	return null;
 });
 
-export default function CustomFieldsForm({ customFieldsData, setCustomFieldsData, onLoadFields = () => {}, ...props }) {
-	const customFieldsJson = useSetting('Accounts_CustomFields');
+export default function CustomFieldsForm({ jsonCustomFields, customFieldsData, setCustomFieldsData, onLoadFields = () => {}, ...props }) {
+	const accountsCustomFieldsJson = useSetting('Accounts_CustomFields');
 
 	const [customFields] = useState(() => {
 		try {
-			return JSON.parse(customFieldsJson || '{}');
+			return jsonCustomFields || JSON.parse(accountsCustomFieldsJson || '{}');
 		} catch {
 			return {};
 		}
