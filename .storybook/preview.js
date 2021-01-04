9056b58e52c88e41a11e08f8e6c8ef8414cf2cf5
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-01-04 17:44:26 -0300

    Fix addon-backgrounds configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,12 +6,16 @@ import { rocketChatDecorator } from './decorators';
 addDecorator(rocketChatDecorator);
 
 addParameters({
+	backgrounds: {
+		grid: {
+			cellSize: 4,
+			cellAmount: 4,
+			opacity: 0.5,
+		},
+	},
 	docs: {
 		container: DocsContainer,
 		page: DocsPage,
-	},
-	grid: {
-		cellSize: 4,
 	},
 	options: {
 		storySort: ([, a], [, b]) =>
