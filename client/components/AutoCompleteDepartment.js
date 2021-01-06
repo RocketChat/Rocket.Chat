5e1e21ec50e35b5ace0a1d5055276e8966820aa1
Rafael Ferreira <rafaelblink@gmail.com>
2021-01-06 18:15:20 -0300

    refactor on AutoCompleteDepartment to accept none label.

--- client/components/AutoCompleteDepartment.js
+++ client/components/AutoCompleteDepartment.js
@@ -9,7 +9,9 @@ export const AutoCompleteDepartment = React.memo((props) => {
 	const [filter, setFilter] = useState('');
 	const { value: data } = useEndpointData('livechat/department', useMemo(() => ({ text: filter }), [filter]));
 
-	const options = useMemo(() => (data && [{ value: 'all', label: t('All') }, ...data.departments.map((department) => ({ value: department._id, label: department.name }))]) || [{ value: 'all', label: t('All') }], [data, t]);
+	const { label } = props;
+
+	const options = useMemo(() => (data && [{ value: 'all', label: label && label }, ...data.departments.map((department) => ({ value: department._id, label: department.name }))]) || [{ value: 'all', label: t('All') }], [data, t]);
 
 	return <AutoComplete
 		{...props}
