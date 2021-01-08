4de89c19a65d7669b59345cae18fd7cb78db74f0
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-01-08 16:13:37 -0300

    Rewrite action links

--- client/adapters.js
+++ client/adapters.js
@@ -1,3 +1,4 @@
 const { createTemplateForComponent } = require('./reactAdapters');
 
 createTemplateForComponent('ThreadReply', () => import('./components/Message/Metrics/Thread'));
+createTemplateForComponent('MessageActions', () => import('./components/Message/Actions'));
