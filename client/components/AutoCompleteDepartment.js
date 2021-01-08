48077ece0d7733d5af4170adb0601ad76133c79a
Rafael Ferreira <rafaelblink@gmail.com>
2021-01-07 23:28:38 -0300

    t('All') as fallback when label is not defined on Department Auto Complete.

--- client/components/AutoCompleteDepartment.js
+++ client/components/AutoCompleteDepartment.js
@@ -1,15 +1,17 @@
 import React, { useMemo, useState } from 'react';
 import { AutoComplete, Option } from '@rocket.chat/fuselage';
 
+import { useTranslation } from '../contexts/TranslationContext';
 import { useEndpointData } from '../hooks/useEndpointData';
 
 export const AutoCompleteDepartment = React.memo((props) => {
+	const t = useTranslation();
 	const [filter, setFilter] = useState('');
 	const { value: data } = useEndpointData('livechat/department', useMemo(() => ({ text: filter }), [filter]));
 
 	const { label } = props;
 
-	const options = useMemo(() => (data && [{ value: 'All', label: label && label }, ...data.departments.map((department) => ({ value: department._id, label: department.name }))]) || [{ value: 'All', label: label && label }], [data, label]);
+	const options = useMemo(() => (data && [{ value: 'All', label: label && t('All') }, ...data.departments.map((department) => ({ value: department._id, label: department.name }))]) || [{ value: 'All', label: label && t('All') }], [data, label, t]);
 
 	return <AutoComplete
 		{...props}
