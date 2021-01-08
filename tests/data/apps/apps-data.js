0ed1c2f71d139d3741d086e53788335c3f794201
lolimay <lolimay@lolimay.cn>
2021-01-08 22:18:54 +0800

    Replace APP_URL to jsdelivr's, since the Github's one is broken

--- tests/data/apps/apps-data.js
+++ tests/data/apps/apps-data.js
@@ -1,4 +1,4 @@
-export const APP_URL = 'https://github.com/RocketChat/Apps.RocketChat.Tester/blob/master/dist/appsrocketchattester_0.0.2.zip?raw=true';
+export const APP_URL = 'https://cdn.jsdelivr.net/gh/RocketChat/Apps.RocketChat.Tester@0.0.2/dist/appsrocketchattester_0.0.2.zip';
 export const APP_NAME = 'Apps.RocketChat.Tester';
 export const APP_USERNAME = 'appsrocketchattester.bot';
 export const apps = (path = '') => `/api/apps${ path }`;
