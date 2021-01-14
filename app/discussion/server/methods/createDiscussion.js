af5993897756ede0c2f7eb3259289ce9afb049ce
Pierre Lehnen <pierre.lehnen@rocket.chat>
2021-01-13 21:44:49 -0300

    Do not copy topic and description from encrypted rooms when starting a discussion

--- app/discussion/server/methods/createDiscussion.js
+++ app/discussion/server/methods/createDiscussion.js
@@ -93,10 +93,13 @@ const create = ({ prid, pmid, t_name, reply, users, user, encrypted }) => {
 	const invitedUsers = message ? [message.u.username, ...users] : users;
 
 	const type = roomTypes.getConfig(p_room.t).getDiscussionType();
+	const description = p_room.encrypted ? '' : message.msg;
+	const topic = p_room.encrypted ? '' : p_room.name;
+
 	const discussion = createRoom(type, name, user.username, [...new Set(invitedUsers)], false, {
 		fname: t_name,
-		description: message.msg, // TODO discussions remove
-		topic: p_room.name, // TODO discussions remove
+		description, // TODO discussions remove
+		topic, // TODO discussions remove
 		prid,
 		encrypted,
 	}, {
