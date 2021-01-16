6788973f53398061e08c944c8902a2b99151b192
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-01-15 21:27:41 -0300

    Rewrite Message action links (#20123)

--- client/adapters.js
+++ client/adapters.js
@@ -1,5 +1,6 @@
 import { createTemplateForComponent } from './reactAdapters';
 
+createTemplateForComponent('MessageActions', () => import('./components/Message/Actions'));
 createTemplateForComponent('reactAttachments', () => import('./components/Message/Attachments'));
 createTemplateForComponent('ThreadMetric', () => import('./components/Message/Metrics/Thread'));
 createTemplateForComponent('DiscussionMetric', () => import('./components/Message/Metrics/Discussion'));
