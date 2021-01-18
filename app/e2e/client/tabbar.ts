757f7d56d5e9711ad35e2c53eb99d602dd7d87f2
Pierre Lehnen <pierre.lehnen@rocket.chat>
2021-01-18 14:03:25 -0300

    Allow e2e toggling on DMs

--- app/e2e/client/tabbar.ts
+++ app/e2e/client/tabbar.ts
@@ -9,7 +9,9 @@ import { e2e } from './rocketchat.e2e';
 
 addAction('e2e', ({ room }) => {
 	const e2eEnabled = useSetting('E2E_Enable');
-	const hasPermission = usePermission('edit-room', room._id) && usePermission('toggle-room-e2e-encryption', room._id) && (e2e.isReady() || room.encrypted);
+	const e2eReady = e2e.isReady() || room.encrypted;
+	const e2ePermission = room.t === 'd' || usePermission('toggle-room-e2e-encryption', room._id);
+	const hasPermission = usePermission('edit-room', room._id) && e2ePermission && e2eReady;
 	const toggleE2E = useMethod('saveRoomSettings');
 
 	const action = useMutableCallback(() => {
