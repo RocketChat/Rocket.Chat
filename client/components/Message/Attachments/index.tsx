4b9392caa9d2b42294158ddad761d693dd0cb0c2
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-01-19 13:49:54 -0300

    Fix TS

--- client/components/Message/Attachments/index.tsx
+++ client/components/Message/Attachments/index.tsx
@@ -52,7 +52,7 @@ const isQuoteAttachment = (attachment: AttachmentPropsGeneric): attachment is Qu
 
 const isActionAttachment = (attachment: AttachmentPropsGeneric): attachment is ActionAttachmentProps => 'actions' in attachment;
 
-const applyMarkdownIfRequires = (list: AttachmentProps['mrkdwn_in']) => (key: PossibleMarkdownFields, text: string): JSX.Element | string => (list?.includes(key) ? <MarkdownText withRichContent={null} content={text}/> : text);
+const applyMarkdownIfRequires = (list: AttachmentProps['mrkdwn_in']) => (key: PossibleMarkdownFields, text: string): JSX.Element | string => (list?.includes(key) ? <MarkdownText withRichContent={undefined} content={text}/> : text);
 
 const Item: FC<{attachment: AttachmentPropsGeneric; file?: FileProp }> = memo(({ attachment, file = null }) => {
 	if (isFileAttachment(attachment)) {
@@ -73,7 +73,7 @@ const Item: FC<{attachment: AttachmentPropsGeneric; file?: FileProp }> = memo(({
 			</Attachment.Author> }
 			{attachment.title && <Attachment.Title {...attachment.title_link && { is: 'a', href: attachment.title_link, target: '_blank', color: undefined }}>{attachment.title}</Attachment.Title> }
 			{attachment.text && <Attachment.Text>{applyMardownFor('text', attachment.text)}</Attachment.Text>}
-			{attachment.fields && <FieldsAttachment fields={attachment.mrkdwn_in?.includes('fields') ? attachment.fields.map(({ value, ...rest }) => ({ ...rest, value: <MarkdownText withRichContent={null} content={value} /> })) : attachment.fields} />}
+			{attachment.fields && <FieldsAttachment fields={attachment.mrkdwn_in?.includes('fields') ? attachment.fields.map(({ value, ...rest }) => ({ ...rest, value: <MarkdownText withRichContent={undefined} content={value} /> })) : attachment.fields} />}
 			{attachment.image_url && <Attachment.Image {...attachment.image_dimensions as any} src={attachment.image_url} />}
 			{/* DEPRECATED */}
 			{isActionAttachment(attachment) && <ActionAttachment {...attachment} />}
