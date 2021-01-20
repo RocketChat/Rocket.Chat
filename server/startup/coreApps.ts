8906a3b2aa4b76af9c2dce417d34e52fb760843b
Diego Sampaio <chinello@gmail.com>
2021-01-20 13:55:37 -0300

    Implement Banner core app

--- server/startup/coreApps.ts
+++ server/startup/coreApps.ts
@@ -1,8 +1,10 @@
 import { MongoInternals } from 'meteor/mongo';
 
 import { Nps } from '../modules/core-apps/nps.module';
+import { BannerModule } from '../modules/core-apps/banner.module';
 import { registerCoreApp } from '../services/uikit-core-app/service';
 
 const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
 
 registerCoreApp(new Nps(db));
+registerCoreApp(new BannerModule());
