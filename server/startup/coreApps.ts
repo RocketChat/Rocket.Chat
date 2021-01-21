031a56efa429ce41ce6977982091f1da1cf58180
Diego Sampaio <chinello@gmail.com>
2021-01-21 15:37:28 -0300

    Fix coreApps

--- server/startup/coreApps.ts
+++ server/startup/coreApps.ts
@@ -1,10 +1,6 @@
-import { MongoInternals } from 'meteor/mongo';
-
 import { Nps } from '../modules/core-apps/nps.module';
 import { BannerModule } from '../modules/core-apps/banner.module';
 import { registerCoreApp } from '../services/uikit-core-app/service';
 
-const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
-
-registerCoreApp(new Nps(db));
+registerCoreApp(new Nps());
 registerCoreApp(new BannerModule());
