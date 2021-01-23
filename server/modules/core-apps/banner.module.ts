98c8bde0b2f341ada0797efaa11387f52b2b9d37
Diego Sampaio <chinello@gmail.com>
2021-01-23 01:17:16 -0300

    Remove unused code

--- server/modules/core-apps/banner.module.ts
+++ server/modules/core-apps/banner.module.ts
@@ -1,31 +1,8 @@
 import { IUiKitCoreApp } from '../../sdk/types/IUiKitCoreApp';
-import { Banner, UiKitCoreApp } from '../../sdk';
+import { Banner } from '../../sdk';
 
 export class BannerModule implements IUiKitCoreApp {
 	appId = 'banner-core';
-
-	// banner block action should just redirect the action to the correct Core App
-	// the Core App identifier will be on 'blockId'
-	async blockAction(payload: any): Promise<any> {
-		// TODO validate payload
-		const {
-			payload: {
-				blockId,
-			},
-		} = payload;
-
-		const isRegistered = await UiKitCoreApp.isRegistered(blockId);
-		if (!isRegistered) {
-			throw new Error('not registered');
-		}
-
-		// change the appId with the one comming from the action to make it compliant (? - not sure this is needed)
-		payload.appId = blockId;
-
-		const result = await UiKitCoreApp.blockAction(payload);
-
-		return result;
-	}
 
 	// when banner view is closed we need to dissmiss that banner for that user
 	async viewClosed(payload: any): Promise<any> {
