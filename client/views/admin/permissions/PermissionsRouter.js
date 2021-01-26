be451463570d97febafcbe301e8e4b5af57dac4b
yash-rajpal <rajpal.yash03@gmail.com>
2021-01-26 15:21:24 +0530

    added permission check

--- client/views/admin/permissions/PermissionsRouter.js
+++ client/views/admin/permissions/PermissionsRouter.js
@@ -3,8 +3,16 @@ import React from 'react';
 import { useRouteParameter } from '../../../contexts/RouterContext';
 import UsersInRole from './UsersInRole';
 import PermissionsTable from './PermissionsTable';
+import { usePermission } from '../../../contexts/AuthorizationContext';
+import NotAuthorizedPage from '../../../components/NotAuthorizedPage';
 
 const PermissionsRouter = () => {
+	const canViewPermission = usePermission('access-permissions');
+
+	if (!canViewPermission) {
+		return <NotAuthorizedPage />;
+	}
+
 	const context = useRouteParameter('context');
 	if (context === 'users-in-role') {
 		return <UsersInRole />;
