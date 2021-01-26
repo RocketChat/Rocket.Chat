901706152cd7ed47f99f2c55c444f27869e5b301
Adithya Vardhan <64399555+im-adithya@users.noreply.github.com>
2021-01-26 12:57:53 +0530

    Update Image.tsx

--- client/components/Message/Attachments/components/Image.tsx
+++ client/components/Message/Attachments/components/Image.tsx
@@ -19,7 +19,7 @@ export type ImageProps = {
 	setLoadImage: () => void;
 } & Dimensions & ({ loadImage: true } | { loadImage: false; setLoadImage: () => void });
 
-const ImageBox: FC<BoxProps> = (props) => <Box display='flex' maxWidth='full' maxHeight='200px flexDirection='column' justifyContent='center' alignItems='center' alignContent='center' borderRadius='x2' borderWidth='x2' borderStyle='solid' borderColor='neutral-200' {...props}/>;
+const ImageBox: FC<BoxProps> = (props) => <Box display='flex' maxWidth='full' maxHeight='200px' flexDirection='column' justifyContent='center' alignItems='center' alignContent='center' borderRadius='x2' borderWidth='x2' borderStyle='solid' borderColor='neutral-200' {...props}/>;
 
 export const Retry: FC<BoxProps & { retry: () => void }> = ({ retry, ...props }) => {
 	const t = useTranslation();
