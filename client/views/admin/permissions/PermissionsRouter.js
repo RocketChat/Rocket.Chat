f34bfc3996331cc29d768603397b875514ff9520
sushant52 <sinhasushant52@gmail.com>
2021-01-26 21:54:54 +0530

    Corrected the user ability to view permissions

--- client/views/admin/permissions/PermissionsRouter.js
+++ client/views/admin/permissions/PermissionsRouter.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { useRouteParameter } from '../../../contexts/RouterContext';
 import UsersInRole from './UsersInRole';
 import PermissionsTable from './PermissionsTable';
+import { usePermission } from '../../../contexts/AuthorizationContext';
+import NotAuthorizedPage from '../../../components/NotAuthorizedPage';
 
 const PermissionsRouter = () => {
+	const canViewPermission = usePermission('access-setting-permissions');
 	const context = useRouteParameter('context');
+
+	if (!canViewPermission) {
+		return <NotAuthorizedPage />;
+	}
+
 	if (context === 'users-in-role') {
 		return <UsersInRole />;
 	}
