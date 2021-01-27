922d0935fc8d765e1aa214a539790f5555ed9162
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-01-27 13:03:34 -0300

    Fix review

--- client/components/MarkdownText.js
+++ client/components/MarkdownText.js
@@ -8,8 +8,10 @@ const renderer = new marked.Renderer();
 marked.InlineLexer.rules.gfm.strong = /^\*\*(?=\S)([\s\S]*?\S)\*\*(?!\*)|^\*(?=\S)([\s\S]*?\S)\*(?!\*)/;
 marked.InlineLexer.rules.gfm.em = /^__(?=\S)([\s\S]*?\S)__(?!_)|^_(?=\S)([\s\S]*?\S)_(?!_)/;
 
+const linkRenderer = renderer.link;
 renderer.link = function(href, title, text) {
-	return `<a target="_blank" href="${ href }">${ text }</a>`;
+	const html = linkRenderer.call(renderer, href, title, text);
+	return html.replace(/^<a /, '<a target="_blank" rel="nofollow" ');
 };
 
 const options = {
@@ -22,7 +24,7 @@ function MarkdownText({ content, preserveHtml = false, withRichContent = true, .
 	const sanitizer = dompurify.sanitize;
 	const __html = useMemo(() => {
 		const html = content && marked(content, options);
-		return preserveHtml ? html : html && sanitizer(html);
+		return preserveHtml ? html : html && sanitizer(html, { ADD_ATTR: ['target'] });
 	}, [content, preserveHtml, sanitizer]);
 	return __html ? <Box dangerouslySetInnerHTML={{ __html }} withRichContent={withRichContent} {...props} /> : null;
 }
