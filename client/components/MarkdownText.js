85c3110b86acca157222012718c8f477075aac3b
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-01-27 04:09:25 -0300

    click on the link open in new tab

--- client/components/MarkdownText.js
+++ client/components/MarkdownText.js
@@ -4,12 +4,19 @@ import marked from 'marked';
 
 import { escapeHTML } from '../../lib/escapeHTML';
 
+const renderer = new marked.Renderer();
+
 marked.InlineLexer.rules.gfm.strong = /^\*\*(?=\S)([\s\S]*?\S)\*\*(?!\*)|^\*(?=\S)([\s\S]*?\S)\*(?!\*)/;
 marked.InlineLexer.rules.gfm.em = /^__(?=\S)([\s\S]*?\S)__(?!_)|^_(?=\S)([\s\S]*?\S)_(?!_)/;
+
+renderer.link = function(href, title, text) {
+	return `<a target="_blank" href="${ href }">${ text }</a>`;
+};
 
 const options = {
 	gfm: true,
 	headerIds: false,
+	renderer,
 };
 
 function MarkdownText({ content, preserveHtml = false, withRichContent = true, ...props }) {
