74ac7e4db3fc48e1d2a3331b6b61702a794b9729
yash-rajpal <rajpal.yash03@gmail.com>
2021-01-28 22:15:58 +0530

    lint fixes

--- client/components/avatar/RoomAvatar.js
+++ client/components/avatar/RoomAvatar.js
@@ -2,11 +2,11 @@ import React, { memo } from 'react';
 
 import BaseAvatar from './BaseAvatar';
 import { useRoomAvatarPath } from '../../contexts/AvatarUrlContext';
-import { useUserRoom } from '../../../client/views/room/hooks/useUserRoom';
+import { useUserRoom } from '../../views/room/hooks/useUserRoom';
 
 function RoomAvatar({ room, ...rest }) {
 	const getRoomPathAvatar = useRoomAvatarPath();
-	const roomWithAvatarTag = useUserRoom(room._id)
+	const roomWithAvatarTag = useUserRoom(room._id);
 	const { url = getRoomPathAvatar(roomWithAvatarTag), ...props } = rest;
 	return <BaseAvatar url={url} {...props}/>;
 }
