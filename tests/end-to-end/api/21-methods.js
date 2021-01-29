b7f6e514939351463f6662d8b33ea9a343a14e1a
Pierre Lehnen <pierre.lehnen@rocket.chat>
2021-01-29 02:44:18 -0300

    Added basic loadMissedMessages tests

---
+++ tests/end-to-end/api/21-methods.js
@@ -0,0 +1,149 @@
+import { expect } from 'chai';
+
+import { getCredentials, request, methodCall, api, credentials, apiPrivateChannelName } from '../../data/api-data.js';
+
+describe('Meteor.methods', function() {
+	this.retries(0);
+
+	before((done) => getCredentials(done));
+
+	describe('[@loadMissedMessages]', () => {
+		let rid = false;
+		const date = {
+			$date: new Date().getTime(),
+		};
+
+		before('@loadMissedMessages', (done) => {
+			request.post(api('groups.create'))
+				.set(credentials)
+				.send({
+					name: apiPrivateChannelName,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.nested.property('group._id');
+					expect(res.body).to.have.nested.property('group.name', apiPrivateChannelName);
+					expect(res.body).to.have.nested.property('group.t', 'p');
+					expect(res.body).to.have.nested.property('group.msgs', 0);
+					rid = res.body.group._id;
+				})
+				.end(done);
+		});
+
+		before('@loadMissedMessages', (done) => {
+			request.post(api('chat.sendMessage'))
+				.set(credentials)
+				.send({
+					message: {
+						text: 'Sample message',
+						rid,
+					},
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				})
+				.end(done);
+		});
+
+		it('should fail if not logged in', (done) => {
+			request.post(methodCall('loadMissedMessages'))
+				.send({
+					message: JSON.stringify({
+						method: 'loadMissedMessages',
+						params: [rid, date],
+					}),
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(401)
+				.expect((res) => {
+					expect(res.body).to.have.property('status', 'error');
+					expect(res.body).to.have.property('message');
+				})
+				.end(done);
+		});
+
+		it('should return an error if the rid param is empty', (done) => {
+			request.post(methodCall('loadMissedMessages'))
+				.set(credentials)
+				.send({
+					message: JSON.stringify({
+						method: 'loadMissedMessages',
+						params: ['', date],
+					}),
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.a.property('success', true);
+					expect(res.body).to.have.a.property('message').that.include('error-invalid-room');
+				})
+				.end(done);
+		});
+
+		it('should return an error if the start param is missing', (done) => {
+			request.post(methodCall('loadMissedMessages'))
+				.set(credentials)
+				.send({
+					message: JSON.stringify({
+						method: 'loadMissedMessages',
+						params: [rid],
+					}),
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.a.property('success', true);
+					expect(res.body).to.have.a.property('message').that.include('Match error');
+				})
+				.end(done);
+		});
+
+		it('should return and empty list if using current time', (done) => {
+			request.post(methodCall('loadMissedMessages'))
+				.set(credentials)
+				.send({
+					message: JSON.stringify({
+						method: 'loadMissedMessages',
+						params: [rid, { $date: new Date().getTime() }],
+					}),
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.a.property('success', true);
+					expect(res.body).to.have.a.property('message').that.is.a('string');
+
+					const data = JSON.parse(res.body.message);
+					expect(data).to.have.a.property('result').that.is.a('array');
+					expect(data.result.length).to.be.equal(0);
+				})
+				.end(done);
+		});
+
+		it('should return a single message if using a time from before the message', (done) => {
+			request.post(methodCall('loadMissedMessages'))
+				.set(credentials)
+				.send({
+					message: JSON.stringify({
+						method: 'loadMissedMessages',
+						params: [rid, date],
+					}),
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.a.property('success', true);
+					expect(res.body).to.have.a.property('message').that.is.a('string');
+
+					const data = JSON.parse(res.body.message);
+					expect(data).to.have.a.property('result').that.is.a('array');
+					expect(data.result.length).to.be.equal(1);
+				})
+				.end(done);
+		});
+	});
+});
