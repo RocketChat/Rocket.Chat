47488689398aca0d216bd67e98d20009ab0b9daa
Réda Housni Alaoui <reda-alaoui@hey.com>
2021-01-30 10:12:50 +0100

    Fix merge conflict

---
+++ server/startup/migrations/v216.js
@@ -0,0 +1,40 @@
+import { Migrations } from '../../../app/migrations';
+import { Settings } from '../../../app/models';
+import { settings } from '../../../app/settings';
+
+Migrations.add({
+	version: 216,
+	up() {
+		Settings.find({
+			_id: /Accounts_OAuth_Custom-/,
+			i18nLabel: 'Accounts_OAuth_Custom_Enable',
+		}).forEach(function(customOauth) {
+			const [, name] = /^Accounts_OAuth_Custom-(.*?)$/.exec(customOauth._id);
+			const mapCustomFieldsId = `Accounts_OAuth_Custom-${ name }-map_custom_fields`;
+			if (!Settings.findOne({ _id: mapCustomFieldsId })) {
+				settings.add(mapCustomFieldsId, false, {
+					type: 'boolean',
+					group: 'OAuth',
+					section: `Custom OAuth: ${ name }`,
+					i18nLabel: 'Accounts_OAuth_Custom_Map_Custom_Fields',
+					persistent: true,
+					sorter: Settings.findOne({ _id: `Accounts_OAuth_Custom-${ name }-map_channels` }).sorter,
+				});
+			}
+			const customFieldsMap = `Accounts_OAuth_Custom-${ name }-custom_fields_map`;
+			if (!Settings.findOne({ _id: customFieldsMap })) {
+				settings.add(customFieldsMap, '{}', {
+					type: 'code',
+					multiline: true,
+					code: 'application/json',
+					group: 'OAuth',
+					section: `Custom OAuth: ${ name }`,
+					i18nLabel: 'Accounts_OAuth_Custom_Custom_Fields_Map',
+					i18nDescription: 'Accounts_OAuth_Custom_Custom_Fields_Map_Description',
+					persistent: true,
+					sorter: Settings.findOne({ _id: `Accounts_OAuth_Custom-${ name }-groups_channel_map` }).sorter,
+				});
+			}
+		});
+	},
+});
