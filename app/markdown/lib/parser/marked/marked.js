ed5fd620a9f2d62ce0c2d69786d4f93ea8151170
Karting06 <git@pvw.ovh>
2021-01-31 19:33:14 +0100

    Fix tables after marked linebreak regression

--- app/markdown/lib/parser/marked/marked.js
+++ app/markdown/lib/parser/marked/marked.js
@@ -10,8 +10,8 @@ import { unescapeHTML } from '../../../../../lib/unescapeHTML';
 const renderer = new _marked.Renderer();
 
 // change Marked default paragraph rules in order to have line breaks (line breaks are normally considered as a new paragraph)
-_marked.Lexer.rules.paragraph = new RegExp(_marked.Lexer.rules.paragraph.source.replace('+)*', '*)*'), '');
-_marked.Lexer.rules.gfm.paragraph = new RegExp(_marked.Lexer.rules.gfm.paragraph.source.replace('+)*', '*)*'), '');
+_marked.Lexer.rules.paragraph = new RegExp(_marked.Lexer.rules.paragraph.source.replace('[^\\n]+)*', '[^\\n|]*)*'), '');
+_marked.Lexer.rules.gfm.paragraph = new RegExp(_marked.Lexer.rules.gfm.paragraph.source.replace('[^\\n]+)*', '[^\\n|]*)*'), '');
 
 let msg = null;
 
