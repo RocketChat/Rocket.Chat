88e52f6e00f9b0b2d6f61cba759433ad4daa9acb
yash-rajpal <rajpal.yash03@gmail.com>
2021-02-02 17:50:11 +0530

    added markdown to UserInfo

--- client/views/room/contextualBar/UserInfo/index.js
+++ client/views/room/contextualBar/UserInfo/index.js
@@ -64,7 +64,7 @@ export const UserInfo = React.memo(function UserInfo({
 
 		<Margins block='x4'>
 			<UserCard.Username name={(showRealNames && name) || username || name} status={status} />
-			<Info>{customStatus}</Info>
+			<Info withTruncatedText={false}><MarkdownText content={customStatus} withRichContent={false}/></Info>
 
 			{!!roles && <>
 				<Label>{t('Roles')}</Label>
