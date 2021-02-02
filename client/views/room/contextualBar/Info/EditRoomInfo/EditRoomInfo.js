73db41bc039e184e1955b6573052531adaf87a57
Darshil Patel <55157259+Darshilp326@users.noreply.github.com>
2021-02-02 07:42:51 +0530

    [FIX]Selected hide system messages would now be viewed in vertical bar. (#20358)

--- client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.js
+++ client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.js
@@ -364,7 +364,7 @@ function EditChannel({ room, onClickClose, onClickBack }) {
 						</Field.Row>
 					</Box>
 					<Field.Row>
-						<MultiSelect options={sysMesOptions} disabled={!hideSysMes} value={systemMessages} onChange={handleSystemMessages} placeholder={t('Select_an_option')} flexGrow={1}/>
+						<MultiSelect maxWidth='100%' options={sysMesOptions} disabled={!hideSysMes} value={systemMessages} onChange={handleSystemMessages} placeholder={t('Select_an_option')} flexGrow={1}/>
 					</Field.Row>
 				</Field>}
 				{canViewEncrypted && <Field>
