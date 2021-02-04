43e80075cb485de27b3f974295fef026c700654b
Pierre Lehnen <pierre.lehnen@rocket.chat>
2021-02-04 00:26:56 -0300

    Update room's last message attribute when the import is done

--- app/importer-slack/server/importer.js
+++ app/importer-slack/server/importer.js
@@ -946,12 +946,21 @@ export class SlackImporter extends Base {
 				try {
 					if (this.channels) {
 						this._archiveChannelsAsNeeded(startedByUserId, this.channels);
+						this._updateRoomsLastMessage(this.channels);
 					}
+
 					if (this.groups) {
 						this._archiveChannelsAsNeeded(startedByUserId, this.groups);
+						this._updateRoomsLastMessage(this.groups);
 					}
+
 					if (this.mpims) {
 						this._archiveChannelsAsNeeded(startedByUserId, this.mpims);
+						this._updateRoomsLastMessage(this.mpims);
+					}
+
+					if (this.dms) {
+						this._updateRoomsLastMessage(this.dms);
 					}
 				} catch (e) {
 					// If it failed to archive some channel, it's no reason to flag the import as incomplete
@@ -978,6 +987,14 @@ export class SlackImporter extends Base {
 				Meteor.runAsUser(startedByUserId, function() {
 					Meteor.call('archiveRoom', channel.rocketId);
 				});
+			}
+		});
+	}
+
+	_updateRoomsLastMessage(list) {
+		list.channels.forEach((channel) => {
+			if (channel.do_import && channel.rocketId) {
+				Rooms.resetLastMessageById(channel.rocketId);
 			}
 		});
 	}
