4c45be6c0f7643b39fa24f672e85be9940ce2551
Douglas Fabris <deefabris@gmail.com>
2021-02-04 23:37:49 -0300

    [NEW] Header with Breadcrumbs (#20609)
    Co-authored-by: Gabriel Henriques <gabriel.henriques@rocket.chat>
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

--- client/hooks/useRoomIcon.tsx
+++ client/hooks/useRoomIcon.tsx
@@ -11,10 +11,14 @@ export const colors = {
 	offline: 'neutral-600',
 };
 
-export const useRoomIcon = (room: IRoom): JSX.Element | { name: string; color?: string } | null => {
+export const useRoomIcon = (room: IRoom, small = true): JSX.Element | { name: string; color?: string } | null => {
+	if (room.prid) {
+		return { name: 'baloons' };
+	}
+
 	switch (room.t) {
 		case 'p':
-			return { name: 'lock' };
+			return { name: 'hashtag-lock' };
 		case 'c':
 			return { name: 'hash' };
 		case 'l':
@@ -22,10 +26,10 @@ export const useRoomIcon = (room: IRoom): JSX.Element | { name: string; color?: 
 		case 'd':
 			const direct = room as unknown as IDirectMessageRoom;
 			if (direct.uids && direct.uids.length > 2) {
-				return { name: 'team' };
+				return { name: 'baloon-arrow-left' };
 			}
 			if (direct.uids && direct.uids.length > 0) {
-				return <ReactiveUserStatus { ...{ small: 'small', uid: direct.uids.filter((uid) => uid !== room.u._id)[0] || room.u._id } as any } />;
+				return <ReactiveUserStatus { ...{ small, uid: direct.uids.filter((uid) => uid !== room.u._id)[0] || room.u._id } as any } />;
 			}
 			return { name: 'at' };
 		default:
