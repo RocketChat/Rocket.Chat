5cce29241ccf5afa5e7820c9d55d7843fb0ae505
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-02-16 00:50:15 +0530

    lint fixes

--- client/components/UserTag.tsx
+++ client/components/UserTag.tsx
@@ -1,33 +1,39 @@
-import React, { memo } from "react";
-import { Box, Chip } from "@rocket.chat/fuselage";
-import { useMutableCallback } from "@rocket.chat/fuselage-hooks";
+import React, { memo, ReactNode } from 'react';
+import { Box, Chip } from '@rocket.chat/fuselage';
+import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 
-import UserAvatar from "./avatar/UserAvatar";
+import UserAvatar from './avatar/UserAvatar';
 
-interface UserDataProp {
-  name: string;
-  status: string;
-  username: string;
+interface IUserData {
+	name: string;
+	status: string;
+	username: string;
 }
 
-function Tag({ userData }: { userData: UserDataProp }) {
-  const { username } = userData;
-  
-  const onClickRemove = useMutableCallback(
-    (e: React.MouseEvent<HTMLOrSVGElement, MouseEvent>) => {
-      e.stopPropagation();
-      e.preventDefault();
-    }
-  );
+function Tag({ userData }: { userData: IUserData }): ReactNode {
+	const { username } = userData;
 
-  return (
-    <Chip key={username} height="x20" value={username} onClick={onClickRemove} mie="x4">
-      <UserAvatar size="x20" username={username} etag="" />
-      <Box is="span" margin="none" mis="x4" value={username}>
-        {username}
-      </Box>
-    </Chip>
-  );
+	const onClickRemove = useMutableCallback(
+		(e: React.MouseEvent<HTMLOrSVGElement, MouseEvent>) => {
+			e.stopPropagation();
+			e.preventDefault();
+		},
+	);
+
+	return (
+		<Chip
+			key={username}
+			height='x20'
+			value={username}
+			onClick={onClickRemove}
+			mie='x4'
+		>
+			<UserAvatar size='x20' username={username} etag='' />
+			<Box is='span' margin='none' mis='x4' value={username}>
+				{username}
+			</Box>
+		</Chip>
+	);
 }
 
 export default memo(Tag);
