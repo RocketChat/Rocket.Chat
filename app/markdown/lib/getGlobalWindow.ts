b60cce71ab267179a05924b4a10fc4dffa02c1e3
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-02-18 02:12:26 -0300

    Fix isomorphic import

--- app/markdown/lib/getGlobalWindow.ts
+++ app/markdown/lib/getGlobalWindow.ts
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-import { getGlobalWindow as getClientGlobalWindow } from '../client/getGlobalWindow';
-import { getGlobalWindow as getServerGlobalWindow } from '../server/getGlobalWindow';
-
-export const getGlobalWindow = Meteor.isServer ? getServerGlobalWindow : getClientGlobalWindow;
+export const { getGlobalWindow } = Meteor.isServer
+	? require('../server/getGlobalWindow')
+	: require('../client/getGlobalWindow');
