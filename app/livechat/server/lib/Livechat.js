7ff4e24f1577da36817da20d7277b77b76ceed3f
Rafael Ferreira <rafaelblink@gmail.com>
2021-02-18 16:00:27 -0300

    new system message for livechat chat started message.

--- app/livechat/server/lib/Livechat.js
+++ app/livechat/server/lib/Livechat.js
@@ -143,6 +143,12 @@ export const Livechat = {
 
 		if (newRoom) {
 			Messages.setRoomIdByToken(guest.token, room._id);
+			const message = {
+				t: 'livechat-started',
+				msg: '',
+				groupable: false,
+			};
+			sendMessage(guest, message, room);
 		}
 
 		return { room, newRoom };
@@ -961,7 +967,7 @@ export const Livechat = {
 		}
 
 		const showAgentInfo = settings.get('Livechat_show_agent_info');
-		const ignoredMessageTypes = ['livechat_navigation_history', 'livechat_transcript_history', 'command', 'livechat-close', 'livechat_video_call'];
+		const ignoredMessageTypes = ['livechat_navigation_history', 'livechat_transcript_history', 'command', 'livechat-close', 'livechat-started', 'livechat_video_call'];
 		const messages = Messages.findVisibleByRoomIdNotContainingTypes(rid, ignoredMessageTypes, { sort: { ts: 1 } });
 
 		let html = '<div> <hr>';
