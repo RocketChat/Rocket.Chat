23684c1923b295a3545d4644c277a6c698bfc5e1
Renato Becker <renato.augusto.becker@gmail.com>
2021-02-19 17:04:08 -0300

    Rename const

--- server/startup/migrations/v217.js
+++ server/startup/migrations/v217.js
@@ -4,8 +4,8 @@ import { Permissions } from '../../../app/models';
 Migrations.add({
 	version: 217,
 	up() {
-		const alreadyExists = Permissions.findOne('view-livechat-queue');
-		if (alreadyExists) {
+		const oldPermission = Permissions.findOne('view-livechat-queue');
+		if (oldPermission) {
 			Permissions.update({ _id: 'view-livechat-queue' }, { $addToSet: { roles: 'livechat-agent' } });
 		}
 	},
