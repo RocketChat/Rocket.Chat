7bc2ef469181e073019e4a9d0fac837e6a6f1a56
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-02-19 16:11:52 -0300

    [FIX] Remove warning problems from console (#20800)
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- client/views/room/contextualBar/Discussions/index.js
+++ client/views/room/contextualBar/Discussions/index.js
@@ -108,7 +108,9 @@ const Row = memo(function Row({
 	const { name = discussion.u.username } = discussion.u;
 
 	return <Discussion
-		{ ...discussion }
+		replies={discussion.replies}
+		dcount={discussion.dcount}
+		dlm={discussion.dlm}
 		name={showRealNames ? name : discussion.u.username }
 		username={ discussion.u.username }
 		following={discussion.replies && discussion.replies.includes(userId)}
