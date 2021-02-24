54b182c6776df3737ad49e90f35bf32dd7ca6840
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2021-02-24 21:38:04 +0530

    Add null message check

--- app/lib/server/methods/savePostProcessedMessage.js
+++ app/lib/server/methods/savePostProcessedMessage.js
@@ -5,6 +5,12 @@ import { Messages } from '../../../models';
 
 Meteor.methods({
     savePostProcessedMessage(_id, message) {
+        const originalMessage = Messages.findOneById(_id);
+
+		if (!originalMessage || !originalMessage._id) {
+			return;
+		}
+
         return Messages.updatePostProcessedPushMessageById(_id, message);
     }
 });
