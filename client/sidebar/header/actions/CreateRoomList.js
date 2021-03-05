bb60ed77043583f5a409ab14ca97ae5d3fa2ebfd
Martin <martin.schoeler@rocket.chat>
2021-03-05 17:14:01 -0300

    Separate create room list item

--- client/sidebar/header/actions/CreateRoomList.js
+++ client/sidebar/header/actions/CreateRoomList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Box, Flex, Margins } from '@rocket.chat/fuselage';
+import { Box, Margins } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 
 
@@ -9,29 +9,12 @@ import { useAtLeastOnePermission, usePermission } from '../../../contexts/Author
 import { useSetting } from '../../../contexts/SettingsContext';
 import { useSetModal } from '../../../contexts/ModalContext';
 import CreateChannel from '../CreateChannel';
+import CreateRoomListItem from './CreateRoomListItem';
 
 const CREATE_CHANNEL_PERMISSIONS = ['create-c', 'create-p'];
 
 const CREATE_DISCUSSION_PERMISSIONS = ['start-discussion', 'start-discussion-other-user'];
 
-function CreateRoomListItem({ text, icon, action }) {
-	return <Flex.Container>
-		<Box is='li' onClick={action}>
-			<Flex.Container>
-				<Box is='label' className='rc-popover__label' style={{ width: '100%' }}>
-					<Flex.Item grow={0}>
-						<Box className='rc-popover__icon'><Icon name={icon} size={20}/></Box>
-					</Flex.Item>
-					<Margins inline='x8'>
-						<Flex.Item grow={1}>
-							<Box is='span' fontScale='p2'>{text}</Box>
-						</Flex.Item>
-					</Margins>
-				</Box>
-			</Flex.Container>
-		</Box>
-	</Flex.Container>;
-}
 const style = {
 	textTransform: 'uppercase',
 };
