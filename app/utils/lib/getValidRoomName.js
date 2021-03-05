eb11254293e2c81d9bdb895bea76bfed28c036d5
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-03-05 13:46:01 -0300

    Add error messages to the edition of channels or usernames containing reserved words

--- app/utils/lib/getValidRoomName.js
+++ app/utils/lib/getValidRoomName.js
@@ -33,7 +33,7 @@ export const getValidRoomName = (displayName, rid = '', options = {}) => {
 		}
 	}
 
-	if (!nameValidation.test(slugifiedName)) {
+	if (!nameValidation.test(slugifiedName) || ['admin', 'administrator', 'system', 'user'].includes(slugifiedName.toLowerCase())) {
 		throw new Meteor.Error('error-invalid-room-name', `${ slugifiedName } is not a valid room name.`, {
 			function: 'RocketChat.getValidRoomName',
 			channel_name: slugifiedName,
