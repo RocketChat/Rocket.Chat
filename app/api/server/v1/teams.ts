fe8fd3dd39b7123c2bd7a0cc1051c360c5c17e52
Gabriel Thomé <gabrielallan.thome@gmail.com>
2021-03-10 12:13:54 -0300

    add teams.info endpoint

--- app/api/server/v1/teams.ts
+++ app/api/server/v1/teams.ts
@@ -60,6 +60,7 @@ API.v1.addRoute('teams.create', { authRequired: true }, {
 	},
 });
 
+<<<<<<< HEAD
 API.v1.addRoute('teams.members', { authRequired: true }, {
 	get() {
 		const { teamId } = this.queryParams;
@@ -71,5 +72,20 @@ API.v1.addRoute('teams.members', { authRequired: true }, {
 		const members = Promise.await(Team.members(this.userId, teamId));
 
 		return API.v1.success({ members });
+=======
+API.v1.addRoute('teams.info', { authRequired: true }, {
+	get() {
+		const { teamId, teamName } = this.requestParams();
+
+		if (!teamId && !teamName) {
+			return API.v1.failure('Provide either the "teamId" or "teamName"');
+		}
+
+		const teamInfo = teamId
+			? Promise.await(Team.getInfoById(teamId))
+			: Promise.await(Team.getInfoByName(teamName));
+
+		return API.v1.success({ teamInfo });
+>>>>>>> edbf42aa8 (add teams.info endpoint)
 	},
 });
