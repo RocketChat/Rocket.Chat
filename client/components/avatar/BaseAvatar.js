c069914993439a7d3db28c8aef54bfb49ead8f06
ABHAYJAIN2601 <51324264+ABHAYJAIN2601@users.noreply.github.com>
2021-03-11 22:34:24 +0530

    issue in profile avatar after selecting URL method

--- client/components/avatar/BaseAvatar.js
+++ client/components/avatar/BaseAvatar.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar, Skeleton } from '@rocket.chat/fuselage';
 
 function BaseAvatar(props) {
 	const [error, setError] = useState(false);
-
+	useEffect(() => { setError(false); }, [props.url]);
 	if (error) {
 		return <Skeleton variant='rect' {...props} />;
 	}
 
 	return <Avatar onError={setError} {...props}/>;
 }
-
 export default BaseAvatar;
