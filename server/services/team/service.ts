f95bbc6bde54eff680a9b617ef06eec2b90cf54e
Kevin Aleman <kevin.aleman@rocket.chat>
2021-03-16 16:31:19 -0600

    Fix lint issues

--- server/services/team/service.ts
+++ server/services/team/service.ts
@@ -442,11 +442,11 @@ export class TeamService extends ServiceClass implements ITeamService {
 			// at this point, users are already part of the team so we won't check for membership
 			for await (const user of users) {
 				// add each user to the default room
-				await addUserToRoom(room._id, user, inviter, false)
+				await addUserToRoom(room._id, user, inviter, false);
 			}
 		});
 	}
-	
+
 	async deleteById(teamId: string): Promise<boolean> {
 		return !!await this.TeamModel.deleteOneById(teamId);
 	}
