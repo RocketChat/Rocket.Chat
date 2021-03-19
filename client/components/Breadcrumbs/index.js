f4f8737d007e7d1368ae9d755cd9ed24084f7f9e
Martin <martin.schoeler@rocket.chat>
2021-03-19 20:07:56 -0300

    fix weight

--- client/components/Breadcrumbs/index.js
+++ client/components/Breadcrumbs/index.js
@@ -5,7 +5,7 @@ import { css } from '@rocket.chat/css-in-js';
 
 const BreadcrumbsSeparator = () => <Box display='inline-block' fontScale='s2' mi='x4' fontWeight='600' color='neutral-500'>/</Box>;
 const BreadcrumbsIcon = ({ name, color, children }) => <Box w='x20' mi='x2' display='inline-flex' justifyContent='center' color={color}>{name ? <Icon size='x20' name={name}/> : children}</Box>;
-const BreadcrumbsIconSmall = ({ name, color, children }) => <Box w='x12' display='inline-flex' justifyContent='center' color={color}>{name ? <Icon size='x12' name={name} fontWeight='inherit'/> : children}</Box>;
+const BreadcrumbsIconSmall = ({ name, color, children }) => <Box w='x12' display='inline-flex' justifyContent='center' color={color}>{name ? <Icon size='x12' name={name}/> : children}</Box>;
 
 const BreadcrumbsLink = (props) => <BreadcrumbsText
 	is='a'
