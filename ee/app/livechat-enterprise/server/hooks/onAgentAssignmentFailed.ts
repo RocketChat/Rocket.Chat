29830874a0c4bd908f88da9bf96ac0bd5525c868
Renato Becker <renato.augusto.becker@gmail.com>
2021-03-20 18:14:18 -0300

    Minor improvements.

--- ee/app/livechat-enterprise/server/hooks/onAgentAssignmentFailed.ts
+++ ee/app/livechat-enterprise/server/hooks/onAgentAssignmentFailed.ts
@@ -3,28 +3,23 @@ import { LivechatInquiry, Subscriptions, LivechatRooms } from '../../../../../ap
 import { queueInquiry } from '../../../../../app/livechat/server/lib/QueueManager';
 
 const handleOnAgentAssignmentFailed = async ({ inquiry, room }: { inquiry: any; room: any }): Promise<any> => {
-	if (!inquiry || !room) {
+	if (!inquiry || !room || !room.onHold) {
 		return;
 	}
 
-	const { onHold, _id: roomId, servedBy } = room;
-	if (!onHold) {
-		return;
-	}
-
-	const defaultAgent = { agentId: servedBy._id, username: servedBy._id };
+	const { _id: roomId, servedBy } = room;
 
 	const { _id: inquiryId } = inquiry;
 	LivechatInquiry.readyInquiry(inquiryId);
 	LivechatInquiry.removeDefaultAgentById(inquiryId);
 	LivechatRooms.removeAgentByRoomId(roomId);
-	if (defaultAgent.agentId) {
-		Subscriptions.removeByRoomIdAndUserId(roomId, defaultAgent.agentId);
+	if (servedBy?._id) {
+		Subscriptions.removeByRoomIdAndUserId(roomId, servedBy._id);
 	}
 
 	const newInquiry = LivechatInquiry.findOneById(inquiryId);
 
-	await queueInquiry(room, newInquiry, defaultAgent);
+	await queueInquiry(room, newInquiry);
 };
 
 callbacks.add('livechat.onAgentAssignmentFailed', handleOnAgentAssignmentFailed, callbacks.priority.HIGH, 'livechat-agent-assignment-failed');
