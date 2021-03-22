e65915e5e07d7c1a15ee9888b6fe063c9cf80af0
Jeanstaquet <jean.staquet8@gmail.com>
2021-03-22 14:11:27 +0100

    Correcting the issue 21241

--- client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.js
+++ client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.js
@@ -248,7 +248,6 @@ function EditChannel({ room, onClickClose, onClickBack }) {
 			rid: room._id,
 			...data,
 			...joinCode && { joinCode: joinCodeRequired ? joinCode : '' },
-			...(data.systemMessages || !hideSysMes) && { systemMessages: hideSysMes ? systemMessages : [] },
 		});
 
 		const archive = () => archiveAction({ rid: room._id, action: archiveSelector });
