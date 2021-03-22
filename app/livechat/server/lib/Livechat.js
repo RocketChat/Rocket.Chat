297dc068ce25bdf916bc549ccafe57ec771d6bd3
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2021-03-23 03:40:36 +0530

    [NEW][Enterprise] Omnichannel On-Hold Queue (#20945)
    * Add settings for on-hold feature
    
    * Add On-Hold Section within Sidebar
    
    * On hold room UI
    
    * OnHold - Automatic
    
    * Add Manual on-hold button UI
    
    * OnHold - manual
    
    - Start manual On-Hold timer after agent reply
    - Stop manual On-Hold timer on visitor message
    - Show manual On-Hold Option when timer expires
    
    * Handle manual On-Hold event
    
    * Add permissions to manual on-hold feature
    
    * [New] Auto-Close On hold chats
    
    * Routing chat when an on-hold get is resumed
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Add migration
    
    * Add new endpoint - livechat/placeChatOnHold
    
    * Move Resume Button login within livechatReadOnly file
    
    * Remove timeout on Manual On-Hold feature
    
    - From now on, the On-Hold option will appear within visitor Info panel, provided the agent has sent the last message
    
    * Apply suggestions from code review
    
    * Move resume On-Hold chat logic inside Queue Manager
    
    * Apply suggestions from code review
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>
    
    * Apply suggestions from code review
    
    * Use takeInquiry() to resume On-Hold chats
    
    * Fix failing test case
    
    * Minor improvements.
    
    * [Regression] Omnichannel On Hold feature - handle following impacted events
    
    - Returning chat to the queue
    - Forwarding chats
    - Closing chats
    
    * Fix failing test cases
    
    * Revert "[Regression] Omnichannel On hold Queue"
    
    * Prevent on hold chat from being returned or forwarded
    
    (cherry picked from commit 2edcb8234224ebad2b479282da3f9be6a5626db6)
    
    * Move checks to Livechat methods
    
    * Add releaseOnHoldChat() method
    
    * Fix callback returning promise.
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- app/livechat/server/lib/Livechat.js
+++ app/livechat/server/lib/Livechat.js
@@ -589,6 +589,10 @@ export const Livechat = {
 	},
 
 	async transfer(room, guest, transferData) {
+		if (room.onHold) {
+			throw new Meteor.Error('error-room-onHold', 'Room On Hold', { method: 'livechat:transfer' });
+		}
+
 		if (transferData.departmentId) {
 			transferData.department = LivechatDepartment.findOneById(transferData.departmentId, { fields: { name: 1 } });
 		}
@@ -604,6 +608,10 @@ export const Livechat = {
 
 		if (!room.open) {
 			throw new Meteor.Error('room-closed', 'Room closed', { method: 'livechat:returnRoomAsInquiry' });
+		}
+
+		if (room.onHold) {
+			throw new Meteor.Error('error-room-onHold', 'Room On Hold', { method: 'livechat:returnRoomAsInquiry' });
 		}
 
 		if (!room.servedBy) {
