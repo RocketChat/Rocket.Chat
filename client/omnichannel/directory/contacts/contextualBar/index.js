fa44029b573e123fac5ae2930496eb0cd692b0ae
Rafael Ferreira <rafaelblink@gmail.com>
2021-03-23 14:44:25 -0300

    show contact profile page.

--- client/omnichannel/directory/contacts/contextualBar/index.js
+++ client/omnichannel/directory/contacts/contextualBar/index.js
@@ -4,6 +4,8 @@ import { Icon, Box } from '@rocket.chat/fuselage';
 import VerticalBar from '../../../../components/VerticalBar';
 import { useRoute } from '../../../../contexts/RouterContext';
 import { useTranslation } from '../../../../contexts/TranslationContext';
+import { useRoom } from '../../../../views/room/providers/RoomProvider';
+import { ContactInfo } from './ContactInfo';
 
 const ContactsContextualBar = ({ id }) => {
 	const t = useTranslation();
@@ -13,11 +15,16 @@ const ContactsContextualBar = ({ id }) => {
 	const closeContextualBar = () => {
 		directoryRoute.push({ id });
 	};
+	const room = useRoom();
+
+	const { v: { _id } } = room;
+
 	return <>
 		<VerticalBar.Header>
 			<Box flexShrink={1} flexGrow={1} withTruncatedText mi='x8'><Icon name='user' size='x20' /> {t('Contact_Profile')}</Box>
 			<VerticalBar.Close onClick={closeContextualBar} />
 		</VerticalBar.Header>
+		<ContactInfo id={_id} />
 	</>;
 };
 
