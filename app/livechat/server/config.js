4f1b2c611332b308d93f53508d018d2937ab2bd4
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-03-24 14:16:36 +0530

    Add livechat-started/livechat-close in Hide Sys Messages Settings

--- app/livechat/server/config.js
+++ app/livechat/server/config.js
@@ -34,6 +34,8 @@ Meteor.startup(function() {
 		section: 'Livechat',
 		values: [
 			...MessageTypesValues,
+			{ key: 'livechat-started', i18nLabel: 'Message_HideType_livechat_started' },
+			{ key: 'livechat-close', i18nLabel: 'Message_HideType_livechat_close' },
 			{ key: 'livechat_transfer_history', i18nLabel: 'Message_HideType_livechat_transfer_history' },
 		],
 		public: true,
