ce2416963064dccf37f4834e1b181e83127f8c64
Diego Sampaio <chinello@gmail.com>
2021-03-26 18:46:21 -0300

    Bump version to 3.10.7

--- app/mailer/server/api.js
+++ app/mailer/server/api.js
@@ -8,6 +8,7 @@ import stripHtml from 'string-strip-html';
 
 import { settings } from '../../settings/server';
 import { escapeHTML } from '../../../lib/escapeHTML';
+import { replaceVariables } from './utils.js';
 
 let contentHeader;
 let contentFooter;
@@ -25,7 +26,7 @@ settings.get('Language', (key, value) => {
 });
 
 export const replacekey = (str, key, value = '') => str.replace(new RegExp(`(\\[${ key }\\]|__${ key }__)`, 'igm'), escapeHTML(value));
-export const translate = (str) => str.replace(/\{ ?([^\} ]+)(( ([^\}]+))+)? ?\}/gmi, (match, key) => TAPi18n.__(key, { lng }));
+export const translate = (str) => replaceVariables(str, (match, key) => TAPi18n.__(key, { lng }));
 export const replace = function replace(str, data = {}) {
 	if (!str) {
 		return '';
