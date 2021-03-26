df115b2c084462a97dc17e119fac20c1dc219222
Diego Sampaio <chinello@gmail.com>
2021-03-26 18:57:01 -0300

    Bump version to 3.8.9

--- app/emoji-custom/server/methods/uploadEmojiCustom.js
+++ app/emoji-custom/server/methods/uploadEmojiCustom.js
@@ -1,13 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import limax from 'limax';
+import sharp from 'sharp';
 
 import { hasPermission } from '../../../authorization';
 import { RocketChatFile } from '../../../file';
 import { RocketChatFileEmojiCustomInstance } from '../startup/emoji-custom';
 import { api } from '../../../../server/sdk/api';
 
+const getFile = async (file, extension) => {
+	if (extension !== 'svg+xml') {
+		return file;
+	}
+
+	return sharp(file)
+		.png()
+		.toBuffer();
+};
+
 Meteor.methods({
-	uploadEmojiCustom(binaryContent, contentType, emojiData) {
+	async uploadEmojiCustom(binaryContent, contentType, emojiData) {
 		if (!hasPermission(this.userId, 'manage-emoji')) {
 			throw new Meteor.Error('not_authorized');
 		}
@@ -15,7 +26,10 @@ Meteor.methods({
 		emojiData.name = limax(emojiData.name, { replacement: '_' });
 		// delete aliases for notification purposes. here, it is a string rather than an array
 		delete emojiData.aliases;
-		const file = new Buffer(binaryContent, 'binary');
+
+		const file = await getFile(Buffer.from(binaryContent, 'binary'), emojiData.extension);
+
+		emojiData.extension = emojiData.extension === 'svg+xml' ? 'png' : emojiData.extension;
 
 		const rs = RocketChatFile.bufferToStream(file);
 		RocketChatFileEmojiCustomInstance.deleteFile(encodeURIComponent(`${ emojiData.name }.${ emojiData.extension }`));
