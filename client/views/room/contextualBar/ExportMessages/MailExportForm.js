8bae492d64a81e1947048f10d0bac4e137c009e1
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-03-28 16:54:57 -0300

    Split some components

--- client/views/room/contextualBar/ExportMessages/MailExportForm.js
+++ client/views/room/contextualBar/ExportMessages/MailExportForm.js
@@ -2,7 +2,6 @@ import { css } from '@rocket.chat/css-in-js';
 import {
 	Field,
 	TextInput,
-	Select,
 	ButtonGroup,
 	Button,
 	Box,
@@ -11,103 +10,19 @@ import {
 	FieldGroup,
 } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { roomTypes, isEmail } from '../../../../../app/utils/client';
 import UserAutoCompleteMultiple from '../../../../../ee/client/audit/UserAutoCompleteMultiple';
-import VerticalBar from '../../../../components/VerticalBar';
 import { useEndpoint } from '../../../../contexts/ServerContext';
 import { useToastMessageDispatch } from '../../../../contexts/ToastMessagesContext';
 import { useTranslation } from '../../../../contexts/TranslationContext';
 import { useForm } from '../../../../hooks/useForm';
 import { useUserRoom } from '../../hooks/useUserRoom';
-import { useTabBarClose } from '../../providers/ToolboxProvider';
 
 const clickable = css`
 	cursor: pointer;
 `;
-
-const FileExport = ({ onCancel, rid }) => {
-	const t = useTranslation();
-
-	const { values, handlers } = useForm({
-		dateFrom: '',
-		dateTo: '',
-		format: 'html',
-	});
-
-	const { dateFrom, dateTo, format } = values;
-
-	const { handleDateFrom, handleDateTo, handleFormat } = handlers;
-
-	const outputOptions = useMemo(
-		() => [
-			['html', t('HTML')],
-			['json', t('JSON')],
-		],
-		[t],
-	);
-
-	const roomsExport = useEndpoint('POST', 'rooms.export');
-
-	const dispatchToastMessage = useToastMessageDispatch();
-
-	const handleSubmit = async () => {
-		try {
-			await roomsExport({
-				rid,
-				type: 'file',
-				...(dateFrom && { dateFrom: new Date(dateFrom) }),
-				...(dateTo && { dateTo: new Date(dateTo) }),
-				format,
-			});
-
-			dispatchToastMessage({
-				type: 'success',
-				message: t('Your_email_has_been_queued_for_sending'),
-			});
-		} catch (error) {
-			dispatchToastMessage({
-				type: 'error',
-				message: error,
-			});
-		}
-	};
-
-	return (
-		<FieldGroup>
-			<Field>
-				<Field.Label>{t('Date_From')}</Field.Label>
-				<Field.Row>
-					<TextInput type='date' value={dateFrom} onChange={handleDateFrom} />
-				</Field.Row>
-			</Field>
-			<Field>
-				<Field.Label>{t('Date_to')}</Field.Label>
-				<Field.Row>
-					<TextInput type='date' value={dateTo} onChange={handleDateTo} />
-				</Field.Row>
-			</Field>
-			<Field>
-				<Field.Label>{t('Output_format')}</Field.Label>
-				<Field.Row>
-					<Select
-						value={format}
-						onChange={handleFormat}
-						placeholder={t('Format')}
-						options={outputOptions}
-					/>
-				</Field.Row>
-			</Field>
-			<ButtonGroup stretch mb='x12'>
-				<Button onClick={onCancel}>{t('Cancel')}</Button>
-				<Button primary onClick={() => handleSubmit()}>
-					{t('Export')}
-				</Button>
-			</ButtonGroup>
-		</FieldGroup>
-	);
-};
 
 const MailExportForm = ({ onCancel, rid }) => {
 	const t = useTranslation();
@@ -272,45 +187,4 @@ const MailExportForm = ({ onCancel, rid }) => {
 	);
 };
 
-export const ExportMessages = function ExportMessages({ rid }) {
-	const t = useTranslation();
-	const close = useTabBarClose();
-
-	const [type, setType] = useState('email');
-
-	const exportOptions = useMemo(
-		() => [
-			['email', t('Send_via_email')],
-			['file', t('Export_as_file')],
-		],
-		[t],
-	);
-
-	return (
-		<>
-			<VerticalBar.Header>
-				{t('Export_Messages')}
-				<VerticalBar.Close onClick={close} />
-			</VerticalBar.Header>
-			<VerticalBar.ScrollableContent>
-				<FieldGroup>
-					<Field>
-						<Field.Label>{t('Method')}</Field.Label>
-						<Field.Row>
-							<Select
-								value={type}
-								onChange={(value) => setType(value)}
-								placeholder={t('Type')}
-								options={exportOptions}
-							/>
-						</Field.Row>
-					</Field>
-				</FieldGroup>
-				{type && type === 'file' && <FileExport rid={rid} onCancel={close} />}
-				{type && type === 'email' && <MailExportForm rid={rid} onCancel={close} />}
-			</VerticalBar.ScrollableContent>
-		</>
-	);
-};
-
-export default ExportMessages;
+export default MailExportForm;
