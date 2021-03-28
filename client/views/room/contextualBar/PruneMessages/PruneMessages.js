4ae0883b0ae47ca8fd3b68113d0b90d9cb26cf19
shrinish123 <56491104+shrinish123@users.noreply.github.com>
2021-03-28 08:17:12 +0530

    lint error fixes

--- client/views/room/contextualBar/PruneMessages/PruneMessages.js
+++ client/views/room/contextualBar/PruneMessages/PruneMessages.js
@@ -219,11 +219,11 @@ export default ({
 				throw new Error(t('No_messages_found_to_prune'));
 			}
 			if (result === 1) {
-				dispatchToastMessage({ type: 'success', message: `${ result } ${ t('message_pruned') }` });
+				dispatchToastMessage({ type: 'success', message: `${ result } ${ t('msg_pruned') }` });
 			} else {
 				dispatchToastMessage({ type: 'success', message: `${ result } ${ t('messages_pruned') }` });
 			}
-			
+
 			closeModal();
 			reset();
 		} catch (error) {
