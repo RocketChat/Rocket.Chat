1eebcded185eb81c5d4822b8d6347bb32b44beb9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-03-28 00:37:46 -0300

    Split some components

--- client/views/omnichannel/agents/AgentsRoute.js
+++ client/views/omnichannel/agents/AgentsRoute.js
@@ -1,115 +1,20 @@
-import { Box, Table, Icon, Button } from '@rocket.chat/fuselage';
+import { Box, Table } from '@rocket.chat/fuselage';
 import { useDebouncedValue, useMediaQuery, useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import React, { useMemo, useCallback, useState } from 'react';
 
-import DeleteWarningModal from '../../../components/DeleteWarningModal';
 import GenericTable from '../../../components/GenericTable';
 import NotAuthorizedPage from '../../../components/NotAuthorizedPage';
 import VerticalBar from '../../../components/VerticalBar';
 import UserAvatar from '../../../components/avatar/UserAvatar';
 import { usePermission } from '../../../contexts/AuthorizationContext';
-import { useSetModal } from '../../../contexts/ModalContext';
 import { useRouteParameter, useRoute } from '../../../contexts/RouterContext';
-import { useToastMessageDispatch } from '../../../contexts/ToastMessagesContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
-import { useEndpointAction } from '../../../hooks/useEndpointAction';
 import { useEndpointData } from '../../../hooks/useEndpointData';
 import AgentEditWithData from './AgentEditWithData';
 import AgentInfo from './AgentInfo';
+import AgentInfoActions from './AgentInfoActions';
 import AgentsPage from './AgentsPage';
-
-export function RemoveAgentButton({ _id, reload }) {
-	const deleteAction = useEndpointAction('DELETE', `livechat/users/agent/${_id}`);
-	const setModal = useSetModal();
-	const dispatchToastMessage = useToastMessageDispatch();
-	const t = useTranslation();
-
-	const handleRemoveClick = useMutableCallback(async () => {
-		const result = await deleteAction();
-		if (result.success === true) {
-			reload();
-		}
-	});
-
-	const handleDelete = useMutableCallback((e) => {
-		e.stopPropagation();
-		const onDeleteAgent = async () => {
-			try {
-				await handleRemoveClick();
-				dispatchToastMessage({ type: 'success', message: t('Agent_removed') });
-			} catch (error) {
-				dispatchToastMessage({ type: 'error', message: error });
-			}
-			setModal();
-		};
-
-		setModal(<DeleteWarningModal onDelete={onDeleteAgent} onCancel={() => setModal()} />);
-	});
-
-	return (
-		<Table.Cell fontScale='p1' color='hint' withTruncatedText>
-			<Button small ghost title={t('Remove')} onClick={handleDelete}>
-				<Icon name='trash' size='x16' />
-			</Button>
-		</Table.Cell>
-	);
-}
-
-export function AgentInfoActions({ reload }) {
-	const t = useTranslation();
-	const _id = useRouteParameter('id');
-	const agentsRoute = useRoute('omnichannel-agents');
-	const deleteAction = useEndpointAction('DELETE', `livechat/users/agent/${_id}`);
-	const setModal = useSetModal();
-	const dispatchToastMessage = useToastMessageDispatch();
-
-	const handleRemoveClick = useMutableCallback(async () => {
-		const result = await deleteAction();
-		if (result.success === true) {
-			agentsRoute.push({});
-			reload();
-		}
-	});
-
-	const handleDelete = useMutableCallback((e) => {
-		e.stopPropagation();
-		const onDeleteAgent = async () => {
-			try {
-				await handleRemoveClick();
-				dispatchToastMessage({ type: 'success', message: t('Agent_removed') });
-			} catch (error) {
-				dispatchToastMessage({ type: 'error', message: error });
-			}
-			setModal();
-		};
-
-		setModal(<DeleteWarningModal onDelete={onDeleteAgent} onCancel={() => setModal()} />);
-	});
-
-	const handleEditClick = useMutableCallback(() =>
-		agentsRoute.push({
-			context: 'edit',
-			id: _id,
-		}),
-	);
-
-	return [
-		<AgentInfo.Action
-			key={t('Remove')}
-			title={t('Remove')}
-			label={t('Remove')}
-			onClick={handleDelete}
-			icon={'trash'}
-		/>,
-		<AgentInfo.Action
-			key={t('Edit')}
-			title={t('Edit')}
-			label={t('Edit')}
-			onClick={handleEditClick}
-			icon={'edit'}
-		/>,
-	];
-}
+import RemoveAgentButton from './RemoveAgentButton';
 
 const sortDir = (sortDir) => (sortDir === 'asc' ? 1 : -1);
 
