23d92cf58affc139a44bc146d631bf2f2026530d
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-03-28 03:18:21 -0300

    Split some components

--- client/views/admin/permissions/PermissionsTable.js
+++ client/views/admin/permissions/PermissionsTable.js
@@ -1,47 +1,21 @@
-import { css } from '@rocket.chat/css-in-js';
-import {
-	TextInput,
-	Table,
-	Margins,
-	Box,
-	Icon,
-	CheckBox,
-	Throbber,
-	Tabs,
-	Button,
-} from '@rocket.chat/fuselage';
-import { useMutableCallback, useDebouncedValue } from '@rocket.chat/fuselage-hooks';
-import React, { useState, useCallback, useEffect, memo } from 'react';
+import { Margins, Icon, Tabs, Button } from '@rocket.chat/fuselage';
+import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
+import React, { useState, useCallback } from 'react';
 
 import { ChatPermissions } from '../../../../app/authorization/client/lib/ChatPermissions';
-import { CONSTANTS, AuthorizationUtils } from '../../../../app/authorization/lib';
+import { CONSTANTS } from '../../../../app/authorization/lib';
 import { Roles } from '../../../../app/models/client';
 import GenericTable from '../../../components/GenericTable';
 import Page from '../../../components/Page';
 import { usePermission } from '../../../contexts/AuthorizationContext';
 import { useRoute } from '../../../contexts/RouterContext';
 import { useMethod } from '../../../contexts/ServerContext';
-import { useToastMessageDispatch } from '../../../contexts/ToastMessagesContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useReactiveValue } from '../../../hooks/useReactiveValue';
+import FilterComponent from './FilterComponent';
+import PermissionRow from './PermissionRow';
 import PermissionsContextBar from './PermissionsContextBar';
-
-const useChangeRole = ({ onGrant, onRemove, permissionId }) => {
-	const dispatchToastMessage = useToastMessageDispatch();
-	return useMutableCallback(async (roleId, granted) => {
-		try {
-			if (granted) {
-				await onRemove(permissionId, roleId);
-			} else {
-				await onGrant(permissionId, roleId);
-			}
-			return !granted;
-		} catch (error) {
-			dispatchToastMessage({ type: 'error', message: error });
-		}
-		return granted;
-	});
-};
+import RoleHeader from './RoleHeader';
 
 const usePermissionsAndRoles = (type = 'permissions', filter = '', limit = 25, skip = 0) => {
 	const getPermissions = useCallback(() => {
@@ -69,141 +43,6 @@ const usePermissionsAndRoles = (type = 'permissions', filter = '', limit = 25, s
 	const roles = useReactiveValue(getRoles);
 
 	return [permissions.fetch(), permissions.count(false), roles];
-};
-
-const RoleCell = memo(
-	({ grantedRoles = [], _id, description, onChange, lineHovered, permissionId }) => {
-		const [granted, setGranted] = useState(() => !!grantedRoles.includes(_id));
-		const [loading, setLoading] = useState(false);
-
-		const isRestrictedForRole = AuthorizationUtils.isPermissionRestrictedForRole(permissionId, _id);
-
-		const handleChange = useMutableCallback(async () => {
-			setLoading(true);
-			const result = await onChange(_id, granted);
-			setGranted(result);
-			setLoading(false);
-		});
-
-		const isDisabled = !!loading || !!isRestrictedForRole;
-
-		return (
-			<Table.Cell withTruncatedText>
-				<Margins inline='x2'>
-					<CheckBox checked={granted} onChange={handleChange} disabled={isDisabled} />
-					{!loading && (
-						<Box display='inline' color='hint' invisible={!lineHovered}>
-							{description || _id}
-						</Box>
-					)}
-					{loading && <Throbber size='x12' display='inline-block' />}
-				</Margins>
-			</Table.Cell>
-		);
-	},
-);
-
-const getName = (t, permission) => {
-	if (permission.level === CONSTANTS.SETTINGS_LEVEL) {
-		let path = '';
-		if (permission.group) {
-			path = `${t(permission.group)} > `;
-		}
-		if (permission.section) {
-			path = `${path}${t(permission.section)} > `;
-		}
-		return `${path}${t(permission.settingId)}`;
-	}
-
-	return t(permission._id);
-};
-
-const PermissionRow = memo(({ permission, t, roleList, onGrant, onRemove, ...props }) => {
-	const { _id, roles } = permission;
-
-	const [hovered, setHovered] = useState(false);
-
-	const onMouseEnter = useMutableCallback(() => setHovered(true));
-	const onMouseLeave = useMutableCallback(() => setHovered(false));
-
-	const changeRole = useChangeRole({ onGrant, onRemove, permissionId: _id });
-	return (
-		<Table.Row
-			key={_id}
-			role='link'
-			action
-			tabIndex={0}
-			onMouseEnter={onMouseEnter}
-			onMouseLeave={onMouseLeave}
-			{...props}
-		>
-			<Table.Cell maxWidth='x300' withTruncatedText title={t(`${_id}_description`)}>
-				{getName(t, permission)}
-			</Table.Cell>
-			{roleList.map(({ _id, description }) => (
-				<RoleCell
-					key={_id}
-					_id={_id}
-					description={description}
-					grantedRoles={roles}
-					onChange={changeRole}
-					lineHovered={hovered}
-					permissionId={_id}
-				/>
-			))}
-		</Table.Row>
-	);
-});
-
-const RoleHeader = memo(({ router, _id, description, ...props }) => {
-	const onClick = useMutableCallback(() => {
-		router.push({
-			context: 'edit',
-			_id,
-		});
-	});
-
-	return (
-		<GenericTable.HeaderCell clickable pi='x4' p='x8' {...props}>
-			<Box
-				className={css`
-					white-space: nowrap;
-				`}
-				pb='x8'
-				pi='x12'
-				mi='neg-x2'
-				borderStyle='solid'
-				borderWidth='x2'
-				borderRadius='x2'
-				borderColor='neutral-300'
-				onClick={onClick}
-			>
-				<Margins inline='x2'>
-					<span>{description || _id}</span>
-					<Icon name='edit' size='x16' />
-				</Margins>
-			</Box>
-		</GenericTable.HeaderCell>
-	);
-});
-
-const FilterComponent = ({ onChange }) => {
-	const t = useTranslation();
-	const [filter, setFilter] = useState('');
-
-	const debouncedFilter = useDebouncedValue(filter, 500);
-
-	useEffect(() => {
-		onChange(debouncedFilter);
-	}, [debouncedFilter, onChange]);
-
-	const handleFilter = useMutableCallback(({ currentTarget: { value } }) => {
-		setFilter(value);
-	});
-
-	return (
-		<TextInput value={filter} onChange={handleFilter} placeholder={t('Search')} flexGrow={0} />
-	);
 };
 
 const PermissionsTable = () => {
