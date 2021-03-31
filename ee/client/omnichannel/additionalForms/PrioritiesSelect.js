7a2188e0677476ec42f8a3f1f27ede7f769678ca
Rafael Ferreira <rafaelblink@gmail.com>
2021-03-31 16:12:56 -0300

    Regression: Room Edit form not rendering priority and custom fields (#21309)
    * show custom fields at chat info page and fix some fields at chat room edit page
    
    * Enterprise property field at Chat edit form.
    
    * show OS and browser info
    
    * Save button available with changes on fields at Edit Room Form.
    
    * Contact page button save only available with custom fields changes.
    
    * Chat room form name isn't required
    
    * check if a EE component.
    
    * livechatData removed after merge
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

---
+++ ee/client/omnichannel/additionalForms/PrioritiesSelect.js
@@ -0,0 +1,15 @@
+import React, { useMemo } from 'react';
+import { Field, Select } from '@rocket.chat/fuselage';
+
+export const PrioritiesSelect = ({ options, value, handler, label }) => {
+	const optionsSelect = useMemo(() => (options && options.length > 0) && options.map((option) => [option._id, option.name], [options]));
+
+	return <Field>
+		<Field.Label>{label}</Field.Label>
+		<Field.Row>
+			<Select value={value} onChange={handler} options={optionsSelect} />
+		</Field.Row>
+	</Field>;
+};
+
+export default PrioritiesSelect;
