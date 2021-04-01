959d8b38b39cc4b55cc15ed4e93240215c086901
Douglas Fabris <deefabris@gmail.com>
2021-04-01 11:47:09 -0300

    Regression: Headers icon breaking DMs (#21412)

--- client/views/room/Header/Header.js
+++ client/views/room/Header/Header.js
@@ -38,7 +38,7 @@ const HeaderIcon = ({ room }) => {
 	const icon = useRoomIcon(room);
 
 	return <Box w='x20' mi='x2' display='inline-flex' justifyContent='center'>
-		{icon.name ? <Icon size='x20' {...icon}/> : Icon}
+		{icon.name ? <Icon size='x20' {...icon}/> : icon}
 	</Box>;
 };
 
