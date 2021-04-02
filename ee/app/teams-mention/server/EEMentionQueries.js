0a80b90984ccefee2daecc8a55a685776a414ba7
Diego Sampaio <chinello@gmail.com>
2021-04-01 21:24:52 -0300

    Remove underscore

--- ee/app/teams-mention/server/EEMentionQueries.js
+++ ee/app/teams-mention/server/EEMentionQueries.js
@@ -1,13 +1,11 @@
-import { _ } from 'meteor/underscore';
-
 import { Team } from '../../../../server/sdk';
 
 export const MentionQueriesEnterprise = {
 	getUsers(sup, usernames) {
-		const uniqueUsernames = _.unique(usernames);
+		const uniqueUsernames = [...new Set(usernames)];
 		const teams = Promise.await(Team.listByNames(uniqueUsernames), { projection: { _id: 1, name: 1 } });
 
-		if (!teams || !teams.length) {
+		if (!teams?.length) {
 			return sup(usernames);
 		}
 
