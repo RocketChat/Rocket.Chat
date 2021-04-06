7a4b3d9088621cc0344023f9ae1c5cc5fca37d2e
Rafael Ferreira <rafaelblink@gmail.com>
2021-04-06 14:43:43 -0300

    [FIX] Wrong useMemo on Priorities EE field. (#21453)
    * remove useless memo from priorities field.
    
    * undo e fix the deps array
    
    Co-authored-by: Renato Becker <renato.augusto.becker@gmail.com>

--- ee/client/omnichannel/additionalForms/PrioritiesSelect.js
+++ ee/client/omnichannel/additionalForms/PrioritiesSelect.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { Field, Select } from '@rocket.chat/fuselage';
 
 export const PrioritiesSelect = ({ options, value, handler, label }) => {
-	const optionsSelect = useMemo(() => (options && options.length > 0) && options.map((option) => [option._id, option.name], [options]));
+	const optionsSelect = useMemo(() => (options && options.length > 0) && options.map((option) => [option._id, option.name]), [options]);
 
 	return <Field>
 		<Field.Label>{label}</Field.Label>
