d8a24b044ebe0a5d93ab2dc1e16d101be6552146
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-04-08 01:21:48 -0300

    [NEW] New set of rules for client code (#21318)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

---
+++ client/components/UserAutoComplete/UserAutoComplete.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import UserAutoComplete from '.';
+
+export default {
+	title: 'components/UserAutoComplete',
+	component: UserAutoComplete,
+};
+
+export const Example = () => <UserAutoComplete />;
