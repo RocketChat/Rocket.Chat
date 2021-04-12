84d547055ea1ce08e55d7a95ed99dd9c0be852b9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-04-12 15:21:13 -0300

    A React-based replacement for BlazeLayout (#21527)

--- client/lib/createRouteGroup.ts
+++ client/lib/createRouteGroup.ts
@@ -1,50 +1,74 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import type { ComponentType } from 'react';
+import { Tracker } from 'meteor/tracker';
+import { ComponentType, createElement, lazy, ReactNode } from 'react';
 
-import { renderRouteComponent } from './portals/renderRouteComponent';
+import { appLayout } from './appLayout';
+import { createTemplateForComponent } from './portals/createTemplateForComponent';
 
 type RouteRegister = {
 	(
 		path: string,
-		params: {
-			name: string;
-			lazyRouteComponent: () => Promise<ComponentType>;
-			props: Record<string, unknown>;
-			action: (params?: Record<string, string>, queryParams?: Record<string, string>) => void;
-		},
+		params: Parameters<typeof FlowRouter.route>[1] &
+			(
+				| {}
+				| {
+						lazyRouteComponent: () => Promise<{ default: ComponentType }>;
+						props: Record<string, unknown>;
+				  }
+			),
 	): void;
 };
 
 export const createRouteGroup = (
 	name: string,
 	prefix: string,
-	importRouter: () => Promise<{ default: ComponentType }>,
+	importRouter: () => Promise<{
+		default: ComponentType<{
+			renderRoute?: () => ReactNode;
+		}>;
+	}>,
 ): RouteRegister => {
 	const routeGroup = FlowRouter.group({
 		name,
 		prefix,
 	});
 
-	const registerRoute: RouteRegister = (
-		path,
-		{ lazyRouteComponent, props, action, ...options },
-	) => {
-		routeGroup.route(path, {
-			...options,
-			action: (params, queryParams) => {
-				if (action) {
-					action(params, queryParams);
-					return;
-				}
+	const registerRoute: RouteRegister = (path, options) => {
+		if ('lazyRouteComponent' in options) {
+			const { lazyRouteComponent, props, ...rest } = options;
 
-				renderRouteComponent(importRouter, {
-					template: 'main',
-					region: 'center',
-					propsFn: () => ({ lazyRouteComponent, ...options, params, queryParams, ...props }),
-				});
-			},
-		});
+			const RouteComponent = lazy(lazyRouteComponent);
+			const renderRoute = (): ReactNode => createElement(RouteComponent, props);
+
+			routeGroup.route(path, {
+				...rest,
+				action() {
+					const center = createTemplateForComponent(
+						Tracker.nonreactive(() => FlowRouter.getRouteName()),
+						importRouter,
+						{
+							attachment: 'at-parent',
+							props: () => ({ renderRoute }),
+						},
+					);
+					appLayout.render('main', { center });
+				},
+			});
+			return;
+		}
+
+		routeGroup.route(path, options);
 	};
+
+	registerRoute('/', {
+		name: `${name}-index`,
+		action() {
+			const center = createTemplateForComponent(`${name}-index`, importRouter, {
+				attachment: 'at-parent',
+			});
+			appLayout.render('main', { center });
+		},
+	});
 
 	return registerRoute;
 };
