0853ae114a3c368655afcfb901765d617b342457
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-15 18:59:23 -0300

    use class instead color component

--- client/sidebar/RoomList/SidebarIcon.js
+++ client/sidebar/RoomList/SidebarIcon.js
@@ -5,8 +5,16 @@ import { useRoomIcon } from '../../hooks/useRoomIcon';
 
 const SidebarIcon = ({ room, small }) => {
 	const icon = useRoomIcon(room, small);
+	const unread = !room.hideUnreadStatus && (room.alert || room.unread);
 
-	return <Sidebar.Item.Icon {...(icon.name && icon)}>{!icon.name && icon}</Sidebar.Item.Icon>;
+	return (
+		<Sidebar.Item.Icon
+			className={unread && 'rcx-sidebar-item--highlighted'}
+			{...(icon.name && icon)}
+		>
+			{!icon.name && icon}
+		</Sidebar.Item.Icon>
+	);
 };
 
 export default SidebarIcon;
