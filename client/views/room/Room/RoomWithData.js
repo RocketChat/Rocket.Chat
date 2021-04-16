b226da3849212b3105dfedf36df77a325f0d27d5
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-04-16 08:00:46 -0300

    Regression: React + Blaze  reconciliation (#21567)

--- client/views/room/Room/RoomWithData.js
+++ client/views/room/Room/RoomWithData.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { useOpenedRoom } from '../../../lib/RoomManager';
 import RoomProvider from '../providers/RoomProvider';
 import Room from './Room';
 
-const RoomWithData = ({ _id }) => (
-	<RoomProvider rid={_id}>
-		<Room />
-	</RoomProvider>
-);
+const RoomWithData = () => {
+	const rid = useOpenedRoom();
+	return rid ? (
+		<RoomProvider rid={rid}>
+			<Room />
+		</RoomProvider>
+	) : null;
+};
 
 export default RoomWithData;
