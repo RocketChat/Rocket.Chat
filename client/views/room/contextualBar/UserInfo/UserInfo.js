ad63b12282d935f9767aabf131a1e9e4f1cc812e
Jeanstaquet <jean.staquet8@gmail.com>
2021-04-20 05:46:12 -0700

    Adding the fix

--- client/views/room/contextualBar/UserInfo/UserInfo.js
+++ client/views/room/contextualBar/UserInfo/UserInfo.js
@@ -50,7 +50,7 @@ function UserInfo({
 				</InfoPanel.Section>
 
 				<InfoPanel.Section>
-					{!!roles && (
+					{!!roles && roles[0] && (
 						<InfoPanel.Field>
 							<InfoPanel.Label>{t('Roles')}</InfoPanel.Label>
 							<UserCard.Roles>{roles}</UserCard.Roles>
