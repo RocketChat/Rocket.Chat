1ca049655305831dcb471aa80337df2ed74b93ac
Rafael <rafaelblink@gmail.com>
2021-04-20 18:32:24 -0300

    undo useless negation

--- client/views/room/Header/Omnichannel/OmnichannelRoomHeader.tsx
+++ client/views/room/Header/Omnichannel/OmnichannelRoomHeader.tsx
@@ -18,7 +18,7 @@ const OmnichannelRoomHeader: FC<RoomHeaderProps> = ({ room, slots: parentSlot })
 				!!isMobile ||
 				(name === 'omnichannel-directory' && (
 					<TemplateHeader.ToolBox>
-						{!!isMobile && <Burger />}
+						{isMobile && <Burger />}
 						{name === 'omnichannel-directory' && <BackButton />}
 					</TemplateHeader.ToolBox>
 				)),
