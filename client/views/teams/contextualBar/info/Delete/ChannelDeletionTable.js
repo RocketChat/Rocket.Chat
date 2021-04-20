53efd7ca8d90a6de231b5a49aded5e743b81e166
Sumukha Hegde <sumukha214@gmail.com>
2021-04-20 18:48:52 +0530

    [FIX] Add channel sorting functionalities in Teams

--- client/views/teams/contextualBar/info/Delete/ChannelDeletionTable.js
+++ client/views/teams/contextualBar/info/Delete/ChannelDeletionTable.js
@@ -1,5 +1,5 @@
 import { Box, CheckBox } from '@rocket.chat/fuselage';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import GenericTable from '../../../../../components/GenericTable';
 import { useTranslation } from '../../../../../contexts/TranslationContext';
@@ -13,9 +13,38 @@ const ChannelDeletionTable = ({
 	selectedRooms,
 	onToggleAllRooms,
 }) => {
+	const [sort, setSort] = useState(['name', 'asc']);
 	const t = useTranslation();
 
 	const selectedRoomsLength = Object.values(selectedRooms).filter(Boolean).length;
+
+	const onHeaderClick = useCallback(
+		(id) => {
+			const [sortBy, sortDirection] = sort;
+			if (sortBy === id) {
+				setSort([id, sortDirection === 'asc' ? 'desc' : 'asc']);
+				return;
+			}
+			setSort([id, 'asc']);
+		},
+		[sort],
+	);
+
+	const getSortedChannels = () => {
+		let sortedRooms = rooms;
+		if (sort[0] === 'name') {
+			sortedRooms = rooms.sort((a, b) => {
+				if (a.name < b.name) return -1;
+				return a.name > b.name ? 1 : 0;
+			});
+		} else if (sort[0] === 'usersCount') {
+			sortedRooms = rooms.sort((a, b) => a.usersCount - b.usersCount);
+		}
+		if (sort[1] === 'asc') {
+			return sortedRooms.reverse();
+		}
+		return sortedRooms;
+	};
 
 	const checked = rooms.length === selectedRoomsLength;
 	const indeterminate = rooms.length > selectedRoomsLength && selectedRoomsLength > 0;
@@ -25,7 +54,13 @@ const ChannelDeletionTable = ({
 			<GenericTable
 				header={
 					<>
-						<GenericTable.HeaderCell key='name' sort='name'>
+						<GenericTable.HeaderCell
+							key='name'
+							sort='name'
+							onClick={onHeaderClick}
+							direction={sort[1]}
+							active={sort[0] === 'name'}
+						>
 							<CheckBox
 								indeterminate={indeterminate}
 								checked={checked}
@@ -33,14 +68,20 @@ const ChannelDeletionTable = ({
 							/>
 							<Box mi='x8'>{t('Channel_name')}</Box>
 						</GenericTable.HeaderCell>
-						<GenericTable.HeaderCell key='usersCount' sort='usersCount'>
+						<GenericTable.HeaderCell
+							key='usersCount'
+							sort='usersCount'
+							onClick={onHeaderClick}
+							direction={sort[1]}
+							active={sort[0] === 'usersCount'}
+						>
 							<Box width='100%' textAlign='end'>
 								{t('Members')}
 							</Box>
 						</GenericTable.HeaderCell>
 					</>
 				}
-				results={rooms}
+				results={getSortedChannels()}
 				params={params}
 				setParams={onChangeParams}
 				fixed={false}
