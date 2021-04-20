561b058010519de573ee5ff94a62daec4e2019f7
Jeanstaquet <jean.staquet8@gmail.com>
2021-04-20 05:19:42 -0700

    Adding the startup

---
+++ app/lib/client/lib/updateLastLogin.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+
+import { handleError } from '../../../utils';
+
+Meteor.startup(function() {
+	if (Meteor.userId) {
+        Meteor.call('updateLastLogin', (error) => {
+            if (error) {
+                return handleError(error);
+            } else {
+                console.log('ok')
+            }
+        });
+    }
+});
