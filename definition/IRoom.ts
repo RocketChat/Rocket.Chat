e7820cca6de485a96f710da5e2805f6e90783b01
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-21 03:01:43 -0300

    [FIX] Send alternative color to unread sidebar icon (#21432)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

--- definition/IRoom.ts
+++ definition/IRoom.ts
@@ -60,6 +60,9 @@ export interface IRoom extends IRocketChatRecord {
 		_id: string;
 	};
 	onHold?: boolean;
+	unread?: number;
+	alert?: boolean;
+	hideUnreadStatus?: boolean;
 	departmentId?: string;
 }
 
