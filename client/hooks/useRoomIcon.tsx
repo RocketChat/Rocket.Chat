d6b97e849d4561d06ddeb2eb2ef3cf62170e814a
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-21 02:07:30 -0300

    [FIX] Avoid sidebar being broke (#21490)

--- client/hooks/useRoomIcon.tsx
+++ client/hooks/useRoomIcon.tsx
@@ -25,7 +25,10 @@ export const useRoomIcon = (room: IRoom): JSX.Element | { name: string; color?: 
 		case 'c':
 			return { name: 'hash' };
 		case 'l':
-			return { name: 'headset', color: colors[((room as unknown) as IOmnichannelRoom).v.status] };
+			return {
+				name: 'headset',
+				color: colors[((room as unknown) as IOmnichannelRoom)?.v.status || 'offline'],
+			};
 		case 'd':
 			const direct = (room as unknown) as IDirectMessageRoom;
 			if (direct.uids && direct.uids.length > 2) {
