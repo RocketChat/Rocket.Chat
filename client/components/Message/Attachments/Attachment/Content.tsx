6130b276c3b1919ee1559cd752a1b6d88f83f276
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-27 10:56:02 -0300

    adjust margins

--- client/components/Message/Attachments/Attachment/Content.tsx
+++ client/components/Message/Attachments/Attachment/Content.tsx
@@ -4,6 +4,13 @@ import React, { ComponentProps, FC } from 'react';
 const Content: FC<ComponentProps<typeof Box> & { isInner: boolean | undefined }> = ({
 	isInner,
 	...props
-}) => <Box rcx-attachment__content width='full' {...(!isInner ? { mb: 'x4' } : {})} {...props} />;
+}) => (
+	<Box
+		rcx-attachment__content
+		width='full'
+		{...(!isInner ? { mb: 'x4' } : { mbs: '4px', mbe: '-8px' })}
+		{...props}
+	/>
+);
 
 export default Content;
