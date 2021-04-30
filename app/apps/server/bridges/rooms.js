46da7cc32273d08d7f6c516e51e2fb08399f67c0
Lucas Sartor Chauvin <lucassartorchauvin@gmail.com>
2021-04-30 10:57:24 -0300

    add bridge method to delete rooms

--- app/apps/server/bridges/rooms.js
+++ app/apps/server/bridges/rooms.js
@@ -120,7 +120,13 @@ export class AppRoomBridge {
 			addUserToRoom(rm._id, member);
 		}
 	}
+	
+	async delete(roomId, appId) {
+		this.orch.debugLog(`The App ${ appId } is deleting a room.`);
 
+		Rooms.removeById(roomId);
+	}
+	
 	async createDiscussion(room, parentMessage = null, reply = '', members = [], appId) {
 		this.orch.debugLog(`The App ${ appId } is creating a new discussion.`, room);
 
