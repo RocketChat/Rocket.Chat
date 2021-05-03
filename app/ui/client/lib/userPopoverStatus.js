d0a09c7824150d44dda7926853808063615e776c
Diego Sampaio <chinello@gmail.com>
2021-05-03 11:31:59 -0300

    Add missing checks to client

--- app/ui/client/lib/userPopoverStatus.js
+++ app/ui/client/lib/userPopoverStatus.js
@@ -1,55 +1,60 @@
 import { t } from '../../../utils';
-import { settings } from '../../../settings/server';
+import { settings } from '../../../settings/client';
 
 export const getPopoverStatusConfig = (currentTarget, actionCallback) => {
-	const popoverStatusConfig = {
+	const items = [
+		{
+			icon: 'circle',
+			name: t('Online'),
+			modifier: 'online',
+			action: () => {
+				(typeof actionCallback === 'function') && actionCallback('online');
+				$('input[name=statusType]').val('online');
+				$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--online');
+			},
+		},
+		{
+			icon: 'circle',
+			name: t('Away'),
+			modifier: 'away',
+			action: () => {
+				(typeof actionCallback === 'function') && actionCallback('away');
+				$('input[name=statusType]').val('away');
+				$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--away');
+			},
+		},
+		{
+			icon: 'circle',
+			name: t('Busy'),
+			modifier: 'busy',
+			action: () => {
+				(typeof actionCallback === 'function') && actionCallback('busy');
+				$('input[name=statusType]').val('busy');
+				$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--busy');
+			},
+		},
+	];
+
+	if (settings.get('Accounts_AllowInvisibleStatusOption')) {
+		items.push({
+			icon: 'circle',
+			name: t('Invisible'),
+			modifier: 'offline',
+			action: () => {
+				(typeof actionCallback === 'function') && actionCallback('offline');
+				$('input[name=statusType]').val('offline');
+				$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--offline');
+			},
+		});
+	}
+
+	return {
 		popoverClass: 'edit-status-type',
 		columns: [
 			{
 				groups: [
 					{
-						items: [
-							{
-								icon: 'circle',
-								name: t('Online'),
-								modifier: 'online',
-								action: () => {
-									(typeof actionCallback === 'function') && actionCallback('online');
-									$('input[name=statusType]').val('online');
-									$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--online');
-								},
-							},
-							{
-								icon: 'circle',
-								name: t('Away'),
-								modifier: 'away',
-								action: () => {
-									(typeof actionCallback === 'function') && actionCallback('away');
-									$('input[name=statusType]').val('away');
-									$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--away');
-								},
-							},
-							{
-								icon: 'circle',
-								name: t('Busy'),
-								modifier: 'busy',
-								action: () => {
-									(typeof actionCallback === 'function') && actionCallback('busy');
-									$('input[name=statusType]').val('busy');
-									$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--busy');
-								},
-							},
-							{
-								icon: 'circle',
-								name: t('Invisible'),
-								modifier: 'offline',
-								action: () => {
-									(typeof actionCallback === 'function') && actionCallback('offline');
-									$('input[name=statusType]').val('offline');
-									$(currentTarget).prop('class', 'rc-input__icon js-status-type edit-status-type-icon--offline');
-								},
-							},
-						],
+						items,
 					},
 				],
 			},
@@ -57,10 +62,4 @@ export const getPopoverStatusConfig = (currentTarget, actionCallback) => {
 		currentTarget,
 		offsetVertical: currentTarget.clientHeight,
 	};
-
-	if (!settings.get('Accounts_AllowInvisibleStatusOption')) {
-		popoverStatusConfig.columns[0].groups[0].items.splice(3, 1);
-	}
-
-	return popoverStatusConfig;
 };
