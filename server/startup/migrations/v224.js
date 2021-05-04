d9df2ae60a430c5e7b34bab6ad09054326c1e5af
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-05-04 12:21:46 +0530

    fix lint

--- server/startup/migrations/v224.js
+++ server/startup/migrations/v224.js
@@ -1,17 +1,10 @@
 import { Migrations } from '../../../app/migrations';
 import { Settings } from '../../../app/models';
 
-// Migrations.add({
-// 	version: 224,
-// 	up() {
-// 		Settings.remove({ _id: 'UTF8_Names_Validation' });
-// 		console.log('Removed Setting UTF8_Names_Validation');
-// 	},
-// });
-
-function removeM(){
-    Settings.remove({ _id: 'UTF8_Names_Validation' });
-    console.log('Removed Setting UTF8_Names_Validation');
-}
-
-removeM();
+Migrations.add({
+	version: 224,
+	up() {
+		Settings.remove({ _id: 'UTF8_Names_Validation' });
+		console.log('Removed Setting - UTF8_Names_Validation');
+	},
+});
