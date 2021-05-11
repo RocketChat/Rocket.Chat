b31b99065b05fd32862a1c3fc95386afcde6ed72
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-05-11 16:37:03 -0300

    useIsVisible

---
+++ client/views/room/hooks/useIsVisible.ts
@@ -0,0 +1,25 @@
+import { useDebouncedState } from '@rocket.chat/fuselage-hooks';
+import { useEffect, MutableRefObject } from 'react';
+
+export const useIsVisible = (ref: MutableRefObject<HTMLInputElement | undefined>): [boolean] => {
+	const [menuVisibility, setMenuVisibility] = useDebouncedState(!!window.DISABLE_ANIMATION, 300);
+
+	useEffect(() => {
+		if (!ref.current) {
+			return;
+		}
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				setMenuVisibility(entry.isIntersecting);
+			});
+		});
+
+		observer.observe(ref.current);
+
+		return (): void => {
+			observer.disconnect();
+		};
+	}, [setMenuVisibility, ref.current]);
+
+	return [menuVisibility];
+};
