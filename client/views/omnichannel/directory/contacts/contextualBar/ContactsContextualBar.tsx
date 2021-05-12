d02f1cc20143028e5abfd962dfe32f48417c2ccf
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-05-12 13:13:12 -0300

    ContactContetualBar -> TS

--- client/views/omnichannel/directory/contacts/contextualBar/ContactsContextualBar.tsx
+++ client/views/omnichannel/directory/contacts/contextualBar/ContactsContextualBar.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
 
+import { IOmnichannelRoom } from '../../../../../../definition/IRoom';
 import VerticalBar from '../../../../../components/VerticalBar';
 import { useRoute, useRouteParameter } from '../../../../../contexts/RouterContext';
 import { useTranslation } from '../../../../../contexts/TranslationContext';
-import { useOmnichannelRoom } from '../../../../room/providers/RoomProvider';
+import { useOmnichannelRoom } from '../../../../room/contexts/RoomContext';
 import { useTabBarClose } from '../../../../room/providers/ToolboxProvider';
 import ContactEditWithData from './ContactEditWithData';
 import ContactInfo from './ContactInfo';
 
 const PATH = 'live';
 
-const ContactsContextualBar = ({ rid }) => {
+const ContactsContextualBar: FC<{ rid: IOmnichannelRoom['_id'] }> = ({ rid }) => {
 	const t = useTranslation();
 
 	const closeContextualBar = useTabBarClose();
@@ -19,7 +20,7 @@ const ContactsContextualBar = ({ rid }) => {
 
 	const context = useRouteParameter('context');
 
-	const handleContactEditBarCloseButtonClick = () => {
+	const handleContactEditBarCloseButtonClick = (): void => {
 		directoryRoute.push({ id: rid, tab: 'contact-profile' });
 	};
 
