fa86775ac762dedd407ee31ea9387b00338e921a
Gabriel Henriques <gabriel.henriques@rocket.chat>
2021-05-20 14:45:29 -0300

    Do not set preference if undefined

--- server/startup/migrations/v225.js
+++ server/startup/migrations/v225.js
@@ -6,7 +6,9 @@ Migrations.add({
 	up() {
 		const hideAvatarsSetting = Settings.findById('Accounts_Default_User_Preferences_hideAvatars');
 		const hideAvatarsSidebarSetting = Settings.findById('Accounts_Default_User_Preferences_sidebarHideAvatar');
+
 		Settings.removeById('Accounts_Default_User_Preferences_sidebarShowDiscussion');
+
 		Settings.removeById('Accounts_Default_User_Preferences_sidebarHideAvatar');
 		Settings.upsert({
 			_id: 'Accounts_Default_User_Preferences_sidebarDisplayAvatar',
@@ -15,6 +17,7 @@ Migrations.add({
 				value: !hideAvatarsSidebarSetting.value,
 			},
 		});
+
 		Settings.removeById('Accounts_Default_User_Preferences_hideAvatars');
 		Settings.upsert({
 			_id: 'Accounts_Default_User_Preferences_displayAvatars',
@@ -23,18 +26,30 @@ Migrations.add({
 				value: !hideAvatarsSetting.value,
 			},
 		});
+
 		const users = Users.find({}, { fields: { _id: 1, 'settings.preferences': 1 } }).fetch();
 		users.forEach((user) => {
-			if (!(user.settings && user.settings.preferences)) {
+			if (!(user.settings && user.settings.preferences && typeof user.setting.preferences !== 'object')) {
 				return;
 			}
+
+			const { preferences } = user.settings;
+			const { hideAvatars } = user.settings.preferences;
+			const { sidebarHideAvatar } = user.settings.preferences;
+
+			let setValues;
+			if (preferences.hasOwnProperty('hideAvatars')) {
+				setValues = { 'settings.preferences.displayAvatars': !hideAvatars };
+			}
+
+			if (preferences.hasOwnProperty('sidebarHideAvatar')) {
+				setValues = { ...setValues, 'settings.preferences.sidebarDisplayAvatar': !sidebarHideAvatar };
+			}
+
 			Users.update(
 				{ _id: user._id },
 				{
-					$set: {
-						'settings.preferences.displayAvatars': !user.settings.preferences.hideAvatars,
-						'settings.preferences.sidebarDisplayAvatar': !user.settings.preferences.sidebarHideAvatar,
-					},
+					$set: setValues,
 					$unset: {
 						'settings.preferences.hideAvatars': 1,
 						'settings.preferences.sidebarHideAvatar': 1,
