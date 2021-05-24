c288ea3ce1273f6c9f82af7461571ec679ac11f2
Rafael Ferreira <rafael.ferreira@rocket.chat>
2021-05-24 17:43:19 -0300

    create and edit canned response endpoint

--- ee/app/api-enterprise/server/canned-responses.js
+++ ee/app/api-enterprise/server/canned-responses.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { Match, check } from 'meteor/check';
+
 import { API } from '../../../../app/api/server';
 import { findAllCannedResponses } from './lib/canned-responses';
 
@@ -6,5 +9,23 @@ API.v1.addRoute('canned-responses.get', { authRequired: true }, {
 		return API.v1.success({
 			responses: Promise.await(findAllCannedResponses({ userId: this.userId })),
 		});
+	},
+});
+
+
+API.v1.addRoute('canned-responses', { authRequired: true }, {
+	post() {
+		check(this.bodyParams, {
+			_id: Match.Maybe(String),
+			shortcut: String,
+			text: String,
+			scope: String,
+			departmentId: Match.Maybe(String),
+		});
+		const { _id, shortcut, text, scope, departmentId } = this.bodyParams;
+		Meteor.runAsUser(this.userId, () => {
+			Meteor.call('saveCannedResponse', _id, { shortcut, text, scope, ...departmentId && { departmentId } });
+		});
+		return API.v1.success();
 	},
 });
