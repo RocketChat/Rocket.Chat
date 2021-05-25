1495928a17f3e9317038967fc1703d370566b772
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2021-05-25 22:52:08 +0530

    provide default value to livechat.applyDepartmentRestrictions params

--- ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
+++ ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
@@ -2,7 +2,7 @@ import { callbacks } from '../../../../../app/callbacks/server';
 import { addQueryRestrictionsToDepartmentsModel } from '../lib/query.helper';
 import { hasRole } from '../../../../../app/authorization/server/functions/hasRole';
 
-callbacks.add('livechat.applyDepartmentRestrictions', (originalQuery = {}, { userId } = {}) => {
+callbacks.add('livechat.applyDepartmentRestrictions', (originalQuery = {}, { userId } = { userId: null }) => {
 	if (!userId || !hasRole(userId, 'livechat-monitor')) {
 		return originalQuery;
 	}
