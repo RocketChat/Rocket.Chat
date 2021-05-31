7c51886735b04ad9f19b848a5059f9a1a3691575
Kevin Aleman <kevin.aleman@rocket.chat>
2021-05-31 14:47:45 -0500

    Move common code to /common

--- app/settings/client/lib/settings.ts
+++ app/settings/client/lib/settings.ts
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { PublicSettingsCachedCollection } from '../../../../client/lib/settings/PublicSettingsCachedCollection';
-import { SettingsBase } from '../../../../server/settings/SettingsBase';
+import { SettingsBase } from '../../../../common/SettingsBase';
 import { SettingValue } from '../../../../definition/ISetting';
 
 class Settings extends SettingsBase {
