27f9ae435949e00298481acc97cd1b532f749a19
Kevin Aleman <kevin.aleman@rocket.chat>
2021-05-31 16:13:15 -0500

    Migrate slashcommands to messages service

--- app/slashcommand-asciiarts/client/index.js
+++ app/slashcommand-asciiarts/client/index.js
@@ -1,5 +1,26 @@
-import '../lib/gimme';
-import '../lib/lenny';
-import '../lib/shrug';
-import '../lib/tableflip';
-import '../lib/unflip';
+import { slashCommands } from '../../utils';
+
+slashCommands.add('gimme', null, {
+	description: 'Slash_Gimme_Description',
+	params: 'your_message_optional',
+});
+
+slashCommands.add('lennyface', null, {
+	description: 'Slash_LennyFace_Description',
+	params: 'your_message_optional',
+});
+
+slashCommands.add('shrug', null, {
+	description: 'Slash_Shrug_Description',
+	params: 'your_message_optional',
+});
+
+slashCommands.add('unflip', null, {
+	description: 'Slash_TableUnflip_Description',
+	params: 'your_message_optional',
+});
+
+slashCommands.add('tableflip', null, {
+	description: 'Slash_Tableflip_Description',
+	params: 'your_message_optional',
+});
