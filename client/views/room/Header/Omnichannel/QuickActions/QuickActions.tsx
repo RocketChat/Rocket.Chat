47c4ab152c46e2d40be90874bf3dd305b42f0daf
Rafael Ferreira <rafaelblink@gmail.com>
2021-06-02 16:44:40 -0300

    normalizing subscription and room into useOmnichannelRoom

--- client/views/room/Header/Omnichannel/QuickActions/QuickActions.tsx
+++ client/views/room/Header/Omnichannel/QuickActions/QuickActions.tsx
@@ -200,7 +200,6 @@ const QuickActions: FC<QuickActionsProps> = ({ room, className }) => {
 	}, [onHoldChat, closeModal, rid, t]);
 
 	const openModal = useMutableCallback((id: string) => {
-		const departmentId = room.departmentId || room.department;
 		switch (id) {
 			case QuickActionsEnum.MoveQueue:
 				setModal(<ReturnChatQueueModal onMoveChat={handleMoveChat} onCancel={closeModal} />);
@@ -224,9 +223,9 @@ const QuickActions: FC<QuickActionsProps> = ({ room, className }) => {
 				break;
 			case QuickActionsEnum.CloseChat:
 				setModal(
-					departmentId ? (
+					room.departmentId ? (
 						<CloseChatModalData
-							departmentId={departmentId}
+							departmentId={room.departmentId}
 							onConfirm={handleClose}
 							onCancel={closeModal}
 						/>
--- client/views/room/contexts/RoomContext.ts
+++ client/views/room/contexts/RoomContext.ts
@@ -1,14 +1,21 @@
 import { createContext, useContext } from 'react';
 
 import { IRoom, IOmnichannelRoom, isOmnichannelRoom } from '../../../../definition/IRoom';
+import { IOmnichannelSubscription } from '../../../../definition/ISubscription';
 
 export type RoomContextValue = {
 	rid: IRoom['_id'];
-	room: IRoom;
-	// tabBar: TabBar;
+	room: IOmnichannelRoom & IOmnichannelSubscription;
 };
 
 export const RoomContext = createContext<RoomContextValue | null>(null);
+
+const normalizeRoomSubscription = (room: IOmnichannelRoom & IOmnichannelSubscription): IRoom => {
+	if (room.department) {
+		room.departmentId = room.department;
+	}
+	return room;
+};
 
 export const useRoom = (): IRoom => {
 	const { room } = useContext(RoomContext) || {};
@@ -20,11 +27,14 @@ export const useRoom = (): IRoom => {
 
 export const useOmnichannelRoom = (): IOmnichannelRoom => {
 	const { room } = useContext(RoomContext) || {};
+
 	if (!room) {
 		throw new Error('use useRoom only inside opened rooms');
 	}
 	if (!isOmnichannelRoom(room)) {
 		throw new Error('invalid room type');
 	}
+
+	normalizeRoomSubscription(room);
 	return room;
 };
