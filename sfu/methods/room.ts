bbc73e4d01f0b20b1909b377c9fdddd2b959a2e3
Rahul Halder <hrahul2605@gmail.com>
2021-06-04 19:14:53 +0530

    mediasoup room protoo request handler

--- sfu/methods/room.ts
+++ sfu/methods/room.ts
@@ -6,6 +6,8 @@ import { types } from 'mediasoup';
 import config from '../config';
 import { getWorker } from './worker';
 import { IPeer } from '../types/IPeer';
+import { createConsumer } from './createConsumer';
+import { createProducer } from './createProducer';
 
 const rooms = new Map<string, Room>();
 
@@ -73,7 +75,8 @@ export class Room extends EventEmitter {
 		peer.data.producers = new Map();
 		peer.data.consumers = new Map();
 
-		// @TODO: peer connection handler
+		peer.on('request', (request, accept, reject) => this.handleProtooRequest(peer, request, accept, reject)
+			.catch((err) => reject(err)));
 
 		peer.on('close', () => {
 			if (this.closed) { return; }
@@ -86,8 +89,202 @@ export class Room extends EventEmitter {
 		});
 	}
 
-	getJoinedPeers(excludePeer?: protoo.Peer): Array<protoo.Peer> {
+	getJoinedPeers(excludePeer?: IPeer): Array<IPeer> {
 		return this.protooRoom.peers.filter((peer) => peer.data.joined && peer !== excludePeer);
+	}
+
+	async handleProtooRequest(peer: IPeer, request: protoo.ProtooRequest, accept: protoo.AcceptFn, _reject: protoo.RejectFn): Promise<void> {
+		switch (request.method) {
+			case 'createWebRtcTransport': {
+				const {
+					forceTcp,
+					producing,
+					consuming,
+				} = request.data;
+
+				const webRtcTransportOptions =				{
+					...config.mediasoup.webRtcTransportOptions,
+					enableSctp: false,
+					appData: { producing, consuming },
+				};
+
+				if (forceTcp) {
+					webRtcTransportOptions.enableUdp = false;
+					webRtcTransportOptions.enableTcp = true;
+				}
+
+				const transport = await this.mediasoupRouter.createWebRtcTransport(webRtcTransportOptions);
+
+				peer.data.transports.set(transport.id, transport);
+
+				accept({
+					id: transport.id,
+					iceParameters: transport.iceParameters,
+					iceCandidates: transport.iceCandidates,
+					dtlsParameters: transport.dtlsParameters,
+					sctpParameters: transport.sctpParameters,
+				});
+
+				break;
+			}
+
+			case 'connectWebRtcTransport': {
+				const { transportId, dtlsParameters } = request.data;
+				const transport = peer.data.transports.get(transportId);
+
+				if (!transport) {
+					throw new Error(`Transport with id:${ transportId } not found.`);
+				}
+
+				await transport.connect({ dtlsParameters });
+
+				accept(null);
+
+				break;
+			}
+
+			case 'getRouterRtpCapibilities': {
+				accept(this.mediasoupRouter.rtpCapabilities);
+				break;
+			}
+
+			case 'join': {
+				if (peer.data.joined) {
+					throw new Error('Peer Already Joined.');
+				}
+
+				const {
+					displayName,
+					device,
+					rtpCapabilities,
+				} = request.data;
+
+				peer.data.joined = true;
+				peer.data.rtpCapabilities = rtpCapabilities;
+				peer.data.displayName = displayName;
+				peer.data.device = device;
+
+				const joinedPeers = { ...this.getJoinedPeers() };
+
+				const peerInfos = joinedPeers
+					.filter((joinedPeer) => joinedPeer.id !== peer.id)
+					.map((joinedPeer) => ({
+						id: joinedPeer.id,
+						displayName: joinedPeer.data.displayName,
+						device: joinedPeer.data.device,
+					}));
+
+				accept({ peers: peerInfos });
+
+				peer.data.joined = true;
+
+				joinedPeers.forEach((joinedPeer) => {
+					for (const producer of joinedPeer.data.producers.values()) {
+						createConsumer(this.mediasoupRouter, peer, joinedPeer, producer);
+					}
+				});
+
+				break;
+			}
+
+			case 'produce': {
+				if (!peer.data.joined) {
+					throw new Error('Peer yet to join.');
+				}
+
+				const producerId = createProducer(peer, request.data);
+
+				accept({ id: producerId });
+
+				break;
+			}
+
+			case 'closeProducer': {
+				if (!peer.data.joined) {
+					throw new Error('Peer yet to join.');
+				}
+
+				const { producerId } = request.data;
+				const producer = peer.data.producers.get(producerId);
+
+
+				if (!producer) {
+					throw new Error(`Producer with ${ producerId } not found.`);
+				}
+
+				producer.close();
+
+				peer.data.producers.delete(producer.id);
+
+				accept(null);
+
+				break;
+			}
+
+			case 'pauseProducer':
+			{
+				if (!peer.data.joined) { throw new Error('Peer yet to joined.'); }
+
+				const { producerId } = request.data;
+				const producer = peer.data.producers.get(producerId);
+
+				if (!producer) { throw new Error(`Producer with id "${ producerId }" not found.`); }
+
+				await producer.pause();
+
+				accept(null);
+
+				break;
+			}
+
+			case 'resumeProducer':
+			{
+				if (!peer.data.joined) { throw new Error('Peer yet to joined.'); }
+
+				const { producerId } = request.data;
+				const producer = peer.data.producers.get(producerId);
+
+				if (!producer) { throw new Error(`Producer with id "${ producerId }" not found.`); }
+
+				await producer.resume();
+
+				accept(null);
+
+				break;
+			}
+
+			case 'pauseConsumer':
+			{
+				if (!peer.data.joined) { throw new Error('Peer yet to joined.'); }
+
+				const { consumerId } = request.data;
+				const consumer = peer.data.consumers.get(consumerId);
+
+				if (!consumer) { throw new Error(`Consumer with id "${ consumerId }" not found.`); }
+
+				await consumer.pause();
+
+				accept(null);
+
+				break;
+			}
+
+			case 'resumeConsumer':
+			{
+				if (!peer.data.joined) { throw new Error('Peer yet to joined'); }
+
+				const { consumerId } = request.data;
+				const consumer = peer.data.consumers.get(consumerId);
+
+				if (!consumer) { throw new Error(`Consumer with id "${ consumerId }" not found.`); }
+
+				await consumer.resume();
+
+				accept(null);
+
+				break;
+			}
+		}
 	}
 }
 
