e4d833b1fe616866a0c231505d317f8a1cd8a785
Kevin Aleman <kevin.aleman@rocket.chat>
2021-06-04 11:14:32 -0500

    Migrate message unread method to new structure

--- app/message-mark-as-unread/server/logger.js
+++
@@ -1,9 +0,0 @@
-import { Logger } from '../../logger';
-
-const logger = new Logger('MessageMarkAsUnread', {
-	sections: {
-		connection: 'Connection',
-		events: 'Events',
-	},
-});
-export default logger;
---
+++ server/api/methods/index.js
@@ -0,0 +1,7 @@
+import './2fa/checkCodesRemaining';
+import './2fa/disable';
+import './2fa/enable';
+import './2fa/regenerateCodes';
+import './2fa/validateTempToken';
+import './mentions/getUserMentionsByChannel';
+import './messages/unreadMessages';
