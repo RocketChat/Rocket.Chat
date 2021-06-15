b42e1ffef8b1ec87fdf5f524d017139627ed4f37
Gabriel Thomé <gabrielallan.thome@gmail.com>
2021-06-15 12:27:23 -0300

    typings disclaimer

--- server/utils/http.ts
+++ server/utils/http.ts
@@ -1,7 +1,12 @@
 import { request } from 'undici';
 
+// undici typings from the official package
+// are kinda awkward but things seem to have
+// been figured out in 4.0 release candidates
+// TODO: remove ts-ignore flags once 4.0 rolls out as stable
 export default class HTTP {
-	static post(url: string, data: any): Promise<any> {
+	static post(url: string, data: any): PromiseLike<any> {
+		// @ts-ignore
 		return request(url, {
 			method: 'POST',
 			headers: data.headers,
