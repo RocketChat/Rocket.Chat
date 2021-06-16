300e0e02ddeb1bdd6f344405456a396593ffa805
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-06-16 10:33:45 -0300

    test

--- client/lib/clickableItem.js
+++ client/lib/clickableItem.js
@@ -5,6 +5,11 @@ import React from 'react';
 // TODO remove border from here
 export function clickableItem(Component) {
 	const clickable = css`
+		cursor: pointer;
+		&:hover,
+		&:focus {
+			background: ${colors.n100};
+		}
 		border-bottom: 2px solid ${colors.n300} !important;
 	`;
 	const WrappedComponent = (props) => <Component className={clickable} tabIndex={0} {...props} />;
--- client/views/room/contextualBar/Threads/components/Message.js
+++ client/views/room/contextualBar/Threads/components/Message.js
@@ -1,4 +1,4 @@
-import { Button, Icon, Message } from '@rocket.chat/fuselage';
+import { Button, Icon, Message, Box } from '@rocket.chat/fuselage';
 import React from 'react';
 
 import * as NotificationStatus from '../../../../../components/Message/NotificationStatus';
@@ -36,60 +36,62 @@ export default React.memo(function MessageThread({
 	const button = !following ? 'bell-off' : 'bell';
 	const actionLabel = t(!following ? 'Not_Following' : 'Following');
 	return (
-		<Message
-			{...props}
-			className={[
-				...(isIterable(className) ? className : [className]),
-				!following && followStyle,
-			].filter(Boolean)}
-		>
-			<Message.LeftContainer>
-				<UserAvatar username={username} className='rcx-message__avatar' size='x36' />
-			</Message.LeftContainer>
-			<Message.Container>
-				<Message.Header>
-					<Message.Name title={username}>{name}</Message.Name>
-					<Message.Timestamp>{formatDate(ts)}</Message.Timestamp>
-				</Message.Header>
-				<Message.Body clamp={2}>
-					<RawText>{msg}</RawText>
-				</Message.Body>
-				<Message.Block>
-					<Message.Metrics>
-						<Message.Metrics.Item>
-							<Message.Metrics.Item.Icon name='thread' />
-							<Message.Metrics.Item.Label>{replies}</Message.Metrics.Item.Label>
-						</Message.Metrics.Item>
-						<Message.Metrics.Item>
-							<Message.Metrics.Item.Icon name='user' />
-							<Message.Metrics.Item.Label>{participants}</Message.Metrics.Item.Label>
-						</Message.Metrics.Item>
-						<Message.Metrics.Item>
-							<Message.Metrics.Item.Icon name='clock' />
-							<Message.Metrics.Item.Label>{formatDate(tlm)}</Message.Metrics.Item.Label>
-						</Message.Metrics.Item>
-					</Message.Metrics>
-				</Message.Block>
-			</Message.Container>
-			<Message.ContainerFixed>
-				<Button
-					className={anchor}
-					small
-					square
-					flexShrink={0}
-					ghost
-					data-following={following}
-					data-id={_id}
-					onClick={handleFollowButton}
-					title={actionLabel}
-					aria-label={actionLabel}
-				>
-					<Icon name={button} size='x20' />
-				</Button>
-				{(mention && <NotificationStatus.Me t={t} mb='x24' />) ||
-					(all && <NotificationStatus.All t={t} mb='x24' />) ||
-					(unread && <NotificationStatus.Unread t={t} mb='x24' />)}
-			</Message.ContainerFixed>
-		</Message>
+		<Box className={className} pb='x8'>
+			<Message
+				{...props}
+				className={[
+					...(isIterable(className) ? className : [className]),
+					!following && followStyle,
+				].filter(Boolean)}
+			>
+				<Message.LeftContainer>
+					<UserAvatar username={username} className='rcx-message__avatar' size='x36' />
+				</Message.LeftContainer>
+				<Message.Container>
+					<Message.Header>
+						<Message.Name title={username}>{name}</Message.Name>
+						<Message.Timestamp>{formatDate(ts)}</Message.Timestamp>
+					</Message.Header>
+					<Message.Body clamp={2}>
+						<RawText>{msg}</RawText>
+					</Message.Body>
+					<Message.Block>
+						<Message.Metrics>
+							<Message.Metrics.Item>
+								<Message.Metrics.Item.Icon name='thread' />
+								<Message.Metrics.Item.Label>{replies}</Message.Metrics.Item.Label>
+							</Message.Metrics.Item>
+							<Message.Metrics.Item>
+								<Message.Metrics.Item.Icon name='user' />
+								<Message.Metrics.Item.Label>{participants}</Message.Metrics.Item.Label>
+							</Message.Metrics.Item>
+							<Message.Metrics.Item>
+								<Message.Metrics.Item.Icon name='clock' />
+								<Message.Metrics.Item.Label>{formatDate(tlm)}</Message.Metrics.Item.Label>
+							</Message.Metrics.Item>
+						</Message.Metrics>
+					</Message.Block>
+				</Message.Container>
+				<Message.ContainerFixed>
+					<Button
+						className={anchor}
+						small
+						square
+						flexShrink={0}
+						ghost
+						data-following={following}
+						data-id={_id}
+						onClick={handleFollowButton}
+						title={actionLabel}
+						aria-label={actionLabel}
+					>
+						<Icon name={button} size='x20' />
+					</Button>
+					{(mention && <NotificationStatus.Me t={t} mb='x24' />) ||
+						(all && <NotificationStatus.All t={t} mb='x24' />) ||
+						(unread && <NotificationStatus.Unread t={t} mb='x24' />)}
+				</Message.ContainerFixed>
+			</Message>
+		</Box>
 	);
 });
