1924e484af7918648da222eb47c0a7c1a6816fdc
Rahul Halder <hrahul2605@gmail.com>
2021-06-17 12:51:05 +0530

    [REMOVE] video mediacodecs for mediasoup router

--- sfu/config.ts
+++ sfu/config.ts
@@ -56,49 +56,6 @@ const config: ISFUConfig = {
 					clockRate: 48000,
 					channels: 2,
 				},
-				{
-					kind: 'video',
-					mimeType: 'video/VP8',
-					clockRate: 90000,
-					parameters:
-					{
-						'x-google-start-bitrate': 1000,
-					},
-				},
-				{
-					kind: 'video',
-					mimeType: 'video/VP9',
-					clockRate: 90000,
-					parameters:
-					{
-						'profile-id': 2,
-						'x-google-start-bitrate': 1000,
-					},
-				},
-				{
-					kind: 'video',
-					mimeType: 'video/h264',
-					clockRate: 90000,
-					parameters:
-					{
-						'packetization-mode': 1,
-						'profile-level-id': '4d0032',
-						'level-asymmetry-allowed': 1,
-						'x-google-start-bitrate': 1000,
-					},
-				},
-				{
-					kind: 'video',
-					mimeType: 'video/h264',
-					clockRate: 90000,
-					parameters:
-					{
-						'packetization-mode': 1,
-						'profile-level-id': '42e01f',
-						'level-asymmetry-allowed': 1,
-						'x-google-start-bitrate': 1000,
-					},
-				},
 			],
 		},
 		// mediasoup WebRtcTransport options for WebRTC endpoints (mediasoup-client,
