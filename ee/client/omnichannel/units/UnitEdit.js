d7259ad865d758d4d91090c1a666f9e0c90f14bf
Martin <martin.schoeler@rocket.chat>
2021-06-17 19:26:16 -0300

    [IMPROVE] Paginated department select on forward chat (#22123)

--- ee/client/omnichannel/units/UnitEdit.js
+++ ee/client/omnichannel/units/UnitEdit.js
@@ -18,7 +18,7 @@ import { useTranslation } from '../../../../client/contexts/TranslationContext';
 import { useRecordList } from '../../../../client/hooks/lists/useRecordList';
 import { AsyncStatePhase } from '../../../../client/hooks/useAsyncState';
 import { useForm } from '../../../../client/hooks/useForm';
-import { useDepartmentsList } from '../../../../client/views/hooks/useDepartmentsList';
+import { useDepartmentsByUnitsList } from '../../../../client/views/hooks/useDepartmentsByUnitsList';
 import { useMonitorsList } from '../../../../client/views/hooks/useMonitorsList';
 
 function UnitEdit({ data, unitId, isNew, unitMonitors, unitDepartments, reload, ...props }) {
@@ -35,8 +35,14 @@ function UnitEdit({ data, unitId, isNew, unitMonitors, unitDepartments, reload, 
 		monitorsList,
 	);
 
-	const { itemsList: departmentsList, loadMoreItems: loadMoreDepartments } = useDepartmentsList(
-		useMemo(() => ({ filter: departmentsFilter, unitId }), [departmentsFilter, unitId]),
+	const {
+		itemsList: departmentsList,
+		loadMoreItems: loadMoreDepartments,
+	} = useDepartmentsByUnitsList(
+		useMemo(() => ({ filter: debouncedDepartmentsFilter, unitId }), [
+			debouncedDepartmentsFilter,
+			unitId,
+		]),
 	);
 
 	const {
