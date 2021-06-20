9abe494101f6a4697783fdbb3cb737e03723a638
Rahul Halder <hrahul2605@gmail.com>
2021-06-20 16:42:58 +0530

    [ADD] Mute individual peer in voice channel

--- client/views/room/Room/PeerView.tsx
+++ client/views/room/Room/PeerView.tsx
@@ -1,12 +1,14 @@
-import { Box } from '@rocket.chat/fuselage';
-import React, { FC, ReactElement, useRef, useEffect } from 'react';
+import { Box, Button, Icon } from '@rocket.chat/fuselage';
+import React, { FC, ReactElement, useRef, useEffect, useState } from 'react';
 
 import { IVoiceRoomPeer } from '../../../../definition/IVoiceRoomPeer';
 import UserAvatar from '../../../components/avatar/UserAvatar';
 
 const PeerView: FC<IVoiceRoomPeer> = (props): ReactElement => {
 	const { id, displayName, track, username } = props;
+
 	const ref = useRef<HTMLAudioElement>(null);
+	const [muted, setMuted] = useState(false);
 
 	useEffect(() => {
 		const stream = new MediaStream();
@@ -17,11 +19,18 @@ const PeerView: FC<IVoiceRoomPeer> = (props): ReactElement => {
 			ref.current!.srcObject = stream;
 		}
 	}, []);
+
+	const toggleMute = (): void => {
+		setMuted((prev) => !prev);
+	};
 	return (
 		<Box textAlign='center' alignItems='center' justifyContent='center' id={id} padding='x16'>
 			<UserAvatar size='x124' rounded username={username || ''} />
-			<audio ref={ref} autoPlay />
+			<audio ref={ref} muted={muted} autoPlay />
 			<p>{displayName}</p>
+			<Button onClick={toggleMute}>
+				{muted ? <Icon name='volume-off' size='x24' /> : <Icon name='volume' size='x24' />}
+			</Button>
 		</Box>
 	);
 };
