84d1667f619e6994aa907c3832fb53652835484c
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-07-01 16:03:46 -0300

    remove canned-response contextual-bar from departments settings

--- client/views/omnichannel/departments/EditDepartment.js
+++ client/views/omnichannel/departments/EditDepartment.js
@@ -18,12 +18,9 @@ import React, { useMemo, useState, useRef } from 'react';
 import { useSubscription } from 'use-subscription';
 
 import { isEmail } from '../../../../app/utils/client';
-import { useHasLicense } from '../../../../ee/client/hooks/useHasLicense';
-import CannedResponsesRouter from '../../../../ee/client/omnichannel/cannedResponses';
 import Page from '../../../components/Page';
-import { useRoute, useRouteParameter, useCurrentRoute } from '../../../contexts/RouterContext';
+import { useRoute } from '../../../contexts/RouterContext';
 import { useMethod } from '../../../contexts/ServerContext';
-import { useSetting } from '../../../contexts/SettingsContext';
 import { useToastMessageDispatch } from '../../../contexts/ToastMessagesContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useComponentDidUpdate } from '../../../hooks/useComponentDidUpdate';
@@ -42,7 +39,6 @@ function EditDepartment({ data, id, title, reload }) {
 	const initialAgents = useRef((data && data.agents) || []);
 
 	const router = useRoute('omnichannel-departments');
-	const [, params] = useCurrentRoute();
 
 	const {
 		useEeNumberInput = () => {},
@@ -257,20 +253,6 @@ function EditDepartment({ data, id, title, reload }) {
 
 	const formId = useUniqueId();
 
-	const tab = useRouteParameter('tab');
-	const hasCannedResponsesLicense = useHasLicense('canned-responses');
-	const cannedResponsesEnabled = useSetting('Canned_Responses_Enable');
-	const showCanned =
-		hasCannedResponsesLicense && cannedResponsesEnabled && tab === 'canned-responses';
-
-	const handleOpenCannedResponses = useMutableCallback(() => {
-		router.push({ ...params, tab: 'canned-responses' });
-	});
-
-	const handleCloseCannedResponses = useMutableCallback(() => {
-		router.push({ ...params, tab: '' });
-	});
-
 	const hasNewAgent = useMemo(() => data.agents.length === agentList.length, [
 		data.agents,
 		agentList,
@@ -281,11 +263,6 @@ function EditDepartment({ data, id, title, reload }) {
 			<Page>
 				<Page.Header title={title}>
 					<ButtonGroup>
-						{id && hasCannedResponsesLicense && cannedResponsesEnabled && (
-							<Button onClick={handleOpenCannedResponses} title={t('Canned Responses')}>
-								<Icon name='baloon-exclamation' size='x16' />
-							</Button>
-						)}
 						<Button onClick={handleReturn}>
 							<Icon name='back' /> {t('Back')}
 						</Button>
@@ -490,9 +467,6 @@ function EditDepartment({ data, id, title, reload }) {
 					</FieldGroup>
 				</Page.ScrollableContentWithShadow>
 			</Page>
-			{showCanned && (
-				<CannedResponsesRouter departmentId={id} onClose={handleCloseCannedResponses} />
-			)}
 		</Page>
 	);
 }
