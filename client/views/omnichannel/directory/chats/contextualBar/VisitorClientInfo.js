26a56bc0f2befb92c87d4508266505aea4e7fb7e
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-07-07 00:09:02 -0300

    include host and ip

--- client/views/omnichannel/directory/chats/contextualBar/VisitorClientInfo.js
+++ client/views/omnichannel/directory/chats/contextualBar/VisitorClientInfo.js
@@ -29,6 +29,8 @@ const VisitorClientInfo = ({ uid }) => {
 	ua.setUA(userData.visitor.userAgent);
 	clientData.os = `${ua.getOS().name} ${ua.getOS().version}`;
 	clientData.browser = `${ua.getBrowser().name} ${ua.getBrowser().version}`;
+	clientData.host = userData.visitor.host;
+	clientData.ip = userData.visitor.ip;
 
 	return (
 		<>
@@ -42,6 +44,18 @@ const VisitorClientInfo = ({ uid }) => {
 				<Field>
 					<Label>{t('Browser')}</Label>
 					<Info>{clientData.browser}</Info>
+				</Field>
+			)}
+			{clientData.host && (
+				<Field>
+					<Label>{t('Host')}</Label>
+					<Info>{clientData.host}</Info>
+				</Field>
+			)}
+			{clientData.ip && (
+				<Field>
+					<Label>{t('IP')}</Label>
+					<Info>{clientData.ip}</Info>
 				</Field>
 			)}
 		</>
