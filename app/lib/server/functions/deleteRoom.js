f0064158f23cda3281d472d02c11c93b79dcfca1
Jeanstaquet <jean.staquet8@gmail.com>
2021-07-10 13:19:18 -0700

    Adding the task deleting when the room is deleted

--- app/lib/server/functions/deleteRoom.js
+++ app/lib/server/functions/deleteRoom.js
@@ -1,10 +1,11 @@
-import { Messages, Subscriptions, Rooms } from '../../../models';
+import { Messages, Subscriptions, Rooms, Tasks } from '../../../models';
 import { callbacks } from '../../../callbacks';
 import { FileUpload } from '../../../file-upload/server';
 
 export const deleteRoom = function(rid) {
 	FileUpload.removeFilesByRoomId(rid);
 	Messages.removeByRoomId(rid);
+	Tasks.removeByRoomId(rid);
 	callbacks.run('beforeDeleteRoom', rid);
 	Subscriptions.removeByRoomId(rid);
 	FileUpload.getStore('Avatars').deleteByRoomId(rid);
