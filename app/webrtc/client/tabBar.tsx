c51a23cf3d71c5856534b62b49412d24419f3678
Dhruv Jain <dhruv.jain93@gmail.com>
2021-07-11 23:17:51 +0530

    [NEW] WebRTC Call Session

--- app/webrtc/client/tabBar.tsx
+++ app/webrtc/client/tabBar.tsx
@@ -2,11 +2,16 @@ import { useMemo } from 'react';
 
 import { useSetting } from '../../../client/contexts/SettingsContext';
 import { addAction } from '../../../client/views/room/lib/Toolbox';
+import { useMethod } from '../../../client/contexts/ServerContext';
 
-addAction('webRTCVideo', () => {
+addAction('webRTCVideo', ({ room }) => {
 	const enabled = useSetting('WebRTC_Enabled') && (useSetting('Omnichannel_call_provider') === 'WebRTC');
+	const createSession = useMethod('webrtc:createSession');
 
-	const handleClick = (): void => { window.open('/', '_blank'); };
+	const handleClick = async (): Promise<void> => {
+		const sessionId = await createSession(room._id);
+		window.open(`/meet/${ sessionId }`, '_blank');
+	};
 
 	return useMemo(() => (enabled ? {
 		groups: ['live'],
