0c5ca11b0a442d4d93ef8b772e2418df5a5e2e55
Martin <martin.schoeler@rocket.chat>
2021-07-12 18:12:22 -0300

    fix lint

--- client/contexts/ServerContext/endpoints/v1/livechat/canned-responses.ts
+++ client/contexts/ServerContext/endpoints/v1/livechat/canned-responses.ts
@@ -1,5 +1,5 @@
-import { IOmnichannelCannedResponse } from '../../../../../../ee/client/omnichannel/cannedResponses/IOmnichannelCannedResponse';
 import { ObjectFromApi } from '../../../../../../definition/ObjectFromApi';
+import { IOmnichannelCannedResponse } from '../../../../../../ee/client/omnichannel/cannedResponses/IOmnichannelCannedResponse';
 
 export type CannedResponses = {
 	GET: (params: { text: string; offset?: number | undefined; count?: number | undefined }) => {
