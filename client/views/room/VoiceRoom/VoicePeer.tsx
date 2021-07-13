5395e724d96f044bf971125bcf1fa9b6ada5c84b
Rahul Halder <hrahul2605@gmail.com>
2021-07-13 22:39:52 +0530

    [NEW] Visual indication for speakers in voice room (#22507)
    * [ADD] hark package, visual indication for speakers in voice room
    
    * [IMPROVE] removed hark, added util func. for analysing audio
    
    * fix: lint
    
    * fix: review
    
    Co-authored-by: dougfabris <devfabris@gmail.com>

--- client/views/room/VoiceRoom/VoicePeer.tsx
+++ client/views/room/VoiceRoom/VoicePeer.tsx
@@ -3,6 +3,7 @@ import React, { FC, ReactElement, useRef, useEffect, useState } from 'react';
 
 import { IVoiceRoomPeer } from '../../../../definition/IVoiceRoomPeer';
 import UserAvatar from '../../../components/avatar/UserAvatar';
+import { analyseAudio } from './util';
 
 const VoicePeer: FC<IVoiceRoomPeer> = ({
 	id,
@@ -13,11 +14,25 @@ const VoicePeer: FC<IVoiceRoomPeer> = ({
 }): ReactElement => {
 	const ref = useRef<HTMLAudioElement>(null);
 	const [muted, setMuted] = useState(false);
+	const [speaking, setSpeaking] = useState<boolean>(false);
+
+	const runAudioAnalyser = (s: MediaStream): void => {
+		const speechEvents = analyseAudio(s);
+		speechEvents.on('speaking', () => {
+			setSpeaking(true);
+		});
+
+		speechEvents.on('stopped_speaking', () => {
+			setSpeaking(false);
+		});
+	};
 
 	useEffect(() => {
 		if (track) {
 			const stream = new MediaStream();
 			stream.addTrack(track);
+
+			runAudioAnalyser(stream);
 
 			if (ref.current) {
 				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -32,7 +47,13 @@ const VoicePeer: FC<IVoiceRoomPeer> = ({
 
 	return (
 		<Box textAlign='center' justifyContent='center' id={id} padding='x16'>
-			<UserAvatar size='x124' username={username || ''} />
+			<Box
+				padding='0.2em'
+				borderWidth='unset'
+				borderColor={speaking ? 'var(--rc-color-success)' : 'transparent'}
+			>
+				<UserAvatar size='x124' username={username || ''} />
+			</Box>
 			<audio ref={ref} muted={muted || deafen} autoPlay />
 			<p>{displayName}</p>
 			{track && (
