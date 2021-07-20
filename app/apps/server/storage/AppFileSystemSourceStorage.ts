4610c829c28d9ab665151d2c66be15bfbc8e3aad
Douglas Gubert <douglas.gubert@gmail.com>
2021-07-20 20:20:17 -0300

    Update FS storage to latest Apps-Engine changes

--- app/apps/server/storage/AppFileSystemSourceStorage.ts
+++ app/apps/server/storage/AppFileSystemSourceStorage.ts
@@ -2,26 +2,66 @@ import { promises as fs } from 'fs';
 
 import { AppSourceStorage, IAppStorageItem } from '@rocket.chat/apps-engine/server/storage';
 
-const tempPath = '/Users/douglasgubert/dev/rocket.chat/Rocket.Chat/apps-engine-files/';
-
 export class AppFileSystemSourceStorage extends AppSourceStorage {
-	public async store(item: IAppStorageItem, zip: Buffer): Promise<boolean> {
-		await fs.writeFile(`${ tempPath + item.id }.zip`, zip);
+	private pathPrefix = 'fs:/';
 
-		return true;
+	private path: string;
+
+	public setPath(path: string): void {
+		this.path = path;
+	}
+
+	public checkPath(): void {
+		if (!this.path) {
+			throw new Error('Invalid path configured for file system App storage');
+		}
+	}
+
+	public async store(item: IAppStorageItem, zip: Buffer): Promise<string> {
+		this.checkPath();
+
+		const filePath = this.itemToFilename(item);
+
+		await fs.writeFile(filePath, zip);
+
+		return this.filenameToSourcePath(filePath);
 	}
 
 	public async fetch(item: IAppStorageItem): Promise<Buffer> {
-		return fs.readFile(`${ tempPath + item.id }.zip`);
+		if (!item.sourcePath) {
+			throw new Error('Invalid source path');
+		}
+
+		return fs.readFile(this.sourcePathToFilename(item.sourcePath));
 	}
 
-	public async update(id: string, zip: Buffer): Promise<boolean> {
-		await fs.writeFile(`${ tempPath + id }.zip`, zip);
-		return true;
+	public async update(item: IAppStorageItem, zip: Buffer): Promise<string> {
+		this.checkPath();
+
+		const filePath = this.itemToFilename(item);
+
+		await fs.writeFile(filePath, zip);
+
+		return this.filenameToSourcePath(filePath);
 	}
 
-	public async remove(item: IAppStorageItem): Promise<boolean> {
-		fs.unlink(`${ tempPath + item.id }.zip`);
-		return true;
+	public async remove(item: IAppStorageItem): Promise<void> {
+		if (!item.sourcePath) {
+			return;
+		}
+
+		return fs.unlink(this.sourcePathToFilename(item.sourcePath));
+	}
+
+	private itemToFilename(item: IAppStorageItem): string {
+		return `${ this.path + item.id }.zip`;
+	}
+
+	private filenameToSourcePath(filename: string): string {
+		return this.pathPrefix + filename;
+	}
+
+	private sourcePathToFilename(sourcePath: string): string {
+		return sourcePath.substring(this.pathPrefix.length);
 	}
 }
