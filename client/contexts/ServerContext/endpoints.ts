71170a4d0b5edd9a4a6677d03a4a1f9bc38d55c9
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2021-07-20 22:36:29 -0300

    [FIX][ENTERPRISE] Engagement Dashboard displaying incorrect data about active users (#22381)
    * Fix data grouping, labels and dates in the Engagement Dashboard
    
    * Fix DAU, WAU and MAU values in Active Users chart
    
    * Fix previous values for WAU, MAU, DAU and timezone in Busiest Chat Time section
    
    * Add engagement-dashboard/users/active-users endpoint to endpoint.ts file
    
    * Add date to tooltip (active users chart)
    
    * Add support to UTC timezone
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- client/contexts/ServerContext/endpoints.ts
+++ client/contexts/ServerContext/endpoints.ts
@@ -1,3 +1,4 @@
+import { EngagementDashboardActiveUsersEndpoint } from '../../../ee/app/engagement-dashboard/client/contexts/ServerContext/endpoints/EngagementDashboardActiveUsers';
 import { ExternalComponentsEndpoint as AppsExternalComponentsEndpoint } from './endpoints/apps/externalComponents';
 import { FilesEndpoint as ChannelsFilesEndpoint } from './endpoints/v1/channels/files';
 import { ChannelsMembersEndpoint } from './endpoints/v1/channels/members';
@@ -62,6 +63,7 @@ export type ServerEndpoints = {
 	'livechat/tags.list': LivechatTagsList;
 	'livechat/department': LivechatDepartment;
 	'livechat/departments.by-unit/': LivechatDepartmentsByUnit;
+	'engagement-dashboard/users/active-users': EngagementDashboardActiveUsersEndpoint;
 	'rooms.info': RoomsInfoEndpoint;
 	'users.2fa.sendEmailCode': SendEmailCodeEndpoint;
 	'livechat/custom-fields': LivechatCustomFieldsEndpoint;
