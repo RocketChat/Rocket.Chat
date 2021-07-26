d41acc545d9e6c7974ee4087df2d6ab9983a9560
Kevin Aleman <kevin.aleman@rocket.chat>
2021-07-26 17:39:52 -0500

    Fixoptional chaining error

--- ee/client/omnichannel/components/CannedResponse/parsePlaceholder.ts
+++ ee/client/omnichannel/components/CannedResponse/parsePlaceholder.ts
@@ -4,7 +4,7 @@ export const parsePlaceHolder = (text: string, room: IOmnichannelRoom): string =
 	const placeholders = text.match(/{{(.*?)}}/g);
 	const placeholderDict = {
 		'contact.name': room.v.name,
-		'contact.email': room.v.visitorEmails.length ? room.v.visitorEmails[0].address : '',
+		'contact.email': room.v.visitorEmails?.length ? room.v.visitorEmails[0].address : '',
 		// 'contact.phone': room.v.status,	//No phone information in livechat form yet
 		'agent.name': room.servedBy?.name ? room.servedBy.name : '',
 		'agent.email': room.servedBy?.emails?.length ? room.servedBy.emails[0].address : '',
