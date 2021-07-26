3d1b1cfaecbb27ba9bdc182e802e26b3fc05dc90
Gabriel Thomé <gabrielallan.thome@gmail.com>
2021-07-26 18:21:04 -0300

    remove console logs

--- app/lib/server/methods/saveCustomFields.ts
+++ app/lib/server/methods/saveCustomFields.ts
@@ -5,9 +5,6 @@ import { saveCustomFields } from '../functions/saveCustomFields';
 
 Meteor.methods({
 	saveCustomFields(fields = {}) {
-		console.log('fields');
-		console.log(fields);
-		console.log('userId: ', Meteor.userId());
 		saveCustomFields(Meteor.userId(), fields);
 	},
 });
