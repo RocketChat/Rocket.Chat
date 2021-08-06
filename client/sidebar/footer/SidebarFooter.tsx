268a898034724843ffe9f72ba52b47f217488e10
dougfabris <devfabris@gmail.com>
2021-08-06 19:03:30 -0300

    fix: modal and sidebar footer styles on IE

--- client/sidebar/footer/SidebarFooter.tsx
+++ client/sidebar/footer/SidebarFooter.tsx
@@ -1,28 +1,29 @@
 import { css } from '@rocket.chat/css-in-js';
 import { Box } from '@rocket.chat/fuselage';
+import colors from '@rocket.chat/fuselage-tokens/colors.json';
 import React, { ReactElement } from 'react';
 
 import { settings } from '../../../app/settings/client';
 
 const SidebarFooter = (): ReactElement => {
 	const sidebarFooterStyle = css`
-		width: auto;
-		height: var(--sidebar-footer-height);
-		padding: var(--sidebar-extra-small-default-padding) var(--sidebar-default-padding);
-
 		& img {
 			max-width: 100%;
 			height: 100%;
 		}
 
 		& a:any-link {
-			color: var(--rc-color-primary-light);
+			color: ${colors.n600};
 		}
 	`;
 
 	return (
 		<Box
 			is='footer'
+			pb='x12'
+			pi='x16'
+			height='x48'
+			width='auto'
 			className={sidebarFooterStyle}
 			dangerouslySetInnerHTML={{ __html: String(settings.get('Layout_Sidenav_Footer')).trim() }}
 		/>
