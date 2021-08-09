71729a7fd44a8b277519c4fcf65b73ae14b84fa2
Dhruv Jain <51796498+djcruz93@users.noreply.github.com>
2021-08-09 11:46:25 +0530

    [NEW] P2P WebRTC Connection Establishment (#22847)
    * [NEW] WebRTC P2P Connection with Basic Call UI
    
    * [FIX] Set Stream on a stable connection
    
    * [FIX] userId typecheck error
    
    * [REFACTOR]
     - Restore type of userId to string by removing `| undefined`
     - Add translation for visitor does not exist toastr
     - Set visitorId from room object fetched instead of fetching from livechat widget as query param
     - Type Checking
    
    * [FIX] Running startCall 2 times for agent
    
    * [FIX] Call declined Page

---
+++ client/views/meet/MeetPage.js
@@ -0,0 +1,60 @@
+import { Meteor } from 'meteor/meteor';
+import React, { useEffect, useState, useCallback } from 'react';
+
+import { APIClient } from '../../../app/utils/client';
+import { useRouteParameter, useQueryStringParameter } from '../../contexts/RouterContext';
+import NotFoundPage from '../notFound/NotFoundPage';
+import PageLoading from '../root/PageLoading';
+import CallPage from './CallPage';
+
+function MeetPage() {
+	const [isRoomMember, setIsRoomMember] = useState(false);
+	const [status, setStatus] = useState(null);
+	const [visitorId, setVisitorId] = useState(null);
+	const roomId = useRouteParameter('rid');
+	const visitorToken = useQueryStringParameter('token');
+
+	const setupCallForVisitor = useCallback(async () => {
+		const room = await APIClient.v1.get(`/livechat/room?token=${visitorToken}&rid=${roomId}`);
+		if (room?.room?.v?.token === visitorToken) {
+			setVisitorId(room.room.v._id);
+			setStatus(room?.room?.callStatus || 'ended');
+			return setIsRoomMember(true);
+		}
+	}, [visitorToken, roomId]);
+
+	const setupCallForAgent = useCallback(async () => {
+		const room = await APIClient.v1.get(`/rooms.info?roomId=${roomId}`);
+		if (room?.room?.servedBy?._id === Meteor.userId()) {
+			setStatus(room?.room?.callStatus || 'ended');
+			return setIsRoomMember(true);
+		}
+	}, [roomId]);
+
+	useEffect(() => {
+		if (visitorToken) {
+			return setupCallForVisitor();
+		}
+		setupCallForAgent();
+	}, [setupCallForAgent, setupCallForVisitor, visitorToken]);
+	if (status === null) {
+		return <PageLoading></PageLoading>;
+	}
+	if (!isRoomMember) {
+		return <NotFoundPage></NotFoundPage>;
+	}
+	if (status === 'ended') {
+		return <h1 style={{ color: 'white', textAlign: 'center', marginTop: 250 }}>Ended!</h1>;
+	}
+	return (
+		<CallPage
+			roomId={roomId}
+			status={status}
+			visitorToken={visitorToken}
+			visitorId={visitorId}
+			setStatus={setStatus}
+		></CallPage>
+	);
+}
+
+export default MeetPage;
