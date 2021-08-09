06d360b156c25f932e0a19d52647bfa6b2b74e02
thassiov <tvmcarvalho@gmail.com>
2021-08-09 11:18:22 -0300

    Revert "Fix apps engine's scheduler faulty tasks update"
    This reverts commit d471479a22736bfbbf728ac79068611d71dc5e2a.

--- packages/rocketchat-mongo-config/server/index.js
+++ packages/rocketchat-mongo-config/server/index.js
@@ -30,7 +30,7 @@ tls.DEFAULT_ECDH_CURVE = 'auto';
 const mongoConnectionOptions = {
 	// add retryWrites=false if not present in MONGO_URL
 	...!process.env.MONGO_URL.includes('retryWrites') && { retryWrites: false },
-	ignoreUndefined: false,
+	// ignoreUndefined: false, // TODO evaluate adding this config
 };
 
 const mongoOptionStr = process.env.MONGO_OPTIONS;
