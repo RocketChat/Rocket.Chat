18d6f14860f6985226fff562269eb3a3840a0559
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-08-13 19:33:51 -0300

    bad words for empty messages

---
+++ app/lib/server/methods/filterBadWords.ts
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import Filter from 'bad-words';
+
+import { settings } from '../../../settings/server';
+import { callbacks } from '../../../callbacks/server';
+
+
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		const allowBadWordsFilter = settings.get('AllowBadWordsFilter');
+
+		if (!allowBadWordsFilter) {
+			return callbacks.remove('beforeSaveMessage', 'filterBadWords');
+		}
+
+		const badWordsList = settings.get('Message_BadWordsFilterList') as string | undefined;
+		const whiteList = settings.get('Message_BadWordsWhitelist') as string | undefined;
+
+		const options = {
+			list: badWordsList?.split(',').map((word) => word.trim()) || [],
+			// library definition does not allow optional definition
+			exclude: undefined,
+			splitRegex: undefined,
+			placeHolder: undefined,
+			regex: undefined,
+			replaceRegex: undefined,
+			emptyList: undefined,
+		};
+
+		const filter = new Filter(options);
+
+		if (whiteList?.length) {
+			filter.removeWords(...whiteList.split(',').map((word) => word.trim()));
+		}
+
+		callbacks.add('beforeSaveMessage', function(message) {
+			if (!message.msg) {
+				return message;
+			}
+			message.msg = filter.clean(message.msg);
+		}, callbacks.priority.HIGH, 'filterBadWords');
+	});
+});
