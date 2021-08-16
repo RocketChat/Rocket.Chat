7b71e3a08f3308c35186da82cbb1d6442738f2e2
dougfabris <devfabris@gmail.com>
2021-08-16 18:47:08 -0300

    fix: race condition when creating room for the first time

--- client/lib/goToRoomById.ts
+++ client/lib/goToRoomById.ts
@@ -3,7 +3,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import { ChatSubscription } from '../../app/models/client';
 import { call } from '../../app/ui-utils/client';
-import { roomTypes } from '../../app/utils/client';
+import { roomTypes, waitUntilFind } from '../../app/utils/client';
 import { IRoom } from '../../definition/IRoom';
 
 const getRoomById = memoize((rid: IRoom['_id']) => call('getRoomById', rid));
@@ -12,7 +12,9 @@ export const goToRoomById = async (rid: IRoom['_id']): Promise<void> => {
 	if (!rid) {
 		return;
 	}
-	const subscription = ChatSubscription.findOne({ rid });
+
+	const subscription = await waitUntilFind(() => ChatSubscription.findOne({ rid }));
+
 	if (subscription) {
 		roomTypes.openRouteLink(subscription.t, subscription, FlowRouter.current().queryParams);
 		return;
