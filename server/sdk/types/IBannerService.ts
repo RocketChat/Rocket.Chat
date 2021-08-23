3e135b8174fb4e9a9b647f735385dcffed965d5b
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-08-23 11:37:37 -0300

    [IMPROVE] Banner Service (#22989)
    * WIP
    
    * Fix type import
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- server/sdk/types/IBannerService.ts
+++ server/sdk/types/IBannerService.ts
@@ -1,7 +1,10 @@
 import { BannerPlatform, IBanner } from '../../../definition/IBanner';
+import { Optional } from '../../../definition/utils';
 
 export interface IBannerService {
-	getNewBannersForUser(userId: string, platform: BannerPlatform, bannerId?: string): Promise<IBanner[]>;
-	create(banner: Omit<IBanner, '_id'>): Promise<IBanner>;
+	getBannersForUser(userId: string, platform: BannerPlatform, bannerId?: string): Promise<IBanner[]>;
+	create(banner: Optional<IBanner, '_id'>): Promise<IBanner>;
 	dismiss(userId: string, bannerId: string): Promise<boolean>;
+	disable(bannerId: string): Promise<boolean>;
+	enable(bannerId: string, doc?: Partial<Omit<IBanner, '_id'>>, keepDismiss?: boolean): Promise<boolean>;
 }
