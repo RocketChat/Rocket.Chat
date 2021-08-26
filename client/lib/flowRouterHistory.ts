ab323f589f2ed14d52bf357dc078f13d952e9bb1
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-08-26 12:43:02 -0300

    Add react-router-dom with FlowRouter support

---
+++ client/lib/flowRouterHistory.ts
@@ -0,0 +1,109 @@
+import {
+	createMemoryHistory,
+	History,
+	LocationDescriptor,
+	LocationDescriptorObject,
+	LocationState,
+} from 'history';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+const promptBeforeUnload = (event: BeforeUnloadEvent): void => {
+	event.preventDefault();
+	event.returnValue = '';
+};
+
+export const createFlowRouterHistory = (): History => {
+	const memoryHistory = createMemoryHistory();
+
+	const contextPrototype = (FlowRouter as any)._page.Context.prototype;
+	const { pushState, save } = contextPrototype;
+
+	contextPrototype.pushState = function (this: any, ...args: any[]): void {
+		memoryHistory.push(this.path, memoryHistory.location.state);
+		return pushState.apply(this, args);
+	};
+
+	contextPrototype.save = function (this: any, ...args: any[]): void {
+		memoryHistory.replace(this.path, memoryHistory.location.state);
+		return save.apply(this, args);
+	};
+
+	memoryHistory.replace(FlowRouter.current().path, {});
+
+	let blockersCount = 0;
+
+	const history: History = {
+		get length() {
+			return memoryHistory.length;
+		},
+		get action() {
+			return memoryHistory.action;
+		},
+		get location() {
+			return memoryHistory.location;
+		},
+		push(to: string | LocationDescriptor, state?: LocationState) {
+			const path = typeof to === 'string' ? to : this.createHref(to);
+			state = state ?? memoryHistory.location.state;
+
+			FlowRouter.go(path);
+			memoryHistory.push(path, state);
+		},
+		replace(to: string | LocationDescriptor, state?: LocationState) {
+			const path = typeof to === 'string' ? to : this.createHref(to);
+			state = state ?? memoryHistory.location.state;
+
+			FlowRouter.withReplaceState(() => {
+				FlowRouter.go(path);
+				memoryHistory.push(path, state);
+			});
+		},
+		createHref(to: LocationDescriptorObject) {
+			return memoryHistory.createHref(to);
+		},
+		go(delta: number) {
+			memoryHistory.go(delta);
+			window.history.go(delta);
+		},
+		goBack() {
+			this.go(-1);
+		},
+		goForward() {
+			this.go(1);
+		},
+		listen(listener) {
+			return memoryHistory.listen(listener);
+		},
+		block(blocker) {
+			const unblock = memoryHistory.block(blocker);
+
+			blockersCount++;
+
+			if (blockersCount === 1) {
+				window.addEventListener('beforeunload', promptBeforeUnload);
+			}
+
+			return function (): void {
+				unblock();
+
+				blockersCount--;
+
+				if (blockersCount === 0) {
+					window.removeEventListener('beforeunload', promptBeforeUnload);
+				}
+			};
+		},
+	};
+
+	return history;
+};
+
+let history: History;
+
+export const getFlowRouterHistory = (): History => {
+	if (!history) {
+		history = createFlowRouterHistory();
+	}
+
+	return history;
+};
--- client/views/root/AppRoot.tsx
+++ client/views/root/AppRoot.tsx
@@ -1,5 +1,7 @@
 import React, { FC, lazy, Suspense } from 'react';
+import { Router } from 'react-router-dom';
 
+import { getFlowRouterHistory } from '../../lib/flowRouterHistory';
 import PageLoading from './PageLoading';
 
 const ConnectionStatusBar = lazy(
@@ -10,13 +12,17 @@ const BannerRegion = lazy(() => import('../banners/BannerRegion'));
 const AppLayout = lazy(() => import('./AppLayout'));
 const PortalsWrapper = lazy(() => import('./PortalsWrapper'));
 
+const history = getFlowRouterHistory();
+
 const AppRoot: FC = () => (
 	<Suspense fallback={<PageLoading />}>
 		<MeteorProvider>
-			<ConnectionStatusBar />
-			<BannerRegion />
-			<AppLayout />
-			<PortalsWrapper />
+			<Router history={history}>
+				<ConnectionStatusBar />
+				<BannerRegion />
+				<AppLayout />
+				<PortalsWrapper />
+			</Router>
 		</MeteorProvider>
 	</Suspense>
 );
