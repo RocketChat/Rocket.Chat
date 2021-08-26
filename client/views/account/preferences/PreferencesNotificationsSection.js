9022f987561f831501aba3dfa6d5a4f37663836b
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-08-26 17:45:51 -0300

    Update user account preferences keys

--- client/views/account/preferences/PreferencesNotificationsSection.js
+++ client/views/account/preferences/PreferencesNotificationsSection.js
@@ -35,7 +35,7 @@ const PreferencesNotificationsSection = ({ onChange, commitRef, ...props }) => {
 		'desktopNotificationRequireInteraction',
 	);
 	const userDesktopNotifications = useUserPreference('desktopNotifications');
-	const userMobileNotifications = useUserPreference('mobileNotifications');
+	const userMobileNotifications = useUserPreference('pushNotifications');
 	const userEmailNotificationMode = useUserPreference('emailNotificationMode');
 	const userDesktopAudioNotifications = useUserPreference('audioNotifications');
 
