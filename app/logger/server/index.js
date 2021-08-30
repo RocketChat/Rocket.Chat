a84183dc349aa9cb04bcdd8708efb818d0339e16
Diego Sampaio <chinello@gmail.com>
2021-08-30 11:13:36 -0300

    Remove LoggerManager

--- app/logger/server/index.js
+++ app/logger/server/index.js
@@ -1,6 +1,7 @@
 import { Logger } from '../../../server/lib/logger/Logger';
 import './streamer';
 
+// TODO there are imports pointing to this file still, ideally we should point everything to "/server/lib/logger/Logger" and remove this file
 export {
 	Logger,
 };
