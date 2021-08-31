981b2230b663b22e6f5df4f6bf3f0de93a91a7c0
Douglas Gubert <douglas.gubert@gmail.com>
2021-08-31 19:35:41 -0300

    Fix linter problem

--- app/apps/server/bridges/scheduler.ts
+++ app/apps/server/bridges/scheduler.ts
@@ -165,9 +165,7 @@ export class AppSchedulerBridge extends SchedulerBridge {
 
 		let cancelQuery;
 		try {
-			cancelQuery = {
-				_id: new ObjectID(jobId.split('_')[0])
-			};
+			cancelQuery = { _id: new ObjectID(jobId.split('_')[0]) };
 		} catch (jobDocIdError) {
 			// it is not a valid objectid, so it won't try to cancel by document id
 			cancelQuery = { name: jobId };
