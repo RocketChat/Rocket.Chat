518d0ce36c40ab35161c27426fea7af612412f1a
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-09-03 10:13:45 -0300

    added enterprise attr to all ee migrated settings

--- ee/server/authentication/settings.ts
+++ ee/server/authentication/settings.ts
@@ -35,6 +35,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_NameId_template`, defaultNameIDTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_NameIdPolicy_Template',
 			i18nDescription: 'SAML_NameIdPolicy_Template_Description',
@@ -43,6 +45,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_custom_authn_context`, defaultAuthnContext, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_Custom_Authn_Context',
 			i18nDescription: 'SAML_Custom_Authn_Context_description',
@@ -56,6 +60,8 @@ export const addEnterpriseSettings = function(name: string): void {
 				{ key: 'minimum', i18nLabel: 'Minimum' },
 			],
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_Custom_Authn_Context_Comparison',
 		});
@@ -63,6 +69,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_AuthnContext_template`, defaultAuthnContextTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_AuthnContext_Template',
 			i18nDescription: 'SAML_AuthnContext_Template_Description',
@@ -73,6 +81,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_AuthRequest_template`, defaultAuthRequestTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_AuthnRequest_Template',
 			i18nDescription: 'SAML_AuthnRequest_Template_Description',
@@ -82,6 +92,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_LogoutResponse_template`, defaultLogoutResponseTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_LogoutResponse_Template',
 			i18nDescription: 'SAML_LogoutResponse_Template_Description',
@@ -91,6 +103,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_LogoutRequest_template`, defaultLogoutRequestTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_LogoutRequest_Template',
 			i18nDescription: 'SAML_LogoutRequest_Template_Description',
@@ -100,6 +114,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_MetadataCertificate_template`, defaultMetadataCertificateTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_MetadataCertificate_Template',
 			i18nDescription: 'SAML_Metadata_Certificate_Template_Description',
@@ -109,6 +125,8 @@ export const addEnterpriseSettings = function(name: string): void {
 		settings.add(`SAML_Custom_${ name }_Metadata_template`, defaultMetadataTemplate, {
 			type: 'string',
 			group: 'SAML',
+			enterprise: true,
+			invalidValue: false,
 			section: 'SAML_Section_6_Advanced',
 			i18nLabel: 'SAML_Metadata_Template',
 			i18nDescription: 'SAML_Metadata_Template_Description',
