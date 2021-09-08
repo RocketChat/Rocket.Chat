bc9c16a6a9c4cae9814f4b368f2377b4fb001aba
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-09-08 17:24:26 -0300

    improved parameter types on rolesync callback

--- ee/server/authentication/SAML.ts
+++ ee/server/authentication/SAML.ts
@@ -2,7 +2,7 @@ import { callbacks } from '../../../app/callbacks/server';
 import { onLicense } from '../../app/license/server';
 
 onLicense('SAML-enterprise', () => {
-	callbacks.add('SAML-rolesync', ({ updateData, globalRoles }: {updateData: any; globalRoles: any}) => {
+	callbacks.add('SAML-rolesync', ({ updateData, globalRoles }: {updateData: Record<string, any>; globalRoles: Array<string>}) => {
 		updateData.roles = globalRoles;
 	});
 });
