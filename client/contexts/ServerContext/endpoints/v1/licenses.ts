9d66dc2f530e106ed689cbd894c4ee42c5b8273d
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-09-09 19:10:01 -0300

    Remove unused param type

--- client/contexts/ServerContext/endpoints/v1/licenses.ts
+++ client/contexts/ServerContext/endpoints/v1/licenses.ts
@@ -2,12 +2,12 @@ import type { ILicense } from '../../../../../ee/app/license/server/license';
 
 export type LicensesEndpoints = {
 	'licenses.get': {
-		GET: (params: void) => { licenses: Array<ILicense> };
+		GET: () => { licenses: Array<ILicense> };
 	};
 	'licenses.maxActiveUsers': {
-		GET: (params: void) => { maxActiveUsers: number | null; activeUsers: number };
+		GET: () => { maxActiveUsers: number | null; activeUsers: number };
 	};
 	'licenses.requestSeatsLink': {
-		GET: (params: void) => { url: string };
+		GET: () => { url: string };
 	};
 };
