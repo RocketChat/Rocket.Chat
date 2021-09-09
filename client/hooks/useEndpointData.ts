232b2db99057ba0c9aa18cf2a5c7a02e1ff6fd54
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-09-09 13:49:39 -0300

    Chore: client endpoints typings (#23152)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- client/hooks/useEndpointData.ts
+++ client/hooks/useEndpointData.ts
@@ -1,33 +1,19 @@
 import { useCallback, useEffect } from 'react';
 
-import {
-	ServerEndpointPath,
-	ServerEndpointRequestPayload,
-	ServerEndpointResponsePayload,
-	ServerEndpoints,
-	useEndpoint,
-} from '../contexts/ServerContext';
+import { Serialized } from '../../definition/Serialized';
+import { useEndpoint } from '../contexts/ServerContext';
+import { Params, PathFor, Return } from '../contexts/ServerContext/endpoints';
 import { useToastMessageDispatch } from '../contexts/ToastMessagesContext';
 import { AsyncState, useAsyncState } from './useAsyncState';
 
 const defaultParams = {};
 
-type ServerGetEndpointPaths = {
-	[K in ServerEndpointPath]: ServerEndpoints[K] extends { GET: any } ? K : never;
-};
-
-export const useEndpointData = <
-	_T,
-	Path extends ServerGetEndpointPaths[keyof ServerGetEndpointPaths],
->(
-	endpoint: Path,
-	params: ServerEndpointRequestPayload<'GET', Path> = defaultParams,
-	initialValue?:
-		| ServerEndpointResponsePayload<'GET', Path>
-		| (() => ServerEndpointResponsePayload<'GET', Path>),
-): AsyncState<ServerEndpointResponsePayload<'GET', Path>> & { reload: () => void } => {
-	const { resolve, reject, reset, ...state } =
-		useAsyncState<ServerEndpointResponsePayload<'GET', Path>>(initialValue);
+export const useEndpointData = <P extends PathFor<'GET'>>(
+	endpoint: P,
+	params: Params<'GET', P>[0] = defaultParams as Params<'GET', P>[0],
+	initialValue?: Serialized<Return<'GET', P>> | (() => Serialized<Return<'GET', P>>),
+): AsyncState<Serialized<Return<'GET', P>>> & { reload: () => void } => {
+	const { resolve, reject, reset, ...state } = useAsyncState(initialValue);
 	const dispatchToastMessage = useToastMessageDispatch();
 	const getData = useEndpoint('GET', endpoint);
 
