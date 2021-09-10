55f6a2dae0a11896dbd8693311a63cefd9f501d7
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-09-10 15:08:04 -0300

    added attr

--- ee/server/oAuth/settings.ts
+++ ee/server/oAuth/settings.ts
@@ -2,7 +2,9 @@ import { onLicense } from '../../app/license/server';
 import { settings } from '../../../app/settings/server/functions/settings';
 
 export function addEnterpriseOAuthSettings(name: string, values: any): void {
+	console.log('add enterprise o auth settings');
 	onLicense('oAuth-enterprise', () => {
-		settings.add(`Accounts_OAuth_Custom-${ name }-map_channels`, values.mapChannels || false, { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Map_Channels', enterprise: true });
+		console.log('license activated (:');
+		settings.add(`Accounts_OAuth_Custom-${ name }-map_channels`, values.mapChannels || false, { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Map_Channels', enterprise: true, invalidValue: false });
 	});
 }
