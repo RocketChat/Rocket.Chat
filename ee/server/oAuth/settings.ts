146590448d905f9d3537669f48e5f21342b02226
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-09-10 23:52:56 -0300

    created EnterpriseOAuthHelpers

--- ee/server/oAuth/settings.ts
+++ ee/server/oAuth/settings.ts
@@ -2,9 +2,7 @@ import { onLicense } from '../../app/license/server';
 import { settings } from '../../../app/settings/server/functions/settings';
 
 export function addEnterpriseOAuthSettings(name: string, values: any): void {
-	console.log('add enterprise o auth settings');
 	onLicense('oAuth-enterprise', () => {
-		console.log('license activated (:');
 		settings.add(`Accounts_OAuth_Custom-${ name }-map_channels`, values.mapChannels || false, { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Map_Channels', enterprise: true, invalidValue: false });
 	});
 }
