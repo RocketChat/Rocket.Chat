a5b198444050a8dcfe929c40b8b004138c467736
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-09-13 09:14:06 -0300

    removed merge roles from SSO setting from CE

--- ee/server/oAuth/settings.ts
+++ ee/server/oAuth/settings.ts
@@ -4,5 +4,6 @@ import { settings } from '../../../app/settings/server/functions/settings';
 export function addEnterpriseOAuthSettings(name: string, values: any): void {
 	onLicense('oAuth-enterprise', () => {
 		settings.add(`Accounts_OAuth_Custom-${ name }-map_channels`, values.mapChannels || false, { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Map_Channels', enterprise: true, invalidValue: false });
+		settings.add(`Accounts_OAuth_Custom-${ name }-merge_roles`, values.mergeRoles || false, { type: 'boolean', group: 'OAuth', section: `Custom OAuth: ${ name }`, i18nLabel: 'Accounts_OAuth_Custom_Merge_Roles', enterprise: true, invalidValue: false });
 	});
 }
