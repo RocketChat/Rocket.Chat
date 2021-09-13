915ac10a0d81b0512fe51756de6dc20f00fe2012
Kevin Aleman <kevin.aleman@rocket.chat>
2021-09-13 14:35:21 -0600

    remove livechat references

--- server/services/omnichannel-voip/service.ts
+++ server/services/omnichannel-voip/service.ts
@@ -1,7 +1,7 @@
 import { ILivechatVoipService } from '../../sdk/types/IOmnichannelVoipService';
 import { ServiceClass } from '../../sdk/types/ServiceClass';
 
-export class LivechatVoipService extends ServiceClass implements ILivechatVoipService {
+export class OmnichannelVoipService extends ServiceClass implements ILivechatVoipService {
 	protected name = 'omnichannel-voip';
 
 	getConfiguration(): any {
