4233e5332a1b9da91eb356ddb78b94560ed1f0d1
amolghode <amol.ghode@gmail.com>
2021-09-14 12:58:49 +0530

    Clickup Tasks : https://app.clickup.com/t/7qdnh4 Description : Added missing return type for a function.

--- client/views/voip/VoIPRouter.tsx
+++ client/views/voip/VoIPRouter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import VoIPLayout from './VoIPLayout';
 
-function VoIPRouter() {
+function VoIPRouter(): React.ReactElement {
 	return <VoIPLayout></VoIPLayout>;
 }
 export default VoIPRouter;
