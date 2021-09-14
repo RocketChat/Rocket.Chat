2c8e1a16c00a316c3edc9b3cf64340c2f3b14873
thassiov <tvmcarvalho@gmail.com>
2021-09-14 12:46:56 -0300

    Simplify retrieval of livechat room data

--- app/livechat/server/lib/Livechat.js
+++ app/livechat/server/lib/Livechat.js
@@ -1107,13 +1107,8 @@ export const Livechat = {
 		}
 
 		const ignoredMessageTypes = ['livechat_navigation_history', 'livechat_transcript_history', 'command', 'livechat-close', 'livechat-started', 'livechat_video_call'];
-		const messageCursor = Promise.await(Messages.findVisibleByRoomIdNotContainingTypes(rid, ignoredMessageTypes, { sort: { ts: 1 } }));
 
-		const messages = [];
-
-		messageCursor.forEach((message) => messages.push(message));
-
-		return messages;
+		 return Messages.findVisibleByRoomIdNotContainingTypes(rid, ignoredMessageTypes, { sort: { ts: 1 } }).fetch();
 	},
 
 	requestTranscript({ rid, email, subject, user }) {
