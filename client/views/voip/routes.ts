1dbe3a8b8379470dfe8edf87705e67370692c1b5
amolghode <amol.ghode@gmail.com>
2021-09-14 12:40:06 +0530

    Clickup Tasks : https://app.clickup.com/t/7qdnh4 Description : Converting javascript files to typescript files.

--- client/views/voip/routes.ts
+++ client/views/voip/routes.ts
@@ -1,7 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
 import { createRouteGroup } from '../../lib/createRouteGroup';
 
 export const registerVoIPRoutes = createRouteGroup('voip', '/voip', () => import('./VoIPRouter'));
-
-Meteor.startup(() => {});
