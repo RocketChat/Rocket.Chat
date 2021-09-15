a663612a680a81f16bd1c00cd1580f2b481578a8
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2021-09-15 11:09:22 -0300

    [NEW] Seats Cap: Request seats link (#23151)
    * create endpoint and consume it in the ui
    
    * Fix review
    
    * Remove unused param type
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

---
+++ ee/app/license/server/getSeatsRequestLink.ts
@@ -0,0 +1,23 @@
+import { Settings, Users } from '../../../../app/models/server';
+import { ISetting } from '../../../../definition/ISetting';
+
+type WizardSettings = Array<ISetting>;
+
+const url = 'https://rocket.chat/sales-contact';
+
+export const getSeatsRequestLink = (): string => {
+	const workspaceId = Settings.findOneById('Cloud_Workspace_Id');
+	const activeUsers = Users.getActiveLocalUserCount();
+	const wizardSettings: WizardSettings = Settings.findSetupWizardSettings().fetch();
+
+	const utmUrl = new URL(url);
+	utmUrl.searchParams.append('workspaceId', String(workspaceId.value));
+	utmUrl.searchParams.append('activeUsers', String(activeUsers));
+	wizardSettings.forEach((setting) => {
+		if (['Industry', 'Country', 'Size'].includes(setting._id)) {
+			utmUrl.searchParams.append(setting._id.toLowerCase(), String(setting.value));
+		}
+	});
+
+	return utmUrl.toString();
+};
