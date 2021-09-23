7110ddf341f085e6f8c0d3e9c6c32537ddcc1181
Diego Sampaio <chinello@gmail.com>
2021-09-23 19:51:05 -0300

    Revert cron

--- server/startup/cron.js
+++ server/startup/cron.js
@@ -2,6 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
 
 import { Logger } from '../../app/logger';
+import { oembedCron } from '../cron/oembed';
+import { statsCron } from '../cron/statistics';
+import { npsCron } from '../cron/nps';
+import { federationCron } from '../cron/federation';
 
 const logger = new Logger('SyncedCron');
 
@@ -13,14 +17,10 @@ SyncedCron.config({
 });
 
 Meteor.defer(async function() {
-	const { oembedCron } = await import('../cron/oembed');
-	const { statsCron } = await import('../cron/statistics');
-	const { npsCron } = await import('../cron/nps');
-	const { federationCron } = await import('../cron/federation');
-
 	oembedCron(SyncedCron);
 	statsCron(SyncedCron, logger);
 	npsCron(SyncedCron);
 	federationCron(SyncedCron);
+
 	SyncedCron.start();
 });
