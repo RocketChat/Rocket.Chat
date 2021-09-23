c2ab5e128cc70652081000c25e9d5d51561a3db8
pierre-lehnen-rc <55164754+pierre-lehnen-rc@users.noreply.github.com>
2021-09-23 20:52:35 -0300

    Regression: LDAP Refactoring (#23231)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- server/lib/ldap/Logger.ts
+++ server/lib/ldap/Logger.ts
@@ -5,3 +5,4 @@ export const connLogger = logger.section('Connection');
 export const bindLogger = logger.section('Bind');
 export const searchLogger = logger.section('Search');
 export const authLogger = logger.section('Auth');
+export const mapLogger = logger.section('Mapping');
