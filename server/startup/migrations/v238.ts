23a982fccfa617e630543951988c5fc9a3650d7d
thassiov <tvmcarvalho@gmail.com>
2021-09-24 16:22:33 -0300

    Adjustments based on linter feedback

--- server/startup/migrations/v238.ts
+++ server/startup/migrations/v238.ts
@@ -1,9 +1,35 @@
 import AdmZip from 'adm-zip';
 
 import { AppManager } from '@rocket.chat/apps-engine/server/AppManager';
-
 import { addMigration } from '../../lib/migrations';
 import { Apps } from '../../../app/apps/server';
+
+function isPreCompilerRemoval(app: any): boolean {
+	const fileNames = Object.keys(app.compiled);
+	return fileNames.some((file) => file.endsWith('$ts'));
+}
+
+function repackageAppZip(app: any): Buffer {
+	const zip = new AdmZip();
+
+	const sourceFiles: string[] = [];
+
+	Object.entries(app.compiled).forEach(([key, value]) => {
+		const actualFileName = key.endsWith('$ts') ? key.replace(/\$ts$/, '.js') : key;
+		sourceFiles.push(actualFileName);
+		zip.addFile(actualFileName, Buffer.from(value as string, 'utf8'));
+	});
+
+	const zipToRead = new AdmZip(Buffer.from(app.zip, 'base64'));
+
+	zipToRead.forEach((entry: any) => {
+		if (!sourceFiles.includes(entry.entryName)) {
+			zip.addFile(entry.entryName, entry.getData());
+		}
+	});
+
+	return zip.toBuffer();
+}
 
 addMigration({
 	version: 238,
@@ -19,30 +45,3 @@ addMigration({
 		}
 	},
 });
-
-function isPreCompilerRemoval(app: any): boolean {
-  const fileNames = Object.keys(app.compiled);
-  return fileNames.some(file => file.endsWith('$ts')) ;
-}
-
-function repackageAppZip(app: any): Buffer {
-  const zip = new AdmZip();
-
-  const sourceFiles: string[] = [];
-
-  Object.entries(app.compiled).forEach(([key, value]) => {
-    const actualFileName = key.endsWith('$ts') ? key.replace(/\$ts$/, '.js') : key;
-    sourceFiles.push(actualFileName);
-    zip.addFile(actualFileName, Buffer.from(value as string, 'utf8'));
-  });
-
-  const zipToRead = new AdmZip(Buffer.from(app.zip, 'base64'));
-
-  zipToRead.forEach((entry: any) => {
-    if (!sourceFiles.includes(entry.entryName)) {
-      zip.addFile(entry.entryName, entry.getData());
-    }
-  });
-
-  return zip.toBuffer();
-}
