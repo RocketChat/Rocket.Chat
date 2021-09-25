40e28e5db8f1780158481e108d352f47d1318e32
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-24 23:32:56 -0300

    .

--- app/lib/server/functions/validateName.ts
+++ app/lib/server/functions/validateName.ts
@@ -2,6 +2,7 @@ import { SettingsVersion4 } from '../../../settings/server';
 
 export const validateName = function(name: string): boolean {
 	const blockedNames = SettingsVersion4.get('Accounts_SystemBlockedUsernameList');
+	console.log(blockedNames);
 	if (!blockedNames || typeof blockedNames !== 'string') {
 		return true;
 	}
