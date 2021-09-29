2f46931fa4a7cf54ae476103cd48d815116f95e7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-29 00:58:50 -0300

    ..

--- app/notifications/client/lib/Presence.ts
+++ app/notifications/client/lib/Presence.ts
@@ -1,12 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Presence, STATUS_MAP } from '../../../../client/lib/presence';
-import { UserStatus } from '../../../../definition/UserStatus';
 
 (Meteor as any).StreamerCentral.on('stream-user-presence', (uid: string, args: unknown) => {
 	if (!Array.isArray(args)) {
 		throw new Error('Presence event must be an array');
 	}
-	const [username, status, statusText] = args as [string, UserStatus, string | undefined];
+	const [username, status, statusText] = args as [string, number, string | undefined];
 	Presence.notify({ _id: uid, username, status: STATUS_MAP[status], statusText });
 });
