63d28a54f417471765ec1a2dcc448daaab7c0464
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2021-09-30 13:13:36 +0530

    [NEW] Add new Endpoints to manage VoIP server configs (#23239)
    * create voip server config collection in DB + modify interfaces
    
    * Add new endpoints to fetch management and server-config info
    
    * Add new endpoints to add or update voip server configs
    
    * Add translations + move serverName property to IVoipServerConfig interface
    
    * Remove VoipServerConfiguration DB model and rely on DB Raw module to create collection
    
    * archive server configs instead of completely deleting them upon update/insert
    
    * Add comment for future scope
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Aleman <kevin.aleman@rocket.chat>
    
    * Apply suggestions from code review
    
    * remove deactive logic from endpoint
    
    Co-authored-by: Kevin Aleman <kevin.aleman@rocket.chat>

--- server/sdk/types/IVoipService.ts
+++ server/sdk/types/IVoipService.ts
@@ -1,3 +1,9 @@
+import { IVoipServerConfig, ServerType } from '../../../definition/IVoipServerConfig';
+
 export interface IVoipService {
 	getConfiguration(): any;
+	getServerConfigData(serverType: ServerType): Promise<IVoipServerConfig | null>;
+	addServerConfigData(config: Omit<IVoipServerConfig, '_id' | '_updatedAt'>): Promise<boolean>;
+	updateServerConfigData(config: Omit<IVoipServerConfig, '_id' | '_updatedAt'>): Promise<boolean>;
+	deactivateServerConfigDataIfAvailable(serverType: ServerType): Promise<boolean>;
 }
