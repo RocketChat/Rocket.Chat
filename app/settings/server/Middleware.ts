13ebb7e753fb7cf0b3bd2603bb315c50165b4e18
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-30 09:46:54 -0300

    ...

--- app/settings/server/Middleware.ts
+++ app/settings/server/Middleware.ts
@@ -5,6 +5,6 @@ type Middleware<T extends (...args: any[]) => any> = (context: Parameters<T>, ne
 const pipe = <T extends (...args: any[]) => any>(fn: T) => (...args: Parameters<T>): ReturnType<T> => fn(...args);
 
 
-export const use = <T extends (...args: any[]) => any>(fn: T, middleware: Middleware<T>): T => function(...context: Parameters<T>): ReturnType<T> {
+export const use = <T extends (...args: any[]) => any>(fn: T, middleware: Middleware<T>): T => function(this: unknown, ...context: Parameters<T>): ReturnType<T> {
 	return middleware(context, pipe(fn.bind(this)));
 } as T;
