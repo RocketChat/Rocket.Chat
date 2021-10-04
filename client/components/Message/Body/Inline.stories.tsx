2bc8460506bcc69b61b2b686c2dd628d81d0d2c9
Damaso Junio <junio.pereirab@gmail.com>
2021-10-03 23:18:47 -0300

    Fix addresses to body stories

--- client/components/Message/Body/Inline.stories.tsx
+++ client/components/Message/Body/Inline.stories.tsx
@@ -3,17 +3,17 @@
 import { Plain } from '@rocket.chat/message-parser';
 import React, { ReactElement } from 'react';
 
-import InlineCode from './InlineCode';
+import Inline from './Inline';
 
 export default {
-	title: 'Message/Body/InlineCode',
-	component: InlineCode,
+	title: 'Message/Body/Inline',
+	component: Inline,
 	decorators: [(storyFn: any): ReactElement => storyFn()],
 };
 
 const defaultValue: Plain = {
 	type: 'PLAIN_TEXT',
-	value: 'Test inline code',
+	value: 'Test inline',
 };
 
-export const Default = () => <InlineCode value={defaultValue} />;
+export const Default = () => <Inline value={[defaultValue]} />;
