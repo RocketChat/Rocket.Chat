1792bee5006b7a5222ed71c1cc8659966cc93aff
Damaso Junio <junio.pereirab@gmail.com>
2021-10-03 21:28:07 -0300

    Create Avatar stories tests

---
+++ client/components/avatar/BaseAvatar.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import BaseAvatar from './BaseAvatar';
+
+export default {
+	title: 'components/Avatar/BaseAvatar',
+	component: BaseAvatar,
+};
+
+export const Default = () => (
+	<BaseAvatar url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAlEtR66nY8F0EqcmCDI_rX5HHaWYmZezmil4RvuJZwkhymEBkwUBeo26KLaYYm_P4bu0&usqp=CAU' />
+);
