e68e0359d6a986696e7d2a5386801957695c166d
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-11 00:02:30 -0300

    Document /api/v1/e2e.updateGroupKey

--- app/api/server/v1/e2e.js
+++ app/api/server/v1/e2e.js
@@ -69,7 +69,7 @@ API.v1.addRoute('e2e.setRoomKeyID', { authRequired: true }, {
  * @openapi
  *  /api/v1/e2e.setUserPublicAndPrivateKeys:
  *    post:
- *      description: Sets the end-to-end keys for the authenticated user
+ *      description: Sets the end-to-end encryption keys for the authenticated user
  *      security:
  *        - autenticated: {}
  *      requestBody:
@@ -111,6 +111,39 @@ API.v1.addRoute('e2e.setUserPublicAndPrivateKeys', { authRequired: true }, {
 	},
 });
 
+/**
+ * @openapi
+ *  /api/v1/e2e.updateGroupKey:
+ *    post:
+ *      description: Updates the end-to-end encryption key for a user on a room
+ *      security:
+ *        - autenticated: {}
+ *      requestBody:
+ *        description: A tuple containing the user ID, the room ID, and the key
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                uid:
+ *                  type: string
+ *                rid:
+ *                  type: string
+ *                key:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/ApiSuccessV1'
+ *        default:
+ *          description: Unexpected error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/ApiFailureV1'
+ */
 API.v1.addRoute('e2e.updateGroupKey', { authRequired: true }, {
 	post() {
 		const { uid, rid, key } = this.bodyParams;
