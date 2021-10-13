d9128d967e9d18981b7b9bde1a77d8a71b8616a1
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-13 10:23:28 -0300

    Remove typings for async_hooks

--- server/main.d.ts
+++ server/main.d.ts
@@ -105,21 +105,3 @@ declare module 'meteor/mongo' {
 		function Connection(): ConnectionClass;
 	}
 }
-
-declare module 'async_hooks' {
-	export class AsyncLocalStorage<T> {
-		disable(): void;
-
-		getStore(): T | undefined;
-
-		run(store: T, callback: (...args: any[]) => void, ...args: any[]): void;
-
-		exit(callback: (...args: any[]) => void, ...args: any[]): void;
-
-		runSyncAndReturn<R>(store: T, callback: (...args: any[]) => R, ...args: any[]): R;
-
-		exitSyncAndReturn<R>(callback: (...args: any[]) => R, ...args: any[]): R;
-
-		enterWith(store: T): void;
-	}
-}
