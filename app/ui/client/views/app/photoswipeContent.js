0a185c03e88988f7c951c0afc70496a87512f99a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-19 00:15:37 -0300

    Workaround Photoswipe bug

--- app/ui/client/views/app/photoswipeContent.js
+++ app/ui/client/views/app/photoswipeContent.js
@@ -5,6 +5,68 @@ import { escapeHTML } from '@rocket.chat/string-helpers';
 
 import './photoswipeContent.html';
 
+const parseLength = (x) => {
+	const length = typeof x === 'string' ? parseInt(x, 10) : undefined;
+	return Number.isFinite(length) ? length : undefined;
+};
+
+const getImageSize = (src) => new Promise((resolve, reject) => {
+	const img = new Image();
+
+	img.addEventListener('load', () => {
+		resolve([img.naturalWidth, img.naturalHeight]);
+	});
+
+	img.addEventListener('error', (error) => {
+		reject(error.error);
+	});
+
+	img.src = src;
+});
+
+const fromElementToSlide = async (element) => {
+	if (element instanceof HTMLAnchorElement) {
+		const item = {
+			src: element.dataset.src || element.href,
+			w: parseLength(element.dataset.width),
+			h: parseLength(element.dataset.height),
+			title: element.dataset.title || element.title,
+			description: element.dataset.description,
+		};
+
+		if (!Number.isFinite(item.w) || !Number.isFinite(item.h)) {
+			[item.w, item.h] = await getImageSize(item.src);
+		}
+
+		return item;
+	}
+
+	if (element instanceof HTMLImageElement) {
+		const item = {
+			src: element.src,
+			w: element.naturalWidth,
+			h: element.naturalHeight,
+			title: element.dataset.title || element.title,
+			description: element.dataset.description,
+		};
+
+		if (element.dataset.src) {
+			item.msrc = element.src;
+			item.src = element.dataset.src;
+			item.w = parseLength(element.dataset.width);
+			item.h = parseLength(element.dataset.height);
+
+			if (!Number.isFinite(item.w) || !Number.isFinite(item.h)) {
+				[item.w, item.h] = await getImageSize(item.src);
+			}
+		}
+
+		return item;
+	}
+
+	return null;
+};
+
 Meteor.startup(() => {
 	let currentGallery = null;
 	const initGallery = async (items, options) => {
@@ -13,10 +75,13 @@ Meteor.startup(() => {
 		if (!currentGallery) {
 			const PhotoSwipe = PhotoSwipeImport.default;
 			const PhotoSwipeUI_Default = PhotoSwipeUI_DefaultImport.default;
+
 			currentGallery = new PhotoSwipe(document.getElementById('pswp'), PhotoSwipeUI_Default, items, options);
+
 			currentGallery.listen('destroy', () => {
 				currentGallery = null;
 			});
+
 			currentGallery.init();
 		}
 	};
@@ -27,78 +92,48 @@ Meteor.startup(() => {
 		counterEl: false,
 		shareEl: false,
 		clickToCloseNonZoomable: false,
+		index: 0,
+		addCaptionHTMLFn(item, captionEl) {
+			captionEl.children[0].innerHTML = `
+				${ escapeHTML(item.title) }<br/>
+				<small>${ escapeHTML(item.description) }</small>
+			`;
+			return true;
+		},
 	};
 
 	const createEventListenerFor = (className) => (event) => {
 		event.preventDefault();
 		event.stopPropagation();
 
-		const galleryOptions = {
-			...defaultGalleryOptions,
-			index: 0,
-			addCaptionHTMLFn(item, captionEl) {
-				captionEl.children[0].innerHTML = `${ escapeHTML(item.title) }<br/><small>${ escapeHTML(item.description) }</small>`;
-				return true;
-			},
-		};
+		const { currentTarget } = event;
 
-		const items = Array.from(document.querySelectorAll(className))
-			.map((element, i) => {
-				if (element === event.currentTarget) {
-					galleryOptions.index = i;
+		Array.from(document.querySelectorAll(className))
+			.sort((a, b) => {
+				if (a === currentTarget) {
+					return -1;
 				}
 
-				const item = {
-					src: element.src,
-					w: element.naturalWidth,
-					h: element.naturalHeight,
-					title: element.dataset.title || element.title,
-					description: element.dataset.description,
-				};
-
-				if (element.dataset.src || element.href) {
-					// use stored sizes if available
-					if (element.dataset.width && element.dataset.height) {
-						return {
-							...item,
-							h: element.dataset.height,
-							w: element.dataset.width,
-							src: element.dataset.src || element.href,
-						};
-					}
-
-					const img = new Image();
-
-					img.addEventListener('load', () => {
-						if (!currentGallery) {
-							return;
-						}
-
-						// stores loaded sizes on original image element
-						element.dataset.width = img.naturalWidth;
-						element.dataset.height = img.naturalHeight;
-
-						delete currentGallery.items[i].html;
-						currentGallery.items[i].src = img.src;
-						currentGallery.items[i].w = img.naturalWidth;
-						currentGallery.items[i].h = img.naturalHeight;
-						currentGallery.invalidateCurrItems();
-						currentGallery.updateSize(true);
-					});
-
-					img.src = element.dataset.src || element.href;
-
-					return {
-						...item,
-						msrc: element.src,
-						src: element.dataset.src || element.href,
-					};
+				if (b === currentTarget) {
+					return 1;
 				}
 
-				return item;
-			});
+				return 0;
+			})
+			.map((element) => fromElementToSlide(element))
+			.reduce((p, curr) => p.then(() => curr).then((slide) => {
+				if (!slide) {
+					return;
+				}
 
-		initGallery(items, galleryOptions);
+				if (!currentGallery) {
+					return initGallery([slide], defaultGalleryOptions);
+				}
+
+				currentGallery.items.push(slide);
+				currentGallery.invalidateCurrItems();
+				currentGallery.updateSize(true);
+			}), Promise.resolve());
 	};
 
 	$(document).on('click', '.gallery-item', createEventListenerFor('.gallery-item'));
