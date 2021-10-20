29c32e9913ba2dc9dbeb7d054f740f00fb0f6d40
Caue Felchar <caue.felchar@rocket.chat>
2021-10-20 18:07:13 -0300

    Removing uneeded migration

--- server/startup/migrations/index.ts
+++ server/startup/migrations/index.ts
@@ -64,5 +64,4 @@ import './v237';
 import './v238';
 import './v239';
 import './v240';
-import './v241';
 import './xrun';
--- server/startup/migrations/v241.ts
+++
@@ -1,9 +0,0 @@
-import { addMigration } from '../../lib/migrations';
-import { Permissions } from '../../../app/models/server';
-
-addMigration({
-	version: 241,
-	up() {
-		(Permissions as any).create("view-omnichannel-contact-center",['livechat-manager', 'livechat-agent', 'livechat-monitor', 'admin']);
-	},
-});
