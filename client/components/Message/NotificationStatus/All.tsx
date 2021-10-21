8e48acb1726906fbcd353a7bb25eccc6e023a927
Damaso Junio <junio.pereirab@gmail.com>
2021-10-21 00:49:22 -0300

    Parse followStyle and All (NotificationStatus) from JS to TSX

---
+++ client/components/Message/NotificationStatus/All.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+import NotificationStatus from './NotificationStatus';
+
+const All: FC = (props) => <NotificationStatus label='mention-all' bg='#F38C39' {...props} />;
+
+export default All;
