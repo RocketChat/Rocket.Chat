1936291a6d23ac2c31bc0beb2ff02f1d13810c15
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-10-21 08:24:38 -0300

    Convert PageSkeleton from js to tsx

--- client/components/Page/PageHeader.tsx
+++ client/components/Page/PageHeader.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@rocket.chat/fuselage';
 import { useMediaQuery } from '@rocket.chat/fuselage-hooks';
-import React, { useContext, FC } from 'react';
+import React, { useContext, FC, ReactElement } from 'react';
 
 import { useSession } from '../../contexts/SessionContext';
 import { useSidebar } from '../../contexts/SidebarContext';
@@ -8,7 +8,7 @@ import BurgerMenuButton from '../burger/BurgerMenuButton';
 import PageContext from './PageContext';
 
 type PageHeaderProps = {
-	title: string;
+	title: string | ReactElement;
 };
 
 const PageHeader: FC<PageHeaderProps> = ({ children = undefined, title, ...props }) => {
