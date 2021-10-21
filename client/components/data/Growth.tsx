8e663631c6d75f162ab3cc8128ab17318ab52822
Damaso Junio <junio.pereirab@gmail.com>
2021-10-21 00:42:02 -0300

    Parse Growth component from JS to TSX

--- client/components/data/Growth.tsx
+++ client/components/data/Growth.tsx
@@ -1,20 +1,25 @@
 import { Box } from '@rocket.chat/fuselage';
-import React from 'react';
+import React, { FC } from 'react';
 
 import NegativeGrowthSymbol from './NegativeGrowthSymbol';
 import PositiveGrowthSymbol from './PositiveGrowthSymbol';
 
-function Growth({ children, ...props }) {
-	if (children === 0) {
+type FontScale = 'h1' | 's1' | 's2' | 'p1' | 'p2' | 'c1' | 'c2' | 'micro';
+interface IGrowth {
+	fontScale: FontScale;
+}
+
+const Growth: FC<IGrowth> = ({ children, ...props }) => {
+	if (children === 0 || children === null || children === undefined) {
 		return null;
 	}
 
 	return (
 		<Box is='span' color={children < 0 ? 'danger' : 'success'} {...props}>
 			{children < 0 ? <NegativeGrowthSymbol /> : <PositiveGrowthSymbol />}
-			{String(Math.abs(children))}
+			{String(Math.abs(Number(children)))}
 		</Box>
 	);
-}
+};
 
 export default Growth;
