3aa296c16b107b6da36ed32e1cf4bad3e313255a
Chad Kullhem <TheDigitalEagle@users.noreply.github.com>
2021-10-23 22:41:41 -0600

    Update beforeCreateRoom.js
    Fix typo changing from E2E_Enabled to E2E_Enable

--- app/e2e/server/beforeCreateRoom.js
+++ app/e2e/server/beforeCreateRoom.js
@@ -3,7 +3,7 @@ import { settings } from '../../settings/server';
 
 callbacks.add('beforeCreateRoom', ({ type, extraData }) => {
 	if (
-		settings.get('E2E_Enabled') && ((type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
+		settings.get('E2E_Enable') && ((type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
 		|| (type === 'p' && settings.get('E2E_Enabled_Default_PrivateRooms')))
 	) {
 		extraData.encrypted = extraData.encrypted ?? true;
