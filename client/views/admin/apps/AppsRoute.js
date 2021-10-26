9722f2806f21d4a683e72e0df8aabbdab29bbfbf
dougfabris <devfabris@gmail.com>
2021-10-26 14:32:28 -0300

    improve: finished the page merging and routing logic

--- client/views/admin/apps/AppsRoute.js
+++ client/views/admin/apps/AppsRoute.js
@@ -1,4 +1,3 @@
-import { Tabs } from '@rocket.chat/fuselage';
 import React, { useState, useEffect } from 'react';
 
 import NotAuthorizedPage from '../../../components/NotAuthorizedPage';
@@ -6,16 +5,13 @@ import PageSkeleton from '../../../components/PageSkeleton';
 import { usePermission } from '../../../contexts/AuthorizationContext';
 import { useRouteParameter, useRoute } from '../../../contexts/RouterContext';
 import { useMethod } from '../../../contexts/ServerContext';
-import { useTranslation } from '../../../contexts/TranslationContext';
 import AppDetailsPage from './AppDetailsPage';
 import AppInstallPage from './AppInstallPage';
 import AppLogsPage from './AppLogsPage';
 import AppsPage from './AppsPage';
 import AppsProvider from './AppsProvider';
-import MarketplacePage from './MarketplacePage';
 
-function AppsRoute() {
-	const t = useTranslation();
+const AppsRoute = () => {
 	const [isLoading, setLoading] = useState(true);
 	const canViewAppsAndMarketplace = usePermission('manage-apps');
 	const isAppsEngineEnabled = useMethod('apps/is-enabled');
@@ -55,8 +51,6 @@ function AppsRoute() {
 	const id = useRouteParameter('id');
 	const version = useRouteParameter('version');
 
-	const marketplaceRoute = useRoute('admin-marketplace');
-
 	if (!canViewAppsAndMarketplace) {
 		return <NotAuthorizedPage />;
 	}
@@ -67,30 +61,14 @@ function AppsRoute() {
 
 	return (
 		<AppsProvider>
-			<Tabs>
-				<Tabs.Item
-					onClick={() => marketplaceRoute.push({ context: 'app' })}
-					selected={context === 'app'}
-				>
-					{t('Apps')}
-				</Tabs.Item>
-				<Tabs.Item onClick={() => marketplaceRoute.push({ context: '' })} selected={isMarketPlace}>
-					{t('Marketplace')}
-				</Tabs.Item>
-			</Tabs>
-			{/* <Route path='/'>
-				<MarketplacePage />
-			</Route>
-			<Route path='/app'>
-				<AppInstallPage />
-			</Route> */}
-			{(!context && isMarketPlace && <MarketplacePage />) ||
-				(context === 'app' && <AppsPage />) ||
+			{((!context || context === 'installed') && (
+				<AppsPage isMarketPlace={isMarketPlace} context={context} />
+			)) ||
 				(context === 'details' && <AppDetailsPage id={id} marketplaceVersion={version} />) ||
 				(context === 'logs' && <AppLogsPage id={id} />) ||
 				(context === 'install' && <AppInstallPage />)}
 		</AppsProvider>
 	);
-}
+};
 
 export default AppsRoute;
