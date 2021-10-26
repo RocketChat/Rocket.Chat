880789269530a798d217077c64e03cadeba5798a
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-10-26 17:02:54 -0300

    added ? for user not being created verification

--- app/authentication/server/lib/restrictLoginAttempts.ts
+++ app/authentication/server/lib/restrictLoginAttempts.ts
@@ -92,7 +92,7 @@ export const isValidAttemptByUser = async (login: ILoginAttempt): Promise<boolea
 
 	const user = login.user || await Users.findOneByUsername(login.methodArguments[0].user?.username);
 
-	if (!user.username) {
+	if (!user?.username) {
 		return true;
 	}
 
