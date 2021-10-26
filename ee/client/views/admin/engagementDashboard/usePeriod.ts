4db20779a3157c401d2215ced7133ee32ed08ba7
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-26 16:24:17 -0300

    Refactor usePeriod

--- ee/client/views/admin/engagementDashboard/usePeriod.ts
+++ ee/client/views/admin/engagementDashboard/usePeriod.ts
@@ -1,75 +1,28 @@
-import moment from 'moment';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { TranslationKey, useTranslation } from '../../../../../client/contexts/TranslationContext';
+import { useTranslation } from '../../../../../client/contexts/TranslationContext';
+import { getPeriod, getPeriodRange, Period, periods } from './utils/periods';
 
-const periodKeys = ['last 7 days', 'last 30 days', 'last 90 days'] as const;
-
-type PeriodKey = typeof periodKeys[number];
-
-const periodLabelKeys: Record<PeriodKey, TranslationKey> = {
-	'last 7 days': 'Last_7_days',
-	'last 30 days': 'Last_30_days',
-	'last 90 days': 'Last_90_days',
-};
-
-const isValidPeriodKey = (periodKey: string): periodKey is PeriodKey =>
-	periodKeys.some((_periodKey) => _periodKey === periodKey);
-
-const computePeriod = (
-	periodKey: PeriodKey,
-	utc: boolean,
-): { readonly start: Date; readonly end: Date } => {
-	switch (periodKey) {
-		case 'last 7 days':
-			return {
-				start: utc
-					? moment.utc().startOf('day').subtract(7, 'days').toDate()
-					: moment().startOf('day').subtract(8, 'days').toDate(),
-				end: utc
-					? moment.utc().endOf('day').subtract(1, 'days').toDate()
-					: moment().endOf('day').toDate(),
-			};
-
-		case 'last 30 days':
-			return {
-				start: utc
-					? moment.utc().startOf('day').subtract(30, 'days').toDate()
-					: moment().startOf('day').subtract(31, 'days').toDate(),
-				end: utc
-					? moment.utc().endOf('day').subtract(1, 'days').toDate()
-					: moment().endOf('day').toDate(),
-			};
-
-		case 'last 90 days':
-			return {
-				start: utc
-					? moment.utc().startOf('day').subtract(90, 'days').toDate()
-					: moment().startOf('day').subtract(91, 'days').toDate(),
-				end: utc
-					? moment.utc().endOf('day').subtract(1, 'days').toDate()
-					: moment().endOf('day').toDate(),
-			};
-	}
-};
+const isValidPeriodKey = (periodKey: string): periodKey is Period['key'] =>
+	periods.some((period) => period.key === periodKey);
 
 export const usePeriod = ({ utc = false }: { utc?: boolean } = {}): [
 	period: { readonly start: Date; readonly end: Date },
 	selectProps: {
-		value: PeriodKey;
-		options: readonly (readonly [periodKey: PeriodKey, label: string])[];
+		value: Period['key'];
+		options: readonly (readonly [periodKey: Period['key'], label: string])[];
 		onChange: (value: string) => void;
 	},
 	periodLabel: string,
 ] => {
 	const t = useTranslation();
 
-	const options = useMemo<readonly (readonly [periodKey: PeriodKey, label: string])[]>(
-		() => periodKeys.map((periodKey) => [periodKey, t(periodLabelKeys[periodKey])]),
+	const options = useMemo<readonly (readonly [periodKey: Period['key'], label: string])[]>(
+		() => periods.map(({ key, label }) => [key, t(...label)]),
 		[t],
 	);
 
-	const [periodKey, setPeriodKey] = useState<PeriodKey>(periodKeys[0]);
+	const [periodKey, setPeriodKey] = useState<Period['key']>(periods[0].key);
 
 	const onChange = useCallback((value: string): void => {
 		if (isValidPeriodKey(value)) {
@@ -77,10 +30,10 @@ export const usePeriod = ({ utc = false }: { utc?: boolean } = {}): [
 		}
 	}, []);
 
-	const [period, setPeriod] = useState(() => computePeriod(periodKey, utc));
+	const [period, setPeriod] = useState(() => getPeriodRange(periodKey, utc));
 
 	useEffect(() => {
-		const updatePeriod = (): void => setPeriod(computePeriod(periodKey, utc));
+		const updatePeriod = (): void => setPeriod(getPeriodRange(periodKey, utc));
 
 		updatePeriod();
 		const timer = setInterval(updatePeriod, 60 * 60 * 1000);
@@ -97,6 +50,6 @@ export const usePeriod = ({ utc = false }: { utc?: boolean } = {}): [
 			options,
 			onChange,
 		},
-		t(periodLabelKeys[periodKey]),
+		t(...getPeriod(periodKey).label),
 	];
 };
