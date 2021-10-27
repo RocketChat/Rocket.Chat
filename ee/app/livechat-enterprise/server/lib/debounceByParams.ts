59bf81c8d3541de7d6f1d201f69576ef6cabd75c
Kevin Aleman <kevin.aleman@rocket.chat>
2021-10-27 15:30:40 -0600

    mem saves some types

--- ee/app/livechat-enterprise/server/lib/debounceByParams.ts
+++ ee/app/livechat-enterprise/server/lib/debounceByParams.ts
@@ -1,17 +1,9 @@
 import debounce from 'lodash.debounce';
-import memoize from 'lodash.memoize';
+import mem from 'mem';
 
 export interface IMemoizeDebouncedFunction<F extends (...args: any[]) => any> {
 	(...args: Parameters<F>): void;
 	flush: (...args: Parameters<F>) => void;
-}
-
-// copied to avoid having to import lodash for a single typedef
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/61bb91fa4eb8090c5be0286fd1d2a677a88a83b8/types/lodash/common/function.d.ts#L371
-interface IDebouncedFunc<T extends (...args: any[]) => any> {
-	(...args: Parameters<T>): ReturnType<T> | undefined;
-	cancel(): void;
-	flush(): ReturnType<T> | undefined;
 }
 
 // Debounce `func` based on passed parameters
@@ -20,12 +12,10 @@ export function memoizeDebounce<F extends(...args: any[]) => any>(
 	func: F,
 	wait = 0,
 	options: any = {},
-	resolver?: (...args: Parameters<F>) => unknown,
 ): IMemoizeDebouncedFunction<F> {
-	const debounceMemo = memoize<(...args: Parameters<F>) => IDebouncedFunc<F>>(
+	const debounceMemo = mem(
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		(..._args: Parameters<F>) => debounce(func, wait, options),
-	resolver,
 	);
 
 	function wrappedFunction(
