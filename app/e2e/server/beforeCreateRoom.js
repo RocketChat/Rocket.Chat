add9c589389c68d2b65b26afd60c95780f7c8f71
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-11-03 16:31:29 -0300

    moved conditional back to beforecreateuser callback

--- app/e2e/server/beforeCreateRoom.js
+++ app/e2e/server/beforeCreateRoom.js
@@ -3,8 +3,8 @@ import { settings } from '../../settings/server';
 
 callbacks.add('beforeCreateRoom', ({ type, extraData }) => {
 	if (
-		settings.get('E2E_Enable') && (type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
-	) {
+		settings.get('E2E_Enable') && ((type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
+		|| (type === 'p' && settings.get('E2E_Enabled_Default_PrivateRooms')))) {
 		extraData.encrypted = extraData.encrypted ?? true;
 	}
 });
