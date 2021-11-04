a2f0b9db66c578af8cf8a4024609b937885002ec
Martin <martin.schoeler@rocket.chat>
2021-11-04 17:21:26 -0300

    Paused and timer components

--- client/components/voip/call/CallComponent.tsx
+++ client/components/voip/call/CallComponent.tsx
@@ -4,28 +4,33 @@ import React, { FC } from 'react';
 import CallControls from './CallControls';
 import CallerInfo from './CallerInfo';
 import Header from './Header';
+import { ButtonsList } from './hooks/useButtonsLists';
 
 type CallComponentProps = {
 	state?: any;
 	data?: any;
 	callData?: any;
 	test?: any;
-	buttons?: any;
+	buttons: ButtonsList;
 };
 
-const CallComponent: FC<CallComponentProps> = ({ data, callData, buttons }) => (
-	<Box is='footer' p='x16' width='auto'>
-		{data.callsInQueue && (
-			<Header state={data.state} buttonList={buttons} calls={data.callsInQueue} />
-		)}
+const CallComponent: FC<CallComponentProps> = ({ data, callData, buttons }) => {
+	const callInfoStates = ['incoming', 'current'];
+	const disabled = buttons.internalStates.find((state) => state.id === 'hold-call')?.state;
+	console.log(disabled, buttons);
 
-		{data.state === 'Incoming' && (
-			<Box display='flex'>
-				<CallerInfo callerData={callData} />
-				<CallControls state={data.state} />
-			</Box>
-		)}
-	</Box>
-);
+	return (
+		<Box is='footer' p='x16' width='auto'>
+			<Header state={data.state} buttonList={buttons} calls={data.callsInQueue} />
+
+			{callInfoStates.includes(data.state) && (
+				<Box display='flex' opacity={disabled ? '0.4' : '1'}>
+					<CallerInfo callerData={callData} data={data} />
+					<CallControls state={data.state} disabled={disabled} />
+				</Box>
+			)}
+		</Box>
+	);
+};
 
 export default CallComponent;
