fa7ef4966e6f6501dc951a5897b24f0736e2924b
Aman-Maheshwari <amanmaheshwari816@gmail.com>
2021-11-05 17:11:51 +0530

    Showing discussion name in popUp

--- app/ui-message/client/popup/messagePopupChannel.js
+++ app/ui-message/client/popup/messagePopupChannel.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import { roomTypes } from '../../../utils';
@@ -6,5 +7,9 @@ import './messagePopupChannel.html';
 Template.messagePopupChannel.helpers({
 	channelIcon() {
 		return roomTypes.getIcon(this);
+	},
+	roomIcon() {
+		const room = roomTypes.findRoom('p', this.topic, Meteor.userId());
+		return roomTypes.getIcon(room);
 	},
 });
