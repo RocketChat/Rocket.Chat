3c9b400067802fe6dfca0790b47c49773063792a
Renato Becker <renato.augusto.becker@gmail.com>
2021-11-08 02:25:23 -0300

    Fix Omnichannel job queue dispatcher.

--- ee/app/livechat-enterprise/server/hooks/afterTakeInquiry.js
+++ ee/app/livechat-enterprise/server/hooks/afterTakeInquiry.js
@@ -1,14 +1,8 @@
 import { callbacks } from '../../../../../app/callbacks';
-import { settings } from '../../../../../app/settings/server';
 import { debouncedDispatchWaitingQueueStatus } from '../lib/Helper';
 import { cbLogger } from '../lib/logger';
 
 callbacks.add('livechat.afterTakeInquiry', async (inquiry) => {
-	if (!settings.get('Livechat_waiting_queue')) {
-		cbLogger.debug('Skipping callback. Waiting queue disabled by setting');
-		return inquiry;
-	}
-
 	if (!inquiry) {
 		cbLogger.debug('Skipping callback. No inquiry provided');
 		return null;
@@ -17,6 +11,6 @@ callbacks.add('livechat.afterTakeInquiry', async (inquiry) => {
 	const { department } = inquiry;
 	debouncedDispatchWaitingQueueStatus(department);
 
-	cbLogger.debug(`Statuses for queue ${ department || 'Public' } updated succesfully`);
+	cbLogger.debug(`Statuses for queue ${ department || 'Public' } updated successfully`);
 	return inquiry;
 }, callbacks.priority.MEDIUM, 'livechat-after-take-inquiry');
--- ee/app/livechat-enterprise/server/hooks/onCloseLivechat.js
+++ ee/app/livechat-enterprise/server/hooks/onCloseLivechat.js
@@ -1,21 +1,12 @@
 import { callbacks } from '../../../../../app/callbacks';
-import { settings } from '../../../../../app/settings/server';
 import { debouncedDispatchWaitingQueueStatus } from '../lib/Helper';
-import { RoutingManager } from '../../../../../app/livechat/server/lib/RoutingManager';
 import { LivechatEnterprise } from '../lib/LivechatEnterprise';
 
 const onCloseLivechat = (room) => {
 	Promise.await(LivechatEnterprise.releaseOnHoldChat(room));
 
-	if (!settings.get('Livechat_waiting_queue')) {
-		return room;
-	}
-
 	const { departmentId } = room || {};
-	if (!RoutingManager.getConfig().autoAssignAgent) {
-		debouncedDispatchWaitingQueueStatus(departmentId);
-		return room;
-	}
+	debouncedDispatchWaitingQueueStatus(departmentId);
 
 	return room;
 };
