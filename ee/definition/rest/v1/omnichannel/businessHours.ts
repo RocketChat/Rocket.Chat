552307e2ddcd19351f1d29ee6c24be12a2132710
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-11-12 11:24:39 -0300

    Update businessHours.ts

--- ee/definition/rest/v1/omnichannel/businessHours.ts
+++ ee/definition/rest/v1/omnichannel/businessHours.ts
@@ -1,29 +1,4 @@
-// import { API } from '../../../../../app/api/server';
 import { ILivechatBusinessHour } from '../../../../../definition/ILivechatBusinessHour';
-// import { findBusinessHours } from '../business-hour/lib/business-hour';
-
-// API.v1.addRoute('livechat/business-hours.list', { authRequired: true }, {
-// 	get() {
-// 		const { offset, count } = this.getPaginationItems();
-// 		const { sort } = this.parseJsonQuery();
-// 		const { name } = this.queryParams;
-
-// 		// @ts-ignore
-// 		return API.v1.success(Promise.await(findBusinessHours(
-// 			this.userId,
-// 			{
-// 				offset,
-// 				count,
-// 				sort,
-// 			},
-// 			name)));
-// 	},
-// });
-
-
-// import { IDirectMessageRoom, IRoom } from '../../../../definition/IRoom';
-// import { IDailyActiveUsers } from '../../../../definition/IUser';
-// import { Serialized } from '../../../../definition/Serialized';
 
 export type OmnichannelBusinessHoursEndpoints = {
 	'livechat/business-hours.list': {
