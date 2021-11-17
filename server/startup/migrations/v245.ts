d3d93bb71c6a3b68b45eefad13f13718a7011a34
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-11-17 13:48:11 -0300

    fixed permission import on migration

--- server/startup/migrations/v245.ts
+++ server/startup/migrations/v245.ts
@@ -1,5 +1,5 @@
 import { addMigration } from '../../lib/migrations';
-import { Permissions } from '../../../app/models/server';
+import { Permissions } from '../../../app/models/server/raw';
 
 addMigration({
 	version: 245,
