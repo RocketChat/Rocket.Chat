3ccf1e8481ddc92bc426ef55a527f0de844ac196
Kevin Aleman <kevin.aleman@rocket.chat>
2021-11-18 08:26:07 -0600

    add new endpoints to manage business units

--- ee/app/livechat-enterprise/server/api/units.js
+++ ee/app/livechat-enterprise/server/api/units.js
@@ -1,7 +1,48 @@
 import { API } from '../../../../../app/api/server';
 import { findUnits, findUnitById, findUnitMonitors } from './lib/units';
+import { LivechatEnterprise } from '../lib/LivechatEnterprise';
 
 API.v1.addRoute('livechat/units.list', { authRequired: true }, {
+	get() {
+		const { offset, count } = this.getPaginationItems();
+		const { sort } = this.parseJsonQuery();
+		const { text } = this.queryParams;
+
+		return API.v1.success(this.deprecationWarning(Promise.await(findUnits({
+			userId: this.userId,
+			text,
+			pagination: {
+				offset,
+				count,
+				sort,
+			},
+		}))));
+	},
+});
+
+API.v1.addRoute('livechat/units.getOne', { authRequired: true }, {
+	get() {
+		const { unitId } = this.queryParams;
+
+		return API.v1.success(this.deprecationWarning(Promise.await(findUnitById({
+			userId: this.userId,
+			unitId,
+		}))));
+	},
+});
+
+API.v1.addRoute('livechat/unitMonitors.list', { authRequired: true }, {
+	get() {
+		const { unitId } = this.queryParams;
+
+		return API.v1.success(Promise.await(findUnitMonitors({
+			userId: this.userId,
+			unitId,
+		})));
+	},
+});
+
+API.v1.addRoute('livechat/units', { authRequired: true, permissionsRequired: ['manage-livechat-units'] }, {
 	get() {
 		const { offset, count } = this.getPaginationItems();
 		const { sort } = this.parseJsonQuery();
@@ -17,26 +58,30 @@ API.v1.addRoute('livechat/units.list', { authRequired: true }, {
 			},
 		})));
 	},
-});
-
-API.v1.addRoute('livechat/units.getOne', { authRequired: true }, {
-	get() {
-		const { unitId } = this.queryParams;
-
-		return API.v1.success(Promise.await(findUnitById({
-			userId: this.userId,
-			unitId,
-		})));
+	post() {
+		const { unitData, unitMonitors, unitDepartments } = this.requestParams();
+		return LivechatEnterprise.saveUnit(null, unitData, unitMonitors, unitDepartments);
 	},
 });
 
-API.v1.addRoute('livechat/unitMonitors.list', { authRequired: true }, {
+API.v1.addRoute('livechat/units/:id', { authRequired: true, permissionsRequired: ['manage-livechat-units'] }, {
 	get() {
-		const { unitId } = this.queryParams;
+		const { id } = this.urlParams;
 
-		return API.v1.success(Promise.await(findUnitMonitors({
+		return API.v1.success(Promise.await(findUnitById({
 			userId: this.userId,
-			unitId,
+			id,
 		})));
+	},
+	put() {
+		const { unitData, unitMonitors, unitDepartments } = this.requestParams();
+		const { id } = this.urlParams;
+
+		return LivechatEnterprise.saveUnit(id, unitData, unitMonitors, unitDepartments);
+	},
+	delete() {
+		const { id } = this.urlParams;
+
+		return LivechatEnterprise.removeUnit(id);
 	},
 });
