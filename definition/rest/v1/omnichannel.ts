0b2079f3f15c5422ce2db6aea2b4b749409b93ba
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-11-19 15:55:49 -0300

    Regression:  Units endpoint to TS (#23757)

--- definition/rest/v1/omnichannel.ts
+++ definition/rest/v1/omnichannel.ts
@@ -55,6 +55,7 @@ export type OmnichannelEndpoints = {
 		};
 	};
 	'livechat/departments.by-unit/': {
+		path: `livechat/departments.by-unit/${ string }`;
 		GET: (params: { text: string; offset: number; count: number }) => {
 			departments: ILivechatDepartment[];
 			total: number;
