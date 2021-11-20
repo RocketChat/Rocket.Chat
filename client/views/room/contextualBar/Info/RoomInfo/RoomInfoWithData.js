4af5144e2347498253276274ef49aa41b709c297
Gaurav Kulkarni <kulkarnigaurav38@gmail.com>
2021-11-20 11:25:03 +0530

    fix undefined channel name in room info warning modal

--- client/views/room/contextualBar/Info/RoomInfo/RoomInfoWithData.js
+++ client/views/room/contextualBar/Info/RoomInfo/RoomInfoWithData.js
@@ -38,7 +38,7 @@ const RoomInfoWithData = ({ rid, openEditing, onClickBack, onEnterRoom, resetSta
 	room.type = room.t;
 	room.rid = rid;
 
-	const { type, fname, prid, joined = true } = room; // TODO implement joined
+	const { type, fname, name, prid, joined = true } = room; // TODO implement joined
 
 	const retentionPolicyEnabled = useSetting('RetentionPolicy_Enabled');
 	const retentionPolicy = {
@@ -111,7 +111,7 @@ const RoomInfoWithData = ({ rid, openEditing, onClickBack, onEnterRoom, resetSta
 
 		setModal(
 			<WarningModal
-				text={t(warnText, fname)}
+				text={t(warnText, name)}
 				confirmText={t('Leave_room')}
 				close={closeModal}
 				cancel={closeModal}
@@ -136,7 +136,7 @@ const RoomInfoWithData = ({ rid, openEditing, onClickBack, onEnterRoom, resetSta
 
 		setModal(
 			<WarningModal
-				text={t(warnText, fname)}
+				text={t(warnText, name)}
 				confirmText={t('Yes_hide_it')}
 				close={closeModal}
 				cancel={closeModal}
