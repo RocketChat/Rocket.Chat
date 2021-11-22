414c4a1bd23dc0b1712b4cea28d4dfcffa3f21d9
Kevin Aleman <kevin.aleman@rocket.chat>
2021-11-22 07:18:50 -0600

    init app engine before using persist model

--- server/startup/migrations/v245.ts
+++ server/startup/migrations/v245.ts
@@ -6,6 +6,7 @@ addMigration({
 	up() {
 		// we now have a compound index on appId + associations
 		// so we can use the index prefix instead of a separate index on appId
+		Apps.initialize();
 		return Apps._persistModel?.tryDropIndex({ appId: 1 });
 	},
 });
