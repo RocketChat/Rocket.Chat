558ea3eca8140c3471d564633f97f442eb20441f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-11-22 10:57:15 -0300

    Update api.ts

--- ee/server/api/api.ts
+++ ee/server/api/api.ts
@@ -5,7 +5,7 @@ import { isEnterprise } from '../../app/license/server/license';
 // Overwrites two factor method to enforce 2FA check for enterprise APIs when
 // no license was provided to prevent abuse on enterprise APIs.
 API.v1.processTwoFactor = use(API.v1.processTwoFactor, function([params, ...context], next) {
-	if (!params.options || !params.options.enterprise || !params.options.twoFactorRequired || !params.options.twoFactorOptions?.requireSecondFactor) {
+	if (!params.options || !params.options.enterprise || !params.options.twoFactorRequired || params.options.twoFactorOptions?.requireSecondFactor) {
 		return next(params, ...context);
 	}
 
