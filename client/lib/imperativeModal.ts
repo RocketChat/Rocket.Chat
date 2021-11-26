889a3991fab80e5b48be7e8fd205e5dfb863cdca
gabriellsh <gabriel.henriques@rocket.chat>
2021-11-26 11:53:15 -0300

    use key instead of queue

--- client/lib/imperativeModal.ts
+++ client/lib/imperativeModal.ts
@@ -1,64 +1,19 @@
 import { Emitter } from '@rocket.chat/emitter';
 import { ComponentType } from 'react';
 
-type BlazeModalOptions = {
-	confirmButtonText?: string;
-	cancelButtonText?: string;
-	showCancelButton?: boolean;
-	confirmButtonColor?: string;
-	cancelButtonColor?: string;
-
-	allowOutsideClick?: boolean;
-	confirmOnEnter?: boolean;
-
-	closeOnConfirm?: boolean;
-	closeOnEscape?: boolean;
-
-	type?: 'input';
-	inputType?: 'text' | 'password';
-	inputActionText?: string;
-	inputAction?: () => any;
-	inputPlaceholder?: string;
-
-	modalIcon?: string;
-
-	timer?: number;
-	dontAskAgain?: {
-		action: string;
-		label: string;
-	};
-
-	data?: Record<string, any>;
-	content?: string;
-	template?: string;
-
-	title?: string;
-	text?: string;
-	html?: boolean;
-};
-
-type BlazeModalDescriptor = {
-	options: BlazeModalOptions;
-	confirmFn?: () => any;
-	cancelFn?: () => any;
-};
-
 type ReactModalDescriptor<Props extends {} = {}> = {
 	component: ComponentType<Props>;
 	props?: Props;
 };
 
-type ModalDescriptor = BlazeModalDescriptor | ReactModalDescriptor | null;
+type ModalDescriptor = ReactModalDescriptor | null;
 
 class ImperativeModalEmmiter extends Emitter<{ update: ModalDescriptor }> {
 	setCurrentValue(descriptor: ModalDescriptor): void {
 		this.emit('update', descriptor);
 	}
 
-	open: {
-		(descriptor: BlazeModalDescriptor): void;
-		<Props = {}>(descriptor: ReactModalDescriptor<Props>): void;
-	} = (templateOrDescriptor: ModalDescriptor): void => {
+	open = (templateOrDescriptor: ModalDescriptor): void => {
 		this.setCurrentValue(templateOrDescriptor);
 	};
 
--- client/views/hooks/useImperativeModal.ts
+++ client/views/hooks/useImperativeModal.ts
@@ -1,33 +1,19 @@
-import { createElement, useEffect, Dispatch, SetStateAction, ReactNode, useState } from 'react';
+import { createElement, useEffect, Dispatch, SetStateAction, ReactNode } from 'react';
 
 import { imperativeModal } from '../../lib/imperativeModal';
 
 export const useImperativeModal = (setModal: Dispatch<SetStateAction<ReactNode>>): void => {
-	// this is so updates are queued, allowing the modal to close before reopening
-	const [updates, setUpdates] = useState<Array<() => void>>([]);
-
-	useEffect(() => {
-		if (updates.length > 0) {
-			setUpdates((updates) => {
-				const update = updates.shift();
-				update && update();
-				return [...updates];
-			});
-		}
-	}, [updates.length]);
-
 	useEffect(() => {
 		const unsub = imperativeModal.on('update', (descriptor) => {
 			if (descriptor === null) {
-				return setUpdates((updates) => updates.concat(updates, [(): void => setModal(null)]));
+				return setModal(null);
 			}
 			if ('component' in descriptor) {
-				setUpdates((updates) =>
-					updates.concat(updates, [
-						(): void => {
-							setModal(createElement(descriptor.component, descriptor.props));
-						},
-					]),
+				setModal(
+					createElement(descriptor.component, {
+						key: Math.random(),
+						...descriptor.props,
+					}),
 				);
 			}
 		});
