9962d15a6700f639f64627db1df5637c3da5ca27
Kevin Aleman <kevin.aleman@rocket.chat>
2021-11-26 14:24:05 -0600

    make canned responses popup dependent on setting

--- app/ui-message/client/popup/customMessagePopups.js
+++ app/ui-message/client/popup/customMessagePopups.js
@@ -2,11 +2,24 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 export const customMessagePopups = new ReactiveVar([]);
 
-export const addMessagePopup = (configGetter) => {
+export const addMessagePopup = (configGetter, name) => {
 	customMessagePopups.set([
 		...customMessagePopups.get(),
 		{
 			configGetter,
+			name,
 		},
+	]);
+};
+
+export const removeMessagePopup = (popupName) => {
+	const customMessagePopupsList = customMessagePopups.get();
+	const element = customMessagePopupsList.findIndex(({ name }) => name === popupName);
+	if (element < 0) {
+		return;
+	}
+	const listWithRemovedElement = [...customMessagePopupsList.slice(0, element), ...customMessagePopupsList.slice(element + 1, customMessagePopupsList.length)];
+	customMessagePopups.set([
+		...listWithRemovedElement,
 	]);
 };
--- ee/app/canned-responses/client/views/popup/addMessagePopupCannedResponse.js
+++ ee/app/canned-responses/client/views/popup/addMessagePopupCannedResponse.js
@@ -1,38 +1,53 @@
 import _ from 'underscore';
+import { Tracker } from 'meteor/tracker';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import { CannedResponse } from '../../collections/CannedResponse';
-import { addMessagePopup } from '../../../../../../app/ui-message/client/popup/customMessagePopups';
+import { addMessagePopup, removeMessagePopup } from '../../../../../../app/ui-message/client/popup/customMessagePopups';
 import { t } from '../../../../../../app/utils';
+import { settings } from '../../../../../../app/settings';
 
-addMessagePopup((template) => ({
-	title: t('Canned_Responses'),
-	collection: CannedResponse,
-	trigger: '!',
-	prefix: '',
-	suffix: ' ',
-	triggerAnywhere: true,
-	template: 'messagePopupCannedResponse',
-	rid: template.data.rid,
-	getInput: template.data.getInput,
-	textFilterDelay: 500,
-	getFilter: (collection, filter) => {
-		const exp = new RegExp(filter, 'i');
-		const records = collection.find({
-			shortcut: exp,
-		}, {
-			reactive: 1,
-			limit: 12,
-			sort: {
-				shortcut: -1,
-			},
-		}).fetch();
+const added = new ReactiveVar(false);
+Tracker.autorun(() => {
+	const templateFunc = (template) => ({
+		title: t('Canned_Responses'),
+		collection: CannedResponse,
+		trigger: '!',
+		prefix: '',
+		suffix: ' ',
+		triggerAnywhere: true,
+		template: 'messagePopupCannedResponse',
+		rid: template.data.rid,
+		getInput: template.data.getInput,
+		textFilterDelay: 500,
+		getFilter: (collection, filter) => {
+			const exp = new RegExp(filter, 'i');
+			const records = collection.find({
+				shortcut: exp,
+			}, {
+				reactive: 1,
+				limit: 12,
+				sort: {
+					shortcut: -1,
+				},
+			}).fetch();
 
-		return records;
-	},
-	getValue: (_id, collection, records) => {
-		const record = _.findWhere(records, {
-			_id,
-		});
-		return record && record.text;
-	},
-}));
+			return records;
+		},
+		getValue: (_id, collection, records) => {
+			const record = _.findWhere(records, {
+				_id,
+			});
+			return record && record.text;
+		},
+	});
+
+	const settingValue = settings.get('Canned_Responses_Enable');
+	if (settingValue && !added.get()) {
+		addMessagePopup(templateFunc, 'cannedResponses');
+		added.set(true);
+	} else if (!settingValue && added.get()) {
+		removeMessagePopup('cannedResponses');
+		added.set(false);
+	}
+});
