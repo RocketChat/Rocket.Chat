91f0820d12917eed67b625af12b7fb2649ba27e7
gabriellsh <gabriel.henriques@rocket.chat>
2021-11-29 12:28:46 -0300

    Fix types

--- client/lib/imperativeModal.ts
+++ client/lib/imperativeModal.ts
@@ -1,24 +1,29 @@
 import { Emitter } from '@rocket.chat/emitter';
 import { ComponentType } from 'react';
 
-type ReactModalDescriptor<Props extends {} = {}> = {
+type PropType = Record<string, unknown>;
+
+type ReactModalDescriptor<Props extends PropType> = {
 	component: ComponentType<Props>;
 	props?: Props;
 };
 
-type ModalDescriptor = ReactModalDescriptor | null;
+type ModalDescriptor = ReactModalDescriptor<PropType> | null;
 
 class ImperativeModalEmmiter extends Emitter<{ update: ModalDescriptor }> {
-	setCurrentValue(descriptor: ModalDescriptor): void {
+	update(descriptor: ModalDescriptor): void {
 		this.emit('update', descriptor);
 	}
 
-	open = (templateOrDescriptor: ModalDescriptor): void => {
-		this.setCurrentValue(templateOrDescriptor);
+	open = <Props extends PropType>(descriptor: ReactModalDescriptor<Props> | null): void => {
+		// There are some TS shenanigans causing errors if this is not asserted
+		// Since this method is for internal use only, it's ok to use this here
+		// This will not affect prop types inference when using the method.
+		this.update(descriptor as ModalDescriptor);
 	};
 
 	close = (): void => {
-		this.setCurrentValue(null);
+		this.update(null);
 	};
 }
 
