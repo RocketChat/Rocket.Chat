bb7495f1704fca65635434dc5879e54f15a628df
gabriellsh <gabriel.henriques@rocket.chat>
2021-11-29 19:13:49 -0300

    little adjustments

--- client/lib/imperativeModal.ts
+++ client/lib/imperativeModal.ts
@@ -1,21 +1,19 @@
 import { Emitter } from '@rocket.chat/emitter';
 import { ComponentType } from 'react';
 
-type PropType = Record<string, unknown>;
-
-type ReactModalDescriptor<Props extends PropType> = {
-	component: ComponentType<Props>;
-	props?: Props;
+type ReactModalDescriptor<TProps extends {}> = {
+	component: ComponentType<TProps>;
+	props?: TProps;
 };
 
-type ModalDescriptor = ReactModalDescriptor<PropType> | null;
+type ModalDescriptor = ReactModalDescriptor<{}> | null;
 
 class ImperativeModalEmmiter extends Emitter<{ update: ModalDescriptor }> {
 	update(descriptor: ModalDescriptor): void {
 		this.emit('update', descriptor);
 	}
 
-	open = <Props extends PropType>(descriptor: ReactModalDescriptor<Props> | null): void => {
+	open = <TProps extends {}>(descriptor: ReactModalDescriptor<TProps> | null): void => {
 		// There are some TS shenanigans causing errors if this is not asserted
 		// Since this method is for internal use only, it's ok to use this here
 		// This will not affect prop types inference when using the method.
