4459d233f25df8a22afcc3b44db0f249f8b9ea0e
Kevin Aleman <kevin.aleman@rocket.chat>
2021-11-30 11:48:41 -0600

    switch fallthrough

--- lib/emailValidator.ts
+++ lib/emailValidator.ts
@@ -3,10 +3,9 @@ export const validateEmail = (email: string, options: { style: string } = { styl
 	const rfcEmailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 	switch (options.style) {
-		case 'basic':
-			return basicEmailRegex.test(email);
 		case 'rfc':
 			return rfcEmailRegex.test(email);
+		case 'basic':
 		default:
 			return basicEmailRegex.test(email);
 	}
