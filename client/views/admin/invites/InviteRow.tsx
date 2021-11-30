688613a609c31b9beb372af8f0a0ab88895e9a48
dougfabris <devfabris@gmail.com>
2021-11-30 20:03:09 -0300

    fix: remove imperativeModal

--- client/views/admin/invites/InviteRow.tsx
+++ client/views/admin/invites/InviteRow.tsx
@@ -5,11 +5,11 @@ import React, { ReactElement, MouseEvent } from 'react';
 
 import { IInvite } from '../../../../definition/IInvite';
 import GenericModal from '../../../components/GenericModal';
+import { useSetModal } from '../../../contexts/ModalContext';
 import { useEndpoint } from '../../../contexts/ServerContext';
 import { useToastMessageDispatch } from '../../../contexts/ToastMessagesContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useFormatDateAndTime } from '../../../hooks/useFormatDateAndTime';
-import { imperativeModal } from '../../../lib/imperativeModal';
 
 const isExpired = (expires: IInvite['expires']): boolean => {
 	if (expires && expires.getTime() < new Date().getTime()) {
@@ -33,6 +33,7 @@ const InviteRow = ({
 	onRemove,
 }: InviteRowProps): ReactElement => {
 	const t = useTranslation();
+	const setModal = useSetModal();
 	const formatDateAndTime = useFormatDateAndTime();
 	const dispatchToastMessage = useToastMessageDispatch();
 	const removeInvite = useEndpoint('DELETE', `removeInvite/${_id}`);
@@ -76,29 +77,30 @@ const InviteRow = ({
 	const handleRemoveButtonClick = async (event: MouseEvent<HTMLElement>): Promise<void> => {
 		event.stopPropagation();
 
-		imperativeModal.open({
-			component: GenericModal,
-			props: {
-				title: t('Are_you_sure'),
-				children: t('Are_you_sure_you_want_to_delete_this_record'),
-				variant: 'danger',
-				confirmText: t('Yes'),
-				cancelText: t('No'),
-				onClose: imperativeModal.close,
-				onCancel: imperativeModal.close,
-				onConfirm: async (): Promise<void> => {
-					try {
-						await removeInvite();
-						onRemove && onRemove(_id);
-						dispatchToastMessage({ type: 'success', message: t('Invite_removed') });
-					} catch (error) {
-						dispatchToastMessage({ type: 'error', message: error });
-					} finally {
-						imperativeModal.close();
-					}
-				},
-			},
-		});
+		const confirmRemove = async (): Promise<void> => {
+			try {
+				await removeInvite();
+				onRemove && onRemove(_id);
+				dispatchToastMessage({ type: 'success', message: t('Invite_removed') });
+			} catch (error) {
+				dispatchToastMessage({ type: 'error', message: error });
+			} finally {
+				setModal();
+			}
+		};
+
+		setModal(
+			<GenericModal
+				title={t('Are_you_sure')}
+				children={t('Are_you_sure_you_want_to_delete_this_record')}
+				variant='danger'
+				confirmText={t('Yes')}
+				cancelText={t('No')}
+				onClose={(): void => setModal()}
+				onCancel={(): void => setModal()}
+				onConfirm={confirmRemove}
+			/>,
+		);
 	};
 
 	const notSmall = useMediaQuery('(min-width: 768px)');
