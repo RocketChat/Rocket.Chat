091b3b7291bf517556a2a3fb423bab0fbbc0ff42
dougfabris <devfabris@gmail.com>
2021-12-01 10:42:20 -0300

    fix: add missing .png extension
    Co-authored-by: yash-rajpal <58601732+yash-rajpal@users.noreply.github.com>

--- app/ui-message/client/messageBox/messageBox.js
+++ app/ui-message/client/messageBox/messageBox.js
@@ -402,9 +402,8 @@ Template.messageBox.events({
 			.filter((item) => item.kind === 'file' && item.type.indexOf('image/') !== -1)
 			.map((item) => ({
 				file: item.getAsFile(),
-				name: `Clipboard - ${ moment().format(settings.get('Message_TimeAndDateFormat')) }`,
-			}))
-			.filter(({ file }) => file !== null);
+				name: `Clipboard - ${ moment().format(settings.get('Message_TimeAndDateFormat')) }.png`,
+			})).filter(({ file }) => file !== null);
 
 		if (files.length) {
 			event.preventDefault();
