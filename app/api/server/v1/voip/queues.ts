807356b2222f0cc75080ee8eaa9323a168333e27
amolghode <amol.ghode@gmail.com>
2021-12-07 23:21:14 +0530

    Fixing merge related issues with develop branch.

--- app/api/server/v1/voip/queues.ts
+++ app/api/server/v1/voip/queues.ts
@@ -3,22 +3,23 @@ import { Match, check } from 'meteor/check';
 import { API } from '../../api';
 import { Voip } from '../../../../../server/sdk';
 import { IVoipConnectorResult } from '../../../../../definition/IVoipConnectorResult';
+import { logger } from './logger';
 
 API.v1.addRoute('voip/queues.getSummary', { authRequired: true }, {
-	get() {
-		const queueSummary = Promise.await(Voip.getQueueSummary()) as IVoipConnectorResult;
-		this.logger.debug({ msg: 'API = voip/queues.getSummary ', result: queueSummary });
+	async get() {
+		const queueSummary = await Voip.getQueueSummary();
+		logger.debug({ msg: 'API = voip/queues.getSummary ', result: queueSummary });
 		return API.v1.success({ summary: queueSummary.result });
 	},
 });
 
 API.v1.addRoute('voip/queues.getQueuedCallsForThisExtension', { authRequired: true }, {
-	get() {
+	async get() {
 		check(this.requestParams(), Match.ObjectIncluding({
 			extension: String,
 		}));
-		const membershipDetails: IVoipConnectorResult = Promise.await(Voip.getQueuedCallsForThisExtension(this.requestParams()));
-		this.logger.debug({ msg: 'API = queues.getCallWaitingInQueuesForThisExtension', result: membershipDetails });
+		const membershipDetails: IVoipConnectorResult = await Voip.getQueuedCallsForThisExtension(this.requestParams());
+		logger.debug({ msg: 'API = queues.getCallWaitingInQueuesForThisExtension', result: membershipDetails });
 		return API.v1.success({ ...membershipDetails.result });
 	},
 });
