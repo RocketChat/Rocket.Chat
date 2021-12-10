c7e8b8461b46fd79a06f565b190cb5bcb3eae3f3
dougfabris <devfabris@gmail.com>
2021-12-10 11:31:13 -0300

    improve: webdav methods

--- definition/IWebdavAccount.ts
+++ definition/IWebdavAccount.ts
@@ -1,9 +1,13 @@
 import { IRocketChatRecord } from './IRocketChatRecord';
 
 export interface IWebdavAccount extends IRocketChatRecord {
-	user_id: string;
-	server_url: string;
+	userId: string;
+	serverURL: string;
 	username: string;
 	password: string;
 	name: string;
 }
+
+declare let { userId, _id, _updatedAt, ...rest }: IWebdavAccount;
+
+export type IWebdavAccountPayload = typeof rest
