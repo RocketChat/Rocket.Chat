d4d3160610d62dca86ccb25d7cbf8712e108db04
juliajforesti <juliajforesti@gmail.com>
2021-12-21 19:57:44 -0300

    Convert js to tsx

---
+++ client/components/Sidebar/ListItem.tsx
@@ -0,0 +1,19 @@
+import { Option, OptionColumn } from '@rocket.chat/fuselage';
+import React, { ComponentProps, MouseEventHandler, ReactElement } from 'react';
+
+type ListItemProps = {
+	text: string;
+	icon?: ComponentProps<typeof Option.Icon>['name'];
+	input?: any;
+	action?: MouseEventHandler<HTMLOrSVGElement> | undefined;
+};
+
+const ListItem = ({ text, icon, input, action }: ListItemProps): ReactElement => (
+	<Option onClick={action || undefined}>
+		{icon && <Option.Icon name={icon} size={20} />}
+		<Option.Content>{text}</Option.Content>
+		{input && <OptionColumn>{input}</OptionColumn>}
+	</Option>
+);
+
+export default ListItem;
