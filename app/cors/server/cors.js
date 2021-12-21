5e53fb874e498c8f412073dbdbe79f90fdf0492d
Cuong Huu Nguyen <cuonghuunguyen98@gmail.com>
2021-12-21 14:07:36 +0700

    Fix cors policy on resources and Rest API

--- app/cors/server/cors.js
+++ app/cors/server/cors.js
@@ -51,7 +51,29 @@ WebApp.rawConnectHandlers.use(function(req, res, next) {
 		);
 	}
 
+	if (settings.get('API_Enable_CORS')) {
+		const CORSOriginSetting = String(settings.get('API_CORS_Origin'));
 
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, PATCH');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-User-Id, X-Auth-Token, x-visitor-token, Authorization');
+
+
+		if (CORSOriginSetting === '*') {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			return next();
+		}
+
+		const allowedOrigin = CORSOriginSetting
+			.trim()
+			.split(',')
+			.map((origin) => String(origin).trim().toLocaleLowerCase())
+			.find((origin) => req.headers.origin === origin);
+
+		if (allowedOrigin) {
+			res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+			res.setHeader('Vary', 'Origin');
+		}
+	}
 	return next();
 });
 
