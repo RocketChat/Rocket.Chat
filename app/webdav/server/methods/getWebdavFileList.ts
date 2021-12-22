160619d2ac4454482b3f5302419300dff0f3870c
Douglas Fabris <devfabris@gmail.com>
2021-12-22 00:57:11 -0300

    [IMPROVE] Webdav methods sanitization (#23924)

--- app/webdav/server/methods/getWebdavFileList.ts
+++ app/webdav/server/methods/getWebdavFileList.ts
@@ -1,13 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
-import { settings } from '../../../settings';
+import { settings } from '../../../settings/server';
 import { getWebdavCredentials } from './getWebdavCredentials';
 import { WebdavAccounts } from '../../../models/server/raw';
 import { WebdavClientAdapter } from '../lib/webdavClientAdapter';
 
 Meteor.methods({
 	async getWebdavFileList(accountId, path) {
-		if (!Meteor.userId()) {
+		const userId = Meteor.userId();
+
+		if (!userId) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid User', { method: 'getWebdavFileList' });
 		}
 
@@ -15,18 +17,18 @@ Meteor.methods({
 			throw new Meteor.Error('error-not-allowed', 'WebDAV Integration Not Allowed', { method: 'getWebdavFileList' });
 		}
 
-		const account = await WebdavAccounts.findOneByIdAndUserId(accountId, Meteor.userId());
+		const account = await WebdavAccounts.findOneByIdAndUserId(accountId, userId, {});
 		if (!account) {
 			throw new Meteor.Error('error-invalid-account', 'Invalid WebDAV Account', { method: 'getWebdavFileList' });
 		}
 
 		try {
 			const cred = getWebdavCredentials(account);
-			const client = new WebdavClientAdapter(account.server_url, cred);
+			const client = new WebdavClientAdapter(account.serverURL, cred);
 			const data = await client.getDirectoryContents(path);
 			return { success: true, data };
 		} catch (error) {
-			throw new Meteor.Error('could-not-access-webdav', { method: 'getWebdavFileList' });
+			throw new Meteor.Error('could-not-access-webdav', 'Could not access webdav', { method: 'getWebdavFileList' });
 		}
 	},
 });
