ef0427e1fc7f15e39cb383809f8c6fc24d5cd5c4
sidmohanty11 <sidmohanty11@gmail.com>
2021-12-29 23:41:28 +0530

    fix: invalid role, role was coming out to be undefined from findOne in useRole

--- client/views/admin/permissions/useRole.js
+++ client/views/admin/permissions/useRole.js
@@ -3,4 +3,13 @@ import { useCallback } from 'react';
 import { Roles } from '../../../../app/models/client';
 import { useReactiveValue } from '../../../hooks/useReactiveValue';
 
-export const useRole = (_id) => useReactiveValue(useCallback(() => Roles.findOne({ _id }), [_id]));
+export const useRole = (_id) =>
+	useReactiveValue(
+		useCallback(
+			() =>
+				Roles.find({})
+					.fetch()
+					.find((r) => r._id === _id),
+			[_id],
+		),
+	);
