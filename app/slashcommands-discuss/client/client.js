884ff78442a16801b300a0377e9f11a44fed21d8
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2021-12-29 13:08:35 +0530

    remove js files
    Signed-off-by: Debdut Chakraborty <debdut.chakraborty@rocket.chat>

--- app/slashcommands-discuss/client/client.js
+++
@@ -1,7 +0,0 @@
-import { slashCommands } from '../../utils';
-
-slashCommands.add('discuss', null, {
-	description: 'Create a new discussion',
-	params: '[#channel] [discussion name]',
-	permission: ['start-discussion-other-user', 'start-discussion'],
-});
