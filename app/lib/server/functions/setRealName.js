cc24d8cc3917b4556c5248d6cbba7c6a3b9e127e
Gaurav Kulkarni <kulkarnigaurav38@gmail.com>
2021-12-31 12:34:45 +0530

    fix the importing users from csv file bug

--- app/lib/server/functions/setRealName.js
+++ app/lib/server/functions/setRealName.js
@@ -41,5 +41,5 @@ export const _setRealName = function(userId, name, fullUser) {
 };
 
 export const setRealName = RateLimiter.limitFunction(_setRealName, 1, 60000, {
-	0() { return !Meteor.userId() || !hasPermission(Meteor.userId(), 'edit-other-user-info'); }, // Administrators have permission to change others names, so don't limit those
+	0() { return !Meteor.userId || !hasPermission(Meteor.userId, 'edit-other-user-info'); }, // Administrators have permission to change others names, so don't limit those
 });
