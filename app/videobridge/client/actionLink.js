1ef76e9a83a58a73700918ecc47aa648f8d934dd
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2022-01-03 16:13:14 -0300

    Open jitsi call on Meteor.call callback

--- app/videobridge/client/actionLink.js
+++ app/videobridge/client/actionLink.js
@@ -6,27 +6,37 @@ import { actionLinks } from '../../action-links/client';
 import { Rooms, Subscriptions } from '../../models';
 import { dispatchToastMessage } from '../../../client/lib/toast';
 
-actionLinks.register('joinJitsiCall', function (message, params, instance) {
+actionLinks.register('joinJitsiCall', function joinJitsiCall(message, params, instance) {
 	if (Session.get('openedRoom')) {
 		const rid = Session.get('openedRoom');
-
-		const room = Rooms.findOne({ _id: rid });
 		const username = Meteor.user()?.username;
-		const currentTime = new Date().getTime();
-		const jitsiTimeout = new Date((room && room.jitsiTimeout) || currentTime).getTime();
 		const userId = Meteor.userId();
 		const sub = Subscriptions.findOne({ rid, 'u._id': userId });
+		const openVideoCall = (rid) => {
+			const room = Rooms.findOne({ _id: rid });
+			const currentTime = new Date().getTime();
+			const jitsiTimeout = new Date((room && room.jitsiTimeout) || currentTime).getTime();
+
+			if (room && room?.muted?.includes(username)) {
+				dispatchToastMessage({ type: 'error', message: TAPi18n.__('You_have_been_muted', '') });
+			} else if (jitsiTimeout > currentTime) {
+				instance.tabBar.open('video');
+			} else {
+				dispatchToastMessage({ type: 'info', message: TAPi18n.__('Call Already Ended', '') });
+			}
+		};
 
 		if (!sub) {
-			Meteor.runAsUser(userId, () => Meteor.call('joinRoom', rid));
-		}
-
-		if (room && room?.muted?.includes(username)) {
-			dispatchToastMessage({ type: 'error', message: TAPi18n.__('You_have_been_muted', '') });
-		} else if (jitsiTimeout > currentTime) {
-			instance.tabBar.open('video');
+			Meteor.call('joinRoom', rid, (err, result) => {
+				if (err) {
+					throw err;
+				}
+				if (result === true) {
+					setTimeout(() => openVideoCall(rid), 2000);
+				}
+			});
 		} else {
-			dispatchToastMessage({ type: 'info', message: TAPi18n.__('Call Already Ended', '') });
+			openVideoCall(rid);
 		}
 	}
 });
