79d423b4ae88f7b1705315700f7d6d120176f6d4
Martin <martin.schoeler@rocket.chat>
2022-01-03 11:35:47 -0300

    fix error

--- client/sidebar/sections/OmnichannelSection.tsx
+++ client/sidebar/sections/OmnichannelSection.tsx
@@ -52,7 +52,7 @@ const OmnichannelSection = (props: typeof Box): ReactElement => {
 
 	// The className is a paliative while we make TopBar.ToolBox optional on fuselage
 	return (
-		<Sidebar.TopBar.ToolBox {...Object.assign(props, { className: 'omnichannel-sidebar' })}>
+		<Sidebar.TopBar.ToolBox className='omnichannel-sidebar' {...props}>
 			<Sidebar.TopBar.Title>{t('Omnichannel')}</Sidebar.TopBar.Title>
 			<Sidebar.TopBar.Actions>
 				{showOmnichannelQueueLink && <Sidebar.TopBar.Action icon='queue' title={t('Queue')} is='a' href={queueLink} />}
