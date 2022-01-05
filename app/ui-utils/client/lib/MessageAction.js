bf7b461877e98178fa00715cf69d4aa2c6fe56a0
Douglas Gubert <douglas.gubert@gmail.com>
2022-01-04 21:39:29 -0300

    [FIX][APPS] Action buttons not removed when app is disabled or uninstalled (#24107)

--- app/ui-utils/client/lib/MessageAction.js
+++ app/ui-utils/client/lib/MessageAction.js
@@ -70,6 +70,8 @@ export const MessageAction = new class {
 		return Tracker.nonreactive(() => {
 			const btns = this.buttons.get();
 			delete btns[id];
+			mem.clear(this._getButtons);
+			mem.clear(this._getButtonsByGroup);
 			return this.buttons.set(btns);
 		});
 	}
@@ -79,6 +81,8 @@ export const MessageAction = new class {
 			const btns = this.buttons.get();
 			if (btns[id]) {
 				btns[id] = _.extend(btns[id], config);
+				mem.clear(this._getButtons);
+				mem.clear(this._getButtonsByGroup);
 				return this.buttons.set(btns);
 			}
 		});
