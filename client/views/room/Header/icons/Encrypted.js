940ee7777e9c91427b6f60ca92b5997590e2b1b5
yash-rajpal <rajpal.yash03@gmail.com>
2022-01-07 21:32:30 +0530

    add tooltip with permission check

--- client/views/room/Header/icons/Encrypted.js
+++ client/views/room/Header/icons/Encrypted.js
@@ -3,6 +3,7 @@ import colors from '@rocket.chat/fuselage-tokens/colors';
 import React, { memo } from 'react';
 
 import Header from '../../../../components/Header';
+import { usePermission } from '../../../../contexts/AuthorizationContext';
 import { useMethod } from '../../../../contexts/ServerContext';
 import { useSetting } from '../../../../contexts/SettingsContext';
 import { useTranslation } from '../../../../contexts/TranslationContext';
@@ -11,8 +12,12 @@ const Encrypted = ({ room }) => {
 	const t = useTranslation();
 	const e2eEnabled = useSetting('E2E_Enable');
 	const toggleE2E = useMethod('saveRoomSettings');
-	const encryptedLabel = t('Encrypted');
+	const canToggleE2E = usePermission('toggle-room-e2e-encryption');
+	const encryptedLabel = canToggleE2E ? t('Encrypted_key_title') : t('Encrypted');
 	const handleE2EClick = useMutableCallback(() => {
+		if (!canToggleE2E) {
+			return;
+		}
 		toggleE2E(room._id, 'encrypted', !(room && room.encrypted));
 	});
 	return e2eEnabled && room?.encrypted ? (
