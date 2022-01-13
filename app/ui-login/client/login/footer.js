185936e7ef544b17c15fa35db7147ad8625e38df
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-01-12 23:59:29 -0300

    Simplify login footer

--- app/ui-login/client/login/footer.js
+++ app/ui-login/client/login/footer.js
@@ -4,60 +4,53 @@ import { Template } from 'meteor/templating';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
 
 import { settings } from '../../../settings';
+import { filterLanguage } from '../../../../client/lib/utils/filterLanguage';
 
-const filterLanguage = (language) => {
-	// Fix browsers having all-lowercase language settings eg. pt-br, en-us
-	const regex = /([a-z]{2,3})-([a-z]{2,4})/;
-	const matches = regex.exec(language);
-	if (matches) {
-		return `${matches[1]}-${matches[2].toUpperCase()}`;
+const loadLanguage = async (language = 'en') => {
+	const loadedLanguage = filterLanguage(language);
+	await TAPi18n._loadLanguage(loadedLanguage);
+	window.setLanguage(loadedLanguage);
+
+	return loadedLanguage;
+};
+
+const getSuggestedLanguage = (loadedLanguage) => {
+	const serverLanguage = filterLanguage(settings.get('Language'));
+
+	if (serverLanguage !== loadedLanguage) {
+		return serverLanguage;
 	}
 
-	return language;
+	if (serverLanguage !== 'en') {
+		return 'en';
+	}
+
+	return undefined;
 };
 
 Template.loginFooter.onCreated(function () {
-	this.suggestedLanguage = new ReactiveVar();
+	const currentLanguage = filterLanguage(Meteor._localStorage.getItem('userLanguage'));
+	this.suggestedLanguage = new ReactiveVar(getSuggestedLanguage(currentLanguage));
 
-	const loadAndSetSuggestedLanguage = async (language = 'en') => {
-		const lng = filterLanguage(language);
-		try {
-			await TAPi18n._loadLanguage(filterLanguage(lng));
-			window.setLanguage(lng);
-
-			const serverLanguage = filterLanguage(settings.get('Language'));
-
-			if (serverLanguage !== lng) {
-				return serverLanguage;
-			}
-			if (serverLanguage !== 'en') {
-				return 'en';
-			}
-		} catch (e) {
-			return null;
-		}
-	};
-
-	this.suggestAnotherLanguageFor = async (language) => {
-		const suggest = await loadAndSetSuggestedLanguage(language);
-		this.suggestedLanguage.set(suggest);
-	};
-
-	const currentLanguage = Meteor._localStorage.getItem('userLanguage');
-	this.suggestAnotherLanguageFor(currentLanguage);
+	loadLanguage(currentLanguage);
 });
 
 Template.loginFooter.helpers({
-	languageVersion() {
-		const lng = Template.instance().suggestedLanguage.get();
-		return lng && TAPi18n.__('Language_Version', { lng });
+	lng() {
+		return Template.instance().suggestedLanguage.get();
+	},
+	lngObj(lng) {
+		return { lng };
 	},
 });
 
 Template.loginFooter.events({
 	'click button.js-switch-language'(e, t) {
+		e.preventDefault();
+
 		const language = t.suggestedLanguage.get();
-		t.suggestAnotherLanguageFor(language);
-		return false;
+		t.suggestedLanguage.set(getSuggestedLanguage(t.suggestedLanguage.get()));
+
+		loadLanguage(language);
 	},
 });
