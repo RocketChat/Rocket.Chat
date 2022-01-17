79f5563a9dbc26eff6a7fd5bdc2f27856a7f6157
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2022-01-17 16:39:52 -0300

    cryptographically secure pseudo-random generator

--- app/e2e/client/helper.js
+++ app/e2e/client/helper.js
@@ -1,5 +1,7 @@
 /* eslint-disable new-cap, no-proto */
 
+import crypto from 'crypto';
+
 import ByteBuffer from 'bytebuffer';
 
 import { words } from './wordList';
@@ -124,13 +126,36 @@ export async function readFileAsArrayBuffer(file) {
 	});
 }
 
+function hexString(digits) {
+	const numBytes = Math.ceil(digits / 2);
+	let bytes;
+	// Try to get cryptographically strong randomness. Fall back to
+	// non-cryptographically strong if not available.
+	try {
+		bytes = crypto.randomBytes(numBytes);
+	} catch (e) {
+		// XXX should re-throw any error except insufficient entropy
+		bytes = crypto.pseudoRandomBytes(numBytes);
+	}
+	const result = bytes.toString('hex');
+	// If the number of digits is odd, we'll have generated an extra 4 bits
+	// of randomness, so we need to trim the last digit.
+	return result.substring(0, digits);
+}
+
+// criptographically secure way of generating a number between 0-1( similar to Math.random())
+function fraction() {
+	const numerator = Number.parseInt(hexString(8), 16);
+	return numerator * 2.3283064365386963e-10; // 2^-3;
+}
+
 export function generateMnemonicPhrase(n, sep = ' ') {
 	const result = new Array(n);
 	let len = words.length;
 	const taken = new Array(len);
 
 	while (n--) {
-		const x = Math.floor(Math.random() * len);
+		const x = Math.floor(fraction() * len);
 		result[n] = words[x in taken ? taken[x] : x];
 		taken[x] = --len in taken ? taken[len] : len;
 	}
