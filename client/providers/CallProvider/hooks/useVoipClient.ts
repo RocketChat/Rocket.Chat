54c2e6ca691fc01005a63ab07f36e18a5deeed90
amolghode <amol.ghode@gmail.com>
2022-01-21 15:57:53 +0530

    Clickup Task: https://app.clickup.com/t/1z58qvr Description: 1. Fixed useVoipClient hook. Removed hardcoded extension. This code waits for user to get loaded Once the user is loaded, it calls API connector.extension.getRegistrationInfoByUserId which returns the registration info for a given userId. If no extension is associated with the user, it returns an error. 2. Added new API to app/api/server/v1/voip/extensions.ts which returns a registration info based on userId. If the extension is not associated with the user, it returns an error. 3. Removed names developer user accounts on asterisk from settings file. 4. Fixed voip api definition.

--- client/providers/CallProvider/hooks/useVoipClient.ts
+++ client/providers/CallProvider/hooks/useVoipClient.ts
@@ -1,9 +1,9 @@
 import { useSafely } from '@rocket.chat/fuselage-hooks';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { IRegistrationInfo } from '../../../../definition/voip/IRegistrationInfo';
-import { useEndpointData } from '../../../hooks/useEndpointData';
-import { AsyncStatePhase } from '../../../lib/asyncState';
+import { useEndpoint } from '../../../contexts/ServerContext';
+import { useUser } from '../../../contexts/UserContext';
 import { SimpleVoipUser } from '../../../lib/voip/SimpleVoipUser';
 import { VoIPUser } from '../../../lib/voip/VoIPUser';
 import { useWebRtcServers } from './useWebRtcServers';
@@ -21,50 +21,46 @@ export const isUseVoipClientResultError = (result: UseVoipClientResult): result 
 	!!(result as UseVoipClientResultError).error;
 
 export const isUseVoipClientResultLoading = (result: UseVoipClientResult): result is UseVoipClientResultLoading =>
-	Object.keys(result).length === 0;
+	result === undefined || Object.keys(result).length === 0;
 
 export const useVoipClient = (): UseVoipClientResult => {
-	const config = useEndpointData(
-		'connector.extension.getRegistrationInfo',
-		useMemo(() => ({ extension: '80000' }), []), // TODO: When we have the extension handler UI, use Agent Extension value
-	);
-
-	// TODO: should we recreate the client if the iceServers list changes? this can disrupt the call
-
+	const registrationInfo = useEndpoint('GET', 'connector.extension.getRegistrationInfoByUserId');
+	const user = useUser();
 	const iceServers = useWebRtcServers();
 
 	const [result, setResult] = useSafely(useState<UseVoipClientResult>({}));
 
 	useEffect(() => {
-		if (config.phase === AsyncStatePhase.REJECTED) {
-			setResult({ error: config.error as Error });
-			return;
-		}
-
-		if (config.phase !== AsyncStatePhase.RESOLVED) {
+		if (!user || !user?._id) {
 			setResult({});
 			return;
 		}
-
-		const {
-			extensionDetails: { extension, password },
-			host,
-			callServerConfig: { websocketPath },
-		} = config.value;
-		let client: VoIPUser;
-		(async (): Promise<void> => {
-			try {
-				client = await SimpleVoipUser.create(extension, password, host, websocketPath, iceServers, 'video');
-				setResult({ voipClient: client, registrationInfo: config.value });
-			} catch (e) {
-				setResult({ error: e as Error });
-			}
-		})();
+		registrationInfo({ id: user._id }).then(
+			(data) => {
+				const {
+					extensionDetails: { extension, password },
+					host,
+					callServerConfig: { websocketPath },
+				} = data;
+				let client: VoIPUser;
+				(async (): Promise<void> => {
+					try {
+						client = await SimpleVoipUser.create(extension, password, host, websocketPath, iceServers, 'video');
+						setResult({ voipClient: client, registrationInfo: data });
+					} catch (e) {
+						setResult({ error: e as Error });
+					}
+				})();
+			},
+			(error) => {
+				setResult({ error: error as Error });
+			},
+		);
 		return (): void => {
 			// client?.disconnect();
 			// TODO how to close the client? before creating a new one?
 		};
-	}, [iceServers, config.phase, config.value, setResult, config.error]);
+	}, [user, iceServers, registrationInfo, setResult]);
 
 	return result;
 };
