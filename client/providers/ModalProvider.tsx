5904ef4066721f9bdb33ef2568763327dddf2f0c
gabriellsh <gabriel.henriques@rocket.chat>
2022-01-24 18:52:07 -0300

    Close modal on esc and outside click

--- client/providers/ModalProvider.tsx
+++ client/providers/ModalProvider.tsx
@@ -1,10 +1,14 @@
 import { Modal } from '@rocket.chat/fuselage';
-import React, { useState, useMemo, memo, FC, ComponentProps, ReactNode } from 'react';
+import React, { useState, useMemo, memo, FC, ComponentProps, ReactNode, useEffect, useCallback } from 'react';
 
 import { modal } from '../../app/ui-utils/client/lib/modal';
 import ModalPortal from '../components/ModalPortal';
 import { ModalContext } from '../contexts/ModalContext';
 import { useImperativeModal } from '../views/hooks/useImperativeModal';
+
+const BACKDROP_ID = 'rc-modal-backdrop';
+
+const getBackdrop = (): HTMLElement | null => document.getElementById(BACKDROP_ID);
 
 const ModalProvider: FC = ({ children }) => {
 	const [currentModal, setCurrentModal] = useState<ReactNode>(null);
@@ -17,6 +21,34 @@ const ModalProvider: FC = ({ children }) => {
 		[],
 	);
 
+	const closeModalOnBackdropClick = useCallback((e) => {
+		const backdrop = getBackdrop();
+
+		// checking for parent since myNode.contains(myNode) is true
+		if (!backdrop || backdrop.contains(e.target.parentElement)) {
+			return;
+		}
+
+		e.stopPropagation();
+		setCurrentModal(null);
+	}, []);
+
+	useEffect(() => {
+		const closeOnEsc = (e: KeyboardEvent): void => {
+			if (e.key !== 'Escape' || !currentModal) {
+				return;
+			}
+			e.stopPropagation();
+			setCurrentModal(null);
+		};
+
+		window.addEventListener('keyup', closeOnEsc);
+
+		return (): void => {
+			window.removeEventListener('keyup', closeOnEsc);
+		};
+	}, [currentModal]);
+
 	useImperativeModal(setCurrentModal);
 
 	return (
@@ -24,7 +56,9 @@ const ModalProvider: FC = ({ children }) => {
 			{children}
 			{currentModal && (
 				<ModalPortal>
-					<Modal.Backdrop zIndex={9999}>{currentModal}</Modal.Backdrop>
+					<Modal.Backdrop zIndex={9999} id={BACKDROP_ID} onClick={closeModalOnBackdropClick}>
+						{currentModal}
+					</Modal.Backdrop>
 				</ModalPortal>
 			)}
 		</ModalContext.Provider>
