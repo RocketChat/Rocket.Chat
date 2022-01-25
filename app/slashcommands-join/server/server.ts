1b68319726d757a7d6fdea8d5268dcc5d9e825b5
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2022-01-25 15:59:59 -0300

    refact

--- app/slashcommands-join/server/server.ts
+++ app/slashcommands-join/server/server.ts
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { Match } from 'meteor/check';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
 
 import { Rooms, Subscriptions } from '../../models/server';
@@ -7,21 +6,22 @@ import { settings } from '../../settings/server';
 import { slashCommands } from '../../utils/lib/slashCommand';
 import { api } from '../../../server/sdk/api';
 
-function Join(command: string, params: string, item: Record<string, string>): void {
-	if (command !== 'join' || !Match.test(params, String)) {
-		return;
-	}
+function Join(_command: 'join', params: string, item: Record<string, string>): void {
 	let channel = params.trim();
 	if (channel === '') {
 		return;
 	}
-	const userId = Meteor.userId();
-	if (!userId) {
-		return;
-	}
+
 	channel = channel.replace('#', '');
+
+	const userId = Meteor.userId() as string;
 	const user = Meteor.users.findOne(userId);
 	const room = Rooms.findOneByNameAndType(channel, 'c');
+
+	if (!user) {
+		return;
+	}
+
 	if (!room) {
 		api.broadcast('notify.ephemeralMessage', userId, item.rid, {
 			msg: TAPi18n.__('Channel_doesnt_exist', {
@@ -32,21 +32,16 @@ function Join(command: string, params: string, item: Record<string, string>): vo
 		});
 	}
 
-	if (!user) {
-		return;
-	}
-	if (!room) {
-		return;
-	}
-
 	const subscription = Subscriptions.findOneByRoomIdAndUserId(room._id, user._id, {
 		fields: { _id: 1 },
 	});
+
 	if (subscription) {
 		throw new Meteor.Error('error-user-already-in-room', 'You are already in the channel', {
 			method: 'slashCommands',
 		});
 	}
+
 	Meteor.call('joinRoom', room._id);
 }
 
