262eff666402cdca862156cb4779e46991b61a64
Eduardo Cabrera <eduardo.cabrera@rocket.chat>
2022-01-27 14:46:12 -0300

    Convert to typescript the slash commands invite all files

---
+++ app/slashcommands-inviteall/client/client.ts
@@ -0,0 +1,28 @@
+import { slashCommands } from '../../utils/lib/slashCommand';
+
+slashCommands.add(
+	'invite-all-to',
+	undefined,
+	{
+		description: 'Invite_user_to_join_channel_all_to',
+		params: '#room',
+		permission: ['add-user-to-joined-room', 'add-user-to-any-c-room', 'add-user-to-any-p-room'],
+	},
+	undefined,
+	false,
+	undefined,
+	undefined,
+);
+slashCommands.add(
+	'invite-all-from',
+	undefined,
+	{
+		description: 'Invite_user_to_join_channel_all_from',
+		params: '#room',
+		permission: 'add-user-to-joined-room',
+	},
+	undefined,
+	false,
+	undefined,
+	undefined,
+);
