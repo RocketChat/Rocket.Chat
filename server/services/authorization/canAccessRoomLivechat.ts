9597eb3cc47897acd763420a294ead1cf218e7fe
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-02-07 17:27:00 -0300

    Fixed invalid import from micro services

--- server/services/authorization/canAccessRoomLivechat.ts
+++ server/services/authorization/canAccessRoomLivechat.ts
@@ -1,7 +1,8 @@
 import { IAuthorizationLivechat } from '../../sdk/types/IAuthorizationLivechat';
 import { proxifyWithWait } from '../../sdk/lib/proxify';
 import { RoomAccessValidator } from '../../sdk/types/IAuthorization';
-import { LivechatRooms } from '../../../app/models/server/raw';
+import { Rooms } from './service';
+import type { IOmnichannelRoom } from '../../../definition/IRoom';
 
 export const AuthorizationLivechat = proxifyWithWait<IAuthorizationLivechat>('authorization-livechat');
 
@@ -14,7 +15,7 @@ export const canAccessRoomLivechat: RoomAccessValidator = async (room, user, ext
 	// if we received a partial room, load the full IOmnichannelRoom data for it
 	// Otherwise, try to load the data from the extraData (this is the case for file uploads)
 	const rid = room?._id || extraData?.rid;
-	const livechatRoom = rid && (await LivechatRooms.findOneById(rid));
+	const livechatRoom = rid && (await Rooms.findOneById(rid));
 
 	// Check the type again in case the room parameter was not received
 	if (livechatRoom?.t !== 'l') {
@@ -22,5 +23,5 @@ export const canAccessRoomLivechat: RoomAccessValidator = async (room, user, ext
 	}
 
 	// Call back core temporarily
-	return AuthorizationLivechat.canAccessRoom(livechatRoom, user, extraData);
+	return AuthorizationLivechat.canAccessRoom(livechatRoom as IOmnichannelRoom, user, extraData);
 };
