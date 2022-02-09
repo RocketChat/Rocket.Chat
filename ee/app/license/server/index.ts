2daf83769d6b0316fd243175a91f7e87c1e7786d
Diego Sampaio <chinello@gmail.com>
2022-02-09 17:49:26 -0300

    Do not register internal services when using MS

--- ee/app/license/server/index.ts
+++ ee/app/license/server/index.ts
@@ -1,11 +1,7 @@
 import './settings';
 import './methods';
 import './startup';
-import { LicenseService } from './license.internalService';
-import { api } from '../../../../server/sdk/api';
 
 export { onLicense, overwriteClassOnLicense, isEnterprise, getMaxGuestUsers } from './license';
 
 export { getStatistics } from './getStatistics';
-
-api.registerService(new LicenseService());
--- ee/server/startup/index.ts
+++ ee/server/startup/index.ts
@@ -1,2 +1,3 @@
 import './engagementDashboard';
 import './seatsCap';
+import './services';
