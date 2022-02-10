f7c0d4de11db1215ca065de480419f20ebb1ff01
Kevin Aleman <kevin.aleman@rocket.chat>
2022-02-10 14:05:07 -0600

    Fix lint issues

--- client/components/voip/composer/Content.tsx
+++ client/components/voip/composer/Content.tsx
@@ -7,14 +7,7 @@ type ContentPropsType = {
 };
 
 const Content = ({ icon, text }: ContentPropsType): ReactElement => (
-	<Box
-		w='full'
-		h='full'
-		display='flex'
-		flexDirection='row'
-		alignItems='center'
-		justifyContent='center'
-	>
+	<Box w='full' h='full' display='flex' flexDirection='row' alignItems='center' justifyContent='center'>
 		<Icon color='info' size='24px' name={icon} />
 		<Box mis='4px' fontScale='p2' color='info'>
 			{text}
