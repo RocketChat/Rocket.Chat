222d35aba9bf50f2e233173f1c9a032cc9ba7e2f
juliajforesti <juliajforesti@gmail.com>
2022-02-14 17:05:19 -0300

    Replace AutoComplete with MultiSelectFiltered on CreateTeamModal/UsersInput

--- client/views/teams/CreateTeamModal/UsersInput.tsx
+++ client/views/teams/CreateTeamModal/UsersInput.tsx
@@ -1,16 +1,16 @@
-import { AutoComplete, Box, Option, Options, Chip, AutoCompleteProps } from '@rocket.chat/fuselage';
+import { Box, Option, Options, Chip, MultiSelectFiltered } from '@rocket.chat/fuselage';
 import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
-import React, { FC, memo, useCallback, useMemo, useState } from 'react';
+import React, { ComponentProps, FC, memo, useCallback, useMemo, useState } from 'react';
 
 import UserAvatar from '../../../components/avatar/UserAvatar';
 import { useEndpointData } from '../../../hooks/useEndpointData';
 
 type UsersInputProps = {
 	value: unknown[];
-	onChange: (value: unknown, action: 'remove' | undefined) => void;
+	onChange?: (value: unknown, action: 'remove' | undefined) => void;
 };
 
-type AutocompleteData = [AutoCompleteProps['options'], { [key: string]: string | undefined }];
+type AutocompleteData = [ComponentProps<typeof MultiSelectFiltered>['options'], { [key: string]: string | undefined }];
 
 const useUsersAutoComplete = (term: string): AutocompleteData => {
 	const params = useMemo(
@@ -26,11 +26,7 @@ const useUsersAutoComplete = (term: string): AutocompleteData => {
 			return [[], {}];
 		}
 
-		const options =
-			data.items.map((user) => ({
-				label: user.name ?? '',
-				value: user._id ?? '',
-			})) || [];
+		const options = data.items.map((user) => [user._id ?? '', user.name ?? '']) || [];
 
 		const labelData = Object.fromEntries(data.items.map((user) => [user._id, user.username]) || []);
 
@@ -38,35 +34,20 @@ const useUsersAutoComplete = (term: string): AutocompleteData => {
 	}, [data]);
 };
 
-const UsersInput: FC<UsersInputProps> = ({ onChange, ...props }) => {
+const UsersInput: FC<UsersInputProps> = (props) => {
 	const [filter, setFilter] = useState('');
 	const [options, labelData] = useUsersAutoComplete(useDebouncedValue(filter, 1000));
 
-	const onClickSelected = useCallback(
-		(e) => {
-			e.stopPropagation();
-			e.preventDefault();
-			onChange(e.currentTarget.value, 'remove');
-		},
-		[onChange],
+	const renderSelected: FC<{ value: string; onMouseDown: () => void }> = ({ value, onMouseDown }) => (
+		<>
+			<Chip {...props} height='x20' value={value} onClick={onMouseDown} mie='x4'>
+				<UserAvatar size='x20' username={labelData[value] as string} />
+				<Box is='span' margin='none' mis='x4'>
+					{labelData[value]}
+				</Box>
+			</Chip>
+		</>
 	);
-
-	const renderSelected = useCallback<FC<{ value?: string[] }>>(
-		({ value: selected }) => (
-			<>
-				{selected?.map((value) => (
-					<Chip key={value} {...props} height='x20' value={value} onClick={onClickSelected} mie='x4'>
-						<UserAvatar size='x20' username={labelData[value] as string} />
-						<Box is='span' margin='none' mis='x4'>
-							{labelData[value]}
-						</Box>
-					</Chip>
-				))}
-			</>
-		),
-		[onClickSelected, props, labelData],
-	);
-
 	const renderItem = useCallback<FC<{ value: string }>>(
 		({ value, ...props }) => (
 			<Option key={value} {...props} avatar={<UserAvatar size={Options.AvatarSize} username={labelData[value] as string} />} />
@@ -75,15 +56,7 @@ const UsersInput: FC<UsersInputProps> = ({ onChange, ...props }) => {
 	);
 
 	return (
-		<AutoComplete
-			{...props}
-			filter={filter}
-			options={options}
-			renderSelected={renderSelected}
-			renderItem={renderItem}
-			setFilter={setFilter}
-			onChange={onChange}
-		/>
+		<MultiSelectFiltered options={options} renderItem={renderItem} filter={filter} setFilter={setFilter} renderSelected={renderSelected} />
 	);
 };
 
