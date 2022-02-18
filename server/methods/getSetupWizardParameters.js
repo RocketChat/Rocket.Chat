43767344a2382ea2adb8845483c293ae291ecaca
dougfabris <devfabris@gmail.com>
2022-02-18 13:28:45 -0300

    fix: remove hasAdmin

--- server/methods/getSetupWizardParameters.js
+++ server/methods/getSetupWizardParameters.js
@@ -1,31 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Settings } from '../../app/models';
-import { Users } from '../../app/models/server';
 import { settings } from '../../app/settings/server';
 
 Meteor.methods({
 	getSetupWizardParameters() {
 		const setupWizardSettings = Settings.findSetupWizardSettings().fetch();
 		const serverAlreadyRegistered = !!settings.get('Cloud_Workspace_Client_Id') || process.env.DEPLOY_PLATFORM === 'rocket-cloud';
-		const hasAdmin = Boolean(
-			Users.findOne(
-				{
-					roles: 'admin',
-					type: 'user',
-				},
-				{
-					fields: {
-						_id: 1,
-					},
-				},
-			),
-		);
 
 		return {
 			settings: setupWizardSettings,
 			serverAlreadyRegistered,
-			hasAdmin,
 		};
 	},
 });
