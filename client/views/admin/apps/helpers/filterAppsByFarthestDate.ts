16e35bb2b20d308ce3f9c48aadf99a5ca343064a
rique223 <henrique.jobs1@gmail.com>
2022-02-18 13:04:45 -0300

    Rename functions for congruence and delete useless helpers

--- client/views/admin/apps/helpers/filterAppsByFarthestDate.ts
+++
@@ -1,6 +0,0 @@
-export const filterAppsByFarthestDate = (firstDateString: string, secondDateString: string): number => {
-	const firstDate = new Date(firstDateString);
-	const secondDate = new Date(secondDateString);
-
-	return +firstDate - +secondDate;
-};
