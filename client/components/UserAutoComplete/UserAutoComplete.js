16a3bc94897705be535ce41d044f96ff7557534c
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2022-02-21 20:44:53 -0300

    [IMPROVE] Replace AutoComplete in UserAutoComplete & UserAutoCompleteMultiple components (#24529)
    * Replace AutoComplete with MultiSelectFiltered on CreateTeamModal/UsersInput
    
    * Add addonIcon on CreateTeamModal/UsersInput multiSelect
    
    * Replace Autocomplete with SelectFiltered on UserAutoComplete
    
    * Replace AutoComplete in UserAutoCompleteMultiple & Unify with UsersInput
    
    * Replace AutoComplete in UserAutoCompleteMultiple & Unify with UsersInput
    
    * Update onChanges passed to UserAutoCompleteMultiple
    
    * remove log
    
    * chore: review
    
    * Update fuselage, fuselage-hooks, fuselage-ui-kit e onboarding-ui versions to @next
    
    * Fix typings
    
    * Bump Fuselage one more time
    
    Co-authored-by: dougfabris <devfabris@gmail.com>
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- client/components/UserAutoComplete/UserAutoComplete.js
+++ client/components/UserAutoComplete/UserAutoComplete.js
@@ -1,10 +1,9 @@
-import { AutoComplete, Option, Box, Chip } from '@rocket.chat/fuselage';
+import { SelectFiltered, Option, Box, Chip } from '@rocket.chat/fuselage';
 import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
 import React, { memo, useMemo, useState } from 'react';
 
 import { useEndpointData } from '../../hooks/useEndpointData';
 import UserAvatar from '../avatar/UserAvatar';
-import Avatar from './Avatar';
 
 const query = (term = '', conditions = {}) => ({ selector: JSON.stringify({ term, conditions }) });
 
@@ -18,29 +17,29 @@ const UserAutoComplete = (props) => {
 		useMemo(() => query(debouncedFilter, conditions), [filter]),
 	);
 
-	const options = useMemo(() => (data && data.items.map((user) => ({ value: user.username, label: user.name }))) || [], [data]);
+	const options = useMemo(() => (data && data.items.map((user) => [user.username, user.name])) || [], [data]);
+
+	const renderSelected = ({ value, label }) =>
+		value ? (
+			<Chip height='x20' value={value} onClick={() => props.onChange()} mie='x4'>
+				<UserAvatar size='x20' username={value} />
+				<Box verticalAlign='middle' is='span' margin='none' mi='x4'>
+					{label}
+				</Box>
+			</Chip>
+		) : null;
+
+	const renderItem = ({ value, ...props }) => <Option key={value} {...props} avatar={<UserAvatar size='x20' username={value} />} />;
 
 	return (
-		<AutoComplete
+		<SelectFiltered
 			{...props}
+			options={options}
 			filter={filter}
 			setFilter={setFilter}
-			renderSelected={({ value, label }) => {
-				if (!value) {
-					return '';
-				}
-
-				return (
-					<Chip height='x20' value={value} onClick={() => props.onChange()} mie='x4'>
-						<UserAvatar size='x20' username={value} />
-						<Box verticalAlign='middle' is='span' margin='none' mi='x4'>
-							{label}
-						</Box>
-					</Chip>
-				);
-			}}
-			renderItem={({ value, ...props }) => <Option key={value} {...props} avatar={<Avatar value={value} />} />}
-			options={options}
+			addonIcon='magnifier'
+			renderItem={renderItem}
+			renderSelected={renderSelected}
 		/>
 	);
 };
