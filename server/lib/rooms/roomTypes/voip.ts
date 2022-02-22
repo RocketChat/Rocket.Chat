fe228b3b3ebb86ecb46c466e18f578f46c5f3f8e
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-02-22 15:06:26 -0300

    updated room types

--- server/lib/rooms/roomTypes/voip.ts
+++ server/lib/rooms/roomTypes/voip.ts
@@ -1,3 +1,7 @@
+import { Users } from '../../../../app/models/server';
+import type { IRoomTypeServerDirectives } from '../../../../definition/IRoomTypeConfig';
+import type { IUser } from '../../../../definition/IUser';
+import type { AtLeast } from '../../../../definition/utils';
 import { getVoipRoomType } from '../../../../lib/rooms/roomTypes/voip';
 import { roomCoordinator } from '../roomCoordinator';
 
@@ -7,4 +11,15 @@ roomCoordinator.add(VoipRoomType, {
 	roomName(room: any): string | undefined {
 		return room.name || room.fname || room.label;
 	},
-});
+
+	getNotificationDetails(room, _user, notificationMessage: string) {
+		const title = `[Omnichannel] ${this.roomName(room)}`;
+		const text = notificationMessage;
+
+		return { title, text };
+	},
+
+	getMsgSender(senderId: string): IUser {
+		return Users.findOneById(senderId);
+	},
+} as AtLeast<IRoomTypeServerDirectives, 'roomName'>);
