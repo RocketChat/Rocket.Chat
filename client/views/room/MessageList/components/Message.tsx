0f3b5b458057ec9d882072c175dcea13e0adaf75
Filipe Marins <filipe.marins@rocket.chat>
2022-02-22 13:15:46 -0300

    feat: add is read receipt icon

--- client/views/room/MessageList/components/Message.tsx
+++ client/views/room/MessageList/components/Message.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable complexity */
+import { css } from '@rocket.chat/css-in-js';
 import {
 	Box,
 	Message as MessageTemplate,
@@ -14,6 +15,7 @@ import {
 	MessageReactions,
 	MessageStatusPrivateIndicator,
 	MessageReactionAction,
+	Icon,
 } from '@rocket.chat/fuselage';
 import React, { FC, memo } from 'react';
 
@@ -26,6 +28,7 @@ import Broadcast from '../../../../components/Message/Metrics/Broadcast';
 import Discussion from '../../../../components/Message/Metrics/Discussion';
 import Thread from '../../../../components/Message/Metrics/Thread';
 import UserAvatar from '../../../../components/avatar/UserAvatar';
+import { useSetting } from '../../../../contexts/SettingsContext';
 import { TranslationKey, useTranslation } from '../../../../contexts/TranslationContext';
 import { useUserId } from '../../../../contexts/UserContext';
 import { useUserData } from '../../../../hooks/useUserData';
@@ -75,6 +78,8 @@ const Message: FC<{ message: IMessage; sequential: boolean; subscription?: ISubs
 	const oembedWidth = useMessageOembedMaxWidth();
 
 	const openEmojiPicker = useOpenEmojiPicker(message);
+
+	const isReadReceiptEnabled = useSetting('Message_Read_Receipt_Enabled');
 
 	const showRoles = useMessageListShowRoles();
 	const showRealName = useMessageListShowRealName();
@@ -200,6 +205,18 @@ const Message: FC<{ message: IMessage; sequential: boolean; subscription?: ISubs
 				{oembedIsEnabled && message.urls && (
 					<Box width={oembedWidth}>
 						<MessageBlock mid={message._id} blocks={convertOembedToUiKit(message.urls)} appId rid={message.rid} />
+					</Box>
+				)}
+
+				{isReadReceiptEnabled && (
+					<Box
+						position='absolute'
+						className={css`
+							top: 2px;
+							right: 0.5rem;
+						`}
+					>
+						<Icon name='check' size='x11' color='primary' />
 					</Box>
 				)}
 			</MessageContainer>
