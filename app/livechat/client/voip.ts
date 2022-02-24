a481c13da6b4a3fb953e0352817c851ae2970ac4
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2022-02-24 19:26:46 +0530

    Regression: Fix time format on Voip system messages (#24603)

--- app/livechat/client/voip.ts
+++ app/livechat/client/voip.ts
@@ -3,23 +3,13 @@ import moment from 'moment';
 import { MessageTypes, IMessageType } from '../../ui-utils/client';
 import { IMessage, isVoipMessage } from '../../../definition/IMessage';
 
-type IMessageFuncReturn = { at: string } | { at: string; time: string } | { comment: string } | { duration: string } | { reason: string };
+type IMessageFuncReturn = { comment: string } | { duration: string } | { reason: string };
 
 const messageTypes: IMessageType[] = [
 	{
 		id: 'voip-call-started',
 		system: true,
 		message: 'Voip_call_started',
-		data(message: IMessage): IMessageFuncReturn {
-			if (!isVoipMessage(message)) {
-				return { at: '', time: '' };
-			}
-			const seconds = message.voipData.callWaitingTime || 0;
-			return {
-				at: message.voipData.callStarted?.toString() || 'unknown date',
-				time: moment.duration(seconds, 'seconds').humanize(),
-			};
-		},
 	},
 	{
 		id: 'voip-call-duration',
@@ -45,31 +35,16 @@ const messageTypes: IMessageType[] = [
 		id: 'voip-call-on-hold',
 		system: true,
 		message: 'Voip_call_on_hold',
-		data(message: IMessage): IMessageFuncReturn {
-			return {
-				at: message.ts.toString(),
-			};
-		},
 	},
 	{
 		id: 'voip-call-unhold',
 		system: true,
 		message: 'Voip_call_unhold',
-		data(message: IMessage): IMessageFuncReturn {
-			return {
-				at: message.ts.toString(),
-			};
-		},
 	},
 	{
 		id: 'voip-call-ended',
 		system: true,
 		message: 'Voip_call_ended',
-		data(message: IMessage): IMessageFuncReturn {
-			return {
-				at: message.ts.toString(),
-			};
-		},
 	},
 	{
 		id: 'voip-call-ended-unexpectedly',
