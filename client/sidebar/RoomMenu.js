d9037aa0dc70cf5aa21541566f3d1b182c1cb29f
Himanshu664 <gs2012023@sgsitsindore.in>
2022-02-24 18:11:47 +0530

    Removed tool tip from sidebar menu

--- client/sidebar/RoomMenu.js
+++ client/sidebar/RoomMenu.js
@@ -182,7 +182,7 @@ const RoomMenu = ({ rid, unread, threadUnread, alert, roomOpen, type, cl, name =
 			aria-keyshortcuts='alt'
 			tabIndex={-1}
 			options={menuOptions}
-			renderItem={({ label: { label, icon }, ...props }) => <Option label={label} title={label} icon={icon} {...props} />}
+			renderItem={({ label: { label, icon }, ...props }) => <Option label={label} icon={icon} {...props} />}
 		/>
 	);
 };
