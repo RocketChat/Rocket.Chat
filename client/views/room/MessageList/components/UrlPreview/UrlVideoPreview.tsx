02d3fb7f73d5e5419767a2b2eafdc49ab14aa0b5
gabriellsh <gabriel.henriques@rocket.chat>
2022-02-25 18:21:59 +0100

    Fix video width

--- client/views/room/MessageList/components/UrlPreview/UrlVideoPreview.tsx
+++ client/views/room/MessageList/components/UrlPreview/UrlVideoPreview.tsx
@@ -2,8 +2,9 @@ import React, { ReactElement } from 'react';
 
 import type { UrlPreview } from './PreviewList';
 
+const style = { maxWidth: '100%' };
 const UrlVideoPreview = ({ url, originalType }: Omit<UrlPreview, 'type'>): ReactElement => (
-	<video controls>
+	<video controls style={style}>
 		<source src={url} type={originalType} />
 		Your browser doesn't support the video element.
 	</video>
