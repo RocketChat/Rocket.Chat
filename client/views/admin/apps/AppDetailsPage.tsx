f03c2f514de4385145ee7072864c7b23c46f3828
rique223 <henrique.jobs1@gmail.com>
2022-03-04 20:06:54 -0300

    feat: :sparkles: Implement tab navigation on app details page(Logic)
    I've implemented the necessary logic for the app details page tabs component to change the page content based on the selected tab. Also, I've created an AppDetailsPageHeader to improve readability in the AppDetailsPage component.

--- client/views/admin/apps/AppDetailsPage.tsx
+++ client/views/admin/apps/AppDetailsPage.tsx
@@ -1,18 +1,15 @@
 import { ISetting } from '@rocket.chat/apps-engine/definition/settings';
 import { Button, ButtonGroup, Icon, Box, Throbber, Tabs } from '@rocket.chat/fuselage';
-import React, { useState, useCallback, useRef, FC } from 'react';
+import React, { useState, useCallback, useRef, FC, MouseEventHandler } from 'react';
 
 import { Apps } from '../../../../app/apps/client/orchestrator';
 import Page from '../../../components/Page';
-import AppAvatar from '../../../components/avatar/AppAvatar';
 import { useRoute, useCurrentRoute } from '../../../contexts/RouterContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import APIsDisplay from './APIsDisplay';
 import AppDetailsPageContent from './AppDetailsPageContent';
-import AppMenu from './AppMenu';
-import AppStatus from './AppStatus';
+import AppDetailsPageHeader from './AppDetailsPageHeader';
 import LoadingDetails from './LoadingDetails';
-import PriceDisplay from './PriceDisplay';
 import SettingsDisplay from './SettingsDisplay';
 import { handleAPIError } from './helpers';
 import { useAppInfo } from './hooks/useAppInfo';
@@ -22,6 +19,12 @@ const AppDetailsPage: FC<{ id: string }> = function AppDetailsPage({ id }) {
 
 	const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
 	const [isSaving, setIsSaving] = useState(false);
+	const [selectedTab, setSelectedTab] = useState({
+		shouldSelectDetails: true,
+		shouldSelectLogs: false,
+		shouldSelectSettings: false,
+	});
+
 	const settingsRef = useRef<Record<string, ISetting['value']>>({});
 
 	const data = useAppInfo(id);
@@ -35,7 +38,6 @@ const AppDetailsPage: FC<{ id: string }> = function AppDetailsPage({ id }) {
 
 	const { settings, apis } = { settings: {}, apis: [], ...data };
 
-	const showSettings = Object.values(settings).length;
 	const showApis = apis.length;
 
 	const saveAppSettings = useCallback(async () => {
@@ -52,7 +54,59 @@ const AppDetailsPage: FC<{ id: string }> = function AppDetailsPage({ id }) {
 		setIsSaving(false);
 	}, [id, settings]);
 
-	const { iconFileData = '', name, author, version, price, purchaseType, pricingPlans, iconFileContent, installed } = data || {};
+	const isClickedTabDetails = useCallback(
+		(e: React.MouseEvent<HTMLElement, MouseEvent>): boolean => e.currentTarget.innerText === t('Details'),
+		[t],
+	);
+
+	const isClickedTabLogs = useCallback(
+		(e: React.MouseEvent<HTMLElement, MouseEvent>): boolean => e.currentTarget.innerText === t('Logs'),
+		[t],
+	);
+
+	const isClickedTabSettings = useCallback(
+		(e: React.MouseEvent<HTMLElement, MouseEvent>): boolean => e.currentTarget.innerText === t('Settings'),
+		[t],
+	);
+
+	const selectTab: MouseEventHandler<HTMLElement> = useCallback(
+		(e) => {
+			if (isClickedTabDetails(e)) {
+				setSelectedTab({
+					shouldSelectDetails: true,
+					shouldSelectLogs: false,
+					shouldSelectSettings: false,
+				});
+			}
+
+			if (isClickedTabLogs(e)) {
+				setSelectedTab({
+					shouldSelectDetails: false,
+					shouldSelectLogs: true,
+					shouldSelectSettings: false,
+				});
+			}
+
+			if (isClickedTabSettings(e)) {
+				setSelectedTab({
+					shouldSelectDetails: false,
+					shouldSelectLogs: false,
+					shouldSelectSettings: true,
+				});
+			}
+		},
+		[isClickedTabDetails, isClickedTabLogs, isClickedTabSettings],
+	);
+
+	const { shouldSelectDetails, shouldSelectLogs, shouldSelectSettings } = selectedTab;
+
+	const isSettingsTabSelected = Boolean(shouldSelectSettings && Object.values(settings).length);
+	const isSettingsTabEnabled = Boolean(Object.values(settings).length);
+
+	const isDetailsTabSelected = shouldSelectDetails;
+	const areApisVisible = Boolean(shouldSelectDetails && !!showApis);
+
+	const isLogsTabSelected = shouldSelectLogs;
 
 	return (
 		<Page flexDirection='column'>
@@ -73,43 +127,28 @@ const AppDetailsPage: FC<{ id: string }> = function AppDetailsPage({ id }) {
 					{!data && <LoadingDetails />}
 					{data && (
 						<>
-							<Box display='flex' flexDirection='row' mbe='x20' w='full'>
-								<AppAvatar size='x124' mie='x20' iconFileContent={iconFileContent} iconFileData={iconFileData} />
-								<Box display='flex' flexDirection='column' justifyContent='space-between' flexGrow={1}>
-									<Box fontScale='h2'>{name}</Box>
-									<Box display='flex' flexDirection='row' color='hint' alignItems='center'>
-										<Box fontScale='p2m' mie='x4'>
-											{t('By_author', { author: author?.name })}
-										</Box>
-										|<Box mis='x4'>{t('Version_version', { version })}</Box>
-									</Box>
-									<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between'>
-										<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center' marginInline='neg-x8'>
-											<AppStatus app={data} marginInline='x8' />
-											{!installed && (
-												<PriceDisplay
-													purchaseType={purchaseType}
-													pricingPlans={pricingPlans}
-													price={price}
-													showType={false}
-													marginInline='x8'
-												/>
-											)}
-										</Box>
-										{installed && <AppMenu app={data} />}
-									</Box>
-								</Box>
-							</Box>
+							<AppDetailsPageHeader app={data} />
 
 							<Tabs mis='-x24' mb='x36'>
-								<Tabs.Item selected>{t('Details')}</Tabs.Item>
-								<Tabs.Item>{t('Logs')}</Tabs.Item>
-								<Tabs.Item>{t('Settings')}</Tabs.Item>
+								<Tabs.Item onClick={selectTab} selected={shouldSelectDetails}>
+									{t('Details')}
+								</Tabs.Item>
+								<Tabs.Item onClick={selectTab} selected={shouldSelectLogs}>
+									{t('Logs')}
+								</Tabs.Item>
+								<Tabs.Item
+									onClick={isSettingsTabEnabled ? selectTab : (): boolean => false}
+									selected={shouldSelectSettings}
+									disabled={!isSettingsTabEnabled}
+								>
+									{t('Settings')}
+								</Tabs.Item>
 							</Tabs>
 
-							<AppDetailsPageContent app={data} />
-							{!!showApis && <APIsDisplay apis={apis} />}
-							{!!showSettings && (
+							{isDetailsTabSelected && <AppDetailsPageContent app={data} />}
+							{areApisVisible && <APIsDisplay apis={apis} />}
+							{isLogsTabSelected && <div>Logs will be here...</div>}
+							{isSettingsTabSelected && (
 								<SettingsDisplay settings={settings} setHasUnsavedChanges={setHasUnsavedChanges} settingsRef={settingsRef} />
 							)}
 						</>
---
+++ client/views/admin/apps/AppDetailsPageHeader.tsx
@@ -0,0 +1,46 @@
+import { Box } from '@rocket.chat/fuselage';
+import { data } from 'jquery';
+import React, { FC } from 'react';
+
+import AppAvatar from '../../../components/avatar/AppAvatar';
+import { useTranslation } from '../../../contexts/TranslationContext';
+import AppMenu from './AppMenu';
+import AppStatus from './AppStatus';
+import PriceDisplay from './PriceDisplay';
+import { App } from './types';
+
+type AppDetailsPageHeaderProps = {
+	app: App;
+};
+
+const AppDetailsPageHeader: FC<AppDetailsPageHeaderProps> = ({ app }) => {
+	const t = useTranslation();
+
+	const { iconFileData = '', name, author, version, price, purchaseType, pricingPlans, iconFileContent, installed } = app || {};
+
+	return (
+		<Box display='flex' flexDirection='row' mbe='x20' w='full'>
+			<AppAvatar size='x124' mie='x20' iconFileContent={iconFileContent} iconFileData={iconFileData} />
+			<Box display='flex' flexDirection='column' justifyContent='space-between' flexGrow={1}>
+				<Box fontScale='h2'>{name}</Box>
+				<Box display='flex' flexDirection='row' color='hint' alignItems='center'>
+					<Box fontScale='p2m' mie='x4'>
+						{t('By_author', { author: author?.name })}
+					</Box>
+					|<Box mis='x4'>{t('Version_version', { version })}</Box>
+				</Box>
+				<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between'>
+					<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center' marginInline='neg-x8'>
+						<AppStatus app={data} marginInline='x8' />
+						{!installed && (
+							<PriceDisplay purchaseType={purchaseType} pricingPlans={pricingPlans} price={price} showType={false} marginInline='x8' />
+						)}
+					</Box>
+					{installed && <AppMenu app={data} />}
+				</Box>
+			</Box>
+		</Box>
+	);
+};
+
+export default AppDetailsPageHeader;
