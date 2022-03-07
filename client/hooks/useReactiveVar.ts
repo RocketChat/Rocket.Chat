af8dfe0d0d1bb5ad91f7845ac9a00a87e6105c8a
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-03-07 14:33:40 -0300

    Apply suggestions from code review

--- client/hooks/useReactiveVar.ts
+++ client/hooks/useReactiveVar.ts
@@ -7,13 +7,11 @@ export const useReactiveVar = <T>(variable: ReactiveVar<T>): T => {
 	const [value, setValue] = useState(() => Tracker.nonreactive(() => variable.get()));
 
 	useEffect(() => {
-		let computation: Tracker.Computation;
-		Tracker.nonreactive(() => {
-			computation = Tracker.autorun(() => {
+		const computation = Tracker.nonreactive(() => Tracker.autorun(() => {
 				const value = variable.get();
 				setValue(() => value);
-			});
-		});
+			})
+		);
 
 		return (): void => {
 			computation?.stop();
