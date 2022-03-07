9c48d871d1b28d3b630bceba02f3f2d784756bd1
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2022-03-07 14:05:41 -0300

    [FIX] Components for user search (#24677)
    * Replace AutoComplete with MultiSelectFiltered on CreateTeamModal/UsersInput
    
    * regression fuselage packages
    
    * Handle loading state
    
    * fix UserAutoCompleteMultiple to handle id as a value
    
    * manage dataLabel filtering
    
    * Set default prop value
    
    * Add default valueIsId in UserAutoCompleteMultiple
    
    * remove log
    
    * Add missing `useMemo` dependency
    
    * Use function state dispatch to avoid triggering a new effect
    
    * Fix prettier formatting
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- client/components/UserAutoCompleteMultiple/UserAutoCompleteMultiple.js
+++ client/components/UserAutoCompleteMultiple/UserAutoCompleteMultiple.js
@@ -1,41 +1,59 @@
 import { MultiSelectFiltered, Box, Option, OptionAvatar, OptionContent, OptionDescription, Chip, CheckBox } from '@rocket.chat/fuselage';
 import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
-import React, { memo, useMemo, useState } from 'react';
+import React, { memo, useEffect, useMemo, useState } from 'react';
 
+import { useTranslation } from '../../contexts/TranslationContext';
 import { useEndpointData } from '../../hooks/useEndpointData';
+import { AsyncStatePhase } from '../../lib/asyncState';
 import UserAvatar from '../avatar/UserAvatar';
 
 const query = (term = '') => ({ selector: JSON.stringify({ term }) });
 
-const UserAutoCompleteMultiple = (props) => {
+const UserAutoCompleteMultiple = ({ valueIsId = false, ...props }) => {
+	const t = useTranslation();
 	const [filter, setFilter] = useState('');
+	const [labelData, setLabelData] = useState({});
 	const debouncedFilter = useDebouncedValue(filter, 1000);
-	const { value: data } = useEndpointData(
+	const { value: data, phase } = useEndpointData(
 		'users.autocomplete',
 		useMemo(() => query(debouncedFilter), [debouncedFilter]),
 	);
-	const options = useMemo(() => (data && data.items.map((user) => [user.username, user.name])) || [], [data]);
-
-	const renderItem = ({ value, label, selected, ...props }) => (
-		<Option key={value} {...props}>
-			<OptionAvatar>
-				<UserAvatar username={value} size='x20' />
-			</OptionAvatar>
-			<OptionContent>
-				{label} <OptionDescription>({value})</OptionDescription>
-			</OptionContent>
-			<CheckBox checked={selected} />
-		</Option>
+	const options = useMemo(
+		() => (data && data.items.map((user) => [valueIsId ? user._id : user.username, user.name])) || [],
+		[data, valueIsId],
 	);
 
-	const renderSelected = ({ value, onMouseDown }) => (
-		<Chip {...props} key={value} value={value} onClick={onMouseDown} margin='x4'>
-			<UserAvatar size='x20' username={value} />
-			<Box is='span' margin='none' mis='x4'>
-				{value}
-			</Box>
-		</Chip>
-	);
+	useEffect(() => {
+		const newLabelData = Object.fromEntries((data && data.items.map((user) => [user._id, user.username])) || []);
+		setLabelData((labelData) => ({ ...labelData, ...newLabelData }));
+	}, [data]);
+
+	const renderItem = ({ value, label, selected, ...props }) => {
+		const username = valueIsId ? labelData[value] : value;
+		return (
+			<Option key={value} {...props}>
+				<OptionAvatar>
+					<UserAvatar username={username} size='x20' />
+				</OptionAvatar>
+				<OptionContent>
+					{label} <OptionDescription>({username})</OptionDescription>
+				</OptionContent>
+				<CheckBox checked={selected} />
+			</Option>
+		);
+	};
+
+	const renderSelected = ({ value, onMouseDown }) => {
+		const username = valueIsId ? labelData[value] : value;
+		return (
+			<Chip {...props} key={value} value={value} onClick={onMouseDown} margin='x4'>
+				<UserAvatar size='x20' username={username} />
+				<Box is='span' margin='none' mis='x4'>
+					{username}
+				</Box>
+			</Chip>
+		);
+	};
 
 	return (
 		<MultiSelectFiltered
@@ -46,6 +64,7 @@ const UserAutoCompleteMultiple = (props) => {
 			renderSelected={renderSelected}
 			renderItem={renderItem}
 			addonIcon='magnifier'
+			customEmpty={phase === AsyncStatePhase.LOADING ? t('Loading') : t('None')} // TODO: add proper empty state
 		/>
 	);
 };
