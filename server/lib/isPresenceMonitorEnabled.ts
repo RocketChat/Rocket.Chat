ce61cde18a542009b62b8a199d98e3f60103fcbb
Diego Sampaio <chinello@gmail.com>
2022-03-08 11:29:02 -0300

    Don't start presence monitor is running ms

--- server/lib/isPresenceMonitorEnabled.ts
+++ server/lib/isPresenceMonitorEnabled.ts
@@ -1,7 +1,9 @@
+import { isRunningMs } from './isRunningMs';
+
 const startMonitor =
 	typeof process.env.DISABLE_PRESENCE_MONITOR === 'undefined' ||
 	!['true', 'yes'].includes(String(process.env.DISABLE_PRESENCE_MONITOR).toLowerCase());
 
 export function isPresenceMonitorEnabled(): boolean {
-	return startMonitor;
+	return startMonitor && !isRunningMs();
 }
