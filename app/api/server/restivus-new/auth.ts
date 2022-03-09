d45e767ad3b0d0b2ff10fb1405a9638489bf35fc
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-03-09 16:18:32 -0300

    chore: converted some restivus module files to ts

---
+++ app/api/server/restivus-new/auth.ts
@@ -0,0 +1,114 @@
+/* eslint-disable prefer-const */
+/*
+   Return a MongoDB query selector for finding the given user
+   A password can be either in plain text or hashed
+
+   A valid user will have exactly one of the following identification fields: id, username, or email
+*/
+
+import { Meteor } from 'meteor/meteor';
+import { Match, check } from 'meteor/check';
+import _ from 'underscore';
+import { Accounts } from 'meteor/accounts-base';
+
+import { IUser } from '../../../../definition/IUser';
+
+let getUserQuerySelector: (user: IUser) => {
+	'_id'?: string;
+	'username'?: string;
+	'emails.address'?: any;
+};
+let passwordValidator: Match.Matcher<string | { digest: string; algorithm: string }>;
+let userValidator: Match.Matcher<unknown>;
+
+// this.Auth || (this.Auth = {});
+const Auth: { [key: string]: unknown } = {};
+
+userValidator = Match.Where(function (user) {
+	check(user, {
+		id: Match.Optional(String),
+		username: Match.Optional(String),
+		email: Match.Optional(String),
+	});
+	if (_.keys(user).length !== 1) {
+		throw new Meteor.Error('User must have exactly one identifier field');
+	}
+	return true;
+});
+
+passwordValidator = Match.OneOf(String, {
+	digest: String,
+	algorithm: String,
+});
+
+getUserQuerySelector = function (user: IUser): {
+	'_id'?: string;
+	'username'?: string;
+	'emails.address'?: any;
+} {
+	if (user._id) {
+		return {
+			_id: user._id,
+		};
+	}
+	if (user.username) {
+		return {
+			username: user.username,
+		};
+	}
+	if (user.emails) {
+		return {
+			'emails.address': user.emails,
+		};
+	}
+	// We shouldn't be here if the user object was properly validated
+	throw new Error('Cannot create selector from invalid user');
+};
+
+// Log a user in with their password
+
+Auth.loginWithPassword = function (
+	user: IUser,
+	password: Accounts.Password,
+): {
+	authToken: string;
+	userId: string;
+} {
+	let authToken;
+	let authenticatingUser;
+	let authenticatingUserSelector;
+	let hashedToken;
+	let passwordVerification;
+	let ref;
+	if (!user || !password) {
+		throw new Meteor.Error(401, 'Unauthorized');
+	}
+	// Validate the login input types
+	check(user, userValidator);
+	check(password, passwordValidator);
+	// Retrieve the user from the database
+	authenticatingUserSelector = getUserQuerySelector(user);
+	authenticatingUser = Meteor.users.findOne(authenticatingUserSelector);
+	if (!authenticatingUser) {
+		throw new Meteor.Error(401, 'Unauthorized');
+	}
+	if (!((ref = authenticatingUser.services) != null ? ref.password : undefined)) {
+		throw new Meteor.Error(401, 'Unauthorized');
+	}
+	// Authenticate the user's password
+	passwordVerification = Accounts._checkPassword(authenticatingUser, password);
+	if (passwordVerification.error) {
+		throw new Meteor.Error(401, 'Unauthorized');
+	}
+	// Add a new auth token to the user's account
+	authToken = Accounts._generateStampedLoginToken();
+	hashedToken = Accounts._hashLoginToken(authToken.token);
+	Accounts._insertHashedLoginToken(authenticatingUser._id, {
+		hashedToken,
+		when: new Date(),
+	});
+	return {
+		authToken: authToken.token,
+		userId: authenticatingUser._id,
+	};
+};
---
+++ app/api/server/restivus-new/iron-router-error-to-response.ts
@@ -0,0 +1,34 @@
+// We need a function that treats thrown errors exactly like Iron Router would.
+// This file is written in JavaScript to enable copy-pasting Iron Router code.
+
+import { IncomingMessage, ServerResponse } from 'http';
+
+// Taken from: https://github.com/iron-meteor/iron-router/blob/9c369499c98af9fd12ef9e68338dee3b1b1276aa/lib/router_server.js#L3
+const env = process.env.NODE_ENV || 'development';
+
+// Taken from: https://github.com/iron-meteor/iron-router/blob/9c369499c98af9fd12ef9e68338dee3b1b1276aa/lib/router_server.js#L47
+// eslint-disable-next-line no-unused-vars
+export function ironRouterSendErrorToResponse(err: Error, req: IncomingMessage, res: ServerResponse): unknown {
+	let msg;
+
+	if (res.statusCode && res.statusCode < 400) res.statusCode = 500;
+
+	if (err.name) res.statusCode = parseInt(err.name);
+
+	if (env === 'development') {
+		msg = `${err.stack || err.toString()}\n`;
+	}
+	// XXX get this from standard dict of error messages?
+	else {
+		msg = 'Server error.';
+	}
+
+	console.error(err.stack || err.toString());
+
+	if (res.headersSent) return req.socket.destroy();
+
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(msg));
+	if (req.method === 'HEAD') return res.end();
+	res.end(msg);
+}
