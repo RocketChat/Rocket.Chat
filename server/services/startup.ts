4e1e1131f6caf7aecbd0135b8b91d8acb640a83b
Diego Sampaio <chinello@gmail.com>
2022-03-10 20:13:58 -0300

    Regression: Register services right away (#24800)

--- server/services/startup.ts
+++ server/services/startup.ts
@@ -1,4 +1,3 @@
-import { Meteor } from 'meteor/meteor';
 import { MongoInternals } from 'meteor/mongo';
 
 import { AnalyticsService } from './analytics/service';
@@ -18,31 +17,28 @@ import { OmnichannelVoipService } from './omnichannel-voip/service';
 import { VoipService } from './voip/service';
 import { isRunningMs } from '../lib/isRunningMs';
 
-// Register services only after Meteor.startup to make sure everything is ready
-Meteor.startup(() => {
-	const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
+const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
 
-	api.registerService(new AnalyticsService(db));
-	api.registerService(new AuthorizationLivechat());
-	api.registerService(new AuthorizationTokenpass());
-	api.registerService(new BannerService(db));
-	api.registerService(new LDAPService());
-	api.registerService(new MediaService());
-	api.registerService(new MeteorService());
-	api.registerService(new NPSService(db));
-	api.registerService(new RoomService(db));
-	api.registerService(new SAUMonitorService());
-	api.registerService(new VoipService(db));
-	api.registerService(new OmnichannelVoipService(db));
-	api.registerService(new TeamService(db));
-	api.registerService(new UiKitCoreApp());
+api.registerService(new AnalyticsService(db));
+api.registerService(new AuthorizationLivechat());
+api.registerService(new AuthorizationTokenpass());
+api.registerService(new BannerService(db));
+api.registerService(new LDAPService());
+api.registerService(new MediaService());
+api.registerService(new MeteorService());
+api.registerService(new NPSService(db));
+api.registerService(new RoomService(db));
+api.registerService(new SAUMonitorService());
+api.registerService(new VoipService(db));
+api.registerService(new OmnichannelVoipService(db));
+api.registerService(new TeamService(db));
+api.registerService(new UiKitCoreApp());
 
-	// if the process is running in micro services mode we don't need to register services that will run separately
-	if (!isRunningMs()) {
-		(async (): Promise<void> => {
-			const { Authorization } = await import('./authorization/service');
+// if the process is running in micro services mode we don't need to register services that will run separately
+if (!isRunningMs()) {
+	(async (): Promise<void> => {
+		const { Authorization } = await import('./authorization/service');
 
-			api.registerService(new Authorization(db));
-		})();
-	}
-});
+		api.registerService(new Authorization(db));
+	})();
+}
