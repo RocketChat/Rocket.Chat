181a27da2b9a563908bf980f9d7dcbe2189ea4b7
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-10 11:28:12 -0300

    [FIX] Opening a new DM from user card (#24623)
    * Change `useRoomIcon` signature
    
    * Fix `IRoom[t]` and `ISubscription[t]`
    
    * Ensure `IRoom['uids']` is not mandatory
    
    * Fix some typings

--- client/views/room/Header/RoomTitle.tsx
+++ client/views/room/Header/RoomTitle.tsx
@@ -4,7 +4,11 @@ import { IRoom } from '../../../../definition/IRoom';
 import Header from '../../../components/Header';
 import HeaderIconWithRoom from './HeaderIconWithRoom';
 
-const RoomTitle = ({ room }: { room: IRoom }): ReactElement => (
+type RoomTitleProps = {
+	room: IRoom;
+};
+
+const RoomTitle = ({ room }: RoomTitleProps): ReactElement => (
 	<>
 		<HeaderIconWithRoom room={room} />
 		<Header.Title>{room.name}</Header.Title>
