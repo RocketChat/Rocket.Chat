9783ad4a3987ea1fe99dfce8f6c152873b9fc003
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-03-11 10:36:35 -0300

    Chore: converted more hooks to typescript (#24628)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- client/hooks/useFormatDate.ts
+++ client/hooks/useFormatDate.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react';
 
 import { useSetting } from '../contexts/SettingsContext';
 
-export const useFormatDate = () => {
+export const useFormatDate = (): ((time: string | Date | number) => string) => {
 	const format = useSetting('Message_DateFormat');
-	return useCallback((time) => moment(time).format(format), [format]);
+	return useCallback((time) => moment(time).format(String(format)), [format]);
 };
