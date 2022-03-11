72c1804ee282971e5760ac84da7125aaac2c4797
rique223 <henrique.jobs1@gmail.com>
2022-03-11 17:59:05 -0300

    feat: :construction: Create first part of new header style
    Some changes on the app header layout, added placeholder description, changed "metadata" layout, created placeholder chip for bundles and changed title size. Will be implementing the rest of the header soon.

--- client/views/admin/apps/AppDetailsHeader.tsx
+++ client/views/admin/apps/AppDetailsHeader.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@rocket.chat/fuselage';
+import { Box, Icon } from '@rocket.chat/fuselage';
 import React, { FC } from 'react';
 
 import AppAvatar from '../../../components/avatar/AppAvatar';
@@ -15,27 +15,61 @@ type AppDetailsPageHeaderProps = {
 const AppDetailsHeader: FC<AppDetailsPageHeaderProps> = ({ app }) => {
 	const t = useTranslation();
 
-	const { iconFileData = '', name, author, version, price, purchaseType, pricingPlans, iconFileContent, installed } = app;
-
+	const {
+		iconFileData = '',
+		name,
+		author,
+		version,
+		price,
+		purchaseType,
+		pricingPlans,
+		iconFileContent,
+		installed,
+		modifiedAt,
+		bundledIn,
+	} = app;
+	console.log(app);
 	return (
 		<Box display='flex' flexDirection='row' mbe='x20' w='full'>
 			<AppAvatar size='x124' mie='x20' iconFileContent={iconFileContent} iconFileData={iconFileData} />
-			<Box display='flex' flexDirection='column' justifyContent='space-between' flexGrow={1}>
-				<Box fontScale='h2'>{name}</Box>
-				<Box display='flex' flexDirection='row' color='hint' alignItems='center'>
-					<Box fontScale='p2m' mie='x4'>
-						{t('By_author', { author: author?.name })}
+			<Box display='flex' flexDirection='column'>
+				<Box display='flex' flexDirection='row' alignItems='center' mbe='x8'>
+					<Box fontScale='h1' mie='x8'>
+						{name}
 					</Box>
-					|<Box mis='x4'>{t('Version_version', { version })}</Box>
+					{Boolean(bundledIn.length) && (
+						<Box
+							display='flex'
+							flexDirection='row'
+							alignItems='center'
+							justifyContent='center'
+							backgroundColor='disabled'
+							pi='x4'
+							height='x20'
+							borderRadius='x2'
+						>
+							<Icon name='bag' size='x20' />
+							<Box fontWeight='700' fontSize='x12' color='info'>
+								{bundledIn[0].bundleName} Bundle
+							</Box>
+						</Box>
+					)}
 				</Box>
-				<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between'>
-					<Box flexGrow={1} display='flex' flexDirection='row' alignItems='center' marginInline='neg-x8'>
-						<AppStatus app={app} marginInline='x8' />
+				<Box mbe='x16'>Unique value proposition for this particular app</Box>
+				<Box display='flex' flexDirection='row' alignItems='center' mbe='x16'>
+					<Box display='flex' flexDirection='row' alignItems='center'>
+						<AppStatus app={app} mie='x8' />
 						{!installed && (
 							<PriceDisplay purchaseType={purchaseType} pricingPlans={pricingPlans} price={price} showType={false} marginInline='x8' />
 						)}
 					</Box>
 					{installed && <AppMenu app={app} />}
+				</Box>
+				<Box display='flex' flexDirection='row' color='hint' alignItems='center'>
+					<Box fontScale='p2m' mie='x16'>
+						{t('By_author', { author: author?.name })}
+					</Box>
+					| <Box mi='x16'>{t('Version_version', { version })}</Box> | <Box mis='x16'>{modifiedAt}</Box>
 				</Box>
 			</Box>
 		</Box>
