43d643fc5bbdcbd534ffa255a68a06e92ef0cf30
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-03-11 12:45:15 -0300

    fix: add JSON Schema to some definitions (work in progress)

--- definition/rest/v1/banners.ts
+++ definition/rest/v1/banners.ts
@@ -1,27 +1,19 @@
-// import Ajv, { JSONSchemaType } from 'ajv';
+import Ajv, { JSONSchemaType } from 'ajv';
 
 import type { BannerPlatform, IBanner } from '../../IBanner';
 
-// const ajv = new Ajv();
+const ajv = new Ajv();
 
 type BannersGetNew = {
 	platform: BannerPlatform;
 	bid: IBanner['_id'];
 };
-/*
+
 const BannersGetNewSchema: JSONSchemaType<BannersGetNew> = {
 	type: 'object',
 	properties: {
 		platform: {
-			type: 'object',
-			properties: {
-				Web: {
-					type: 'string',
-				},
-				Mobile: {
-					type: 'string',
-				},
-			},
+			type: 'string',
 		},
 		bid: {
 			type: 'string',
@@ -32,7 +24,58 @@ const BannersGetNewSchema: JSONSchemaType<BannersGetNew> = {
 };
 
 export const isBannersGetNew = ajv.compile(BannersGetNewSchema);
-*/
+
+type BannersId = {
+	platform: BannerPlatform;
+};
+
+const BannersIdSchema: JSONSchemaType<BannersId> = {
+	type: 'object',
+	properties: {
+		platform: {
+			type: 'string',
+		},
+	},
+	required: ['platform'],
+	additionalProperties: false,
+};
+
+export const isBannersId = ajv.compile(BannersIdSchema);
+
+type Banners = {
+	platform: BannerPlatform;
+};
+
+const BannersSchema: JSONSchemaType<Banners> = {
+	type: 'object',
+	properties: {
+		platform: {
+			type: 'string',
+		},
+	},
+	required: ['platform'],
+	additionalProperties: false,
+};
+
+export const isBanners = ajv.compile(BannersSchema);
+
+type BannersDismiss = {
+	bannerId: string;
+};
+
+const BannersDismissSchema: JSONSchemaType<BannersDismiss> = {
+	type: 'object',
+	properties: {
+		bannerId: {
+			type: 'string',
+		},
+	},
+	required: ['bannerId'],
+	additionalProperties: false,
+};
+
+export const isBannersDismiss = ajv.compile(BannersDismissSchema);
+
 export type BannersEndpoints = {
 	/* @deprecated */
 	'banners.getNew': {
@@ -42,18 +85,18 @@ export type BannersEndpoints = {
 	};
 
 	'banners/:id': {
-		GET: (params: { platform: BannerPlatform }) => {
+		GET: (params: BannersId) => {
 			banners: IBanner[];
 		};
 	};
 
 	'banners': {
-		GET: (params: { platform: BannerPlatform }) => {
+		GET: (params: Banners) => {
 			banners: IBanner[];
 		};
 	};
 
 	'banners.dismiss': {
-		POST: (params: { bannerId: string }) => void;
+		POST: (params: BannersDismiss) => void;
 	};
 };
--- definition/rest/v1/channels.ts
+++ definition/rest/v1/channels.ts
@@ -1,16 +1,91 @@
+import Ajv, { JSONSchemaType } from 'ajv';
+
 import type { IMessage } from '../../IMessage/IMessage';
 import type { IRoom } from '../../IRoom';
 import type { IUser } from '../../IUser';
 
+const ajv = new Ajv();
+
+type ChannelsFiles = {
+	roomId: IRoom['_id'];
+	offset: number;
+	count: number;
+	sort: string;
+	query: string;
+};
+
+const ChannelsFilesSchema: JSONSchemaType<ChannelsFiles> = {
+	type: 'object',
+	properties: {
+		roomId: {
+			type: 'string',
+		},
+		offset: {
+			type: 'number',
+		},
+		count: {
+			type: 'number',
+		},
+		sort: {
+			type: 'string',
+		},
+		query: {
+			type: 'string',
+		},
+	},
+	required: ['roomId', 'offset', 'count', 'sort', 'query'],
+	additionalProperties: false,
+};
+
+export const isChannelsFiles = ajv.compile(ChannelsFilesSchema);
+
+type ChannelsMembers = {
+	roomId: IRoom['_id'];
+	offset?: number;
+	count?: number;
+	filter?: string;
+	status?: string[];
+};
+
+const ChannelsMembersSchema: JSONSchemaType<ChannelsMembers> = {
+	type: 'object',
+	properties: {
+		roomId: {
+			type: 'string',
+		},
+		offset: {
+			type: 'number',
+			nullable: true,
+		},
+		count: {
+			type: 'number',
+			nullable: true,
+		},
+		filter: {
+			type: 'string',
+			nullable: true,
+		},
+		status: {
+			type: 'array',
+			items: { type: 'string' },
+			nullable: true,
+		},
+	},
+	required: ['roomId'],
+	additionalProperties: false,
+};
+
+export const isChannelsMembers = ajv.compile(ChannelsMembersSchema);
+
 export type ChannelsEndpoints = {
 	'channels.files': {
-		GET: (params: { roomId: IRoom['_id']; offset: number; count: number; sort: string; query: string }) => {
+		GET: (params: ChannelsMembers) => {
 			files: IMessage[];
 			total: number;
 		};
 	};
 	'channels.members': {
-		GET: (params: { roomId: IRoom['_id']; offset?: number; count?: number; filter?: string; status?: string[] }) => {
+		GET: (params: ChannelsMembers) => {
 			count: number;
 			offset: number;
 			members: IUser[];
