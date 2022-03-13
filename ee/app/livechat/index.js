f5d32178b2cfb52f273efe10035eae07b70f5935
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-03-13 21:59:35 +0530

    Rename files from JS to TS

--- ee/app/livechat/index.js
+++ ee/app/livechat/index.js
@@ -1,5 +1,5 @@
 import './imports/server/rest/departments.js';
 import './imports/server/rest/inquiries.js';
 import './imports/server/rest/rooms.js';
-import './imports/server/rest/sms.js';
+import './imports/server/rest/sms';
 import './imports/server/rest/upload.js';
