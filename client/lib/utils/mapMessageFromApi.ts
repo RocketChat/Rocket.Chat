5bc1441e8033a9097f3350da0d8c4218f3867856
Kevin Aleman <kevin.aleman@rocket.chat>
2022-03-15 14:40:52 -0600

    Fix type

--- client/lib/utils/mapMessageFromApi.ts
+++ client/lib/utils/mapMessageFromApi.ts
@@ -1,10 +1,11 @@
 import { IMessage } from '../../../definition/IMessage';
 import { Serialized } from '../../../definition/Serialized';
 
-export const mapMessageFromApi = ({ attachments = [], tlm, ts, _updatedAt, ...message }: Serialized<IMessage>): IMessage => ({
+export const mapMessageFromApi = ({ attachments = [], tlm, ts, _updatedAt, editedAt, ...message }: Serialized<IMessage>): IMessage => ({
 	...message,
 	ts: new Date(ts),
 	...(tlm && { tlm: new Date(tlm) }),
+	...(editedAt && { editedAt: new Date(editedAt) }),
 	_updatedAt: new Date(_updatedAt),
 	attachments: attachments.map(({ ts, ...attachment }) => ({
 		...(ts && { ts: new Date(ts) }),
