9745da5e1bae3beff5c173fa1faedccbb0609578
Filipe Marins <filipe.marins@rocket.chat>
2022-03-18 14:21:51 -0300

    refactor: rename plain to plain text

---
+++ client/components/Message/MessageBodyRender/PlainText.tsx
@@ -0,0 +1,21 @@
+import { Plain as ASTPlain } from '@rocket.chat/message-parser';
+import React, { FC, memo } from 'react';
+
+import Highlighter from '../../Highlighter';
+import { useMessageBodyHighlights } from './contexts/MessageBodyContext';
+
+type PlainTextType = {
+	value: ASTPlain['value'];
+};
+
+const PlainText: FC<PlainTextType> = ({ value: text }) => {
+	const highlights = useMessageBodyHighlights();
+
+	if (highlights.length) {
+		return <Highlighter text={text} wordsToHighlight={highlights} />;
+	}
+
+	return <>{text}</>;
+};
+
+export default memo(PlainText);
