622c4ff133426512aa72d6e9c93a613d617a70b7
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2022-03-21 10:22:03 -0300

    Fix room archived/unarchived system messages

--- app/lib/server/functions/unarchiveRoom.ts
+++ app/lib/server/functions/unarchiveRoom.ts
@@ -1,6 +1,9 @@
-import { Rooms, Subscriptions } from '../../../models/server';
+import { Meteor } from 'meteor/meteor';
+
+import { Rooms, Messages, Subscriptions } from '../../../models/server';
 
 export const unarchiveRoom = function (rid: string): void {
 	Rooms.unarchiveById(rid);
 	Subscriptions.unarchiveByRoomId(rid);
+	Messages.createRoomUnarchivedByRoomIdAndUser(rid, Meteor.user());
 };
