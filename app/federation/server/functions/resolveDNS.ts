20d2654c01a76ee65a1562cc3ed84037458eedfb
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-22 14:50:55 -0300

    Remove pointless try-catch clause

--- app/federation/server/functions/resolveDNS.ts
+++ app/federation/server/functions/resolveDNS.ts
@@ -3,20 +3,12 @@ import dns from 'dns';
 const { resolveSrv: dnsResolveSRV, resolveTxt: dnsResolveTXT } = dns.promises;
 
 export const resolveSRV = async (url: string): Promise<Omit<dns.SrvRecord, 'name'> & { target: dns.SrvRecord['name'] }> => {
-	try {
-		const [{ name, ...resolved }] = await dnsResolveSRV(url);
-		return { target: name, ...resolved };
-	} catch (error: any) {
-		throw error;
-	}
+	const [{ name, ...resolved }] = await dnsResolveSRV(url);
+	return { target: name, ...resolved };
 };
 
 export const resolveTXT = async (url: string): Promise<string> => {
-	try {
-		const [resolved] = await dnsResolveTXT(url);
+	const [resolved] = await dnsResolveTXT(url);
 
-		return Array.isArray(resolved) ? resolved.join('') : resolved;
-	} catch (error: any) {
-		throw error;
-	}
+	return Array.isArray(resolved) ? resolved.join('') : resolved;
 };
