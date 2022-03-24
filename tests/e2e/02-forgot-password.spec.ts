ce8f99b7d6443d8b410a766d37a762af8d001123
Weslley de Campos <weslley.campos.ext@sodexo.com>
2022-03-24 17:44:08 -0300

    test: ensure all flow of unlloged area is work

---
+++ tests/e2e/02-forgot-password.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+import LoginPage from './utils/pageobjects/login.page';
+
+test.describe('recoverPassword', () => {
+	let loginPage: LoginPage;
+	test.beforeAll(async ({ browser, baseURL }) => {
+		loginPage = new LoginPage(browser, baseURL as string);
+		await loginPage.open();
+	});
+
+	test.beforeEach(async () => {
+		await loginPage.goto('');
+		await loginPage.gotToForgotPassword();
+	});
+
+	test('it should be required', async () => {
+		loginPage.submit();
+		// loginPage.emailField.should('have.class', 'error');
+		await expect(loginPage.emailInvalidText()).toBeVisible();
+	});
+
+	test('it should be invalid for email without domain', async () => {
+		const emailField = loginPage.emailField();
+		await emailField.type('invalidmail');
+		await loginPage.submit();
+		// loginPage.emailField.should('have.class', 'error');
+		await expect(loginPage.emailInvalidText()).toBeVisible();
+	});
+
+	test('it should be invalid for email with invalid domain', async () => {
+		const emailField = loginPage.emailField();
+		await emailField.type('email@mail');
+		await loginPage.submit();
+		await expect(loginPage.emailInvalidText()).toBeVisible();
+	});
+
+	test('expect user type a valçid email', async () => {
+		const emailField = loginPage.emailField();
+		await emailField.type('any_user@gmail.com');
+		await loginPage.submit();
+		const toastMessageSuccess = loginPage.getToastMessageSuccess();
+		await expect(toastMessageSuccess).toBeVisible();
+	});
+});
