a2ea84e32904ec5f4029582cabc3d965d10f335a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-24 01:56:09 -0300

    Rearrange some stories

--- client/components/Page/Page.stories.tsx
+++ client/components/Page/Page.stories.tsx
@@ -1,38 +1,50 @@
-import { Button, ButtonGroup, Tile } from '@rocket.chat/fuselage';
-import { Story } from '@storybook/react';
-import React, { ReactElement } from 'react';
+import { Box, Button, ButtonGroup } from '@rocket.chat/fuselage';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
 
 import Page from '.';
-import { fullHeightDecorator } from '../../../.storybook/decorators';
 
 export default {
-	title: 'components/Page',
+	title: 'Community/Components/Page',
 	component: Page,
-};
+	subcomponents: {
+		'Page.Content': Page.Content,
+		'Page.Header': Page.Header,
+		'Page.ScrollableContent': Page.ScrollableContent,
+		'Page.ScrollableContentWithShadow': Page.ScrollableContentWithShadow,
+	},
+	parameters: {
+		layout: 'fullscreen',
+		controls: { hideNoControlsWarning: true },
+	},
+} as ComponentMeta<typeof Page>;
 
-const DummyContent = (): ReactElement => (
+const DummyContent = ({ rows = 10 }: { rows?: number }) => (
 	<>
-		{Array.from({ length: 60 }, (_, i) => (
-			<Tile key={i} children='Content slice' marginBlock='x16' />
+		{Array.from({ length: rows }, (_, i) => (
+			<Box key={i} children='Content slice' marginBlock={16} />
 		))}
 	</>
 );
 
-export const Basic: Story = () => (
+export const Example: ComponentStory<typeof Page> = () => (
 	<Page>
-		<Page.Header title='Header' />
+		<Page.Header title='A platform that prioritizes collaboration over vendor choices' />
 		<Page.Content>
-			<DummyContent />
+			<Box marginBlock={16}>
+				Say goodbye to inefficient email threads and managing multiple guest accounts. Enable teams to communicate safely with partners,
+				vendors, and suppliers directly from Rocket.Chat regardless of which collaboration platform they use.
+			</Box>
 		</Page.Content>
 	</Page>
 );
 
-export const WithButtonsAtTheHeader: Story = (): ReactElement => (
+export const WithButtonsAtTheHeader: ComponentStory<typeof Page> = () => (
 	<Page>
-		<Page.Header title='Header'>
+		<Page.Header title='Page Title'>
 			<ButtonGroup>
 				<Button primary type='button'>
-					Hooray!
+					Perform action
 				</Button>
 			</ButtonGroup>
 		</Page.Header>
@@ -42,22 +54,20 @@ export const WithButtonsAtTheHeader: Story = (): ReactElement => (
 	</Page>
 );
 
-export const WithScrollableContent = (): ReactElement => (
+export const WithScrollableContent: ComponentStory<typeof Page> = () => (
 	<Page>
-		<Page.Header title='Header' />
+		<Page.Header title='Page Title' />
 		<Page.ScrollableContent>
-			<DummyContent />
+			<DummyContent rows={60} />
 		</Page.ScrollableContent>
 	</Page>
 );
-WithScrollableContent.decorators = [fullHeightDecorator];
 
-export const WithScrollableContentWithShadow = (): ReactElement => (
+export const WithScrollableContentWithShadow: ComponentStory<typeof Page> = () => (
 	<Page>
-		<Page.Header title='Header' />
+		<Page.Header title='Page Title' />
 		<Page.ScrollableContentWithShadow>
-			<DummyContent />
+			<DummyContent rows={60} />
 		</Page.ScrollableContentWithShadow>
 	</Page>
 );
-WithScrollableContentWithShadow.decorators = [fullHeightDecorator];
