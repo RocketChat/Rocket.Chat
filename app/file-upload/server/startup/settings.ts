e4801ba405d930ee36df58dd43513285cf31e326
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2022-03-24 16:07:38 -0300

    [FIX] LDAP avatars being rotated according to metadata even if the setting to rotate uploads is off (#24320)
    Co-authored-by: Pierre Lehnen <pierre.lehnen@rocket.chat>

--- app/file-upload/server/startup/settings.ts
+++ app/file-upload/server/startup/settings.ts
@@ -32,6 +32,7 @@ settingsRegistry.addGroup('FileUpload', function () {
 
 	this.add('FileUpload_RotateImages', true, {
 		type: 'boolean',
+		public: true,
 	});
 
 	this.add('FileUpload_Enable_json_web_token_for_files', true, {
