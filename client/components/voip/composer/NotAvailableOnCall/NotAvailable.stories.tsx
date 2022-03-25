f0cda61edf0c6d9341c87096ebaa1e014eb8747a
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-03-25 15:45:59 -0300

    fix review

--- client/components/voip/composer/NotAvailableOnCall/NotAvailable.stories.tsx
+++ client/components/voip/composer/NotAvailableOnCall/NotAvailable.stories.tsx
@@ -1,16 +1,11 @@
 import { Story } from '@storybook/react';
 import React from 'react';
 
-import NotAvailableContent from './NotAvailableContent';
-import NotAvailableContentWrapper from './NotAvailableContentWrapper';
+import NotAvailableOnCall from './NotAvailableOnCall';
 
-export const Default: Story = () => (
-	<NotAvailableContentWrapper>
-		<NotAvailableContent text='Messages are not available on phone calls' icon='message' />
-	</NotAvailableContentWrapper>
-);
+export const Default: Story = () => <NotAvailableOnCall />;
 
 export default {
 	title: 'components/Composer/NotAvailableOnCall',
-	component: Default,
+	component: NotAvailableOnCall,
 };
