677c310986961fe7adb934a32145f9720bc39e98
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-25 16:08:28 -0300

    Squashed commit of the following:
    commit f0cda61edf0c6d9341c87096ebaa1e014eb8747a
    Author: Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
    Date:   Fri Mar 25 15:45:59 2022 -0300
    
        fix review
    
    commit 4113ffb9336d661bd31711155738ca6f1bc4c3e1
    Author: Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
    Date:   Thu Mar 24 17:34:48 2022 -0300
    
        fix
    
    commit 82e14b6c187b78b2ca73e6fbcbeb524443b6a7ad
    Author: Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
    Date:   Thu Mar 24 17:18:57 2022 -0300
    
        fix

---
+++ client/components/voip/composer/NotAvailableOnCall/NotAvailable.stories.tsx
@@ -0,0 +1,11 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import NotAvailableOnCall from './NotAvailableOnCall';
+
+export const Default: Story = () => <NotAvailableOnCall />;
+
+export default {
+	title: 'components/Composer/NotAvailableOnCall',
+	component: NotAvailableOnCall,
+};
