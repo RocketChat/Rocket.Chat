2453e1630ed639860383119475063c5f77c1c922
juliajforesti <juliajforesti@gmail.com>
2022-04-01 12:15:11 -0300

    Update fuselage to next version

--- client/views/room/MessageList/components/MessageSystem.tsx
+++ client/views/room/MessageList/components/MessageSystem.tsx
@@ -43,7 +43,6 @@ export const MessageSystem: FC<{ message: IMessage }> = ({ message }) => {
 							}}
 						/>
 					)}
-					{/* @ts-ignore title should be on fuselage component */}
 					<MessageSystemTimestamp title={formatters.dateAndTime(message.ts)}>{formatters.time(message.ts)}</MessageSystemTimestamp>
 				</MessageSystemBlock>
 				{message.attachments && (
