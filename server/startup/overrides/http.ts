797b43f1d7961181b7e311363b2c9b59d272acca
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-04-01 06:20:49 -0300

    log

--- server/startup/overrides/http.ts
+++ server/startup/overrides/http.ts
@@ -2,7 +2,6 @@ import { HTTP } from 'meteor/http';
 
 import { httpCall } from '../../lib/http/call';
 
-console.log('replace it');
 HTTP.call = function _call(...args: Parameters<typeof HTTP.call>): ReturnType<typeof HTTP.call> {
 	return httpCall.call(HTTP, ...args);
 };
