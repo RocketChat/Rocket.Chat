fb207535bd361dc226f4b6580221c46dfb172cbf
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-04-04 12:30:54 -0300

    Update Skeleton.tsx

--- client/components/Omnichannel/Skeleton.tsx
+++ client/components/Omnichannel/Skeleton.tsx
@@ -1,8 +1,8 @@
 import { Box, Skeleton } from '@rocket.chat/fuselage';
 import React, { FC } from 'react';
 
-export const FormSkeleton: FC = () => (
-	<Box w='full' pb='x24'>
+export const FormSkeleton: FC = (props) => (
+	<Box w='full' pb='x24' {...props}>
 		<Skeleton mbe='x8' />
 		<Skeleton mbe='x4' />
 	</Box>
