83e94f78e34f5663f8dc375f6df13b59f2468d2d
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-04-04 17:48:20 -0300

    Use `window`

--- definition/externals/meteor/url.d.ts
+++ definition/externals/meteor/url.d.ts
@@ -1,6 +1,6 @@
 declare module 'meteor/url' {
-	export const URL: typeof globalThis.URL & {
+	export const URL: typeof window.URL & {
 		_constructUrl(url: string, query?: string, paramsForUrl?: Record<string, string>): string;
 	};
-	export const URLSearchParams: typeof globalThis.URLSearchParams;
+	export const URLSearchParams: typeof window.URLSearchParams;
 }
