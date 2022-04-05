2f56fec15c916b857df95fc41f1a938fa22aa62f
Weslley de Campos <weslleydrum@hotmail.com>
2022-04-05 15:45:48 -0300

    adjust file in githubaction

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,10 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	outputDir: 'tests/e2e/test-failures',
-	reporter: [['list']],
+	reporter: [['github']],
 	workers: 2,
 	use: {
-		baseURL: process.env.BASE_URL || 'http://localhost:3000',
+		baseURL: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
 		headless: true,
 		viewport: { width: 1368, height: 768 },
 		ignoreHTTPSErrors: false,
