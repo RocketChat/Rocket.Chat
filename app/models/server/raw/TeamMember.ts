196655a0ffcce6d6d7b9e8a4f4ad2defe45b5603
Diego Sampaio <chinello@gmail.com>
2022-04-07 15:52:27 -0300

    Convert other indexes to same pattern

--- app/models/server/raw/TeamMember.ts
+++ app/models/server/raw/TeamMember.ts
@@ -1,5 +1,4 @@
 import {
-	Collection,
 	WithoutProjection,
 	FindOneOptions,
 	Cursor,
@@ -15,13 +14,16 @@ import type { IUser, IRole } from '../../../../definition/IUser';
 
 type T = ITeamMember;
 export class TeamMemberRaw extends BaseRaw<T> {
-	constructor(public readonly col: Collection<T>, trash?: Collection<T>) {
-		super(col, trash);
-
-		this.col.createIndexes([{ key: { teamId: 1 } }]);
-
-		// teamId => userId should be unique
-		this.col.createIndex({ teamId: 1, userId: 1 }, { unique: true });
+	protected modelIndexes() {
+		return [
+			{
+				key: { teamId: 1 },
+			},
+			{
+				key: { teamId: 1, userId: 1 },
+				unique: true,
+			},
+		];
 	}
 
 	findByUserId(userId: string): Cursor<ITeamMember>;
