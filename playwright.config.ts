2eec90dbdc2518f4c0046c86d1c93331931ad6d5
Weslley de Campos <weslleydrum@hotmail.com>
2022-04-07 15:13:53 -0300

    adjust reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	outputDir: 'tests/e2e/test-failures',
-	reporter: [['github']],
+	reporter: [['list']],
 	workers: 1,
 	use: {
 		baseURL: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
-		headless: true,
+		headless: false,
 		viewport: { width: 1368, height: 768 },
-		ignoreHTTPSErrors: false,
+		ignoreHTTPSErrors: true,
 		video: 'retain-on-failure',
 		screenshot: 'only-on-failure',
 		trace: 'retain-on-failure',
