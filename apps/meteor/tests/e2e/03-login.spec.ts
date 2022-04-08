8eafca9da355056f1a82a61f09371b40979b9c72
Weslley de Campos <weslleydrum@hotmail.com>
2022-04-08 12:35:22 -0300

    updating with monorepo

---
+++ apps/meteor/tests/e2e/03-login.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+import { validUser } from './utils/mocks/userAndPasswordMock';
+import LoginPage from './utils/pageobjects/LoginPage';
+import { LOCALHOST } from './utils/mocks/urlMock';
+import { HOME_SELECTOR } from './utils/mocks/waitSelectorsMock';
+
+test.describe('[Login]', () => {
+	let loginPage: LoginPage;
+
+	test.beforeEach(async ({ page, baseURL }) => {
+		const baseUrl = baseURL || LOCALHOST;
+		loginPage = new LoginPage(page);
+		await loginPage.goto(baseUrl);
+	});
+
+	test('expect user write a password incorrectly', async () => {
+		const invalidUserPassword = {
+			email: validUser.email,
+			password: 'any_password1',
+		};
+		await loginPage.login(invalidUserPassword);
+		await expect(loginPage.getToastError()).toBeVisible();
+	});
+
+	test('expect user make login', async () => {
+		await loginPage.login(validUser);
+		await loginPage.waitForSelector(HOME_SELECTOR);
+	});
+});
---
+++ apps/meteor/tests/e2e/05-channel-creation.spec.ts
@@ -0,0 +1,42 @@
+import { test } from '@playwright/test';
+import { v4 } from 'uuid';
+
+import ChannelCreation from './utils/pageobjects/ChannelCreation';
+import LoginPage from './utils/pageobjects/LoginPage';
+import { validUser, ROCKET_CAT } from './utils/mocks/userAndPasswordMock';
+import { LOCALHOST } from './utils/mocks/urlMock';
+
+test.describe('[Channel]', async () => {
+	let channelCreation: ChannelCreation;
+	let loginPage: LoginPage;
+
+	const HELLO = 'Hello';
+
+	test.beforeEach(async ({ page, baseURL }) => {
+		const baseUrl = baseURL || LOCALHOST;
+		loginPage = new LoginPage(page);
+		await loginPage.goto(baseUrl);
+		await loginPage.login(validUser);
+
+		channelCreation = new ChannelCreation(page);
+	});
+
+	test.describe('[Public and private channel creation]', () => {
+		let channelName: string;
+		test.beforeEach(async () => {
+			channelName = v4();
+		});
+
+		test('expect create privateChannel channel', async () => {
+			await channelCreation.createChannel(channelName, true);
+		});
+
+		test('expect create public channel', async () => {
+			await channelCreation.createChannel(channelName, false);
+		});
+	});
+
+	test('expect send to channel created', async () => {
+		await channelCreation.sendMessage(ROCKET_CAT, HELLO);
+	});
+});
