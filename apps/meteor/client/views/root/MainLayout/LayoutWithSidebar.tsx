32db2ad3eaced8da90f8c3f6c3d93ec8f1bd2ea4
keddelyronjoz@gmail.com <keddelyronjoz@gmail.com>
2022-04-14 11:03:42 +0300

    feat: Added dynamic data for blog posts

--- apps/meteor/client/views/root/MainLayout/LayoutWithSidebar.tsx
+++ apps/meteor/client/views/root/MainLayout/LayoutWithSidebar.tsx
@@ -18,6 +18,49 @@ const LayoutWithSidebar = ({ children }: { children: ReactNode }): ReactElement 
 	);
 	const readReceiptsEnabled = useSetting('Message_Read_Receipt_Store_Users');
 
+	const blogData = [
+		{
+			title: 'Companies use NFTs to boost image in metaverse',
+			body: 'Companies use NFTs to boost image in metaverse Indian companies...'
+		},
+		{
+			title: 'NFT platform bitsCrunch partners with MasterCard',
+			body: 'Mastercard is entering the crypto space to facilitate...'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+		{
+			title: 'Elon Musk takes a dig at Twitter, Web3 and NFTs, on Twitter',
+			body: 'The worlds richest person in the past months has said he is a free speech absolutist...'
+		},
+		{
+			title: 'Salman Khan-backed BollyCoin announces launch of Chulbul Pandey NFT collection',
+			body: 'The NFT marketplace, which kick-started in October 2021, is aiming to get Bollywood into the Metaverse....'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+		{
+			title: 'NFT marketplace Hot Drops raises $2.4 million in seed funding',
+			body: 'Hot Drops seeks to solve issues that have long plagued the entertainment industries...'
+		},
+	]
+
 	return (
 		<div id='rocket-chat' className={[embeddedLayout ? 'embedded-view' : undefined, 'menu-nav'].filter(Boolean).join(' ')}>
 			{!removeSidenav ? <BlazeTemplate template='sideNav' /> : null}
@@ -27,15 +70,11 @@ const LayoutWithSidebar = ({ children }: { children: ReactNode }): ReactElement 
 					.join(' ')}
 			>
 				<Flex.Container wrap='wrap' direction='row' justifyContent='start'>
-					<Flex.Item>
-						<BlogView title={'Companies use NFTs to boost image in metaverse'} body={'Companies use NFTs to boost image in metaverse Indian companies...'} />
-					</Flex.Item>
-					<Flex.Item>
-						<BlogView title={'Companies use NFTs to boost image in metaverse'} body={'Companies use NFTs to boost image in metaverse Indian companies...'} />
-					</Flex.Item>
-					<Flex.Item>
-						<BlogView title={'Companies use NFTs to boost image in metaverse'} body={'Companies use NFTs to boost image in metaverse Indian companies...'} />
-					</Flex.Item>
+					{blogData.map((blog, index) => (
+						<Flex.Item key={index}>
+							<BlogView title={blog.title} body={blog.body} />
+						</Flex.Item>
+					))}
 				</Flex.Container>
 			</div>
 		</div>
