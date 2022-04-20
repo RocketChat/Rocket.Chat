06669cb425695791bbcdc7a63058308414e01762
Ramon Souza <ramon.souza@objective.com.br>
2022-04-20 17:45:24 -0300

    test: expect enable message deleting

--- apps/meteor/tests/e2e/12-settings.spec.ts
+++ apps/meteor/tests/e2e/12-settings.spec.ts
@@ -57,5 +57,16 @@ test.describe('[API Settings Change]', async () => {
 			expect(response.status()).toBe(200);
 			expect(data).toHaveProperty('success', true);
 		});
+
+		test('(API) expect enable message deleting', async ({ request }) => {
+			const response = await request.post(`${BASE_API_URL}/settings/Message_AllowDeleting`, {
+				headers: headersSession,
+				data: { value: true },
+			});
+			const data = await response.json();
+
+			expect(response.status()).toBe(200);
+			expect(data).toHaveProperty('success', true);
+		});
 	});
 });
