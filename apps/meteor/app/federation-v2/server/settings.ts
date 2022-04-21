bad68f15f3dcd81a0635bb7649f87eb8e15ad1e8
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2022-04-21 19:23:01 -0300

    Remove unneeded Meteor.startup wrappers for settings registration

--- apps/meteor/app/federation-v2/server/settings.ts
+++ apps/meteor/app/federation-v2/server/settings.ts
@@ -1,5 +1,4 @@
 import yaml from 'js-yaml';
-import { Meteor } from 'meteor/meteor';
 import { SHA256 } from 'meteor/sha';
 
 import { settings, settingsRegistry } from '../../settings/server';
@@ -7,72 +6,70 @@ import { Settings } from '../../models/server/raw';
 import { setupLogger } from './logger';
 import { getConfig, config } from './config';
 
-Meteor.startup(async function () {
-	const uniqueId = await settings.get('uniqueID');
-	const hsToken = SHA256(`hs_${uniqueId}`);
-	const asToken = SHA256(`as_${uniqueId}`);
+settingsRegistry.addGroup('Federation', function () {
+	this.section('Matrix Bridge', async function () {
+		this.add('Federation_Matrix_enabled', false, {
+			readonly: false,
+			type: 'boolean',
+			i18nLabel: 'Federation_Matrix_enabled',
+			i18nDescription: 'Federation_Matrix_enabled_desc',
+			alert: 'Federation_Matrix_Enabled_Alert',
+		});
 
-	settingsRegistry.addGroup('Federation', function () {
-		this.section('Matrix Bridge', function () {
-			this.add('Federation_Matrix_enabled', false, {
-				readonly: false,
-				type: 'boolean',
-				i18nLabel: 'Federation_Matrix_enabled',
-				i18nDescription: 'Federation_Matrix_enabled_desc',
-				alert: 'Federation_Matrix_Enabled_Alert',
-			});
+		const uniqueId = await settings.get('uniqueID');
+		const hsToken = SHA256(`hs_${uniqueId}`);
+		const asToken = SHA256(`as_${uniqueId}`);
 
-			this.add('Federation_Matrix_id', `rocketchat_${uniqueId}`, {
-				readonly: true,
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_id',
-				i18nDescription: 'Federation_Matrix_id_desc',
-			});
+		this.add('Federation_Matrix_id', `rocketchat_${uniqueId}`, {
+			readonly: true,
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_id',
+			i18nDescription: 'Federation_Matrix_id_desc',
+		});
 
-			this.add('Federation_Matrix_hs_token', hsToken, {
-				readonly: true,
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_hs_token',
-				i18nDescription: 'Federation_Matrix_hs_token_desc',
-			});
+		this.add('Federation_Matrix_hs_token', hsToken, {
+			readonly: true,
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_hs_token',
+			i18nDescription: 'Federation_Matrix_hs_token_desc',
+		});
 
-			this.add('Federation_Matrix_as_token', asToken, {
-				readonly: true,
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_as_token',
-				i18nDescription: 'Federation_Matrix_as_token_desc',
-			});
+		this.add('Federation_Matrix_as_token', asToken, {
+			readonly: true,
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_as_token',
+			i18nDescription: 'Federation_Matrix_as_token_desc',
+		});
 
-			this.add('Federation_Matrix_homeserver_url', 'http://localhost:8008', {
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_homeserver_url',
-				i18nDescription: 'Federation_Matrix_homeserver_url_desc',
-			});
+		this.add('Federation_Matrix_homeserver_url', 'http://localhost:8008', {
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_homeserver_url',
+			i18nDescription: 'Federation_Matrix_homeserver_url_desc',
+		});
 
-			this.add('Federation_Matrix_homeserver_domain', 'local.rocket.chat', {
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_homeserver_domain',
-				i18nDescription: 'Federation_Matrix_homeserver_domain_desc',
-			});
+		this.add('Federation_Matrix_homeserver_domain', 'local.rocket.chat', {
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_homeserver_domain',
+			i18nDescription: 'Federation_Matrix_homeserver_domain_desc',
+		});
 
-			this.add('Federation_Matrix_bridge_url', 'http://host.docker.internal:3300', {
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_bridge_url',
-				i18nDescription: 'Federation_Matrix_bridge_url_desc',
-			});
+		this.add('Federation_Matrix_bridge_url', 'http://host.docker.internal:3300', {
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_bridge_url',
+			i18nDescription: 'Federation_Matrix_bridge_url_desc',
+		});
 
-			this.add('Federation_Matrix_bridge_localpart', 'rocket.cat', {
-				type: 'string',
-				i18nLabel: 'Federation_Matrix_bridge_localpart',
-				i18nDescription: 'Federation_Matrix_bridge_localpart_desc',
-			});
+		this.add('Federation_Matrix_bridge_localpart', 'rocket.cat', {
+			type: 'string',
+			i18nLabel: 'Federation_Matrix_bridge_localpart',
+			i18nDescription: 'Federation_Matrix_bridge_localpart_desc',
+		});
 
-			this.add('Federation_Matrix_registration_file', '', {
-				readonly: true,
-				type: 'code',
-				i18nLabel: 'Federation_Matrix_registration_file',
-				i18nDescription: 'Federation_Matrix_registration_file_desc',
-			});
+		this.add('Federation_Matrix_registration_file', '', {
+			readonly: true,
+			type: 'code',
+			i18nLabel: 'Federation_Matrix_registration_file',
+			i18nDescription: 'Federation_Matrix_registration_file_desc',
 		});
 	});
 });
--- apps/meteor/app/federation/server/startup/settings.ts
+++ apps/meteor/app/federation/server/startup/settings.ts
@@ -1,5 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
 import { settingsRegistry, settings } from '../../../settings/server';
 import { updateStatus, updateEnabled, isRegisteringOrEnabled } from '../functions/helpers';
 import { getFederationDomain } from '../lib/getFederationDomain';
@@ -10,62 +8,60 @@ import { setupLogger } from '../lib/logger';
 import { FederationKeys } from '../../../models/server/raw';
 import { STATUS_ENABLED, STATUS_REGISTERING, STATUS_ERROR_REGISTERING, STATUS_DISABLED } from '../constants';
 
-Meteor.startup(async function () {
-	const federationPublicKey = await FederationKeys.getPublicKeyString();
+settingsRegistry.addGroup('Federation', function () {
+	this.section('Rocket.Chat Federation', async function () {
+		this.add('FEDERATION_Enabled', false, {
+			type: 'boolean',
+			i18nLabel: 'Enabled',
+			i18nDescription: 'FEDERATION_Enabled',
+			alert: 'FEDERATION_Enabled_Alert',
+			public: true,
+		});
 
-	settingsRegistry.addGroup('Federation', function () {
-		this.section('Rocket.Chat Federation', function () {
-			this.add('FEDERATION_Enabled', false, {
-				type: 'boolean',
-				i18nLabel: 'Enabled',
-				i18nDescription: 'FEDERATION_Enabled',
-				alert: 'FEDERATION_Enabled_Alert',
-				public: true,
-			});
+		this.add('FEDERATION_Status', 'Disabled', {
+			readonly: true,
+			type: 'string',
+			i18nLabel: 'FEDERATION_Status',
+		});
 
-			this.add('FEDERATION_Status', 'Disabled', {
-				readonly: true,
-				type: 'string',
-				i18nLabel: 'FEDERATION_Status',
-			});
+		this.add('FEDERATION_Domain', '', {
+			type: 'string',
+			i18nLabel: 'FEDERATION_Domain',
+			i18nDescription: 'FEDERATION_Domain_Description',
+			alert: 'FEDERATION_Domain_Alert',
+			// disableReset: true,
+		});
 
-			this.add('FEDERATION_Domain', '', {
-				type: 'string',
-				i18nLabel: 'FEDERATION_Domain',
-				i18nDescription: 'FEDERATION_Domain_Description',
-				alert: 'FEDERATION_Domain_Alert',
-				// disableReset: true,
-			});
+		const federationPublicKey = await FederationKeys.getPublicKeyString();
 
-			this.add('FEDERATION_Public_Key', federationPublicKey || '', {
-				readonly: true,
-				type: 'string',
-				multiline: true,
-				i18nLabel: 'FEDERATION_Public_Key',
-				i18nDescription: 'FEDERATION_Public_Key_Description',
-			});
+		this.add('FEDERATION_Public_Key', federationPublicKey || '', {
+			readonly: true,
+			type: 'string',
+			multiline: true,
+			i18nLabel: 'FEDERATION_Public_Key',
+			i18nDescription: 'FEDERATION_Public_Key_Description',
+		});
 
-			this.add('FEDERATION_Discovery_Method', 'dns', {
-				type: 'select',
-				values: [
-					{
-						key: 'dns',
-						i18nLabel: 'DNS',
-					},
-					{
-						key: 'hub',
-						i18nLabel: 'Hub',
-					},
-				],
-				i18nLabel: 'FEDERATION_Discovery_Method',
-				i18nDescription: 'FEDERATION_Discovery_Method_Description',
-				public: true,
-			});
+		this.add('FEDERATION_Discovery_Method', 'dns', {
+			type: 'select',
+			values: [
+				{
+					key: 'dns',
+					i18nLabel: 'DNS',
+				},
+				{
+					key: 'hub',
+					i18nLabel: 'Hub',
+				},
+			],
+			i18nLabel: 'FEDERATION_Discovery_Method',
+			i18nDescription: 'FEDERATION_Discovery_Method_Description',
+			public: true,
+		});
 
-			this.add('FEDERATION_Test_Setup', 'FEDERATION_Test_Setup', {
-				type: 'action',
-				actionText: 'FEDERATION_Test_Setup',
-			});
+		this.add('FEDERATION_Test_Setup', 'FEDERATION_Test_Setup', {
+			type: 'action',
+			actionText: 'FEDERATION_Test_Setup',
 		});
 	});
 });
