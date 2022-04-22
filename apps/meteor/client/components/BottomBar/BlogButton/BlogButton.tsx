92659c144cda7518e75306e9a44d0e7739d921be
keddelyronjoz@gmail.com <keddelyronjoz@gmail.com>
2022-04-22 16:51:46 +0300

    fix: Fixed linting errors - part 3

--- apps/meteor/client/components/BottomBar/BlogButton/BlogButton.tsx
+++ apps/meteor/client/components/BottomBar/BlogButton/BlogButton.tsx
@@ -1,7 +1,6 @@
 import { Icon } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-
 import React, { ReactElement } from 'react';
 
 const BlogButton = (): ReactElement => {
