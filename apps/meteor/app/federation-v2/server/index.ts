1fe9dc5d5375157accba46a9731ef93a9b606981
Diego Sampaio <chinello@gmail.com>
2022-04-22 13:48:20 -0300

    Fix federation startup

--- apps/meteor/app/federation-v2/server/index.ts
+++ apps/meteor/app/federation-v2/server/index.ts
@@ -1,20 +1,2 @@
-import { matrixBridge } from './bridge';
-import { bridgeUrlTuple, config } from './config';
-import { bridgeLogger } from './logger';
 import './settings';
-import { isFederationMatrixEnabled } from './tools';
-
-((): void => {
-	if (!isFederationMatrixEnabled()) return;
-
-	bridgeLogger.info(`Running Federation V2:
-	  id: ${config.id}
-	  bridgeUrl: ${config.bridgeUrl}
-	  homeserverURL: ${config.homeserverUrl}
-	  homeserverDomain: ${config.homeserverDomain}
-	`);
-
-	const [, , port] = config.bridgeUrl.split(':') as bridgeUrlTuple;
-
-	matrixBridge.run(port);
-})();
+import './startup';
