1e7856cc2ba897a683b3e16e2a51636fc86c6d97
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-04-25 14:14:52 -0300

    Partial endpoint definition

---
+++ packages/rest-typings/src/v1/integrations.ts
@@ -0,0 +1,44 @@
+import type { IIntegration, IIntegrationHistory } from "@rocket.chat/core-typings";
+
+import type { PaginatedRequest } from "../helpers/PaginatedRequest";
+import type { PaginatedResult } from "../helpers/PaginatedResult";
+
+export type IntegrationsEndpoints = {
+  "integrations.create": {
+    POST: (params: {
+      type: string;
+      name: string;
+      enabled: boolean;
+      username: string;
+      urls?: string[];
+      channel: string;
+      event?: string;
+      triggerWords?: string[];
+      alias?: string;
+      avatar?: string;
+      emoji?: string;
+      token?: string;
+      scriptEnabled: Boolean;
+      script?: string;
+      targetChannel?: string;
+    }) => {
+      integration: IIntegration;
+    };
+  };
+
+  "integrations.history": {
+    GET: (params: PaginatedRequest<{
+      id: string;
+    }>) => PaginatedResult<{
+      history: IIntegrationHistory[];
+    }>;
+  };
+
+  "integrations.list": {
+    GET: (params: PaginatedRequest<{
+
+    }>) => PaginatedResult<{
+      integrations: IIntegration[];
+    }>;
+  };
+};
