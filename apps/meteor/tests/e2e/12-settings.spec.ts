2fadb3a0e2d0c1b92278d3c8400ea3ba05e1a399
Ramon Souza <ramon.souza@objective.com.br>
2022-04-25 10:10:17 -0300

    test: manual approve new users suite and expect enable manual approve new users

--- apps/meteor/tests/e2e/12-settings.spec.ts
+++ apps/meteor/tests/e2e/12-settings.spec.ts
@@ -274,4 +274,17 @@ test.describe('[API Settings Change]', async () => {
 			});
 		})
 	})
+
+	test.describe('Manual new users approve', () => {
+		test('(API) expect enable manually approve new users', async ({ request }) => {
+			const response = await request.post(`${BASE_API_URL}/settings/Accounts_ManuallyApproveNewUsers`, {
+				headers: headersSession,
+				data: { value: true },
+			});
+			const data = await response.json();
+
+			expect(response.status()).toBe(200);
+			expect(data).toHaveProperty('success', true);
+		});
+	})
 });
