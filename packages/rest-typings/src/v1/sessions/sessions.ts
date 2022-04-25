72038fd3980e9889d600c94421a2b1c7fa3122d7
albuquerquefabio <albuquerquefabio@icloud.com>
2022-04-25 18:31:35 -0300

    Chore: type pattern

--- packages/rest-typings/src/v1/sessions/sessions.ts
+++ packages/rest-typings/src/v1/sessions/sessions.ts
@@ -1,11 +1,17 @@
-import type { IMDMSession, IMDMSessionParams } from "./IMDMSession";
+import type { ISession } from "@rocket.chat/core-typings";
+import type { PaginatedRequest } from "../../helpers/PaginatedRequest";
+import type { PaginatedResult } from "../../helpers/PaginatedResult";
 
 export type SessionsEndpoints = {
   "sessions.list": {
-    GET: (params: IMDMSessionParams) => IMDMSession;
+    GET: (
+      params: PaginatedRequest<{ search?: string }>
+    ) => PaginatedResult<{ sessions: ISession[] }>;
   };
 
   "sessions.list.all": {
-    GET: (params: IMDMSessionParams) => IMDMSession;
+    GET: (
+      params: PaginatedRequest<{ search?: string }>
+    ) => PaginatedResult<{ sessions: ISession[] }>;
   };
 };
