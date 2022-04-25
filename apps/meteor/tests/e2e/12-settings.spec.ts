8d5702928bf28ccc45603a649a2c1aa06b6c73d9
Ramon Souza <ramon.souza@objective.com.br>
2022-04-25 09:35:46 -0300

    test: expect enable message pinning

--- apps/meteor/tests/e2e/12-settings.spec.ts
+++ apps/meteor/tests/e2e/12-settings.spec.ts
@@ -164,5 +164,16 @@ test.describe('[API Settings Change]', async () => {
 			expect(response.status()).toBe(200);
 			expect(data).toHaveProperty('success', true);
 		});
+
+		test('(API) expect enable message pinning', async ({ request }) => {
+			const response = await request.post(`${BASE_API_URL}/settings/Message_AllowPinning`, {
+				headers: headersSession,
+				data: { value: true },
+			});
+			const data = await response.json();
+
+			expect(response.status()).toBe(200);
+			expect(data).toHaveProperty('success', true);
+		});
 	})
 });
