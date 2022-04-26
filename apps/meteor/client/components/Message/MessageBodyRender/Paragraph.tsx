c94a38b5239cdd18cb7111151eb37a12796df164
Filipe Marins <filipe.marins@rocket.chat>
2022-04-26 16:47:24 -0300

    fix: render empty line

--- apps/meteor/client/components/Message/MessageBodyRender/Paragraph.tsx
+++ apps/meteor/client/components/Message/MessageBodyRender/Paragraph.tsx
@@ -1,12 +1,21 @@
+import { css } from '@rocket.chat/css-in-js';
+import { Box } from '@rocket.chat/fuselage';
 import { Paragraph as ASTParagraph } from '@rocket.chat/message-parser';
 import React, { FC } from 'react';
 
 import Inline from './Inline';
 
+const paragraphClass = css`
+	&:empty::before {
+		content: '';
+		display: inline-block;
+	}
+`;
+
 const Paragraph: FC<{ value: ASTParagraph['value'] }> = ({ value = [] }) => (
-	<p>
+	<Box is='p' className={paragraphClass}>
 		<Inline value={value} />
-	</p>
+	</Box>
 );
 
 export default Paragraph;
