46350893e4a8c4dc8c001de2c55d7314f11c63b7
albuquerquefabio <albuquerquefabio@icloud.com>
2022-04-29 11:21:48 -0300

    Session logout by admin

--- apps/meteor/app/api/server/v1/sessions.ts
+++ apps/meteor/app/api/server/v1/sessions.ts
@@ -4,6 +4,7 @@ import { check, Match } from 'meteor/check';
 import { hasRole } from '../../../authorization/server';
 import { Users, Sessions } from '../../../models/server/raw/index';
 import { API } from '../api';
+import { hasPermission } from '../../../authorization/server/functions/hasPermission';
 
 API.v1.addRoute(
 	'sessions/list',
@@ -93,6 +94,44 @@ API.v1.addRoute(
 
 				const sessions = await Sessions.getAllSessions(search, { offset, count });
 				return API.v1.success(sessions);
+			} catch (error) {
+				return API.v1.failure(`${error}`);
+			}
+		},
+	},
+);
+
+API.v1.addRoute(
+	'sessions/logout',
+	{ authRequired: true },
+	{
+		async post() {
+			if (!hasPermission(this.userId, 'logout-other-user') && !hasRole(this.userId, 'admin')) {
+				return API.v1.unauthorized();
+			}
+
+			check(this.bodyParams, {
+				sessionId: String,
+			});
+
+			try {
+				const { sessionId } = this.bodyParams;
+				const sessionObj = await Sessions.findOneBySessionId(sessionId);
+
+				if (!sessionObj) {
+					return API.v1.notFound('Session not found');
+				}
+
+				Meteor.users.update(sessionObj.userId, {
+					$pull: {
+						'services.resume.loginTokens': {
+							hashedToken: sessionObj.loginToken,
+						},
+					},
+				});
+
+				await Sessions.updateOne({ sessionId }, { $set: { logoutAt: new Date(), logoutBy: this.userId } });
+				return API.v1.success({ sessionId });
 			} catch (error) {
 				return API.v1.failure(`${error}`);
 			}
