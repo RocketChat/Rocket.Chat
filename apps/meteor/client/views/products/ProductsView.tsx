c029fe2ee23c5656c7707aaa8965d67d91769bb1
keddelyronjoz@gmail.com <keddelyronjoz@gmail.com>
2022-04-30 10:06:23 +0300

    feat: Added a header to every page

--- apps/meteor/client/views/products/ProductsView.tsx
+++ apps/meteor/client/views/products/ProductsView.tsx
@@ -2,10 +2,12 @@ import React, { ReactElement } from 'react';
 
 import BottomBar from '../../components/BottomBar';
 import Page from '../../components/Page';
+import TopBar from '../../topbar/TopBar';
 
 const ProductsView = (): ReactElement => (
 	<Page flexDirection='row'>
 		<Page>
+			<TopBar />
 			<Page.Content>ProductsView</Page.Content>
 			<BottomBar />
 		</Page>
