9c590063ac7acda0ab8c6a06cb246aeb62d0a6cd
Diego Sampaio <chinello@gmail.com>
2022-05-04 14:07:54 -0300

    Chore: Convert getStatistics (#25342)

--- apps/meteor/app/api/server/v1/stats.ts
+++ apps/meteor/app/api/server/v1/stats.ts
@@ -6,15 +6,14 @@ API.v1.addRoute(
 	'statistics',
 	{ authRequired: true },
 	{
-		get() {
-			const { refresh } = this.requestParams();
+		async get() {
+			const { refresh = 'false' } = this.requestParams();
+
 			return API.v1.success(
-				Promise.await(
-					getLastStatistics({
-						userId: this.userId,
-						refresh: refresh && refresh === 'true',
-					}),
-				),
+				await getLastStatistics({
+					userId: this.userId,
+					refresh: refresh === 'true',
+				}),
 			);
 		},
 	},
@@ -24,23 +23,21 @@ API.v1.addRoute(
 	'statistics.list',
 	{ authRequired: true },
 	{
-		get() {
+		async get() {
 			const { offset, count } = this.getPaginationItems();
 			const { sort, fields, query } = this.parseJsonQuery();
 
 			return API.v1.success(
-				Promise.await(
-					getStatistics({
-						userId: this.userId,
-						query,
-						pagination: {
-							offset,
-							count,
-							sort,
-							fields,
-						},
-					}),
-				),
+				await getStatistics({
+					userId: this.userId,
+					query,
+					pagination: {
+						offset,
+						count,
+						sort,
+						fields,
+					},
+				}),
 			);
 		},
 	},
