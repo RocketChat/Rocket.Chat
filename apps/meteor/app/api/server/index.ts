d8f938f8f92eb3744b17b032ceacf31e327da906
Diego Sampaio <chinello@gmail.com>
2022-05-04 14:05:10 -0300

    Chore: Add /v1/video-conference endpoint types (#25278)
    Co-authored-by: Pierre Lehnen <pierre.lehnen@rocket.chat>
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/app/api/server/index.ts
+++ apps/meteor/app/api/server/index.ts
@@ -34,7 +34,7 @@ import './v1/settings';
 import './v1/stats';
 import './v1/subscriptions';
 import './v1/users';
-import './v1/video-conference';
+import './v1/videoConference';
 import './v1/autotranslate';
 import './v1/webdav';
 import './v1/oauthapps';
--- packages/rest-typings/src/index.ts
+++ packages/rest-typings/src/index.ts
@@ -28,6 +28,7 @@ import type { SettingsEndpoints } from './v1/settings';
 import type { StatisticsEndpoints } from './v1/statistics';
 import type { TeamsEndpoints } from './v1/teams';
 import type { UsersEndpoints } from './v1/users';
+import type { VideoConferenceEndpoints } from './v1/videoConference';
 import type { VoipEndpoints } from './v1/voip';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/interface-name-prefix
@@ -56,6 +57,7 @@ export interface Endpoints
 		PermissionsEndpoints,
 		InstancesEndpoints,
 		VoipEndpoints,
+		VideoConferenceEndpoints,
 		InvitesEndpoints,
 		E2eEndpoints,
 		CustomSoundEndpoint {}
