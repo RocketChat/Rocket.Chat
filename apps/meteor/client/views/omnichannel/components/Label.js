c1ccd0ef95f3afdabfb0fffa83a3b2367dfaf819
Jean Brito <jeanfbrito@gmail.com>
2022-05-05 17:47:27 -0300

    Chore: Convert AdminSideBar to ts (#25372)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/client/views/omnichannel/components/Label.js
+++
@@ -1,6 +0,0 @@
-import { Box } from '@rocket.chat/fuselage';
-import React from 'react';
-
-const Label = (props) => <Box mbe='x8' fontScale='p2m' color='default' {...props} />;
-
-export default Label;
