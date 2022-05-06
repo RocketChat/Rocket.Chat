6ab3a2136741cd3cd52134af3d6b76b72e2ce1b2
Jean Brito <jeanfbrito@gmail.com>
2022-05-06 17:37:06 -0300

    Chore: Convert componentes/sidebar to TS

---
+++ apps/meteor/client/components/Sidebar/ItemsAssembler.tsx
@@ -0,0 +1,29 @@
+import React, { memo, FC } from 'react';
+
+import { useTranslation } from '../../contexts/TranslationContext';
+import SidebarNavigationItem from './SidebarNavigationItem';
+
+type ItemsAssemblerProps = {
+	items: [];
+	currentPath: string;
+};
+
+const ItemsAssembler: FC<ItemsAssemblerProps> = ({ items, currentPath }) => {
+	const t = useTranslation();
+	return <>{
+		items.map(({ href, pathSection, i18nLabel, name, icon, permissionGranted, pathGroup, tag }) => (
+			<SidebarNavigationItem
+				permissionGranted={permissionGranted}
+				pathGroup={pathGroup}
+				pathSection={href || pathSection}
+				icon={icon}
+				label={t(i18nLabel || name)}
+				key={i18nLabel || name}
+				currentPath={currentPath}
+				tag={t(tag)}
+			/>
+		))};
+		</>;
+};
+
+export default memo(ItemsAssembler);
