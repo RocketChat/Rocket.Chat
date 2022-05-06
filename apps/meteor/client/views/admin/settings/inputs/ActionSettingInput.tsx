14d168793c335e1b4a722367fe3d534beb0d9586
juliajforesti <juliajforesti@gmail.com>
2022-05-06 10:28:23 -0300

    Convert ActionSettingInput to tsx

---
+++ apps/meteor/client/views/admin/settings/inputs/ActionSettingInput.tsx
@@ -0,0 +1,45 @@
+import { Button, Field } from '@rocket.chat/fuselage';
+import React, { ReactElement } from 'react';
+
+import type keys from '../../../../../packages/rocketchat-i18n/i18n/en.i18n.json';
+import { useMethod } from '../../../../contexts/ServerContext';
+import type { ServerMethods } from '../../../../contexts/ServerContext';
+import { useToastMessageDispatch } from '../../../../contexts/ToastMessagesContext';
+import { useTranslation } from '../../../../contexts/TranslationContext';
+
+type ActionSettingInputProps = {
+	_id: string;
+	actionText: keyof typeof keys;
+	value: keyof ServerMethods;
+	disabled: boolean;
+	sectionChanged: boolean;
+};
+function ActionSettingInput({ _id, actionText, value, disabled, sectionChanged }: ActionSettingInputProps): ReactElement {
+	const t = useTranslation();
+
+	const dispatchToastMessage = useToastMessageDispatch();
+	const actionMethod = useMethod(value);
+
+	const handleClick = async (): Promise<void> => {
+		try {
+			const data: { message: keyof typeof keys; params: string[] } = await actionMethod();
+
+			dispatchToastMessage({ type: 'success', message: t(data.message, ...data.params) });
+		} catch (error) {
+			dispatchToastMessage({ type: 'error', message: String(error) });
+		}
+	};
+
+	return (
+		<>
+			<Field.Row>
+				<Button data-qa-setting-id={_id} disabled={disabled || sectionChanged} primary onClick={handleClick}>
+					{t(actionText)}
+				</Button>
+			</Field.Row>
+			{sectionChanged && <Field.Hint>{t('Save_to_enable_this_action')}</Field.Hint>}
+		</>
+	);
+}
+
+export default ActionSettingInput;
