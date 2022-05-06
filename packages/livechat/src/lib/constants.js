3e13a13815a3e5a6199db13ac99af4389a97e7ee
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-05-06 10:27:46 -0300

    Chore: Add Livechat repo into Monorepo packages (#25312)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>
    Co-authored-by: Martin <martin.schoeler@rocket.chat>

---
+++ packages/livechat/src/lib/constants.js
@@ -0,0 +1,9 @@
+export default {
+	unreadMessagesAlertId: 'UNREAD_MESSAGES',
+	connectingAgentAlertId: 'CONNECTING_AGENT',
+	livechatConnectedAlertId: 'LIVECHAT_CONNECTED',
+	livechatDisconnectedAlertId: 'LIVECHAT_DISCONNECTED',
+	livechatQueueMessageId: 'LIVECHAT_QUEUE_MESSAGE',
+	webRTCCallStartedMessageType: 'livechat_webrtc_video_call',
+	jitsiCallStartedMessageType: 'jitsi_call_started',
+};
