4c05254cc365563b72ceffb0108c7e26323fb4dc
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-05-06 10:27:46 -0300

    Chore: Add Livechat repo into Monorepo packages (#25312)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>
    Co-authored-by: Martin <martin.schoeler@rocket.chat>

---
+++ packages/livechat/src/components/Button/stories.js
@@ -0,0 +1,212 @@
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+
+import { Button } from '.';
+import { avatarResolver, centered } from '../../helpers.stories';
+import ChatIcon from '../../icons/chat.svg';
+
+
+const defaultSrc = avatarResolver('guilherme.gazzo');
+
+const defaultText = 'Powered by Rocket.Chat';
+const defaultBadge = 'badged';
+
+storiesOf('Components/Button', module)
+	.addDecorator(centered)
+	.addDecorator(withKnobs)
+	.add('normal', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('disabled', () => (
+		<Button
+			disabled={boolean('disabled', true)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('outline', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', true)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('nude', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', true)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('danger', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', true)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('secondary', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', true)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('stack', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', true)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('small', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', true)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('loading', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', true)}
+			badge={text('badge', '')}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('with badge', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', defaultBadge)}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('with icon', () => (
+		<Button
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			nude={boolean('nude', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', '')}
+			icon={<ChatIcon />}
+			onClick={action('clicked')}
+		>
+			{text('text', defaultText)}
+		</Button>
+	))
+	.add('transparent with background image', () => (
+		<Button
+			img={defaultSrc}
+			disabled={boolean('disabled', false)}
+			outline={boolean('outline', false)}
+			danger={boolean('danger', false)}
+			secondary={boolean('secondary', false)}
+			stack={boolean('stack', false)}
+			small={boolean('small', false)}
+			loading={boolean('loading', false)}
+			badge={text('badge', 1)}
+			icon={<ChatIcon />}
+			onClick={action('clicked')}
+			transparent={boolean('transparent', true)}
+		>
+			{text('text', defaultText)}
+		</Button>
+	));
---
+++ packages/livechat/src/components/Tooltip/index.js
@@ -0,0 +1,123 @@
+import { cloneElement, Component, createContext, toChildArray } from 'preact';
+
+import { createClassName } from '../helpers';
+import styles from './styles.scss';
+
+
+const getPositioningStyle = (placement, { left, top, right, bottom }) => {
+	switch (placement) {
+		case 'left':
+			return {
+				left: `${ left }px`,
+				top: `${ (top + bottom) / 2 }px`,
+			};
+
+		case 'top':
+		case 'top-left':
+		case 'top-right':
+			return {
+				left: `${ (left + right) / 2 }px`,
+				top: `${ top }px`,
+			};
+
+		case 'right':
+			return {
+				left: `${ right }px`,
+				top: `${ (top + bottom) / 2 }px`,
+			};
+
+		case 'bottom':
+		case 'bottom-left':
+		case 'bottom-right':
+		default:
+			return {
+				left: `${ (left + right) / 2 }px`,
+				top: `${ bottom }px`,
+			};
+	}
+};
+
+
+export const Tooltip = ({ children, hidden = false, placement, floating = false, triggerBounds, ...props }) => (
+	<div
+		className={createClassName(styles, 'tooltip', { hidden, placement, floating })}
+		style={floating ? getPositioningStyle(placement, triggerBounds) : {}}
+		{...props}
+	>
+		{children}
+	</div>
+);
+
+
+const TooltipContext = createContext();
+
+
+export class TooltipContainer extends Component {
+	state = {
+		tooltip: null,
+		activeChild: null,
+		event: null,
+		placement: null,
+	}
+
+	showTooltip = (event, { content, placement = 'bottom', childIndex }) => {
+		const triggerBounds = event.target.getBoundingClientRect();
+		this.setState({ tooltip: <Tooltip floating placement={placement} triggerBounds={triggerBounds}>{content}</Tooltip>, activeChild: childIndex, event, placement, content });
+	}
+
+	hideTooltip = () => {
+		this.setState({ tooltip: null });
+	}
+
+	UNSAFE_componentWillReceiveProps(props) {
+		if (this.state.tooltip) {
+			const activeChildren = props?.children?.props?.children[this.state.activeChild];
+			if (activeChildren && activeChildren.props.content !== this.state.content) {
+				this.showTooltip(this.state.event, { content: activeChildren.props.content, placement: this.state.placement, childIndex: this.state.activeChild });
+			}
+		}
+	}
+
+	render({ children }) {
+		return (
+			<TooltipContext.Provider value={{ ...this.state, showTooltip: this.showTooltip, hideTooltip: this.hideTooltip }}>
+				{children}
+				<TooltipContext.Consumer>
+					{({ tooltip }) => tooltip}
+				</TooltipContext.Consumer>
+			</TooltipContext.Provider>
+		);
+	}
+}
+
+
+export const TooltipTrigger = ({ children, content, placement }) => (
+	<TooltipContext.Consumer>
+		{({ showTooltip, hideTooltip }) => toChildArray(children).map((child, index) => cloneElement(child, {
+			onMouseEnter: (event) => showTooltip(event, { content, placement, childIndex: index }),
+			onMouseLeave: (event) => hideTooltip(event),
+			onFocusCapture: (event) => showTooltip(event, { content, placement, childIndex: index }),
+			onBlurCapture: (event) => hideTooltip(event),
+			content,
+		}))}
+	</TooltipContext.Consumer>
+);
+
+
+export const withTooltip = (component) => {
+	const TooltipConnection = ({ tooltip, ...props }) => (
+		<Tooltip.Trigger content={tooltip}>
+			{component(props)}
+		</Tooltip.Trigger>
+	);
+	TooltipConnection.displayName = `withTooltip(${ component.displayName })`;
+
+	return TooltipConnection;
+};
+
+
+Tooltip.Container = TooltipContainer;
+Tooltip.Trigger = TooltipTrigger;
+
+
+export default Tooltip;
