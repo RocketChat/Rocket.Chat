727f652fbb537b76e0273e0a528fd497768991e0
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-05-09 17:25:22 -0300

    [NEW] Video Conference Model

--- packages/core-typings/src/IVideoConference.ts
+++ packages/core-typings/src/IVideoConference.ts
@@ -1,5 +1,7 @@
 import type { IRocketChatRecord } from './IRocketChatRecord';
 import type { IUser } from './IUser';
+import type { IMessage } from './IMessage';
+import type { ValueOf } from './utils';
 
 export type DirectCallInstructions = {
 	type: 'direct';
@@ -13,14 +15,35 @@ export type ConferenceInstructions = {
 };
 
 export type VideoConferenceType = DirectCallInstructions['type'] | ConferenceInstructions['type'];
+export interface IVideoConferenceUser extends Pick<IUser, '_id' | 'username' | 'name'> {
+	ts: Date;
+}
+
+export const VideoConferenceStatus = {
+	CALLING: 0,
+	STARTED: 1,
+	ENDED: 2,
+} as const;
 
 export interface IVideoConference extends IRocketChatRecord {
 	type: VideoConferenceType;
-	users: Pick<IUser, '_id' | 'username' | 'name'>[];
+	users: IVideoConferenceUser[];
 	rid: string;
+	anonymousUsers: number;
+	title: string;
+	messages: {
+		calling?: IMessage['_id'];
+		missed?: IMessage['_id'];
+		started?: IMessage['_id'];
+		ended?: IMessage['_id'];
+	};
+	status: ValueOf<typeof VideoConferenceStatus>;
 
-	_createdBy: Pick<IUser, '_id' | 'username' | 'name'>;
-	_createdAt: Date;
+	createdBy: Pick<IUser, '_id' | 'username' | 'name'>;
+	createdAt: Date;
+
+	endedBy: Pick<IUser, '_id' | 'username' | 'name'>;
+	endedAt: Date;
 }
 
 export type VideoConferenceInstructions = DirectCallInstructions | ConferenceInstructions;
