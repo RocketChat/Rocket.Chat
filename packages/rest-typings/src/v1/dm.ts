37682f134604ef4edf638ff700699119c5b64109
albuquerquefabio <albuquerquefabio@icloud.com>
2022-05-09 12:02:37 -0300

    All rest types for dm endpoint

--- packages/rest-typings/src/v1/dm.ts
+++ packages/rest-typings/src/v1/dm.ts
@@ -1,20 +1,17 @@
-import type { IRoom, IUser } from '@rocket.chat/core-typings';
+import type { ImEndpoints } from './im';
 
 export type DmEndpoints = {
-	'dm.create': {
-		POST: (
-			params: (
-				| {
-						username: Exclude<IUser['username'], undefined>;
-				  }
-				| {
-						usernames: string;
-				  }
-			) & {
-				excludeSelf?: boolean;
-			},
-		) => {
-			room: IRoom & { rid: IRoom['_id'] };
-		};
-	};
+	'dm.create': ImEndpoints['im.create'];
+	'dm.delete': ImEndpoints['im.delete'];
+	'dm.close': ImEndpoints['im.close'];
+	'dm.counters': ImEndpoints['im.counters'];
+	'dm.files': ImEndpoints['im.files'];
+	'dm.history': ImEndpoints['im.history'];
+	'dm.members': ImEndpoints['im.members'];
+	'dm.messages': ImEndpoints['im.messages'];
+	'dm.messages.other': ImEndpoints['im.messages.other'];
+	'dm.list': ImEndpoints['im.list'];
+	'dm.list.everyone': ImEndpoints['im.list.everyone'];
+	'dm.open': ImEndpoints['im.open'];
+	'dm.setTopic': ImEndpoints['im.setTopic'];
 };
