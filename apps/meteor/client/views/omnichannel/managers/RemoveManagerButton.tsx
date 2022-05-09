462832a5a0a047bce9876f1946eb0685a07e95d1
Martin Schoeler <martin.schoeler@rocket.chat>
2022-05-09 09:28:03 -0300

    Chore: Manager Page Rewrite (#25431)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/client/views/omnichannel/managers/RemoveManagerButton.tsx
+++ apps/meteor/client/views/omnichannel/managers/RemoveManagerButton.tsx
@@ -1,6 +1,6 @@
 import { Table, Icon, Button } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import GenericModal from '../../../components/GenericModal';
 import { useSetModal } from '../../../contexts/ModalContext';
@@ -8,7 +8,7 @@ import { useToastMessageDispatch } from '../../../contexts/ToastMessagesContext'
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useEndpointAction } from '../../../hooks/useEndpointAction';
 
-function RemoveManagerButton({ _id, reload }) {
+const RemoveManagerButton = ({ _id, reload }: { _id: string; reload: () => void }): ReactElement => {
 	const t = useTranslation();
 	const deleteAction = useEndpointAction('DELETE', `livechat/users/manager/${_id}`);
 	const setModal = useSetModal();
@@ -16,23 +16,31 @@ function RemoveManagerButton({ _id, reload }) {
 
 	const handleRemoveClick = useMutableCallback(async () => {
 		const result = await deleteAction();
-		if (result.success === true) {
+		if (result?.success === true) {
 			reload();
 		}
 	});
 	const handleDelete = useMutableCallback((e) => {
 		e.stopPropagation();
-		const onDeleteManager = async () => {
+		const onDeleteManager = async (): Promise<void> => {
 			try {
 				await handleRemoveClick();
 				dispatchToastMessage({ type: 'success', message: t('Manager_removed') });
-			} catch (error) {
-				dispatchToastMessage({ type: 'error', message: error });
+			} catch (error: unknown) {
+				(typeof error === 'string' || error instanceof Error) && dispatchToastMessage({ type: 'error', message: error });
 			}
 			setModal();
 		};
 
-		setModal(<GenericModal variant='danger' onConfirm={onDeleteManager} onCancel={() => setModal()} confirmText={t('Delete')} />);
+		setModal(
+			<GenericModal
+				variant='danger'
+				onConfirm={onDeleteManager}
+				onCancel={(): void => setModal()}
+				onClose={(): void => setModal()}
+				confirmText={t('Delete')}
+			/>,
+		);
 	});
 
 	return (
@@ -42,6 +50,6 @@ function RemoveManagerButton({ _id, reload }) {
 			</Button>
 		</Table.Cell>
 	);
-}
+};
 
 export default RemoveManagerButton;
