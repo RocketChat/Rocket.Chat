f6c79406e4eb6febeac63e2434d23c5cc2be237c
Diego Sampaio <chinello@gmail.com>
2022-05-09 18:33:35 -0300

    Fix overwritten settings not getting cached

--- apps/meteor/app/settings/server/SettingsRegistry.ts
+++ apps/meteor/app/settings/server/SettingsRegistry.ts
@@ -163,12 +163,15 @@ export class SettingsRegistry {
 					}),
 				},
 			);
+
+			this.store.set(settingOverwritten);
 			return;
 		}
 
 		if (settingStored && isOverwritten) {
 			if (settingStored.value !== settingOverwritten.value) {
 				this.model.upsert({ _id }, settingProps);
+				this.store.set(settingOverwritten);
 			}
 			return;
 		}
