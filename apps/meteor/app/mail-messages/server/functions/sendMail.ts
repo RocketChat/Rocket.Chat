0e462e99b07823e2766b4b319928b058e515dd86
Kevin Aleman <kaleman960@gmail.com>
2022-05-09 07:40:19 -0600

    Chore: Rewrite mail-messages to ts (#25421)

--- apps/meteor/app/mail-messages/server/functions/sendMail.ts
+++ apps/meteor/app/mail-messages/server/functions/sendMail.ts
@@ -2,20 +2,23 @@ import { Meteor } from 'meteor/meteor';
 import { EJSON } from 'meteor/ejson';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { escapeHTML } from '@rocket.chat/string-helpers';
+import { FilterQuery } from 'mongodb';
+import { IUser } from '@rocket.chat/core-typings';
 
 import { placeholders } from '../../../utils/server';
 import { SystemLogger } from '../../../../server/lib/logger/system';
 import * as Mailer from '../../../mailer';
 
-export const sendMail = function (from, subject, body, dryrun, query) {
+export const sendMail = function (from: string, subject: string, body: string, dryrun: boolean, query: string): void {
 	Mailer.checkAddressFormatAndThrow(from, 'Mailer.sendMail');
+
 	if (body.indexOf('[unsubscribe]') === -1) {
 		throw new Meteor.Error('error-missing-unsubscribe-link', 'You must provide the [unsubscribe] link.', {
 			function: 'Mailer.sendMail',
 		});
 	}
 
-	let userQuery = { 'mailer.unsubscribed': { $exists: 0 } };
+	let userQuery: FilterQuery<any> = { 'mailer.unsubscribed': { $exists: 0 } };
 	if (query) {
 		userQuery = { $and: [userQuery, EJSON.parse(query)] };
 	}
@@ -25,13 +28,14 @@ export const sendMail = function (from, subject, body, dryrun, query) {
 			.find({
 				'emails.address': from,
 			})
-			.forEach((user) => {
-				const email = `${user.name} <${user.emails[0].address}>`;
+			.forEach((u): void => {
+				const user: Partial<IUser> & Pick<IUser, '_id'> = u;
+				const email = `${user.name} <${user.emails?.[0].address}>`;
 				const html = placeholders.replace(body, {
 					unsubscribe: Meteor.absoluteUrl(
 						FlowRouter.path('mailer/unsubscribe/:_id/:createdAt', {
 							_id: user._id,
-							createdAt: user.createdAt.getTime(),
+							createdAt: user.createdAt?.getTime().toString() || '',
 						}),
 					),
 					name: user.name,
@@ -47,18 +51,20 @@ export const sendMail = function (from, subject, body, dryrun, query) {
 				});
 			});
 	}
-	return Meteor.users.find(userQuery).forEach(function (user) {
-		if (user && user.emails && Array.isArray(user.emails) && user.emails.length) {
+
+	return Meteor.users.find(userQuery).forEach(function (u) {
+		const user: Partial<IUser> & Pick<IUser, '_id'> = u;
+		if (user?.emails && Array.isArray(user.emails) && user.emails.length) {
 			const email = `${user.name} <${user.emails[0].address}>`;
 
 			const html = placeholders.replace(body, {
 				unsubscribe: Meteor.absoluteUrl(
 					FlowRouter.path('mailer/unsubscribe/:_id/:createdAt', {
 						_id: user._id,
-						createdAt: user.createdAt.getTime(),
+						createdAt: user.createdAt?.getTime().toString() || '',
 					}),
 				),
-				name: escapeHTML(user.name),
+				name: escapeHTML(user.name || ''),
 				email: escapeHTML(email),
 			});
 			SystemLogger.debug(`Sending email to ${email}`);
