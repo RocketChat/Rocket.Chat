fbb883bd11f8d213eced319c79e2a732558e7651
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-05-09 20:03:24 -0300

    fixes

--- packages/rest-typings/src/v1/videoConference/index.ts
+++ packages/rest-typings/src/v1/videoConference/index.ts
@@ -2,8 +2,6 @@ import type { IRoom, VideoConferenceInstructions } from '@rocket.chat/core-typin
 
 import type { VideoConfStartProps } from './VideoConfStartProps';
 
-export * from './VideoConfStartProps';
-
 export type VideoConferenceEndpoints = {
 	'video-conference/jitsi.update-timeout': {
 		POST: (params: { roomId: IRoom['_id']; joiningNow?: boolean }) => {
