b904a17cc0d73950bcece1825d82a8a49cde14cc
murtaza98 <murtaza.patrawala@rocket.chat>
2022-05-10 12:26:34 +0530

    [FIX] allow only livechat-agents to be contact manager for any omnichannel contact

--- apps/meteor/ee/client/omnichannel/additionalForms/ContactManager.js
+++ apps/meteor/ee/client/omnichannel/additionalForms/ContactManager.js
@@ -7,11 +7,13 @@ import UserAutoComplete from '../../../../client/components/UserAutoComplete';
 export const ContactManager = ({ value, handler }) => {
 	const t = useTranslation();
 
+	const conditions = { roles: 'livechat-agent' };
+
 	return (
 		<Field>
 			<Field.Label>{t('Contact_Manager')}</Field.Label>
 			<Field.Row>
-				<UserAutoComplete value={value} onChange={handler} />
+				<UserAutoComplete conditions={conditions} value={value} onChange={handler} />
 			</Field.Row>
 		</Field>
 	);
