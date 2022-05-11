22efe02105fd5ee471422a224966e18935cb7f78
guijun13 <guilhermejun13.grillo@gmail.com>
2022-05-11 10:52:48 -0300

    fix: regression of settings use for import rules

--- apps/meteor/app/utils/lib/getValidRoomName.js
+++ apps/meteor/app/utils/lib/getValidRoomName.js
@@ -2,13 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import limax from 'limax';
 import { escapeHTML } from '@rocket.chat/string-helpers';
 
-import { Settings, Rooms } from '../../models';
+import { settings } from '../../settings';
+import { Rooms } from '../../models';
 import { validateName } from '../../lib/server/functions/validateName';
 
 export const getValidRoomName = (displayName, rid = '', options = {}) => {
 	let slugifiedName = displayName;
 
-	if (Settings.findOneById('UI_Allow_room_names_with_special_chars').value) {
+	if (settings.get('UI_Allow_room_names_with_special_chars')) {
 		const cleanName = limax(displayName, { maintainCase: true });
 		if (options.allowDuplicates !== true) {
 			const room = Rooms.findOneByDisplayName(displayName);
@@ -35,7 +36,7 @@ export const getValidRoomName = (displayName, rid = '', options = {}) => {
 		nameValidation = new RegExp(options.nameValidationRegex);
 	} else {
 		try {
-			nameValidation = new RegExp(`^${Settings.findOneById('UTF8_Channel_Names_Validation').value}$`);
+			nameValidation = new RegExp(`^${settings.get('UTF8_Channel_Names_Validation')}$`);
 		} catch (error) {
 			nameValidation = new RegExp('^[0-9a-zA-Z-_.]+$');
 		}
@@ -51,7 +52,7 @@ export const getValidRoomName = (displayName, rid = '', options = {}) => {
 	if (options.allowDuplicates !== true) {
 		const room = Rooms.findOneByName(slugifiedName);
 		if (room && room._id !== rid) {
-			if (Settings.findOneById('UI_Allow_room_names_with_special_chars').value) {
+			if (settings.get('UI_Allow_room_names_with_special_chars')) {
 				let tmpName = slugifiedName;
 				let next = 0;
 				while (Rooms.findOneByNameAndNotId(tmpName, rid)) {
