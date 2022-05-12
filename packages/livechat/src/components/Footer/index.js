69b0f53aee99b4bb70fca64fc20da7e66888b3e4
Martin <martin.schoeler@rocket.chat>
2022-05-12 11:19:32 -0300

    Use @rocketchat/logo

--- packages/livechat/src/components/Footer/index.js
+++ packages/livechat/src/components/Footer/index.js
@@ -1,8 +1,8 @@
+import { RocketChatLogo } from '@rocket.chat/logo';
 import { withTranslation } from 'react-i18next';
 
 import { PopoverMenu } from '../Menu';
 import { createClassName } from '../helpers';
-import Logo from './logo.svg';
 import styles from './styles.scss';
 
 
@@ -24,7 +24,8 @@ export const PoweredBy = withTranslation()(({ className, t, ...props }) => (
 	<h3 className={createClassName(styles, 'powered-by', {}, [className])} {...props}>
 		{t('powered_by_rocket_chat').split('Rocket.Chat')[0]}
 		<a href='https://rocket.chat' target='_blank' rel='noopener noreferrer'>
-			<Logo className={createClassName(styles, 'powered-by__logo')} width={60} height={60 * 272 / 1500} role='img' aria-label='Rocket.Chat' />
+			<RocketChatLogo />
+			{/* <Logo className={createClassName(styles, 'powered-by__logo')} width={60} height={60 * 272 / 1500} role='img' aria-label='Rocket.Chat' /> */}
 		</a>
 		{t('powered_by_rocket_chat').split('Rocket.Chat')[1]}
 	</h3>
