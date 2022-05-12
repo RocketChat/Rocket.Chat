24f33c23f0851585d05c5a268acbea59600feec1
Kunal Verma <67605729+Kunalvrm555@users.noreply.github.com>
2022-05-12 23:32:48 +0530

    accomodate #25265  (Convert to tsx)+

--- client/components/UserStatusMenu.js
+++ client/components/UserStatusMenu.js
@@ -1,17 +1,32 @@
+import { UserStatus as UserStatusType } from '@rocket.chat/core-typings';
 import { Button, PositionAnimated, Options, useCursor, Box } from '@rocket.chat/fuselage';
-import React, { useRef, useCallback, useState, useMemo, useEffect } from 'react';
+import type { Placements } from '@rocket.chat/fuselage-hooks';
+import { useSetting, useTranslation } from '@rocket.chat/ui-contexts';
+import React, { ReactElement, ComponentProps, useRef, useCallback, useState, useMemo, useEffect } from 'react';
 
-import { useSetting } from '../contexts/SettingsContext';
-import { useTranslation } from '../contexts/TranslationContext';
 import { UserStatus } from './UserStatus';
 
-const UserStatusMenu = ({ onChange, optionWidth = undefined, initialStatus = 'offline', placement = 'bottom-end', ...props }) => {
+type UserStatusMenuProps = {
+	margin: ComponentProps<typeof Box>['margin'];
+	onChange: (type: UserStatusType) => void;
+	initialStatus?: UserStatusType;
+	optionWidth?: ComponentProps<typeof Box>['width'];
+	placement?: Placements;
+};
+
+const UserStatusMenu = ({
+	margin,
+	onChange,
+	initialStatus = UserStatusType.OFFLINE,
+	optionWidth = undefined,
+	placement = 'bottom-end',
+}: UserStatusMenuProps): ReactElement => {
 	const t = useTranslation();
 	const [status, setStatus] = useState(initialStatus);
-	const allowInvisibleStatus = useSetting('Accounts_AllowInvisibleStatusOption');
+	const allowInvisibleStatus = useSetting('Accounts_AllowInvisibleStatusOption') as boolean;
 
 	const options = useMemo(() => {
-		const renderOption = (status, label) => (
+		const renderOption = (status: UserStatusType, label: string): ReactElement => (
 			<Box display='flex' flexDirection='row' alignItems='center'>
 				<Box marginInlineEnd='x8'>
 					<UserStatus status={status} />
@@ -20,14 +35,14 @@ const UserStatusMenu = ({ onChange, optionWidth = undefined, initialStatus = 'of
 			</Box>
 		);
 
-		const statuses = [
-			['online', renderOption('online', t('Online'))],
-			['away', renderOption('away', t('Away'))],
-			['busy', renderOption('busy', t('Busy'))],
+		const statuses: Array<[value: UserStatusType, label: ReactElement]> = [
+			[UserStatusType.ONLINE, renderOption(UserStatusType.ONLINE, t('Online'))],
+			[UserStatusType.AWAY, renderOption(UserStatusType.AWAY, t('Away'))],
+			[UserStatusType.BUSY, renderOption(UserStatusType.BUSY, t('Busy'))],
 		];
 
 		if (allowInvisibleStatus) {
-			statuses.push(['offline', renderOption('offline', t('Offline'))]);
+			statuses.push([UserStatusType.OFFLINE, renderOption(UserStatusType.OFFLINE, t('Offline'))]);
 		}
 
 		return statuses;
@@ -39,9 +54,13 @@ const UserStatusMenu = ({ onChange, optionWidth = undefined, initialStatus = 'of
 		hide();
 	});
 
-	const ref = useRef();
+	const ref = useRef<HTMLButtonElement>(null);
 	const onClick = useCallback(() => {
-		ref.current.focus() & show();
+		if (!ref?.current) {
+			return;
+		}
+		ref.current.focus();
+		show();
 		ref.current.classList.add('focus-visible');
 	}, [show]);
 
@@ -58,7 +77,7 @@ const UserStatusMenu = ({ onChange, optionWidth = undefined, initialStatus = 'of
 
 	return (
 		<>
-			<Button ref={ref} small square ghost onClick={onClick} onBlur={hide} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} {...props}>
+			<Button ref={ref} small square ghost onClick={onClick} onBlur={hide} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} margin={margin}>
 				<UserStatus status={status} />
 			</Button>
 			<PositionAnimated width='auto' visible={visible} anchor={ref} placement={placement}>
