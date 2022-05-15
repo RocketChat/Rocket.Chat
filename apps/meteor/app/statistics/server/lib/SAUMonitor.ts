56df9a42fdf1b666c68b6cd1e8bc69f1b7b694f5
Fábio Albuquerque <albuquerquefabio@icloud.com>
2022-05-15 09:47:51 -0300

    Chore: Store login token on session collection (#25054)
    * Store login on session collection
    
    * New Sessions indexes
    
    * Create indexes
    
    * Skip loginToken Object if undefined
    
    * Ternary condition to check loginToken value
    
    * destructuring user._id assignment
    
    * Some types and interfaces have been updated
    
    * Fix lint on IMethodThisType.ts
    
    * Connection type as ISocketConnection
    
    * Revert changes
    
    * Reuse code; fix login token record key case
    
    * Add TODO comments
    
    * Stop prettier from screaming
    
    * Fix formatting
    
    Co-authored-by: Debdut Chakraborty <debdut.chakraborty@rocket.chat>

--- apps/meteor/app/statistics/server/lib/SAUMonitor.ts
+++ apps/meteor/app/statistics/server/lib/SAUMonitor.ts
@@ -10,6 +10,7 @@ import { aggregates } from '../../../models/server/raw/Sessions';
 import { Logger } from '../../../../server/lib/logger/Logger';
 import { getMostImportantRole } from '../../../../lib/roles/getMostImportantRole';
 import { sauEvents } from '../../../../server/services/sauMonitor/events';
+import { getClientAddress } from '../../../../server/lib/getClientAddress';
 
 type DateObj = { day: number; month: number; year: number };
 
@@ -103,7 +104,7 @@ export class SAUMonitorClass {
 			return;
 		}
 
-		sauEvents.on('accounts.login', async ({ userId, connection }) => {
+		sauEvents.on('accounts.login', async ({ userId, connection }: { userId: string; connection: ISocketConnection }) => {
 			if (!this.isRunning()) {
 				return;
 			}
@@ -189,15 +190,16 @@ export class SAUMonitorClass {
 			return;
 		}
 
-		const ip = connection.clientAddress || connection.httpHeaders?.['x-real-ip'] || connection.httpHeaders?.['x-forwarded-for'];
+		const ip = getClientAddress(connection);
 
-		const host = connection.httpHeaders?.host || '';
-
+		const host = connection.httpHeaders?.host ?? '';
+		const loginToken = connection?.loginToken ? { loginToken: connection?.loginToken } : {};
 		return {
 			type: 'session',
 			sessionId: connection.id,
 			instanceId: connection.instanceId,
-			ip: (Array.isArray(ip) ? ip[0] : ip) || '',
+			...loginToken,
+			ip,
 			host,
 			...this._getUserAgentInfo(connection),
 			...params,
--- apps/meteor/server/hooks/sauMonitorHooks.ts
+++ apps/meteor/server/hooks/sauMonitorHooks.ts
@@ -5,13 +5,22 @@ import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 
 import { sauEvents } from '../services/sauMonitor/events';
+import { ILoginAttempt } from '../../app/authentication/server/ILoginAttempt';
 
-Accounts.onLogin((info: { user: Meteor.User; connection: Meteor.Connection }) => {
-	const { httpHeaders } = info.connection;
+Accounts.onLogin((info: ILoginAttempt) => {
+	const {
+		methodArguments,
+		connection: { httpHeaders },
+	} = info;
+
+	// Sometimes there is no resume token
+	// TODO: check what case triggers onLogin that can cause a nonexistent resume token in methodArguments
+	const { resume } = methodArguments.find((arg) => 'resume' in arg) ?? {};
+	const loginToken = resume ? Accounts._hashLoginToken(resume) : '';
 
 	sauEvents.emit('accounts.login', {
 		userId: info.user._id,
-		connection: { instanceId: InstanceStatus.id(), ...info.connection, httpHeaders: httpHeaders as IncomingHttpHeaders },
+		connection: { ...info.connection, loginToken, instanceId: InstanceStatus.id(), httpHeaders: httpHeaders as IncomingHttpHeaders },
 	});
 });
 
