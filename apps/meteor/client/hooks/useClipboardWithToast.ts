47cb86949df1b6c673c6ed593669754e6c1b3f12
dougfabris <devfabris@gmail.com>
2022-05-16 13:22:19 -0300

    wip

--- apps/meteor/client/hooks/useClipboardWithToast.ts
+++ apps/meteor/client/hooks/useClipboardWithToast.ts
@@ -1,9 +1,10 @@
 import { useClipboard, UseClipboardReturn, useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import { useToastMessageDispatch, useTranslation } from '@rocket.chat/ui-contexts';
+import { useToastBarDispatch } from '@rocket.chat/fuselage-toastbar';
+import { useTranslation } from '@rocket.chat/ui-contexts';
 
 export default function useClipboardWithToast(text: string): UseClipboardReturn {
 	const t = useTranslation();
-	const dispatchToastMessage = useToastMessageDispatch();
+	const dispatchToastMessage = useToastBarDispatch();
 
 	return useClipboard(text, {
 		onCopySuccess: useMutableCallback(() => dispatchToastMessage({ type: 'success', message: t('Copied') })),
--- apps/meteor/client/providers/ToastMessagesProvider.tsx
+++ apps/meteor/client/providers/ToastMessagesProvider.tsx
@@ -1,33 +1,34 @@
-import { ToastMessagesContext } from '@rocket.chat/ui-contexts';
+import { ToastBarProvider } from '@rocket.chat/fuselage-toastbar';
+// import { ToastMessagesContext } from '@rocket.chat/ui-contexts';
 import React, { FC, useEffect } from 'react';
 import toastr from 'toastr';
 
 import { dispatchToastMessage, subscribeToToastMessages } from '../lib/toast';
 import { handleError } from '../lib/utils/handleError';
 
-const contextValue = {
-	dispatch: dispatchToastMessage,
-};
+// const contextValue = {
+// 	dispatch: ToastBarContext,
+// };
 
 const ToastMessagesProvider: FC = ({ children }) => {
-	useEffect(
-		() =>
-			subscribeToToastMessages(({ type, message, title, options }) => {
-				if (type === 'error' && typeof message === 'object') {
-					handleError(message);
-					return;
-				}
+	// useEffect(
+	// 	() =>
+	// 		subscribeToToastMessages(({ type, message, title, options }) => {
+	// 			if (type === 'error' && typeof message === 'object') {
+	// 				handleError(message);
+	// 				return;
+	// 			}
 
-				if (typeof message !== 'string') {
-					message = `[${message.name}] ${message.message}`;
-				}
+	// 			if (typeof message !== 'string') {
+	// 				message = `[${message.name}] ${message.message}`;
+	// 			}
 
-				toastr[type](message, title, options);
-			}),
-		[],
-	);
+	// 			toastr[type](message, title, options);
+	// 		}),
+	// 	[],
+	// );
 
-	return <ToastMessagesContext.Provider children={children} value={contextValue} />;
+	return <ToastBarProvider children={children} />;
 };
 
 export default ToastMessagesProvider;
