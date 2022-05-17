9557c5120d1c141a5260426b590994e26d716a6d
Weslley de Campos <weslley.campos@objective.com.br>
2022-05-17 13:38:13 -0300

    adjust in intermitences

--- apps/meteor/tests/e2e/04-main-elements-render.spec.ts
+++ apps/meteor/tests/e2e/04-main-elements-render.spec.ts
@@ -128,8 +128,6 @@ test.describe('[Main Elements Render]', function () {
 				await expect(mainContent.messageBoxActions()).toBeVisible();
 			});
 
-			// issues with the new message box action button and the no animations on tests
-
 			test('expect show the audio recording button', async () => {
 				await expect(mainContent.recordBtn()).toBeVisible();
 			});
