0eb60a3755afcecf708eedf76957e79d68da7701
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-05-17 17:57:15 -0300

    ..

--- packages/rest-typings/src/v1/invites.ts
+++ packages/rest-typings/src/v1/invites.ts
@@ -1,33 +1,33 @@
 import type { IUser, IInvite } from '@rocket.chat/core-typings';
 
-export type invitesEndpoints = {
-   'listInvites': {
-      GET: (params: { userId: IUser['_id'], rid: string }) => {
-         invites: IInvite[];
-      };
-   };
+export type InvitesEndpoints = {
+	listInvites: {
+		GET: (params: { userId: IUser['_id']; rid: string }) => {
+			invites: IInvite[];
+		};
+	};
 
-   'findOrCreateInvite': {
-      POST: (params: { userId: IUser['_id'], rid: IInvite['rid'], days: IInvite['days'], maxUses: IInvite['maxUses'] }) => {
-         invite: IInvite;
-      };
-   };
+	findOrCreateInvite: {
+		POST: (params: { userId: IUser['_id']; rid: IInvite['rid']; days: IInvite['days']; maxUses: IInvite['maxUses'] }) => {
+			invite: IInvite;
+		};
+	};
 
-   'removeInvite': {
-      DELETE: (params: { userId: IUser['_id'], _id: IInvite['_id'] }) => {
-         removed: boolean;
-      };
-   };
+	removeInvite: {
+		DELETE: (params: { userId: IUser['_id']; _id: IInvite['_id'] }) => {
+			removed: boolean;
+		};
+	};
 
-   'useInviteToken': {
-      POST: (params: { userId: IUser['_id'], token: IInvite['token'] }) => {
-         invite: IInvite;
-      };
-   };
+	useInviteToken: {
+		POST: (params: { userId: IUser['_id']; token: string }) => {
+			invite: IInvite;
+		};
+	};
 
-   'validateInviteToken': {
-      POST: (params: { userId: IUser['_id'], token: IInvite['token'] }) => {
-         invite: IInvite;
-      };
-   };
+	validateInviteToken: {
+		POST: (params: { userId: IUser['_id']; token: string }) => {
+			invite: IInvite;
+		};
+	};
 };
