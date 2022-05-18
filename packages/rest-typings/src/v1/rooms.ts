0d32f7c98462e6e59be2d622e1b28db36488b7d6
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-05-18 08:58:25 -0300

    remove duplicated imports

--- packages/rest-typings/src/v1/rooms.ts
+++ packages/rest-typings/src/v1/rooms.ts
@@ -3,9 +3,6 @@ import type { IMessage, IRoom, IUser, RoomAdminFieldsType } from '@rocket.chat/c
 import type { PaginatedRequest } from '../helpers/PaginatedRequest';
 import type { PaginatedResult } from '../helpers/PaginatedResult';
 
-import type { PaginatedRequest } from '../helpers/PaginatedRequest';
-import type { PaginatedResult } from '../helpers/PaginatedResult';
-
 export type RoomsEndpoints = {
 	'rooms.autocomplete.channelAndPrivate': {
 		GET: (params: { selector: string }) => {
