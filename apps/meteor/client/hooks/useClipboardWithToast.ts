6ea72b64a94771a7c15f9811def91a077e460dcf
dougfabris <devfabris@gmail.com>
2022-05-20 13:10:08 -0300

    improve: mix providers

--- apps/meteor/client/hooks/useClipboardWithToast.ts
+++ apps/meteor/client/hooks/useClipboardWithToast.ts
@@ -1,10 +1,9 @@
 import { useClipboard, UseClipboardReturn, useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import { useToastBarDispatch } from '@rocket.chat/fuselage-toastbar';
-import { useTranslation } from '@rocket.chat/ui-contexts';
+import { useToastMessageDispatch, useTranslation } from '@rocket.chat/ui-contexts';
 
 export default function useClipboardWithToast(text: string): UseClipboardReturn {
 	const t = useTranslation();
-	const dispatchToastMessage = useToastBarDispatch();
+	const dispatchToastMessage = useToastMessageDispatch();
 
 	return useClipboard(text, {
 		onCopySuccess: useMutableCallback(() => dispatchToastMessage({ type: 'success', message: t('Copied') })),
