377d09ea857f3cd53267f5fd0b47cb076ebb5e20
Kevin Aleman <kaleman960@gmail.com>
2022-05-20 13:24:10 -0600

    Chore: Rewrite autotranslate to ts (#25425)

--- apps/meteor/app/autotranslate/server/methods/getSupportedLanguages.ts
+++ apps/meteor/app/autotranslate/server/methods/getSupportedLanguages.ts
@@ -1,12 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
 
-import { hasPermission } from '../../../authorization';
+import { hasPermission } from '../../../authorization/server';
 import { TranslationProviderRegistry } from '..';
 
 Meteor.methods({
 	'autoTranslate.getSupportedLanguages'(targetLanguage) {
-		if (!hasPermission(Meteor.userId(), 'auto-translate')) {
+		const userId = Meteor.userId();
+		if (!userId) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
+				method: 'getSupportedLanguages',
+			});
+		}
+
+		if (!hasPermission(userId, 'auto-translate')) {
 			throw new Meteor.Error('error-action-not-allowed', 'Auto-Translate is not allowed', {
 				method: 'autoTranslate.saveSettings',
 			});
