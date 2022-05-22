a14ab6ad6f39c5d009c567a56a6c794dd043eba4
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-05-22 02:05:08 -0300

    Restored user language /msg

--- apps/meteor/app/slashcommands-msg/server/server.ts
+++ apps/meteor/app/slashcommands-msg/server/server.ts
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
+import type { IMessage } from '@rocket.chat/core-typings';
 
 import { slashCommands } from '../../utils/lib/slashCommand';
 import { settings } from '../../settings/server';
@@ -11,25 +12,27 @@ import { api } from '../../../server/sdk/api';
  * Msg is a named function that will replace /msg commands
  */
 
-function Msg(_command: 'msg', params: string, item: Record<string, string>): void | Promise<void> {
+function Msg(_command: 'msg', params: string, item: IMessage): void {
 	const trimmedParams = params.trim();
 	const separator = trimmedParams.indexOf(' ');
 	const userId = Meteor.userId() as string;
 	if (separator === -1) {
-		return api.broadcast('notify.ephemeralMessage', userId, item.rid, {
+		api.broadcast('notify.ephemeralMessage', userId, item.rid, {
 			msg: TAPi18n.__('Username_and_message_must_not_be_empty', { lng: settings.get('Language') || 'en' }),
 		});
+		return;
 	}
 	const message = trimmedParams.slice(separator + 1);
 	const targetUsernameOrig = trimmedParams.slice(0, separator);
 	const targetUsername = targetUsernameOrig.replace('@', '');
 	const targetUser = Users.findOneByUsernameIgnoringCase(targetUsername);
 	if (targetUser == null) {
+		const user = Users.findOneById(userId, { fields: { language: 1 } });
 		api.broadcast('notify.ephemeralMessage', userId, item.rid, {
 			msg: TAPi18n.__('Username_doesnt_exist', {
 				postProcess: 'sprintf',
 				sprintf: [targetUsernameOrig],
-				lng: settings.get('Language') || 'en',
+				lng: user?.language || settings.get('Language') || 'en',
 			}),
 		});
 		return;
@@ -43,16 +46,8 @@ function Msg(_command: 'msg', params: string, item: Record<string, string>): voi
 	Meteor.call('sendMessage', msgObject);
 }
 
-slashCommands.add(
-	'msg',
-	Msg,
-	{
-		description: 'Direct_message_someone',
-		params: '@username <message>',
-		permission: 'create-d',
-	},
-	undefined,
-	false,
-	undefined,
-	undefined,
-);
+slashCommands.add('msg', Msg, {
+	description: 'Direct_message_someone',
+	params: '@username <message>',
+	permission: 'create-d',
+});
