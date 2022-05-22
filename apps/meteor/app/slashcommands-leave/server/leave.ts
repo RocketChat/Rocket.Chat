50a5a3b65456a10333cf9ce6ee8af1fde0944272
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-05-22 01:58:44 -0300

    Restored user language on /leave

--- apps/meteor/app/slashcommands-leave/server/leave.ts
+++ apps/meteor/app/slashcommands-leave/server/leave.ts
@@ -1,15 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/rocketchat:tap-i18n';
+import type { IMessage } from '@rocket.chat/core-typings';
 
 import { slashCommands } from '../../utils/lib/slashCommand';
 import { settings } from '../../settings/server';
 import { api } from '../../../server/sdk/api';
+import { Users } from '../../models/server';
 
 /*
  * Leave is a named function that will replace /leave commands
  * @param {Object} message - The message object
  */
-function Leave(_command: string, _params: string, item: Record<string, string>): void {
+function Leave(_command: string, _params: string, item: IMessage): void {
 	try {
 		Meteor.call('leaveRoom', item.rid);
 	} catch ({ error }) {
@@ -17,33 +19,18 @@ function Leave(_command: string, _params: string, item: Record<string, string>):
 		if (typeof error !== 'string') {
 			return;
 		}
+		const user = Users.findOneById(userId);
 		api.broadcast('notify.ephemeralMessage', userId, item.rid, {
-			msg: TAPi18n.__(error, { lng: settings.get('Language') || 'en' }),
+			msg: TAPi18n.__(error, { lng: user?.language || settings.get('Language') || 'en' }),
 		});
 	}
 }
 
-slashCommands.add(
-	'leave',
-	Leave,
-	{
-		description: 'Leave_the_current_channel',
-		permission: ['leave-c', 'leave-p'],
-	},
-	undefined,
-	false,
-	undefined,
-	undefined,
-);
-slashCommands.add(
-	'part',
-	Leave,
-	{
-		description: 'Leave_the_current_channel',
-		permission: ['leave-c', 'leave-p'],
-	},
-	undefined,
-	false,
-	undefined,
-	undefined,
-);
+slashCommands.add('leave', Leave, {
+	description: 'Leave_the_current_channel',
+	permission: ['leave-c', 'leave-p'],
+});
+slashCommands.add('part', Leave, {
+	description: 'Leave_the_current_channel',
+	permission: ['leave-c', 'leave-p'],
+});
