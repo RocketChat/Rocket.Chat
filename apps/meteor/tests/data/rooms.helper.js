ca43ab139266e9b1e6844f65bc33d688710343bc
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-05-23 18:04:40 -0300

    room.helpers

--- apps/meteor/tests/data/rooms.helper.js
+++ apps/meteor/tests/data/rooms.helper.js
@@ -1,6 +1,6 @@
 import { api, credentials, request } from './api-data';
 
-export const createRoom = ({ name, type, username, token, agentId, members = [], credentials: customCredentials }) => {
+export const createRoom = ({ name, type, username, token, agentId, members, credentials: customCredentials }) => {
 	if (!type) {
 		throw new Error('"type" is required in "createRoom.ts" test helper');
 	}
