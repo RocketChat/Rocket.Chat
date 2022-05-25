3019f4041a7df6dad471ac520aea68731c3a251f
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-05-25 08:08:41 -0300

    add cloud.ts and directory.ts json schemas

--- packages/rest-typings/src/v1/cloud.ts
+++ packages/rest-typings/src/v1/cloud.ts
@@ -1,16 +1,79 @@
 import type { CloudRegistrationIntentData, CloudConfirmationPollData, CloudRegistrationStatus } from '@rocket.chat/core-typings';
+import Ajv, { JSONSchemaType } from 'ajv';
+
+const ajv = new Ajv();
+
+type CloudManualRegister = {
+	cloudBlob: string;
+};
+
+const CloudManualRegisterSchema: JSONSchemaType<CloudManualRegister> = {
+	type: 'object',
+	properties: {
+		cloudBlob: {
+			type: 'string',
+		},
+	},
+	required: ['cloudBlob'],
+	additionalProperties: false,
+};
+
+export const isCloudManualRegisterProps = ajv.compile(CloudManualRegisterSchema);
+
+type CloudCreateRegistrationIntent = {
+	resend: boolean;
+	email: string;
+};
+
+const CloudCreateRegistrationIntentSchema: JSONSchemaType<CloudCreateRegistrationIntent> = {
+	type: 'object',
+	properties: {
+		resend: {
+			type: 'boolean',
+		},
+		email: {
+			type: 'string',
+		},
+	},
+	required: ['resend', 'email'],
+	additionalProperties: false,
+};
+
+export const isCloudCreateRegistrationIntentProps = ajv.compile(CloudCreateRegistrationIntentSchema);
+
+type CloudConfirmationPoll = {
+	deviceCode: string;
+	resend?: string;
+};
+
+const CloudConfirmationPollSchema: JSONSchemaType<CloudConfirmationPoll> = {
+	type: 'object',
+	properties: {
+		deviceCode: {
+			type: 'string',
+		},
+		resend: {
+			type: 'string',
+			nullable: true,
+		},
+	},
+	required: ['deviceCode'],
+	additionalProperties: false,
+};
+
+export const isCloudConfirmationPollProps = ajv.compile(CloudConfirmationPollSchema);
 
 export type CloudEndpoints = {
 	'cloud.manualRegister': {
-		POST: (params: { cloudBlob: string }) => void;
+		POST: (params: CloudManualRegister) => void;
 	};
 	'cloud.createRegistrationIntent': {
-		POST: (params: { resend: boolean; email: string }) => {
+		POST: (params: CloudCreateRegistrationIntent) => {
 			intentData: CloudRegistrationIntentData;
 		};
 	};
 	'cloud.confirmationPoll': {
-		GET: (params: { deviceCode: string; resend?: boolean }) => {
+		GET: (params: CloudConfirmationPoll) => {
 			pollData: CloudConfirmationPollData;
 		};
 	};
--- packages/rest-typings/src/v1/directory.ts
+++ packages/rest-typings/src/v1/directory.ts
@@ -1,14 +1,41 @@
 import type { IRoom } from '@rocket.chat/core-typings';
+import Ajv, { JSONSchemaType } from 'ajv';
 
 import type { PaginatedResult } from '../helpers/PaginatedResult';
 
+const ajv = new Ajv();
+
+type DirectoryProps = {
+	query: { [key: string]: string };
+	count: number;
+	offset: number;
+	sort: { [key: string]: number };
+};
+
+const DirectorySchema: JSONSchemaType<DirectoryProps> = {
+	type: 'object',
+	properties: {
+		query: {
+			type: 'object',
+		},
+		count: {
+			type: 'number',
+		},
+		offset: {
+			type: 'number',
+		},
+		sort: {
+			type: 'object',
+		},
+	},
+	required: ['query', 'count', 'offset', 'sort'],
+	additionalProperties: false,
+};
+
+export const isDirectoryProps = ajv.compile(DirectorySchema);
+
 export type DirectoryEndpoint = {
 	directory: {
-		GET: (params: {
-			query: { [key: string]: string };
-			count: number;
-			offset: number;
-			sort: { [key: string]: number };
-		}) => PaginatedResult<{ result: IRoom[] }>;
+		GET: (params: DirectoryProps) => PaginatedResult<{ result: IRoom[] }>;
 	};
 };
