e87b3a9ac75a0dadb06c723371827d2e4622f44a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-05-31 11:03:08 -0300

    Remove mandatory Blaze template from `createRouteGroup`

--- apps/meteor/client/lib/createRouteGroup.tsx
+++ apps/meteor/client/lib/createRouteGroup.tsx
@@ -1,38 +1,23 @@
 import { FlowRouter, Group, RouteOptions } from 'meteor/kadira:flow-router';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Tracker } from 'meteor/tracker';
-import React, { ComponentType, createElement, lazy, ReactNode } from 'react';
+import React, { ElementType, ReactNode } from 'react';
 
-import { appLayout, BlazeTemplate, MainLayout } from './appLayout';
-import { createTemplateForComponent } from './portals/createTemplateForComponent';
-
-type RouteRegister = {
-	(
-		path: string,
-		options: RouteOptions & {
-			lazyRouteComponent: () => Promise<{ default: ComponentType }>;
-			props?: Record<string, unknown>;
-			ready?: boolean;
-		},
-	): [register: () => void, unregister: () => void];
-	(path: string, options: RouteOptions): void;
-};
+import { appLayout, MainLayout } from './appLayout';
 
 const registerLazyComponentRoute = (
 	routeGroup: Group,
-	importRouter: () => Promise<{
-		default: ComponentType<{
-			renderRoute?: () => ReactNode;
-		}>;
+	RouterComponent: ElementType<{
+		children?: ReactNode;
 	}>,
 	path: string,
 	{
-		lazyRouteComponent,
+		component: RouteComponent,
 		props,
 		ready = true,
 		...rest
 	}: RouteOptions & {
-		lazyRouteComponent: () => Promise<{ default: ComponentType }>;
+		component: ElementType;
 		props?: Record<string, unknown>;
 		ready?: boolean;
 	},
@@ -60,25 +45,16 @@ const registerLazyComponentRoute = (
 		computation?.stop();
 	};
 
-	const RouteComponent = lazy(lazyRouteComponent);
-	const renderRoute = (): ReactNode => createElement(RouteComponent, props);
-
 	routeGroup.route(path, {
 		...rest,
 		triggersEnter: [handleEnter, ...(rest.triggersEnter ?? [])],
 		triggersExit: [handleExit, ...(rest.triggersExit ?? [])],
 		action() {
-			const center = createTemplateForComponent(
-				Tracker.nonreactive(() => FlowRouter.getRouteName()),
-				importRouter,
-				{
-					attachment: 'at-parent',
-					props: () => ({ renderRoute }),
-				},
-			);
 			appLayout.render(
 				<MainLayout>
-					<BlazeTemplate template={center} />
+					<RouterComponent>
+						<RouteComponent {...props} />
+					</RouterComponent>
 				</MainLayout>,
 			);
 		},
@@ -90,12 +66,20 @@ const registerLazyComponentRoute = (
 export const createRouteGroup = (
 	name: string,
 	prefix: string,
-	importRouter: () => Promise<{
-		default: ComponentType<{
-			renderRoute?: () => ReactNode;
-		}>;
+	RouterComponent: ElementType<{
+		children?: ReactNode;
 	}>,
-): RouteRegister => {
+): {
+	(
+		path: string,
+		options: RouteOptions & {
+			component: ElementType;
+			props?: Record<string, unknown>;
+			ready?: boolean;
+		},
+	): [register: () => void, unregister: () => void];
+	(path: string, options: RouteOptions): void;
+} => {
 	const routeGroup = FlowRouter.group({
 		name,
 		prefix,
@@ -104,7 +88,7 @@ export const createRouteGroup = (
 	function registerRoute(
 		path: string,
 		options: RouteOptions & {
-			lazyRouteComponent: () => Promise<{ default: ComponentType }>;
+			component: ElementType;
 			props?: Record<string, unknown>;
 			ready?: boolean;
 		},
@@ -115,13 +99,13 @@ export const createRouteGroup = (
 		options:
 			| RouteOptions
 			| (RouteOptions & {
-					lazyRouteComponent: () => Promise<{ default: ComponentType }>;
+					component: ElementType;
 					props?: Record<string, unknown>;
 					ready?: boolean;
 			  }),
 	): [register: () => void, unregister: () => void] | void {
-		if ('lazyRouteComponent' in options) {
-			return registerLazyComponentRoute(routeGroup, importRouter, path, options);
+		if ('component' in options) {
+			return registerLazyComponentRoute(routeGroup, RouterComponent, path, options);
 		}
 
 		routeGroup.route(path, options);
@@ -130,12 +114,9 @@ export const createRouteGroup = (
 	registerRoute('/', {
 		name: `${name}-index`,
 		action() {
-			const center = createTemplateForComponent(`${name}-index`, importRouter, {
-				attachment: 'at-parent',
-			});
 			appLayout.render(
 				<MainLayout>
-					<BlazeTemplate template={center} />
+					<RouterComponent />
 				</MainLayout>,
 			);
 		},
