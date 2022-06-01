6b41aa62071323d91e75c111777ba1a677d4739a
Jean Brito <jeanfbrito@gmail.com>
2022-05-31 22:52:51 -0300

    Chore: Convert components/sidebar to TS (#25429)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/client/components/Sidebar/Header.tsx
+++ apps/meteor/client/components/Sidebar/Header.tsx
@@ -1,7 +1,12 @@
 import { Box, ActionButton } from '@rocket.chat/fuselage';
-import React from 'react';
+import React, { FC, ReactElement } from 'react';
 
-const Header = ({ title, onClose, children = undefined, ...props }) => (
+type HeaderProps = {
+	title?: ReactElement | string;
+	onClose?: () => void;
+};
+
+const Header: FC<HeaderProps> = ({ title, onClose, children, ...props }) => (
 	<Box is='header' display='flex' flexDirection='column' pb='x16' {...props}>
 		{(title || onClose) && (
 			<Box display='flex' flexDirection='row' alignItems='center' pi='x24' justifyContent='space-between' flexGrow={1}>
