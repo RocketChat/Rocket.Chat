e3cf6f1693e3273742f3c77de0ffbf60772c99ac
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-06-02 14:08:24 -0300

    Chore: Taking out Blaze from routes with `MainLayout`  (#25697)

--- apps/meteor/client/views/omnichannel/routes.ts
+++ apps/meteor/client/views/omnichannel/routes.ts
@@ -1,83 +1,89 @@
+import { lazy } from 'react';
+
 import { createRouteGroup } from '../../lib/createRouteGroup';
 
-export const registerOmnichannelRoute = createRouteGroup('omnichannel', '/omnichannel', () => import('./OmnichannelRouter'));
+export const registerOmnichannelRoute = createRouteGroup(
+	'omnichannel',
+	'/omnichannel',
+	lazy(() => import('./OmnichannelRouter')),
+);
 
 registerOmnichannelRoute('/installation', {
 	name: 'omnichannel-installation',
-	lazyRouteComponent: () => import('./installation/Installation'),
+	component: lazy(() => import('./installation/Installation')),
 });
 
 registerOmnichannelRoute('/managers', {
 	name: 'omnichannel-managers',
-	lazyRouteComponent: () => import('./managers/ManagersRoute'),
+	component: lazy(() => import('./managers/ManagersRoute')),
 });
 
 registerOmnichannelRoute('/agents/:context?/:id?', {
 	name: 'omnichannel-agents',
-	lazyRouteComponent: () => import('./agents/AgentsRoute'),
+	component: lazy(() => import('./agents/AgentsRoute')),
 });
 
 registerOmnichannelRoute('/webhooks', {
 	name: 'omnichannel-webhooks',
-	lazyRouteComponent: () => import('./webhooks/WebhooksPageContainer'),
+	component: lazy(() => import('./webhooks/WebhooksPageContainer')),
 });
 
 registerOmnichannelRoute('/customfields/:context?/:id?', {
 	name: 'omnichannel-customfields',
-	lazyRouteComponent: () => import('./customFields/CustomFieldsRoute'),
+	component: lazy(() => import('./customFields/CustomFieldsRoute')),
 });
 
 registerOmnichannelRoute('/appearance', {
 	name: 'omnichannel-appearance',
-	lazyRouteComponent: () => import('./appearance/AppearancePageContainer'),
+	component: lazy(() => import('./appearance/AppearancePageContainer')),
 });
 
 registerOmnichannelRoute('/businessHours/:context?/:type?/:id?', {
 	name: 'omnichannel-businessHours',
-	lazyRouteComponent: () => import('./businessHours/BusinessHoursRouter'),
+	component: lazy(() => import('./businessHours/BusinessHoursRouter')),
 });
 
 registerOmnichannelRoute('/units/:context?/:id?', {
 	name: 'omnichannel-units',
-	lazyRouteComponent: () => import('../../../ee/client/omnichannel/units/UnitsRoute'),
+	component: lazy(() => import('../../../ee/client/omnichannel/units/UnitsRoute')),
 });
 
 registerOmnichannelRoute('/tags/:context?/:id?', {
 	name: 'omnichannel-tags',
-	lazyRouteComponent: () => import('../../../ee/client/omnichannel/tags/TagsRoute'),
+	component: lazy(() => import('../../../ee/client/omnichannel/tags/TagsRoute')),
 });
 
 registerOmnichannelRoute('/priorities/:context?/:id?', {
 	name: 'omnichannel-priorities',
-	lazyRouteComponent: () => import('../../../ee/client/omnichannel/priorities/PrioritiesRoute'),
+	component: lazy(() => import('../../../ee/client/omnichannel/priorities/PrioritiesRoute')),
 });
 
 registerOmnichannelRoute('/triggers/:context?/:id?', {
 	name: 'omnichannel-triggers',
-	lazyRouteComponent: () => import('./triggers/TriggersPage'),
+	component: lazy(() => import('./triggers/TriggersPage')),
 });
 
 registerOmnichannelRoute('/facebook', {
 	name: 'omnichannel-facebook',
-	lazyRouteComponent: () => import('./facebook/FacebookPageContainer'),
+	component: lazy(() => import('./facebook/FacebookPageContainer')),
 });
 
 registerOmnichannelRoute('/current/:id?/:tab?/:context?', {
 	name: 'omnichannel-current-chats',
-	lazyRouteComponent: () => import('./currentChats/CurrentChatsRoute'),
+	component: lazy(() => import('./currentChats/CurrentChatsRoute')),
 });
 
 registerOmnichannelRoute('/departments/:context?/:id?/:tab?', {
 	name: 'omnichannel-departments',
-	lazyRouteComponent: () => import('./departments/DepartmentsRoute'),
+	component: lazy(() => import('./departments/DepartmentsRoute')),
 });
 
 registerOmnichannelRoute('/realtime-monitoring', {
 	name: 'omnichannel-realTime',
-	lazyRouteComponent: () => import('./realTimeMonitoring/RealTimeMonitoringPage'),
+	component: lazy(() => import('./realTimeMonitoring/RealTimeMonitoringPage')),
 });
 
 registerOmnichannelRoute('/analytics', {
 	name: 'omnichannel-analytics',
-	lazyRouteComponent: () => import('./analytics/AnalyticsPage'),
+	component: lazy(() => import('./analytics/AnalyticsPage')),
 });
