1c557df4ded77a001043bb2bb74ad4d9d080519b
Marcos Defendi <marcos.defendi@rocket.chat>
2022-06-07 09:15:21 -0300

    fix: remove extra object

--- apps/meteor/app/federation-v2/client/Federation.ts
+++ apps/meteor/app/federation-v2/client/Federation.ts
@@ -5,20 +5,6 @@ import { RoomMemberActions } from '../../../definition/IRoomTypeConfig';
 export type FEDERATION_MESSAGE_ACTION_CONTEXT = 'federated';
 const allowedActionsInFederatedRooms: ValueOf<typeof RoomMemberActions>[] = [RoomMemberActions.REMOVE_USER];
 
-const A = {
-	thread: false,
-	discussion: false,
-	call: false,
-	files: false,
-	pinned: false,
-	livestream: false,
-	prune: false,
-	private: false,
-	readonly: false,
-	encrypted: false,
-	broadcast: false,
-}
-
 export class Federation {
 	public static isAFederatedRoom(room: IRoom): boolean {
 		return room.federated === true;
