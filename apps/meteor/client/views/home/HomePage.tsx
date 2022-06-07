f65013fb42b7d1e3cdfc626598dedcb37993e1b3
guijun13 <guilhermejun13.grillo@gmail.com>
2022-06-07 18:29:14 -0300

    feat: create header for new homepage

--- apps/meteor/client/views/home/HomePage.tsx
+++ apps/meteor/client/views/home/HomePage.tsx
@@ -1,16 +1,46 @@
+import { Box, Button, Icon } from '@rocket.chat/fuselage';
+import { useSetting, useLayout } from '@rocket.chat/ui-contexts';
 import React, { ReactElement } from 'react';
 
+import BurgerMenu from '../../components/BurgerMenu';
+import TemplateHeader from '../../components/Header';
+import Page from '../../components/Page/Page';
 import CustomHomePage from './CustomHomePage';
 
 // TODO: use a setting to determine if the user has a custom home page
-const custom = true;
+const custom = false;
+// TODO: verify if user is admin or not
+const isAdmin = true;
 
 const HomePage = (): ReactElement => {
+	const title = useSetting('Layout_Home_Title') as string;
+	const { isMobile } = useLayout();
+
 	if (custom) {
 		return <CustomHomePage />;
 	}
 
-	return <></>; // TODO: render a default home page
+	return (
+		<Page data-qa='page-home'>
+			<Box marginBlock='x16' marginInline='x24' display='flex' flexDirection='row' justifyContent='space-between'>
+				{isMobile && (
+					<TemplateHeader.ToolBox>
+						<BurgerMenu />
+					</TemplateHeader.ToolBox>
+				)}
+				<Box as='h2' fontScale='h2' flexGrow={1}>
+					{title}
+				</Box>
+				{isAdmin && (
+					<Button is='a' href='/admin/settings/Layout'>
+						<>
+							<Icon name='pencil' size='x16' /> Customize
+						</>
+					</Button>
+				)}
+			</Box>
+		</Page>
+	);
 };
 
 export default HomePage;
