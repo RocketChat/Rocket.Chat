15be5519dfe6803d5d543f27c8fb16cee92b96f2
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-06-10 01:20:45 -0300

    fix review

--- apps/meteor/client/providers/VideoConfPopupProvider.tsx
+++ apps/meteor/client/providers/VideoConfPopupProvider.tsx
@@ -1,12 +1,10 @@
 import { IRoom } from '@rocket.chat/core-typings';
-import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { useCustomSound, useSetModal } from '@rocket.chat/ui-contexts';
 import { VideoConfPopupBackdrop } from '@rocket.chat/ui-video-conf';
 import React, { ReactElement, useState, ReactNode, useEffect, useMemo } from 'react';
 
 import GenericModal from '../components/GenericModal';
 import { VideoConfPopupContext, VideoConfIncomingCall, VideoConfPopupPayload } from '../contexts/VideoConfPopupContext';
-import { useBlockedAction } from '../hooks/useBlockedAction';
 import { VideoConfManager, useVideoConfIncomingCalls, useIsRinging } from '../lib/VideoConfManager';
 import VideoConfPopupPortal from '../portals/VideoConfPopupPortal';
 import VideoConfPopup from '../views/room/contextualBar/VideoConference/VideoConfPopup';
@@ -17,33 +15,33 @@ const VideoConfContextProvider = ({ children }: { children: ReactNode }): ReactE
 	const customSound = useCustomSound();
 	const isRinging = useIsRinging();
 
-	const [simpleState, setSimpleState] = useState<{ url: string; callId: string }>();
 	const setModal = useSetModal();
 
-	// TODO: This is an experiemntal hook and behavior
-	VideoConfManager.once('call/join', (props) => setSimpleState(props));
-	const { isBlocked, handleRetry, setIsBlocked } = useBlockedAction({ url: simpleState?.url });
+	useEffect(
+		() =>
+			VideoConfManager.once('call/join', (props) => {
+				const open = (): void => {
+					const popup = window.open(props.url);
 
-	const handleClose = useMutableCallback((): void => {
-		setSimpleState(undefined);
-		setIsBlocked(false);
-		return setModal();
-	});
+					if (popup !== null) {
+						return;
+					}
 
-	useEffect(() => {
-		if (isBlocked) {
-			return setModal(
-				<GenericModal
-					variant='warning'
-					title='Action blocked'
-					confirmText={'Open_again'}
-					onConfirm={handleRetry}
-					onCancel={handleClose}
-					onClose={handleClose}
-				>{`Your browser has blocked the page {PAGE}. <br/> Consider enable popups for this domain url`}</GenericModal>,
-			);
-		}
-	}, [handleClose, isBlocked, handleRetry, setModal]);
+					setModal(
+						<GenericModal
+							variant='warning'
+							title='Action blocked'
+							confirmText={'Open_again'}
+							onConfirm={open}
+							onCancel={(): void => setModal()}
+							onClose={(): void => setModal()}
+						>{`Your browser has blocked the page {PAGE}. <br/> Consider enable popups for this domain url`}</GenericModal>,
+					);
+				};
+				open();
+			}),
+		[setModal],
+	);
 
 	const contextValue = useMemo(
 		() => ({
@@ -58,7 +56,7 @@ const VideoConfContextProvider = ({ children }: { children: ReactNode }): ReactE
 			rejectIncomingCall: (callId: string): void => VideoConfManager.rejectIncomingCall(callId),
 			abortCall: (): void => VideoConfManager.abortCall(),
 			useIncomingCalls: (): VideoConfIncomingCall[] => useVideoConfIncomingCalls(),
-			setPreferences: (prefs: Partial<typeof VideoConfManager['preferences']>): void => VideoConfManager.setPreferences(prefs),
+			setPreferences: (preferences: Partial<typeof VideoConfManager['preferences']>): void => VideoConfManager.setPreferences(preferences),
 			changePreference: (key: 'cam' | 'mic', value: boolean): void => VideoConfManager.changePreference(key, value),
 			useIsRinging: (): boolean => useIsRinging(),
 		}),
