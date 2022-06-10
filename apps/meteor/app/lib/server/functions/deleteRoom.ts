e9b501d9e520a652b1a42c0d719a2e20da89bdc0
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2022-06-10 18:13:31 -0300

    fix: ensure that a federated room cant be deleted

--- apps/meteor/app/lib/server/functions/deleteRoom.ts
+++ apps/meteor/app/lib/server/functions/deleteRoom.ts
@@ -1,10 +1,17 @@
 import { DeleteWriteOpResultObject } from 'mongodb';
+import { Meteor } from 'meteor/meteor';
 
 import { Messages, Subscriptions, Rooms } from '../../../models/server';
 import { callbacks } from '../../../../lib/callbacks';
 import { FileUpload } from '../../../file-upload/server';
 
 export const deleteRoom = function (rid: string): Promise<DeleteWriteOpResultObject> {
+	const [room] = Rooms.findById(rid).fetch();
+
+	if (room.federated) {
+		throw new Meteor.Error('error-cannot-delete-federated-room', 'Cannot delete federated room');
+	}
+
 	FileUpload.removeFilesByRoomId(rid);
 	Messages.removeByRoomId(rid);
 	callbacks.run('beforeDeleteRoom', rid);
