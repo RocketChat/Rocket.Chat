7a0efa733707a7cde57c7105febf88e343cbd4b1
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-06-10 19:07:16 -0300

    dial pad hook

--- apps/meteor/client/components/voip/modal/DialPad/DialPadModal.tsx
+++ apps/meteor/client/components/voip/modal/DialPad/DialPadModal.tsx
@@ -1,65 +1,25 @@
 import { Box, Button, Icon, Field, Modal, TextInput } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
-import React, { FormEvent, ReactElement, useCallback, useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { ReactElement } from 'react';
 import InputMask from 'react-input-mask';
 
 import Pad from './Pad';
+import { useDialPad } from './hooks/useDialPad';
 
 const DialPadModal = (): ReactElement => {
 	const t = useTranslation();
 
 	const {
-		setFocus,
-		register,
-		setValue,
-		getValues,
-		setError,
-		clearErrors,
-		formState: {
-			errors: { PhoneInput: error },
-		},
-	} = useForm<{ PhoneInput: string }>();
-	const { name, ref, onChange } = register('PhoneInput');
-
-	const [disabled, setDisabled] = useState(true);
-
-	const handleChange = useCallback((): void => {
-		const value = getValues('PhoneInput');
-		setDisabled(!value);
-		clearErrors('PhoneInput');
-	}, [clearErrors, getValues]);
-
-	const handleBackspaceClick = useCallback((): void => {
-		const value = getValues('PhoneInput').slice(0, -1);
-		setValue('PhoneInput', value);
-		handleChange();
-	}, [getValues, handleChange, setValue]);
-
-	const handleInputChange = useCallback(
-		(e: FormEvent): void => {
-			onChange(e);
-			handleChange();
-		},
-		[handleChange, onChange],
-	);
-
-	const onClickPadButton = useCallback(
-		(digit: string | number): void => {
-			const value = getValues('PhoneInput') + digit;
-			setValue('PhoneInput', value);
-			handleChange();
-		},
-		[getValues, handleChange, setValue],
-	);
-
-	const onClickCallButton = useCallback((): void => {
-		setError('PhoneInput', { message: t('Something_went_wrong_try_again_later') });
-	}, [setError, t]);
-
-	useEffect(() => {
-		setFocus('PhoneInput');
-	}, [setFocus]);
+		inputName,
+		inputValue,
+		inputRef,
+		inputError,
+		isButtonDisabled,
+		handleInputChange,
+		handleBackspaceClick,
+		handlePadButtonClick,
+		handleCallButtonClick,
+	} = useDialPad();
 
 	return (
 		<Modal w='400px'>
@@ -70,38 +30,38 @@ const DialPadModal = (): ReactElement => {
 			<Modal.Content display='flex' justifyContent='center' flexDirection='column'>
 				<Field>
 					<InputMask
-						value={getValues('PhoneInput')}
-						name={name}
-						mask='(99) 99999-9999'
+						value={inputValue}
+						name={inputName}
+						mask='+99 (99) 99999-9999'
 						onChange={handleInputChange}
 						maskChar=''
 						alwaysShowMask={false}
 					>
 						{({ onChange, name }: any): ReactElement => (
 							<TextInput
-								ref={ref}
+								ref={inputRef}
 								textAlign='center'
 								placeholder={t('Phone_number')}
 								addon={
-									<Button nude small square size='20px' disabled={disabled} onClick={handleBackspaceClick}>
+									<Button nude small square size='20px' disabled={isButtonDisabled} onClick={handleBackspaceClick}>
 										<Icon name='backspace' size='20px' />
 									</Button>
 								}
-								error={error?.message}
+								error={inputError}
 								onChange={onChange}
 								name={name}
 							/>
 						)}
 					</InputMask>
 					<Field.Error h='20px' textAlign='center'>
-						{error?.message}
+						{inputError}
 					</Field.Error>
 				</Field>
-				<Pad onClickPadButton={onClickPadButton} />
+				<Pad onClickPadButton={handlePadButtonClick} />
 			</Modal.Content>
 			<Modal.Footer>
 				<Box display='flex' justifyContent='center'>
-					<Button disabled={disabled} borderRadius='full' primary size='64px' onClick={onClickCallButton}>
+					<Button disabled={isButtonDisabled} borderRadius='full' primary size='64px' onClick={handleCallButtonClick}>
 						<Icon color='white' name='phone' size='32px' />
 					</Button>
 				</Box>
---
+++ apps/meteor/client/components/voip/modal/DialPad/hooks/useDialPad.tsx
@@ -0,0 +1,70 @@
+import { useTranslation } from '@rocket.chat/ui-contexts';
+import { FormEvent, RefCallback, useCallback, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type DialPadStateHandlers = {
+	inputName: string;
+	inputValue: string;
+	inputRef: RefCallback<HTMLInputElement>;
+	inputError: string | undefined;
+	isButtonDisabled: boolean;
+	handleInputChange: (event: FormEvent<HTMLInputElement>) => void;
+	handleBackspaceClick: () => void;
+	handlePadButtonClick: (digit: string | number) => void;
+	handleCallButtonClick: () => void;
+};
+
+export const useDialPad = (): DialPadStateHandlers => {
+	const t = useTranslation();
+
+	const {
+		setFocus,
+		register,
+		setValue,
+		setError,
+		clearErrors,
+		watch,
+		formState: { errors },
+	} = useForm<{ PhoneInput: string }>();
+
+	const { ref, onChange } = register('PhoneInput');
+	const value = watch('PhoneInput');
+
+	const [disabled, setDisabled] = useState(true);
+
+	const handleBackspaceClick = useCallback((): void => {
+		setValue('PhoneInput', value.slice(0, -1));
+	}, [setValue, value]);
+
+	const handlePadButtonClick = useCallback(
+		(digit: string | number): void => {
+			setValue('PhoneInput', value + digit);
+		},
+		[setValue, value],
+	);
+
+	const handleCallButtonClick = useCallback((): void => {
+		setError('PhoneInput', { message: t('Something_went_wrong_try_again_later') });
+	}, [setError, t]);
+
+	useEffect(() => {
+		setDisabled(!value);
+		clearErrors('PhoneInput');
+	}, [clearErrors, value]);
+
+	useEffect(() => {
+		setFocus('PhoneInput');
+	}, [setFocus]);
+
+	return {
+		inputName: 'PhoneInput',
+		inputValue: value,
+		inputRef: ref,
+		inputError: errors.PhoneInput?.message,
+		isButtonDisabled: disabled,
+		handleInputChange: onChange,
+		handleBackspaceClick,
+		handlePadButtonClick,
+		handleCallButtonClick,
+	};
+};
