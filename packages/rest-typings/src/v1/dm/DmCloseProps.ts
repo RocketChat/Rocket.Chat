b02fc8d0321a646ea5dd17d00231ebdb6f1a8e80
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-06-13 12:59:36 -0300

    ..

--- packages/rest-typings/src/v1/dm/DmCloseProps.ts
+++ packages/rest-typings/src/v1/dm/DmCloseProps.ts
@@ -4,35 +4,19 @@ const ajv = new Ajv({
 	coerceTypes: true,
 });
 
-export type DmCloseProps =
-	| {
-			roomId: string;
-	  }
-	| { roomName: string };
+export type DmCloseProps = {
+	roomId: string;
+};
 
 const DmClosePropsSchema = {
-	oneOf: [
-		{
-			type: 'object',
-			properties: {
-				roomId: {
-					type: 'string',
-				},
-			},
-			required: ['roomId'],
-			additionalProperties: false,
+	type: 'object',
+	properties: {
+		roomId: {
+			type: 'string',
 		},
-		{
-			type: 'object',
-			properties: {
-				roomName: {
-					type: 'string',
-				},
-			},
-			required: ['roomName'],
-			additionalProperties: false,
-		},
-	],
+	},
+	required: ['roomId'],
+	additionalProperties: false,
 };
 
 export const isDmCloseProps = ajv.compile<DmCloseProps>(DmClosePropsSchema);
