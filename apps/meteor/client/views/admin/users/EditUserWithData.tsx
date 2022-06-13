949fcae91975ade218c092d7b4c8cd7675f5c032
Douglas Fabris <devfabris@gmail.com>
2022-06-13 18:02:38 -0300

    Chore: Rewrite Admin UsersTable to Typescript (#25698)
    <!-- This is a pull request template, you do not need to uncomment or remove the comments, they won't show up in the PR text. -->
    
    <!-- Your Pull Request name should start with one of the following tags
      [NEW] For new features
      [IMPROVE] For an improvement (performance or little improvements) in existing features
      [FIX] For bug fixes that affect the end-user
      [BREAK] For pull requests including breaking changes
      Chore: For small tasks
      Doc: For documentation
    -->
    
    <!-- Checklist!!! If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code. 
      - I have read the Contributing Guide - https://github.com/RocketChat/Rocket.Chat/blob/develop/.github/CONTRIBUTING.md#contributing-to-rocketchat doc
      - I have signed the CLA - https://cla-assistant.io/RocketChat/Rocket.Chat
      - Lint and unit tests pass locally with my changes
      - I have added tests that prove my fix is effective or that my feature works (if applicable)
      - I have added necessary documentation (if applicable)
      - Any dependent changes have been merged and published in downstream modules
    -->
    
    ## Proposed changes (including videos or screenshots)
    <!-- CHANGELOG -->
    <!--
      Describe the big picture of your changes here to communicate to the maintainers why we should accept this pull request.
      If it fixes a bug or resolves a feature request, be sure to link to that issue below.
      This description will appear in the release notes if we accept the contribution.
    -->
    
    <!-- END CHANGELOG -->
    
    ## Issue(s)
    <!-- Link the issues being closed by or related to this PR. For example, you can use #594 if this PR closes issue number 594 -->
    
    ## Steps to test or reproduce
    <!-- Mention how you would reproduce the bug if not mentioned on the issue page already. Also mention which screens are going to have the changes if applicable -->
    
    ## Further comments
    <!-- If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc... -->
    
    
    Co-authored-by: Tasso Evangelista <2263066+tassoevan@users.noreply.github.com>

--- apps/meteor/client/views/admin/users/EditUserWithData.tsx
+++ apps/meteor/client/views/admin/users/EditUserWithData.tsx
@@ -1,15 +1,21 @@
+import { IUser } from '@rocket.chat/core-typings';
 import { Box, Callout } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
-import React, { useMemo } from 'react';
+import React, { useMemo, ReactElement } from 'react';
 
 import { FormSkeleton } from '../../../components/Skeleton';
 import { AsyncStatePhase } from '../../../hooks/useAsyncState';
 import { useEndpointData } from '../../../hooks/useEndpointData';
 import EditUser from './EditUser';
 
-function EditUserWithData({ uid, ...props }) {
+type EditUserWithDataProps = {
+	uid: IUser['_id'];
+	onReload: () => void;
+};
+
+const EditUserWithData = ({ uid, onReload, ...props }: EditUserWithDataProps): ReactElement => {
 	const t = useTranslation();
-	const { value: roleData, phase: roleState, error: roleError } = useEndpointData('/v1/roles.list', '');
+	const { value: roleData, phase: roleState, error: roleError } = useEndpointData('/v1/roles.list');
 	const {
 		value: data,
 		phase: state,
@@ -35,7 +41,7 @@ function EditUserWithData({ uid, ...props }) {
 		);
 	}
 
-	return <EditUser data={data.user} roles={roleData.roles} {...props} />;
-}
+	return <EditUser data={data?.user} roles={roleData?.roles} onReload={onReload} {...props} />;
+};
 
 export default EditUserWithData;
---
+++ apps/meteor/client/views/admin/users/UsersTable/UsersTableRow.tsx
@@ -0,0 +1,63 @@
+import { IRole, IUser } from '@rocket.chat/core-typings';
+import { Box, TableRow, TableCell } from '@rocket.chat/fuselage';
+import { capitalize } from '@rocket.chat/string-helpers';
+import { useTranslation, TranslationKey } from '@rocket.chat/ui-contexts';
+import React, { ReactElement } from 'react';
+
+import { Roles } from '../../../../../app/models/client';
+import UserAvatar from '../../../../components/avatar/UserAvatar';
+
+type UsersTableRowProps = {
+	user: Pick<IUser, '_id' | 'username' | 'name' | 'status' | 'roles' | 'emails' | 'active' | 'avatarETag'>;
+	onClick: (id: IUser['_id']) => void;
+	mediaQuery: boolean;
+};
+
+const UsersTableRow = ({ user, onClick, mediaQuery }: UsersTableRowProps): ReactElement => {
+	const t = useTranslation();
+	const { _id, emails, username, name, roles, status, active, avatarETag } = user;
+	const statusText = active ? t(capitalize(status as string) as TranslationKey) : t('Disabled');
+
+	const roleNames = (roles || [])
+		.map((roleId) => (Roles.findOne(roleId, { fields: { name: 1 } }) as IRole | undefined)?.name)
+		.filter((roleName): roleName is string => !!roleName)
+		.join(', ');
+
+	return (
+		<TableRow onKeyDown={(): void => onClick(_id)} onClick={(): void => onClick(_id)} tabIndex={0} role='link' action qa-user-id={_id}>
+			<TableCell withTruncatedText>
+				<Box display='flex' alignItems='center'>
+					{username && <UserAvatar size={mediaQuery ? 'x28' : 'x40'} username={username} etag={avatarETag} />}
+					<Box display='flex' mi='x8' withTruncatedText>
+						<Box display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
+							<Box fontScale='p2m' color='default' withTruncatedText>
+								{name || username}
+							</Box>
+							{!mediaQuery && name && (
+								<Box fontScale='p2' color='hint' withTruncatedText>
+									{' '}
+									{`@${username}`}{' '}
+								</Box>
+							)}
+						</Box>
+					</Box>
+				</Box>
+			</TableCell>
+			{mediaQuery && (
+				<TableCell>
+					<Box fontScale='p2m' color='hint' withTruncatedText>
+						{username}
+					</Box>{' '}
+					<Box mi='x4' />
+				</TableCell>
+			)}
+			<TableCell withTruncatedText>{emails?.length && emails[0].address}</TableCell>
+			{mediaQuery && <TableCell withTruncatedText>{roleNames}</TableCell>}
+			<TableCell fontScale='p2' color='hint' withTruncatedText>
+				{statusText}
+			</TableCell>
+		</TableRow>
+	);
+};
+
+export default UsersTableRow;
