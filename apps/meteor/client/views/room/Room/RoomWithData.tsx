7df62592a2b1e92b451aa9c0050ad52a338d3f50
dougfabris <devfabris@gmail.com>
2022-06-13 17:53:48 -0300

    chore: rewrtie RoomWithData

--- apps/meteor/client/views/room/Room/RoomWithData.tsx
+++ apps/meteor/client/views/room/Room/RoomWithData.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import { useOpenedRoom } from '../../../lib/RoomManager';
 import RoomProvider from '../providers/RoomProvider';
 import { Room } from './Room';
 
-const RoomWithData = () => {
+const RoomWithData = (): ReactElement | null => {
 	const rid = useOpenedRoom();
 	return rid ? (
 		<RoomProvider rid={rid}>
