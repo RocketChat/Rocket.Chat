4543d8881d577b5bff0a74c691a3524c7527f8b1
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2022-06-14 12:31:35 -0300

    wip: add async calls to getAvatarPath

--- apps/meteor/definition/IRoomTypeConfig.ts
+++ apps/meteor/definition/IRoomTypeConfig.ts
@@ -2,6 +2,9 @@ import type { RouteOptions } from 'meteor/kadira:flow-router';
 import type { IRoom, RoomType, IRocketChatRecord, IUser, IMessage, ReadReceipt, ValueOf, AtLeast } from '@rocket.chat/core-typings';
 
 export type RoomIdentification = { rid?: IRoom['_id']; name?: string };
+export type AvatarPathData = AtLeast<IRoom, '_id' | 'name' | 'fname' | 'prid' | 'avatarETag' | 'uids' | 'usernames'> & {
+	username?: IRoom['_id'];
+};
 export interface IRoomTypeRouteConfig {
 	name: string;
 	path?: string;
@@ -67,9 +70,7 @@ export interface IRoomTypeClientDirectives {
 	openCustomProfileTab: (instance: any, room: IRoom, username: string) => boolean;
 	getUiText: (context: ValueOf<typeof UiTextContext>) => string;
 	condition: () => boolean;
-	getAvatarPath: (
-		room: AtLeast<IRoom, '_id' | 'name' | 'fname' | 'prid' | 'avatarETag' | 'uids' | 'usernames'> & { username?: IRoom['_id'] },
-	) => Promise<string>;
+	getAvatarPath: (data: AvatarPathData) => Promise<string>;
 	getIcon: (room: Partial<IRoom>) => IRoomTypeConfig['icon'];
 	getUserStatus: (roomId: string) => string | undefined;
 	findRoom: (identifier: string) => IRoom | undefined;
