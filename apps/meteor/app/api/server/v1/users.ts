7d7400813dc7d11a152ee29e0e1f535a155cf5bf
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-06-15 16:01:17 -0300

    Chore: create Ajv schemas for some users.js endpoints (#25879)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

--- apps/meteor/app/api/server/v1/users.ts
+++ apps/meteor/app/api/server/v1/users.ts
@@ -7,7 +7,11 @@ import {
 	isUserLogoutParamsPOST,
 	isUsersListTeamsProps,
 	isUsersAutocompleteProps,
-} from '@rocket.chat/rest-typings/dist/v1/users';
+	isUsersSetAvatarProps,
+	isUsersUpdateParamsPOST,
+	isUsersUpdateOwnBasicInfoParamsPOST,
+	isUsersSetPreferencesParamsPOST,
+} from '@rocket.chat/rest-typings';
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Match, check } from 'meteor/check';
@@ -24,6 +28,8 @@ import {
 	saveCustomFieldsWithoutValidation,
 	checkUsernameAvailability,
 	setStatusText,
+	setUserAvatar,
+	saveCustomFields,
 } from '../../../lib/server';
 import { getFullUserDataByIdOrUsername } from '../../../lib/server/functions/getFullUserData';
 import { API } from '../api';
@@ -34,6 +40,191 @@ import { resetTOTP } from '../../../2fa/server/functions/resetTOTP';
 import { Team } from '../../../../server/sdk';
 import { isValidQuery } from '../lib/isValidQuery';
 import { setUserStatus } from '../../../../imports/users-presence/server/activeUsers';
+import { getURL } from '../../../utils/server';
+import { getUploadFormData } from '../lib/getUploadFormData';
+
+API.v1.addRoute(
+	'users.getAvatar',
+	{ authRequired: false },
+	{
+		get() {
+			const user = this.getUserFromParams();
+
+			const url = getURL(`/avatar/${user.username}`, { cdn: false, full: true });
+			this.response.setHeader('Location', url);
+
+			return {
+				statusCode: 307,
+				body: url,
+			};
+		},
+	},
+);
+
+API.v1.addRoute(
+	'users.update',
+	{ authRequired: true, twoFactorRequired: true, validateParams: isUsersUpdateParamsPOST },
+	{
+		post() {
+			const userData = { _id: this.bodyParams.userId, ...this.bodyParams.data };
+
+			Meteor.runAsUser(this.userId, () => saveUser(this.userId, userData));
+
+			if (this.bodyParams.data.customFields) {
+				saveCustomFields(this.bodyParams.userId, this.bodyParams.data.customFields);
+			}
+
+			if (typeof this.bodyParams.data.active !== 'undefined') {
+				const {
+					userId,
+					data: { active },
+					confirmRelinquish,
+				} = this.bodyParams;
+
+				Meteor.call('setUserActiveStatus', userId, active, Boolean(confirmRelinquish));
+			}
+			const { fields } = this.parseJsonQuery();
+
+			return API.v1.success({ user: Users.findOneById(this.bodyParams.userId, { fields }) });
+		},
+	},
+);
+
+API.v1.addRoute(
+	'users.updateOwnBasicInfo',
+	{ authRequired: true, validateParams: isUsersUpdateOwnBasicInfoParamsPOST },
+	{
+		post() {
+			const userData = {
+				email: this.bodyParams.data.email,
+				realname: this.bodyParams.data.name,
+				username: this.bodyParams.data.username,
+				nickname: this.bodyParams.data.nickname,
+				statusText: this.bodyParams.data.statusText,
+				newPassword: this.bodyParams.data.newPassword,
+				typedPassword: this.bodyParams.data.currentPassword,
+			};
+
+			// saveUserProfile now uses the default two factor authentication procedures, so we need to provide that
+			const twoFactorOptions = !userData.typedPassword
+				? null
+				: {
+						twoFactorCode: userData.typedPassword,
+						twoFactorMethod: 'password',
+				  };
+
+			Meteor.call('saveUserProfile', userData, this.bodyParams.customFields, twoFactorOptions);
+
+			return API.v1.success({
+				user: Users.findOneById(this.userId, { fields: API.v1.defaultFieldsToExclude }),
+			});
+		},
+	},
+);
+
+API.v1.addRoute(
+	'users.setPreferences',
+	{ authRequired: true, validateParams: isUsersSetPreferencesParamsPOST },
+	{
+		post() {
+			if (this.bodyParams.userId && this.bodyParams.userId !== this.userId && !hasPermission(this.userId, 'edit-other-user-info')) {
+				throw new Meteor.Error('error-action-not-allowed', 'Editing user is not allowed');
+			}
+			const userId = this.bodyParams.userId ? this.bodyParams.userId : this.userId;
+			if (!Users.findOneById(userId)) {
+				throw new Meteor.Error('error-invalid-user', 'The optional "userId" param provided does not match any users');
+			}
+
+			Meteor.runAsUser(userId, () => Meteor.call('saveUserPreferences', this.bodyParams.data));
+			const user = Users.findOneById(userId, {
+				fields: {
+					'settings.preferences': 1,
+					'language': 1,
+				},
+			});
+			return API.v1.success({
+				user: {
+					_id: user._id,
+					settings: {
+						preferences: {
+							...user.settings.preferences,
+							language: user.language,
+						},
+					},
+				},
+			});
+		},
+	},
+);
+
+API.v1.addRoute(
+	'users.setAvatar',
+	{ authRequired: true, validateParams: isUsersSetAvatarProps },
+	{
+		async post() {
+			const canEditOtherUserAvatar = hasPermission(this.userId, 'edit-other-user-avatar');
+
+			if (!settings.get('Accounts_AllowUserAvatarChange') && !canEditOtherUserAvatar) {
+				throw new Meteor.Error('error-not-allowed', 'Change avatar is not allowed', {
+					method: 'users.setAvatar',
+				});
+			}
+
+			let user = ((): IUser | undefined => {
+				if (this.isUserFromParams()) {
+					return Meteor.users.findOne(this.userId) as IUser | undefined;
+				}
+				if (canEditOtherUserAvatar) {
+					return this.getUserFromParams();
+				}
+			})();
+
+			if (!user) {
+				return API.v1.unauthorized();
+			}
+
+			if (this.bodyParams.avatarUrl) {
+				setUserAvatar(user, this.bodyParams.avatarUrl, '', 'url');
+				return API.v1.success();
+			}
+
+			const [image, fields] = await getUploadFormData(
+				{
+					request: this.request,
+				},
+				{
+					field: 'image',
+				},
+			);
+
+			if (!image) {
+				return API.v1.failure("The 'image' param is required");
+			}
+
+			const sentTheUserByFormData = fields.userId || fields.username;
+			if (sentTheUserByFormData) {
+				if (fields.userId) {
+					user = Users.findOneById(fields.userId, { fields: { username: 1 } });
+				} else if (fields.username) {
+					user = Users.findOneByUsernameIgnoringCase(fields.username, { fields: { username: 1 } });
+				}
+
+				if (!user) {
+					throw new Meteor.Error('error-invalid-user', 'The optional "userId" or "username" param provided does not match any users');
+				}
+
+				const isAnotherUser = this.userId !== user._id;
+				if (isAnotherUser && !hasPermission(this.userId, 'edit-other-user-avatar')) {
+					throw new Meteor.Error('error-not-allowed', 'Not allowed');
+				}
+			}
+
+			setUserAvatar(user, image.fileBuffer, image.mimetype, 'rest');
+
+			return API.v1.success();
+		},
+	},
+);
 
 API.v1.addRoute(
 	'users.create',
@@ -593,7 +784,12 @@ API.v1.addRoute(
 	{ authRequired: true },
 	{
 		async post() {
-			const hashedToken = Accounts._hashLoginToken(this.request.headers['x-auth-token']);
+			const xAuthToken = this.request.headers['x-auth-token'] as string;
+
+			if (!xAuthToken) {
+				throw new Meteor.Error('error-parameter-required', 'x-auth-token is required');
+			}
+			const hashedToken = Accounts._hashLoginToken(xAuthToken);
 
 			if (!(await UsersRaw.removeNonPATLoginTokensExcept(this.userId, hashedToken))) {
 				throw new Meteor.Error('error-invalid-user-id', 'Invalid user id');
@@ -607,8 +803,8 @@ API.v1.addRoute(
 			const tokenExpires = new Date(token!.when.getTime() + settings.get<number>('Accounts_LoginExpiration') * 1000);
 
 			return API.v1.success({
-				token: this.request.headers['x-auth-token'],
-				tokenExpires: tokenExpires.toISOString(),
+				token: xAuthToken,
+				tokenExpires: tokenExpires.toISOString() || '',
 			});
 		},
 	},
@@ -874,3 +1070,9 @@ API.v1.addRoute(
 		},
 	},
 );
+
+settings.watch<number>('Rate_Limiter_Limit_RegisterUser', (value) => {
+	const userRegisterRoute = '/api/v1/users.registerpost';
+
+	API.v1.updateRateLimiterDictionaryForRoute(userRegisterRoute, value);
+});
