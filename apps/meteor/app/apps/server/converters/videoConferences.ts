00845232c0f43f3c7366a5192dc1f09edc04c9d4
Pierre Lehnen <pierre.lehnen@rocket.chat>
2022-06-16 02:19:25 -0300

    Removed conflicting name

--- apps/meteor/app/apps/server/converters/videoConferences.ts
+++ apps/meteor/app/apps/server/converters/videoConferences.ts
@@ -1,4 +1,4 @@
-import type { VideoConference as AppVideoConference } from '@rocket.chat/apps-engine/definition/videoConferences';
+import type { VideoConference } from '@rocket.chat/apps-engine/definition/videoConferences';
 import type { IVideoConference } from '@rocket.chat/core-typings';
 
 import { VideoConf } from '../../../../server/sdk';
@@ -12,23 +12,23 @@ export class AppVideoConferencesConverter {
 		this.orch = orch;
 	}
 
-	async convertById(callId: string): Promise<AppVideoConference | undefined> {
+	async convertById(callId: string): Promise<VideoConference | undefined> {
 		const call = await VideoConf.getUnfiltered(callId);
 
 		return this.convertVideoConference(call);
 	}
 
-	convertVideoConference(call: IVideoConference | null): AppVideoConference | undefined {
+	convertVideoConference(call: IVideoConference | null): VideoConference | undefined {
 		if (!call) {
 			return;
 		}
 
 		return {
 			...call,
-		} as AppVideoConference;
+		} as VideoConference;
 	}
 
-	convertAppVideoConference(call: AppVideoConference): IVideoConference {
+	convertAppVideoConference(call: VideoConference): IVideoConference {
 		return {
 			...call,
 		} as IVideoConference;
