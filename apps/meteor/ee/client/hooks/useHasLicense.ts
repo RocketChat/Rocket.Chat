3a52918070729c5567e42f8c9768ceea249c2f5a
gabriellsh <gabriel.henriques@rocket.chat>
2022-06-21 14:44:52 -0300

    convert useHasLicense to TS

--- apps/meteor/ee/client/hooks/useHasLicense.ts
+++ apps/meteor/ee/client/hooks/useHasLicense.ts
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 import { hasLicense } from '../../app/license/client';
 
-export const useHasLicense = (licenseName) => {
-	const [license, setLicense] = useState('loading');
+export const useHasLicense = (licenseName: string): 'loading' | boolean => {
+	const [license, setLicense] = useState<'loading' | boolean>('loading');
 
 	useEffect(() => {
 		hasLicense(licenseName).then((enabled) => {
