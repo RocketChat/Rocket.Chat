6c349e135da3f5479bed349837c7353dbee8ede9
albuquerquefabio <albuquerquefabio@icloud.com>
2022-06-21 17:27:08 -0300

    Device Management email template

--- apps/meteor/ee/server/lib/deviceManagement/session.ts
+++ apps/meteor/ee/server/lib/deviceManagement/session.ts
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { UAParser } from 'ua-parser-js';
 import { ISocketConnection, IUser } from '@rocket.chat/core-typings';
 
@@ -8,6 +9,15 @@ import { settings } from '../../../../app/settings/server';
 import { UAParserDesktop, UAParserMobile } from '../../../../app/statistics/server/lib/UAParserCustom';
 import { sauEvents } from '../../../../server/services/sauMonitor/events';
 import { hasLicense } from '../../../app/license/server/license';
+import { t } from '../../../../app/utils/server';
+
+let mailTemplates: string;
+
+Meteor.startup(() => {
+	Mailer.getTemplate('Device_Management_Email_Body', (template) => {
+		mailTemplates = template;
+	});
+});
 
 const uaParser = async (
 	uaString: ISocketConnection['httpHeaders']['user-agent'],
@@ -38,79 +48,74 @@ export const listenSessionLogin = async (): Promise<void> => {
 			} = user;
 			const { browser, os, device, cpu, app } = await uaParser(connection.httpHeaders['user-agent']);
 
-			let html = `<p><strong>${name} (${username}) logged in via</strong></p>`;
+			let mailData = {};
 
 			switch (device.type) {
 				case 'mobile':
 				case 'tablet':
 				case 'smarttv':
-					html += `
-						<ul>
-							<li><strong>Client:</strong> ${browser.name} ${browser.version}</li>
-							<li><strong>OS:</strong> ${os.name} ${os.version || ''}</li>
-							<li><strong>Device:</strong> ${device.type} ${device.vendor || ''} ${device.model || ''} ${cpu.architecture || ''}</li>
-							<li><strong>IP:</strong> <a href='https://ipinfo.io/${connection.clientAddress}'>${connection.clientAddress}</a></li>
-						</ul>
-					`;
+					mailData = {
+						name,
+						username,
+						browserInfo: `${browser.name} ${browser.version}`,
+						osInfo: `${os.name} ${os.version || ''}`,
+						deviceInfo: `${device.type} ${device.vendor || ''} ${device.model || ''} ${cpu.architecture || ''}`,
+						ipInfo: connection.clientAddress,
+						userAgent: '',
+					};
 					break;
 				case 'mobile-app':
-					html += `
-							<ul>
-								<li><strong>Client:</strong> ${app?.name} ${app?.bundle || app?.version}</li>
-								<li><strong>OS:</strong> ${os.name} ${os.version || ''}</li>
-								<li><strong>Device:</strong> ${device.type}</li>
-								<li><strong>IP:</strong> <a href='https://ipinfo.io/${connection.clientAddress}'>${connection.clientAddress}</a></li>
-							</ul>
-					`;
+					mailData = {
+						name,
+						username,
+						browserInfo: `Rocket.Chat App ${app?.bundle || app?.version}`,
+						osInfo: `${os.name} ${os.version || ''}`,
+						deviceInfo: 'Mobile App',
+						ipInfo: connection.clientAddress,
+						userAgent: '',
+					};
 					break;
 				case 'desktop-app':
-					html += `
-							<ul>
-								<li><strong>Client:</strong> ${app?.name || browser.name} ${app?.bundle || app?.version || browser.version}</li>
-								<li><strong>OS:</strong> ${os.name} ${os.version || ''}</li>
-								<li><strong>Device:</strong> ${device.type} ${cpu.architecture || ''}</li>
-								<li><strong>IP:</strong> <a href='https://ipinfo.io/${connection.clientAddress}'>${connection.clientAddress}</a></li>
-							</ul>
-					`;
+					mailData = {
+						name,
+						username,
+						browserInfo: `Rocket.Chat ${app?.name || browser.name} ${app?.bundle || app?.version || browser.version}`,
+						osInfo: `${os.name} ${os.version || ''}`,
+						deviceInfo: `Desktop App ${cpu.architecture || ''}`,
+						ipInfo: connection.clientAddress,
+						userAgent: '',
+					};
 					break;
 				default:
-					html += `
-						<ul>
-							<li><strong>Client:</strong> ${browser.name} ${browser.version}</li>
-							<li><strong>OS:</strong> ${os.name} ${os.version || ''}</li>
-							<li><strong>Device:</strong> ${device.type || 'UNKNOWN'} ${device.vendor || ''} ${device.model || ''} ${cpu.architecture || ''}</li>
-							<li>
-								<strong>IP:</strong> 
-								<a href='https://ipinfo.io/${connection.clientAddress}'>${connection.clientAddress}</a>
-							</li>
-						</ul>
-						<small>${connection.httpHeaders['user-agent']}</small>
-					`;
+					mailData = {
+						name,
+						username,
+						browserInfo: `${browser.name} ${browser.version}`,
+						osInfo: `${os.name} ${os.version || ''}`,
+						deviceInfo: `${device.type || t('Device_Management_Device_Unknown')} ${device.vendor || ''} ${device.model || ''} ${
+							cpu.architecture || ''
+						}`,
+						ipInfo: connection.clientAddress,
+						userAgent: connection.httpHeaders['user-agent'],
+					};
+
 					break;
 			}
 
-			html += `
-				<br>
-				<p style="text-align: center;">
-					<a href='${settings.get('Site_Url')}'>Click here to access your account.</a>
-				</p>
-			`;
-
-			Meteor.defer(() => {
-				try {
-					Mailer.send({
-						to: `${name} <${email}>`,
-						from: `Rocket.Chat <${settings.get('From_Email')}>`,
-						subject: 'Login started',
-						html,
-					});
-				} catch ({ message }) {
-					throw new Meteor.Error('error-email-send-failed', `Error trying to send email: ${message}`, {
-						method: 'listenSessionLogin',
-						message,
-					});
-				}
-			});
+			try {
+				Mailer.send({
+					to: `${name} <${email}>`,
+					from: Accounts.emailTemplates.from,
+					subject: settings.get('Device_Management_Email_Subject'),
+					html: mailTemplates,
+					data: mailData,
+				});
+			} catch ({ message }) {
+				throw new Meteor.Error('error-email-send-failed', `Error trying to send email: ${message}`, {
+					method: 'listenSessionLogin',
+					message,
+				});
+			}
 		}
 	});
 };
