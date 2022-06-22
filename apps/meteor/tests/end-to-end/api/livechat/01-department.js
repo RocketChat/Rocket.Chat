d0c4fa0686f71572328a5795247b180859ebee75
Kevin Aleman <kaleman960@gmail.com>
2022-06-22 10:37:18 -0600

    cant belive

--- apps/meteor/tests/end-to-end/api/livechat/01-department.js
+++ apps/meteor/tests/end-to-end/api/livechat/01-department.js
@@ -29,7 +29,7 @@ describe('LIVECHAT - departments', function () {
 						.get(api('livechat/department'))
 						.set(credentials)
 						.expect('Content-Type', 'application/json')
-						.expect(400)
+						.expect(403)
 						.expect((res) => {
 							expect(res.body).to.have.property('success', false);
 							expect(res.body.error).to.be.equal('error-not-authorized');
