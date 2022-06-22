47fc14b93067d068b4471656f6f624abb1f77f4d
Hugo Costa <hugocarreiracosta@gmail.com>
2022-06-22 13:17:11 -0300

    fix: Add HomePageHeader to CustomHomePage

--- apps/meteor/client/views/home/CustomHomePage.tsx
+++ apps/meteor/client/views/home/CustomHomePage.tsx
@@ -3,16 +3,15 @@ import { useSetting } from '@rocket.chat/ui-contexts';
 import React, { ReactElement } from 'react';
 
 import Page from '../../components/Page/Page';
-import PageHeader from '../../components/Page/PageHeader';
 import PageScrollableContentWithShadow from '../../components/Page/PageScrollableContentWithShadow';
+import HomePageHeader from './HomePageHeader';
 
 const CustomHomePage = (): ReactElement => {
-	const title = useSetting('Layout_Home_Title') as string;
 	const body = useSetting('Layout_Home_Body') as string;
 
 	return (
 		<Page data-qa='page-home' data-qa-type='custom'>
-			<PageHeader title={title} />
+			<HomePageHeader />
 			<PageScrollableContentWithShadow>
 				<Box withRichContent dangerouslySetInnerHTML={{ __html: body }} />
 			</PageScrollableContentWithShadow>
