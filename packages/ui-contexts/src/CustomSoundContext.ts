b36e95515a1bf03ef8537e1ea838d077fb1adf02
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-06-23 13:57:09 -0300

    ..

--- packages/ui-contexts/src/CustomSoundContext.ts
+++ packages/ui-contexts/src/CustomSoundContext.ts
@@ -3,7 +3,13 @@ import { createContext } from 'react';
 
 export type CustomSoundContextValue = {
 	play: (sound: string, options?: { volume?: number; loop?: boolean }) => void;
-	getList: () => ICustomSound[] | undefined;
+	getList: () => {
+		_id: string;
+		name: string;
+		extension: string;
+		src: string;
+		random?: string;
+	}[];
 };
 
 export const CustomSoundContext = createContext<CustomSoundContextValue | undefined>(undefined);
