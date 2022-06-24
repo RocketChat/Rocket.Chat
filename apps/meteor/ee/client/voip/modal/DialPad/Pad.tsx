4e81a9dc9fcf3d207c330513e401770952c444c5
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-06-24 16:20:38 -0300

    0

--- apps/meteor/ee/client/voip/modal/DialPad/Pad.tsx
+++ apps/meteor/ee/client/voip/modal/DialPad/Pad.tsx
@@ -5,18 +5,14 @@ import PadButton from './PadButton';
 
 const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, '*', 0, '#'];
 
-const Pad = ({ onClickPadButton }: { onClickPadButton: (digit: string | number) => void }): ReactElement => {
-	0;
-
-	return (
-		<Box display='flex' flexWrap='wrap' m='-8px' mbs='28px'>
-			{digits.map((digit, idx) => (
-				<PadButton key={idx} onClickPadButton={onClickPadButton}>
-					{digit}
-				</PadButton>
-			))}
-		</Box>
-	);
-};
+const Pad = ({ onClickPadButton }: { onClickPadButton: (digit: string | number) => void }): ReactElement => (
+	<Box display='flex' flexWrap='wrap' m='-8px' mbs='28px'>
+		{digits.map((digit, idx) => (
+			<PadButton key={idx} onClickPadButton={onClickPadButton}>
+				{digit}
+			</PadButton>
+		))}
+	</Box>
+);
 
 export default Pad;
