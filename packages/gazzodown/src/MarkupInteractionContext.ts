4969a9d1b1e7c691c8f4cbd22e51af428ceaf98b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-06-24 19:21:49 -0300

    Add events for task lists

--- packages/gazzodown/src/MarkupInteractionContext.ts
+++ packages/gazzodown/src/MarkupInteractionContext.ts
@@ -1,5 +1,6 @@
 import type { IRoom, IUser } from '@rocket.chat/core-typings';
-import { createContext, UIEvent } from 'react';
+import type * as MessageParser from '@rocket.chat/message-parser';
+import { createContext, FormEvent, UIEvent } from 'react';
 
 export type UserMention = Pick<IUser, '_id' | 'username' | 'name'>;
 export type ChannelMention = Pick<IRoom, '_id' | 'name'>;
@@ -7,6 +8,7 @@ export type ChannelMention = Pick<IRoom, '_id' | 'name'>;
 type MarkupInteractionContextValue = {
 	detectEmoji?: (text: string) => { name: string; className: string; image?: string; content: string }[];
 	highlightRegex?: () => RegExp;
+	onTaskChecked?: (task: MessageParser.Task) => ((e: FormEvent) => void) | undefined;
 	resolveUserMention?: (mention: string) => UserMention | undefined;
 	onUserMentionClick?: (mentionedUser: UserMention) => ((e: UIEvent) => void) | undefined;
 	resolveChannelMention?: (mention: string) => ChannelMention | undefined;
