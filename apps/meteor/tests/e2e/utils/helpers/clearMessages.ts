74649a3c90f9f7c0711bbf36c0d8957ea1c6c689
Ramon Souza <ramon.souza@objective.com.br>
2022-06-25 23:05:14 -0300

    refactor: remove clearMessage.ts

--- apps/meteor/tests/e2e/utils/helpers/clearMessages.ts
+++
@@ -1,9 +0,0 @@
-import { MongoHelper } from './MongoHelper';
-import { URL_MONGODB } from '../constants';
-
-export const clearMessages = async (channelIds: string[]): Promise<void> => {
-	await MongoHelper.connect(URL_MONGODB);
-	const messageCollection = await MongoHelper.getCollection('rocketchat_message');
-	await messageCollection.deleteMany({ rid: { $in: channelIds } });
-	await MongoHelper.disconnect();
-};
