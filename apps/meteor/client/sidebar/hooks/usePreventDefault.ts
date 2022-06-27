7b417b47fb3074e56d56659e9280b82f19df1443
juliajforesti <juliajforesti@gmail.com>
2022-06-27 16:49:29 -0300

    convert usePreventDefault, useQueryOptions, useShortcutOpenMenu

--- apps/meteor/client/sidebar/hooks/usePreventDefault.ts
+++ apps/meteor/client/sidebar/hooks/usePreventDefault.ts
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
+import { RefObject, useEffect } from 'react';
 
-export const usePreventDefault = (ref) => {
+export const usePreventDefault = (ref: RefObject<HTMLElement>): { ref: RefObject<HTMLElement> } => {
 	// Flowrouter uses an addEventListener on the document to capture any clink link, since the react synthetic event use an addEventListener on the document too,
 	// it is impossible/hard to determine which one will happen before and prevent/stop propagation, so feel free to remove this effect after remove flow router :)
 
 	useEffect(() => {
 		const { current } = ref;
-		const stopPropagation = (e) => {
-			if ([e.target.nodeName, e.target.parentElement.nodeName].includes('BUTTON')) {
+		const stopPropagation: EventListener = (e) => {
+			if ([(e.target as HTMLElement).nodeName, (e.target as HTMLElement).parentElement?.nodeName].includes('BUTTON')) {
 				e.preventDefault();
 			}
 		};
 		current?.addEventListener('click', stopPropagation);
 
-		return () => current?.addEventListener('click', stopPropagation);
+		return (): void => current?.addEventListener('click', stopPropagation);
 	}, [ref]);
 
 	return { ref };
