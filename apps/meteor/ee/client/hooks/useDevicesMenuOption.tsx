a1de57c75b87abd8b4e508ee06ee507d0ac11aea
Martin <martin.schoeler@rocket.chat>
2022-06-27 16:59:05 -0300

    Fix icon && translation on option

--- apps/meteor/ee/client/hooks/useDevicesMenuOption.tsx
+++ apps/meteor/ee/client/hooks/useDevicesMenuOption.tsx
@@ -20,7 +20,7 @@ export const useDevicesMenuOption = (): DevicesMenuOption | null => {
 	const option = {
 		label: (
 			<Box alignItems='center' display='flex'>
-				<Icon mie='x4' name='settings' size='x16' />
+				<Icon mie='x4' name='customize' size='x16' />
 				{t('Device_settings')}
 			</Box>
 		),
