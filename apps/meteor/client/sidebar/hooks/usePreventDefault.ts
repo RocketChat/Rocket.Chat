e13ce6ff6b91590e3c08ec13c8c0948787cd86ef
juliajforesti <juliajforesti@gmail.com>
2022-06-27 19:32:25 -0300

    fix

--- apps/meteor/client/sidebar/hooks/usePreventDefault.ts
+++ apps/meteor/client/sidebar/hooks/usePreventDefault.ts
@@ -1,6 +1,6 @@
 import { RefObject, useEffect } from 'react';
 
-export const usePreventDefault = (ref: RefObject<HTMLElement>): { ref: RefObject<HTMLElement> } => {
+export const usePreventDefault = (ref: RefObject<Element>): { ref: RefObject<Element> } => {
 	// Flowrouter uses an addEventListener on the document to capture any clink link, since the react synthetic event use an addEventListener on the document too,
 	// it is impossible/hard to determine which one will happen before and prevent/stop propagation, so feel free to remove this effect after remove flow router :)
 
--- apps/meteor/client/sidebar/hooks/useShortcutOpenMenu.ts
+++ apps/meteor/client/sidebar/hooks/useShortcutOpenMenu.ts
@@ -2,7 +2,7 @@ import { RefObject, useEffect } from 'react';
 import tinykeys from 'tinykeys';
 
 // used to open the menu option by keyboard
-export const useShortcutOpenMenu = (ref: RefObject<HTMLElement>): void => {
+export const useShortcutOpenMenu = (ref: RefObject<Element>): void => {
 	useEffect(() => {
 		const unsubscribe = tinykeys(ref.current as HTMLElement, {
 			Alt: (event) => {
