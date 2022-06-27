d002d6f9c047ffdf9572eb157d853f74abb976f9
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-06-27 10:10:56 -0300

    lint

--- packages/rest-typings/src/v1/autotranslate/AutotranslateSaveSettingsParamsPOST.ts
+++ packages/rest-typings/src/v1/autotranslate/AutotranslateSaveSettingsParamsPOST.ts
@@ -21,7 +21,7 @@ const AutotranslateSaveSettingsParamsPostSchema = {
 			enum: ['autoTranslate', 'autoTranslateLanguage'],
 		},
 		value: {
-			"type": ["boolean", "string"]
+			type: ['boolean', 'string'],
 		},
 		defaultLanguage: {
 			type: 'string',
