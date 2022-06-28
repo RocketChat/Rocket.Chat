897d02601fc4b9afe9b245219d018cfef9499d8d
albuquerquefabio <albuquerquefabio@icloud.com>
2022-06-28 00:32:47 -0300

    Remove unnecessary permission

--- apps/meteor/ee/server/lib/modalManagement/startup.ts
+++
@@ -1,5 +0,0 @@
-import { Permissions } from '../../../../app/models/server/raw';
-
-export const createPermissions = async (): Promise<void> => {
-	Permissions.create('view-modal-management', ['admin']);
-};
