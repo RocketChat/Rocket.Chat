3232ef34082c242182494eb1898742a7b1300a6a
Diego Sampaio <chinello@gmail.com>
2022-06-28 09:12:14 -0300

    fix permissions' raw  model

--- apps/meteor/server/models/raw/Permissions.ts
+++ apps/meteor/server/models/raw/Permissions.ts
@@ -16,35 +16,38 @@ export class PermissionsRaw extends BaseRaw<IPermission> implements IPermissions
 				_id: name,
 				roles,
 			},
-			{ fields: { _id: 1 } },
+			{ projection: { _id: 1 } },
 		);
 
 		if (exists) {
 			return exists._id;
 		}
 
-		return this.update({ _id: name }, { $set: { roles } }, { upsert: true }).then((result) => result.result._id);
+		await this.updateOne({ _id: name }, { $set: { roles } }, { upsert: true });
+
+		return name;
 	}
 
 	async create(id: string, roles: IRole['_id'][]): Promise<IPermission['_id']> {
-		const exists = await this.findOneById<Pick<IPermission, '_id'>>(id, { fields: { _id: 1 } });
-
+		const exists = await this.findOneById<Pick<IPermission, '_id'>>(id, { projection: { _id: 1 } });
 		if (exists) {
 			return exists._id;
 		}
 
-		return this.update({ _id: id }, { $set: { roles } }, { upsert: true }).then((result) => result.result._id);
+		await this.updateOne({ _id: id }, { $set: { roles } }, { upsert: true });
+
+		return id;
 	}
 
 	async addRole(permission: string, role: IRole['_id']): Promise<void> {
-		await this.update({ _id: permission, roles: { $ne: role } }, { $addToSet: { roles: role } });
+		await this.updateOne({ _id: permission, roles: { $ne: role } }, { $addToSet: { roles: role } });
 	}
 
 	async setRoles(permission: string, roles: IRole['_id'][]): Promise<void> {
-		await this.update({ _id: permission }, { $set: { roles } });
+		await this.updateOne({ _id: permission }, { $set: { roles } });
 	}
 
 	async removeRole(permission: string, role: IRole['_id']): Promise<void> {
-		await this.update({ _id: permission, roles: role }, { $pull: { roles: role } });
+		await this.updateOne({ _id: permission, roles: role }, { $pull: { roles: role } });
 	}
 }
