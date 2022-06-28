f8e72311e41ad854f92ff8d9b497ea4c8a26d0e8
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-06-28 00:56:14 -0300

    ts fix

--- apps/meteor/app/lib/server/startup/settingsOnLoadDirectReply.ts
+++ apps/meteor/app/lib/server/startup/settingsOnLoadDirectReply.ts
@@ -1,10 +1,9 @@
 import _ from 'underscore';
 
-import { settings } from '../../../settings';
+import { settings } from '../../../settings/server';
 import { DirectReplyIMAPInterceptor, POP3Helper } from '../lib/interceptDirectReplyEmails.js';
 
-let client;
-
+let client: DirectReplyIMAPInterceptor | POP3Helper | undefined;
 const startEmailIntercepter = _.debounce(async function () {
 	console.log('Email Intercepter...');
 	const protocol = settings.get('Direct_Reply_Protocol');
@@ -16,7 +15,10 @@ const startEmailIntercepter = _.debounce(async function () {
 		settings.get('Direct_Reply_Port') &&
 		settings.get('Direct_Reply_Username') &&
 		settings.get('Direct_Reply_Password');
-	await new Promise((resolve) => (client ? client.stop(resolve) : resolve()));
+
+	if (client) {
+		await client.stop();
+	}
 
 	if (!isEnabled) {
 		console.log('Email Intercepter Stopped...');
@@ -26,13 +28,15 @@ const startEmailIntercepter = _.debounce(async function () {
 
 	if (protocol === 'IMAP') {
 		client = new DirectReplyIMAPInterceptor();
+		client.start();
 	}
+
 	if (protocol === 'POP') {
 		client = new POP3Helper(settings.get('Direct_Reply_Frequency'));
+		client.start();
 	}
-	client.start();
 }, 1000);
 
-settings.onload(/^Direct_Reply_.+/, startEmailIntercepter);
+settings.watchByRegex(/^Direct_Reply_.+/, startEmailIntercepter);
 
 startEmailIntercepter();
