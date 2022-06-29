dd68bf43a49d118b3fd3d555e38bb81f9dabedc4
juliajforesti <juliajforesti@gmail.com>
2022-06-29 15:44:04 -0300

    remove commented function

--- apps/meteor/client/sidebar/hooks/useSidebarPaletteColor.ts
+++ apps/meteor/client/sidebar/hooks/useSidebarPaletteColor.ts
@@ -52,43 +52,6 @@ const getStyleTag = (): HTMLStyleElement | HTMLElement => {
 	return newElement;
 };
 
-// function lightenDarkenColor(col: string, amt: number): string {
-// 	let usePound = false;
-
-// 	if (col[0] === '#') {
-// 		col = col.slice(1);
-// 		usePound = true;
-// 	}
-
-// 	const num = parseInt(col, 16);
-
-// 	let r = (num >> 16) + amt;
-
-// 	if (r > 255) {
-// 		r = 255;
-// 	} else if (r < 0) {
-// 		r = 0;
-// 	}
-
-// 	let b = ((num >> 8) & 0x00ff) + amt;
-
-// 	if (b > 255) {
-// 		b = 255;
-// 	} else if (b < 0) {
-// 		b = 0;
-// 	}
-
-// 	let g = (num & 0x0000ff) + amt;
-
-// 	if (g > 255) {
-// 		g = 255;
-// 	} else if (g < 0) {
-// 		g = 0;
-// 	}
-
-// 	return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
-// }
-
 function h2r(hex: string, a: any): string {
 	const [hash, r, g, b] = hex.match(/#([0-f]{2})([0-f]{2})([0-f]{2})/i) || [];
 
