51ce967647adc378046d647fdf5fdf3e0e733144
Cauê Felchar <11652381+cauefcr@users.noreply.github.com>
2022-06-28 23:41:39 -0300

    [IMPROVE] VoIP admin page cleanup: remove unused settings (#25993)
    <!-- This is a pull request template, you do not need to uncomment or remove the comments, they won't show up in the PR text. -->
    
    <!-- Your Pull Request name should start with one of the following tags
      [NEW] For new features
      [IMPROVE] For an improvement (performance or little improvements) in existing features
      [FIX] For bug fixes that affect the end-user
      [BREAK] For pull requests including breaking changes
      Chore: For small tasks
      Doc: For documentation
    -->
    
    <!-- Checklist!!! If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code. 
      - I have read the Contributing Guide - https://github.com/RocketChat/Rocket.Chat/blob/develop/.github/CONTRIBUTING.md#contributing-to-rocketchat doc
      - I have signed the CLA - https://cla-assistant.io/RocketChat/Rocket.Chat
      - Lint and unit tests pass locally with my changes
      - I have added tests that prove my fix is effective or that my feature works (if applicable)
      - I have added necessary documentation (if applicable)
      - Any dependent changes have been merged and published in downstream modules
    -->
    
    ## Proposed changes (including videos or screenshots)
    <!-- CHANGELOG -->
    <!--
      Describe the big picture of your changes here to communicate to the maintainers why we should accept this pull request.
      If it fixes a bug or resolves a feature request, be sure to link to that issue below.
      This description will appear in the release notes if we accept the contribution.
    -->
    https://app.clickup.com/t/2n4m61m
    <!-- END CHANGELOG -->
    
    ## Issue(s)
    <!-- Link the issues being closed by or related to this PR. For example, you can use #594 if this PR closes issue number 594 -->
    
    ## Steps to test or reproduce
    <!-- Mention how you would reproduce the bug if not mentioned on the issue page already. Also mention which screens are going to have the changes if applicable -->
    
    ## Further comments
    <!-- If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc... -->
    
    
    Co-authored-by: Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
    Co-authored-by: Kevin Aleman <11577696+KevLehman@users.noreply.github.com>

--- apps/meteor/server/services/voip/lib/Helper.ts
+++ apps/meteor/server/services/voip/lib/Helper.ts
@@ -10,7 +10,6 @@ export function getServerConfigDataFromSettings(type: ServerType): IVoipCallServ
 				host: settings.get<string>('VoIP_Server_Host'),
 				name: settings.get<string>('VoIP_Server_Name'),
 				configData: {
-					websocketPort: Number(settings.get<number>('VoIP_Server_Websocket_Port')),
 					websocketPath: settings.get<string>('VoIP_Server_Websocket_Path'),
 				},
 			};
