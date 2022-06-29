4918fbb795562a594d93dba6b566bd0119ff0e11
LucianoPierdona <lucianompj1@gmail.com>
2022-06-29 16:58:15 -0300

    add migration `v273` to remove `null` or `undefined` fields from messages

---
+++ apps/meteor/server/startup/migrations/v273.ts
@@ -0,0 +1,31 @@
+import { Messages } from "@rocket.chat/models";
+
+import { addMigration } from "/server/lib/migrations";
+
+
+addMigration({
+	version: 273,
+	async up() {
+        const fieldsToUpdate = ['editedBy', 'editedAt', 'emoji', 'avatar', 'alias', 'customFields', 'groupable', 'attachments', 'reactions'];
+
+        fieldsToUpdate.map(async (field) => {
+            await Messages.update(
+                {
+                    [field]: {
+                        $in: [null, undefined],
+                    },
+                },
+                {
+                    $unset: {
+                        [field]: '',
+                    },
+                },
+                {
+                    multi: true,
+                },
+            );
+        });
+        
+        await Promise.all(fieldsToUpdate);
+	},
+});
