3f220026febea553b597d58935e43a1666408c15
Diego Sampaio <chinello@gmail.com>
2022-06-30 18:16:17 -0300

    Remove Modal models

--- packages/core-typings/src/IModalDismiss.ts
+++
@@ -1,8 +0,0 @@
-import type { IModal } from './IModal';
-import type { IUser } from './IUser';
-
-export interface IModalDismiss {
-	modalId: IModal['_id'];
-	userId: IUser['_id'];
-	createdAt: Date;
-}
