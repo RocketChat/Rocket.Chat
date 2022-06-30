c58d52be45370ce51d2d57ebc28baa8807ade469
LucianoPierdona <lucianompj1@gmail.com>
2022-06-30 14:21:18 -0300

    updates to migration `v274`

--- apps/meteor/server/startup/migrations/v274.ts
+++ apps/meteor/server/startup/migrations/v274.ts
@@ -3,7 +3,7 @@ import { Messages } from '@rocket.chat/models';
 import { addMigration } from '../../lib/migrations';
 
 addMigration({
-	version: 273,
+	version: 274,
 	async up() {
 		const fieldsToUpdate = ['editedBy', 'editedAt', 'emoji', 'avatar', 'alias', 'customFields', 'groupable', 'attachments', 'reactions'];
 
