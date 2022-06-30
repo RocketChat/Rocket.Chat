036a7c939ea312ca1b74c62ce288309b69778420
Weslley de Campos <weslley.campos@objective.com.br>
2022-06-30 13:28:24 -0300

    refactor: adjust path do drag and drop file

--- apps/meteor/tests/e2e/pageobjects/MainContent.ts
+++ apps/meteor/tests/e2e/pageobjects/MainContent.ts
@@ -231,7 +231,7 @@ export class MainContent extends BasePage {
 	}
 
 	async dragAndDropFile(): Promise<void> {
-		const contract = await fs.promises.readFile('./tests/e2e/utils/fixtures/any_file.txt', 'utf-8');
+		const contract = await fs.promises.readFile('./tests/e2e/fixtures/any_file.txt', 'utf-8');
 
 		const dataTransfer = await this.page.evaluateHandle((contract) => {
 			const data = new DataTransfer();
