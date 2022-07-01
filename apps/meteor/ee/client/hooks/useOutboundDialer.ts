040d6e8d4a53efa52c74f15d729b8931d26058b0
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2022-07-01 16:25:37 -0300

    [FIX] Fixed useOutboundDialer using incorrect hook

--- apps/meteor/ee/client/hooks/useOutboundDialer.ts
+++ apps/meteor/ee/client/hooks/useOutboundDialer.ts
@@ -1,10 +1,9 @@
-import { useCallClient } from '../../../client/contexts/CallContext';
+import { useCallClient, useIsVoipEnterprise } from '../../../client/contexts/CallContext';
 import { EEVoipClient } from '../lib/voip/EEVoipClient';
-import { useHasLicense } from './useHasLicense';
 
 export const useOutboundDialer = (): EEVoipClient | null => {
 	const voipClient = useCallClient();
-	const isEnterprise = useHasLicense('voip-enterprise') === true;
+	const isEnterprise = useIsVoipEnterprise();
 	const isOutboundClient = voipClient instanceof EEVoipClient;
 
 	return isEnterprise && isOutboundClient ? voipClient : null;
