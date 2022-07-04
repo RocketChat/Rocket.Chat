d544ca9dd40f201dfdf781fcc896f0f2317f89f4
Diego Sampaio <chinello@gmail.com>
2022-07-04 15:28:01 -0300

    fix MONGO_URL on e2e tests

--- apps/meteor/tests/e2e/utils/constants.ts
+++ apps/meteor/tests/e2e/utils/constants.ts
@@ -5,5 +5,5 @@ export const IS_LOCALHOST = BASE_URL.startsWith('http://localhost');
 export const IS_EE = Boolean(process.env.IS_EE);
 
 export const URL_MONGODB = process.env.MONGO_URL
-	? `${process.env.MONGO_URL}?retryWrites=false`
+	? `${process.env.MONGO_URL}${process.env.MONGO_URL.includes('?') ? '&' : '?'}retryWrites=false`
 	: 'mongodb://localhost:3001/meteor?retryWrites=false';
