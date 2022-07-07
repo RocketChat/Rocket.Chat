978b5eab94b36ae24c57b655ea3385f2b4e29f6e
Fabio Surrage <fabiosurrage@gmail.com>
2022-07-07 10:17:57 -0300

    Add keyboard navigation in CofirmationModal

--- packages/livechat/src/components/Modal/component.js
+++ packages/livechat/src/components/Modal/component.js
@@ -62,27 +62,86 @@ export class Modal extends Component {
 
 
 export const ModalMessage = ({ children }) => (
-	<div className={createClassName(styles, 'modal__message')}>
+	<div className={createClassName(styles, 'modal__message')} id='chat-confirmation-modal'>
 		{children}
 	</div>
 );
 
 
-export const ConfirmationModal = withTranslation()(({
-	text,
-	confirmButtonText,
-	cancelButtonText,
-	onConfirm,
-	onCancel,
-	t,
-	...props
-}) => <Modal open animated dismissByOverlay={false} {...props}>
-	<Modal.Message>{text}</Modal.Message>
-	<ButtonGroup>
-		<Button outline secondary onClick={onCancel}>{cancelButtonText || t('no')}</Button>
-		<Button secondaryDanger onClick={onConfirm}>{confirmButtonText || t('yes')}</Button>
-	</ButtonGroup>
-</Modal>);
+export class ConfirmationModal extends Component {
+	handleRef = (ref) => {
+		this.confirmationModalRef = ref;
+	}
+
+	getFocusableElements = () => this.confirmationModalRef.base.querySelectorAll(
+		'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select',
+	)
+
+	handleKeyDown = (e) => {
+		const { key } = e;
+
+		switch (key) {
+			case 'Tab':
+				this.handleTabKey(e);
+				break;
+			case 'Escape':
+				this.props.onCancel();
+				break;
+			default:
+				break;
+		}
+
+		e.stopPropagation();
+	}
+
+	handleTabKey = (e) => {
+		const focusableElements = this.getFocusableElements();
+
+		if (focusableElements.length > 0) {
+			const firstElement = focusableElements[0];
+			const lastElement = focusableElements[focusableElements.length - 1];
+
+			if (focusableElements.length === 1) {
+				firstElement.focus();
+				return e.preventDefault();
+			}
+
+			if (!e.shiftKey && document.activeElement !== firstElement) {
+				firstElement.focus();
+				return e.preventDefault();
+			}
+
+			if (e.shiftKey && document.activeElement !== lastElement) {
+				lastElement.focus();
+				return e.preventDefault();
+			}
+		}
+	};
+
+	componentDidMount() {
+		const focusableElements = this.getFocusableElements();
+		focusableElements[0].focus();
+	}
+
+	render = ({
+
+		text,
+		confirmButtonText,
+		cancelButtonText,
+		onConfirm,
+		onCancel,
+		t,
+		...props
+	}) => (
+	    <Modal open animated dismissByOverlay={false} onkeydown={this.handleKeyDown} ref={this.handleRef} role='dialog' aria-describedby='chat-confirmation-modal' {...props}>
+			<Modal.Message>{text}</Modal.Message>
+			<ButtonGroup>
+				<Button outline secondary onClick={onCancel}>{cancelButtonText || t('no')}</Button>
+				<Button secondaryDanger onClick={onConfirm}>{confirmButtonText || t('yes')}</Button>
+			</ButtonGroup>
+		</Modal>
+	)
+}
 
 export const AlertModal = withTranslation()(({ text, buttonText, onConfirm, t, ...props }) => (
 	<Modal open animated dismissByOverlay={false} {...props}>
