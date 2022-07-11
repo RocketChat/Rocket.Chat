6136e4c0a8fe5eda4b1d7112d6ece042f8c56341
Diego Sampaio <chinello@gmail.com>
2022-07-11 20:22:58 -0300

    Regression: Meteor uses projection for its observes (#26223)

--- apps/meteor/server/services/meteor/service.ts
+++ apps/meteor/server/services/meteor/service.ts
@@ -51,7 +51,10 @@ if (disableOplog) {
 		}: {
 			collectionName: string;
 			selector: Record<string, any>;
-			options?: { fields?: Record<string, number> };
+			options?: {
+				projection?: Record<string, number>;
+				fields?: Record<string, number>;
+			};
 		},
 		_ordered: boolean,
 		callbacks: Callbacks,
@@ -60,7 +63,14 @@ if (disableOplog) {
 		let cbs: Set<{ hashedToken: string; callbacks: Callbacks }>;
 		let data: { hashedToken: string; callbacks: Callbacks };
 		if (callbacks?.added) {
-			const records = Promise.await(mongo.rawCollection(collectionName).find(selector, { projection: options.fields }).toArray());
+			const records = Promise.await(
+				mongo
+					.rawCollection(collectionName)
+					.find(selector, {
+						...(options.projection || options.fields ? { projection: options.projection || options.fields } : {}),
+					})
+					.toArray(),
+			);
 			for (const { _id, ...fields } of records) {
 				callbacks.added(String(_id), fields);
 			}
