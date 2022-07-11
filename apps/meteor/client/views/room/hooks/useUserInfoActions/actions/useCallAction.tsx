fd75c7a1574bb115c0ec2fc7a24eb6a38e1a11ed
dougfabris <devfabris@gmail.com>
2022-07-11 14:56:06 -0300

    break: remove webRTC ui

---
+++ apps/meteor/client/views/room/hooks/useUserInfoActions/actions/useCallAction.tsx
@@ -0,0 +1,47 @@
+import { IUser, isRoomFederated } from '@rocket.chat/core-typings';
+import { useTranslation, useUserRoom, useUserId, useUserSubscriptionByName } from '@rocket.chat/ui-contexts';
+import { useMemo } from 'react';
+
+import { closeUserCard } from '../../../../../../app/ui/client/lib/UserCard';
+import { useVideoConfDispatchOutgoing, useVideoConfIsCalling, useVideoConfIsRinging } from '../../../../../contexts/VideoConfContext';
+import { VideoConfManager } from '../../../../../lib/VideoConfManager';
+import { Action } from '../../../../hooks/useActionSpread';
+import { useVideoConfWarning } from '../../../contextualBar/VideoConference/useVideoConfWarning';
+
+export const useCallAction = (user: Pick<IUser, '_id' | 'username'>): Action | undefined => {
+	const t = useTranslation();
+	const usernameSubscription = useUserSubscriptionByName(user.username ?? '');
+	const room = useUserRoom(usernameSubscription?.rid || '');
+
+	const dispatchWarning = useVideoConfWarning();
+	const dispatchPopup = useVideoConfDispatchOutgoing();
+	const isCalling = useVideoConfIsCalling();
+	const isRinging = useVideoConfIsRinging();
+	const ownUserId = useUserId();
+
+	const videoCallOption = useMemo(() => {
+		const action = async (): Promise<void> => {
+			if (isCalling || isRinging || !room) {
+				return;
+			}
+
+			try {
+				await VideoConfManager.loadCapabilities();
+				closeUserCard();
+				dispatchPopup({ rid: room._id });
+			} catch (error: any) {
+				dispatchWarning(error.error);
+			}
+		};
+
+		return room && !isRoomFederated(room) && user._id !== ownUserId
+			? {
+					label: t('Start_call'),
+					icon: 'phone',
+					action,
+			  }
+			: undefined;
+	}, [t, room, dispatchPopup, dispatchWarning, isCalling, isRinging, ownUserId, user._id]);
+
+	return videoCallOption;
+};
