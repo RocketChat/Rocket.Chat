ceb446f6191978f0e9014da71d80adc2754cc37d
guijun13 <guilhermejun13.grillo@gmail.com>
2022-07-13 15:47:40 -0300

    remove box component

--- apps/meteor/client/sidebar/header/actions/CreateRoom.tsx
+++ apps/meteor/client/sidebar/header/actions/CreateRoom.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, Dropdown, Box } from '@rocket.chat/fuselage';
+import { Sidebar, Dropdown } from '@rocket.chat/fuselage';
 import { useAtLeastOnePermission } from '@rocket.chat/ui-contexts';
 import React, { HTMLAttributes, useRef, VFC } from 'react';
 import { createPortal } from 'react-dom';
@@ -17,11 +17,7 @@ const CreateRoom: VFC<Omit<HTMLAttributes<HTMLElement>, 'is'>> = (props) => {
 
 	return (
 		<>
-			{showCreate && (
-				<Box ref={reference} {...props}>
-					<Sidebar.TopBar.Action icon='edit-rounded' onClick={(): void => toggle()} />
-				</Box>
-			)}
+			{showCreate && <Sidebar.TopBar.Action icon='edit-rounded' onClick={(): void => toggle()} {...props} ref={reference} />}
 			{isVisible &&
 				createPortal(
 					<Dropdown reference={reference} ref={target}>
--- apps/meteor/client/sidebar/header/actions/Sort.tsx
+++ apps/meteor/client/sidebar/header/actions/Sort.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, Dropdown, Box } from '@rocket.chat/fuselage';
+import { Sidebar, Dropdown } from '@rocket.chat/fuselage';
 import React, { VFC, useRef, HTMLAttributes } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -12,9 +12,7 @@ const Sort: VFC<Omit<HTMLAttributes<HTMLElement>, 'is'>> = (props) => {
 
 	return (
 		<>
-			<Box ref={reference} {...props}>
-				<Sidebar.TopBar.Action icon='sort' onClick={(): void => toggle()} />
-			</Box>
+			<Sidebar.TopBar.Action {...props} icon='sort' onClick={(): void => toggle()} ref={reference} />
 			{isVisible &&
 				createPortal(
 					<Dropdown reference={reference} ref={target}>
