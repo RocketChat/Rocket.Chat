6d3c06638074e56e183071a18b6e463186f954e3
souzaramon <ramon.souza@objective.com.br>
2022-07-13 07:34:46 -0300

    feat: add ADMIN_CREDENTIALS constant

--- apps/meteor/tests/e2e/utils/constants.ts
+++ apps/meteor/tests/e2e/utils/constants.ts
@@ -5,3 +5,8 @@ export const IS_LOCALHOST = BASE_URL.startsWith('http://localhost');
 export const IS_EE = Boolean(process.env.IS_EE);
 
 export const URL_MONGODB = process.env.MONGO_URL || 'mongodb://localhost:3001/meteor?retryWrites=false';
+
+export const ADMIN_CREDENTIALS = {
+	email: 'rocketchat.internal.admin.test@rocket.chat',
+	password: 'rocketchat.internal.admin.test',
+};
