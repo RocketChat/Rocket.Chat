2a034c656aff709da6c8c1a21b41badcf489128b
gabriellsh <gabriel.henriques@rocket.chat>
2022-07-15 14:43:11 -0300

    use useQuery

--- apps/meteor/client/components/RoomForewordAvatar.tsx
+++ apps/meteor/client/components/RoomForewordAvatar.tsx
@@ -1,15 +1,14 @@
 import { IUser } from '@rocket.chat/core-typings';
 import { Avatar } from '@rocket.chat/fuselage';
-import React, { ReactElement, useMemo } from 'react';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import React, { ReactElement } from 'react';
+import { useQuery } from 'react-query';
 
 import { getUserAvatarURL } from '../../app/utils/client';
-import { useEndpointData } from '../hooks/useEndpointData';
 
 const RoomForewordAvatar = ({ username, ...props }: { username: Exclude<IUser['username'], undefined> }): ReactElement => {
-	const { value: data } = useEndpointData(
-		'/v1/users.info',
-		useMemo(() => ({ username }), [username]),
-	);
+	const getUserInfo = useEndpoint('GET', '/v1/users.info');
+	const { data } = useQuery(['users.info', username], () => getUserInfo({ username }));
 	const avatarUrl = getUserAvatarURL(username, data?.user.avatarETag);
 
 	return <Avatar size='x48' title={username} url={avatarUrl} data-username={username} {...props} />;
