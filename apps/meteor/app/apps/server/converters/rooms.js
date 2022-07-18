6ef12cc3d0a1e15d608fa649ddd1efa36863d49f
Himanshu664 <gs2012023@sgsitsindore.in>
2022-07-18 18:21:44 +0530

    Achive slashcommand moved to apps

--- apps/meteor/app/apps/server/converters/rooms.js
+++ apps/meteor/app/apps/server/converters/rooms.js
@@ -101,6 +101,7 @@ export class AppRoomsConverter {
 					...room.source,
 				},
 			}),
+			...(room.isArchived && { archived: room.isArchived }),
 		};
 
 		return Object.assign(newRoom, room._unmappedProperties_);
@@ -129,6 +130,7 @@ export class AppRoomsConverter {
 			_USERNAMES: '_USERNAMES',
 			description: 'description',
 			source: 'source',
+			isArchived: 'archived',
 			isDefault: (room) => {
 				const result = !!room.default;
 				delete room.default;
