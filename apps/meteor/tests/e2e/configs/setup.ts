cdd826e5ee94c08fc95e531c1c33593573e84005
Weslley de Campos <weslley.campos@objective.com.br>
2022-07-19 16:13:57 -0300

    refactor: set setup wizard in setup

--- apps/meteor/tests/e2e/configs/setup.ts
+++ apps/meteor/tests/e2e/configs/setup.ts
@@ -23,35 +23,37 @@ const subscribeUserInChannels = async (): Promise<void> => {
 };
 
 export default async (config: FullConfig): Promise<void> => {
-	// await MongoHelper.connect(URL_MONGODB);
-	// await insertRoom();
-	// await insertUser();
-	// await subscribeUserInChannels();
-	// await MongoHelper.disconnect();
+	await MongoHelper.connect(URL_MONGODB);
+	await insertRoom();
+	await insertUser();
+	await subscribeUserInChannels();
+	await MongoHelper.disconnect();
 	const { baseURL } = config.projects[0].use;
+
 	const browser = await chromium.launch();
 	const page = await browser.newPage();
 
-	await page.goto(baseURL!);
+	await page.goto(baseURL as string);
 
 	await page.locator('[name=emailOrUsername]').type(ADMIN_CREDENTIALS.email);
 	await page.locator('[name=pass]').type(ADMIN_CREDENTIALS.password);
 	await page.locator('.login').click();
 
-	await page.locator('[name="organizationName"]').type('');
+	await page.locator('[name="organizationName"]').type('any_name');
 	await page.locator('[name="organizationType"]').click();
-	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('.rcx-options .rcx-option:first-child >> text="Community"').click();
 	await page.locator('[name="organizationIndustry"]').click();
-	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('.rcx-options .rcx-option:first-child >> text="Aerospace & Defense"').click();
 	await page.locator('[name="organizationSize"]').click();
 	await page.locator('.rcx-options .rcx-option:first-child >> text="1-10 people"').click();
 	await page.locator('[name="country"]').click();
 	await page.locator('.rcx-options .rcx-option:first-child >> text="Afghanistan"').click();
-	await page.locator('.rcx-button--primary .rcx-button >> text="Next"').click();
+	await page.locator('.rcx-button--primary.rcx-button >> text="Next"').click();
 
 	await page.locator('a.rcx-box.rcx-box--full >> text="Continue as standalone"').click();
 
-	await page.locator('rcx-button--primary rcx-button >> text="Confirm"').click();
+	await page.locator('.rcx-button--primary.rcx-button >> text="Confirm"').click();
 
+	await page.waitForURL('http://localhost:3000/home');
 	await browser.close();
 };
