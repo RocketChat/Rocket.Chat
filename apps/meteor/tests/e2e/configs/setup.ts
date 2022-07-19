81a37ee8e95edac9402f72620e6d0e41d3e5f6a0
Weslley de Campos <weslley.campos@objective.com.br>
2022-07-19 11:12:47 -0300

    refactor: move wizzard to setup

--- apps/meteor/tests/e2e/configs/setup.ts
+++ apps/meteor/tests/e2e/configs/setup.ts
@@ -1,30 +1,29 @@
-import type { IRoom, IUser, ISubscription } from '@rocket.chat/core-typings';
+import { chromium } from '@playwright/test';
 
-import { MongoHelper } from '../utils/MongoHelper';
-import { URL_MONGODB } from '../utils/constants';
-import { roomMock } from '../utils/mocks/roomMock';
-import { userMock } from '../utils/mocks/userMock';
-import { subscriptionMock } from '../utils/mocks/subscriptionMock';
-
-const insertRoom = async (): Promise<void> => {
-	const roomCollection = await MongoHelper.getCollection<IRoom>('rocketchat_room');
-	await roomCollection.insertMany(roomMock);
-};
-
-const insertUser = async (): Promise<void> => {
-	const userCollection = await MongoHelper.getCollection<IUser>('users');
-	await userCollection.insertOne(userMock);
-};
-
-const subscribeUserInChannels = async (): Promise<void> => {
-	const subscribeCollections = await MongoHelper.getCollection<ISubscription>('rocketchat_subscription');
-	await subscribeCollections.insertMany(subscriptionMock);
-};
+import { pageAuth } from '..';
+import * as constants from '../utils/constants';
 
 export default async (): Promise<void> => {
-	await MongoHelper.connect(URL_MONGODB);
-	await insertRoom();
-	await insertUser();
-	await subscribeUserInChannels();
-	await MongoHelper.disconnect();
+	const browser = await chromium.launch();
+	const page = await browser.newPage();
+
+	const pageAuth = new PageAuth(page);
+	await pageAuth.doLogin(constants.ADMIN_CREDENTIALS, false);
+
+	await page.locator('[name="organizationName"]').type('');
+	await page.locator('[name="organizationType"]').click();
+	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('[name="organizationIndustry"]').click();
+	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('[name="organizationSize"]').click();
+	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('[name="country"]').click();
+	await page.locator('.rcx-options .rcx-option:first-child').click();
+	await page.locator('.rcx-button--primary .rcx-button >> text="Next"').click();
+
+	await page.locator('a.rcx-box.rcx-box--full >> text="Continue as standalone"').click();
+
+	await page.locator('rcx-button--primary rcx-button >> text="Confirm"').click();
+
+	await browser.close();
 };
