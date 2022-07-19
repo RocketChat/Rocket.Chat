fcfa668f83c87e93f389504fc789ae9f31d53104
murtaza98 <murtaza.patrawala@rocket.chat>
2022-07-19 20:38:55 +0530

    CRS

--- apps/meteor/.mocharc.api.js
+++ apps/meteor/.mocharc.api.js
@@ -11,11 +11,9 @@ module.exports = {
 	file: 'tests/end-to-end/teardown.js',
 	spec: [
 		'tests/unit/app/api/server/v1/**/*.spec.ts',
-		'tests/end-to-end/api/*.js',
-		'tests/end-to-end/api/*.ts',
+		'tests/end-to-end/api/**/*.js',
+		'tests/end-to-end/api/**/*.ts',
 		'tests/end-to-end/apps/*.js',
-		'tests/end-to-end/api/livechat/*.js',
-		'tests/end-to-end/api/livechat/*.ts',
 		'tests/end-to-end/apps/*.ts',
 	],
 };
