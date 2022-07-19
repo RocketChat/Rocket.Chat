4d1206aae84ea0dfc318c30241ff56b84e03da9a
Hugo Costa <hugocarreiracosta@gmail.com>
2022-07-19 08:45:42 -0300

    fix: improving migration logic

--- apps/meteor/server/startup/migrations/v278.ts
+++ apps/meteor/server/startup/migrations/v278.ts
@@ -1,9 +1,7 @@
-import { BannerPlatform } from '@rocket.chat/core-typings';
-import { Users, Settings } from '@rocket.chat/models';
+import { Banners, Settings } from '@rocket.chat/models';
 
 import { addMigration } from '../../lib/migrations';
 import { isEnterprise } from '../../../ee/app/license/server';
-import { Banner } from '../../sdk';
 import { settings } from '../../../app/settings/server';
 
 addMigration({
@@ -24,22 +22,17 @@ addMigration({
 			return;
 		}
 
-		const users = Promise.await(Users.find().toArray());
-
-		const banners = users.map((a) => Promise.await(Banner.getBannersForUser(a._id, BannerPlatform.Web))).flat();
-		const oldMsg =
-			'Please notice that after the next release (4.0) advanced functionalities of LDAP, SAML, and Custom Oauth will be available only in Enterprise Edition and Gold plan. Check the official announcement for more info: https://go.rocket.chat/i/authentication-changes';
-		const newMsg =
-			'Please note that after release 4.0 certain advanced authentication services features are available only in Enterprise Edition and Gold plan. Check the official announcement for more details: https://go.rocket.chat/i/authentication-changes';
-		// @ts-ignore
-		const authBanner = banners.find((b) => b.view.blocks[0].text.text === oldMsg || b.view.blocks[0].text.text === newMsg);
-
-		if (!authBanner) {
-			return;
-		}
-
-		Banner.disable(authBanner._id);
-
-		users.map((a) => Banner.dismiss(a._id, authBanner._id));
+		Banners.updateOne(
+			{
+				'view.blocks.0.text.text': /authentication\-changes/,
+				'active': { $ne: false },
+			},
+			{
+				$set: {
+					active: false,
+					inactivedAt: new Date(),
+				},
+			},
+		);
 	},
 });
