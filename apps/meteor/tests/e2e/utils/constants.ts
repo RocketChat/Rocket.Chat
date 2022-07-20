31b9cc9e8795930fdc9301f6ee752c5618503735
Weslley de Campos <weslley.campos@objective.com.br>
2022-07-20 10:31:07 -0300

    fix: removing unused constants

--- apps/meteor/tests/e2e/utils/constants.ts
+++ apps/meteor/tests/e2e/utils/constants.ts
@@ -7,7 +7,6 @@ export const IS_LOCALHOST = BASE_URL.startsWith('http://localhost');
 export const IS_EE = Boolean(process.env.IS_EE);
 
 export const URL_MONGODB = process.env.MONGO_URL || 'mongodb://localhost:3001/meteor?retryWrites=false';
-export const DATABASE_NAME = process.env.MONGO_URL ? 'rocketchat' : 'meteor';
 
 export const ADMIN_CREDENTIALS = {
 	email: 'rocketchat.internal.admin.test@rocket.chat',
