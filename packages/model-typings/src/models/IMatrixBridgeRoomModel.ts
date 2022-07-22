2ca2a452ca911d9c36e7f20dbb929dd11a6af70f
Marcos Defendi <marcos.defendi@rocket.chat>
2022-07-22 13:10:27 -0300

    chore: fix lint

--- packages/model-typings/src/models/IMatrixBridgeRoomModel.ts
+++ packages/model-typings/src/models/IMatrixBridgeRoomModel.ts
@@ -3,6 +3,6 @@ import type { IMatrixBridgedRoom } from '@rocket.chat/core-typings';
 import type { IBaseModel } from './IBaseModel';
 
 export interface IMatrixBridgedRoomModel extends IBaseModel<IMatrixBridgedRoom> {
-	getExternalRoomId(localRoomId: string): Promise<string | null>
+	getExternalRoomId(localRoomId: string): Promise<string | null>;
 	getLocalRoomId(externalRoomId: string): Promise<string | null>;
 }
--- packages/model-typings/src/models/IMatrixBridgeUserModel.ts
+++ packages/model-typings/src/models/IMatrixBridgeUserModel.ts
@@ -3,8 +3,8 @@ import type { IMatrixBridgedUser } from '@rocket.chat/core-typings';
 import type { IBaseModel } from './IBaseModel';
 
 export interface IMatrixBridgedUserModel extends IBaseModel<IMatrixBridgedUser> {
-    getExternalUserIdByLocalUserId(localUserId: string): Promise<string | null>;
-    getBridgedUserByExternalUserId(externalUserId: string): Promise<IMatrixBridgedUser | null>;
-    getLocalUserIdByExternalId(externalUserId: string): Promise<string | null>;
-    getBridgedUserByLocalId(localUserId: string): Promise<IMatrixBridgedUser | null>;
+	getExternalUserIdByLocalUserId(localUserId: string): Promise<string | null>;
+	getBridgedUserByExternalUserId(externalUserId: string): Promise<IMatrixBridgedUser | null>;
+	getLocalUserIdByExternalId(externalUserId: string): Promise<string | null>;
+	getBridgedUserByLocalId(localUserId: string): Promise<IMatrixBridgedUser | null>;
 }
