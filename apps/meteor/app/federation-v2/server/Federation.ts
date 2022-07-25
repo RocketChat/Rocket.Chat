2629a8136c4647f4dfe2d3c63920731621b779d0
Marcos Defendi <marcos.defendi@rocket.chat>
2022-07-25 11:46:57 -0300

    refactor: removing unused function

--- apps/meteor/app/federation-v2/server/Federation.ts
+++ apps/meteor/app/federation-v2/server/Federation.ts
@@ -10,9 +10,6 @@ const allowedActionsInFederatedRooms: ValueOf<typeof RoomMemberActions>[] = [
 ];
 
 export class Federation {
-	public static isAFederatedRoom(room: IRoom): boolean {
-		return room.federated === true;
-	}
 
 	public static actionAllowed(room: IRoom, action: ValueOf<typeof RoomMemberActions>): boolean {
 		return isDirectMessageRoom(room) && action === RoomMemberActions.REMOVE_USER ? false : allowedActionsInFederatedRooms.includes(action);
--- apps/meteor/tests/unit/app/federation-v2/server/unit/Federation.spec.ts
+++ apps/meteor/tests/unit/app/federation-v2/server/unit/Federation.spec.ts
@@ -4,19 +4,6 @@ import { Federation } from '../../../../../../app/federation-v2/server/Federatio
 import { RoomMemberActions } from '../../../../../../definition/IRoomTypeConfig';
 
 describe('Federation[Server] - Federation', () => {
-	describe('#isAFederatedRoom()', () => {
-		it('should return true if the room is federated', () => {
-			expect(Federation.isAFederatedRoom({ federated: true } as any)).to.be.true;
-		});
-
-		it('should return false if the room is NOT federated', () => {
-			expect(Federation.isAFederatedRoom({ federated: false } as any)).to.be.false;
-		});
-
-		it('should return false if the room does NOT have the federated property', () => {
-			expect(Federation.isAFederatedRoom({} as any)).to.be.false;
-		});
-	});
 
 	describe('#actionAllowed()', () => {
 		const allowedActions = [RoomMemberActions.REMOVE_USER, RoomMemberActions.INVITE, RoomMemberActions.JOIN, RoomMemberActions.LEAVE];
