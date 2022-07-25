288c3f3de8265148d79cfcb22596ab95ed77c900
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-07-25 09:30:31 -0300

    Move user data download settings module

--- apps/meteor/server/importPackages.ts
+++ apps/meteor/server/importPackages.ts
@@ -84,7 +84,7 @@ import '../app/threads/server';
 import '../app/tokenpass/server';
 import '../app/ui-master/server';
 import '../app/ui-vrecord/server';
-import '../app/user-data-download';
+import '../app/user-data-download/server';
 import '../app/webdav/server';
 import '../app/webrtc/server';
 import '../app/wordpress/server';
