587b91f8f673b4232770fad968ec2a7d4a5fc5f0
gabriellsh <gabriel.henriques@rocket.chat>
2022-07-26 16:48:59 -0300

    Remove invalid rule

--- apps/meteor/client/views/home/DefaultHomePage.tsx
+++ apps/meteor/client/views/home/DefaultHomePage.tsx
@@ -26,7 +26,6 @@ const DefaultHomePage = (): ReactElement => {
 			<HomePageHeader />
 			<PageScrollableContent>
 				<Box is='h1' fontScale='h1' marginBlock='x42' minHeight='x40'>
-					{/* eslint-disable-next-line @typescript-eslint/camelcase */}
 					{t('Welcome_to', { Site_Name: workspaceName || 'Rocket.Chat' })}
 				</Box>
 				<Box is='h3' fontScale='h3' marginBlock='x16'>
