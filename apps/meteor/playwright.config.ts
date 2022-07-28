13e7aea8c6f30d72042d82c016d0fc0237790f96
souzaramon <ramon.souza@objective.com.br>
2022-07-27 23:01:22 -0300

    chore: less retries, remove timeout, add more workers

--- apps/meteor/playwright.config.ts
+++ apps/meteor/playwright.config.ts
@@ -1,23 +1,18 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
-import * as constants from './tests/e2e/utils/constants';
+import * as constants from './tests/e2e/config/constants';
 
 export default {
-	globalSetup: require.resolve('./tests/e2e/configs/setup.ts'),
-	globalTeardown: require.resolve('./tests/e2e/configs/teardown.ts'),
+	globalSetup: require.resolve('./tests/e2e/config/global-setup.ts'),
 	use: {
 		headless: true,
-		viewport: { width: 1368, height: 768 },
 		ignoreHTTPSErrors: true,
-		video: 'retain-on-failure',
-		screenshot: 'only-on-failure',
 		trace: 'retain-on-failure',
 		baseURL: constants.BASE_URL,
 	},
 	outputDir: 'tests/e2e/test-failures',
 	reporter: process.env.CI ? 'github' : 'list',
 	testDir: 'tests/e2e',
-	retries: 3,
-	workers: 1,
-	timeout: 42 * 1000,
+	retries: 2,
+	workers: process.env.CI ? 2 : undefined,
 } as PlaywrightTestConfig;
