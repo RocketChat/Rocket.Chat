281e5207bcb473cac58aeaa51d124a9864ed7b66
souzaramon <ramon.souza@objective.com.br>
2022-07-29 09:41:09 -0300

    chore: remove _old folder

--- apps/meteor/tests/e2e_/page-objects/fragments/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '../../../e2e/page-objects/fragments/admin-flextab';
-export * from '../../../e2e/page-objects/fragments/admin-sidenav';
-export * from './home-content';
-export * from './home-flextab';
-export * from './home-sidenav';
