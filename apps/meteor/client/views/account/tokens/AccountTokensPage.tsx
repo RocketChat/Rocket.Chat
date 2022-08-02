b9f76b41632970ac9a262f96553b5cc9bf3436e5
Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>
2022-08-03 04:06:24 +0530

    Chore: Accounts/token to TS (#26434)
    Co-authored-by: dougfabris <devfabris@gmail.com>

--- apps/meteor/client/views/account/tokens/AccountTokensPage.tsx
+++ apps/meteor/client/views/account/tokens/AccountTokensPage.tsx
@@ -1,21 +1,17 @@
 import { useTranslation } from '@rocket.chat/ui-contexts';
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import Page from '../../../components/Page';
-import { useEndpointData } from '../../../hooks/useEndpointData';
 import AccountTokensTable from './AccountTokensTable';
-import AddToken from './AddToken';
 
-const AccountTokensPage = () => {
+const AccountTokensPage = (): ReactElement => {
 	const t = useTranslation();
-	const { value: data, reload } = useEndpointData('/v1/users.getPersonalAccessTokens');
 
 	return (
 		<Page>
 			<Page.Header title={t('Personal_Access_Tokens')} />
 			<Page.Content>
-				<AddToken onDidAddToken={reload} />
-				<AccountTokensTable data={data} reload={reload} />
+				<AccountTokensTable />
 			</Page.Content>
 		</Page>
 	);
