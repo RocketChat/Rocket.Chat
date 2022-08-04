ff3178cd4555a47627e9d4ab51595fc0c885833d
Diego Sampaio <chinello@gmail.com>
2022-08-04 12:25:44 -0300

    only refresh voip if enabled

--- apps/meteor/app/voip/server/startup.ts
+++ apps/meteor/app/voip/server/startup.ts
@@ -14,6 +14,8 @@ settings.changeMultiple(
 	(_values) => {
 		// Here, if 4 settings are changed at once, we're getting 4 diff callbacks. The good part is that all callbacks are fired almost instantly
 		// So to avoid stopping/starting voip too often, we debounce the call and restart 1 second after the last setting has reached us.
-		return debouncedRefresh();
+		if (settings.get('VoIP_Enabled')) {
+			return debouncedRefresh();
+		}
 	},
 );
