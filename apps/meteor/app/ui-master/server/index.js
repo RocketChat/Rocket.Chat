a89c44500e72b964f824509ee008db7a0eed1287
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-08-03 23:13:47 -0300

    fix

--- apps/meteor/app/ui-master/server/index.js
+++ apps/meteor/app/ui-master/server/index.js
@@ -127,14 +127,9 @@ Meteor.startup(() => {
 const renderDynamicCssList = _.debounce(
 	Meteor.bindEnvironment(async () => {
 		// const variables = RocketChat.models.Settings.findOne({_id:'theme-custom-variables'}, {fields: { value: 1}});
-		const colors = await Settings.find({ _id: /theme-color-rc/i }, { projection: { value: 1, editor: 1 } })
-			.toArray()
-			.filter((color) => color && color.value);
-
-		if (!colors) {
-			return;
-		}
+		const colors = await Settings.find({ _id: /theme-color-rc/i }, { projection: { value: 1, editor: 1 } }).toArray();
 		const css = colors
+			.filter((color) => color && color.value)
 			.map(({ _id, value, editor }) => {
 				if (editor === 'expression') {
 					return `--${_id.replace('theme-color-', '')}: var(--${value});`;
