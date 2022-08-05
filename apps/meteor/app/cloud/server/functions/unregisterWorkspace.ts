b80b4ae5213b5e49370a82a6145033c51f0a3c42
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-08-05 10:40:18 -0300

    Review

--- apps/meteor/app/cloud/server/functions/unregisterWorkspace.ts
+++ apps/meteor/app/cloud/server/functions/unregisterWorkspace.ts
@@ -8,13 +8,15 @@ export async function unregisterWorkspace() {
 		return true;
 	}
 
-	Settings.updateValueById('Cloud_Workspace_Id', undefined);
-	Settings.updateValueById('Cloud_Workspace_Name', undefined);
-	Settings.updateValueById('Cloud_Workspace_Client_Id', undefined);
-	Settings.updateValueById('Cloud_Workspace_Client_Secret', undefined);
-	Settings.updateValueById('Cloud_Workspace_Client_Secret_Expires_At', undefined);
-	Settings.updateValueById('Cloud_Workspace_PublicKey', undefined);
-	Settings.updateValueById('Cloud_Workspace_Registration_Client_Uri', undefined);
+	await Promise.all([
+		Settings.updateValueById('Cloud_Workspace_Id', undefined),
+		Settings.updateValueById('Cloud_Workspace_Name', undefined),
+		Settings.updateValueById('Cloud_Workspace_Client_Id', undefined),
+		Settings.updateValueById('Cloud_Workspace_Client_Secret', undefined),
+		Settings.updateValueById('Cloud_Workspace_Client_Secret_Expires_At', undefined),
+		Settings.updateValueById('Cloud_Workspace_PublicKey', undefined),
+		Settings.updateValueById('Cloud_Workspace_Registration_Client_Uri', undefined),
+	]);
 
 	return true;
 }
--- apps/meteor/app/livechat/server/methods/saveIntegration.ts
+++ apps/meteor/app/livechat/server/methods/saveIntegration.ts
@@ -5,7 +5,7 @@ import s from 'underscore.string';
 import { hasPermission } from '../../../authorization/server';
 
 Meteor.methods({
-	'livechat:saveIntegration'(values) {
+	async 'livechat:saveIntegration'(values) {
 		const uid = Meteor.userId();
 		if (!uid || !hasPermission(uid, 'view-livechat-manager')) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', {
@@ -14,43 +14,43 @@ Meteor.methods({
 		}
 
 		if (typeof values.Livechat_webhookUrl !== 'undefined') {
-			Settings.updateValueById('Livechat_webhookUrl', s.trim(values.Livechat_webhookUrl));
+			await Settings.updateValueById('Livechat_webhookUrl', s.trim(values.Livechat_webhookUrl));
 		}
 
 		if (typeof values.Livechat_secret_token !== 'undefined') {
-			Settings.updateValueById('Livechat_secret_token', s.trim(values.Livechat_secret_token));
+			await Settings.updateValueById('Livechat_secret_token', s.trim(values.Livechat_secret_token));
 		}
 
 		if (typeof values.Livechat_webhook_on_start !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_start', !!values.Livechat_webhook_on_start);
+			await Settings.updateValueById('Livechat_webhook_on_start', !!values.Livechat_webhook_on_start);
 		}
 
 		if (typeof values.Livechat_webhook_on_close !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_close', !!values.Livechat_webhook_on_close);
+			await Settings.updateValueById('Livechat_webhook_on_close', !!values.Livechat_webhook_on_close);
 		}
 
 		if (typeof values.Livechat_webhook_on_chat_taken !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_chat_taken', !!values.Livechat_webhook_on_chat_taken);
+			await Settings.updateValueById('Livechat_webhook_on_chat_taken', !!values.Livechat_webhook_on_chat_taken);
 		}
 
 		if (typeof values.Livechat_webhook_on_chat_queued !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_chat_queued', !!values.Livechat_webhook_on_chat_queued);
+			await Settings.updateValueById('Livechat_webhook_on_chat_queued', !!values.Livechat_webhook_on_chat_queued);
 		}
 
 		if (typeof values.Livechat_webhook_on_forward !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_forward', !!values.Livechat_webhook_on_forward);
+			await Settings.updateValueById('Livechat_webhook_on_forward', !!values.Livechat_webhook_on_forward);
 		}
 
 		if (typeof values.Livechat_webhook_on_offline_msg !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_offline_msg', !!values.Livechat_webhook_on_offline_msg);
+			await Settings.updateValueById('Livechat_webhook_on_offline_msg', !!values.Livechat_webhook_on_offline_msg);
 		}
 
 		if (typeof values.Livechat_webhook_on_visitor_message !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_visitor_message', !!values.Livechat_webhook_on_visitor_message);
+			await Settings.updateValueById('Livechat_webhook_on_visitor_message', !!values.Livechat_webhook_on_visitor_message);
 		}
 
 		if (typeof values.Livechat_webhook_on_agent_message !== 'undefined') {
-			Settings.updateValueById('Livechat_webhook_on_agent_message', !!values.Livechat_webhook_on_agent_message);
+			await Settings.updateValueById('Livechat_webhook_on_agent_message', !!values.Livechat_webhook_on_agent_message);
 		}
 	},
 });
