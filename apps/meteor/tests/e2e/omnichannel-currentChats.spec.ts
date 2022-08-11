11d72b1d03a470cd3757e76c99d886b934086f15
Martin <martin.schoeler@rocket.chat>
2022-08-11 15:11:08 -0300

    Fix Reviews

--- apps/meteor/tests/e2e/omnichannel-currentChats.spec.ts
+++ apps/meteor/tests/e2e/omnichannel-currentChats.spec.ts
@@ -14,19 +14,25 @@ test.describe.serial('Current Chats', () => {
 	});
 
 	test.describe('Render Fields', () => {
-		test('expect render all form fields', async () => {
+		test('expect show guest Field', async () => {
 			await expect(pageOmnichannelCurrentChats.guestField).toBeVisible();
-
+		});
+		test('expect show servedBy Field', async () => {
 			await expect(pageOmnichannelCurrentChats.servedByField).toBeVisible();
-
+		});
+		test('expect show status Field', async () => {
 			await expect(pageOmnichannelCurrentChats.statusField).toBeVisible();
-
+		});
+		test('expect show from Field', async () => {
 			await expect(pageOmnichannelCurrentChats.fromField).toBeVisible();
-
+		});
+		test('expect show to Field', async () => {
 			await expect(pageOmnichannelCurrentChats.toField).toBeVisible();
-
+		});
+		test('expect show department Field', async () => {
 			await expect(pageOmnichannelCurrentChats.departmentField).toBeVisible();
-
+		});
+		test('expect show form options button', async () => {
 			await expect(pageOmnichannelCurrentChats.formOptions).toBeVisible();
 		});
 	});
@@ -35,31 +41,40 @@ test.describe.serial('Current Chats', () => {
 		test.beforeEach(async () => {
 			await pageOmnichannelCurrentChats.doOpenOptions();
 		});
-		test('expect to render options', async () => {
+		test('expect show form options button', async () => {
 			await expect(pageOmnichannelCurrentChats.formOptions).toBeVisible();
-
+		});
+		test('expect show clear filters option', async () => {
 			await expect(pageOmnichannelCurrentChats.clearFiltersOption).toBeVisible();
-
+		});
+		test('expect show removeAllClosed option', async () => {
 			await expect(pageOmnichannelCurrentChats.removeAllClosedOption).toBeVisible();
-
+		});
+		test('expect show custom fields option', async () => {
 			await expect(pageOmnichannelCurrentChats.customFieldsOption).toBeVisible();
 		});
 	});
 
 	test.describe('Render Table', () => {
-		test('expect to render table header', async () => {
+		test('expect show table', async () => {
 			await expect(pageOmnichannelCurrentChats.table).toBeVisible();
-
+		});
+		test('expect show name header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderName).toBeVisible();
-
+		});
+		test('expect show department header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderDepartment).toBeVisible();
-
+		});
+		test('expect show servedBy header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderServedBy).toBeVisible();
-
+		});
+		test('expect show startedAt header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderStartedAt).toBeVisible();
-
+		});
+		test('expect show lastMessage header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderLastMessage).toBeVisible();
-
+		});
+		test('expect show form status header', async () => {
 			await expect(pageOmnichannelCurrentChats.tableHeaderStatus).toBeVisible();
 		});
 	});
