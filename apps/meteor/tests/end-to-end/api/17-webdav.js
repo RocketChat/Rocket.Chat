c456aa5cc105f1020c1fa111a9a731ea4d99f4c5
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-08-15 21:45:10 -0300

    add success case test

--- apps/meteor/tests/end-to-end/api/17-webdav.js
+++ apps/meteor/tests/end-to-end/api/17-webdav.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import { getCredentials, api, request, credentials } from '../../data/api-data.js';
+import { findWebdavAccountsByUserId } from '../../app/api/server/lib/webdav';
 
 describe('[Webdav]', function () {
 	this.retries(0);
@@ -22,6 +23,22 @@ describe('[Webdav]', function () {
 	});
 
 	describe('/webdav.removeWebdavAccount', () => {
+		const accountId = findWebdavAccountsByUserId({ uid: credentials['X-User-Id'] })[0].username;
+
+		it('should return my webdav accounts', (done) => {
+			request
+				.post(api('webdav.removeWebdavAccount'))
+				.set(credentials)
+				.send({
+					accountId,
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('accounts').and.to.be.a('array');
+				})
+				.end(done);
+		});
 		it('should return an error when send an invalid request', (done) => {
 			request
 				.post(api('webdav.removeWebdavAccount'))
