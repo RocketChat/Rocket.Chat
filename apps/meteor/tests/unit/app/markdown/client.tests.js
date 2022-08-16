f08997ec75395615c8a507cf7f8f334bbf848c23
Filipe Marins <filipe.marins@rocket.chat>
2022-08-16 17:38:25 -0300

    [FIX] Katex is not respecting the 'Katex_Enabled' setting (#26542)

--- apps/meteor/tests/unit/app/markdown/client.tests.js
+++ apps/meteor/tests/unit/app/markdown/client.tests.js
@@ -1,11 +1,7 @@
-import './client.mocks.js';
-
 import { expect } from 'chai';
 import { escapeHTML } from '@rocket.chat/string-helpers';
 
-import { original } from '../../../../app/markdown/lib/parser/original/original';
-import { filtered } from '../../../../app/markdown/lib/parser/filtered/filtered';
-import { Markdown } from '../../../../app/markdown/lib/markdown';
+import { Markdown, original, filtered } from './client.mocks.js';
 
 const wrapper = (text, tag) => `<span class="copyonly">${tag}</span>${text}<span class="copyonly">${tag}</span>`;
 const boldWrapper = (text) => wrapper(`<strong>${text}</strong>`, '*');
