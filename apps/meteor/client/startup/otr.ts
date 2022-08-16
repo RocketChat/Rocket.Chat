715e4b466872aa7ffc227b0b7acab213f95b9857
Fábio Albuquerque <albuquerquefabio@icloud.com>
2022-08-16 18:30:24 -0300

    Import order

--- apps/meteor/client/startup/otr.ts
+++ apps/meteor/client/startup/otr.ts
@@ -3,11 +3,11 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Notifications } from '../../app/notifications/client';
-import { t } from '../../app/utils/client';
 import { onClientBeforeSendMessage } from '../lib/onClientBeforeSendMessage';
 import { onClientMessageReceived } from '../lib/onClientMessageReceived';
 import OTR from '../../app/otr/client/OTR';
 import { OtrRoomState } from '../../app/otr/lib/OtrRoomState';
+import { t } from '../../app/utils/client';
 
 type NotifyUserData = {
 	roomId: IRoom['_id'];
