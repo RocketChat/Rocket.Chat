b7b5dbf127071b914d76f4754337dffc3363342e
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-08-17 10:30:31 -0300

    Update apps/meteor/tests/data/interactions.js
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/tests/data/interactions.js
+++ apps/meteor/tests/data/interactions.js
@@ -1,3 +1,5 @@
+import { BASE_URL } from '../e2e/config/constants'
+
 export const targetUser = 'rocket.cat';
 export const imgURL = './public/images/logo/1024x1024.png';
 export const avatarURL = BASE_URL +  '/images/logo/1024x1024.png';
