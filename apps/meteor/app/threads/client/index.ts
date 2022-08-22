8a2899fe1720eac06f875f85da4a6fe4644b468e
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-08-22 15:48:30 -0300

    Chore: Migrate modules related to `room` template to TypeScript (#25881)

---
+++ apps/meteor/app/threads/client/index.ts
@@ -0,0 +1,7 @@
+import './flextab/threadlist';
+import './flextab/thread.ts';
+import './flextab/messageBoxFollow.ts';
+import './flextab/threads.ts';
+import './messageAction/follow';
+import './messageAction/unfollow';
+import './messageAction/replyInThread';
