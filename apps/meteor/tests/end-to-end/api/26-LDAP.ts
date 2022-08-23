8c2c016ac184f40f4e411eedcf288dfbc3b968d0
Diego Sampaio <chinello@gmail.com>
2022-08-23 15:50:12 -0300

    add todo

--- apps/meteor/tests/end-to-end/api/26-LDAP.ts
+++ apps/meteor/tests/end-to-end/api/26-LDAP.ts
@@ -10,6 +10,8 @@ describe('LDAP', function () {
 
 	describe('[/ldap.syncNow]', () => {
 		it('should throw an error containing totp-required error when not running EE', function (done) {
+			// TODO this is not the right way to do it. We're doing this way for now just because we have separate CI jobs for EE and CE,
+			// ideally we should have a single CI job that adds a license and runs both CE and EE tests.
 			if (process.env.IS_EE) {
 				this.skip();
 				return;
