a914480287da60741428bcd4fbf1f3b05a8afe48
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-08-23 11:51:34 -0300

    Revert "Change translation key"
    This reverts commit 93c55c5c788fc8123dc263a3de91d40a059c0bc1.

--- apps/meteor/client/views/room/contextualBar/PruneMessages/PruneMessagesWithData.tsx
+++ apps/meteor/client/views/room/contextualBar/PruneMessages/PruneMessagesWithData.tsx
@@ -89,7 +89,7 @@ const PruneMessagesWithData = ({ rid, tabBar }: { rid: IRoom['_id']; tabBar: Too
 					throw new Error(t('No_messages_found_to_prune'));
 				}
 
-				dispatchToastMessage({ type: 'success', message: t('message_pruned', { count }) });
+				dispatchToastMessage({ type: 'success', message: t('__count__message_pruned', { count }) });
 				closeModal();
 				reset();
 			} catch (error: unknown) {
