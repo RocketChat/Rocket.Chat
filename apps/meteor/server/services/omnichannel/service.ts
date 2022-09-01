9b8d8e9f881a45609b3b790e8170f7a4f89f2496
Diego Sampaio <chinello@gmail.com>
2022-09-01 16:10:03 -0300

    improve check

--- apps/meteor/server/services/omnichannel/service.ts
+++ apps/meteor/server/services/omnichannel/service.ts
@@ -1,6 +1,5 @@
 import { ServiceClassInternal } from '../../sdk/types/ServiceClass';
 import type { IOmnichannelService } from '../../sdk/types/IOmnichannelService';
-import { hasAnyRole } from '../../../app/authorization/server/functions/hasRole';
 import { Livechat } from '../../../app/livechat/server';
 
 export class OmnichannelService extends ServiceClassInternal implements IOmnichannelService {
@@ -11,8 +10,8 @@ export class OmnichannelService extends ServiceClassInternal implements IOmnicha
 			if (!user?._id) {
 				return;
 			}
-			// TODO change `hasAnyRole` to a service call
-			if (hasAnyRole(user._id, ['livechat-manager', 'livechat-monitor', 'livechat-agent'])) {
+			const hasRole = user.roles.some((role) => ['livechat-manager', 'livechat-monitor', 'livechat-agent'].includes(role));
+			if (hasRole) {
 				// TODO change `Livechat.notifyAgentStatusChanged` to a service call
 				Livechat.notifyAgentStatusChanged(user._id, user.status);
 			}
