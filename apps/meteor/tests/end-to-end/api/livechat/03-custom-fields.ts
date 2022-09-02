e7290ac9554d35242f057c085a89f5cf363e3453
Kevin Aleman <kaleman960@gmail.com>
2022-09-02 11:20:56 -0600

    Chore: Add E2E tests to missing omnichannel endpoints (#26730)

--- apps/meteor/tests/end-to-end/api/livechat/03-custom-fields.ts
+++ apps/meteor/tests/end-to-end/api/livechat/03-custom-fields.ts
@@ -1,9 +1,12 @@
 /* eslint-env mocha */
 
+import type { ILivechatCustomField } from '@rocket.chat/core-typings';
 import { expect } from 'chai';
 import type { Response } from 'supertest';
 
 import { getCredentials, api, request, credentials } from '../../../data/api-data';
+import { createCustomField } from '../../../data/livechat/custom-fields';
+import { createVisitor } from '../../../data/livechat/rooms';
 import { updatePermission, updateSetting } from '../../../data/permissions.helper';
 
 describe('LIVECHAT - custom fields', function () {
@@ -71,6 +74,120 @@ describe('LIVECHAT - custom fields', function () {
 					.expect(403)
 					.end(() => updatePermission('view-l-room', ['admin']).then(done));
 			});
+		});
+	});
+
+	describe('livechat/custom.field', () => {
+		it('should fail when token is not on body params', async () => {
+			await request.post(api('livechat/custom.field')).expect(400);
+		});
+		it('should fail when key is not on body params', async () => {
+			await request.post(api('livechat/custom.field')).send({ token: 'invalid-token' }).expect(400);
+		});
+		it('should fail when value is not on body params', async () => {
+			await request.post(api('livechat/custom.field')).send({ token: 'invalid-token', key: 'invalid-key' }).expect(400);
+		});
+		it('should fail when overwrite is not on body params', async () => {
+			await request
+				.post(api('livechat/custom.field'))
+				.send({ token: 'invalid-token', key: 'invalid-key', value: 'invalid-value' })
+				.expect(400);
+		});
+		it('should fail when token is invalid', async () => {
+			await request
+				.post(api('livechat/custom.field'))
+				.send({ token: 'invalid-token', key: 'invalid-key', value: 'invalid-value', overwrite: true })
+				.expect(400);
+		});
+		it('should fail when key is invalid', async () => {
+			const visitor = await createVisitor();
+			await request
+				.post(api('livechat/custom.field'))
+				.send({ token: visitor.token, key: 'invalid-key', value: 'invalid-value', overwrite: true })
+				.expect(400);
+		});
+		it('should save a custom field on visitor', async () => {
+			const visitor = await createVisitor();
+			const customFieldName = `new_custom_field_${Date.now()}`;
+			await createCustomField({
+				searchable: true,
+				field: customFieldName,
+				label: customFieldName,
+				defaultValue: 'test_default_address',
+				scope: 'visitor',
+				visibility: 'public',
+				regexp: '',
+			} as unknown as ILivechatCustomField & { field: string });
+
+			const { body } = await request
+				.post(api('livechat/custom.field'))
+				.send({ token: visitor.token, key: customFieldName, value: 'test_address', overwrite: true })
+				.expect(200);
+
+			expect(body).to.have.property('success', true);
+			expect(body).to.have.property('field');
+			expect(body.field).to.have.property('value', 'test_address');
+		});
+	});
+
+	describe('livechat/custom.fields', () => {
+		it('should fail when token is not on body params', async () => {
+			await request.post(api('livechat/custom.fields')).expect(400);
+		});
+		it('should fail if customFields is not on body params', async () => {
+			await request.post(api('livechat/custom.fields')).send({ token: 'invalid-token' }).expect(400);
+		});
+		it('should fail if customFields is not an array', async () => {
+			await request.post(api('livechat/custom.fields')).send({ token: 'invalid-token', customFields: 'invalid-custom-fields' }).expect(400);
+		});
+		it('should fail if customFields is an empty array', async () => {
+			await request.post(api('livechat/custom.fields')).send({ token: 'invalid-token', customFields: [] }).expect(400);
+		});
+		it('should fail if customFields is an array with invalid objects', async () => {
+			await request
+				.post(api('livechat/custom.fields'))
+				.send({ token: 'invalid-token', customFields: [{}] })
+				.expect(400);
+		});
+		it('should fail if token is not a valid token', async () => {
+			await request
+				.post(api('livechat/custom.fields'))
+				.send({ token: 'invalid-token', customFields: [{ key: 'invalid-key', value: 'invalid-value', overwrite: true }] })
+				.expect(400);
+		});
+		it('should fail when customFields.key is invalid', async () => {
+			const visitor = await createVisitor();
+			await request
+				.post(api('livechat/custom.fields'))
+				.send({
+					token: visitor.token,
+					customFields: [{ key: 'invalid-key', value: 'invalid-value', overwrite: true }],
+				})
+				.expect(400);
+		});
+		it('should save a custom field on visitor', async () => {
+			const visitor = await createVisitor();
+			const customFieldName = `new_custom_field_${Date.now()}`;
+			await createCustomField({
+				searchable: true,
+				field: customFieldName,
+				label: customFieldName,
+				defaultValue: 'test_default_address',
+				scope: 'visitor',
+				visibility: 'public',
+				regexp: '',
+			} as unknown as ILivechatCustomField & { field: string });
+
+			const { body } = await request
+				.post(api('livechat/custom.fields'))
+				.send({ token: visitor.token, customFields: [{ key: customFieldName, value: 'test_address', overwrite: true }] })
+				.expect(200);
+
+			expect(body).to.have.property('success', true);
+			expect(body).to.have.property('fields');
+			expect(body.fields).to.be.an('array');
+			expect(body.fields).to.have.lengthOf(1);
+			expect(body.fields[0]).to.have.property('value', 'test_address');
 		});
 	});
 });
