fc1a9aec91b2cc24b4717f7f56602246e2ebc07a
Hugo Costa <hugocarreiracosta@gmail.com>
2022-09-02 10:41:48 -0300

    reverting typo issue

--- packages/core-typings/src/Serialized.ts
+++ packages/core-typings/src/Serialized.ts
@@ -2,7 +2,7 @@ export type Serialized<T> = T extends Date
 	? Exclude<T, Date> | string
 	: T extends boolean | number | string | null | undefined
 	? T
-	: T extends Record<string, unknown>
+	: T extends {}
 	? {
 			[K in keyof T]: Serialized<T[K]>;
 	  }
