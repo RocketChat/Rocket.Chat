9b3877db12a9c956bd28f66d0756457ce80400ac
Fábio Albuquerque <albuquerquefabio@icloud.com>
2022-09-06 16:09:56 -0300

    [IMPROVE] OTR Message (#24297)
    Co-authored-by: Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>

--- apps/meteor/app/lib/server/functions/sendMessage.js
+++ apps/meteor/app/lib/server/functions/sendMessage.js
@@ -10,6 +10,7 @@ import { hasPermission } from '../../../authorization/server';
 import { SystemLogger } from '../../../../server/lib/logger/system';
 import { parseUrlsInMessage } from './parseUrlsInMessage';
 import { isRelativeURL } from '../../../../lib/utils/isRelativeURL';
+import notifications from '../../../notifications/server/lib/Notifications';
 
 /**
  * IMPORTANT
@@ -244,7 +245,10 @@ export const sendMessage = function (user, message, room, upsert = false) {
 
 	message = callbacks.run('beforeSaveMessage', message, room);
 	if (message) {
-		if (message._id && upsert) {
+		if (message.t === 'otr') {
+			const otrStreamer = notifications.streamRoomMessage;
+			otrStreamer.emit(message.rid, message, user, room);
+		} else if (message._id && upsert) {
 			const { _id } = message;
 			delete message._id;
 			Messages.upsert(
