899247508b75bba80d965fd5c1634d38e4148e32
Diego Sampaio <chinello@gmail.com>
2022-09-08 18:59:17 -0300

    Move startup service's calls to Meteor.startup

--- apps/meteor/server/startup/presenceTroubleshoot.ts
+++ apps/meteor/server/startup/presenceTroubleshoot.ts
@@ -2,4 +2,8 @@ import { settings } from '../../app/settings/server';
 import { Presence } from '../sdk';
 
 // maybe this setting should disable the listener to 'presence.status' event on listerners.module.ts
-settings.watch('Troubleshoot_Disable_Presence_Broadcast', (value) => Presence.toggleBroadcast(!value));
+Meteor.startup(function () {
+	settings.watch('Troubleshoot_Disable_Presence_Broadcast', (value) => {
+		Presence.toggleBroadcast(!value);
+	});
+});
