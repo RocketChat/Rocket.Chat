7d35b72ff86df4177b41cda2bd6774f35b462768
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2022-09-09 21:46:54 -0300

    Chore: Configure Prettier for `@rocket.chat/livechat` (#26846)

--- packages/livechat/src/components/Messages/MessageAvatars/stories.js
+++ packages/livechat/src/components/Messages/MessageAvatars/stories.js
@@ -4,27 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { MessageAvatars } from '.';
 import { avatarResolver, centered } from '../../../helpers.stories';
 
-
 storiesOf('Messages/MessageAvatars', module)
 	.addDecorator(centered)
 	.addDecorator(withKnobs)
-	.add('empty', () => (
-		<MessageAvatars
-			avatarResolver={avatarResolver}
-			usernames={object('usernames', [])}
-		/>
-	))
-	.add('with one avatar', () => (
-		<MessageAvatars
-			avatarResolver={avatarResolver}
-			usernames={object('usernames', ['guilherme.gazzo'])}
-		/>
-	))
+	.add('empty', () => <MessageAvatars avatarResolver={avatarResolver} usernames={object('usernames', [])} />)
+	.add('with one avatar', () => <MessageAvatars avatarResolver={avatarResolver} usernames={object('usernames', ['guilherme.gazzo'])} />)
 	.add('with two avatars', () => (
-		<MessageAvatars
-			avatarResolver={avatarResolver}
-			usernames={object('usernames', ['guilherme.gazzo', 'tasso.evangelista'])}
-		/>
+		<MessageAvatars avatarResolver={avatarResolver} usernames={object('usernames', ['guilherme.gazzo', 'tasso.evangelista'])} />
 	))
 	.add('with three avatars', () => (
 		<MessageAvatars
@@ -33,8 +19,5 @@ storiesOf('Messages/MessageAvatars', module)
 		/>
 	))
 	.add('with name as avatar instead of username for guests', () => (
-		<MessageAvatars
-			avatarResolver={avatarResolver}
-			usernames={object('usernames', ['livechat guest'])}
-		/>
+		<MessageAvatars avatarResolver={avatarResolver} usernames={object('usernames', ['livechat guest'])} />
 	));
