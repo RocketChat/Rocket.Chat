4761de942e631a7f1dc4042a09d3548645306791
Diego Sampaio <chinello@gmail.com>
2022-09-12 20:53:44 -0300

    Add TODO

--- apps/meteor/app/apps/server/status.ts
+++ apps/meteor/app/apps/server/status.ts
@@ -2,6 +2,7 @@ import { UserPresenceMonitor } from 'meteor/konecty:user-presence';
 
 import { AppEvents, Apps } from './orchestrator';
 
+// TODO change this to a service event listener
 UserPresenceMonitor.onSetUserStatus((...args: any) => {
 	const [user, status] = args;
 
