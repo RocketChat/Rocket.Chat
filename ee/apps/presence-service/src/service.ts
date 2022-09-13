eee89cd4937d1f86c9773f2ede100de8ea4ab797
Diego Sampaio <chinello@gmail.com>
2022-09-13 19:35:38 -0300

    add health check endpoint

--- ee/apps/presence-service/src/service.ts
+++ ee/apps/presence-service/src/service.ts
@@ -1,10 +1,13 @@
 import type { Document } from 'mongodb';
+import polka from 'polka';
 
 import '../../../../apps/meteor/ee/server/startup/broker';
 
 import { api } from '../../../../apps/meteor/server/sdk/api';
 import { Collections, getCollection, getConnection } from '../../../../apps/meteor/ee/server/services/mongo';
 import { registerServiceModels } from '../../../../apps/meteor/ee/server/lib/registerServiceModels';
+
+const PORT = process.env.PORT || 3031;
 
 getConnection().then(async (db) => {
 	const trash = await getCollection<Document>(Collections.Trash);
@@ -15,4 +18,11 @@ getConnection().then(async (db) => {
 	const { Presence } = await import('@rocket.chat/presence');
 
 	api.registerService(new Presence());
+
+	polka()
+		.get('/health', async function (_req, res) {
+			await api.nodeList();
+			res.end('ok');
+		})
+		.listen(PORT);
 });
