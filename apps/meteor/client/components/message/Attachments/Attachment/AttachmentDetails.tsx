fd61aec1c637c5bb6d5088995b909fba2e7614f7
Hugo Costa <hugocarreiracosta@gmail.com>
2022-09-13 16:24:06 -0300

    fix: fix attachmentDetails padding token to x16

--- apps/meteor/client/components/message/Attachments/Attachment/AttachmentDetails.tsx
+++ apps/meteor/client/components/message/Attachments/Attachment/AttachmentDetails.tsx
@@ -2,7 +2,7 @@ import { Box } from '@rocket.chat/fuselage';
 import React, { FC, ComponentProps } from 'react';
 
 const AttachmentDetails: FC<ComponentProps<typeof Box>> = ({ ...props }) => (
-	<Box rcx-attachment__details fontScale='p2' color='info' bg='neutral-200' pi='x14' pb='x14' {...props} />
+	<Box rcx-attachment__details fontScale='p2' color='info' bg='neutral-200' pi='x16' pb='x16' {...props} />
 );
 
 export default AttachmentDetails;
