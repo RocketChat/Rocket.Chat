ba9aafa84d9f421c4e60f653cc9ea84373b10995
Diego Sampaio <chinello@gmail.com>
2022-09-14 11:47:48 -0300

    Remove Meteor.startup

--- apps/meteor/server/startup/presenceTroubleshoot.ts
+++ apps/meteor/server/startup/presenceTroubleshoot.ts
@@ -1,15 +1,11 @@
-import { Meteor } from 'meteor/meteor';
-
 import { settings } from '../../app/settings/server';
 import { Presence } from '../sdk';
 
 // maybe this setting should disable the listener to 'presence.status' event on listerners.module.ts
-Meteor.startup(function () {
-	settings.watch('Troubleshoot_Disable_Presence_Broadcast', async function (value) {
-		try {
-			await Presence.toggleBroadcast(!value);
-		} catch (e) {
-			// do nothing
-		}
-	});
+settings.watch('Troubleshoot_Disable_Presence_Broadcast', async function (value) {
+	try {
+		await Presence.toggleBroadcast(!value);
+	} catch (e) {
+		// do nothing
+	}
 });
