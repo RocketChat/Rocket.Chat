2731cb25e924fe68d7308ecdaa3b84590386b796
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-09-14 11:52:51 -0300

    Dispatch Message if take inquiry throws an error

--- apps/meteor/client/views/room/components/body/composer/ComposerOmnichannel/ComposerOmnichannelInquiry.tsx
+++ apps/meteor/client/views/room/components/body/composer/ComposerOmnichannel/ComposerOmnichannelInquiry.tsx
@@ -1,11 +1,13 @@
 import { MessageComposerDisabled, MessageComposerDisabledAction } from '@rocket.chat/ui-composer';
-import { useEndpoint, useMethod, useTranslation } from '@rocket.chat/ui-contexts';
+import { useEndpoint, useMethod, useToastMessageDispatch, useTranslation } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 import React, { ReactElement } from 'react';
 
 import { useOmnichannelRoom } from '../../../../contexts/RoomContext';
 
 export const ComposerOmnichannelInquiry = (): ReactElement => {
+	const dispatchToastMessage = useToastMessageDispatch();
+
 	const room = useOmnichannelRoom();
 	const getInquire = useEndpoint('GET', `/v1/livechat/inquiries.getOne`);
 	const result = useQuery(['inquire', room._id], () =>
@@ -15,15 +17,23 @@ export const ComposerOmnichannelInquiry = (): ReactElement => {
 	);
 	const takeInquiry = useMethod('livechat:takeInquiry');
 
+	const handleTakeInquiry = async (): Promise<void> => {
+		if (!result.isSuccess) {
+			return;
+		}
+		try {
+			await takeInquiry(result.data.inquiry._id);
+		} catch (error) {
+			dispatchToastMessage({ type: 'error', message: error });
+		}
+	};
+
 	const t = useTranslation();
 	return (
 		<footer className='rc-message-box footer'>
 			<MessageComposerDisabled aria-busy={result.isLoading}>
 				{t('you_are_in_preview_mode_of_incoming_livechat')}
-				<MessageComposerDisabledAction
-					disabled={result.isLoading}
-					onClick={async (): Promise<unknown> => result.isSuccess && takeInquiry(result.data.inquiry._id)}
-				>
+				<MessageComposerDisabledAction disabled={result.isLoading} onClick={handleTakeInquiry}>
 					{t('Take_it')}
 				</MessageComposerDisabledAction>
 			</MessageComposerDisabled>
