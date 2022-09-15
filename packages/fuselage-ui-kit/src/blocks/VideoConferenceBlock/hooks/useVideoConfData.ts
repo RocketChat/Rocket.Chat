6e288e465d193227740c3922acc2473740ffdc08
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-09-15 18:29:54 -0300

    review

--- packages/fuselage-ui-kit/src/blocks/VideoConferenceBlock/hooks/useVideoConfData.ts
+++ packages/fuselage-ui-kit/src/blocks/VideoConferenceBlock/hooks/useVideoConfData.ts
@@ -4,5 +4,9 @@ import { useEndpoint } from '@rocket.chat/ui-contexts';
 export const useVideoConfData = ({ callId }: { callId: string }) => {
   const videoConf = useEndpoint('GET', '/v1/video-conference.info');
 
-  return useQuery(['video-conference', callId], () => videoConf({ callId }));
+  return useQuery(['video-conference', callId], () => videoConf({ callId }), {
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    // refetchOnMount: false,
+  });
 };
