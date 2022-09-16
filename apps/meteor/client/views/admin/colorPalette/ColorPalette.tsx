9dd31ec6ee08716e395de9e9d7ca901c4f882207
juliajforesti <juliajforesti@gmail.com>
2022-09-16 14:35:39 -0300

    fix margin

--- apps/meteor/client/views/admin/colorPalette/ColorPalette.tsx
+++ apps/meteor/client/views/admin/colorPalette/ColorPalette.tsx
@@ -1,31 +1,27 @@
 import { Box } from '@rocket.chat/fuselage';
-import React, { ReactElement } from 'react';
+import React, { Fragment, ReactElement } from 'react';
 
 import ColorToken from './ColorToken';
 import { palette } from './palette';
 
-const ColorPalette = (): ReactElement => {
-	console.log('oi');
-
-	return (
-		<Box display='flex' flexDirection='column'>
-			{palette.map((paletteItem) => (
-				<>
-					{paletteItem.category && (
-						<Box fontScale='h2' mb='x16'>
-							{paletteItem.category}
-						</Box>
-					)}
-					{paletteItem.description && <Box fontScale='p2'>{paletteItem.description}</Box>}
-					<Box display='flex' mb='x8'>
-						{paletteItem.list.map((tokenItem) => (
-							<ColorToken item={tokenItem} />
-						))}
+const ColorPalette = (): ReactElement => (
+	<Box display='flex' flexDirection='column' flexWrap='wrap'>
+		{palette.map((paletteItem) => (
+			<Fragment key={paletteItem.category}>
+				{paletteItem.category && (
+					<Box fontScale='h2' mb='x16'>
+						{paletteItem.category}
 					</Box>
-				</>
-			))}
-		</Box>
-	);
-};
+				)}
+				{paletteItem.description && <Box fontScale='p2'>{paletteItem.description}</Box>}
+				<Box display='flex' mb='x8'>
+					{paletteItem.list.map((tokenItem, i) => (
+						<ColorToken item={tokenItem} key={tokenItem.name} position={i} />
+					))}
+				</Box>
+			</Fragment>
+		))}
+	</Box>
+);
 
 export default ColorPalette;
--- apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
+++ apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
@@ -3,31 +3,30 @@ import React, { ReactElement } from 'react';
 
 type ColorTokenProps = {
 	item: { name: string; token: string; color: string; isDark: boolean };
+	position: number;
 };
-const ColorToken = ({ item }: ColorTokenProps): ReactElement => {
-	console.log('oi');
-	return (
-		<Box
-			width='120px'
-			height='120px'
-			backgroundColor={item.color}
-			display='flex'
-			flexDirection='column'
-			justifyContent='space-between'
-			flexShrink={0}
-			m='x4'
-			p='x8'
-			fontSize='10px'
-			color={item.isDark ? 'white' : 'black'}
-			fontWeight='600'
-		>
-			<Box>{item.name}</Box>
-			<Box display='flex' justifyContent='space-between'>
-				<Box>{item.color}</Box>
-				<Box>{item.token}</Box>
-			</Box>
+const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => (
+	<Box
+		width='120px'
+		height='120px'
+		backgroundColor={item.color}
+		display='flex'
+		flexDirection='column'
+		justifyContent='space-between'
+		flexShrink={0}
+		m='x4'
+		mis={position === 0 ? '0' : 'x4'}
+		p='x8'
+		fontSize='10px'
+		color={item.isDark ? 'white' : 'black'}
+		fontWeight='600'
+	>
+		<Box>{item.name}</Box>
+		<Box display='flex' justifyContent='space-between'>
+			<Box>{item.color}</Box>
+			<Box>{item.token}</Box>
 		</Box>
-	);
-};
+	</Box>
+);
 
 export default ColorToken;
