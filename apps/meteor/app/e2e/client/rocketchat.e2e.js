a89911c4e544e5b11bcd6acce56f5728d697f2a8
yash-rajpal <rajpal.yash03@gmail.com>
2022-09-20 21:54:14 +0530

    remove logs & fix lint

--- apps/meteor/app/e2e/client/rocketchat.e2e.js
+++ apps/meteor/app/e2e/client/rocketchat.e2e.js
@@ -358,20 +358,20 @@ class E2E extends Emitter {
 			return message;
 		}
 
-		if(!message?.attachments || !message.attachments[0]?.text || message.attachments[0]?.t !== 'e2e') {
+		if (!message?.attachments || !message.attachments[0]?.text || message.attachments[0]?.t !== 'e2e') {
 			return message;
 		}
 
 		const e2eRoom = await this.getInstanceByRoomId(message.rid);
 		const pinnedMessage = message.attachments[0]?.text;
 
-		if(!e2eRoom || !pinnedMessage) {
+		if (!e2eRoom || !pinnedMessage) {
 			return message;
 		}
 
 		const data = await e2eRoom.decrypt(pinnedMessage);
 
-		if(!data) {
+		if (!data) {
 			return message;
 		}
 
@@ -407,7 +407,6 @@ class E2E extends Emitter {
 
 	async decryptPendingMessages() {
 		return Messages.find({ t: 'e2e', e2e: 'pending' }).forEach(async ({ _id, ...msg }) => {
-			console.log("Msg = ", msg);
 			Messages.direct.update({ _id }, await this.decryptMessage(msg));
 		});
 	}
