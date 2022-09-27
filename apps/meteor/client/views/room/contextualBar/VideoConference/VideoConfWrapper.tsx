60003ee7f978336527f43475b0674be7ef9ba6e1
Pierre <pierre.lehnen@rocket.chat>
2022-09-27 14:33:09 -0300

    temp: load pexip sdk

--- apps/meteor/client/views/room/contextualBar/VideoConference/VideoConfWrapper.tsx
+++ apps/meteor/client/views/room/contextualBar/VideoConference/VideoConfWrapper.tsx
@@ -1,18 +1,39 @@
-import React, { ReactElement, useEffect } from 'react';
+import type { VideoConference, VideoConferenceCapabilities } from '@rocket.chat/core-typings';
+import React, { ReactElement, useEffect, useState } from 'react';
 
-// TODO: props type
+import { getURL } from '../../../../../app/utils/client';
 
-const VideoConfWrapper = (props: any): ReactElement => {
-	console.log(props);
+const VideoConfWrapper = (props: Omit<VideoConference, 'providerData'> & { capabilities: VideoConferenceCapabilities }): ReactElement => {
+	const [sdkState, setSdkState] = useState('none');
 
 	useEffect(() => {
+		if (props.providerName !== 'pexip') {
+			setSdkState('skipped');
+			return;
+		}
+
 		const pexipScript = document.createElement('script');
 
-		pexipScript.src = 'pathToScript';
+		pexipScript.src = getURL('pexrtc.js');
 		pexipScript.async = true;
+		pexipScript.addEventListener('load', () => {
+			setSdkState('loaded');
+		});
+
+		pexipScript.addEventListener('error', () => {
+			setSdkState('error');
+		});
 
 		document.body.appendChild(pexipScript);
-	}, []);
+	}, [props.providerName]);
+
+	if (sdkState === 'none') {
+		return <div>Loading</div>;
+	}
+
+	if (sdkState === 'error') {
+		return <div>Failed to load SDK</div>;
+	}
 
 	return <iframe width='100%' height='100%' src={props.url} />;
 };
