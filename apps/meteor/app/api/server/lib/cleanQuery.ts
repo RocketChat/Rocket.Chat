2266e80955b86ccac062c60ef850142c00b76020
Pierre <pierre.lehnen@rocket.chat>
2022-09-29 19:39:47 -0300

    bug fix

--- apps/meteor/app/api/server/lib/cleanQuery.ts
+++ apps/meteor/app/api/server/lib/cleanQuery.ts
@@ -5,7 +5,7 @@ const denyList = ['constructor', '__proto__', 'prototype'];
 export const removeDangerousProps = (v: Query): Query => {
 	const query = Object.create(null);
 	for (const key in v) {
-		if (v.hasOwnProperty(key) && !denyList.includes(key)) {
+		if (Object.prototype.hasOwnProperty.call(v, key) && !denyList.includes(key)) {
 			query[key] = v[key];
 		}
 	}
