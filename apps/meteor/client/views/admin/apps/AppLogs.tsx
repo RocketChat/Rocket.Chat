b152afb023df042bef8b45a3c4eaa6f7883200e7
rique223 <henrique.jobs1@gmail.com>
2022-10-04 11:25:40 -0300

    refactor: :recycle: Use react query for app logs

--- apps/meteor/client/views/admin/apps/AppLogs.tsx
+++ apps/meteor/client/views/admin/apps/AppLogs.tsx
@@ -1,33 +1,26 @@
 import { Accordion, Box } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
 import React, { ReactElement } from 'react';
 
-import { useEndpointData } from '../../../hooks/useEndpointData';
 import { useFormatDateAndTime } from '../../../hooks/useFormatDateAndTime';
-import { AsyncStatePhase } from '../../../lib/asyncState';
 import AccordionLoading from './AccordionLoading';
 import LogItem from './LogItem';
+import { useLogs } from './hooks/useLogs';
 
 const AppLogs = ({ id }: { id: string }): ReactElement => {
-	const t = useTranslation();
 	const formatDateAndTime = useFormatDateAndTime();
-	const { value, phase: logsPhase, error } = useEndpointData(`/apps/${id}/logs`);
-
-	const loading = logsPhase === AsyncStatePhase.LOADING;
-
-	const isLogsFailing = logsPhase === AsyncStatePhase.REJECTED;
+	const { data, isSuccess, isError, isLoading, error } = useLogs(id);
 
 	return (
 		<>
-			{loading && <AccordionLoading />}
-			{isLogsFailing && (
+			{isLoading && <AccordionLoading />}
+			{isError && (
 				<Box maxWidth='x600' alignSelf='center'>
-					{error ? error.message : t('Unknown_error')}
+					{error}
 				</Box>
 			)}
-			{!loading && logsPhase === AsyncStatePhase.RESOLVED && (
+			{isSuccess && (
 				<Accordion width='100%' alignSelf='center'>
-					{value?.logs?.map((log) => (
+					{data.logs.map((log) => (
 						<LogItem
 							key={log._createdAt}
 							title={`${formatDateAndTime(log._createdAt)}: "${log.method}" (${log.totalTime}ms)`}
