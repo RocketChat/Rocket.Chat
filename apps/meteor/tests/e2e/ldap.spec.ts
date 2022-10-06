d0d4d577e609875f1abd007eeb18b5d342fc080a
Weslley de Campos <weslley.campos@objective.com.br>
2022-10-06 09:42:46 -0300

    test: add steps to configure ldap server

--- apps/meteor/tests/e2e/ldap.spec.ts
+++ apps/meteor/tests/e2e/ldap.spec.ts
@@ -7,12 +7,11 @@ import { expect, test } from './utils/test';
 import { AdminLdap } from './page-objects/admin-ldap';
 
 test.use({ storageState: 'admin-session.json' });
-test.describe('ldap test', async () => {
+test.describe.only('ldap test', async () => {
 	let container: StartedTestContainer;
-	const ldapConnectionUrl = '/admin/settings/LDAP';
 
 	let poAdminLdap: AdminLdap;
-	test.beforeEach(async () => {
+	test.beforeAll(async () => {
 		const buildContext = path.resolve(__dirname, 'fixtures', 'ldap-client');
 
 		container = await (await GenericContainer.fromDockerfile(buildContext).build())
@@ -25,25 +24,27 @@ test.describe('ldap test', async () => {
 		poAdminLdap = new AdminLdap(page);
 	});
 
-	test.afterEach(async () => {
+	test.afterAll(async () => {
 		await container.stop();
 	});
 
 	test('expect connection is ok', async ({ page }) => {
-		console.log(container);
-		await page.goto(ldapConnectionUrl);
-		const isChecked = await poAdminLdap.ldapConnection.inputCheck.isChecked();
-		// FIXME: why is possible verify connection with reload in page
-		if (!isChecked) {
-			await poAdminLdap.ldapConnection.btnEnable.click();
-			await poAdminLdap.ldapConnection.selectLdapServerType();
-			await poAdminLdap.ldapConnection.inputLdapHost.fill('localhost');
-			await poAdminLdap.ldapConnection.btnLdapReconnect.click();
-			await poAdminLdap.ldapConnection.btnLoginFallBack.click();
-			await poAdminLdap.ldapConnection.btnSaveChanges.click();
+		await test.step('expect set ldap configuration', async () => {
+			await page.goto('/admin/settings/LDAP');
+			await poAdminLdap.ldapConnection.inputCheck.waitFor();
+			const isChecked = await poAdminLdap.ldapConnection.inputCheck.isChecked();
+			// FIXME: why is possible verify connection with reload in page
+			if (!isChecked) {
+				await poAdminLdap.ldapConnection.btnEnable.click();
+				await poAdminLdap.ldapConnection.selectLdapServerType();
+				await poAdminLdap.ldapConnection.inputLdapHost.fill('localhost');
+				await poAdminLdap.ldapConnection.btnLdapReconnect.click();
+				await poAdminLdap.ldapConnection.btnLoginFallBack.click();
+				await poAdminLdap.ldapConnection.btnSaveChanges.click();
 
-			await page.reload();
-		}
+				await page.reload();
+			}
+		});
 
 		await poAdminLdap.ldapConnection.btnTestConnection.click();
 		await expect(poAdminLdap.toastSuccess).toBeVisible();
