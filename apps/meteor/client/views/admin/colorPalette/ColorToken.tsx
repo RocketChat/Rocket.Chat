077cceffae1fe652b160638dd242a19b66701657
juliajforesti <juliajforesti@gmail.com>
2022-10-11 16:19:45 -0300

    Add hex input

--- apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
+++ apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
@@ -1,13 +1,11 @@
 import { parseColor } from '@react-stately/color';
-import { Box, Button, Dropdown } from '@rocket.chat/fuselage';
-import React, { ReactElement, useCallback, useEffect, useRef } from 'react';
+import { Box, Button, Dropdown, Input } from '@rocket.chat/fuselage';
+import { useOutsideClick, useToggle } from '@rocket.chat/fuselage-hooks';
+import React, { ReactElement, useCallback, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
-
-import { useDropdownVisibility } from '/client/sidebar/header/hooks/useDropdownVisibility';
 
 import ColorArea from './ColorArea';
 import ColorSlider from './ColorSlider';
-import { useOutsideClick, useToggle } from '@rocket.chat/fuselage-hooks';
 
 type ColorTokenProps = {
 	item: { name: string; token: string; color: string; isDark: boolean; rgb: string };
@@ -20,7 +18,9 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 	const target = useRef(null);
 	const [isVisible, toggle] = useToggle(false);
 
-	const [color, setColor] = React.useState(parseColor(item.rgb).toFormat('hsba'));
+	const [isTyping, setIsTyping] = useState(false);
+	const [input, setInput] = useState(item.color);
+	const [color, setColor] = useState(parseColor(item.rgb).toFormat('hsba'));
 	const [xChannel, yChannel, zChannel] = color.getColorChannels();
 
 	const openColorPicker = (): void => toggle(true);
@@ -34,6 +34,16 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 		[target, reference],
 		useCallback(() => closeColorPicker(), [closeColorPicker]),
 	);
+	const isHexColor = (hex: string): boolean => typeof hex === 'string' && hex.length === 6 && !isNaN(Number(`0x${hex}`));
+
+	useEffect(() => {
+		isTyping && isHexColor(input.replace(/^#/, '')) && setColor(parseColor(input).toFormat('hsba'));
+		setIsTyping(false);
+	}, [input, isTyping]);
+
+	useEffect(() => {
+		setInput(color.toString('hex'));
+	}, [color]);
 
 	return (
 		<>
@@ -47,6 +57,14 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 							<Box display='flex' justifyContent='center' flexDirection='column' mb='x8'>
 								<ColorArea aria-labelledby='hsb-label-id-1' value={color} onChange={setColor} xChannel={xChannel} yChannel={yChannel} />
 								<ColorSlider channel={zChannel} value={color} onChange={setColor} />
+								<Input
+									value={input}
+									onChange={(e): void => {
+										setIsTyping(true);
+										const value = (e.target as HTMLInputElement).value.toUpperCase();
+										setInput(value);
+									}}
+								/>
 							</Box>
 							<Box display='flex' justifyContent='space-between'>
 								<Button onClick={closeColorPicker}>Cancel</Button>
@@ -80,7 +98,7 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 			>
 				<Box>{item.name}</Box>
 				<Box display='flex' justifyContent='space-between'>
-					<Box>{color.toString('rgb')}</Box>
+					<Box>{color.toString('hex')}</Box>
 					<Box>{item.token}</Box>
 				</Box>
 			</Box>
