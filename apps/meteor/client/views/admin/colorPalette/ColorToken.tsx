a9483e7f742fdba47c44fbd68ec9091c1bc965ec
juliajforesti <juliajforesti@gmail.com>
2022-10-11 10:26:42 -0300

    Fix Cancel and Apply behavior. Adjust css

--- apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
+++ apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
@@ -1,13 +1,13 @@
-import { Box, Button, Dropdown } from '@rocket.chat/fuselage';
-import React, { ReactElement, useEffect, useRef } from 'react';
-import { createPortal } from 'react-dom';
 import { parseColor } from '@react-stately/color';
-
-import ColorPicker from './ColorPicker';
+import { Box, Button, Dropdown } from '@rocket.chat/fuselage';
+import React, { ReactElement, useCallback, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 import { useDropdownVisibility } from '/client/sidebar/header/hooks/useDropdownVisibility';
-import ColorSlider from './ColorSlider';
+
 import ColorArea from './ColorArea';
+import ColorSlider from './ColorSlider';
+import { useOutsideClick, useToggle } from '@rocket.chat/fuselage-hooks';
 
 type ColorTokenProps = {
 	item: { name: string; token: string; color: string; isDark: boolean; rgb: string };
@@ -18,45 +18,41 @@ type ColorTokenProps = {
 const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 	const reference = useRef(null);
 	const target = useRef(null);
+	const [isVisible, toggle] = useToggle(false);
 
 	const [color, setColor] = React.useState(parseColor(item.rgb).toFormat('hsba'));
-	const [endColor, setEndColor] = React.useState(color);
 	const [xChannel, yChannel, zChannel] = color.getColorChannels();
 
-	const { isVisible, toggle } = useDropdownVisibility({ reference, target });
-
 	const openColorPicker = (): void => toggle(true);
-	const closeColorPicker = (): void => {
-		setEndColor(parseColor(item.rgb).toFormat('hsba'));
+	const closeColorPicker = useCallback((): void => {
 		setColor(parseColor(item.rgb).toFormat('hsba'));
 		toggle(false);
-	};
+	}, [item.rgb, toggle]);
+	const applyColor = (): void => toggle(false);
+
+	useOutsideClick(
+		[target, reference],
+		useCallback(() => closeColorPicker(), [closeColorPicker]),
+	);
 
 	return (
 		<>
 			{isVisible &&
 				createPortal(
-					<Dropdown reference={reference} ref={target}>
+					<Dropdown reference={reference} ref={target} key={item.name}>
 						<Box pi='x8'>
 							<Box fontSize='p2b' fontWeight='p2b' display='flex' justifyContent='center'>
 								{item.name}
 							</Box>
 							<Box display='flex' justifyContent='center' flexDirection='column' mb='x8'>
-								<ColorArea
-									aria-labelledby='hsb-label-id-1'
-									value={color}
-									onChange={setColor}
-									onChangeEnd={setEndColor}
-									xChannel={xChannel}
-									yChannel={yChannel}
-								/>
-								<ColorSlider channel={zChannel} value={color} onChange={setColor} onChangeEnd={setEndColor} />
+								<ColorArea aria-labelledby='hsb-label-id-1' value={color} onChange={setColor} xChannel={xChannel} yChannel={yChannel} />
+								<ColorSlider channel={zChannel} value={color} onChange={setColor} />
 							</Box>
-							<Box>
-								<Button mi='x2' onClick={closeColorPicker}>
+							<Box display='flex' justifyContent='space-between'>
+								<Button onClick={closeColorPicker}>
 									Cancel
 								</Button>
-								<Button mi='x2' primary>
+								<Button primary onClick={applyColor}>
 									Apply
 								</Button>
 							</Box>
@@ -66,10 +62,11 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 				)}
 
 			<Box
+				key={item.name}
 				ref={reference}
 				width='120px'
 				height='120px'
-				backgroundColor={endColor.toString('rgb')}
+				backgroundColor={color.toString('rgb')}
 				display='flex'
 				flexDirection='column'
 				justifyContent='space-between'
@@ -85,7 +82,7 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 			>
 				<Box>{item.name}</Box>
 				<Box display='flex' justifyContent='space-between'>
-					<Box>{endColor.toString('rgb')}</Box>
+					<Box>{color.toString('rgb')}</Box>
 					<Box>{item.token}</Box>
 				</Box>
 			</Box>
