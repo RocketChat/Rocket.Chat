a9a92a38150c09057f9b98ea59a0388c7843d1a7
juliajforesti <juliajforesti@gmail.com>
2022-10-13 15:21:40 -0300

    Add function to check if color is light or dark to define text color

--- apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
+++ apps/meteor/client/views/admin/colorPalette/ColorToken.tsx
@@ -1,7 +1,7 @@
 import { parseColor } from '@react-stately/color';
 import { Box, Button, Dropdown, Input } from '@rocket.chat/fuselage';
 import { useOutsideClick, useToggle } from '@rocket.chat/fuselage-hooks';
-import React, { ReactElement, useCallback, useEffect, useRef, useState } from 'react';
+import React, { ReactElement, SyntheticEvent, useCallback, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import ColorArea from './ColorArea';
@@ -28,13 +28,28 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 		setColor(parseColor(item.rgb).toFormat('hsba'));
 		toggle(false);
 	}, [item.rgb, toggle]);
+
 	const applyColor = (): void => toggle(false);
+
+	const isHexColor = (hex: string): boolean => typeof hex === 'string' && hex.length === 6 && !isNaN(Number(`0x${hex}`));
+	const isLightColor = (color: string): boolean => {
+		const hex = color.replace('#', '');
+		const r = parseInt(hex.substring(0, 0 + 2), 16);
+		const g = parseInt(hex.substring(2, 2 + 2), 16);
+		const b = parseInt(hex.substring(4, 4 + 2), 16);
+		const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+		return brightness > 155;
+	};
+	const handleInputChange = (e: SyntheticEvent): void => {
+		setIsTyping(true);
+		const value = (e.target as HTMLInputElement).value.toUpperCase();
+		setInput(value);
+	};
 
 	useOutsideClick(
 		[target, reference],
 		useCallback(() => closeColorPicker(), [closeColorPicker]),
 	);
-	const isHexColor = (hex: string): boolean => typeof hex === 'string' && hex.length === 6 && !isNaN(Number(`0x${hex}`));
 
 	useEffect(() => {
 		isTyping && isHexColor(input.replace(/^#/, '')) && setColor(parseColor(input).toFormat('hsba'));
@@ -57,15 +72,7 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 							<Box display='flex' justifyContent='center' flexDirection='column' mb='x8'>
 								<ColorArea aria-labelledby='hsb-label-id-1' value={color} onChange={setColor} xChannel={xChannel} yChannel={yChannel} />
 								<ColorSlider channel={zChannel} value={color} onChange={setColor} />
-								<Input
-									mbs='x8'
-									value={input}
-									onChange={(e): void => {
-										setIsTyping(true);
-										const value = (e.target as HTMLInputElement).value.toUpperCase();
-										setInput(value);
-									}}
-								/>
+								<Input mbs='x8' value={input} onChange={handleInputChange} />
 							</Box>
 							<Box display='flex' justifyContent='space-between'>
 								<Button onClick={closeColorPicker}>Cancel</Button>
@@ -92,7 +99,7 @@ const ColorToken = ({ item, position }: ColorTokenProps): ReactElement => {
 				mis={position === 0 ? '0' : 'x4'}
 				p='x8'
 				fontSize='10px'
-				color={item.isDark ? 'white' : 'black'}
+				color={isLightColor(color.toString('hex')) ? 'black' : 'white'}
 				fontWeight='600'
 				onClick={openColorPicker}
 				style={{ cursor: 'pointer' }}
