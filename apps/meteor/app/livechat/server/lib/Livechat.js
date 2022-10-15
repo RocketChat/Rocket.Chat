b2a34080c6067634c40d80ced21c542b2f6a9d18
Kevin Aleman <kaleman960@gmail.com>
2022-10-14 18:05:39 -0700

    [FIX] Remove voip extension when agent is removed or deactivated (#26960)

--- apps/meteor/app/livechat/server/lib/Livechat.js
+++ apps/meteor/app/livechat/server/lib/Livechat.js
@@ -945,7 +945,7 @@ export const Livechat = {
 			Users.removeLivechatData(_id);
 			this.setUserStatusLivechat(_id, 'not-available');
 			LivechatDepartmentAgents.removeByAgentId(_id);
-			Promise.await(LivechatVisitors.removeContactManagerByUsername(username));
+			Promise.await(Promise.all([LivechatVisitors.removeContactManagerByUsername(username), UsersRaw.unsetExtension(_id)]));
 			return true;
 		}
 
