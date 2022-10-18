8c92240dc00597df4c8046a31bb4cb47e1d088bf
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-10-18 01:29:00 -0300

    ..

--- packages/ui-client/src/components/Card/Card.tsx
+++ packages/ui-client/src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ const Card: FC<CardProps> = ({ variant, ...props }: CardProps) => (
 		height='full'
 		display='flex'
 		flexDirection='column'
-		backgroundColor={variant === 'light' ? 'white' : 'tint'}
+		backgroundColor={variant === 'light' ? 'light' : 'tint'}
 		{...props}
 	/>
 );
