107c57b2473837b903a992d8cd2fea905ba4b06d
Filipe Marins <filipemarins123@gmail.com>
2022-10-27 11:30:57 -0300

    add category list

--- apps/meteor/client/views/directory/CategoriesTab.tsx
+++ apps/meteor/client/views/directory/CategoriesTab.tsx
@@ -1,24 +1,13 @@
-import { Box, Table, Avatar, Icon, Button } from '@rocket.chat/fuselage';
-import { useAutoFocus, useMediaQuery, useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import { useRoute, useSetModal, useTranslation } from '@rocket.chat/ui-contexts';
-import React, { useMemo, useState, useCallback, ReactElement, FC } from 'react';
+import { Box, Table, Button } from '@rocket.chat/fuselage';
+import { useAutoFocus, useLocalStorage, useMutableCallback } from '@rocket.chat/fuselage-hooks';
+import { useSetModal, useTranslation } from '@rocket.chat/ui-contexts';
+import React, { useMemo, useCallback, ReactElement, FC } from 'react';
 
 import { popover } from '../../../app/ui-utils/client';
 import CreateCategory from '../../components/CreateCategory';
 import FilterByText from '../../components/FilterByText';
 import GenericTable from '../../components/GenericTable';
-import MarkdownText from '../../components/MarkdownText';
-import { useEndpointData } from '../../hooks/useEndpointData';
-import { useFormatDate } from '../../hooks/useFormatDate';
-import { roomCoordinator } from '../../lib/rooms/roomCoordinator';
-import RoomTags from './RoomTags';
-import { useQuery } from './hooks';
 
-const style = {
-	whiteSpace: 'nowrap',
-	textOverflow: 'ellipsis',
-	overflow: 'hidden',
-};
 const useReactModal = (Component: FC<any>): ((e: React.MouseEvent<HTMLElement>) => void) => {
 	const setModal = useSetModal();
 
@@ -37,106 +26,42 @@ const useReactModal = (Component: FC<any>): ((e: React.MouseEvent<HTMLElement>) 
 
 const CategoriesTable = (): ReactElement => {
 	const t = useTranslation();
-	const [sort, setSort] = useState(['name', 'asc']);
-	const [params, setParams] = useState({ current: 0, itemsPerPage: 25 });
 
 	const refAutoFocus = useAutoFocus(true);
-	const mediaQuery = useMediaQuery('(min-width: 768px)');
-
-	const onHeaderClick = useCallback(
-		(id) => {
-			const [sortBy, sortDirection] = sort;
-
-			if (sortBy === id) {
-				setSort([id, sortDirection === 'asc' ? 'desc' : 'asc']);
-				return;
-			}
-			setSort([id, 'asc']);
-		},
-		[sort],
-	);
 
 	const createCategory = useReactModal(CreateCategory);
 
 	const header = useMemo(
 		() =>
 			[
-				<GenericTable.HeaderCell key={'name'} direction={sort[1]} active={sort[0] === 'name'} onClick={onHeaderClick} sort='name'>
-					{t('Name')}
+				<GenericTable.HeaderCell key={'name'}>Category</GenericTable.HeaderCell>,
+				<GenericTable.HeaderCell key={'channelsCount'}>{t('Channels')}</GenericTable.HeaderCell>,
+				<GenericTable.HeaderCell key={'createdAt'} sort='createdAt' style={{ width: '150px' }}>
+					{t('Created_at')}
 				</GenericTable.HeaderCell>,
-				<GenericTable.HeaderCell key={'channelsCount'} style={{ width: '100px' }}>
-					{t('Channels')}
-				</GenericTable.HeaderCell>,
-				mediaQuery && (
-					<GenericTable.HeaderCell
-						key={'createdAt'}
-						direction={sort[1]}
-						active={sort[0] === 'createdAt'}
-						onClick={onHeaderClick}
-						sort='createdAt'
-						style={{ width: '150px' }}
-					>
-						{t('Created_at')}
-					</GenericTable.HeaderCell>
-				),
 			].filter(Boolean),
-		[sort, onHeaderClick, t, mediaQuery],
+		[t],
 	);
 
-	const channelsRoute = useRoute('channel');
-	const groupsRoute = useRoute('group');
+	const [category] = useLocalStorage('rc-category', {});
 
-	const query = useQuery(params, sort, 'teams');
+	const data = Object.entries(category);
 
-	const { value: data = {} } = useEndpointData('/v1/directory', query);
+	const renderRow = useCallback((category) => {
+		const { 0: name, 1: channels } = category;
 
-	const onClick = useMemo(
-		() => (name, type) => (e) => {
-			if (e.type === 'click' || e.key === 'Enter') {
-				type === 'c' ? channelsRoute.push({ name }) : groupsRoute.push({ name });
-			}
-		},
-		[channelsRoute, groupsRoute],
-	);
-
-	const formatDate = useFormatDate();
-	const renderRow = useCallback(
-		(team) => {
-			const { _id, ts, t, name, fname, topic, roomsCount } = team;
-			const avatarUrl = roomCoordinator.getRoomDirectives(t)?.getAvatarPath(team);
-
-			return (
-				<Table.Row key={_id} onKeyDown={onClick(name, t)} onClick={onClick(name, t)} tabIndex={0} role='link' action>
-					<Table.Cell>
-						<Box display='flex'>
-							<Box flexGrow={0}>
-								<Avatar size='x40' title={fname || name} url={avatarUrl} />
-							</Box>
-							<Box grow={1} mi='x8' style={style}>
-								<Box display='flex' alignItems='center'>
-									<Icon name={roomCoordinator.getIcon(team)} color='hint' />{' '}
-									<Box fontScale='p2m' mi='x4'>
-										{fname || name}
-									</Box>
-									<RoomTags room={team} style={style} />
-								</Box>
-								{topic && <MarkdownText variant='inlineWithoutBreaks' fontScale='p2' color='hint' style={style} content={topic} />}
-							</Box>
-						</Box>
-					</Table.Cell>
-					<Table.Cell fontScale='p2' color='hint' style={style}>
-						{roomsCount}
-					</Table.Cell>
-					{mediaQuery && (
-						<Table.Cell fontScale='p2' color='hint' style={style}>
-							{formatDate(ts)}
-						</Table.Cell>
-					)}
-				</Table.Row>
-			);
-		},
-		[formatDate, mediaQuery, onClick],
-	);
+		return (
+			<Table.Row key={category[0]} tabIndex={0} role='link' action>
+				<Table.Cell>{name}</Table.Cell>
+				<Table.Cell fontScale='p2' color='hint'>
+					{channels.map((channel) => channel)}
+				</Table.Cell>
+				<Table.Cell fontScale='p2' color='hint'>
+					{new Date().toDateString()}
+				</Table.Cell>
+			</Table.Row>
+		);
+	}, []);
 
 	return (
 		<GenericTable
@@ -150,9 +75,8 @@ const CategoriesTable = (): ReactElement => {
 				</Box>
 			)}
 			renderRow={renderRow}
-			results={data.result}
-			setParams={setParams}
-			total={data.total}
+			results={data}
+			total={Object.keys(category).length}
 		/>
 	);
 };
