b1e072506c12f4060076fe6ccb016f6646065bf6
gabriellsh <gabriel.henriques@rocket.chat>
2022-10-31 12:26:58 -0300

    Remove unused util

--- apps/meteor/tests/e2e/utils/getSettingValueById.ts
+++
@@ -1,5 +0,0 @@
-import type { BaseTest } from './test';
-
-export const getSettingById = (api: BaseTest['api'], settingId: string): Promise<unknown> => {
-	return api.get(`/settings/${settingId}`);
-};
