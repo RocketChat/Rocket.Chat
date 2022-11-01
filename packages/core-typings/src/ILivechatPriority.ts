4f7c8897e817d887484bcf4061ba9c4578fd66a7
Kevin Aleman <kaleman960@gmail.com>
2022-11-01 12:49:22 -0600

    Add translations & remove icon prop in favor of i18n

--- packages/core-typings/src/ILivechatPriority.ts
+++ packages/core-typings/src/ILivechatPriority.ts
@@ -3,7 +3,7 @@ import type { IRocketChatRecord } from './IRocketChatRecord';
 export interface ILivechatPriority extends IRocketChatRecord {
 	name: string;
 	defaultValue: string;
-	icon: string;
+	i18n: string;
 	sortItem: number;
 
 	// Whether the priority has been modified by the user or not
