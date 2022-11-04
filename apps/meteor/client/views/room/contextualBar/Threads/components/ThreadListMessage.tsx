fc6a60ae8b85c3394f97e07312d178b968eaa95a
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2022-11-03 23:42:30 -0300

    [FIX] ThreadRow hover color (#27166)

--- apps/meteor/client/views/room/contextualBar/Threads/components/ThreadListMessage.tsx
+++ apps/meteor/client/views/room/contextualBar/Threads/components/ThreadListMessage.tsx
@@ -24,7 +24,7 @@ type ThreadListMessageProps = {
 	all: boolean;
 	tlm: number;
 	className?: string | string[];
-} & Omit<ComponentProps<typeof Message>, 'className'>;
+} & Omit<ComponentProps<typeof Message>, 'className' | 'is'>;
 
 function ThreadListMessage({
 	_id,
@@ -49,8 +49,8 @@ function ThreadListMessage({
 	const button = !following ? 'bell-off' : 'bell';
 	const actionLabel = t(!following ? 'Not_Following' : 'Following');
 	return (
-		<Box className={[className, !following && followStyle].flat()} pb='x8'>
-			<Message {...props}>
+		<Box className={[className, !following && followStyle].flat()}>
+			<Box pbs='x16' is={Message} {...props}>
 				<Message.LeftContainer>
 					<UserAvatar username={username} className='rcx-message__avatar' size='x36' />
 				</Message.LeftContainer>
@@ -95,7 +95,7 @@ function ThreadListMessage({
 						{(mention && <NotificationStatus.Me />) || (all && <NotificationStatus.All />) || (unread && <NotificationStatus.Unread />)}
 					</Box>
 				</Message.ContainerFixed>
-			</Message>
+			</Box>
 		</Box>
 	);
 }
