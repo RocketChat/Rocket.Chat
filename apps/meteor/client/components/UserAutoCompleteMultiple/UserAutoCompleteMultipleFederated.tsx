1f6aad30f5d71f7e8071d7610c6639a048ed42ba
yash-rajpal <rajpal.yash03@gmail.com>
2022-11-08 21:46:07 +0530

    handle undefined values

--- apps/meteor/client/components/UserAutoCompleteMultiple/UserAutoCompleteMultipleFederated.tsx
+++ apps/meteor/client/components/UserAutoCompleteMultiple/UserAutoCompleteMultipleFederated.tsx
@@ -75,13 +75,13 @@ const UserAutoCompleteMultipleFederated = ({
 				filter={filter}
 				setFilter={setFilter}
 				renderSelected={({ value, onMouseDown }: { value: string; onMouseDown: () => void }): ReactElement => {
-					const currentCachedOption = selectedCache[value];
+					const currentCachedOption = selectedCache[value] || {};
 
 					return (
 						<Chip key={value} {...props} height='x20' onMouseDown={onMouseDown} mie='x4' mb='x2'>
 							{currentCachedOption._federated ? <Icon size='x20' name='globe' /> : <UserAvatar size='x20' username={value} />}
 							<Box is='span' margin='none' mis='x4'>
-								{currentCachedOption.name || currentCachedOption.username}
+								{currentCachedOption.name || currentCachedOption.username || value}
 							</Box>
 						</Chip>
 					);
