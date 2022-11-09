d6f9b0ed6bfa8c9eb2a4f72293fc18eff96f53ba
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-11-09 11:19:12 -0300

    ...

--- apps/meteor/client/providers/UserProvider.tsx
+++ apps/meteor/client/providers/UserProvider.tsx
@@ -1,4 +1,4 @@
-import type { ILoginServiceConfiguration, IRoom, ISubscription, IUser } from '@rocket.chat/core-typings';
+import type { IRoom, ISubscription, IUser } from '@rocket.chat/core-typings';
 import { UserContext, LoginService, useSetting } from '@rocket.chat/ui-contexts';
 import { Meteor } from 'meteor/meteor';
 import React, { FC, useEffect, useMemo } from 'react';
