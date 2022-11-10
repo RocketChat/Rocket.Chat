e749960721793110ca370dea2a9be38b4ebdd78c
Vassilis Kritharakis <bkrith@hotmail.com>
2022-11-10 20:16:21 +0200

    improve loading, fix bugs and add mark as read

--- apps/meteor/client/views/unreads/UnreadsPage.tsx
+++ apps/meteor/client/views/unreads/UnreadsPage.tsx
@@ -1,88 +1,112 @@
-import { Accordion, Box, Button, ButtonGroup, Icon, Skeleton } from '@rocket.chat/fuselage';
+import { Accordion, Box, Button, ButtonGroup, Icon } from '@rocket.chat/fuselage';
 import { Header } from '@rocket.chat/ui-client';
-import { useTranslation } from '@rocket.chat/ui-contexts';
-import React, { FC, useMemo } from 'react';
+import { useEndpoint, useTranslation } from '@rocket.chat/ui-contexts';
+import React, { FC, memo, useEffect, useMemo, useState } from 'react';
 
-import MarkdownText from '../../components/MarkdownText';
 import Page from '../../components/Page';
+import PageSkeleton from '../../components/PageSkeleton';
+import { MessageWithMdEnforced } from '../room/MessageList/lib/parseMessageTextToAstMarkdown';
 import AccordionHeader from './components/body/AccordionHeader';
 import ResultMessage from './components/body/ResultMessage';
 import MessageList from './components/messages/MessageList';
 import { useUnreads } from './hooks/useUnreads';
 
+import './styles/accordion.css';
+
 const UnreadsPage: FC = () => {
 	const t = useTranslation();
-
-	const [loading, error, unreads] = useUnreads();
+	const readMessages = useEndpoint('POST', '/v1/subscriptions.read');
+	const [loading, error, unreads, fetchMessages] = useUnreads();
+	const [expandedItem, setExpandedItem] = useState(null);
+	const [activeMessages, setActiveMessages] = useState<MessageWithMdEnforced[]>([]);
 
 	const totalMessages = useMemo(() => {
 		let total = 0;
 
-		unreads.forEach((room) => {
-			total += room?.messages?.length || 0;
+		unreads?.forEach((room) => {
+			total += room?.unread + (room?.tunread?.length || 0);
 		});
 
 		return total;
 	}, [unreads]);
 
+	async function handleMarkAll(): Promise<void> {
+		if (unreads) await Promise.all(unreads.map((room) => readMessages({ rid: room.rid })));
+		setExpandedItem(null);
+		setActiveMessages([]);
+	}
+
+	async function handleMark(rid: string): Promise<void> {
+		await readMessages({ rid });
+		setExpandedItem(null);
+		setActiveMessages([]);
+	}
+
+	async function getMessages(room: any): Promise<void> {
+		if (expandedItem === room._id) {
+			setExpandedItem(null);
+			setActiveMessages([]);
+			return;
+		}
+		const messages = await fetchMessages(room);
+		setExpandedItem(room._id);
+		setActiveMessages(messages);
+	}
+
+	useEffect(() => {
+		unreads?.forEach(async (room) => {
+			if (expandedItem === room._id) {
+				const messages = await fetchMessages(room);
+				setExpandedItem(room._id);
+				setActiveMessages(messages);
+			}
+		});
+	}, [unreads, expandedItem, fetchMessages]);
+
 	return (
 		<Page>
-			<Page.Header
-				title={
-					<>
-						<Header.Content.Row>
-							<Header.Title is='h1'>{t('Unread_Messages')}</Header.Title>
-						</Header.Content.Row>
-						<Header.Content.Row>
-							<Header.Subtitle is='h2'>
-								<MarkdownText
-									parseEmoji={true}
-									variant='inlineWithoutBreaks'
-									withTruncatedText
-									content={t('Total_unreads').replace('{messages}', totalMessages.toString())}
-								/>
-							</Header.Subtitle>
-						</Header.Content.Row>
-					</>
-				}
-			>
-				<ButtonGroup>
-					<Button
-						onClick={(): void => {
-							console.log('test');
-						}}
+			{error && !loading && <ResultMessage />}
+			{unreads && !unreads.length && !loading && <ResultMessage empty />}
+			{!!loading && <PageSkeleton />}
+			{unreads && unreads.length > 0 && !loading && (
+				<>
+					<Page.Header
+						title={
+							<Header.Content.Row>
+								<Header.Title is='h1'>{`${totalMessages} ${t('Unread_Messages')}`}</Header.Title>
+							</Header.Content.Row>
+						}
 					>
-						<Icon name={'flag'} size='x20' margin='4x' />
-						<span style={{ marginLeft: '10px' }}>{'Mark All Unread'}</span>
-					</Button>
-				</ButtonGroup>
-			</Page.Header>
-
-			<Page.Content>
-				<Page.ScrollableContentWithShadow>
-					<Box marginBlock='none' marginInline='auto' width='full'>
-						{error && !loading && <ResultMessage />}
-						{!unreads.length && !loading && <ResultMessage empty />}
-						{unreads.length && !loading ? (
+						<ButtonGroup>
+							<Button onClick={handleMarkAll}>
+								<Icon name={'flag'} size='x20' margin='4x' />
+								<span style={{ marginLeft: '10px' }}>{t('Mark_all_as_read_short')}</span>
+							</Button>
+						</ButtonGroup>
+					</Page.Header>
+					<Page.ScrollableContentWithShadow>
+						<Box marginBlock='none' paddingBlock='none' marginInline='auto' width='full'>
 							<Accordion borderBlockStyle='unset'>
 								{unreads.map((room) => (
-									<Accordion.Item key={room._id} title={<AccordionHeader room={room} />}>
-										<MessageList rid={room._id} messages={room.messages} />
-									</Accordion.Item>
+									<div className='unreadsAccordionHeader' key={room._id}>
+										<Accordion.Item
+											title={<AccordionHeader room={room} handleMark={(): Promise<void> => handleMark(room.rid)} />}
+											expanded={expandedItem === room._id}
+											onToggle={(): void => {
+												getMessages(room);
+											}}
+										>
+											<MessageList messages={activeMessages} rid={room.rid} />
+										</Accordion.Item>
+									</div>
 								))}
 							</Accordion>
-						) : (
-							<Box is='p' color='hint' fontScale='p2'>
-								<Skeleton />
-								<Skeleton />
-								<Skeleton width='75%' />
-							</Box>
-						)}
-					</Box>
-				</Page.ScrollableContentWithShadow>
-			</Page.Content>
+						</Box>
+					</Page.ScrollableContentWithShadow>
+				</>
+			)}
 		</Page>
 	);
 };
 
-export default UnreadsPage;
+export default memo(UnreadsPage);
