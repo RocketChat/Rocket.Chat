4bed09490311a5752d60cd61ff7363a6c5128346
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-11-10 21:15:45 -0300

    Remove commented code

--- packages/web-ui-registration/src/components/LoginTerms.tsx
+++ packages/web-ui-registration/src/components/LoginTerms.tsx
@@ -1,11 +1,8 @@
 import { Trans } from 'react-i18next';
-// import { useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import { Link, HorizontalWizardLayoutCaption } from '@rocket.chat/layout';
 
-export const LoginTerms = (): ReactElement | null => {
-	// const loginTerms = useSetting('Layout_Login_Terms') as string;
-
+export const LoginTerms = (): ReactElement => {
 	return (
 		<HorizontalWizardLayoutCaption>
 			<Trans
@@ -13,17 +10,8 @@ export const LoginTerms = (): ReactElement | null => {
 				values={{ terms: 'Terms of Service', privacyPolicy: 'Privacy Policy', legalNotice: 'Legal Notice' }}
 				components={[<Link href='/terms-of-service' />, <Link href='/privacy-policy' />, <Link href='/legal-notice' />]}
 			/>
-			{/* <div dangerouslySetInnerHTML={{ __html: loginTerms }} /> */}
 		</HorizontalWizardLayoutCaption>
 	);
-	// return loginTerms ? <div dangerouslySetInnerHTML={{ __html: loginTerms }} /> : null;
 };
-// {
-// 	/* <a href="terms-of-service">Terms of Service</a>
-// <a href="privacy-policy">Privacy Policy</a> */
-// 	{
-// 		/* <a href="legal-notice">Legal Notice</a> */
-// 	}
-// }
 
 export default LoginTerms;
