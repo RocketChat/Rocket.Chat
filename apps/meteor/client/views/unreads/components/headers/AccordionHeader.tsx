d650e724629812d3dec8300537bd48fa0130bf5f
Vassilis Kritharakis <bkrith@hotmail.com>
2022-11-12 21:03:47 +0200

    undo and fix bugs

--- apps/meteor/client/views/unreads/components/headers/AccordionHeader.tsx
+++ apps/meteor/client/views/unreads/components/headers/AccordionHeader.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonGroup, Icon } from '@rocket.chat/fuselage';
 import { Header } from '@rocket.chat/ui-client';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import React, { FC } from 'react';
@@ -6,8 +7,13 @@ import { getAvatarURL } from '../../../../../app/utils/lib/getAvatarURL';
 import MarkdownText from '../../../../components/MarkdownText';
 import RoomAvatar from '../../../../components/avatar/RoomAvatar';
 import { useRoomIcon } from '../../../../hooks/useRoomIcon';
+import { IUnreadRoom } from '../../hooks/useUnreads';
 
-const AccordionHeader: FC<{ room: any }> = ({ room }) => {
+const AccordionHeader: FC<{ room: any; handleRedirect: () => Promise<void>; handleMark: (room: IUnreadRoom) => Promise<void> }> = ({
+	room,
+	handleMark,
+	handleRedirect,
+}) => {
 	const t = useTranslation();
 	const icon = useRoomIcon(room);
 	const defaultUrl = room.prid ? getAvatarURL({ roomId: room.prid }) : getAvatarURL({ username: `@${room.name}` });
@@ -28,11 +34,26 @@ const AccordionHeader: FC<{ room: any }> = ({ room }) => {
 							parseEmoji={true}
 							variant='inlineWithoutBreaks'
 							withTruncatedText
-							content={t('Total_unreads').replace('{messages}', room?.unread + (room?.tunread?.length || 0))}
+							content={(room.undo ? t('Total_reads') : t('Total_unreads')).replace(
+								'{messages}',
+								room?.unread + (room?.tunread?.length || 0),
+							)}
 						/>
 					</Header.Subtitle>
 				</Header.Content.Row>
 			</Header.Content>
+			{room.undo && (
+				<ButtonGroup>
+					<Button small onClick={(): Promise<void> => handleRedirect()} backgroundColor='transparent' borderColor='transparent'>
+						<Icon name={'reply-directly'} size='x20' margin='4x' />
+						<span style={{ marginLeft: '8px' }}>{t('Jump_to')}</span>
+					</Button>
+					<Button small onClick={(): Promise<void> => handleMark(room)} backgroundColor='transparent' borderColor='transparent'>
+						<Icon name={'flag'} size='x20' margin='4x' />
+						<span style={{ marginLeft: '10px' }}>{t('Undo')}</span>
+					</Button>
+				</ButtonGroup>
+			)}
 		</Header>
 	);
 };
