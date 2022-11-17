216ae6ef55c313a34bc31821a27055261aae65e3
rique223 <henrique.jobs1@gmail.com>
2022-11-17 17:11:05 -0300

    style: :lipstick: Refactor app details page header style
    Increased the rightside margin of the app avatar, removed the 'by' substring from the app author metadata and changed the fontScale and colors for the metadata section and short description.

--- apps/meteor/client/views/admin/apps/AppDetailsPage/AppDetailsPageHeader.tsx
+++ apps/meteor/client/views/admin/apps/AppDetailsPage/AppDetailsPageHeader.tsx
@@ -11,12 +11,12 @@ import AppStatus from './tabs/AppStatus';
 
 const AppDetailsPageHeader = ({ app }: { app: App }): ReactElement => {
 	const t = useTranslation();
-	const { iconFileData, name, author, version, iconFileContent, installed, isSubscribed, modifiedAt, bundledIn } = app;
+	const { iconFileData, name, author, version, iconFileContent, installed, isSubscribed, modifiedAt, bundledIn, shortDescription } = app;
 	const lastUpdated = modifiedAt && moment(modifiedAt).fromNow();
 
 	return (
 		<Box display='flex' flexDirection='row' mbe='x20' w='full'>
-			<AppAvatar size='x124' mie='x20' iconFileContent={iconFileContent} iconFileData={iconFileData} />
+			<AppAvatar size='x124' mie='x32' iconFileContent={iconFileContent} iconFileData={iconFileData} />
 			<Box display='flex' flexDirection='column'>
 				<Box display='flex' flexDirection='row' alignItems='center' mbe='x8'>
 					<Box fontScale='h1' mie='x8'>
@@ -24,21 +24,31 @@ const AppDetailsPageHeader = ({ app }: { app: App }): ReactElement => {
 					</Box>
 					{bundledIn && Boolean(bundledIn.length) && <BundleChips bundledIn={bundledIn} />}
 				</Box>
-				{app?.shortDescription && <Box mbe='x16'>{app.shortDescription}</Box>}
+				{shortDescription && (
+					<Box fontScale='p1' color='default' mbe='x16'>
+						{shortDescription}
+					</Box>
+				)}
 				<Box display='flex' flexDirection='row' alignItems='center' mbe='x16'>
 					<AppStatus app={app} installed={installed} isAppDetailsPage={true} isSubscribed={isSubscribed} />
 					{(installed || isSubscribed) && <AppMenu app={app} mis='x8' />}
 				</Box>
 				<Box display='flex' flexDirection='row' color='hint' alignItems='center'>
-					<Box fontScale='p2m' mie='x16'>
-						{t('By_author', { author: author?.name })}
+					<Box fontScale='c1' mie='x16'>
+						{author?.name}
 					</Box>
-					<Box is='span'> | </Box>
-					<Box mi='x16'>{t('Version_version', { version })}</Box>
+					<Box fontScale='c1' color='stroke-light'>
+						|
+					</Box>
+					<Box fontScale='c1' mi='x16'>
+						{t('Version_version', { version })}
+					</Box>
 					{lastUpdated && (
 						<>
-							<Box is='span'> | </Box>
-							<Box mis='x16'>
+							<Box fontScale='c1' color='stroke-light'>
+								|
+							</Box>
+							<Box fontScale='c1' mis='x16'>
 								{t('Marketplace_app_last_updated', {
 									lastUpdated,
 								})}
