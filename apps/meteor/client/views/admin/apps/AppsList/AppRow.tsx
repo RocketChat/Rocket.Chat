bb89db82d01b99b8a9a239688e12c6df8a46408a
rique223 <henrique.jobs1@gmail.com>
2022-11-17 17:34:47 -0300

    Typecheck

--- apps/meteor/client/views/admin/apps/AppsList/AppRow.tsx
+++ apps/meteor/client/views/admin/apps/AppsList/AppRow.tsx
@@ -94,7 +94,7 @@ const AppRow = (props: AppRowProps): ReactElement => {
 				{canUpdate && <Badge small variant='primary' />}
 				<AppStatus app={props} isAppDetailsPage={false} installed={installed} mis='x4' />
 				<Box minWidth='x32'>
-					<AppMenu app={props} mis='x4' />
+					<AppMenu app={props} isAppDetailsPage={false} mis='x4' />
 				</Box>
 			</Box>
 		</Box>
