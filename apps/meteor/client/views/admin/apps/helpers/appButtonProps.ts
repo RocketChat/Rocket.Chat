e8be9a1085a3e22883587cd31e87243ea48b6be3
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-11-21 15:55:02 -0300

    move more functions from helper.ts to new files

---
+++ apps/meteor/client/views/admin/apps/helpers/appButtonProps.ts
@@ -0,0 +1,79 @@
+import { App } from '@rocket.chat/core-typings';
+import semver from 'semver';
+
+type appButtonResponseProps = {
+	action: 'update' | 'install' | 'purchase';
+	icon?: 'reload';
+	label: 'Update' | 'Install' | 'Subscribe' | 'See Pricing' | 'Try now' | 'Buy';
+};
+
+export const appButtonProps = ({
+	installed,
+	version,
+	marketplaceVersion,
+	isPurchased,
+	price,
+	purchaseType,
+	subscriptionInfo,
+	pricingPlans,
+	isEnterpriseOnly,
+}: App): appButtonResponseProps | undefined => {
+	const canUpdate = installed && version && marketplaceVersion && semver.lt(version, marketplaceVersion);
+	if (canUpdate) {
+		return {
+			action: 'update',
+			icon: 'reload',
+			label: 'Update',
+		};
+	}
+
+	if (installed) {
+		return;
+	}
+
+	const canDownload = isPurchased;
+	if (canDownload) {
+		return {
+			action: 'install',
+			label: 'Install',
+		};
+	}
+
+	const canSubscribe = purchaseType === 'subscription' && !subscriptionInfo.status;
+	if (canSubscribe) {
+		const cannotTry = pricingPlans.every((currentPricingPlan) => currentPricingPlan.trialDays === 0);
+		const isTierBased = pricingPlans.every((currentPricingPlan) => currentPricingPlan.tiers && currentPricingPlan.tiers.length > 0);
+
+		if (cannotTry || isEnterpriseOnly) {
+			return {
+				action: 'purchase',
+				label: 'Subscribe',
+			};
+		}
+
+		if (isTierBased) {
+			return {
+				action: 'purchase',
+				label: 'See Pricing',
+			};
+		}
+
+		return {
+			action: 'purchase',
+			label: 'Try now',
+		};
+	}
+
+	const canBuy = price > 0;
+	if (canBuy) {
+		return {
+			action: 'purchase',
+			label: 'Buy',
+		};
+	}
+
+	return {
+		action: 'purchase',
+		label: 'Install',
+	};
+};
---
+++ apps/meteor/client/views/admin/apps/helpers/appStatusSpanProps.ts
@@ -0,0 +1,54 @@
+import { AppStatus } from '@rocket.chat/apps-engine/definition/AppStatus';
+import { App } from '@rocket.chat/core-typings';
+
+export const appEnabledStatuses = [AppStatus.AUTO_ENABLED, AppStatus.MANUALLY_ENABLED];
+
+const appErroredStatuses = [
+	AppStatus.COMPILER_ERROR_DISABLED,
+	AppStatus.ERROR_DISABLED,
+	AppStatus.INVALID_SETTINGS_DISABLED,
+	AppStatus.INVALID_LICENSE_DISABLED,
+];
+
+type appStatusSpanResponseProps = {
+	type?: 'failed' | 'warning';
+	icon: 'warning' | 'ban' | 'checkmark-circled' | 'check';
+	label: 'Config Needed' | 'Failed' | 'Disabled' | 'Trial period' | 'Installed';
+};
+
+export const appStatusSpanProps = ({ installed, status, subscriptionInfo }: App): appStatusSpanResponseProps | undefined => {
+	if (!installed) {
+		return;
+	}
+
+	const isFailed = status && appErroredStatuses.includes(status);
+	if (isFailed) {
+		return {
+			type: 'failed',
+			icon: 'warning',
+			label: status === AppStatus.INVALID_SETTINGS_DISABLED ? 'Config Needed' : 'Failed',
+		};
+	}
+
+	const isEnabled = status && appEnabledStatuses.includes(status);
+	if (!isEnabled) {
+		return {
+			type: 'warning',
+			icon: 'ban',
+			label: 'Disabled',
+		};
+	}
+
+	const isOnTrialPeriod = subscriptionInfo && subscriptionInfo.status === 'trialing';
+	if (isOnTrialPeriod) {
+		return {
+			icon: 'checkmark-circled',
+			label: 'Trial period',
+		};
+	}
+
+	return {
+		icon: 'check',
+		label: 'Installed',
+	};
+};
