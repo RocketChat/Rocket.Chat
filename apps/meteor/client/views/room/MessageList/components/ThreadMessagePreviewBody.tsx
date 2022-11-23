e33e432eaf630dd52de7f3e9dbc36093cfde6f7a
Filipe Marins <filipemarins123@gmail.com>
2022-11-23 13:49:01 -0300

    refactor: remove duplicated call of parse message function

--- apps/meteor/client/views/room/MessageList/components/ThreadMessagePreviewBody.tsx
+++ apps/meteor/client/views/room/MessageList/components/ThreadMessagePreviewBody.tsx
@@ -1,21 +1,12 @@
 import { IMessage } from '@rocket.chat/core-typings';
 import { PreviewMarkup } from '@rocket.chat/gazzodown';
-import { useUserSubscription } from '@rocket.chat/ui-contexts';
 import React, { ReactElement } from 'react';
-
-import { useAutoTranslate } from '../hooks/useAutoTranslate';
-import { parseMessageTextToAstMarkdown } from '../lib/parseMessageTextToAstMarkdown';
 
 type ThreadMessagePreviewBodyProps = {
 	message: IMessage;
 };
 
-const ThreadMessagePreviewBody = ({ message }: ThreadMessagePreviewBodyProps): ReactElement => {
-	const subscription = useUserSubscription(message.rid);
-	const autoTranslateOptions = useAutoTranslate(subscription);
-	const parsedMessage = parseMessageTextToAstMarkdown(message, { colors: true, emoticons: true }, autoTranslateOptions);
-
-	return parsedMessage.md ? <PreviewMarkup tokens={parsedMessage.md} /> : <></>;
-};
+const ThreadMessagePreviewBody = ({ message }: ThreadMessagePreviewBodyProps): ReactElement =>
+	message.md ? <PreviewMarkup tokens={message.md} /> : <></>;
 
 export default ThreadMessagePreviewBody;
