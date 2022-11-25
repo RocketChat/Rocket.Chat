1f54898e583a99b98c38ea4f29fd587a4cbeae6e
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2022-11-25 14:47:53 -0300

    [NEW] Created PriorityIcon component

---
+++ apps/meteor/ee/client/omnichannel/priorities/PriorityIcon.tsx
@@ -0,0 +1,39 @@
+import { Icon } from '@rocket.chat/fuselage';
+import React, { ComponentProps, ReactElement } from 'react';
+
+type PriorityIconProps = Omit<ComponentProps<typeof Icon>, 'name' | 'color'> & {
+	level: 1 | 2 | 3 | 4 | 5;
+};
+
+const PRIORITIES = {
+	1: {
+		iconName: 'chevron-double-up',
+		color: '#B30A20',
+	},
+	2: {
+		iconName: 'chevron-up',
+		color: '#B30A20',
+	},
+	3: {
+		iconName: 'equal',
+		color: '#B68D00',
+	},
+	4: {
+		iconName: 'chevron-down',
+		color: '#10529E',
+	},
+	5: {
+		iconName: 'chevron-double-down',
+		color: '#10529E',
+	},
+} as const;
+
+export const PriorityIcon = ({ level, size = 20, ...props }: PriorityIconProps): ReactElement | null => {
+	const { iconName, color } = PRIORITIES[level] || {};
+
+	if (!iconName) {
+		return null;
+	}
+
+	return <Icon {...props} name={iconName} color={color} size={size} />;
+};
