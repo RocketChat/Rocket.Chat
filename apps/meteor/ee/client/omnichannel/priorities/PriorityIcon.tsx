30e94bf7a4c92278ea4a1cef9cb41cf087f3f8c0
Martin <martin.schoeler@rocket.chat>
2022-11-28 14:36:43 -0300

    add colors from palette

--- apps/meteor/ee/client/omnichannel/priorities/PriorityIcon.tsx
+++ apps/meteor/ee/client/omnichannel/priorities/PriorityIcon.tsx
@@ -1,4 +1,4 @@
-import { Icon } from '@rocket.chat/fuselage';
+import { Icon, Palette } from '@rocket.chat/fuselage';
 import React, { ComponentProps, ReactElement } from 'react';
 
 type PriorityIconProps = Omit<ComponentProps<typeof Icon>, 'name' | 'color'> & {
@@ -9,23 +9,23 @@ const PRIORITIES = {
 	// TODO: Update colors to fuselage Palette
 	1: {
 		iconName: 'chevron-double-up',
-		color: '#B30A20',
+		color: Palette.badge['badge-background-level-4'].toString(),
 	},
 	2: {
 		iconName: 'chevron-up',
-		color: '#B30A20',
+		color: Palette.badge['badge-background-level-4'].toString(),
 	},
 	3: {
 		iconName: 'equal',
-		color: '#B68D00',
+		color: Palette.badge['badge-background-level-3'].toString(),
 	},
 	4: {
 		iconName: 'chevron-down',
-		color: '#10529E',
+		color: Palette.badge['badge-background-level-2'].toString(),
 	},
 	5: {
 		iconName: 'chevron-double-down',
-		color: '#10529E',
+		color: Palette.badge['badge-background-level-2'].toString(),
 	},
 } as const;
 
