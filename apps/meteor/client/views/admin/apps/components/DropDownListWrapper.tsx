19acef4f8b3c83f07e910c67c2b06061d3a95ebf
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2022-11-28 15:22:03 -0300

    fix dropdown menu

--- apps/meteor/client/views/admin/apps/components/DropDownListWrapper.tsx
+++ apps/meteor/client/views/admin/apps/components/DropDownListWrapper.tsx
@@ -1,26 +1,50 @@
 import { Box } from '@rocket.chat/fuselage';
-import { usePosition, useOutsideClick } from '@rocket.chat/fuselage-hooks';
-import React, { forwardRef, ComponentProps, useRef } from 'react';
-
-const options = {
-	margin: 8,
-	placement: 'bottom-end',
-} as const;
-
-const hidden = {
-	visibility: 'hidden',
-	opacity: 0,
-	position: 'fixed',
-} as const;
+import { useOutsideClick } from '@rocket.chat/fuselage-hooks';
+import React, { forwardRef, ComponentProps, useRef, useState, useEffect } from 'react';
 
 const DropDownListWrapper = forwardRef<Element, ComponentProps<typeof Box> & { onClose: (e: MouseEvent) => void }>(
-	function CategoryDropDownListWrapper({ children, onClose }, ref) {
+	function CategoryDropDownListWrapper({ children, onClose }) {
 		const target = useRef<HTMLElement>(null);
 		useOutsideClick([target], onClose);
-		const { style = hidden } = usePosition(ref as Parameters<typeof usePosition>[0], target, options);
+
+		const [windowSize, setWindowSize] = useState(getWindowSize());
+
+		useEffect(() => {
+			function handleWindowResize(): void {
+				setWindowSize(getWindowSize());
+			}
+
+			window.addEventListener('resize', handleWindowResize);
+
+			return (): void => {
+				window.removeEventListener('resize', handleWindowResize);
+			};
+		}, []);
+
+		function getWindowSize(): { innerWidth: number; innerHeight: number } {
+			const { innerWidth, innerHeight } = window;
+			return { innerWidth, innerHeight };
+		}
+
 		return (
-			<Box ref={target} style={style} minWidth={224} zIndex='99999'>
-				{children}
+			<Box ref={target} maxHeight={16} maxWidth={224} zIndex='99999' position='relative' overflowY='initial'>
+				<div
+					style={
+						windowSize.innerWidth > 1024
+							? {
+									position: 'absolute',
+									top: 40,
+									right: 2.8,
+									textAlign: 'center',
+							  }
+							: {
+									position: 'relative',
+									marginTop: 4,
+							  }
+					}
+				>
+					{children}
+				</div>
 			</Box>
 		);
 	},
