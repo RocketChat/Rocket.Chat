49f4a84b5a47e2dc8fdb9f3931e8efe0ec7ed012
Douglas Fabris <devfabris@gmail.com>
2022-11-30 17:58:58 -0300

    Regression: Login and Registration UI Tweaks (#27411)

--- packages/web-ui-registration/src/LoginServices.tsx
+++ packages/web-ui-registration/src/LoginServices.tsx
@@ -3,23 +3,24 @@ import { useLoginServices } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import ServicesButton from './ServicesButton';
+import LoginServicesButton from './LoginServicesButton';
 
 const Services = ({ disabled }: { disabled?: boolean }): ReactElement | null => {
-	const services = useLoginServices();
 	const { t } = useTranslation();
+	const services = useLoginServices();
+
 	if (services.length === 0) {
 		return null;
 	}
 
 	return (
 		<>
+			<Divider mb={24} p={0} children={t('registration.component.form.divider')} />
 			<ButtonGroup vertical stretch small>
 				{services.map((service) => (
-					<ServicesButton disabled={disabled} key={service.service} {...service} />
+					<LoginServicesButton disabled={disabled} key={service.service} {...service} />
 				))}
 			</ButtonGroup>
-			<Divider children={t('registration.component.form.divider')} />
 		</>
 	);
 };
