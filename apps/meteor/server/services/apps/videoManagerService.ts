b1fb6a36a2b8a8ba7281a58bfbbdb2ff358a1634
Rafael Tapia <rafael.tapia@rocket.chat>
2022-12-01 09:23:40 -0400

    AE-67 feat: add new videoconf methods

--- apps/meteor/server/services/apps/videoManagerService.ts
+++ apps/meteor/server/services/apps/videoManagerService.ts
@@ -1,6 +1,7 @@
-import type { IVideoConferenceUser } from '@rocket.chat/apps-engine/definition/videoConferences';
+import type { IVideoConferenceUser, VideoConference } from '@rocket.chat/apps-engine/definition/videoConferences';
 import type { VideoConfData, VideoConfDataExtended, IVideoConferenceOptions } from '@rocket.chat/apps-engine/definition/videoConfProviders';
 import type { AppVideoConfProviderManager } from '@rocket.chat/apps-engine/server/managers';
+import type { IBlock } from '@rocket.chat/apps-engine/definition/uikit';
 
 import { ServiceClass } from '../../sdk/types/ServiceClass';
 import type { AppServerOrchestrator } from './orchestrator';
@@ -45,5 +46,25 @@ export class AppsVideoManagerService extends ServiceClass implements IAppsVideoM
 		options?: IVideoConferenceOptions | undefined,
 	): Promise<string> {
 		return this.getVideoConfProviderManager().customizeUrl(providerName, call, user, options);
+	}
+
+	async onUserJoin(providerName: string, call: VideoConference, user?: IVideoConferenceUser | undefined): Promise<void> {
+		this.getVideoConfProviderManager().onUserJoin(providerName, call, user);
+	}
+
+	async onNewVideoConference(providerName: string, call: VideoConference): Promise<void> {
+		this.getVideoConfProviderManager().onNewVideoConference(providerName, call);
+	}
+
+	async onVideoConferenceChanged(providerName: string, call: VideoConference): Promise<void> {
+		this.getVideoConfProviderManager().onVideoConferenceChanged(providerName, call);
+	}
+
+	async getVideoConferenceInfo(
+		providerName: string,
+		call: VideoConference,
+		user?: IVideoConferenceUser | undefined,
+	): Promise<IBlock[] | undefined> {
+		return this.getVideoConfProviderManager().getVideoConferenceInfo(providerName, call, user);
 	}
 }
