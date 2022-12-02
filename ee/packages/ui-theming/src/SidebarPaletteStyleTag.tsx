d12270ee28e280af7fef50c77d856f1e865a47c0
juliajforesti <juliajforesti@gmail.com>
2022-12-02 15:18:43 -0300

    fix lint

--- ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
+++ ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
@@ -2,10 +2,8 @@ import type { ReactElement } from 'react';
 import React from 'react';
 import { createPortal } from 'react-dom';
 import { useSessionStorage } from '@rocket.chat/fuselage-hooks';
-
 import { convertToCss } from '@rocket.chat/ui-theming/src/sidebarConvertToCss';
 import { filterOnlyChangedColors } from '@rocket.chat/ui-theming/src/filterOnlyChangedColors';
-
 import { sidebarPaletteDark } from '@rocket.chat/ui-theming/src/sidebarPaletteDark';
 import { defaultSidebarPalette } from '@rocket.chat/ui-theming/src/sidebarPalette';
 
