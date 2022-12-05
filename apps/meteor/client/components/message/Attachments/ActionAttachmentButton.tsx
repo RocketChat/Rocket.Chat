def87ffa0450efeb334c0871d292779bb91acf6c
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-12-05 15:22:26 -0300

    Chore: Restrict `ChatMessages API` - Phase 2 (#27457)

---
+++ apps/meteor/client/components/message/Attachments/ActionAttachmentButton.tsx
@@ -0,0 +1,84 @@
+import { IMessage, MessageAttachmentAction } from '@rocket.chat/core-typings';
+import { Button } from '@rocket.chat/fuselage';
+import { useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import { useMutation, UseMutationOptions, UseMutationResult } from '@tanstack/react-query';
+import React, { ReactElement, ReactNode } from 'react';
+
+import { useChat } from '../../../views/room/contexts/ChatContext';
+
+type ProcessingType = Exclude<MessageAttachmentAction['actions'][number]['msg_processing_type'], undefined>;
+
+type UsePerfomActionMutationParams = {
+	processingType: ProcessingType;
+	msg?: string;
+	mid?: IMessage['_id'];
+};
+
+type ActionAttachmentButtonProps = {
+	children: ReactNode;
+	mid?: IMessage['_id'];
+	msg?: string;
+	processingType: ProcessingType;
+};
+
+const usePerformActionMutation = (
+	options?: Omit<UseMutationOptions<void, Error, UsePerfomActionMutationParams>, 'mutationFn'>,
+): UseMutationResult<void, Error, UsePerfomActionMutationParams> => {
+	const chat = useChat();
+
+	return useMutation(async ({ processingType, msg, mid }) => {
+		if (!chat) {
+			return;
+		}
+
+		switch (processingType) {
+			case 'sendMessage':
+				if (!msg) return;
+				await chat.flows.sendMessage({ text: msg });
+				return;
+
+			case 'respondWithMessage':
+				if (!msg) return;
+				await chat.composer?.replyWith(msg);
+				return;
+
+			case 'respondWithQuotedMessage':
+				if (!mid) return;
+				const message = await chat.data.getMessageByID(mid);
+				await chat.composer?.quoteMessage(message);
+		}
+	}, options);
+};
+
+const ActionAttachmentButton = ({ children, processingType, msg, mid }: ActionAttachmentButtonProps): ReactElement => {
+	const dispatchToastMessage = useToastMessageDispatch();
+
+	const performActionMutation = usePerformActionMutation({
+		onError: (error) => {
+			console.error(error);
+			dispatchToastMessage({ type: 'error', message: error });
+		},
+	});
+
+	return (
+		<Button
+			small
+			value={msg}
+			id={mid}
+			disabled={performActionMutation.isLoading}
+			onClick={(event): void => {
+				event.preventDefault();
+
+				performActionMutation.mutate({
+					processingType,
+					msg,
+					mid,
+				});
+			}}
+		>
+			{children}
+		</Button>
+	);
+};
+
+export default ActionAttachmentButton;
