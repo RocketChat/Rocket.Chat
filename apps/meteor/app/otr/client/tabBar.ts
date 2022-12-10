c6bf8d692d0366c6477e6b9f137e057990738539
Marcos Defendi <marcos.defendi@rocket.chat>
2022-12-10 18:13:34 -0300

    fix: disabling OTR in federated rooms

--- apps/meteor/app/otr/client/tabBar.ts
+++ apps/meteor/app/otr/client/tabBar.ts
@@ -1,12 +1,16 @@
 import { useMemo, lazy, useEffect } from 'react';
 import { useSetting } from '@rocket.chat/ui-contexts';
+import type { IRoom, ISubscription } from '@rocket.chat/core-typings';
+import { isRoomFederated } from '@rocket.chat/core-typings';
 
 import OTR from './OTR';
 import { addAction } from '../../../client/views/room/lib/Toolbox';
 
 const template = lazy(() => import('../../../client/views/room/contextualBar/OTR'));
 
-addAction('otr', () => {
+addAction('otr', (options) => {
+	const room = options.room as unknown as ISubscription & IRoom;
+	const federated = isRoomFederated(room);
 	const enabled = useSetting('OTR_Enable') as boolean;
 
 	const shouldAddAction = enabled && Boolean(global.crypto);
@@ -26,6 +30,10 @@ addAction('otr', () => {
 						template,
 						order: 13,
 						full: true,
+						...(federated && {
+							'data-tooltip': 'OTR_unavailable_for_federation',
+							'disabled': true,
+						}),
 				  }
 				: null,
 		[shouldAddAction],
