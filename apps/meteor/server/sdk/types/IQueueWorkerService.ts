f9441a009952dee943dd1a06b3d60b32612139f6
Kevin Aleman <kaleman960@gmail.com>
2022-12-14 13:16:20 -0600

    save it

--- apps/meteor/server/sdk/types/IQueueWorkerService.ts
+++ apps/meteor/server/sdk/types/IQueueWorkerService.ts
@@ -1,3 +1,10 @@
+export type HealthAggResult = {
+	total: number;
+	type: string;
+	status: 'Rejected' | 'In progress';
+};
+
 export interface IQueueWorkerService {
 	queueWork<T extends Record<string, unknown>>(to: string, data: T): Promise<void>;
+	queueInfo(): Promise<HealthAggResult[]>;
 }
