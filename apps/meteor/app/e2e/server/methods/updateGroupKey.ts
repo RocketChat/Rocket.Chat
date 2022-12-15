f32446872b4c12af1df3dd4816f39a17b65e0acf
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2022-12-15 16:59:21 -0300

    Change e2e.updateGroupKey to set key to the final field if subscription is from current user

--- apps/meteor/app/e2e/server/methods/updateGroupKey.ts
+++ apps/meteor/app/e2e/server/methods/updateGroupKey.ts
@@ -8,12 +8,18 @@ Meteor.methods({
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'e2e.acceptSuggestedGroupKey' });
 		}
 
+		// I have a subscription to this room
 		const mySub = await Subscriptions.findOneByRoomIdAndUserId(rid, userId);
+
 		if (mySub) {
-			// I have a subscription to this room
+			// Setting the key to myself, can set directly to the final field
+			if (userId === uid) {
+				return Subscriptions.setGroupE2EKey(mySub._id, key);
+			}
+
+			// uid also has subscription to this room
 			const userSub = await Subscriptions.findOneByRoomIdAndUserId(rid, uid);
 			if (userSub) {
-				// uid also has subscription to this room
 				return Subscriptions.setGroupE2ESuggestedKey(userSub._id, key);
 			}
 		}
