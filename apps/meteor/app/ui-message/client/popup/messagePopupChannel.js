f977b65cf6910293978233215f218cd466889da8
Marcos Defendi <marcos.defendi@rocket.chat>
2022-12-17 05:41:36 -0300

    fix: fixing also teh popup channels to support federated rooms

--- apps/meteor/app/ui-message/client/popup/messagePopupChannel.js
+++ apps/meteor/app/ui-message/client/popup/messagePopupChannel.js
@@ -7,4 +7,7 @@ Template.messagePopupChannel.helpers({
 	channelIcon() {
 		return roomCoordinator.getIcon(this);
 	},
+	channelName() {
+		return roomCoordinator.getRoomName(this.t, this);
+	},
 });
