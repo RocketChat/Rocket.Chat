94534aae757859eba14f141c3aa91245914cb3b8
Filipe Marins <filipemarins123@gmail.com>
2022-12-19 17:36:23 -0300

    feat: add toolbox action options component

--- packages/ui-client/src/components/Header/ToolBox/ToolBoxAction.tsx
+++ packages/ui-client/src/components/Header/ToolBox/ToolBoxAction.tsx
@@ -1,20 +1,27 @@
 import { IconButton } from '@rocket.chat/fuselage';
 import type { FC } from 'react';
+import React, { forwardRef } from 'react';
 
-const ToolBoxAction: FC<any> = ({ id, icon, color, action, className, index, title, 'data-tooltip': tooltip, ...props }) => (
-	<IconButton
-		data-qa-id={`ToolBoxAction-${icon}`}
-		className={className}
-		onClick={() => action(id)}
-		data-toolbox={index}
-		key={id}
-		icon={icon}
-		position='relative'
-		tiny
-		overflow='visible'
-		color={!!color && color}
-		{...{ ...props, ...(tooltip ? { 'data-tooltip': tooltip, 'title': '' } : { title }) }}
-	/>
-);
+const ToolBoxAction: FC<any> = forwardRef(function ToolBoxAction(
+	{ id, icon, color, action, className, index, title, 'data-tooltip': tooltip, ...props },
+	ref,
+) {
+	return (
+		<IconButton
+			data-qa-id={`ToolBoxAction-${icon}`}
+			className={className}
+			onClick={() => action(id)}
+			data-toolbox={index}
+			key={id}
+			icon={icon}
+			position='relative'
+			tiny
+			overflow='visible'
+			ref={ref}
+			color={!!color && color}
+			{...{ ...props, ...(tooltip ? { 'data-tooltip': tooltip, 'title': '' } : { title }) }}
+		/>
+	);
+});
 
 export default ToolBoxAction;
