b7d060002b1f20852ace28d61ce3407ac7bf8013
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-12-20 00:40:55 -0300

    add await for each expect

--- apps/meteor/tests/e2e/email-inboxes.spec.ts
+++ apps/meteor/tests/e2e/email-inboxes.spec.ts
@@ -38,13 +38,13 @@ test.describe.serial('email-inboxes', () => {
 
 		await poAdminEmailInboxes.btnSave.click();
 
-		expect(poAdminEmailInboxes.itemRow(name)).toBeVisible();
+		await expect(poAdminEmailInboxes.itemRow(name)).toBeVisible();
 	});
 
 	test('expect delete an email inbox', async () => {
 		await poAdminEmailInboxes.findEmailInbox(email).click();
 		await poAdminEmailInboxes.btnDelete.click();
 		await poUtils.btnModalConfirmDelete.click();
-		expect(poUtils.toastBarSuccess).toBeVisible();
+		await expect(poUtils.toastBarSuccess).toBeVisible();
 	});
 });
