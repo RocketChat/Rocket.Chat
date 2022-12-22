dc777fa7076cc3377d8a65a07a20af200b630059
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-12-22 01:10:23 -0300

    undo message-parser

--- apps/meteor/client/templates.ts
+++ apps/meteor/client/templates.ts
@@ -2,16 +2,8 @@ import { HTML } from 'meteor/htmljs';
 
 import { createTemplateForComponent } from './lib/portals/createTemplateForComponent';
 
-createTemplateForComponent('omnichannelFlex', () => import('./views/omnichannel/sidebar/OmnichannelSidebar'), {
-	renderContainerView: () => HTML.DIV({ style: 'height: 100%; position: relative;' }),
-});
-
 createTemplateForComponent('ModalBlock', () => import('./views/blocks/ConnectedModalBlock'), {
 	renderContainerView: () => HTML.DIV({ style: 'display: flex; width: 100%; height: 100%;' }),
-});
-
-createTemplateForComponent('loggedOutBanner', () => import('../ee/client/components/deviceManagement/LoggedOutBanner'), {
-	renderContainerView: () => HTML.DIV({ style: 'max-width: 520px; margin: 0 auto;' }),
 });
 
 // TODO: Remove those after threads Message/Pinned/Starred/Audit are migrated to React
