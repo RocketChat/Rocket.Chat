6ef0a7fcc012a1b0846ebdd3bb5e83928691a655
Lucas Andrade <63932475+nYCSTs@users.noreply.github.com>
2022-12-22 19:47:12 -0300

    Chore: Convert UserTable on Directory to TS (#27534)
    Co-authored-by: Douglas Fabris <27704687+dougfabris@users.noreply.github.com>

--- apps/meteor/client/views/directory/UsersTab.tsx
+++ apps/meteor/client/views/directory/UsersTab.tsx
@@ -3,17 +3,17 @@ import type { ReactElement } from 'react';
 import React from 'react';
 
 import NotAuthorizedPage from '../notAuthorized/NotAuthorizedPage';
-import UserTable from './UserTable';
+import UsersTable from './UsersTable';
 
-function UserTab(props: { workspace?: 'external' | 'local' }): ReactElement {
+const UsersTab = (props: { workspace?: 'external' | 'local' }): ReactElement => {
 	const canViewOutsideRoom = usePermission('view-outside-room');
 	const canViewDM = usePermission('view-d-room');
 
 	if (canViewOutsideRoom && canViewDM) {
-		return <UserTable {...props} />;
+		return <UsersTable {...props} />;
 	}
 
 	return <NotAuthorizedPage />;
-}
+};
 
-export default UserTab;
+export default UsersTab;
