273a7a091acaacfaaced0ca14cb7886018001f3b
guijun13 <guilhermejun13.grillo@gmail.com>
2022-12-23 11:52:48 -0300

    changes on homepage custom block

--- apps/meteor/client/views/home/CustomHomePageContent.tsx
+++ apps/meteor/client/views/home/CustomHomePageContent.tsx
@@ -10,47 +10,46 @@ const CustomHomePageContent = (): ReactElement | null => {
 	const body = String(useSetting('Layout_Home_Body'));
 	const { data } = useIsEnterprise();
 	const isAdmin = usePermission('view-user-administration');
-	// const [showOnlyThisContent, setShowOnlyThisContent] = useState(useSetting('Layout_Custom_Body_Only'));
-	const [isCustomContentVisible, setIsCustomContentVisible] = useState(useSetting('Layout_Custom_Body'));
+	const [isCustomContentVisible, setIsCustomContentVisible] = useState(useSetting('Layout_Home_Custom_Block_Visible'));
 
 	const isEnterprise = data?.isEnterprise;
 
-	if (!isAdmin && isCustomContentVisible) {
-		return <Box withRichContent dangerouslySetInnerHTML={{ __html: body }} />;
+	if (isAdmin) {
+		return (
+			<Card variant='light' data-qa-id='homepage-custom-content'>
+				<Card.Title>
+					<Tag>
+						<Icon mie='x4' name={!isCustomContentVisible ? 'eye-off' : 'eye'} size='x12' />
+						{!isCustomContentVisible ? 'Not visible to workspace' : 'Visible to workspace'}
+					</Tag>
+				</Card.Title>
+				<Card.Body>
+					<Box withRichContent dangerouslySetInnerHTML={{ __html: body }} />
+				</Card.Body>
+				<Card.FooterWrapper>
+					<Card.Footer>
+						<Button is='a' href='/admin/settings/Layout' title='Layout / Home page content'>
+							Customize content
+						</Button>
+						<Button
+							title={!isCustomContentVisible ? `Now it's available only for admins` : `Now it's available for everyone`}
+							onClick={(): void => {
+								setIsCustomContentVisible(!isCustomContentVisible);
+							}}
+						>
+							<Icon name='eye-off' size='x16' />
+							Show to workspace
+						</Button>
+						<Button disabled={!isEnterprise} title='It will hide all other white blocks in the homepage (Enterprise only)'>
+							<Icon name='lightning' size='x16' /> Show only this content
+						</Button>
+					</Card.Footer>
+				</Card.FooterWrapper>
+			</Card>
+		);
 	}
 
-	return (
-		<Card variant='light' data-qa-id='homepage-custom-content'>
-			<Card.Title>
-				<Tag>
-					<Icon mie='x4' name={!isCustomContentVisible ? 'eye' : 'eye-off'} size='x12' />
-					{!isCustomContentVisible ? 'Visible to workspace' : 'Not visible to workspace'}
-				</Tag>
-			</Card.Title>
-			<Card.Body>
-				<Box withRichContent dangerouslySetInnerHTML={{ __html: body }} />
-			</Card.Body>
-			<Card.FooterWrapper>
-				<Card.Footer>
-					<Button is='a' href='/admin/settings/Layout' title='Layout / Home page content'>
-						Customize content
-					</Button>
-					<Button
-						title={!isCustomContentVisible ? `Now it's available only for admins` : `Now it's available for everyone`}
-						onClick={(): void => {
-							setIsCustomContentVisible(!isCustomContentVisible);
-						}}
-					>
-						<Icon name='eye-off' size='x16' />
-						Show to workspace
-					</Button>
-					<Button disabled={!isEnterprise} title='It will hide all other white blocks in the homepage (Enterprise only)'>
-						<Icon name='lightning' size='x16' /> Show only this content
-					</Button>
-				</Card.Footer>
-			</Card.FooterWrapper>
-		</Card>
-	);
+	return <Box withRichContent dangerouslySetInnerHTML={{ __html: body }} />;
 };
 
 export default CustomHomePageContent;
