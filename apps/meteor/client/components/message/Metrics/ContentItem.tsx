8729f8d4477ec3ba1a86b5d7d6ca2518d51a1ebb
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-12-29 10:55:47 -0300

    Delete unused modules

--- apps/meteor/client/components/message/Metrics/ContentItem.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@rocket.chat/fuselage';
-import type { ComponentProps, ReactElement } from 'react';
-import React from 'react';
-
-type ContentItemProps = ComponentProps<typeof Box>;
-
-const ContentItem = (props: ContentItemProps): ReactElement => <Box display='flex' margin={4} {...props} />;
-
-export default ContentItem;
