ae832af5fabdef1d72955b6e10f29958cc863349
Matheus Carmo <matheus.carmo@rocket.chat>
2022-12-30 13:47:42 -0300

    ref: extract app requests related cronjob to its own file

--- apps/meteor/app/apps/server/cron.js
+++ apps/meteor/app/apps/server/cron.js
@@ -9,7 +9,6 @@ import { Apps } from './orchestrator';
 import { getWorkspaceAccessToken } from '../../cloud/server';
 import { Users } from '../../models/server';
 import { sendMessagesToAdmins } from '../../../server/lib/sendMessagesToAdmins';
-import { appRequestNotififyForUsers } from './marketplace/appRequestNotifyUsers';
 
 const notifyAdminsAboutInvalidApps = Meteor.bindEnvironment(function _notifyAdminsAboutInvalidApps(apps) {
 	if (!apps) {
@@ -102,57 +101,6 @@ export const appsUpdateMarketplaceInfo = Meteor.bindEnvironment(function _appsUp
 	}
 
 	Promise.await(Apps.updateAppsMarketplaceInfo(data).then(notifyAdminsAboutInvalidApps).then(notifyAdminsAboutRenewedApps));
-});
-
-export const appsNotifyAppRequests = Meteor.bindEnvironment(function _appsNotifyAppRequests() {
-	try {
-		const installedApps = Promise.await(Apps.installedApps({ enabled: true }));
-		if (!installedApps || installedApps.length === 0) {
-			return;
-		}
-
-		const token = Promise.await(getWorkspaceAccessToken());
-		const baseUrl = Apps.getMarketplaceUrl();
-		const options = {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		};
-
-		const pendingSentUrl = `${baseUrl}/v1/app-request/sent/pending`;
-		const result = HTTP.get(pendingSentUrl, options);
-		const data = result.data?.data;
-		const filtered = installedApps.filter((app) => data.indexOf(app.getID()) !== -1);
-
-		filtered.forEach((app) => {
-			const appId = app.getID();
-			const appName = app.getName();
-
-			const usersNotified = Promise.await(
-				appRequestNotififyForUsers(baseUrl, appId, appName)
-					.then(() => HTTP.post(`${baseUrl}/v1/app-request/markAsSent/${appId}`, options))
-					.catch((err) => {
-						Apps.debugLog(`could not send app request notifications for app ${appId}. Error: ${err}`);
-					}),
-			);
-
-			const errors = usersNotified.filter((batch) => batch instanceof Error);
-			if (errors.length > 0) {
-				Apps.debugLog(`Some batches of users could not be notified for app ${appId}. Errors: ${errors}`);
-			}
-		});
-	} catch (err) {
-		Apps.debugLog(err);
-	}
-});
-
-// Scheduling as every 24 hours to avoid multiple instances hiting the marketplace at the same time
-SyncedCron.add({
-	name: 'Apps-Request-End-Users:notify',
-	schedule: (parser) => parser.text('every 24 hours'),
-	job() {
-		appsNotifyAppRequests();
-	},
 });
 
 SyncedCron.add({
