4a53dc258a508f336206327fda3e6664827403fd
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2023-01-06 19:59:30 +0000

    [fix] Missing placeholders are now showed over encrypted hash

--- apps/meteor/client/components/message/variants/threadPreview/ThreadMessagePreviewBody.tsx
+++ apps/meteor/client/components/message/variants/threadPreview/ThreadMessagePreviewBody.tsx
@@ -1,5 +1,7 @@
 import type { IMessage } from '@rocket.chat/core-typings';
+import { isE2EEMessage } from '@rocket.chat/core-typings';
 import { PreviewMarkup } from '@rocket.chat/gazzodown';
+import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
@@ -10,7 +12,14 @@ type ThreadMessagePreviewBodyProps = {
 };
 
 const ThreadMessagePreviewBody = ({ message }: ThreadMessagePreviewBodyProps): ReactElement | null => {
-	const parsedMessage = parseMessageTextToAstMarkdown(message, { colors: true, emoticons: true });
+	const t = useTranslation();
+
+	const isEncryptedMessage = isE2EEMessage(message);
+
+	const parsedMessage = parseMessageTextToAstMarkdown(
+		message.e2e === 'done' || !isEncryptedMessage ? message : { ...message, msg: t('E2E_message_encrypted_placeholder') },
+		{ colors: true, emoticons: true },
+	);
 
 	return parsedMessage.md ? <PreviewMarkup tokens={parsedMessage.md} /> : null;
 };
