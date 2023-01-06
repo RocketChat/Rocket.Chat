8bc89fbf49c562f60c9b0adfb8f6e74608c0e29d
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-01-06 12:11:46 -0300

    fix component

--- apps/meteor/client/components/EnabledAppsCount.tsx
+++ apps/meteor/client/components/EnabledAppsCount.tsx
@@ -1,21 +1,20 @@
 import { Box, ProgressBar } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
+// import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
 const EnabledAppsCount = ({ numberOfEnabledApps, width }: { numberOfEnabledApps: number; width: string | number }): ReactElement => {
-	const t = useTranslation();
+	// const t = useTranslation();
 
-	if (numberOfEnabledApps > 5) {
-		throw new Error('You have enabled more than 5 apps');
-	}
+	// eslint-disable-next-line no-nested-ternary
+	const barColor = numberOfEnabledApps < 5 ? 'font-on-success' : numberOfEnabledApps === 5 ? 'font-on-warning' : 'font-on-danger';
 
 	return (
-		<Box display='flex' flexDirection='column' mbe='x16' w={width}>
+		<Box display='flex' flexDirection='column' mb='x8' w={width}>
 			<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' w='full'>
 				<Box is='span' color='default' fontScale='p2m'>
 					{/* TODO: fix translation */}
-					{`${numberOfEnabledApps} ${t('apps enabled')}`}
+					{`${numberOfEnabledApps} apps enabled`}
 				</Box>
 
 				<Box is='span' color='annotation' fontScale='p2m'>
@@ -25,10 +24,11 @@ const EnabledAppsCount = ({ numberOfEnabledApps, width }: { numberOfEnabledApps:
 			<ProgressBar
 				borderRadius='x8'
 				overflow='hidden'
-				percentage={numberOfEnabledApps * 20}
-				barColor={numberOfEnabledApps < 5 ? 'font-on-success' : 'font-on-warning'}
+				percentage={numberOfEnabledApps <= 5 ? numberOfEnabledApps * 20 : 100}
+				barColor={barColor}
 				animated={false}
 				w='full'
+				mb='x4'
 			/>
 		</Box>
 	);
