bd2364782f83e9e6981b8b4e024b41f837ae6a2d
Dnouv <sdevanshu90@yahoo.com>
2023-01-09 16:39:10 +0530

    useQuery -> useEndpoindata for app releases

--- apps/meteor/client/views/admin/apps/AppDetailsPage/tabs/AppReleases/AppReleases.tsx
+++ apps/meteor/client/views/admin/apps/AppDetailsPage/tabs/AppReleases/AppReleases.tsx
@@ -1,24 +1,29 @@
 import type { App } from '@rocket.chat/core-typings';
 import { Accordion } from '@rocket.chat/fuselage';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
 import type { ReactElement } from 'react';
 import React from 'react';
 
-import { useEndpointData } from '../../../../../../hooks/useEndpointData';
-import { AsyncStatePhase } from '../../../../../../lib/asyncState/AsyncStatePhase';
 import AccordionLoading from '../../../AccordionLoading';
 import AppReleasesItem from './AppReleasesItem';
 
 // TODO: replace useEndpointData
 const AppReleases = ({ id }: { id: App['id'] }): ReactElement => {
-	const result = useEndpointData('/apps/:id/versions', { keys: { id } });
+	const getVersions = useEndpoint('GET', `/apps/${id}/versions`);
+
+	const { data, isLoading, isFetched } = useQuery(['versions'], async () => {
+		const versions = await getVersions();
+		return versions;
+	});
 
 	return (
 		<>
 			<Accordion width='100%' alignSelf='center'>
-				{result.phase === AsyncStatePhase.LOADING && <AccordionLoading />}
-				{result.phase === AsyncStatePhase.RESOLVED && (
+				{isLoading && <AccordionLoading />}
+				{isFetched && (
 					<>
-						{result.value.apps.map((release) => (
+						{data?.apps.map((release) => (
 							<AppReleasesItem release={release} key={release.version} />
 						))}
 					</>
