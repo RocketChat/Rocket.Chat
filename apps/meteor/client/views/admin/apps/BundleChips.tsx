f204458e3278271b9b793c4619749085d3c3babd
rique223 <henrique.jobs1@gmail.com>
2023-01-10 15:56:34 -0300

    Refactor unnecessary code for enterprise tag tooltip

--- apps/meteor/client/views/admin/apps/BundleChips.tsx
+++ apps/meteor/client/views/admin/apps/BundleChips.tsx
@@ -1,7 +1,7 @@
-import { Box, PositionAnimated, Tag, Tooltip, AnimatedVisibility } from '@rocket.chat/fuselage';
+import { Box, Tag } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
-import type { ReactElement, RefObject } from 'react';
-import React, { Fragment, useRef, useState } from 'react';
+import type { ReactElement } from 'react';
+import React, { Fragment, useRef } from 'react';
 
 import type { App } from './types';
 
@@ -16,26 +16,20 @@ type BundleChipsProps = {
 const BundleChips = ({ bundledIn }: BundleChipsProps): ReactElement => {
 	const t = useTranslation();
 	const bundleRef = useRef<Element>();
-	const [isHovered, setIsHovered] = useState(false);
 	return (
 		<>
 			{bundledIn.map((bundle) => (
 				<Fragment key={bundle.bundleId}>
-					<Box ref={bundleRef} onMouseEnter={(): void => setIsHovered(true)} onMouseLeave={(): void => setIsHovered(false)}>
-						<Tag variant='featured'>{bundle.bundleName}</Tag>
-					</Box>
-					<PositionAnimated
-						anchor={bundleRef as RefObject<Element>}
-						placement='top-middle'
-						margin={8}
-						visible={isHovered ? AnimatedVisibility.VISIBLE : AnimatedVisibility.HIDDEN}
-					>
-						<Tooltip>
-							{t('this_app_is_included_with_subscription', {
+					<Box ref={bundleRef}>
+						<Tag
+							variant='featured'
+							title={t('this_app_is_included_with_subscription', {
 								bundleName: bundle.bundleName,
 							})}
-						</Tooltip>
-					</PositionAnimated>
+						>
+							{bundle.bundleName}
+						</Tag>
+					</Box>
 				</Fragment>
 			))}
 		</>
