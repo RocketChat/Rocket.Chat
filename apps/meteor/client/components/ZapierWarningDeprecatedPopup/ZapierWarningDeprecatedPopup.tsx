2a17353a54f69b18e509ffdac63b1b77e810c6b4
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2023-01-11 19:19:00 +0000

    Zapier warning deprecation popup prototype

--- apps/meteor/client/components/ZapierWarningDeprecatedPopup/ZapierWarningDeprecatedPopup.tsx
+++ apps/meteor/client/components/ZapierWarningDeprecatedPopup/ZapierWarningDeprecatedPopup.tsx
@@ -2,11 +2,12 @@
 import { Banner, Icon } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import React from 'react';
+import type { ReactElement } from 'react';
 
-const ZapierDeprecatedWarningPopup = () => {
+const ZapierWarningDeprecatedPopup = (): ReactElement => {
 	const t = useTranslation();
 
 	return <Banner icon={<Icon name='warning' />} title={t('This_is_a_deprecated_feature_alert')} variant='warning' />;
 };
 
-export default ZapierDeprecatedWarningPopup;
+export default ZapierWarningDeprecatedPopup;
