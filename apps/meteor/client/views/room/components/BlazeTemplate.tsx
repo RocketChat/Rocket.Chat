bde4f6146ddd8e04d8d1b068082af37148e8157f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-01-11 19:21:41 -0300

    Pass contexts on `BlazeTemplate`

--- apps/meteor/client/views/room/components/BlazeTemplate.tsx
+++ apps/meteor/client/views/room/components/BlazeTemplate.tsx
@@ -1,37 +1,56 @@
 import { Box } from '@rocket.chat/fuselage';
 import { Blaze } from 'meteor/blaze';
+import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
 import type { ComponentProps, FC } from 'react';
 import React, { memo, useLayoutEffect, useRef } from 'react';
+
+import { useBlazePortals } from '../../../lib/portals/blazePortals';
 
 const BlazeTemplate: FC<
 	Omit<ComponentProps<typeof Box>, 'children'> & {
 		name: string;
 	} & Record<string, unknown>
 > = ({ name, flexShrink, overflow, onClick, w, ...props }) => {
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	const ref = useRef(null!);
+	const [portals, portalsSubscription] = useBlazePortals();
+
+	const reactiveDataContextRef = useRef(new ReactiveVar(props));
+
 	useLayoutEffect(() => {
-		if (!ref.current || !Template[name]) {
+		reactiveDataContextRef.current.set(props);
+	});
+
+	const ref = useRef<HTMLDivElement>();
+
+	useLayoutEffect(() => {
+		if (!ref.current) {
 			return;
 		}
 
-		const view = Blaze.renderWithData(Template[name], props, ref.current);
+		const view = Blaze.renderWithData(
+			Template[name],
+			() => ({ ...reactiveDataContextRef.current.get(), portalsSubscription: () => portalsSubscription }),
+			ref.current,
+		);
+
 		return (): void => {
-			view && Blaze.remove(view);
+			Blaze.remove(view);
 		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [name, JSON.stringify(props)]);
+	}, [name, portalsSubscription]);
+
 	return (
-		<Box
-			rcx-blaze-template
-			display='flex'
-			onClick={onClick}
-			flexDirection='column'
-			flexGrow={1}
-			ref={ref}
-			{...{ w, flexShrink, overflow }}
-		/>
+		<>
+			<Box
+				rcx-blaze-template
+				display='flex'
+				flexDirection='column'
+				flexGrow={1}
+				ref={ref}
+				onClick={onClick}
+				{...{ w, flexShrink, overflow }}
+			/>
+			{portals}
+		</>
 	);
 };
 
