b62da11b33a3fa53cce9cf2cd40d2db556319930
Filipe Marins <filipemarins123@gmail.com>
2023-01-13 12:19:59 -0300

    feat: update packages add fixtures and tests

---
+++ packages/pdf-worker/src/templates/ChatTranscriptTemplate/ChatTranscriptTemplate.stories.ts
@@ -0,0 +1,27 @@
+import { Font, PDFViewer } from '@react-pdf/renderer';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { exampleData } from './ChatTranscriptTemplate.fixtures';
+import { ChatTranscript } from '../../strategies/ChatTranscript';
+import type { ChatTranscriptData } from '.';
+import { ChatTranscriptPDF } from '.';
+
+Font.register({
+	family: 'Inter',
+	fonts: [{ src: '/inter400.ttf' }, { src: '/inter500.ttf', fontWeight: 500 }, { src: '/inter700.ttf', fontWeight: 700 }],
+});
+
+Font.registerHyphenationCallback((word) => [word]);
+
+export default {
+	title: 'ChatTranscriptPDFTemplate',
+	component: ChatTranscriptPDF,
+} as ComponentMeta<typeof ChatTranscriptPDF>;
+
+const data = new ChatTranscript().parseTemplateData(exampleData) as unknown as ChatTranscriptData;
+
+export const ChatTranscriptPDFTemplate: ComponentStory<typeof ChatTranscriptPDF> = () => (
+	<PDFViewer width='100%' height='800'>
+		<ChatTranscriptPDF {...data} />
+	</PDFViewer>
+);
