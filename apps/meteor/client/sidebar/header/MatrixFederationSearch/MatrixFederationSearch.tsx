310646fab32cf32fd0dd21455b3e957f2cff52d8
gabriellsh <gabriel.henriques@rocket.chat>
2023-01-16 20:48:42 -0300

    substitute mocks

--- apps/meteor/client/sidebar/header/MatrixFederationSearch/MatrixFederationSearch.tsx
+++ apps/meteor/client/sidebar/header/MatrixFederationSearch/MatrixFederationSearch.tsx
@@ -1,6 +1,5 @@
-// import { useEndpoint } from '@rocket.chat/ui-contexts';
 import { Modal, Skeleton } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
+import { useEndpoint, useTranslation } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import type { VFC } from 'react';
@@ -11,12 +10,8 @@ type MatrixFederationSearchProps = {
 	onClose: () => void;
 };
 
-const fetchServerList = () => ({
-	servers: Array.from({ length: 5 }).map((_, index) => ({ name: `Server ${index}`, default: true, local: false })),
-});
-
 const MatrixFederationSearch: VFC<MatrixFederationSearchProps> = ({ onClose }) => {
-	// const fetchRoomList = useEndpoint('GET', '/v1/federation/searchPublicRooms');
+	const fetchServerList = useEndpoint('GET', '/v1/federation/listServersByUser');
 	const t = useTranslation();
 	const { data, isLoading } = useQuery(['federation/listServersByUsers'], async () => fetchServerList());
 
@@ -26,10 +21,11 @@ const MatrixFederationSearch: VFC<MatrixFederationSearchProps> = ({ onClose }) =
 				<Modal.Title>{t('Federation_Federated_room_search')}</Modal.Title>
 				<Modal.Close onClick={onClose} />
 			</Modal.Header>
-			<Modal.Content>
+			<Modal.Content overflow='hidden'>
 				{isLoading && <Skeleton />}
 				{!isLoading && data?.servers && <MatrixFederationSearchModalContent servers={data.servers} />}
 			</Modal.Content>
+			<Modal.Footer />
 		</Modal>
 	);
 };
