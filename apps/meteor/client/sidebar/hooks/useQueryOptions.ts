38afc0a6cd0b2bd7194341b76cd94a3f895e6fcb
murtaza98 <murtaza.patrawala@rocket.chat>
2023-01-19 17:21:19 +0530

    Revert to standard logic of sorting by "LM" on chats in progress

--- apps/meteor/client/sidebar/hooks/useQueryOptions.ts
+++ apps/meteor/client/sidebar/hooks/useQueryOptions.ts
@@ -1,11 +1,7 @@
-import { OmnichannelSortingMechanismSettingType as OmniSortType } from '@rocket.chat/core-typings';
-import { OmnichannelSortingMechanismSettingType } from '@rocket.chat/core-typings/src';
 import { useUserPreference, useSetting } from '@rocket.chat/ui-contexts';
 import { useMemo } from 'react';
 
-import { getOmniChatSortQuery } from '../../../app/livechat/lib/inquiries';
-
-type QueryOptions = {
+export const useQueryOptions = (): {
 	sort:
 		| {
 				lm?: number | undefined;
@@ -17,31 +13,20 @@ type QueryOptions = {
 		| {
 				lowerCaseName: number;
 				lm?: number | undefined;
-		  }
-		| ReturnType<typeof getOmniChatSortQuery>;
-};
-
-const getSortOption = (sortingMechanism: OmniSortType, sortBy: string, showRealName: boolean) =>
-	({
-		[OmniSortType.Priority]: getOmniChatSortQuery(OmnichannelSortingMechanismSettingType.Priority),
-		[OmniSortType.SLAs]: getOmniChatSortQuery(OmnichannelSortingMechanismSettingType.SLAs),
-		[OmniSortType.Timestamp]: {
-			...(sortBy === 'activity' && { lm: -1 }),
-			...(sortBy !== 'activity' && {
-				...(showRealName ? { lowerCaseFName: 1 } : { lowerCaseName: 1 }),
-			}),
-		},
-	}[sortingMechanism]);
-
-export const useQueryOptions = (): QueryOptions => {
-	const sortBy = useUserPreference('sidebarSortby') as string;
-	const showRealName = useSetting('UI_Use_Real_Name') as boolean;
-	const sortingMechanism = useSetting('Omnichannel_sorting_mechanism') as OmniSortType;
+		  };
+} => {
+	const sortBy = useUserPreference('sidebarSortby');
+	const showRealName = useSetting('UI_Use_Real_Name');
 
 	return useMemo(
 		() => ({
-			sort: getSortOption(sortingMechanism, sortBy, showRealName),
+			sort: {
+				...(sortBy === 'activity' && { lm: -1 }),
+				...(sortBy !== 'activity' && {
+					...(showRealName ? { lowerCaseFName: 1 } : { lowerCaseName: 1 }),
+				}),
+			},
 		}),
-		[showRealName, sortBy, sortingMechanism],
+		[sortBy, showRealName],
 	);
 };
