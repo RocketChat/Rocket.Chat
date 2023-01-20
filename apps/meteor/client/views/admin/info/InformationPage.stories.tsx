e3017ef94427545b03623f7a177368a3976cfcac
Heitor Tanoue <heitortanoue@usp.br>
2023-01-20 11:37:55 -0300

    update *.stories.tsx

--- apps/meteor/client/views/admin/info/InformationPage.stories.tsx
+++ apps/meteor/client/views/admin/info/InformationPage.stories.tsx
@@ -286,6 +286,7 @@ export default {
 			},
 			totalSubscriptionRoles: 0,
 			totalUserRoles: 0,
+            totalUserCustomRoles: 0,
 			totalWebRTCCalls: 0,
 			uncaughtExceptionsCount: 0,
 			matrixFederation: {
