77b3e5d8417930060ca3ab1b81750d06737312b0
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-01-20 17:47:51 -0300

    enterprise license check

--- apps/meteor/client/views/marketplace/components/MarketplaceHeaderComponent.tsx
+++ apps/meteor/client/views/marketplace/components/MarketplaceHeaderComponent.tsx
@@ -2,12 +2,14 @@ import { Box, Button, ButtonGroup } from '@rocket.chat/fuselage';
 import { usePermission, useRoute, useRouteParameter, useSetting, useTranslation } from '@rocket.chat/ui-contexts';
 import React, { useCallback } from 'react';
 
+import { useHasLicenseModule } from '../../../../ee/client/hooks/useHasLicenseModule';
 import EnabledAppsCount from './EnabledAppsCount';
 
 const MarketplaceHeaderComponent = ({ title }: { title: string }) => {
 	const t = useTranslation();
 	const hasPermission = usePermission('enable-unlimited-apps');
 	const isDevelopmentMode = useSetting('Apps_Framework_Development_Mode');
+	const hasEnterpriseLicense = useHasLicenseModule('marketplace-enterprise');
 	const context = useRouteParameter('context') || 'explore';
 	const route = useRoute('marketplace');
 
@@ -22,14 +24,16 @@ const MarketplaceHeaderComponent = ({ title }: { title: string }) => {
 				<EnabledAppsCount />
 				{hasPermission && (
 					<ButtonGroup>
-						<Button
-							onClick={() => {
-								// TODO: Show upsell modal
-								return null;
-							}}
-						>
-							{t('Enable_unlimited_apps')}
-						</Button>
+						{!hasEnterpriseLicense && (
+							<Button
+								onClick={() => {
+									// TODO: Show upsell modal
+									return null;
+								}}
+							>
+								{t('Enable_unlimited_apps')}
+							</Button>
+						)}
 						{context === 'private' && isDevelopmentMode && <Button onClick={handleUploadButtonClick}>{t('Upload_private_app')}</Button>}
 					</ButtonGroup>
 				)}
