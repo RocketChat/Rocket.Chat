9b30f3f3bd1b51134e1bc52d4367d7b03b925cd1
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-01-23 14:27:41 -0300

    urls

--- apps/meteor/client/views/marketplace/UnlimitedAppsUpsellModal.tsx
+++ apps/meteor/client/views/marketplace/UnlimitedAppsUpsellModal.tsx
@@ -1,25 +1,30 @@
 import { useTranslation } from '@rocket.chat/ui-contexts';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import React from 'react';
 
 import UpsellModal from '../../components/UpsellModal';
 
 const UnlimitedAppsUpsellModal = () => {
 	const t = useTranslation();
-	const trial = true;
+	const canTrial = true;
+	const urls = {
+		goFullyFeaturedRegistered: 'admin/upgrade/go-fully-featured-registered',
+		talkToSales: 'go.rocket.chat/i/contact-sales',
+	};
 
 	return (
 		<UpsellModal
 			title={t('Enable_unlimited_apps')}
 			img='images/unlimited-apps-modal.svg'
 			subtitle={t('Get_all_apps')}
-			description={trial ? t('Workspaces_on_community_edition_trial_on') : t('Workspaces_on_community_edition_trial_off')}
-			confirmText={trial ? t('Start_free_trial') : t('Learn_more')}
+			description={canTrial ? t('Workspaces_on_community_edition_trial_on') : t('Workspaces_on_community_edition_trial_off')}
+			confirmText={canTrial ? t('Start_free_trial') : t('Learn_more')}
 			cancelText={t('Talk_to_sales')}
 			onConfirm={() => {
-				return null;
+				FlowRouter.go(urls.goFullyFeaturedRegistered);
 			}}
 			onCancel={() => {
-				return null;
+				window.open(urls.talkToSales, '_blank');
 			}}
 			onClose={() => {
 				return null;
