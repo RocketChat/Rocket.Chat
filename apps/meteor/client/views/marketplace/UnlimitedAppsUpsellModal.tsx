2a7bf44a18292364c805a916e14c6a0490ac8913
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-01-25 09:47:12 -0300

    [NEW] Create upsell modal (#27756)
    * create modal component (work in progress)
    
    * improve component
    
    * add image to modal
    
    * use generic UpsellModal
    
    * urls
    
    * trial check
    
    Co-authored-by: Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>

---
+++ apps/meteor/client/views/marketplace/UnlimitedAppsUpsellModal.tsx
@@ -0,0 +1,36 @@
+import { useSetting, useTranslation } from '@rocket.chat/ui-contexts';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import React from 'react';
+
+import UpsellModal from '../../components/UpsellModal';
+
+const UnlimitedAppsUpsellModal = () => {
+	const t = useTranslation();
+	const cloudWorkspaceHadTrial = useSetting('Cloud_Workspace_Had_Trial') as boolean;
+	const urls = {
+		goFullyFeaturedRegistered: 'admin/upgrade/go-fully-featured-registered',
+		talkToSales: 'go.rocket.chat/i/contact-sales',
+	};
+
+	return (
+		<UpsellModal
+			title={t('Enable_unlimited_apps')}
+			img='images/unlimited-apps-modal.svg'
+			subtitle={t('Get_all_apps')}
+			description={!cloudWorkspaceHadTrial ? t('Workspaces_on_community_edition_trial_on') : t('Workspaces_on_community_edition_trial_off')}
+			confirmText={!cloudWorkspaceHadTrial ? t('Start_free_trial') : t('Learn_more')}
+			cancelText={t('Talk_to_sales')}
+			onConfirm={() => {
+				FlowRouter.go(urls.goFullyFeaturedRegistered);
+			}}
+			onCancel={() => {
+				window.open(urls.talkToSales, '_blank');
+			}}
+			onClose={() => {
+				return null;
+			}}
+		/>
+	);
+};
+
+export default UnlimitedAppsUpsellModal;
