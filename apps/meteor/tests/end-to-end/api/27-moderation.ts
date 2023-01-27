087bcddcab3e9c16f975e0c8b9455e48d3eb76d0
デヴぁんす <sdevanshu90@yahoo.com>
2023-01-27 19:44:10 +0530

    add test for report hide

--- apps/meteor/tests/end-to-end/api/27-moderation.ts
+++ apps/meteor/tests/end-to-end/api/27-moderation.ts
@@ -1,3 +1,4 @@
+import type { IMessage, IReport } from '@rocket.chat/core-typings';
 import { expect } from 'chai';
 
 import { getCredentials, api, request, credentials } from '../../data/api-data';
@@ -68,6 +69,100 @@ describe('[Moderation]', function () {
 				.expect((res) => {
 					expect(res.body).to.have.property('success', true);
 					expect(res.body).to.have.property('reports').and.to.be.an('array');
+				})
+				.end(done);
+		});
+	});
+
+	// test for testing out the moderation.hide endpoint
+
+	describe('[/moderation.hide]', () => {
+		let reportedMessage: IReport;
+		let message: IMessage;
+
+		// post a new message to the channel 'general' by sending a request to chat.postMessage
+		before((done) => {
+			request
+				.post(api('chat.postMessage'))
+				.set(credentials)
+				.send({
+					channel: 'general',
+					text: 'messageId',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('message').and.to.be.an('object');
+					message = res.body.message;
+				})
+				.end(done);
+		});
+
+		// create a reported message by sending a request to chat.reportMessage
+		before((done) => {
+			request
+				.post(api('chat.reportMessage'))
+				.set(credentials)
+				.send({
+					messageId: message._id,
+					description: 'sample report',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('report').and.to.be.an('object');
+					reportedMessage = res.body.report;
+				})
+				.end(done);
+		});
+
+		after((done) => {
+			request
+				.post(api('chat.delete'))
+				.set(credentials)
+				.send({
+					roomId: 'general',
+					msgId: message._id,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				})
+				.end(done);
+		});
+
+		it('should hide a report', (done) => {
+			request
+				.post(api('moderation.hide'))
+				.set(credentials)
+				.send({
+					reportId: reportedMessage._id,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('report').and.to.be.an('object');
+					expect(res.body.report).to.have.property('_hidden', true);
+				})
+				.end(done);
+		});
+
+		it('should return an error when the reportId is not provided', (done) => {
+			request
+				.post(api('moderation.hide'))
+				.set(credentials)
+				.send({
+					reportId: '',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('error').and.to.be.a('string');
 				})
 				.end(done);
 		});
