f93dc402185d9ef6af5f9adf7eda51fe12ad5fb8
Hugo Costa <hugocarreiracosta@gmail.com>
2023-01-27 09:09:47 -0300

    adding migration

---
+++ apps/meteor/server/startup/migrations/v283.ts
@@ -0,0 +1,17 @@
+import { addMigration } from '../../lib/migrations';
+import { Users } from '../../../app/models/server';
+
+addMigration({
+	version: 283,
+	up() {
+		Users.update(
+			{ 'settings.preferences.messageViewMode': { $exists: 1 } },
+			{
+				$unset: {
+					'settings.preferences.messageViewMode': 1,
+				},
+			},
+			{ multi: true },
+		);
+	},
+});
