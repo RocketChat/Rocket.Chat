8e4c36398b4d2681648642fac3606661484aa4b5
Cuong Huu Nguyen <cuonghuunguyen98@gmail.com>
2023-01-30 09:41:41 +0000

    [FIX][APP] Fix room data is not erased after deleting rooms by app

--- apps/meteor/app/apps/server/bridges/rooms.ts
+++ apps/meteor/app/apps/server/bridges/rooms.ts
@@ -1,14 +1,15 @@
+import type { IMessage } from '@rocket.chat/apps-engine/definition/messages';
 import type { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
 import { RoomType } from '@rocket.chat/apps-engine/definition/rooms';
-import { RoomBridge } from '@rocket.chat/apps-engine/server/bridges/RoomBridge';
 import type { IUser } from '@rocket.chat/apps-engine/definition/users';
-import type { IMessage } from '@rocket.chat/apps-engine/definition/messages';
-import { Meteor } from 'meteor/meteor';
+import { RoomBridge } from '@rocket.chat/apps-engine/server/bridges/RoomBridge';
 import type { ISubscription, IUser as ICoreUser } from '@rocket.chat/core-typings';
+import { Meteor } from 'meteor/meteor';
 
-import type { AppServerOrchestrator } from '../orchestrator';
-import { Rooms, Subscriptions, Users } from '../../../models/server';
 import { addUserToRoom } from '../../../lib/server/functions/addUserToRoom';
+import { deleteRoom } from '../../../lib/server/functions/deleteRoom';
+import { Rooms, Subscriptions, Users } from '../../../models/server';
+import type { AppServerOrchestrator } from '../orchestrator';
 
 export class AppRoomBridge extends RoomBridge {
 	// eslint-disable-next-line no-empty-function
@@ -130,7 +131,7 @@ export class AppRoomBridge extends RoomBridge {
 
 	protected async delete(roomId: string, appId: string): Promise<void> {
 		this.orch.debugLog(`The App ${appId} is deleting a room.`);
-		Rooms.removeById(roomId);
+		deleteRoom(roomId);
 	}
 
 	protected async createDiscussion(
