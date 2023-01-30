432f047bda83c289f3a6a62aeef1551228f6cbc7
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-01-30 17:48:05 -0300

    fix lint

--- apps/meteor/client/views/admin/apps/utils/appStatusSpanResponseProps.ts
+++ apps/meteor/client/views/admin/apps/utils/appStatusSpanResponseProps.ts
@@ -1,6 +1,6 @@
 export type appStatusSpanResponseProps = {
 	type?: 'failed' | 'warning';
-	icon: 'warning' | 'checkmark-circled' | 'check';
+	icon?: 'warning' | 'checkmark-circled' | 'check';
 	label: 'Config Needed' | 'Failed' | 'Disabled' | 'Trial period' | 'Installed' | 'Incompatible';
 	tooltipText?: string;
 };
