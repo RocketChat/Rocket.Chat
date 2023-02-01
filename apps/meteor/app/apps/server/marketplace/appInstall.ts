350d3337d181777f1c1a38d62425cc47c4811aac
Bradley Hilton <graywolf336@craftyn.com>
2023-02-01 10:18:23 -0600

    Linter fixes

--- apps/meteor/app/apps/server/marketplace/appInstall.ts
+++ apps/meteor/app/apps/server/marketplace/appInstall.ts
@@ -7,38 +7,38 @@ import { Info } from '../../../utils/server';
 export type installAction = 'install' | 'update' | 'uninstall';
 
 export async function notifyAppInstall(marketplaceBaseUrl: string, action: installAction, appInfo: IAppInfo): Promise<void> {
-    let headers = {
-        Authorization: '',
-    };
+	let headers = {
+		Authorization: '',
+	};
 
-    try {
-        const token = await getWorkspaceAccessToken();
+	try {
+		const token = await getWorkspaceAccessToken();
 		headers = {
 			Authorization: `Bearer ${token}`,
 		};
-    } catch {}
+	} catch {}
 
-    let siteUrl = '';
-    try {
-        siteUrl = settings.get<string>('Site_Url');
-    } catch {}
+	let siteUrl = '';
+	try {
+		siteUrl = settings.get<string>('Site_Url');
+	} catch {}
 
-    const data = {
-        action,
-        appName: appInfo.name,
-        appSlug: appInfo.nameSlug,
-        appVersion: appInfo.version,
-        rocketChatVersion: Info.version,
-        engineVersion: Info.marketplaceApiVersion,
-        siteUrl,
-    };
+	const data = {
+		action,
+		appName: appInfo.name,
+		appSlug: appInfo.nameSlug,
+		appVersion: appInfo.version,
+		rocketChatVersion: Info.version,
+		engineVersion: Info.marketplaceApiVersion,
+		siteUrl,
+	};
 
-    const pendingSentUrl = `${marketplaceBaseUrl}/v1/apps/${ appInfo.id }/install`;
+	const pendingSentUrl = `${marketplaceBaseUrl}/v1/apps/${appInfo.id}/install`;
 
-    try {
-        HTTP.post(pendingSentUrl, {
-            headers,
-            data,
-        });
-    } catch {}
+	try {
+		HTTP.post(pendingSentUrl, {
+			headers,
+			data,
+		});
+	} catch {}
 }
