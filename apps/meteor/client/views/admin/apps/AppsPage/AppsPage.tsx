4705853082fe80418fdbdc8b8ed304384619bad9
PedroRorato <pedroberorato@gmail.com>
2023-02-01 16:25:43 -0300

    fix: add developmentModeButton

--- apps/meteor/client/views/admin/apps/AppsPage/AppsPage.tsx
+++ apps/meteor/client/views/admin/apps/AppsPage/AppsPage.tsx
@@ -1,5 +1,5 @@
-import { Tabs } from '@rocket.chat/fuselage';
-import { useRoute, useTranslation, useCurrentRoute, useRouteParameter } from '@rocket.chat/ui-contexts';
+import { Button, ButtonGroup, Icon, Tabs } from '@rocket.chat/fuselage';
+import { useRoute, useSetting, useTranslation, useCurrentRoute, useRouteParameter } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
@@ -9,6 +9,7 @@ import AppsPageContent from './AppsPageContent';
 const AppsPage = (): ReactElement => {
 	const t = useTranslation();
 
+	const isDevelopmentMode = useSetting('Apps_Framework_Development_Mode');
 	const [currentRouteName] = useCurrentRoute();
 	if (!currentRouteName) {
 		throw new Error('No current route name');
@@ -17,13 +18,25 @@ const AppsPage = (): ReactElement => {
 
 	const context = useRouteParameter('context');
 
+	const handleUploadButtonClick = (): void => {
+		context && router.push({ context, page: 'install' });
+	};
+
 	const handleMarketplaceTabClick = (): void => router.push({ context: 'all', page: 'list' });
 
 	const handleInstalledTabClick = (): void => router.push({ context: 'installed', page: 'list' });
 
 	return (
 		<Page background='tint'>
-			<Page.Header title={t('Apps')} />
+			<Page.Header title={t('Apps')}>
+				<ButtonGroup>
+					{Boolean(isDevelopmentMode) && (
+						<Button primary onClick={handleUploadButtonClick}>
+							<Icon size='x20' name='upload' /> {t('Upload_app')}
+						</Button>
+					)}
+				</ButtonGroup>
+			</Page.Header>
 			<Tabs>
 				<Tabs.Item onClick={handleMarketplaceTabClick} selected={context === 'all'}>
 					{t('Marketplace')}
