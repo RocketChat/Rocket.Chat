3f0418b227ecccedf3dc4c3f2450977b12c94b9b
Diego Sampaio <chinello@gmail.com>
2023-02-01 18:27:25 -0300

    [FIX][ENTERPRISE] DDP streamer sending data to destroyed streams (#27929)

--- ee/apps/ddp-streamer/src/Server.ts
+++ ee/apps/ddp-streamer/src/Server.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events';
 
-import type WebSocket from 'ws';
+import WebSocket from 'ws';
 import ejson from 'ejson';
 import { v1 as uuidv1 } from 'uuid';
 
@@ -54,6 +54,10 @@ export class Server extends EventEmitter {
 	};
 
 	async call(client: Client, packet: IPacket): Promise<void> {
+		// if client is not connected we don't need to do anything
+		if (client.ws.readyState !== WebSocket.OPEN) {
+			return;
+		}
 		try {
 			// if method was not defined on DDP Streamer we fall back to Meteor
 			if (!this._methods.has(packet.method)) {
@@ -87,6 +91,10 @@ export class Server extends EventEmitter {
 	}
 
 	async subscribe(client: Client, packet: IPacket): Promise<void> {
+		// if client is not connected we don't need to do anything
+		if (client.ws.readyState !== WebSocket.OPEN) {
+			return;
+		}
 		try {
 			if (!this._subscriptions.has(packet.name)) {
 				throw new MeteorError(404, `Subscription '${packet.name}' not found`);
