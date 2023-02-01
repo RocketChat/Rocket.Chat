fda86c04f83ae9125040b17e318f69c07e6a2b5e
Bradley Hilton <graywolf336@craftyn.com>
2023-02-01 10:14:58 -0600

    Notify Rocket.Chat's Marketplace on app install, update or uninstall. We do this to help with app requests and ensure the flow of the app requests are correctly handled.

---
+++ apps/meteor/app/apps/server/marketplace/appInstall.ts
@@ -0,0 +1,44 @@
+import type { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+
+import { getWorkspaceAccessToken } from '../../../cloud/server';
+import { settings } from '../../../settings/server';
+import { Info } from '../../../utils/server';
+
+export type installAction = 'install' | 'update' | 'uninstall';
+
+export async function notifyAppInstall(marketplaceBaseUrl: string, action: installAction, appInfo: IAppInfo): Promise<void> {
+    let headers = {
+        Authorization: '',
+    };
+
+    try {
+        const token = await getWorkspaceAccessToken();
+		headers = {
+			Authorization: `Bearer ${token}`,
+		};
+    } catch {}
+
+    let siteUrl = '';
+    try {
+        siteUrl = settings.get<string>('Site_Url');
+    } catch {}
+
+    const data = {
+        action,
+        appName: appInfo.name,
+        appSlug: appInfo.nameSlug,
+        appVersion: appInfo.version,
+        rocketChatVersion: Info.version,
+        engineVersion: Info.marketplaceApiVersion,
+        siteUrl,
+    };
+
+    const pendingSentUrl = `${marketplaceBaseUrl}/v1/apps/${ appInfo.id }/install`;
+
+    try {
+        HTTP.post(pendingSentUrl, {
+            headers,
+            data,
+        });
+    } catch {}
+}
