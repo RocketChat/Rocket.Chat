c52d3883557242937663ac03daae16831aa1f536
Kevin Aleman <kaleman960@gmail.com>
2023-02-02 07:56:22 -0600

    hehehe

--- apps/meteor/app/livechat/imports/server/rest/departments.ts
+++ apps/meteor/app/livechat/imports/server/rest/departments.ts
@@ -270,7 +270,7 @@ API.v1.addRoute(
 		},
 		async post() {
 			check(this.urlParams, {
-				departmentId: String,
+				_id: String,
 			});
 
 			check(
@@ -280,7 +280,7 @@ API.v1.addRoute(
 					remove: Array,
 				}),
 			);
-			Livechat.saveDepartmentAgents(this.urlParams.departmentId, this.bodyParams);
+			Livechat.saveDepartmentAgents(this.urlParams._id, this.bodyParams);
 
 			return API.v1.success();
 		},
