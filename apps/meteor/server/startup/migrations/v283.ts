0af4d30b1dcae936c054a570048e08b3c36c6ac9
Douglas Gubert <douglas.gubert@gmail.com>
2023-02-03 19:48:38 -0300

    Fix migration breaking startup

--- apps/meteor/server/startup/migrations/v283.ts
+++ apps/meteor/server/startup/migrations/v283.ts
@@ -1,21 +1,28 @@
 import type { IAppStorageItem } from '@rocket.chat/apps-engine/server/storage';
 
 import { AppRealStorage } from '../../../app/apps/server/storage';
-import { AppsModel } from '../../../app/models/server';
 import { addMigration } from '../../lib/migrations';
+import { AppsModel } from '../../../app/models/server';
 
 addMigration({
 	version: 283,
 	name: "Mark all installed apps as 'migrated'",
 	async up() {
-		const appsModel = new AppsModel();
-		const appsStorage = new AppRealStorage(appsModel);
+		const appsStorage = new AppRealStorage(AppsModel);
 
 		const apps = await appsStorage.retrieveAll();
 
 		const promises: Array<ReturnType<typeof appsStorage.update>> = [];
 
-		apps.forEach((app) => promises.push(appsStorage.update({ ...app, migrated: true } as IAppStorageItem)));
+		apps.forEach((app) =>
+			promises.push(
+				appsStorage.update({
+					...app,
+					migrated: true,
+					installationSource: 'marketplaceInfo' in app ? 'marketplace' : 'private',
+				} as IAppStorageItem),
+			),
+		);
 
 		await Promise.all(promises);
 	},
