c545e58fc502a0152265b587ffb1e9ded7f973b6
Hugo Costa <hugocarreiracosta@gmail.com>
2023-02-06 14:11:55 -0300

    removing user update in the lastest migration

--- apps/meteor/server/startup/migrations/v283.ts
+++ apps/meteor/server/startup/migrations/v283.ts
@@ -1,21 +1,10 @@
 import { Settings } from '@rocket.chat/models';
 
 import { addMigration } from '../../lib/migrations';
-import { Users } from '../../../app/models/server';
 
 addMigration({
 	version: 283,
-	up() {
-		Users.update(
-			{ 'settings.preferences.messageViewMode': { $exists: 1 } },
-			{
-				$unset: {
-					'settings.preferences.messageViewMode': 1,
-				},
-			},
-			{ multi: true },
-		);
-
-		return Settings.removeById('Accounts_Default_User_Preferences_messageViewMode');
+	async up() {
+		await Settings.removeById('Accounts_Default_User_Preferences_messageViewMode');
 	},
 });
