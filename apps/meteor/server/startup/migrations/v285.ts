956f3b6ef7b34ef6dc99dfd0bcd0c19ca38aadf9
murtaza98 <murtaza.patrawala@rocket.chat>
2023-02-07 15:46:48 +0530

    code review: optimize migration

--- apps/meteor/server/startup/migrations/v285.ts
+++ apps/meteor/server/startup/migrations/v285.ts
@@ -6,20 +6,9 @@ addMigration({
 	version: 285,
 	async up() {
 		// migrate old priority history messages to new sla history messages
-		const legacySlaSysMsgIds = await Messages.find(
+		await Messages.updateMany(
 			// intentionally using any since this is a legacy type which we've removed
 			{ t: 'livechat_priority_history' as any },
-			{
-				projection: {
-					_id: 1,
-				},
-			},
-		)
-			.map(({ _id }) => _id)
-			.toArray();
-
-		await Messages.updateMany(
-			{ _id: { $in: legacySlaSysMsgIds } },
 			{
 				$set: {
 					't': 'omnichannel_sla_change_history',
