aca3cd867a7e81e05e80223ead49899880ed7d29
デヴぁんす <sdevanshu90@yahoo.com>
2023-02-07 12:35:26 +0530

    define the moderation console table row

--- apps/meteor/client/views/admin/moderation/ModerationConsoleTable.tsx
+++ apps/meteor/client/views/admin/moderation/ModerationConsoleTable.tsx
@@ -1,40 +1,47 @@
+import { Pagination } from '@rocket.chat/fuselage';
 import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
-import React, { FC, MutableRefObject, useEffect, useMemo, useState } from 'react';
-
 import { useEndpoint, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
+import type { FC, MutableRefObject } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { sort } from 'semver';
 
+import FilterByText from '../../../components/FilterByText';
+import {
+	GenericTable,
+	GenericTableLoadingTable,
+	GenericTableHeaderCell,
+	GenericTableBody,
+	GenericTableHeader,
+} from '../../../components/GenericTable';
 import { usePagination } from '../../../components/GenericTable/hooks/usePagination';
 import { useSort } from '../../../components/GenericTable/hooks/useSort';
+import ModerationConsoleTableRow from './ModerationConsoleTableRow';
 
 const ModerationConsoleTable: FC<{ reload: MutableRefObject<() => void> }> = ({ reload }) => {
 	const [text, setText] = useState('');
 
-	const { sortBy, sortDirection, ...params } = useSort('ts');
-	const { current, itemsPerPage, currentItemsPerPage, onChangeItemsPerPage } = usePagination();
+	const { sortBy, sortDirection, setSort } = useSort<'ts' | 'u.username' | 'description'>('ts');
+	const {
+		current,
+		itemsPerPage,
+		currentItemsPerPage,
+		onChangeItemsPerPage,
+		setItemsPerPage,
+		setCurrent: onSetCurrent,
+		...paginationProps
+	} = usePagination();
 	// write a custom query to get the reports data from the database
+
 	const query = useDebouncedValue(
 		useMemo(
 			() => ({
-				fields: JSON.stringify({
-					_id: 1,
-					message: 1,
-					description: 1,
-					ts: 1,
-					userId: 1,
-				}),
-				query: JSON.stringify({
-					$or: [
-						{ 'message.msg': { $regex: text, $options: 'i' } },
-						{ 'message.u.username': { $regex: text, $options: 'i' } },
-						{ description: { $regex: text, $options: 'i' } },
-					],
-				}),
-				sortBy: JSON.stringify({ [sortBy]: sortDirection === 'asc' ? 1 : -1 }),
+				selector: text,
+				sort: JSON.stringify({ [sortBy]: sortDirection === 'asc' ? 1 : -1 }),
 				count: itemsPerPage,
 				offset: current,
 			}),
-			[current, itemsPerPage],
+			[current, itemsPerPage, sortBy, sortDirection, text],
 		),
 		500,
 	);
@@ -47,6 +54,8 @@ const ModerationConsoleTable: FC<{ reload: MutableRefObject<() => void> }> = ({ 
 		data,
 		reload: reloadReports,
 		isLoading,
+		isSuccess,
+		isError,
 	} = useQuery(
 		['reports', query],
 		async () => {
@@ -66,10 +75,67 @@ const ModerationConsoleTable: FC<{ reload: MutableRefObject<() => void> }> = ({ 
 
 	console.log('data', data);
 
+	// header sequence would be: name, reportedMessage, room, postdate, reports, actions
+	const headers = useMemo(
+		() => [
+			<GenericTableHeaderCell key={'name'} direction={sortDirection} active={sort === 'u.username'} onClick={setSort} sort='u.username'>
+				Name
+			</GenericTableHeaderCell>,
+			<GenericTableHeaderCell
+				key={'reportedMessage'}
+				direction={sortDirection}
+				active={sortBy === 'description'}
+				onClick={setSort}
+				sort='description'
+			>
+				Reported Message
+			</GenericTableHeaderCell>,
+			<GenericTableHeaderCell key={'room'} direction={sortDirection}>
+				Room
+			</GenericTableHeaderCell>,
+			<GenericTableHeaderCell key={'postdate'} direction={sortDirection} active={sortBy === 'ts'} onClick={setSort} sort='ts'>
+				Post Date
+			</GenericTableHeaderCell>,
+			<GenericTableHeaderCell key={'reports'} direction={sortDirection}>
+				Reports
+			</GenericTableHeaderCell>,
+			<GenericTableHeaderCell key={'actions'} width={'5%'} />,
+		],
+		[sortDirection, setSort, sortBy],
+	);
+
 	return (
-		<div>
-			<h1>Moderation Console Table</h1>
-		</div>
+		<>
+			{isLoading && <div>Loading...</div>}
+			<FilterByText autoFocus placeholder={'Search'} onChange={({ text }): void => setText(text)} />
+			{isLoading && (
+				<GenericTable>
+					<GenericTableHeader>{headers}</GenericTableHeader>
+					<GenericTableBody>{isLoading && <GenericTableLoadingTable headerCells={6} />}</GenericTableBody>
+				</GenericTable>
+			)}
+			{isSuccess && data.reports.length > 0 && (
+				<>
+					<GenericTable>
+						<GenericTableHeader>{headers}</GenericTableHeader>
+						<GenericTableBody>
+							{data.reports.map((report) => (
+								<ModerationConsoleTableRow key={report._id} report={report} />
+							))}
+						</GenericTableBody>
+					</GenericTable>
+					<Pagination
+						current={current}
+						divider
+						itemsPerPage={currentItemsPerPage}
+						count={data?.total || 0}
+						onSetItemsPerPage={onChangeItemsPerPage}
+						onSetCurrent={onSetCurrent}
+						{...paginationProps}
+					/>
+				</>
+			)}
+		</>
 	);
 };
 
---
+++ apps/meteor/client/views/admin/moderation/ModerationConsoleTableRow.tsx
@@ -0,0 +1,86 @@
+import type { IReport } from '@rocket.chat/core-typings';
+import { TableRow, TableCell, Box, Menu } from '@rocket.chat/fuselage';
+import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
+import { useEndpoint, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
+import React, { useMemo, useState } from 'react';
+
+import UserAvatar from '../../../components/avatar/UserAvatar';
+
+const ModerationConsoleTableRow = ({ report }: IReport): JSX.Element => {
+	const { _id, description, message, ts } = report;
+	const { username } = message.u;
+	const { rid } = message;
+	const { msg } = message;
+	const [text, setText] = useState('');
+
+	// write a custom query to get the reports data from the database
+
+	const query = useDebouncedValue(
+		useMemo(
+			() => ({
+				count: 50,
+				msgId: message._id,
+				selector: text,
+			}),
+			[message._id, text],
+		),
+		500,
+	);
+
+	const dispatchToastMessage = useToastMessageDispatch();
+
+	const getReportsByMessage = useEndpoint('GET', '/v1/moderation.reportsByMessage');
+
+	const {
+		data: reportsByMessage,
+		reload: reloadReportsByMessage,
+		isLoading: isLoadingReportsByMessage,
+		isSuccess: isSuccessReportsByMessage,
+		isError: isErrorReportsByMessage,
+	} = useQuery(
+		['reportsByMessage', query],
+		async () => {
+			const reports = await getReportsByMessage(query);
+			return reports;
+		},
+		{
+			onError: (error) => {
+				dispatchToastMessage({ type: 'error', message: error });
+			},
+		},
+	);
+
+	return (
+		<TableRow key={_id}>
+			<TableCell withTruncatedText>
+				<Box display='flex' alignItems='center'>
+					{username && <UserAvatar size={'x40'} username={username} />}
+					<Box display='flex' mi='x8' withTruncatedText>
+						<Box display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
+							<Box fontScale='p2m' color='default' withTruncatedText>
+								{username}
+							</Box>
+						</Box>
+					</Box>
+				</Box>
+			</TableCell>
+			<TableCell withTruncatedText>{description}</TableCell>
+			<TableCell withTruncatedText>{rid}</TableCell>
+			<TableCell withTruncatedText>{ts}</TableCell>
+			<TableCell withTruncatedText>{isSuccessReportsByMessage ? reportsByMessage.total : '-'}</TableCell>
+			<TableCell>
+				<Menu
+					options={{
+						seeReports: {
+							label: 'See Reports',
+							action: () => console.log('See Reports'),
+						},
+					}}
+				/>
+			</TableCell>
+		</TableRow>
+	);
+};
+
+export default ModerationConsoleTableRow;
