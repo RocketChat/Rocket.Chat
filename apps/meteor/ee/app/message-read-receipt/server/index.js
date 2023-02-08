7e6538aa73efc5d99ee58ce9b74975bc45590b71
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-02-08 10:53:16 -0300

    Make read receipts settings visible to community

--- apps/meteor/ee/app/message-read-receipt/server/index.js
+++ apps/meteor/ee/app/message-read-receipt/server/index.js
@@ -1,8 +1,5 @@
 import { onLicense } from '../../license/server';
 
 onLicense('message-read-receipt', () => {
-	const { createSettings } = require('./settings');
 	require('./hooks');
-
-	createSettings();
 });
