ec407a0d67462931c857325d516f29ebe6c388fb
juliajforesti <juliajforesti@gmail.com>
2023-02-08 11:20:29 -0300

    disabled status

--- packages/ui-contexts/src/ConnectionStatusContext.ts
+++ packages/ui-contexts/src/ConnectionStatusContext.ts
@@ -4,7 +4,7 @@ export type ConnectionStatusContextValue = {
 	connected: boolean;
 	retryCount?: number;
 	retryTime?: number;
-	status: 'connected' | 'connecting' | 'failed' | 'waiting' | 'offline';
+	status: 'connected' | 'connecting' | 'failed' | 'waiting' | 'offline' | 'disabled';
 	reconnect: () => void;
 };
 
