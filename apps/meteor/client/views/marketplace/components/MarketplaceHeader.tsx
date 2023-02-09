290844ae70881dcd26bff31510bec278d2438d78
Douglas Gubert <douglas.gubert@gmail.com>
2023-02-09 15:17:56 -0300

    Remove app dev mode setting

--- apps/meteor/client/views/marketplace/components/MarketplaceHeader.tsx
+++ apps/meteor/client/views/marketplace/components/MarketplaceHeader.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, ButtonGroup } from '@rocket.chat/fuselage';
-import { usePermission, useRoute, useRouteParameter, useSetModal, useSetting, useTranslation } from '@rocket.chat/ui-contexts';
+import { usePermission, useRoute, useRouteParameter, useSetModal, useTranslation } from '@rocket.chat/ui-contexts';
 import React, { useCallback } from 'react';
 
 import { useHasLicenseModule } from '../../../../ee/client/hooks/useHasLicenseModule';
@@ -8,8 +8,7 @@ import EnabledAppsCount from './EnabledAppsCount';
 
 const MarketplaceHeader = ({ title }: { title: string }) => {
 	const t = useTranslation();
-	const hasPermission = usePermission('enable-unlimited-apps');
-	const isDevelopmentMode = useSetting('Apps_Framework_Development_Mode');
+	const isAdmin = usePermission('manage-apps');
 	const hasEnterpriseLicense = useHasLicenseModule('marketplace-enterprise');
 	const context = useRouteParameter('context') || 'explore';
 	const route = useRoute('marketplace');
@@ -24,7 +23,7 @@ const MarketplaceHeader = ({ title }: { title: string }) => {
 			<Box fontScale='h2'>{title}</Box>
 			<Box display='flex' flexDirection='row' flexWrap='wrap'>
 				<EnabledAppsCount />
-				{hasPermission && (
+				{isAdmin && (
 					<ButtonGroup>
 						{!hasEnterpriseLicense && (
 							<Button
@@ -35,7 +34,7 @@ const MarketplaceHeader = ({ title }: { title: string }) => {
 								{t('Enable_unlimited_apps')}
 							</Button>
 						)}
-						{context === 'private' && isDevelopmentMode && <Button onClick={handleUploadButtonClick}>{t('Upload_private_app')}</Button>}
+						{context === 'private' && <Button onClick={handleUploadButtonClick}>{t('Upload_private_app')}</Button>}
 					</ButtonGroup>
 				)}
 			</Box>
