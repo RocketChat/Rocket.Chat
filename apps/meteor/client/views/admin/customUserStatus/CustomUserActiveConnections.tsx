f4a7d96959b4f6cfe6eebfea612b1d39df2dfa7f
juliajforesti <juliajforesti@gmail.com>
2023-02-08 21:48:13 -0300

    fix loading and error status on CustomUserActiveConnections

--- apps/meteor/client/views/admin/customUserStatus/CustomUserActiveConnections.tsx
+++ apps/meteor/client/views/admin/customUserStatus/CustomUserActiveConnections.tsx
@@ -1,4 +1,4 @@
-import { Box, ProgressBar } from '@rocket.chat/fuselage';
+import { Box, ProgressBar, Skeleton } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import React from 'react';
 
@@ -9,11 +9,13 @@ const CustomUserActiveConnections = () => {
 
 	const result = useActiveConnections();
 
-	if (result.isLoading) {
-		return <>loading</>;
-	}
-	if (result.isError) {
-		return <>error</>;
+	if (result.isLoading || result.isError) {
+		return (
+			<Box w='x180' h='x40' mi='x8' fontScale='c1' display='flex' flexDirection='column' justifyContent='space-around'>
+				<Box color='default'>{t('Active_connections')}</Box>
+				<Skeleton w='full' />
+			</Box>
+		);
 	}
 
 	const { current, max, percentage } = result.data;
