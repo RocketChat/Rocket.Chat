49f0a30ca8c036c04edc1e22265f0b6c13f9466a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-02-10 19:54:59 -0300

    Show thread messages

--- apps/meteor/ee/client/views/audit/components/AuditMessageList.tsx
+++ apps/meteor/ee/client/views/audit/components/AuditMessageList.tsx
@@ -1,7 +1,5 @@
 import type { IMessage } from '@rocket.chat/core-typings';
-import { isThreadMessage } from '@rocket.chat/core-typings';
 import { MessageDivider } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React, { Fragment, memo } from 'react';
 
@@ -16,27 +14,19 @@ type AuditMessageListProps = {
 };
 
 export const AuditMessageList = ({ messages }: AuditMessageListProps): ReactElement => {
-	const t = useTranslation();
 	const formatDate = useFormatDate();
 
 	return (
 		<>
 			{messages.map((message, index, { [index - 1]: previous }) => {
 				const newDay = isMessageNewDay(message, previous);
-				const firstUnread = false; // isMessageFirstUnread(subscription, message, previous);
-				const showDivider = newDay || firstUnread;
 				const system = MessageTypes.isSystemMessage(message);
-				const visible = !isThreadMessage(message) && !system;
 
 				return (
 					<Fragment key={message._id}>
-						{showDivider && (
-							<MessageDivider unreadLabel={firstUnread ? t('Unread_Messages').toLowerCase() : undefined}>
-								{newDay && formatDate(message.ts)}
-							</MessageDivider>
-						)}
+						{newDay && <MessageDivider>{formatDate(message.ts)}</MessageDivider>}
 
-						{visible && <RoomMessage message={message} sequential={false} unread={false} mention={false} all={false} ignoredUser={false} />}
+						{!system && <RoomMessage message={message} sequential={false} unread={false} mention={false} all={false} ignoredUser={false} />}
 
 						{system && <SystemMessage message={message} />}
 					</Fragment>
