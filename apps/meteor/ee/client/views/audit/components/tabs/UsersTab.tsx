5fdab978e540dd3771be0e2b2ff277d42f09f547
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-02-10 19:42:44 -0300

    Extract `UsernamesAutoComplete`

--- apps/meteor/ee/client/views/audit/components/tabs/UsersTab.tsx
+++ apps/meteor/ee/client/views/audit/components/tabs/UsersTab.tsx
@@ -1,14 +1,12 @@
-import type { IUser } from '@rocket.chat/core-typings';
 import { Field } from '@rocket.chat/fuselage';
-import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 import type { UseFormReturn } from 'react-hook-form';
 import { useController } from 'react-hook-form';
 
-import UserAutoCompleteMultiple from '../../../../../../client/components/UserAutoCompleteMultiple';
 import type { AuditFields } from '../../hooks/useAuditForm';
+import UsernamesAutoComplete from '../forms/UsernamesAutoComplete';
 
 type UsersTabProps = {
 	form: UseFormReturn<AuditFields>;
@@ -16,17 +14,6 @@ type UsersTabProps = {
 
 const UsersTab = ({ form: { control } }: UsersTabProps): ReactElement => {
 	const t = useTranslation();
-
-	const handleChange = useMutableCallback((currentUsername: Exclude<IUser['username'], undefined>, action: 'remove' | undefined) => {
-		const { value, onChange } = usersField;
-
-		if (action === 'remove') {
-			onChange(value.filter((username) => username !== currentUsername));
-			return;
-		}
-
-		onChange([...value, currentUsername]);
-	});
 
 	const { field: usersField, fieldState: usersFieldState } = useController({
 		name: 'users',
@@ -45,10 +32,10 @@ const UsersTab = ({ form: { control } }: UsersTabProps): ReactElement => {
 		<Field flexShrink={1}>
 			<Field.Label>{t('Users')}</Field.Label>
 			<Field.Row>
-				<UserAutoCompleteMultiple
+				<UsernamesAutoComplete
 					error={!!usersFieldState.error}
 					value={usersField.value}
-					onChange={handleChange}
+					onChange={usersField.onChange}
 					placeholder={t('Username_Placeholder')}
 				/>
 			</Field.Row>
