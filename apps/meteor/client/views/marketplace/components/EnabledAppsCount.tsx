e5fcaa234cec04509eb13f52cba686e4fc6c12c4
Douglas Gubert <douglas.gubert@gmail.com>
2023-02-10 17:01:39 -0300

    Remove useMemo and adapt tooltip

--- apps/meteor/client/views/marketplace/components/EnabledAppsCount.tsx
+++ apps/meteor/client/views/marketplace/components/EnabledAppsCount.tsx
@@ -1,15 +1,17 @@
 import { Box, ProgressBar } from '@rocket.chat/fuselage';
-import { useRouteParameter, useTooltipClose, useTooltipOpen, useTranslation } from '@rocket.chat/ui-contexts';
+import { useRouteParameter, useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import React, { useMemo, useRef } from 'react';
+import React from 'react';
 
-import MarkdownText from '../../../components/MarkdownText';
 import { useAppsCount } from '../AppsContext';
 
 type Variant = 'success' | 'warning' | 'danger';
 
-const getProgressBarValues = (numberOfEnabledApps: number, enabledAppsLimit: number): { variant: Variant; percentage: number } => {
+const getProgressBarValues = (numberOfEnabledApps: string, enabledAppsLimit: string): { variant: Variant; percentage: number } => {
 	const values = { variant: 'danger', percentage: 0 } as { variant: Variant; percentage: number };
+
+	const enabled = Number(numberOfEnabledApps) || 0;
+	const limit = Number(enabledAppsLimit) || 0;
 
 	if (numberOfEnabledApps < enabledAppsLimit) {
 		values.variant = 'success';
@@ -19,7 +21,7 @@ const getProgressBarValues = (numberOfEnabledApps: number, enabledAppsLimit: num
 		values.variant = 'warning';
 	}
 
-	values.percentage = Math.round((numberOfEnabledApps / enabledAppsLimit) * 100);
+	values.percentage = Math.round((enabled / limit) * 100);
 
 	return values;
 };
@@ -29,24 +31,9 @@ const EnabledAppsCount = (): ReactElement => {
 	const context = useRouteParameter('context');
 	const appsCount = useAppsCount();
 
-	const elRef = useRef<HTMLDivElement>(null);
-	const openTooltip = useTooltipOpen();
-	const closeTooltip = useTooltipClose();
-
-	const numberOfEnabledApps = useMemo(() => {
-		return context === 'private' ? appsCount.totalPrivateEnabled : appsCount.totalMarketplaceEnabled;
-	}, [context, appsCount.totalMarketplaceEnabled, appsCount.totalPrivateEnabled]);
-
-	const enabledAppsLimit = useMemo(
-		() => (context === 'private' ? appsCount.maxPrivateApps : appsCount.maxMarketplaceApps),
-		[context, appsCount.maxMarketplaceApps, appsCount.maxPrivateApps],
-	);
-
-	const { variant, percentage } = useMemo(() => {
-		const enabled = Number(numberOfEnabledApps) || 0;
-		const limit = Number(enabledAppsLimit) || 0;
-		return getProgressBarValues(enabled, limit);
-	}, [enabledAppsLimit, numberOfEnabledApps]);
+	const numberOfEnabledApps = context === 'private' ? appsCount.totalPrivateEnabled : appsCount.totalMarketplaceEnabled;
+	const enabledAppsLimit = context === 'private' ? appsCount.maxPrivateApps : appsCount.maxMarketplaceApps;
+	const { variant, percentage } = getProgressBarValues(numberOfEnabledApps, enabledAppsLimit);
 
 	return (
 		<Box
@@ -55,25 +42,7 @@ const EnabledAppsCount = (): ReactElement => {
 			mi='16px'
 			minWidth='200px'
 			justifyContent='center'
-			ref={elRef}
-			onMouseOver={(e): void => {
-				e.stopPropagation();
-				e.preventDefault();
-				elRef.current &&
-					openTooltip(
-						<MarkdownText
-							content={t('Apps_Count_Enabled_tooltip', {
-								number: enabledAppsLimit,
-								context,
-							})}
-							variant='inline'
-						/>,
-						elRef.current,
-					);
-			}}
-			onMouseLeave={(): void => {
-				closeTooltip();
-			}}
+			data-tooltip={t('Apps_Count_Enabled_tooltip', { number: enabledAppsLimit, context })}
 		>
 			<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' w='full'>
 				<Box fontScale='c1'>{t('Apps_Count_Enabled', { count: numberOfEnabledApps })}</Box>
