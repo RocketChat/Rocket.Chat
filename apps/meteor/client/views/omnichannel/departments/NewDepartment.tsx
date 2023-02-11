679c67ff9e7aa0d919567c5c5b49cb1327f67381
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-02-11 17:18:20 -0300

    Refactor minor pet peeves

--- apps/meteor/client/views/omnichannel/departments/NewDepartment.tsx
+++ apps/meteor/client/views/omnichannel/departments/NewDepartment.tsx
@@ -1,27 +1,32 @@
-import { useEndpoint, useTranslation } from '@rocket.chat/ui-contexts';
+import { useEndpoint, useSetModal, useTranslation } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
+import EnterpriseDepartmentsModal from '../../../components/Omnichannel/modals/EnterpriseDepartmentsModal';
 import PageSkeleton from '../../../components/PageSkeleton';
 import EditDepartment from './EditDepartment';
-import UpgradeDepartments from './UpgradeDepartments';
 
 type NewDepartmentProps = {
 	id?: string;
 };
 
 const NewDepartment = ({ id }: NewDepartmentProps) => {
+	const setModal = useSetModal();
 	const getDepartmentCreationAvailable = useEndpoint('GET', '/v1/livechat/department/isDepartmentCreationAvailable');
-	const { data, isLoading } = useQuery(['getDepartments'], async () => getDepartmentCreationAvailable());
+	const { data, isLoading } = useQuery(['getDepartments'], async () => getDepartmentCreationAvailable(), {
+		onSuccess: (data) => {
+			if (data.isDepartmentCreationAvailable === false) {
+				setModal(<EnterpriseDepartmentsModal closeModal={(): void => setModal(null)} />);
+			}
+		},
+	});
 
 	const t = useTranslation();
 
-	if (!data || isLoading) {
+	if (!data || isLoading || !data.isDepartmentCreationAvailable) {
 		return <PageSkeleton />;
 	}
-	if (data.isDepartmentCreationAvailable === false) {
-		return <UpgradeDepartments />;
-	}
+
 	// TODO: remove allowedToForwardData and data props once the EditDepartment component is migrated to TS
 	return <EditDepartment id={id} title={t('New_Department')} allowedToForwardData={undefined} data={undefined} />;
 };
