e199349d1e6ba54580fb28b1520037239f8a17f2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-02-11 18:21:15 -0300

    Refactor minor pet peeves

--- apps/meteor/client/components/Omnichannel/hooks/useDepartmentsList.ts
+++ apps/meteor/client/components/Omnichannel/hooks/useDepartmentsList.ts
@@ -17,17 +17,35 @@ type DepartmentsListOptions = {
 	showArchived?: boolean;
 };
 
+type DepartmentListItem =
+	| (ILivechatDepartment & {
+			label: string;
+			value: { value: string; label: string };
+	  })
+	| {
+			_id: '';
+			label: string;
+			value: { value: 'all'; label: string };
+			_updatedAt: Date;
+	  }
+	| {
+			_id: '';
+			label: string;
+			value: { value: ''; label: string };
+			_updatedAt: Date;
+	  };
+
 export const useDepartmentsList = (
 	options: DepartmentsListOptions,
 ): {
-	itemsList: RecordList<ILivechatDepartment>;
+	itemsList: RecordList<DepartmentListItem>;
 	initialItemCount: number;
 	reload: () => void;
 	loadMoreItems: (start: number, end: number) => void;
 } => {
 	const t = useTranslation();
-	const [itemsList, setItemsList] = useState(() => new RecordList<ILivechatDepartment>());
-	const reload = useCallback(() => setItemsList(new RecordList<ILivechatDepartment>()), []);
+	const [itemsList, setItemsList] = useState(() => new RecordList<DepartmentListItem>());
+	const reload = useCallback(() => setItemsList(new RecordList<DepartmentListItem>()), []);
 
 	const getDepartments = useEndpoint('GET', '/v1/livechat/department');
 
@@ -55,18 +73,20 @@ export const useDepartmentsList = (
 					}
 					return true;
 				})
-				.map((department: any) => {
-					if (department.archived) {
-						department.name = `${department.name} [${t('Archived')}]`;
-					}
-					department._updatedAt = new Date(department._updatedAt);
-					department.label = department.name;
-					department.value = { value: department._id, label: department.name };
-					return department;
+				.map(({ _id, name, _updatedAt, ...department }): DepartmentListItem => {
+					return {
+						...department,
+						_id,
+						name: department.archived ? `${name} [${t('Archived')}]` : name,
+						label: department.archived ? `${name} [${t('Archived')}]` : name,
+						value: { value: _id, label: name },
+						...(_updatedAt && { _updatedAt: new Date(_updatedAt) }),
+					};
 				});
 
 			options.haveAll &&
 				items.unshift({
+					_id: '',
 					label: t('All'),
 					value: { value: 'all', label: t('All') },
 					_updatedAt: new Date(),
@@ -74,6 +94,7 @@ export const useDepartmentsList = (
 
 			options.haveNone &&
 				items.unshift({
+					_id: '',
 					label: t('None'),
 					value: { value: '', label: t('None') },
 					_updatedAt: new Date(),
