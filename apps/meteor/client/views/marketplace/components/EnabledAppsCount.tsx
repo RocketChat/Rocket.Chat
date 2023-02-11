b6e33f655828378ae49bc2c3c62e727e81c29765
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-02-11 16:12:32 -0300

    use title

--- apps/meteor/client/views/marketplace/components/EnabledAppsCount.tsx
+++ apps/meteor/client/views/marketplace/components/EnabledAppsCount.tsx
@@ -1,9 +1,8 @@
 import { Box, ProgressBar } from '@rocket.chat/fuselage';
-import { useRouteParameter, useTooltipClose, useTooltipOpen, useTranslation } from '@rocket.chat/ui-contexts';
+import { useRouteParameter, useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo } from 'react';
 
-import MarkdownText from '../../../components/MarkdownText';
 import { useAppsCount } from '../AppsContext';
 
 type Variant = 'success' | 'warning' | 'danger';
@@ -29,10 +28,6 @@ const EnabledAppsCount = (): ReactElement => {
 	const context = useRouteParameter('context');
 	const appsCount = useAppsCount();
 
-	const elRef = useRef<HTMLDivElement>(null);
-	const openTooltip = useTooltipOpen();
-	const closeTooltip = useTooltipClose();
-
 	const numberOfEnabledApps = useMemo(() => {
 		return context === 'private' ? appsCount.totalPrivateEnabled : appsCount.totalMarketplaceEnabled;
 	}, [context, appsCount.totalMarketplaceEnabled, appsCount.totalPrivateEnabled]);
@@ -53,25 +48,7 @@ const EnabledAppsCount = (): ReactElement => {
 			mi='16px'
 			minWidth='200px'
 			justifyContent='center'
-			ref={elRef}
-			onMouseOver={(e): void => {
-				e.stopPropagation();
-				e.preventDefault();
-				elRef.current &&
-					openTooltip(
-						<MarkdownText
-							content={t('Apps_Count_Enabled_tooltip', {
-								number: enabledAppsLimit,
-								context,
-							})}
-							variant='inline'
-						/>,
-						elRef.current,
-					);
-			}}
-			onMouseLeave={(): void => {
-				closeTooltip();
-			}}
+			title={t('Apps_Count_Enabled_tooltip', { number: enabledAppsLimit, context })}
 		>
 			<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' w='full'>
 				<Box fontScale='c1'>{t('Apps_Count_Enabled', { count: numberOfEnabledApps })}</Box>
