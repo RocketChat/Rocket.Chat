d8c8e10027cdddb1f3ed51ae4f837dfa4c270e28
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-02-13 18:54:22 -0300

    usersearch

--- apps/meteor/app/ui/client/index.ts
+++ apps/meteor/app/ui/client/index.ts
@@ -4,7 +4,6 @@ import './lib/iframeCommands';
 import './lib/parentTemplate';
 import './lib/codeMirror';
 import './views/app/roomSearch.html';
-import './views/app/userSearch.html';
 import './views/app/roomSearch';
 import './views/app/photoswipeContent.ts'; // without the *.ts extension, *.html gets loaded first
 import './components/icon';
