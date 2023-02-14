55d671f8418aa46700594ca170ad4f6cae70aade
juliajforesti <juliajforesti@gmail.com>
2023-02-14 14:09:43 -0300

    fix PaletteStyleTag memo. Create hook to create style element

--- ee/packages/ui-theming/src/PaletteStyleTag.tsx
+++ ee/packages/ui-theming/src/PaletteStyleTag.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react';
+import { memo } from 'react';
 import { createPortal } from 'react-dom';
 
 import { convertToCss } from './convertToCss';
@@ -6,20 +7,15 @@ import { filterOnlyChangedColors } from './filterOnlyChangedColors';
 import { defaultPalette } from './palette';
 import { darkPalette } from './paletteDark';
 import { useThemeMode } from './hooks/useThemeMode';
+import { useCreateStyleContainer } from './hooks/useCreateStyleContainer';
 
-export const PaletteStyleTag = (): ReactElement | null => {
+export const PaletteStyleTag = memo((): ReactElement | null => {
 	const [, , theme] = useThemeMode();
 
 	if (theme !== 'dark') {
 		return null;
 	}
-
-	const customCssElement = document.getElementById('css-theme');
-	const styleElement = document.createElement('style');
-	styleElement.setAttribute('id', 'main-palette');
-	document.head.insertBefore(styleElement, customCssElement);
-
 	const palette = convertToCss(filterOnlyChangedColors(defaultPalette, darkPalette), '.rcx-content--main');
 
-	return createPortal(palette, document.getElementById('main-palette') || document.head);
-};
+	return createPortal(palette, useCreateStyleContainer('main-palette'));
+});
--- ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
+++ ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
@@ -8,19 +8,15 @@ import { darkPalette } from './paletteDark';
 import { filterOnlyChangedColors } from './filterOnlyChangedColors';
 import { convertToCss } from './convertToCss';
 import { useThemeMode } from './hooks/useThemeMode';
+import { useCreateStyleContainer } from './hooks/useCreateStyleContainer';
 
 export const SidebarPaletteStyleTag = memo((): ReactElement | null => {
 	const [, , theme] = useThemeMode();
-
-	const customCssElement = document.getElementById('css-theme');
-	const styleElement = document.createElement('style');
-	styleElement.setAttribute('id', 'sidebar-palette');
-	document.head.insertBefore(styleElement, customCssElement);
 
 	const palette = convertToCss(
 		theme === 'dark' ? filterOnlyChangedColors(darkPalette, sidebarPaletteDark) : { ...darkPalette, ...defaultSidebarPalette },
 		'.rcx-sidebar--main',
 	);
 
-	return createPortal(palette, document.getElementById('sidebar-palette') || document.head);
+	return createPortal(palette, useCreateStyleContainer('sidebar-palette'));
 });
