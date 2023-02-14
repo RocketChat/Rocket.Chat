3af4928c073d939182a4cd9fbaf59d53062a0d22
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-02-14 18:45:02 -0300

    fix review

--- ee/packages/ui-theming/src/hooks/useCreateStyleContainer.ts
+++ ee/packages/ui-theming/src/hooks/useCreateStyleContainer.ts
@@ -1,7 +1,11 @@
+import { useMemo } from 'react';
+
 export const useCreateStyleContainer = (id: string) => {
-	const refElement = document.getElementById('css-theme') || document.head.lastChild;
-	const styleElement = document.createElement('style');
-	styleElement.setAttribute('id', id);
-	document.head.insertBefore(styleElement, refElement);
-	return document.getElementById(id) || document.head.appendChild(document.createElement('style'));
+	return useMemo(() => {
+		const refElement = document.getElementById('css-theme') || document.head.lastChild;
+		const styleElement = document.createElement('style');
+		styleElement.setAttribute('id', id);
+		document.head.insertBefore(styleElement, refElement);
+		return document.getElementById(id) || document.head.appendChild(document.createElement('style'));
+	}, [id]);
 };
