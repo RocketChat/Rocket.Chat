2b22e2f7508c5e02f14871f7e25bfb66cc0f6a7b
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-02-13 22:40:00 -0300

    fix review

--- apps/meteor/client/views/room/components/body/composer/ComposerReadOnly.tsx
+++ apps/meteor/client/views/room/components/body/composer/ComposerReadOnly.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@rocket.chat/fuselage';
 import { MessageFooterCallout, MessageFooterCalloutContent } from '@rocket.chat/ui-composer';
 import { useEndpoint, useTranslation } from '@rocket.chat/ui-contexts';
+import { useMutation } from '@tanstack/react-query';
 import type { ReactElement } from 'react';
-import React, { useCallback, useRef, useEffect } from 'react';
+import React from 'react';
 
 import { dispatchToastMessage } from '../../../../../lib/toast';
 import { useRoom, useUserIsSubscribed } from '../../../contexts/RoomContext';
@@ -11,30 +12,20 @@ export const ComposerReadOnly = (): ReactElement => {
 	const t = useTranslation();
 	const room = useRoom();
 	const isSubscribed = useUserIsSubscribed();
-	const calloutRef = useRef<HTMLButtonElement>(null);
 	const joinChannel = useEndpoint('POST', '/v1/channels.join');
 
-	useEffect(() => {
-		if (!calloutRef.current) {
-			return;
-		}
-		calloutRef.current.style.flex = !isSubscribed ? '4' : 'none';
-	}, [isSubscribed]);
-
-	const join = useCallback(async () => {
-		try {
-			await joinChannel({ roomId: room._id });
-		} catch (error: unknown) {
+	const join = useMutation(() => joinChannel({ roomId: room._id }), {
+		onError: (error: unknown) => {
 			dispatchToastMessage({ type: 'error', message: error });
-		}
-	}, [joinChannel, room._id]);
+		},
+	});
 
 	return (
 		<footer className='rc-message-box footer'>
 			<MessageFooterCallout>
-				<MessageFooterCalloutContent ref={calloutRef}>{t('room_is_read_only')}</MessageFooterCalloutContent>
+				<MessageFooterCalloutContent>{t('room_is_read_only')}</MessageFooterCalloutContent>
 				{!isSubscribed && (
-					<Button primary onClick={join}>
+					<Button primary onClick={() => join.mutate()} disabled={join.isLoading}>
 						{t('Join')}
 					</Button>
 				)}
