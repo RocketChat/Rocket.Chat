4240029b7f52324dab30492b0a029543d921ab62
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-02-14 22:45:20 -0300

    create stories component

---
+++ apps/meteor/client/views/marketplace/components/GrandfatheredAppsModal.stories.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Modal } from '@rocket.chat/fuselage';
+import { useTranslation } from '@rocket.chat/ui-contexts';
+import React from 'react';
+
+const GrandfatheredAppsModal = () => {
+	const t = useTranslation();
+	const appName = 'Example-App-Name';
+
+	return (
+		<>
+			<Modal>
+				<Modal.Header>
+					<Modal.HeaderText>
+						<Modal.Title>{t('Uninstall_grandfathered_app', appName)}</Modal.Title>
+					</Modal.HeaderText>
+					<Modal.Close />
+				</Modal.Header>
+				<Modal.Content>
+					<Box fontScale='p2b' mbe='x16' display='flex' flexDirection='column'>
+						{t('App_will_lose_grandfathered_status')}
+					</Box>
+					{t('Workspaces_grandfathered_apps')}
+				</Modal.Content>
+				<Modal.Footer>
+					<Modal.FooterControllers>
+						<Button>{t('Cancel')}</Button>
+						<Button danger onClick={action('click')}>
+							{t('Uninstall')}
+						</Button>
+					</Modal.FooterControllers>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};
+
+export default GrandfatheredAppsModal;
