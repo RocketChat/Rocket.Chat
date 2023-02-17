f4c50055197bf5c41904cd6b6a40af2d68d9b05a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-02-17 02:14:33 -0300

    Chore: OAuth authorization pages reconditioned (#28085)

--- apps/meteor/app/oauth2-server-config/server/admin/methods/updateOAuthApp.ts
+++ apps/meteor/app/oauth2-server-config/server/admin/methods/updateOAuthApp.ts
@@ -1,29 +1,44 @@
 import { Meteor } from 'meteor/meteor';
-import _ from 'underscore';
 import { OAuthApps } from '@rocket.chat/models';
+import type { IOAuthApps } from '@rocket.chat/core-typings';
 
-import { hasPermission } from '../../../../authorization';
+import { hasPermission } from '../../../../authorization/server';
 import { Users } from '../../../../models/server';
 import { parseUriList } from '../functions/parseUriList';
 
+declare module '@rocket.chat/ui-contexts' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface ServerMethods {
+		updateOAuthApp: (applicationId: IOAuthApps['_id'], application: { name: string; redirectUri: string; active: boolean }) => void;
+	}
+}
+
 Meteor.methods({
-	async updateOAuthApp(applicationId, application) {
+	async updateOAuthApp(applicationId: IOAuthApps['_id'], application: Pick<IOAuthApps, 'name' | 'redirectUri' | 'active'>) {
+		if (!this.userId) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'updateOAuthApp' });
+		}
+
 		if (!hasPermission(this.userId, 'manage-oauth-apps')) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'updateOAuthApp' });
 		}
+
 		if (!application.name || typeof application.name.valueOf() !== 'string' || application.name.trim() === '') {
 			throw new Meteor.Error('error-invalid-name', 'Invalid name', { method: 'updateOAuthApp' });
 		}
+
 		if (!application.redirectUri || typeof application.redirectUri.valueOf() !== 'string' || application.redirectUri.trim() === '') {
 			throw new Meteor.Error('error-invalid-redirectUri', 'Invalid redirectUri', {
 				method: 'updateOAuthApp',
 			});
 		}
-		if (!_.isBoolean(application.active)) {
+
+		if (typeof application.active !== 'boolean') {
 			throw new Meteor.Error('error-invalid-arguments', 'Invalid arguments', {
 				method: 'updateOAuthApp',
 			});
 		}
+
 		const currentApplication = await OAuthApps.findOneById(applicationId);
 		if (currentApplication == null) {
 			throw new Meteor.Error('error-application-not-found', 'Application not found', {
