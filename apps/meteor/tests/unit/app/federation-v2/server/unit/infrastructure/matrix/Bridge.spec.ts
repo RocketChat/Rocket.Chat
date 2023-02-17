14dd794245f6f14c8121267ab240ccd859c458cc
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2023-02-17 13:43:33 -0600

    adds unit tests

--- apps/meteor/tests/unit/app/federation-v2/server/unit/infrastructure/matrix/Bridge.spec.ts
+++ apps/meteor/tests/unit/app/federation-v2/server/unit/infrastructure/matrix/Bridge.spec.ts
@@ -10,7 +10,7 @@ const { MatrixBridge } = proxyquire.noCallThru().load('../../../../../../../../a
 
 describe('Federation - Infrastructure - Matrix - Bridge', () => {
 	const defaultProxyDomain = 'server.com';
-	const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => { }); // eslint-disable-line
+	const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => {}); // eslint-disable-line
 
 	describe('#isUserIdFromTheSameHomeserver()', () => {
 		it('should return true if the userId is from the same homeserver', () => {
@@ -39,6 +39,80 @@ describe('Federation - Infrastructure - Matrix - Bridge', () => {
 
 		it('should return false if the room is from a different homeserver', () => {
 			expect(bridge.isRoomFromTheSameHomeserver('!room:server2.com', 'server.com')).to.be.false;
+		});
+	});
+
+	describe('#verifyInviteeId()', () => {
+		it('should return `valid-invitee-id` when the matrixId exists', async () => {
+			const fetchStub = {
+				fetch: () => Promise.resolve({ status: 400, json: () => Promise.resolve({ errcode: 'M_USER_IN_USE' }) }),
+			};
+
+			const { MatrixBridge } = proxyquire
+				.noCallThru()
+				.load('../../../../../../../../app/federation-v2/server/infrastructure/matrix/Bridge', {
+					'../../../../../server/lib/http/fetch': fetchStub,
+				});
+
+			const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => {}); // eslint-disable-line
+
+			const verificationStatus = await bridge.verifyInviteeId('@user:server.com');
+
+			expect(verificationStatus).to.be.equal('valid-invitee-id');
+		});
+
+		it('should return `invalid-invitee-id` when the matrixId does not exists', async () => {
+			const fetchStub = {
+				fetch: () => Promise.resolve({ status: 200 }),
+			};
+
+			const { MatrixBridge } = proxyquire
+				.noCallThru()
+				.load('../../../../../../../../app/federation-v2/server/infrastructure/matrix/Bridge', {
+					'../../../../../server/lib/http/fetch': fetchStub,
+				});
+
+			const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => {}); // eslint-disable-line
+
+			const verificationStatus = await bridge.verifyInviteeId('@user:server.com');
+
+			expect(verificationStatus).to.be.equal('invalid-invitee-id');
+		});
+
+		it('should return `unable-to-verify` when the fetch() call fails', async () => {
+			const fetchStub = {
+				fetch: () => Promise.reject(new Error('Error')),
+			};
+
+			const { MatrixBridge } = proxyquire
+				.noCallThru()
+				.load('../../../../../../../../app/federation-v2/server/infrastructure/matrix/Bridge', {
+					'../../../../../server/lib/http/fetch': fetchStub,
+				});
+
+			const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => {}); // eslint-disable-line
+
+			const verificationStatus = await bridge.verifyInviteeId('@user:server.com');
+
+			expect(verificationStatus).to.be.equal('unable-to-verify');
+		});
+
+		it('should return `unable-to-verify` when an unexepected status comes', async () => {
+			const fetchStub = {
+				fetch: () => Promise.resolve({ status: 500 }),
+			};
+
+			const { MatrixBridge } = proxyquire
+				.noCallThru()
+				.load('../../../../../../../../app/federation-v2/server/infrastructure/matrix/Bridge', {
+					'../../../../../server/lib/http/fetch': fetchStub,
+				});
+
+			const bridge = new MatrixBridge('', '', defaultProxyDomain, '', 3030, {} as any, () => {}); // eslint-disable-line
+
+			const verificationStatus = await bridge.verifyInviteeId('@user:server.com');
+
+			expect(verificationStatus).to.be.equal('unable-to-verify');
 		});
 	});
 });
