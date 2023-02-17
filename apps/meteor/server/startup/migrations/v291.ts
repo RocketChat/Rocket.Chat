60f51e940a02c1cc7c0a5a22a9319a7bc8eaaca9
Diego Sampaio <chinello@gmail.com>
2023-02-17 20:59:29 -0300

    use single migration

--- apps/meteor/server/startup/migrations/v291.ts
+++ apps/meteor/server/startup/migrations/v291.ts
@@ -1,3 +1,4 @@
+import { Settings } from '@rocket.chat/models';
 import type { IAppStorageItem } from '@rocket.chat/apps-engine/server/storage';
 import type { AppSignatureManager } from '@rocket.chat/apps-engine/server/managers/AppSignatureManager';
 
@@ -9,6 +10,10 @@ addMigration({
 	version: 291,
 	name: "Mark all installed apps as 'migrated', add 'installationSource' and 'signature'",
 	async up() {
+		// remove non-used settings
+		await Settings.removeById('Apps_Framework_Development_Mode');
+		await Settings.removeById('Apps_Framework_enabled');
+
 		Apps.initialize();
 
 		const sigMan = Apps.getManager()?.getSignatureManager() as AppSignatureManager;
