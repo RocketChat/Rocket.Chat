ec99e1518c1af9cdae7a88b5eaa12bb3de6c4127
Douglas Gubert <douglas.gubert@gmail.com>
2023-02-22 17:32:02 -0300

    Regression: Re-add app signature migration to fix the faulty previous one (#28107)

--- apps/meteor/server/startup/migrations/v292.ts
+++ apps/meteor/server/startup/migrations/v292.ts
@@ -1,20 +1,17 @@
-import { Settings } from '@rocket.chat/models';
+import type { AppSignatureManager } from '@rocket.chat/apps-engine/server/managers/AppSignatureManager';
 import type { IAppStorageItem } from '@rocket.chat/apps-engine/server/storage';
 
+import { Apps } from '../../../ee/server/apps';
 import type { AppRealStorage } from '../../../ee/server/apps/storage';
 import { addMigration } from '../../lib/migrations';
-import { Apps } from '../../../ee/server/apps';
 
 addMigration({
-	version: 291,
-	name: "Mark all installed apps as 'migrated', add 'installationSource'",
+	version: 292,
+	name: 'Add checksum signature to existing apps',
 	async up() {
-		// remove non-used settings
-		await Settings.removeById('Apps_Framework_Development_Mode');
-		await Settings.removeById('Apps_Framework_enabled');
-
 		Apps.initialize();
 
+		const sigMan = Apps.getManager()?.getSignatureManager() as AppSignatureManager;
 		const appsStorage = Apps.getStorage() as AppRealStorage;
 
 		const apps = await appsStorage.retrieveAll();
@@ -25,8 +22,7 @@ addMigration({
 			promises.push(
 				appsStorage.update({
 					...app,
-					migrated: true,
-					installationSource: 'marketplaceInfo' in app ? 'marketplace' : 'private',
+					signature: await sigMan.signApp(app),
 				} as IAppStorageItem),
 			),
 		);
