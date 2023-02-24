8d661e04a72ac2e81948ae32aa2b204779818f87
Hugo Costa <hugocarreiracosta@gmail.com>
2023-02-24 08:56:55 -0300

    fix: Image Attachment Description

--- apps/meteor/client/components/message/content/attachments/file/ImageAttachment.tsx
+++ apps/meteor/client/components/message/content/attachments/file/ImageAttachment.tsx
@@ -29,20 +29,22 @@ export const ImageAttachment: FC<ImageAttachmentProps> = ({
 	const getURL = useMediaUrl();
 
 	return (
-		<Attachment>
+		<>
 			{descriptionMd ? <MessageContentBody md={descriptionMd} /> : <MarkdownText parseEmoji content={description} />}
-			<MessageCollapsible title={title} hasDownload={hasDownload} link={getURL(link || url)} size={size}>
-				<AttachmentContent>
-					<AttachmentImage
-						{...imageDimensions}
-						loadImage={loadImage}
-						setLoadImage={setLoadImage}
-						dataSrc={getURL(link || url)}
-						src={getURL(url)}
-						previewUrl={`data:image/png;base64,${imagePreview}`}
-					/>
-				</AttachmentContent>
-			</MessageCollapsible>
-		</Attachment>
+			<Attachment>
+				<MessageCollapsible title={title} hasDownload={hasDownload} link={getURL(link || url)} size={size}>
+					<AttachmentContent>
+						<AttachmentImage
+							{...imageDimensions}
+							loadImage={loadImage}
+							setLoadImage={setLoadImage}
+							dataSrc={getURL(link || url)}
+							src={getURL(url)}
+							previewUrl={`data:image/png;base64,${imagePreview}`}
+						/>
+					</AttachmentContent>
+				</MessageCollapsible>
+			</Attachment>
+		</>
 	);
 };
