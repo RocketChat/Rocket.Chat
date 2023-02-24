b9c6aa2a0add79a95d84896f6c056bfde9294660
Hugo Costa <hugocarreiracosta@gmail.com>
2023-02-24 09:25:57 -0300

    fix: removing unnecessary components

--- apps/meteor/client/components/message/content/attachments/file/ImageAttachment.tsx
+++ apps/meteor/client/components/message/content/attachments/file/ImageAttachment.tsx
@@ -6,8 +6,6 @@ import React from 'react';
 import MarkdownText from '../../../../MarkdownText';
 import MessageCollapsible from '../../../MessageCollapsible';
 import MessageContentBody from '../../../MessageContentBody';
-import Attachment from '../structure/Attachment';
-import AttachmentContent from '../structure/AttachmentContent';
 import AttachmentImage from '../structure/AttachmentImage';
 import { useLoadImage } from './hooks/useLoadImage';
 
@@ -31,20 +29,16 @@ export const ImageAttachment: FC<ImageAttachmentProps> = ({
 	return (
 		<>
 			{descriptionMd ? <MessageContentBody md={descriptionMd} /> : <MarkdownText parseEmoji content={description} />}
-			<Attachment>
-				<MessageCollapsible title={title} hasDownload={hasDownload} link={getURL(link || url)} size={size}>
-					<AttachmentContent>
-						<AttachmentImage
-							{...imageDimensions}
-							loadImage={loadImage}
-							setLoadImage={setLoadImage}
-							dataSrc={getURL(link || url)}
-							src={getURL(url)}
-							previewUrl={`data:image/png;base64,${imagePreview}`}
-						/>
-					</AttachmentContent>
-				</MessageCollapsible>
-			</Attachment>
+			<MessageCollapsible title={title} hasDownload={hasDownload} link={getURL(link || url)} size={size}>
+				<AttachmentImage
+					{...imageDimensions}
+					loadImage={loadImage}
+					setLoadImage={setLoadImage}
+					dataSrc={getURL(link || url)}
+					src={getURL(url)}
+					previewUrl={`data:image/png;base64,${imagePreview}`}
+				/>
+			</MessageCollapsible>
 		</>
 	);
 };
