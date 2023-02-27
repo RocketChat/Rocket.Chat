d8646fca4125407aa2a06fc1a9756e2109096678
dougfabris <devfabris@gmail.com>
2023-02-27 17:50:55 -0300

    chore: update handleOpenConference

--- packages/web-ui-registration/src/GuestForm.tsx
+++ packages/web-ui-registration/src/GuestForm.tsx
@@ -2,9 +2,24 @@ import { Form } from '@rocket.chat/layout';
 import { Button, ButtonGroup } from '@rocket.chat/fuselage';
 import { useRoute, useCurrentRoute } from '@rocket.chat/ui-contexts';
 
+type WindowMaybeDesktop = typeof window & {
+	RocketChatDesktop?: {
+		openInternalVideoChatWindow?: (url: string, options: undefined) => void;
+	};
+};
+
 const GuestForm = () => {
 	const homeRoute = useRoute('home');
 	const [, , queryParams] = useCurrentRoute();
+
+	const handleOpenConference = () => {
+		const windowMaybeDesktop = window as WindowMaybeDesktop;
+		if (windowMaybeDesktop.RocketChatDesktop?.openInternalVideoChatWindow && queryParams?.callUrl) {
+			windowMaybeDesktop.RocketChatDesktop.openInternalVideoChatWindow(queryParams?.callUrl, undefined);
+		} else {
+			window.open(queryParams?.callUrl, '_blank', 'rel=noreferrer noopener');
+		}
+	};
 
 	return (
 		<Form>
@@ -16,7 +31,7 @@ const GuestForm = () => {
 					<Button primary onClick={() => homeRoute.push({ context: 'redirectConference' }, queryParams)}>
 						Login with Rocket.chat
 					</Button>
-					<Button onClick={() => window.open(queryParams?.callUrl, '_blank', 'rel=noreferrer noopener')}>Continue as guest</Button>
+					<Button onClick={handleOpenConference}>Continue as guest</Button>
 				</ButtonGroup>
 			</Form.Container>
 		</Form>
