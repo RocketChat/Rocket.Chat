9547ea75ac060674af7bc807c0654c7d73482dfc
dougfabris <devfabris@gmail.com>
2023-02-27 14:29:11 -0300

    chore: ooops

--- apps/meteor/client/views/home/HomePage.tsx
+++ apps/meteor/client/views/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useSetting, useCurrentRoute, useRoute } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import CustomHomePage from './CustomHomePage';
 import DefaultHomePage from './DefaultHomePage';
@@ -8,11 +8,13 @@ import DefaultHomePage from './DefaultHomePage';
 const HomePage = (): ReactElement => {
 	const conferenceRoute = useRoute('conference');
 	const [, params, queryParams] = useCurrentRoute();
-	if (params?.context === 'redirectConference') {
-		conferenceRoute.push(params, queryParams);
-	}
-
 	const customOnly = useSetting('Layout_Custom_Body_Only');
+
+	useEffect(() => {
+		if (params?.context === 'redirectConference') {
+			conferenceRoute.push(params, queryParams);
+		}
+	}, [conferenceRoute, params, queryParams]);
 
 	if (customOnly) {
 		return <CustomHomePage />;
