ea5f110c3ecde898b11aeb397db37894daa2f6cc
juliajforesti <juliajforesti@gmail.com>
2023-02-28 20:47:16 -0300

    add settings-assets test

--- apps/meteor/tests/e2e/settings-assets.spec.ts
+++ apps/meteor/tests/e2e/settings-assets.spec.ts
@@ -16,8 +16,42 @@ test.describe.serial('settings-assets', () => {
 		await expect(page.locator('[data-qa-type="PageHeader-title"]')).toHaveText('Assets');
 	});
 
-	test('expect upload and delete asset and label should be visible', async ({ page }) => {
+	test('expect upload and delete logo asset and label should be visible', async ({ page }) => {
 		await expect(page.locator('[title="Assets_logo"]')).toHaveText('logo (svg, png, jpg)');
+
+		await poAdmin.inputAssetsLogo.setInputFiles('./tests/e2e/fixtures/files/test-image.jpeg');
+
+		await expect(page.locator('role=img[name="Asset preview"]')).toBeVisible();
+
+		await poAdmin.btnDeleteAssetsLogo.click();
+
+		await expect(page.locator('role=img[name="Asset preview"]')).not.toBeVisible();
+	});
+
+	test('expect upload and delete logo asset for dark theme and label should be visible', async ({ page }) => {
+		await expect(page.locator('[title="Assets_logo_dark"]')).toHaveText('logo - dark theme (svg, png, jpg)');
+
+		await poAdmin.inputAssetsLogo.setInputFiles('./tests/e2e/fixtures/files/test-image.jpeg');
+
+		await expect(page.locator('role=img[name="Asset preview"]')).toBeVisible();
+
+		await poAdmin.btnDeleteAssetsLogo.click();
+
+		await expect(page.locator('role=img[name="Asset preview"]')).not.toBeVisible();
+	});
+	test('expect upload and delete background asset and label should be visible', async ({ page }) => {
+		await expect(page.locator('[title="Assets_background"]')).toHaveText('login background (svg, png, jpg)');
+
+		await poAdmin.inputAssetsLogo.setInputFiles('./tests/e2e/fixtures/files/test-image.jpeg');
+
+		await expect(page.locator('role=img[name="Asset preview"]')).toBeVisible();
+
+		await poAdmin.btnDeleteAssetsLogo.click();
+
+		await expect(page.locator('role=img[name="Asset preview"]')).not.toBeVisible();
+	});
+	test('expect upload and delete background asset for dark theme and label should be visible', async ({ page }) => {
+		await expect(page.locator('[title="Assets_background_dark"]')).toHaveText('login background - dark theme (svg, png, jpg)');
 
 		await poAdmin.inputAssetsLogo.setInputFiles('./tests/e2e/fixtures/files/test-image.jpeg');
 
