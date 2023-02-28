1a243419c59973e4b7693acd4769ecd7969588b0
juliajforesti <juliajforesti@gmail.com>
2023-02-28 16:50:37 -0300

    useAssetPath according to seleted theme mode

--- packages/ui-contexts/src/hooks/useAssetPath.ts
+++ packages/ui-contexts/src/hooks/useAssetPath.ts
@@ -1,8 +1,15 @@
+import { useDarkMode } from '@rocket.chat/fuselage-hooks';
+
 import { useAbsoluteUrl } from './useAbsoluteUrl';
 import { useSetting } from './useSetting';
+import { useUserPreference } from './useUserPreference';
 
 export const useAssetPath = (assetId: string): string | undefined => {
-	const asset = useSetting<{ url?: string; defaultUrl?: string }>(assetId);
+	const userThemePreference = useUserPreference('themeAppearence') || 'auto';
+	const theme = useDarkMode(userThemePreference === 'auto' ? undefined : userThemePreference === 'dark') ? 'dark' : 'light';
+	const themeAssetId = theme === 'dark' ? `${assetId}_dark` : assetId;
+
+	const asset = useSetting<{ url?: string; defaultUrl?: string }>(themeAssetId);
 	const absoluteUrl = useAbsoluteUrl();
 
 	return (asset?.url ?? asset?.defaultUrl) && absoluteUrl(asset?.url ?? asset?.defaultUrl ?? '');
