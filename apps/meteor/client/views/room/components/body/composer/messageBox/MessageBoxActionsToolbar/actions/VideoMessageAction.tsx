41f39cbe25ee7f00abc4c27ba9ddd44e118ffd8d
dougfabris <devfabris@gmail.com>
2023-02-28 14:44:58 -0300

    chore: videoMessagePermission

--- apps/meteor/client/views/room/components/body/composer/messageBox/MessageBoxActionsToolbar/actions/VideoMessageAction.tsx
+++ apps/meteor/client/views/room/components/body/composer/messageBox/MessageBoxActionsToolbar/actions/VideoMessageAction.tsx
@@ -1,8 +1,9 @@
 import { Option, OptionIcon, OptionContent } from '@rocket.chat/fuselage';
+import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { MessageComposerAction } from '@rocket.chat/ui-composer';
 import { useTranslation, useSetting } from '@rocket.chat/ui-contexts';
 import type { AllHTMLAttributes } from 'react';
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
 import type { ChatAPI } from '../../../../../../../../lib/chats/ChatAPI';
 import { useChat } from '../../../../../../contexts/ChatContext';
@@ -14,8 +15,9 @@ type VideoMessageActionProps = {
 
 const VideoMessageAction = ({ collapsed, chatContext, disabled, ...props }: VideoMessageActionProps) => {
 	const t = useTranslation();
-	const fileUploadEnabled = useSetting('FileUpload_Enabled');
-	const messageVideoRecorderEnabled = useSetting('Message_VideoRecorderEnabled');
+	const [isVideoDenied, setIsVideoDenied] = useState(false);
+	const isFileUploadEnabled = useSetting('FileUpload_Enabled');
+	const isVideoRecorderEnabled = useSetting('Message_VideoRecorderEnabled');
 	const fileUploadMediaTypeBlackList = useSetting('FileUpload_MediaTypeBlackList') as string;
 	const fileUploadMediaTypeWhiteList = useSetting('FileUpload_MediaTypeWhiteList') as string;
 
@@ -27,26 +29,80 @@ const VideoMessageAction = ({ collapsed, chatContext, disabled, ...props }: Vide
 		}
 	};
 
-	const enableVideoMessage =
+	const handleDenyVideo = useMutableCallback((isDenied) => {
+		if (isDenied) {
+			chat?.composer?.setRecordingVideo(false);
+		}
+
+		setIsVideoDenied(isDenied);
+	});
+
+	const handleMount = useMutableCallback(async (): Promise<void> => {
+		if (navigator.permissions) {
+			try {
+				const permissionStatus = await navigator.permissions.query({ name: 'camera' as PermissionName });
+				handleDenyVideo(permissionStatus.state === 'denied');
+				permissionStatus.onchange = (): void => {
+					handleDenyVideo(permissionStatus.state === 'denied');
+				};
+				return;
+			} catch (error) {
+				console.warn(error);
+			}
+		}
+
+		if (!navigator.mediaDevices?.enumerateDevices) {
+			handleDenyVideo(true);
+			return;
+		}
+
+		try {
+			if (!(await navigator.mediaDevices.enumerateDevices()).some(({ kind }) => kind === 'videoinput')) {
+				handleDenyVideo(true);
+				return;
+			}
+		} catch (error) {
+			console.warn(error);
+		}
+	});
+
+	useEffect(() => {
+		handleMount();
+	}, [handleMount]);
+
+	const isAllowed =
+		!isVideoDenied &&
 		navigator.mediaDevices &&
 		window.MediaRecorder &&
-		fileUploadEnabled &&
-		messageVideoRecorderEnabled &&
-		(!fileUploadMediaTypeBlackList || !fileUploadMediaTypeBlackList.match(/video\/webm|video\/\*/i)) &&
+		isFileUploadEnabled &&
+		isVideoRecorderEnabled &&
+		!fileUploadMediaTypeBlackList?.match(/video\/webm|video\/\*/i) &&
 		(!fileUploadMediaTypeWhiteList || fileUploadMediaTypeWhiteList.match(/video\/webm|video\/\*/i)) &&
 		window.MediaRecorder.isTypeSupported('video/webm; codecs=vp8,opus');
 
-	if (!enableVideoMessage) {
-		return null;
-	}
+	const getVideoActionTitle = useMemo(() => {
+		if (isVideoDenied) {
+			return t('Camera_access_not_allowed');
+		}
+
+		if (!isFileUploadEnabled) {
+			return t('File_Upload_Disabled');
+		}
+
+		if (!isVideoRecorderEnabled) {
+			return t('Message_Video_Recording_Disabled');
+		}
+
+		if (!isAllowed) {
+			return t('error-not-allowed');
+		}
+
+		return t('Video_message');
+	}, [isVideoDenied, isFileUploadEnabled, isVideoRecorderEnabled, isAllowed, t]);
 
 	if (collapsed) {
 		return (
-			<Option
-				{...((!enableVideoMessage || disabled) && { title: t('Not_Available') })}
-				disabled={!enableVideoMessage || disabled}
-				onClick={handleOpenVideoMessage}
-			>
+			<Option title={getVideoActionTitle} disabled={!isAllowed || disabled} onClick={handleOpenVideoMessage}>
 				<OptionIcon name='video' />
 				<OptionContent>{t('Video_message')}</OptionContent>
 			</Option>
@@ -57,9 +113,9 @@ const VideoMessageAction = ({ collapsed, chatContext, disabled, ...props }: Vide
 		<MessageComposerAction
 			data-qa-id='video-message'
 			icon='video'
-			disabled={!enableVideoMessage || disabled}
+			disabled={!isAllowed || disabled}
 			onClick={handleOpenVideoMessage}
-			title={t('Video_message')}
+			title={getVideoActionTitle}
 			{...props}
 		/>
 	);
