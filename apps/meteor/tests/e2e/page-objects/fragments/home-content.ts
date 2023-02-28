ae4308b2b89f596ea1be31e58dc388a7f261569f
Marcos Defendi <marcos.defendi@rocket.chat>
2023-02-28 09:53:16 -0300

    fix: add missing wait

--- apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
@@ -56,6 +56,7 @@ export class HomeContent {
 
 	async dispatchSlashCommand(text: string): Promise<void> {
 		await this.joinRoomIfNeeded();
+		await this.page.waitForSelector('[name="msg"]:not([disabled])');
 		await this.page.locator('[name="msg"]').type(text);
 		await this.page.keyboard.press('Enter');
 		await this.page.keyboard.press('Enter');
