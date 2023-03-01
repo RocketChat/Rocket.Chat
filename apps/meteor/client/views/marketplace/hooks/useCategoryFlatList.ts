9a563e071474257a404113febea5ba15ede3d463
rique223 <henrique.jobs1@gmail.com>
2023-02-28 21:17:44 -0300

    Typecheck

--- apps/meteor/client/views/marketplace/hooks/useCategoryFlatList.ts
+++ apps/meteor/client/views/marketplace/hooks/useCategoryFlatList.ts
@@ -2,5 +2,5 @@ import { useMemo } from 'react';
 
 import type { CategoryDropdownItem, CategoryDropDownListProps } from '../definitions/CategoryDropdownDefinitions';
 
-export const useCategoryFlatList = (data: CategoryDropDownListProps['groups']): CategoryDropdownItem[] =>
-	useMemo(() => data.flatMap((group) => group.items).filter(({ id }) => id !== 'all'), [data]);
+export const useCategoryFlatList = (categories: CategoryDropDownListProps['categories']): CategoryDropdownItem[] =>
+	useMemo(() => categories.flatMap((group) => group.items).filter(({ id }) => id !== 'all'), [categories]);
