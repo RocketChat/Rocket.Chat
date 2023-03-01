ceff8a74176c2c1cd88439d580a3811a2acf247e
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-02-28 23:05:30 -0300

    fix review

--- packages/ui-contexts/src/hooks/useAssetPath.ts
+++ packages/ui-contexts/src/hooks/useAssetPath.ts
@@ -1,15 +1,10 @@
-import { useDarkMode } from '@rocket.chat/fuselage-hooks';
+import type { IRocketChatAssets } from '@rocket.chat/core-typings';
 
 import { useAbsoluteUrl } from './useAbsoluteUrl';
 import { useSetting } from './useSetting';
-import { useUserPreference } from './useUserPreference';
 
-export const useAssetPath = (assetId: string): string | undefined => {
-	const userThemePreference = useUserPreference('themeAppearence') || 'auto';
-	const theme = useDarkMode(userThemePreference === 'auto' ? undefined : userThemePreference === 'dark') ? 'dark' : 'light';
-	const themeAssetId = theme === 'dark' ? `${assetId}_dark` : assetId;
-
-	const asset = useSetting<{ url?: string; defaultUrl?: string }>(themeAssetId);
+export const useAssetPath = (assetId: keyof IRocketChatAssets): string | undefined => {
+	const asset = useSetting<{ url?: string; defaultUrl?: string }>(`Assets_${assetId}`);
 	const absoluteUrl = useAbsoluteUrl();
 
 	return (asset?.url ?? asset?.defaultUrl) && absoluteUrl(asset?.url ?? asset?.defaultUrl ?? '');
