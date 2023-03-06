18b0da41582e8bacd80fd87fd60de82d4084505b
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-03-06 18:58:12 +0000

    Unnecessary hover effect when hover on user / room name in room information and user information section

--- apps/meteor/client/components/InfoPanel/InfoPanelTitle.tsx
+++ apps/meteor/client/components/InfoPanel/InfoPanelTitle.tsx
@@ -10,7 +10,7 @@ type InfoPanelTitleProps = {
 const isValidIcon = (icon: ReactNode): icon is ComponentProps<typeof Icon>['name'] => typeof icon === 'string';
 
 const InfoPanelTitle: FC<InfoPanelTitleProps> = ({ title, icon }) => (
-	<Box display='flex' title={title} flexShrink={0} alignItems='center' fontScale='h4' color='default' withTruncatedText>
+	<Box display='flex' flexShrink={0} alignItems='center' fontScale='h4' color='default' withTruncatedText>
 		{isValidIcon(icon) ? <Icon name={icon} size='x22' /> : icon}
 		<Box mis='x8' flexGrow={1} withTruncatedText>
 			{title}
