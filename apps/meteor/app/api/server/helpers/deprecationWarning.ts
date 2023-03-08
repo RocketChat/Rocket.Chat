ec6d0b3934d1d639aaf8b7a998558a99a1e24087
Douglas Gubert <douglas.gubert@gmail.com>
2023-03-07 22:55:29 -0300

    Regression: OAuth Login (#28303)
    Co-authored-by: Rodrigo Nascimento <234261+rodrigok@users.noreply.github.com>
    Co-authored-by: Guilherme Gazzo <5263975+ggazzo@users.noreply.github.com>

--- apps/meteor/app/api/server/helpers/deprecationWarning.ts
+++ apps/meteor/app/api/server/helpers/deprecationWarning.ts
@@ -1,20 +1,24 @@
 import { API } from '../api';
 import { apiDeprecationLogger } from '../../../lib/server/lib/deprecationWarningLogger';
 
-export function deprecationWarning<T>({
-	endpoint,
-	versionWillBeRemoved = '5.0',
-	response,
-}: {
+type DeprecationWarningParams<T> = {
 	endpoint: string;
 	versionWillBeRemoved?: string;
 	response: T;
-}): T {
-	const warningMessage = `The endpoint "${endpoint}" is deprecated and will be removed after version ${versionWillBeRemoved}`;
-	apiDeprecationLogger.warn(warningMessage);
+	warningMessage?: string | ((props: Omit<DeprecationWarningParams<T>, 'warningMessage'>) => string);
+};
+export function deprecationWarning<T>({
+	endpoint,
+	versionWillBeRemoved = '6.0',
+	response,
+	warningMessage = `The endpoint "${endpoint}" is deprecated and will be removed on version ${versionWillBeRemoved}`,
+}: DeprecationWarningParams<T>): T {
+	const warning = typeof warningMessage === 'function' ? warningMessage({ endpoint, versionWillBeRemoved, response }) : warningMessage;
+
+	apiDeprecationLogger.warn(warning);
 	if (process.env.NODE_ENV === 'development') {
 		return {
-			warning: warningMessage,
+			warning,
 			...response,
 		};
 	}
