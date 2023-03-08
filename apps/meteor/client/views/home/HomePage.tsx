0f0ec2320b2a9ede7dd4fd7052de42cd1247885b
dougfabris <devfabris@gmail.com>
2023-03-08 17:29:30 -0300

    chore: translation

--- apps/meteor/client/views/home/HomePage.tsx
+++ apps/meteor/client/views/home/HomePage.tsx
@@ -1,20 +1,12 @@
-import { useSetting, useCurrentRoute, useRoute } from '@rocket.chat/ui-contexts';
+import { useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import CustomHomePage from './CustomHomePage';
 import DefaultHomePage from './DefaultHomePage';
 
 const HomePage = (): ReactElement => {
-	const conferenceRoute = useRoute('conference');
-	const [, params, queryParams] = useCurrentRoute();
 	const customOnly = useSetting('Layout_Custom_Body_Only');
-
-	useEffect(() => {
-		if (params?.context === 'redirectConference') {
-			conferenceRoute.push(params, queryParams);
-		}
-	}, [conferenceRoute, params, queryParams]);
 
 	if (customOnly) {
 		return <CustomHomePage />;
--- packages/web-ui-registration/src/GuestForm.tsx
+++ packages/web-ui-registration/src/GuestForm.tsx
@@ -1,20 +1,23 @@
 import { Form } from '@rocket.chat/layout';
 import { Button, ButtonGroup } from '@rocket.chat/fuselage';
+import { useTranslation } from 'react-i18next';
 
 import type { DispatchLoginRouter } from './hooks/useLoginRouter';
 
 const GuestForm = ({ setLoginRoute }: { setLoginRoute: DispatchLoginRouter }) => {
+	const { t } = useTranslation();
+
 	return (
 		<Form>
 			<Form.Header>
-				<Form.Title>Choose how you want to join</Form.Title>
+				<Form.Title>{t('registration.page.guest.chooseHowToJoin')}</Form.Title>
 			</Form.Header>
 			<Form.Container>
 				<ButtonGroup large stretch vertical>
 					<Button primary onClick={() => setLoginRoute('login')}>
-						Login with Rocket.chat
+						{t('registration.page.guest.loginWithRocketChat')}
 					</Button>
-					<Button onClick={() => setLoginRoute('anonymous')}>Continue as guest</Button>
+					<Button onClick={() => setLoginRoute('anonymous')}>{t('registration.page.guest.continueAsGuest')}</Button>
 				</ButtonGroup>
 			</Form.Container>
 		</Form>
