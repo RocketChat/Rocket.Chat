b92b6761d117d6a0f53ccd6afd565414740355d9
Kevin Aleman <kaleman960@gmail.com>
2023-03-13 10:26:54 -0600

    chore: Migrate SMS to Service (#28123)

--- apps/meteor/app/livechat/server/livechat.ts
+++ apps/meteor/app/livechat/server/livechat.ts
@@ -1,17 +1,20 @@
 import url from 'url';
 
-import _ from 'underscore';
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import { settings } from '../../settings/server';
 import { addServerUrlToIndex } from '../lib/Assets';
 
-const indexHtmlWithServerURL = addServerUrlToIndex(Assets.getText('livechat/index.html'));
+const indexHtmlWithServerURL = addServerUrlToIndex(Assets.getText('livechat/index.html') || '');
 
 WebApp.connectHandlers.use(
 	'/livechat',
 	Meteor.bindEnvironment((req, res, next) => {
+		if (!req.url) {
+			return next();
+		}
+
 		const reqUrl = url.parse(req.url);
 		if (reqUrl.pathname !== '/') {
 			return next();
@@ -19,14 +22,13 @@ WebApp.connectHandlers.use(
 
 		res.setHeader('content-type', 'text/html; charset=utf-8');
 
-		let domainWhiteList = settings.get('Livechat_AllowedDomainsList');
-		if (req.headers.referer && !_.isEmpty(domainWhiteList.trim())) {
-			domainWhiteList = _.map(domainWhiteList.split(','), function (domain) {
-				return domain.trim();
-			});
+		const domainWhiteListSetting = settings.get<string>('Livechat_AllowedDomainsList');
+		let domainWhiteList = [];
+		if (req.headers.referer && !domainWhiteListSetting.trim()) {
+			domainWhiteList = domainWhiteListSetting.split(',').map((domain) => domain.trim());
 
 			const referer = url.parse(req.headers.referer);
-			if (!_.contains(domainWhiteList, referer.host)) {
+			if (referer.host && !domainWhiteList.includes(referer.host)) {
 				res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
 				return next();
 			}
--- apps/meteor/app/livechat/server/sendMessageBySMS.ts
+++ apps/meteor/app/livechat/server/sendMessageBySMS.ts
@@ -1,8 +1,9 @@
+import { isEditedMessage, isOmnichannelRoom } from '@rocket.chat/core-typings';
 import { LivechatVisitors } from '@rocket.chat/models';
+import { OmnichannelIntegration } from '@rocket.chat/core-services';
 
 import { callbacks } from '../../../lib/callbacks';
 import { settings } from '../../settings/server';
-import { SMS } from '../../sms/server';
 import { normalizeMessageFileUpload } from '../../utils/server/functions/normalizeMessageFileUpload';
 import { callbackLogger } from './lib/callbackLogger';
 
@@ -11,18 +12,18 @@ callbacks.add(
 	function (message, room) {
 		callbackLogger.debug('Attempting to send SMS message');
 		// skips this callback if the message was edited
-		if (message.editedAt) {
+		if (isEditedMessage(message)) {
 			callbackLogger.debug('Message was edited, skipping SMS send');
 			return message;
 		}
 
-		if (!SMS.enabled) {
+		if (!settings.get('SMS_Enabled')) {
 			callbackLogger.debug('SMS is not enabled, skipping SMS send');
 			return message;
 		}
 
 		// only send the sms by SMS if it is a livechat room with SMS set to true
-		if (!(typeof room.t !== 'undefined' && room.t === 'l' && room.sms && room.v && room.v.token)) {
+		if (!(isOmnichannelRoom(room) && room.sms && room.v && room.v.token)) {
 			callbackLogger.debug('Room is not a livechat room, skipping SMS send');
 			return message;
 		}
@@ -39,9 +40,10 @@ callbacks.add(
 			return message;
 		}
 
-		let extraData;
+		let extraData = {};
 		if (message.file) {
 			message = Promise.await(normalizeMessageFileUpload(message));
+			// @ts-expect-error TODO: investigate from where fileUpload comes
 			const { fileUpload, rid, u: { _id: userId } = {} } = message;
 			extraData = Object.assign({}, { rid, userId, fileUpload });
 		}
@@ -51,16 +53,16 @@ callbacks.add(
 			extraData = Object.assign({}, extraData, { location });
 		}
 
-		const SMSService = SMS.getService(settings.get('SMS_Service'));
+		const SMSService = Promise.await(OmnichannelIntegration.getSmsService(settings.get('SMS_Service')));
 
 		if (!SMSService) {
 			callbackLogger.debug('SMS Service is not configured, skipping SMS send');
 			return message;
 		}
 
-		const visitor = Promise.await(LivechatVisitors.getVisitorByToken(room.v.token));
+		const visitor = Promise.await(LivechatVisitors.getVisitorByToken(room.v.token, { projection: { phone: 1 } }));
 
-		if (!visitor || !visitor.phone || visitor.phone.length === 0) {
+		if (!visitor?.phone || visitor.phone.length === 0) {
 			callbackLogger.debug('Visitor does not have a phone number, skipping SMS send');
 			return message;
 		}
