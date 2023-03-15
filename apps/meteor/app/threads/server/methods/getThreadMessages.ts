1bc24026fe318f0167d3a854adef244b6ea677f0
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-03-15 10:22:33 -0300

    refactor: Types of Meteor methods (2/N) (#28424)

--- apps/meteor/app/threads/server/methods/getThreadMessages.ts
+++ apps/meteor/app/threads/server/methods/getThreadMessages.ts
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import type { IMessage } from '@rocket.chat/core-typings';
+import type { ServerMethods } from '@rocket.chat/ui-contexts';
 
 import { callbacks } from '../../../../lib/callbacks';
 import { Messages, Rooms } from '../../../models/server';
@@ -6,11 +8,18 @@ import { canAccessRoom } from '../../../authorization/server';
 import { settings } from '../../../settings/server';
 import { readThread } from '../functions';
 
+declare module '@rocket.chat/ui-contexts' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface ServerMethods {
+		getThreadMessages(params: { tmid: IMessage['_id']; limit?: number; skip?: number }): IMessage[];
+	}
+}
+
 const MAX_LIMIT = 100;
 
-Meteor.methods({
+Meteor.methods<ServerMethods>({
 	getThreadMessages({ tmid, limit, skip }) {
-		if (limit > MAX_LIMIT) {
+		if ((limit ?? 0) > MAX_LIMIT) {
 			throw new Meteor.Error('error-not-allowed', `max limit: ${MAX_LIMIT}`, {
 				method: 'getThreadMessages',
 			});
@@ -30,7 +39,7 @@ Meteor.methods({
 		const user = Meteor.user();
 		const room = Rooms.findOneById(thread.rid);
 
-		if (!canAccessRoom(room, user)) {
+		if (!user || !canAccessRoom(room, user)) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'getThreadMessages' });
 		}
 
--- packages/ui-contexts/src/ServerContext/methods/webdav/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './getWebdavFileList';
-export * from './getWebdavFilePreview';
