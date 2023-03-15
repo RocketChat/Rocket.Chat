4bb07753c3596b5e19f69678165cc7ede6c231c4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-03-15 10:22:33 -0300

    refactor: Types of Meteor methods (2/N) (#28424)

--- apps/meteor/server/methods/loadMissedMessages.ts
+++ apps/meteor/server/methods/loadMissedMessages.ts
@@ -1,15 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import type { IMessage, IRoom } from '@rocket.chat/core-typings';
+import type { ServerMethods } from '@rocket.chat/ui-contexts';
 
 import { canAccessRoomId } from '../../app/authorization/server';
 import { Messages } from '../../app/models/server';
 
-Meteor.methods({
+declare module '@rocket.chat/ui-contexts' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface ServerMethods {
+		loadMissedMessages(rid: IRoom['_id'], ts: Date): IMessage[];
+	}
+}
+
+Meteor.methods<ServerMethods>({
 	loadMissedMessages(rid, start) {
 		check(rid, String);
 		check(start, Date);
 
-		const fromId = Meteor.userId();
+		const fromId = Meteor.userId() ?? undefined;
 
 		if (!rid) {
 			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'getUsersOfRoom' });
