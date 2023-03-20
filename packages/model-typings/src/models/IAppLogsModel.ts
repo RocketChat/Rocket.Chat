7c971da9721c373a4d19a609168ba6c532b6f516
thassiov <tvmcarvalho@gmail.com>
2023-03-20 18:44:56 -0300

    Remove duplicate model typing for apps logs

--- packages/model-typings/src/models/IAppLogsModel.ts
+++
@@ -1,6 +0,0 @@
-import type { IBaseModel } from './IBaseModel';
-
-// TODO: type for AppLogs
-export interface IAppLogsModel extends IBaseModel<any> {
-	resetTTLIndex(expireAfterSeconds: number): Promise<void>;
-}
