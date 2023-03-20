9c641131a3d0d07a4dbc9114e345d31469cfb35b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-03-20 19:16:41 -0300

    Retype some Meteor methods

--- apps/meteor/app/federation/server/methods/dashboard.ts
+++ apps/meteor/app/federation/server/methods/dashboard.ts
@@ -1,5 +1,8 @@
 import type { IFederationServer } from '@rocket.chat/core-typings';
-import '@rocket.chat/ui-contexts';
+import type { ServerMethods } from '@rocket.chat/ui-contexts';
+import { Meteor } from 'meteor/meteor';
+
+import { federationGetServers, federationGetOverviewData } from '../functions/dashboard';
 
 declare module '@rocket.chat/ui-contexts' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
@@ -8,3 +11,8 @@ declare module '@rocket.chat/ui-contexts' {
 		'federation:getOverviewData': () => { data: { title: string; value: number }[] };
 	}
 }
+
+Meteor.methods<ServerMethods>({
+	'federation:getServers': federationGetServers,
+	'federation:getOverviewData': federationGetOverviewData,
+});
