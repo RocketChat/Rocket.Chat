56a6736272200d91becb55b0bdf2f6a0bfc7aadb
Kevin Aleman <kaleman960@gmail.com>
2023-03-22 17:59:32 -0600

    refactor: Don't use meteor collections on migrations (#28563)

--- apps/meteor/server/startup/migrations/v269.ts
+++ apps/meteor/server/startup/migrations/v269.ts
@@ -1,10 +1,15 @@
+import { Rooms } from '@rocket.chat/models';
+
 import { addMigration } from '../../lib/migrations';
-import { Rooms } from '../../../app/models/server';
 
 addMigration({
 	version: 269,
-	up() {
-		Rooms.tryDropIndex({ 'tokenpass.tokens.token': 1 });
-		Rooms.tryDropIndex({ tokenpass: 1 });
+	async up() {
+		try {
+			await Rooms.col.dropIndex('tokenpass.tokens.token_1');
+			await Rooms.col.dropIndex('tokenpass_1');
+		} catch (e) {
+			console.log(e);
+		}
 	},
 });
