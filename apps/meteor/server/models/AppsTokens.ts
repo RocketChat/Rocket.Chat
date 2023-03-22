a2e0a54f746b765d36000329cec9226db98a138a
Kevin Aleman <kaleman960@gmail.com>
2023-03-22 15:36:24 -0600

    wrong model name

--- apps/meteor/server/models/AppsTokens.ts
+++ apps/meteor/server/models/AppsTokens.ts
@@ -3,4 +3,4 @@ import { registerModel } from '@rocket.chat/models';
 import { db } from '../database/utils';
 import { AppsTokens } from './raw/AppsTokens';
 
-registerModel('IAppsTokens', new AppsTokens(db));
+registerModel('IAppsTokensModel', new AppsTokens(db));
