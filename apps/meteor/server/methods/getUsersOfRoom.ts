ee3df58ac0eab38eb04eebc992c5ac625273d660
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-03-22 11:10:36 -0300

    refactor: Types of Meteor methods (9/N) (#28528)

--- apps/meteor/server/methods/getUsersOfRoom.ts
+++ apps/meteor/server/methods/getUsersOfRoom.ts
@@ -1,12 +1,29 @@
+import type { ServerMethods } from '@rocket.chat/ui-contexts';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import type { IRoom, IUser } from '@rocket.chat/core-typings';
 
 import { Rooms, Subscriptions } from '../../app/models/server';
 import { canAccessRoomAsync, roomAccessAttributes } from '../../app/authorization/server';
 import { hasPermissionAsync } from '../../app/authorization/server/functions/hasPermission';
 import { findUsersOfRoom } from '../lib/findUsersOfRoom';
 
-Meteor.methods({
+declare module '@rocket.chat/ui-contexts' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface ServerMethods {
+		getUsersOfRoom(
+			rid: IRoom['_id'],
+			showAll?: boolean,
+			options?: { limit?: number; skip?: number },
+			filter?: string,
+		): {
+			total: number;
+			records: IUser[];
+		};
+	}
+}
+
+Meteor.methods<ServerMethods>({
 	async getUsersOfRoom(rid, showAll, { limit, skip } = {}, filter) {
 		if (!rid) {
 			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'getUsersOfRoom' });
