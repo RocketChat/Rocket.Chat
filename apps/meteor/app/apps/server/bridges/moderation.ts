1086d1044ff72be7eb422b5e4c99788707a8003e
デヴぁんす <sdevanshu90@yahoo.com>
2023-03-24 11:28:15 +0530

    fix reportMessage emtfunction and moderation bridge

--- apps/meteor/app/apps/server/bridges/moderation.ts
+++ apps/meteor/app/apps/server/bridges/moderation.ts
@@ -1,15 +1,15 @@
-import { Meteor } from 'meteor/meteor';
 import { ModerationBridge } from '@rocket.chat/apps-engine/server/bridges/ModerationBridge';
 import type { IMessage } from '@rocket.chat/core-typings';
 
-import type { AppServerOrchestrator } from '../orchestrator';
+import type { AppServerOrchestrator } from '../../../../ee/server/apps/orchestrator';
+import { reportMessage } from '../../../../server/lib/moderation/reportMessage';
 
 export class AppModerationBridge extends ModerationBridge {
 	constructor(private readonly orch: AppServerOrchestrator) {
 		super();
 	}
 
-	protected async report(messageId: IMessage['_id'], description: string, appId: string): Promise<void> {
+	protected async report(messageId: IMessage['_id'], description: string, userId: string, appId: string): Promise<void> {
 		this.orch.debugLog(`The App ${appId} is creating a new report.`);
 
 		if (!messageId) {
@@ -20,6 +20,8 @@ export class AppModerationBridge extends ModerationBridge {
 			throw new Error('Invalid description');
 		}
 
-		Meteor.call('reportMessage', { messageId, description });
+		await reportMessage(messageId, description, userId || 'rocket.cat');
+
+		// Meteor.call('reportMessage', { messageId, description });
 	}
 }
