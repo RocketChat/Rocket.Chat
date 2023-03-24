1d58d2e07b86d5ed6852b68d941e8bf9565634f4
Kevin Aleman <kaleman960@gmail.com>
2023-03-23 19:19:33 -0600

    ts

--- packages/model-typings/src/models/ILivechatRoomsModel.ts
+++ packages/model-typings/src/models/ILivechatRoomsModel.ts
@@ -152,7 +152,7 @@ export interface ILivechatRoomsModel extends IBaseModel<IOmnichannelRoom> {
 		emailThread: string,
 		options: FindOptions<IOmnichannelRoom>,
 	): Promise<IOmnichannelRoom | null>;
-	updateEmailThreadByRoomId(roomId: string, threadIds: string): Promise<UpdateResult>;
+	updateEmailThreadByRoomId(roomId: string, threadIds: string[]): Promise<UpdateResult>;
 	findOneLastServedAndClosedByVisitorToken(visitorToken: string, options?: FindOptions<IOmnichannelRoom>): Promise<IOmnichannelRoom | null>;
 	findOneByVisitorToken(visitorToken: string, fields?: FindOptions<IOmnichannelRoom>['projection']): Promise<IOmnichannelRoom | null>;
 	updateRoomCount(): Promise<ISetting | null>;
