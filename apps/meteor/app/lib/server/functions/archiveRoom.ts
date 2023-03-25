68752f3d203a6ade374d0f3cf56f6dfcab71a521
Kevin Aleman <kaleman960@gmail.com>
2023-03-25 10:59:13 -0600

    refactor: Ditch `Meteor.user` in favor of `Meteor.userAsync` - 1x (#28601)
    Co-authored-by: Rodrigo Nascimento <234261+rodrigok@users.noreply.github.com>

--- apps/meteor/app/lib/server/functions/archiveRoom.ts
+++ apps/meteor/app/lib/server/functions/archiveRoom.ts
@@ -3,10 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { Rooms, Messages, Subscriptions } from '../../../models/server';
 import { callbacks } from '../../../../lib/callbacks';
 
-export const archiveRoom = function (rid: string): void {
+export const archiveRoom = async function (rid: string): Promise<void> {
 	Rooms.archiveById(rid);
 	Subscriptions.archiveByRoomId(rid);
-	Messages.createRoomArchivedByRoomIdAndUser(rid, Meteor.user());
+	Messages.createRoomArchivedByRoomIdAndUser(rid, await Meteor.userAsync());
 
-	callbacks.run('afterRoomArchived', Rooms.findOneById(rid), Meteor.user());
+	callbacks.run('afterRoomArchived', Rooms.findOneById(rid), await Meteor.userAsync());
 };
