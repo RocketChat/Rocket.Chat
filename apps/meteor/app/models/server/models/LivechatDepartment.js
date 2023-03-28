9446b3cebddb81b087e930369ab7bce875fcc64a
Kevin Aleman <kaleman960@gmail.com>
2023-03-28 10:15:35 -0600

    findEnabledWithAgentsAndBusinessUnit

--- apps/meteor/app/models/server/models/LivechatDepartment.js
+++ apps/meteor/app/models/server/models/LivechatDepartment.js
@@ -27,14 +27,6 @@ export class LivechatDepartment extends Base {
 		return this.find(query, fields && { fields });
 	}
 
-	findEnabledWithAgentsAndBusinessUnit(_, fields) {
-		const query = {
-			numAgents: { $gt: 0 },
-			enabled: true,
-		};
-		return this.find(query, fields && { fields });
-	}
-
 	findOneByIdOrName(_idOrName, options) {
 		const query = {
 			$or: [
