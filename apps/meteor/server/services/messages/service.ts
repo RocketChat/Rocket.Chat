b6b07284c9b0082c656826aeda354eaa397261c9
Diego Sampaio <chinello@gmail.com>
2023-03-29 16:00:44 -0300

    create service method to save system messages

--- apps/meteor/server/services/messages/service.ts
+++ apps/meteor/server/services/messages/service.ts
@@ -1,13 +1,34 @@
-import type { IMessage } from '@rocket.chat/core-typings';
+import type { IMessage, MessageTypesValues } from '@rocket.chat/core-typings';
 import type { IMessageService } from '@rocket.chat/core-services';
 import { ServiceClassInternal } from '@rocket.chat/core-services';
+import { Messages } from '@rocket.chat/models';
 
 import { executeSendMessage } from '../../../app/lib/server/methods/sendMessage';
+import { settings } from '../../../app/settings/server';
 
 export class MessageService extends ServiceClassInternal implements IMessageService {
 	protected name = 'message';
 
 	async sendMessage({ fromId, rid, msg }: { fromId: string; rid: string; msg: string }): Promise<IMessage> {
 		return executeSendMessage(fromId, { rid, msg });
+	}
+
+	async saveSystemMessage<T = IMessage>(
+		type: MessageTypesValues,
+		rid: string,
+		message: string,
+		user: Pick<IMessage['u'], '_id' | 'username'>,
+		extraData?: Partial<T>,
+	): Promise<IMessage['_id']> {
+		const result = await Messages.createWithTypeRoomIdMessageUserAndUnread(
+			type,
+			rid,
+			message,
+			user,
+			settings.get('Message_Read_Receipt_Enabled'),
+			extraData,
+		);
+
+		return result.insertedId;
 	}
 }
