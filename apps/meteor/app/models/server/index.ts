8e6cb6d10b6fd7c038dc5f823fc6f00f07d269f8
Kevin Aleman <kaleman960@gmail.com>
2023-03-30 14:21:52 -0600

    remove livechat departments model

--- apps/meteor/app/models/server/index.ts
+++ apps/meteor/app/models/server/index.ts
@@ -3,6 +3,5 @@ import { BaseDb } from './models/_BaseDb';
 import Messages from './models/Messages';
 import Rooms from './models/Rooms';
 import Users from './models/Users';
-import LivechatDepartment from './models/LivechatDepartment';
 
-export { Base, BaseDb, Messages, Rooms, Users, LivechatDepartment };
+export { Base, BaseDb, Messages, Rooms, Users };
