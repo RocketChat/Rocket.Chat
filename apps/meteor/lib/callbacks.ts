b977e5339ddc60fcdc5e539a96f1aafe3edfd5c1
Kevin Aleman <kaleman960@gmail.com>
2023-03-30 10:17:36 -0600

    offlinemessage

--- apps/meteor/lib/callbacks.ts
+++ apps/meteor/lib/callbacks.ts
@@ -195,6 +195,7 @@ type ChainedCallbackSignatures = {
 		content: OEmbedUrlContent;
 	};
 	'livechat.beforeListTags': () => ILivechatTag[];
+	'livechat.offlineMessage': (data: { name: string; email: string; message: string }) => void;
 };
 
 type Hook =
