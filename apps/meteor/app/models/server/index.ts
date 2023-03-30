7da0d229a9148bf5ac73ead2b7730e754b08f439
Diego Sampaio <chinello@gmail.com>
2023-03-29 23:13:09 -0300

    remove Meteor model

--- apps/meteor/app/models/server/index.ts
+++ apps/meteor/app/models/server/index.ts
@@ -1,6 +1,5 @@
 import { Base } from './models/_Base';
 import { BaseDb } from './models/_BaseDb';
-import Messages from './models/Messages';
 import Rooms from './models/Rooms';
 import Subscriptions from './models/Subscriptions';
 import Users from './models/Users';
