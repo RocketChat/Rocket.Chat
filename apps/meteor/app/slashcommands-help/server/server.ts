d4b1be2f8a18002cd4cf4643bc2a3979c92d2199
chinmayshewale <shewalechinmay54@gmail.com>
2023-04-03 22:24:55 +0530

    Single message

--- apps/meteor/app/slashcommands-help/server/server.ts
+++ apps/meteor/app/slashcommands-help/server/server.ts
@@ -56,15 +56,17 @@ slashCommands.add({
 				command: 'Shift + Enter',
 			},
 		];
+		let msg = "";
 		keys.forEach((key) => {
-			void api.broadcast('notify.ephemeralMessage', userId, item.rid, {
-				msg: TAPi18n.__(key.key, {
-					postProcess: 'sprintf',
-					sprintf: [key.command],
-					lng: user?.language || settings.get('Language') || 'en',
-				}),
-			});
+			msg = msg + `\n` + TAPi18n.__(key.key, {
+				postProcess: 'sprintf',
+				sprintf: [key.command],
+				lng: user?.language || settings.get('language') || 'en',
+			})
 		});
+		api.broadcast('notify.ephemeralMessage', userId, item.rid, {
+			msg: msg
+		})
 	},
 	options: {
 		description: 'Show_the_keyboard_shortcut_list',
