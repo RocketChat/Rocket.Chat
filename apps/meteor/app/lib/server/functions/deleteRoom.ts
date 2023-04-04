fbaf614c8149b6e28ac76f1b76491526e0448bf9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-04-04 15:56:55 -0300

    refactor: deleteRoom to async (#28786)

--- apps/meteor/app/lib/server/functions/deleteRoom.ts
+++ apps/meteor/app/lib/server/functions/deleteRoom.ts
@@ -3,12 +3,12 @@ import { Messages, Rooms, Subscriptions } from '@rocket.chat/models';
 import { callbacks } from '../../../../lib/callbacks';
 import { FileUpload } from '../../../file-upload/server';
 
-export const deleteRoom = function (rid: string): void {
-	Promise.await(FileUpload.removeFilesByRoomId(rid));
-	Promise.await(Messages.removeByRoomId(rid));
-	callbacks.run('beforeDeleteRoom', rid);
-	Promise.await(Subscriptions.removeByRoomId(rid));
-	FileUpload.getStore('Avatars').deleteByRoomId(rid);
-	callbacks.run('afterDeleteRoom', rid);
-	Promise.await(Rooms.removeById(rid));
+export const deleteRoom = async function (rid: string): Promise<void> {
+	await FileUpload.removeFilesByRoomId(rid);
+	await Messages.removeByRoomId(rid);
+	await callbacks.run('beforeDeleteRoom', rid);
+	await Subscriptions.removeByRoomId(rid);
+	await FileUpload.getStore('Avatars').deleteByRoomId(rid);
+	await callbacks.run('afterDeleteRoom', rid);
+	await Rooms.removeById(rid);
 };
