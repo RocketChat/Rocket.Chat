8531aadb193eaae2a1a38171e3e2be3a4909f8ec
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2023-04-04 22:23:35 -0300

    refactor: Remove fibers from Users Model (#28797)

--- apps/meteor/app/models/index.ts
+++ apps/meteor/app/models/index.ts
@@ -3,6 +3,3 @@ import { Meteor } from 'meteor/meteor';
 if (Meteor.isClient) {
 	module.exports = require('./client/index');
 }
-if (Meteor.isServer) {
-	module.exports = require('./server/index');
-}
