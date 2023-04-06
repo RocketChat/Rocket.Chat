a8d192e55980beb3152ca2a782f52ea034ccccf5
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-04-05 23:17:58 -0300

    Overhaul room opening

--- apps/meteor/client/views/room/hooks/useOpenRoom.ts
+++ apps/meteor/client/views/room/hooks/useOpenRoom.ts
@@ -1,11 +1,13 @@
-import type { RoomType, IRoom } from '@rocket.chat/core-typings';
-import { useMethod, useRoute, useSetting, useToastMessageDispatch, useUser } from '@rocket.chat/ui-contexts';
+import type { IRoom, RoomType } from '@rocket.chat/core-typings';
+import { useMethod, useRoute, useSetting, useUser } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 
 import { ChatRoom, ChatSubscription } from '../../../../app/models/client';
-import { LegacyRoomManager, readMessage } from '../../../../app/ui-utils/client';
+import { LegacyRoomManager } from '../../../../app/ui-utils/client';
 import { omit } from '../../../../lib/utils/omit';
 import { RoomManager } from '../../../lib/RoomManager';
+import { NotAuthorizedError } from '../../../lib/errors/NotAuthorizedError';
+import { RoomNotFoundError } from '../../../lib/errors/RoomNotFoundError';
 import { roomCoordinator } from '../../../lib/rooms/roomCoordinator';
 import { fireGlobalEvent } from '../../../lib/utils/fireGlobalEvent';
 import { waitUntilFind } from '../../../lib/utils/waitUntilFind';
@@ -17,80 +19,59 @@ export function useOpenRoom({ type, reference }: { type: RoomType; reference: st
 	const createDirectMessage = useMethod('createDirectMessage');
 	const openRoom = useMethod('openRoom');
 	const directRoute = useRoute('direct');
-	const dispatchToastMessage = useToastMessageDispatch();
 
 	return useQuery(
 		['rooms', { type, reference }] as const,
-		() =>
-			new Promise<{ type: RoomType; id: string } | { rid: IRoom['_id'] }>((resolve, reject) => {
-				queueMicrotask(async () => {
-					if ((user && !user.username) || (!user && !allowAnonymousRead)) {
-						reject(new Error('Not authorized'));
-						return;
-					}
+		async (): Promise<{ rid: IRoom['_id'] }> => {
+			if ((user && !user.username) || (!user && !allowAnonymousRead)) {
+				throw new NotAuthorizedError();
+			}
 
-					try {
-						const room = roomCoordinator.getRoomDirectives(type).findRoom(reference) || (await getRoomByTypeAndName(type, reference));
-						if (!room._id) {
-							reject(new Error('Room not found'));
-							return;
-						}
+			{
+				const room = roomCoordinator.getRoomDirectives(type).findRoom(reference) || (await getRoomByTypeAndName(type, reference));
 
-						ChatRoom.upsert({ _id: room._id }, { $set: room });
-
-						if (room._id !== reference && type === 'd') {
-							// Redirect old url using username to rid
-							await LegacyRoomManager.close(type + reference);
-							resolve({ type: 'd', id: room._id });
-							return;
-						}
-
-						LegacyRoomManager.open({ typeName: type + reference, rid: room._id });
-
-						if (room._id === RoomManager.opened) {
-							resolve({ rid: room._id });
-							return;
-						}
-
-						fireGlobalEvent('room-opened', omit(room, 'usernames'));
-
-						// update user's room subscription
-						const sub = ChatSubscription.findOne({ rid: room._id });
-						if (sub && sub.open === false) {
-							try {
-								await openRoom(room._id);
-							} catch (error) {
-								dispatchToastMessage({ type: 'error', message: error });
-								reject(error);
-								return;
-							}
-						}
-
-						if (sub) {
-							const { rid } = sub;
-							setTimeout(() => readMessage.read(rid), 1000);
-						}
-
-						resolve({ rid: room._id });
-					} catch (error) {
-						if (type === 'd') {
-							try {
-								const { rid } = await createDirectMessage(...reference.split(', '));
-								await waitUntilFind(() => ChatSubscription.findOne({ rid }));
-								resolve({ type: 'd', id: rid });
-								return;
-							} catch (error) {
-								reject(error);
-							}
-						}
-					}
-				});
-			}),
-		{
-			onSuccess: (data) => {
-				if ('type' in data && 'id' in data && data.type === 'd') {
-					directRoute.push({ rid: data.id }, (prev) => prev);
+				if (!room._id) {
+					throw new RoomNotFoundError(undefined, { type, reference });
 				}
+
+				ChatRoom.upsert({ _id: room._id }, { $set: room });
+
+				if (room._id !== reference && type === 'd') {
+					// Redirect old url using username to rid
+					await LegacyRoomManager.close(type + reference);
+					throw new RoomNotFoundError(undefined, { rid: room._id });
+				}
+
+				LegacyRoomManager.open({ typeName: type + reference, rid: room._id });
+
+				if (room._id === RoomManager.opened) {
+					return { rid: room._id };
+				}
+
+				fireGlobalEvent('room-opened', omit(room, 'usernames'));
+
+				// update user's room subscription
+				const sub = ChatSubscription.findOne({ rid: room._id });
+				if (sub && sub.open === false) {
+					await openRoom(room._id);
+				}
+				return { rid: room._id };
+			}
+		},
+		{
+			onError: async (error) => {
+				if (type !== 'd') {
+					return;
+				}
+
+				if (error instanceof RoomNotFoundError && error.details !== undefined && 'rid' in error.details) {
+					directRoute.push({ rid: error.details.rid }, (prev) => prev);
+					return;
+				}
+
+				const { rid } = await createDirectMessage(...reference.split(', '));
+				await waitUntilFind(() => ChatSubscription.findOne({ rid }));
+				directRoute.push({ rid }, (prev) => prev);
 			},
 		},
 	);
