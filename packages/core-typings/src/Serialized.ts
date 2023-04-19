86f7f5362f10c247d86dcc157ec4e9e06c152083
Douglas Gubert <douglas.gubert@gmail.com>
2023-04-19 13:39:29 -0300

    Improve Serialized type definition

--- packages/core-typings/src/Serialized.ts
+++ packages/core-typings/src/Serialized.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-types -- We need the generic Function type here */
+
 export type Serialized<T> = T extends Date
 	? Exclude<T, Date> | string
-	: T extends boolean | number | string | null | undefined
+	: T extends Function
+	? never
+	: T extends boolean | number | string | null | undefined | Buffer
 	? T
 	: T extends {}
 	? {
 			[K in keyof T]: Serialized<T[K]>;
 	  }
-	: null;
+	: never;
