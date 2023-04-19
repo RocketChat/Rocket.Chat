d2f51b1ba452a726ded97d9f62abcd19df54d141
dougfabris <devfabris@gmail.com>
2023-04-19 17:26:59 -0300

    fix: review

--- packages/ui-client/src/components/Header/HeaderTag.tsx
+++ packages/ui-client/src/components/Header/HeaderTag.tsx
@@ -2,7 +2,7 @@ import { Box, Tag } from '@rocket.chat/fuselage';
 import type { ComponentProps, FC } from 'react';
 
 const HeaderTag: FC<ComponentProps<typeof Tag>> = ({ children, ...props }) => (
-	<Box mi='x2'>
+	<Box mi='x4' withTruncatedText minWidth='x64'>
 		<Tag medium {...props}>
 			{children}
 		</Tag>
