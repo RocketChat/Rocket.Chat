34392a76fa327885aa882b0b1ea7125a539a2cb7
Rafael Tapia <rafael.tapia@rocket.chat>
2023-04-20 19:09:03 -0300

    fix: export communication startup

--- apps/meteor/ee/server/apps/communication/index.ts
+++ apps/meteor/ee/server/apps/communication/index.ts
@@ -2,6 +2,6 @@ import { AppsRestApi } from './rest';
 import { AppUIKitInteractionApi } from './uikit';
 import { AppServerListener, AppServerNotifier } from './websockets';
 import { AppEvents } from './events';
-import './startup';
+import startup from './startup';
 
-export { AppUIKitInteractionApi, AppsRestApi, AppEvents, AppServerNotifier, AppServerListener };
+export { AppUIKitInteractionApi, AppsRestApi, AppEvents, AppServerNotifier, AppServerListener, startup };
