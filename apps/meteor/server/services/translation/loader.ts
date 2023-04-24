405610e4f4058b1c49e565ee68eccd2a991cb790
Marcos Defendi <marcos.defendi@rocket.chat>
2023-04-24 19:08:42 -0300

    refactor: move i18n files to private and fix symlinks

--- apps/meteor/server/services/translation/loader.ts
+++ apps/meteor/server/services/translation/loader.ts
@@ -2,11 +2,11 @@ import fs from 'fs';
 
 import * as locales from 'locale-codes';
 
-export const getPathFromTranslationFile = (language: string): string => Assets.absoluteFilePath(`i18n/${language}.i18n.json`);
+export const getPathFromTranslationFile = (language: string): string => Assets.absoluteFilePath(`locales/${language}.i18n.json`);
 export const getSupportedLanguages = async (): Promise<string[]> =>
 	new Promise((resolve, reject) => {
 		try {
-			fs.readdir(`${Assets.getServerDir()}/assets/app/i18n`, (err, files) => {
+			fs.readdir(`${Assets.getServerDir()}/assets/app/locales`, (err, files) => {
 				if (err) {
 					return reject(err);
 				}
