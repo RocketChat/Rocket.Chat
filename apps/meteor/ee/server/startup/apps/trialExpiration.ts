9192c419a5555e2779ad34995d809ddb16ff13d4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-04-24 21:06:32 -0300

    Add more scaffolding

--- apps/meteor/ee/server/startup/apps/trialExpiration.ts
+++ apps/meteor/ee/server/startup/apps/trialExpiration.ts
@@ -1,11 +1,48 @@
 import { SyncedCron } from 'meteor/littledata:synced-cron';
+import type { ILicense } from '@rocket.chat/core-typings';
 
-async function checkIfTrialLicenseWasExpired() {
-	// TODO
+import { flatModules, getLicenses } from '../../../app/license/server/license';
+
+function isTrialLicenseJustExpired(_license: ILicense) {
+	throw new Error('Not implemented');
+}
+
+function markNewExpiredTrialLicenses() {
+	throw new Error('Not implemented');
+}
+
+function disableAllApps() {
+	throw new Error('Not implemented');
+}
+
+async function checkIfAnyTrialLicenseJustExpired() {
+	const licenses = getLicenses()
+		.filter(({ valid }) => valid)
+		.map(({ license }) => ({
+			...license,
+			modules: flatModules(license.modules),
+		}));
+
+	const hasValidLicense = licenses.some((license) => license.modules.length > 0) ?? false;
+
+	if (hasValidLicense) {
+		return;
+	}
+
+	const trialLicenses = licenses.filter((license) => license.meta?.trial ?? false);
+
+	const justExpiredTrialLicenses = trialLicenses.filter(isTrialLicenseJustExpired);
+
+	if (justExpiredTrialLicenses.length === 0) {
+		return;
+	}
+
+	markNewExpiredTrialLicenses();
+	disableAllApps();
 }
 
 SyncedCron.add({
 	name: 'Apps:trialLicenseExpiration',
 	schedule: (parser) => parser.text('every 12 hours'),
-	job: checkIfTrialLicenseWasExpired,
+	job: checkIfAnyTrialLicenseJustExpired,
 });
