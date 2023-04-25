d5e318d8acdff5ffd66256f14e80fb8386e3880a
Kevin Aleman <kaleman960@gmail.com>
2023-04-25 12:23:57 -0600

    getavatarurl

--- apps/meteor/app/utils/client/index.ts
+++ apps/meteor/app/utils/client/index.ts
@@ -2,7 +2,7 @@ export { t } from '../lib/tapi18n';
 export { Info } from '../rocketchat.info';
 export { getUserPreference } from './lib/getUserPreference';
 export { fileUploadIsValidContentType } from './restrictions';
-export { getUserAvatarURL } from '../lib/getUserAvatarURL';
+export { getUserAvatarURL } from './getUserAvatarURL';
 export { slashCommands } from '../lib/slashCommand';
 export { getURL } from './getURL';
 export { APIClient } from './lib/RestApiClient';
