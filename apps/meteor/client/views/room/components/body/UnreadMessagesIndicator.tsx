cc30490d09401abe1f145c769291af80117f9d8f
juliajforesti <juliajforesti@gmail.com>
2023-04-25 11:52:03 -0300

    wip: improve unread indicator colors

--- apps/meteor/client/views/room/components/body/UnreadMessagesIndicator.tsx
+++ apps/meteor/client/views/room/components/body/UnreadMessagesIndicator.tsx
@@ -1,3 +1,5 @@
+import { css } from '@rocket.chat/css-in-js';
+import { Box, Palette } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
@@ -11,6 +13,44 @@ type UnreadMessagesIndicatorProps = {
 	onMarkAsReadButtonClick: () => void;
 };
 
+const unreadBarStyle = css`
+	color: ${Palette.text['font-pure-black']};
+	background-color: ${Palette.status['status-background-info']};
+
+	text-align: center;
+	text-transform: uppercase;
+
+	& > button.mark-read {
+		float: right;
+
+		line-height: inherit;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+
+	& .unread-count {
+		display: none;
+	}
+
+	& > button.jump-to {
+		float: left;
+
+		line-height: inherit;
+
+		& .jump-to-small {
+			display: none;
+
+			line-height: inherit;
+		}
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+`;
+
 const UnreadMessagesIndicator = ({
 	count,
 	since,
@@ -21,7 +61,8 @@ const UnreadMessagesIndicator = ({
 	const formatTimeAgo = useTimeAgo();
 
 	return (
-		<div className='unread-bar color-primary-action-color background-component-color'>
+		// <div className='unread-bar color-primary-action-color background-component-color'>
+		<Box className={unreadBarStyle}>
 			<button type='button' className='jump-to' onClick={onJumpButtonClick}>
 				<span className='jump-to-large'>{t('Jump_to_first_unread')}</span>
 				<span className='jump-to-small'>{t('Jump')}</span>
@@ -31,7 +72,8 @@ const UnreadMessagesIndicator = ({
 			<button type='button' className='mark-read' onClick={onMarkAsReadButtonClick}>
 				{t('Mark_as_read')}
 			</button>
-		</div>
+		</Box>
+		// </div>
 	);
 };
 
