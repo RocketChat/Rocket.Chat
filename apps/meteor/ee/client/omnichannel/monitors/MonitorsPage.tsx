e681d1359776c9871ff695b22a78bceb336068b7
dougfabris <devfabris@gmail.com>
2023-04-27 14:15:27 -0300

    fix: review

--- apps/meteor/ee/client/omnichannel/monitors/MonitorsPage.tsx
+++ apps/meteor/ee/client/omnichannel/monitors/MonitorsPage.tsx
@@ -1,117 +1,18 @@
-import { Button, Box, Callout, Field } from '@rocket.chat/fuselage';
-import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
-import { useToastMessageDispatch, useMethod, useTranslation, useEndpoint, useSetModal } from '@rocket.chat/ui-contexts';
-import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import { useTranslation } from '@rocket.chat/ui-contexts';
+import React from 'react';
 
-import FilterByText from '../../../../client/components/FilterByText';
-import GenericModal from '../../../../client/components/GenericModal';
-import GenericNoResults from '../../../../client/components/GenericNoResults';
-import { usePagination } from '../../../../client/components/GenericTable/hooks/usePagination';
-import { useSort } from '../../../../client/components/GenericTable/hooks/useSort';
 import Page from '../../../../client/components/Page';
-import UserAutoComplete from '../../../../client/components/UserAutoComplete';
-import { queryClient } from '../../../../client/lib/queryClient';
 import MonitorsTable from './MonitorsTable';
 
 const MonitorsPage = () => {
 	const t = useTranslation();
-	const dispatchToastMessage = useToastMessageDispatch();
-
-	const [text, setText] = useState('');
-	const pagination = usePagination();
-	const sort = useSort<'name' | 'username' | 'email'>('name');
-
-	const [username, setUsername] = useState('');
-
-	const debouncedText = useDebouncedValue(text, 500);
-
-	const getMonitors = useEndpoint('GET', '/v1/livechat/monitors');
-
-	const setModal = useSetModal();
-
-	// TODO: implement endpoints for monitors add/remove
-	const removeMonitor = useMethod('livechat:removeMonitor');
-	const addMonitor = useMethod('livechat:addMonitor');
-
-	const handleAdd = async () => {
-		try {
-			await addMonitor(username);
-			setUsername('');
-			dispatchToastMessage({ type: 'success', message: t('Monitor_added') });
-		} catch (error) {
-			dispatchToastMessage({ type: 'error', message: error });
-		}
-		queryClient.invalidateQueries(['omnichannel', 'monitors']);
-	};
-
-	const handleRemove = (username: string) => {
-		const onDeleteMonitor = async () => {
-			try {
-				await removeMonitor(username);
-				dispatchToastMessage({ type: 'success', message: t('Monitor_removed') });
-			} catch (error) {
-				dispatchToastMessage({ type: 'error', message: error });
-			}
-			queryClient.invalidateQueries(['omnichannel', 'monitors']);
-			setModal();
-		};
-
-		setModal(<GenericModal variant='danger' onConfirm={onDeleteMonitor} onCancel={() => setModal()} confirmText={t('Delete')} />);
-	};
-
-	const {
-		data,
-		isInitialLoading: isLoadingMonitors,
-		isError: isErrorMonitors,
-	} = useQuery(['omnichannel', 'monitors', debouncedText, pagination, sort], () =>
-		getMonitors({
-			text,
-			sort: JSON.stringify({ [sort.sortBy]: sort.sortDirection === 'asc' ? 1 : -1 }),
-			...(pagination.current && { offset: pagination.current }),
-			...(pagination.itemsPerPage && { count: pagination.itemsPerPage }),
-		}),
-	);
-
-	if (isErrorMonitors) {
-		return <Callout>{t('Error')}</Callout>;
-	}
 
 	return (
 		<Page flexDirection='row'>
 			<Page>
 				<Page.Header title={t('Livechat_Monitors')} />
 				<Page.Content>
-					<Box display='flex' flexDirection='column'>
-						<Field>
-							<Field.Label>{t('Username')}</Field.Label>
-							<Field.Row>
-								<UserAutoComplete value={username} onChange={setUsername as () => void} />
-								<Button primary disabled={!username} onClick={() => handleAdd()} mis='x8'>
-									{t('Add')}
-								</Button>
-							</Field.Row>
-						</Field>
-					</Box>
-
-					<FilterByText onChange={({ text }): void => setText(text)} />
-					{!data || data.total < 1 ? (
-						<GenericNoResults
-							icon='baloon-exclamation'
-							title={t('No_monitors_found')}
-							description={t('No_monitors_found-description')}
-						></GenericNoResults>
-					) : (
-						<MonitorsTable
-							aria-busy={text !== debouncedText}
-							aria-live='assertive'
-							pagination={pagination}
-							data={data}
-							loading={isLoadingMonitors}
-							sort={sort}
-							handleRemove={handleRemove}
-						/>
-					)}
+					<MonitorsTable />
 				</Page.Content>
 			</Page>
 		</Page>
--- apps/meteor/ee/client/omnichannel/monitors/MonitorsTable.tsx
+++ apps/meteor/ee/client/omnichannel/monitors/MonitorsTable.tsx
@@ -1,9 +1,23 @@
-import type { ILivechatMonitor } from '@rocket.chat/core-typings';
-import { IconButton, Pagination } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
-import type { TableHTMLAttributes } from 'react';
-import React, { useMemo } from 'react';
+import {
+	IconButton,
+	Pagination,
+	Button,
+	Field,
+	Box,
+	States,
+	StatesIcon,
+	StatesTitle,
+	StatesActions,
+	StatesAction,
+} from '@rocket.chat/fuselage';
+import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
+import { useTranslation, useToastMessageDispatch, useMethod, useEndpoint, useSetModal } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
+import React, { useMemo, useState } from 'react';
 
+import FilterByText from '../../../../client/components/FilterByText';
+import GenericModal from '../../../../client/components/GenericModal';
+import GenericNoResults from '../../../../client/components/GenericNoResults';
 import {
 	GenericTable,
 	GenericTableBody,
@@ -13,21 +27,67 @@ import {
 	GenericTableLoadingTable,
 	GenericTableRow,
 } from '../../../../client/components/GenericTable';
+import { usePagination } from '../../../../client/components/GenericTable/hooks/usePagination';
+import { useSort } from '../../../../client/components/GenericTable/hooks/useSort';
+import UserAutoComplete from '../../../../client/components/UserAutoComplete';
+import { queryClient } from '../../../../client/lib/queryClient';
 
-type MonitorsTableProps = {
-	data?: { monitors: ILivechatMonitor[]; count?: number; offset?: number; total: number };
-	handleRemove: (username: string) => void;
-	pagination: any;
-	sort: any;
-	loading: boolean;
-} & TableHTMLAttributes<HTMLTableElement>;
-
-function MonitorsTable({ data, handleRemove, pagination, sort, loading, ...props }: MonitorsTableProps) {
+const MonitorsTable = () => {
 	const t = useTranslation();
+	const setModal = useSetModal();
 
-	const { monitors } = data || {};
+	const [text, setText] = useState('');
+	const [username, setUsername] = useState('');
+	const debouncedText = useDebouncedValue(text, 500);
+
+	const dispatchToastMessage = useToastMessageDispatch();
+
+	const pagination = usePagination();
+	const sort = useSort<'name' | 'username' | 'email'>('name');
+
+	const getMonitors = useEndpoint('GET', '/v1/livechat/monitors');
+
+	// TODO: implement endpoints for monitors add/remove
+	const removeMonitor = useMethod('livechat:removeMonitor');
+	const addMonitor = useMethod('livechat:addMonitor');
+
 	const { current, itemsPerPage, setItemsPerPage: onSetItemsPerPage, setCurrent: onSetCurrent, ...paginationProps } = pagination;
 	const { sortBy, sortDirection, setSort } = sort;
+
+	const { data, refetch, isLoading, isSuccess, isError } = useQuery(['omnichannel', 'monitors', debouncedText, pagination, sort], () =>
+		getMonitors({
+			text,
+			sort: JSON.stringify({ [sort.sortBy]: sort.sortDirection === 'asc' ? 1 : -1 }),
+			...(pagination.current && { offset: pagination.current }),
+			...(pagination.itemsPerPage && { count: pagination.itemsPerPage }),
+		}),
+	);
+
+	const handleAdd = async () => {
+		try {
+			await addMonitor(username);
+			setUsername('');
+			dispatchToastMessage({ type: 'success', message: t('Monitor_added') });
+		} catch (error) {
+			dispatchToastMessage({ type: 'error', message: error });
+		}
+		queryClient.invalidateQueries(['omnichannel', 'monitors']);
+	};
+
+	const handleRemove = (username: string) => {
+		const onDeleteMonitor = async () => {
+			try {
+				await removeMonitor(username);
+				dispatchToastMessage({ type: 'success', message: t('Monitor_removed') });
+			} catch (error) {
+				dispatchToastMessage({ type: 'error', message: error });
+			}
+			queryClient.invalidateQueries(['omnichannel', 'monitors']);
+			setModal();
+		};
+
+		setModal(<GenericModal variant='danger' onConfirm={onDeleteMonitor} onCancel={() => setModal()} confirmText={t('Delete')} />);
+	};
 
 	const headers = useMemo(
 		() => [
@@ -47,36 +107,72 @@ function MonitorsTable({ data, handleRemove, pagination, sort, loading, ...props
 
 	return (
 		<>
-			<GenericTable {...props}>
-				<GenericTableHeader>{headers}</GenericTableHeader>
-				<GenericTableBody>
-					{monitors && !loading ? (
-						monitors.map((monitor: ILivechatMonitor) => (
-							<GenericTableRow key={monitor._id} tabIndex={0} width='full'>
-								<GenericTableCell withTruncatedText>{monitor.name}</GenericTableCell>
-								<GenericTableCell withTruncatedText>{monitor.username}</GenericTableCell>
-								<GenericTableCell withTruncatedText>{monitor.email}</GenericTableCell>
-								<GenericTableCell withTruncatedText>
-									<IconButton icon='trash' mini title={t('Remove')} onClick={() => handleRemove(monitor.username)} />
-								</GenericTableCell>
-							</GenericTableRow>
-						))
-					) : (
-						<GenericTableLoadingTable headerCells={6} />
-					)}
-				</GenericTableBody>
-			</GenericTable>
-			<Pagination
-				divider
-				current={current}
-				itemsPerPage={itemsPerPage}
-				count={data?.total || 0}
-				onSetItemsPerPage={onSetItemsPerPage}
-				onSetCurrent={onSetCurrent}
-				{...paginationProps}
-			/>
+			<Box display='flex' flexDirection='column'>
+				<Field>
+					<Field.Label>{t('Username')}</Field.Label>
+					<Field.Row>
+						<UserAutoComplete value={username} onChange={setUsername as () => void} />
+						<Button primary disabled={!username} onClick={() => handleAdd()} mis='x8'>
+							{t('Add')}
+						</Button>
+					</Field.Row>
+				</Field>
+			</Box>
+			<FilterByText onChange={({ text }): void => setText(text)} />
+			{isLoading && (
+				<GenericTable>
+					<GenericTableHeader>{headers}</GenericTableHeader>
+					<GenericTableBody>
+						<GenericTableLoadingTable headerCells={4} />
+					</GenericTableBody>
+				</GenericTable>
+			)}
+			{isSuccess && data.monitors.length > 0 && (
+				<>
+					<GenericTable aria-busy={text !== debouncedText} aria-live='assertive'>
+						<GenericTableHeader>{headers}</GenericTableHeader>
+						<GenericTableBody>
+							{data.monitors?.map((monitor) => (
+								<GenericTableRow key={monitor._id} tabIndex={0} width='full'>
+									<GenericTableCell withTruncatedText>{monitor.name}</GenericTableCell>
+									<GenericTableCell withTruncatedText>{monitor.username}</GenericTableCell>
+									<GenericTableCell withTruncatedText>{monitor.email}</GenericTableCell>
+									<GenericTableCell withTruncatedText>
+										<IconButton icon='trash' mini title={t('Remove')} onClick={() => handleRemove(monitor.username)} />
+									</GenericTableCell>
+								</GenericTableRow>
+							))}
+						</GenericTableBody>
+					</GenericTable>
+					<Pagination
+						divider
+						current={current}
+						itemsPerPage={itemsPerPage}
+						count={data?.total || 0}
+						onSetItemsPerPage={onSetItemsPerPage}
+						onSetCurrent={onSetCurrent}
+						{...paginationProps}
+					/>
+				</>
+			)}
+			{isSuccess && data?.total === 0 && (
+				<GenericNoResults
+					icon='baloon-exclamation'
+					title={t('No_monitors_found')}
+					description={t('No_monitors_found_description')}
+				></GenericNoResults>
+			)}
+			{isError && (
+				<States>
+					<StatesIcon name='warning' variation='danger' />
+					<StatesTitle>{t('Something_went_wrong')}</StatesTitle>
+					<StatesActions>
+						<StatesAction onClick={() => refetch()}>{t('Reload_page')}</StatesAction>
+					</StatesActions>
+				</States>
+			)}
 		</>
 	);
-}
+};
 
 export default MonitorsTable;
