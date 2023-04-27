5fcabf4fecbd96535a9abf57fb0fcca9233de11a
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-04-27 00:32:08 -0300

    on invalidate license

--- apps/meteor/ee/server/startup/apps/trialExpiration.ts
+++ apps/meteor/ee/server/startup/apps/trialExpiration.ts
@@ -1,48 +1,10 @@
-import { SyncedCron } from 'meteor/littledata:synced-cron';
-import type { ILicense } from '@rocket.chat/core-typings';
+import { Meteor } from 'meteor/meteor';
 
-import { flatModules, getLicenses } from '../../../app/license/server/license';
+import { Apps } from '../../apps';
+import { onInvalidateLicense } from '../../../app/license/server/license';
 
-function isTrialLicenseJustExpired(_license: ILicense) {
-	throw new Error('Not implemented');
-}
-
-function markNewExpiredTrialLicenses() {
-	throw new Error('Not implemented');
-}
-
-function disableAllApps() {
-	throw new Error('Not implemented');
-}
-
-async function checkIfAnyTrialLicenseJustExpired() {
-	const licenses = getLicenses()
-		.filter(({ valid }) => valid)
-		.map(({ license }) => ({
-			...license,
-			modules: flatModules(license.modules),
-		}));
-
-	const hasValidLicense = licenses.some((license) => license.modules.length > 0) ?? false;
-
-	if (hasValidLicense) {
-		return;
-	}
-
-	const trialLicenses = licenses.filter((license) => license.meta?.trial ?? false);
-
-	const justExpiredTrialLicenses = trialLicenses.filter(isTrialLicenseJustExpired);
-
-	if (justExpiredTrialLicenses.length === 0) {
-		return;
-	}
-
-	markNewExpiredTrialLicenses();
-	disableAllApps();
-}
-
-SyncedCron.add({
-	name: 'Apps:trialLicenseExpiration',
-	schedule: (parser) => parser.text('every 12 hours'),
-	job: checkIfAnyTrialLicenseJustExpired,
+Meteor.startup(async () => {
+	onInvalidateLicense(() => {
+		Apps.disableApps();
+	});
 });
