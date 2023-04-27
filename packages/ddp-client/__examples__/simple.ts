0c14297c6e085ce7e96e9e834a43cddd06a04b49
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-04-26 21:56:57 -0300

    improve sdk stream types

--- packages/ddp-client/__examples__/simple.ts
+++ packages/ddp-client/__examples__/simple.ts
@@ -1,9 +1,10 @@
 import WebSocket from 'ws';
 import type { ServerMethods, ServerMethodReturn } from '@rocket.chat/ui-contexts';
-import type { ServerStreamerNames, StreamerEvents } from '@rocket.chat/ui-contexts/src/ServerContext/streams';
+// import type { ServerStreamerNames, StreamerEvents } from '@rocket.chat/ui-contexts/src/ServerContext/streams';
 
 import { DDPSDK } from '../src/DDPSDK';
 import type { DDPDispatchOptions } from '../src/types/DDPClient';
+import { StreamNames, StreamKeys, StreamerCallbackArgs } from '@rocket.chat/ui-contexts/src/ServerContext/streams';
 
 declare module '../src/ClientStream' {
 	interface ClientStream {
@@ -21,10 +22,10 @@ declare module '../src/ClientStream' {
 
 declare module '../src/DDPSDK' {
 	interface DDPSDK {
-		stream<StreamName extends ServerStreamerNames>(
-			streamName: StreamName,
-			args: Parameters<StreamerEvents[StreamName]>[0],
-			callback: Parameters<StreamerEvents[StreamName]>[1],
+		stream<N extends StreamNames, K extends StreamKeys<N>>(
+			streamName: N,
+			key: K,
+			callback: (...args: StreamerCallbackArgs<N, K>) => void,
 		): () => void;
 	}
 }
@@ -37,7 +38,16 @@ const run = async (url: string, token: string) => {
 	try {
 		await sdk.account.loginWithToken(token);
 
-		await sdk.stream('room-messages', 'GENERAL', (args) => console.log('room-messages -> GENERAL', JSON.stringify(args, undefined, 2)));
+		await sdk.stream('notify-room', 'GENERAL/user-activity', (args) =>
+			console.log('notify-user -> GENERAL/user-activity', JSON.stringify(args, undefined, 2)),
+		);
+		await sdk.stream('notify-user', 'GENERAL/rooms-changed', (args) =>
+			console.log('notify-user -> GENERAL/rooms-changed', JSON.stringify(args, undefined, 2)),
+		);
+
+		await sdk.stream('room-messages', 'GENERAL', (message) =>
+			console.log('room-messages -> GENERAL', JSON.stringify(message, undefined, 2)),
+		);
 		await sdk.stream('roles', 'roles', (args) => console.log('roles -> roles', JSON.stringify(args, undefined, 2)));
 
 		console.log('ROOMS', await sdk.client.callAsync('subscriptions/get'));
