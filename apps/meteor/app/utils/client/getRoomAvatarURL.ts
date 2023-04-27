9540bd5cfe901695bbde4b80ae108f1b9d94a608
gabriellsh <gabriel.henriques@rocket.chat>
2023-04-27 11:12:19 -0300

    fix

--- apps/meteor/app/utils/client/getRoomAvatarURL.ts
+++ apps/meteor/app/utils/client/getRoomAvatarURL.ts
@@ -1,11 +1,7 @@
 import type { IRoom } from '@rocket.chat/core-typings';
 
 import { getAvatarURL } from './getAvatarURL';
-/* This import throws a lint error because the export is behind a conditional that is treated by meteor as a runtime check.
- We can't specify client/server folder here because this function can be used by both */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { settings } from '../../settings';
+import { settings } from '../../settings/client';
 
 export const getRoomAvatarURL = ({ roomId, cache = '' }: { roomId: IRoom['_id']; cache: IRoom['avatarETag'] }) => {
 	const externalSource = (settings.get('Accounts_RoomAvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
