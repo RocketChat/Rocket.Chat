43482b612ad72290f34e7db302cfac6af9e736e4
Douglas Fabris <devfabris@gmail.com>
2023-04-28 13:59:44 -0300

    fix: Custom Sounds not being added to the DOM correctly (#29089)

--- apps/meteor/client/providers/CustomSoundProvider.tsx
+++ apps/meteor/client/providers/CustomSoundProvider.tsx
@@ -4,6 +4,15 @@ import React from 'react';
 
 import { CustomSounds } from '../../app/custom-sounds/client/lib/CustomSounds';
 
-const CustomSoundProvider: FC = ({ children }) => <CustomSoundContext.Provider children={children} value={CustomSounds} />;
+const CustomSoundProvider: FC = ({ children }) => {
+	const contextValue = {
+		play: CustomSounds.play,
+		pause: CustomSounds.pause,
+		getList: CustomSounds.getList,
+		isPlaying: CustomSounds.isPlaying,
+	};
+
+	return <CustomSoundContext.Provider children={children} value={contextValue} />;
+};
 
 export default CustomSoundProvider;
