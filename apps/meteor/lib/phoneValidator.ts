1f48eb6950213e0b4220b60ebba7ae3eb4ce7e2b
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-05-01 20:50:30 +0530

    Update apps/meteor/lib/phoneValidator.ts
    Co-authored-by: Debdut Chakraborty <debdutdeb@outlook.com>

--- apps/meteor/lib/phoneValidator.ts
+++ apps/meteor/lib/phoneValidator.ts
@@ -1,5 +1,5 @@
 export const validatePhone = (phone: string): boolean => {
-    const rfcPhoneRegex =/^[+]?[\s./0-9]*[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/g;
+    const regex = /^[+]?[\s./0-9]*[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/g;
        
           return rfcPhoneRegex.test(phone);
 };
