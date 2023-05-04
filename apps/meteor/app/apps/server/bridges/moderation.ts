dee7a60da144ff77d12eadc1d5b3bcc6a34214a0
デヴぁんす <sdevanshu90@yahoo.com>
2023-05-05 02:59:41 +0530

    migrate the bridges to user, and messages

--- apps/meteor/app/apps/server/bridges/moderation.ts
+++ apps/meteor/app/apps/server/bridges/moderation.ts
@@ -1,13 +1,10 @@
 import { ModerationBridge } from '@rocket.chat/apps-engine/server/bridges/ModerationBridge';
-import { Meteor } from 'meteor/meteor';
 import { ModerationReports } from '@rocket.chat/models';
 import type { IMessage } from '@rocket.chat/apps-engine/definition/messages';
 import type { IUser } from '@rocket.chat/apps-engine/definition/users';
 
 import type { AppServerOrchestrator } from '../../../../ee/server/apps/orchestrator';
 import { reportMessage } from '../../../../server/lib/moderation/reportMessage';
-import { deleteMessage } from '../../../lib/server';
-import { setUserActiveStatus } from '../../../lib/server/functions/setUserActiveStatus';
 
 export class AppModerationBridge extends ModerationBridge {
 	constructor(private readonly orch: AppServerOrchestrator) {
@@ -28,47 +25,22 @@ export class AppModerationBridge extends ModerationBridge {
 		await reportMessage(messageId, description, userId || 'rocket.cat');
 	}
 
-	protected async deleteMessage(message: IMessage, user: IUser, reason: string, action: string, appId: string): Promise<void> {
-		this.orch.debugLog(`The App ${appId} is deleting a message.`);
+	protected async dismissReportsByMessageId(messageId: IMessage['id'], reason: string, action: string, appId: string): Promise<void> {
+		this.orch.debugLog(`The App ${appId} is dismissing reports by message id.`);
 
-		if (!message.id) {
+		if (!messageId) {
 			throw new Error('Invalid message id');
 		}
 
-		const convertedMsg = await this.orch.getConverters()?.get('messages').convertAppMessage(message);
-		const convertedUser = await this.orch.getConverters()?.get('users').convertById(user.id);
-
-		await deleteMessage(convertedMsg, convertedUser);
-
-		await ModerationReports.hideReportsByMessageId(convertedMsg._id, appId, reason, action);
+		await ModerationReports.hideReportsByMessageId(messageId, appId, reason, action);
 	}
 
-	protected async deactivateUser(
-		userId: IUser['id'],
-		confirmRelinquish: boolean,
-		reason: string,
-		action: string,
-		appId: string,
-	): Promise<void> {
-		this.orch.debugLog(`The App ${appId} is deactivating a user.`);
+	protected async dismissReportsByUserId(userId: IUser['id'], reason: string, action: string, appId: string): Promise<void> {
+		this.orch.debugLog(`The App ${appId} is dismissing reports by user id.`);
 
 		if (!userId) {
 			throw new Error('Invalid user id');
 		}
-		const convertedUser = await this.orch.getConverters()?.get('users').convertById(userId);
-
-		await setUserActiveStatus(convertedUser.id, false, confirmRelinquish);
-
 		await ModerationReports.hideReportsByUserId(userId, appId, reason, action);
-	}
-
-	protected async resetUserAvatar(userId: IUser['id'], appId: string): Promise<void> {
-		this.orch.debugLog(`The App ${appId} is resetting a user avatar.`);
-
-		if (!userId) {
-			throw new Error('Invalid user id');
-		}
-
-		await Meteor.callAsync('resetAvatar', userId);
 	}
 }
