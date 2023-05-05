9720acff93f2fdce1fbb362e78625e989208c117
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2023-05-05 17:00:42 -0300

    Implement deasync for apps engine

--- apps/meteor/server/deasync/deasync.ts
+++ apps/meteor/server/deasync/deasync.ts
@@ -1,15 +1,3 @@
-import deasync from 'deasync';
-
-const deasyncPromise = function (promise: Promise<any>) {
-	return deasync((cb) => {
-		promise
-			.then((r) => {
-				cb(undefined, r);
-			})
-			.catch((e) => {
-				cb(e, undefined);
-			});
-	})();
-};
+import { awaitSync as deasyncPromise, deasync } from '@kaciras/deasync';
 
 export { deasyncPromise, deasync };
