6b734f658a1682dce565000c0a5771b661141372
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-05-09 16:29:50 -0300

    create filters (work in progress)

--- apps/meteor/client/views/admin/rooms/FilterByTypeAndText.tsx
+++ apps/meteor/client/views/admin/rooms/FilterByTypeAndText.tsx
@@ -1,12 +1,18 @@
-import { Box, Icon, TextInput, Field, CheckBox, Margins } from '@rocket.chat/fuselage';
+import type { IRoom } from '@rocket.chat/core-typings';
+import type { SelectOption } from '@rocket.chat/fuselage';
+import { Box, Icon, TextInput, Field, CheckBox, Margins, MultiSelectFiltered } from '@rocket.chat/fuselage';
 import { useUniqueId } from '@rocket.chat/fuselage-hooks';
-import { useTranslation } from '@rocket.chat/ui-contexts';
-import type { ReactElement, Dispatch, SetStateAction } from 'react';
+import { useRouteParameter, useTranslation } from '@rocket.chat/ui-contexts';
+import type { ReactElement, Dispatch, SetStateAction, ContextType } from 'react';
 import React, { useCallback, useState, useEffect } from 'react';
+
+import { AsyncState } from '../../../lib/asyncState';
 
 const DEFAULT_TYPES = ['d', 'p', 'c', 'teams'];
 
 const FilterByTypeAndText = ({ setFilter, ...props }: { setFilter?: Dispatch<SetStateAction<any>> }): ReactElement => {
+	const t = useTranslation();
+
 	const [text, setText] = useState('');
 	const [types, setTypes] = useState({
 		d: false,
@@ -18,34 +24,26 @@ const FilterByTypeAndText = ({ setFilter, ...props }: { setFilter?: Dispatch<Set
 	});
 
 	// TODO: add all missing translations!
-	const [roomTypeFilterStructure, setRoomTypeFilterStructure] = useState({
-		label: t('Filter_By_Room'),
-		items: [
-			{ id: 'all', label: t('All_Rooms'), checked: true },
-			{ id: 'channels', label: t('Channels'), checked: false },
-			{ id: 'directMessage', label: t('Direct_Message'), checked: false },
-			{ id: 'discussions', label: t('Discussions'), checked: false },
-			{ id: 'omnichannel', label: t('Omnichannel'), checked: false },
-			{ id: 'teams', label: t('Teams'), checked: false },
-		],
-	});
+	const [roomTypeFilterStructure, setRoomTypeFilterStructure] = useState<SelectOption[]>([
+		['all', t('All_Rooms'), true],
+		['channels', t('Channels'), false],
+		['directMessage', t('Direct_Message'), false],
+		['discussions', t('Discussions'), false],
+		['omnichannel', t('Omnichannel'), false],
+		['teams', t('Teams'), false],
+	]);
 	// Is this necessary?
 	//	const roomTypeFilterOnSelected = useRadioToggle(setroomTypeFilterStructure);
 
 	// TODO: add all missing translations!
-	const [visibilityFilterStructure, setVisibilityFilterStructure] = useState({
-		label: t('Filter_By_Visibility'),
-		items: [
-			{ id: 'all', label: t('All_Visibilities'), checked: true },
-			{ id: 'private', label: t('Private'), checked: false },
-			{ id: 'public', label: t('Public'), checked: false },
-		],
-	});
-
-	const t = useTranslation();
+	const [visibilityFilterStructure, setVisibilityFilterStructure] = useState<SelectOption[]>([
+		['all', t('All_Visibilities'), true],
+		['private', t('Private'), false],
+		['public', t('Public'), false],
+	]);
 
 	const handleChange = useCallback((event) => setText(event.currentTarget.value), []);
-	const handleCheckBox = useCallback((type: keyof typeof types) => setTypes({ ...types, [type]: !types[type] }), [types]);
+	// const handleCheckBox = useCallback((type: keyof typeof types) => setTypes({ ...types, [type]: !types[type] }), [types]);
 
 	useEffect(() => {
 		if (Object.values(types).filter(Boolean).length === 0) {
@@ -57,51 +55,76 @@ const FilterByTypeAndText = ({ setFilter, ...props }: { setFilter?: Dispatch<Set
 		setFilter?.({ text, types: _types });
 	}, [setFilter, text, types]);
 
-	const idDirect = useUniqueId();
-	const idDPublic = useUniqueId();
-	const idPrivate = useUniqueId();
-	const idOmnichannel = useUniqueId();
-	const idDiscussions = useUniqueId();
-	const idTeam = useUniqueId();
+	// TODO: improve the code and separate functions into smaller files!
+	const handleOnChangeAllRooms = useCallback(() => {}, []);
+
+	/* 
+		This function should receive all the available rooms in an array roomsData: IRoom[], and returns an array with the filtered results.
+		For each category selected, this function should update the list by adding the rooms that correspond to that prop
+	*/
+	const useFilteredRooms = ({
+		roomsData,
+		typeFilter,
+		visibilityFilter,
+	}: {
+		roomsData: IRoom[];
+		typeFilter: string;
+		visibilityFilter: string;
+	}): IRoom[] | undefined => {
+		// fazer outro filtro pra visibilidade e concatenar a intersecção dos dois arrays num resultado final
+		// otimizar com useMemo ou useCallback
+
+		let filteredRoomsByType: IRoom[] = [];
+		let filteredRoomsByVisibility: IRoom[] = [];
+
+		const typeFilteredResults: Record<string, () => IRoom[]> = {
+			private: () => roomsData.filter(filterRoomsByPrivate),
+			public: () => roomsData.filter(filterRoomsByPublic),
+		};
+
+		if (typeFilter && typeFilter !== 'all') {
+			filteredRoomsByType = typeFilteredResults[typeFilter]();
+		}
+
+		const visibilityFilteredResults: Record<string, () => IRoom[]> = {
+			channels: () => roomsData.filter(filterRoomsByChannels),
+			directMessage: () => roomsData.filter(filterRoomsByDirectMessages),
+			discussions: () => roomsData.filter(filterRoomsByDiscussions),
+			omnichannel: () => roomsData.filter(filterRoomsByOmnichannel),
+			teams: () => roomsData.filter(filterRoomsByTeams),
+		};
+
+		if (visibilityFilter && visibilityFilter !== 'all') {
+			filteredRoomsByVisibility = visibilityFilteredResults[visibilityFilter]();
+		}
+
+		return filteredRoomsByType.concat(filteredRoomsByVisibility);
+	};
+
+	// TODO: check if this is the correct prop for the rooms type!!!
+	const filterRoomsByPrivate = ({ t }: Partial<IRoom>): boolean => t === 'p';
+	const filterRoomsByPublic = ({ t }: Partial<IRoom>): boolean => t === 'p';
+	// TODO: check if this is the correct prop for the rooms visibility!!!
+	const filterRoomsByChannels = ({ t }: Partial<IRoom>): boolean => t === 'd';
+	const filterRoomsByDirectMessages = ({ t }: Partial<IRoom>): boolean => t === 'd';
+	const filterRoomsByDiscussions = ({ t }: Partial<IRoom>): boolean => t === 'd';
+	const filterRoomsByOmnichannel = ({ t }: Partial<IRoom>): boolean => t === 'd';
+	const filterRoomsByTeams = ({ t }: Partial<IRoom>): boolean => t === 'd';
 
 	return (
-		<Box mb='x16' is='form' onSubmit={useCallback((e) => e.preventDefault(), [])} display='flex' flexDirection='column' {...props}>
-			<TextInput
-				flexShrink={0}
-				placeholder={t('Search_Rooms')}
-				addon={<Icon name='magnifier' size='x20' />}
-				onChange={handleChange}
-				value={text}
-			/>
+		<Box mb='x16' is='form' onSubmit={useCallback((e) => e.preventDefault(), [])} display='flex' flexDirection='row' {...props}>
 			<Field>
-				<Box display='flex' flexDirection='row' flexWrap='wrap' justifyContent='flex-start' mbs='x8' mi='neg-x8'>
-					<Margins inline='x8'>
-						<Field.Row>
-							<CheckBox checked={types.d} id={idDirect} onChange={(): void => handleCheckBox('d')} />
-							<Field.Label htmlFor={idDirect}>{t('Direct')}</Field.Label>
-						</Field.Row>
-						<Field.Row>
-							<CheckBox checked={types.c} id={idDPublic} onChange={(): void => handleCheckBox('c')} />
-							<Field.Label htmlFor={idDPublic}>{t('Public')}</Field.Label>
-						</Field.Row>
-						<Field.Row>
-							<CheckBox checked={types.p} id={idPrivate} onChange={(): void => handleCheckBox('p')} />
-							<Field.Label htmlFor={idPrivate}>{t('Private')}</Field.Label>
-						</Field.Row>
-						<Field.Row>
-							<CheckBox checked={types.l} id={idOmnichannel} onChange={(): void => handleCheckBox('l')} />
-							<Field.Label htmlFor={idOmnichannel}>{t('Omnichannel')}</Field.Label>
-						</Field.Row>
-						<Field.Row>
-							<CheckBox checked={types.discussions} id={idDiscussions} onChange={(): void => handleCheckBox('discussions')} />
-							<Field.Label htmlFor={idDiscussions}>{t('Discussions')}</Field.Label>
-						</Field.Row>
-						<Field.Row>
-							<CheckBox checked={types.teams} id={idTeam} onChange={(): void => handleCheckBox('teams')} />
-							<Field.Label htmlFor={idTeam}>{t('Teams')}</Field.Label>
-						</Field.Row>
-					</Margins>
-				</Box>
+				<TextInput placeholder={t('Search_Rooms')} addon={<Icon name='magnifier' size='x20' />} onChange={handleChange} value={text} />
+
+				<MultiSelectFiltered onChange={handleOnChangeAllRooms} options={roomTypeFilterStructure} placeholder={t('All_rooms')} />
+
+				<MultiSelectFiltered
+					onChange={() => {
+						console.log('onChange for All_visible multiselect');
+					}}
+					options={visibilityFilterStructure}
+					placeholder={t('All_visible')}
+				/>
 			</Field>
 		</Box>
 	);
