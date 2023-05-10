e63b651d6bdf7678fd57cc27f158746b1717d605
yash-rajpal <rajpal.yash03@gmail.com>
2023-05-10 23:45:12 +0530

    add required not auth page

--- apps/meteor/client/views/marketplace/AppsRoute.tsx
+++ apps/meteor/client/views/marketplace/AppsRoute.tsx
@@ -19,6 +19,7 @@ const AppsRoute = (): ReactElement => {
 	const page = useRouteParameter('page');
 
 	const isAdminUser = usePermission('manage-apps');
+	const hasAccessMarketplacePermission = usePermission('access-marketplace');
 
 	if (!page) marketplaceRoute.push({ context, page: 'list' });
 
@@ -39,6 +40,10 @@ const AppsRoute = (): ReactElement => {
 			mounted = false;
 		};
 	}, [marketplaceRoute, context]);
+
+	if (!hasAccessMarketplacePermission) {
+		return <NotAuthorizedPage />;
+	}
 
 	if ((context === 'requested' || page === 'install') && !isAdminUser) return <NotAuthorizedPage />;
 
