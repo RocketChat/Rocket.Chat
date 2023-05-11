bf3cba7d4dd9332f908b1cd94b6da18ea49a48fc
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2023-05-11 12:39:43 -0300

    Enable async local storage again

--- packages/core-services/src/lib/ContextStore.ts
+++ packages/core-services/src/lib/ContextStore.ts
@@ -7,14 +7,3 @@ interface IContextStore<T> {
 
 // This is the default implementation of the context store but there is a bug on Meteor 2.5 that prevents us from using it
 export class AsyncContextStore<T> extends AsyncLocalStorage<T> implements IContextStore<T> {}
-
-export class FakeContextStore<T extends object> implements IContextStore<T> {
-	getStore(): T | undefined {
-		return {} as unknown as T;
-	}
-
-	run(_store: T, callback: (...args: any) => void, ...args: any): void {
-		// eslint-disable-next-line new-cap
-		return callback(...args);
-	}
-}
--- packages/core-services/src/lib/asyncLocalStorage.ts
+++ packages/core-services/src/lib/asyncLocalStorage.ts
@@ -1,7 +1,4 @@
-// import { AsyncLocalStorage } from 'async_hooks';
-
 import type { IServiceContext } from '../types/ServiceClass';
-import { FakeContextStore } from './ContextStore';
+import { AsyncContextStore } from './ContextStore';
 
-// TODO Evalute again using AsyncContextStore instead of FibersContextStore in a future Meteor release (after 2.5)
-export const asyncLocalStorage = new FakeContextStore<IServiceContext>();
+export const asyncLocalStorage = new AsyncContextStore<IServiceContext>();
