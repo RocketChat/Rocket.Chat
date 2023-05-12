8f95d5d25c91901087867b147ad915aa53e2c648
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-05-12 16:43:17 -0300

    fix: convert OVERWRITE_SETTING type MULTISELECT to array (#29208)

--- apps/meteor/tests/unit/app/settings/server/functions/settings.tests.ts
+++ apps/meteor/tests/unit/app/settings/server/functions/settings.tests.ts
@@ -247,6 +247,87 @@ describe('Settings', () => {
 		});
 	});
 
+	it('should work with a setting type multiSelect with a default value', async () => {
+		const settings = new CachedSettings();
+		Settings.settings = settings;
+		settings.initialized();
+		const settingsRegistry = new SettingsRegistry({ store: settings, model: Settings as any });
+		await settingsRegistry.addGroup('group', async function () {
+			await this.section('section', async function () {
+				await this.add('my_setting_multiselect', ['a'], {
+					type: 'multiSelect',
+					sorter: 0,
+					values: [
+						{ key: 'a', i18nLabel: 'a' },
+						{ key: 'b', i18nLabel: 'b' },
+						{ key: 'c', i18nLabel: 'c' },
+					],
+				});
+			});
+		});
+
+		expect(Settings.insertCalls).to.be.equal(2);
+		expect(Settings.upsertCalls).to.be.equal(0);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).value).to.be.deep.equal(['a']);
+	});
+	it('should respect override via environment as multiSelect', async () => {
+		process.env.OVERWRITE_SETTING_my_setting_multiselect = '["a","b"]';
+
+		const settings = new CachedSettings();
+		Settings.settings = settings;
+		settings.initialized();
+		const settingsRegistry = new SettingsRegistry({ store: settings, model: Settings as any });
+
+		await settingsRegistry.addGroup('group', async function () {
+			await this.section('section', async function () {
+				await this.add('my_setting_multiselect', ['a'], {
+					type: 'multiSelect',
+					sorter: 0,
+					values: [
+						{ key: 'a', i18nLabel: 'a' },
+						{ key: 'b', i18nLabel: 'b' },
+						{ key: 'c', i18nLabel: 'c' },
+					],
+				});
+			});
+		});
+
+		expect(Settings.insertCalls).to.be.equal(2);
+		expect(Settings.upsertCalls).to.be.equal(0);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).value).to.be.deep.equal(['a', 'b']);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).processEnvValue).to.be.deep.equal(['a', 'b']);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).valueSource).to.be.equal('processEnvValue');
+	});
+
+	it('should ignore override via environment as multiSelect if value is invalid', async () => {
+		process.env.OVERWRITE_SETTING_my_setting_multiselect = '[INVALID_ARRAY]';
+
+		const settings = new CachedSettings();
+		Settings.settings = settings;
+		settings.initialized();
+		const settingsRegistry = new SettingsRegistry({ store: settings, model: Settings as any });
+
+		await settingsRegistry.addGroup('group', async function () {
+			await this.section('section', async function () {
+				await this.add('my_setting_multiselect', ['a'], {
+					type: 'multiSelect',
+					sorter: 0,
+					values: [
+						{ key: 'a', i18nLabel: 'a' },
+						{ key: 'b', i18nLabel: 'b' },
+						{ key: 'c', i18nLabel: 'c' },
+					],
+				});
+			});
+		});
+
+		expect(Settings.insertCalls).to.be.equal(2);
+		expect(Settings.upsertCalls).to.be.equal(0);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).value).to.be.deep.equal(['a']);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).processEnvValue).to.be.equal(undefined);
+		expect(Settings.findOne({ _id: 'my_setting_multiselect' }).valueSource).to.be.equal('packageValue');
+	});
+
 	it('should respect initial value via environment', async () => {
 		process.env.my_setting = '1';
 		const settings = new CachedSettings();
