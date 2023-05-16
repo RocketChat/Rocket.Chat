b82213f57af24f2480f3152f33d286f81cf33686
yash-rajpal <rajpal.yash03@gmail.com>
2023-05-16 14:33:32 +0530

    update migration number

--- apps/meteor/server/startup/migrations/v296.ts
+++ apps/meteor/server/startup/migrations/v296.ts
@@ -2,7 +2,7 @@ import { addMigration } from '../../lib/migrations';
 import { upsertPermissions } from '../../../app/authorization/server/functions/upsertPermissions';
 
 addMigration({
-	version: 295,
+	version: 296,
 	async up() {
 		await upsertPermissions();
 	},
