af1ca1f616d0bc4f6a4937ac4a424457dbabb474
Keanby <62653256+Keanby@users.noreply.github.com>
2023-05-20 19:10:32 +0300

    Fix Member list is randomly sorted #22145

--- apps/meteor/client/views/room/contextualBar/RoomMembers/RoomMembersWithData.tsx
+++ apps/meteor/client/views/room/contextualBar/RoomMembers/RoomMembersWithData.tsx
@@ -95,6 +95,20 @@ const RoomMembersWithData = ({ rid }: { rid: IRoom['_id'] }): ReactElement => {
 	if (state.tab === ROOM_MEMBERS_TABS.ADD) {
 		return <AddUsers rid={rid} onClickBack={handleBack} reload={reload} />;
 	}
+		
+	items.sort((a, b) => {
+		const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+		const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+		if (nameA < nameB) {
+		  return -1;
+		}
+		if (nameA > nameB) {
+		  return 1;
+		}
+
+		// names must be equal
+		return 0;
+	  });
 
 	return (
 		<RoomMembers
