27321ad160840ccc6148b036daa29ce4bfd66bdd
Guilherme Gazzo <guilherme@gazzo.xyz>
2023-05-22 23:39:24 -0300

    fix build

--- packages/uikit-playground/vite.config.ts
+++ packages/uikit-playground/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	optimizeDeps: {
+		include: ['@rocket.chat/ui-contexts'],
+	},
+	build: {
+		commonjsOptions: {
+			include: [/ui-contexts/, /node_modules/],
+		},
+	},
 });
