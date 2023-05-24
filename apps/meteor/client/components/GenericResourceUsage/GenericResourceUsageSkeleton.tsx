b8d9896711461484cef6f7919d186c46b70bc10f
Guilherme Gazzo <guilherme@gazzo.xyz>
2023-05-23 23:59:21 -0300

    Normalize the cap bar usage

---
+++ apps/meteor/client/components/GenericResourceUsage/GenericResourceUsageSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Box, Skeleton } from '@rocket.chat/fuselage';
+import React from 'react';
+
+export const GenericResourceUsageSkeleton = ({ title, ...props }: { title?: string }) => {
+	return (
+		<Box w='x180' h='x40' mi='x8' fontScale='c1' display='flex' flexDirection='column' justifyContent='space-around' {...props}>
+			{title ? <Box color='default'>{title}</Box> : <Skeleton w='full' />}
+			<Skeleton w='full' />
+		</Box>
+	);
+};
