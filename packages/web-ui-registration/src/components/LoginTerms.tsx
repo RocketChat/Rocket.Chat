77f10b5db532b28efd56de5ff9fda3d08bffd4d9
hugocostadev <hugocarreiracosta@gmail.com>
2023-05-25 15:36:26 -0300

    fix: changing default value with migration to use translation as default

--- packages/web-ui-registration/src/components/LoginTerms.tsx
+++ packages/web-ui-registration/src/components/LoginTerms.tsx
@@ -7,15 +7,10 @@ import { useTranslation } from 'react-i18next';
 export const LoginTerms = (): ReactElement => {
 	const { t } = useTranslation();
 	const loginTerms = useSetting('Layout_Login_Terms') as string;
-	const loginTermsDefault =
-		'By proceeding you are agreeing to our <a href="terms-of-service">Terms of Service</a>, <a href="privacy-policy">Privacy Policy</a> and <a href="legal-notice">Legal Notice</a>.';
 
 	return (
 		<HorizontalWizardLayoutCaption>
-			<Box
-				withRichContent
-				dangerouslySetInnerHTML={{ __html: loginTerms !== loginTermsDefault ? loginTerms : t('Layout_Login_Terms_Content') }}
-			/>
+			<Box withRichContent dangerouslySetInnerHTML={{ __html: loginTerms !== '' ? loginTerms : t('Layout_Login_Terms_Content') }} />
 		</HorizontalWizardLayoutCaption>
 	);
 };
