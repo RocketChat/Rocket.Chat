f554809013b6cb1f7ec5da554a82ce379fb3b910
dougfabris <devfabris@gmail.com>
2023-05-29 17:48:21 -0300

    chore: change toolbox pressed state

--- packages/ui-client/src/components/Header/ToolBox/ToolBoxAction.tsx
+++ packages/ui-client/src/components/Header/ToolBox/ToolBoxAction.tsx
@@ -1,25 +1,32 @@
+import type { Icon } from '@rocket.chat/fuselage';
 import { IconButton } from '@rocket.chat/fuselage';
-import type { FC } from 'react';
+import type { ComponentProps } from 'react';
 import { forwardRef } from 'react';
 
-const ToolBoxAction: FC<any> = forwardRef(function ToolBoxAction(
-	{ id, icon, color, action, className, index, title, 'data-tooltip': tooltip, ...props },
+type ToolBoxActionProps = {
+	'id': string;
+	'icon': ComponentProps<typeof Icon>['name'];
+	'action': (id: string) => void;
+	'index': string;
+	'title': string;
+	'data-tooltip': string;
+} & ComponentProps<typeof IconButton>;
+
+const ToolBoxAction = forwardRef<HTMLButtonElement, ToolBoxActionProps>(function ToolBoxAction(
+	{ id, icon, action, index, title, 'data-tooltip': tooltip, ...props },
 	ref,
 ) {
 	return (
 		<IconButton
 			data-qa-id={`ToolBoxAction-${icon}`}
-			className={className}
+			ref={ref}
 			onClick={() => action(id)}
 			data-toolbox={index}
 			key={id}
 			icon={icon}
-			position='relative'
 			tiny
-			overflow='visible'
-			ref={ref}
-			color={!!color && color}
-			{...{ ...props, ...(tooltip ? { 'data-tooltip': tooltip, 'title': '' } : { title }) }}
+			{...(tooltip ? { 'data-tooltip': tooltip, 'title': '' } : { title })}
+			{...props}
 		/>
 	);
 });
