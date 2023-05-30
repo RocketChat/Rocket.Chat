50ab08a27833423a79915ee19c63eaf70adf2f99
デヴぁんす <sdevanshu90@yahoo.com>
2023-05-30 18:18:46 +0530

    restrict role edit on site refresh

--- apps/meteor/app/moderation/client/index.ts
+++ apps/meteor/app/moderation/client/index.ts
@@ -2,8 +2,9 @@ import { settings } from '../../settings/client';
 import { addRoleEditRestriction } from '../lib/addRoleEditRestriction';
 
 Meteor.startup(() => {
-	const trustRoles = settings.get('Trust_Roles');
-	if (trustRoles) {
-		addRoleEditRestriction();
-	}
+	settings.onload('Trust_Roles', (_key, value) => {
+		if (value) {
+			addRoleEditRestriction();
+		}
+	});
 });
