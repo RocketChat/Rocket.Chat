efa80b2257236da0d719ddfb43bc1eeefb54fcf8
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-06-01 12:29:27 -0300

    Fix percentage calc

--- apps/meteor/client/views/admin/import/ImportProgressPage.tsx
+++ apps/meteor/client/views/admin/import/ImportProgressPage.tsx
@@ -41,13 +41,13 @@ const ImportProgressPage = function ImportProgressPage() {
 		},
 		{
 			onSuccess: ({ valid, status }) => {
-				console.log('currentOperation', valid, status);
 				if (!valid) {
 					importHistoryRoute.push();
 					return;
 				}
 
 				if (status === 'importer_done') {
+					dispatchToastMessage({ type: 'success', message: t('Importer_done') });
 					importHistoryRoute.push();
 					return;
 				}
@@ -65,7 +65,6 @@ const ImportProgressPage = function ImportProgressPage() {
 
 	const handleProgressUpdated = useMutableCallback(
 		({ key, step, completed, total }: { key: string; step: ProgressStep; completed: number; total: number }) => {
-			console.log('handleProgressUpdated', key, step, completed, total);
 			if (!currentOperation.isSuccess) {
 				return;
 			}
@@ -113,7 +112,6 @@ const ImportProgressPage = function ImportProgressPage() {
 			refetchInterval: 1000,
 			enabled: !!currentOperation.isSuccess,
 			onSuccess: (progress) => {
-				console.log('progress', progress);
 				if (!progress) {
 					dispatchToastMessage({ type: 'warning', message: t('Importer_not_in_progress') });
 					prepareImportRoute.push();
@@ -158,7 +156,8 @@ const ImportProgressPage = function ImportProgressPage() {
 								<Box display='flex' justifyContent='center'>
 									<Box is='progress' value={progress.data.completed} max={progress.data.total} marginInlineEnd='x24' />
 									<Box is='span' fontScale='p2'>
-										{progress.data.completed}/{progress.data.total} ({numberFormat(progress.data.completed / progress.data.total, 0)}
+										{progress.data.completed}/{progress.data.total} (
+										{numberFormat((progress.data.completed / progress.data.total) * 100, 0)}
 										%)
 									</Box>
 								</Box>
