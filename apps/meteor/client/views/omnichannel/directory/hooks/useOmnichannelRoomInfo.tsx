cdfbe515436a293b5542bc818dcf22e66c16b719
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-06-01 14:36:23 -0300

    fix: Omnichannel making an excessive amount of requests to room.info

--- apps/meteor/client/views/omnichannel/directory/hooks/useOmnichannelRoomInfo.tsx
+++ apps/meteor/client/views/omnichannel/directory/hooks/useOmnichannelRoomInfo.tsx
@@ -1,28 +1,15 @@
 import type { IOmnichannelRoom, Serialized } from '@rocket.chat/core-typings';
-import { useStream, useEndpoint } from '@rocket.chat/ui-contexts';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
 import { minutesToMilliseconds } from 'date-fns';
-import { useEffect } from 'react';
 
 export const useOmnichannelRoomInfo = (
 	roomId: string,
 	{ cacheTime = minutesToMilliseconds(15), staleTime = minutesToMilliseconds(5) } = {},
 ) => {
 	const getRoomInfo = useEndpoint('GET', '/v1/rooms.info');
-	const subscribeToRoom = useStream('room-data');
-	const queryClient = useQueryClient();
-
 	const { data: roomData, ...props } = useQuery(['/v1/rooms.info', roomId], () => getRoomInfo({ roomId }), { cacheTime, staleTime });
 	const room = roomData?.room as unknown as Serialized<IOmnichannelRoom>;
-
-	useEffect(
-		() =>
-			subscribeToRoom(roomId, () => {
-				queryClient.invalidateQueries(['/v1/rooms.info', roomId], { exact: true });
-				queryClient.invalidateQueries(['/v1/livechat/rooms']);
-			}),
-		[queryClient, roomId, subscribeToRoom],
-	);
 
 	return {
 		data: room,
