f566bfe0fef09d1bcca58e5ce90e8280fbefcfb3
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-06-01 16:40:10 -0300

    agora vai

--- packages/fuselage-ui-kit/src/elements/I18nTextElement.tsx
+++ packages/fuselage-ui-kit/src/elements/I18nTextElement.tsx
@@ -1,14 +1,11 @@
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import { Fragment } from 'react';
 
-const I18nTextElement = ({
-  i18nKey,
-  appId,
-}: {
-  i18nKey: string;
-  appId: string;
-}) => {
+import { useUiKitContext } from '../contexts/kitContext';
+
+const I18nTextElement = ({ i18nKey }: { i18nKey: string }) => {
   const t = useTranslation() as (key: string) => string;
+  const { appId } = useUiKitContext();
 
   return <Fragment>{t(`apps-${appId}-${i18nKey}`)}</Fragment>;
 };
