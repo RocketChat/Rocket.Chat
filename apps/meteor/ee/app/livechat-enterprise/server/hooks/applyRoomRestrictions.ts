e39d2b839062110faa390c54b21ad08709a70d3a
Kevin Aleman <kaleman960@gmail.com>
2023-06-02 09:19:32 -0600

    bad if

--- apps/meteor/ee/app/livechat-enterprise/server/hooks/applyRoomRestrictions.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/hooks/applyRoomRestrictions.ts
@@ -2,7 +2,6 @@ import type { FilterOperators } from 'mongodb';
 import type { IOmnichannelRoom } from '@rocket.chat/core-typings';
 
 import { callbacks } from '../../../../../lib/callbacks';
-import { hasRoleAsync } from '../../../../../app/authorization/server/functions/hasRole';
 import { cbLogger } from '../lib/logger';
 import { getUnitsFromUser } from '../lib/units';
 
@@ -19,18 +18,14 @@ export const restrictQuery = async (originalQuery: FilterOperators<IOmnichannelR
 		$or: [{ departmentAncestors: { $in: units } }, { departmentId: { $in: units } }],
 	};
 	query.$and = [condition, ...expressions];
+
 	return query;
 };
 
 callbacks.add(
 	'livechat.applyRoomRestrictions',
-	async (originalQuery: FilterOperators<IOmnichannelRoom> = {}, { userId }: { userId?: string | null } = { userId: null }) => {
-		if (!userId || !(await hasRoleAsync(userId, 'livechat-monitor'))) {
-			cbLogger.debug('Skipping callback. No user id provided or user is not a monitor');
-			return originalQuery;
-		}
-
-		cbLogger.debug('Applying department query restrictions');
+	async (originalQuery: FilterOperators<IOmnichannelRoom> = {}) => {
+		cbLogger.debug('Applying room query restrictions');
 		return restrictQuery(originalQuery);
 	},
 	callbacks.priority.HIGH,
