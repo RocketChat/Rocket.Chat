38e725fcdda4d2b3826a0e68a09864cb6798f893
Allan Ribeiro <allan_paz_ribeiro@hotmail.com>
2023-06-05 14:40:43 -0300

    fix: fixing the old settings comparation

--- apps/meteor/server/services/apps-engine/service.ts
+++ apps/meteor/server/services/apps-engine/service.ts
@@ -90,6 +90,18 @@ export class AppsEngineService extends ServiceClassInternal implements IAppsEngi
 
 			// avoid updating the setting if the value is the same,
 			// which caused an infinite loop
+			// and sometimes the settings can be an array
+			// so we need to convert it to JSON stringified to compare it
+
+			if (Array.isArray(oldSetting) && Array.isArray(setting.value)) {
+				if (JSON.stringify(oldSetting) === JSON.stringify(setting.value)) {
+					Apps.getRocketChatLogger().info(
+						`"apps.settingUpdated" event received for setting ${setting.id} of app "${appId}", but the setting value is the same`,
+					);
+					return;
+				}
+			}
+
 			if (oldSetting === setting.value) {
 				Apps.getRocketChatLogger().info(
 					`"apps.settingUpdated" event received for setting ${setting.id} of app "${appId}", but the setting value is the same`,
