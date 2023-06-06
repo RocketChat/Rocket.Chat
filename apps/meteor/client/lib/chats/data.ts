5db4e1ffe5ef7a2fe62ae36b20f0168d6998ce33
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-06-06 19:20:13 -0300

    chore: replace meteor.call -> sdk.call (#29318)

--- apps/meteor/client/lib/chats/data.ts
+++ apps/meteor/client/lib/chats/data.ts
@@ -6,8 +6,8 @@ import { hasAtLeastOnePermission, hasPermission } from '../../../app/authorizati
 import { Messages, ChatRoom, ChatSubscription } from '../../../app/models/client';
 import { settings } from '../../../app/settings/client';
 import { readMessage, MessageTypes } from '../../../app/ui-utils/client';
+import { sdk } from '../../../app/utils/client/lib/SDKClient';
 import { onClientBeforeSendMessage } from '../onClientBeforeSendMessage';
-import { call } from '../utils/call';
 import { prependReplies } from '../utils/prependReplies';
 import type { DataAPI } from './ChatAPI';
 
@@ -33,7 +33,7 @@ export const createDataAPI = ({ rid, tmid }: { rid: IRoom['_id']; tmid: IMessage
 	};
 
 	const findMessageByID = async (mid: IMessage['_id']): Promise<IMessage | null> =>
-		Messages.findOne({ _id: mid, _hidden: { $ne: true } }, { reactive: false }) ?? call('getSingleMessage', mid);
+		Messages.findOne({ _id: mid, _hidden: { $ne: true } }, { reactive: false }) ?? sdk.call('getSingleMessage', mid);
 
 	const getMessageByID = async (mid: IMessage['_id']): Promise<IMessage> => {
 		const message = await findMessageByID(mid);
@@ -175,7 +175,7 @@ export const createDataAPI = ({ rid, tmid }: { rid: IRoom['_id']; tmid: IMessage
 		Messages.upsert({ _id: message._id }, { $set: { ...message, rid, ...(tmid && { tmid }) } });
 	};
 
-	const updateMessage = async (message: IEditedMessage): Promise<void> => call('updateMessage', message);
+	const updateMessage = async (message: IEditedMessage): Promise<void> => sdk.call('updateMessage', message);
 
 	const canDeleteMessage = async (message: IMessage): Promise<boolean> => {
 		const uid = Meteor.userId();
@@ -215,7 +215,7 @@ export const createDataAPI = ({ rid, tmid }: { rid: IRoom['_id']; tmid: IMessage
 	};
 
 	const deleteMessage = async (mid: IMessage['_id']): Promise<void> => {
-		await call('deleteMessage', { _id: mid });
+		await sdk.call('deleteMessage', { _id: mid });
 		Messages.remove({ _id: mid });
 	};
 
@@ -246,7 +246,7 @@ export const createDataAPI = ({ rid, tmid }: { rid: IRoom['_id']; tmid: IMessage
 	const isSubscribedToRoom = async (): Promise<boolean> => !!ChatSubscription.findOne({ rid }, { reactive: false });
 
 	const joinRoom = async (): Promise<void> => {
-		await call('joinRoom', rid);
+		await sdk.call('joinRoom', rid);
 	};
 
 	const markRoomAsRead = async (): Promise<void> => {
