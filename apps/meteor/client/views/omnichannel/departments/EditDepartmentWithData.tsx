da4acb6979104bdfb73bdd7cb45805fbbeb8ca2b
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-06-06 10:18:36 -0300

    fix: Editting a department was showing the previous data

--- apps/meteor/client/views/omnichannel/departments/EditDepartmentWithData.tsx
+++ apps/meteor/client/views/omnichannel/departments/EditDepartmentWithData.tsx
@@ -17,7 +17,9 @@ type EditDepartmentWithDataProps = {
 const EditDepartmentWithData = ({ id, title }: EditDepartmentWithDataProps) => {
 	const t = useTranslation();
 	const getDepartment = useEndpoint('GET', '/v1/livechat/department/:_id', { _id: id ?? '' });
-	const { data, isInitialLoading, isError } = useQuery(['/v1/livechat/department/:_id'], () => getDepartment(params), { enabled: !!id });
+	const { data, isInitialLoading, isError } = useQuery(['/v1/livechat/department/:_id', id], () => getDepartment(params), {
+		enabled: !!id,
+	});
 
 	if (isInitialLoading) {
 		return <FormSkeleton />;
