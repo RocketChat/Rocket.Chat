8b1a98fcb812f4a0ba9644d8f96362991d70b6e3
Douglas Fabris <devfabris@gmail.com>
2023-06-07 11:46:41 -0300

    chore: Tags empty state (#29491)

--- apps/meteor/ee/client/omnichannel/tags/TagsPage.tsx
+++ apps/meteor/ee/client/omnichannel/tags/TagsPage.tsx
@@ -1,15 +1,14 @@
 import { Button, Icon, ButtonGroup } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import { useRoute, useTranslation } from '@rocket.chat/ui-contexts';
+import type { MutableRefObject } from 'react';
 import React from 'react';
 
-import FilterByText from '../../../../client/components/FilterByText';
-import GenericTable from '../../../../client/components/GenericTable';
 import Page from '../../../../client/components/Page';
+import TagsTable from './TagsTable';
 
-function TagsPage({ data, header, setParams, params, title, renderRow, children }) {
+const TagsPage = ({ reload }: { reload: MutableRefObject<() => void> }) => {
 	const t = useTranslation();
-
 	const tagsRoute = useRoute('omnichannel-tags');
 
 	const handleClick = useMutableCallback(() =>
@@ -21,7 +20,7 @@ function TagsPage({ data, header, setParams, params, title, renderRow, children 
 	return (
 		<Page flexDirection='row'>
 			<Page>
-				<Page.Header title={title}>
+				<Page.Header title={t('Tags')}>
 					<ButtonGroup>
 						<Button onClick={handleClick} title={t('New_Tag')}>
 							<Icon name='plus' /> {t('New')}
@@ -29,20 +28,11 @@ function TagsPage({ data, header, setParams, params, title, renderRow, children 
 					</ButtonGroup>
 				</Page.Header>
 				<Page.Content>
-					<GenericTable
-						header={header}
-						renderRow={renderRow}
-						results={data && data.tags}
-						total={data && data.total}
-						setParams={setParams}
-						params={params}
-						renderFilter={({ onChange, ...props }) => <FilterByText onChange={onChange} {...props} />}
-					/>
+					<TagsTable reload={reload} />
 				</Page.Content>
 			</Page>
-			{children}
 		</Page>
 	);
-}
+};
 
 export default TagsPage;
