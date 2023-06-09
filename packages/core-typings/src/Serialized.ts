06c7b4104752b8dd09008328399813d37c3b1843
Guilherme Gazzo <guilherme@gazzo.xyz>
2023-06-09 14:35:18 -0300

    make Serialized generic to allow different serializers

--- packages/core-typings/src/Serialized.ts
+++ packages/core-typings/src/Serialized.ts
@@ -1,9 +1,23 @@
-export type Serialized<T> = T extends Date
-	? Exclude<T, Date> | string
-	: T extends boolean | number | string | null | undefined
+type SerializableFields = boolean | number | string | null | undefined;
+
+type SerializableFieldsToString = Date;
+
+/**
+ * Base Serialized type. Common use to serialize objects to send to the client using JSON.stringify.
+ *
+ * You can define different types for the fields that should be serialized to string.
+ */
+
+export type Serialized<T, S = SerializableFields, F = SerializableFieldsToString> = T extends S
 	? T
-	: T extends {}
+	: T extends F
+	? Exclude<T, F> | string
+	: T extends Record<string, unknown>
 	? {
-			[K in keyof T]: Serialized<T[K]>;
+			[K in keyof T]: Serialized<T[K], S, F>;
 	  }
 	: null;
+
+export type RESTSerialized<T> = Serialized<T>;
+
+export type DDPSerialized<T> = Serialized<T, boolean | number | string | null | undefined | Date>;
