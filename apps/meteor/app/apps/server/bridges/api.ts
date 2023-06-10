edce5a4f89dca9944aa153fe222b30c84fff4242
Rafael Tapia <rafael.tapia@rocket.chat>
2023-06-10 09:15:57 -0300

    add routes back to api bridge

--- apps/meteor/app/apps/server/bridges/api.ts
+++ apps/meteor/app/apps/server/bridges/api.ts
@@ -3,6 +3,7 @@ import { ApiBridge } from '@rocket.chat/apps-engine/server/bridges';
 import type { AppApi } from '@rocket.chat/apps-engine/server/managers/AppApi';
 import { AppsApiService } from '@rocket.chat/core-services';
 import express from 'express';
+import type { Response, Request, IRouter } from 'express';
 import { WebApp } from 'meteor/webapp';
 
 import type { AppServerOrchestrator } from '../../../../ee/server/apps/orchestrator';
@@ -13,9 +14,43 @@ apiServer.disable('x-powered-by');
 
 WebApp.connectHandlers.use(apiServer);
 
+interface IRequestWithPrivateHash extends Request {
+	_privateHash?: string;
+	content?: any;
+}
+
 export class AppApisBridge extends ApiBridge {
+	appRouters: Map<string, IRouter>;
+
+	// eslint-disable-next-line no-empty-function
 	constructor(private readonly orch: AppServerOrchestrator) {
 		super();
+		this.appRouters = new Map();
+
+		apiServer.use('/api/apps/private/:appId/:hash', (req: IRequestWithPrivateHash, res: Response) => {
+			const notFound = (): Response => res.sendStatus(404);
+
+			const router = this.appRouters.get(req.params.appId);
+
+			if (router) {
+				req._privateHash = req.params.hash;
+				return router(req, res, notFound);
+			}
+
+			notFound();
+		});
+
+		apiServer.use('/api/apps/public/:appId', (req: Request, res: Response) => {
+			const notFound = (): Response => res.sendStatus(404);
+
+			const router = this.appRouters.get(req.params.appId);
+
+			if (router) {
+				return router(req, res, notFound);
+			}
+
+			notFound();
+		});
 	}
 
 	protected async registerApi({ api, computedPath, endpoint }: AppApi, appId: string): Promise<void> {
