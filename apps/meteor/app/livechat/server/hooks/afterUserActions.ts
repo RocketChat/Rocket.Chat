0b28b3c8be388aa8c15e971a6e5ddf5c27849951
murtaza98 <murtaza.patrawala@rocket.chat>
2023-06-14 11:25:29 +0530

    CR suggestions + listen to activate user events to add agent

--- apps/meteor/app/livechat/server/hooks/afterUserActions.ts
+++ apps/meteor/app/livechat/server/hooks/afterUserActions.ts
@@ -1,43 +1,50 @@
 import type { IUser } from '@rocket.chat/core-typings';
-import { Users } from '@rocket.chat/models';
 
 import { callbacks } from '../../../../lib/callbacks';
 import { Livechat } from '../lib/Livechat';
 import { callbackLogger } from '../lib/logger';
 
-type Props = {
+type IAfterSaveUserProps = {
 	user: IUser;
 	oldUser: IUser | null;
 };
 
-const handleAgentUpdated = async (userData: Props) => {
+const wasAgent = (user: Pick<IUser, 'roles'> | null) => user?.roles?.includes('livechat-agent');
+const isAgent = (user: Pick<IUser, 'roles'> | null) => user?.roles?.includes('livechat-agent');
+
+const handleAgentUpdated = async (userData: IAfterSaveUserProps) => {
 	const {
 		user: { _id: userId, username },
 		oldUser,
 	} = userData;
 
-	if (!userData.user?.roles?.includes('livechat-agent')) {
-		callbackLogger.debug('Removing extension', userId);
-		await Users.unsetExtension(userId);
-	}
-
-	if (oldUser?.roles?.includes('livechat-agent') && !userData.user?.roles?.includes('livechat-agent')) {
-		callbackLogger.error('Removing agent', userId);
+	if (wasAgent(oldUser) && !isAgent(userData.user)) {
+		callbackLogger.debug('Removing agent', userId);
 		await Livechat.removeAgent(username);
 	}
 
-	if (userData.user?.roles?.includes('livechat-agent') && !oldUser?.roles?.includes('livechat-agent')) {
-		callbackLogger.error('Adding agent', userId);
+	if (!wasAgent(oldUser) && isAgent(userData.user)) {
+		callbackLogger.debug('Adding agent', userId);
 		await Livechat.addAgent(username);
 	}
 };
 
-const handleDeactivateUser = async (userData: IUser) => {
-	if (userData?.roles?.includes('livechat-agent')) {
-		await Users.unsetExtension(userData._id);
+const handleDeactivateUser = async (user: IUser) => {
+	if (wasAgent(user)) {
+		callbackLogger.debug('Removing agent', user._id);
+		await Livechat.removeAgent(user.username);
+	}
+};
+
+const handleActivateUser = async (user: IUser) => {
+	if (isAgent(user)) {
+		callbackLogger.debug('Adding agent', user._id);
+		await Livechat.addAgent(user.username);
 	}
 };
 
 callbacks.add('afterSaveUser', handleAgentUpdated, callbacks.priority.LOW, 'livechat-after-save-user-remove-extension');
 
 callbacks.add('afterDeactivateUser', handleDeactivateUser, callbacks.priority.LOW, 'livechat-after-deactivate-user-remove-extension');
+
+callbacks.add('afterActivateUser', handleActivateUser, callbacks.priority.LOW, 'livechat-after-activate-user-remove-extension');
