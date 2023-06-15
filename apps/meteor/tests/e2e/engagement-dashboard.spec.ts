ca45144b70baf5142010a5718a3bcd188b834ea0
Guilherme Gazzo <guilherme@gazzo.xyz>
2023-06-15 13:25:53 -0300

    ..

--- apps/meteor/tests/e2e/engagement-dashboard.spec.ts
+++ apps/meteor/tests/e2e/engagement-dashboard.spec.ts
@@ -6,21 +6,20 @@ test.skip(!IS_EE, 'Engagemente Dashboard > Enterprise Only');
 
 test.use({ storageState: Users.admin.state });
 
-test.describe('engagement-dashboard', () => {
-	test.describe.parallel('expect to trigger fallback error component', () => {
-		test.beforeEach(async ({ page }) => {
-			await page.goto('/admin/engagement-dashboard');
-			await page.route('**/api/v1/engagement-dashboard/**', (route) => route.abort());
-		});
-
-		test('expect to show 4 fallback errors components inside widget at Users Tab', async ({ page }) => {
+test.describe.only('engagement-dashboard', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/admin/engagement-dashboard');
+		await page.route('**/api/v1/engagement-dashboard/**', (route) => route.abort());
+	});
+	test('expect to trigger fallback error component', async ({ page }) => {
+		await test.step('expect to show 4 fallback errors components inside widget at Users Tab', async () => {
 			await expect(page.locator('role=tab[name="Users"][selected]')).toBeVisible();
 
 			await page.waitForSelector('[data-qa="EngagementDashboardCardErrorBoundary"]');
 			await expect(page.locator('[data-qa="EngagementDashboardCardErrorBoundary"]')).toHaveCount(4);
 		});
 
-		test('expect to show 2 fallback errors components inside widget at Messages Tab', async ({ page }) => {
+		await test.step('expect to show 2 fallback errors components inside widget at Messages Tab', async () => {
 			await page.locator('role=tab[name="Messages"]').click();
 			await expect(page.locator('role=tab[name="Messages"][selected]')).toBeVisible();
 
@@ -28,7 +27,7 @@ test.describe('engagement-dashboard', () => {
 			await expect(page.locator('[data-qa="EngagementDashboardCardErrorBoundary"]')).toHaveCount(2);
 		});
 
-		test('expect to show a fallback error component inside widget at Channels Tab', async ({ page }) => {
+		await test.step('expect to show a fallback error component inside widget at Channels Tab', async () => {
 			await page.locator('role=tab[name="Channels"]').click();
 			await expect(page.locator('role=tab[name="Channels"][selected]')).toBeVisible();
 
