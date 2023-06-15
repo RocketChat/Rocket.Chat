565783b59f1bb4b191d1c67b6406307f9317f516
murtaza98 <murtaza.patrawala@rocket.chat>
2023-06-15 15:23:42 +0530

    fix: Dept w/o any BH config do not adhere to the default BH rules.

--- apps/meteor/ee/app/livechat-enterprise/server/business-hour/Helper.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/business-hour/Helper.ts
@@ -21,9 +21,30 @@ const getAllAgentIdsWithoutDepartment = async (): Promise<string[]> => {
 	return agentIdsWithoutDepartment;
 };
 
+const getAllAgentIdsWithDepartmentNotConnectedToBusinessHour = async (): Promise<string[]> => {
+	const activeDepartmentsWithoutBusinessHour = (
+		await LivechatDepartment.findActiveDepartmentsWithoutBusinessHour({
+			projection: { _id: 1 },
+		}).toArray()
+	).map((dept) => dept._id);
+
+	const agentIdsWithDepartmentNotConnectedToBusinessHour = await LivechatDepartmentAgents.col.distinct('agentId', {
+		departmentId: { $in: activeDepartmentsWithoutBusinessHour },
+	});
+
+	return agentIdsWithDepartmentNotConnectedToBusinessHour;
+};
+
+const getAllAgentIdsForDefaultBusinessHour = async (): Promise<string[]> => {
+	const withoutDepartment = await getAllAgentIdsWithoutDepartment();
+	const withDepartmentNotConnectedToBusinessHour = await getAllAgentIdsWithDepartmentNotConnectedToBusinessHour();
+
+	return [...new Set([...withoutDepartment, ...withDepartmentNotConnectedToBusinessHour])];
+};
+
 const getAgentIdsToHandle = async (businessHour: Record<string, any>): Promise<string[]> => {
 	if (businessHour.type === LivechatBusinessHourTypes.DEFAULT) {
-		return getAllAgentIdsWithoutDepartment();
+		return getAllAgentIdsForDefaultBusinessHour();
 	}
 	const departmentIds = (
 		await LivechatDepartment.findEnabledByBusinessHourId(businessHour._id, {
