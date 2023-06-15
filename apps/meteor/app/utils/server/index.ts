85451b45d9780a5c2d3090f5f03c9ff3a1d3aaca
Guilherme Gazzo <guilherme@gazzo.xyz>
2023-06-15 15:38:47 -0300

    remove stub

--- apps/meteor/app/utils/server/index.ts
+++ apps/meteor/app/utils/server/index.ts
@@ -4,7 +4,7 @@ export { getUserPreference } from './lib/getUserPreference';
 export { fileUploadIsValidContentType } from './restrictions';
 export { isDocker } from './functions/isDocker';
 export { getMongoInfo } from './functions/getMongoInfo';
-export { slashCommands } from '../lib/slashCommand';
+export { slashCommands } from './slashCommand';
 export { getUserNotificationPreference } from './getUserNotificationPreference';
 export { getAvatarColor } from '../lib/getAvatarColor';
 export { getURL } from './getURL';
