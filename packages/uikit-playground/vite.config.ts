e89a5fb03673faca3eb43e4ff44c408640016e4f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-06-23 11:24:13 -0300

    Apply suggestions from code review

--- packages/uikit-playground/vite.config.ts
+++ packages/uikit-playground/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	logLevel: 'silent',
+	logLevel: 'error',
 	esbuild: {
 		drop: mode === 'development' ? ['console', 'debugger'] : [],
 	},
