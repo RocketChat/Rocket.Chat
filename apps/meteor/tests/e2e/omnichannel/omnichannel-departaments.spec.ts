e22dfc15ef2aed2700eac5b1f224a01464bdc579
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-06-22 23:52:38 -0300

    chore: improve omnichannel tests reliability (#29572)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/tests/e2e/omnichannel/omnichannel-departaments.spec.ts
+++ apps/meteor/tests/e2e/omnichannel/omnichannel-departaments.spec.ts
@@ -1,10 +1,10 @@
 import { faker } from '@faker-js/faker';
 import type { Page } from '@playwright/test';
 
-import { IS_EE } from './config/constants';
-import { Users } from './fixtures/userStates';
-import { OmnichannelDepartments } from './page-objects';
-import { test, expect } from './utils/test';
+import { IS_EE } from '../config/constants';
+import { Users } from '../fixtures/userStates';
+import { OmnichannelDepartments } from '../page-objects';
+import { test, expect } from '../utils/test';
 
 const ERROR = {
 	requiredName: 'The field name is required.',
