265a18cab120c68f6e76458faf6d3a88e20dc79f
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-06-23 12:36:00 -0300

    fix

--- packages/fuselage-ui-kit/src/elements/PlainTextElement.tsx
+++ packages/fuselage-ui-kit/src/elements/PlainTextElement.tsx
@@ -1,7 +1,22 @@
 import type { TextObject } from '@rocket.chat/ui-kit';
 import { Fragment } from 'react';
+import { useTranslation } from '@rocket.chat/ui-contexts';
+
+import { useUiKitContext } from '../contexts/kitContext';
 
 const PlainTextElement = ({ textObject }: { textObject: TextObject }) => {
+  const t = useTranslation() as (
+    key: string,
+    args: { [key: string]: string | number }
+  ) => string;
+  const { appId } = useUiKitContext();
+
+  const { i18n } = textObject;
+
+  if (i18n) {
+    textObject.text = t(`apps-${appId}-${i18n.key}`, { ...i18n.args });
+  }
+
   return <Fragment>{textObject.text}</Fragment>;
 };
 
