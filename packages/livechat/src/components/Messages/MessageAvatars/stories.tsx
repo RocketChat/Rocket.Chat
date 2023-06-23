d685000cc543ec20f509cd498f0999de6e9c46ed
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-06-23 03:31:10 -0300

    Migrate `Messages/MessageAvatars` story to TS

---
+++ packages/livechat/src/components/Messages/MessageAvatars/stories.tsx
@@ -0,0 +1,45 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { MessageAvatars } from '.';
+import { avatarResolver } from '../../../helpers.stories';
+
+export default {
+	title: 'Messages/MessageAvatars',
+	component: MessageAvatars,
+	args: {
+		avatarResolver,
+		usernames: [],
+	},
+	parameters: {
+		layout: 'centered',
+	},
+} satisfies ComponentMeta<typeof MessageAvatars>;
+
+const Template: ComponentStory<typeof MessageAvatars> = (args) => <MessageAvatars {...args} />;
+
+export const Empty = Template.bind({});
+Empty.storyName = 'empty';
+
+export const WithOneAvatar = Template.bind({});
+WithOneAvatar.storyName = 'with one avatar';
+WithOneAvatar.args = {
+	usernames: ['guilherme.gazzo'],
+};
+
+export const WithTwoAvatars = Template.bind({});
+WithTwoAvatars.storyName = 'with two avatars';
+WithTwoAvatars.args = {
+	usernames: ['guilherme.gazzo', 'tasso.evangelista'],
+};
+
+export const WithThreeAvatars = Template.bind({});
+WithThreeAvatars.storyName = 'with three avatars';
+WithThreeAvatars.args = {
+	usernames: ['guilherme.gazzo', 'tasso.evangelista', 'martin.schoeler'],
+};
+
+export const WithNameAsAvatarInsteadOfUsernameForGuests = Template.bind({});
+WithNameAsAvatarInsteadOfUsernameForGuests.storyName = 'with name as avatar instead of username for guests';
+WithNameAsAvatarInsteadOfUsernameForGuests.args = {
+	usernames: ['livechat guest'],
+};
