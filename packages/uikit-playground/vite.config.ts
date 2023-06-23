0567fa07d4a8837f90eda2ecc5e9f18e0cfd8de9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-06-23 10:19:32 -0300

    uikit loglevel silent

--- packages/uikit-playground/vite.config.ts
+++ packages/uikit-playground/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	logLevel: 'silent',
 	esbuild: {
 		drop: mode === 'development' ? ['console', 'debugger'] : [],
 	},
