3fa8287092650b32da2872693c8ee128eebe9556
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-06-23 04:21:23 -0300

    Migrate `Forms/SelectInput` story to TS

---
+++ packages/livechat/src/components/Form/SelectInput/stories.tsx
@@ -0,0 +1,63 @@
+import { action } from '@storybook/addon-actions';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { SelectInput } from '.';
+import { Form, FormField } from '..';
+
+export default {
+	title: 'Forms/SelectInput',
+	component: SelectInput,
+	args: {
+		value: '',
+		options: [
+			{ value: '1', label: 'Option 1' },
+			{ value: '2', label: 'Option 2' },
+			{ value: '3', label: 'Option 3' },
+		],
+		placeholder: 'Placeholder',
+		disabled: false,
+		small: false,
+		error: false,
+		onChange: action('change'),
+		onInput: action('input'),
+	},
+	decorators: [
+		(storyFn) => (
+			<Form>
+				<FormField>{storyFn()}</FormField>
+			</Form>
+		),
+	],
+	parameters: {
+		layout: 'centered',
+	},
+} satisfies ComponentMeta<typeof SelectInput>;
+
+const Template: ComponentStory<typeof SelectInput> = (args) => <SelectInput {...args} />;
+
+export const Empty = Template.bind({});
+Empty.storyName = 'empty';
+
+export const Selected = Template.bind({});
+Selected.storyName = 'selected';
+Selected.args = {
+	value: '2',
+};
+
+export const Disabled = Template.bind({});
+Disabled.storyName = 'disabled';
+Disabled.args = {
+	disabled: true,
+};
+
+export const Small = Template.bind({});
+Small.storyName = 'small';
+Small.args = {
+	small: true,
+};
+
+export const WithError = Template.bind({});
+WithError.storyName = 'with error';
+WithError.args = {
+	error: true,
+};
