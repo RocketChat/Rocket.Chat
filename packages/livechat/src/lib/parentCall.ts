ab157b074c179af29411a5bc648b91d558ce566b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-06-25 07:13:15 -0300

    Migrate small modules

--- packages/livechat/src/lib/parentCall.ts
+++ packages/livechat/src/lib/parentCall.ts
@@ -1,15 +1,15 @@
 import { validCallbacks } from '../widget';
 
-export function parentCall(method, args = []) {
+export const parentCall = (method: string, args: any[] = []) => {
 	const data = {
 		src: 'rocketchat',
 		fn: method,
 		args,
 	};
 
-	// This lgtm ignoring deserves more attention urgently!
+	// TODO: This lgtm ignoring deserves more attention urgently!
 	window.parent.postMessage(data, '*'); // lgtm [js/cross-window-information-leak]
-}
+};
 
-export const runCallbackEventEmitter = (callbackName, data) =>
+export const runCallbackEventEmitter = (callbackName: string, data: unknown) =>
 	validCallbacks.includes(callbackName) && parentCall('callback', [callbackName, data]);
