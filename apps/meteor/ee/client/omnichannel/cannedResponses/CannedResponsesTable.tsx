bba3c6530ddf8a060819da4de086bbe6dd320f96
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-06-26 17:00:04 -0300

    refactor: simplified canned response filter query

--- apps/meteor/ee/client/omnichannel/cannedResponses/CannedResponsesTable.tsx
+++ apps/meteor/ee/client/omnichannel/cannedResponses/CannedResponsesTable.tsx
@@ -67,10 +67,7 @@ const CannedResponsesTable = () => {
 	);
 
 	const getCannedResponses = useEndpoint('GET', '/v1/canned-responses');
-	const { data, isLoading, isSuccess, refetch } = useQuery(
-		['/v1/canned-responses', { debouncedText, sortDirection, itemsPerPage, current, sharing, createdBy }],
-		() => getCannedResponses(query),
-	);
+	const { data, isLoading, isSuccess, refetch } = useQuery(['/v1/canned-responses', query], () => getCannedResponses(query));
 
 	const getTime = useFormatDateAndTime();
 
