1550bd5bc4c2665acccbf2893f2d712247f5ccea
Kevin Aleman <kaleman960@gmail.com>
2023-07-03 12:23:55 -0600

    ref

--- apps/meteor/ee/app/api-enterprise/server/lib/canned-responses.js
+++ apps/meteor/ee/app/api-enterprise/server/lib/canned-responses.js
@@ -1,37 +1,8 @@
 import { escapeRegExp } from '@rocket.chat/string-helpers';
-import { LivechatDepartmentAgents, CannedResponse, LivechatUnit, LivechatTag } from '@rocket.chat/models';
+import { LivechatDepartmentAgents, CannedResponse, LivechatUnit } from '@rocket.chat/models';
 
 import { hasPermissionAsync } from '../../../../../app/authorization/server/functions/hasPermission';
-
-const getTagsInformation = async (cannedResponses) => {
-	return Promise.all(
-		cannedResponses.map(async (cannedResponse) => {
-			const { tags } = cannedResponse;
-
-			if (!Array.isArray(tags) || !tags.length) {
-				return cannedResponse;
-			}
-
-			const serverTags = await LivechatTag.find({ _id: { $in: tags } }, { projection: { _id: 1, name: 1 } }).toArray();
-
-			// filter out tags that were found
-			const newTags = tags.reduce((acc, tag) => {
-				const found = serverTags.find((serverTag) => serverTag._id === tag);
-				if (found) {
-					acc.push(found.name);
-				} else {
-					acc.push(tag);
-				}
-				return acc;
-			}, []);
-
-			// Known limitation: if a tag was added and removed before this, it will return the tag id instead of the name
-			cannedResponse.tags = newTags;
-
-			return cannedResponse;
-		}),
-	);
-};
+import { getTagsInformation } from './tags';
 
 export async function findAllCannedResponses({ userId }) {
 	// If the user is an admin or livechat manager, get his own responses and all responses from all departments
