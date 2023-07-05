419dc927a2f489252c4587d769a47199d986bd88
Marcos Defendi <marcos.defendi@rocket.chat>
2023-07-05 09:45:34 -0300

    feat: add federation service and package

---
+++ ee/apps/federation-service/src/FederationService.ts
@@ -0,0 +1,13 @@
+import { ServiceClass } from '@rocket.chat/core-services';
+
+export class FederationService extends ServiceClass {
+	protected name = 'federation-service';
+
+	constructor() {
+		super();
+
+		// your stuff
+	}
+
+	// more stuff
+}
