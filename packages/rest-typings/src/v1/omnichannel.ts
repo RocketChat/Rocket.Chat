adcb9cbdb205aa050986c564722095253fe27cc6
murtaza98 <murtaza.patrawala@rocket.chat>
2023-07-06 14:51:24 +0530

    fix type error

--- packages/rest-typings/src/v1/omnichannel.ts
+++ packages/rest-typings/src/v1/omnichannel.ts
@@ -955,8 +955,8 @@ export type LivechatRoomsProps = {
 
 export type VisitorSearchChatsResult = Pick<
 	IOmnichannelRoom,
-	'fname' | 'ts' | 'msgs' | 'servedBy' | 'closedAt' | 'closedBy' | 'closer' | 'tags' | '_id' | 'closingMessage' | 'v'
->;
+	'fname' | 'ts' | 'msgs' | 'servedBy' | 'closedAt' | 'closedBy' | 'closer' | 'tags' | '_id' | 'closingMessage'
+> & { v: Omit<IOmnichannelRoom['v'], 'lastMessageTs'> };
 
 const LivechatRoomsSchema = {
 	type: 'object',
