f3160e622d5cb6f43f1249dc2ad94df350ea395d
rique223 <henrique.jobs1@gmail.com>
2023-07-06 14:59:26 -0300

    fix: :bug: Adapt engagementDashboard route to new routing system
    Removed the engagementDashboard route creation point from ee due to the new system requiring it to be on CE for the route to work. Did the same to deviceManagement and also removed the ready parameter from engagement.

--- apps/meteor/ee/client/startup/deviceManagement.ts
+++ apps/meteor/ee/client/startup/deviceManagement.ts
@@ -1,27 +1,16 @@
 import { lazy } from 'react';
 
 import { registerAccountRoute, registerAccountSidebarItem, unregisterSidebarItem } from '../../../client/views/account';
-import { registerAdminRoute } from '../../../client/views/admin';
 import { onToggledFeature } from '../lib/onToggledFeature';
 
 declare module '@rocket.chat/ui-contexts' {
 	interface IRouterPaths {
-		'device-management': {
-			pathname: `/admin/device-management${`/${string}` | ''}${`/${string}` | ''}`;
-			pattern: '/admin/device-management/:context?/:id?';
-		};
 		'manage-devices': {
 			pathname: '/account/manage-devices';
 			pattern: '/account/manage-devices';
 		};
 	}
 }
-
-const [registerAdminRouter, unregisterAdminRouter] = registerAdminRoute('/device-management/:context?/:id?', {
-	name: 'device-management',
-	component: lazy(() => import('../views/admin/deviceManagement/DeviceManagementAdminRoute')),
-	ready: false,
-});
 
 const [registerAccountRouter, unregisterAccountRouter] = registerAccountRoute('/manage-devices', {
 	name: 'manage-devices',
@@ -35,12 +24,10 @@ onToggledFeature('device-management', {
 			i18nLabel: 'Manage_Devices',
 			icon: 'mobile',
 		});
-		registerAdminRouter();
 		registerAccountRouter();
 	},
 	down: () => {
 		unregisterSidebarItem('Manage_Devices');
-		unregisterAdminRouter();
 		unregisterAccountRouter();
 	},
 });
