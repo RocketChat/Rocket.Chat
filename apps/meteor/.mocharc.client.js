760b905d6f04c6d9b56fe7800e94cdb4755816e6
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-07 09:37:53 -0300

    ..

--- apps/meteor/.mocharc.client.js
+++ apps/meteor/.mocharc.client.js
@@ -34,9 +34,9 @@ module.exports = {
 	slow: 200,
 	spec: [
 		'client/**/*.spec.{ts,tsx}',
-		'!client/hooks/*.spec.{ts,tsx}',
 		'tests/unit/client/**/*.spec.{ts,tsx}',
 		'tests/unit/lib/**/*.tests.ts',
 		'tests/unit/client/**/*.test.ts',
 	],
+	exclude: ['client/hooks/*.spec.{ts,tsx}'],
 };
