a5f3db83360653cd88aa240ab94be1f416b5583c
Kevin Aleman <kaleman960@gmail.com>
2023-07-10 10:16:06 -0700

    Update saveLastVisitorMessageTs.ts

--- apps/meteor/app/livechat/server/hooks/saveLastVisitorMessageTs.ts
+++ apps/meteor/app/livechat/server/hooks/saveLastVisitorMessageTs.ts
@@ -2,7 +2,6 @@ import { isOmnichannelRoom } from '@rocket.chat/core-typings';
 import { LivechatRooms } from '@rocket.chat/models';
 
 import { callbacks } from '../../../../lib/callbacks';
-import { callbackLogger } from '../lib/logger';
 
 callbacks.add(
 	'afterSaveMessage',
@@ -15,11 +14,6 @@ callbacks.add(
 		}
 		if (message.token) {
 			await LivechatRooms.setVisitorLastMessageTimestampByRoomId(room._id, message.ts);
-			callbackLogger.debug({
-				msg: 'Saved last visitor message timestamp',
-				rid: room._id,
-				msgId: message._id,
-			});
 		}
 		return message;
 	},
