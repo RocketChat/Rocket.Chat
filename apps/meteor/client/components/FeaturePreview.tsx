ea7db8d5736f3384290f4b75362d4c85d02fca22
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-10 14:46:01 -0300

    create feature preview component

---
+++ apps/meteor/client/components/FeaturePreview.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-multi-comp */
+import type { ReactElement, ReactNode } from 'react';
+import React, { Suspense } from 'react';
+
+import { useFeaturePreview } from '../hooks/useFeaturePreview';
+import type { FeaturesAvailable } from '../hooks/useFeaturePreviewList';
+
+export const FeaturePreview = ({ feature, children }: { feature: FeaturesAvailable; children: ReactElement[] }) => {
+	const featureToggleEnabled = useFeaturePreview(feature);
+
+	const toggledChildren = React.Children.map(children, (child) =>
+		React.cloneElement(child, {
+			featureToggleEnabled,
+		}),
+	);
+
+	return <Suspense fallback={null}>{toggledChildren}</Suspense>;
+};
+
+export const FeaturePreviewOn = ({ children, featureToggleEnabled }: { children: ReactNode; featureToggleEnabled?: boolean }) => (
+	<>{featureToggleEnabled && children}</>
+);
+
+export const FeaturePreviewOff = ({ children, featureToggleEnabled }: { children: ReactNode; featureToggleEnabled?: boolean }) => (
+	<>{!featureToggleEnabled && children}</>
+);
