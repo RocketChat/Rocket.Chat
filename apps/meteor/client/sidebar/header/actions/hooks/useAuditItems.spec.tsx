e9f5ec9fb98d7040e2389a47cfd66176f825559c
dougfabris <devfabris@gmail.com>
2023-07-13 17:59:24 -0300

    feat: useAuditItems

--- apps/meteor/client/sidebar/header/actions/hooks/useAuditItems.spec.tsx
+++ apps/meteor/client/sidebar/header/actions/hooks/useAuditItems.spec.tsx
@@ -17,28 +17,66 @@ const queryClient = new QueryClient({
 	},
 });
 
-// it('should return an empty array if not have license', () => {
-// 	const { result } = renderHook(() => useAuditItems(), {
-// 		wrapper: ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>,
-// 	});
+it('should return an empty array if doesn`t have license', async () => {
+	const { result, waitFor } = renderHook(() => useAuditItems(), {
+		wrapper: ({ children }) => (
+			<QueryClientProvider client={queryClient}>
+				<MockedServerContext
+					handleMethod={(methodName, ..._) => {
+						if (methodName === 'license:getModules') {
+							return [] as any;
+						}
 
-// 	expect(result.all[0]).toEqual([]);
-// });
+						throw new Error('Method not mocked');
+					}}
+				>
+					<MockedSettingsContext settings={{}}>
+						<MockedUserContext userPreferences={{}}>
+							<MockedAuthorizationContext permissions={['can-audit', 'can-audit-log']}>{children}</MockedAuthorizationContext>
+						</MockedUserContext>
+					</MockedSettingsContext>
+				</MockedServerContext>
+			</QueryClientProvider>
+		),
+	});
 
-// it('should return an empty array if have license and not have permissions', () => {
-// 	const { result } = renderHook(() => useAuditItems(), {
-// 		wrapper: ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>,
-// 	});
+	await waitFor(() => Boolean(result.all.length > 1));
+	expect(result.current).toEqual([]);
+});
 
-// 	expect(result.all[0]).toEqual([]);
-// });
+it('should return an empty array if have license and not have permissions', async () => {
+	const { result, waitFor } = renderHook(() => useAuditItems(), {
+		wrapper: ({ children }) => (
+			<QueryClientProvider client={queryClient}>
+				<MockedServerContext
+					handleMethod={(methodName, ..._) => {
+						if (methodName === 'license:getModules') {
+							return ['auditing'] as any;
+						}
+
+						throw new Error('Method not mocked');
+					}}
+				>
+					<MockedSettingsContext settings={{}}>
+						<MockedUserContext userPreferences={{}}>
+							<MockedAuthorizationContext permissions={[]}>{children}</MockedAuthorizationContext>
+						</MockedUserContext>
+					</MockedSettingsContext>
+				</MockedServerContext>
+			</QueryClientProvider>
+		),
+	});
+
+	await waitFor(() => Boolean(result.all.length > 1));
+	expect(result.current).toEqual([]);
+});
 
 it('should return auditItems if have license and permissions', async () => {
 	const { result, waitFor } = renderHook(() => useAuditItems(), {
 		wrapper: ({ children }) => (
 			<QueryClientProvider client={queryClient}>
 				<MockedServerContext
-					handleMethod={async (methodName, ..._: unknown[]) => {
+					handleMethod={(methodName, ..._) => {
 						if (methodName === 'license:getModules') {
 							return ['auditing'] as any;
 						}
@@ -57,7 +95,77 @@ it('should return auditItems if have license and permissions', async () => {
 	});
 
 	await waitFor(() => Boolean(result.current.length));
-	expect(result.current).toHaveLength(2);
+	expect(result.current[0]).toEqual(
+		expect.objectContaining({
+			id: 'messages',
+		}),
+	);
+
+	expect(result.current[1]).toEqual(
+		expect.objectContaining({
+			id: 'auditLog',
+		}),
+	);
 });
 
-// it('should return an empty array if don`t have permission can-audit && can-audit-log', () => {});
+it('should return auditMessages item if have license and can-audit permission', async () => {
+	const { result, waitFor } = renderHook(() => useAuditItems(), {
+		wrapper: ({ children }) => (
+			<QueryClientProvider client={queryClient}>
+				<MockedServerContext
+					handleMethod={(methodName, ..._) => {
+						if (methodName === 'license:getModules') {
+							return ['auditing'] as any;
+						}
+
+						throw new Error('Method not mocked');
+					}}
+				>
+					<MockedSettingsContext settings={{}}>
+						<MockedUserContext userPreferences={{}}>
+							<MockedAuthorizationContext permissions={['can-audit']}>{children}</MockedAuthorizationContext>
+						</MockedUserContext>
+					</MockedSettingsContext>
+				</MockedServerContext>
+			</QueryClientProvider>
+		),
+	});
+
+	await waitFor(() => Boolean(result.current.length));
+	expect(result.current[0]).toEqual(
+		expect.objectContaining({
+			id: 'messages',
+		}),
+	);
+});
+
+it('should return audiLogs item if have license and can-audit-log permission', async () => {
+	const { result, waitFor } = renderHook(() => useAuditItems(), {
+		wrapper: ({ children }) => (
+			<QueryClientProvider client={queryClient}>
+				<MockedServerContext
+					handleMethod={(methodName, ..._) => {
+						if (methodName === 'license:getModules') {
+							return ['auditing'] as any;
+						}
+
+						throw new Error('Method not mocked');
+					}}
+				>
+					<MockedSettingsContext settings={{}}>
+						<MockedUserContext userPreferences={{}}>
+							<MockedAuthorizationContext permissions={['can-audit-log']}>{children}</MockedAuthorizationContext>
+						</MockedUserContext>
+					</MockedSettingsContext>
+				</MockedServerContext>
+			</QueryClientProvider>
+		),
+	});
+
+	await waitFor(() => Boolean(result.current.length));
+	expect(result.current[0]).toEqual(
+		expect.objectContaining({
+			id: 'auditLog',
+		}),
+	);
+});
