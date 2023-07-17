7bfcbf920b93eb24829732460e72b653b36d1797
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2023-07-17 12:16:23 +0530

    removing all private methods from public interface

--- packages/core-typings/src/omnichannel/queue.ts
+++ packages/core-typings/src/omnichannel/queue.ts
@@ -1,13 +1,5 @@
-import type { InquiryWithAgentInfo } from '../IInquiry';
-
 export interface IOmnichannelQueue {
-	delay(): number;
 	start(): Promise<void>;
 	shouldStart(): void;
 	stop(): Promise<void>;
-	getActiveQueues(): Promise<(string | undefined)[]>;
-	nextQueue(): Promise<string | undefined>;
-	execute(): Promise<void>;
-	checkQueue(queue: string | undefined): Promise<void>;
-	processWaitingQueue(department: string | undefined, inquiry: InquiryWithAgentInfo): Promise<boolean>;
 }
