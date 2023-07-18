20869dce13d806002e5fe02cc5df8d6163e87f41
juliajforesti <juliajforesti@gmail.com>
2023-07-18 19:07:31 -0300

    refactor themeItems

--- apps/meteor/client/views/account/themes/ThemesPage.tsx
+++ apps/meteor/client/views/account/themes/ThemesPage.tsx
@@ -5,13 +5,14 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 import Page from '../../../components/Page';
-import { useThemeItems } from './useThemeItems';
+import { useIsEnterprise } from '../../../hooks/useIsEnterprise';
+import { themeItems as themes } from './themeItems';
 
 const ThemesPage = () => {
 	const t = useTranslation();
 	const dispatchToastMessage = useToastMessageDispatch();
+	const { data: license } = useIsEnterprise();
 
-	const themes = useThemeItems();
 	const themePreference = useUserPreference<'light' | 'dark' | 'auto' | 'high-contrast'>('themeAppearence') || 'auto';
 	const setUserPreferences = useEndpoint('POST', '/v1/users.setPreferences');
 
@@ -53,27 +54,31 @@ const ThemesPage = () => {
 					<Accordion>
 						<Accordion.Item defaultExpanded={true} title={t('Themes')}>
 							{themes.map(({ id, title, description, ...item }, index) => {
-								const disabled = 'disabled' in item && item.disabled;
 								const externalLink = 'externalLink' in item && item.externalLink;
+								const comunityDisabled = 'isEEOnly' in item && item.isEEOnly && !license?.isEnterprise;
 
 								return (
 									<Field key={id} pbe={themes.length - 1 ? undefined : 'x28'} pbs={index === 0 ? undefined : 'x28'}>
 										<Box display='flex' flexDirection='row' justifyContent='spaceBetween' flexGrow={1}>
 											<Field.Label display='flex' alignItems='center'>
 												{t.has(title) ? t(title) : title}
-												{disabled && (
+												{comunityDisabled && (
 													<Box is='span' mis='x8'>
 														<Tag variant='featured'>{t('Enterprise')}</Tag>
 													</Box>
 												)}
 											</Field.Label>
 											<Field.Row>
-												<RadioButton {...register('themeAppearence')} value={id} disabled={disabled} />
+												<RadioButton {...register('themeAppearence')} value={id} disabled={comunityDisabled} />
 											</Field.Row>
 										</Box>
 										<Field.Hint mbs='x12' style={{ whiteSpace: 'break-spaces' }}>
 											{t.has(description) ? t(description) : description}
-											<Box mbs='x12'>{externalLink && <ExternalLink to={externalLink}>{t('Talk_to_an_expert')}</ExternalLink>}</Box>
+											{externalLink && comunityDisabled && (
+												<Box mbs='x12'>
+													<ExternalLink to={externalLink}>{t('Talk_to_an_expert')}</ExternalLink>
+												</Box>
+											)}
 										</Field.Hint>
 									</Field>
 								);
