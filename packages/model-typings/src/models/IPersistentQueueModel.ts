b5608fb2acea02e24fe22012890a53c4a86b0068
Luis Mauro <mauromb3@gmail.com>
2023-07-19 17:30:02 -0600

    misc: add missing type and config

--- packages/model-typings/src/models/IPersistentQueueModel.ts
+++ packages/model-typings/src/models/IPersistentQueueModel.ts
@@ -1,3 +1,5 @@
+import type { IPersistentQueue } from '@rocket.chat/core-typings';
+
 import type { IBaseModel } from './IBaseModel';
 
-export type IPersistentQueueModel = IBaseModel<any>;
+export type IPersistentQueueModel = IBaseModel<IPersistentQueue>;
