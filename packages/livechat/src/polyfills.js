6e13b001be9411ba580a34e3c9f234ac8209eb09
MartinSchoeler <martin.schoeler@rocket.chat>
2023-07-20 17:01:17 -0300

    remove duplicate files

--- packages/livechat/src/polyfills.js
+++
@@ -1,6 +0,0 @@
-import 'whatwg-fetch';
-import cssVars from 'css-vars-ponyfill';
-
-if (typeof window.CSS === 'undefined' || typeof CSS.supports !== 'function' || !CSS.supports('--foo: bar')) {
-	cssVars();
-}
