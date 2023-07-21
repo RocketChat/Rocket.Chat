edda1b34507ee577e5851e715de41e5c3d6adb50
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-21 18:53:56 -0300

    unarchiveRoom

--- apps/meteor/app/lib/server/functions/index.ts
+++ apps/meteor/app/lib/server/functions/index.ts
@@ -1,4 +1,3 @@
-export { unarchiveRoom } from './unarchiveRoom';
 export { updateMessage } from './updateMessage';
 export { validateCustomFields } from './validateCustomFields';
 export { validateName } from './validateName';
