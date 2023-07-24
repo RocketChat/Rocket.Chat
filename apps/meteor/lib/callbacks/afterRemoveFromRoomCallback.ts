a65653403944750f1e7d2a461953e898ea354d8c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-24 18:48:48 -0300

    afterRemoveFromRoomCallback

--- apps/meteor/lib/callbacks/afterRemoveFromRoomCallback.ts
+++ apps/meteor/lib/callbacks/afterRemoveFromRoomCallback.ts
@@ -2,6 +2,5 @@ import type { IRoom, IUser } from '@rocket.chat/core-typings';
 
 import { Callbacks } from './callbacksBase';
 
-export const afterRemoveFromRoomCallback = Callbacks.create<{ removedUser: IUser; userWhoRemoved: IUser }, void, IRoom>(
-	'afterRemoveFromRoom',
-);
+export const afterRemoveFromRoomCallback =
+	Callbacks.create<(data: { removedUser: IUser; userWhoRemoved: IUser }, room: IRoom) => void>('afterRemoveFromRoom');
