24606c5701c45745ee9bb9c05e41468be44409ab
Marcos Defendi <marcos.defendi@rocket.chat>
2023-07-25 15:37:07 -0300

    chore: partially move federation unit tests to the new pkg

---
+++ ee/packages/federation/tests/setup/chaiPlugins.ts
@@ -0,0 +1,8 @@
+import chai from 'chai';
+import chaiSpies from 'chai-spies';
+import chaiDatetime from 'chai-datetime';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiSpies);
+chai.use(chaiDatetime);
+chai.use(chaiAsPromised);
