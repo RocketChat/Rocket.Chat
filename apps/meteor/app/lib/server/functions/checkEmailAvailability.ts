ad76fca19efa536f59487afb88f89629a75bbc76
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-26 16:00:15 -0300

    lint

--- apps/meteor/app/lib/server/functions/checkEmailAvailability.ts
+++ apps/meteor/app/lib/server/functions/checkEmailAvailability.ts
@@ -1,5 +1,4 @@
 import { Users } from '@rocket.chat/models';
-import { escapeRegExp } from '@rocket.chat/string-helpers';
 
 export const checkEmailAvailability = async function (email: string): Promise<boolean> {
 	return !(await Users.findOneByEmailAddress(email));
