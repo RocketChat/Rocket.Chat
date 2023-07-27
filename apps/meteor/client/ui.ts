e12ea8a6c59607ed127ec5c38b946b2e97c1553a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-07-27 01:02:16 -0300

    Migrate Start Call room action

--- apps/meteor/client/ui.ts
+++ apps/meteor/client/ui.ts
@@ -1,5 +1,8 @@
 import { useCallsRoomAction } from '../ee/client/hooks/roomActions/useCallsRoomAction';
 import { useAutotranslateRoomAction } from './hooks/roomActions/useAutotranslateRoomAction';
+import { useStartCallRoomAction } from './hooks/roomActions/useStartCallRoomAction';
 import type { ToolboxActionConfig } from './views/room/lib/Toolbox';
 
-export const roomActionHooks = [useAutotranslateRoomAction, useCallsRoomAction] satisfies (() => ToolboxActionConfig | undefined)[];
+export const roomActionHooks = [useAutotranslateRoomAction, useCallsRoomAction, useStartCallRoomAction] satisfies (() =>
+	| ToolboxActionConfig
+	| undefined)[];
