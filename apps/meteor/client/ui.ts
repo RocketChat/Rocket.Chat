19ef575f8ac5f552ec70daf49939d694e2b4f019
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-07-27 11:12:10 -0300

    refactor: Migrate some room actions to hooks (#29937)

--- apps/meteor/client/ui.ts
+++ apps/meteor/client/ui.ts
@@ -1,8 +1,18 @@
 import { useCallsRoomAction } from '../ee/client/hooks/roomActions/useCallsRoomAction';
+import { useCannedResponsesRoomAction } from '../ee/client/hooks/roomActions/useCannedResponsesRoomAction';
+import { useGameCenterRoomAction } from '../ee/client/hooks/roomActions/useGameCenterRoomAction';
 import { useAutotranslateRoomAction } from './hooks/roomActions/useAutotranslateRoomAction';
+import { useChannelSettingsRoomAction } from './hooks/roomActions/useChannelSettingsRoomAction';
+import { useCleanHistoryRoomAction } from './hooks/roomActions/useCleanHistoryRoomAction';
 import { useStartCallRoomAction } from './hooks/roomActions/useStartCallRoomAction';
 import type { ToolboxActionConfig } from './views/room/lib/Toolbox';
 
-export const roomActionHooks = [useAutotranslateRoomAction, useCallsRoomAction, useStartCallRoomAction] satisfies (() =>
-	| ToolboxActionConfig
-	| undefined)[];
+export const roomActionHooks = [
+	useAutotranslateRoomAction,
+	useCallsRoomAction,
+	useCannedResponsesRoomAction,
+	useChannelSettingsRoomAction,
+	useCleanHistoryRoomAction,
+	useGameCenterRoomAction,
+	useStartCallRoomAction,
+] satisfies (() => ToolboxActionConfig | undefined)[];
--- apps/meteor/ee/app/canned-responses/client/index.ts
+++ apps/meteor/ee/app/canned-responses/client/index.ts
@@ -1,2 +1,1 @@
-import './tabBar';
 import './startup/responses';
