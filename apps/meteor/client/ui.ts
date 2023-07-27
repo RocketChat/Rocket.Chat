d9d49162f1f79c24bef761f59ce2cf0c8395b692
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-07-27 02:14:51 -0300

    Migrate Canned Responses room action

--- apps/meteor/client/ui.ts
+++ apps/meteor/client/ui.ts
@@ -1,8 +1,12 @@
 import { useCallsRoomAction } from '../ee/client/hooks/roomActions/useCallsRoomAction';
+import { useCannedResponsesRoomAction } from '../ee/client/hooks/roomActions/useCannedResponsesRoomAction';
 import { useAutotranslateRoomAction } from './hooks/roomActions/useAutotranslateRoomAction';
 import { useStartCallRoomAction } from './hooks/roomActions/useStartCallRoomAction';
 import type { ToolboxActionConfig } from './views/room/lib/Toolbox';
 
-export const roomActionHooks = [useAutotranslateRoomAction, useCallsRoomAction, useStartCallRoomAction] satisfies (() =>
-	| ToolboxActionConfig
-	| undefined)[];
+export const roomActionHooks = [
+	useAutotranslateRoomAction,
+	useCallsRoomAction,
+	useCannedResponsesRoomAction,
+	useStartCallRoomAction,
+] satisfies (() => ToolboxActionConfig | undefined)[];
