d0d4cc90e42449d915efc07315c586d80791ef0b
デヴぁんす <sdevanshu90@yahoo.com>
2023-07-28 21:54:20 +0530

    initial draft of roles bridge

---
+++ apps/meteor/app/apps/server/bridges/roles.ts
@@ -0,0 +1,26 @@
+import type { IRole } from '@rocket.chat/core-typings';
+import { Roles } from '@rocket.chat/models';
+
+import type { AppServerOrchestrator } from '../../../../ee/server/apps/orchestrator';
+
+export class AppRolesBridge extends RoleBridge {
+	constructor(private readonly orch: AppServerOrchestrator) {
+		super();
+	}
+
+	protected async getOneByIdOrName(idOrName: IRole['_id'] | IRole['name'], appId: string): Promise<IRole | null> {
+		this.orch.debugLog(`The App ${appId} is getting the roleByIdOrName: "${idOrName}"`);
+
+		const role = await Roles.findOneByIdOrName(idOrName);
+		return role;
+	}
+
+	protected async getCustomRoles(appId: string): Promise<Array<IRole>> {
+		this.orch.debugLog(`The App ${appId} is getting the custom roles`);
+
+		const cursor = Roles.findCustomRoles();
+
+		const roles = await cursor.toArray();
+		return roles;
+	}
+}
