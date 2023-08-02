bc7f3ab5c189d9c215b412cbdce2b02d159158da
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-08-02 18:01:26 -0300

    PasswordVerifier

---
+++ packages/ui-client/src/components/PasswordVerifier/PasswordVerifiers.spec.tsx
@@ -0,0 +1,91 @@
+import { MockedServerContext } from '@rocket.chat/mock-providers/src/MockedServerContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, waitFor } from '@testing-library/react';
+
+import { PasswordVerifier } from './PasswordVerifier';
+
+jest.mock('react-i18next', () => ({
+	useTranslation: () => ({
+		t: (str: string) => str,
+		i18n: {
+			changeLanguage: () => new Promise(() => undefined),
+		},
+	}),
+}));
+
+beforeEach(() => {
+	queryClient.clear();
+});
+
+it('should render no policy if its disabled ', () => {
+	const { container } = render(
+		<MockedServerContext
+			handleRequest={(request) => {
+				if (request.method === 'GET' && request.pathPattern === '/v1/pw.getPolicy') {
+					return {
+						enabled: false,
+						policy: [],
+					} as any;
+				}
+				throw new Error('Not implemented');
+			}}
+		>
+			<PasswordVerifier password='' />
+		</MockedServerContext>,
+	);
+
+	expect(container.firstChild).toBeNull();
+});
+
+it('should render no policy if its enabled but empty', () => {
+	const { container } = render(
+		<MockedServerContext
+			handleRequest={(request) => {
+				if (request.method === 'GET' && request.pathPattern === '/v1/pw.getPolicy') {
+					return {
+						enabled: true,
+						policy: [],
+					} as any;
+				}
+				throw new Error('Not implemented');
+			}}
+		>
+			<PasswordVerifier password='' />
+		</MockedServerContext>,
+	);
+
+	expect(container.firstChild).toBeNull();
+});
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			// ✅ turns retries off
+			retry: false,
+		},
+	},
+});
+
+it('should render policy if its enabled and not empty', async () => {
+	const { getByText } = render(
+		<QueryClientProvider client={queryClient}>
+			<MockedServerContext
+				handleRequest={(request) => {
+					if (request.method === 'GET' && request.pathPattern === '/v1/pw.getPolicy') {
+						return {
+							enabled: true,
+							policy: [['get-password-policy-minLength', { minLength: 10 }]],
+						} as any;
+					}
+					throw new Error('Not implemented');
+				}}
+			>
+				<PasswordVerifier password='asd' />
+			</MockedServerContext>
+		</QueryClientProvider>,
+	);
+
+	await waitFor(() => {
+		expect(getByText('get-password-policy-minLength-label'));
+	});
+});
