3668dbd614db9f17f85d15053e65dba4225da698
dougfabris <devfabris@gmail.com>
2023-08-07 12:58:42 -0300

    chore: tweaks

--- apps/meteor/client/views/composer/EmojiPicker/EmojiPickerDesktopDropdown.tsx
+++ apps/meteor/client/views/composer/EmojiPicker/EmojiPickerDesktopDropdown.tsx
@@ -62,7 +62,7 @@ const EmojiPickerDesktopDropdown = forwardRef(function ToolboxDropdownDesktop(
 	const style = useDropdownPosition(reference, targetRef);
 
 	return (
-		<Tile is='ul' style={style} ref={mergedRef} elevation='2' pi='0' pb='0' display='flex' flexDirection='column' overflow='auto'>
+		<Tile style={style} ref={mergedRef} elevation='2' pi='0' pb='0' display='flex' flexDirection='column' overflow='auto'>
 			<Box flexShrink={1} pb={12}>
 				{children}
 			</Box>
