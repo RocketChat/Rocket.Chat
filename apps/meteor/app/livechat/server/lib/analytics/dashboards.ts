d707627103ec10d96dc3dc072183b04fba1a4c26
Kevin Aleman <kaleman960@gmail.com>
2023-08-09 09:17:59 -0600

    undef

--- apps/meteor/app/livechat/server/lib/analytics/dashboards.ts
+++ apps/meteor/app/livechat/server/lib/analytics/dashboards.ts
@@ -381,22 +381,22 @@ export const findAllResponseTimeMetricsAsync = async ({
 	};
 };
 
-export const findAllConversationsBySource = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult> => {
+export const findAllConversationsBySource = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult | undefined> => {
 	return (await LivechatRooms.getConversationsBySource(start, end).toArray())[0];
 };
 
-export const findAllConversationsByStatus = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult> => {
+export const findAllConversationsByStatus = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult | undefined> => {
 	return (await LivechatRooms.getConversationsByStatus(start, end).toArray())[0];
 };
 
-export const findAllConversationsByDepartment = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult> => {
+export const findAllConversationsByDepartment = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult | undefined> => {
 	return (await LivechatRooms.getConversationsByDepartment(start, end).toArray())[0];
 };
 
-export const findAllConversationsByTags = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult> => {
+export const findAllConversationsByTags = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult | undefined> => {
 	return (await LivechatRooms.getConversationsByTags(start, end).toArray())[0];
 };
 
-export const findAllConversationsByAgents = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult> => {
+export const findAllConversationsByAgents = async ({ start, end }: { start: Date; end: Date }): Promise<ReportResult | undefined> => {
 	return (await LivechatRooms.getConversationsByAgents(start, end).toArray())[0];
 };
