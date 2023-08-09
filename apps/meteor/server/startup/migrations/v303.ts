e0b828556dfcc95956390c9d6fe162cbe19ff1d7
Rafael Tapia <rafael.tapia@rocket.chat>
2023-08-09 14:07:26 -0300

    fix: use getNewUserRoles to handle the setting parsing

--- apps/meteor/server/startup/migrations/v303.ts
+++ apps/meteor/server/startup/migrations/v303.ts
@@ -1,20 +1,19 @@
 import { Users } from '@rocket.chat/models';
 
-import { settings } from '../../../app/settings/server';
-import { parseCSV } from '../../../lib/utils/parseCSV';
 import { addMigration } from '../../lib/migrations';
+import { getNewUserRoles } from '../../services/user/lib/getNewUserRoles';
 
 addMigration({
 	version: 303,
 	name: 'Add default roles to users without roles field',
 	async up() {
-		const defaultRoles = settings.get('Accounts_Registration_Users_Default_Roles');
+		const defaultRoles = getNewUserRoles();
 
 		await Users.updateMany(
 			{
 				roles: { $exists: false },
 			},
-			{ $set: { roles: defaultRoles ? parseCSV(String(defaultRoles)) : [] } },
+			{ $set: { roles: defaultRoles } },
 		);
 	},
 });
