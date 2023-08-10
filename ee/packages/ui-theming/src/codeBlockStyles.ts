efa88fa3f08e035cbbaaf174b4565806665864ee
juliajforesti <juliajforesti@gmail.com>
2023-08-10 16:58:36 -0300

    message codeBlock colors

---
+++ ee/packages/ui-theming/src/codeBlockStyles.ts
@@ -0,0 +1,74 @@
+export const codeBlock = `pre code.hljs {
+	display: block;
+	overflow-x: auto;
+	padding: 1em;
+}
+code.hljs {
+	padding: 3px 5px;
+}
+.hljs {
+	background: #1d1f21 !important !important;
+	color: #c5c8c6 !important;
+}
+.hljs span::selection,
+.hljs::selection {
+	background: #373b41 !important;
+}
+.hljs span::-moz-selection,
+.hljs::-moz-selection {
+	background: #373b41 !important;
+}
+.hljs-name,
+.hljs-title {
+	color: #f0c674 !important;
+}
+.hljs-comment,
+.hljs-meta,
+.hljs-meta .hljs-keyword {
+	color: #707880 !important;
+}
+.hljs-deletion,
+.hljs-link,
+.hljs-literal,
+.hljs-number,
+.hljs-symbol {
+	color: #c66 !important;
+}
+.hljs-addition,
+.hljs-doctag,
+.hljs-regexp,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-string {
+	color: #b5bd68 !important;
+}
+.hljs-attribute,
+.hljs-code,
+.hljs-selector-id {
+	color: #b294bb !important;
+}
+.hljs-bullet,
+.hljs-keyword,
+.hljs-selector-tag,
+.hljs-tag {
+	color: #81a2be !important;
+}
+.hljs-subst,
+.hljs-template-tag,
+.hljs-template-variable,
+.hljs-variable {
+	color: #8abeb7 !important;
+}
+.hljs-built_in,
+.hljs-quote,
+.hljs-section,
+.hljs-selector-class,
+.hljs-type {
+	color: #de935f !important;
+}
+.hljs-emphasis {
+	font-style: italic;
+}
+.hljs-strong {
+	font-weight: 700;
+}`;
