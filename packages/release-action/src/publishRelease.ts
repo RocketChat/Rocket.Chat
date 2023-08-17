a56f5f0a5fb90cdcb39c6e5e7a21f0ba539f1f32
Diego Sampaio <chinello@gmail.com>
2023-08-17 14:42:20 -0300

    chore(release): add engine versions to release (#30021)

--- packages/release-action/src/publishRelease.ts
+++ packages/release-action/src/publishRelease.ts
@@ -7,7 +7,7 @@ import * as core from '@actions/core';
 
 import { createNpmFile } from './createNpmFile';
 import { setupOctokit } from './setupOctokit';
-import { bumpFileVersions, createBumpFile, getChangelogEntry, readPackageJson } from './utils';
+import { bumpFileVersions, createBumpFile, getChangelogEntry, getEngineVersionsMd, readPackageJson } from './utils';
 import { fixWorkspaceVersionsBeforePublish } from './fixWorkspaceVersionsBeforePublish';
 import { checkoutBranch, commitChanges, createTag, getCurrentBranch, mergeBranch, pushChanges } from './gitUtils';
 
@@ -73,7 +73,7 @@ export async function publishRelease({
 		throw new Error('Could not find changelog entry for version newVersion');
 	}
 
-	const releaseBody = changelogEntry.content;
+	const releaseBody = (await getEngineVersionsMd(cwd)) + changelogEntry.content;
 
 	core.info('update version in all files to new');
 	await bumpFileVersions(cwd, currentVersion, newVersion);
