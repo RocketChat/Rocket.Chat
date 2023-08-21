bf96a9dc0c73dda7b18cff6a7d34c64a54449204
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-08-21 20:31:45 -0300

    Update Storybook configuration

--- packages/fuselage-ui-kit/.storybook/preview.tsx
+++ packages/fuselage-ui-kit/.storybook/preview.tsx
@@ -1,10 +1,26 @@
-import { DocsPage, DocsContainer } from '@storybook/addon-docs';
-import { addParameters } from '@storybook/react';
+import { codeBlock } from '@rocket.chat/ui-theming/src/codeBlockStyles';
+import { convertToCss } from '@rocket.chat/ui-theming/src/helpers/convertToCss';
+import { filterOnlyChangedColors } from '@rocket.chat/ui-theming/src/helpers/filterOnlyChangedColors';
+import { useCreateStyleContainer } from '@rocket.chat/ui-theming/src/hooks/useCreateStyleContainer';
+import { defaultPalette } from '@rocket.chat/ui-theming/src/palette';
+import { darkPalette } from '@rocket.chat/ui-theming/src/paletteDark';
+import { type Parameters } from '@storybook/addons';
+import { type DecoratorFn } from '@storybook/react';
+import { themes } from '@storybook/theming';
+import { createElement } from 'react';
+import { createPortal } from 'react-dom';
+import { useDarkMode } from 'storybook-dark-mode';
+
+import manifest from '../package.json';
+import logo from './logo.svg';
+
+import '@rocket.chat/fuselage/dist/fuselage.css';
 import '@rocket.chat/icons/dist/rocketchat.css';
 import '@rocket.chat/fuselage-polyfills';
 import 'normalize.css/normalize.css';
 
-addParameters({
+export const parameters: Parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: {
     grid: {
       cellSize: 4,
@@ -12,11 +28,44 @@ addParameters({
       opacity: 0.5,
     },
   },
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
-  },
   options: {
     storySort: ([, a], [, b]) => a.kind.localeCompare(b.kind),
   },
-});
+  layout: 'fullscreen',
+  darkMode: {
+    dark: {
+      ...themes.dark,
+      brandTitle: manifest.name,
+      brandImage: logo,
+      brandUrl: manifest.homepage,
+    },
+    light: {
+      ...themes.normal,
+      brandTitle: manifest.name,
+      brandImage: logo,
+      brandUrl: manifest.homepage,
+    },
+  },
+};
+
+export const decorators: DecoratorFn[] = [
+  (fn) =>
+    createElement(function RocketChatDarkMode() {
+      const dark = useDarkMode();
+
+      const palette = convertToCss(
+        filterOnlyChangedColors(defaultPalette, dark ? darkPalette : {}),
+        'body'
+      );
+
+      return (
+        <>
+          {createPortal(
+            dark ? palette + codeBlock : palette,
+            useCreateStyleContainer('main-palette')
+          )}
+          {fn()}
+        </>
+      );
+    }),
+];
