3599f70ab8fb4e585b11b2849b838f933432375c
juliajforesti <juliajforesti@gmail.com>
2023-08-25 19:07:59 -0300

    wip PasswordVerifierItem checkbox SR

--- packages/ui-client/src/components/PasswordVerifier/PasswordVerifierItem.tsx
+++ packages/ui-client/src/components/PasswordVerifier/PasswordVerifierItem.tsx
@@ -1,4 +1,5 @@
 import { Box, Icon } from '@rocket.chat/fuselage';
+import { useUniqueId } from '@rocket.chat/fuselage-hooks';
 import { AllHTMLAttributes } from 'react';
 
 export const PasswordVerifierItem = ({
@@ -10,9 +11,16 @@ export const PasswordVerifierItem = ({
 	text: string;
 	color: 'status-font-on-success' | 'status-font-on-danger';
 	icon: 'success-circle' | 'error-circle';
-} & Omit<AllHTMLAttributes<HTMLElement>, 'is'>) => (
-	<Box display='flex' flexBasis='50%' alignItems='center' mbe={8} fontScale='c1' color={color} role='listitem' {...props}>
-		<Icon name={icon} color={color} size='x16' mie={4} />
-		{text}
-	</Box>
-);
+} & Omit<AllHTMLAttributes<HTMLElement>, 'is'>) => {
+	const success = icon === 'success-circle';
+	const id = useUniqueId();
+	return (
+		<Box display='flex' flexBasis='50%' alignItems='center' mbe={8} fontScale='c1' color={color} role='listitem' {...props}>
+			<Icon name={icon} color={color} size='x16' mie={4} />
+			<span role='checkbox' aria-checked={success ? 'true' : 'false'} tabIndex={0} aria-labelledby={id} aria-readonly={true}></span>
+			<label id={id} aria-hidden>
+				{text}
+			</label>
+		</Box>
+	);
+};
