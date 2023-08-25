cb98d3c57b52de7d2976a771abf7559ededbc31a
juliajforesti <juliajforesti@gmail.com>
2023-08-25 19:47:24 -0300

    chore: PasswordVerifierItem SR

--- packages/ui-client/src/components/PasswordVerifier/PasswordVerifierItem.tsx
+++ packages/ui-client/src/components/PasswordVerifier/PasswordVerifierItem.tsx
@@ -1,5 +1,4 @@
 import { Box, Icon } from '@rocket.chat/fuselage';
-import { useUniqueId } from '@rocket.chat/fuselage-hooks';
 import { AllHTMLAttributes } from 'react';
 
 export const PasswordVerifierItem = ({
@@ -13,14 +12,12 @@ export const PasswordVerifierItem = ({
 	icon: 'success-circle' | 'error-circle';
 } & Omit<AllHTMLAttributes<HTMLElement>, 'is'>) => {
 	const success = icon === 'success-circle';
-	const id = useUniqueId();
+
 	return (
 		<Box display='flex' flexBasis='50%' alignItems='center' mbe={8} fontScale='c1' color={color} role='listitem' {...props}>
 			<Icon name={icon} color={color} size='x16' mie={4} />
-			<span role='checkbox' aria-checked={success ? 'true' : 'false'} tabIndex={0} aria-labelledby={id} aria-readonly={true}></span>
-			<label id={id} aria-hidden>
-				{text}
-			</label>
+			<span aria-label={`${text} ${success ? 'checked' : 'unchecked'}`} />
+			<span aria-hidden>{text}</span>
 		</Box>
 	);
 };
