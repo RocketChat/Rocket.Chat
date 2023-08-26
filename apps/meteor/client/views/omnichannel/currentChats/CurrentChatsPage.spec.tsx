3959d131d0eda0102b1b98a335b35e7ca88ad712
Martin Schoeler <martin.schoeler@rocket.chat>
2023-08-26 01:09:54 -0300

    chore: swap flaky current chats e2e tests for unit tests (#30182)

--- apps/meteor/client/views/omnichannel/currentChats/CurrentChatsPage.spec.tsx
+++ apps/meteor/client/views/omnichannel/currentChats/CurrentChatsPage.spec.tsx
@@ -3,15 +3,13 @@ import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
-import OmnichannelPreferencesPage from './OmnichannelPreferencesPage';
+import CurrentChatsPage from './CurrentChatsPage';
 
 expect.extend(toHaveNoViolations);
 
 it('should have no a11y violations', async () => {
-	const { container } = render(<OmnichannelPreferencesPage />, {
-		wrapper: mockAppRoot()
-			.withMethod('license:getModules', () => ['livechat-enterprise'])
-			.build(),
+	const { container } = render(<CurrentChatsPage onRowClick={() => undefined} />, {
+		wrapper: mockAppRoot().build(),
 	});
 
 	const results = await axe(container);
