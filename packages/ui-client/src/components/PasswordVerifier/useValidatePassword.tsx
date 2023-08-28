8736ef9ec0467c231cb0561fb34db18550775cf9
juliajforesti <juliajforesti@gmail.com>
2023-08-28 20:36:07 -0300

    chore: add translation

--- packages/ui-client/src/components/PasswordVerifier/useValidatePassword.tsx
+++ packages/ui-client/src/components/PasswordVerifier/useValidatePassword.tsx
@@ -1,6 +1,8 @@
-import { useVerifyPassword } from '@rocket.chat/ui-contexts';
+import { useTranslation, useVerifyPassword } from '@rocket.chat/ui-contexts';
 
 export const useValidatePassword = (password: string) => {
+	const t = useTranslation();
+
 	const passwordVerifications = useVerifyPassword(password);
-	return () => (passwordVerifications.every(({ isValid }) => isValid) ? undefined : 'Password must meet the complexity requirements.');
+	return () => (passwordVerifications.every(({ isValid }) => isValid) ? undefined : t('Password_must_meet_the_complexity_requirements'));
 };
