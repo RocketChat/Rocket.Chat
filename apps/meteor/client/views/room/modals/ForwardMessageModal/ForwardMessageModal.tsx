a8af6e3a18385921e17fcfbbd4bd64f8cdfa1694
Guilherme Jun Grillo <48109548+guijun13@users.noreply.github.com>
2023-08-29 15:37:00 -0300

    Update ForwardMessageModal.tsx
    Co-authored-by: Douglas Fabris <devfabris@gmail.com>

--- apps/meteor/client/views/room/modals/ForwardMessageModal/ForwardMessageModal.tsx
+++ apps/meteor/client/views/room/modals/ForwardMessageModal/ForwardMessageModal.tsx
@@ -7,7 +7,7 @@ import type { ReactElement } from 'react';
 import React, { memo } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 
-import UserAndRoomAutoCompleteMultiple from '../../../../components/UserAndRoomAutoCompleteMultiple/UserAndRoomAutoCompleteMultiple';
+import UserAndRoomAutoCompleteMultiple from '../../../../components/UserAndRoomAutoCompleteMultiple';
 import { QuoteAttachment } from '../../../../components/message/content/attachments/QuoteAttachment';
 import { useUserDisplayName } from '../../../../hooks/useUserDisplayName';
 import { prependReplies } from '../../../../lib/utils/prependReplies';
