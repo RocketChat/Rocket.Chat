b1db1dbbd8331987ac1fdbd96a20a196725c91b6
juliajforesti <juliajforesti@gmail.com>
2023-08-30 17:02:15 -0300

    chore: stories with new mock builder

--- packages/ui-client/src/components/PasswordVerifier/PasswordVerifier.stories.tsx
+++ packages/ui-client/src/components/PasswordVerifier/PasswordVerifier.stories.tsx
@@ -1,39 +1,56 @@
-import { MockedServerContext } from '@rocket.chat/mock-providers/src/MockedServerContext';
+import { mockAppRoot } from '@rocket.chat/mock-providers';
+// import { MockedServerContext } from '@rocket.chat/mock-providers/src/MockedServerContext';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { PasswordVerifier } from './PasswordVerifier';
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			// ✅ turns retries off
-			retry: false,
-		},
-	},
-});
+type Response = {
+	enabled: boolean;
+	policy: [
+		name: string,
+		value?:
+			| {
+					[x: string]: number;
+			  }
+			| undefined,
+	][];
+};
+
+// const queryClient = new QueryClient({
+// 	defaultOptions: {
+// 		queries: {
+// 			// ✅ turns retries off
+// 			retry: false,
+// 		},
+// 	},
+// });
 
 export default {
 	title: 'Components/PasswordVerifier',
 	component: PasswordVerifier,
 } as ComponentMeta<typeof PasswordVerifier>;
 
+const response: Response = {
+	enabled: true,
+	policy: [
+		['get-password-policy-minLength', { minLength: 10 }],
+		['get-password-policy-forbidRepeatingCharactersCount', { maxRepeatingChars: 3 }],
+		['get-password-policy-mustContainAtLeastOneLowercase'],
+		['get-password-policy-mustContainAtLeastOneUppercase'],
+		['get-password-policy-mustContainAtLeastOneNumber'],
+		['get-password-policy-mustContainAtLeastOneSpecialCharacter'],
+	],
+};
+
+const Wrapper = mockAppRoot()
+	.withEndpoint('GET', '/v1/pw.getPolicy', () => response)
+	.build();
+
 export const Default: ComponentStory<typeof PasswordVerifier> = (args) => (
-	<QueryClientProvider client={queryClient}>
-		<MockedServerContext
-			handleRequest={(request) => {
-				if (request.method === 'GET' && request.pathPattern === '/v1/pw.getPolicy') {
-					return {
-						enabled: true,
-						policy: [['get-password-policy-minLength', { minLength: 10 }]],
-					} as any;
-				}
-				throw new Error('Not implemented');
-			}}
-		>
-			<PasswordVerifier {...args} />
-		</MockedServerContext>
-	</QueryClientProvider>
+	<Wrapper>
+		<PasswordVerifier {...args} />
+	</Wrapper>
 );
 
 Default.storyName = 'PasswordVerifier';
