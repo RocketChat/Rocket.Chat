b06764f10f89806e2466f11c799b60e0cd0447fa
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-08-30 19:00:52 -0300

    create users table for pending invites tab

--- apps/meteor/client/views/admin/users/PendingInvites/PendingInvitesTable.tsx
+++ apps/meteor/client/views/admin/users/PendingInvites/PendingInvitesTable.tsx
@@ -1,4 +1,5 @@
-import { IUser } from '@rocket.chat/core-typings';
+import { type IRole, type IUser, UserStatus as Status } from '@rocket.chat/core-typings';
+import { Box, Pagination } from '@rocket.chat/fuselage';
 import { useMediaQuery, useDebouncedValue } from '@rocket.chat/fuselage-hooks';
 import type { OptionProp } from '@rocket.chat/ui-client';
 import { useEndpoint, useToastMessageDispatch, useRouter, useTranslation } from '@rocket.chat/ui-contexts';
@@ -7,9 +8,23 @@ import { escapeRegExp } from 'lodash';
 import type { CSSProperties, MutableRefObject, ReactElement } from 'react';
 import React, { useState, useRef, useMemo, useEffect, useCallback } from 'react';
 
-import { GenericTableHeaderCell } from '../../../../components/GenericTable';
+import { Roles } from '../../../../../app/models/client';
+import FilterByText from '../../../../components/FilterByText';
+import GenericNoResults from '../../../../components/GenericNoResults';
+import {
+	GenericTable,
+	GenericTableBody,
+	GenericTableCell,
+	GenericTableHeader,
+	GenericTableHeaderCell,
+	GenericTableLoadingTable,
+	GenericTableRow,
+} from '../../../../components/GenericTable';
 import { usePagination } from '../../../../components/GenericTable/hooks/usePagination';
 import { useSort } from '../../../../components/GenericTable/hooks/useSort';
+import { UserStatus } from '../../../../components/UserStatus';
+import UserAvatar from '../../../../components/avatar/UserAvatar';
+import UsersTableRow from '../UsersTable/UsersTableRow';
 
 const style: CSSProperties = { whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' };
 
@@ -69,7 +84,7 @@ export const PendingInvitesTable = ({ reload }: { reload: MutableRefObject<() =>
 
 	const dispatchToastMessage = useToastMessageDispatch();
 
-	const { data, refetch, isSuccess, isLoading, isError } = useQuery(
+	const { data, refetch, isSuccess, isLoading, error } = useQuery(
 		['users', query],
 		async () => {
 			const users = await getUsers(query);
@@ -105,47 +120,119 @@ export const PendingInvitesTable = ({ reload }: { reload: MutableRefObject<() =>
 	);
 
 	const headers = useMemo(
-		() => [
-			<GenericTableHeaderCell w='x200' key='name' direction={sortDirection} active={sortBy === 'name'} onClick={setSort} sort='name'>
-				{t('Name')}
-			</GenericTableHeaderCell>,
-			mediaQuery && (
+		() =>
+			[
+				<GenericTableHeaderCell w='x200' key='name' direction={sortDirection} active={sortBy === 'name'} onClick={setSort} sort='name'>
+					{t('Name')}
+				</GenericTableHeaderCell>,
+				mediaQuery && (
+					<GenericTableHeaderCell
+						w='x140'
+						key='username'
+						direction={sortDirection}
+						active={sortBy === 'username'}
+						onClick={setSort}
+						sort='username'
+					>
+						{t('Username')}
+					</GenericTableHeaderCell>
+				),
 				<GenericTableHeaderCell
-					w='x140'
-					key='username'
+					w='x120'
+					key='email'
 					direction={sortDirection}
-					active={sortBy === 'username'}
+					active={sortBy === 'emails.address'}
 					onClick={setSort}
-					sort='username'
+					sort='emails.address'
 				>
-					{t('Username')}
-				</GenericTableHeaderCell>
-			),
-			<GenericTableHeaderCell
-				w='x120'
-				key='email'
-				direction={sortDirection}
-				active={sortBy === 'emails.address'}
-				onClick={setSort}
-				sort='emails.address'
-			>
-				{t('Email')}
-			</GenericTableHeaderCell>,
-			mediaQuery && (
-				<GenericTableHeaderCell w='x120' key='roles' onClick={setSort}>
-					{t('Roles')}
-				</GenericTableHeaderCell>
-			),
-			<GenericTableHeaderCell w='x100' key='action' direction={sortDirection} active={sortBy === 'action'} onClick={setSort} sort='action'>
-				{t('Pending_action')}
-			</GenericTableHeaderCell>,
-		],
+					{t('Email')}
+				</GenericTableHeaderCell>,
+				mediaQuery && (
+					<GenericTableHeaderCell w='x120' key='roles' onClick={setSort}>
+						{t('Roles')}
+					</GenericTableHeaderCell>
+				),
+				<GenericTableHeaderCell
+					w='x100'
+					key='action'
+					direction={sortDirection}
+					active={sortBy === 'action'}
+					onClick={setSort}
+					sort='action'
+				>
+					{t('Pending_action')}
+				</GenericTableHeaderCell>,
+			].filter(Boolean),
 		[mediaQuery, setSort, sortBy, sortDirection, t],
 	);
 
+	if (error) {
+		throw error;
+	}
+
+	const renderRow = useCallback(
+		(user: IUser) => {
+			const { _id, emails, username, name, status, roles, avatarETag } = user;
+
+			// TODO: create a function to get user pending actions!
+			const pendingAction = getPendingAction(user);
+
+			const roleNames = (roles || [])
+				.map((roleId) => (Roles.findOne(roleId, { fields: { name: 1 } }) as IRole | undefined)?.name)
+				.filter((roleName): roleName is string => !!roleName)
+				.join(', ');
+
+			return (
+				<GenericTableRow action key={_id} onKeyDown={onClick(_id)} onClick={onClick(_id)} tabIndex={0} role='link' qa-room-id={_id}>
+					<GenericTableCell withTruncatedText>
+						<Box display='flex' alignItems='center'>
+							{username && <UserAvatar size={mediaQuery ? 'x28' : 'x40'} username={username} etag={avatarETag} />}
+							<Box display='flex' mi={8} withTruncatedText>
+								<Box display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
+									<Box fontScale='p2m' color='default' withTruncatedText>
+										<Box display='inline' mie='x8'>
+											<UserStatus status={status || Status.OFFLINE} />
+										</Box>
+										{name || username}
+									</Box>
+									{!mediaQuery && name && (
+										<Box fontScale='p2' color='hint' withTruncatedText>
+											{`@${username}`}
+										</Box>
+									)}
+								</Box>
+							</Box>
+						</Box>
+					</GenericTableCell>
+					{mediaQuery && (
+						<GenericTableCell>
+							<Box fontScale='p2m' color='hint' withTruncatedText>
+								{username}
+							</Box>
+							<Box mi={4} />
+						</GenericTableCell>
+					)}
+
+					<GenericTableCell withTruncatedText>{emails?.length && emails[0].address}</GenericTableCell>
+
+					{mediaQuery && <GenericTableCell withTruncatedText>{roleNames}</GenericTableCell>}
+
+					<GenericTableCell fontScale='p2' color='hint' withTruncatedText>
+						{pendingAction}
+					</GenericTableCell>
+				</GenericTableRow>
+			);
+		},
+		[mediaQuery, onClick],
+	);
+
+	// TODO: create hook useFilteredUserRoles and PendingInvitesTableFilters
+	const usersList = useFilteredUserRoles(rolesFilters.roles, isLoading, data?.users);
+
 	return (
 		<>
-			<FilterByText autoFocus placeholder={t('Search_Users')} onChange={({ text }): void => setText(text)} />
+			<PendingInvitesTableFilters setFilters={setRolesFilters} />
+
 			{isLoading && (
 				<GenericTable>
 					<GenericTableHeader>{headers}</GenericTableHeader>
@@ -158,7 +245,7 @@ export const PendingInvitesTable = ({ reload }: { reload: MutableRefObject<() =>
 						<GenericTableHeader>{headers}</GenericTableHeader>
 						<GenericTableBody>
 							{data?.users.map((user) => (
-								<UsersTableRow key={user._id} onClick={handleClick} mediaQuery={mediaQuery} user={user} />
+								<UsersTableRow key={user._id} onClick={onClick} mediaQuery={mediaQuery} user={user} />
 							))}
 						</GenericTableBody>
 					</GenericTable>
