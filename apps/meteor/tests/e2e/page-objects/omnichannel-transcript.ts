c7ee7e24f263b6ebb218c9d33570bad2f54c6f2a
rocketchat <rocketchat@rocketchats-MacBook-Pro.local>
2023-08-31 23:19:40 +0530

    disabled button

--- apps/meteor/tests/e2e/page-objects/omnichannel-transcript.ts
+++ apps/meteor/tests/e2e/page-objects/omnichannel-transcript.ts
@@ -3,44 +3,44 @@ import type { Locator, Page } from '@playwright/test';
 import { OmnichannelSidenav } from './fragments';
 
 export class OmnichannelTranscript {
-    private readonly page: Page;
+	private readonly page: Page;
 
-    readonly sidenav: OmnichannelSidenav;
+	readonly sidenav: OmnichannelSidenav;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.sidenav = new OmnichannelSidenav(page);
-    }
+	constructor(page: Page) {
+		this.page = page;
+		this.sidenav = new OmnichannelSidenav(page);
+	}
 
-    get checkboxPDF(): Locator {
-        return this.page.locator('//input[@name="transcriptPDF"]//following::i[1]');
-    }
+	get checkboxPDF(): Locator {
+		return this.page.locator('//input[@name="transcriptPDF"]//following::i[1]');
+	}
 
-    get exportedPDF(): Locator {
-        return this.page.locator('//div[contains(text(),"PDF Transcript successfully generated")]');
-    }
+	get exportedPDF(): Locator {
+		return this.page.locator('//div[contains(text(),"PDF Transcript successfully generated")]');
+	}
 
-    get contactCenter(): Locator {
-        return this.page.locator('//button[@data-tooltip="Contact Center"]');
-    }
+	get contactCenter(): Locator {
+		return this.page.locator('//button[@data-tooltip="Contact Center"]');
+	}
 
-    get contactCenterChats(): Locator {
-        return this.page.locator('//button[contains(.,"Chats")]');
-    }
+	get contactCenterChats(): Locator {
+		return this.page.locator('//button[contains(.,"Chats")]');
+	}
 
-    get contactCenterSearch(): Locator {
-        return this.page.locator('[placeholder="Search"]');
-    }
+	get contactCenterSearch(): Locator {
+		return this.page.locator('[placeholder="Search"]');
+	}
 
-    get firstRow(): Locator {
-        return this.page.locator('//tr[1]//td[1]');
-    }
+	get firstRow(): Locator {
+		return this.page.locator('//tr[1]//td[1]');
+	}
 
-    get viewFullConversation(): Locator {
-        return this.page.locator('//button[@title="View full conversation"]/i');
-    }
+	get viewFullConversation(): Locator {
+		return this.page.locator('//button[@title="View full conversation"]/i');
+	}
 
-    get DownloadedPDF(): Locator {
-        return this.page.locator('[data-qa-type="attachment-title-link"]').last();
-    }
+	get DownloadedPDF(): Locator {
+		return this.page.locator('[data-qa-type="attachment-title-link"]').last();
+	}
 }
