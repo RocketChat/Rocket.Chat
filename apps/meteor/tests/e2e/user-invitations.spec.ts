6d67ed45f00afa977c7000f882b9609900899666
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-09-05 12:54:54 -0300

    ..

--- apps/meteor/tests/e2e/user-invitations.spec.ts
+++ apps/meteor/tests/e2e/user-invitations.spec.ts
@@ -7,13 +7,11 @@ test.use({ storageState: Users.admin.state });
 test.describe.serial('user-invites', () => {
 	let poAdmin: Admin;
 
-	test.beforeEach(async ({ page }) => {
+	test('expect SMTP setup warning and routing to email settings', async ({ page }) => {
+		const response = page.waitForResponse((response) => response.url().includes('/api/v1/user.list'));
 		poAdmin = new Admin(page);
 		await page.goto('/admin/users/invite');
-	});
-
-	test('expect SMTP setup warning and routing to email settings', async ({ page }) => {
-		await page.waitForResponse((response) => response.url().includes('/api/v1/user.list'));
+		await response;
 		await expect(poAdmin.tabs.users.setupSmtpLink).toBeVisible();
 		await poAdmin.tabs.users.setupSmtpLink.click();
 		await expect(page).toHaveURL('/admin/settings/Email');
