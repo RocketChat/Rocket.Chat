6b2ad46352622813ee276e1e0624f43a26620c15
Hugo Costa <hugocarreiracosta@gmail.com>
2023-09-08 13:55:26 -0300

    fixing storybook story

--- packages/ui-client/src/components/PasswordVerifier/PasswordVerifier.stories.tsx
+++ packages/ui-client/src/components/PasswordVerifier/PasswordVerifier.stories.tsx
@@ -3,46 +3,25 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { PasswordVerifier } from './PasswordVerifier';
 
-type Response = {
-	enabled: boolean;
-	policy: [
-		name: string,
-		value?:
-			| {
-					[x: string]: number;
-			  }
-			| undefined,
-	][];
-};
-
 export default {
 	title: 'Components/PasswordVerifier',
 	component: PasswordVerifier,
+	decorators: [
+		mockAppRoot()
+			.withSetting('Accounts_Password_Policy_Enabled', 'true')
+			.withSetting('Accounts_Password_Policy_MinLength', '6')
+			.withSetting('Accounts_Password_Policy_MaxLength', '24')
+			.withSetting('Accounts_Password_Policy_ForbidRepeatingCharacters', 'true')
+			.withSetting('Accounts_Password_Policy_ForbidRepeatingCharactersCount', '3')
+			.withSetting('Accounts_Password_Policy_AtLeastOneLowercase', 'true')
+			.withSetting('Accounts_Password_Policy_AtLeastOneUppercase', 'true')
+			.withSetting('Accounts_Password_Policy_AtLeastOneNumber', 'true')
+			.withSetting('Accounts_Password_Policy_AtLeastOneSpecialCharacter', 'true')
+			.buildStoryDecorator(),
+	],
+	args: {
+		password: '123',
+	},
 } as ComponentMeta<typeof PasswordVerifier>;
 
-const response: Response = {
-	enabled: true,
-	policy: [
-		['get-password-policy-minLength', { minLength: 10 }],
-		['get-password-policy-forbidRepeatingCharactersCount', { maxRepeatingChars: 3 }],
-		['get-password-policy-mustContainAtLeastOneLowercase'],
-		['get-password-policy-mustContainAtLeastOneUppercase'],
-		['get-password-policy-mustContainAtLeastOneNumber'],
-		['get-password-policy-mustContainAtLeastOneSpecialCharacter'],
-	],
-};
-
-const Wrapper = mockAppRoot()
-	.withEndpoint('GET', '/v1/pw.getPolicy', () => response)
-	.build();
-
-export const Default: ComponentStory<typeof PasswordVerifier> = (args) => (
-	<Wrapper>
-		<PasswordVerifier {...args} />
-	</Wrapper>
-);
-
-Default.storyName = 'PasswordVerifier';
-Default.args = {
-	password: 'asd',
-};
+export const Default: ComponentStory<typeof PasswordVerifier> = (args) => <PasswordVerifier {...args} />;
