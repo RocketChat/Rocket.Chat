f6277d625151398e41c9d47c0dd5d7aadd3fe989
Hugo Costa <hugocarreiracosta@gmail.com>
2023-09-08 17:10:36 -0300

    replacing Card component to naming imports

--- packages/ui-client/src/components/Card/index.ts
+++ packages/ui-client/src/components/Card/index.ts
@@ -1,54 +1,10 @@
-import Card from './Card';
-import CardBody from './CardBody';
-import CardCol from './CardCol';
-import CardColSection from './CardColSection';
-import CardColTitle from './CardColTitle';
-import CardDivider from './CardDivider';
-import CardFooter from './CardFooter';
-import CardFooterWrapper from './CardFooterWrapper';
-import CardIcon from './CardIcon';
-import CardTitle from './CardTitle';
-
-export const DOUBLE_COLUMN_CARD_WIDTH = 552;
-
-export { Card, CardBody, CardCol, CardColSection, CardColTitle, CardDivider, CardFooter, CardFooterWrapper, CardIcon, CardTitle };
-
-export default Object.assign(Card, {
-	/**
-	 * @deprecated Use named import `CardTitle` instead
-	 */
-	Title: CardTitle,
-	/**
-	 * @deprecated Use named import `CardBody` instead
-	 */
-	Body: CardBody,
-	/**
-	 * @deprecated Use named import `CardCol` instead
-	 */
-	Col: Object.assign(CardCol, {
-		/**
-		 * @deprecated Use named import `CardColTitle` instead
-		 */
-		Title: CardColTitle,
-		/**
-		 * @deprecated Use named import `CardColSection` instead
-		 */
-		Section: CardColSection,
-	}),
-	/**
-	 * @deprecated Use named import `CardFooter` instead
-	 */
-	Footer: CardFooter,
-	/**
-	 * @deprecated Use named import `CardFooterWrapper` instead
-	 */
-	FooterWrapper: CardFooterWrapper,
-	/**
-	 * @deprecated Use named import `CardDivider` instead
-	 */
-	Divider: CardDivider,
-	/**
-	 * @deprecated Use named import `CardIcon` instead
-	 */
-	Icon: CardIcon,
-});
+export { default as Card } from './Card';
+export { default as CardBody } from './CardBody';
+export { default as CardCol } from './CardCol';
+export { default as CardColSection } from './CardColSection';
+export { default as CardColTitle } from './CardColTitle';
+export { default as CardDivider } from './CardDivider';
+export { default as CardFooter } from './CardFooter';
+export { default as CardFooterWrapper } from './CardFooterWrapper';
+export { default as CardIcon } from './CardIcon';
+export { default as CardTitle } from './CardTitle';
