e1b980d94de414b5f181ad6fab04fa950923c457
Hugo Costa <hugocarreiracosta@gmail.com>
2023-09-08 11:16:39 -0300

    fixing error return

--- packages/password-policies/src/PasswordPolicyError.ts
+++ packages/password-policies/src/PasswordPolicyError.ts
@@ -1,11 +1,11 @@
 export class PasswordPolicyError extends Error {
 	public error: string;
 
-	public reasons?: { error: string; message: string }[] | undefined;
+	public details?: { error: string; message: string }[] | undefined;
 
-	constructor(message: string, error: string, reasons?: { error: string; message: string }[]) {
+	constructor(message: string, error: string, details?: { error: string; message: string }[]) {
 		super(message);
 		this.error = error;
-		this.reasons = reasons;
+		this.details = details;
 	}
 }
