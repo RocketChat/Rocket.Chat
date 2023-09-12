4e6908cb9b375c8ab0a46878eebf77ef8ec28f10
Kevin Aleman <kaleman960@gmail.com>
2023-09-12 14:18:15 -0600

    Add flag to disable teams mention via troubleshoot

--- apps/meteor/app/mentions/server/getMentionedTeamMembers.ts
+++ apps/meteor/app/mentions/server/getMentionedTeamMembers.ts
@@ -2,6 +2,7 @@ import { Team } from '@rocket.chat/core-services';
 import type { IMessage } from '@rocket.chat/core-typings';
 
 import { callbacks } from '../../../lib/callbacks';
+import { settings } from '../../settings/server';
 
 interface IExtraDataForNotification {
 	userMentions: any[];
@@ -9,7 +10,7 @@ interface IExtraDataForNotification {
 	message: IMessage;
 }
 
-callbacks.add('beforeGetMentions', async (mentionIds: string[], extra?: IExtraDataForNotification) => {
+const beforeGetMentions = async (mentionIds: string[], extra?: IExtraDataForNotification) => {
 	const { otherMentions } = extra ?? {};
 
 	const teamIds = otherMentions?.filter(({ type }) => type === 'team').map(({ _id }) => _id);
@@ -22,4 +23,13 @@ callbacks.add('beforeGetMentions', async (mentionIds: string[], extra?: IExtraDa
 	mentionIds.push(...new Set(members.map(({ userId }) => userId).filter((userId) => !mentionIds.includes(userId))));
 
 	return mentionIds;
+};
+
+settings.watch<boolean>('Troubleshoot_Disable_Teams_Mention', (value) => {
+	if (value) {
+		callbacks.remove('beforeGetMentions', 'before-get-mentions-get-teams');
+		return;
+	}
+
+	callbacks.add('beforeGetMentions', beforeGetMentions, callbacks.priority.MEDIUM, 'before-get-mentions-get-teams');
 });
