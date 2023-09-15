1ced0c5fe1d2c95d5fa7e775b8e42f16b76d664c
gabriellsh <gabriel.henriques@rocket.chat>
2023-09-15 15:58:49 -0300

    POC

--- apps/meteor/server/main.ts
+++ apps/meteor/server/main.ts
@@ -75,6 +75,7 @@ import './publications/spotlight';
 import './publications/subscription';
 import './routes/avatar';
 import './routes/health';
+import './routes/iframeLogin';
 import './routes/i18n';
 import './routes/timesync';
 import './routes/userDataDownload';
