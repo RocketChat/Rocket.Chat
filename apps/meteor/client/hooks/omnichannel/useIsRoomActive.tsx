e6784d372cd144e4278d6832a4597a47aaa713a0
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-09-15 10:19:43 -0300

    fix: added default value to activity

--- apps/meteor/client/hooks/omnichannel/useIsRoomActive.tsx
+++ apps/meteor/client/hooks/omnichannel/useIsRoomActive.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 
 export const useIsRoomActive = (room: IOmnichannelRoom) => {
 	// @ts-ignore
-	const { activity } = room.v; // TODO: add activity to IOmnichannelRoom['v']
+	const { activity = [] } = room.v; // TODO: add activity to IOmnichannelRoom['v']
 	const isContactActive = useMemo(() => {
 		const date = new Date();
 		const currentPeriod = `${date.getFullYear()}-${date.getMonth() + 1}`;
