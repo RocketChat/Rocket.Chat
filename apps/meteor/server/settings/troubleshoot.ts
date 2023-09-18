20bd4451963b55bac6321acb097713176bb89408
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-09-18 11:50:33 -0300

    Troubleshoot_Disable_Statistics_Generator

--- apps/meteor/server/settings/troubleshoot.ts
+++ apps/meteor/server/settings/troubleshoot.ts
@@ -19,7 +19,6 @@ export const createTroubleshootSettings = () =>
 			i18nDescription: 'Troubleshoot_Disable_Presence_Broadcast_Alert',
 			enableQuery: { _id: 'Presence_broadcast_disabled', value: false },
 		});
-
 		await this.add('Troubleshoot_Disable_Instance_Broadcast', false, {
 			type: 'boolean',
 			i18nDescription: 'Troubleshoot_Disable_Instance_Broadcast_Alert',
@@ -32,18 +31,16 @@ export const createTroubleshootSettings = () =>
 			type: 'boolean',
 			i18nDescription: 'Troubleshoot_Disable_Livechat_Activity_Monitor_Alert',
 		});
-		await this.add('Troubleshoot_Disable_Statistics_Generator', false, {
-			type: 'boolean',
-			i18nDescription: 'Troubleshoot_Disable_Statistics_Generator_Alert',
-		});
 		await this.add('Troubleshoot_Disable_Data_Exporter_Processor', false, {
 			type: 'boolean',
 			i18nDescription: 'Troubleshoot_Disable_Data_Exporter_Processor_Alert',
 		});
-		await this.add('Troubleshoot_Disable_Workspace_Sync', false, {
-			type: 'boolean',
-			i18nDescription: 'Troubleshoot_Disable_Workspace_Sync_Alert',
-		});
+
+		// await this.add('Troubleshoot_Disable_Workspace_Sync', false, {
+		// 	type: 'boolean',
+		// 	i18nDescription: 'Troubleshoot_Disable_Workspace_Sync_Alert',
+		// });
+
 		await this.add('Troubleshoot_Disable_Teams_Mention', false, {
 			type: 'boolean',
 			i18nDescription: 'Troubleshoot_Disable_Teams_Mention_Alert',
