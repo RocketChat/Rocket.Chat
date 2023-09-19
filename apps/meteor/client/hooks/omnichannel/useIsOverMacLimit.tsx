4ea725bd5b154c061800644db2b6302d0c1f4d5f
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-09-19 09:31:44 -0300

    chore: return always true for over mac limit (test only)

--- apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
+++ apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
-import { useEndpoint } from '@rocket.chat/ui-contexts';
+// import { useEndpoint } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 
-export const useIsOverMacLimit = () => {
-	const getMacLimit = useEndpoint('GET', '/v1/livechat/mac'); // TODO: add the correct endpoint
+export const useIsOverMacLimit = (): boolean => {
+	const getMacLimit = () => Promise.resolve(true); // useEndpoint('GET', '/v1/livechat/mac'); // TODO: add the correct endpoint
 	const { data: isOverMacLimit } = useQuery(['omnichannel', '/v1/livechat/mac'], () => getMacLimit());
 	return isOverMacLimit;
 };
