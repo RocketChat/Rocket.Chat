7560cecb5c71c93ebe36ef34ab32a5f602fba562
gabriellsh <gabriel.henriques@rocket.chat>
2023-09-19 15:59:44 -0300

    mimic event name

--- apps/meteor/public/iframe/login.js
+++ apps/meteor/public/iframe/login.js
@@ -5,7 +5,7 @@ window.addEventListener('message', function (event) {
      * @type data.path {string}
      */
     const data = event.data;
-    if (data.event === 'login' && data.loginToken) {
+    if (data.event === 'login-with-token' && data.loginToken) {
         localStorage.setItem('Meteor.loginToken', data.loginToken);
 
         window.location.href = window.location.href.replace('/iframeLogin', data.path ?? '/home');
