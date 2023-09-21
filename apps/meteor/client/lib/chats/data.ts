08ff382476a3b1f89a8949fd523fa0eef48e8629
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-09-21 10:08:59 -0300

    Do not remove message from minimongo on client deleteMessage

--- apps/meteor/client/lib/chats/data.ts
+++ apps/meteor/client/lib/chats/data.ts
@@ -217,9 +217,6 @@ export const createDataAPI = ({ rid, tmid }: { rid: IRoom['_id']; tmid: IMessage
 
 	const deleteMessage = async (mid: IMessage['_id']): Promise<void> => {
 		await sdk.call('deleteMessage', { _id: mid });
-		if (!settings.get('Message_ShowDeletedStatus')) {
-			Messages.remove({ _id: mid });
-		}
 	};
 
 	const drafts = new Map<IMessage['_id'] | undefined, string>();
