9490809d58220feef9c4e100378847fd1ca08eeb
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-09-22 11:13:50 -0300

    feat: adapted watermark to support new licenses

--- apps/meteor/ee/client/sidebar/footer/SidebarFooterWatermark.tsx
+++ apps/meteor/ee/client/sidebar/footer/SidebarFooterWatermark.tsx
@@ -3,14 +3,26 @@ import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
-import { useIsEnterprise } from '../../../../client/hooks/useIsEnterprise';
+import { useLicenseV2 } from '../../../../client/hooks/useLicenseV2';
 
 export const SidebarFooterWatermark = (): ReactElement | null => {
 	const t = useTranslation();
 
-	const { isLoading, isError, data } = useIsEnterprise();
+	const { isLoading, isError, license } = useLicenseV2();
 
-	if (isError || isLoading || data?.isEnterprise) {
+	if (isError || isLoading) {
+		return null;
+	}
+
+	const {
+		grantedModules,
+		information: {
+			tags: [firstTag],
+		},
+	} = license;
+	const isWatermarkVisible = grantedModules.find(({ module }) => module === 'rocketchat-watermark'); // TODO: Possibly change to useHasLicenseModule
+
+	if (!isWatermarkVisible) {
 		return null;
 	}
 
@@ -21,7 +33,7 @@ export const SidebarFooterWatermark = (): ReactElement | null => {
 					{t('Powered_by_RocketChat')}
 				</Box>
 				<Box fontScale='micro' color='pure-white' pbe={4}>
-					{t('Free_Edition')}
+					{firstTag.name}
 				</Box>
 			</Box>
 		</Box>
