e248a34fa2ed05ca29e1253cef968fc771caf262
Pierre Lehnen <pierre.lehnen@rocket.chat>
2023-09-22 11:26:42 -0300

    added limitReached event

--- ee/packages/license/src/events/listeners.ts
+++ ee/packages/license/src/events/listeners.ts
@@ -1,3 +1,4 @@
+import type { LicenseLimitKind } from '../definition/ILicenseV3';
 import type { LicenseModule } from '../definition/LicenseModule';
 import { hasModule } from '../modules';
 import { EnterpriseLicenses } from './emitter';
@@ -66,4 +67,8 @@ export const onValidateLicense = (cb: (...args: any[]) => void) => {
 
 export const onInvalidateLicense = (cb: (...args: any[]) => void) => {
 	EnterpriseLicenses.on('invalidate', cb);
+};
+
+export const onLimitReached = (limitKind: LicenseLimitKind, cb: (...args: any[]) => void) => {
+	EnterpriseLicenses.on(`limitReached:${limitKind}`, cb);
 };
