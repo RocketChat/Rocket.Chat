56aa8bf63cd41233a70e7a362d52cbe1fdc6d9e8
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-09-29 17:12:37 -0300

    review

--- apps/meteor/server/models/raw/CloudAnnouncements.ts
+++ apps/meteor/server/models/raw/CloudAnnouncements.ts
@@ -1,15 +1,11 @@
-import type { Cloud, RocketChatRecordDeleted } from '@rocket.chat/core-typings';
+import type { Cloud } from '@rocket.chat/core-typings';
 import type { ICloudAnnouncementsModel } from '@rocket.chat/model-typings';
-import type { Collection, Db, IndexDescription } from 'mongodb';
+import type { Db } from 'mongodb';
 
 import { BaseRaw } from './BaseRaw';
 
 export class CloudAnnouncementsRaw extends BaseRaw<Cloud.Announcement> implements ICloudAnnouncementsModel {
-	constructor(db: Db, trash?: Collection<RocketChatRecordDeleted<Cloud.Announcement>>) {
-		super(db, 'cloud_announcements', trash);
-	}
-
-	modelIndexes(): IndexDescription[] {
-		return [{ key: { status: 1, expireAt: 1 } }];
+	constructor(db: Db) {
+		super(db, 'cloud_announcements');
 	}
 }
