27718859624827e73bd1db5c8c14f9cd015c88ca
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-10-02 11:43:52 -0300

    add the new endpoints to sync with cloud

--- packages/server-cloud-communication/src/index.ts
+++ packages/server-cloud-communication/src/index.ts
@@ -1,3 +1,27 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import type { ILicenseV3 } from '@rocket.chat/license';
+
 import type { SupportedVersions, SignedSupportedVersions } from './definitions';
 
 export { SupportedVersions, SignedSupportedVersions };
+
+export interface WorkspaceSyncRequestPayload {
+	uniqueId: string;
+	workspaceId: string;
+	seats: number;
+	MAC: number; // Need to align on the property
+	address: string;
+	siteName: string;
+	deploymentMethod: string;
+	deploymentPlatform: string;
+	version: string;
+	licenseVersion: number;
+
+	connectionDisable: boolean;
+}
+
+export interface WorkspaceSyncResponse {
+	workspaceId: string;
+	publicKey: string;
+	license: ILicenseV3;
+}
