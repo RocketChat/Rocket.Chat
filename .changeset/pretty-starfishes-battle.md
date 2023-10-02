b986f5842c3c1db89e89293b1963de8b4a5b7321
Kevin Aleman <kaleman960@gmail.com>
2023-10-02 10:58:30 -0600

    fix: Remove model-level query restrictions for monitors & improve `transfer` error handling (#30522)

---
+++ .changeset/pretty-starfishes-battle.md
@@ -0,0 +1,6 @@
+---
+"@rocket.chat/meteor": patch
+---
+
+fix: Remove model-level query restrictions for monitors
+fix: `onAssignmentFailed` & `onTransferFailure` callbacks not working properly
--- apps/meteor/ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
@@ -4,16 +4,18 @@ import type { ILivechatDepartment } from '@rocket.chat/core-typings';
 import { callbacks } from '../../../../../lib/callbacks';
 import { hasRoleAsync } from '../../../../../app/authorization/server/functions/hasRole';
 import { cbLogger } from '../lib/logger';
-import { getUnitsFromUser } from '../lib/units';
+import { getUnitsFromUser } from '../methods/getUnitsFromUserRoles';
 
-export const addQueryRestrictionsToDepartmentsModel = async (originalQuery: FilterOperators<ILivechatDepartment> = {}) => {
+export const addQueryRestrictionsToDepartmentsModel = async (originalQuery: FilterOperators<ILivechatDepartment> = {}, userId: string) => {
 	const query: FilterOperators<ILivechatDepartment> = { ...originalQuery, type: { $ne: 'u' } };
 
-	const units = await getUnitsFromUser();
+	const units = await getUnitsFromUser(userId);
 	if (Array.isArray(units)) {
 		query.ancestors = { $in: units };
+		query._id = { $in: units };
 	}
 
+	cbLogger.debug({ msg: 'Applying department query restrictions', userId, units });
 	return query;
 };
 
@@ -25,8 +27,7 @@ callbacks.add(
 			return originalQuery;
 		}
 
-		cbLogger.debug('Applying department query restrictions');
-		return addQueryRestrictionsToDepartmentsModel(originalQuery);
+		return addQueryRestrictionsToDepartmentsModel(originalQuery, userId);
 	},
 	callbacks.priority.HIGH,
 	'livechat-apply-department-restrictions',
