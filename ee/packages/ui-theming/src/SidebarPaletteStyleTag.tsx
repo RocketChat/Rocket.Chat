f1f5339b1351c2d4a33ce26fc9c72e60454d1a70
juliajforesti <juliajforesti@gmail.com>
2023-10-05 14:41:43 -0300

    chore: initial test overriding palette

--- ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
+++ ee/packages/ui-theming/src/SidebarPaletteStyleTag.tsx
@@ -5,7 +5,7 @@ import { createPortal } from 'react-dom';
 import { convertToCss } from './helpers/convertToCss';
 import { useCreateStyleContainer } from './hooks/useCreateStyleContainer';
 import { darkPalette } from './paletteDark';
-import { defaultSidebarPalette } from './sidebarPalette';
+// import { defaultSidebarPalette } from './sidebarPalette';
 
 export const SidebarPaletteStyleTag = memo(function SidebarPaletteStyleTag(): ReactElement | null {
 	// Commented code below: sidebar palette currently the same in both themes.
@@ -16,7 +16,7 @@ export const SidebarPaletteStyleTag = memo(function SidebarPaletteStyleTag(): Re
 	// 	'.rcx-sidebar--main',
 	// );
 
-	const palette = convertToCss({ ...darkPalette, ...defaultSidebarPalette }, '.rcx-sidebar--main');
+	const palette = convertToCss({ ...darkPalette }, '.rcx-sidebar--main');
 
 	return createPortal(palette, useCreateStyleContainer('sidebar-palette'));
 });
