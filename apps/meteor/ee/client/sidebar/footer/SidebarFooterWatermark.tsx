a1674e3345919b8551c64b361c3e7fcc850ace3e
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-10-05 15:49:01 -0300

    chore: adjusted footer watermark to license v3

--- apps/meteor/ee/client/sidebar/footer/SidebarFooterWatermark.tsx
+++ apps/meteor/ee/client/sidebar/footer/SidebarFooterWatermark.tsx
@@ -1,20 +1,17 @@
 import { Box } from '@rocket.chat/fuselage';
-import type { ILicenseV3 } from '@rocket.chat/license';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
 import { useLicense } from '../../../../client/hooks/useLicense';
-import { useHasLicenseModule } from '../../hooks/useHasLicenseModule';
 
 export const SidebarFooterWatermark = (): ReactElement | null => {
 	const t = useTranslation();
 
-	const { data } = useLicense();
-	const [license] = (data?.licenses ?? []) as (ILicenseV3 & { modules: string[] })[]; // TODO: temporary, remover after #30473 is merged
-	const [planTag] = license.information?.tags ?? [];
+	const { data: { license, activeModules = [] } = {} } = useLicense();
+	const [planTag] = license?.information?.tags ?? [];
 
-	const isWatermarkVisible = useHasLicenseModule('watermark') === true;
+	const isWatermarkVisible = activeModules.includes('watermark');
 
 	if (!isWatermarkVisible) {
 		return null;
