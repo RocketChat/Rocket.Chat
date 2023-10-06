db6f60ff1d0ac2a66a9f495e473a5146162cc673
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-10-06 18:06:55 -0300

    fix: incomplete validation on useIsRoomActive

--- apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
+++ apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
@@ -1,18 +1,9 @@
-import { useEndpoint, useStream } from '@rocket.chat/ui-contexts';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
 
 export const useIsOverMacLimit = (): boolean => {
-	const queryClient = useQueryClient();
-	const notifyLogged = useStream('notify-logged');
 	const getMacLimit = useEndpoint('GET', '/v1/omnichannel/mac/check');
 	const { data: { onLimit: isOverMacLimit = false } = {} } = useQuery(['/v1/omnichannel/mac/check'], () => getMacLimit());
-
-	useEffect(() => {
-		return notifyLogged(`mac.limit`, ({ limitReached }) => {
-			limitReached && queryClient.invalidateQueries(['/v1/omnichannel/mac/check']);
-		});
-	}, [notifyLogged, queryClient]);
 
 	return !!isOverMacLimit;
 };
