f1e00048245104f3c7695f39a69dc1d1d7c89e2c
murtaza98 <murtaza.patrawala@rocket.chat>
2023-10-09 17:00:59 +0530

    Decrease cache lifetime of AnalyticsOverviewData since it's being used for realtime analytics

--- apps/meteor/app/livechat/server/lib/AnalyticsTyped.ts
+++ apps/meteor/app/livechat/server/lib/AnalyticsTyped.ts
@@ -3,4 +3,4 @@ import mem from 'mem';
 import { Analytics } from './Analytics';
 
 export const getAgentOverviewDataCached = mem(Analytics.getAgentOverviewData, { maxAge: 60000, cacheKey: JSON.stringify });
-export const getAnalyticsOverviewDataCached = mem(Analytics.getAnalyticsOverviewData, { maxAge: 60000, cacheKey: JSON.stringify });
+export const getAnalyticsOverviewDataCached = mem(Analytics.getAnalyticsOverviewData, { maxAge: 5000, cacheKey: JSON.stringify });
