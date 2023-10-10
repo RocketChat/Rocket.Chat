1f09f395deb9ca58d51c3541f89898037b0a3ba0
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-10-10 13:41:45 -0300

    fix: show red dot highlight only to managers

--- apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
+++ apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
@@ -1,9 +1,10 @@
-import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useEndpoint, usePermission } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 
 export const useIsOverMacLimit = (): boolean => {
 	const getMacLimit = useEndpoint('GET', '/v1/omnichannel/mac/check');
-	const { data: { onLimit = false } = {} } = useQuery(['/v1/omnichannel/mac/check'], () => getMacLimit());
+	const isLivechatManager = usePermission('view-livechat-manager');
+	const { data: { onLimit = true } = {} } = useQuery(['/v1/omnichannel/mac/check'], () => getMacLimit(), { enabled: isLivechatManager });
 
 	return !onLimit;
 };
