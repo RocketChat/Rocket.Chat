b8a6ac4eebc797dbfb3eaf53bf296f2be5ae541d
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-10-10 16:16:47 -0300

    fix: incorrect over mac limit validation

--- apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
+++ apps/meteor/client/hooks/omnichannel/useIsOverMacLimit.tsx
@@ -1,10 +1,9 @@
-import { useEndpoint, usePermission } from '@rocket.chat/ui-contexts';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 
 export const useIsOverMacLimit = (): boolean => {
 	const getMacLimit = useEndpoint('GET', '/v1/omnichannel/mac/check');
-	const isLivechatManager = usePermission('view-livechat-manager');
-	const { data: { onLimit = true } = {} } = useQuery(['/v1/omnichannel/mac/check'], () => getMacLimit(), { enabled: isLivechatManager });
+	const { data: { onLimit = true } = {} } = useQuery(['/v1/omnichannel/mac/check'], () => getMacLimit());
 
 	return !onLimit;
 };
--- apps/meteor/ee/client/omnichannel/hooks/useOmnichannelHighlight.tsx
+++ apps/meteor/ee/client/omnichannel/hooks/useOmnichannelHighlight.tsx
@@ -1,7 +1,10 @@
+import { usePermission } from '@rocket.chat/ui-contexts';
+
 import { useCurrentChatsHighlight } from './useCurrentChatsHighlight';
 
 export const useOmnichannelHighlight = () => {
+	const isLivechatManager = usePermission('view-livechat-manager');
 	const { isHighlit } = useCurrentChatsHighlight();
 
-	return { isHighlit };
+	return { isHighlit: isLivechatManager && isHighlit };
 };
