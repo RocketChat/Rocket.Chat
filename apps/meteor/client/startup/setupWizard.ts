c31273fa4d74bf1f85f32342343baa907f0f9153
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-10-11 15:54:13 -0300

    review 1

--- apps/meteor/client/startup/setupWizard.ts
+++ apps/meteor/client/startup/setupWizard.ts
@@ -1,21 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
-import { hasRole } from '../../app/authorization/client';
+import { hasPermission, hasRole } from '../../app/authorization/client';
 import { settings } from '../../app/settings/client';
+import { sdk } from '../../app/utils/client/lib/SDKClient';
 import { router } from '../providers/RouterProvider';
 
 Meteor.startup(() => {
-	Tracker.autorun(() => {
+	Tracker.autorun(async () => {
 		const userId = Meteor.userId();
 		const setupWizardState = settings.get('Show_Setup_Wizard');
 
 		const isAdmin = userId && hasRole(userId, 'admin');
 
 		const isWizardInProgress = isAdmin && setupWizardState === 'in_progress';
-		const isWorkspaceRegistered = isAdmin && !!settings.get('Cloud_Workspace_Client_Id') && !!settings.get('Cloud_Workspace_Client_Secret');
+		const { registrationStatus } = await sdk.rest.get('/v1/cloud.registrationStatus');
 
-		const mustRedirect = (!userId && setupWizardState === 'pending') || isWizardInProgress || !isWorkspaceRegistered;
+		const hasRegisterCloudPermission = hasPermission('register-on-cloud');
+		const isWorkspaceRegistered = registrationStatus?.workspaceRegistered;
+
+		const mustRedirect =
+			(!userId && setupWizardState === 'pending') || isWizardInProgress || (!isWorkspaceRegistered && hasRegisterCloudPermission);
 
 		if (mustRedirect) {
 			router.navigate('/setup-wizard');
