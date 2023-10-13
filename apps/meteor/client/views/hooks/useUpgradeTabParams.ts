b454c9b5d8dcfcaefa797a254d05401a9e4a4f9d
Hugo Costa <hugocarreiracosta@gmail.com>
2023-10-13 10:55:20 -0300

    chore: improving useRegistrationStatus hook

--- apps/meteor/client/views/hooks/useUpgradeTabParams.ts
+++ apps/meteor/client/views/hooks/useUpgradeTabParams.ts
@@ -10,9 +10,7 @@ export const useUpgradeTabParams = (): { tabType: UpgradeTabVariant | false; tri
 	const cloudWorkspaceHadTrial = useSetting('Cloud_Workspace_Had_Trial') as boolean;
 
 	const { data: licensesData, isSuccess: isSuccessLicense } = useLicense();
-	const { data: registrationStatusData, isSuccess: isSuccessRegistrationStatus } = useRegistrationStatus();
-
-	const registered = registrationStatusData?.registrationStatus?.workspaceRegistered ?? false;
+	const { isRegistered, isSuccess: isSuccessRegistrationStatus } = useRegistrationStatus();
 
 	const license = licensesData?.data.license;
 	const activeModules = licensesData?.data.activeModules || [];
@@ -26,7 +24,7 @@ export const useUpgradeTabParams = (): { tabType: UpgradeTabVariant | false; tri
 	const trialEndDate = trialEndDateStr ? format(new Date(trialEndDateStr), 'yyyy-MM-dd') : undefined;
 
 	const upgradeTabType = getUpgradeTabType({
-		registered,
+		registered: isRegistered || false,
 		hasValidLicense,
 		hadExpiredTrials,
 		isTrial,
