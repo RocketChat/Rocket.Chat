062e6a8d974121a9441faa9e42d2457cba8e85a5
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-10-13 16:38:57 -0300

    fix

--- apps/meteor/client/startup/setupWizard.ts
+++ apps/meteor/client/startup/setupWizard.ts
@@ -6,29 +6,26 @@ import { settings } from '../../app/settings/client';
 import { sdk } from '../../app/utils/client/lib/SDKClient';
 import { router } from '../providers/RouterProvider';
 
-Meteor.startup(() => {
-	Tracker.autorun(async (computation) => {
-		const { registrationStatus } = await sdk.rest.get('/v1/cloud.registrationStatus');
+Meteor.startup(async () => {
+	const { registrationStatus } = await sdk.rest.get('/v1/cloud.registrationStatus');
 
-		Tracker.withComputation(computation, async () => {
-			const setupWizardState = settings.get('Show_Setup_Wizard');
+	Tracker.autorun(() => {
+		const setupWizardState = settings.get('Show_Setup_Wizard');
 
-			const hasRegisterCloudPermission = hasPermission('register-on-cloud');
+		const hasRegisterCloudPermission = hasPermission('register-on-cloud');
+		const userId = Meteor.userId();
 
-			const userId = Meteor.userId();
+		const isAdmin = userId && hasRole(userId, 'admin');
 
-			const isAdmin = userId && hasRole(userId, 'admin');
+		const isWizardInProgress = isAdmin && setupWizardState === 'in_progress';
 
-			const isWizardInProgress = isAdmin && setupWizardState === 'in_progress';
+		const isWorkspaceRegistered = registrationStatus?.workspaceRegistered;
 
-			const isWorkspaceRegistered = registrationStatus?.workspaceRegistered;
+		const mustRedirect =
+			(!userId && setupWizardState === 'pending') || isWizardInProgress || (!isWorkspaceRegistered && hasRegisterCloudPermission);
 
-			const mustRedirect =
-				(!userId && setupWizardState === 'pending') || isWizardInProgress || (!isWorkspaceRegistered && hasRegisterCloudPermission);
-
-			if (mustRedirect) {
-				router.navigate('/setup-wizard');
-			}
-		});
+		if (mustRedirect) {
+			router.navigate('/setup-wizard');
+		}
 	});
 });
