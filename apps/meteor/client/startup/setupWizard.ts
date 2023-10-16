16cf6dff11843b71171f168c1d508218597001b7
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-10-16 10:40:35 -0300

    test

--- apps/meteor/client/startup/setupWizard.ts
+++ apps/meteor/client/startup/setupWizard.ts
@@ -1,31 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
-import { hasPermission, hasRole } from '../../app/authorization/client';
+import { hasRole } from '../../app/authorization/client';
 import { settings } from '../../app/settings/client';
-import { sdk } from '../../app/utils/client/lib/SDKClient';
+// import { sdk } from '../../app/utils/client/lib/SDKClient';
 import { router } from '../providers/RouterProvider';
 
-Meteor.startup(async () => {
-	const { registrationStatus } = await sdk.rest.get('/v1/cloud.registrationStatus');
+Meteor.startup(
+	/* async*/ () => {
+		// const { registrationStatus } = await sdk.rest.get('/v1/cloud.registrationStatus');
 
-	Tracker.autorun(() => {
-		const setupWizardState = settings.get('Show_Setup_Wizard');
+		Tracker.autorun(() => {
+			const setupWizardState = settings.get('Show_Setup_Wizard');
 
-		const hasRegisterCloudPermission = hasPermission('register-on-cloud');
-		const userId = Meteor.userId();
+			// const hasRegisterCloudPermission = hasPermission('register-on-cloud');
+			const userId = Meteor.userId();
 
-		const isAdmin = userId && hasRole(userId, 'admin');
+			const isAdmin = userId && hasRole(userId, 'admin');
 
-		const isWizardInProgress = isAdmin && setupWizardState === 'in_progress';
+			const isWizardInProgress = isAdmin && setupWizardState === 'in_progress';
 
-		const isWorkspaceRegistered = registrationStatus?.workspaceRegistered;
+			// const isWorkspaceRegistered = registrationStatus?.workspaceRegistered;
 
-		const mustRedirect =
-			(!userId && setupWizardState === 'pending') || isWizardInProgress || (!isWorkspaceRegistered && hasRegisterCloudPermission);
+			const mustRedirect =
+				(!userId && setupWizardState === 'pending') || isWizardInProgress; /* || (!isWorkspaceRegistered && hasRegisterCloudPermission)*/
 
-		if (mustRedirect) {
-			router.navigate('/setup-wizard');
-		}
-	});
-});
+			if (mustRedirect) {
+				router.navigate('/setup-wizard');
+			}
+		});
+	},
+);
