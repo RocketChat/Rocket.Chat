4789616afd08c83919a100b4fe6562240f2d8502
Douglas Gubert <douglas.gubert@gmail.com>
2023-10-26 16:57:06 -0300

    fix: Private apps list infinite loading in air-gapped environments (#30775)

--- apps/meteor/client/providers/AppsProvider.tsx
+++ apps/meteor/client/providers/AppsProvider.tsx
@@ -66,24 +66,23 @@ const AppsProvider: FC = ({ children }) => {
 		},
 		{
 			staleTime: Infinity,
-			keepPreviousData: true,
 			onSettled: () => queryClient.invalidateQueries(['marketplace', 'apps-stored']),
 		},
 	);
 
 	const store = useQuery(
-		['marketplace', 'apps-stored', isAdminUser],
+		['marketplace', 'apps-stored', instance.data, marketplace.data],
 		() => {
-			if (!marketplace.isSuccess || !instance.isSuccess) {
+			if (!marketplace.isFetched && !instance.isFetched) {
 				throw new Error('Apps not loaded');
 			}
 
 			const marketplaceApps: App[] = [];
 			const installedApps: App[] = [];
 			const privateApps: App[] = [];
-			const clonedData = [...instance.data];
+			const clonedData = [...(instance.data || [])];
 
-			sortByName(marketplace.data).forEach((app) => {
+			sortByName(marketplace.data || []).forEach((app) => {
 				const appIndex = clonedData.findIndex(({ id }) => id === app.id);
 				const [installedApp] = appIndex > -1 ? clonedData.splice(appIndex, 1) : [];
 
@@ -117,8 +116,7 @@ const AppsProvider: FC = ({ children }) => {
 			return [marketplaceApps, installedApps, privateApps];
 		},
 		{
-			enabled: marketplace.isSuccess && instance.isSuccess && !instance.isRefetching,
-			keepPreviousData: true,
+			enabled: marketplace.isFetched && instance.isFetched,
 		},
 	);
 
@@ -130,9 +128,9 @@ const AppsProvider: FC = ({ children }) => {
 		<AppsContext.Provider
 			children={children}
 			value={{
-				installedApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data[1] } },
-				marketplaceApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data[0] } },
-				privateApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data[2] } },
+				installedApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data?.[1] || [] } },
+				marketplaceApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data?.[0] || [] } },
+				privateApps: { phase: AsyncStatePhase.RESOLVED, value: { apps: store.data?.[2] || [] } },
 				reload: async () => {
 					await Promise.all([queryClient.invalidateQueries(['marketplace'])]);
 				},
