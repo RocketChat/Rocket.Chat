47081dc4996d48ecdc1c865fbe79be92676624d4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-10-26 17:34:03 -0300

    Migrate modules to TS

--- apps/meteor/client/views/omnichannel/realTimeMonitoring/charts/getMomentChartLabelsAndData.ts
+++ apps/meteor/client/views/omnichannel/realTimeMonitoring/charts/getMomentChartLabelsAndData.ts
@@ -1,11 +1,11 @@
 import moment from 'moment';
 
-export const getMomentChartLabelsAndData = () => {
+export const getMomentChartLabelsAndData = (timestamp = Date.now()) => {
 	const timingLabels = [];
 	const initData = [];
-	const today = moment().startOf('day');
-	for (let m = today; m.diff(moment(), 'hours') < 0; m.add(1, 'hours')) {
-		const numberHour = moment(new Date(), ['H']).hour();
+	const today = moment(timestamp).startOf('day');
+	for (let m = today; m.diff(moment(timestamp), 'hours') < 0; m.add(1, 'hours')) {
+		const numberHour = moment(timestamp, ['H']).hour();
 		timingLabels.push(`${moment(numberHour, ['H']).format('hA')}-${moment((numberHour + 1) % 24, ['H']).format('hA')}`);
 		initData.push(0);
 	}
--- apps/meteor/client/views/omnichannel/realTimeMonitoring/charts/getMomentCurrentLabel.ts
+++ apps/meteor/client/views/omnichannel/realTimeMonitoring/charts/getMomentCurrentLabel.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
-export const getMomentCurrentLabel = () => {
-	const numberHour = moment(new Date(), ['H']).hour();
+export const getMomentCurrentLabel = (timestamp = Date.now()) => {
+	const numberHour = moment(timestamp, ['H']).hour();
 
 	return `${moment(numberHour, ['H']).format('hA')}-${moment((numberHour + 1) % 24, ['H']).format('hA')}`;
 };
