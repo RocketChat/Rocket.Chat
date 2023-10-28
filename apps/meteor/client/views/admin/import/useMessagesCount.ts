ba04471d74ecc0b3ab46d2cac22b7f6ba11a1f87
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2023-10-28 14:49:14 -0300

    create filters for messageCount

--- apps/meteor/client/views/admin/import/useMessagesCount.ts
+++ apps/meteor/client/views/admin/import/useMessagesCount.ts
@@ -1,5 +1,20 @@
-const useMessagesCount = () => {
-	return 'messageCount';
+import { useMemo } from 'react';
+
+const useMessagesCount = (users, channels, messages, setMessageCount) => {
+	useMemo(() => {
+		let messageCount = 0;
+
+		const selectedUsers = new Set(users.map((user) => user._id));
+		const selectedChannels = new Set(users.map((channel) => channel._id));
+
+		for (const message of messages) {
+			if (selectedUsers.has(message.u._id) || selectedChannels.has(message.rid)) {
+				messageCount++;
+			}
+		}
+
+		setMessageCount(messageCount);
+	}, [users, setMessageCount, messages]);
 };
 
 export default useMessagesCount;
