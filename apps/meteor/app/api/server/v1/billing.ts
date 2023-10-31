590fc33e2dc1854bd07cc12432b7ce2cd471f3d9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-10-31 00:58:13 -0300

    fix billing rest type location

--- apps/meteor/app/api/server/v1/billing.ts
+++ apps/meteor/app/api/server/v1/billing.ts
@@ -1,6 +1,15 @@
 import { getCheckoutUrl } from '../../../cloud/server/functions/getCheckoutUrl';
 import { API } from '../api';
 
+declare module '@rocket.chat/rest-typings' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	interface Endpoints {
+		'/v1/billing.checkoutUrl': {
+			GET: () => { url: string };
+		};
+	}
+}
+
 API.v1.addRoute(
 	'billing.checkoutUrl',
 	{ authRequired: true, permissionsRequired: ['manage-cloud'] },
--- packages/rest-typings/src/v1/billing.ts
+++
@@ -1,5 +0,0 @@
-export type BillingEndpoints = {
-	'/v1/billing.checkoutUrl': {
-		GET: () => { url: string };
-	};
-};
