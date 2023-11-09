c7ac6741899c3eaf25e21dd98700fa72fbb5032a
csuarez <carlos.suarez@rocket.chat>
2023-11-09 11:32:35 -0500

    Change plan name to premium when enterprise from 6.5 to support old plan name

--- apps/meteor/client/views/marketplace/BundleChips.tsx
+++ apps/meteor/client/views/marketplace/BundleChips.tsx
@@ -16,19 +16,31 @@ type BundleChipsProps = {
 const BundleChips = ({ bundledIn }: BundleChipsProps): ReactElement => {
 	const t = useTranslation();
 
+	const handleLabel = (label: string): string => {
+		if (label === 'Enterprise') {
+			return 'Premium';
+		}
+		return label;
+	};
+
 	return (
 		<>
-			{bundledIn.map(({ bundleId, bundleName }) => (
-				<Tag
-					key={bundleId}
-					variant='featured'
-					title={t('this_app_is_included_with_subscription', {
-						bundleName,
-					})}
-				>
-					{bundleName}
-				</Tag>
-			))}
+			{bundledIn.map(({ bundleId, bundleName }) => {
+				// this is a workaround to not change plan name for versions lower than 6.5.0
+				const handledBundledName = handleLabel(bundleName);
+
+				return (
+					<Tag
+						key={bundleId}
+						variant='featured'
+						title={t('this_app_is_included_with_subscription', {
+							bundleName: handledBundledName,
+						})}
+					>
+						{handledBundledName}
+					</Tag>
+				);
+			})}
 		</>
 	);
 };
