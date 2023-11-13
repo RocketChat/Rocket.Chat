da17794412fb29487f6f89bdab9e80aec2052dda
Hugo Costa <hugocarreiracosta@gmail.com>
2023-11-13 10:39:38 -0300

    fix: go links

--- apps/meteor/client/views/admin/subscription/utils/links.ts
+++ apps/meteor/client/views/admin/subscription/utils/links.ts
@@ -1,4 +1,4 @@
 export const CONTACT_SALES_LINK = 'https://go.rocket.chat/i/contact-sales-product';
 export const PRICING_LINK = 'https://go.rocket.chat/i/pricing-product';
-export const DOWNGRADE_LINK = 'https://go.rocket.chat/i/docs-downgrade';
-export const TRIAL_LINK = 'https://go.rocket.chat/i/docs-trial';
+export const DOWNGRADE_LINK = 'https://docs.rocket.chat/resources/frequently-asked-questions/license-faqs/downgrade-behavior';
+export const TRIAL_LINK = 'https://docs.rocket.chat/resources/frequently-asked-questions/license-faqs/trial-faq';
