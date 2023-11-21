bcbf385207e20291d2a94902f3af7a097c12cf8a
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2023-11-21 17:19:24 -0300

    regression: error when archiving rooms in the UI (#31034)

--- apps/meteor/client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.tsx
+++ apps/meteor/client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.tsx
@@ -97,6 +97,7 @@ const EditRoomInfo = ({ room, onClickClose, onClickBack }: EditRoomInfoProps) =>
 
 	const handleUpdateRoomData = useMutableCallback(async ({ hideSysMes, ...formData }) => {
 		const data = getDirtyFields(formData, dirtyFields);
+		delete data.archived;
 
 		try {
 			await saveAction({
