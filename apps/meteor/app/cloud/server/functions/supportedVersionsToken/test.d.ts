f78ff72bb9336b885784b83d54943d989f469a61
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-11-21 10:17:41 -0300

    ..

--- apps/meteor/app/cloud/server/functions/supportedVersionsToken/test.d.ts
+++
@@ -1,5 +0,0 @@
-import type { SignedSupportedVersions } from '@rocket.chat/server-cloud-communication';
-
-declare module '*.versions' {
-	export const supportedVersions: SignedSupportedVersions;
-}
