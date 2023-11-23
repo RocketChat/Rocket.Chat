398265f6376eb95febb8bdae8853a9b02128f1e1
Hugo Costa <hugocarreiracosta@gmail.com>
2023-11-23 11:28:57 -0300

    fix: update seats cap modal

--- apps/meteor/ee/client/views/admin/users/ReachedSeatsCapModal.tsx
+++ apps/meteor/ee/client/views/admin/users/ReachedSeatsCapModal.tsx
@@ -1,49 +1,34 @@
 import { Modal, Button, Box } from '@rocket.chat/fuselage';
-import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
-
-import { useExternalLink } from '../../../../../client/hooks/useExternalLink';
-import SeatsCapUsage from './SeatsCapUsage';
+import { useTranslation } from 'react-i18next';
 
 type ReachedSeatsCapModalProps = {
-	members: number;
-	limit: number;
-	requestSeatsLink: string;
 	onClose: () => void;
+	onContinue: () => void;
+	onBuyMoreSeats: () => void;
+	showContinue: boolean;
 };
 
-const ReachedSeatsCapModal = ({ members, limit, onClose, requestSeatsLink }: ReachedSeatsCapModalProps): ReactElement => {
-	const t = useTranslation();
-	const handleExternalLink = useExternalLink();
+const ReachedSeatsCapModal = ({ onClose, onContinue, onBuyMoreSeats, showContinue }: ReachedSeatsCapModalProps): ReactElement => {
+	const { t } = useTranslation();
 
 	return (
 		<Modal>
 			<Modal.Header>
-				<Modal.Title>{t('Request_more_seats_title')}</Modal.Title>
+				<Modal.Title>{t('Seat_limit_reached')}</Modal.Title>
 				<Modal.Close onClick={onClose} />
 			</Modal.Header>
 			<Modal.Content>
 				<Box is='p' mbe={16}>
-					{t('Request_more_seats_out_of_seats')}
+					{t('Seat_limit_reached_Description')}
 				</Box>
-				<Box is='p' mbe={24}>
-					{t('Request_more_seats_sales_team')}
-				</Box>
-				<SeatsCapUsage members={members} limit={limit} />
 			</Modal.Content>
 			<Modal.Footer>
 				<Modal.FooterControllers>
-					<Button onClick={onClose}>{t('Cancel')}</Button>
-					<Button
-						icon='new-window'
-						onClick={() => {
-							handleExternalLink(requestSeatsLink);
-							onClose();
-						}}
-						primary
-					>
-						{t('Request')}
+					{showContinue && <Button onClick={onContinue}>{t('Continue')}</Button>}
+					<Button primary onClick={onBuyMoreSeats}>
+						{t('Buy_more_seats')}
 					</Button>
 				</Modal.FooterControllers>
 			</Modal.Footer>
--- apps/meteor/ee/client/views/admin/users/UserPageHeaderContentWithSeatsCap.tsx
+++ apps/meteor/ee/client/views/admin/users/UserPageHeaderContentWithSeatsCap.tsx
@@ -1,14 +1,13 @@
 import { Button, ButtonGroup, Margins } from '@rocket.chat/fuselage';
-import { useSetModal, useRoute, useTranslation } from '@rocket.chat/ui-contexts';
+import { useSetModal, useTranslation, useRouter } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
 import { useExternalLink } from '../../../../../client/hooks/useExternalLink';
 import { useShouldPreventAction } from '../../../../../client/hooks/useShouldPreventAction';
-import CloseToSeatsCapModal from './CloseToSeatsCapModal';
+import { useCheckoutUrl } from '../../../../../client/views/admin/subscription/hooks/useCheckoutUrl';
 import ReachedSeatsCapModal from './ReachedSeatsCapModal';
 import SeatsCapUsage from './SeatsCapUsage';
-import { useRequestSeatsLink } from './useRequestSeatsLink';
 
 type UserPageHeaderContentWithSeatsCapProps = {
 	activeUsers: number;
@@ -16,80 +15,44 @@ type UserPageHeaderContentWithSeatsCapProps = {
 };
 
 const UserPageHeaderContentWithSeatsCap = ({ activeUsers, maxActiveUsers }: UserPageHeaderContentWithSeatsCapProps): ReactElement => {
-	const seatsLinkUrl = useRequestSeatsLink();
-	const handleExternalLink = useExternalLink();
-
 	const isCreateUserDisabled = useShouldPreventAction('activeUsers');
 
 	const t = useTranslation();
-	const usersRoute = useRoute('admin-users');
+	const router = useRouter();
 
 	const setModal = useSetModal();
 	const closeModal = (): void => setModal(null);
 
-	const isCloseToLimit = (): boolean => {
-		const ratio = activeUsers / maxActiveUsers;
-		return ratio >= 0.8;
-	};
+	const openExternalLink = useExternalLink();
+	const manageSubscriptionUrl = useCheckoutUrl()({ target: 'user-page', action: 'buy_more' });
 
-	const withPreventionOnReachedLimit = (fn: () => void) => (): void => {
-		if (typeof seatsLinkUrl !== 'string') {
-			return;
-		}
-		if (isCreateUserDisabled) {
-			setModal(<ReachedSeatsCapModal members={activeUsers} limit={maxActiveUsers} requestSeatsLink={seatsLinkUrl} onClose={closeModal} />);
+	const withReachedLimit = (fn: () => void) => (): void => {
+		if (activeUsers >= maxActiveUsers || isCreateUserDisabled) {
+			setModal(
+				<ReachedSeatsCapModal
+					onClose={closeModal}
+					onContinue={() => {
+						router.navigate('/admin/users/new');
+						closeModal();
+					}}
+					onBuyMoreSeats={() => {
+						openExternalLink(manageSubscriptionUrl);
+					}}
+					showContinue={!isCreateUserDisabled}
+				/>,
+			);
 			return;
 		}
 
 		fn();
 	};
 
-	const handleNewButtonClick = withPreventionOnReachedLimit(() => {
-		if (typeof seatsLinkUrl !== 'string') {
-			return;
-		}
-		if (isCloseToLimit()) {
-			setModal(
-				<CloseToSeatsCapModal
-					members={activeUsers}
-					limit={maxActiveUsers}
-					title={t('Create_new_members')}
-					requestSeatsLink={seatsLinkUrl}
-					onConfirm={(): void => {
-						usersRoute.push({ context: 'new' });
-						closeModal();
-					}}
-					onClose={closeModal}
-				/>,
-			);
-			return;
-		}
-
-		usersRoute.push({ context: 'new' });
+	const handleNewButtonClick = withReachedLimit(() => {
+		router.navigate('/admin/users/new');
 	});
 
-	const handleInviteButtonClick = withPreventionOnReachedLimit(() => {
-		if (typeof seatsLinkUrl !== 'string') {
-			return;
-		}
-		if (isCloseToLimit()) {
-			setModal(
-				<CloseToSeatsCapModal
-					members={activeUsers}
-					limit={maxActiveUsers}
-					title={t('Invite_Users')}
-					requestSeatsLink={seatsLinkUrl}
-					onConfirm={(): void => {
-						usersRoute.push({ context: 'invite' });
-						closeModal();
-					}}
-					onClose={closeModal}
-				/>,
-			);
-			return;
-		}
-
-		usersRoute.push({ context: 'invite' });
+	const handleInviteButtonClick = withReachedLimit(() => {
+		router.navigate('/admin/users/invite');
 	});
 
 	return (
@@ -98,14 +61,14 @@ const UserPageHeaderContentWithSeatsCap = ({ activeUsers, maxActiveUsers }: User
 				<SeatsCapUsage members={activeUsers} limit={maxActiveUsers} />
 			</Margins>
 			<ButtonGroup>
-				<Button icon='user-plus' onClick={handleNewButtonClick}>
-					{t('New')}
-				</Button>
 				<Button icon='mail' onClick={handleInviteButtonClick}>
 					{t('Invite')}
 				</Button>
-				<Button icon='new-window' onClick={() => handleExternalLink(seatsLinkUrl)}>
-					{t('Request_seats')}
+				<Button icon='user-plus' onClick={handleNewButtonClick}>
+					{t('New_user')}
+				</Button>
+				<Button primary onClick={() => openExternalLink(manageSubscriptionUrl)}>
+					{t('Buy_more_seats')}
 				</Button>
 			</ButtonGroup>
 		</>
