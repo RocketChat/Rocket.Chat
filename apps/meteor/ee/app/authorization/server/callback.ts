de4c7deb66dce8a0cc78376621d84d22b7285082
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-11-24 20:32:53 -0300

    chore: add callback events to user activation (#31073)

--- apps/meteor/ee/app/authorization/server/callback.ts
+++ apps/meteor/ee/app/authorization/server/callback.ts
@@ -3,21 +3,26 @@ import { License } from '@rocket.chat/license';
 import { callbacks } from '../../../../lib/callbacks';
 import { validateUserRoles } from './validateUserRoles';
 
-License.onInstall(async () => {
+License.onInstall(() => {
 	callbacks.add(
 		'beforeSaveUser',
-		async ({ user, oldUser }) => {
-			await validateUserRoles(user, oldUser);
-		},
+		async ({ user, oldUser }) => validateUserRoles(user, oldUser),
 		callbacks.priority.HIGH,
 		'validateUserRoles',
 	);
 	callbacks.add('afterSaveUser', () => License.shouldPreventAction('activeUsers'), callbacks.priority.HIGH, 'validateUserRoles');
 	callbacks.add('afterDeleteUser', () => License.shouldPreventAction('activeUsers'), callbacks.priority.HIGH, 'validateUserRoles');
+
+	callbacks.add('afterDeactivateUser', () => License.shouldPreventAction('activeUsers'), callbacks.priority.HIGH, 'validateUserStatus');
+
+	callbacks.add('beforeActivateUser', () => License.shouldPreventAction('activeUsers'), callbacks.priority.HIGH, 'validateUserStatus');
 });
 
-License.onInvalidate(async () => {
+License.onInvalidate(() => {
 	callbacks.remove('beforeSaveUser', 'validateUserRoles');
 	callbacks.remove('afterSaveUser', 'validateUserRoles');
 	callbacks.remove('afterDeleteUser', 'validateUserRoles');
+
+	callbacks.remove('afterDeactivateUser', 'validateUserStatus');
+	callbacks.remove('beforeActivateUser', 'validateUserStatus');
 });
