0e41d74795a46674f09b22458f0c8f489b03032d
Marcos Defendi <marcos.defendi@rocket.chat>
2023-11-28 12:23:53 -0300

    chore: add a manual msg stream update for autotranslate

--- apps/meteor/app/autotranslate/server/methods/translateMessage.ts
+++ apps/meteor/app/autotranslate/server/methods/translateMessage.ts
@@ -1,9 +1,11 @@
+import { api } from '@rocket.chat/core-services';
 import type { IMessage } from '@rocket.chat/core-typings';
 import { Rooms } from '@rocket.chat/models';
 import type { ServerMethods } from '@rocket.chat/ui-contexts';
 import { Meteor } from 'meteor/meteor';
 
 import { TranslationProviderRegistry } from '..';
+import { broadcastMessageSentEvent } from '../../../../server/modules/watchers/lib/messages';
 
 declare module '@rocket.chat/ui-contexts' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
@@ -24,6 +26,10 @@ Meteor.methods<ServerMethods>({
 		const room = await Rooms.findOneById(message?.rid);
 		if (message && room) {
 			await TranslationProviderRegistry.translateMessage(message, room, targetLanguage);
+			void broadcastMessageSentEvent({
+				id: message._id,
+				broadcastCallback: (message) => api.broadcast('message.sent', message),
+			});
 		}
 	},
 });
