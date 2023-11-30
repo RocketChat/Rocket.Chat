81110a21a95fd23be75e49ef685b59ec0ae13711
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-11-30 16:42:37 -0300

    fix

--- apps/meteor/client/views/admin/settings/inputs/MultiSelectSettingInput.tsx
+++ apps/meteor/client/views/admin/settings/inputs/MultiSelectSettingInput.tsx
@@ -40,7 +40,7 @@ function MultiSelectSettingInput({
 		onChangeValue?.(value);
 		// onChangeValue && onChangeValue([...event.currentTarget.querySelectorAll('option')].filter((e) => e.selected).map((el) => el.value));
 	};
-	const Component = autocomplete ? MultiSelectFiltered : MultiSelect;
+	const Component = false ? MultiSelectFiltered : MultiSelect;
 	return (
 		<>
 			<Flex.Container>
@@ -52,6 +52,7 @@ function MultiSelectSettingInput({
 				</Box>
 			</Flex.Container>
 			<Component
+				// display='flex'
 				data-qa-setting-id={_id}
 				id={_id}
 				value={value}
