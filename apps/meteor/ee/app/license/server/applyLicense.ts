64b63153e37d6f8cffaa7975aa0cadcc91cee063
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-11-29 22:55:30 -0300

    fix duplicated license removing the current one

--- apps/meteor/ee/app/license/server/applyLicense.ts
+++ apps/meteor/ee/app/license/server/applyLicense.ts
@@ -2,14 +2,14 @@ import { License } from '@rocket.chat/license';
 
 const applyLicenseBase =
 	(fn: () => Promise<boolean>) =>
-	(license: string, isNewLicense: boolean): Promise<boolean> => {
+	async (license: string, isNewLicense: boolean): Promise<boolean> => {
 		const enterpriseLicense = (license ?? '').trim();
 		if (!enterpriseLicense) {
 			return fn();
 		}
 
 		if (enterpriseLicense === License.encryptedLicense) {
-			return fn();
+			return false;
 		}
 
 		try {
