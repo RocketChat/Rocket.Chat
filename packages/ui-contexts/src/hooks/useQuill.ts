2df8d03fa1a6211aa01f6cae55b172c494aa4664
Fahad Ali <alifahad2210@gmail.com>
2023-11-30 14:31:26 +0530

    use quill formatting

--- packages/ui-contexts/src/hooks/useQuill.ts
+++ packages/ui-contexts/src/hooks/useQuill.ts
@@ -1,29 +1,9 @@
 /* eslint-disable prefer-rest-params */
-import type { QuillOptionsStatic } from 'quill';
 import type Quill from 'quill';
 import type { RefObject } from 'react';
 import { useRef, useState, useEffect } from 'react';
 
-const theme = 'snow';
-
-const modules = {
-	toolbar: [
-		['bold', 'italic', 'underline', 'strike'],
-		[{ align: [] }],
-
-		[{ list: 'ordered' }, { list: 'bullet' }],
-		[{ header: [1, 2, 3, 4, 5, 6, false] }],
-	],
-	clipboard: {
-		matchVisual: false,
-	},
-};
-
-const formats = ['bold', 'italic', 'underline', 'strike', 'align'];
-
 function assign(target: any, _varArgs: any) {
-	'use strict';
-
 	if (target === null || target === undefined) {
 		throw new TypeError('Cannot convert undefined or null to object');
 	}
@@ -44,7 +24,7 @@ function assign(target: any, _varArgs: any) {
 	return to;
 }
 
-export const useQuill = (options: QuillOptionsStatic | undefined = { modules }) => {
+export const useQuill = () => {
 	const quillRef: RefObject<any> = useRef();
 
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -52,8 +32,6 @@ export const useQuill = (options: QuillOptionsStatic | undefined = { modules }) 
 		Quill: undefined as any | undefined,
 		quillRef,
 		quill: undefined as Quill | undefined,
-		editorRef: quillRef,
-		editor: undefined as Quill | undefined,
 	});
 
 	useEffect(() => {
@@ -61,17 +39,17 @@ export const useQuill = (options: QuillOptionsStatic | undefined = { modules }) 
 			setObj((prev) => assign(prev, { Quill: require('quill') }));
 		}
 		if (obj.Quill && !obj.quill && quillRef && quillRef.current && isLoaded) {
-			const opts = assign(options, {
-				modules: assign(modules, options.modules),
-				formats: options.formats || formats,
-				theme: options.theme || theme,
-			});
+			const opts = {
+				modules: { toolbar: '#toolbar' },
+				formats: ['bold', 'italic', 'underline', 'strike', 'list'],
+				theme: 'snow',
+			};
 			const quill = new obj.Quill(quillRef.current, opts);
 
-			setObj(assign(assign({}, obj), { quill, editor: quill }));
+			setObj(assign(assign({}, obj), { quill }));
 		}
 		setIsLoaded(true);
-	}, [isLoaded, obj, options]);
+	}, [isLoaded, obj]);
 
 	return obj;
 };
