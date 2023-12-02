f24b00f40463d9bcdf261d7a175cfedc6d11f30c
sahid shaikh <122199801+sahidshaikh23@users.noreply.github.com>
2023-12-02 18:51:37 +0530

    Update fix-i18n.js

--- apps/meteor/.scripts/fix-i18n.js
+++ apps/meteor/.scripts/fix-i18n.js
@@ -6,23 +6,35 @@
  * - remove all keys not present in source i18n file
  */
 
-const fs = require('fs');
-
+const fs = require('fs').promises;
+const path = require('path');
 const fg = require('fast-glob');
 
-const fixFiles = (path, source, newlineAtEnd = false) => {
-	const sourceFile = JSON.parse(fs.readFileSync(`${path}${source}`, 'utf8'));
-	const sourceKeys = Object.keys(sourceFile);
+const fixFiles = async (dirPath, sourceFileName, newlineAtEnd = false) => {
+    try {
+        const sourceFilePath = path.join(dirPath, sourceFileName);
+        const sourceFileContent = await fs.readFile(sourceFilePath, 'utf8');
+        const sourceKeys = Object.keys(JSON.parse(sourceFileContent));
 
-	fg([`${path}/**/*.i18n.json`]).then((entries) => {
-		entries.forEach((file) => {
-			console.log(file);
+        const entries = await fg([path.join(dirPath, '**/*.i18n.json')]);
 
-			const json = JSON.parse(fs.readFileSync(file, 'utf8'));
+        await Promise.all(
+            entries.map(async (file) => {
+                try {
+                    const jsonContent = await fs.readFile(file, 'utf8');
+                    await fs.writeFile(file, `${JSON.stringify(JSON.parse(jsonContent), null, 2)}${newlineAtEnd ? '\n' : ''}`);
+                } catch (writeError) {
+                    console.error(`Error writing to file ${file}: ${writeError.message}`);
+                }
+            })
+        );
 
-			fs.writeFileSync(file, `${JSON.stringify(json, sourceKeys, 2)}${newlineAtEnd ? '\n' : ''}`);
-		});
-	});
+        console.log('Fixing files completed successfully.');
+    } catch (error) {
+        console.error('Error fixing files:', error.message);
+    }
 };
 
+// Example usage
 fixFiles('./packages/rocketchat-i18n', '/i18n/en.i18n.json');
+
