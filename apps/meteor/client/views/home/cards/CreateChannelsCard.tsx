15d435390ca2131bc52a68738c0ee81ada08b129
juliajforesti <juliajforesti@gmail.com>
2023-12-04 17:25:24 -0300

    chore: replace Card on `CreateChannelsCard`

--- apps/meteor/client/views/home/cards/CreateChannelsCard.tsx
+++ apps/meteor/client/views/home/cards/CreateChannelsCard.tsx
@@ -1,9 +1,8 @@
-import { Button } from '@rocket.chat/fuselage';
-import { Card, CardBody, CardFooter, CardFooterWrapper, CardTitle } from '@rocket.chat/ui-client';
 import { useTranslation, useSetModal } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
+import GenericCard from '../../../components/GenericCard';
 import CreateChannelWithData from '../../../sidebar/header/CreateChannel';
 
 const CreateChannelsCard = (): ReactElement => {
@@ -13,15 +12,12 @@ const CreateChannelsCard = (): ReactElement => {
 	const openCreateChannelModal = (): void => setModal(<CreateChannelWithData onClose={(): void => setModal(null)} />);
 
 	return (
-		<Card data-qa-id='homepage-create-channels-card'>
-			<CardTitle>{t('Create_channels')}</CardTitle>
-			<CardBody>{t('Create_a_public_channel_that_new_workspace_members_can_join')}</CardBody>
-			<CardFooterWrapper>
-				<CardFooter>
-					<Button onClick={openCreateChannelModal}>{t('Create_channel')}</Button>
-				</CardFooter>
-			</CardFooterWrapper>
-		</Card>
+		<GenericCard
+			title={t('Create_channels')}
+			body={t('Create_a_public_channel_that_new_workspace_members_can_join')}
+			controls={[{ onClick: openCreateChannelModal, label: t('Create_channel') }]}
+			data-qa-id='homepage-create-channels-card'
+		/>
 	);
 };
 
