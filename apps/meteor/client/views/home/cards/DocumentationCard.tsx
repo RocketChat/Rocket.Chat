4830d58b68794ae83b3083439620e2718fb5b7c4
juliajforesti <juliajforesti@gmail.com>
2023-12-04 17:25:50 -0300

    chore: replace Card on `DocumentationCard`

--- apps/meteor/client/views/home/cards/DocumentationCard.tsx
+++ apps/meteor/client/views/home/cards/DocumentationCard.tsx
@@ -1,9 +1,8 @@
-import { Button } from '@rocket.chat/fuselage';
-import { Card, CardBody, CardFooter, CardFooterWrapper, CardTitle } from '@rocket.chat/ui-client';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React from 'react';
 
+import GenericCard from '../../../components/GenericCard';
 import { useExternalLink } from '../../../hooks/useExternalLink';
 
 const DOCS_URL = 'https://go.rocket.chat/i/hp-documentation';
@@ -13,15 +12,12 @@ const DocumentationCard = (): ReactElement => {
 	const handleOpenLink = useExternalLink();
 
 	return (
-		<Card data-qa-id='homepage-documentation-card'>
-			<CardTitle>{t('Documentation')}</CardTitle>
-			<CardBody>{t('Learn_how_to_unlock_the_myriad_possibilities_of_rocket_chat')}</CardBody>
-			<CardFooterWrapper>
-				<CardFooter>
-					<Button onClick={() => handleOpenLink(DOCS_URL)}>{t('See_documentation')}</Button>
-				</CardFooter>
-			</CardFooterWrapper>
-		</Card>
+		<GenericCard
+			title={t('Documentation')}
+			body={t('Learn_how_to_unlock_the_myriad_possibilities_of_rocket_chat')}
+			controls={[{ onClick: () => handleOpenLink(DOCS_URL), label: t('See_documentation') }]}
+			data-qa-id='homepage-documentation-card'
+		/>
 	);
 };
 
