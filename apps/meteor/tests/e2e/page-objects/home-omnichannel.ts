5a9ff8762727e3eff92c5532c986cebbe434b91a
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-12-04 16:05:03 -0300

    chore: added agents, managers and monitors to home omnichannel

--- apps/meteor/tests/e2e/page-objects/home-omnichannel.ts
+++ apps/meteor/tests/e2e/page-objects/home-omnichannel.ts
@@ -1,8 +1,11 @@
 import type { Locator, Page } from '@playwright/test';
 
 import { HomeOmnichannelContent, HomeSidenav, HomeFlextab, OmnichannelSidenav } from './fragments';
+import { OmnichannelAgents } from './omnichannel-agents';
 import { OmnichannelCannedResponses } from './omnichannel-canned-responses';
 import { OmnichannelCurrentChats } from './omnichannel-current-chats';
+import { OmnichannelManager } from './omnichannel-manager';
+import { OmnichannelMonitors } from './omnichannel-monitors';
 import { OmnichannelTranscript } from './omnichannel-transcript';
 import { OmnichannelTriggers } from './omnichannel-triggers';
 
@@ -25,6 +28,12 @@ export class HomeOmnichannel {
 
 	readonly cannedResponses: OmnichannelCannedResponses;
 
+	readonly agents: OmnichannelAgents;
+
+	readonly managers: OmnichannelManager;
+
+	readonly monitors: OmnichannelMonitors;
+
 	constructor(page: Page) {
 		this.page = page;
 		this.content = new HomeOmnichannelContent(page);
@@ -35,6 +44,9 @@ export class HomeOmnichannel {
 		this.currentChats = new OmnichannelCurrentChats(page);
 		this.transcript = new OmnichannelTranscript(page);
 		this.cannedResponses = new OmnichannelCannedResponses(page);
+		this.agents = new OmnichannelAgents(page);
+		this.managers = new OmnichannelManager(page);
+		this.monitors = new OmnichannelMonitors(page);
 	}
 
 	get toastSuccess(): Locator {
