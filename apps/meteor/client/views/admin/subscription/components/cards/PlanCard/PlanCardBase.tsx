521706998c42539db9980361fbcced6f908c4d11
juliajforesti <juliajforesti@gmail.com>
2023-12-11 18:08:23 -0300

    chore: replace Card on `PlanCardBase`

--- apps/meteor/client/views/admin/subscription/components/cards/PlanCard/PlanCardBase.tsx
+++ apps/meteor/client/views/admin/subscription/components/cards/PlanCard/PlanCardBase.tsx
@@ -1,19 +1,16 @@
-import { Box, Icon, Palette } from '@rocket.chat/fuselage';
-import { Card, CardBody, CardColSection, CardTitle } from '@rocket.chat/ui-client';
+import { Box, Card, CardTitle, CardBody, Icon, Palette } from '@rocket.chat/fuselage';
 import type { ReactElement, ReactNode } from 'react';
 import React from 'react';
 
 const PlanCardBase = ({ name, children }: { name: string; children: ReactNode }): ReactElement => {
 	return (
-		<Card>
+		<Card height='full'>
 			<CardTitle>
 				<Icon name='rocketchat' color={Palette.badge['badge-background-level-4'].toString()} size={28} mie={4} />
 				<Box fontScale='h3'>{name}</Box>
 			</CardTitle>
 			<CardBody>
-				<CardColSection display='flex' flexDirection='column' h='full'>
-					{children}
-				</CardColSection>
+				<div>{children}</div>
 			</CardBody>
 		</Card>
 	);
