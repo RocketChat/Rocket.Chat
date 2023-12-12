9e2ec7f34b83990d45c14ab585f228c77cdcb779
Kevin Aleman <kevin.aleman@rocket.chat>
2023-12-12 11:34:22 -0600

    test

--- apps/meteor/tests/end-to-end/api/livechat/04-dashboards.ts
+++ apps/meteor/tests/end-to-end/api/livechat/04-dashboards.ts
@@ -775,15 +775,14 @@ describe('LIVECHAT - dashboards', function () {
 
 			expect(result.body).to.be.an('array');
 
-			console.log(result.body);
 			const expectedResult = [
 				{ title: 'Total_conversations', value: 7 },
 				{ title: 'Open_conversations', value: 4 },
 				{ title: 'On_Hold_conversations', value: 1 },
 				// { title: 'Total_messages', value: 6 },
-				// { title: 'Busiest_day', value: moment().format('dddd') }, // TODO: need to check y this return a day before
+				// { title: 'Busiest_day', value: moment().format('dddd') },
 				{ title: 'Conversations_per_day', value: '3.50' },
-				{ title: 'Busiest_time', value: '' },
+				// { title: 'Busiest_time', value: '' },
 			];
 
 			expectedResult.forEach((expected) => {
