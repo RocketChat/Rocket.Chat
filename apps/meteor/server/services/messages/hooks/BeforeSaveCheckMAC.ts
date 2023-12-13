d87ab70a722e64ab2dcd04c5bc0c7caaeb6f76d4
Diego Sampaio <chinello@gmail.com>
2023-12-13 12:26:14 -0300

    tests: add tests

--- apps/meteor/server/services/messages/hooks/BeforeSaveCheckMAC.ts
+++ apps/meteor/server/services/messages/hooks/BeforeSaveCheckMAC.ts
@@ -1,14 +1,10 @@
 import { MeteorError, Omnichannel } from '@rocket.chat/core-services';
 import type { IRoom } from '@rocket.chat/core-typings';
-import { isOmnichannelRoom, type IMessage, isEditedMessage } from '@rocket.chat/core-typings';
+import { isOmnichannelRoom, type IMessage } from '@rocket.chat/core-typings';
 
 export class BeforeSaveCheckMAC {
 	async isWithinLimits({ message, room }: { message: IMessage; room: IRoom }): Promise<void> {
 		if (!isOmnichannelRoom(room)) {
-			return;
-		}
-
-		if (isEditedMessage(message)) {
 			return;
 		}
 
