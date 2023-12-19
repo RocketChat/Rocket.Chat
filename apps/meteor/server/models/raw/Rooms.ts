9ec47a276afeeca3f692d0aff024fb4781386900
Diego Sampaio <chinello@gmail.com>
2023-12-19 10:23:20 -0300

    chore: remove Messages model dep of Rooms model

--- apps/meteor/server/models/raw/Rooms.ts
+++ apps/meteor/server/models/raw/Rooms.ts
@@ -1566,20 +1566,26 @@ export class RoomsRaw extends BaseRaw<IRoom> implements IRoomsModel {
 		return this.updateOne(query, update);
 	}
 
-	async resetLastMessageById(_id: IRoom['_id'], lastMessage: IRoom['lastMessage']): Promise<UpdateResult> {
+	async resetLastMessageById(_id: IRoom['_id'], lastMessage?: IRoom['lastMessage'], msgCounter?: number): Promise<UpdateResult> {
 		const query: Filter<IRoom> = { _id };
 
-		const update: UpdateFilter<IRoom> = lastMessage
-			? {
-					$set: {
-						lastMessage,
-					},
-			  }
-			: {
-					$unset: {
-						lastMessage: 1,
-					},
-			  };
+		const update: UpdateFilter<IRoom> = {};
+
+		if (lastMessage) {
+			update.$set = {
+				lastMessage,
+			};
+		} else {
+			update.$unset = {
+				lastMessage: 1,
+			};
+		}
+
+		if (msgCounter) {
+			update.$inc = {
+				msgs: msgCounter,
+			};
+		}
 
 		return this.updateOne(query, update);
 	}
