b3723968ad96804a1a2d45ebf4493a316d2a67d8
Sayan Banerjee <sayanbanerjee2002@gmail.com>
2023-12-23 01:16:22 +0530

    delete previous message index if it exsists

--- apps/meteor/server/models/raw/BaseRaw.ts
+++ apps/meteor/server/models/raw/BaseRaw.ts
@@ -88,6 +88,10 @@ export abstract class BaseRaw<
 			indexes?.push({ ...this.options._updatedAtIndexOptions, key: { _updatedAt: 1 } });
 		}
 
+			if(this.collectionName === 'rocketchat_message' && await this.col.indexExists('msg_text')) {
+			await this.col.dropIndex('msg_text');
+		}
+
 		if (indexes?.length) {
 			if (this.pendingIndexes) {
 				await this.pendingIndexes;
