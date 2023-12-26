3364b21e5de138b22b4804e76c0f3ec098e7d255
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-12-26 16:43:00 -0300

    Fix code check

--- apps/meteor/tests/end-to-end/api/22-push.ts
+++ apps/meteor/tests/end-to-end/api/22-push.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { before, after, describe, it } from 'mocha';
+import { before, describe, it } from 'mocha';
 
 import { getCredentials, api, request, credentials } from '../../data/api-data.js';
 import { updateSetting } from '../../data/permissions.helper';
