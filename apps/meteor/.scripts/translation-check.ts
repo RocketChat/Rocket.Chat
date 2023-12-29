5c9709a5e6ba42b3107d06de0517c8288a2dadf4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-12-29 17:25:32 -0300

    Add check for obsolete placeholder format

--- apps/meteor/.scripts/translation-check.ts
+++ apps/meteor/.scripts/translation-check.ts
@@ -1,9 +1,11 @@
-import fs from 'fs';
-import path from 'path';
+import type { PathLike } from 'node:fs';
+import fs from 'node:fs';
+import { readFile, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { inspect } from 'node:util';
 
 import fg from 'fast-glob';
-
-const regexVar = /__[a-zA-Z_]+__/g;
+import i18next from 'i18next';
 
 const validateKeys = (json: Record<string, string>, usedKeys: { key: string; replaces: string[] }[]) =>
 	usedKeys
@@ -20,27 +22,31 @@ const validateKeys = (json: Record<string, string>, usedKeys: { key: string; rep
 			return prev;
 		}, [] as { key: string; miss: string[] }[]);
 
-const removeMissingKeys = (i18nFiles: string[], usedKeys: { key: string; replaces: string[] }[]) => {
-	i18nFiles.forEach((file) => {
-		const json = JSON.parse(fs.readFileSync(file, 'utf8'));
+const removeMissingKeys = async (i18nFiles: string[], usedKeys: { key: string; replaces: string[] }[]) => {
+	for await (const file of i18nFiles) {
+		const json = await parseFile(file);
 		if (Object.keys(json).length === 0) {
 			return;
 		}
 
 		validateKeys(json, usedKeys).forEach(({ key }) => {
-			json[key] = null;
+			delete json[key];
 		});
 
-		fs.writeFileSync(file, JSON.stringify(json, null, 2));
-	});
+		await writeFile(file, JSON.stringify(json, null, 2), 'utf8');
+	}
 };
 
-const checkUniqueKeys = (content: string, json: Record<string, string>, filename: string) => {
+const hasDuplicatedKeys = (content: string, json: Record<string, string>) => {
 	const matchKeys = content.matchAll(/^\s+"([^"]+)"/gm);
 
 	const allKeys = [...matchKeys];
 
-	if (allKeys.length !== Object.keys(json).length) {
+	return allKeys.length !== Object.keys(json).length;
+};
+
+const checkUniqueKeys = (content: string, json: Record<string, string>, filename: string) => {
+	if (hasDuplicatedKeys(content, json)) {
 		throw new Error(`Duplicated keys found on file ${filename}`);
 	}
 };
@@ -72,13 +78,96 @@ const validate = (i18nFiles: string[], usedKeys: { key: string; replaces: string
 	}
 };
 
-const checkFiles = async (sourcePath: string, sourceFile: string, fix = false) => {
-	const content = fs.readFileSync(path.join(sourcePath, sourceFile), 'utf8');
-	const sourceContent = JSON.parse(content) as Record<string, string>;
+const parseFile = async (path: PathLike) => {
+	const content = await readFile(path, 'utf8');
+	let json: Record<string, string>;
+	try {
+		json = JSON.parse(content);
+	} catch (e) {
+		if (e instanceof SyntaxError) {
+			const matches = /^Unexpected token .* in JSON at position (\d+)$/.exec(e.message);
 
-	checkUniqueKeys(content, sourceContent, sourceFile);
+			if (matches) {
+				const [, positionStr] = matches;
+				const position = parseInt(positionStr, 10);
+				const line = content.slice(0, position).split('\n').length;
+				const column = position - content.slice(0, position).lastIndexOf('\n');
+				throw new SyntaxError(`Invalid JSON on file ${path}:${line}:${column}`);
+			}
+		}
+		throw new SyntaxError(`Invalid JSON on file ${path}: ${e.message}`);
+	}
 
-	const usedKeys = Object.entries(sourceContent)
+	if (hasDuplicatedKeys(content, json)) {
+		throw new SyntaxError(`Duplicated keys found on file ${path}`);
+	}
+
+	return json;
+};
+
+const oldPlaceholderFormat = /__[a-zA-Z_]+__/g;
+
+const checkPlaceholdersFormat = (json: Record<string, string>, path: PathLike) => {
+	const outdatedKeys = Object.entries(json)
+		.map(([key, value]) => ({
+			key,
+			value,
+			placeholders: value.match(oldPlaceholderFormat),
+		}))
+		.filter((outdatedKey): outdatedKey is { key: string; value: string; placeholders: RegExpMatchArray } => !!outdatedKey.placeholders);
+
+	if (outdatedKeys.length > 0) {
+		throw new Error(`Outdated placeholder format on file ${path}: ${outdatedKeys.map((key) => inspect(key, { colors: true })).join(', ')}`);
+	}
+};
+
+const checkMissingPlurals = (json: Record<string, string>, path: PathLike, lng: string) => {
+	const keys = Object.keys(json);
+
+	if (!i18next.isInitialized) {
+		i18next.init({ initImmediate: false });
+	}
+
+	const pluralSuffixes = i18next.services.pluralResolver.getSuffixes(lng) as string[];
+	const allKeys = keys.flatMap((key) => {
+		for (const pluralSuffix of pluralSuffixes) {
+			if (key.endsWith(`_${pluralSuffix}`)) {
+				const normalizedKey = key.slice(0, -pluralSuffix.length);
+				return [normalizedKey, ...pluralSuffixes.map((suffix) => `${normalizedKey}_${suffix}`)];
+			}
+		}
+
+		return [key];
+	});
+
+	if (keys.length !== allKeys.length) {
+		const missingKeys = allKeys.filter((key) => !keys.includes(key));
+
+		throw new Error(`Missing plural keys on file ${path}: ${missingKeys.map((key) => inspect(key, { colors: true })).join(', ')}`);
+	}
+};
+
+const checkFiles = async (sourceDirPath: string, sourceFile: string, fix = false) => {
+	const sourcePath = join(sourceDirPath, sourceFile);
+	const sourceJson = await parseFile(sourcePath);
+
+	checkPlaceholdersFormat(sourceJson, sourcePath);
+	checkMissingPlurals(sourceJson, sourcePath, 'en');
+
+	const i18nFiles = await fg([join(sourceDirPath, `**/*.i18n.json`), `!${sourcePath}`]);
+
+	const translations = await Promise.all(
+		i18nFiles.map(async (path) => ({ path, json: await parseFile(path), lng: /\/(.*).i18n.json$/.exec(path)?.[1] ?? 'FIXME' })),
+	);
+
+	for await (const { path, json, lng } of translations) {
+		checkPlaceholdersFormat(json, path);
+		checkMissingPlurals(json, path, lng);
+	}
+
+	const regexVar = /__[a-zA-Z_]+__/g;
+
+	const usedKeys = Object.entries(sourceJson)
 		.map(([key, value]) => {
 			const replaces = value.match(regexVar);
 			return {
@@ -88,8 +177,6 @@ const checkFiles = async (sourcePath: string, sourceFile: string, fix = false) =
 		})
 		.filter((usedKey): usedKey is { key: string; replaces: RegExpMatchArray } => !!usedKey.replaces);
 
-	const i18nFiles = await fg([`${sourcePath}/**/*.i18n.json`]);
-
 	if (fix) {
 		return removeMissingKeys(i18nFiles, usedKeys);
 	}
@@ -97,11 +184,8 @@ const checkFiles = async (sourcePath: string, sourceFile: string, fix = false) =
 	validate(i18nFiles, usedKeys);
 };
 
-(async () => {
-	try {
-		await checkFiles('./packages/rocketchat-i18n/i18n', 'en.i18n.json', process.argv[2] === '--fix');
-	} catch (e) {
-		console.error(e);
-		process.exit(1);
-	}
-})();
+const fix = process.argv[2] === '--fix';
+checkFiles('./packages/rocketchat-i18n/i18n', 'en.i18n.json', fix).catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
