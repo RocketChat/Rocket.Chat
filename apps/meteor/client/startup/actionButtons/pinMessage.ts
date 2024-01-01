b18131c703ec1fafa2f978653f9d5a22b0172435
Sayan Banerjee <sayanbanerjee2002@gmail.com>
2024-01-01 11:25:35 +0530

    feature: Added modal confirmation before pinning message

--- apps/meteor/client/startup/actionButtons/pinMessage.ts
+++ apps/meteor/client/startup/actionButtons/pinMessage.ts
@@ -4,6 +4,8 @@ import { hasAtLeastOnePermission } from '../../../app/authorization/client';
 import { settings } from '../../../app/settings/client';
 import { MessageAction } from '../../../app/ui-utils/client';
 import { sdk } from '../../../app/utils/client/lib/SDKClient';
+import GenericModal from '../../components/GenericModal/index';
+import { imperativeModal } from '../../lib/imperativeModal';
 import { queryClient } from '../../lib/queryClient';
 import { roomCoordinator } from '../../lib/rooms/roomCoordinator';
 import { dispatchToastMessage } from '../../lib/toast';
@@ -18,13 +20,31 @@ Meteor.startup(() => {
 		context: ['pinned', 'message', 'message-mobile', 'threads', 'direct', 'videoconf', 'videoconf-threads'],
 		async action(_, props) {
 			const { message = messageArgs(this).msg } = props;
-			message.pinned = true;
-			try {
-				await sdk.call('pinMessage', message);
-				queryClient.invalidateQueries(['rooms', message.rid, 'pinned-messages']);
-			} catch (error) {
-				dispatchToastMessage({ type: 'error', message: error });
-			}
+			const onCloseModal = () => {
+				imperativeModal.close();
+			};
+			const onConfirm = async (): Promise<void> => {
+				message.pinned = true;
+				try {
+					await sdk.call('pinMessage', message);
+					queryClient.invalidateQueries(['rooms', message.rid, 'pinned-messages']);
+				} catch (error) {
+					dispatchToastMessage({ type: 'error', message: error });
+				}
+				onCloseModal();
+			};
+
+			imperativeModal.open({
+				component: GenericModal,
+				props: {
+					title: 'Are you sure you want to pin this message?',
+					variant: 'info',
+					confirmText: 'Yes pin it',
+					onConfirm,
+					onClose: onCloseModal,
+					onCancel: onCloseModal,
+				},
+			});
 		},
 		condition({ message, subscription, room }) {
 			if (!settings.get('Message_AllowPinning') || message.pinned || !subscription) {
