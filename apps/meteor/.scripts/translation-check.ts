bdd7ff164cf3e4db75b81ccfdfabab1e09a7204d
Tasso Evangelista <tasso.evangelista@rocket.chat>
2024-01-02 19:57:49 -0300

    Fix plurals detection

--- apps/meteor/.scripts/translation-check.ts
+++ apps/meteor/.scripts/translation-check.ts
@@ -1,5 +1,4 @@
 import type { PathLike } from 'node:fs';
-import fs from 'node:fs';
 import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { inspect } from 'node:util';
@@ -7,36 +6,6 @@ import { inspect } from 'node:util';
 import fg from 'fast-glob';
 import i18next from 'i18next';
 import supportsColor from 'supports-color';
-
-const validateKeys = (json: Record<string, string>, usedKeys: { key: string; replaces: string[] }[]) =>
-	usedKeys
-		.filter(({ key }) => typeof json[key] !== 'undefined')
-		.reduce((prev, cur) => {
-			const { key, replaces } = cur;
-
-			const miss = replaces.filter((replace) => json[key] && json[key].indexOf(replace) === -1);
-
-			if (miss.length > 0) {
-				prev.push({ key, miss });
-			}
-
-			return prev;
-		}, [] as { key: string; miss: string[] }[]);
-
-const removeMissingKeys = async (i18nFiles: string[], usedKeys: { key: string; replaces: string[] }[]) => {
-	for await (const file of i18nFiles) {
-		const json = await parseFile(file);
-		if (Object.keys(json).length === 0) {
-			return;
-		}
-
-		validateKeys(json, usedKeys).forEach(({ key }) => {
-			delete json[key];
-		});
-
-		await writeFile(file, JSON.stringify(json, null, 2), 'utf8');
-	}
-};
 
 const hasDuplicatedKeys = (content: string, json: Record<string, string>) => {
 	const matchKeys = content.matchAll(/^\s+"([^"]+)"/gm);
@@ -46,41 +15,8 @@ const hasDuplicatedKeys = (content: string, json: Record<string, string>) => {
 	return allKeys.length !== Object.keys(json).length;
 };
 
-const checkUniqueKeys = (content: string, json: Record<string, string>, filename: string) => {
-	if (hasDuplicatedKeys(content, json)) {
-		throw new Error(`Duplicated keys found on file ${filename}`);
-	}
-};
-
-const validate = (i18nFiles: string[], usedKeys: { key: string; replaces: string[] }[]) => {
-	const totalErrors = i18nFiles.reduce((errors, file) => {
-		const content = fs.readFileSync(file, 'utf8');
-		const json = JSON.parse(content);
-
-		checkUniqueKeys(content, json, file);
-
-		const result = validateKeys(json, usedKeys);
-
-		if (result.length === 0) {
-			return errors;
-		}
-
-		console.log('\n## File', file, `(${result.length} errors)`);
-
-		result.forEach(({ key, miss }) => {
-			console.log('\n- Key:', key, '\n  Missing variables:', miss.join(', '));
-		});
-
-		return errors + result.length;
-	}, 0);
-
-	if (totalErrors > 0) {
-		throw new Error(`\n${totalErrors} errors found`);
-	}
-};
-
 const parseFile = async (path: PathLike) => {
-	const content = await readFile(path, 'utf8');
+	const content = await readFile(path, 'utf-8');
 	let json: Record<string, string>;
 	try {
 		json = JSON.parse(content);
@@ -106,9 +42,37 @@ const parseFile = async (path: PathLike) => {
 	return json;
 };
 
-const oldPlaceholderFormat = /__[a-zA-Z_]+__/g;
+const insertTranslation = (json: Record<string, string>, refKey: string, [key, value]: [key: string, value: string]) => {
+	const entries = Object.entries(json);
 
-const checkPlaceholdersFormat = (json: Record<string, string>, path: PathLike) => {
+	const refIndex = entries.findIndex(([entryKey]) => entryKey === refKey);
+
+	if (refIndex === -1) {
+		throw new Error(`Reference key ${refKey} not found`);
+	}
+
+	const movingEntries = entries.slice(refIndex + 1);
+
+	for (const [key] of movingEntries) {
+		delete json[key];
+	}
+
+	json[key] = value;
+
+	for (const [key, value] of movingEntries) {
+		json[key] = value;
+	}
+};
+
+const persistFile = async (path: PathLike, json: Record<string, string>) => {
+	const content = JSON.stringify(json, null, 2);
+
+	await writeFile(path, content, 'utf-8');
+};
+
+const oldPlaceholderFormat = /__([a-zA-Z_]+)__/g;
+
+const checkPlaceholdersFormat = async ({ json, path, fix = false }: { json: Record<string, string>; path: PathLike; fix?: boolean }) => {
 	const outdatedKeys = Object.entries(json)
 		.map(([key, value]) => ({
 			key,
@@ -118,11 +82,23 @@ const checkPlaceholdersFormat = (json: Record<string, string>, path: PathLike) =
 		.filter((outdatedKey): outdatedKey is { key: string; value: string; placeholders: RegExpMatchArray } => !!outdatedKey.placeholders);
 
 	if (outdatedKeys.length > 0) {
-		throw new Error(
-			`Outdated placeholder format on file ${path}: ${outdatedKeys
-				.map((key) => inspect(key, { colors: !!supportsColor.stdout }))
-				.join(', ')}`,
-		);
+		const message = `Outdated placeholder format on file ${path}: ${inspect(outdatedKeys, { colors: !!supportsColor.stdout })}`;
+
+		if (fix) {
+			console.warn(message);
+
+			for (const { key, value } of outdatedKeys) {
+				const newValue = value.replace(oldPlaceholderFormat, (_, name) => `{{${name}}}`);
+
+				json[key] = newValue;
+			}
+
+			await persistFile(path, json);
+
+			return;
+		}
+
+		throw new Error(message);
 	}
 };
 
@@ -136,7 +112,7 @@ export const extractSingularKeys = (json: Record<string, string>, lng: string) =
 	const singularKeys = new Set(
 		Object.keys(json).map((key) => {
 			for (const pluralSuffix of pluralSuffixes) {
-				if (key.endsWith(`_${pluralSuffix}`)) {
+				if (key.endsWith(pluralSuffix)) {
 					return key.slice(0, -pluralSuffix.length);
 				}
 			}
@@ -148,87 +124,126 @@ export const extractSingularKeys = (json: Record<string, string>, lng: string) =
 	return [singularKeys, pluralSuffixes] as const;
 };
 
-const checkMissingPlurals = (json: Record<string, string>, path: PathLike, lng: string) => {
+const checkMissingPlurals = async ({
+	json,
+	path,
+	lng,
+	fix = false,
+}: {
+	json: Record<string, string>;
+	path: PathLike;
+	lng: string;
+	fix?: boolean;
+}) => {
 	const [singularKeys, pluralSuffixes] = extractSingularKeys(json, lng);
 
-	const missingPluralKeys: { singularKey: string; pluralKeys: string[] }[] = [];
+	const missingPluralKeys: { singularKey: string; existing: string[]; missing: string[] }[] = [];
 
 	for (const singularKey of singularKeys) {
 		if (singularKey in json) {
 			continue;
 		}
 
-		const pluralKeys = pluralSuffixes.map((suffix) => `${singularKey}_${suffix}`);
+		const pluralKeys = pluralSuffixes.map((suffix) => `${singularKey}${suffix}`);
 
-		const missingOnes = pluralKeys.filter((key) => !(key in json));
+		const existing = pluralKeys.filter((key) => key in json);
+		const missing = pluralKeys.filter((key) => !(key in json));
 
-		if (missingOnes.length > 0) {
-			missingPluralKeys.push({ singularKey, pluralKeys: missingOnes });
+		if (missing.length > 0) {
+			missingPluralKeys.push({ singularKey, existing, missing });
 		}
 	}
 
 	if (missingPluralKeys.length > 0) {
-		throw new Error(`Missing plural keys on file ${path}: ${inspect(missingPluralKeys, { colors: !!supportsColor.stdout })}`);
+		const message = `Missing plural keys on file ${path}: ${inspect(missingPluralKeys, { colors: !!supportsColor.stdout })}`;
+
+		if (fix) {
+			console.warn(message);
+
+			for (const { existing, missing } of missingPluralKeys) {
+				for (const missingKey of missing) {
+					const refKey = existing.slice(-1)[0];
+					const value = json[refKey];
+					insertTranslation(json, refKey, [missingKey, value]);
+				}
+			}
+
+			await persistFile(path, json);
+
+			return;
+		}
+
+		throw new Error(message);
 	}
 };
 
-const checkExceedingKeys = (
-	json: Record<string, string>,
-	path: PathLike,
-	lng: string,
-	sourceJson: Record<string, string>,
-	sourceLng: string,
-) => {
+const checkExceedingKeys = async ({
+	json,
+	path,
+	lng,
+	sourceJson,
+	sourceLng,
+	fix = false,
+}: {
+	json: Record<string, string>;
+	path: PathLike;
+	lng: string;
+	sourceJson: Record<string, string>;
+	sourceLng: string;
+	fix?: boolean;
+}) => {
 	const [singularKeys] = extractSingularKeys(json, lng);
 	const [sourceSingularKeys] = extractSingularKeys(sourceJson, sourceLng);
 
 	const exceedingKeys = [...singularKeys].filter((key) => !sourceSingularKeys.has(key));
 
 	if (exceedingKeys.length > 0) {
-		throw new Error(`Exceeding keys on file ${path}: ${inspect(exceedingKeys, { colors: !!supportsColor.stdout })}`);
+		const message = `Exceeding keys on file ${path}: ${inspect(exceedingKeys, { colors: !!supportsColor.stdout })}`;
+
+		if (fix) {
+			for (const key of exceedingKeys) {
+				delete json[key];
+			}
+
+			await persistFile(path, json);
+
+			return;
+		}
+
+		throw new Error(message);
 	}
 };
 
-const checkFiles = async (sourceDirPath: string, sourceFile: string, fix = false) => {
-	const sourcePath = join(sourceDirPath, sourceFile);
+const checkFiles = async (sourceDirPath: string, sourceLng: string, fix = false) => {
+	const sourcePath = join(sourceDirPath, `${sourceLng}.i18n.json`);
 	const sourceJson = await parseFile(sourcePath);
 
-	checkPlaceholdersFormat(sourceJson, sourcePath);
-	checkMissingPlurals(sourceJson, sourcePath, 'en');
+	await checkPlaceholdersFormat({ json: sourceJson, path: sourcePath, fix });
+	await checkMissingPlurals({ json: sourceJson, path: sourcePath, lng: sourceLng, fix });
 
 	const i18nFiles = await fg([join(sourceDirPath, `**/*.i18n.json`), `!${sourcePath}`]);
 
+	const languageFileRegex = /\/([^\/]*?).i18n.json$/;
 	const translations = await Promise.all(
-		i18nFiles.map(async (path) => ({ path, json: await parseFile(path), lng: /\/(.*).i18n.json$/.exec(path)?.[1] ?? 'FIXME' })),
+		i18nFiles.map(async (path) => {
+			const lng = languageFileRegex.exec(path)?.[1];
+			if (!lng) {
+				throw new Error(`Invalid language file path ${path}`);
+			}
+
+			return { path, json: await parseFile(path), lng };
+		}),
 	);
 
 	for await (const { path, json, lng } of translations) {
-		checkPlaceholdersFormat(json, path);
-		checkMissingPlurals(json, path, lng);
-		checkExceedingKeys(json, path, lng, sourceJson, 'en');
+		await checkPlaceholdersFormat({ json, path, fix });
+		await checkMissingPlurals({ json, path, lng, fix });
+		await checkExceedingKeys({ json, path, lng, sourceJson, sourceLng, fix });
 	}
-
-	const regexVar = /__[a-zA-Z_]+__/g;
-
-	const usedKeys = Object.entries(sourceJson)
-		.map(([key, value]) => {
-			const replaces = value.match(regexVar);
-			return {
-				key,
-				replaces,
-			};
-		})
-		.filter((usedKey): usedKey is { key: string; replaces: RegExpMatchArray } => !!usedKey.replaces);
-
-	if (fix) {
-		return removeMissingKeys(i18nFiles, usedKeys);
-	}
-
-	validate(i18nFiles, usedKeys);
 };
 
 const fix = process.argv[2] === '--fix';
-checkFiles('./packages/rocketchat-i18n/i18n', 'en.i18n.json', fix).catch((e) => {
+checkFiles('./packages/rocketchat-i18n/i18n', 'en', fix).catch((e) => {
 	console.error(e);
 	process.exit(1);
 });
