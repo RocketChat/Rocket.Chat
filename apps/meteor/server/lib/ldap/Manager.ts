0aa947319a91bd6a9862d096b926ef1ce6b74a14
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-01-09 17:11:59 -0300

    Check group before authentication

--- apps/meteor/server/lib/ldap/Manager.ts
+++ apps/meteor/server/lib/ldap/Manager.ts
@@ -200,6 +200,10 @@ export class LDAPManager {
 			}
 
 			const [ldapUser] = users;
+			if (!(await ldap.isUserAcceptedByGroupFilter(escapedUsername, ldapUser.dn))) {
+				throw new Error('User not found');
+			}
+
 			if (!(await ldap.authenticate(ldapUser.dn, password))) {
 				logger.debug(`Wrong password for ${escapedUsername}`);
 				throw new Error('Invalid user or wrong password');
@@ -212,21 +216,6 @@ export class LDAPManager {
 					authLogger.debug(`Bind successful but user ${ldapUser.dn} was not found via search`);
 				}
 			}
-
-			const adminLdap = new LDAPConnection();
-			try {
-				try {
-					await adminLdap.connect();
-					if (!(await adminLdap.isUserAcceptedByGroupFilter(escapedUsername, ldapUser.dn))) {
-						throw new Error('User not in a valid group');
-					}
-				} catch (error) {
-					logger.error(error);
-				}
-			} finally {
-				adminLdap.disconnect();
-			}
-
 			return ldapUser;
 		} catch (error) {
 			logger.error(error);
