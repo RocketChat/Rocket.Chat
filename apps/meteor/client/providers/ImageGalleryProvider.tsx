09fec34b0e1c5c62917adfa4bd526f4061592c79
juliajforesti <juliajforesti@gmail.com>
2024-01-10 17:09:11 -0300

    fix: ImageGalleryProvider selector for quoted image

--- apps/meteor/client/providers/ImageGalleryProvider.tsx
+++ apps/meteor/client/providers/ImageGalleryProvider.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactNode, useEffect, useState } from 'react';
 
 import ImageGallery from '../components/ImageGallery/ImageGallery';
+import ImageGalleryData from '../components/ImageGallery/ImageGalleryData';
 import { ImageGalleryContext } from '../contexts/ImageGalleryContext';
 
 type ImageGalleryProviderProps = {
@@ -9,34 +10,43 @@ type ImageGalleryProviderProps = {
 
 const ImageGalleryProvider = ({ children }: ImageGalleryProviderProps) => {
 	const [imageId, setImageId] = useState<string>();
+	const [quotedImageUrl, setQuotedImageUrl] = useState<string>();
+
+	const handleImageClick = (event: Event) => {
+		const target = event?.target as HTMLElement | null;
+
+		if (target?.closest('.rcx-attachment__details')) {
+			return setQuotedImageUrl(target.dataset.id);
+		}
+		if (target?.classList.contains('gallery-item')) {
+			return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
+		}
+
+		if (target?.classList.contains('gallery-item-container')) {
+			return setImageId(target.dataset.id);
+		}
+		if (target?.classList.contains('gallery-item') && target?.parentElement?.parentElement?.classList.contains('gallery-item-container')) {
+			return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
+		}
+
+		if (target?.classList.contains('rcx-avatar__element') && target?.parentElement?.classList.contains('gallery-item')) {
+			return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
+		}
+	};
 
 	useEffect(() => {
-		document.addEventListener('click', (event: Event) => {
-			const target = event?.target as HTMLElement | null;
-			if (target?.classList.contains('gallery-item')) {
-				return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
-			}
+		document.addEventListener('click', handleImageClick);
 
-			if (target?.classList.contains('gallery-item-container')) {
-				return setImageId(target.dataset.id);
-			}
-			if (
-				target?.classList.contains('gallery-item') &&
-				target?.parentElement?.parentElement?.classList.contains('gallery-item-container')
-			) {
-				return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
-			}
-
-			if (target?.classList.contains('rcx-avatar__element') && target?.parentElement?.classList.contains('gallery-item')) {
-				return setImageId(target.dataset.id || target?.parentElement?.parentElement?.dataset.id);
-			}
-		});
+		return () => document.removeEventListener('click', handleImageClick);
 	}, []);
 
 	return (
 		<ImageGalleryContext.Provider value={{ imageId: imageId || '', isOpen: !!imageId, onClose: () => setImageId(undefined) }}>
 			{children}
-			{!!imageId && <ImageGallery />}
+			{!!quotedImageUrl && (
+				<ImageGallery images={[{ _id: quotedImageUrl, url: quotedImageUrl }]} onClose={() => setQuotedImageUrl(undefined)} />
+			)}
+			{!!imageId && <ImageGalleryData />}
 		</ImageGalleryContext.Provider>
 	);
 };
