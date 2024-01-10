adc741e75ab801b4944beb2832cf7f6d8a72353d
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-01-10 17:55:55 +0530

    email validation regular expression fix

--- apps/meteor/lib/emailValidator.ts
+++ apps/meteor/lib/emailValidator.ts
@@ -1,5 +1,5 @@
 export const validateEmail = (email: string, options: { style: string } = { style: 'basic' }): boolean => {
-	const basicEmailRegex = /^[^\s@]+@[^\s@]+$/;
+	const basicEmailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 	const rfcEmailRegex =
 		/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
