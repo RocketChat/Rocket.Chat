6f44faa6cd942171ed8e4a628c5a911243ab6109
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-01-12 12:37:27 -0300

    change accounts  to async api

--- apps/meteor/app/authentication/server/startup/index.js
+++ apps/meteor/app/authentication/server/startup/index.js
@@ -266,7 +266,8 @@ Accounts.onCreateUser(function (...args) {
 });
 
 const { insertUserDoc } = Accounts;
-const insertUserDocAsync = async function (options, user) {
+
+Accounts.insertUserDoc = async function (options, user) {
 	const globalRoles = new Set();
 
 	if (Match.test(options.globalRoles, [String]) && options.globalRoles.length > 0) {
@@ -307,7 +308,7 @@ const insertUserDocAsync = async function (options, user) {
 		user.roles = [];
 	}
 
-	const _id = insertUserDoc.call(Accounts, options, user);
+	const _id = await insertUserDoc.call(Accounts, options, user);
 
 	user = await Users.findOne({
 		_id,
@@ -366,11 +367,6 @@ const insertUserDocAsync = async function (options, user) {
 	}
 
 	return _id;
-};
-
-Accounts.insertUserDoc = function (...args) {
-	// Depends on meteor support for Async
-	return insertUserDocAsync.call(this, ...args);
 };
 
 const validateLoginAttemptAsync = async function (login) {
