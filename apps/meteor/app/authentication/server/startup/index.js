2aa0c469cd4714b99cc74b30c19ee515d0512774
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-01-12 11:42:33 -0300

    bump METEOR@3.0.3 and node 20.15.1

--- apps/meteor/app/authentication/server/startup/index.js
+++ apps/meteor/app/authentication/server/startup/index.js
@@ -262,7 +262,7 @@ const onCreateUserAsync = async function (options, user = {}) {
 
 Accounts.onCreateUser(function (...args) {
 	// Depends on meteor support for Async
-	return Promise.await(onCreateUserAsync.call(this, ...args));
+	return onCreateUserAsync.call(this, ...args);
 });
 
 const { insertUserDoc } = Accounts;
@@ -370,7 +370,7 @@ const insertUserDocAsync = async function (options, user) {
 
 Accounts.insertUserDoc = function (...args) {
 	// Depends on meteor support for Async
-	return Promise.await(insertUserDocAsync.call(this, ...args));
+	return insertUserDocAsync.call(this, ...args);
 };
 
 const validateLoginAttemptAsync = async function (login) {
@@ -442,7 +442,7 @@ const validateLoginAttemptAsync = async function (login) {
 
 Accounts.validateLoginAttempt(function (...args) {
 	// Depends on meteor support for Async
-	return Promise.await(validateLoginAttemptAsync.call(this, ...args));
+	return validateLoginAttemptAsync.call(this, ...args);
 });
 
 Accounts.validateNewUser((user) => {
