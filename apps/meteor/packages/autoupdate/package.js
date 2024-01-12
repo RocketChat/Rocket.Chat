08bb16dfa26dd734a9ecd3a1f909ad647e590041
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-01-12 11:42:33 -0300

    bump METEOR@3.0-beta.7 and node 20.9.0

--- apps/meteor/packages/autoupdate/package.js
+++ apps/meteor/packages/autoupdate/package.js
@@ -1,6 +1,7 @@
 Package.describe({
+	name: 'autoupdate',
 	summary: 'Update the client when new client code is available',
-	version: '1.8.0',
+	version: '2.0.0-beta300.7',
 });
 
 Package.onUse(function (api) {
