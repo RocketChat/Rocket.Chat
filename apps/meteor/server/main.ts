f7a6ab4e28d873928d903df394ccbe61c951a575
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-01-15 14:32:23 -0300

    import order

--- apps/meteor/server/main.ts
+++ apps/meteor/server/main.ts
@@ -1,38 +1,38 @@
 import './models/startup';
+
 /**
  * ./settings uses top level await, in theory the settings creation
  * and the startup should be done in parallel
  */
 import './settings';
+
+import { libStartup } from '../app/lib/server/startup';
 import { startLicense } from '../ee/app/license/server/startup';
 import { registerEEBroker } from '../ee/server';
 import { configureLogLevel } from './configureLogLevel';
 import { registerServices } from './services/startup';
 import { startup } from './startup';
-
-import './lib/logger/startup';
-
-import '../app/lib/server/startup';
-
 import './importPackages';
 import './methods';
 import './publications';
 import './routes';
-import '../lib/oauthRedirectUriServer';
 
-import './lib/pushConfig';
+await import('./lib/logger/startup');
 
-import './configuration/accounts_meld';
-import './configuration/ldap';
+await import('../lib/oauthRedirectUriServer');
 
-import './stream/stdout';
-import './features/EmailInbox/index';
+await import('./lib/pushConfig');
 
-await (async () => {
-	await configureLogLevel();
-	await registerServices();
-	await import('../app/settings/server');
-	await registerEEBroker();
-	await startup();
-	await startLicense();
-})();
+await import('./configuration/accounts_meld');
+await import('./configuration/ldap');
+
+await import('./stream/stdout');
+await import('./features/EmailInbox/index');
+
+await libStartup();
+await configureLogLevel();
+await registerServices();
+await import('../app/settings/server');
+await registerEEBroker();
+await startup();
+await startLicense();
