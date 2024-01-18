4c558e791f0678388e792f40f3e3dd4ecac4748b
Marcos Defendi <marcos.defendi@rocket.chat>
2024-01-18 14:44:13 -0300

    fix: properties index order

--- apps/meteor/server/models/raw/Sessions.ts
+++ apps/meteor/server/models/raw/Sessions.ts
@@ -984,7 +984,7 @@ export class SessionsRaw extends BaseRaw<ISession> implements ISessionsModel {
 			{ key: { userId: 1, sessionId: 1 } },
 			{ key: { type: 1, year: 1, month: 1, day: 1 } },
 			{ key: { sessionId: 1, instanceId: 1, year: 1, month: 1, day: 1 } },
-			{ key: { instanceId: 1, year: 1, month: 1, day: 1 } },
+			{ key: { year: 1, month: 1, day: 1, instanceId: 1 } },
 			{ key: { _computedAt: 1 }, expireAfterSeconds: 60 * 60 * 24 * 45 },
 			{
 				key: { 'loginToken': 1, 'logoutAt': 1, 'userId': 1, 'device.name': 1, 'device.os.name': 1, 'logintAt': -1 },
