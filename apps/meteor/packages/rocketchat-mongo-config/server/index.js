4985f3f1e4853ef2ad3bcc4c9ad6daf505199e53
Diego Sampaio <chinello@gmail.com>
2024-01-19 12:20:23 -0300

    chore: dont ignoreUndefined

--- apps/meteor/packages/rocketchat-mongo-config/server/index.js
+++ apps/meteor/packages/rocketchat-mongo-config/server/index.js
@@ -19,7 +19,7 @@ tls.DEFAULT_ECDH_CURVE = 'auto';
 const mongoConnectionOptions = {
 	// add retryWrites=false if not present in MONGO_URL
 	...(!process.env.MONGO_URL.includes('retryWrites') && { retryWrites: false }),
-	// ignoreUndefined: false, // TODO evaluate adding this config
+	ignoreUndefined: false,
 
 	// TODO ideally we should call isTracingEnabled(), but since this is a Meteor package we can't :/
 	monitorCommands: ['yes', 'true'].includes(String(process.env.TRACING_ENABLED).toLowerCase()),
