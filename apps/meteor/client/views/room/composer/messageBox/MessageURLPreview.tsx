ab00cc1abb0203ea606e303f7baf7e1928474b87
gustrb <gustavoreisbauer@gmail.com>
2024-01-26 14:34:09 -0300

    chore: Fetching the metadata inside the list
    This is being done so we can control how many `MessageURLPreview` are showing,
    this is useful because we need to render them all in order, but we are never sure how can be
    fetched

--- apps/meteor/client/views/room/composer/messageBox/MessageURLPreview.tsx
+++ apps/meteor/client/views/room/composer/messageBox/MessageURLPreview.tsx
@@ -1,15 +1,7 @@
-import type { IRoom } from '@rocket.chat/core-typings';
-import { useEndpoint } from '@rocket.chat/ui-contexts';
-import { useQuery } from '@tanstack/react-query';
-import React, { memo, useMemo } from 'react';
+import React from 'react';
 import type { ReactElement } from 'react';
 
-type MessageURLPreviewProps = {
-	url: string;
-	roomId: IRoom['_id'];
-};
-
-type URLMeta = {
+export type URLMeta = {
 	ogImage?: string;
 	ogImageHeight?: string;
 	ogImageWidth?: string;
@@ -18,34 +10,11 @@ type URLMeta = {
 	pageTitle?: string;
 };
 
-const MessageURLPreview = ({ url, roomId }: MessageURLPreviewProps): ReactElement => {
-	const query = useMemo(
-		() => ({
-			roomId,
-			url,
-		}),
-		[roomId, url],
-	);
-	const getURLMetadataEndpoint = useEndpoint('GET', '/v1/chat.getURLPreview');
-
-	const { data, isLoading } = useQuery(['urlMetadata', query], async () => getURLMetadataEndpoint(query), {
-		refetchOnWindowFocus: false,
-	});
-
-	if (isLoading) {
-		return <></>;
-	}
-
-	if (!data?.urlPreview.meta || Object.keys(data.urlPreview.meta).length === 0) {
-		return <></>;
-	}
-
-	const { meta }: { meta: URLMeta } = data.urlPreview;
-
+const MessageURLPreview = ({ meta }: { meta: URLMeta }): ReactElement => {
 	// TODO: Use meta to render a preview of the URL
 	console.log(meta);
-
+	// TODO: Style this component
 	return <h1>hello world</h1>;
 };
 
-export default memo(MessageURLPreview);
+export default MessageURLPreview;
--- apps/meteor/client/views/room/composer/messageBox/MessageURLPreviewList.tsx
+++ apps/meteor/client/views/room/composer/messageBox/MessageURLPreviewList.tsx
@@ -1,21 +1,48 @@
-import React, { memo } from 'react';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import React, { memo, useEffect, useState } from 'react';
 import type { ReactElement } from 'react';
 
 import { useRoom } from '../../contexts/RoomContext';
+import type { URLMeta } from './MessageURLPreview';
 import MessageURLPreview from './MessageURLPreview';
 
 type MessageURLPreviewListProps = {
 	urls: string[];
 };
 
+const MAX_URLS_TO_PREVIEW = 5;
+
+function isInternalURL(url: string): boolean {
+	const { hostname } = new URL(url);
+	return hostname === window.location.hostname;
+}
+
 const MessageURLPreviewList = ({ urls }: MessageURLPreviewListProps): ReactElement => {
 	const room = useRoom();
+	const getURLMetadataEndpoint = useEndpoint('GET', '/v1/chat.getURLPreview');
+	const [urlsMetadata, setURLMetadatas] = useState<URLMeta[]>([]);
 
+	useEffect(() => {
+		// We should only fetch metadata for external URLs
+		const promises = urls.filter((url) => !isInternalURL(url)).map((url) => getURLMetadataEndpoint({ url, roomId: room._id }));
+
+		const fetchURLMetadata = async (): Promise<URLMeta[]> => {
+			const result = await Promise.all(promises);
+			return result
+				.map(({ urlPreview }) => urlPreview.meta)
+				.filter((meta) => Object.keys(meta).length > 0)
+				.slice(0, MAX_URLS_TO_PREVIEW);
+		};
+
+		fetchURLMetadata().then((result) => setURLMetadatas(result));
+	}, [getURLMetadataEndpoint, setURLMetadatas, urls, room._id]);
+
+	// TODO: Align all previews to the left and add a margin between them
 	return (
 		<>
-			{urls.map((url, index) => {
-				return <MessageURLPreview key={index} url={url} roomId={room._id} />;
-			})}
+			{urlsMetadata.map((meta, i) => (
+				<MessageURLPreview key={i} meta={meta} />
+			))}
 		</>
 	);
 };
