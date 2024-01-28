5827207c2498eb95a3bfcc46bf23fcdba971c6e3
shivang-16 <shivangyadav121@gmail.com>
2024-01-28 19:20:53 +0530

    added_url_shortner_in_messages for long urls

--- apps/meteor/client/components/message/MessageContentBody.tsx
+++ apps/meteor/client/components/message/MessageContentBody.tsx
@@ -4,6 +4,7 @@ import React, { Suspense } from 'react';
 
 import type { MessageWithMdEnforced } from '../../lib/parseMessageTextToAstMarkdown';
 import GazzodownText from '../GazzodownText';
+import ShortURLInMessage from './content/actions/MessageURLShortner';
 
 type MessageContentBodyProps = Pick<MessageWithMdEnforced, 'mentions' | 'channels' | 'md'> & {
 	searchText?: string;
@@ -13,7 +14,7 @@ const MessageContentBody = ({ mentions, channels, md, searchText }: MessageConte
 	<MessageBody data-qa-type='message-body' dir='auto'>
 		<Suspense fallback={<Skeleton />}>
 			<GazzodownText channels={channels} mentions={mentions} searchText={searchText}>
-				<Markup tokens={md} />
+				<Markup tokens={ShortURLInMessage(md)} />
 			</GazzodownText>
 		</Suspense>
 	</MessageBody>
---
+++ apps/meteor/client/components/message/content/actions/MessageURLShortner.tsx
@@ -0,0 +1,34 @@
+import type { Root } from '@rocket.chat/message-parser';
+
+const ShortURLInMessage = (md: Root) => {
+	return md?.map((mdItem) => {
+		if (mdItem.value && Array.isArray(mdItem.value)) {
+			return {
+				...mdItem,
+				value: mdItem.value.map((valueItem) => {
+					if (valueItem.type === 'LINK' && valueItem.value && Array.isArray(valueItem.value.label)) {
+						return {
+							...valueItem,
+							value: {
+								...valueItem.value,
+								label: valueItem.value.label.map((labelItem) => {
+									if (typeof labelItem.value === 'string') {
+										return {
+											...labelItem,
+											value: labelItem.value.length > 100 ? labelItem.value.substring(0, 100) + '...' : labelItem.value,
+										};
+									}
+									return labelItem;
+								}),
+							},
+						};
+					}
+					return valueItem;
+				}),
+			};
+		}
+		return mdItem;
+	}) as Root;
+};
+
+export default ShortURLInMessage;
