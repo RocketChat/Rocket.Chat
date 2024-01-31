491fe6efc73205c969b554c21c39fb61bbfea1d4
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-01-31 10:25:52 -0300

    Remove meteor check from server-connection endpoints

--- apps/meteor/app/api/server/v1/voip/server-connection.ts
+++ apps/meteor/app/api/server/v1/voip/server-connection.ts
@@ -1,22 +1,17 @@
 import { Voip } from '@rocket.chat/core-services';
-import { Match, check } from 'meteor/check';
+import { isVoipCallServerCheckConnectionProps, isVoipManagementServerCheckConnectionProps } from '@rocket.chat/rest-typings';
 
 import { API } from '../../api';
 
 API.v1.addRoute(
 	'voip/managementServer/checkConnection',
-	{ authRequired: true, permissionsRequired: ['manage-voip-contact-center-settings'] },
+	{
+		authRequired: true,
+		permissionsRequired: ['manage-voip-contact-center-settings'],
+		validateParams: isVoipManagementServerCheckConnectionProps,
+	},
 	{
 		async get() {
-			check(
-				this.queryParams,
-				Match.ObjectIncluding({
-					host: String,
-					port: String,
-					username: String,
-					password: String,
-				}),
-			);
 			const { host, port, username, password } = this.queryParams;
 			return API.v1.success(await Voip.checkManagementConnection(host, port, username, password));
 		},
@@ -25,18 +20,13 @@ API.v1.addRoute(
 
 API.v1.addRoute(
 	'voip/callServer/checkConnection',
-	{ authRequired: true, permissionsRequired: ['manage-voip-contact-center-settings'] },
+	{
+		authRequired: true,
+		permissionsRequired: ['manage-voip-contact-center-settings'],
+		validateParams: isVoipCallServerCheckConnectionProps,
+	},
 	{
 		async get() {
-			check(
-				this.queryParams,
-				Match.ObjectIncluding({
-					websocketUrl: Match.Maybe(String),
-					host: Match.Maybe(String),
-					port: Match.Maybe(String),
-					path: Match.Maybe(String),
-				}),
-			);
 			const { websocketUrl, host, port, path } = this.queryParams;
 			if (!websocketUrl && !(host && port && path)) {
 				return API.v1.failure('Incorrect / Insufficient Parameters');
