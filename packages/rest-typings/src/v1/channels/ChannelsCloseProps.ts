1f80ece362d48dcba5fc2c8fecd10f1616439d3b
Heitor Tanoue <68477006+heitortanoue@users.noreply.github.com>
2024-02-05 12:49:17 -0300

    update schema
    Co-authored-by: Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>

--- packages/rest-typings/src/v1/channels/ChannelsCloseProps.ts
+++ packages/rest-typings/src/v1/channels/ChannelsCloseProps.ts
@@ -5,24 +5,13 @@ const ajv = new Ajv();
 export type ChannelsCloseProps = { roomId: string } | { roomName: string };
 
 const ChannelsClosePropsSchema = {
-	oneOf: [
-		{
-			type: 'object',
-			properties: {
-				roomId: { type: 'string' },
-			},
-			required: ['roomId'],
-			additionalProperties: false,
-		},
-		{
-			type: 'object',
-			properties: {
-				roomName: { type: 'string' },
-			},
-			required: ['roomName'],
-			additionalProperties: false,
-		},
-	],
+	type: 'object',
+	properties: {
+		roomId: { type: 'string' },
+		roomName: { type: 'string' },
+	},
+	oneOf: [{ required: ['roomId'] }, { required: ['roomName'] }],
+	additionalProperties: false,
 };
 
 export const isChannelsCloseProps = ajv.compile<ChannelsCloseProps>(ChannelsClosePropsSchema);
