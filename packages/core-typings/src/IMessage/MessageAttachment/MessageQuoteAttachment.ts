de7d43244d2cf39e80f132f2bb7d61253d56c647
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-02-05 20:58:06 -0300

    Fix MessageQuoteAttachment type

--- packages/core-typings/src/IMessage/MessageAttachment/MessageQuoteAttachment.ts
+++ packages/core-typings/src/IMessage/MessageAttachment/MessageQuoteAttachment.ts
@@ -7,10 +7,11 @@ export type MessageQuoteAttachment = {
 	author_name: string;
 	author_link: string;
 	author_icon: string;
-	message_link?: string;
+	message_link: string;
 	text: string;
 	md?: Root;
 	attachments?: Array<MessageQuoteAttachment>; // TODO this is cauising issues to define a model, see @ts-expect-error at apps/meteor/app/api/server/v1/channels.ts:274
 } & MessageAttachmentBase;
 
-export const isQuoteAttachment = (attachment: MessageAttachment): attachment is MessageQuoteAttachment => 'message_link' in attachment;
+export const isQuoteAttachment = (attachment: MessageAttachment): attachment is MessageQuoteAttachment =>
+	'message_link' in attachment && attachment.message_link !== undefined;
