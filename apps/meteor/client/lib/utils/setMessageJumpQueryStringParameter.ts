c1f604f8ca15424b5e52c809e753cc94c443b618
Sagar <sysagar07@gmail.com>
2024-02-09 00:00:06 +0530

    fix: dynamic slug during search

--- apps/meteor/client/lib/utils/setMessageJumpQueryStringParameter.ts
+++ apps/meteor/client/lib/utils/setMessageJumpQueryStringParameter.ts
@@ -1,13 +1,14 @@
 import type { IMessage } from '@rocket.chat/core-typings';
-
+import { LocationPathname } from '@rocket.chat/ui-contexts';
 import { router } from '../../providers/RouterProvider';
 
 export const setMessageJumpQueryStringParameter = async (msg: IMessage['_id'] | null) => {
 	const { msg: _, ...search } = router.getSearchParameters();
+	const locationPathname = new URL(window.location.href).pathname as LocationPathname
 
 	router.navigate(
 		{
-			pathname: router.getLocationPathname(),
+			pathname: locationPathname,
 			search: msg ? { ...search, msg } : search,
 		},
 		{ replace: true },
