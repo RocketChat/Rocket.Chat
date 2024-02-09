f4bb40b9b9ba445efcdca82abbcc181efc58f476
gustrb <gustavoreisbauer@gmail.com>
2024-02-09 16:26:42 -0300

    fix: Considering the auto join channels inside teams
    Earlier, whenever we had auto join teams, when the user joined, we would not look at the auto-join channels
    inside of it

--- apps/meteor/app/lib/server/functions/addUserToDefaultChannels.ts
+++ apps/meteor/app/lib/server/functions/addUserToDefaultChannels.ts
@@ -10,6 +10,23 @@ export const addUserToDefaultChannels = async function (user: IUser, silenced?: 
 	const defaultRooms = await Rooms.findByDefaultAndTypes(true, ['c', 'p'], {
 		projection: { usernames: 0 },
 	}).toArray();
+
+	const roomsThatAreGoingToBeJoined = new Set(defaultRooms.map((room) => room._id));
+
+	// If any of those are teams, we need to get all the channels that have the auto-join flag as well
+	const teamRooms = defaultRooms.filter((room) => room.teamMain && room.teamId);
+	if (teamRooms.length > 0) {
+		for await (const teamRoom of teamRooms) {
+			const defaultTeamRooms = await Rooms.findDefaultRoomsForTeam(teamRoom.teamId).toArray();
+
+			const defaultTeamRoomsThatWereNotAlreadyAdded = defaultTeamRooms.filter((channel) => !roomsThatAreGoingToBeJoined.has(channel._id));
+
+			defaultTeamRoomsThatWereNotAlreadyAdded.forEach((channel) => roomsThatAreGoingToBeJoined.add(channel._id));
+			// Add the channels to the defaultRooms list
+			defaultRooms.push(...defaultTeamRoomsThatWereNotAlreadyAdded);
+		}
+	}
+
 	for await (const room of defaultRooms) {
 		if (!(await Subscriptions.findOneByRoomIdAndUserId(room._id, user._id, { projection: { _id: 1 } }))) {
 			const autoTranslateConfig = getSubscriptionAutotranslateDefaultConfig(user);
