97b26c46ed2daea52b4d2c5403380b0b3bf3c053
gustrb <gustavoreisbauer@gmail.com>
2024-02-12 18:00:32 -0300

    refactor: Moving the logic to get default channels to its own file

--- apps/meteor/app/lib/server/functions/addUserToDefaultChannels.ts
+++ apps/meteor/app/lib/server/functions/addUserToDefaultChannels.ts
@@ -1,31 +1,14 @@
 import { Message } from '@rocket.chat/core-services';
 import type { IUser } from '@rocket.chat/core-typings';
-import { Subscriptions, Rooms } from '@rocket.chat/models';
+import { Subscriptions } from '@rocket.chat/models';
 
 import { callbacks } from '../../../../lib/callbacks';
 import { getSubscriptionAutotranslateDefaultConfig } from '../../../../server/lib/getSubscriptionAutotranslateDefaultConfig';
+import { getDefaultChannels } from './getDefaultChannels';
 
 export const addUserToDefaultChannels = async function (user: IUser, silenced?: boolean): Promise<void> {
 	await callbacks.run('beforeJoinDefaultChannels', user);
-	const defaultRooms = await Rooms.findByDefaultAndTypes(true, ['c', 'p'], {
-		projection: { usernames: 0 },
-	}).toArray();
-
-	const roomsThatAreGoingToBeJoined = new Set(defaultRooms.map((room) => room._id));
-
-	// If any of those are teams, we need to get all the channels that have the auto-join flag as well
-	const teamRooms = defaultRooms.filter((room) => room.teamMain && room.teamId);
-	if (teamRooms.length > 0) {
-		for await (const teamRoom of teamRooms) {
-			const defaultTeamRooms = await Rooms.findDefaultRoomsForTeam(teamRoom.teamId).toArray();
-
-			const defaultTeamRoomsThatWereNotAlreadyAdded = defaultTeamRooms.filter((channel) => !roomsThatAreGoingToBeJoined.has(channel._id));
-
-			defaultTeamRoomsThatWereNotAlreadyAdded.forEach((channel) => roomsThatAreGoingToBeJoined.add(channel._id));
-			// Add the channels to the defaultRooms list
-			defaultRooms.push(...defaultTeamRoomsThatWereNotAlreadyAdded);
-		}
-	}
+	const defaultRooms = await getDefaultChannels();
 
 	for await (const room of defaultRooms) {
 		if (!(await Subscriptions.findOneByRoomIdAndUserId(room._id, user._id, { projection: { _id: 1 } }))) {
