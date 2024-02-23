b5fe28e809a5be03020001d25fe49650691885f2
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-02-23 12:25:08 -0300

    Deprecate insertOrUpdateUser meteor method

--- apps/meteor/app/lib/server/methods/insertOrUpdateUser.ts
+++ apps/meteor/app/lib/server/methods/insertOrUpdateUser.ts
@@ -3,7 +3,9 @@ import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
 import { twoFactorRequired } from '../../../2fa/server/twoFactorRequired';
+import type { ISaveUserDataParams } from '../functions/saveUser';
 import { saveUser } from '../functions/saveUser';
+import { methodDeprecationLogger } from '../lib/deprecationWarningLogger';
 
 declare module '@rocket.chat/ui-contexts' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
@@ -14,14 +16,17 @@ declare module '@rocket.chat/ui-contexts' {
 
 Meteor.methods<ServerMethods>({
 	insertOrUpdateUser: twoFactorRequired(async (userData) => {
-		check(userData, Object);
+		methodDeprecationLogger.method('insertOrUpdateUser', '8.0.0');
 
-		if (!Meteor.userId()) {
+		check(userData, Object);
+		const userId = Meteor.userId();
+
+		if (!userId) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
 				method: 'insertOrUpdateUser',
 			});
 		}
 
-		return saveUser(Meteor.userId(), userData);
+		return saveUser(userId, userData as ISaveUserDataParams);
 	}),
 });
