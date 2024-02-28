acc886547e388943cdfbcb4e4f40a4a65aa3449c
gustrb <gustavoreisbauer@gmail.com>
2024-02-28 11:10:11 -0300

    fix: Type check failing

--- apps/meteor/tests/data/rooms.helper.js
+++ apps/meteor/tests/data/rooms.helper.js
@@ -1,6 +1,6 @@
 import { api, credentials, request } from './api-data';
 
-export const createRoom = ({ name, type, username, token, agentId, members, extraData, credentials: customCredentials, voipCallDirection = 'inbound' }) => {
+export const createRoom = ({ name, type, username, token, agentId, members, credentials: customCredentials, extraData = undefined, voipCallDirection = 'inbound' }) => {
 	if (!type) {
 		throw new Error('"type" is required in "createRoom.ts" test helper');
 	}
