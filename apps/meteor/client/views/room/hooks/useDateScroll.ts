48d041048c006615b46f9a8485cc40d8fabf5511
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-01 00:31:32 -0300

    chore: remove hardcoded position for message date (#31866)

--- apps/meteor/client/views/room/hooks/useDateScroll.ts
+++ apps/meteor/client/views/room/hooks/useDateScroll.ts
@@ -3,34 +3,43 @@ import { useSafely } from '@rocket.chat/fuselage-hooks';
 import { useCallback, useState } from 'react';
 
 import { withThrottling } from '../../../../lib/utils/highOrderFunctions';
+import { useDateListController } from '../providers/DateListProvider';
 
 type useDateScrollReturn = {
 	bubbleDate: string | undefined;
-	onScroll: (list: Set<HTMLElement>) => void;
-	style: ReturnType<typeof css>;
+	callbackRef: (node: HTMLElement | null) => void;
+	style?: ReturnType<typeof css>;
 	showBubble: boolean;
 	listStyle?: ReturnType<typeof css>;
 };
 
-export const useDateScroll = (offset = 0): useDateScrollReturn => {
+export const useDateScroll = (margin = 8): useDateScrollReturn => {
 	const [bubbleDate, setBubbleDate] = useSafely(
 		useState<{
 			date: string;
 			show: boolean;
+			style?: ReturnType<typeof css>;
 		}>({
 			date: '',
 			show: false,
+			style: undefined,
 		}),
 	);
 
-	const onScroll = useCallback(
-		withThrottling({ wait: 30 })(
-			(() => {
+	const { list } = useDateListController();
+
+	const callbackRef = useCallback(
+		(node: HTMLElement | null) => {
+			if (!node) {
+				return;
+			}
+			const onScroll = (() => {
 				let timeout: ReturnType<typeof setTimeout>;
-				return (elements: Set<HTMLElement>) => {
+				return (elements: Set<HTMLElement>, offset: number) => {
 					clearTimeout(timeout);
 
-					const bubbleOffset = offset + 56;
+					const bubbleOffset = offset;
+
 					// Gets the first non visible message date and sets the bubble date to it
 					const [date, message] = [...elements].reduce((ret, message) => {
 						// Sanitize elements
@@ -49,16 +58,30 @@ export const useDateScroll = (offset = 0): useDateScrollReturn => {
 					}, [] as [string, HTMLElement] | []);
 
 					// We always keep the previous date if we don't have a new one, so when the bubble disappears it doesn't flicker
+
 					setBubbleDate(() => ({
 						date: '',
 						...(date && { date }),
 						show: Boolean(date),
+						style: css`
+							position: absolute;
+							top: ${margin}px;
+							left: 50%;
+							translate: -50%;
+							z-index: 1;
+
+							opacity: 0;
+							transition: opacity 0.6s;
+
+							&.bubble-visible {
+								opacity: 1;
+							}
+						`,
 					}));
 
 					if (message) {
 						const { top } = message.getBoundingClientRect();
-
-						if (top - offset > 0) {
+						if (top < offset && top > 0) {
 							return;
 						}
 					}
@@ -72,34 +95,26 @@ export const useDateScroll = (offset = 0): useDateScrollReturn => {
 						1000,
 					);
 				};
-			})(),
-		),
-		[offset],
+			})();
+			const fn = withThrottling({ wait: 30 })(() => {
+				const offset = node.getBoundingClientRect().top;
+				onScroll(list, offset);
+			});
+
+			node.addEventListener('scroll', fn, { passive: true });
+		},
+		[list, margin, setBubbleDate],
 	);
-
-	const dateBubbleStyle = css`
-		position: absolute;
-		top: ${offset}px;
-		left: 50%;
-		translate: -50%;
-		z-index: 1;
-
-		opacity: 0;
-		transition: opacity 0.6s;
-
-		&.bubble-visible {
-			opacity: 1;
-		}
-	`;
 
 	const listStyle =
 		bubbleDate.show && bubbleDate.date
 			? css`
+					position: relative;
 					& [data-time='${bubbleDate.date.replaceAll(/[-T:.]/g, '').substring(0, 8)}'] {
 						opacity: 0;
 					}
 			  `
 			: undefined;
 
-	return { listStyle, bubbleDate: bubbleDate.date, onScroll, style: dateBubbleStyle, showBubble: Boolean(bubbleDate.show) };
+	return { callbackRef, listStyle, bubbleDate: bubbleDate.date, style: bubbleDate.style, showBubble: Boolean(bubbleDate.show) };
 };
