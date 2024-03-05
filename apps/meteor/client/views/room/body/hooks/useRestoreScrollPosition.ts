3e483c519b8343b9706d8e58aa4c993732bc3729
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-05 17:32:14 -0300

    ..

--- apps/meteor/client/views/room/body/hooks/useRestoreScrollPosition.ts
+++ apps/meteor/client/views/room/body/hooks/useRestoreScrollPosition.ts
@@ -1,31 +1,24 @@
 import type { IRoom } from '@rocket.chat/core-typings';
-import { useMergedRefs } from '@rocket.chat/fuselage-hooks';
 import type { RefObject } from 'react';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback } from 'react';
 
 import { isAtBottom } from '../../../../../app/ui/client/views/app/lib/scrolling';
 import { withThrottling } from '../../../../../lib/utils/highOrderFunctions';
 import { RoomManager } from '../../../../lib/RoomManager';
 
 export function useRestoreScrollPosition(roomId: IRoom['_id']) {
-	const ref = useRef<HTMLElement | null>(null);
-	useEffect(() => {
-		const store = RoomManager.getStore(roomId);
-
-		if (store?.scroll && !store.atBottom) {
-			ref.current?.scrollTo({ top: store.scroll });
-		} else {
-			ref.current?.scrollTo({ top: ref.current?.scrollHeight });
-		}
-	}, [roomId]);
-
 	const refCallback = useCallback(
 		(node: HTMLElement | null) => {
 			if (!node) {
 				return;
 			}
-
 			const store = RoomManager.getStore(roomId);
+
+			if (store?.scroll && !store.atBottom) {
+				node.scrollTo({ top: store.scroll });
+			} else {
+				node.scrollTo({ top: node.scrollHeight });
+			}
 
 			const handleWrapperScroll = withThrottling({ wait: 100 })(() => {
 				store?.update({ scroll: node.scrollTop, atBottom: isAtBottom(node, 50) });
@@ -38,5 +31,5 @@ export function useRestoreScrollPosition(roomId: IRoom['_id']) {
 		[roomId],
 	);
 
-	return useMergedRefs(refCallback, ref) as unknown as RefObject<HTMLElement>;
+	return refCallback as unknown as RefObject<HTMLElement>;
 }
