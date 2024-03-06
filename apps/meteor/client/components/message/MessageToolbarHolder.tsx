1887cabd5ef72e7d906a1cae4ab7b936ec28f72b
dougfabris <devfabris@gmail.com>
2024-03-06 15:17:19 -0300

    fix: `MessageToolbarMenu` not rendering on demand

--- apps/meteor/client/components/message/MessageToolbarHolder.tsx
+++ apps/meteor/client/components/message/MessageToolbarHolder.tsx
@@ -2,10 +2,11 @@ import type { IMessage } from '@rocket.chat/core-typings';
 import { MessageToolbarWrapper } from '@rocket.chat/fuselage';
 import { useQuery } from '@tanstack/react-query';
 import type { ReactElement } from 'react';
-import React, { Suspense, lazy, memo, useState } from 'react';
+import React, { Suspense, lazy, memo, useRef, useState } from 'react';
 
 import type { MessageActionContext } from '../../../app/ui-utils/client/lib/MessageAction';
 import { useChat } from '../../views/room/contexts/ChatContext';
+import { useIsVisible } from '../../views/room/hooks/useIsVisible';
 
 type MessageToolbarHolderProps = {
 	message: IMessage;
@@ -16,7 +17,9 @@ const MessageToolbar = lazy(() => import('./toolbar/MessageToolbar'));
 
 const MessageToolbarHolder = ({ message, context }: MessageToolbarHolderProps): ReactElement => {
 	const chat = useChat();
-	const [showToolbar, setShowToolbar] = useState(false);
+	const ref = useRef(null);
+	const [isVisible] = useIsVisible(ref);
+	const [isToolbarMenuOpen, setIsToolbarMenuOpen] = useState(false);
 
 	const depsQueryResult = useQuery(['toolbox', message._id, context], async () => {
 		const room = await chat?.data.findRoom();
@@ -27,16 +30,18 @@ const MessageToolbarHolder = ({ message, context }: MessageToolbarHolderProps): 
 		};
 	});
 
+	const showToolbar = isVisible || isToolbarMenuOpen;
+
 	return (
-		<MessageToolbarWrapper visible={showToolbar}>
-			{depsQueryResult.isSuccess && depsQueryResult.data.room && (
+		<MessageToolbarWrapper ref={ref} visible={isToolbarMenuOpen}>
+			{showToolbar && depsQueryResult.isSuccess && depsQueryResult.data.room && (
 				<Suspense fallback={null}>
 					<MessageToolbar
 						message={message}
 						messageContext={context}
 						room={depsQueryResult.data.room}
 						subscription={depsQueryResult.data.subscription}
-						onChangeMenuVisibility={setShowToolbar}
+						onChangeMenuVisibility={setIsToolbarMenuOpen}
 					/>
 				</Suspense>
 			)}
