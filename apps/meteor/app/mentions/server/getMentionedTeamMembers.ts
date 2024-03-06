ebae2c96359c8f798ecc9e9d516a02167a82aa12
Ricardo Garim <rswarovsky@gmail.com>
2024-03-06 11:21:51 -0300

    fix: rewrite beforeGetTeamMentions length check and remove useless type casting

--- apps/meteor/app/mentions/server/getMentionedTeamMembers.ts
+++ apps/meteor/app/mentions/server/getMentionedTeamMembers.ts
@@ -5,10 +5,10 @@ import { callbacks } from '../../../lib/callbacks';
 import { settings } from '../../settings/server';
 
 const beforeGetTeamMentions = async (mentionIds: string[], teamMentions?: MessageMention[]): Promise<string[]> => {
-	if (teamMentions?.length) return mentionIds;
+	if (!teamMentions?.length) return mentionIds;
 	const teamsIds = teamMentions?.map(({ _id }) => _id);
-	const members = await Team.getMembersByTeamIds(teamsIds as string[], { projection: { userId: 1 } } as { projection: { userId: 1 } });
-	return [...new Set([...mentionIds, ...members.map(({ userId }: { userId: string }) => userId)])];
+	const members = await Team.getMembersByTeamIds(teamsIds, { projection: { userId: 1 } } as { projection: { userId: 1 } });
+	return [...new Set([...mentionIds, ...members.map((data) => data.userId)])];
 };
 
 settings.watch<boolean>('Troubleshoot_Disable_Teams_Mention', (value) => {
