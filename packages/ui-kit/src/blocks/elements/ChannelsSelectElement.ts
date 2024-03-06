3c3e734a4086287fb266d2e6f22c60dad1252650
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2024-03-06 16:38:23 -0300

    fix

--- packages/ui-kit/src/blocks/elements/ChannelsSelectElement.ts
+++ packages/ui-kit/src/blocks/elements/ChannelsSelectElement.ts
@@ -3,5 +3,5 @@ import type { PlainText } from '../text/PlainText';
 
 export type ChannelsSelectElement = Actionable<{
 	type: 'channels_select';
-	placeholder: PlainText;
+	placeholder?: PlainText;
 }>;
