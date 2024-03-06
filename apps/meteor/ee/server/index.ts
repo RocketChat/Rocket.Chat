fa525b400a5dec56a45c23711a0ceecb9439441d
Pierre <pierre.lehnen@rocket.chat>
2024-03-06 13:52:35 -0300

    missing import

--- apps/meteor/ee/server/index.ts
+++ apps/meteor/ee/server/index.ts
@@ -12,5 +12,6 @@ import './requestSeatsRoute';
 import './configuration/index';
 import './local-services/ldap/service';
 import './methods/getReadReceipts';
+import './patches';
 
 export { registerEEBroker } from './startup';
