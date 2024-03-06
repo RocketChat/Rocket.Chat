f9ee3cd775c28eb42908f9ea2660382c3421cafb
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-06 13:15:54 -0300

    chore: create useListIsAtBottom hook (#31902)

---
+++ apps/meteor/client/views/room/body/hooks/useListIsAtBottom.ts
@@ -0,0 +1,29 @@
+import { useCallback, useRef } from 'react';
+
+import { isAtBottom } from '../../../../../app/ui/client/views/app/lib/scrolling';
+import { withThrottling } from '../../../../../lib/utils/highOrderFunctions';
+
+export const useListIsAtBottom = () => {
+	const atBottomRef = useRef(true);
+
+	const ref = useCallback((node: HTMLElement | null) => {
+		if (!node) {
+			return;
+		}
+
+		node.addEventListener(
+			'scroll',
+			withThrottling({ wait: 100 })(() => {
+				atBottomRef.current = isAtBottom(node, 100);
+			}),
+			{
+				passive: true,
+			},
+		);
+	}, []);
+
+	return {
+		atBottomRef,
+		ref,
+	};
+};
