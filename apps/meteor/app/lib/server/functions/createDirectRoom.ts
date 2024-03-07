a6802cacafa37a9e40e22ca7af1bf898a46239dc
Pierre Lehnen <55164754+pierre-lehnen-rc@users.noreply.github.com>
2024-03-07 00:50:54 -0300

    chore: Remove references to EE code from the app events (#31775)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/app/lib/server/functions/createDirectRoom.ts
+++ apps/meteor/app/lib/server/functions/createDirectRoom.ts
@@ -1,3 +1,4 @@
+import { AppEvents, Apps } from '@rocket.chat/apps';
 import { AppsEngineException } from '@rocket.chat/apps-engine/definition/exceptions';
 import type { ISubscriptionExtraData } from '@rocket.chat/core-services';
 import type { ICreatedRoom, IRoom, ISubscription, IUser } from '@rocket.chat/core-typings';
@@ -6,7 +7,6 @@ import { Random } from '@rocket.chat/random';
 import { Meteor } from 'meteor/meteor';
 import type { MatchKeysAndValues } from 'mongodb';
 
-import { Apps } from '../../../../ee/server/apps';
 import { callbacks } from '../../../../lib/callbacks';
 import { isTruthy } from '../../../../lib/isTruthy';
 import { settings } from '../../../settings/server';
@@ -104,7 +104,7 @@ export async function createDirectRoom(
 			_USERNAMES: usernames,
 		};
 
-		const prevent = await Apps.triggerEvent('IPreRoomCreatePrevent', tmpRoom).catch((error) => {
+		const prevent = await Apps?.triggerEvent(AppEvents.IPreRoomCreatePrevent, tmpRoom).catch((error) => {
 			if (error.name === AppsEngineException.name) {
 				throw new Meteor.Error('error-app-prevented', error.message);
 			}
@@ -116,7 +116,10 @@ export async function createDirectRoom(
 			throw new Meteor.Error('error-app-prevented', 'A Rocket.Chat App prevented the room creation.');
 		}
 
-		const result = await Apps.triggerEvent('IPreRoomCreateModify', await Apps.triggerEvent('IPreRoomCreateExtend', tmpRoom));
+		const result = await Apps?.triggerEvent(
+			AppEvents.IPreRoomCreateModify,
+			await Apps?.triggerEvent(AppEvents.IPreRoomCreateExtend, tmpRoom),
+		);
 
 		if (typeof result === 'object') {
 			Object.assign(roomInfo, result);
@@ -170,7 +173,7 @@ export async function createDirectRoom(
 
 		await callbacks.run('afterCreateDirectRoom', insertedRoom, { members: roomMembers, creatorId: options?.creator });
 
-		void Apps.triggerEvent('IPostRoomCreate', insertedRoom);
+		void Apps?.triggerEvent(AppEvents.IPostRoomCreate, insertedRoom);
 	}
 
 	return {
