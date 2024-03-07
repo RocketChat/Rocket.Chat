5bfc893c05cd595a9694e26192aca7f55cc60a1a
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-03-07 16:01:10 -0300

    fix: added fallback value for hide livechat watermark setting

--- apps/meteor/app/livechat/server/api/lib/livechat.ts
+++ apps/meteor/app/livechat/server/api/lib/livechat.ts
@@ -172,7 +172,7 @@ export async function settings({ businessUnit = '' }: { businessUnit?: string } 
 				(initSettings.Livechat_message_character_limit || initSettings.Message_MaxAllowedSize),
 			hiddenSystemMessages: initSettings.Livechat_hide_system_messages,
 			livechatLogo: initSettings.Assets_livechat_widget_logo,
-			hideWatermark: initSettings.Livechat_hide_watermark,
+			hideWatermark: initSettings.Livechat_hide_watermark || false,
 		},
 		theme: {
 			title: initSettings.Livechat_title,
