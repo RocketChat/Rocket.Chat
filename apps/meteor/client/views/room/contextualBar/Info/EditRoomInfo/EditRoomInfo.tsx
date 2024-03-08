a28b80340d8f81c5fb338af886f40135aa29a073
juliajforesti <juliajforesti@gmail.com>
2024-03-08 17:55:22 -0300

    chore: move `encrypted` to bottom

--- apps/meteor/client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.tsx
+++ apps/meteor/client/views/room/contextualBar/Info/EditRoomInfo/EditRoomInfo.tsx
@@ -277,29 +277,6 @@ const EditRoomInfo = ({ room, onClickClose, onClickBack }: EditRoomInfoProps) =>
 								</FieldRow>
 							</Field>
 						)}
-						{canViewEncrypted && (
-							<Field>
-								<FieldRow>
-									<FieldLabel htmlFor={encryptedField}>{t('Encrypted')}</FieldLabel>
-									<Controller
-										control={control}
-										name='encrypted'
-										render={({ field: { value, ...field } }) => (
-											<ToggleSwitch
-												id={encryptedField}
-												aria-describedby={`${encryptedField}-hint`}
-												{...field}
-												disabled={!canToggleEncryption || isFederated}
-												checked={value}
-											/>
-										)}
-									/>
-								</FieldRow>
-								<FieldRow>
-									<FieldHint id={`${encryptedField}-hint`}>{t('Encrypted_field_hint')}</FieldHint>
-								</FieldRow>
-							</Field>
-						)}
 						{canViewReadOnly && (
 							<Field>
 								<FieldRow>
@@ -499,6 +476,29 @@ const EditRoomInfo = ({ room, onClickClose, onClickBack }: EditRoomInfoProps) =>
 													/>
 												</FieldRow>
 											</Field>
+											{canViewEncrypted && (
+												<Field>
+													<FieldRow>
+														<FieldLabel htmlFor={encryptedField}>{t('Encrypted')}</FieldLabel>
+														<Controller
+															control={control}
+															name='encrypted'
+															render={({ field: { value, ...field } }) => (
+																<ToggleSwitch
+																	id={encryptedField}
+																	aria-describedby={`${encryptedField}-hint`}
+																	{...field}
+																	disabled={!canToggleEncryption || isFederated}
+																	checked={value}
+																/>
+															)}
+														/>
+													</FieldRow>
+													<FieldRow>
+														<FieldHint id={`${encryptedField}-hint`}>{t('Encrypted_field_hint')}</FieldHint>
+													</FieldRow>
+												</Field>
+											)}
 										</>
 									)}
 								</FieldGroup>
