d6b28f8d0cf8bc4045621fafce56e257676aedc4
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-08 13:19:56 -0300

    chore: improve connecting procedure (#31938)
    * use one single websocket
    
    * lets get down to business

--- packages/livechat/src/providers/SDKProvider.tsx
+++ packages/livechat/src/providers/SDKProvider.tsx
@@ -1,5 +1,7 @@
-import { DDPSDK } from '@rocket.chat/ddp-client';
-import React, { createContext, useEffect, useMemo } from 'react';
+import type { DDPSDK } from '@rocket.chat/ddp-client';
+import React, { createContext, useMemo } from 'react';
+
+import { Livechat } from '../api';
 
 type SDKContextValue = {
 	sdk?: DDPSDK;
@@ -15,12 +17,8 @@ export const useSDK = () => {
 	return context.sdk;
 };
 
-const SDKProvider = ({ children, serverURL }: { serverURL: string; children: React.ReactNode }) => {
-	const sdk = useMemo(() => DDPSDK.create(serverURL), [serverURL]);
-
-	useEffect(() => {
-		void sdk.connection.connect();
-	}, [sdk]);
+const SDKProvider = ({ children }: { serverURL: string; children: React.ReactNode }) => {
+	const sdk = useMemo(() => Livechat, []);
 
 	return <SDKContext.Provider value={{ sdk }}>{children}</SDKContext.Provider>;
 };
