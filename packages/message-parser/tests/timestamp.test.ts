7718be5308a62ae922e21198c06e04ff73334885
Diego Sampaio <chinello@gmail.com>
2024-03-08 18:37:07 -0300

    test: fix timestamps

--- packages/message-parser/tests/timestamp.test.ts
+++ packages/message-parser/tests/timestamp.test.ts
@@ -2,26 +2,26 @@ import { parse } from '../src';
 import { bold, paragraph, plain, strike, timestamp } from '../src/utils';
 
 test.each([
-  [`<t:1708551317642>`, [paragraph([timestamp('1708551317642')])]],
-  [`<t:1708551317642:R>`, [paragraph([timestamp('1708551317642', 'R')])]],
+  [`<t:1708551317>`, [paragraph([timestamp('1708551317')])]],
+  [`<t:1708551317:R>`, [paragraph([timestamp('1708551317', 'R')])]],
   [
-    'hello <t:1708551317642>',
-    [paragraph([plain('hello '), timestamp('1708551317642')])],
+    'hello <t:1708551317>',
+    [paragraph([plain('hello '), timestamp('1708551317')])],
   ],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
 });
 
 test.each([
-  ['<t:1708551317642:I>', [paragraph([plain('<t:1708551317642:I>')])]],
+  ['<t:1708551317:I>', [paragraph([plain('<t:1708551317:I>')])]],
   ['<t:17>', [paragraph([plain('<t:17>')])]],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
 });
 
 test.each([
-  ['~<t:1708551317642>~', [paragraph([strike([timestamp('1708551317642')])])]],
-  ['*<t:1708551317642>*', [paragraph([bold([plain('<t:1708551317642>')])])]],
+  ['~<t:1708551317>~', [paragraph([strike([timestamp('1708551317')])])]],
+  ['*<t:1708551317>*', [paragraph([bold([plain('<t:1708551317>')])])]],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
 });
