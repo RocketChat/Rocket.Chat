ef0e904503998759a99d0ee8aaffb3190fb7ab61
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-09 00:47:24 -0300

    App tsx into fc

--- packages/livechat/src/components/App/App.tsx
+++ packages/livechat/src/components/App/App.tsx
@@ -1,17 +1,19 @@
 import type { ILivechatTrigger } from '@rocket.chat/core-typings';
-import i18next from 'i18next';
-import { Component } from 'preact';
+import type i18next from 'i18next';
+import type { FunctionalComponent } from 'preact';
 import Router, { route } from 'preact-router';
-import { withTranslation } from 'react-i18next';
+import { useEffect } from 'preact/hooks';
+import { useTranslation, withTranslation } from 'react-i18next';
+import '../../i18next';
 
 import type { Department } from '../../definitions/departments';
 import { setInitCookies } from '../../helpers/cookies';
 import { isRTL } from '../../helpers/isRTL';
 import { visibility } from '../../helpers/visibility';
 import history from '../../history';
-import Connection from '../../lib/connection';
 import CustomFields from '../../lib/customFields';
 import Hooks from '../../lib/hooks';
+import { loadConfig } from '../../lib/main';
 import { parentCall } from '../../lib/parentCall';
 import Triggers from '../../lib/triggers';
 import userPresence from '../../lib/userPresence';
@@ -23,8 +25,8 @@ import Register from '../../routes/Register';
 import SwitchDepartment from '../../routes/SwitchDepartment';
 import TriggerMessage from '../../routes/TriggerMessage';
 import type { Dispatch, StoreState } from '../../store';
+import { useStore } from '../../store';
 import { ScreenProvider } from '../Screen/ScreenProvider';
-import { loadConfig } from '../../lib/main';
 
 type AppProps = {
 	config: {
@@ -66,34 +68,75 @@ type AppProps = {
 	i18n: typeof i18next;
 };
 
-type AppState = {
-	initialized: boolean;
-	poppedOut: boolean;
-};
+export const App: FunctionalComponent<AppProps> = ({
+	config: {
+		settings: { registrationForm, nameFieldRegistrationForm, emailFieldRegistrationForm, forceAcceptDataProcessingConsent: gdprRequired },
+		online,
+		departments,
+	},
+	gdpr: { accepted: gdprAccepted },
+	user,
+}) => {
+	const { t } = useTranslation();
 
-export class App extends Component<AppProps, AppState> {
-	state = {
-		initialized: false,
-		poppedOut: false,
-	};
+	const {
+		dispatch,
+		minimized,
+		iframe: { visible },
+		config: { theme },
+	} = useStore();
 
-	protected handleRoute = async ({ url }: { url: string }) => {
+	useEffect(() => {
+		document.dir = isRTL(t('yes')) ? 'rtl' : 'ltr';
+	}, [t]);
+
+	useEffect(() => {
+		loadConfig();
+
+		// const {
+		// 	config: { online, enabled },
+		// } = useStore();
+
+		// if (online && enabled) {
+		// 	Triggers.init();
+		// }
+
+		Triggers.processTriggers();
+
+		const handleVisibilityChange = () => {
+			dispatch(({ visible }) => ({
+				visible: !visible,
+			}));
+		};
+
+		parentCall(minimized ? 'minimizeWindow' : 'restoreWindow');
+		parentCall(visible ? 'showWidget' : 'hideWidget');
+		parentCall('setWidgetPosition', theme.position || 'right');
+
+		visibility.addListener(handleVisibilityChange);
+
+		handleVisibilityChange();
+
+		window.addEventListener('beforeunload', () => {
+			visibility.removeListener(handleVisibilityChange);
+			dispatch({ minimized: true, undocked: false });
+		});
+	}, [dispatch]);
+
+	useEffect(() => {
+		CustomFields.init();
+		userPresence.init();
+		Hooks.init();
+
+		parentCall('ready');
+		return () => {
+			CustomFields.reset();
+			userPresence.reset();
+		};
+	}, []);
+
+	const handleRoute = async ({ url }: { url: string }) => {
 		setTimeout(() => {
-			const {
-				config: {
-					settings: {
-						registrationForm,
-						nameFieldRegistrationForm,
-						emailFieldRegistrationForm,
-						forceAcceptDataProcessingConsent: gdprRequired,
-					},
-					online,
-					departments,
-				},
-				gdpr: { accepted: gdprAccepted },
-				user,
-			} = this.props;
-
 			setInitCookies();
 
 			if (gdprRequired && !gdprAccepted) {
@@ -115,104 +158,19 @@ export class App extends Component<AppProps, AppState> {
 		}, 100);
 	};
 
-	protected handleTriggers() {
-		const {
-			config: { online, enabled },
-		} = this.props;
-		if (online && enabled) {
-			Triggers.init();
-		}
-
-		Triggers.processTriggers();
-	}
-
-	protected handleVisibilityChange = async () => {
-		const { dispatch } = this.props;
-		dispatch({ visible: !visibility.hidden });
-	};
-
-	protected handleLanguageChange = () => {
-		this.forceUpdate();
-	};
-
-	protected initWidget() {
-		const {
-			minimized,
-			iframe: { visible },
-			config: { theme },
-			dispatch,
-		} = this.props;
-
-		parentCall(minimized ? 'minimizeWindow' : 'restoreWindow');
-		parentCall(visible ? 'showWidget' : 'hideWidget');
-		parentCall('setWidgetPosition', theme.position || 'right');
-
-		visibility.addListener(this.handleVisibilityChange);
-
-		this.handleVisibilityChange();
-
-		window.addEventListener('beforeunload', () => {
-			visibility.removeListener(this.handleVisibilityChange);
-			dispatch({ minimized: true, undocked: false });
-		});
-
-		i18next.on('languageChanged', this.handleLanguageChange);
-	}
-
-	protected async initialize() {
-		// TODO: split these behaviors into composable components
-		await import('../../i18next');
-		await loadConfig();
-		CustomFields.init();
-		userPresence.init();
-		Hooks.init();
-		this.handleTriggers();
-		this.initWidget();
-		this.setState({ initialized: true });
-		parentCall('ready');
-	}
-
-	protected async finalize() {
-		CustomFields.reset();
-		userPresence.reset();
-		visibility.removeListener(this.handleVisibilityChange);
-	}
-
-	componentDidMount() {
-		this.initialize();
-	}
-
-	componentWillUnmount() {
-		this.finalize();
-	}
-
-	componentDidUpdate() {
-		const { i18n } = this.props;
-
-		if (i18n.t) {
-			document.dir = isRTL(i18n.t('yes')) ? 'rtl' : 'ltr';
-		}
-	}
-
-	render = (_: AppProps, { initialized }: AppState) => {
-		if (!initialized) {
-			return null;
-		}
-
-		return (
-			<ScreenProvider>
-				<Router history={history} onChange={this.handleRoute}>
-					<ChatConnector path='/' default />
-					<ChatFinished path='/chat-finished' />
-					<GDPRAgreement path='/gdpr' />
-					<LeaveMessage path='/leave-message' />
-					<Register path='/register' />
-					<SwitchDepartment path='/switch-department' />
-					<TriggerMessage path='/trigger-messages' />
-				</Router>
-			</ScreenProvider>
-		);
-	};
-}
+	return (
+		<ScreenProvider>
+			<Router history={history} onChange={handleRoute}>
+				<ChatConnector path='/' default />
+				<ChatFinished path='/chat-finished' />
+				<GDPRAgreement path='/gdpr' />
+				<LeaveMessage path='/leave-message' />
+				<Register path='/register' />
+				<SwitchDepartment path='/switch-department' />
+				<TriggerMessage path='/trigger-messages' />
+			</Router>
+		</ScreenProvider>
+	);
+};
 
 export default withTranslation()(App);
