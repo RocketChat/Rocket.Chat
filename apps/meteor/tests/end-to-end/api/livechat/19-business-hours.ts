c6efdbae4f35f8f4ab515ab632d5d531e7f8910d
Kevin Aleman <kaleman960@gmail.com>
2024-03-12 07:20:06 -0600

    Update apps/meteor/tests/end-to-end/api/livechat/19-business-hours.ts

--- apps/meteor/tests/end-to-end/api/livechat/19-business-hours.ts
+++ apps/meteor/tests/end-to-end/api/livechat/19-business-hours.ts
@@ -308,7 +308,7 @@ describe('LIVECHAT - business hours', function () {
 			defaultBusinessHour = await getDefaultBusinessHour();
 
 			// close default business hour
-			await openOrCloseBusinessHour(defaultBusinessHour, false);
+			await openOrCloseBusinessHour(defaultBusinessHour, true);
 		});
 
 		it('should create a custom business hour which is closed by default', async () => {
