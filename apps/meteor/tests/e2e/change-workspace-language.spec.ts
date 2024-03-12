3be646016730a8427b5d1056e80063f93edc572d
csuarez <carlos.suarez@rocket.chat>
2024-03-12 14:16:41 +0100

    Fix tests

--- apps/meteor/tests/e2e/change-workspace-language.spec.ts
+++ apps/meteor/tests/e2e/change-workspace-language.spec.ts
@@ -21,45 +21,36 @@ test.describe.serial('setting-language', () => {
 
   test('Change workspace language', async ({ page }) => {
     const languageDropdownSelector = page.locator('button#Language');
-    const languageOption = page.locator('text=Spanish');
+    const languageOption = page.locator('text=Brazilian Portuguese');
     const saveChangesButton = page.locator('button[type="submit"]');
-    const welcomeTextLocator = page.locator('[data-qa-id="homepage-welcome-text"]');
+    const settingsPageTitle = page.locator('[data-qa-type="PageHeader-title"]');
 
-    await test.step('Expect workspace language to be in English initially', async () => {
-      await page.goto('/home');
-      await expect(welcomeTextLocator).toHaveText(/Welcome to.*/);
-    });
+    await page.goto('/admin/settings/General');
+    await expect(settingsPageTitle).toHaveText(/General.*/);
+    await languageDropdownSelector.click();
+    await languageOption.click();
+    await saveChangesButton.click();
 
-    await test.step('Change language from English to Spanish', async () => {
-      await page.goto('/admin/settings/General');
-      await languageDropdownSelector.click();
-      await languageOption.click();
-      await saveChangesButton.click();
-      await page.waitForTimeout(3000);
-
-      await page.goto('/home');
-      await expect(welcomeTextLocator).toHaveText(/Te damos la bienvenida a.*/);
-    });
+    await expect(settingsPageTitle).toHaveText(/Geral.*/);
   });
 
   test('Change workspace language to default language', async ({ page, api }) => {
-    const welcomeTextLocator = page.locator('[data-qa-id="homepage-welcome-text"]');
     const resetButton = page.locator('[data-qa-reset-setting-id="Language"]');
     const saveChangesButton = page.locator('button[type="submit"]');
+    const settingsPageTitle = page.locator('[data-qa-type="PageHeader-title"]');
 
-    await test.step('Change language to Spanish first', async () => {
-      const response = await api.post('/settings/Language', { value: 'es' });
+    await test.step('Change language to Portuguese first', async () => {
+      const response = await api.post('/settings/Language', { value: 'pt' });
       expect(response.status()).toBe(200);
     });
 
     await test.step('Change language to default language', async () => {
       await page.goto('/admin/settings/General');
+      await expect(settingsPageTitle).toHaveText(/Geral.*/);
       await resetButton.click();
       await saveChangesButton.click();
-      await page.waitForTimeout(3000);
 
-      await page.goto('/home');
-      await expect(welcomeTextLocator).toHaveText(/Welcome to.*/);
+      await expect(settingsPageTitle).toHaveText(/General.*/);
     });
   });
 });
