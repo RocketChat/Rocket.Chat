bc8ea57d106ec64da6b64abe903f79c8982be5c9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-13 14:28:23 -0300

    regression: adjusted trigger flags

--- packages/livechat/src/lib/room.js
+++ packages/livechat/src/lib/room.js
@@ -241,7 +241,7 @@ export const onMessage = async (originalMessage) => {
 	await doPlaySound(message);
 };
 
-export const getGreetingMessages = (messages) => messages && messages.filter((msg) => msg.trigger && msg.triggerAfterRegistration);
+export const getGreetingMessages = (messages) => messages && messages.filter((msg) => msg.trigger);
 export const getLatestCallMessage = (messages) => messages && messages.filter((msg) => isVideoCallMessage(msg)).pop();
 
 export const loadMessages = async () => {
