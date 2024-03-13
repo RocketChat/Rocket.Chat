462bc9036a6ab6cb72ef530f50c5e98e2122dcbe
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-03-13 17:07:13 -0300

    Allow redirecting to referer on SAML login

--- apps/meteor/client/views/root/SAMLLoginRoute.tsx
+++ apps/meteor/client/views/root/SAMLLoginRoute.tsx
@@ -1,26 +1,36 @@
-import { useRouter, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import { useRouter, useSetting, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
 import { Meteor } from 'meteor/meteor';
 import { useEffect } from 'react';
 
 const SAMLLoginRoute = () => {
+	const siteUrl = useSetting<string>('Site_Url');
 	const router = useRouter();
 	const dispatchToastMessage = useToastMessageDispatch();
 
 	useEffect(() => {
 		const { token } = router.getRouteParameters();
+		const { redirectUrl } = router.getSearchParameters();
+
+		if (!siteUrl) {
+			return;
+		}
 		Meteor.loginWithSamlToken(token, (error?: unknown) => {
 			if (error) {
 				dispatchToastMessage({ type: 'error', message: error });
 			}
 
-			router.navigate(
-				{
-					pathname: '/home',
-				},
-				{ replace: true },
-			);
+			if (redirectUrl?.startsWith(siteUrl)) {
+				window.location.href = decodeURIComponent(redirectUrl);
+			} else {
+				router.navigate(
+					{
+						pathname: '/home',
+					},
+					{ replace: true },
+				);
+			}
 		});
-	}, [dispatchToastMessage, router]);
+	}, [dispatchToastMessage, router, siteUrl]);
 
 	return null;
 };
