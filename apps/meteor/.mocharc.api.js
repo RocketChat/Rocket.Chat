479abcc1f310f809392a01cc200cdc8451b47081
Rafael Tapia <rafael.tapia@rocket.chat>
2024-03-20 14:34:36 -0300

    test: set timeout to original value

--- apps/meteor/.mocharc.api.js
+++ apps/meteor/.mocharc.api.js
@@ -6,7 +6,7 @@
 
 module.exports = {
 	...require('./.mocharc.base.json'), // see https://github.com/mochajs/mocha/issues/3916
-	timeout: 15000,
+	timeout: 10000,
 	bail: true,
 	file: 'tests/end-to-end/teardown.js',
 	spec: ['tests/end-to-end/api/**/*', 'tests/end-to-end/apps/*'],
