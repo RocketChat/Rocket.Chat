e5fe07a2888cb9735fd8d5b4cae906681c045567
rique223 <henrique.jobs1@gmail.com>
2024-03-26 16:57:36 -0300

    refactor: :sparkles: Implement hide custom fields button
    Implemented a 'hide/show custom fields' button in the new user form and refactored it to follow new figma specs.

--- apps/meteor/client/views/admin/users/AdminUserForm.tsx
+++ apps/meteor/client/views/admin/users/AdminUserForm.tsx
@@ -1,4 +1,4 @@
-import type { AvatarObject, IUser, Serialized } from '@rocket.chat/core-typings';
+import type { AvatarObject, IRole, IUser, Serialized } from '@rocket.chat/core-typings';
 import {
 	Field,
 	FieldLabel,
@@ -11,10 +11,8 @@ import {
 	Box,
 	ToggleSwitch,
 	Icon,
-	Divider,
 	FieldGroup,
 	ContextualbarFooter,
-	ButtonGroup,
 	Button,
 	Callout,
 } from '@rocket.chat/fuselage';
@@ -30,8 +28,8 @@ import {
 	useToastMessageDispatch,
 	useTranslation,
 } from '@rocket.chat/ui-contexts';
-import { useQuery, useMutation } from '@tanstack/react-query';
-import React, { useCallback } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
 import { validateEmail } from '../../../../lib/emailValidator';
@@ -49,21 +47,24 @@ type AdminUserFormProps = {
 	userData?: Serialized<IUser>;
 	onReload: () => void;
 	context: string;
+	refetchUserFormData?: () => void;
+	roleData: { roles: IRole[] } | undefined;
+	roleError: unknown;
 };
 
-export type UserFormProps = Omit<UserCreateParamsPOST & { avatar: AvatarObject; passwordConfirmation: string }, 'fields'>;
+export type userFormProps = Omit<UserCreateParamsPOST & { avatar: AvatarObject; passwordConfirmation: string }, 'fields'>;
 
 const getInitialValue = ({
 	data,
 	defaultUserRoles,
 	isSmtpEnabled,
-	isEditingExistingUser,
+	isNewUserPage,
 }: {
 	data?: Serialized<IUser>;
 	defaultUserRoles?: IUser['roles'];
 	isSmtpEnabled?: boolean;
-	isEditingExistingUser?: boolean;
-}) => ({
+	isNewUserPage?: boolean;
+}): userFormProps => ({
 	roles: data?.roles ?? defaultUserRoles,
 	name: data?.name ?? '',
 	password: '',
@@ -72,20 +73,21 @@ const getInitialValue = ({
 	nickname: data?.nickname ?? '',
 	email: (data?.emails?.length && data.emails[0].address) || '',
 	verified: (data?.emails?.length && data.emails[0].verified) || false,
-	setRandomPassword: false,
-	requirePasswordChange: data?.requirePasswordChange || false,
+	setRandomPassword: isNewUserPage && isSmtpEnabled,
+	requirePasswordChange: isNewUserPage && (data?.requirePasswordChange ?? true),
 	customFields: data?.customFields ?? {},
 	statusText: data?.statusText ?? '',
-	...(!isEditingExistingUser && { joinDefaultChannels: true }),
+	...(isNewUserPage && { joinDefaultChannels: true }),
 	sendWelcomeEmail: isSmtpEnabled,
 	avatar: '' as AvatarObject,
 	passwordConfirmation: '',
 });
 
-const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps) => {
+const AdminUserForm = ({ userData, onReload, context, refetchUserFormData, roleData, roleError, ...props }: AdminUserFormProps) => {
 	const t = useTranslation();
 	const router = useRouter();
 	const dispatchToastMessage = useToastMessageDispatch();
+	const queryClient = useQueryClient();
 
 	const customFieldsMetadata = useAccountsCustomFields();
 	const defaultRoles = useSetting<string>('Accounts_Registration_Users_Default_Roles') || '';
@@ -96,32 +98,24 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 	const isSmtpEnabled = data?.isSMTPConfigured;
 	const isNewUserPage = context === 'new';
 
-	const eventStats = useEndpointAction('POST', '/v1/statistics.telemetry');
-	const updateUserAction = useEndpoint('POST', '/v1/users.update');
-	const createUserAction = useEndpoint('POST', '/v1/users.create');
-
-	const getRoles = useEndpoint('GET', '/v1/roles.list');
-	const { data: roleData, error: roleError } = useQuery(['roles'], async () => getRoles());
-
-	const availableRoles: SelectOption[] = roleData?.roles.map(({ _id, name, description }) => [_id, description || name]) || [];
-
-	const goToUser = useCallback((id) => router.navigate(`/admin/users/info/${id}`), [router]);
-
-	const isEditingExistingUser = Boolean(userData?._id);
-
 	const {
 		control,
 		watch,
 		handleSubmit,
-		reset,
 		formState: { errors, isDirty },
 		setValue,
-	} = useForm<UserFormProps>({
-		defaultValues: getInitialValue({ data: userData, defaultUserRoles, isSmtpEnabled }),
+	} = useForm({
+		defaultValues: getInitialValue({ data: userData, defaultUserRoles, isSmtpEnabled, isNewUserPage }),
 		mode: 'onBlur',
 	});
 
 	const { avatar, username, setRandomPassword, password } = watch();
+
+	const eventStats = useEndpointAction('POST', '/v1/statistics.telemetry');
+	const updateUserAction = useEndpoint('POST', '/v1/users.update');
+	const createUserAction = useEndpoint('POST', '/v1/users.create');
+
+	const availableRoles: SelectOption[] = roleData?.roles.map(({ _id, name, description }) => [_id, description || name]) || [];
 
 	const updateAvatar = useUpdateAvatar(avatar, userData?._id || '');
 
@@ -132,6 +126,7 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 			await updateAvatar();
 			router.navigate(`/admin/users/info/${_id}`);
 			onReload();
+			refetchUserFormData?.();
 		},
 		onError: (error) => {
 			dispatchToastMessage({ type: 'error', message: error });
@@ -140,12 +135,15 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 
 	const handleCreateUser = useMutation({
 		mutationFn: createUserAction,
-		onSuccess: async (data) => {
+		onSuccess: async ({ user: { _id } }) => {
 			dispatchToastMessage({ type: 'success', message: t('User_created_successfully!') });
 			await eventStats({
 				params: [{ eventName: 'updateCounter', settingsId: 'Manual_Entry_User_Count' }],
 			});
-			goToUser(data.user._id);
+			queryClient.invalidateQueries(['pendingUsersCount'], {
+				refetchType: 'all',
+			});
+			router.navigate(`/admin/users/created/${_id}`);
 			onReload();
 		},
 		onError: (error) => {
@@ -153,12 +151,14 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 		},
 	});
 
-	const handleSaveUser = useMutableCallback(async (userFormPayload) => {
-		const { avatar, ...userFormData } = userFormPayload;
-		if (userData?._id) {
+	const handleSaveUser = useMutableCallback(async (userFormPayload: userFormProps) => {
+		const { avatar, passwordConfirmation, ...userFormData } = userFormPayload;
+
+		if (!isNewUserPage && userData?._id) {
 			return handleUpdateUser.mutateAsync({ userId: userData?._id, data: userFormData });
 		}
-		return handleCreateUser.mutateAsync(userFormData);
+
+		return handleCreateUser.mutateAsync({ ...userFormData, fields: '' });
 	});
 
 	const nameId = useUniqueId();
@@ -169,11 +169,16 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 	const bioId = useUniqueId();
 	const nicknameId = useUniqueId();
 	const passwordId = useUniqueId();
-	const requirePasswordChangeId = useUniqueId();
-	const setRandomPasswordId = useUniqueId();
 	const rolesId = useUniqueId();
 	const joinDefaultChannelsId = useUniqueId();
 	const sendWelcomeEmailId = useUniqueId();
+	const setRandomPasswordId = useUniqueId();
+
+	const [showCustomFields, setShowCustomFields] = useState(true);
+
+	if (!context) {
+		return null;
+	}
 
 	return (
 		<>
@@ -325,6 +330,76 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 						)}
 					</Field>
 					<Field>
+						<FieldLabel htmlFor={rolesId}>{t('Roles')}</FieldLabel>
+						<FieldRow>
+							{roleError && <Callout>{roleError}</Callout>}
+							{!roleError && (
+								<Controller
+									control={control}
+									name='roles'
+									rules={{ required: t('The_field_is_required', t('Roles')) }}
+									render={({ field: { onChange, value } }) => (
+										<MultiSelectFiltered
+											id={rolesId}
+											value={value}
+											onChange={onChange}
+											flexGrow={1}
+											placeholder={t('Select_role')}
+											options={availableRoles}
+										/>
+									)}
+								/>
+							)}
+						</FieldRow>
+						{errors?.roles && <FieldError>{errors.roles.message}</FieldError>}
+					</Field>
+					<Field>
+						<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' flexGrow={1}>
+							<FieldLabel htmlFor={joinDefaultChannelsId}>{t('Join_default_channels')}</FieldLabel>
+							<FieldRow>
+								<Controller
+									control={control}
+									name='joinDefaultChannels'
+									render={({ field: { ref, onChange, value } }) => (
+										<ToggleSwitch id={joinDefaultChannelsId} ref={ref} onChange={onChange} checked={value} />
+									)}
+								/>
+							</FieldRow>
+						</Box>
+					</Field>
+					<Field>
+						<Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' flexGrow={1} mbe={8}>
+							<FieldLabel htmlFor={sendWelcomeEmailId} disabled={!isSmtpEnabled}>
+								{t('Send_welcome_email')}
+							</FieldLabel>
+							<FieldRow>
+								{isSmtpStatusAvailable && (
+									<Controller
+										control={control}
+										name='sendWelcomeEmail'
+										defaultValue={isSmtpEnabled}
+										render={({ field: { onChange, value } }) => (
+											<ToggleSwitch
+												id={sendWelcomeEmailId}
+												aria-describedby={`${sendWelcomeEmailId}-hint`}
+												onChange={onChange}
+												checked={value}
+												disabled={!isSmtpEnabled}
+											/>
+										)}
+									/>
+								)}
+							</FieldRow>
+						</Box>
+						{!isSmtpEnabled && (
+							<FieldHint
+								id={`${sendWelcomeEmailId}-hint`}
+								dangerouslySetInnerHTML={{ __html: t('Send_Email_SMTP_Warning', { url: 'admin/settings/Email' }) }}
+								mbs={0}
+							/>
+						)}
+					</Field>
+					<Field>
 						<FieldLabel htmlFor={statusTextId}>{t('StatusMessage')}</FieldLabel>
 						<FieldRow>
 							<Controller
@@ -339,7 +414,6 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 										aria-invalid={errors.statusText ? 'true' : 'false'}
 										aria-describedby={`${statusTextId}-error`}
 										flexGrow={1}
-										addon={<Icon name='edit' size='x20' />}
 									/>
 								)}
 							/>
@@ -380,175 +454,34 @@ const UserForm = ({ userData, onReload, context, ...props }: AdminUserFormProps)
 					<Field>
 						<FieldLabel htmlFor={nicknameId}>{t('Nickname')}</FieldLabel>
 						<FieldRow>
-							<Controller
-								control={control}
-								name='nickname'
-								render={({ field }) => (
-									<TextInput {...field} id={nicknameId} flexGrow={1} addon={<Icon name='edit' size='x20' alignSelf='center' />} />
-								)}
-							/>
+							<Controller control={control} name='nickname' render={({ field }) => <TextInput {...field} id={nicknameId} flexGrow={1} />} />
 						</FieldRow>
-					</Field>
-				</FieldGroup>
-				<FieldGroup>
-					{/* {!setRandomPassword && (
-						<Field>
-							<FieldLabel htmlFor={passwordId}>{t('Password')}</FieldLabel>
-							<FieldRow>
-								<Controller
-									control={control}
-									name='password'
-									rules={{ required: !isEditingExistingUser && t('The_field_is_required', t('Password')) }}
-									render={({ field }) => (
-										<PasswordInput
-											{...field}
-											id={passwordId}
-											aria-invalid={errors.password ? 'true' : 'false'}
-											aria-describedby={`${passwordId}-error`}
-											error={errors.password?.message}
-											flexGrow={1}
-											addon={<Icon name='key' size='x20' />}
-										/>
-									)}
-								/>
-							</FieldRow>
-							{errors?.password && (
-								<FieldError aria-live='assertive' id={`${passwordId}-error`}>
-									{errors.password.message}
-								</FieldError>
-							)}
-						</Field>
-					)}
-					<Field>
-						<FieldRow>
-							<FieldLabel htmlFor={requirePasswordChangeId}>{t('Require_password_change')}</FieldLabel>
-							<Controller
-								control={control}
-								name='requirePasswordChange'
-								render={({ field: { ref, onChange, value } }) => (
-									<ToggleSwitch
-										ref={ref}
-										id={requirePasswordChangeId}
-										disabled={setRandomPassword}
-										checked={setRandomPassword || value}
-										onChange={onChange}
-									/>
-								)}
-							/>
-						</FieldRow>
-					</Field>
-					<Field>
-						<FieldRow>
-							<FieldLabel htmlFor={setRandomPasswordId}>{t('Set_random_password_and_send_by_email')}</FieldLabel>
-							<Controller
-								control={control}
-								name='setRandomPassword'
-								render={({ field: { ref, onChange, value } }) => (
-									<ToggleSwitch
-										ref={ref}
-										id={setRandomPasswordId}
-										aria-describedby={`${setRandomPasswordId}-hint`}
-										checked={value}
-										onChange={onChange}
-										disabled={!isSmtpEnabled}
-									/>
-								)}
-							/>
-						</FieldRow>
-						{!isSmtpEnabled && (
-							<FieldHint
-								id={`${setRandomPasswordId}-hint`}
-								dangerouslySetInnerHTML={{ __html: t('Send_Email_SMTP_Warning', { url: 'admin/settings/Email' }) }}
-							/>
-						)}
-					</Field> */}
-					<Field>
-						<FieldLabel htmlFor={rolesId}>{t('Roles')}</FieldLabel>
-						<FieldRow>
-							{roleError && <Callout>{roleError}</Callout>}
-							{!roleError && (
-								<Controller
-									control={control}
-									name='roles'
-									rules={{ required: t('The_field_is_required', t('Roles')) }}
-									render={({ field: { onChange, value } }) => (
-										<MultiSelectFiltered
-											id={rolesId}
-											value={value}
-											onChange={onChange}
-											flexGrow={1}
-											placeholder={t('Select_an_option')}
-											options={availableRoles}
-										/>
-									)}
-								/>
-							)}
-						</FieldRow>
-						{errors?.roles && <FieldError>{errors.roles.message}</FieldError>}
-					</Field>
-					{!isEditingExistingUser && (
-						<Field>
-							<FieldRow>
-								<FieldLabel htmlFor={joinDefaultChannelsId}>{t('Join_default_channels')}</FieldLabel>
-								<Controller
-									control={control}
-									name='joinDefaultChannels'
-									render={({ field: { ref, onChange, value } }) => (
-										<ToggleSwitch id={joinDefaultChannelsId} ref={ref} onChange={onChange} checked={value} />
-									)}
-								/>
-							</FieldRow>
-						</Field>
-					)}
-					<Field>
-						<FieldRow>
-							<FieldLabel htmlFor={sendWelcomeEmailId}>{t('Send_welcome_email')}</FieldLabel>
-							<Controller
-								control={control}
-								name='sendWelcomeEmail'
-								render={({ field: { onChange, value } }) => (
-									<ToggleSwitch
-										id={sendWelcomeEmailId}
-										aria-describedby={`${sendWelcomeEmailId}-hint`}
-										onChange={onChange}
-										checked={value}
-										disabled={!isSmtpEnabled}
-									/>
-								)}
-							/>
-						</FieldRow>
-						{!isSmtpEnabled && (
-							<FieldHint
-								id={`${sendWelcomeEmailId}-hint`}
-								dangerouslySetInnerHTML={{ __html: t('Send_Email_SMTP_Warning', { url: 'admin/settings/Email' }) }}
-							/>
-						)}
 					</Field>
 					{Boolean(customFieldsMetadata.length) && (
 						<>
-							<Divider />
-							<Box fontScale='h4'>{t('Custom_Fields')}</Box>
-							<CustomFieldsForm formName='customFields' formControl={control} metadata={customFieldsMetadata} />
+							<Button
+								fontScale='c2'
+								w='x140'
+								h='x28'
+								display='flex'
+								alignItems='center'
+								justifyContent='center'
+								onClick={() => setShowCustomFields((prevState) => !prevState)}
+							>
+								{showCustomFields ? t('Hide_additional_fields') : t('Show_additional_fields')}
+							</Button>
+							{showCustomFields && <CustomFieldsForm formName='customFields' formControl={control} metadata={customFieldsMetadata} />}
 						</>
 					)}
 				</FieldGroup>
 			</ContextualbarScrollableContent>
 			<ContextualbarFooter>
-				<ButtonGroup stretch>
-					<Button
-						type='reset'
-						disabled={!isDirty}
-						onClick={() => reset(getInitialValue({ data: userData, defaultUserRoles, isSmtpEnabled }))}
-					>
-						{t('Reset')}
-					</Button>
-					<Button primary disabled={!isDirty} onClick={handleSubmit(handleSaveUser)}>
-						{t('Save')}
-					</Button>
-				</ButtonGroup>
+				<Button primary disabled={!isDirty} onClick={handleSubmit(handleSaveUser)} w='100%'>
+					{isNewUserPage ? t('Add_user') : t('Save_user')}
+				</Button>
 			</ContextualbarFooter>
 		</>
 	);
 };
 
-export default UserForm;
+export default AdminUserForm;
