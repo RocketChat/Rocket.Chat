47e183e725a4cc7e0cfcaaccf0f7cdc1ef46676a
Kevin Aleman <kaleman960@gmail.com>
2024-03-26 09:27:44 -0600

    Delete apps/meteor/tests/e2e/administration-rooms.spec.ts

--- apps/meteor/tests/e2e/administration-rooms.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Users } from './fixtures/userStates';
-import { HomeDiscussion } from './page-objects';
-import { test, expect } from './utils/test';
-
-test.use({ storageState: Users.admin.state });
-
-test.describe.serial('Rooms Administration', () => {
-
-});
