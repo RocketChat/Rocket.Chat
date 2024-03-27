870bb27981880d013cabdc675b4c2d6cc48cea77
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-27 18:00:06 -0300

    reorg imports

--- apps/meteor/server/main.ts
+++ apps/meteor/server/main.ts
@@ -6,6 +6,7 @@ import './models/startup';
  * and the startup should be done in parallel
  */
 import './settings';
+import '../app/settings/server';
 
 import { startLicense } from '../ee/app/license/server/startup';
 import { registerEEBroker } from '../ee/server';
@@ -16,29 +17,29 @@ import { startup } from './startup';
 
 import './routes';
 import '../app/lib/server/startup';
+import './importPackages';
+import './methods';
+import './publications';
+import './lib/logger/startup';
+import '../lib/oauthRedirectUriServer';
+import './lib/pushConfig';
+import './features/EmailInbox/index';
 
 const StartupLogger = new Logger('StartupLogger');
 
 StartupLogger.startup('Starting Rocket.Chat server...');
 
-await import('../app/settings/server').then(() => StartupLogger.startup('Settings started'));
 await registerEEBroker().then(() => StartupLogger.startup('EE Broker registered'));
 
-await Promise.all([
-	import('./importPackages').then(() => StartupLogger.startup('Imported packages')),
-	import('./methods').then(() => StartupLogger.startup('Imported methods')),
-	import('./publications').then(() => StartupLogger.startup('Imported publications')),
-	import('./lib/logger/startup').then(() => StartupLogger.startup('Logger started')),
-	import('../lib/oauthRedirectUriServer').then(() => StartupLogger.startup('OAuth redirect uri server started')),
-	import('./lib/pushConfig').then(() => StartupLogger.startup('Push configuration started')),
-	import('./features/EmailInbox/index').then(() => StartupLogger.startup('EmailInbox started')),
-	configureLogLevel().then(() => StartupLogger.startup('Log level configured')),
-	registerServices().then(() => StartupLogger.startup('Services registered')),
-	import('../ee/server/startup/services'),
-	configureLoginServices().then(() => StartupLogger.startup('Login services configured')),
-	startup().then(() => StartupLogger.startup('Startup finished')),
-]);
+await (async () => {
+	await Promise.all([
+		configureLogLevel().then(() => StartupLogger.startup('Log level configured')),
+		registerServices().then(() => StartupLogger.startup('Services registered')),
+		configureLoginServices().then(() => StartupLogger.startup('Login services configured')),
+		startup().then(() => StartupLogger.startup('Startup finished')),
+	]);
+})();
 
 await startLicense().then(() => StartupLogger.startup('License started'));
-
+await import('../ee/server/startup/services');
 StartupLogger.startup('Rocket.Chat server started.');
