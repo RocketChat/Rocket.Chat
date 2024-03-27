e6f31fc8cdfdb8fd2c9af2d8b979f9447a5833b9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-27 00:10:27 -0300

    fix temporarily login error message - drop it

--- apps/meteor/app/lib/server/lib/loginErrorMessageOverride.js
+++ apps/meteor/app/lib/server/lib/loginErrorMessageOverride.js
@@ -1,14 +1,16 @@
 // Do not disclose if user exists when password is invalid
 import { Accounts } from 'meteor/accounts-base';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 
-const { _runLoginHandlers } = Accounts;
-Accounts._runLoginHandlers = function (methodInvocation, options) {
-	const result = _runLoginHandlers.call(Accounts, methodInvocation, options);
+// const { _runLoginHandlers } = Accounts;
 
-	if (result.error && result.error.reason === 'Incorrect password') {
-		result.error = new Meteor.Error(403, 'User not found');
-	}
+Accounts._options.ambiguousErrorMessages = false;
+// Accounts._runLoginHandlers = async function (methodInvocation, options) {
+// 	const result = await _runLoginHandlers.call(Accounts, methodInvocation, options);
 
-	return result;
-};
+// 	// if (result.error && result.error.reason === 'Incorrect password') {
+// 	// 	result.error = new Meteor.Error(403, 'User not found');
+// 	// }
+
+// 	return result;
+// };
