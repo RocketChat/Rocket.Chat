0f983df59eb8932e2993793c6ee416fd6a5e39f2
yash-rajpal <rajpal.yash03@gmail.com>
2024-04-01 20:44:26 +0530

    changes requested: font-size

--- apps/meteor/client/views/e2e/SaveE2EPasswordModal.tsx
+++ apps/meteor/client/views/e2e/SaveE2EPasswordModal.tsx
@@ -31,21 +31,19 @@ const SaveE2EPasswordModal = ({ randomPassword, onClose, onCancel, onConfirm }: 
 			title={t('Save_your_encryption_password')}
 			annotation={t('You_can_do_from_account_preferences')}
 		>
-			<Box fontScale='p1'>
-				<Box is='span'>
-					<Box is='span'>{t('E2E_password_reveal_text')}</Box>
-					<ExternalLink to={DOCS_URL} mis={4}>
-						{t('Learn_more_about_E2EE')}
-					</ExternalLink>
-				</Box>
-				<Box fontWeight='bold' mb={20}>
-					{t('E2E_password_save_text')}
-				</Box>
-				{t('Your_E2EE_password_is')}
-				<CodeSnippet buttonText={hasCopied ? t('Copied') : t('Copy_password')} buttonDisabled={hasCopied} onClick={() => copy()}>
-					{randomPassword}
-				</CodeSnippet>
+			<Box is='span'>
+				<Box is='span'>{t('E2E_password_reveal_text')}</Box>
+				<ExternalLink to={DOCS_URL} mis={4}>
+					{t('Learn_more_about_E2EE')}
+				</ExternalLink>
 			</Box>
+			<Box fontWeight='bold' mb={20}>
+				{t('E2E_password_save_text')}
+			</Box>
+			{t('Your_E2EE_password_is')}
+			<CodeSnippet buttonText={hasCopied ? t('Copied') : t('Copy_password')} buttonDisabled={hasCopied} onClick={() => copy()}>
+				{randomPassword}
+			</CodeSnippet>
 		</GenericModal>
 	);
 };
