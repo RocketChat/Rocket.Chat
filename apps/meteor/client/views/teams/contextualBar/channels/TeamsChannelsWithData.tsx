d02196476237f75935261e83e3851300f910c052
dougfabris <devfabris@gmail.com>
2024-04-04 10:53:34 -0300

    fix: review

--- apps/meteor/client/views/teams/contextualBar/channels/TeamsChannelsWithData.tsx
+++ apps/meteor/client/views/teams/contextualBar/channels/TeamsChannelsWithData.tsx
@@ -1,7 +1,6 @@
 import type { IRoom } from '@rocket.chat/core-typings';
-import { useMutableCallback, useLocalStorage, useDebouncedValue, useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useLocalStorage, useDebouncedValue, useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { useSetModal, usePermission } from '@rocket.chat/ui-contexts';
-import type { FC, SyntheticEvent } from 'react';
 import React, { useCallback, useMemo, useState } from 'react';
 
 import { useRecordList } from '../../../../hooks/lists/useRecordList';
@@ -10,40 +9,24 @@ import { roomCoordinator } from '../../../../lib/rooms/roomCoordinator';
 import CreateChannelWithData from '../../../../sidebar/header/CreateChannel';
 import { useRoom } from '../../../room/contexts/RoomContext';
 import { useRoomToolbox } from '../../../room/contexts/RoomToolboxContext';
-import RoomInfo from '../../../room/contextualBar/Info';
 import AddExistingModal from './AddExistingModal';
 import TeamsChannels from './TeamsChannels';
 import { useTeamsChannelList } from './hooks/useTeamsChannelList';
 
-const useReactModal = (Component: FC<any>, teamId: string, reload: () => void) => {
-	const setModal = useSetModal();
-
-	return useMutableCallback((e: SyntheticEvent) => {
-		e.preventDefault();
-
-		const handleClose = () => {
-			setModal(null);
-			reload();
-		};
-
-		setModal(() => <Component onClose={handleClose} teamId={teamId} />);
-	});
-};
-
 const TeamsChannelsWithData = () => {
 	const room = useRoom();
+	const setModal = useSetModal();
+	const { closeTab } = useRoomToolbox();
+	const canAddExistingTeam = usePermission('add-team-channel', room._id);
+
 	const { teamId } = room;
 
 	if (!teamId) {
 		throw new Error('Invalid teamId');
 	}
 
-	const [state, setState] = useState<{ tab?: string; rid?: string }>({});
-	const { closeTab } = useRoomToolbox();
-
 	const [type, setType] = useLocalStorage<'all' | 'autoJoin'>('channels-list-type', 'all');
 	const [text, setText] = useState('');
-
 	const debouncedText = useDebouncedValue(text, 800);
 
 	const { teamsChannelList, loadMoreItems, reload } = useTeamsChannelList(
@@ -56,19 +39,17 @@ const TeamsChannelsWithData = () => {
 		setText(event.currentTarget.value);
 	}, []);
 
-	const canAddExistingTeam = usePermission('add-team-channel', room._id);
-	const addExisting = useReactModal(AddExistingModal, teamId, reload);
-	const createNew = useReactModal(CreateChannelWithData, teamId, reload);
-
-	const goToRoom = useCallback((room) => roomCoordinator.openRouteLink(room.t, room), []);
-	const handleBack = useCallback(() => setState({}), [setState]);
-	const viewRoom = useEffectEvent((room: IRoom) => {
-		goToRoom(room);
+	const handleAddExisting = useEffectEvent(() => {
+		setModal(<AddExistingModal teamId={teamId} onClose={() => setModal(null)} reload={reload} />);
 	});
 
-	if (state?.tab === 'RoomInfo' && state?.rid) {
-		return <RoomInfo onClickBack={handleBack} onEnterRoom={goToRoom} resetState={() => setState({})} />;
-	}
+	const handleCreateNew = useEffectEvent(() => {
+		setModal(<CreateChannelWithData teamId={teamId} onClose={() => setModal(null)} reload={reload} />);
+	});
+
+	const goToRoom = useEffectEvent((room: IRoom) => {
+		roomCoordinator.openRouteLink(room.t, room);
+	});
 
 	return (
 		<TeamsChannels
@@ -80,9 +61,9 @@ const TeamsChannelsWithData = () => {
 			channels={items}
 			total={total}
 			onClickClose={closeTab}
-			onClickAddExisting={canAddExistingTeam && addExisting}
-			onClickCreateNew={canAddExistingTeam && createNew}
-			onClickView={viewRoom}
+			onClickAddExisting={canAddExistingTeam && handleAddExisting}
+			onClickCreateNew={canAddExistingTeam && handleCreateNew}
+			onClickView={goToRoom}
 			loadMoreItems={loadMoreItems}
 			reload={reload}
 		/>
