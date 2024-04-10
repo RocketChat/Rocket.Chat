8135d93e5c7fb5815f96f626774ae0608801716f
gabriellsh <gabriel.henriques@rocket.chat>
2024-04-10 13:46:37 -0300

    tests

---
+++ apps/meteor/tests/e2e/translations.spec.ts
@@ -0,0 +1,58 @@
+import { Users } from './fixtures/userStates';
+import { setSettingValueById } from './utils/setSettingValueById';
+import { setUserPreferences } from './utils/setUserPreferences';
+import { test, expect } from './utils/test';
+
+test.use({ storageState: Users.user1.state });
+
+test.describe('Translations', () => {
+    test.beforeAll(async ({ api }) => {
+        expect((await setUserPreferences(api, { language: '' })).status()).toBe(200);
+        expect((await setSettingValueById(api, 'Language', 'en')).status()).toBe(200);
+	});
+
+    test.afterAll(async ({ api }) => {
+        expect((await setUserPreferences(api, { language: '' })).status()).toBe(200);
+        expect((await setSettingValueById(api, 'Language', 'en')).status()).toBe(200);
+	});
+
+    test.afterEach(async ({ api, page }) => {
+        expect((await setUserPreferences(api, { language: '' })).status()).toBe(200);
+        await page.goto('/home');
+	})
+
+    test('expect to respect user preference', async ({ page, api }) => {
+        await expect(page.locator('html')).toHaveAttribute('lang', 'en');
+        await expect(page.locator('h2')).toHaveText('Welcome to Rocket.Chat');
+
+        expect((await setUserPreferences(api, { language: 'pt-BR' })).status()).toBe(200);
+        // Wait for lang to load
+        expect((await api.get('/i18n/pt-BR.json')).status()).toBe(200);
+
+        await expect(page.locator('html')).toHaveAttribute('lang', 'pt-BR');
+        await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
+    });
+
+    test.describe('Browser language', ( ) => {
+        test.use({ locale: 'pt-BR' })
+        test('expect to respect browser language', async ({ page }) => {
+            await expect(page.locator('html')).toHaveAttribute('lang', 'pt-BR');
+            await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
+        });
+    })
+
+    test.describe('Server language', ( ) => {
+        test.use({ locale: '' })
+        test('expect to respect server language', async ({ page, api }) => {
+            await expect(page.locator('html')).toHaveAttribute('lang', 'en');
+            await expect(page.locator('h2')).toHaveText('Welcome to Rocket.Chat');
+
+            expect((await setSettingValueById(api, 'Language', 'pt-BR')).status()).toBe(200);
+            // Wait for lang to load
+            expect((await api.get('/i18n/pt-BR.json')).status()).toBe(200);
+    
+            await expect(page.locator('html')).toHaveAttribute('lang', 'pt-BR');
+            await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
+        });
+    })
+});
