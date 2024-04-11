b2b2286ee88082c1ea085996916542bd85cd2e14
Pierre Lehnen <55164754+pierre-lehnen-rc@users.noreply.github.com>
2024-04-11 12:00:50 -0300

    chore!: remove hipchat importer (#32154)

--- apps/meteor/app/importer-hipchat-enterprise/server/index.ts
+++
@@ -1,8 +0,0 @@
-import { Importers } from '../../importer/server';
-import { HipChatEnterpriseImporter } from './HipChatEnterpriseImporter';
-
-Importers.add({
-	key: 'hipchatenterprise',
-	name: 'HipChat (tar.gz)',
-	importer: HipChatEnterpriseImporter,
-});
