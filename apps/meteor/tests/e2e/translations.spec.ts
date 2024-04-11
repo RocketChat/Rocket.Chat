ae76fc26c8060d092d196b347a7dd43b4ba48588
gabriellsh <gabriel.henriques@rocket.chat>
2024-04-11 16:35:04 -0300

    change order of tests

--- apps/meteor/tests/e2e/translations.spec.ts
+++ apps/meteor/tests/e2e/translations.spec.ts
@@ -3,7 +3,7 @@ import { setSettingValueById } from './utils/setSettingValueById';
 import { setUserPreferences } from './utils/setUserPreferences';
 import { test, expect } from './utils/test';
 
-test.use({ storageState: Users.admin.state });
+test.use({ storageState: Users.admin.state, locale: 'pt-BR' });
 
 test.describe('Translations', () => {
     test.beforeAll(async ({ api }) => {
@@ -16,25 +16,18 @@ test.describe('Translations', () => {
         expect((await setSettingValueById(api, 'Language', 'en')).status()).toBe(200);
 	});
 
-    test.beforeEach(async ({ api }) => {
-        expect((await setUserPreferences(api, { language: '' })).status()).toBe(200);
-	})
-
-    test('expect to respect user preference', async ({ page, api }) => {
+    test('expect to respect browser language', async ({ page }) => {
         await page.goto('/home');
-        await expect(page.locator('h2')).toHaveText('Welcome to Rocket.Chat');
-
-        const response = page.waitForResponse('**/i18n/pt-BR.json');
-        expect((await setUserPreferences(api, { language: 'pt-BR' })).status()).toBe(200);
-        await response;
         await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
     });
 
-    test.describe('Browser language', () => {
-        test.use({ locale: 'pt-BR' })
-        test('expect to respect browser language', async ({ page }) => {
-            await page.goto('/home');
-            await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
-        });
-    })
+    test('expect to respect user preference', async ({ page, api }) => {
+        await page.goto('/home');
+        await expect(page.locator('h2')).toHaveText('Bem-vindo ao Rocket.Chat');
+
+        const response = page.waitForResponse('**/i18n/es.json');
+        expect((await setUserPreferences(api, { language: 'es' })).status()).toBe(200);
+        await response;
+        await expect(page.locator('h2')).toHaveText('Te damos la bienvenida a Rocket.Chat');
+    });
 });
