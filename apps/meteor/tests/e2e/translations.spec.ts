be395eb20c37bc1114f85fb9960acbb6b84de7b0
gabriellsh <gabriel.henriques@rocket.chat>
2024-04-15 14:36:50 -0300

    unset preference

--- apps/meteor/tests/e2e/translations.spec.ts
+++ apps/meteor/tests/e2e/translations.spec.ts
@@ -7,6 +7,7 @@ test.use({ storageState: Users.admin.state, locale: 'pt-BR' });
 
 test.describe('Translations', () => {
     test.beforeAll(async ({ api }) => {
+        expect((await setUserPreferences(api, { language: '' })).status()).toBe(200);
         expect((await setSettingValueById(api, 'Language', 'en')).status()).toBe(200);
         expect((await setSettingValueById(api, 'Site_Name', 'Rocket.Chat')).status()).toBe(200);
 	});
