24862f0f8add3e8f7d3cf60841cd0b25e3f1732d
dougfabris <devfabris@gmail.com>
2024-04-18 12:56:34 -0300

    chore: sanitize `MultiSelectCustom`

--- packages/ui-client/src/components/MultiSelectCustom/MultiSelectCustomAnchor.tsx
+++ packages/ui-client/src/components/MultiSelectCustom/MultiSelectCustomAnchor.tsx
@@ -1,42 +1,28 @@
 import { css } from '@rocket.chat/css-in-js';
-import { Box, Button, Icon, Palette } from '@rocket.chat/fuselage';
+import { Box, Icon } from '@rocket.chat/fuselage';
 import type { TranslationKey } from '@rocket.chat/ui-contexts';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import type { ComponentProps } from 'react';
 import { forwardRef } from 'react';
 
 type MultiSelectCustomAnchorProps = {
-	onClick?: (value: boolean) => void;
 	collapsed: boolean;
 	defaultTitle: TranslationKey;
 	selectedOptionsTitle: TranslationKey;
 	selectedOptionsCount: number;
 	maxCount: number;
-} & ComponentProps<typeof Button>;
+} & ComponentProps<typeof Box>;
 
 const MultiSelectCustomAnchor = forwardRef<HTMLElement, MultiSelectCustomAnchorProps>(function MultiSelectCustomAnchor(
-	{ onClick, collapsed, selectedOptionsCount, selectedOptionsTitle, defaultTitle, maxCount, ...props },
+	{ collapsed, selectedOptionsCount, selectedOptionsTitle, defaultTitle, maxCount, ...props },
 	ref,
 ) {
 	const t = useTranslation();
-
-	const inputStyle = collapsed
-		? css`
-				&,
-				&:hover,
-				&:active,
-				&:focus {
-					cursor: pointer;
-					border-color: ${Palette.stroke['stroke-highlight'].toString()}!important;
-					box-shadow: 0 0 0 2px ${Palette.shadow['shadow-highlight'].toString()};
-				}
-		  `
-		: css`
-				& {
-					cursor: pointer;
-				}
-		  `;
-
+	const customStyle = css`
+		&:hover {
+			cursor: pointer;
+		}
+	`;
 	const isDirty = selectedOptionsCount > 0 && selectedOptionsCount !== maxCount - 1;
 
 	return (
@@ -44,26 +30,15 @@ const MultiSelectCustomAnchor = forwardRef<HTMLElement, MultiSelectCustomAnchorP
 			ref={ref}
 			role='button'
 			tabIndex={0}
-			onClick={onClick}
-			onKeyDown={(e) => (e.code === 'Enter' || e.code === 'Space') && onClick}
 			display='flex'
 			justifyContent='space-between'
 			alignItems='center'
-			flexDirection='row'
-			borderColor={Palette.stroke['stroke-light'].toString()}
-			borderWidth='x1'
-			borderRadius={4}
-			bg={Palette.surface['surface-light'].toString()}
 			h='x40'
-			w='full'
-			pb={10}
-			pi={16}
-			color={isDirty ? Palette.text['font-default'].toString() : Palette.text['font-annotation'].toString()}
-			className={inputStyle}
+			className={['rcx-input-box__wrapper', customStyle].filter(Boolean)}
 			{...props}
 		>
 			{isDirty ? `${t(selectedOptionsTitle)} (${selectedOptionsCount})` : t(defaultTitle)}
-			<Icon name='chevron-down' fontSize='x20' color='hint' />
+			<Icon name={collapsed ? 'chevron-up' : 'chevron-down'} fontSize='x20' color='hint' />
 		</Box>
 	);
 });
