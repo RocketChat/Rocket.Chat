e85a3dc687ff5c46a56b17f475a27a310f68e16a
MartinSchoeler <martin.schoeler@rocket.chat>
2024-04-19 09:43:22 -0300

    remove startup

---
+++ apps/meteor/client/hooks/useContinuousSoundNotification.ts
@@ -0,0 +1,54 @@
+import type { ICustomSound } from '@rocket.chat/core-typings';
+import { useSetting, useUserPreference, useUserSubscriptions } from '@rocket.chat/ui-contexts';
+import { useEffect } from 'react';
+
+import { CustomSounds } from '../../app/custom-sounds/client/lib/CustomSounds';
+
+const query = { t: 'l', ls: { $exists: false }, open: true };
+export const useContinuousSoundNotification = () => {
+	const userSubscriptions = useUserSubscriptions(query);
+
+	const setting = useSetting('Livechat_continuous_sound_notification_new_livechat_room');
+
+	const newRoomNotification = useUserPreference<string>('newRoomNotification') || '';
+	const audioVolume = useUserPreference<number>('notificationsSoundVolume');
+
+	const continuousCustomSoundId = `${newRoomNotification}-continuous`;
+
+	const volume = audioVolume ? Number((audioVolume / 100).toPrecision(2)) : 1;
+
+	useEffect(() => {
+		let audio: ICustomSound;
+		if (setting && newRoomNotification) {
+			audio = CustomSounds.getSound(newRoomNotification);
+
+			if (audio) {
+				audio._id = continuousCustomSoundId;
+				CustomSounds.add(audio);
+			}
+		}
+
+		return () => {
+			if (audio) {
+				CustomSounds.remove(audio);
+			}
+		};
+	}, [continuousCustomSoundId, newRoomNotification, setting]);
+
+	useEffect(() => {
+		if (!setting) {
+			CustomSounds.pause(continuousCustomSoundId);
+			return;
+		}
+
+		if (userSubscriptions.length === 0) {
+			CustomSounds.pause(continuousCustomSoundId);
+			return;
+		}
+
+		CustomSounds.play(continuousCustomSoundId, {
+			volume,
+			loop: true,
+		});
+	}, [continuousCustomSoundId, newRoomNotification, setting, userSubscriptions, volume]);
+};
