bfb21ced60eed046620f978fa03630e0d846efb3
juliajforesti <juliajforesti@gmail.com>
2024-04-23 09:44:36 -0300

    fix: lint

--- packages/ui-client/src/components/Header/Header.tsx
+++ packages/ui-client/src/components/Header/Header.tsx
@@ -1,19 +1,17 @@
 import { Box } from '@rocket.chat/fuselage';
 import type { ComponentProps } from 'react';
 
-const Header = (props: ComponentProps<typeof Box>) => {
-	return (
-		<Box
-			rcx-room-header
-			is='header'
-			display='flex'
-			justifyContent='center'
-			flexDirection='column'
-			overflow='hidden'
-			flexShrink={0}
-			{...props}
-		/>
-	);
-};
+const Header = (props: ComponentProps<typeof Box>) => (
+	<Box
+		rcx-room-header
+		is='header'
+		display='flex'
+		justifyContent='center'
+		flexDirection='column'
+		overflow='hidden'
+		flexShrink={0}
+		{...props}
+	/>
+);
 
 export default Header;
