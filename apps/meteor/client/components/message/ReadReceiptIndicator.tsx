7c34a383c59c0dae7cf4cdc97e8787bfc5cab42b
dougfabris <devfabris@gmail.com>
2024-04-25 20:39:43 -0300

    feat: replace icon

--- apps/meteor/client/components/message/ReadReceiptIndicator.tsx
+++ apps/meteor/client/components/message/ReadReceiptIndicator.tsx
@@ -8,7 +8,7 @@ type ReadReceiptIndicatorProps = {
 
 const ReadReceiptIndicator = ({ unread }: ReadReceiptIndicatorProps): ReactElement | null => (
 	<Box position='absolute' insetBlockStart={2} insetInlineEnd={8}>
-		<Icon name='check' size='x16' color={unread ? 'annotation' : 'info'} />
+		<Icon size='x16' name={unread ? 'check' : 'double-check'} color={unread ? 'annotation' : 'info'} />
 	</Box>
 );
 
