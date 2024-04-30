e236469a4c52b922a651c16088f0ac037c12843d
dougfabris <devfabris@gmail.com>
2024-04-30 10:27:45 -0300

    fix: e2e test

--- apps/meteor/tests/e2e/read-receipts.spec.ts
+++ apps/meteor/tests/e2e/read-receipts.spec.ts
@@ -21,23 +21,19 @@ test.describe.serial('read-receipts', () => {
 		await page.goto('/home');
 	});
 
-	test('should show read receipts message sent status in the sent message', async () => {
-		await poHomeChannel.sidenav.openChat(targetChannel);
-		await poHomeChannel.content.sendMessage('hello world');
-
-		await expect(poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message sent' })).toBeVisible();
-	});
-
-  test('should show read receipts message viewed status in the sent message', async ({ browser }) => {
+	test('should show read receipts message sent status in the sent message', async ({ browser }) => {
 		const { page } = await createAuxContext(browser, Users.user1);
 		const auxContext = { page, poHomeChannel: new HomeChannel(page) };
 		await auxContext.poHomeChannel.sidenav.openChat(targetChannel);
 		await auxContext.poHomeChannel.content.sendMessage('hello admin');
 
+		await expect(auxContext.poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message sent' })).toBeVisible();
+		await auxContext.page.close();
+	});
+
+  test('should show read receipts message viewed status in the sent message', async () => {
 		await poHomeChannel.sidenav.openChat(targetChannel);
 		await expect(poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message viewed' })).toBeVisible();		
-
-		await auxContext.page.close();
 	});
 
 	test('should show the reads receipt modal with the users who read the message', async ({ page, api }) => {
