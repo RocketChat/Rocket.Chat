b98cd333e596db4d6501b319bbee7377cae884ec
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2024-05-03 17:05:32 -0300

    chore: Improve permissions check on instances endpoints (#32334)

--- apps/meteor/tests/end-to-end/api/miscellaneous.ts
+++ apps/meteor/tests/end-to-end/api/miscellaneous.ts
@@ -540,7 +540,7 @@ describe('miscellaneous', () => {
 				.expect(403)
 				.expect((res) => {
 					expect(res.body).to.have.property('success', false);
-					expect(res.body).to.have.property('error', 'unauthorized');
+					expect(res.body).to.have.property('error', 'User does not have the permissions required for this action [error-unauthorized]');
 				})
 				.end(done);
 		});
