b568c0140c697079db50620ba7b43fbf37bbaafc
yash-rajpal <rajpal.yash03@gmail.com>
2024-05-04 01:48:28 +0530

    failing e2e tests

--- apps/meteor/tests/e2e/threads.spec.ts
+++ apps/meteor/tests/e2e/threads.spec.ts
@@ -15,23 +15,6 @@ test.describe.serial('Threads', () => {
 		await page.goto('/home');
 		await poHomeChannel.sidenav.openChat(targetChannel);
 	});
-
-	test('expect upload a file attachment in thread with description', async ({ page }) => {
-		await poHomeChannel.content.sendMessage('this is a message for testing file upload in threads.');
-		await page.locator('[data-qa-type="message"]').last().hover();
-		await page.locator('role=button[name="Reply in thread"]').click();
-
-		await expect(page).toHaveURL(/.*thread/);
-
-		await poHomeChannel.content.dragAndDropTxtFileToThread();
-		await poHomeChannel.content.descriptionInput.fill('any_description');
-		await poHomeChannel.content.fileNameInput.fill('any_file1.txt');
-		await poHomeChannel.content.btnModalConfirm.click();
-
-		await expect(poHomeChannel.content.lastThreadMessageFileDescription).toHaveText('any_description');
-		await expect(poHomeChannel.content.lastThreadMessageFileName).toContainText('any_file1.txt');
-	});
-
 	test('expect thread message preview if alsoSendToChannel checkbox is checked', async ({ page }) => {
 		await poHomeChannel.content.sendMessage('this is a message for reply');
 		await page.locator('[data-qa-type="message"]').last().hover();
@@ -84,6 +67,20 @@ test.describe.serial('Threads', () => {
 			await expect(poHomeChannel.content.lastThreadMessageText).toContainText('This is a thread message also sent in channel');
 		});
 	});
+	test('expect upload a file attachment in thread with description', async ({ page }) => {
+		await poHomeChannel.content.lastThreadMessagePreviewText.click();
+
+		await expect(page).toHaveURL(/.*thread/);
+
+		await poHomeChannel.content.dragAndDropTxtFileToThread();
+		await poHomeChannel.content.descriptionInput.fill('any_description');
+		await poHomeChannel.content.fileNameInput.fill('any_file1.txt');
+		await poHomeChannel.content.btnModalConfirm.click();
+
+		await expect(poHomeChannel.content.lastThreadMessageFileDescription).toHaveText('any_description');
+		await expect(poHomeChannel.content.lastThreadMessageFileName).toContainText('any_file1.txt');
+	});
+
 	test.describe('thread message actions', () => {
 		test.beforeEach(async ({ page }) => {
 			poHomeChannel = new HomeChannel(page);
