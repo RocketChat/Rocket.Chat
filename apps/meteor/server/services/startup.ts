3529f75e14ae8f1f14f939dba0aa6fb937d310af
Pierre <pierre.lehnen@rocket.chat>
2024-05-06 17:09:03 -0300

    chore: rename VoIP service to VoipAsterisk

--- apps/meteor/server/services/startup.ts
+++ apps/meteor/server/services/startup.ts
@@ -29,7 +29,7 @@ import { TranslationService } from './translation/service';
 import { UiKitCoreAppService } from './uikit-core-app/service';
 import { UploadService } from './upload/service';
 import { VideoConfService } from './video-conference/service';
-import { VoipService } from './voip/service';
+import { VoipAsteriskService } from './voip-asterisk/service';
 
 export const registerServices = async (): Promise<void> => {
 	const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
@@ -45,7 +45,7 @@ export const registerServices = async (): Promise<void> => {
 	api.registerService(new NPSService());
 	api.registerService(new RoomService());
 	api.registerService(new SAUMonitorService());
-	api.registerService(new VoipService(db));
+	api.registerService(new VoipAsteriskService(db));
 	api.registerService(new OmnichannelService());
 	api.registerService(new OmnichannelVoipService());
 	api.registerService(new TeamService());
