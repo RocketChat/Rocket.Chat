017ebc45682174e5fbd92896cdc0d115dee70765
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2024-05-13 09:00:35 -0300

    chore!: Improve permissions check on licenses endpoints (#32354)

--- apps/meteor/tests/end-to-end/api/licenses.ts
+++ apps/meteor/tests/end-to-end/api/licenses.ts
@@ -48,7 +48,7 @@ describe('licenses', () => {
 				.expect(403)
 				.expect((res) => {
 					expect(res.body).to.have.property('success', false);
-					expect(res.body).to.have.property('error', 'unauthorized');
+					expect(res.body).to.have.property('error', 'User does not have the permissions required for this action [error-unauthorized]');
 				})
 				.end(done);
 		});
