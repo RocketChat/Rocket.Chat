02e003bfee32c382f9c7286f739d46cb6ff2b264
dougfabris <devfabris@gmail.com>
2024-05-16 13:20:33 -0300

    chore: merge e2e tests

--- apps/meteor/tests/e2e/read-receipts.spec.ts
+++ apps/meteor/tests/e2e/read-receipts.spec.ts
@@ -14,9 +14,6 @@ test.describe.serial('read-receipts', () => {
 	test.skip(!IS_EE, 'Enterprise Only');
 
 	test.beforeAll(async ({ api }) => {
-		await setSettingValueById(api, 'Message_Read_Receipt_Enabled', true);
-		await setSettingValueById(api, 'Message_Read_Receipt_Store_Users', true);
-		
 		targetChannel = await createTargetChannel(api);
 	});
 
@@ -26,26 +23,47 @@ test.describe.serial('read-receipts', () => {
 		await page.goto('/home');
 	});
 
-	test('should show read receipts message sent status in the sent message', async ({ browser }) => {
-		const { page } = await createAuxContext(browser, Users.user1);
-		const auxContext = { page, poHomeChannel: new HomeChannel(page) };
-		await auxContext.poHomeChannel.sidenav.openChat(targetChannel);
-		await auxContext.poHomeChannel.content.sendMessage('hello admin');
-
-		await expect(auxContext.poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message sent' })).toBeVisible();
-		await auxContext.page.close();
+	test.describe('read receipt settings disabled', async () => {
+		test('should not show read receipts item menu', async ({ page }) => {
+			await poHomeChannel.sidenav.openChat(targetChannel);
+			await poHomeChannel.content.sendMessage('hello world');
+			await poHomeChannel.content.openLastMessageMenu();
+			expect(page.locator('role=menuitem[name="Read receipts"]')).not.toBeVisible;
+		});
 	});
 
-  test('should show read receipts message viewed status in the sent message', async () => {
-		await poHomeChannel.sidenav.openChat(targetChannel);
-		await expect(poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message viewed' })).toBeVisible();		
-	});
+	test.describe('read receipts enabled', async () => {
+		test.beforeAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Read_Receipt_Enabled', true);
+			await setSettingValueById(api, 'Message_Read_Receipt_Store_Users', true);			
+		});
 
-	test('should show the reads receipt modal with the users who read the message', async ({ page }) => {
-		await poHomeChannel.sidenav.openChat(targetChannel);
-		await poHomeChannel.content.openLastMessageMenu();
-		await page.locator('role=menuitem[name="Read receipts"]').click();
+		test.afterAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Read_Receipt_Enabled', false);
+			await setSettingValueById(api, 'Message_Read_Receipt_Store_Users', false);
+		});
 
-		await expect(page.getByRole('dialog').getByRole('listitem')).toHaveCount(2);
+		test('should show read receipts message sent status in the sent message', async ({ browser }) => {
+			const { page } = await createAuxContext(browser, Users.user1);
+			const auxContext = { page, poHomeChannel: new HomeChannel(page) };
+			await auxContext.poHomeChannel.sidenav.openChat(targetChannel);
+			await auxContext.poHomeChannel.content.sendMessage('hello admin');
+	
+			await expect(auxContext.poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message sent' })).toBeVisible();
+			await auxContext.page.close();
+		});
+	
+		test('should show read receipts message viewed status in the sent message', async () => {
+			await poHomeChannel.sidenav.openChat(targetChannel);
+			await expect(poHomeChannel.content.lastUserMessage.getByRole('status', { name: 'Message viewed' })).toBeVisible();		
+		});
+	
+		test('should show the reads receipt modal with the users who read the message', async ({ page }) => {
+			await poHomeChannel.sidenav.openChat(targetChannel);
+			await poHomeChannel.content.openLastMessageMenu();
+			await page.locator('role=menuitem[name="Read receipts"]').click();
+	
+			await expect(page.getByRole('dialog').getByRole('listitem')).toHaveCount(2);
+		});
 	});
 });
