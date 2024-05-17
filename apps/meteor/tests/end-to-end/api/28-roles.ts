8a49bd78a736b68b3dfa5703b31148177aff73f0
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-05-17 15:55:30 -0300

    test: add more e2e test cases

--- apps/meteor/tests/end-to-end/api/28-roles.ts
+++ apps/meteor/tests/end-to-end/api/28-roles.ts
@@ -174,18 +174,6 @@ describe('[Roles]', function () {
 					expect(res.body.role).to.have.property('name', testRoleName);
 					testRoleId = res.body.role._id;
 				});
-			await request
-				.post(api('roles.addUserToRole'))
-				.set(credentials)
-				.send({
-					roleId: testRoleId,
-					username: adminUsername,
-				})
-				.expect('Content-Type', 'application/json')
-				.expect(200)
-				.expect((res) => {
-					expect(res.body).to.have.property('success', true);
-				});
 		});
 
 		after(async () => {
@@ -199,7 +187,7 @@ describe('[Roles]', function () {
 			await deleteUser(testUser);
 		});
 
-		it('should successfully get a list of users in a role', async function () {
+		it('should successfully get an empty list of users in a role if no user has been assigned to it', async function () {
 			if (!isEnterprise) {
 				this.skip();
 				return;
@@ -215,9 +203,63 @@ describe('[Roles]', function () {
 				.expect(200)
 				.expect((res: Response) => {
 					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('total', 0);
+					expect(res.body).to.have.property('users').that.is.an('array').that.is.empty;
+				});
+		});
+
+		it('should successfully get a list of users in a role', async function () {
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.post(api('roles.addUserToRole'))
+				.set(credentials)
+				.send({
+					roleId: testRoleId,
+					username: adminUsername,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				});
+
+			await request
+				.get(api('roles.getUsersInRole'))
+				.set(credentials)
+				.query({
+					role: testRoleId,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', true);
 					expect(res.body).to.have.property('total', 1);
 					expect(res.body).to.have.property('users').that.is.an('array').of.length(1);
 					expect(res.body.users[0]).to.have.property('_id', credentials['X-User-Id']);
+				});
+		});
+
+		it('should fail getting a list of users in a role in case an invalid role is provided', async function () {
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.get(api('roles.getUsersInRole'))
+				.set(credentials)
+				.query({
+					role: 'invalid-role',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'error-invalid-roleId');
 				});
 		});
 
@@ -293,6 +335,29 @@ describe('[Roles]', function () {
 				.expect((res: Response) => {
 					expect(res.body).to.have.property('success', false);
 					expect(res.body).to.have.property('error', 'User does not have the permissions required for this action [error-unauthorized]');
+				});
+		});
+
+		it('should fail deleting a role in EE in case an invalid role is provided', async function () {
+			// TODO this is not the right way to do it. We're doing this way for now just because we have separate CI jobs for EE and CE,
+			// ideally we should have a single CI job that adds a license and runs both CE and EE tests.
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.post(api('roles.delete'))
+				.set(credentials)
+				.send({
+					roleId: 'invalid-role',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'error-invalid-roleId');
+					expect(res.body).to.have.property('error', 'This role does not exist [error-invalid-roleId]');
 				});
 		});
 
@@ -380,6 +445,78 @@ describe('[Roles]', function () {
 				.expect((res: Response) => {
 					expect(res.body).to.have.property('success', false);
 					expect(res.body).to.have.property('error', 'User does not have the permissions required for this action [error-unauthorized]');
+				});
+		});
+
+		it('should fail removing an invalid user from a role', async function () {
+			// TODO this is not the right way to do it. We're doing this way for now just because we have separate CI jobs for EE and CE,
+			// ideally we should have a single CI job that adds a license and runs both CE and EE tests.
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.post(api('roles.removeUserFromRole'))
+				.set(credentials)
+				.send({
+					roleId: testRoleId,
+					username: 'invalid-username',
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'error-invalid-user');
+					expect(res.body).to.have.property('error', 'There is no user with this username [error-invalid-user]');
+				});
+		});
+
+		it('should fail removing a user from an invalid role', async function () {
+			// TODO this is not the right way to do it. We're doing this way for now just because we have separate CI jobs for EE and CE,
+			// ideally we should have a single CI job that adds a license and runs both CE and EE tests.
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.post(api('roles.removeUserFromRole'))
+				.set(credentials)
+				.send({
+					roleId: 'invalid-role',
+					username: testUser.username,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'error-invalid-roleId');
+					expect(res.body).to.have.property('error', 'This role does not exist [error-invalid-roleId]');
+				});
+		});
+
+		it('should fail removing a user from a role they do not have', async function () {
+			// TODO this is not the right way to do it. We're doing this way for now just because we have separate CI jobs for EE and CE,
+			// ideally we should have a single CI job that adds a license and runs both CE and EE tests.
+			if (!isEnterprise) {
+				this.skip();
+				return;
+			}
+
+			await request
+				.post(api('roles.removeUserFromRole'))
+				.set(credentials)
+				.send({
+					roleId: 'admin',
+					username: testUser.username,
+				})
+				.expect('Content-Type', 'application/json')
+				.expect(400)
+				.expect((res: Response) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'error-user-not-in-role');
+					expect(res.body).to.have.property('error', 'User is not in this role [error-user-not-in-role]');
 				});
 		});
 
