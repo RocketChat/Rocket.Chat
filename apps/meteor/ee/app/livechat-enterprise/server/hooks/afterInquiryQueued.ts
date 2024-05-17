f299068a2d5f1f08124d67fccd1c9ea42726057e
Kevin Aleman <kaleman960@gmail.com>
2024-05-17 11:39:38 -0600

    update typings

--- apps/meteor/ee/app/livechat-enterprise/server/hooks/afterInquiryQueued.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/hooks/afterInquiryQueued.ts
@@ -1,3 +1,4 @@
+import type { ILivechatInquiryRecord } from '@rocket.chat/core-typings';
 import moment from 'moment';
 
 import { settings } from '../../../../../app/settings/server';
@@ -5,24 +6,23 @@ import { callbacks } from '../../../../../lib/callbacks';
 import { OmnichannelQueueInactivityMonitor } from '../lib/QueueInactivityMonitor';
 import { cbLogger } from '../lib/logger';
 
-let timer = 0;
+const withTimer = (timer: number) => {
+	return async (inquiry: ILivechatInquiryRecord) => {
+		if (!inquiry?._id || !inquiry?._updatedAt) {
+			return;
+		}
 
-const scheduleInquiry = async (inquiry: any): Promise<void> => {
-	if (!inquiry?._id || !inquiry?._createdAt) {
-		return;
-	}
-
-	// schedule individual jobs instead of property for close inactivty
-	const newQueueTime = moment(inquiry._updatedAt).add(timer, 'minutes');
-	cbLogger.debug(`Scheduling estimated close time at ${newQueueTime} for queued inquiry ${inquiry._id}`);
-	await OmnichannelQueueInactivityMonitor.scheduleInquiry(inquiry._id, new Date(newQueueTime.format()));
+		// schedule individual jobs instead of property for close inactivty
+		const newQueueTime = moment(inquiry._updatedAt).add(timer, 'minutes');
+		cbLogger.debug(`Scheduling estimated close time at ${newQueueTime} for queued inquiry ${inquiry._id}`);
+		await OmnichannelQueueInactivityMonitor.scheduleInquiry(inquiry._id, new Date(newQueueTime.format()));
+	};
 };
 
-settings.watch('Livechat_max_queue_wait_time', (value) => {
-	timer = value as number;
-	if (timer <= 0) {
+settings.watch<number>('Livechat_max_queue_wait_time', (value) => {
+	if (value <= 0) {
 		callbacks.remove('livechat.afterInquiryQueued', 'livechat-inquiry-queued-set-queue-timer');
 		return;
 	}
-	callbacks.add('livechat.afterInquiryQueued', scheduleInquiry, callbacks.priority.HIGH, 'livechat-inquiry-queued-set-queue-timer');
+	callbacks.add('livechat.afterInquiryQueued', withTimer(value), callbacks.priority.HIGH, 'livechat-inquiry-queued-set-queue-timer');
 });
