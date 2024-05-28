345e5e049669736c41dad2023b53e5350753b9af
Tasso <tasso.evangelista@rocket.chat>
2024-05-28 01:20:27 -0300

    Move test mock

--- apps/meteor/tests/mocks/client/meteor.ts
+++ apps/meteor/tests/mocks/client/meteor.ts
@@ -3,7 +3,3 @@ import { jest } from '@jest/globals';
 export const Meteor = {
 	loginWithSamlToken: jest.fn((_token, callback: () => void) => callback()),
 };
-
-export const Mongo = {
-	Collection: class Collection {},
-};
