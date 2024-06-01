87352f49cda496cb737910ae888bd4553f67cefc
Tasso <tasso.evangelista@rocket.chat>
2024-06-01 00:49:38 -0300

    Fix `ModalPortal` and the `ModalProvider` unit test

--- apps/meteor/client/portals/ModalPortal.tsx
+++ apps/meteor/client/portals/ModalPortal.tsx
@@ -1,18 +1,43 @@
-import type { ReactElement, ReactNode } from 'react';
-import React, { memo, useEffect, useState } from 'react';
+import { useUniqueId } from '@rocket.chat/fuselage-hooks';
+import type { ReactNode } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-import { createAnchor } from '../lib/utils/createAnchor';
-import { deleteAnchor } from '../lib/utils/deleteAnchor';
+function createModalRoot(id: string): HTMLElement {
+	const existing = document.getElementById(id);
+
+	if (existing) return existing;
+
+	const newOne = document.createElement('div');
+	newOne.id = id;
+	document.body.append(newOne);
+
+	return newOne;
+}
 
 type ModalPortalProps = {
 	children?: ReactNode;
 };
 
-const ModalPortal = ({ children }: ModalPortalProps): ReactElement => {
-	const [modalRoot] = useState(() => createAnchor('modal-root'));
-	useEffect(() => (): void => deleteAnchor(modalRoot), [modalRoot]);
-	return <>{createPortal(children, modalRoot)}</>;
+const ModalPortal = ({ children }: ModalPortalProps) => {
+	const id = `modal-root-${useUniqueId()}`;
+	const modalRootRef = useRef<HTMLElement | null>(null);
+
+	useEffect(
+		() => () => {
+			if (!modalRootRef.current) return;
+
+			modalRootRef.current.remove();
+			modalRootRef.current = null;
+		},
+		[id],
+	);
+
+	if (!modalRootRef.current) {
+		modalRootRef.current = createModalRoot(id);
+	}
+
+	return createPortal(children, modalRootRef.current);
 };
 
 export default memo(ModalPortal);
