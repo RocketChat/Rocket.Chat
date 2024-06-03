d2ef9e6587ac60040ae672504ba2823d353358c6
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-06-03 17:06:58 -0300

    chore: Remove meteor/check from custom-user-status endpoints

--- apps/meteor/tests/end-to-end/api/custom-user-status.ts
+++ apps/meteor/tests/end-to-end/api/custom-user-status.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { before, describe, it } from 'mocha';
+import { before, after, describe, it } from 'mocha';
 
 import { getCredentials, api, request, credentials } from '../../data/api-data';
 
@@ -36,6 +36,178 @@ describe('[CustomUserStatus]', () => {
 					expect(res.body).to.have.property('count');
 				})
 				.end(done);
+		});
+	});
+
+	describe('[/custom-user-status.create]', () => {
+		let customUserStatusId;
+
+		after(async () => {
+			return request
+				.post(api('custom-user-status.delete'))
+				.set(credentials)
+				.send({
+					customUserStatusId,
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				});
+		});
+
+		it('should fail creating a custom user status if no name is provided', async () => {
+			return request
+				.post(api('custom-user-status.create'))
+				.set(credentials)
+				.send({
+					statusType: 'online',
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'invalid-params');
+				});
+		});
+
+		it('should fail creating a custom user status if an invalid status type is provided', async () => {
+			return request
+				.post(api('custom-user-status.create'))
+				.set(credentials)
+				.send({
+					name: 'custom-status',
+					statusType: 'invalid-type',
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'invalid-params');
+				});
+		});
+
+		it('should succesfully create a custom user status', async () => {
+			const statusName = 'custom-status';
+			const statusType = 'online';
+			return request
+				.post(api('custom-user-status.create'))
+				.set(credentials)
+				.send({
+					name: statusName,
+					statusType,
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('customUserStatus').that.is.an('object');
+					expect(res.body.customUserStatus).to.have.property('_id');
+					expect(res.body.customUserStatus).to.have.property('name', statusName);
+					expect(res.body.customUserStatus).to.have.property('statusType', statusType);
+					expect(res.body.customUserStatus).to.have.property('_updatedAt');
+					customUserStatusId = res.body.customUserStatus._id;
+				});
+		});
+	});
+
+	describe('[/custom-user-status.update]', () => {
+		let customUserStatusId;
+		const customUserStatusName = 'custom-status';
+
+		before(async () => {
+			return request
+				.post(api('custom-user-status.create'))
+				.set(credentials)
+				.send({
+					name: customUserStatusName,
+					statusType: 'online',
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('customUserStatus').that.is.an('object');
+					expect(res.body.customUserStatus).to.have.property('_id');
+					expect(res.body.customUserStatus).to.have.property('name', customUserStatusName);
+					expect(res.body.customUserStatus).to.have.property('statusType', 'online');
+					expect(res.body.customUserStatus).to.have.property('_updatedAt');
+					customUserStatusId = res.body.customUserStatus._id;
+				});
+		});
+
+		after(async () => {
+			return request
+				.post(api('custom-user-status.delete'))
+				.set(credentials)
+				.send({
+					customUserStatusId,
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+				});
+		});
+
+		it('should fail updating a custom user status if no name is provided', async () => {
+			return request
+				.post(api('custom-user-status.update'))
+				.set(credentials)
+				.send({
+					_id: customUserStatusId,
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'invalid-params');
+				});
+		});
+
+		it('should fail updating a custom user status if no id is provided', async () => {
+			return request
+				.post(api('custom-user-status.update'))
+				.set(credentials)
+				.send({
+					name: customUserStatusName,
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'invalid-params');
+				});
+		});
+
+		it('should fail updating a custom user status if an invalid status type is provided', async () => {
+			return request
+				.post(api('custom-user-status.create'))
+				.set(credentials)
+				.send({
+					_id: customUserStatusId,
+					name: customUserStatusName,
+					statusType: 'invalid-type',
+				})
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('errorType', 'invalid-params');
+				});
+		});
+
+		it('should succesfully update a custom user status', async () => {
+			const statusName = 'custom-status-updated';
+			const statusType = 'offline';
+			return request
+				.post(api('custom-user-status.update'))
+				.set(credentials)
+				.send({
+					_id: customUserStatusId,
+					name: statusName,
+					statusType,
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', true);
+					expect(res.body).to.have.property('customUserStatus').that.is.an('object');
+					expect(res.body.customUserStatus).to.have.property('_id', customUserStatusId);
+					expect(res.body.customUserStatus).to.have.property('name', statusName);
+					expect(res.body.customUserStatus).to.have.property('statusType', statusType);
+					expect(res.body.customUserStatus).to.have.property('_updatedAt');
+				});
 		});
 	});
 });
--- packages/rest-typings/src/v1/customUserStatus.ts
+++ packages/rest-typings/src/v1/customUserStatus.ts
@@ -1,4 +1,5 @@
 import type { ICustomUserStatus, IUserStatus } from '@rocket.chat/core-typings';
+import { UserStatus } from '@rocket.chat/core-typings';
 import Ajv from 'ajv';
 
 import type { PaginatedRequest } from '../helpers/PaginatedRequest';
@@ -35,6 +36,47 @@ const CustomUserStatusListSchema = {
 
 export const isCustomUserStatusListProps = ajv.compile<CustomUserStatusListProps>(CustomUserStatusListSchema);
 
+type CustomUserStatusCreateProps = { name: string; statusType?: UserStatus };
+
+const CustomUserStatusCreateSchema = {
+	type: 'object',
+	properties: {
+		name: {
+			type: 'string',
+		},
+		statusType: {
+			type: 'string',
+			enum: Object.values(UserStatus),
+		},
+	},
+	required: ['name'],
+	additionalProperties: false,
+};
+
+export const isCustomUserStatusCreateProps = ajv.compile<CustomUserStatusCreateProps>(CustomUserStatusCreateSchema);
+
+type CustomUserStatusUpdateProps = { _id: string; name: string; statusType?: UserStatus };
+
+const CustomUserStatusUpdateSchema = {
+	type: 'object',
+	properties: {
+		_id: {
+			type: 'string',
+		},
+		name: {
+			type: 'string',
+		},
+		statusType: {
+			type: 'string',
+			enum: Object.values(UserStatus),
+		},
+	},
+	required: ['_id', 'name'],
+	additionalProperties: false,
+};
+
+export const isCustomUserStatusUpdateProps = ajv.compile<CustomUserStatusUpdateProps>(CustomUserStatusUpdateSchema);
+
 export type CustomUserStatusEndpoints = {
 	'/v1/custom-user-status.list': {
 		GET: (params: CustomUserStatusListProps) => PaginatedResult<{
@@ -42,7 +84,7 @@ export type CustomUserStatusEndpoints = {
 		}>;
 	};
 	'/v1/custom-user-status.create': {
-		POST: (params: { name: string; statusType?: string }) => {
+		POST: (params: CustomUserStatusCreateProps) => {
 			customUserStatus: ICustomUserStatus;
 		};
 	};
