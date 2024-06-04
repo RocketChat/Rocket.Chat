cda0ceac9969b7dd530c5846c1e15af984a1fc58
Kevin Aleman <kaleman960@gmail.com>
2024-06-04 07:57:29 -0600

    rename

--- packages/tools/src/getLoginExpiration.spec.ts
+++ packages/tools/src/getLoginExpiration.spec.ts
@@ -1,26 +1,26 @@
-import { getLoginExpiration } from './getLoginExpiration';
+import { getLoginExpirationInDays } from './getLoginExpiration';
 
 describe('getLoginExpiration', () => {
 	it('should return 90 by default', () => {
-		expect(getLoginExpiration()).toBe(90);
+		expect(getLoginExpirationInDays()).toBe(90);
 	});
 	it('should return 90 when value is 0', () => {
-		expect(getLoginExpiration(0)).toBe(90);
+		expect(getLoginExpirationInDays(0)).toBe(90);
 	});
 	it('should return 90 when value is NaN', () => {
-		expect(getLoginExpiration(NaN)).toBe(90);
+		expect(getLoginExpirationInDays(NaN)).toBe(90);
 	});
 	it('should return 90 when value is negative', () => {
-		expect(getLoginExpiration(-1)).toBe(90);
+		expect(getLoginExpirationInDays(-1)).toBe(90);
 	});
 	it('should return 90 when value is undefined', () => {
-		expect(getLoginExpiration(undefined)).toBe(90);
+		expect(getLoginExpirationInDays(undefined)).toBe(90);
 	});
 	it('should return 90 when value is not a number', () => {
 		// @ts-expect-error - Testing
-		expect(getLoginExpiration('90')).toBe(90);
+		expect(getLoginExpirationInDays('90')).toBe(90);
 	});
 	it('should return the value passed when its valid', () => {
-		expect(getLoginExpiration(85)).toBe(85);
+		expect(getLoginExpirationInDays(85)).toBe(85);
 	});
 });
