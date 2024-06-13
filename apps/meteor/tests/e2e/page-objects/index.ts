01e914cf17872b611c39cf2139807863cb26eed2
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-06-13 17:25:16 -0400

    Revert "test: Add Omnichannel Business Hours E2E tests (#32303)"
    This reverts commit c4e59b9f5853478f9fa7bbd16b8c1b8b3487f2c4.

--- apps/meteor/tests/e2e/page-objects/index.ts
+++ apps/meteor/tests/e2e/page-objects/index.ts
@@ -16,6 +16,5 @@ export * from './omnichannel-units';
 export * from './home-omnichannel';
 export * from './omnichannel-monitors';
 export * from './omnichannel-settings';
-export * from './omnichannel-business-hours';
 export * from './omnichannel-tags';
 export * from './utils';
