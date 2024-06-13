5c908a2449c272c9c7c3768e3e98fc500c6e9adb
Kevin Aleman <kaleman960@gmail.com>
2024-06-13 15:04:00 -0600

    me dumb me forget callbacks added 1 time

--- apps/meteor/ee/app/livechat-enterprise/server/hooks/afterInquiryQueued.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/hooks/afterInquiryQueued.ts
@@ -6,17 +6,21 @@ import { callbacks } from '../../../../../lib/callbacks';
 import { OmnichannelQueueInactivityMonitor } from '../lib/QueueInactivityMonitor';
 import { cbLogger } from '../lib/logger';
 
-export const withTimer = (timer: number) => {
-	return async (inquiry: ILivechatInquiryRecord) => {
-		if (!inquiry?._id || !inquiry?._updatedAt) {
-			return;
-		}
+export const afterInquiryQueued = async (inquiry: ILivechatInquiryRecord) => {
+	if (!inquiry?._id || !inquiry?._updatedAt) {
+		return;
+	}
 
-		// schedule individual jobs instead of property for close inactivty
-		const newQueueTime = moment(inquiry._updatedAt).add(timer, 'minutes');
-		cbLogger.debug(`Scheduling estimated close time at ${newQueueTime} for queued inquiry ${inquiry._id}`);
-		await OmnichannelQueueInactivityMonitor.scheduleInquiry(inquiry._id, new Date(newQueueTime.format()));
-	};
+	const timer = settings.get<number>('Livechat_max_queue_wait_time');
+
+	if (timer <= 0) {
+		return;
+	}
+
+	// schedule individual jobs instead of property for close inactivty
+	const newQueueTime = moment(inquiry._updatedAt).add(timer, 'minutes');
+	cbLogger.debug(`Scheduling estimated close time at ${newQueueTime} for queued inquiry ${inquiry._id}`);
+	await OmnichannelQueueInactivityMonitor.scheduleInquiry(inquiry._id, new Date(newQueueTime.format()));
 };
 
 settings.watch<number>('Livechat_max_queue_wait_time', (value) => {
@@ -24,5 +28,5 @@ settings.watch<number>('Livechat_max_queue_wait_time', (value) => {
 		callbacks.remove('livechat.afterInquiryQueued', 'livechat-inquiry-queued-set-queue-timer');
 		return;
 	}
-	callbacks.add('livechat.afterInquiryQueued', withTimer(value), callbacks.priority.HIGH, 'livechat-inquiry-queued-set-queue-timer');
+	callbacks.add('livechat.afterInquiryQueued', afterInquiryQueued, callbacks.priority.HIGH, 'livechat-inquiry-queued-set-queue-timer');
 });
--- apps/meteor/ee/tests/unit/apps/livechat-enterprise/server/hooks/afterInquiryQueued.spec.ts
+++ apps/meteor/ee/tests/unit/apps/livechat-enterprise/server/hooks/afterInquiryQueued.spec.ts
@@ -6,6 +6,7 @@ import sinon from 'sinon';
 
 const settingStub = {
 	watch: sinon.stub(),
+	get: sinon.stub(),
 };
 
 const callbackStub = {
@@ -18,26 +19,29 @@ const queueMonitorStub = {
 	scheduleInquiry: sinon.stub(),
 };
 
-const { withTimer } = proxyquire.noCallThru().load('../../../../../../app/livechat-enterprise/server/hooks/afterInquiryQueued.ts', {
-	'../../../../../app/settings/server': {
-		settings: settingStub,
-	},
-	'../../../../../lib/callbacks': {
-		callbacks: callbackStub,
-	},
-	'../lib/QueueInactivityMonitor': {
-		OmnichannelQueueInactivityMonitor: queueMonitorStub,
-	},
-	'../lib/logger': {
-		cbLogger: { debug: sinon.stub() },
-	},
-});
+const { afterInquiryQueued } = proxyquire
+	.noCallThru()
+	.load('../../../../../../app/livechat-enterprise/server/hooks/afterInquiryQueued.ts', {
+		'../../../../../app/settings/server': {
+			settings: settingStub,
+		},
+		'../../../../../lib/callbacks': {
+			callbacks: callbackStub,
+		},
+		'../lib/QueueInactivityMonitor': {
+			OmnichannelQueueInactivityMonitor: queueMonitorStub,
+		},
+		'../lib/logger': {
+			cbLogger: { debug: sinon.stub() },
+		},
+	});
 
 describe('hooks/afterInquiryQueued', () => {
 	beforeEach(() => {
 		callbackStub.add.resetHistory();
 		callbackStub.remove.resetHistory();
 		queueMonitorStub.scheduleInquiry.resetHistory();
+		settingStub.get.resetHistory();
 	});
 
 	it('should call settings.watch at first', () => {
@@ -50,36 +54,66 @@ describe('hooks/afterInquiryQueued', () => {
 		func(1);
 		expect(callbackStub.add.callCount).to.be.equal(1);
 
+		func(2);
+		expect(callbackStub.add.callCount).to.be.equal(2);
+
 		func(0);
 		expect(callbackStub.remove.callCount).to.be.equal(1);
 
 		func(-1);
 		expect(callbackStub.remove.callCount).to.be.equal(2);
+
+		func(3);
+		expect(callbackStub.add.callCount).to.be.equal(3);
 	});
 
-	it('withTimer should return a function', () => {
-		expect(typeof withTimer(1)).to.be.equal('function');
+	it('should return undefined if no inquiry is passed, or if inquiry doesnt have valid properties', async () => {
+		expect(await afterInquiryQueued(null)).to.be.equal(undefined);
+		expect(await afterInquiryQueued({})).to.be.equal(undefined);
+		expect(await afterInquiryQueued({ _id: 'invalid' })).to.be.equal(undefined);
+		expect(await afterInquiryQueued({ _updatedAt: new Date() }));
+		expect(await afterInquiryQueued({ _updatedAt: null, _id: 'afsd34asdX' })).to.be.equal(undefined);
 	});
 
-	it('returned function should fail if no inquiry is passed, or if inquiry doesnt have valid properties', async () => {
-		const func = withTimer(1);
-		expect(await func(null)).to.be.equal(undefined);
-		expect(await func({})).to.be.equal(undefined);
-		expect(await func({ _id: 'invalid' })).to.be.equal(undefined);
-		expect(await func({ _updatedAt: null, _id: 'afsd34asdX' })).to.be.equal(undefined);
-	});
-
-	it('should call .scheduleInquiry with proper data', async () => {
-		const func = withTimer(1);
-
+	it('should do nothing if timer is set to 0 or less', async () => {
 		const inquiry = {
 			_id: 'afsd34asdX',
 			_updatedAt: new Date(),
 		};
 
-		await func(inquiry);
+		settingStub.get.returns(0);
+		await afterInquiryQueued(inquiry);
+		expect(queueMonitorStub.scheduleInquiry.callCount).to.be.equal(0);
+
+		settingStub.get.returns(-1);
+		await afterInquiryQueued(inquiry);
+		expect(queueMonitorStub.scheduleInquiry.callCount).to.be.equal(0);
+	});
+
+	it('should call .scheduleInquiry with proper data', async () => {
+		const inquiry = {
+			_id: 'afsd34asdX',
+			_updatedAt: new Date(),
+		};
+
+		settingStub.get.returns(1);
+		await afterInquiryQueued(inquiry);
 
 		const newQueueTime = moment(inquiry._updatedAt).add(1, 'minutes');
+
+		expect(queueMonitorStub.scheduleInquiry.calledWith(inquiry._id, new Date(newQueueTime.format()))).to.be.true;
+	});
+
+	it('should call .scheduleInquiry with proper data when more than 1 min is passed as param', async () => {
+		const inquiry = {
+			_id: 'afv34avzx',
+			_updatedAt: new Date(),
+		};
+
+		settingStub.get.returns(3);
+		await afterInquiryQueued(inquiry);
+
+		const newQueueTime = moment(inquiry._updatedAt).add(3, 'minutes');
 
 		expect(queueMonitorStub.scheduleInquiry.calledWith(inquiry._id, new Date(newQueueTime.format()))).to.be.true;
 	});
