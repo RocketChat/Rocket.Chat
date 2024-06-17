68d67744d0ef8d046efbd4b7c49ea441b6083a16
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-06-17 15:46:28 -0300

    improve deprecation message

--- apps/meteor/ee/server/api/engagementDashboard/channels.ts
+++ apps/meteor/ee/server/api/engagementDashboard/channels.ts
@@ -55,25 +55,14 @@ API.v1.addRoute(
 			const { start, end, hideRoomsWithNoActivity } = this.queryParams;
 			const { offset, count } = await getPaginationItems(this.queryParams);
 
-			if (hideRoomsWithNoActivity) {
-				apiDeprecationLogger.deprecatedParameterUsage(
-					this.request.route,
-					'hideRoomsWithNoActivity',
-					'7.0.0',
-					this.response,
-					({ parameter, endpoint, version }) =>
-						`Setting the \`${parameter}\` param is deprecated in ${endpoint} and will be removed on version ${version}. This endpoint will behave as when \`${parameter}\` is set as \`true\` by default starting on ${version}`,
-				);
-			} else {
-				apiDeprecationLogger.deprecatedParameterUsage(
-					this.request.route,
-					'hideRoomsWithNoActivity',
-					'7.0.0',
-					this.response,
-					({ parameter, endpoint, version }) =>
-						`Returning rooms that had no activity in ${endpoint} is deprecated and will be removed on version ${version}. Set \`${parameter}\` as \`true\` to check how the endpoint will behave starting on ${version}`,
-				);
-			}
+			apiDeprecationLogger.deprecatedParameterUsage(
+				this.request.route,
+				'hideRoomsWithNoActivity',
+				'7.0.0',
+				this.response,
+				({ parameter, endpoint, version }) =>
+					`Returning rooms that had no activity in ${endpoint} is deprecated and will be removed on version ${version} along with the \`${parameter}\` param. Set \`${parameter}\` as \`true\` to check how the endpoint will behave starting on ${version}`,
+			);
 
 			const { channels, total } = await findChannelsWithNumberOfMessages({
 				start: mapDateForAPI(start),
