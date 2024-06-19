45dc3d5f725e395aad485d24d10e8f31feb15db0
Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>
2024-06-20 04:00:40 +0530

    feat: Disable some message menu items for encrypted messages. (#32559)

--- apps/meteor/client/components/GenericMenu/GenericMenu.tsx
+++ apps/meteor/client/components/GenericMenu/GenericMenu.tsx
@@ -41,7 +41,7 @@ const GenericMenu = ({ title, icon = 'menu', disabled, onAction, ...props }: Gen
 
 	const hasIcon = itemsList.some(({ icon }) => icon);
 	const handleItems = (items: GenericMenuItemProps[]) =>
-		hasIcon ? items.map((item) => ({ ...item, gap: !item.icon && !item.status })) : items;
+		hasIcon ? items.map((item) => ({ ...item, gap: item.gap ?? (!item.icon && !item.status) })) : items;
 
 	const isMenuEmpty = !(sections && sections.length > 0) && !(items && items.length > 0);
 
