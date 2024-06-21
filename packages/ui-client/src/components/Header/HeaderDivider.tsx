2a01dee4d6cef86076ad5569c30cce7a4d44518b
juliajforesti <juliajforesti@gmail.com>
2024-06-21 15:43:14 -0300

    chore: HeaderDivider margin

--- packages/ui-client/src/components/Header/HeaderDivider.tsx
+++ packages/ui-client/src/components/Header/HeaderDivider.tsx
@@ -1,6 +1,6 @@
 import { Divider } from '@rocket.chat/fuselage';
 import type { FC } from 'react';
 
-const HeaderDivider: FC = () => <Divider mbs={-1} mbe={0} />;
+const HeaderDivider: FC = () => <Divider mbs={-2} mbe={0} />;
 
 export default HeaderDivider;
