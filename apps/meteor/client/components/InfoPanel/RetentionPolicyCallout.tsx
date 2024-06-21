49092af267b47121be0659308636a2c72dac4366
gabriellsh <gabriel.henriques@rocket.chat>
2024-06-21 16:32:31 -0300

    fix aria-label

--- apps/meteor/client/components/InfoPanel/RetentionPolicyCallout.tsx
+++ apps/meteor/client/components/InfoPanel/RetentionPolicyCallout.tsx
@@ -1,14 +1,16 @@
 import type { IRoom } from '@rocket.chat/core-typings';
 import { Callout } from '@rocket.chat/fuselage';
+import { useTranslation } from '@rocket.chat/ui-contexts';
 import React from 'react';
 
 import { usePruneWarningMessage } from '../../hooks/usePruneWarningMessage';
 
 const RetentionPolicyCallout = ({ room }: { room: IRoom }) => {
 	const message = usePruneWarningMessage(room);
+	const t = useTranslation();
 
 	return (
-		<Callout arial-label={message} role='alert' aria-live='polite' type='warning'>
+		<Callout arial-label={t('Retention_policy_warning_callout')} role='alert' aria-live='polite' type='warning'>
 			<div>
 				<p>{message}</p>
 			</div>
