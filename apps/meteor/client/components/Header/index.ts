fa22d7a6bb1666df0128ae6fbee328fe89406d59
juliajforesti <juliajforesti@gmail.com>
2024-06-25 17:36:50 -0300

    chore: reexport `ui-client/Header` in `client/components with feat preview

---
+++ apps/meteor/client/components/Header/index.ts
@@ -0,0 +1,37 @@
+import Header from './Header';
+import HeaderAvatar from './HeaderAvatar';
+import HeaderContent from './HeaderContent';
+import HeaderContentRow from './HeaderContentRow';
+import HeaderDivider from './HeaderDivider';
+import HeaderIcon from './HeaderIcon';
+import HeaderState from './HeaderState';
+import HeaderSubtitle from './HeaderSubtitle';
+import HeaderTag from './HeaderTag';
+import HeaderTagIcon from './HeaderTagIcon';
+import HeaderTagSkeleton from './HeaderTagSkeleton';
+import HeaderTitle from './HeaderTitle';
+import HeaderTitleButton from './HeaderTitleButton';
+import HeaderToolbar from './HeaderToolbar';
+import HeaderToolbarAction from './HeaderToolbarAction';
+import HeaderToolbarActionBadge from './HeaderToolbarActionBadge';
+import HeaderToolbarDivider from './HeaderToolbarDivider';
+
+export {
+	Header,
+	HeaderAvatar,
+	HeaderContent,
+	HeaderContentRow,
+	HeaderDivider,
+	HeaderIcon,
+	HeaderState,
+	HeaderSubtitle,
+	HeaderTag,
+	HeaderTagIcon,
+	HeaderTagSkeleton,
+	HeaderTitle,
+	HeaderTitleButton,
+	HeaderToolbar,
+	HeaderToolbarAction,
+	HeaderToolbarActionBadge,
+	HeaderToolbarDivider,
+};
