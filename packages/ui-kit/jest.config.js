33c28c23ca7924eb93ca4bcc896eb650d535007e
Tasso <tasso.evangelista@rocket.chat>
2024-06-26 13:17:50 -0300

    Add tests for types

--- packages/ui-kit/jest.config.js
+++ packages/ui-kit/jest.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+module.exports = /** @satisfies {import('jest').Config} */ ({
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	errorOnDeprecated: true,
 	testMatch: ['<rootDir>/src/**/*.spec.[jt]s?(x)'],
-};
+	setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
+});
