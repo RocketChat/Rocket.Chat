4a1f323dcb761236c9219301edbf804e8d88885a
Aseem Pratap Singh <89309144+Aseem22@users.noreply.github.com>
2024-06-26 21:04:09 +0530

    fix lint in RoomsTable.tsx

--- apps/meteor/client/views/admin/rooms/RoomsTable.tsx
+++ apps/meteor/client/views/admin/rooms/RoomsTable.tsx
@@ -34,7 +34,7 @@ const RoomsTable = ({ reload }: { reload: MutableRefObject<() => void> }): React
 
 	const prevRoomFilterText = useRef<string>(roomFilters.searchText);
 
-	const { sortBy, sortDirection, setSort } = useSort<'name' | 't' | 'usersCount' | 'msgs' | 'default' | 'featured'|'createdAt'>('name');
+	const { sortBy, sortDirection, setSort } = useSort<'name' | 't' | 'usersCount' | 'msgs' | 'default' | 'featured' | 'createdAt'>('name');
 	const { current, itemsPerPage, setItemsPerPage, setCurrent, ...paginationProps } = usePagination();
 	const searchText = useDebouncedValue(roomFilters.searchText, 500);
 
