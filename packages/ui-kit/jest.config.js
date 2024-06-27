12427c05e9a344b12e8816ccdb7d3007c70fb183
Tasso <tasso.evangelista@rocket.chat>
2024-06-27 10:20:28 -0300

    Enable code coverage collecting

--- packages/ui-kit/jest.config.js
+++ packages/ui-kit/jest.config.js
@@ -4,4 +4,5 @@ module.exports = /** @satisfies {import('jest').Config} */ ({
 	errorOnDeprecated: true,
 	testMatch: ['<rootDir>/src/**/*.spec.[jt]s?(x)'],
 	setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
+	collectCoverage: true,
 });
