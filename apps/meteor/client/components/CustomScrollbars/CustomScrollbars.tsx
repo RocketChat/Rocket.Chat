8e32f7e089267967361006948fd5eb50af6dda95
akshithere <narbhakshit@gmail.com>
2024-07-09 21:24:16 +0530

    Fixed RTL languages double scrollbar issue by a direction prop

--- apps/meteor/client/components/CustomScrollbars/CustomScrollbars.tsx
+++ apps/meteor/client/components/CustomScrollbars/CustomScrollbars.tsx
@@ -2,7 +2,7 @@ import { Palette } from '@rocket.chat/fuselage';
 import type { ScrollValues } from 'rc-scrollbars';
 import { Scrollbars } from 'rc-scrollbars';
 import type { MutableRefObject, CSSProperties, ReactNode } from 'react';
-import React, { memo, forwardRef, useCallback, useMemo } from 'react';
+import React, { memo, forwardRef, useCallback, useMemo, useEffect } from 'react';
 
 export type CustomScrollbarsProps = {
 	overflowX?: boolean;
@@ -12,6 +12,7 @@ export type CustomScrollbarsProps = {
 	renderView?: typeof Scrollbars.defaultProps.renderView;
 	renderTrackHorizontal?: typeof Scrollbars.defaultProps.renderTrackHorizontal;
 	autoHide?: boolean;
+	direction?: 'rtl' | 'ltr';
 };
 
 const styleDefault: CSSProperties = {
@@ -21,7 +22,7 @@ const styleDefault: CSSProperties = {
 };
 
 const CustomScrollbars = forwardRef<HTMLElement, CustomScrollbarsProps>(function CustomScrollbars(
-	{ children, style, onScroll, overflowX, renderView, ...props },
+	{ children, style, onScroll, overflowX, renderView, direction, ...props },
 	ref,
 ) {
 	const scrollbarsStyle = useMemo(() => ({ ...style, ...styleDefault }), [style]);
@@ -40,13 +41,22 @@ const CustomScrollbars = forwardRef<HTMLElement, CustomScrollbarsProps>(function
 		[ref],
 	);
 
+	// Apply styles to hide the default scrollbar when the component mounts
+	useEffect(() => {
+		const root = document.documentElement;
+		root.style.overflow = 'hidden';
+		return () => {
+			root.style.overflow = ''; // Reset when component unmounts
+		};
+	}, []);
+
 	return (
 		<Scrollbars
 			{...props}
 			autoHide
 			autoHideTimeout={2000}
 			autoHideDuration={500}
-			style={scrollbarsStyle}
+			style={{ ...scrollbarsStyle, direction: direction || 'ltr' }} // Apply direction style
 			onScrollFrame={onScroll}
 			renderView={renderView}
 			renderTrackHorizontal={overflowX ? undefined : (props) => <div {...props} className='track-horizontal' style={{ display: 'none' }} />}
