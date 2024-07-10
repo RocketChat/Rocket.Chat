4b79a776bc8e71aa7d56412675db93a50bbe4247
Kevin Aleman <kaleman960@gmail.com>
2024-07-10 09:58:17 -0600

    missing notify

--- apps/meteor/app/api/server/v1/users.ts
+++ apps/meteor/app/api/server/v1/users.ts
@@ -1295,6 +1295,8 @@ API.v1.addRoute(
 						},
 					);
 
+					void notifyOnUserChange({ clientAction: 'updated', id: user._id, diff: { status } });
+
 					const { _id, username, statusText, roles, name } = user;
 					void api.broadcast('presence.status', {
 						user: { status, _id, username, statusText, roles, name },
