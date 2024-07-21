f7fa0201a7a23f916a5f29dc452c12560b89cb63
Debdut Chakraborty <debdutdeb@outlook.com>
2024-07-21 23:34:44 +0530

    prevent deactivation of federation related users

--- apps/meteor/server/methods/setUserActiveStatus.ts
+++ apps/meteor/server/methods/setUserActiveStatus.ts
@@ -1,5 +1,6 @@
 import type { ServerMethods } from '@rocket.chat/ui-contexts';
-import { MatrixBridgedUser } from '@rocket.chat/models';
+import { MatrixBridgedUser, Users } from '@rocket.chat/models';
+import type { IUser } from '@rocket.chat/core-typings';
 import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
@@ -18,13 +19,14 @@ Meteor.methods<ServerMethods>({
 		check(userId, String);
 		check(active, Boolean);
 
-		if (!Meteor.userId()) {
+		const uid = Meteor.userId();
+
+		if (!uid) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
 				method: 'setUserActiveStatus',
 			});
 		}
 
-		const uid = Meteor.userId();
 
 		if (!uid || (await hasPermissionAsync(uid, 'edit-other-user-active-status')) !== true) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', {
@@ -32,12 +34,22 @@ Meteor.methods<ServerMethods>({
 			});
 		}
 
-		const remoteUser = await MatrixBridgedUser.getExternalUserIdByLocalUserId(uid);
-		if (remoteUser) {
-			throw new Meteor.Error('error-not-allowed', 'User is participating in matrix federation, user deactivation is not allowed, only deletion is allowed',
+		const { federated } = await Users.findOneById<Pick<IUser, 'federated'>>(uid, { projection: { federated: 1 } }) || {};
+
+		if (federated) {
+			throw new Meteor.Error('error-not-allowed', 'Deactivating federated user is not allowed',
 				{ method: 'deleteUser' },
 			);
 		}
+
+		const remoteUser = await MatrixBridgedUser.getExternalUserIdByLocalUserId(uid);
+
+		if (remoteUser) {
+			throw new Meteor.Error('error-not-allowed', 'User is participating in federation, deactivation is not allowed',
+				{ method: 'deleteUser' },
+			);
+		}
+
 		await setUserActiveStatus(userId, active, confirmRelinquish);
 
 		return true;
