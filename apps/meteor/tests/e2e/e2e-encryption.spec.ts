2028d188a4e579a4149cf103aeb6a10cf3705418
yash-rajpal <rajpal.yash03@gmail.com>
2024-07-23 00:40:16 +0530

    oops

--- apps/meteor/tests/e2e/e2e-encryption.spec.ts
+++ apps/meteor/tests/e2e/e2e-encryption.spec.ts
@@ -421,7 +421,7 @@ test.describe.serial('e2e-encryption', () => {
 		await expect(poHomeChannel.content.nthMessage(0)).toContainText(
 			'This message is end-to-end encrypted. To view it, you must enter your encryption key in your account settings.',
 		);
-		await expect(poHomeChannel.content.lastUserMessage.locator('.rcx-icon--name-key')).toBeVisible();
+		await expect(poHomeChannel.content.nthMessage(0).locator('.rcx-icon--name-key')).toBeVisible();
 	});
 
 	test('expect slash commands to be enabled in an e2ee room', async ({ page }) => {
