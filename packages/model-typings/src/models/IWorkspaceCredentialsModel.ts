7a8dfeaea9b7e23c0125b218f8e1ff8b287ffa73
gustrb <gustavoreisbauer@gmail.com>
2024-07-22 08:16:00 -0300

    feat: Add model definition for the IWorkspaceCredential model
    Here we will have a few operations into a WorkspaceCredential, such as creating, deleting, updating, etc

---
+++ packages/model-typings/src/models/IWorkspaceCredentialsModel.ts
@@ -0,0 +1,11 @@
+
+import type { IWorkspaceCredentials } from '@rocket.chat/core-typings';
+
+import type { IBaseModel } from './IBaseModel';
+
+export interface IWorkspaceCredentialsModel extends IBaseModel<IWorkspaceCredentials> {
+	getCredentialByScope(scope: string): Promise<IWorkspaceCredentials | null>;
+	unsetCredentialByScope(scope: string): Promise<void>;
+	updateCredentialByScope(scope: string, accessToken: string, expirationDate: Date): Promise<void>;
+	removeAllCredentials(): Promise<void>;
+}
