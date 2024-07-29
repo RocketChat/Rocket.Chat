3cf1467c98848dbda6a3bcc10eb807ab5a226ae8
Ricardo Garim <rswarovsky@gmail.com>
2024-07-29 09:21:35 -0300

    chore: typecheck fix

--- apps/meteor/app/livechat/server/lib/LivechatTyped.ts
+++ apps/meteor/app/livechat/server/lib/LivechatTyped.ts
@@ -1271,7 +1271,7 @@ class LivechatClass {
 			},
 		};
 
-		await Message.saveSystemMessage(transferMessage.t, room._id, '', { _id, username }, transferMessage);
+		await Message.saveSystemMessage('livechat_transfer_history', room._id, '', { _id, username }, transferMessage);
 	}
 
 	async saveGuest(guestData: Pick<ILivechatVisitor, '_id' | 'name' | 'livechatData'> & { email?: string; phone?: string }, userId: string) {
