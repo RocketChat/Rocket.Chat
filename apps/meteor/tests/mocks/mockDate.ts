3c540b9c22b1332f83b205a1806c8c238c564f65
gabriellsh <gabriel.henriques@rocket.chat>
2024-07-31 09:23:34 -0300

    fix date

--- apps/meteor/tests/mocks/mockDate.ts
+++ apps/meteor/tests/mocks/mockDate.ts
@@ -1,12 +1,13 @@
 // you must use jest.useFakeTimers for this to work.
 export const setDate = (minutes = 1, hours = 0, date = 1) => {
-	// June 12, 2024, 12:00 AM
+	// June 1, 2024, 12:00 AM
 	const fakeDate = new Date();
 	fakeDate.setFullYear(2024);
-	fakeDate.setMonth(5);
 	fakeDate.setDate(date);
 	fakeDate.setHours(hours);
 	fakeDate.setMinutes(minutes);
 	fakeDate.setSeconds(0);
+	// month has to come after day, because june doesn't have a 31st day so it skips to july depending on the day the CI runs
+	fakeDate.setMonth(5);
 	jest.setSystemTime(fakeDate);
 };
