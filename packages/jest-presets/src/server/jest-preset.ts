d68c1043ccada03d56d41f7c74f1c260924b7698
Tasso <tasso.evangelista@rocket.chat>
2024-08-02 20:09:27 -0300

    Fix Jest transform issue in `@rocket.chat/i18n`

--- packages/jest-presets/src/server/jest-preset.ts
+++ packages/jest-presets/src/server/jest-preset.ts
@@ -1,3 +1,4 @@
+import type * as SWC from '@swc/core';
 import type { Config } from 'jest';
 
 export default {
@@ -17,9 +18,10 @@ export default {
 						dynamicImport: true,
 					},
 				},
-			},
+			} satisfies SWC.Config,
 		],
 	},
+	transformIgnorePatterns: ['<rootDir>/node_modules/@babel', '<rootDir>/node_modules/@jest', '/node_modules/(?!@testing-library/)'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
 
 	collectCoverage: true,
