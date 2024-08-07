cffe4c92224b3b6336b2f1bccea9edca46e93bd6
Tasso <tasso.evangelista@rocket.chat>
2024-08-07 15:49:31 -0300

    Use `@rocket.chat/jest-presets`

--- packages/ui-video-conf/.jest/setup.ts
+++
@@ -1,3 +0,0 @@
-import { toHaveNoViolations } from 'jest-axe';
-
-expect.extend(toHaveNoViolations);
--- packages/ui-video-conf/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	preset: 'ts-jest/presets/js-with-babel',
-	errorOnDeprecated: true,
-	testMatch: ['<rootDir>/src/**/*.spec.{ts,tsx}'],
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', '<rootDir>/.jest/setup.ts'],
-};
