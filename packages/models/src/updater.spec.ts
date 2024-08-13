d94ca8030b739fd86a0a140e73232a328155af3d
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-08-13 17:30:21 -0300

    fix spec tests

--- packages/models/src/updater.spec.ts
+++ packages/models/src/updater.spec.ts
@@ -17,56 +17,62 @@ test('updater typings', () => {
 		e: string[];
 	}>();
 
+	// @ts-expect-error: it should not allow any string to `t` only `l` is allowed
+	updater.set('t', 'a');
+	// `l` is allowed
+	updater.set('t', 'l');
+
+	// `a` is { b: string }
+	updater.set('a', { b: 'test' });
+	updater.set('a.b', 'test');
+	// @ts-expect-error: it should not allow strings to `a`, a is an object containing `b: string`
+	updater.set('a', 'b');
+	// @ts-expect-error: `a` is not optional so unset is not allowed
+	updater.unset('a');
+	// @ts-expect-error: strings cannot be incremented
+	updater.inc('a', 1);
+
+	// `c` is number but it should be optional, so unset is allowed
+	updater.unset('c');
+	updater.set('c', 1);
+	// @ts-expect-error: `c` is a number
+	updater.set('c', 'b');
+	// inc is allowed for numbers
+	updater.inc('c', 1);
+
+	// `d` is { e: string } but it should be optional, so unset is allowed
+	updater.unset('d');
+	updater.set('d', { e: 'a' });
+	// @ts-expect-error: `d` is an object
+	updater.set('d', 'a');
+
+	// @ts-expect-error: it should not allow numbers, since e is a string
+	updater.addToSet('e', 1);
+	// @ts-expect-error: it should not allow strings, since a is an object
+	updater.addToSet('a', 'b');
+
+	updater.addToSet('e', 'a');
+	// @ts-expect-error: it should not allow `njame` its not specified in the model
+	updater.set('njame', 1);
+
+	// `d` is { e: string } and also it should be optional, so unset is allowed
+	updater.unset('d.e');
+	// @ts-expect-error: `d` is an object cannot be incremented
+	updater.inc('d', 1);
+
+	// `activity` is a string
 	const omnichannel = new UpdaterImpl<IOmnichannelRoom>();
 	omnichannel.addToSet('v.activity', 'asd');
-	// @ts-expect-error
+	// @ts-expect-error: it should not allow numbers, since activity is a string
 	omnichannel.addToSet('v.activity', 1);
-	// @ts-expect-error
+	// @ts-expect-error: it should not allow objects, since activity is a string
 	omnichannel.addToSet('v.activity', {
 		asdas: 1,
 	});
-
-	// @ts-expect-error
+	// @ts-expect-error: it should not allow sub properties, since activity is a string
 	omnichannel.addToSet('v.activity.asd', {
 		asdas: 1,
 	});
-
-	updater.addToSet('e', 'a');
-
-	// @ts-expect-error
-	updater.addToSet('e', 1);
-	// @ts-expect-error
-	updater.addToSet('a', 'b');
-
-	// @ts-expect-error
-	updater.set('njame', 1);
-	// @ts-expect-error
-	updater.set('ttes', 1);
-	// @ts-expect-error
-	updater.set('t', 'a');
-	updater.set('t', 'l');
-	// @ts-expect-error
-	updater.set('a', 'b');
-	// @ts-expect-error
-	updater.set('c', 'b');
-	updater.set('c', 1);
-
-	updater.set('a', {
-		b: 'set',
-	});
-	updater.set('a.b', 'test');
-
-	// @ts-expect-error
-	updater.unset('a');
-
-	updater.unset('c');
-
-	updater.unset('d');
-
-	updater.unset('d.e');
-	// @ts-expect-error
-	updater.inc('d', 1);
-	updater.inc('c', 1);
 });
 
 test('updater $set operations', async () => {
