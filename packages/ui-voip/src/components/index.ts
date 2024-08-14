5d7465ba944f64457c2637b3b6058ec467cc048f
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-08-14 12:18:35 -0300

    feat: implemented component VoipContactId

--- packages/ui-voip/src/components/index.ts
+++ packages/ui-voip/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as VoipDialPad } from './VoipDialPad';
 export { default as VoipTimer } from './VoipTimer';
 export { default as VoipStatus } from './VoipStatus';
+export { default as VoipContactId } from './VoipContactId';
