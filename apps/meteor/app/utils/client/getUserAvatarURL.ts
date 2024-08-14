598c89971cfac1f61dbd04fe69fa1027f449db4f
Tasso <tasso.evangelista@rocket.chat>
2024-08-14 16:17:33 -0300

    Fix lint issues

--- apps/meteor/app/utils/client/getUserAvatarURL.ts
+++ apps/meteor/app/utils/client/getUserAvatarURL.ts
@@ -3,7 +3,7 @@ import { getAvatarURL } from './getAvatarURL';
 
 export const getUserAvatarURL = function (username: string, cache = ''): string | undefined {
 	const externalSource = (settings.get<string>('Accounts_AvatarExternalProviderUrl') || '').trim().replace(/\/$/, '');
-	const externalSourceProxy = (settings.get<boolean>('Accounts_AvatarExternalProviderProxy') || false)
+	const externalSourceProxy = settings.get<boolean>('Accounts_AvatarExternalProviderProxy') || false;
 	if (externalSource !== '' && externalSourceProxy) {
 		return externalSource.replace('{username}', username);
 	}
