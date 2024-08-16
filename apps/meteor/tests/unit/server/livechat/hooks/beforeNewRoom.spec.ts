375eef88d0b65d0e3ac4bc0564f3272932369b5f
Abhinav Kumar <abhinav@avitechlab.com>
2024-08-16 19:49:21 +0530

    improved tests
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- apps/meteor/tests/unit/server/livechat/hooks/beforeNewRoom.spec.ts
+++ apps/meteor/tests/unit/server/livechat/hooks/beforeNewRoom.spec.ts
@@ -13,7 +13,12 @@ proxyquire.noCallThru().load('../../../../../ee/app/livechat-enterprise/server/h
 	},
 	'@rocket.chat/models': {
 		OmnichannelServiceLevelAgreements: {
-			findOneByIdOrName: sinon.stub().returns({ _id: 'slaId' }),
+			findOneByIdOrName: sinon.stub().callsFake((id) => {
+				if (id === 'high') {
+					return Promise.resolve({ _id: 'high' });
+				}
+				return Promise.resolve(null);
+			}),
 		},
 	},
 });
@@ -26,14 +31,20 @@ describe('livechat.beforeRoom', () => {
 		expect(result).to.deep.equal({ ...roomInfo, customFields: extraData.customFields });
 	});
 
+	it('should throw an error when provided with an invalid sla', async () => {
+		const roomInfo = { name: 'test' };
+		const extraData = { customFields: { test: 'test' }, sla: 'invalid' };
+		await expect(callbacks.run('livechat.beforeRoom', roomInfo, extraData)).to.be.rejectedWith(Error);
+	});
+
 	it('should not include field in roomInfo when extraData has field other than customFields, sla', async () => {
 		const roomInfo = { name: 'test' };
 		const extraData = { customFields: { test: 'test' }, sla: 'high' };
 		const result = await callbacks.run('livechat.beforeRoom', roomInfo, extraData);
-		expect(result).to.deep.equal({ ...roomInfo, customFields: extraData.customFields, slaId: 'slaId' });
+		expect(result).to.deep.equal({ ...roomInfo, customFields: extraData.customFields, slaId: 'high' });
 	});
 
-	it('should return roomInfo with empty customFields when customFields is not an object', async () => {
+	it('should return roomInfo with no customFields when customFields is not an object', async () => {
 		const roomInfo = { name: 'test' };
 		const extraData = { customFields: 'not an object' };
 		const result = await callbacks.run('livechat.beforeRoom', roomInfo, extraData);
