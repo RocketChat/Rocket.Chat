ea1c31bddb598466540cc90d8f67335071e37d4a
yash-rajpal <rajpal.yash03@gmail.com>
2024-08-20 19:06:06 +0530

    fix tests for session storage

--- apps/meteor/tests/e2e/account-forgetSessionOnWindowClose.spec.ts
+++ apps/meteor/tests/e2e/account-forgetSessionOnWindowClose.spec.ts
@@ -16,16 +16,17 @@ test.describe.serial('Forget session on window close setting', () => {
 			await api.post('/settings/Accounts_ForgetUserSessionOnWindowClose', { value: false });
 		});
 
-		test('Login using credentials and reload to stay logged in', async ({ page }) => {
+		test('Login using credentials and reload to stay logged in', async ({ page, context }) => {
 			await poRegistration.username.type('user1');
 			await poRegistration.inputPassword.type(DEFAULT_USER_CREDENTIALS.password);
 			await poRegistration.btnLogin.click();
 
 			await expect(page.locator('role=heading[name="Welcome to Rocket.Chat"]')).toBeVisible();
 
-			await page.reload();
+			const newPage = await context.newPage();
+			await newPage.goto('/home');
 
-			await expect(page.locator('role=heading[name="Welcome to Rocket.Chat"]')).toBeVisible();
+			await expect(newPage.locator('role=heading[name="Welcome to Rocket.Chat"]')).toBeVisible();
 		});
 	});
 
@@ -38,16 +39,17 @@ test.describe.serial('Forget session on window close setting', () => {
 			await api.post('/settings/Accounts_ForgetUserSessionOnWindowClose', { value: false });
 		});
 
-		test('Login using credentials and reload to get logged out', async ({ page }) => {
+		test('Login using credentials and reload to get logged out', async ({ page, context }) => {
 			await poRegistration.username.type('user1');
 			await poRegistration.inputPassword.type(DEFAULT_USER_CREDENTIALS.password);
 			await poRegistration.btnLogin.click();
 
 			await expect(page.locator('role=heading[name="Welcome to Rocket.Chat"]')).toBeVisible();
 
-			await page.reload();
+			const newPage = await context.newPage();
+			await newPage.goto('/home');
 
-			await expect(page.locator('role=button[name="Login"]')).toBeVisible();
+			await expect(newPage.locator('role=button[name="Login"]')).toBeVisible();
 		});
 	});
 });
