12920691c871b8b3b629defce0981bc16bf0836f
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-08-20 14:11:22 -0300

    test: added legacyRoot to tests

--- apps/meteor/client/views/admin/users/UserPageHeaderContentWithSeatsCap.spec.tsx
+++ apps/meteor/client/views/admin/users/UserPageHeaderContentWithSeatsCap.spec.tsx
@@ -7,6 +7,7 @@ import UserPageHeaderContent from './UserPageHeaderContentWithSeatsCap';
 
 it('should render "Associate Extension" button when VoIP_TeamCollab_Enabled setting is enabled', async () => {
 	render(<UserPageHeaderContent activeUsers={1} maxActiveUsers={1} isSeatsCapExceeded={false} />, {
+		legacyRoot: true,
 		wrapper: mockAppRoot().withJohnDoe().withSetting('VoIP_TeamCollab_Enabled', true).build(),
 	});
 
@@ -15,6 +16,7 @@ it('should render "Associate Extension" button when VoIP_TeamCollab_Enabled sett
 
 it('should not render "Associate Extension" button when VoIP_TeamCollab_Enabled setting is disabled', async () => {
 	render(<UserPageHeaderContent activeUsers={1} maxActiveUsers={1} isSeatsCapExceeded={false} />, {
+		legacyRoot: true,
 		wrapper: mockAppRoot().withJohnDoe().withSetting('VoIP_TeamCollab_Enabled', false).build(),
 	});
 
