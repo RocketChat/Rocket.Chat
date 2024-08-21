d667dee171e90937bff97bd27a23110aee4810be
Douglas Fabris <devfabris@gmail.com>
2024-08-21 17:08:58 -0300

    feat: Omnichannel's security and privacy settings page (#32988)
    * chore: remove hidden prop in new settings
    
    * feat: new security and privacy page
    
    * fix: review
    
    * chore: changeset

---
+++ apps/meteor/client/omnichannel/securityPrivacy/SecurityPrivacyPage.tsx
@@ -0,0 +1,22 @@
+import { useIsPrivilegedSettingsContext } from '@rocket.chat/ui-contexts';
+import React from 'react';
+
+import { useEditableSettingsGroupSections } from '../../views/admin/EditableSettingsContext';
+import GenericGroupPage from '../../views/admin/settings/groups/GenericGroupPage';
+import NotAuthorizedPage from '../../views/notAuthorized/NotAuthorizedPage';
+
+const GROUP_ID = 'Omnichannel';
+const SECTION_ID = 'Contact_identification';
+
+const SecurityPrivacyPage = () => {
+	const hasPermission = useIsPrivilegedSettingsContext();
+	const sections = useEditableSettingsGroupSections(GROUP_ID).filter((id) => id === SECTION_ID);
+
+	if (!hasPermission) {
+		return <NotAuthorizedPage />;
+	}
+
+	return <GenericGroupPage i18nLabel='Security_and_privacy' sections={sections} _id={GROUP_ID} />;
+};
+
+export default SecurityPrivacyPage;
