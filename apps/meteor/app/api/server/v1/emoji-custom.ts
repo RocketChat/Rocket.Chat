8347c29defb0557bb6369bb896d8090429ab5e44
Kevin Aleman <kaleman960@gmail.com>
2024-08-22 13:04:08 -0600

    Update apps/meteor/app/api/server/v1/emoji-custom.ts

--- apps/meteor/app/api/server/v1/emoji-custom.ts
+++ apps/meteor/app/api/server/v1/emoji-custom.ts
@@ -185,7 +185,7 @@ API.v1.addRoute(
 			};
 
 			const updatedEmojiData = await insertOrUpdateEmoji(this.userId, emojiData);
-			if (newFile && fileBuffer) {
+			if (newFile) {
 				await uploadEmojiCustomWithBuffer(this.userId, fileBuffer, mimetype, updatedEmojiData);
 			}
 			return API.v1.success();
