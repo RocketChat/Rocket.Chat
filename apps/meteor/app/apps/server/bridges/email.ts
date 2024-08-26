71225717b881169cd0f2e74f2a5864d4f1569e85
Rafael Tapia <rafael.tapia@rocket.chat>
2024-08-26 18:37:47 -0300

    feat: implement email bridge

---
+++ apps/meteor/app/apps/server/bridges/email.ts
@@ -0,0 +1,16 @@
+import type { IAppServerOrchestrator } from '@rocket.chat/apps';
+import { EmailBridge } from '@rocket.chat/apps-engine/server/bridges/EmailBridge';
+import type { IEmail } from '@rocket.chat/apps-engine/server/definitions/email';
+
+import * as Mailer from '../../../mailer/server/api';
+
+export class AppEmailBridge extends EmailBridge {
+	constructor(private readonly orch: IAppServerOrchestrator) {
+		super();
+	}
+
+	protected async sendEmail(email: IEmail, appId: string): Promise<void> {
+		this.orch.debugLog(`The app ${appId} is sending an email.`);
+		await Mailer.send(email);
+	}
+}
