9ef09d8ff3ce0c144494ffe4054913c156d83dd1
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-08-27 20:41:08 -0300

    Revert "refactor: Realtime Monitoring Counters to TS" (#33170)

--- apps/meteor/client/views/omnichannel/realTimeMonitoring/counter/CounterRow.js
+++ apps/meteor/client/views/omnichannel/realTimeMonitoring/counter/CounterRow.js
@@ -1,16 +1,11 @@
 import { Box, Divider } from '@rocket.chat/fuselage';
-import type { ComponentProps, ReactNode } from 'react';
 import React, { Fragment } from 'react';
 import flattenChildren from 'react-keyed-flatten-children';
 
-type CounterRowProps = {
-	children: ReactNode[];
-} & ComponentProps<typeof Box>;
-
-const CounterRow = ({ children, ...props }: CounterRowProps) => (
+const CounterRow = ({ children, ...props }) => (
 	<Box pb={28} pi={20} display='flex' flexDirection='row' justifyContent='space-around' alignItems='center' flexGrow={1} {...props}>
 		{children &&
-			flattenChildren(children).reduce((acc: ReactNode[], child, i) => {
+			flattenChildren(children).reduce((acc, child, i) => {
 				acc =
 					children.length - 1 !== i
 						? [
