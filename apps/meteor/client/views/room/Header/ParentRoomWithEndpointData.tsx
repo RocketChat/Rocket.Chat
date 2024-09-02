afd2bf27ed5d23de817b2c6fd55a5b50ded61724
Kevin Aleman <kaleman960@gmail.com>
2024-09-02 12:01:26 -0600

    Discard changes to apps/meteor/client/views/room/Header/ParentRoomWithEndpointData.tsx

--- apps/meteor/client/views/room/Header/ParentRoomWithEndpointData.tsx
+++ apps/meteor/client/views/room/Header/ParentRoomWithEndpointData.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { HeaderTagSkeleton } from '../../../components/Header';
 import { useRoomInfoEndpoint } from '../../../hooks/useRoomInfoEndpoint';
 import ParentRoom from './ParentRoom';
-import ParentTeam from './ParentTeam';
 
 type ParentRoomWithEndpointDataProps = {
 	rid: IRoom['_id'];
@@ -20,17 +19,6 @@ const ParentRoomWithEndpointData = ({ rid }: ParentRoomWithEndpointDataProps): R
 
 	if (isError || !data?.room) {
 		return null;
-	}
-
-	if (data.parent) {
-		return <ParentRoom room={data.parent} />;
-	}
-
-	if (data.team) {
-		if (data.team.roomId === rid) {
-			return null;
-		}
-		return <ParentTeam team={data.team} />;
 	}
 
 	return <ParentRoom room={data.room} />;
