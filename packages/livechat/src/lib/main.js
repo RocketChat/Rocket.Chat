07b632095749ccb09b0c0bccd7ddd8fe26221889
Ricardo Garim <rswarovsky@gmail.com>
2024-09-07 19:01:42 -0300

    fix: reset unreads and set lastReadMessageId on message sent by visitor
    chore: refactor processUnread for readability
    
    chore: add changeset
    
    r
    
    a

--- packages/livechat/src/lib/main.js
+++ packages/livechat/src/lib/main.js
@@ -92,30 +92,20 @@ export const getUnreadMessages = () => {
 	const renderedMessages = messages.filter((message) => canRenderMessage(message));
 	const lastReadMessageIndex = lastReadMessageId
 		? renderedMessages.findIndex((item) => item._id === lastReadMessageId)
-		: renderedMessages
-				.slice()
-				.reverse()
-				.findIndex((item) => item.u._id === user?._id);
+		: renderedMessages.findIndex((item) => item.u._id === user?._id);
 
-	if (lastReadMessageIndex !== -1) {
-		const unreadMessages = renderedMessages.slice(lastReadMessageIndex + 1).filter((message) => message.u._id !== user?._id);
-
-		return unreadMessages;
-	}
-
-	return [];
+	return renderedMessages.slice(lastReadMessageIndex).filter((item) => !item.token).length;
 };
 
 export const processUnread = async () => {
 	const shouldMarkUnread = shouldMarkAsUnread();
 	if (shouldMarkUnread) {
 		const unreadMessages = getUnreadMessages();
-
-		if (unreadMessages.length > 0) {
+		if (unreadMessages > 0) {
 			const { alerts } = store.state;
 			const lastReadMessage = getLastReadMessage();
 			const alertMessage = i18next.t('count_new_messages_since_since', {
-				count: unreadMessages.length,
+				count: unreadMessages,
 				val: new Date(lastReadMessage.ts),
 				formatParams: {
 					val: { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' },
@@ -124,7 +114,9 @@ export const processUnread = async () => {
 
 			const alert = { id: constants.unreadMessagesAlertId, children: alertMessage, success: true, timeout: 0 };
 			const newAlerts = alerts.filter((item) => item.id !== constants.unreadMessagesAlertId);
-			await store.setState({ alerts: (newAlerts.push(alert), newAlerts), unread: unreadMessages.length });
+			await store.setState({ alerts: (newAlerts.push(alert), newAlerts), unread: unreadMessages });
+			return;
 		}
+		await store.setState({ unread: 0 });
 	}
 };
