edf9026500bbbc3b76ec3624905545a98fa024f7
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-09-11 19:25:33 -0300

    feat: implemented component VoipActions

--- packages/ui-voip/src/components/index.ts
+++ packages/ui-voip/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as VoipTimer } from './VoipTimer';
 export { default as VoipStatus } from './VoipStatus';
 export { default as VoipContactId } from './VoipContactId';
 export { default as VoipActionButton } from './VoipActionButton';
+export { default as VoipActions } from './VoipActions';
