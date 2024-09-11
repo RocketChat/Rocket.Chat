e2888cf1b6707ce1474f37de57137b3a594f3e2a
ggazzo <ggazzo@users.noreply.github.com>
2024-09-11 13:09:22 +0000

    Deploy preview for PR 33248 🛫

---
+++ pr-preview/pr-33248/uikit-playground/assets/KatexElement-791d2bd1.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-575be6a6.js";import{k as o}from"./katex-c2839aef.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"}}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
