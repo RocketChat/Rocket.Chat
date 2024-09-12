c328eb931e7ce56182083fd532225cb1f9de8a50
MartinSchoeler <MartinSchoeler@users.noreply.github.com>
2024-09-12 20:06:50 +0000

    Deploy preview for PR 33238 🛫

---
+++ pr-preview/pr-33238/uikit-playground/assets/KatexElement-ead134c5.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-a697576b.js";import{k as o}from"./katex-c2839aef.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
