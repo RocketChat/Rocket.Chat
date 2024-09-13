b3a67d3433eba64243bb319d12fdbd14a6603240
Marcos Defendi <marcos.defendi@rocket.chat>
2024-09-13 08:11:37 -0300

    fix: make setting available on client side

--- apps/meteor/ee/app/license/server/settings.ts
+++ apps/meteor/ee/app/license/server/settings.ts
@@ -22,9 +22,7 @@ await settingsRegistry.addGroup('Enterprise', async function () {
 	});
 	await this.add('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days', -1, {
 		type: 'int',
-		hidden: true,
 		readonly: true,
-		secret: true,
 		public: true,
 	});
 });
