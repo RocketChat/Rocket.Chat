a5602d9849eb2d77f65b2429b210e69ecec14264
Martin Schoeler <martin.schoeler@rocket.chat>
2024-09-18 13:56:18 -0300

    feat: new modal on Private Apps install (#33275)
    * feat: new modal on Private Apps install
    
    * add more variations
    
    * Create eleven-rockets-hug.md

---
+++ .changeset/eleven-rockets-hug.md
@@ -0,0 +1,6 @@
+---
+"@rocket.chat/meteor": feat
+"@rocket.chat/i18n": feat
+---
+
+Added a new button and modal to the private apps screen
--- apps/meteor/client/views/marketplace/components/MarketplaceHeader.tsx
+++ apps/meteor/client/views/marketplace/components/MarketplaceHeader.tsx
@@ -1,13 +1,15 @@
 import { Button, ButtonGroup } from '@rocket.chat/fuselage';
 import { usePermission, useRoute, useRouteParameter, useSetModal, useTranslation } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import React, { useCallback } from 'react';
+import React from 'react';
 
 import { GenericResourceUsageSkeleton } from '../../../components/GenericResourceUsage';
 import { PageHeader } from '../../../components/Page';
+import UpgradeButton from '../../admin/subscription/components/UpgradeButton';
 import UnlimitedAppsUpsellModal from '../UnlimitedAppsUpsellModal';
 import { useAppsCountQuery } from '../hooks/useAppsCountQuery';
 import EnabledAppsCount from './EnabledAppsCount';
+import PrivateAppInstallModal from './PrivateAppInstallModal/PrivateAppInstallModal';
 
 const MarketplaceHeader = ({ title }: { title: string }): ReactElement | null => {
 	const t = useTranslation();
@@ -17,9 +19,16 @@ const MarketplaceHeader = ({ title }: { title: string }): ReactElement | null =>
 	const setModal = useSetModal();
 	const result = useAppsCountQuery(context);
 
-	const handleUploadButtonClick = useCallback((): void => {
+	const handleProceed = (): void => {
+		setModal(null);
 		route.push({ context, page: 'install' });
-	}, [context, route]);
+	};
+
+	const handleClickPrivate = () => {
+		result?.data?.limit === 0
+			? setModal(<PrivateAppInstallModal onClose={() => setModal(null)} onProceed={handleProceed} />)
+			: route.push({ context, page: 'install' });
+	};
 
 	if (result.isError) {
 		return null;
@@ -29,8 +38,10 @@ const MarketplaceHeader = ({ title }: { title: string }): ReactElement | null =>
 		<PageHeader title={title}>
 			<ButtonGroup wrap align='end'>
 				{result.isLoading && <GenericResourceUsageSkeleton />}
-				{result.isSuccess && !result.data.hasUnlimitedApps && <EnabledAppsCount {...result.data} context={context} />}
-				{isAdmin && result.isSuccess && !result.data.hasUnlimitedApps && (
+
+				{!result.isSuccess && !result.data.hasUnlimitedApps && <EnabledAppsCount {...result.data} context={context} />}
+
+				{isAdmin && result.isSuccess && !result.data.hasUnlimitedApps && context !== 'private' && (
 					<Button
 						onClick={() => {
 							setModal(<UnlimitedAppsUpsellModal onClose={() => setModal(null)} />);
@@ -39,7 +50,14 @@ const MarketplaceHeader = ({ title }: { title: string }): ReactElement | null =>
 						{t('Enable_unlimited_apps')}
 					</Button>
 				)}
-				{isAdmin && context === 'private' && <Button onClick={handleUploadButtonClick}>{t('Upload_private_app')}</Button>}
+
+				{isAdmin && context === 'private' && <Button onClick={handleClickPrivate}>{t('Upload_private_app')}</Button>}
+
+				{isAdmin && result.isSuccess && result.data.limit === 0 && context === 'private' && (
+					<UpgradeButton primary icon={undefined} target='private-apps-header' action='upgrade'>
+						{t('Upgrade')}
+					</UpgradeButton>
+				)}
 			</ButtonGroup>
 		</PageHeader>
 	);
