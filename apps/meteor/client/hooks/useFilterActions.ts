81b0daa9c8d9e5bd013e1e9e6d3aab655a646718
Dnouv <evan.shu.dev@gmail.com>
2024-09-18 20:07:43 +0530

    optimize bool checks

--- apps/meteor/client/hooks/useFilterActions.ts
+++ apps/meteor/client/hooks/useFilterActions.ts
@@ -5,20 +5,19 @@ import { useCallback } from 'react';
 export const useFilterActionsByContextAndCategory = (context: string | undefined, category: UIActionButtonCategory) => {
 	return useCallback(
 		(action: IUIActionButton) => {
-			if (context) {
-				const isDefaultCategory = category === UIActionButtonCategory.DEFAULT;
-				const isCategoryMatch = isDefaultCategory
-					? !action.category || action.category === UIActionButtonCategory.DEFAULT
-					: action?.category === category;
-				const isContextMatch = (action.when?.messageActionContext || ['message', 'message-mobile', 'threads', 'starred']).includes(
-					context as any,
-				);
-
-				if (!isContextMatch || !isCategoryMatch) {
-					return false;
-				}
+			if (!context) {
+				return true;
 			}
-			return true;
+
+			const actionCategory = action?.category ?? UIActionButtonCategory.DEFAULT;
+			const isContextMatch = (action.when?.messageActionContext || ['message', 'message-mobile', 'threads', 'starred']).includes(
+				context as any,
+			);
+
+			const isCategoryMatch =
+				category === UIActionButtonCategory.DEFAULT ? actionCategory === UIActionButtonCategory.DEFAULT : actionCategory === category;
+
+			return isContextMatch && isCategoryMatch;
 		},
 		[context, category],
 	);
