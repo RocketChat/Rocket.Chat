95ebcf2f28c4103dacf5b57ff7fb632c53bb4614
Pierre <pierre.lehnen@rocket.chat>
2024-09-19 10:48:39 -0300

    Use 'view-user-voip-extension' permission to decide if extension should be loaded

--- apps/meteor/app/lib/server/functions/getFullUserData.ts
+++ apps/meteor/app/lib/server/functions/getFullUserData.ts
@@ -20,7 +20,6 @@ const defaultFields = {
 	statusText: 1,
 	avatarETag: 1,
 	extension: 1,
-	freeSwitchExtension: 1,
 	federated: 1,
 	statusLivechat: 1,
 } as const;
@@ -36,6 +35,7 @@ const fullFields = {
 	requirePasswordChangeReason: 1,
 	roles: 1,
 	importIds: 1,
+	freeSwitchExtension: 1,
 } as const;
 
 let publicCustomFields: Record<string, 0 | 1> = {};
@@ -86,6 +86,7 @@ export async function getFullUserDataByIdOrUsernameOrImportId(
 		(searchType === 'username' && searchValue === caller.username) ||
 		(searchType === 'importId' && caller.importIds?.includes(searchValue));
 	const canViewAllInfo = !!myself || (await hasPermissionAsync(userId, 'view-full-other-user-info'));
+	const canViewExtension = !!myself || (await hasPermissionAsync(userId, 'view-user-voip-extension'));
 
 	// Only search for importId if the user has permission to view the import id
 	if (searchType === 'importId' && !canViewAllInfo) {
@@ -97,6 +98,7 @@ export async function getFullUserDataByIdOrUsernameOrImportId(
 	const options = {
 		projection: {
 			...fields,
+			...(canViewExtension && { freeSwitchExtension: 1 }),
 			...(myself && { services: 1 }),
 		},
 	};
