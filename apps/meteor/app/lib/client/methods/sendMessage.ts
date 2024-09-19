a541f64c8ca02e4448a4bfbff2699d6e96f17b69
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-09-19 10:42:07 -0300

    fix: error on sendmessage stub (#33317)

--- apps/meteor/app/lib/client/methods/sendMessage.ts
+++ apps/meteor/app/lib/client/methods/sendMessage.ts
@@ -43,7 +43,7 @@ Meteor.methods<ServerMethods>({
 
 		await onClientMessageReceived(message as IMessage).then((message) => {
 			ChatMessage.insert(message);
-			return callbacks.run('afterSaveMessage', message, room);
+			return callbacks.run('afterSaveMessage', message, { room });
 		});
 	},
 });
