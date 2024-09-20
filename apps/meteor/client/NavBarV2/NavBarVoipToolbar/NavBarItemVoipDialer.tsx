66a53f2d6a9208da92fbe8a9d4782e761beba9bf
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-09-20 17:26:25 -0300

    feat: added dialer button to NavBarV2

---
+++ apps/meteor/client/NavBarV2/NavBarVoipToolbar/NavBarItemVoipDialer.tsx
@@ -0,0 +1,47 @@
+import { NavBarItem } from '@rocket.chat/fuselage';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useLayout } from '@rocket.chat/ui-contexts';
+import { useVoipDialer, useVoipState } from '@rocket.chat/ui-voip';
+import type { HTMLAttributes } from 'react';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+type NavBarItemVoipDialerProps = Omit<HTMLAttributes<HTMLElement>, 'is'>;
+
+const NavBarItemVoipDialer = (props: NavBarItemVoipDialerProps) => {
+	const { t } = useTranslation();
+	const { sidebar } = useLayout();
+	const { isEnabled, isReady, isRegistered } = useVoipState();
+	const { open: isDialerOpen, openDialer, closeDialer } = useVoipDialer();
+
+	const handleToggleDialer = useEffectEvent(() => {
+		sidebar.toggle();
+		isDialerOpen ? closeDialer() : openDialer();
+	});
+
+	const title = useMemo(() => {
+		if (!isReady) {
+			return t('Loading');
+		}
+
+		if (!isRegistered) {
+			return t('Voice_calling_disabled');
+		}
+
+		return t('New_call');
+	}, [isReady, isRegistered, t]);
+
+	return isEnabled ? (
+		<NavBarItem
+			{...props}
+			primary
+			title={title}
+			icon='phone'
+			onClick={handleToggleDialer}
+			pressed={isDialerOpen}
+			disabled={!isReady || !isRegistered}
+		/>
+	) : null;
+};
+
+export default NavBarItemVoipDialer;
