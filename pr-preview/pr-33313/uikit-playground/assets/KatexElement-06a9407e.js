423ffe324b95c8df2e505fecfbf020eab3af3cbe
pierre-lehnen-rc <pierre-lehnen-rc@users.noreply.github.com>
2024-09-23 15:12:59 +0000

    Deploy preview for PR 33313 🛫

---
+++ pr-preview/pr-33313/uikit-playground/assets/KatexElement-06a9407e.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-2095e4e3.js";import{k as o}from"./katex-3c610da1.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
