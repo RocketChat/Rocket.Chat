6cec2ce5749785c498245794ff61f5881838bd2d
Martin Schoeler <martin.schoeler@rocket.chat>
2024-09-27 18:22:49 -0300

    chore!: Remove deprecated URL App installation (#33210)

--- packages/rest-typings/src/apps/index.ts
+++ packages/rest-typings/src/apps/index.ts
@@ -248,9 +248,8 @@ export type AppsEndpoints = {
 							version: string;
 							permissionsGranted?: IPermission[];
 							url?: string;
-							downloadOnly?: boolean;
 					  }
-					| { url: string; downloadOnly?: boolean },
+					| { url: string },
 			):
 				| {
 						app: App;
