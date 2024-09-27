530a5afc20019c89c24b0e8a79ee220f3a33f1d3
gabriellsh <gabriel.henriques@rocket.chat>
2024-09-27 19:46:05 -0300

    Update logic and tests

--- ee/packages/license/src/AirGappedRestriction.spec.ts
+++ ee/packages/license/src/AirGappedRestriction.spec.ts
@@ -9,10 +9,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			await AirGappedRestriction.checkRemainingDaysSinceLastStatsReport('invalid-token');
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: 0,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(true);
 		});
 		it('should notify remaining days (8) within the accepted range (1 - 10) when the last reported stats happened 2 days ago', async () => {
 			const now = new Date();
@@ -23,10 +24,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			await AirGappedRestriction.checkRemainingDaysSinceLastStatsReport(token);
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: 8,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(false);
 		});
 		it('should notify remaining days = 0 (apply restrictions) when the last reported stats happened more than 10 days ago', async () => {
 			const now = new Date();
@@ -37,10 +39,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			await AirGappedRestriction.checkRemainingDaysSinceLastStatsReport(token);
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: 0,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(true);
 		});
 		it('should notify remaining days = 0 (apply restrictions) when the last reported stats happened 10 days ago', async () => {
 			const now = new Date();
@@ -51,10 +54,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			await AirGappedRestriction.checkRemainingDaysSinceLastStatsReport(token);
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: 0,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(true);
 		});
 	});
 	describe('#applyRestrictions()', () => {
@@ -64,10 +68,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			AirGappedRestriction.applyRestrictions();
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: 0,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(true);
 		});
 	});
 	describe('#removeRestrictions()', () => {
@@ -77,10 +82,11 @@ describe('AirGappedRestriction', () => {
 			AirGappedRestriction.on('remainingDays', handler);
 			AirGappedRestriction.removeRestrictions();
 
-			expect(handler).toBeCalledTimes(1);
-			expect(handler).toBeCalledWith({
+			expect(handler).toHaveBeenCalledTimes(1);
+			expect(handler).toHaveBeenCalledWith({
 				days: -1,
 			});
+			expect(AirGappedRestriction.isRestricted).toBe(false);
 		});
 	});
 });
