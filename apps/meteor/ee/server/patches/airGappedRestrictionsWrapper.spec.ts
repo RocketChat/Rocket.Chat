288910138e605a19aac83d25ade5bd1b045e6ba5
gabriellsh <gabriel.henriques@rocket.chat>
2024-09-27 18:47:03 -0300

    wip

--- apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.spec.ts
+++ apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.spec.ts
@@ -1,27 +1,24 @@
+import { AirGappedRestriction } from '@rocket.chat/license';
+
 import { applyAirGappedRestrictionsValidation } from '../../../app/license/server/airGappedRestrictionsWrapper';
-import { settings } from '../../../app/settings/server';
+// import { settings } from '../../../app/settings/server';
 import './airGappedRestrictionsWrapper';
 
-jest.mock('../../../app/settings/server', () => ({
-	settings: {
-		get: jest.fn(),
-	},
-}));
+// jest.mock('@rocket.chat/license', () => ({
+// 	AirGappedRestriction: {
+// 		isRestricted: false,
+// 	},
+// }));
 
 describe('#airGappedRestrictionsWrapper()', () => {
-	it('should throw an error when the remaining days for airgapped restrictions is equal to 0', async () => {
-		(settings.get as jest.Mock).mockReturnValue(0);
+	it('should throw an error when the workspace is restricted', async () => {
+		// (settings.get as jest.Mock).mockReturnValue(0);
+		jest.spyOn(AirGappedRestriction, 'isRestricted', 'get').mockReturnValue(true);
 		await expect(() => applyAirGappedRestrictionsValidation(jest.fn())).rejects.toThrow(new Error('restricted-workspace'));
 	});
-	it('should NOT throw an error when the remaining days for airgapped restrictions is greater than 0', async () => {
-		(settings.get as jest.Mock).mockReturnValue(5);
-		const spy = jest.fn();
-		await expect(() => applyAirGappedRestrictionsValidation(spy)).not.toThrow();
-		await applyAirGappedRestrictionsValidation(spy);
-		expect(spy).toHaveBeenCalled();
-	});
-	it('should NOT throw an error when the remaining days for airgapped restrictions is equal to -1', async () => {
-		(settings.get as jest.Mock).mockReturnValue(-1);
+	it('should NOT throw an error when the workspace is not restricted', async () => {
+		// (settings.get as jest.Mock).mockReturnValue(5);
+		jest.spyOn(AirGappedRestriction, 'isRestricted', 'get').mockReturnValue(false);
 		const spy = jest.fn();
 		await expect(() => applyAirGappedRestrictionsValidation(spy)).not.toThrow();
 		await applyAirGappedRestrictionsValidation(spy);
