b891617a3a4086c35b87f1232aaa7f3c2b125664
gabriellsh <gabriel.henriques@rocket.chat>
2024-09-27 18:47:03 -0300

    wip

--- apps/meteor/ee/app/license/server/airGappedRestrictions.ts
+++ apps/meteor/ee/app/license/server/airGappedRestrictions.ts
@@ -9,28 +9,32 @@ import { i18n } from '../../../../server/lib/i18n';
 import { sendMessagesToAdmins } from '../../../../server/lib/sendMessagesToAdmins';
 import { checkAirGappedRestrictions } from './airGappedRestrictionsCheck';
 
-AirGappedRestriction.on('remainingDays', async ({ days }: { days: number }) => {
+const updateRestrictionSetting = async (remainingDays: number) => {
 	const value = settings.get('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days');
-	if (value === days) {
+	if (value === remainingDays) {
 		return;
 	}
-	await Settings.updateValueById('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days', days);
+	await Settings.updateValueById('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days', remainingDays);
 	void notifyOnSettingChangedById('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days');
-});
+};
 
-const WARNING_PERIOD_IN_DAYS = 7;
-settings.watch<number>('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days', async (days) => {
-	const lastDayOrNoRestrictionsAtAll = days <= 0;
+const sendRocketCatWarningToAdmins = async (remainingDays: number) => {
+	const lastDayOrNoRestrictionsAtAll = remainingDays <= 0;
 	if (lastDayOrNoRestrictionsAtAll) {
 		return;
 	}
-	if (days <= WARNING_PERIOD_IN_DAYS) {
+	if (remainingDays <= AirGappedRestriction.WARNING_PERIOD_IN_DAYS) {
 		await sendMessagesToAdmins({
 			msgs: async ({ adminUser }) => ({
-				msg: i18n.t('AirGapped_Restriction_Warning', { lng: adminUser.language || 'en', remainingDays: days }),
+				msg: i18n.t('AirGapped_Restriction_Warning', { lng: adminUser.language || 'en', remainingDays }),
 			}),
 		});
 	}
+};
+
+AirGappedRestriction.on('remainingDays', async ({ days }: { days: number }) => {
+	await updateRestrictionSetting(days);
+	await sendRocketCatWarningToAdmins(days);
 });
 
 Meteor.startup(async () => {
--- apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.ts
+++ apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.ts
@@ -1,8 +1,9 @@
+import { AirGappedRestriction } from '@rocket.chat/license';
+
 import { applyAirGappedRestrictionsValidation } from '../../../app/license/server/airGappedRestrictionsWrapper';
-import { settings } from '../../../app/settings/server';
 
 applyAirGappedRestrictionsValidation.patch(async <T>(_: any, fn: () => Promise<T>): Promise<T> => {
-	if (settings.get<number>('Cloud_Workspace_AirGapped_Restrictions_Remaining_Days') === 0) {
+	if (AirGappedRestriction.isRestricted) {
 		throw new Error('restricted-workspace');
 	}
 	return fn();
