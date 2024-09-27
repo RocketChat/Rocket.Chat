ca5cab61759bbf921c0b18b573aee6b090e30544
gabriellsh <gabriel.henriques@rocket.chat>
2024-09-27 19:50:40 -0300

    undo

--- apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.spec.ts
+++ apps/meteor/ee/server/patches/airGappedRestrictionsWrapper.spec.ts
@@ -1,24 +1,20 @@
-import { AirGappedRestriction } from '@rocket.chat/license';
-
 import { applyAirGappedRestrictionsValidation } from '../../../app/license/server/airGappedRestrictionsWrapper';
-// import { settings } from '../../../app/settings/server';
+import { settings } from '../../../app/settings/server';
 import './airGappedRestrictionsWrapper';
 
-// jest.mock('@rocket.chat/license', () => ({
-// 	AirGappedRestriction: {
-// 		isRestricted: false,
-// 	},
-// }));
+jest.mock('../../../app/settings/server', () => ({
+	settings: {
+		get: jest.fn(),
+	},
+}));
 
 describe('#airGappedRestrictionsWrapper()', () => {
 	it('should throw an error when the workspace is restricted', async () => {
-		// (settings.get as jest.Mock).mockReturnValue(0);
-		jest.spyOn(AirGappedRestriction, 'isRestricted', 'get').mockReturnValue(true);
+		(settings.get as jest.Mock).mockReturnValue(0);
 		await expect(() => applyAirGappedRestrictionsValidation(jest.fn())).rejects.toThrow(new Error('restricted-workspace'));
 	});
 	it('should NOT throw an error when the workspace is not restricted', async () => {
-		// (settings.get as jest.Mock).mockReturnValue(5);
-		jest.spyOn(AirGappedRestriction, 'isRestricted', 'get').mockReturnValue(false);
+		(settings.get as jest.Mock).mockReturnValue(5);
 		const spy = jest.fn();
 		await expect(() => applyAirGappedRestrictionsValidation(spy)).not.toThrow();
 		await applyAirGappedRestrictionsValidation(spy);
