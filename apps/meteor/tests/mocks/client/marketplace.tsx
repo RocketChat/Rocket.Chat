c5b0c9880386ed9ade286217373d5de4353ab5f6
Martin Schoeler <martin.schoeler@rocket.chat>
2024-10-01 02:14:11 -0300

    feat: New Private apps limitations (#33316)
    * feat: New empty state for upgrading private Apps
    
    * chore: Change Marketplace info modal text (#33239)
    
    * feat: New tooltips and color behavior for private apps bar (#33243)
    
    * feat: New tooltips and behavior for private apps bar
    
    * Create brown-pants-press.md
    
    * feat: new modal on Private Apps install (#33275)
    
    * feat: new modal on Private Apps install
    
    * add more variations
    
    * Create eleven-rockets-hug.md
    
    * chore: change grandfathered modal text (#33291)
    
    * chore: Use apps provider to check maxPrivateApps
    
    * fix: adds minor fixes to UI and changes requested on review
    
    * Update changeset
    
    * Replace negative boolean
    
    * Refactor `AppsUsageCard`
    
    * Add unit test for `AppsUsageCard`
    
    * Add unit test for `PrivateEmptyState`
    
    * Add unit test for `EnabledAppsCount`
    
    * Move tooltip logic away from `useAppsCountQuery`
    
    ---------
    
    Co-authored-by: Lucas Pelegrino <lucas.pelegrino@icloud.com>
    Co-authored-by: Tasso <tasso.evangelista@rocket.chat>

--- apps/meteor/tests/mocks/client/marketplace.tsx
+++ apps/meteor/tests/mocks/client/marketplace.tsx
@@ -62,6 +62,7 @@ export const mockedAppsContext = (children: ReactNode) => (
 			},
 			reload: () => Promise.resolve(),
 			orchestrator: mockAppsOrchestrator(),
+			privateAppsEnabled: false,
 		}}
 	>
 		{children}
