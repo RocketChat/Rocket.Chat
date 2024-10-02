35049f14a1b506bf7e29bef08d00799f3d6c821f
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-10-02 01:25:43 -0300

    run migration only in CE workspaces

--- apps/meteor/server/startup/migrations/v307.ts
+++ apps/meteor/server/startup/migrations/v307.ts
@@ -1,6 +1,6 @@
 import { Apps, type AppMetadataStorage } from '@rocket.chat/apps';
-import { AppStatusUtils } from '@rocket.chat/apps-engine/definition/AppStatus';
 import type { IAppStorageItem } from '@rocket.chat/apps-engine/server/storage';
+import { License } from '@rocket.chat/license';
 
 import { addMigration } from '../../lib/migrations';
 
@@ -8,6 +8,11 @@ addMigration({
 	version: 307,
 	name: "Mark all installed private apps as 'migrated'",
 	async up() {
+		const isEE = License.hasValidLicense();
+		if (isEE) {
+			return;
+		}
+
 		if (!Apps.self) {
 			throw new Error('Apps Orchestrator not registered.');
 		}
@@ -19,15 +24,13 @@ addMigration({
 		const promises: Array<ReturnType<AppMetadataStorage['update']>> = [];
 
 		apps.forEach((app) => {
-			if (AppStatusUtils.isEnabled(app.status)) {
-				promises.push(
-					appsStorage.update({
-						...app,
-						migrated: true,
-						installationSource: 'marketplaceInfo' in app ? 'marketplace' : 'private',
-					} as IAppStorageItem),
-				);
-			}
+			promises.push(
+				appsStorage.update({
+					...app,
+					migrated: true,
+					installationSource: 'marketplaceInfo' in app ? 'marketplace' : 'private',
+				} as IAppStorageItem),
+			);
 		});
 
 		await Promise.all(promises);
