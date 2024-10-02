62fb2f5e32f6141b58fb65f547098ec5b21736af
Kevin Aleman <kaleman960@gmail.com>
2024-10-02 10:19:21 -0600

    refactor!: Room's Key ID generation (#33329)
    Co-authored-by: Hugo Costa <hugocarreiracosta@gmail.com>
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/app/e2e/client/helper.js
+++ apps/meteor/app/e2e/client/helper.js
@@ -146,3 +146,10 @@ export async function generateMnemonicPhrase(n, sep = ' ') {
 	}
 	return result.join(sep);
 }
+
+export async function createSha256Hash(data) {
+	const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(data));
+	return Array.from(new Uint8Array(hash))
+		.map((b) => b.toString(16).padStart(2, '0'))
+		.join('');
+}
