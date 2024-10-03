22b541f732196c74779307f9cc6bd5687f59bf63
Douglas Gubert <douglas.gubert@gmail.com>
2024-10-03 20:31:22 -0300

    When an external module becomes invalid, disable any apps that depend on it

---
+++ apps/meteor/ee/server/startup/apps.ts
@@ -0,0 +1,28 @@
+import { AppStatus } from '@rocket.chat/apps-engine/definition/AppStatus';
+import { License } from '@rocket.chat/license';
+import { Meteor } from 'meteor/meteor';
+
+import { Apps } from '../apps';
+
+Meteor.startup(() => {
+	async function disableAppsCallback() {
+		void Apps.disableApps();
+	}
+
+	License.onInvalidateLicense(disableAppsCallback);
+	License.onRemoveLicense(disableAppsCallback);
+	// Disable apps that depend on add-ons (external modules) if they are invalidated
+	License.onModule(async ({ module, external, valid }) => {
+		if (!external || valid) return;
+
+		const enabledApps = await Apps.installedApps({ enabled: true });
+
+		if (!enabledApps) return;
+
+		Promise.all(enabledApps.map(async (app) => {
+			if (app.getInfo().addon !== module) return;
+
+			await Apps.getManager()?.disable(app.getID(), AppStatus.DISABLED, false);
+		}));
+	});
+});
--- ee/packages/license/src/events/listeners.ts
+++ ee/packages/license/src/events/listeners.ts
@@ -83,7 +83,7 @@ export function onToggledFeature(
 	};
 }
 
-export function onModule(this: LicenseManager, cb: (data: { module: string; valid: boolean }) => void) {
+export function onModule(this: LicenseManager, cb: (data: { module: string; external: boolean; valid: boolean }) => void) {
 	this.on('module', cb);
 }
 
