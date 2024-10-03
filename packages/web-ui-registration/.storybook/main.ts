674a8e0f266a983a70ef7f57e6ea5c47d65895a2
Tasso <tasso.evangelista@rocket.chat>
2024-10-03 04:20:36 -0300

    Fix storybooks

--- packages/web-ui-registration/.storybook/main.ts
+++ packages/web-ui-registration/.storybook/main.ts
@@ -2,7 +2,16 @@ import { type StorybookConfig } from '@storybook/core-common';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-essentials', 'storybook-dark-mode'],
+	addons: [
+		'@storybook/addon-essentials',
+		'storybook-dark-mode',
+		{
+			name: '@newhighsco/storybook-addon-transpile-modules',
+			options: {
+				transpileModules: ['date-fns', 'typia', 'react-i18next'],
+			},
+		},
+	],
 	features: {
 		postcss: false,
 	},
