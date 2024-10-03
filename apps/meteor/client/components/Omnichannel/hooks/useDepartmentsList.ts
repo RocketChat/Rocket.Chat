9512b60daa08401b8e438113d2bff4a7ec894d09
rique223 <henrique.jobs1@gmail.com>
2024-10-03 13:02:34 -0300

    feat: :sparkles: Introduce fallback for when department is deleted while another user has it selected
    Added a fallback to the current chats departments filter in which when a department is deleted and another user has the department selected in the filter, if the page is reloaded the user will be warned about the deletion and sent back to the default option of the filter.

--- apps/meteor/client/components/Omnichannel/hooks/useDepartmentsList.ts
+++ apps/meteor/client/components/Omnichannel/hooks/useDepartmentsList.ts
@@ -16,7 +16,7 @@ type DepartmentsListOptions = {
 	excludeDepartmentId?: string;
 	enabled?: boolean;
 	showArchived?: boolean;
-	selectedDepartment?: string;
+	selectedDepartment?: DepartmentListItem | undefined;
 };
 
 export const useDepartmentsList = (
@@ -32,7 +32,6 @@ export const useDepartmentsList = (
 	const reload = useCallback(() => setItemsList(new RecordList<DepartmentListItem>()), []);
 
 	const getDepartments = useEndpoint('GET', '/v1/livechat/department');
-	const getDepartment = useEndpoint('GET', '/v1/livechat/department/:_id', { _id: options.selectedDepartment ?? '' });
 
 	useComponentDidUpdate(() => {
 		options && reload();
@@ -66,21 +65,21 @@ export const useDepartmentsList = (
 					}),
 				);
 
-			const normalizedItems = await normalizeDepartments(items, options.selectedDepartment ?? '', getDepartment);
-
 			options.haveAll &&
-				normalizedItems.unshift({
+				items.unshift({
 					_id: '',
 					label: t('All'),
 					value: 'all',
 				});
 
 			options.haveNone &&
-				normalizedItems.unshift({
+				items.unshift({
 					_id: '',
 					label: t('None'),
 					value: '',
 				});
+
+			const normalizedItems = await normalizeDepartments(items, options.selectedDepartment);
 
 			return {
 				items: normalizedItems,
@@ -98,12 +97,11 @@ export const useDepartmentsList = (
 			options.haveAll,
 			options.haveNone,
 			options.departmentId,
-			getDepartment,
 			t,
 		],
 	);
 
-	const { loadMoreItems, initialItemCount } = useScrollableRecordList(itemsList, fetchData, 25);
+	const { loadMoreItems, initialItemCount } = useScrollableRecordList(itemsList, fetchData, 5);
 
 	return {
 		reload,
