62f467a0f1df23f745de357eadc3debbabf5c28b
Pierre <pierre.lehnen@rocket.chat>
2024-10-04 08:13:26 -0300

    fixed type

--- packages/core-typings/src/import/IImporterSelection.ts
+++ packages/core-typings/src/import/IImporterSelection.ts
@@ -2,7 +2,7 @@ import type { IImporterSelectionChannel } from './IImporterSelectionChannel';
 import type { IImporterSelectionContactOrIdentifier } from './IImporterSelectionContact';
 import type { IImporterSelectionUser } from './IImporterSelectionUser';
 
-export interface IImporterSelection<WithData extends boolean> {
+export interface IImporterSelection<WithData extends boolean = true> {
 	name: string;
 	users: IImporterSelectionUser[];
 	channels: IImporterSelectionChannel[];
