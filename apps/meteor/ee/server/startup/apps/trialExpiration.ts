8134fcee2521a0660d3442e51aa8f2b602789e2a
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2024-10-04 09:17:57 -0300

    chore: tag apps as migrated when downgrading to CE (#33439)

--- apps/meteor/ee/server/startup/apps/trialExpiration.ts
+++ apps/meteor/ee/server/startup/apps/trialExpiration.ts
@@ -1,9 +1,12 @@
 import { License } from '@rocket.chat/license';
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
+Meteor.startup(async () => {
+	const { Apps } = await import('../../apps');
 	License.onInvalidateLicense(async () => {
-		const { Apps } = await import('../../apps');
-		void Apps.disableApps();
+		void Apps.migratePrivateApps();
+	});
+	License.onRemoveLicense(async () => {
+		void Apps.migratePrivateApps();
 	});
 });
