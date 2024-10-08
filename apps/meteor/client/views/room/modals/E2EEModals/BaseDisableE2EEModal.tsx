e1280359a22e78b13fb5f16f8e1974bd3fdc292b
Hugo Costa <hugocarreiracosta@gmail.com>
2024-10-08 16:51:44 -0300

    feat: E2EE room action and Base Modal

---
+++ apps/meteor/client/views/room/modals/E2EEModals/BaseDisableE2EEModal.tsx
@@ -0,0 +1,49 @@
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import type { ReactElement } from 'react';
+import React, { useState } from 'react';
+
+import DisableE2EEModal from './DisableE2EEModal';
+import ResetKeysE2EEModal from './ResetKeysE2EEModal';
+
+const STEPS = {
+	DISABLE_E2EE: 'DISABLE_E2EE',
+	RESET_ROOM_KEY: 'RESET_ROOM_KEY',
+};
+
+type BaseDisableE2EEModalProps = {
+	onConfirm: () => void;
+	onClose: () => void;
+	roomType: string;
+	roomId: string;
+	canResetRoomKey: boolean;
+};
+
+const BaseDisableE2EEModal = ({
+	onConfirm,
+	onClose,
+	roomType,
+	roomId,
+	canResetRoomKey,
+}: BaseDisableE2EEModalProps): ReactElement | null => {
+	const [step, setStep] = useState(STEPS.DISABLE_E2EE);
+
+	const onResetRoomKey = useEffectEvent(() => {
+		setStep(STEPS.RESET_ROOM_KEY);
+	});
+
+	if (step === STEPS.RESET_ROOM_KEY && canResetRoomKey) {
+		return <ResetKeysE2EEModal roomType={roomType} roomId={roomId} onCancel={onClose} />;
+	}
+
+	return (
+		<DisableE2EEModal
+			onConfirm={onConfirm}
+			onCancel={onClose}
+			roomType={roomType}
+			canResetRoomKey={canResetRoomKey}
+			onResetRoomKey={onResetRoomKey}
+		/>
+	);
+};
+
+export default BaseDisableE2EEModal;
