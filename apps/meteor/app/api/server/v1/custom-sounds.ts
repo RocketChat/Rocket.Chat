375858179d04172da039bb4aa0619817a1bb9a1c
Marcos Defendi <marcos.defendi@rocket.chat>
2024-10-08 14:44:48 -0300

    refactor: make custom sounds page stop to use query param

--- apps/meteor/app/api/server/v1/custom-sounds.ts
+++ apps/meteor/app/api/server/v1/custom-sounds.ts
@@ -1,16 +1,20 @@
 import { CustomSounds } from '@rocket.chat/models';
+import { isCustomSoundsListProps } from '@rocket.chat/rest-typings';
+import { escapeRegExp } from '@rocket.chat/string-helpers';
 
 import { API } from '../api';
 import { getPaginationItems } from '../helpers/getPaginationItems';
 
 API.v1.addRoute(
 	'custom-sounds.list',
-	{ authRequired: true },
+	{ authRequired: true, validateParams: isCustomSoundsListProps },
 	{
 		async get() {
 			const { offset, count } = await getPaginationItems(this.queryParams);
-			const { sort, query } = await this.parseJsonQuery();
-			const { cursor, totalCount } = CustomSounds.findPaginated(query, {
+			const { sort } = await this.parseJsonQuery();
+			const { name } = this.queryParams;
+			const filter = name ? { name: { $regex: escapeRegExp(name), $options: 'i' } } : {};
+			const { cursor, totalCount } = CustomSounds.findPaginated(filter, {
 				sort: sort || { name: 1 },
 				skip: offset,
 				limit: count,
