f11f67d98fb6bd24d3960b51a124cce88d682813
gustrb <gustavoreisbauer@gmail.com>
2024-10-08 14:56:21 -0300

    feat: implement bridge in R.C

---
+++ apps/meteor/app/apps/server/bridges/contact.ts
@@ -0,0 +1,20 @@
+import type { IAppServerOrchestrator } from '@rocket.chat/apps';
+import { ContactBridge } from '@rocket.chat/apps-engine/server/bridges';
+import type { ILivechatContact } from '@rocket.chat/core-typings';
+import { LivechatContacts } from '@rocket.chat/models';
+
+export class AppContactBridge extends ContactBridge {
+	constructor(private readonly orch: IAppServerOrchestrator) {
+		super();
+	}
+
+	async getById(id: ILivechatContact['_id'], appId: string): Promise<ILivechatContact> {
+		this.orch.debugLog(`The app ${appId} is fetching a contact`);
+		return await LivechatContacts.findOne({ _id: id }) as ILivechatContact;
+	}
+
+	verifyContact(verifyContactChannelParams: VerifyContactChannelParams, appId: string): Promise<void> {
+		this.orch.debugLog(`The app ${appId} is fetching a contact`);
+		throw new Error('TODO');
+	}
+}
