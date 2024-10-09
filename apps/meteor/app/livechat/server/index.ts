e8ac72c6942d0ab06920d660cde62768f7c24cd3
Abhinav Kumar <abhinav@avitechlab.com>
2024-10-10 01:34:09 +0530

    chore!: remove deprecated method livechat:saveAppearance (#33448)

--- apps/meteor/app/livechat/server/index.ts
+++ apps/meteor/app/livechat/server/index.ts
@@ -34,7 +34,6 @@ import './methods/removeManager';
 import './methods/removeTrigger';
 import './methods/removeRoom';
 import './methods/saveAgentInfo';
-import './methods/saveAppearance';
 import './methods/saveCustomField';
 import './methods/saveDepartment';
 import './methods/saveDepartmentAgents';
