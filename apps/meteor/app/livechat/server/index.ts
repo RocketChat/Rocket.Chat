fb75920d54877936371a33fdeb50cf3855deeaa1
Abhinav Kumar <abhinav@avitechlab.com>
2024-10-10 01:28:57 +0530

    chore!: remove deprecated method livechat:sendOfflineMessage (#33452)

--- apps/meteor/app/livechat/server/index.ts
+++ apps/meteor/app/livechat/server/index.ts
@@ -43,7 +43,6 @@ import './methods/saveIntegration';
 import './methods/saveTrigger';
 import './methods/sendMessageLivechat';
 import './methods/sendFileLivechatMessage';
-import './methods/sendOfflineMessage';
 import './methods/setCustomField';
 import './methods/setDepartmentForVisitor';
 import './methods/transfer';
