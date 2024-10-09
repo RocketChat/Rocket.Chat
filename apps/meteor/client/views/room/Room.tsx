eef15b4b2e14922ff0fe9832a3392b3d1f774e82
juliajforesti <juliajforesti@gmail.com>
2024-10-09 14:50:11 -0300

    chore: adjust imports

--- apps/meteor/client/views/room/Room.tsx
+++ apps/meteor/client/views/room/Room.tsx
@@ -1,4 +1,3 @@
-import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
 import { useTranslation, useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import React, { createElement, lazy, memo, Suspense } from 'react';
@@ -8,10 +7,8 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { ContextualbarSkeleton } from '../../components/Contextualbar';
 import RoomE2EESetup from './E2EESetup/RoomE2EESetup';
 import Header from './Header';
-import { HeaderV2 } from './HeaderV2';
 import MessageHighlightProvider from './MessageList/providers/MessageHighlightProvider';
-import RoomBody from './body/RoomBody';
-import RoomBodyV2 from './body/RoomBodyV2';
+import RoomBodyV2 from './body/RoomBody';
 import { useRoom } from './contexts/RoomContext';
 import { useRoomToolbox } from './contexts/RoomToolboxContext';
 import { useAppsContextualBar } from './hooks/useAppsContextualBar';
@@ -43,34 +40,8 @@ const Room = (): ReactElement => {
 									? t('Conversation_with__roomName__', { roomName: room.name })
 									: t('Channel__roomName__', { roomName: room.name })
 							}
-							header={
-								<>
-									<FeaturePreview feature='newNavigation'>
-										<FeaturePreviewOn>
-											<HeaderV2 room={room} />
-										</FeaturePreviewOn>
-										<FeaturePreviewOff>
-											<Header room={room} />
-										</FeaturePreviewOff>
-									</FeaturePreview>
-								</>
-							}
-							body={
-								shouldDisplayE2EESetup ? (
-									<RoomE2EESetup />
-								) : (
-									<>
-										<FeaturePreview feature='newNavigation'>
-											<FeaturePreviewOn>
-												<RoomBodyV2 />
-											</FeaturePreviewOn>
-											<FeaturePreviewOff>
-												<RoomBody />
-											</FeaturePreviewOff>
-										</FeaturePreview>
-									</>
-								)
-							}
+							header={<Header room={room} />}
+							body={shouldDisplayE2EESetup ? <RoomE2EESetup /> : <RoomBodyV2 />}
 							aside={
 								(toolbox.tab?.tabComponent && (
 									<ErrorBoundary fallback={null}>
