3aa1b413292d7adb80022897eb3eb3edd5b36283
Kevin Aleman <kaleman960@gmail.com>
2024-10-10 17:12:18 -0600

    Discard changes to apps/meteor/client/ui.ts

--- apps/meteor/client/ui.ts
+++ apps/meteor/client/ui.ts
@@ -12,7 +12,6 @@ import { useContactChatHistoryRoomAction } from './hooks/roomActions/useContactC
 import { useContactProfileRoomAction } from './hooks/roomActions/useContactProfileRoomAction';
 import { useDiscussionsRoomAction } from './hooks/roomActions/useDiscussionsRoomAction';
 import { useE2EERoomAction } from './hooks/roomActions/useE2EERoomAction';
-import { useE2EEResetRoomKeyRoomAction } from './hooks/roomActions/useE2EERoomKeyResetAction';
 import { useExportMessagesRoomAction } from './hooks/roomActions/useExportMessagesRoomAction';
 import { useGameCenterRoomAction } from './hooks/roomActions/useGameCenterRoomAction';
 import { useKeyboardShortcutListRoomAction } from './hooks/roomActions/useKeyboardShortcutListRoomAction';
@@ -52,7 +51,6 @@ export const roomActionHooks = [
 	useContactProfileRoomAction,
 	useDiscussionsRoomAction,
 	useE2EERoomAction,
-	useE2EEResetRoomKeyRoomAction,
 	useExportMessagesRoomAction,
 	useGameCenterRoomAction,
 	useKeyboardShortcutListRoomAction,
@@ -81,9 +79,4 @@ export const quickActionHooks = [
 	useOnHoldChatQuickAction,
 ] satisfies (() => QuickActionsActionConfig | undefined)[];
 
-export const roomActionHooksForE2EESetup = [
-	useChannelSettingsRoomAction,
-	useMembersListRoomAction,
-	useE2EERoomAction,
-	useE2EEResetRoomKeyRoomAction,
-];
+export const roomActionHooksForE2EESetup = [useChannelSettingsRoomAction, useMembersListRoomAction, useE2EERoomAction];
