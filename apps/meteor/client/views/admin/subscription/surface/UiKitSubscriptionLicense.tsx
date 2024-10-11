6cb10a5a42b2256a0e1bd15224f6b95c28c61b63
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-10-11 15:42:12 -0300

    feat: create `UiKitSubscriptionLicenseSurface`

--- apps/meteor/client/views/admin/subscription/surface/UiKitSubscriptionLicense.tsx
+++ apps/meteor/client/views/admin/subscription/surface/UiKitSubscriptionLicense.tsx
@@ -1,9 +1,11 @@
+import { useDebouncedCallback } from '@rocket.chat/fuselage-hooks';
 import { UiKitContext, bannerParser, UiKitComponent } from '@rocket.chat/fuselage-ui-kit';
-import type { ReactElement } from 'react';
-import React from 'react';
+import type { View } from '@rocket.chat/ui-kit';
+import type { ContextType, Dispatch, ReactElement } from 'react';
+import React, { useMemo } from 'react';
 
 import MarkdownText from '../../../../components/MarkdownText';
-import { useBannerContextValue } from '../../../../uikit/hooks/useBannerContextValue';
+import { useUiKitActionManager } from '../../../../uikit/hooks/useUiKitActionManager';
 import { useUiKitView } from '../../../../uikit/hooks/useUiKitView';
 import type { SubscriptionLicenseLayout } from './UiKitSubscriptionLicenseSurface';
 import { UiKitSubscriptionLicenseSurface } from './UiKitSubscriptionLicenseSurface';
@@ -20,9 +22,75 @@ type UiKitSubscriptionLicenseProps = {
 	};
 };
 
+type UseSubscriptionLicenseContextValueParams = {
+	view: View & {
+		viewId: string;
+	};
+	values: {
+		[actionId: string]: {
+			value: unknown;
+			blockId?: string | undefined;
+		};
+	};
+	updateValues: Dispatch<{
+		actionId: string;
+		payload: {
+			value: unknown;
+			blockId?: string | undefined;
+		};
+	}>;
+};
+type UseSubscriptionLicenseContextValueReturn = ContextType<typeof UiKitContext>;
+
+const useSubscriptionLicenseContextValue = ({
+	view,
+	values,
+	updateValues,
+}: UseSubscriptionLicenseContextValueParams): UseSubscriptionLicenseContextValueReturn => {
+	const actionManager = useUiKitActionManager();
+
+	const emitInteraction = useMemo(() => actionManager.emitInteraction.bind(actionManager), [actionManager]);
+	const debouncedEmitInteraction = useDebouncedCallback(emitInteraction, 700);
+
+	return {
+		action: async ({ appId, viewId, actionId, dispatchActionConfig, blockId, value }): Promise<void> => {
+			if (!appId || !viewId) {
+				return;
+			}
+
+			const emit = dispatchActionConfig?.includes('on_character_entered') ? debouncedEmitInteraction : emitInteraction;
+
+			await emit(appId, {
+				type: 'blockAction',
+				actionId,
+				container: {
+					type: 'view',
+					id: viewId,
+				},
+				payload: {
+					blockId,
+					value,
+				},
+			});
+		},
+		updateState: ({ actionId, value, blockId = 'default' }) => {
+			updateValues({
+				actionId,
+				payload: {
+					blockId,
+					value,
+				},
+			});
+		},
+		...view,
+		values,
+		viewId: view.viewId,
+	};
+};
+
 const UiKitSubscriptionLicense = ({ initialView }: UiKitSubscriptionLicenseProps) => {
-	const { view, values } = useUiKitView(initialView);
-	const contextValue = useBannerContextValue({ view, values });
+	const { view, values, updateValues } = useUiKitView(initialView);
+	const contextValue = useSubscriptionLicenseContextValue({ view, values, updateValues });
 
 	return (
 		<UiKitContext.Provider value={contextValue}>
