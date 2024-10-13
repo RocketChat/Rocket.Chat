97dff07c608d174fc11fb6ec0aa64f9c9a6e6fab
Ricardo Garim <rswarovsky@gmail.com>
2024-10-13 12:12:27 -0300

    chore: remove integrations query and fields params

--- apps/meteor/app/api/server/helpers/parseJsonQuery.ts
+++ apps/meteor/app/api/server/helpers/parseJsonQuery.ts
@@ -54,7 +54,7 @@ export async function parseJsonQuery(api: PartialThis): Promise<{
 	}
 
 	// TODO: Remove this once we have all routes migrated to the new API params
-	const hasSupportedRoutes = ([] as string[]).includes(route);
+	const hasSupportedRoutes = ['/api/v1/integrations.list'].includes(route);
 	const isUnsafeQueryParamsAllowed = process.env.ALLOW_UNSAFE_QUERY_AND_FIELDS_API_PARAMS?.toUpperCase() === 'TRUE';
 	const messageGenerator = ({ endpoint, version, parameter }: { endpoint: string; version: string; parameter: string }): string =>
 		`The usage of the "${parameter}" parameter in endpoint "${endpoint}" breaks the security of the API and can lead to data exposure. It has been deprecated and will be removed in the version ${version}.`;
