67e084d0f9b2dc1faee7f81943ed841d0d88032b
gustrb <gustavoreisbauer@gmail.com>
2024-10-15 18:05:01 -0300

    chore: improve docs

--- apps/meteor/app/livechat/server/lib/QueueManager.ts
+++ apps/meteor/app/livechat/server/lib/QueueManager.ts
@@ -258,9 +258,12 @@ export class QueueManager {
 		}
 
 		let newRoom;
+		// Note: if the contact is not verified, we cannot add it to the queue, since no agent should be able to pick the conversation
+		//       up. So if the contact is not verified we don't add it to queue and wait an event to be sent to properly add it to queue
 		if (await Omnichannel.isUnverifiedContact(room)) {
 			newRoom = await LivechatRooms.findOneById(rid);
 			await unverifyContactChannel(inquiry.v.contactId, room.source.type, inquiry.v._id);
+			// void Apps.self?.triggerEvent(AppEvents.IPostContactVerificationAppAssigned, inquiry.v, room);
 		} else {
 			newRoom = (await this.queueInquiry(inquiry, room, defaultAgent)) ?? (await LivechatRooms.findOneById(rid));
 		}
