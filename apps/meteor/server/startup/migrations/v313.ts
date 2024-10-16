471b5073e3b8c681ee2d7305ac3ad465de5b57bc
Hugo Costa <hugocarreiracosta@gmail.com>
2024-10-15 21:22:36 -0300

    chore!: add migration to remove settings

---
+++ apps/meteor/server/startup/migrations/v313.ts
@@ -0,0 +1,18 @@
+import { Settings } from '@rocket.chat/models';
+
+import { addMigration } from '../../lib/migrations';
+
+// Removes deprecated Show Message In Main Thread preference
+addMigration({
+	version: 313,
+	async up() {
+		const customOauthServicesButtonColors = await Settings.find(
+			{ _id: /Accounts_OAuth_.+button.+color$/ },
+			{ projection: { _id: 1 } },
+		).toArray();
+
+		for await (const setting of customOauthServicesButtonColors) {
+			await Settings.removeById(setting._id);
+		}
+	},
+});
