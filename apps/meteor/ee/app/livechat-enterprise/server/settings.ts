0d26dbaed5c46c3cd8dfe626e0ba1656892dcaac
gustrb <gustavoreisbauer@gmail.com>
2024-10-16 16:29:56 -0300

    chore: remove right settings

--- apps/meteor/ee/app/livechat-enterprise/server/settings.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/settings.ts
@@ -176,37 +176,47 @@ export const createSettings = async (): Promise<void> => {
 				enterprise: true,
 				modules: ['livechat-enterprise'],
 			});
-			await this.add('Livechat_Block_Unverified_Contacts', false, {
-				type: 'boolean',
-				public: true,
-				hidden: true,
-				enableQuery: omnichannelEnabledQuery,
-				invalidValue: false,
-				section: 'Contact_identification',
-				enterprise: true,
-				modules: ['livechat-enterprise'],
-			});
-			await this.add('Livechat_Contact_Verification_App', '', {
-				type: 'select',
-				public: true,
-				hidden: true,
-				values: [{ key: 'verifychat', i18nLabel: 'VerifyChat' }],
-				enableQuery: omnichannelEnabledQuery,
-				invalidValue: '',
-				section: 'Contact_identification',
-				enterprise: true,
-				modules: ['livechat-enterprise'],
-			});
-			await this.add('Livechat_Request_Verification_On_First_Contact_Only', false, {
-				type: 'boolean',
-				public: true,
-				hidden: true,
-				enableQuery: omnichannelEnabledQuery,
-				invalidValue: false,
-				section: 'Contact_identification',
-				enterprise: true,
-				modules: ['livechat-enterprise'],
-			});
+		});
+
+		await this.add('Livechat_Block_Unknown_Contacts', false, {
+			type: 'boolean',
+			public: true,
+			enableQuery: omnichannelEnabledQuery,
+			invalidValue: false,
+			section: 'Contact_identification',
+			enterprise: true,
+			modules: ['livechat-enterprise'],
+		});
+
+		await this.add('Livechat_Block_Unverified_Contacts', false, {
+			type: 'boolean',
+			public: true,
+			enableQuery: omnichannelEnabledQuery,
+			invalidValue: false,
+			section: 'Contact_identification',
+			enterprise: true,
+			modules: ['livechat-enterprise'],
+		});
+
+		await this.add('Livechat_Contact_Verification_App', '', {
+			type: 'select',
+			public: true,
+			values: [{ key: 'VerifyChat', i18nLabel: 'VerifyChat' }],
+			enableQuery: omnichannelEnabledQuery,
+			invalidValue: '',
+			section: 'Contact_identification',
+			enterprise: true,
+			modules: ['livechat-enterprise'],
+		});
+
+		await this.add('Livechat_Request_Verification_On_First_Contact_Only', false, {
+			type: 'boolean',
+			public: true,
+			enableQuery: omnichannelEnabledQuery,
+			invalidValue: false,
+			section: 'Contact_identification',
+			enterprise: true,
+			modules: ['livechat-enterprise'],
 		});
 	});
 
