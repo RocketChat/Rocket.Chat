7da9ec6ee41c847a06349ec2dbea5918d568cc4d
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2024-10-16 21:27:44 -0300

    chore!: make custom sounds page stop to use query param (#33499)

--- apps/meteor/app/api/server/v1/custom-sounds.ts
+++ apps/meteor/app/api/server/v1/custom-sounds.ts
@@ -1,16 +1,26 @@
 import { CustomSounds } from '@rocket.chat/models';
+import { isCustomSoundsListProps } from '@rocket.chat/rest-typings';
+import { escapeRegExp } from '@rocket.chat/string-helpers';
 
 import { API } from '../api';
 import { getPaginationItems } from '../helpers/getPaginationItems';
 
 API.v1.addRoute(
 	'custom-sounds.list',
-	{ authRequired: true },
+	{ authRequired: true, validateParams: isCustomSoundsListProps },
 	{
 		async get() {
-			const { offset, count } = await getPaginationItems(this.queryParams);
+			const { offset, count } = await getPaginationItems(this.queryParams as Record<string, string | number | null | undefined>);
 			const { sort, query } = await this.parseJsonQuery();
-			const { cursor, totalCount } = CustomSounds.findPaginated(query, {
+
+			const { name } = this.queryParams;
+
+			const filter = {
+				...query,
+				...(name ? { name: { $regex: escapeRegExp(name as string), $options: 'i' } } : {}),
+			};
+
+			const { cursor, totalCount } = CustomSounds.findPaginated(filter, {
 				sort: sort || { name: 1 },
 				skip: offset,
 				limit: count,
