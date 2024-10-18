8e21c10b522aea1945d63745bf4c9165fea8739a
Debdut Chakraborty <debdutdeb@outlook.com>
2024-10-18 13:13:11 +0530

    better logs

--- apps/meteor/tests/data/permissions.helper.ts
+++ apps/meteor/tests/data/permissions.helper.ts
@@ -38,7 +38,10 @@ export const updateSetting = (setting: string, value: ISetting['value']): Promis
 			.send({ value })
 			.expect('Content-Type', 'application/json')
 			.expect(200)
-			.end((err?: Error) => setTimeout(() => (!err && resolve()) || reject(err), 100));
+			.end((err?: Error) => setTimeout(() => {
+				console.log(`done ${setting}`);
+				(!err && resolve()) || reject(err);
+			}, 100));
 	});
 
 export const getSettingValueById = async (setting: string): Promise<ISetting['value']> => {
