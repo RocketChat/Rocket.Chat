f91e051868b826bb8eb7b61ece6845dab474bec8
Pierre <pierre.lehnen@rocket.chat>
2024-10-18 02:15:14 -0300

    chore: enable contact importer only after enabling the SCI feature

--- apps/meteor/app/importer-omnichannel-contacts/server/index.ts
+++ apps/meteor/app/importer-omnichannel-contacts/server/index.ts
@@ -1,8 +1,12 @@
+import { License } from '@rocket.chat/license';
+
 import { Importers } from '../../importer/server';
 import { ContactImporter } from './ContactImporter';
 
-Importers.add({
-	key: 'omnichannel_contact',
-	name: 'omnichannel_contacts_importer',
-	importer: ContactImporter,
+License.onValidFeature('contact-id-verification', () => {
+	Importers.add({
+		key: 'omnichannel_contact',
+		name: 'omnichannel_contacts_importer',
+		importer: ContactImporter,
+	});
 });
