4c5b36562fe3ef10c115b943ccbca033ef51fa69
Debdut Chakraborty <debdutdeb@outlook.com>
2024-10-18 16:27:40 +0530

    ...

--- apps/meteor/tests/end-to-end/api/federation.ts
+++ apps/meteor/tests/end-to-end/api/federation.ts
@@ -7,70 +7,68 @@ import { updateSetting } from '../../data/permissions.helper';
 describe('federation', () => {
 	before((done) => getCredentials(done));
 
-	describe('well-known', () => {
-		describe('when matrix disabled', () => {
-			before(async () => {
-				await updateSetting('Federation_Matrix_enabled', false);
-				await updateSetting('Federation_Matrix_serve_well_known', true);
-			});
-
-			after(async () => {
-				await updateSetting('Federation_Matrix_serve_well_known', false);
-			});
-
-			it('should return 404 not found', async () => {
-				await request.get('/.well-known/matrix/server').expect(404);
-
-				await request.get('/.well-known/matrix/client').expect(404);
-			});
+	describe('well-known when matrix disabled', () => {
+		before(async () => {
+			await updateSetting('Federation_Matrix_enabled', false);
+			await updateSetting('Federation_Matrix_serve_well_known', true);
 		});
 
-		describe('when matrix enabled but well-known disabled', () => {
-			before(async () => {
-				await updateSetting('Federation_Matrix_enabled', true);
-				await updateSetting('Federation_Matrix_serve_well_known', false);
-			});
-
-			after(async () => {
-				await updateSetting('Federation_Matrix_enabled', false);
-			});
-
-			it('should return 404 not found', async () => {
-				console.log('should start the actual test now');
-				await request.get('/.well-known/matrix/server').expect(404);
-
-				await request.get('/.well-known/matrix/client').expect(404);
-			});
+		after(async () => {
+			await updateSetting('Federation_Matrix_serve_well_known', false);
 		});
 
-		describe('when enabled', () => {
-			before(async () => {
-				await updateSetting('Federation_Matrix_enabled', true);
-				await updateSetting('Federation_Matrix_serve_well_known', true);
-			});
+		it('should return 404 not found', async () => {
+			await request.get('/.well-known/matrix/server').expect(404);
 
-			after(async () => {
-				await updateSetting('Federation_Matrix_enabled', false);
-				await updateSetting('Federation_Matrix_serve_well_known', false);
-			});
+			await request.get('/.well-known/matrix/client').expect(404);
+		});
+	});
 
-			it('should return matrix information', async () => {
-				await request
-					.get('/.well-known/matrix/server')
-					.expect('Content-Type', 'application/json')
-					.expect(200)
-					.expect((res) => {
-						expect(res.body).to.have.property('m.server', 'localhost:8008');
-					});
+	describe('well-known when matrix enabled but well-known disabled', () => {
+		before(async () => {
+			await updateSetting('Federation_Matrix_enabled', true);
+			await updateSetting('Federation_Matrix_serve_well_known', false);
+		});
 
-				await request
-					.get('/.well-known/matrix/client')
-					.expect('Content-Type', 'application/json')
-					.expect(200)
-					.expect((res) => {
-						expect(res.body['m.homeserver']).to.have.property('base_url', 'http://localhost');
-					});
-			});
+		after(async () => {
+			await updateSetting('Federation_Matrix_enabled', false);
+		});
+
+		it('should return 404 not found', async () => {
+			console.log('should start the actual test now');
+			await request.get('/.well-known/matrix/server').expect(404);
+
+			await request.get('/.well-known/matrix/client').expect(404);
+		});
+	});
+
+	describe('well-known when everything is enabled', () => {
+		before(async () => {
+			await updateSetting('Federation_Matrix_enabled', true);
+			await updateSetting('Federation_Matrix_serve_well_known', true);
+		});
+
+		after(async () => {
+			await updateSetting('Federation_Matrix_enabled', false);
+			await updateSetting('Federation_Matrix_serve_well_known', false);
+		});
+
+		it('should return matrix information', async () => {
+			await request
+				.get('/.well-known/matrix/server')
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).to.have.property('m.server', 'localhost:8008');
+				});
+
+			await request
+				.get('/.well-known/matrix/client')
+				.expect('Content-Type', 'application/json')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body['m.homeserver']).to.have.property('base_url', 'http://localhost');
+				});
 		});
 	});
 });
