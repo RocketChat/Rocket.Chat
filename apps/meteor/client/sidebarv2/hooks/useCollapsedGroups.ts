302c10f262907e7891a675861e594b6139d1d417
juliajforesti <juliajforesti@gmail.com>
2024-10-21 18:08:46 -0300

    regression: add keys to `handleKeyDown` on groups collapser

--- apps/meteor/client/sidebarv2/hooks/useCollapsedGroups.ts
+++ apps/meteor/client/sidebarv2/hooks/useCollapsedGroups.ts
@@ -1,10 +1,11 @@
 import { useLocalStorage } from '@rocket.chat/fuselage-hooks';
+import type { KeyboardEvent } from 'react';
 import { useCallback } from 'react';
 
 export const useCollapsedGroups = () => {
 	const [collapsedGroups, setCollapsedGroups] = useLocalStorage<string[]>('sidebarGroups', []);
 
-	const handleCollapsedGroups = useCallback(
+	const handleClick = useCallback(
 		(group: string) => {
 			if (collapsedGroups.includes(group)) {
 				setCollapsedGroups(collapsedGroups.filter((item) => item !== group));
@@ -15,5 +16,11 @@ export const useCollapsedGroups = () => {
 		[collapsedGroups, setCollapsedGroups],
 	);
 
-	return { collapsedGroups, handleCollapsedGroups };
+	const handleKeyDown = (event: KeyboardEvent, group: string) => {
+		if (['Enter', 'Space'].includes(event.code)) {
+			event.preventDefault();
+			handleClick(group);
+		}
+	};
+	return { collapsedGroups, handleClick, handleKeyDown };
 };
