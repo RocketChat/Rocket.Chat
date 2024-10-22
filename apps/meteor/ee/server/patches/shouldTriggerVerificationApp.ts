22eb638cd9ac6399da07b42294baadf794f82a69
gustrb <gustavoreisbauer@gmail.com>
2024-10-22 09:41:49 -0300

    fix: getting out early if setting is never

--- apps/meteor/ee/server/patches/shouldTriggerVerificationApp.ts
+++ apps/meteor/ee/server/patches/shouldTriggerVerificationApp.ts
@@ -5,11 +5,24 @@ import { LivechatContacts } from '@rocket.chat/models';
 import { shouldTriggerVerificationApp } from '../../../app/livechat/server/lib/Contacts';
 import { settings } from '../../../app/settings/server';
 
+/**
+ * Theese are the possible values the Livechat_Require_Contact_Verification might assume, they represent:
+ * - never: the contact verification app should never be triggered, therefore, the conversation will be always put onto the queue
+ * - once: if the contact was already verified in this channel, there is no need to assign it to the bot again
+ * - always: assign it to the bot every time
+ */
+type AvailableLivechatRequireContactVerificationSetting = 'never' | 'once' | 'always';
+
 const runShouldTriggerVerificationApp = async (
 	_next: any,
 	contactId: ILivechatContact['_id'],
 	source: IOmnichannelSource,
 ): Promise<boolean> => {
+	const verificationRequirement = settings.get<AvailableLivechatRequireContactVerificationSetting>('Livechat_Require_Contact_Verification');
+	if (verificationRequirement === 'never') {
+		return false;
+	}
+
 	const contact = await LivechatContacts.findOneById<Pick<ILivechatContact, '_id' | 'unknown' | 'channels'>>(contactId, {
 		projection: {
 			_id: 1,
@@ -28,7 +41,6 @@ const runShouldTriggerVerificationApp = async (
 	}
 
 	const isContactVerified = (contact.channels?.filter((channel) => channel.verified && channel.name === source.type) || []).length > 0;
-	const verificationRequirement = settings.get<string>('Livechat_Require_Contact_Verification');
 
 	// If the contact has never been verified and it needs to be verified at least once, trigger the app
 	if (!isContactVerified && verificationRequirement === 'once') {
