5c577176f6b6b43dc7846a86c6a56e1b7db81708
dougfabris <devfabris@gmail.com>
2024-10-28 13:08:19 -0300

    chore: query not being debounced in contact info history

--- apps/meteor/client/views/omnichannel/contactInfo/tabs/ContactInfoHistory/ContactInfoHistoryMessages.tsx
+++ apps/meteor/client/views/omnichannel/contactInfo/tabs/ContactInfoHistory/ContactInfoHistoryMessages.tsx
@@ -13,7 +13,7 @@ import {
 	TextInput,
 	Throbber,
 } from '@rocket.chat/fuselage';
-import { useResizeObserver } from '@rocket.chat/fuselage-hooks';
+import { useDebouncedValue, useResizeObserver } from '@rocket.chat/fuselage-hooks';
 import { useSetting, useTranslation, useUserPreference, useUserId } from '@rocket.chat/ui-contexts';
 import type { ChangeEvent, ReactElement } from 'react';
 import React, { useMemo, useState } from 'react';
@@ -44,10 +44,12 @@ const ContactInfoHistoryMessages = ({ chatId, onBack, onOpenRoom }: ContactHisto
 		debounceDelay: 200,
 	});
 
-	const { itemsList: messageList, loadMoreItems } = useHistoryMessageList(
+	const query = useDebouncedValue(
 		useMemo(() => ({ roomId: chatId, filter: text }), [chatId, text]),
-		userId,
+		500,
 	);
+
+	const { itemsList: messageList, loadMoreItems } = useHistoryMessageList(query, userId);
 
 	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
 		setText(event.currentTarget.value);
