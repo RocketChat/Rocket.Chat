e5f5264481a68c6cddd2884ecde1fa82e48176af
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-10-30 10:02:20 -0300

    close omnichannel room in each test

--- apps/meteor/tests/end-to-end/api/livechat/23-mac.ts
+++ apps/meteor/tests/end-to-end/api/livechat/23-mac.ts
@@ -1,4 +1,4 @@
-import type { ILivechatVisitor, IOmnichannelRoom } from '@rocket.chat/core-typings';
+import type { ILivechatVisitor } from '@rocket.chat/core-typings';
 import { expect } from 'chai';
 import { before, afterEach, after, describe, it } from 'mocha';
 import moment from 'moment';
@@ -15,7 +15,6 @@ import {
 	closeOmnichannelRoom,
 	deleteVisitor,
 } from '../../../data/livechat/rooms';
-import { updatePermission } from '../../../data/permissions.helper';
 
 describe('MAC', () => {
 	before((done) => getCredentials(done));
@@ -28,17 +27,14 @@ describe('MAC', () => {
 	describe('MAC rooms', () => {
 		let visitor: ILivechatVisitor;
 		let multipleContactsVisitor: ILivechatVisitor;
-		let room: IOmnichannelRoom;
 
-		before(() => updatePermission('close-others-livechat-room', ['livechat-manager', 'livechat-monitor', 'admin']));
-
-		afterEach(() => Promise.all([deleteVisitor(visitor.token), closeOmnichannelRoom(room._id)]));
+		afterEach(() => deleteVisitor(visitor.token));
 
 		after(() => deleteVisitor(multipleContactsVisitor.token));
 
 		it('Should create an innactive room and contact by default', async () => {
 			visitor = await createVisitor();
-			room = await createLivechatRoom(visitor.token);
+			const room = await createLivechatRoom(visitor.token);
 
 			expect(room).to.be.an('object');
 			expect(room.v.activity).to.be.undefined;
@@ -46,7 +42,7 @@ describe('MAC', () => {
 
 		it('Should create an innactive contact by default', async () => {
 			visitor = await createVisitor();
-			room = await createLivechatRoom(visitor.token);
+			const room = await createLivechatRoom(visitor.token);
 
 			const res = await request.get(api(`omnichannel/contacts.get`)).set(credentials).query({ contactId: room.v.contactId });
 			expect(res.body.contact.channels[0].visitorId).to.be.equal(visitor._id);
@@ -55,7 +51,7 @@ describe('MAC', () => {
 
 		it('should mark room as active when agent sends a message', async () => {
 			visitor = await createVisitor();
-			room = await createLivechatRoom(visitor.token);
+			const room = await createLivechatRoom(visitor.token);
 
 			await sendAgentMessage(room._id);
 
@@ -66,7 +62,7 @@ describe('MAC', () => {
 
 		it('should mark contact as active when agent sends a message', async () => {
 			multipleContactsVisitor = await createVisitor();
-			room = await createLivechatRoom(visitor.token);
+			const room = await createLivechatRoom(visitor.token);
 
 			await sendAgentMessage(room._id);
 
@@ -77,17 +73,18 @@ describe('MAC', () => {
 		});
 
 		it('should mark multiple rooms as active when they come from same contact after an agent sends a message', async () => {
-			room = await createLivechatRoom(multipleContactsVisitor.token);
+			const room = await createLivechatRoom(multipleContactsVisitor.token);
 
 			await sendAgentMessage(room._id);
 
 			const updatedRoom = await getLivechatRoomInfo(room._id);
 
 			expect(updatedRoom).to.have.nested.property('v.activity').and.to.be.an('array');
+			await closeOmnichannelRoom(room._id);
 		});
 
 		it('should keep contact active when reusing it and an agent response is received', async () => {
-			room = await createLivechatRoom(multipleContactsVisitor.token);
+			const room = await createLivechatRoom(multipleContactsVisitor.token);
 
 			await sendAgentMessage(room._id);
 
@@ -95,27 +92,30 @@ describe('MAC', () => {
 			expect(res.body.contact.channels[0].visitorId).to.be.equal(multipleContactsVisitor._id);
 			expect(res.body.contact).to.have.property('activity').that.is.an('array').with.lengthOf(1);
 			expect(res.body.contact.activity[0]).to.equal(moment.utc().format('YYYY-MM'));
+			await closeOmnichannelRoom(room._id);
 		});
 
 		it('should mark room as active when it comes from same contact on same period, even without agent interaction', async () => {
-			room = await createLivechatRoom(multipleContactsVisitor.token);
+			const room = await createLivechatRoom(multipleContactsVisitor.token);
 
 			expect(room).to.have.nested.property('v.activity').and.to.be.an('array');
 			expect(room.v.activity?.includes(moment.utc().format('YYYY-MM'))).to.be.true;
+			await closeOmnichannelRoom(room._id);
 		});
 
 		it('should mark an inquiry as active when it comes from same contact on same period, even without agent interaction', async () => {
-			room = await createLivechatRoom(multipleContactsVisitor.token);
+			const room = await createLivechatRoom(multipleContactsVisitor.token);
 			const inquiry = await fetchInquiry(room._id);
 
 			expect(inquiry).to.have.nested.property('v.activity').and.to.be.an('array');
 			expect(inquiry.v.activity?.includes(moment.utc().format('YYYY-MM'))).to.be.true;
 			expect(room.v.activity?.includes(moment.utc().format('YYYY-MM'))).to.be.true;
+			await closeOmnichannelRoom(room._id);
 		});
 
 		it('contact should be marked as active for period', async () => {
 			visitor = await createVisitor();
-			room = await createLivechatRoom(visitor.token);
+			const room = await createLivechatRoom(visitor.token);
 			await sendAgentMessage(room._id);
 			const res = await request.get(api(`omnichannel/contacts.get`)).set(credentials).query({ contactId: room.v.contactId });
 
@@ -128,6 +128,7 @@ describe('MAC', () => {
 
 			expect(res.body.contact).to.have.nested.property('activity').and.to.be.an('array').with.lengthOf(1);
 			expect(res.body.contact.activity[0]).to.equal(moment.utc().format('YYYY-MM'));
+			await closeOmnichannelRoom(room._id);
 		});
 	});
 });
