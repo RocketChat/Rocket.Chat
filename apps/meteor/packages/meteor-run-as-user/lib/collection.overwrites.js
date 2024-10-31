bd0dca272bb34b6d6779044f62d1d7fbd4f62a95
gabriellsh <henriques.gabriell@gmail.com>
2024-10-31 11:41:06 -0300

    overwrite async and remove logs

--- apps/meteor/packages/meteor-run-as-user/lib/collection.overwrites.js
+++ apps/meteor/packages/meteor-run-as-user/lib/collection.overwrites.js
@@ -1,9 +1,25 @@
 // TODO: Remove this after meteor fixes the issue
 // This override fixes a recent issue introduced after the update to Meteor 3.0
 // MinimongoCollection.remove(query) where `query` has `_id: { $in: Array }` removes only the first found record.
-LocalCollection.prototype['_eachPossiblyMatchingDocSync'] = function (selector, fn) {
+LocalCollection.prototype['_eachPossiblyMatchingDocAsync'] =  async function (selector, fn) {
     const specificIds = LocalCollection._idsMatchedBySelector(selector);
-	console.log('here');
+
+    if (specificIds) {
+      for (const id of specificIds) {
+        const doc = this._docs.get(id);
+
+        if (doc && (await fn(doc, id)) === false) { // Changed from `!fn(doc,id)`
+          break
+        }
+      }
+    } else {
+      await this._docs.forEachAsync(fn);
+    }
+  }
+
+  LocalCollection.prototype['_eachPossiblyMatchingDocSync'] = function (selector, fn) {
+    const specificIds = LocalCollection._idsMatchedBySelector(selector);
+
     if (specificIds) {
       for (const id of specificIds) {
         const doc = this._docs.get(id);
@@ -29,7 +45,6 @@ _.each(['insert', 'update', 'remove'], function (method) {
 	var _super = Mongo.Collection.prototype[method];
 
 	Mongo.Collection.prototype[method] = function (/* arguments */) {
-		// console.log(Mongo.Collection.prototype['_eachPossiblyMatchingDocSync']);
 		var self = this;
 		var args = _.toArray(arguments);
 
