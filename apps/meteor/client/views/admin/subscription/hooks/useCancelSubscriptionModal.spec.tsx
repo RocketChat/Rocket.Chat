4e745a0b60b9b05d252ef5dc4a71370805e25d1b
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-10-31 09:04:58 -0300

    test: refactored to use createDeferredMockFn

--- apps/meteor/client/views/admin/subscription/hooks/useCancelSubscriptionModal.spec.tsx
+++ apps/meteor/client/views/admin/subscription/hooks/useCancelSubscriptionModal.spec.tsx
@@ -2,6 +2,7 @@ import { mockAppRoot } from '@rocket.chat/mock-providers';
 import { act, renderHook, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import createDeferredMockFn from '../../../../../tests/mocks/utils/createDeferredMockFn';
 import { useCancelSubscriptionModal } from './useCancelSubscriptionModal';
 
 jest.mock('../../../../hooks/useLicense', () => ({
@@ -45,11 +46,11 @@ it('should close modal cancel is clicked', async () => {
 });
 
 it('should call remove license endpoint when confirm is clicked', async () => {
-	const removeLicenseEndpoint = jest.fn();
+	const { fn: removeLicenseEndpoint, resolve } = createDeferredMockFn<{ success: boolean }>();
+
 	const { result } = renderHook(() => useCancelSubscriptionModal(), {
 		wrapper: mockAppRoot()
 			.withEndpoint('POST', '/v1/cloud.removeLicense', removeLicenseEndpoint)
-			.withLatency(100)
 			.withTranslations('en', 'core', {
 				Cancel__planName__subscription: 'Cancel {{planName}} subscription',
 			})
@@ -63,6 +64,7 @@ it('should call remove license endpoint when confirm is clicked', async () => {
 
 	await userEvent.click(screen.getByRole('button', { name: 'Cancel_subscription' }));
 	expect(result.current.isLoading).toBeTruthy();
+	await act(() => resolve({ success: true }));
 	await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
 	expect(removeLicenseEndpoint).toHaveBeenCalled();
