ce18b473e75f57709d7de47cb715559248225832
Julio A. <52619625+julio-cfa@users.noreply.github.com>
2024-11-01 21:20:36 +0100

    fix: imported fixes (#33466)

--- apps/meteor/app/otr/server/methods/updateOTRAck.ts
+++ apps/meteor/app/otr/server/methods/updateOTRAck.ts
@@ -1,7 +1,10 @@
 import type { IOTRMessage } from '@rocket.chat/core-typings';
+import { Rooms } from '@rocket.chat/models';
 import type { ServerMethods } from '@rocket.chat/ui-contexts';
+import { check, Match } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
+import { canAccessRoomAsync } from '../../../authorization/server';
 import notifications from '../../../notifications/server/lib/Notifications';
 
 declare module '@rocket.chat/ui-contexts' {
@@ -12,10 +15,42 @@ declare module '@rocket.chat/ui-contexts' {
 }
 
 Meteor.methods<ServerMethods>({
-	updateOTRAck({ message, ack }) {
-		if (!Meteor.userId()) {
+	async updateOTRAck({ message, ack }) {
+		const uid = Meteor.userId();
+
+		if (!uid) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'updateOTRAck' });
 		}
+
+		check(ack, String);
+		check(message, {
+			_id: String,
+			rid: String,
+			msg: String,
+			t: Match.Any,
+			ts: Date,
+			u: {
+				_id: String,
+				username: String,
+				name: String,
+			},
+			otr: Match.Maybe({ ack: String }),
+		});
+
+		if (message?.t !== 'otr') {
+			throw new Meteor.Error('error-invalid-message', 'Invalid message type', { method: 'updateOTRAck' });
+		}
+
+		const room = await Rooms.findOneById(message.rid, { projection: { t: 1, _id: 1, uids: 1 } });
+
+		if (!room) {
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'updateOTRAck' });
+		}
+
+		if (!(await canAccessRoomAsync(room, { _id: uid })) || (room.uids && (!message.u._id || !room.uids.includes(message.u._id)))) {
+			throw new Meteor.Error('error-invalid-user', 'Invalid user, not in room', { method: 'updateOTRAck' });
+		}
+
 		const acknowlegedMessage: IOTRMessage = { ...message, otrAck: ack };
 		notifications.streamRoomMessage.emit(message.rid, acknowlegedMessage);
 	},
