94752c2e731665a75f8571de225d5882d123f355
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-11-14 16:18:10 -0300

    test: improved error message in setSettingValueById

--- apps/meteor/tests/e2e/utils/setSettingValueById.ts
+++ apps/meteor/tests/e2e/utils/setSettingValueById.ts
@@ -6,7 +6,9 @@ export const setSettingValueById = async (api: BaseTest['api'], settingId: strin
 	const response = await api.post(`/settings/${settingId}`, { value });
 
 	if (response.status() !== 200) {
-		throw new Error(`Failed to update setting [http status: ${response.status()}]`);
+		throw new Error(
+			`Failed to update setting ${settingId} with value "${value}" [http status: ${response.status()} ${response.statusText()}]`,
+		);
 	}
 
 	return response;
