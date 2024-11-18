3d0335bdc733ce6e05175b5227c32f00e4f8774a
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-11-18 10:17:07 -0300

    chore: ci madness

--- apps/meteor/client/views/admin/subscription/components/CancelSubscriptionModal.spec.tsx
+++ apps/meteor/client/views/admin/subscription/components/CancelSubscriptionModal.spec.tsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import { DOWNGRADE_LINK } from '../utils/links';
 import { CancelSubscriptionModal } from './CancelSubscriptionModal';
+import { DOWNGRADE_LINK } from '../utils/links';
 
 it('should display plan name in the title', async () => {
 	const confirmFn = jest.fn();
