1a815aee9ef9895ccd63449e4a44c38adc41f0ba
juliajforesti <juliajforesti@gmail.com>
2024-11-18 13:23:55 -0300

    test: add test case

--- apps/meteor/client/sidebarv2/hooks/useRoomList.spec.tsx
+++ apps/meteor/client/sidebarv2/hooks/useRoomList.spec.tsx
@@ -275,3 +275,21 @@ it('should accumulate unread data into `groupedUnreadInfo` when group is collaps
 	expect(tunread).toEqual(fakeRooms.reduce((acc, cv) => [...acc, ...(cv.tunread || [])], [] as string[]));
 	expect(tunreadUser).toEqual(fakeRooms.reduce((acc, cv) => [...acc, ...(cv.tunreadUser || [])], [] as string[]));
 });
+
+it('should add to unread group when has thread unread, even if alert is false', async () => {
+	const fakeRoom = {
+		...createFakeSubscription({ ...emptyUnread, tunread: ['1'], alert: false }),
+	} as unknown as SubscriptionWithRoom;
+
+	const { result } = renderHook(() => useRoomList({ collapsedGroups: [] }), {
+		legacyRoot: true,
+		wrapper: getWrapperSettings({
+			sidebarGroupByType: true,
+			sidebarShowUnread: true,
+			fakeRoom,
+		}).build(),
+	});
+
+	const unreadGroup = result.current.roomList.splice(0, result.current.groupsCount[0]);
+	expect(unreadGroup.find((room) => room.name === fakeRoom.name)).toBeDefined();
+});
