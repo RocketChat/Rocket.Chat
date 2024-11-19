659cbf26440089dfdcb343dc0c3c08d607007429
gustrb <gustavoreisbauer@gmail.com>
2024-11-19 11:32:57 -0300

    chore: Use GenericModal

--- apps/meteor/client/views/marketplace/AppExemptModal.tsx
+++ apps/meteor/client/views/marketplace/AppExemptModal.tsx
@@ -1,7 +1,8 @@
-import { Button, Box, Modal } from '@rocket.chat/fuselage';
+import { Box, Modal } from '@rocket.chat/fuselage';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import GenericModal from '../../components/GenericModal';
 import { useExternalLink } from '../../hooks/useExternalLink';
 import { useCheckoutUrl } from '../admin/subscription/hooks/useCheckoutUrl';
 import { PRICING_LINK } from '../admin/subscription/utils/links';
@@ -23,33 +24,25 @@ const AppExemptModal = ({ onCancel, appName }: AppExemptModalProps) => {
 	};
 
 	return (
-		<Modal>
-			<Modal.Header>
-				<Modal.HeaderText>
-					<Modal.Title>{t('Apps_Cannot_Be_Updated')}</Modal.Title>
-				</Modal.HeaderText>
-				<Modal.Close aria-label={t('Close')} onClick={onCancel} />
-			</Modal.Header>
-
-			<Modal.Content>
-				<Box mbe={28}>{t('Apps_Private_App_Is_Exempt', { appName })}</Box>
-				{t('Upgrade_subscription_to_enable_private_apps')}
-			</Modal.Content>
-
-			<Modal.Footer justifyContent='space-between'>
+		<GenericModal
+			title={t('Apps_Cannot_Be_Updated')}
+			onClose={onCancel}
+			dontAskAgain={
 				<Modal.FooterAnnotation>
 					<a target='_blank' rel='noopener noreferrer' href={PRICING_LINK}>
 						{t('Compare_plans')}
 					</a>
 				</Modal.FooterAnnotation>
-				<Modal.FooterControllers>
-					<Button onClick={onCancel}>{t('Cancel')}</Button>
-					<Button onClick={goToManageSubscriptionPage} primary>
-						{t('Upgrade')}
-					</Button>
-				</Modal.FooterControllers>
-			</Modal.Footer>
-		</Modal>
+			}
+			variant='warning'
+			cancelText={t('Cancel')}
+			confirmText={t('Upgrade')}
+			onCancel={onCancel}
+			onConfirm={goToManageSubscriptionPage}
+		>
+			<Box mbe={28}>{t('Apps_Private_App_Is_Exempt', { appName })}</Box>
+			{t('Upgrade_subscription_to_enable_private_apps')}
+		</GenericModal>
 	);
 };
 
