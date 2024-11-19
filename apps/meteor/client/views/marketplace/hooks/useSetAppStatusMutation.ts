f9df1441be3e51324a8239df8cecc7fc8c61457f
Tasso <tasso.evangelista@rocket.chat>
2024-11-19 18:56:04 -0300

    Use optimistic update on `useSetAppStatusMutation`

--- apps/meteor/client/views/marketplace/hooks/useSetAppStatusMutation.ts
+++ apps/meteor/client/views/marketplace/hooks/useSetAppStatusMutation.ts
@@ -1,11 +1,12 @@
 import { AppStatus } from '@rocket.chat/apps-engine/definition/AppStatus';
 import type { App } from '@rocket.chat/core-typings';
 import { useToastMessageDispatch, useEndpoint } from '@rocket.chat/ui-contexts';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 
 import { appErroredStatuses } from '../helpers/appErroredStatuses';
 import { handleAPIError } from '../helpers/handleAPIError';
+import { marketplaceQueryKeys } from '../queryKeys';
 
 export const useSetAppStatusMutation = (app: App) => {
 	const { t } = useTranslation();
@@ -13,10 +14,33 @@ export const useSetAppStatusMutation = (app: App) => {
 
 	const setAppStatus = useEndpoint('POST', '/apps/:id/status', { id: app.id });
 
+	const queryClient = useQueryClient();
+
 	return useMutation({
 		mutationFn: async (status: AppStatus) => {
 			const { status: updatedStatus } = await setAppStatus({ status });
 			return updatedStatus;
+		},
+		onMutate: (status) => {
+			const previous:
+				| {
+						marketplace: App[];
+						installed: App[];
+						private: App[];
+				  }
+				| undefined = queryClient.getQueryData(marketplaceQueryKeys.apps({ canManageApps: true }));
+
+			if (!previous) {
+				return;
+			}
+
+			queryClient.setQueryData(marketplaceQueryKeys.apps({ canManageApps: true }), {
+				marketplace: previous.marketplace,
+				installed: previous.installed.map((_app) => (_app.id === app.id ? { ..._app, status } : _app)),
+				private: previous.private.map((_app) => (_app.id === app.id ? { ..._app, status } : _app)),
+			});
+
+			return () => queryClient.setQueryData(marketplaceQueryKeys.apps({ canManageApps: true }), previous);
 		},
 		onSuccess: (status) => {
 			if (appErroredStatuses.includes(status)) {
@@ -33,8 +57,12 @@ export const useSetAppStatusMutation = (app: App) => {
 				dispatchToastMessage({ type: 'success', message: `${app.name} disabled` }); // TODO: i18n
 			}
 		},
-		onError: (error) => {
+		onError: (error, _status, revert) => {
 			handleAPIError(error);
+			revert?.();
+		},
+		onSettled: () => {
+			queryClient.invalidateQueries(marketplaceQueryKeys.apps({ canManageApps: true }));
 		},
 	});
 };
