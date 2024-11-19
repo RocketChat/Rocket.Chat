a397c789b99b2f1f1381eb4ca91fd4cbeab2ff68
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-11-19 16:09:57 -0300

    fix: make sure username is set for required actions

--- apps/meteor/app/api/server/definition.ts
+++ apps/meteor/app/api/server/definition.ts
@@ -62,6 +62,7 @@ export type Options = (
 						'*'?: { operation: TOperation; permissions: string[] };
 				  });
 			authRequired?: boolean;
+			incompleteUserAllowed?: boolean;
 			forceTwoFactorAuthenticationForNonEnterprise?: boolean;
 			rateLimiterOptions?:
 				| {
@@ -159,11 +160,17 @@ export type ActionThis<TMethod extends Method, TPathPattern extends PathPattern,
 		query: Record<string, unknown>;
 	}>;
 } & (TOptions extends { authRequired: true }
-	? {
-			user: IUser;
-			userId: string;
-			readonly token: string;
-		}
+	? TOptions extends { incompleteUserAllowed: true }
+		? {
+				user: IUser;
+				userId: string;
+				readonly token: string;
+			}
+		: {
+				user: Omit<IUser, 'username'> & Required<Pick<IUser, 'username'>>;
+				userId: string;
+				readonly token: string;
+			}
 	: TOptions extends { authOrAnonRequired: true }
 		? {
 				user?: IUser;
