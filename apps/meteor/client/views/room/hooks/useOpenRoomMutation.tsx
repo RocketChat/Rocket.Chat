d92dd428d1dcc5762e56c09750b04772c315a48c
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-11-21 19:18:50 -0300

    feat: added optmistic update

--- apps/meteor/client/views/room/hooks/useOpenRoomMutation.tsx
+++ apps/meteor/client/views/room/hooks/useOpenRoomMutation.tsx
@@ -1,7 +1,7 @@
 import { useEndpoint, useUserId } from '@rocket.chat/ui-contexts';
 import { useMutation } from '@tanstack/react-query';
 
-import { ChatSubscription } from '../../../../app/models/client';
+import { Subscriptions } from '../../../../app/models/client';
 
 export const useOpenRoomMutation = () => {
 	const openRoom = useEndpoint('POST', '/v1/rooms.open');
@@ -14,10 +14,21 @@ export const useOpenRoomMutation = () => {
 			}
 
 			await openRoom({ roomId });
+
 			return roomId;
 		},
-		onSuccess: (roomId: string) => {
-			ChatSubscription.update({ roomId, 'u._id': Meteor.userId() }, { $set: { open: true } });
+		onMutate: async ({ roomId: rid }) => {
+			const ogDocument = await Subscriptions.findOne({ rid, 'u._id': userId }, { fields: { open: 1 } });
+			await Subscriptions.update({ rid, 'u._id': userId }, { $set: { open: true } });
+			return { ogDocument };
+		},
+		onError: async (_, { roomId }, context) => {
+			if (!context?.ogDocument) {
+				return;
+			}
+
+			const { open } = context.ogDocument;
+			Subscriptions.update({ roomId, 'u._id': userId }, { $set: { open } });
 		},
 	});
 };
