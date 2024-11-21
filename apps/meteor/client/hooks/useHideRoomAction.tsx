aa099fb0025f5e9abc705293f7325ef672009aae
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-11-21 15:48:18 -0300

    feat: added optimistic update

--- apps/meteor/client/hooks/useHideRoomAction.tsx
+++ apps/meteor/client/hooks/useHideRoomAction.tsx
@@ -1,12 +1,13 @@
 import type { RoomType } from '@rocket.chat/core-typings';
 import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import type { TranslationKey } from '@rocket.chat/ui-contexts';
-import { useEndpoint, useSetModal, useToastMessageDispatch, useRouter } from '@rocket.chat/ui-contexts';
+import { useEndpoint, useSetModal, useToastMessageDispatch, useRouter, useUserId } from '@rocket.chat/ui-contexts';
 import { useMutation } from '@tanstack/react-query';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useDontAskAgain } from './useDontAskAgain';
+import { Subscriptions } from '../../app/models/client';
 import { UiTextContext } from '../../definition/IRoomTypeConfig';
 import { GenericModalDoNotAskAgain } from '../components/GenericModal';
 import { roomCoordinator } from '../lib/rooms/roomCoordinator';
@@ -36,18 +37,36 @@ export const useHideRoomAction = ({ rid, type, name }: HideRoomProps, { redirect
 	const dispatchToastMessage = useToastMessageDispatch();
 	const dontAskHideRoom = useDontAskAgain('hideRoom');
 	const router = useRouter();
+	const userId = useUserId();
 
 	const hideRoomEndpoint = useEndpoint('POST', CLOSE_ENDPOINTS_BY_ROOM_TYPE[type]);
 
 	const hideRoom = useMutation({
 		mutationFn: () => hideRoomEndpoint({ roomId: rid }),
+		onMutate: async () => {
+			closeModal();
+
+			const ogDocument = await Subscriptions.findOne({ rid, 'u._id': userId }, { fields: { alert: 1, open: 1 } });
+
+			// Optmistic update
+			await Subscriptions.update({ rid, 'u._id': userId }, { $set: { alert: false, open: false } });
+
+			return { ogDocument };
+		},
 		onSuccess: () => {
 			if (redirect) {
 				router.navigate('/home');
 			}
 		},
-		onError: (error) => dispatchToastMessage({ type: 'error', message: error }),
-		onSettled: () => closeModal(),
+		onError: (error, _, context) => {
+			dispatchToastMessage({ type: 'error', message: error });
+
+			// Revert optimistic changes
+			if (context?.ogDocument) {
+				const { alert, open } = context.ogDocument;
+				Subscriptions.update({ rid, 'u._id': userId }, { $set: { alert, open } });
+			}
+		},
 	});
 
 	const handleHide = useEffectEvent(async () => {
