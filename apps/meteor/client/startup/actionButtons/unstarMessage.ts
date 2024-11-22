4b13e39fb8e98870774050c18ff9490eaf147b7e
Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>
2024-11-22 06:23:58 +0530

    chore: Remove `starMessage` client meteor method (#33817)
    Co-authored-by: Tasso <tasso.evangelista@rocket.chat>

--- apps/meteor/client/startup/actionButtons/unstarMessage.ts
+++ apps/meteor/client/startup/actionButtons/unstarMessage.ts
@@ -3,7 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { settings } from '../../../app/settings/client';
 import { MessageAction } from '../../../app/ui-utils/client';
 import { sdk } from '../../../app/utils/client/lib/SDKClient';
+import { toggleStarredMessage } from '../../lib/mutationEffects/starredMessage';
 import { queryClient } from '../../lib/queryClient';
+import { roomsQueryKeys } from '../../lib/queryKeys';
 import { dispatchToastMessage } from '../../lib/toast';
 
 Meteor.startup(() => {
@@ -15,10 +17,13 @@ Meteor.startup(() => {
 		context: ['starred', 'message', 'message-mobile', 'threads', 'federated', 'videoconf', 'videoconf-threads'],
 		async action(_, { message }) {
 			try {
-				await sdk.call('starMessage', { ...message, starred: false });
-				queryClient.invalidateQueries(['rooms', message.rid, 'starred-messages']);
+				await sdk.rest.post('/v1/chat.unStarMessage', { messageId: message._id });
+				toggleStarredMessage(message, false);
 			} catch (error) {
 				dispatchToastMessage({ type: 'error', message: error });
+			} finally {
+				queryClient.invalidateQueries(roomsQueryKeys.starredMessages(message.rid));
+				queryClient.invalidateQueries(roomsQueryKeys.messageActions(message.rid, message._id));
 			}
 		},
 		condition({ message, subscription, user }) {
