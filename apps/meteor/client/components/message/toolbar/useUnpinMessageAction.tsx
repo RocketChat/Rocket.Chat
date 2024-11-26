ae2e9ec92bc38c85b99b05f438cc676c902b41fc
MartinSchoeler <martinschoeler8@gmail.com>
2024-11-26 09:56:51 -0300

    chore: remove more conditions

--- apps/meteor/client/components/message/toolbar/useUnpinMessageAction.tsx
+++ apps/meteor/client/components/message/toolbar/useUnpinMessageAction.tsx
@@ -1,23 +1,40 @@
-import type { IRoom } from '@rocket.chat/core-typings';
-import { usePermission, useSetting, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import type { IMessage, IRoom, ISubscription } from '@rocket.chat/core-typings';
+import { isOmnichannelRoom } from '@rocket.chat/core-typings';
+import { useSetting, useToastMessageDispatch, useSetModal, usePermission } from '@rocket.chat/ui-contexts';
 import { useEffect } from 'react';
 
 import { MessageAction } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { sdk } from '../../../../app/utils/client/lib/SDKClient';
 import { queryClient } from '../../../lib/queryClient';
+import PinMessageModal from '../../../views/room/modals/PinMessageModal';
 
-export const useUnpinMessageAction = (room: IRoom) => {
+export const useUnpinMessageAction = (
+	message: IMessage,
+	{ room, subscription }: { room: IRoom; subscription: ISubscription | undefined },
+) => {
 	const dispatchToastMessage = useToastMessageDispatch();
+
+	const setModal = useSetModal();
 
 	const allowPinning = useSetting('Message_AllowPinning');
 	const hasPermission = usePermission('pin-message', room._id);
 
 	useEffect(() => {
-		if (!allowPinning || !hasPermission) {
+		if (!allowPinning || isOmnichannelRoom(room) || !hasPermission || message.pinned || !subscription) {
 			return () => {
 				MessageAction.removeButton('unpin-message');
 			};
 		}
+
+		const onConfirm = async () => {
+			try {
+				await sdk.call('unpinMessage', message);
+				queryClient.invalidateQueries(['rooms', message.rid, 'pinned-messages']);
+			} catch (error) {
+				dispatchToastMessage({ type: 'error', message: error });
+			}
+			setModal(null);
+		};
 
 		MessageAction.addButton({
 			id: 'unpin-message',
@@ -25,20 +42,15 @@ export const useUnpinMessageAction = (room: IRoom) => {
 			label: 'Unpin',
 			type: 'interaction',
 			context: ['pinned', 'message', 'message-mobile', 'threads', 'direct', 'videoconf', 'videoconf-threads'],
-			async action(_, { message }) {
-				message.pinned = false;
-				try {
-					await sdk.call('unpinMessage', message);
-					queryClient.invalidateQueries(['rooms', message.rid, 'pinned-messages']);
-				} catch (error) {
-					dispatchToastMessage({ type: 'error', message: error });
-				}
-			},
-			condition({ message, subscription }) {
-				if (!subscription || !message.pinned) {
-					return false;
-				}
-				return true;
+			async action() {
+				setModal({
+					component: PinMessageModal,
+					props: {
+						message,
+						onConfirm,
+						onCancel: () => setModal(null),
+					},
+				});
 			},
 			order: 2,
 			group: 'menu',
@@ -47,5 +59,5 @@ export const useUnpinMessageAction = (room: IRoom) => {
 		return () => {
 			MessageAction.removeButton('unpin-message');
 		};
-	}, [allowPinning, dispatchToastMessage, hasPermission]);
+	}, [allowPinning, dispatchToastMessage, hasPermission, message, room, setModal, subscription]);
 };
