bb82ddd2b7c4fe6c6afd47d76866f0dc9d645fed
MartinSchoeler <martinschoeler8@gmail.com>
2024-11-26 16:39:06 -0300

    chore: trim message action data

--- apps/meteor/client/components/message/toolbar/useJumpToSearchMessageAction.tsx
+++ apps/meteor/client/components/message/toolbar/useJumpToSearchMessageAction.tsx
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 import { MessageAction } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { setMessageJumpQueryStringParameter } from '../../../lib/utils/setMessageJumpQueryStringParameter';
 
-export const useJumpToSearchMessageAction = () => {
+export const useJumpToSearchMessageAction = (message) => {
 	useEffect(() => {
 		MessageAction.addButton({
 			id: 'jump-to-search-message',
 			icon: 'jump',
 			label: 'Jump_to_message',
 			context: ['search'],
-			async action(_, { message }) {
+			async action() {
 				setMessageJumpQueryStringParameter(message._id);
 			},
 			order: 100,
