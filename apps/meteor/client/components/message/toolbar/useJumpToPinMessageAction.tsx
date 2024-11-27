d900eba16b1c362750c3ff6abb17e8d9420d473a
MartinSchoeler <martinschoeler8@gmail.com>
2024-11-27 15:11:42 -0300

    chore: remove more conditions

--- apps/meteor/client/components/message/toolbar/useJumpToPinMessageAction.tsx
+++ apps/meteor/client/components/message/toolbar/useJumpToPinMessageAction.tsx
@@ -1,19 +1,23 @@
+import type { IMessage, ISubscription } from '@rocket.chat/core-typings';
 import { useEffect } from 'react';
 
 import { MessageAction } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { setMessageJumpQueryStringParameter } from '../../../lib/utils/setMessageJumpQueryStringParameter';
 
-export const useJumpToPinMessageAction = () => {
+export const useJumpToPinMessageAction = (message: IMessage, { subscription }: { subscription: ISubscription | undefined }) => {
 	useEffect(() => {
+		if (subscription) {
+			return;
+		}
 		MessageAction.addButton({
 			id: 'jump-to-pin-message',
 			icon: 'jump',
 			label: 'Jump_to_message',
 			context: ['pinned', 'message-mobile', 'direct'],
-			action(_, { message }) {
+			action() {
 				setMessageJumpQueryStringParameter(message._id);
 			},
-			condition({ subscription }) {
+			condition() {
 				return !!subscription;
 			},
 			order: 100,
@@ -23,5 +27,5 @@ export const useJumpToPinMessageAction = () => {
 		return () => {
 			MessageAction.removeButton('jump-to-pin-message');
 		};
-	}, []);
+	}, [message._id, subscription]);
 };
