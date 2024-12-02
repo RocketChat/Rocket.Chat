bc241f66c60106ddc3085f072f9ae53def190f85
Pierre <pierre.lehnen@rocket.chat>
2024-12-02 14:52:39 -0300

    removed JS privates

--- packages/core-services/src/lib/ServiceStarter.ts
+++ packages/core-services/src/lib/ServiceStarter.ts
@@ -4,65 +4,65 @@
 // Using this class, you ensure that calls to .start and .stop will be chained, so you avoid race conditions
 // At the same time, it prevents those functions from running more times than necessary if there are several calls to them (for example when loading setting values)
 export class ServiceStarter {
-	#lock = Promise.resolve();
+	private lock = Promise.resolve();
 
-	#currentCall?: 'start' | 'stop';
+	private currentCall?: 'start' | 'stop';
 
-	#nextCall?: 'start' | 'stop';
+	private nextCall?: 'start' | 'stop';
 
-	#starterFn: () => Promise<void>;
+	private starterFn: () => Promise<void>;
 
-	#stopperFn?: () => Promise<void>;
+	private stopperFn?: () => Promise<void>;
 
 	constructor(starterFn: () => Promise<void>, stopperFn?: () => Promise<void>) {
-		this.#starterFn = starterFn;
-		this.#stopperFn = stopperFn;
+		this.starterFn = starterFn;
+		this.stopperFn = stopperFn;
 	}
 
-	async #checkStatus(): Promise<void> {
-		if (this.#nextCall === 'start') {
-			return this.#doCall('start');
+	private async checkStatus(): Promise<void> {
+		if (this.nextCall === 'start') {
+			return this.doCall('start');
 		}
 
-		if (this.#nextCall === 'stop') {
-			return this.#doCall('stop');
+		if (this.nextCall === 'stop') {
+			return this.doCall('stop');
 		}
 	}
 
-	async #doCall(call: 'start' | 'stop'): Promise<void> {
-		this.#nextCall = undefined;
-		this.#currentCall = call;
+	private async doCall(call: 'start' | 'stop'): Promise<void> {
+		this.nextCall = undefined;
+		this.currentCall = call;
 		try {
 			if (call === 'start') {
-				await this.#starterFn();
-			} else if (this.#stopperFn) {
-				await this.#stopperFn();
+				await this.starterFn();
+			} else if (this.stopperFn) {
+				await this.stopperFn();
 			}
 		} finally {
-			this.#currentCall = undefined;
-			await this.#checkStatus();
+			this.currentCall = undefined;
+			await this.checkStatus();
 		}
 	}
 
-	async #call(call: 'start' | 'stop'): Promise<void> {
+	private async call(call: 'start' | 'stop'): Promise<void> {
 		// If something is already chained to run after the current call, it's okay to replace it with the new call
-		this.#nextCall = call;
-		if (this.#currentCall) {
-			return this.#lock;
+		this.nextCall = call;
+		if (this.currentCall) {
+			return this.lock;
 		}
-		this.#lock = this.#lock.then(() => this.#checkStatus());
-		return this.#lock;
+		this.lock = this.lock.then(() => this.checkStatus());
+		return this.lock;
 	}
 
 	async start(): Promise<void> {
-		return this.#call('start');
+		return this.call('start');
 	}
 
 	async stop(): Promise<void> {
-		return this.#call('stop');
+		return this.call('stop');
 	}
 
 	async wait(): Promise<void> {
-		return this.#lock;
+		return this.lock;
 	}
 }
