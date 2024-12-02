7e21c602fff645e2528a56821445d58f58e3e39b
juliajforesti <juliajforesti@gmail.com>
2024-12-02 13:35:26 -0300

    chore: adjust imports

--- apps/meteor/client/views/room/RoomSkeleton.tsx
+++ apps/meteor/client/views/room/RoomSkeleton.tsx
@@ -1,25 +1,14 @@
-import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
 import type { ReactElement } from 'react';
 import React from 'react';
 
 import HeaderSkeleton from './Header/HeaderSkeleton';
-import HeaderSkeletonV2 from './Header/HeaderSkeleton';
 import RoomComposerSkeleton from './composer/RoomComposer/RoomComposerSkeleton';
 import RoomLayout from './layout/RoomLayout';
 import MessageListSkeleton from '../../components/message/list/MessageListSkeleton';
 
 const RoomSkeleton = (): ReactElement => (
 	<RoomLayout
-		header={
-			<FeaturePreview feature='newNavigation'>
-				<FeaturePreviewOff>
-					<HeaderSkeleton />
-				</FeaturePreviewOff>
-				<FeaturePreviewOn>
-					<HeaderSkeletonV2 />
-				</FeaturePreviewOn>
-			</FeaturePreview>
-		}
+		header={<HeaderSkeleton />}
 		body={
 			<>
 				<MessageListSkeleton />
