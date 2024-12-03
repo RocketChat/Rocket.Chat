0c9df7da5e2f3a7b6c8d1c634061461e2ae92695
dougfabris <devfabris@gmail.com>
2024-12-03 10:05:53 -0300

    chore: remove duplicated verifications

--- apps/meteor/client/components/message/hooks/usePinMessageMutation.ts
+++ apps/meteor/client/components/message/hooks/usePinMessageMutation.ts
@@ -1,45 +1,20 @@
 import type { IMessage } from '@rocket.chat/core-typings';
-import { useEndpoint, useSetting, useToastMessageDispatch, useUserId } from '@rocket.chat/ui-contexts';
+import { useEndpoint, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
 import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 
-import { Subscriptions } from '../../../../app/models/client';
-import { InvalidMessage } from '../../../lib/errors/InvalidMessage';
-import { NotAuthorizedError } from '../../../lib/errors/NotAuthorizedError';
-import { PinMessagesNotAllowed } from '../../../lib/errors/PinMessagesNotAllowed';
 import { updatePinMessage } from '../../../lib/mutationEffects/updatePinMessage';
 import { roomsQueryKeys } from '../../../lib/queryKeys';
 
 export const usePinMessageMutation = () => {
 	const { t } = useTranslation();
-	const userId = useUserId();
-	const allowPinning = useSetting('Message_AllowPinning');
 	const pinMessageEndpoint = useEndpoint('POST', '/v1/chat.pinMessage');
 	const dispatchToastMessage = useToastMessageDispatch();
 
 	const queryClient = useQueryClient();
 
 	return useMutation({
-		mutationFn: async (message: IMessage) => {
-			if (!userId) {
-				throw new NotAuthorizedError('Not authorized', {
-					method: 'pinMessage',
-				});
-			}
-			if (!allowPinning || !Subscriptions.findOne({ rid: message.rid })) {
-				throw new PinMessagesNotAllowed('Pinning messages is not allowed', {
-					method: 'pinMessage',
-				});
-			}
-
-			if (typeof message._id !== 'string') {
-				throw new InvalidMessage('Invalid message', {
-					method: 'pinMessage',
-				});
-			}
-
-			await pinMessageEndpoint({ messageId: message._id });
-		},
+		mutationFn: async (message: IMessage) => pinMessageEndpoint({ messageId: message._id }),
 		onMutate: (message) => {
 			updatePinMessage(message, { pinned: true });
 		},
--- apps/meteor/client/lib/errors/InvalidMessage.ts
+++
@@ -1,7 +0,0 @@
-import { RocketChatError } from './RocketChatError';
-
-export class InvalidMessage extends RocketChatError<'invalid-message'> {
-	constructor(message = 'Invalid message', details?: unknown) {
-		super('invalid-message', message, details);
-	}
-}
