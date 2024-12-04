c80bb2d35049d81e58a381d8e1426800d1bbff9f
juliajforesti <juliajforesti@gmail.com>
2024-12-04 12:48:16 -0300

    test: deprecate `home-sidenav` fragments. moved and refactored to `sidebar`, `navbar` and `create-room-modal`

---
+++ apps/meteor/tests/e2e/page-objects/fragments/create-room-modal.ts
@@ -0,0 +1,56 @@
+import type { Locator, Page } from '@playwright/test';
+
+export class CreateRoomModal {
+	private readonly page: Page;
+
+	constructor(page: Page) {
+		this.page = page;
+	}
+
+	get modalRoot(): Locator {
+		return this.page.locator('#modal-root');
+	}
+
+	getModalByRoomType(roomType: 'channel' | 'team' | 'direct message' | 'discussion'): Locator {
+		if (roomType === 'channel' || roomType === 'team') {
+			return this.modalRoot.getByRole('dialog', { name: `Create ${roomType}`, exact: true });
+		}
+		if (roomType === 'direct message') {
+			return this.modalRoot.getByRole('dialog', { name: `New ${roomType}`, exact: true });
+		}
+		// TODO: Add discussion form id labelledby
+		return this.modalRoot.getByRole('dialog');
+	}
+
+	get inputChannelName(): Locator {
+		return this.getModalByRoomType('channel').getByRole('textbox', { name: 'Name' });
+	}
+
+	get inputDirectUsernames(): Locator {
+		return this.getModalByRoomType('direct message').getByRole('textbox');
+	}
+
+	get advancedSettingsAccordion(): Locator {
+		return this.modalRoot.getByRole('dialog').getByRole('button', { name: 'Advanced settings', exact: true });
+	}
+
+	getCheckboxByLabel(label: string): Locator {
+		return this.modalRoot.locator('label', { has: this.page.getByRole('checkbox', { name: label }) });
+	}
+
+	get checkboxPrivate(): Locator {
+		return this.getCheckboxByLabel('Private');
+	}
+
+	get checkboxEncryption(): Locator {
+		return this.getCheckboxByLabel('Encrypted');
+	}
+
+	get checkboxReadOnly(): Locator {
+		return this.getCheckboxByLabel('Read-only');
+	}
+
+	get btnCreate(): Locator {
+		return this.modalRoot.getByRole('button', { name: 'Create' });
+	}
+}
--- apps/meteor/tests/e2e/page-objects/fragments/sidebar.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/sidebar.ts
@@ -1,12 +1,16 @@
 import type { Locator, Page } from '@playwright/test';
 
+import { CreateRoomModal } from './create-room-modal';
 import { expect } from '../../utils/test';
 
 export class Sidebar {
 	private readonly page: Page;
 
+	readonly newRoomModal: CreateRoomModal;
+
 	constructor(page: Page) {
 		this.page = page;
+		this.newRoomModal = new CreateRoomModal(page);
 	}
 
 	// New navigation locators
@@ -14,20 +18,20 @@ export class Sidebar {
 		return this.page.getByRole('navigation', { name: 'sidebar' });
 	}
 
-	get sidebarSearchSection(): Locator {
-		return this.sidebar.getByRole('search');
-	}
-
 	get btnRecent(): Locator {
-		return this.sidebarSearchSection.getByRole('button', { name: 'Recent' });
+		return this.searchSection.getByRole('button', { name: 'Recent' });
 	}
 
 	get channelsList(): Locator {
 		return this.sidebar.getByRole('list', { name: 'Channels' });
 	}
 
+	get searchSection(): Locator {
+		return this.sidebar.getByRole('search');
+	}
+
 	get searchList(): Locator {
-		return this.sidebar.getByRole('search').getByRole('list', { name: 'Channels' });
+		return this.searchSection.getByRole('list', { name: 'Channels' });
 	}
 
 	get firstCollapser(): Locator {
@@ -39,7 +43,7 @@ export class Sidebar {
 	}
 
 	get searchInput(): Locator {
-		return this.sidebarSearchSection.getByRole('searchbox');
+		return this.searchSection.getByRole('searchbox');
 	}
 
 	async escSearch(): Promise<void> {
@@ -62,17 +66,30 @@ export class Sidebar {
 		return this.searchList.getByRole('link', { name });
 	}
 
+	getSidebarItemByName(name: string): Locator {
+		return this.channelsList.getByRole('link', { name });
+	}
+
 	async openChat(name: string): Promise<void> {
 		await this.typeSearch(name);
 		await this.getSearchRoomByName(name).click();
-		await this.waitForChannel();
 	}
 
-	async markItemAsUnread(item: Locator): Promise<void> {
+	async openItemMenu(item: Locator): Promise<void> {
 		await item.hover();
 		await item.focus();
 		await item.locator('.rcx-sidebar-item__menu').click();
+	}
+
+	async markItemAsUnread(item: Locator): Promise<void> {
+		await this.openItemMenu(item);
 		await this.page.getByRole('option', { name: 'Mark Unread' }).click();
+	}
+
+	async selectPriority(name: string, priority: string) {
+		const item = this.getSidebarItemByName(name);
+		await this.openItemMenu(item);
+		await this.page.getByRole('option', { name: priority }).click();
 	}
 
 	getCollapseGroupByName(name: string): Locator {
@@ -81,5 +98,33 @@ export class Sidebar {
 
 	getItemUnreadBadge(item: Locator): Locator {
 		return item.getByRole('status', { name: 'unread' });
+	}
+
+	// Note: this is different from openChat because queued chats are not searchable
+	getQueuedChat(name: string): Locator {
+		return this.page.locator('[data-qa="sidebar-item-title"]', { hasText: new RegExp(`^${name}$`) }).first();
+	}
+
+	async openCreateNewByLabel(name: 'Direct message' | 'Discussion' | 'Channel' | 'Team'): Promise<void> {
+		await this.searchSection.getByRole('button', { name: 'Create new', exact: true }).click();
+		await this.page.getByRole('menuitem', { name }).click();
+	}
+
+	async setDisplayMode(mode: 'Extended' | 'Medium' | 'Condensed'): Promise<void> {
+		await this.searchSection.getByRole('button', { name: 'Display', exact: true }).click();
+		await this.sidebar.getByRole('menuitemcheckbox', { name: mode }).click();
+		await this.searchSection.click();
+	}
+
+	async createEncryptedChannel(name: string) {
+		await this.openCreateNewByLabel('Channel');
+		await this.newRoomModal.inputChannelName.fill(name);
+	}
+
+	async createPublicChannel(name: string) {
+		await this.openCreateNewByLabel('Channel');
+		await this.newRoomModal.checkboxPrivate.click();
+		await this.newRoomModal.inputChannelName.fill(name);
+		await this.newRoomModal.btnCreate.click();
 	}
 }
