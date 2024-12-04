96ed957261014c63efb443500bf7e145ec486d06
juliajforesti <juliajforesti@gmail.com>
2024-12-04 12:53:08 -0300

    fix: `CreateDirectMessage` a11y form to be located by the test fragment

--- apps/meteor/client/sidebar/header/CreateDirectMessage.tsx
+++ apps/meteor/client/sidebar/header/CreateDirectMessage.tsx
@@ -14,10 +14,12 @@ type CreateDirectMessageProps = { onClose: () => void };
 const CreateDirectMessage = ({ onClose }: CreateDirectMessageProps) => {
 	const t = useTranslation();
 	const directMaxUsers = useSetting('DirectMesssage_maxUsers', 1);
-	const membersFieldId = useUniqueId();
 	const dispatchToastMessage = useToastMessageDispatch();
 
 	const createDirectAction = useEndpoint('POST', '/v1/dm.create');
+
+	const membersFieldId = useUniqueId();
+	const createDirectFormId = useUniqueId();
 
 	const {
 		control,
@@ -43,9 +45,12 @@ const CreateDirectMessage = ({ onClose }: CreateDirectMessageProps) => {
 	};
 
 	return (
-		<Modal data-qa='create-direct-modal' wrapperFunction={(props) => <Box is='form' onSubmit={handleSubmit(handleCreate)} {...props} />}>
+		<Modal
+			aria-labelledby={`${createDirectFormId}-title`}
+			wrapperFunction={(props) => <Box is='form' onSubmit={handleSubmit(handleCreate)} {...props} />}
+		>
 			<Modal.Header>
-				<Modal.Title>{t('Create_direct_message')}</Modal.Title>
+				<Modal.Title id={`${createDirectFormId}-title`}>{t('Create_direct_message')}</Modal.Title>
 				<Modal.Close tabIndex={-1} onClick={onClose} />
 			</Modal.Header>
 			<Modal.Content mbe={2}>
