5fc1e7ef54c7e16dbaaece722784f04dd67e85a6
juliajforesti <juliajforesti@gmail.com>
2024-12-04 17:37:26 -0300

    test: replace test fragments

--- apps/meteor/tests/e2e/sidebar.spec.ts
+++ apps/meteor/tests/e2e/sidebar.spec.ts
@@ -14,22 +14,21 @@ test.describe.serial('sidebar', () => {
 	});
 
 	test('should navigate on sidebar toolbar using arrow keys', async ({ page }) => {
-		await poHomeDiscussion.sidenav.userProfileMenu.focus();
-		await page.keyboard.press('Tab');
+		await poHomeDiscussion.navbar.homeButton.focus();
 		await page.keyboard.press('ArrowRight');
 
-		await expect(poHomeDiscussion.sidenav.sidebarToolbar.getByRole('button', { name: 'Search' })).toBeFocused();
+		await expect(poHomeDiscussion.navbar.directoryButton).toBeFocused();
 	});
 
 	test('should navigate on sidebar items using arrow keys and restore focus', async ({ page }) => {
 		// focus should be on the next item
-		await poHomeDiscussion.sidenav.sidebarChannelsList.getByRole('link').first().focus();
+		await poHomeDiscussion.sidebar.channelsList.getByRole('link').first().focus();
 		await page.keyboard.press('ArrowDown');
-		await expect(poHomeDiscussion.sidenav.sidebarChannelsList.getByRole('link').first()).not.toBeFocused();
+		await expect(poHomeDiscussion.sidebar.channelsList.getByRole('link').first()).not.toBeFocused();
 
 		// shouldn't focus the first item
 		await page.keyboard.press('Shift+Tab');
 		await page.keyboard.press('Tab');
-		await expect(poHomeDiscussion.sidenav.sidebarChannelsList.getByRole('link').first()).not.toBeFocused();
+		await expect(poHomeDiscussion.sidebar.channelsList.getByRole('link').first()).not.toBeFocused();
 	});
 });
