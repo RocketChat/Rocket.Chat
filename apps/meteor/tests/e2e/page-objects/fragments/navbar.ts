f6bc3b09de60d912765e4992c7d952137bc77fb4
juliajforesti <juliajforesti@gmail.com>
2024-12-04 12:48:16 -0300

    test: deprecate `home-sidenav` fragments. moved and refactored to `sidebar`, `navbar` and `create-room-modal`

--- apps/meteor/tests/e2e/page-objects/fragments/navbar.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/navbar.ts
@@ -15,7 +15,65 @@ export class Navbar {
 		return this.navbar.getByRole('toolbar', { name: 'Pages' });
 	}
 
+	get omnichannelToolbar(): Locator {
+		return this.navbar.getByRole('toolbar', { name: 'Omnichannel', exact: true });
+	}
+
 	get homeButton(): Locator {
-		return this.pagesToolbar.getByRole('button', { name: 'Home' });
+		return this.pagesToolbar.getByRole('button', { name: 'Home', exact: true });
+	}
+
+	async openInstalledApps(): Promise<void> {
+		await this.pagesToolbar.getByRole('button', { name: 'Marketplace', exact: true }).click();
+		await this.pagesToolbar.getByRole('menu').getByRole('menuitem', { name: 'Installed', exact: true }).click();
+	}
+
+	get settingsGroup(): Locator {
+		return this.navbar.getByRole('group', { name: 'Workspace and user settings', exact: true });
+	}
+
+	get userProfileMenu(): Locator {
+		return this.settingsGroup.getByRole('button', { name: 'User menu', exact: true });
+	}
+
+	get accountProfileOption(): Locator {
+		return this.settingsGroup.getByRole('menuitemcheckbox', { name: 'Profile', exact: true });
+	}
+
+	async openWorkspaceSettings(): Promise<void> {
+		await this.settingsGroup.getByRole('button', { name: 'Workspace', exact: true }).click();
+	}
+
+	async switchStatus(status: 'offline' | 'online'): Promise<void> {
+		await this.userProfileMenu.click();
+		await this.settingsGroup.getByRole('menu').getByRole('menuitemcheckbox', { name: status, exact: true }).click();
+	}
+
+	async switchOmnichannelStatus(status: 'offline' | 'online'): Promise<void> {
+		const toggleButton = this.omnichannelToolbar.getByRole('button', { name: 'answer chats' });
+		expect(toggleButton).toBeVisible();
+
+		enum StatusTitleMap {
+			offline = 'Turn on answer chats',
+			online = 'Turn off answer chats',
+		}
+
+		const currentStatus = await toggleButton.getAttribute('title');
+		if (status === 'offline') {
+			if (currentStatus === StatusTitleMap.online) {
+				await toggleButton.click();
+			}
+		} else if (currentStatus === StatusTitleMap.offline) {
+			await toggleButton.click();
+		}
+		await this.page.waitForTimeout(500);
+
+		const newStatus = await this.omnichannelToolbar.getByRole('button', { name: 'answer chats' }).getAttribute('title');
+		expect(newStatus).toBe(status === 'offline' ? StatusTitleMap.offline : StatusTitleMap.online);
+	}
+
+	async logout(): Promise<void> {
+		await this.userProfileMenu.click();
+		await this.settingsGroup.getByRole('menu').getByRole('menuitemcheckbox', { name: 'Locoug', exact: true }).click();
 	}
 }
