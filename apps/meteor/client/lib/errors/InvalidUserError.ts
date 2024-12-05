8bac5fcdde542f73a131d80c66c3f7a9b2761f3d
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-12-05 11:07:45 -0300

    chore: removed InvalidUserError

--- apps/meteor/client/lib/errors/InvalidUserError.ts
+++
@@ -1,7 +0,0 @@
-import { RocketChatError } from './RocketChatError';
-
-export class InvalidUserError extends RocketChatError<'invalid-user'> {
-	constructor(message = 'Invalid user', details?: unknown) {
-		super('invalid-user', message, details);
-	}
-}
