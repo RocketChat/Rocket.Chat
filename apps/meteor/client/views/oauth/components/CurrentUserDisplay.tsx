82cc72416e4ffdd1073e625d7e02045aa39a1042
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-12-05 17:04:31 -0300

    refactor: updated CurrentUserDisplay

--- apps/meteor/client/views/oauth/components/CurrentUserDisplay.tsx
+++ apps/meteor/client/views/oauth/components/CurrentUserDisplay.tsx
@@ -31,23 +31,23 @@ const CurrentUserDisplay = ({ user }: CurrentUserDisplayProps) => {
 		<>
 			<p>{t('core.You_are_logged_in_as')}</p>
 			<UserCard
-				username={user.username}
-				etag={user.avatarETag}
-				name={showRealNames ? user.name : user.username}
-				nickname={user.nickname}
-				status={<UserStatus.Online />}
-				customStatus={user.statusText ?? <></>}
-				roles={user.roles && getRoles(user.roles).map((role, index) => <UserCardRole key={index}>{role}</UserCardRole>)}
-				localTime={user.utcOffset && Number.isInteger(user.utcOffset) && <LocalTime utcOffset={user.utcOffset} />}
-				bio={
-					user.bio ? (
+				user={{
+					username: user.username,
+					etag: user.avatarETag,
+					name: showRealNames ? user.name : user.username,
+					nickname: user.nickname,
+					status: <UserStatus.Online />,
+					customStatus: user.statusText ?? <></>,
+					roles: user.roles && getRoles(user.roles).map((role, index) => <UserCardRole key={index}>{role}</UserCardRole>),
+					localTime: user.utcOffset && Number.isInteger(user.utcOffset) && <LocalTime utcOffset={user.utcOffset} />,
+					bio: user.bio ? (
 						<UserCardInfo withTruncatedText={false} className={clampStyle} height='x60'>
 							{typeof user.bio === 'string' ? <MarkdownText variant='inline' content={user.bio} /> : user.bio}
 						</UserCardInfo>
 					) : (
 						<></>
-					)
-				}
+					),
+				}}
 			/>
 		</>
 	);
