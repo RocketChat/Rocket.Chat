613ec132de7f5d10bd74bd68fc6811a36b62db5e
juliajforesti <juliajforesti@gmail.com>
2024-12-05 18:14:24 -0300

    feat: use only `ContextualbarV2` in `client/components/Contextualbar`

--- apps/meteor/client/components/Contextualbar/ContextualbarButton.tsx
+++ apps/meteor/client/components/Contextualbar/ContextualbarButton.tsx
@@ -1,17 +1,7 @@
-import { ContextualbarV2Button, ContextualbarButton as ContextualbarButtonComponent } from '@rocket.chat/fuselage';
-import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
+import { ContextualbarV2Button } from '@rocket.chat/fuselage';
 import type { ComponentProps } from 'react';
 import React, { memo } from 'react';
 
-const ContextualbarButton = (props: ComponentProps<typeof ContextualbarButtonComponent>) => (
-	<FeaturePreview feature='newNavigation'>
-		<FeaturePreviewOff>
-			<ContextualbarButtonComponent {...props} />
-		</FeaturePreviewOff>
-		<FeaturePreviewOn>
-			<ContextualbarV2Button {...props} />
-		</FeaturePreviewOn>
-	</FeaturePreview>
-);
+const ContextualbarButton = (props: ComponentProps<typeof ContextualbarV2Button>) => <ContextualbarV2Button {...props} />;
 
 export default memo(ContextualbarButton);
--- apps/meteor/client/components/Contextualbar/ContextualbarContent.tsx
+++ apps/meteor/client/components/Contextualbar/ContextualbarContent.tsx
@@ -1,20 +1,10 @@
-import { ContextualbarV2Content, ContextualbarContent as ContextualbarContentComponent } from '@rocket.chat/fuselage';
-import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
+import { ContextualbarV2Content } from '@rocket.chat/fuselage';
 import type { ComponentProps } from 'react';
 import React, { forwardRef, memo } from 'react';
 
-const ContextualbarContent = forwardRef<HTMLElement, ComponentProps<typeof ContextualbarContentComponent>>(
+const ContextualbarContent = forwardRef<HTMLElement, ComponentProps<typeof ContextualbarV2Content>>(
 	function ContextualbarContent(props, ref) {
-		return (
-			<FeaturePreview feature='newNavigation'>
-				<FeaturePreviewOff>
-					<ContextualbarContentComponent ref={ref} {...props} />
-				</FeaturePreviewOff>
-				<FeaturePreviewOn>
-					<ContextualbarV2Content ref={ref} {...props} />
-				</FeaturePreviewOn>
-			</FeaturePreview>
-		);
+		return <ContextualbarV2Content ref={ref} {...props} />;
 	},
 );
 
