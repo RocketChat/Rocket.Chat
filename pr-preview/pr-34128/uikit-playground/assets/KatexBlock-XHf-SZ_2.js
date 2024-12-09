b658bf8ed1862fbc0b9bb34487ae710614774e26
lucas-a-pelegrino <lucas-a-pelegrino@users.noreply.github.com>
2024-12-09 18:48:39 +0000

    Deploy preview for PR 34128 🛫

---
+++ pr-preview/pr-34128/uikit-playground/assets/KatexBlock-XHf-SZ_2.js
@@ -0,0 +1,1 @@
+import{r as e,j as o}from"./index-DvBTdCVs.js";import{k as a}from"./katex-bPvl3H-i.js";const m=({code:r})=>{const t=e.useMemo(()=>a.renderToString(r,{displayMode:!0,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[r]);return o.jsx("div",{role:"math",style:{overflowX:"auto"},"aria-label":r,dangerouslySetInnerHTML:{__html:t}})};export{m as default};
