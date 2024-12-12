eb794b7fd6bf3ff5a37a38bccaba247ed36db744
Douglas Gubert <douglas.gubert@gmail.com>
2024-12-12 18:14:20 -0300

    fix: app subprocess restart not running onEnable lifecycle (#34172)

--- packages/apps-engine/src/server/AppManager.ts
+++ packages/apps-engine/src/server/AppManager.ts
@@ -1035,6 +1035,10 @@ export class AppManager {
             result = false;
 
             await app.setStatus(status, silenceStatus);
+
+            // If some error has happened in initialization, like license or installations invalidation
+            // we need to store this on the DB regardless of what the parameter requests
+            saveToDb = true;
         }
 
         if (saveToDb) {
@@ -1113,6 +1117,10 @@ export class AppManager {
             }
 
             console.error(e);
+
+            // If some error has happened during enabling, like license or installations invalidation
+            // we need to store this on the DB regardless of what the parameter requests
+            saveToDb = true;
         }
 
         if (enable) {
