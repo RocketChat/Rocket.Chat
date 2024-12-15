dbbe7330ef6328a9491175f93c9a3385a4cfd95d
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2024-12-16 00:16:25 +0530

    lint

--- apps/meteor/client/views/account/preferences/IdleTimeEditor.tsx
+++ apps/meteor/client/views/account/preferences/IdleTimeEditor.tsx
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import { NumberInput, FieldRow, FieldLabel } from '@rocket.chat/fuselage'
+import { NumberInput, FieldRow, FieldLabel } from '@rocket.chat/fuselage';
 import { useUniqueId } from '@rocket.chat/fuselage-hooks';
 import { useTranslation } from '@rocket.chat/ui-contexts';
+import React, { useState, useEffect } from 'react';
 
 type IdleTimeEditorProps = {
-    onChangeTime: (time: number | undefined) => void;
-}
+	onChangeTime: (time: number | undefined) => void;
+};
 
-const IdleTimeEditor = ({onChangeTime}:IdleTimeEditorProps) => {
-    const t = useTranslation();
-    const [hours, setHours] = useState<number>(0)
-    const [minutes, setMinutes] = useState<number>(5)
-    const [seconds, setSeconds] = useState<number>(0)
-    const [finalSecondCount, setFinalSecondCount] = useState<number | undefined>(hours * 3600 + minutes * 60 + seconds)
+const IdleTimeEditor = ({ onChangeTime }: IdleTimeEditorProps) => {
+	const t = useTranslation();
+	const [hours, setHours] = useState<number>(0);
+	const [minutes, setMinutes] = useState<number>(5);
+    const [seconds, setSeconds] = useState<number>(0);
+    const [finalSecondCount, setFinalSecondCount] = useState<number | undefined>(hours * 3600 + minutes * 60 + seconds);
 
     const idleTimeLimitHrs = useUniqueId();
 	const idleTimeLimitMin = useUniqueId();
