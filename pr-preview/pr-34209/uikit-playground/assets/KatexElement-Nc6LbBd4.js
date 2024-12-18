b5bdebca460f1e278896907959bb294f0fba1b1c
juliajforesti <juliajforesti@users.noreply.github.com>
2024-12-18 00:20:02 +0000

    Deploy preview for PR 34209 🛫

---
+++ pr-preview/pr-34209/uikit-playground/assets/KatexElement-Nc6LbBd4.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-DvBTdCVs.js";import{k as o}from"./katex-bPvl3H-i.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
