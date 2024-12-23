40d625eec8f32efcd7621d9b611ff235db116824
Marcos Defendi <marcos.defendi@rocket.chat>
2024-12-23 08:01:11 -0300

    refactor: remove Meteor.call for addAllUserToRoom method (server)

--- apps/meteor/server/methods/addAllUserToRoom.ts
+++ apps/meteor/server/methods/addAllUserToRoom.ts
@@ -19,62 +19,72 @@ declare module '@rocket.chat/ddp-client' {
 	}
 }
 
+export const addAllUserToRoomFn = async (userId: string, rid: IRoom['_id'], activeUsersOnly = false): Promise<true> => {
+	check(rid, String);
+	check(activeUsersOnly, Boolean);
+
+	if (!(await hasPermissionAsync(userId, 'add-all-to-room'))) {
+		throw new Meteor.Error(403, 'Access to Method Forbidden', {
+			method: 'addAllToRoom',
+		});
+	}
+
+	const userFilter: {
+		active?: boolean;
+	} = {};
+	if (activeUsersOnly === true) {
+		userFilter.active = true;
+	}
+
+	const users = await Users.find(userFilter).toArray();
+	if (users.length > settings.get<number>('API_User_Limit')) {
+		throw new Meteor.Error('error-user-limit-exceeded', 'User Limit Exceeded', {
+			method: 'addAllToRoom',
+		});
+	}
+
+	const room = await Rooms.findOneById(rid);
+	if (!room) {
+		throw new Meteor.Error('error-invalid-room', 'Invalid room', {
+			method: 'addAllToRoom',
+		});
+	}
+
+	const now = new Date();
+	for await (const user of users) {
+		const subscription = await Subscriptions.findOneByRoomIdAndUserId(rid, user._id);
+		if (subscription != null) {
+			continue;
+		}
+		await callbacks.run('beforeJoinRoom', user, room);
+		const autoTranslateConfig = getSubscriptionAutotranslateDefaultConfig(user);
+		const { insertedId } = await Subscriptions.createWithRoomAndUser(room, user, {
+			ts: now,
+			open: true,
+			alert: true,
+			unread: 1,
+			userMentions: 1,
+			groupMentions: 0,
+			...autoTranslateConfig,
+			...getDefaultSubscriptionPref(user),
+		});
+		if (insertedId) {
+			void notifyOnSubscriptionChangedById(insertedId, 'inserted');
+		}
+		await Message.saveSystemMessage('uj', rid, user.username || '', user, { ts: now });
+		await callbacks.run('afterJoinRoom', user, room);
+	}
+	return true;
+};
+
 Meteor.methods<ServerMethods>({
 	async addAllUserToRoom(rid, activeUsersOnly = false) {
-		check(rid, String);
-		check(activeUsersOnly, Boolean);
-
-		if (!this.userId || !(await hasPermissionAsync(this.userId, 'add-all-to-room'))) {
+		if (!this.userId) {
 			throw new Meteor.Error(403, 'Access to Method Forbidden', {
 				method: 'addAllToRoom',
 			});
 		}
 
-		const userFilter: {
-			active?: boolean;
-		} = {};
-		if (activeUsersOnly === true) {
-			userFilter.active = true;
-		}
-
-		const users = await Users.find(userFilter).toArray();
-		if (users.length > settings.get<number>('API_User_Limit')) {
-			throw new Meteor.Error('error-user-limit-exceeded', 'User Limit Exceeded', {
-				method: 'addAllToRoom',
-			});
-		}
-
-		const room = await Rooms.findOneById(rid);
-		if (!room) {
-			throw new Meteor.Error('error-invalid-room', 'Invalid room', {
-				method: 'addAllToRoom',
-			});
-		}
-
-		const now = new Date();
-		for await (const user of users) {
-			const subscription = await Subscriptions.findOneByRoomIdAndUserId(rid, user._id);
-			if (subscription != null) {
-				continue;
-			}
-			await callbacks.run('beforeJoinRoom', user, room);
-			const autoTranslateConfig = getSubscriptionAutotranslateDefaultConfig(user);
-			const { insertedId } = await Subscriptions.createWithRoomAndUser(room, user, {
-				ts: now,
-				open: true,
-				alert: true,
-				unread: 1,
-				userMentions: 1,
-				groupMentions: 0,
-				...autoTranslateConfig,
-				...getDefaultSubscriptionPref(user),
-			});
-			if (insertedId) {
-				void notifyOnSubscriptionChangedById(insertedId, 'inserted');
-			}
-			await Message.saveSystemMessage('uj', rid, user.username || '', user, { ts: now });
-			await callbacks.run('afterJoinRoom', user, room);
-		}
-		return true;
+		return addAllUserToRoomFn(this.userId, rid, activeUsersOnly);
 	},
 });
