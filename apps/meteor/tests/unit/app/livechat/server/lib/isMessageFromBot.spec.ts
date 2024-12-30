ed8facd457165efba1f155b3e1b583333aa0cb54
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2024-12-30 16:27:43 -0300

    tests: adds unit test for isMessageFromBot method

---
+++ apps/meteor/tests/unit/app/livechat/server/lib/isMessageFromBot.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import p from 'proxyquire';
+import sinon from 'sinon';
+
+import { createFakeMessage, createFakeUser } from '../../../../../mocks/data';
+
+const modelsMock = {
+	Users: {
+		findOneById: sinon.stub(),
+	},
+};
+
+const { isMessageFromBot } = p.noCallThru().load('../../../../../../app/livechat/server/lib/isMessageFromBot', {
+	'@rocket.chat/models': modelsMock,
+});
+
+describe('isMessageFromBot', () => {
+	const mockUser = createFakeUser({ roles: ['bot'] });
+	const mockMessage = createFakeMessage();
+
+	beforeEach(() => {
+		modelsMock.Users.findOneById.reset();
+	});
+
+	it('Should return true if user has bot role', async () => {
+		modelsMock.Users.findOneById.resolves(mockUser.roles);
+		const result = await isMessageFromBot(mockMessage);
+		expect(result).to.be.true;
+	});
+
+	it('Should return false if user does not have bot role', async () => {
+		mockUser.roles = ['user'];
+		modelsMock.Users.findOneById.resolves(mockUser.roles);
+		const result = await isMessageFromBot(mockMessage);
+		expect(result).to.be.false;
+	});
+});
