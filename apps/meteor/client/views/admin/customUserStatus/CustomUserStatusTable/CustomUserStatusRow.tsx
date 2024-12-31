ab07b1a2e02d5f363a6e8080627d0509696eb698
AyushKumar123456789 <ayushjitendra28@gmail.com>
2024-12-31 12:56:01 +0000

    Add translation to statusType in CustomUserStatusRow

--- apps/meteor/client/views/admin/customUserStatus/CustomUserStatusTable/CustomUserStatusRow.tsx
+++ apps/meteor/client/views/admin/customUserStatus/CustomUserStatusTable/CustomUserStatusRow.tsx
@@ -3,6 +3,7 @@ import type { CSSProperties, ReactElement } from 'react';
 
 import { GenericTableRow, GenericTableCell } from '../../../../components/GenericTable';
 import MarkdownText from '../../../../components/MarkdownText';
+import { useTranslation } from 'react-i18next';
 
 const style: CSSProperties = { whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' };
 
@@ -13,6 +14,7 @@ type CustomUserStatusRowProps = {
 
 const CustomUserStatusRow = ({ status, onClick }: CustomUserStatusRowProps): ReactElement => {
 	const { _id, name, statusType } = status;
+	const { t } = useTranslation();
 
 	return (
 		<GenericTableRow
@@ -28,7 +30,7 @@ const CustomUserStatusRow = ({ status, onClick }: CustomUserStatusRowProps): Rea
 				<MarkdownText content={name} parseEmoji={true} variant='inline' />
 			</GenericTableCell>
 			<GenericTableCell fontScale='p2' color='default' style={style}>
-				{statusType}
+				{t(statusType)}
 			</GenericTableCell>
 		</GenericTableRow>
 	);
