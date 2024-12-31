3893c331dc583d904d3871573913d04f51ee9e25
Tasso <tasso.evangelista@rocket.chat>
2024-12-31 16:31:56 -0300

    Revert lint

--- packages/apps-engine/deno-runtime/lib/sanitizeDeprecatedUsage.ts
+++ packages/apps-engine/deno-runtime/lib/sanitizeDeprecatedUsage.ts
@@ -1,13 +1,14 @@
-import { fixBrokenSynchronousAPICalls } from './ast/mod.ts';
+import { fixBrokenSynchronousAPICalls } from "./ast/mod.ts";
 
 function hasPotentialDeprecatedUsage(source: string) {
     return (
         // potential usage of Room.usernames getter
         source.includes('.usernames') ||
         // potential usage of LivechatRead.isOnline method
-        source.includes('.isOnline(') || // potential usage of LivechatCreator.createToken method
+        source.includes('.isOnline(') ||
+        // potential usage of LivechatCreator.createToken method
         source.includes('.createToken(')
-    );
+    )
 }
 
 export function sanitizeDeprecatedUsage(source: string) {
