96968523ac72ba2ac3913d2e676e447c992021ea
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2025-01-07 17:03:05 +0530

    add Minutes, Seconds in en.i18n file

--- apps/meteor/client/views/account/preferences/IdleTimeEditor.tsx
+++ apps/meteor/client/views/account/preferences/IdleTimeEditor.tsx
@@ -43,9 +43,9 @@ const IdleTimeEditor = ({ onChangeTime }: IdleTimeEditorProps) => {
 		<FieldRow>
 			<FieldLabel htmlFor={idleTimeLimitHrs}>{t('Hours')}</FieldLabel>
 			<NumberInput value={hours} onChange={handleHours} id={idleTimeLimitHrs} min={0}/>
-			<FieldLabel htmlFor={idleTimeLimitMin}>{t('minutes')}</FieldLabel>
+			<FieldLabel htmlFor={idleTimeLimitMin}>{t('Minutes')}</FieldLabel>
 			<NumberInput value={minutes} onChange={handleMinutes} id={idleTimeLimitMin} max={59} min={0} />
-			<FieldLabel htmlFor={idleTimeLimitSec}>{t('seconds')}</FieldLabel>
+			<FieldLabel htmlFor={idleTimeLimitSec}>{t('Seconds')}</FieldLabel>
 			<NumberInput value={seconds} onChange={handleSeconds} id={idleTimeLimitSec} max={59} min={0} />
 		</FieldRow>
 	);
