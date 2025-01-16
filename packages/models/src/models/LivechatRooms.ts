4bbcc60d15bb6c9f48c2e0fd6d13cd0ff10e4fd1
Rafael Tapia <rafael.tapia@rocket.chat>
2025-01-16 16:23:04 -0300

    feat: allow to search by exact term to increase performance

--- packages/models/src/models/LivechatRooms.ts
+++ packages/models/src/models/LivechatRooms.ts
@@ -1276,11 +1276,16 @@ export class LivechatRoomsRaw extends BaseRaw<IOmnichannelRoom> implements ILive
 		options?: { offset?: number; count?: number; sort?: { [k: string]: SortDirection } };
 		extraQuery?: Filter<IOmnichannelRoom>;
 	}) {
+		const isRoomNameExactTerm = roomName?.startsWith(`"`) && roomName?.endsWith(`"`);
+		const roomNameQuery = isRoomNameExactTerm ? roomName?.slice(1, -1) : roomName;
+
 		const query: Filter<IOmnichannelRoom> = {
 			t: 'l',
 			...extraQuery,
 			...(agents && { 'servedBy._id': { $in: agents } }),
-			...(roomName && { fname: new RegExp(escapeRegExp(roomName), 'i') }),
+			...(roomName && isRoomNameExactTerm
+				? { fname: roomNameQuery } // exact match
+				: roomName && { fname: new RegExp(escapeRegExp(roomName), 'i') }), // regex match
 			...(departmentId && departmentId !== 'undefined' && { departmentId }),
 			...(open !== undefined && { open: { $exists: open }, onHold: { $ne: true } }),
 			...(served !== undefined && { servedBy: { $exists: served } }),
