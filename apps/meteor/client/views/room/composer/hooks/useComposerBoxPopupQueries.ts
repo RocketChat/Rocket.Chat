5804fde5cf6e8c841ea9ddb4e782ff377a2658c8
Tiago Evangelista <tiago.evangelista@rocket.chat>
2025-01-21 20:29:25 -0300

    fix

--- apps/meteor/client/views/room/composer/hooks/useComposerBoxPopupQueries.ts
+++ apps/meteor/client/views/room/composer/hooks/useComposerBoxPopupQueries.ts
@@ -1,18 +1,11 @@
 import type { QueriesResults } from '@tanstack/react-query';
 import { keepPreviousData, useQueries } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
 
 import { useEnablePopupPreview } from './useEnablePopupPreview';
 import { slashCommands } from '../../../../../app/utils/client/slashCommand';
 import type { ComposerPopupOption } from '../../contexts/ComposerPopupContext';
 
 export const useComposerBoxPopupQueries = <T extends { _id: string; sort?: number }>(filter: unknown, popup?: ComposerPopupOption<T>) => {
-	const [counter, setCounter] = useState(0);
-
-	useEffect(() => {
-		setCounter(0);
-	}, [popup, filter]);
-
 	const shouldPopupPreview = useEnablePopupPreview(filter, popup);
 
 	const enableQuery =
@@ -22,28 +15,27 @@ export const useComposerBoxPopupQueries = <T extends { _id: string; sort?: numbe
 			slashCommands.commands[(filter as any)?.cmd].providesPreview) ||
 		shouldPopupPreview;
 
+	const fetchData = async (source: 'local' | 'server') => {
+		if (source === 'local' && popup?.getItemsFromLocal) {
+			return popup.getItemsFromLocal(filter);
+		}
+
+		if (source === 'server' && popup?.getItemsFromServer) {
+			return popup.getItemsFromServer(filter);
+		}
+
+		return [];
+	};
+
 	return {
 		queries: useQueries({
-			queries: [
-				popup?.getItemsFromLocal && {
-					placeholderData: keepPreviousData,
-					queryKey: ['message-popup', 'local', filter, popup],
-					queryFn: () => popup?.getItemsFromLocal && popup.getItemsFromLocal(filter),
-					onSuccess: (args: T[]) => {
-						if (args.length < 5) {
-							setCounter(1);
-						}
-					},
-					enabled: enableQuery,
-				},
-				popup?.getItemsFromServer && {
-					placeholderData: keepPreviousData,
-					queryKey: ['message-popup', 'server', filter, popup],
-					queryFn: () => popup?.getItemsFromServer && popup.getItemsFromServer(filter),
-					enabled: counter > 0,
-				},
-			].filter(Boolean) as any,
+			queries: (['local', 'server'] as const).map((source) => ({
+				queryKey: ['message-popup', source, filter, popup],
+				queryFn: () => fetchData(source),
+				enabled: enableQuery,
+			})),
 		}) as QueriesResults<T[]>,
 		suspended: !enableQuery,
+		placeholderData: keepPreviousData,
 	};
 };
