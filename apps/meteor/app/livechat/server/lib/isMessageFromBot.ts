4221773ada0c24d22ad9e045978310f032fed941
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-01-24 13:08:56 -0300

    refactor: replaces Users.findOne for Users.isUserInRole

--- apps/meteor/app/livechat/server/lib/isMessageFromBot.ts
+++ apps/meteor/app/livechat/server/lib/isMessageFromBot.ts
@@ -1,12 +1,6 @@
-import type { IMessage, IUser } from '@rocket.chat/core-typings';
+import type { IMessage } from '@rocket.chat/core-typings';
 import { Users } from '@rocket.chat/models';
 
 export async function isMessageFromBot(message: IMessage): Promise<boolean> {
-	const user = await Users.findOneById<Pick<IUser, 'roles'>>(message.u._id, { projection: { roles: 1 } });
-
-	if (!user) {
-		throw new Error('User not found');
-	}
-
-	return !!user.roles.includes('bot');
+	return Users.isUserInRole(message.u._id, 'bot');
 }
--- apps/meteor/tests/unit/app/livechat/server/lib/isMessageFromBot.spec.ts
+++ apps/meteor/tests/unit/app/livechat/server/lib/isMessageFromBot.spec.ts
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import p from 'proxyquire';
 import sinon from 'sinon';
 
-import { createFakeMessage, createFakeUser } from '../../../../../mocks/data';
+import { createFakeMessage } from '../../../../../mocks/data';
 
 const modelsMock = {
 	Users: {
-		findOneById: sinon.stub(),
+		isUserInRole: sinon.stub(),
 	},
 };
 
@@ -15,29 +15,21 @@ const { isMessageFromBot } = p.noCallThru().load('../../../../../../app/livechat
 });
 
 describe('isMessageFromBot', () => {
-	const mockUser = createFakeUser({ roles: ['bot'] });
 	const mockMessage = createFakeMessage();
 
 	beforeEach(() => {
-		modelsMock.Users.findOneById.reset();
+		modelsMock.Users.isUserInRole.reset();
 	});
 
 	it('Should return true if user has bot role', async () => {
-		modelsMock.Users.findOneById.resolves(mockUser);
+		modelsMock.Users.isUserInRole.resolves(true);
 		const result = await isMessageFromBot(mockMessage);
 		expect(result).to.be.true;
 	});
 
 	it('Should return false if user does not have bot role', async () => {
-		mockUser.roles = ['user'];
-		modelsMock.Users.findOneById.resolves(mockUser);
+		modelsMock.Users.isUserInRole.resolves(false);
 		const result = await isMessageFromBot(mockMessage);
 		expect(result).to.be.false;
-	});
-
-	it('Should throw error when user is not found', async () => {
-		modelsMock.Users.findOneById.resolves(null);
-
-		await expect(isMessageFromBot(mockMessage)).to.be.rejectedWith(Error, 'User not found');
 	});
 });
