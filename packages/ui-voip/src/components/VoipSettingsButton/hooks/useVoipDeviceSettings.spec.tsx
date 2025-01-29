8e2c989105bdb5b186412bff3de09415799f0615
Tasso Evangelista <tasso.evangelista@rocket.chat>
2025-01-29 00:22:33 -0300

    chore: Upgrade React (#32288)

--- packages/ui-voip/src/components/VoipSettingsButton/hooks/useVoipDeviceSettings.spec.tsx
+++ packages/ui-voip/src/components/VoipSettingsButton/hooks/useVoipDeviceSettings.spec.tsx
@@ -6,7 +6,6 @@ import { useVoipDeviceSettings } from './useVoipDeviceSettings';
 it('should be disabled when there are no devices', () => {
 	const { result } = renderHook(() => useVoipDeviceSettings(), {
 		wrapper: mockAppRoot().build(),
-		legacyRoot: true,
 	});
 
 	expect(result.current.title).toBe('Device_settings_not_supported_by_browser');
@@ -19,7 +18,6 @@ it('should be enabled when there are devices', () => {
 			.withAudioInputDevices([{ type: '', id: '', label: '' }])
 			.withAudioOutputDevices([{ type: '', id: '', label: '' }])
 			.build(),
-		legacyRoot: true,
 	});
 
 	expect(result.current.title).toBe('Device_settings');
