8e78778bb2073ab97c9e72007f3e3a0a79db42c0
dougfabris <devfabris@gmail.com>
2025-01-29 10:15:42 -0300

    chore: mock utils

--- apps/meteor/client/views/room/contextualBar/VideoConference/VideoConfPopups/VideoConfPopups.spec.tsx
+++ apps/meteor/client/views/room/contextualBar/VideoConference/VideoConfPopups/VideoConfPopups.spec.tsx
@@ -2,16 +2,12 @@ import { mockAppRoot } from '@rocket.chat/mock-providers';
 import { render, screen } from '@testing-library/react';
 
 import VideoConfPopups from './VideoConfPopups';
-import { createFakeVideoConfCall, createFakeRoom } from '../../../../../../tests/mocks/data';
+import { createFakeRoom } from '../../../../../../tests/mocks/data';
+import { createFakeVideoConfCall, createFakeIncomingCall } from '../../../../../../tests/mocks/utils/video-conference';
 
 const fakeRoom = createFakeRoom({ t: 'd' });
-const fakeDirectVideoConfCall = createFakeVideoConfCall({ type: 'direct' });
-const fakeIncomingCall = {
-	uid: '123',
-	rid: '123',
-	callId: '123',
-	dismissed: false,
-};
+const fakeDirectVideoConfCall = createFakeVideoConfCall({ type: 'direct', rid: fakeRoom._id });
+const fakeIncomingCall = createFakeIncomingCall({ rid: fakeRoom._id });
 
 test('should render video conference incoming popup', async () => {
 	render(<VideoConfPopups />, {
