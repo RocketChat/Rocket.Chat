c12b4970bcf750d52496786e4c5c574d5cb46f53
Diego Sampaio <chinello@gmail.com>
2025-01-31 13:44:56 -0300

    handler CORS only on OPTIONS

--- apps/meteor/app/api/server/middlewares/cors.spec.ts
+++ apps/meteor/app/api/server/middlewares/cors.spec.ts
@@ -7,7 +7,7 @@ import { CachedSettings } from '../../../settings/server/CachedSettings';
 import { Router } from '../router';
 
 describe('Cors middleware', () => {
-	it('should handle CORS if enabled to *', async () => {
+	it('should not enforce CORS headers for GET', async () => {
 		const ajv = new Ajv();
 		const app = express();
 		const api = new Router('/api');
@@ -50,6 +50,101 @@ describe('Cors middleware', () => {
 
 		expect(response.statusCode).toBe(200);
 		expect(response.body).toHaveProperty('message', 'CORS test successful');
+		expect(response.headers).not.toHaveProperty('access-control-allow-origin', '*');
+		expect(response.headers).toHaveProperty('access-control-allow-methods', 'GET, POST, PUT, DELETE, HEAD, PATCH');
+		expect(response.headers).toHaveProperty(
+			'access-control-allow-headers',
+			'Origin, X-Requested-With, Content-Type, Accept, X-User-Id, X-Auth-Token, x-visitor-token, Authorization',
+		);
+	});
+
+	it('should not return CORS headers for GET if CORS disabled', async () => {
+		const ajv = new Ajv();
+		const app = express();
+		const api = new Router('/api');
+		const settings = new CachedSettings();
+
+		settings.set({
+			_id: 'API_Enable_CORS',
+			value: false,
+		} as any);
+
+		api.use(cors(settings)).get(
+			'/test',
+			{
+				response: {
+					200: ajv.compile({
+						type: 'object',
+						properties: {
+							message: { type: 'string' },
+						},
+					}),
+				},
+			},
+			async () => {
+				return {
+					statusCode: 200,
+					body: {
+						message: 'CORS test successful',
+					},
+				};
+			},
+		);
+
+		app.use(api.router);
+
+		const response = await request(app).get('/api/test').set('origin', 'http://localhost');
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toHaveProperty('message', 'CORS test successful');
+		expect(response.headers).not.toHaveProperty('access-control-allow-origin');
+		expect(response.headers).not.toHaveProperty('access-control-allow-methods');
+		expect(response.headers).not.toHaveProperty('access-control-allow-headers');
+	});
+
+	it('should handle CORS if enabled to *', async () => {
+		const ajv = new Ajv();
+		const app = express();
+		const api = new Router('/api');
+		const settings = new CachedSettings();
+
+		settings.set({
+			_id: 'API_Enable_CORS',
+			value: true,
+		} as any);
+		settings.set({
+			_id: 'API_CORS_Origin',
+			value: '*',
+		} as any);
+
+		api.use(cors(settings)).get(
+			'/test',
+			{
+				response: {
+					200: ajv.compile({
+						type: 'object',
+						properties: {
+							message: { type: 'string' },
+						},
+					}),
+				},
+			},
+			async () => {
+				return {
+					statusCode: 200,
+					body: {
+						message: 'CORS test successful',
+					},
+				};
+			},
+		);
+
+		app.use(api.router);
+
+		const response = await request(app).options('/api/test').set('origin', 'http://localhost');
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body).not.toHaveProperty('message', 'CORS test successful');
 		expect(response.headers).toHaveProperty('access-control-allow-origin', '*');
 		expect(response.headers).toHaveProperty('access-control-allow-methods', 'GET, POST, PUT, DELETE, HEAD, PATCH');
 		expect(response.headers).toHaveProperty(
@@ -97,10 +192,10 @@ describe('Cors middleware', () => {
 
 		app.use(api.router);
 
-		const response = await request(app).get('/api/test').set('origin', 'http://localhost');
+		const response = await request(app).options('/api/test').set('origin', 'http://localhost');
 
 		expect(response.statusCode).toBe(200);
-		expect(response.body).toHaveProperty('message', 'CORS test successful');
+		expect(response.body).not.toHaveProperty('message', 'CORS test successful');
 		expect(response.headers).toHaveProperty('access-control-allow-origin', 'http://localhost');
 		expect(response.headers).toHaveProperty('access-control-allow-methods', 'GET, POST, PUT, DELETE, HEAD, PATCH');
 		expect(response.headers).toHaveProperty(
@@ -148,7 +243,7 @@ describe('Cors middleware', () => {
 
 		app.use(api.router);
 
-		const response = await request(app).get('/api/test').set('origin', 'http://example.com');
+		const response = await request(app).options('/api/test').set('origin', 'http://example.com');
 
 		expect(response.statusCode).toBe(403);
 	});
@@ -188,7 +283,7 @@ describe('Cors middleware', () => {
 
 		app.use(api.router);
 
-		const response = await request(app).get('/api/test').set('origin', 'http://localhost');
+		const response = await request(app).options('/api/test').set('origin', 'http://localhost');
 
 		expect(response.statusCode).toBe(405);
 		expect(response.text).toBe('CORS not enabled. Go to "Admin > General > REST Api" to enable it.');
--- apps/meteor/app/api/server/middlewares/cors.ts
+++ apps/meteor/app/api/server/middlewares/cors.ts
@@ -2,7 +2,23 @@ import type { NextFunction, Request, Response } from 'express';
 
 import type { CachedSettings } from '../../../settings/server/CachedSettings';
 
+const defaultHeaders = {
+	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, HEAD, PATCH',
+	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-User-Id, X-Auth-Token, x-visitor-token, Authorization',
+};
+
 export const cors = (settings: CachedSettings) => (req: Request, res: Response, next: NextFunction) => {
+	if (req.method !== 'OPTIONS') {
+		if (settings.get('API_Enable_CORS')) {
+			res.setHeader('Vary', 'Origin');
+			res.setHeader('Access-Control-Allow-Methods', defaultHeaders['Access-Control-Allow-Methods']);
+			res.setHeader('Access-Control-Allow-Headers', defaultHeaders['Access-Control-Allow-Headers']);
+		}
+
+		next();
+		return;
+	}
+
 	// check if a pre-flight request
 	if (!req.headers['access-control-request-method'] && !req.headers.origin) {
 		next();
@@ -17,12 +33,6 @@ export const cors = (settings: CachedSettings) => (req: Request, res: Response, 
 	}
 
 	const CORSOriginSetting = String(settings.get('API_CORS_Origin'));
-
-	const defaultHeaders = {
-		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, HEAD, PATCH',
-		'Access-Control-Allow-Headers':
-			'Origin, X-Requested-With, Content-Type, Accept, X-User-Id, X-Auth-Token, x-visitor-token, Authorization',
-	};
 
 	if (CORSOriginSetting === '*') {
 		res.setHeader('Access-Control-Allow-Origin', '*');
