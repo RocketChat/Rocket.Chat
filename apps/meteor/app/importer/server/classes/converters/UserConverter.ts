a055b05a47ece151953996cd59e3df68b58cf95a
Rafael Tapia <rafael.tapia@rocket.chat>
2025-02-12 17:01:04 -0300

    feat: check license before inserting a new user

--- apps/meteor/app/importer/server/classes/converters/UserConverter.ts
+++ apps/meteor/app/importer/server/classes/converters/UserConverter.ts
@@ -1,4 +1,5 @@
 import type { IImportUser, IImportUserRecord, IUser, IUserEmail } from '@rocket.chat/core-typings';
+import { License } from '@rocket.chat/license';
 import { Users } from '@rocket.chat/models';
 import { Random } from '@rocket.chat/random';
 import { SHA256 } from '@rocket.chat/sha256';
@@ -50,8 +51,23 @@ export class UserConverter extends RecordConverter<IImportUserRecord, UserConver
 			return;
 		}
 
+		if (!existingUser) {
+			if (await this.willExceedLicenseLimit()) {
+				this._logger?.warn(`License limit exceeded: User ${data.username || data.emails[0]} will not be created.`);
+				await this.skipRecord(_id);
+				return;
+			}
+		}
+
 		await this.insertOrUpdateUser(existingUser, data);
 		return !existingUser;
+	}
+
+	private async willExceedLicenseLimit(): Promise<boolean> {
+		const currentUserCount = await Users.getActiveLocalUserCount();
+		const licenseLimit = License.getMaxActiveUsers();
+
+		return currentUserCount + 1 > licenseLimit;
 	}
 
 	async convertData(userCallbacks: IConversionCallbacks = {}): Promise<void> {
