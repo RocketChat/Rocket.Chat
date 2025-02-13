2da08a9b5351139647d159a268c22f28c952df21
gabriellsh <henriques.gabriell@gmail.com>
2025-02-13 15:59:01 -0300

    update saveCustomFields

--- apps/meteor/app/lib/server/functions/saveCustomFields.ts
+++ apps/meteor/app/lib/server/functions/saveCustomFields.ts
@@ -1,16 +1,13 @@
-import type { IUser } from '@rocket.chat/core-typings';
-import type { Updater } from '@rocket.chat/models';
-
 import { saveCustomFieldsWithoutValidation } from './saveCustomFieldsWithoutValidation';
 import { validateCustomFields } from './validateCustomFields';
 import { trim } from '../../../../lib/utils/stringUtils';
 import { settings } from '../../../settings/server';
 
-export const saveCustomFields = async function (userId: string, formData: Record<string, any>, updater?: Updater<IUser>): Promise<void> {
+export const saveCustomFields = async function (userId: string, formData: Record<string, any>): Promise<void> {
 	if (trim(settings.get('Accounts_CustomFields')).length === 0) {
 		return;
 	}
 
 	validateCustomFields(formData);
-	return saveCustomFieldsWithoutValidation(userId, formData, updater);
+	return saveCustomFieldsWithoutValidation(userId, formData);
 };
