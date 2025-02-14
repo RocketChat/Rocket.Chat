a9956c04d8f789b030bb3fef52bb2326949761b6
Martin Schoeler <martin.schoeler@rocket.chat>
2025-02-14 13:57:43 -0300

    refactor: remove meteor from drupal auth (#34991)
    Co-authored-by: Douglas Fabris <27704687+dougfabris@users.noreply.github.com>

--- apps/meteor/client/views/root/AppLayout.tsx
+++ apps/meteor/client/views/root/AppLayout.tsx
@@ -7,6 +7,7 @@ import { useGoogleTagManager } from './hooks/useGoogleTagManager';
 import { useMessageLinkClicks } from './hooks/useMessageLinkClicks';
 import { useSettingsOnLoadSiteUrl } from './hooks/useSettingsOnLoadSiteUrl';
 import { useAnalytics } from '../../../app/analytics/client/loadScript';
+import { useDrupal } from '../../../app/drupal/client/hooks/useDrupal';
 import { useGitHubEnterpriseAuth } from '../../../app/github-enterprise/client/hooks/useGitHubEnterpriseAuth';
 import { useGitLabAuth } from '../../../app/gitlab/client/hooks/useGitLabAuth';
 import { useLivechatEnterprise } from '../../../app/livechat-enterprise/hooks/useLivechatEnterprise';
@@ -39,6 +40,7 @@ const AppLayout = () => {
 	useNextcloud();
 	useGitLabAuth();
 	useGitHubEnterpriseAuth();
+	useDrupal();
 
 	const layout = useSyncExternalStore(appLayout.subscribe, appLayout.getSnapshot);
 
