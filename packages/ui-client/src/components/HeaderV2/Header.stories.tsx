23f282b0b6a5293f1f9e76d253b8d3c609d9a309
MartinSchoeler <martinschoeler8@gmail.com>
2025-02-18 15:07:32 -0300

    ci

--- packages/ui-client/src/components/HeaderV2/Header.stories.tsx
+++ packages/ui-client/src/components/HeaderV2/Header.stories.tsx
@@ -61,6 +61,7 @@ export default {
 						}),
 					],
 					querySettings: () => [() => () => undefined, () => []],
+					countSettings: () => 0,
 					dispatch: async () => undefined,
 				}}
 			>
