bab891264a548de43231165c98bf6ead7802da86
gabriellsh <henriques.gabriell@gmail.com>
2025-02-26 10:35:09 -0300

    unregister permission listener when unmounting

--- apps/meteor/client/views/room/composer/messageBox/hooks/useMediaPermissions.ts
+++ apps/meteor/client/views/room/composer/messageBox/hooks/useMediaPermissions.ts
@@ -15,15 +15,19 @@ const getDeviceKind = (name: MediaDevices): MediaDeviceKind => {
 export const useMediaPermissions = (name: MediaDevices): [isPermissionDenied: boolean, setIsPermissionDenied: (state: boolean) => void] => {
 	const [isPermissionDenied, setIsPermissionDenied] = useState(false);
 
-	const handleMount = useEffectEvent(async (): Promise<void> => {
+	const handleMount = useEffectEvent(async (): Promise<(() => void) | void> => {
 		if (navigator.permissions) {
 			try {
 				const permissionStatus = await navigator.permissions.query({ name: name as PermissionName });
 				setIsPermissionDenied(permissionStatus.state === 'denied');
-				permissionStatus.onchange = (): void => {
-					setIsPermissionDenied(permissionStatus.state === 'denied');
-				};
-				return;
+
+				// We need this to be a function declaration so that we can use `this` parameter
+				// eslint-disable-next-line no-inner-declarations
+				function permissionChangeEvent(this: PermissionStatus) {
+					setIsPermissionDenied(this.state === 'denied');
+				}
+				permissionStatus.addEventListener('change', permissionChangeEvent);
+				return () => permissionStatus.removeEventListener('change', permissionChangeEvent);
 			} catch (error) {
 				console.warn(error);
 			}
@@ -44,7 +48,15 @@ export const useMediaPermissions = (name: MediaDevices): [isPermissionDenied: bo
 	});
 
 	useEffect(() => {
-		handleMount();
+		let offCallback;
+
+		const mount = async () => {
+			offCallback = await handleMount();
+		};
+
+		mount();
+
+		return offCallback;
 	}, [handleMount]);
 
 	return [isPermissionDenied, setIsPermissionDenied];
