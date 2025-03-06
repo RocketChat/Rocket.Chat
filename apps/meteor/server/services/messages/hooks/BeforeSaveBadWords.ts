ab2d91e616aa7d7d2279085e086fb31bb4db965e
yash-rajpal <rajpal.yash03@gmail.com>
2025-03-06 22:25:28 +0530

    fix: filter non ascii bad words using regex matching

--- apps/meteor/server/services/messages/hooks/BeforeSaveBadWords.ts
+++ apps/meteor/server/services/messages/hooks/BeforeSaveBadWords.ts
@@ -4,15 +4,18 @@ import type BadWordsFilter from 'bad-words';
 export class BeforeSaveBadWords {
 	badWords: BadWordsFilter | null = null;
 
+	badWordsRegex: RegExp | null = null;
+
 	async configure(badWordsList?: string, goodWordsList?: string) {
 		const { default: Filter } = await import('bad-words');
+		const badWords =
+			badWordsList
+				?.split(',')
+				.map((word) => word.trim())
+				.filter(Boolean) || [];
 
 		const options = {
-			list:
-				badWordsList
-					?.split(',')
-					.map((word) => word.trim())
-					.filter(Boolean) || [],
+			list: badWords,
 			// library definition does not allow optional definition
 			exclude: undefined,
 			splitRegex: undefined,
@@ -23,6 +26,7 @@ export class BeforeSaveBadWords {
 		};
 
 		this.badWords = new Filter(options);
+		this.badWordsRegex = new RegExp(`(?<=^|[\\p{P}\\p{Z}])(${badWords.join('|')})(?=$|[\\p{P}\\p{Z}])`, 'gmiu');
 
 		if (goodWordsList?.length) {
 			this.badWords.removeWords(...goodWordsList.split(',').map((word) => word.trim()));
@@ -31,10 +35,11 @@ export class BeforeSaveBadWords {
 
 	disable() {
 		this.badWords = null;
+		this.badWordsRegex = null;
 	}
 
 	async filterBadWords({ message }: { message: IMessage }): Promise<IMessage> {
-		if (!message.msg || !this.badWords) {
+		if (!message.msg || !this.badWords || !this.badWordsRegex) {
 			return message;
 		}
 
@@ -42,6 +47,8 @@ export class BeforeSaveBadWords {
 			message.msg = this.badWords.clean(message.msg);
 		} catch (error) {
 			// ignore
+		} finally {
+			message.msg = message.msg.replace(this.badWordsRegex, (match) => '*'.repeat(match.length));
 		}
 
 		return message;
