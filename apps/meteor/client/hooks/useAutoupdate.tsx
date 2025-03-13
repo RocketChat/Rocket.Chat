850f8a018032d41f6cb53705addeeca9de3df8bd
juliajforesti <juliajforesti@gmail.com>
2025-03-13 09:42:46 -0300

    chore: `useAutoupdate`

--- apps/meteor/client/hooks/useAutoupdate.tsx
+++ apps/meteor/client/hooks/useAutoupdate.tsx
@@ -1,14 +1,37 @@
+import { css } from '@rocket.chat/css-in-js';
+import { Box, Button } from '@rocket.chat/fuselage';
+import { useToastMessageDispatch } from '@rocket.chat/ui-contexts';
 import { useEffect } from 'react';
 
 export const useAutoupdate = () => {
+	const toast = useToastMessageDispatch();
+
 	useEffect(() => {
 		const fn = () => {
-			console.log('---client_changed');
+			toast({
+				type: 'info',
+				title: 'Update available',
+				message: (
+					<Box
+						display='flex'
+						flexDirection='column'
+						className={css`
+							gap: 8px;
+						`}
+					>
+						An update is available
+						<Button small onClick={() => window.location.reload()}>
+							Reload to update
+						</Button>
+					</Box>
+				),
+				options: { position: 'bottom-end', isPersistent: true },
+			});
 		};
 		document.addEventListener('client_changed', fn);
 
 		return () => {
 			document.removeEventListener('client_changed', fn);
 		};
-	}, []);
+	}, [toast]);
 };
