289603ae06d6b1f91323a59071882e5823b989b9
priyanshu <priyanshuqpwp@gmail.com>
2025-03-14 01:19:26 +0530

    Css done inline except buttons

--- apps/meteor/client/components/NotFoundState.tsx
+++ apps/meteor/client/components/NotFoundState.tsx
@@ -1,30 +1,84 @@
-import { Box, States, StatesAction, StatesActions, StatesIcon, StatesSubtitle, StatesTitle } from '@rocket.chat/fuselage';
+import { Box, States, StatesAction, StatesActions, StatesSubtitle, StatesTitle } from '@rocket.chat/fuselage';
 import { useRouter } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type NotFoundProps = {
-	title: string;
-	subtitle: string;
+	title?: string;
+	subtitle?: string;
 };
 
 const NotFoundState = ({ title, subtitle }: NotFoundProps): ReactElement => {
 	const { t } = useTranslation();
 	const router = useRouter();
 
+	const displayTitle = title || '404';
+	const displaySubtitle = subtitle || "The page you're looking for doesn't exist or has been moved.";
+
 	const handleGoHomeClick = () => {
 		router.navigate('/home');
 	};
 
+	// const handleGoBackClick = () => {
+	// 	router.navigate(-1);
+	// };
+
 	return (
-		<Box display='flex' justifyContent='center' height='full'>
+		<Box style={{ marginBlockStart: '2rem' }} display='flex' justifyContent='center' alignItems='center' height='100vh' bg='neutral-100'>
 			<States>
-				<StatesIcon name='magnifier' />
-				<StatesTitle>{title}</StatesTitle>
-				<StatesSubtitle>{subtitle}</StatesSubtitle>
-				<Box mbs={16}>
+				<div
+					style={{
+						fontSize: '7.5rem',
+						fontWeight: 700,
+						lineHeight: 1,
+						marginBottom: '1.5rem',
+						color: '#6c757d',
+					}}
+				>
+					404
+				</div>
+				<StatesTitle
+					style={{
+						fontSize: '1.9rem',
+						marginBottom: '1rem',
+						color: '#2F343D',
+					}}
+				>
+					{displayTitle}
+				</StatesTitle>
+				<StatesSubtitle
+					style={{
+						fontSize: '1.2rem',
+						marginBottom: '3rem',
+						color: '#6c757d',
+					}}
+				>
+					{displaySubtitle}
+				</StatesSubtitle>
+				<Box mbs={26}>
 					<StatesActions>
-						<StatesAction onClick={handleGoHomeClick}>{t('Homepage')}</StatesAction>
+						{/* <StatesAction
+							// bg='neutral-100'
+							// color={blue}
+							style={{
+								// color: 'blue',
+								// bg: 'white',
+								width: '100%',
+								maxWidth: '10rem', // Adjust as needed
+							}}
+							onClick={handleGoBackClick}
+						>
+							{t('Back')}
+						</StatesAction> */}
+						<StatesAction
+							// style={{
+							// 	width: '100%',
+							// 	maxWidth: '10rem', // Adjust as needed
+							// }}
+							onClick={handleGoHomeClick}
+						>
+							{t('Homepage')}
+						</StatesAction>
 					</StatesActions>
 				</Box>
 			</States>
