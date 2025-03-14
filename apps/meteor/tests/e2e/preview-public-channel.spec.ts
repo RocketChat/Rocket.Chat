d3be1a01a7ae89cc03c502ac1f32dd557a067b76
Tiago Evangelista <tiago.evangelista@rocket.chat>
2025-03-14 17:38:14 -0300

    improve and fix tests

--- apps/meteor/tests/e2e/preview-public-channel.spec.ts
+++ apps/meteor/tests/e2e/preview-public-channel.spec.ts
@@ -1,56 +1,78 @@
 import { Users } from './fixtures/userStates';
 import { HomeChannel, Utils } from './page-objects';
 import { Directory } from './page-objects/directory';
-import { createTargetChannel } from './utils';
+import { createTargetChannel, sendTargetChannelMessage } from './utils';
 import { test, expect } from './utils/test';
 
-test.use({ storageState: Users.user1.state });
+test.use({ storageState: Users.admin.state });
 
 test.describe('Preview public channel', () => {
 	let poHomeChannel: HomeChannel;
 	let poDirectory: Directory;
 	let poUtils: Utils;
 	let targetChannel: string;
+	let targetChannelMessage: string;
 
-	test.beforeEach(async ({ page, api }) => {
+	test.beforeEach(async ({ page }) => {
 		poHomeChannel = new HomeChannel(page);
-		targetChannel = await createTargetChannel(api);
 		poDirectory = new Directory(page);
 		poUtils = new Utils(page);
 
 		await page.goto('/home');
 	});
 
-	test.afterEach(async ({ api }) => {
-		await api.post('/channels.delete', { roomName: targetChannel });
+	test.beforeAll(async ({ api }) => {
+		targetChannel = await createTargetChannel(api);
+		targetChannelMessage = await sendTargetChannelMessage(api, targetChannel);
 	});
 
-	test.describe('without preview public room permission', () => {
-		test.beforeAll(async ({ api }) => {
-			await api.post('/permissions.update', { permissions: [{ _id: 'preview-c-room', roles: ['admin'] }] });
-		});
+	test.afterAll(async ({ api }) => {
+		await api.post('/channels.delete', { roomName: targetChannel });
+		await api.post('/permissions.update', { permissions: [{ _id: 'preview-c-room', roles: ['admin', 'user', 'anonymous'] }] });
+	});
 
-		test.afterAll(async ({ api }) => {
+	test('should let user preview public rooms messages', async () => {
+		await poHomeChannel.sidenav.openDirectory();
+		await poDirectory.openChannel(targetChannel);
+
+		await expect(poHomeChannel.content.lastUserMessageBody).toContainText(targetChannelMessage);
+	});
+
+	test('should not let user role preview public rooms', async ({ api }) => {
+		await api.post('/permissions.update', { permissions: [{ _id: 'preview-c-room', roles: ['admin'] }] });
+
+		await poHomeChannel.sidenav.openDirectory();
+		await poDirectory.openChannel(targetChannel);
+
+		await expect(poHomeChannel.content.btnJoinChannel).toBeVisible();
+		await expect(poHomeChannel.content.lastUserMessageBody).not.toBeVisible();
+	});
+
+	test.describe('App', () => {
+		test.use({ storageState: Users.userNotAllowedByApp.state });
+
+		test('should prevent user from join the room', async ({ api }) => {
 			await api.post('/permissions.update', { permissions: [{ _id: 'preview-c-room', roles: ['admin', 'user', 'anonymous'] }] });
-		});
-
-		test('should not let user role preview public rooms', async () => {
 			await poHomeChannel.sidenav.openDirectory();
 			await poDirectory.openChannel(targetChannel);
 
-			expect(poHomeChannel.content.btnJoinChannel).toBeVisible;
+			await expect(poHomeChannel.content.lastUserMessageBody).toContainText(targetChannelMessage);
+
+			await poHomeChannel.btnJoinRoom.click();
+
+			await expect(poUtils.getAlertByText('TEST OF NOT ALLOWED USER')).not.toBeVisible();
 		});
 
-		test.describe('apps', () => {
-			test.use({ storageState: Users.userNotAllowedByApp.state });
+		test('should prevent user from join the room without preview permission', async ({ api }) => {
+			await api.post('/permissions.update', { permissions: [{ _id: 'preview-c-room', roles: ['admin'] }] });
 
-			test('should prevent user from join the room', async () => {
-				await poHomeChannel.sidenav.openDirectory();
-				await poDirectory.openChannel(targetChannel);
-				await poHomeChannel.content.btnJoinChannel.click();
+			await poHomeChannel.sidenav.openDirectory();
+			await poDirectory.openChannel(targetChannel);
+			await expect(poHomeChannel.content.lastUserMessageBody).not.toBeVisible();
 
-				expect(poUtils.getAlertByText('TEST OF NOT ALLOWED USER')).toBeVisible;
-			});
+			await poHomeChannel.content.btnJoinChannel.click();
+
+			await expect(poUtils.getAlertByText('TEST OF NOT ALLOWED USER')).toBeVisible();
 		});
 	});
 });
