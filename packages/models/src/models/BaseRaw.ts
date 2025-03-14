7bd5d0647e3ca1668d64ada19f7de5cb50fdff9c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-03-14 00:40:38 -0300

    remove `_updatedAtIndexOptions` from options

--- packages/models/src/models/BaseRaw.ts
+++ packages/models/src/models/BaseRaw.ts
@@ -44,7 +44,6 @@ type ModelOptions = {
 	preventSetUpdatedAt?: boolean;
 	collectionNameResolver?: (name: string) => string;
 	collection?: CollectionOptions;
-	_updatedAtIndexOptions?: Omit<IndexDescription, 'key'>;
 };
 
 export abstract class BaseRaw<
@@ -91,9 +90,6 @@ export abstract class BaseRaw<
 
 	public async createIndexes() {
 		const indexes = this.modelIndexes();
-		if (this.options?._updatedAtIndexOptions) {
-			indexes?.push({ ...this.options._updatedAtIndexOptions, key: { _updatedAt: 1 } });
-		}
 
 		if (indexes?.length) {
 			if (this.pendingIndexes) {
