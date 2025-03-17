991cb0e44201088c7de661fc421c4fa2ec054dbc
juliajforesti <juliajforesti@gmail.com>
2025-03-17 17:32:42 -0300

    chore: add `useAutoupdate` to `AppLayout`

---
+++ apps/meteor/client/components/AutoupdateToastMessage.tsx
@@ -0,0 +1,22 @@
+import { css } from '@rocket.chat/css-in-js';
+import { Box, Button } from '@rocket.chat/fuselage';
+import { useTranslation } from 'react-i18next';
+
+export const AutoupdateToastMessage = () => {
+	const { t } = useTranslation();
+
+	return (
+		<Box
+			display='flex'
+			alignItems='center'
+			className={css`
+				gap: 8px;
+			`}
+		>
+			{t('An_update_is_available')}
+			<Button primary small onClick={() => window.location.reload()}>
+				{t('Reload_to_update')}
+			</Button>
+		</Box>
+	);
+};
