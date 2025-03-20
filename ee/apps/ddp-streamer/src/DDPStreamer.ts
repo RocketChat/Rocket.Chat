d06ded3c7b2762741c9f3ccc391ca4a908e3eb08
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-03-20 16:05:57 -0300

    lint

--- ee/apps/ddp-streamer/src/DDPStreamer.ts
+++ ee/apps/ddp-streamer/src/DDPStreamer.ts
@@ -157,7 +157,7 @@ export class DDPStreamer extends ServiceClass {
 					'extension': 1,
 					'openBusinessHours': 1,
 					'services.totp.enabled': 1,
-					'services.email2fa.enabled': 1
+					'services.email2fa.enabled': 1,
 				},
 			});
 			if (!loggedUser) {
