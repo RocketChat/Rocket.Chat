3242300d582592be27ddfd8e4a6c2b762bb8691e
rique223 <henrique.jobs1@gmail.com>
2025-03-25 10:47:41 -0300

    Reviews

--- apps/meteor/client/views/root/hooks/useWordPressOAuth.ts
+++ apps/meteor/client/views/root/hooks/useWordPressOAuth.ts
@@ -4,58 +4,6 @@ import { useEffect } from 'react';
 
 import { CustomOAuth } from '../../../../app/custom-oauth/client/CustomOAuth';
 
-const config: OauthConfig = {
-	serverURL: '',
-	identityPath: '/oauth/me',
-	addAutopublishFields: {
-		forLoggedInUser: ['services.wordpress'],
-		forOtherUsers: ['services.wordpress.user_login'],
-	},
-	accessTokenParam: 'access_token',
-};
-
-const serverTypeHandlers = {
-	'custom': (
-		identityPath: string | undefined,
-		identityTokenSentVia: string | undefined,
-		tokenPath: string | undefined,
-		authorizePath: string | undefined,
-		scope: string | undefined,
-	) => {
-		if (identityPath) {
-			config.identityPath = identityPath;
-		}
-
-		if (identityTokenSentVia) {
-			config.identityTokenSentVia = identityTokenSentVia;
-		}
-
-		if (tokenPath) {
-			config.tokenPath = tokenPath;
-		}
-
-		if (authorizePath) {
-			config.authorizePath = authorizePath;
-		}
-
-		if (scope) {
-			config.scope = scope;
-		}
-	},
-	'wordpress-com': () => {
-		config.identityPath = 'https://public-api.wordpress.com/rest/v1/me';
-		config.identityTokenSentVia = 'header';
-		config.authorizePath = 'https://public-api.wordpress.com/oauth2/authorize';
-		config.tokenPath = 'https://public-api.wordpress.com/oauth2/token';
-		config.scope = 'auth';
-	},
-	'default': () => {
-		config.identityPath = '/oauth/me';
-	},
-};
-
-const WordPress = new CustomOAuth('wordpress', config);
-
 export const useWordPressOAuth = (): void => {
 	const wordpressURL = useSetting('API_Wordpress_URL') as string;
 	const serverType = useSetting('Accounts_OAuth_Wordpress_server_type') as string;
@@ -70,6 +18,61 @@ export const useWordPressOAuth = (): void => {
 			return;
 		}
 
+		const config: OauthConfig = {
+			serverURL: '',
+			identityPath: '/oauth/me',
+			addAutopublishFields: {
+				forLoggedInUser: ['services.wordpress'],
+				forOtherUsers: ['services.wordpress.user_login'],
+			},
+			accessTokenParam: 'access_token',
+		};
+
+		const configureServerType = (
+			serverType: string,
+			identityPath?: string,
+			identityTokenSentVia?: string,
+			tokenPath?: string,
+			authorizePath?: string,
+			scope?: string,
+		) => {
+			switch (serverType) {
+				case 'custom':
+					if (identityPath) {
+						config.identityPath = identityPath;
+					}
+
+					if (identityTokenSentVia) {
+						config.identityTokenSentVia = identityTokenSentVia;
+					}
+
+					if (tokenPath) {
+						config.tokenPath = tokenPath;
+					}
+
+					if (authorizePath) {
+						config.authorizePath = authorizePath;
+					}
+
+					if (scope) {
+						config.scope = scope;
+					}
+					break;
+
+				case 'wordpress-com':
+					config.identityPath = 'https://public-api.wordpress.com/rest/v1/me';
+					config.identityTokenSentVia = 'header';
+					config.authorizePath = 'https://public-api.wordpress.com/oauth2/authorize';
+					config.tokenPath = 'https://public-api.wordpress.com/oauth2/token';
+					config.scope = 'auth';
+					break;
+
+				default:
+					config.identityPath = '/oauth/me';
+					break;
+			}
+		};
+
 		config.serverURL = wordpressURL;
 
 		delete config.identityPath;
@@ -78,14 +81,8 @@ export const useWordPressOAuth = (): void => {
 		delete config.tokenPath;
 		delete config.scope;
 
-		(serverTypeHandlers[serverType as keyof typeof serverTypeHandlers] || serverTypeHandlers.default)(
-			identityPath,
-			identityTokenSentVia,
-			tokenPath,
-			authorizePath,
-			scope,
-		);
-
+		configureServerType(serverType, identityPath, identityTokenSentVia, tokenPath, authorizePath, scope);
+		const WordPress = new CustomOAuth('wordpress', config);
 		WordPress.configure(config);
 	}, [authorizePath, identityPath, identityTokenSentVia, scope, serverType, tokenPath, wordpressURL]);
 };
