59c8848b14d42d378a2b29bbd7e5013c943d74f5
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-03-25 18:40:37 -0300

    expose `LicenseManagerInterface`

--- ee/packages/license/src/index.ts
+++ ee/packages/license/src/index.ts
@@ -1,5 +1,6 @@
 export { DuplicatedLicenseError } from './errors/DuplicatedLicenseError';
 export * from './licenseImp';
+export * from './license';
 export * from './MockedLicenseBuilder';
 export * from './applyLicense';
 export * from './AirGappedRestriction';
