b01d38f109eb436c23f78f3b23cf163fe8b3c814
dougfabris <devfabris@gmail.com>
2025-03-26 17:12:15 -0300

    fix: review

--- apps/meteor/client/views/room/HeaderV2/RoomToolbox/RoomToolboxE2EESetup.tsx
+++ apps/meteor/client/views/room/HeaderV2/RoomToolbox/RoomToolboxE2EESetup.tsx
@@ -1,4 +1,6 @@
+import type { Box } from '@rocket.chat/fuselage';
 import { useStableArray } from '@rocket.chat/fuselage-hooks';
+import type { ComponentProps } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { HeaderToolbarAction } from '../../../../components/Header';
@@ -6,7 +8,11 @@ import { roomActionHooksForE2EESetup } from '../../../../ui';
 import type { RoomToolboxActionConfig } from '../../contexts/RoomToolboxContext';
 import { useRoomToolbox } from '../../contexts/RoomToolboxContext';
 
-const RoomToolboxE2EESetup = ({ className }: { className?: string }) => {
+type RoomToolboxE2EESetupProps = {
+	className?: ComponentProps<typeof Box>['className'];
+};
+
+const RoomToolboxE2EESetup = ({ className }: RoomToolboxE2EESetupProps) => {
 	const { t } = useTranslation();
 	const toolbox = useRoomToolbox();
 
