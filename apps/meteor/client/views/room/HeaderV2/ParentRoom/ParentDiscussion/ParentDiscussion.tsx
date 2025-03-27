0020b39eb9989363c6e9969bbaf7829a32cbcb46
dougfabris <devfabris@gmail.com>
2025-03-27 16:52:46 -0300

    fix: translation tweak

--- apps/meteor/client/views/room/HeaderV2/ParentRoom/ParentDiscussion/ParentDiscussion.tsx
+++ apps/meteor/client/views/room/HeaderV2/ParentRoom/ParentDiscussion/ParentDiscussion.tsx
@@ -1,4 +1,3 @@
-import { isPrivateRoom } from '@rocket.chat/core-typings';
 import type { IRoom } from '@rocket.chat/core-typings';
 import { useTranslation } from 'react-i18next';
 
@@ -14,9 +13,8 @@ const ParentDiscussion = ({ loading = false, room }: ParentDiscussionProps) => {
 	const { t } = useTranslation();
 	const roomName = roomCoordinator.getRoomName(room.t, room);
 	const handleRedirect = (): void => roomCoordinator.openRouteLink(room.t, { rid: room._id, ...room });
-	const title = isPrivateRoom(room) ? t('Back_to__roomName__group', { roomName }) : t('Back_to__roomName__channel', { roomName });
 
-	return <ParentRoomButton loading={loading} onClick={handleRedirect} title={title} />;
+	return <ParentRoomButton loading={loading} onClick={handleRedirect} title={t('Back_to__roomName__channel', { roomName })} />;
 };
 
 export default ParentDiscussion;
