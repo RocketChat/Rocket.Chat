650cff5c95d014f0a773181a321de9739293c2ad
Tasso <tasso.evangelista@rocket.chat>
2025-03-26 23:18:06 -0300

    Replace `Mongo.Collection` with `MinimongoCollection`

--- apps/meteor/app/models/client/models/Messages.ts
+++ apps/meteor/app/models/client/models/Messages.ts
@@ -1,20 +1,6 @@
 import type { IMessage } from '@rocket.chat/core-typings';
-import { Mongo } from 'meteor/mongo';
 
-import type { MinimongoCollection } from '../../../../client/definitions/MinimongoCollection';
-
-class ChatMessageCollection
-	extends Mongo.Collection<IMessage & { ignored?: boolean }>
-	implements MinimongoCollection<IMessage & { ignored?: boolean }>
-{
-	constructor() {
-		super(null);
-	}
-
-	public declare _collection: MinimongoCollection<IMessage & { ignored?: boolean }>['_collection'];
-
-	public declare queries: MinimongoCollection<IMessage & { ignored?: boolean }>['queries'];
-}
+import { MinimongoCollection } from '../../../../client/lib/cachedCollections/MinimongoCollection';
 
 /** @deprecated new code refer to Minimongo collections like this one; prefer fetching data from the REST API, listening to changes via streamer events, and storing the state in a Tanstack Query */
-export const Messages = new ChatMessageCollection();
+export const Messages = new MinimongoCollection<IMessage & { ignored?: boolean }>();
