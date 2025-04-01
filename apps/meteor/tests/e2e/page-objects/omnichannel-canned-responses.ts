7733cda8b7657b99b68003ce3e96257cfa474413
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-04-01 18:54:47 -0300

    test: added e2e tests

--- apps/meteor/tests/e2e/page-objects/omnichannel-canned-responses.ts
+++ apps/meteor/tests/e2e/page-objects/omnichannel-canned-responses.ts
@@ -3,8 +3,52 @@ import type { Locator } from '@playwright/test';
 import { OmnichannelAdministration } from './omnichannel-administration';
 
 export class OmnichannelCannedResponses extends OmnichannelAdministration {
-	get radioPublic(): Locator {
-		return this.page.locator('[data-qa-id="canned-response-public-radio"]').first();
+	get inputShortcut() {
+		return this.page.getByRole('textbox', { name: 'Shortcut', exact: true });
+	}
+
+	get inputMessage() {
+		return this.page.getByRole('textbox', { name: 'Message', exact: true });
+	}
+
+	get radioPublic() {
+		// NOTE: Playwright struggles to click the 1x1 px radio button, so we select the label instead
+		return this.page.locator('label', { hasText: /^Public$/ });
+	}
+
+	get radioDepartment() {
+		// NOTE: Playwright struggles to click the 1x1 px radio button, so we select the label instead
+		return this.page.locator('label', { hasText: /^Department$/ });
+	}
+
+	get radioPrivate() {
+		// NOTE: Playwright struggles to click the 1x1 px radio button, so we select the label instead
+		return this.page.locator('label', { hasText: /^Private$/ });
+	}
+
+	get inputTags() {
+		return this.page.getByRole('textbox', { name: 'Tags', exact: true });
+	}
+
+	get btnAddTag() {
+		return this.page.getByRole('button', { name: 'Add', exact: true });
+	}
+
+	listItem(name: string) {
+		return this.page.getByText(`!${name}`, { exact: true });
+	}
+
+	async addTag(tag: string) {
+		await this.inputTags.fill(tag);
+		await this.btnAddTag.click();
+	}
+
+	get btnEdit() {
+		return this.page.getByRole('button', { name: 'Edit', exact: true });
+	}
+
+	get btnSave(): Locator {
+		return this.page.getByRole('button', { name: 'Save', exact: true });
 	}
 
 	get btnNew(): Locator {
