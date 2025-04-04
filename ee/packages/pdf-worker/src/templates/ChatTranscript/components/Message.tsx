dddad6c16999d1cc19414ebb891f62e90c7c9787
Kevin Aleman <kaleman960@gmail.com>
2025-04-04 09:09:36 -0600

    fix pdf worker failing to generate files when messages had tons of markdown

--- ee/packages/pdf-worker/src/templates/ChatTranscript/components/Message.tsx
+++ ee/packages/pdf-worker/src/templates/ChatTranscript/components/Message.tsx
@@ -24,19 +24,53 @@ const styles = StyleSheet.create({
 });
 
 const messageLongerThanPage = (message: string | undefined) => (message?.length ?? 0) > 1200;
+const markupEntriesGreaterThan10 = (messageMd: any[]) => messageMd.length > 10;
+// When a markup list is greater than 10 (magic number, but a reasonable small/big number) we're gonna split the markdown into multiple <View> element
+// So react-pdf can split them evenly across pages
+const splitByTens = (array: any[] = []): any[][] => {
+	const result = [];
+	for (let i = 0; i < array.length; i += 10) {
+		result.push(array.slice(i, i + 10));
+	}
+	return result;
+};
+
+const processMd = (message: PDFMessage) =>
+	splitByTens(message.md).map((chunk, index) => (
+		<View style={{ ...styles.message, ...(isSystemMessage(message) && styles.systemMessage) }} key={index}>
+			<Markup tokens={chunk} />
+		</View>
+	));
 
 const isSystemMessage = (message: PDFMessage) => !!message.t;
 
-const Message = ({ message, invalidFileMessage }: { message: PDFMessage; invalidFileMessage: string }) => (
-	<View style={styles.wrapper}>
-		<View wrap={!!message.quotes || messageLongerThanPage(message.msg)}>
-			{message.divider && <Divider divider={message.divider} />}
-			<MessageHeader name={message.u.name || message.u.username} time={message.ts} />
+const processMessage = (message: PDFMessage) => {
+	if (message.md) {
+		if (markupEntriesGreaterThan10(message.md)) {
+			return processMd(message);
+		}
+
+		return (
 			<View style={{ ...styles.message, ...(isSystemMessage(message) && styles.systemMessage) }}>
-				{message.md ? <Markup tokens={message.md} /> : <Text>{message.msg}</Text>}
+				<Markup tokens={message.md}></Markup>
 			</View>
-			{message.quotes && <Quotes quotes={message.quotes} />}
+		);
+	}
+
+	return (
+		<View style={{ ...styles.message, ...(isSystemMessage(message) && styles.systemMessage) }}>
+			<Text>{message.msg}</Text>
 		</View>
+	);
+};
+
+const Message = ({ message, invalidFileMessage }: { message: PDFMessage; invalidFileMessage: string }) => (
+	<View style={styles.wrapper} wrap={!!message.quotes || messageLongerThanPage(message.msg)}>
+		{message.divider && <Divider divider={message.divider} />}
+		<MessageHeader name={message.u.name || message.u.username} time={message.ts} />
+
+		{processMessage(message)}
+		{message.quotes && <Quotes quotes={message.quotes} />}
 
 		{message.files && <Files files={message.files} invalidMessage={invalidFileMessage} />}
 	</View>
