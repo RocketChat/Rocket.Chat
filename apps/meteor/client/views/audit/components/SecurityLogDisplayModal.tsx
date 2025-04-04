2488d0daacacc4ce3cf110a512b059c7864efc29
MartinSchoeler <martinschoeler8@gmail.com>
2025-04-04 11:58:25 -0300

    review: Modal margins and colors

--- apps/meteor/client/views/audit/components/SecurityLogDisplayModal.tsx
+++ apps/meteor/client/views/audit/components/SecurityLogDisplayModal.tsx
@@ -3,8 +3,10 @@ import { UserAvatar } from '@rocket.chat/ui-avatar';
 import { format } from 'date-fns';
 import { useTranslation } from 'react-i18next';
 
+import AuditModalField from './AuditModalField';
+import AuditModalLabel from './AuditModalLabel';
+import AuditModalText from './AuditModalText';
 import GenericModal from '../../../components/GenericModal';
-import { InfoPanelLabel, InfoPanelText } from '../../../components/InfoPanel';
 
 type SecurityLogDisplayProps = {
 	timestamp: string;
@@ -20,27 +22,36 @@ const SecurityLogDisplayModal = ({ timestamp, actor, actorId, setting, changedFr
 	const { t } = useTranslation();
 
 	return (
-		<GenericModal icon={null} onClose={onCancel} title={t('Setting_change')}>
-			<InfoPanelLabel>{t('Timestamp')}</InfoPanelLabel>
-			<InfoPanelText>{format(new Date(timestamp), 'MMMM d yyyy, h:mm:ss a')}</InfoPanelText>
+		<GenericModal maxHeight={550} icon={null} onClose={onCancel} title={t('Setting_change')}>
+			<AuditModalField>
+				<AuditModalLabel>{t('Timestamp')}</AuditModalLabel>
+				<AuditModalText>{format(new Date(timestamp), 'MMMM d yyyy, h:mm:ss a')}</AuditModalText>
+			</AuditModalField>
 
-			<InfoPanelLabel>{t('Actor')}</InfoPanelLabel>
-
-			<Box display='flex' alignItems='center' mbe={16}>
-				<UserAvatar size='x24' username={actorId} />
-				<Box mi={8} fontScale='p2m' display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
-					{actor}
+			<AuditModalField>
+				<AuditModalLabel>{t('Actor')}</AuditModalLabel>
+				<Box display='flex' alignItems='center'>
+					<UserAvatar size='x24' username={actorId} />
+					<Box mi={8} fontScale='p2m' display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
+						{actor}
+					</Box>
 				</Box>
-			</Box>
+			</AuditModalField>
 
-			<InfoPanelLabel>{t('Setting')}</InfoPanelLabel>
-			<InfoPanelText>{t(setting)}</InfoPanelText>
+			<AuditModalField>
+				<AuditModalLabel>{t('Setting')}</AuditModalLabel>
+				<AuditModalText>{t(setting)}</AuditModalText>
+			</AuditModalField>
 
-			<InfoPanelLabel>{t('Changed_from')}</InfoPanelLabel>
-			<InfoPanelText>{changedFrom}</InfoPanelText>
+			<AuditModalField>
+				<AuditModalLabel>{t('Changed_from')}</AuditModalLabel>
+				<AuditModalText>{changedFrom}</AuditModalText>
+			</AuditModalField>
 
-			<InfoPanelLabel>{t('Changed_to')}</InfoPanelLabel>
-			<InfoPanelText>{changedTo}</InfoPanelText>
+			<AuditModalField>
+				<AuditModalLabel>{t('Changed_to')}</AuditModalLabel>
+				<AuditModalText>{changedTo}</AuditModalText>
+			</AuditModalField>
 		</GenericModal>
 	);
 };
