709f2408aa9fbbfa299f62e3f46365ba11773502
MartinSchoeler <martinschoeler8@gmail.com>
2025-04-07 10:24:34 -0300

    chore: update tests

--- apps/meteor/client/views/audit/hooks/useSettingSelectOptions.spec.ts
+++ apps/meteor/client/views/audit/hooks/useSettingSelectOptions.spec.ts
@@ -5,7 +5,7 @@ import { useSettingSelectOptions } from './useSettingSelectOptions';
 
 // TODO: check if the return of items matches the settings we mocked
 describe('useSettingSelectOptions', () => {
-	it('should return the list of options', async () => {
+	it('should return the ordered list of options', async () => {
 		const { result } = renderHook(() => useSettingSelectOptions(), {
 			wrapper: mockAppRoot()
 				.withSetting('test1', true)
@@ -26,8 +26,23 @@ describe('useSettingSelectOptions', () => {
 				.build(),
 		});
 
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test1', label: 'test1', value: 'test1' }));
-		await waitFor(() => expect(result.current.itemsList.items).toHaveLength(15));
+		await waitFor(() => expect(result.current?.data?.pages[0][0]).toEqual({ _id: 'test1', label: 'test1', value: 'test1' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][1]).toEqual({ _id: 'test2', label: 'test2', value: 'test2' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][2]).toEqual({ _id: 'test3', label: 'test3', value: 'test3' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][3]).toEqual({ _id: 'test4', label: 'test4', value: 'test4' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][4]).toEqual({ _id: 'test5', label: 'test5', value: 'test5' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][5]).toEqual({ _id: 'test6', label: 'test6', value: 'test6' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][6]).toEqual({ _id: 'test7', label: 'test7', value: 'test7' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][7]).toEqual({ _id: 'test8', label: 'test8', value: 'test8' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][8]).toEqual({ _id: 'test9', label: 'test9', value: 'test9' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][9]).toEqual({ _id: 'test10', label: 'test10', value: 'test10' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][10]).toEqual({ _id: 'test11', label: 'test11', value: 'test11' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][11]).toEqual({ _id: 'test12', label: 'test12', value: 'test12' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][12]).toEqual({ _id: 'test13', label: 'test13', value: 'test13' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][13]).toEqual({ _id: 'test14', label: 'test14', value: 'test14' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][14]).toEqual({ _id: 'test15', label: 'test15', value: 'test15' }));
+
+		await waitFor(() => expect(result.current?.data?.pages[0]).toHaveLength(15));
 	});
 
 	it('should return the list of filtered options', async () => {
@@ -51,13 +66,13 @@ describe('useSettingSelectOptions', () => {
 				.build(),
 		});
 
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test1', label: 'test1', value: 'test1' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test10', label: 'test10', value: 'test10' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test11', label: 'test11', value: 'test11' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test12', label: 'test12', value: 'test12' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test13', label: 'test13', value: 'test13' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test14', label: 'test14', value: 'test14' }));
-		await waitFor(() => expect(result.current.itemsList.items).toContainEqual({ _id: 'test15', label: 'test15', value: 'test15' }));
-		await waitFor(() => expect(result.current.itemsList.items).toHaveLength(7));
+		await waitFor(() => expect(result.current?.data?.pages[0][0]).toEqual({ _id: 'test1', label: 'test1', value: 'test1' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][1]).toEqual({ _id: 'test10', label: 'test10', value: 'test10' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][2]).toEqual({ _id: 'test11', label: 'test11', value: 'test11' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][3]).toEqual({ _id: 'test12', label: 'test12', value: 'test12' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][4]).toEqual({ _id: 'test13', label: 'test13', value: 'test13' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][5]).toEqual({ _id: 'test14', label: 'test14', value: 'test14' }));
+		await waitFor(() => expect(result.current?.data?.pages[0][6]).toEqual({ _id: 'test15', label: 'test15', value: 'test15' }));
+		await waitFor(() => expect(result.current?.data?.pages[0]).toHaveLength(7));
 	});
 });
