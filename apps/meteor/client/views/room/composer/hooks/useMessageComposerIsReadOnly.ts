6b4c7a6e1fa47be4eba6199b1f5f2b4bf64c529e
Tasso <tasso.evangelista@rocket.chat>
2025-04-07 11:48:31 -0300

    Replace some `findOne` calls

--- apps/meteor/client/views/room/composer/hooks/useMessageComposerIsReadOnly.ts
+++ apps/meteor/client/views/room/composer/hooks/useMessageComposerIsReadOnly.ts
@@ -1,19 +1,12 @@
 import { useUserId } from '@rocket.chat/ui-contexts';
-import { useCallback } from 'react';
 
 import { Users } from '../../../../../app/models/client';
-import { useReactiveValue } from '../../../../hooks/useReactiveValue';
 import { roomCoordinator } from '../../../../lib/rooms/roomCoordinator';
 
 export const useMessageComposerIsReadOnly = (rid: string): boolean => {
 	const uid = useUserId();
 
-	const isReadOnly = useReactiveValue(
-		useCallback(
-			() => Boolean(roomCoordinator.readOnly(rid, (uid ? Users.findOne(uid, { fields: { username: 1 } }) : undefined)!)),
-			[uid, rid],
-		),
-	);
+	const isReadOnly = Users.use((state) => Boolean(roomCoordinator.readOnly(rid, (uid ? state.get(uid) : undefined)!)));
 
 	return isReadOnly;
 };
