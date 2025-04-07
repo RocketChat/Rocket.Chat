2e55e4e956c668ced6ef64b34b0a3695e5ce856d
Tasso <tasso.evangelista@rocket.chat>
2025-04-07 11:48:31 -0300

    Replace some `findOne` calls

--- apps/meteor/client/views/root/MainLayout/TwoFactorAuthSetupCheck.tsx
+++ apps/meteor/client/views/root/MainLayout/TwoFactorAuthSetupCheck.tsx
@@ -1,12 +1,11 @@
 import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
 import { useLayout, useUser, useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement, ReactNode } from 'react';
-import { lazy, useCallback } from 'react';
+import { lazy } from 'react';
 
 import LayoutWithSidebar from './LayoutWithSidebar';
 import LayoutWithSidebarV2 from './LayoutWithSidebarV2';
 import { Roles } from '../../../../app/models/client';
-import { useReactiveValue } from '../../../hooks/useReactiveValue';
 
 const AccountSecurityPage = lazy(() => import('../../account/security/AccountSecurityPage'));
 
@@ -14,18 +13,15 @@ const TwoFactorAuthSetupCheck = ({ children }: { children: ReactNode }): ReactEl
 	const { isEmbedded: embeddedLayout } = useLayout();
 	const user = useUser();
 	const tfaEnabled = useSetting('Accounts_TwoFactorAuthentication_Enabled');
-	const require2faSetup = useReactiveValue(
-		useCallback(() => {
-			// User is already using 2fa
-			if (!user || user?.services?.totp?.enabled || user?.services?.email2fa?.enabled) {
-				return false;
-			}
+	const require2faSetup = Roles.use((state) => {
+		// User is already using 2fa
+		if (!user || user?.services?.totp?.enabled || user?.services?.email2fa?.enabled) {
+			return false;
+		}
 
-			const mandatoryRole = Roles.findOne({ _id: { $in: user.roles ?? [] }, mandatory2fa: true });
-			return mandatoryRole !== undefined && tfaEnabled;
-		}, [tfaEnabled, user]),
-	);
-
+		const mandatoryRole = state.find((role) => !!role.mandatory2fa && user.roles?.includes(role._id));
+		return mandatoryRole !== undefined && tfaEnabled;
+	});
 	if (require2faSetup) {
 		return (
 			<main id='rocket-chat' className={embeddedLayout ? 'embedded-view' : undefined}>
