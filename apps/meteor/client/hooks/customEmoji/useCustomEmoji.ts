da48c4365ba0d3db4bf4be839907f6d87e03f349
juliajforesti <juliajforesti@gmail.com>
2025-04-08 15:17:09 -0300

    chore: create `useCustomEmoji` hook and helpers

---
+++ apps/meteor/client/hooks/customEmoji/useCustomEmoji.ts
@@ -0,0 +1,59 @@
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { emoji } from '../../../app/emoji/client';
+import { customRender } from '../../lib/customEmoji';
+
+export const useCustomEmoji = () => {
+	const getCustomEmojis = useEndpoint('GET', '/v1/emoji-custom.list');
+	const result = useQuery({
+		queryKey: ['emoji-custom.list'],
+		queryFn: () => getCustomEmojis({ query: '' }),
+	});
+
+	useEffect(() => {
+		if (result.isError) {
+			console.error('Error getting custom emoji ', result.error);
+		}
+
+		if (!result.data?.emojis.update.length) {
+			return;
+		}
+
+		const {
+			emojis: { update: customEmojis },
+		} = result.data;
+
+		const addCustomEmojis = () => {
+			emoji.packages.emojiCustom = {
+				emojiCategories: [{ key: 'rocket', i18n: 'Custom' }],
+				categoryIndex: 1,
+				toneList: {},
+				list: [],
+				_regexpSignature: null,
+				_regexp: null,
+				emojisByCategory: {},
+				render: customRender,
+				renderPicker: customRender,
+			};
+
+			emoji.packages.emojiCustom.emojisByCategory = { rocket: [] };
+
+			for (const currentEmoji of customEmojis) {
+				emoji.packages.emojiCustom.emojisByCategory.rocket.push(currentEmoji.name);
+				emoji.packages.emojiCustom.list?.push(`:${currentEmoji.name}:`);
+				emoji.list[`:${currentEmoji.name}:`] = { ...currentEmoji, emojiPackage: 'emojiCustom' } as any;
+				for (const alias of currentEmoji.aliases) {
+					emoji.packages.emojiCustom.list?.push(`:${alias}:`);
+					emoji.list[`:${alias}:`] = {
+						emojiPackage: 'emojiCustom',
+						aliasOf: currentEmoji.name,
+					};
+				}
+			}
+			emoji.dispatchUpdate();
+		};
+		addCustomEmojis();
+	}, [result.data, result.error, result.isError]);
+};
