be89548ebfe71a0c93d11b41fecf043f6f5f5afa
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-04-09 15:40:28 -0300

    test: added strict option to setSettingValueById

--- apps/meteor/tests/e2e/utils/setSettingValueById.ts
+++ apps/meteor/tests/e2e/utils/setSettingValueById.ts
@@ -2,10 +2,10 @@ import type { APIResponse } from '@playwright/test';
 
 import type { BaseTest } from './test';
 
-export const setSettingValueById = async (api: BaseTest['api'], settingId: string, value: unknown): Promise<APIResponse> => {
+export const setSettingValueById = async (api: BaseTest['api'], settingId: string, value: unknown, strict = true): Promise<APIResponse> => {
 	const response = await api.post(`/settings/${settingId}`, { value });
 
-	if (!response.ok()) {
+	if (strict && !response.ok()) {
 		throw new Error(
 			`Failed to update setting "${settingId}" with value "${value}" [http status: ${response.status()} ${response.statusText()}]`,
 		);
