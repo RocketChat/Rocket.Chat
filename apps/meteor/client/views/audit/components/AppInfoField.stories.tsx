de59b97f6d8b03a9e7a96139903291f5c9f5825b
MartinSchoeler <martinschoeler8@gmail.com>
2025-04-09 16:07:40 -0300

    review: use full app object

--- apps/meteor/client/views/audit/components/AppInfoField.stories.tsx
+++ apps/meteor/client/views/audit/components/AppInfoField.stories.tsx
@@ -1,3 +1,4 @@
+import type { AppSubscriptionStatus } from '@rocket.chat/core-typings';
 import { mockAppRoot } from '@rocket.chat/mock-providers';
 import type { Meta, StoryFn } from '@storybook/react';
 
@@ -11,16 +12,71 @@ export default {
 	},
 	decorators: [
 		mockAppRoot()
-			.withEndpoint(
-				'GET',
-				'/apps/:id',
-				() =>
-					({
-						app: {
-							name: 'App Name',
+			.withEndpoint('GET', '/apps/:id', () => ({
+				app: {
+					name: 'App Name',
+					id: '',
+					iconFileData: '',
+					appRequestStats: {
+						appId: '',
+						totalSeen: 0,
+						totalUnseen: 0,
+					},
+					author: {
+						name: '',
+						homepage: '',
+						support: '',
+					},
+					description: '',
+					privacyPolicySummary: '',
+					detailedDescription: {
+						raw: '',
+						rendered: '',
+					},
+					detailedChangelog: {
+						raw: '',
+						rendered: '',
+					},
+					categories: [],
+					version: '',
+					price: 0,
+					purchaseType: 'buy' as const,
+					pricingPlans: [],
+					iconFileContent: '',
+					isSubscribed: false,
+					bundledIn: [],
+					marketplaceVersion: '',
+					// Recursive typem expect an App type here
+					latest: undefined as any,
+					subscriptionInfo: {
+						typeOf: '',
+						status: 'Active' as AppSubscriptionStatus,
+						statusFromBilling: false,
+						isSeatBased: false,
+						seats: 0,
+						maxSeats: 0,
+						license: {
+							license: '',
+							version: 0,
+							expireDate: '',
 						},
-					}) as any,
-			)
+						startDate: '',
+						periodEnd: '',
+						endDate: '',
+						externallyManaged: false,
+						isSubscribedViaBundle: false,
+					},
+					tosLink: '',
+					privacyLink: '',
+					modifiedAt: '',
+					permissions: [],
+					languages: [],
+					createdDate: '',
+					private: false,
+					documentationUrl: '',
+					migrated: false,
+				},
+			}))
 			.withTranslations('en', 'core', { App_name: 'App Name' })
 			.buildStoryDecorator(),
 	],
