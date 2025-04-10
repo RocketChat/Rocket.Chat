680da74e9b7ecff00088f44854bdce9d1681040a
Tiago Evangelista <tiago.evangelista@rocket.chat>
2025-04-10 17:01:16 -0300

    Revert "use api instead Roles model directly"
    This reverts commit 811e7764d3829faf21af0122331a36dd1ac74c40.

--- apps/meteor/client/views/hooks/useRequire2faSetup.ts
+++ apps/meteor/client/views/hooks/useRequire2faSetup.ts
@@ -1,17 +1,22 @@
 import { useSetting, useUser } from '@rocket.chat/ui-contexts';
+import { useCallback } from 'react';
 
-import { useRoles } from './useRoles';
+import { Roles } from '../../../app/models/client';
+import { useReactiveValue } from '../../hooks/useReactiveValue';
 
 export const useRequire2faSetup = () => {
 	const user = useUser();
 	const tfaEnabled = useSetting('Accounts_TwoFactorAuthentication_Enabled');
-	const roles = useRoles({ roles: user?.roles || [] });
 
-	// User is already using 2fa
-	if (!user || user?.services?.totp?.enabled || user?.services?.email2fa?.enabled) {
-		return false;
-	}
+	return useReactiveValue(
+		useCallback(() => {
+			// User is already using 2fa
+			if (!user || user?.services?.totp?.enabled || user?.services?.email2fa?.enabled) {
+				return false;
+			}
 
-	const mandatoryRole = roles.data?.some((role) => role.mandatory2fa);
-	return !!tfaEnabled && mandatoryRole !== undefined;
+			const mandatoryRole = Roles.findOne({ _id: { $in: user.roles ?? [] }, mandatory2fa: true });
+			return !!(mandatoryRole !== undefined && tfaEnabled);
+		}, [tfaEnabled, user]),
+	);
 };
