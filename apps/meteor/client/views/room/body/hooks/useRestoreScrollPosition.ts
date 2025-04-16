4bfed3cd1967156f37770df0f8431bbb49a366cb
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-04-16 12:06:09 -0300

    test: change unit test

--- apps/meteor/client/views/room/body/hooks/useRestoreScrollPosition.ts
+++ apps/meteor/client/views/room/body/hooks/useRestoreScrollPosition.ts
@@ -16,7 +16,7 @@ export function useRestoreScrollPosition() {
 
 		const store = RoomManager.getStore(roomId);
 
-		if (store?.scroll && !store.atBottom) {
+		if (store?.scroll !== undefined && !store.atBottom) {
 			ref.current.scrollTop = store.scroll;
 			ref.current.scrollLeft = 30;
 		}
