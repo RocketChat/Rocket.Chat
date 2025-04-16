0fd5b7260512bbabb751fd908bd98f84618a4f9c
Matheus Cardoso <matheus@cardo.so>
2025-04-16 19:39:19 -0300

    test: expect not visible after activated/deactivated

--- apps/meteor/tests/e2e/admin-users.spec.ts
+++ apps/meteor/tests/e2e/admin-users.spec.ts
@@ -6,44 +6,64 @@ import { expect, test } from './utils/test';
 
 test.use({ storageState: Users.admin.state });
 
-test('Newly created user', async ({ page }) => {
-	const poAdmin = new Admin(page);
-	await page.goto('/admin/users');
+test(
+	'New user shows in correct tabs when deactivated',
+	{
+		tag: '@admin',
+		annotation: {
+			type: 'issue',
+			description: 'https://rocketchat.atlassian.net/browse/SUP-775',
+		},
+	},
+	async ({ page }) => {
+		const poAdmin = new Admin(page);
+		await page.goto('/admin/users');
 
-	const { username } = await createUser(poAdmin);
+		const { username } = await createUser(poAdmin);
 
-	await page.getByPlaceholder('Search Users').fill(username);
+		await page.getByPlaceholder('Search Users').fill(username);
 
-	await test.step('is only visible in the All and Pending tabs', async () => {
-		await page.getByRole('tab', { name: 'All' }).click();
-		await expect(page.getByRole('link', { name: username })).toBeVisible();
+		await test.step('is visible in the All tab', async () => {
+			await page.getByRole('tab', { name: 'All' }).click();
+			await expect(page.getByRole('link', { name: username })).toBeVisible();
+		});
 
-		await page.getByRole('tab', { name: 'Pending' }).click();
-		await expect(page.getByRole('link', { name: username })).toBeVisible();
+		await test.step('is visible in the Pending tab', async () => {
+			await page.getByRole('tab', { name: 'Pending' }).click();
+			await expect(page.getByRole('link', { name: username })).toBeVisible();
+		});
 
-		await page.getByRole('tab', { name: 'Active' }).click();
-		await expect(page.getByRole('link', { name: username })).not.toBeVisible();
+		await test.step('is not visible in the Active tab', async () => {
+			await page.getByRole('tab', { name: 'Active' }).click();
+			await expect(page.getByRole('link', { name: username })).not.toBeVisible();
+		});
 
-		await page.getByRole('tab', { name: 'Deactivated' }).click();
-		await expect(page.getByRole('link', { name: username })).not.toBeVisible();
-	});
+		await test.step('is not visible in the Deactivated tab', async () => {
+			await page.getByRole('tab', { name: 'Deactivated' }).click();
+			await expect(page.getByRole('link', { name: username })).not.toBeVisible();
+		});
 
-	await test.step('moves from Pending to Deactivated tab', async () => {
-		await page.getByRole('tab', { name: 'Pending' }).click();
-		await page.getByRole('button', { name: 'More actions' }).click();
-		await page.getByRole('menuitem', { name: 'Deactivate' }).click();
-		await page.getByRole('tab', { name: 'Deactivated' }).click();
-		await expect(page.getByRole('link', { name: username })).toBeVisible();
-	});
+		await test.step('moves from Pending to Deactivated tab', async () => {
+			await page.getByRole('tab', { name: 'Pending' }).click();
+			await page.getByRole('button', { name: 'More actions' }).click();
+			await page.getByRole('menuitem', { name: 'Deactivate' }).click();
+			await expect(page.getByRole('link', { name: username })).not.toBeVisible();
 
-	await test.step('moves from Deactivated to Pending tab', async () => {
-		await page.getByRole('tab', { name: 'Deactivated' }).click();
-		await page.getByRole('button', { name: 'More actions' }).click();
-		await page.getByRole('menuitem', { name: 'Activate' }).click();
-		await page.getByRole('tab', { name: 'Pending' }).click();
-		await expect(page.getByRole('link', { name: username })).toBeVisible();
-	});
-});
+			await page.getByRole('tab', { name: 'Deactivated' }).click();
+			await expect(page.getByRole('link', { name: username })).toBeVisible();
+		});
+
+		await test.step('moves from Deactivated to Pending tab', async () => {
+			await page.getByRole('tab', { name: 'Deactivated' }).click();
+			await page.getByRole('button', { name: 'More actions' }).click();
+			await page.getByRole('menuitem', { name: 'Activate' }).click();
+			await expect(page.getByRole('link', { name: username })).not.toBeVisible();
+
+			await page.getByRole('tab', { name: 'Pending' }).click();
+			await expect(page.getByRole('link', { name: username })).toBeVisible();
+		});
+	},
+);
 
 async function createUser(poAdmin: Admin) {
 	const firstName = faker.person.firstName();
