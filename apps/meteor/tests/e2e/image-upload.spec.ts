36c44adcff1925305f89392aa36ca73d8ae6c8e7
Matheus Cardoso <matheus@cardo.so>
2025-04-22 20:08:06 -0300

    test: with and without Message_Attachments_Strip_Exif

--- apps/meteor/tests/e2e/image-upload.spec.ts
+++ apps/meteor/tests/e2e/image-upload.spec.ts
@@ -1,16 +1,17 @@
 import { Users } from './fixtures/userStates';
 import { HomeChannel } from './page-objects';
-import { createTargetChannel, setSettingValueById } from './utils';
+import { createTargetChannel, getSettingValueById, setSettingValueById } from './utils';
 import { test, expect } from './utils/test';
 
 test.use({ storageState: Users.user1.state });
 
 test.describe('image-upload', () => {
+	let settingDefaultValue: unknown;
 	let poHomeChannel: HomeChannel;
 	let targetChannel: string;
 
 	test.beforeAll(async ({ api }) => {
-		await setSettingValueById(api, 'Message_Attachments_Strip_Exif', false);
+		settingDefaultValue = await getSettingValueById(api, 'Message_Attachments_Strip_Exif');
 		targetChannel = await createTargetChannel(api, { members: ['user1'] });
 	});
 
@@ -21,16 +22,37 @@ test.describe('image-upload', () => {
 	});
 
 	test.afterAll(async ({ api }) => {
-		await setSettingValueById(api, 'Message_Attachments_Strip_Exif', true);
+		await setSettingValueById(api, 'Message_Attachments_Strip_Exif', settingDefaultValue);
 		expect((await api.post('/channels.delete', { roomName: targetChannel })).status()).toBe(200);
 	});
 
-	test('shows error indicator when upload fails', async ({ page }) => {
-		await poHomeChannel.content.sendFileMessage('bad-orientation.jpeg');
-		await poHomeChannel.content.fileNameInput.fill('bad-orientation.jpeg');
-		await poHomeChannel.content.descriptionInput.fill('bad-orientation_description');
-		await poHomeChannel.content.btnModalConfirm.click();
+	test.describe('strip exif disabled', () => {
+		test.beforeAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Attachments_Strip_Exif', false);
+		});
 
-		await expect(page.getByText('bad-orientation.jpeg')).toContainText('Error:');
+		test('should show error indicator when upload fails', async ({ page }) => {
+			await poHomeChannel.content.sendFileMessage('bad-orientation.jpeg');
+			await poHomeChannel.content.fileNameInput.fill('bad-orientation.jpeg');
+			await poHomeChannel.content.descriptionInput.fill('bad-orientation_description');
+			await poHomeChannel.content.btnModalConfirm.click();
+
+			await expect(page.getByText('bad-orientation.jpeg')).toContainText('Error:');
+		});
+	});
+
+	test.describe('strip exif enabled', () => {
+		test.beforeAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Attachments_Strip_Exif', true);
+		});
+
+		test('should succeed upload of bad-orientation.jpeg', async () => {
+			await poHomeChannel.content.sendFileMessage('bad-orientation.jpeg');
+			await poHomeChannel.content.fileNameInput.fill('bad-orientation.jpeg');
+			await poHomeChannel.content.descriptionInput.fill('bad-orientation_description');
+			await poHomeChannel.content.btnModalConfirm.click();
+
+			await expect(poHomeChannel.content.getFileDescription).toHaveText('bad-orientation_description');
+		});
 	});
 });
