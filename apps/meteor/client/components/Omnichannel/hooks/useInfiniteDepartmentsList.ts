990d3a34d31100fe0b2761fb274b7a8003e7de2f
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-04-24 15:37:34 -0300

    feat: added selectedDepartementId behavior

--- apps/meteor/client/components/Omnichannel/hooks/useInfiniteDepartmentsList.ts
+++ apps/meteor/client/components/Omnichannel/hooks/useInfiniteDepartmentsList.ts
@@ -1,6 +1,6 @@
 import type { ILivechatDepartment, Serialized } from '@rocket.chat/core-typings';
 import { useEndpoint } from '@rocket.chat/ui-contexts';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 
 import type { DepartmentListItem } from '../Definitions/DepartmentsDefinitions';
@@ -13,7 +13,7 @@ type DepartmentsListOptions = {
 	excludeId?: string;
 	enabled?: boolean;
 	showArchived?: boolean;
-	selectedDepartment?: string;
+	selectedDepartmentId?: string;
 	limit?: number;
 	unitId?: string;
 };
@@ -23,11 +23,19 @@ export const useInfiniteDepartmentsList = (options: DepartmentsListOptions) => {
 	const getLivechatDepartments = useEndpoint('GET', '/v1/livechat/department');
 	const getUnitDepartments = useEndpoint('GET', '/v1/livechat/units/:unitId/departments/available', { unitId: options.unitId || 'none' });
 	const getDepartments = options.unitId !== undefined ? getUnitDepartments : getLivechatDepartments;
+	const getDepartment = useEndpoint('GET', '/v1/livechat/department/:_id', { _id: options.selectedDepartmentId ?? '' });
 
 	const formatDepartmentItem = ({ _id, name, ...department }: Serialized<ILivechatDepartment>): DepartmentListItem => ({
 		_id,
 		label: department.archived ? `${name} [${t('Archived')}]` : name,
 		value: _id,
+	});
+
+	const { data: selectedDepartmentItem } = useQuery({
+		queryKey: ['/v1/livechat/department/:_id', options.selectedDepartmentId],
+		queryFn: async () => getDepartment({}),
+		select: (data) => formatDepartmentItem(data.department),
+		enabled: options.selectedDepartmentId !== undefined,
 	});
 
 	return useInfiniteQuery({
@@ -48,25 +56,31 @@ export const useInfiniteDepartmentsList = (options: DepartmentsListOptions) => {
 
 			return {
 				...data,
-				departments: departments.map(formatDepartmentItem),
+				departments: departments.map(formatDepartmentItem).filter((item) => item._id !== options.selectedDepartmentId),
 			};
 		},
 		select: (data) => {
 			const items = data.pages.flatMap<DepartmentListItem>((page) => page.departments);
 
-			options.haveAll &&
+			if (selectedDepartmentItem) {
+				items.unshift(selectedDepartmentItem);
+			}
+
+			if (options.haveAll) {
 				items.unshift({
 					_id: '',
 					label: t('All'),
 					value: 'all',
 				});
+			}
 
-			options.haveNone &&
+			if (options.haveNone) {
 				items.unshift({
 					_id: '',
 					label: t('None'),
 					value: '',
 				});
+			}
 
 			return items;
 		},
