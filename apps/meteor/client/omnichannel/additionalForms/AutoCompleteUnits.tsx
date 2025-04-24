8d8758e79ade4b7f55cde1e73459a53eea470c02
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-04-24 12:35:18 -0300

    refactor: AutoCompleteUnit(s) to use useInfiniteUnitsList

--- apps/meteor/client/omnichannel/additionalForms/AutoCompleteUnits.tsx
+++ apps/meteor/client/omnichannel/additionalForms/AutoCompleteUnits.tsx
@@ -2,12 +2,10 @@ import type { PaginatedMultiSelectOption } from '@rocket.chat/fuselage';
 import { PaginatedMultiSelectFiltered } from '@rocket.chat/fuselage';
 import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
 import type { ReactElement } from 'react';
-import { memo, useMemo, useState } from 'react';
+import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { useUnitsList } from '../../components/Omnichannel/hooks/useUnitsList';
-import { useRecordList } from '../../hooks/lists/useRecordList';
-import { AsyncStatePhase } from '../../lib/asyncState';
+import { useInfiniteUnitsList } from '../../components/Omnichannel/hooks/useInfiniteUnitsList';
 
 type AutoCompleteUnitsProps = {
 	value?: PaginatedMultiSelectOption[];
@@ -22,11 +20,7 @@ const AutoCompleteUnits = ({ value, error, placeholder, onChange }: AutoComplete
 
 	const debouncedUnitFilter = useDebouncedValue(unitsFilter, 500);
 
-	const { itemsList: unitsList, loadMoreItems: loadMoreUnits } = useUnitsList(
-		useMemo(() => ({ text: debouncedUnitFilter }), [debouncedUnitFilter]),
-	);
-
-	const { phase: unitsPhase, itemCount: unitsTotal, items: unitItems } = useRecordList(unitsList);
+	const { data: unitItems = [], fetchNextPage } = useInfiniteUnitsList({ text: debouncedUnitFilter });
 
 	return (
 		<PaginatedMultiSelectFiltered
@@ -41,9 +35,7 @@ const AutoCompleteUnits = ({ value, error, placeholder, onChange }: AutoComplete
 			setFilter={setUnitsFilter as (value: string | number | undefined) => void}
 			options={unitItems}
 			data-qa='autocomplete-multiple-unit'
-			endReached={
-				unitsPhase === AsyncStatePhase.LOADING ? (): void => undefined : (start): void => loadMoreUnits(start!, Math.min(50, unitsTotal))
-			}
+			endReached={() => fetchNextPage()}
 		/>
 	);
 };
