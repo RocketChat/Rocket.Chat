2cf25903068cb5e28e9b2678a2383e2e6c42f84d
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-04-30 12:06:09 -0300

    plim

--- apps/meteor/client/hooks/useIdleConnection.ts
+++ apps/meteor/client/hooks/useIdleConnection.ts
@@ -1,11 +1,10 @@
 import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
-import { ServerContext, useConnectionStatus, useSetting, useUserId } from '@rocket.chat/ui-contexts';
+import { ServerContext, useConnectionStatus, useSetting } from '@rocket.chat/ui-contexts';
 import { useContext } from 'react';
 
 import { useIdleActiveEvents } from './useIdleActiveEvents';
 
-export const useIdleConnection = () => {
-	const uid = useUserId();
+export const useIdleConnection = (uid: string | null) => {
 	const { status } = useConnectionStatus();
 	const allowAnonymousRead = useSetting('Accounts_AllowAnonymousRead');
 	const { disconnect: disconnectServer, reconnect: reconnectServer } = useContext(ServerContext);
