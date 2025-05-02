1517a0d8f2ddf1c38c79529388d97ae785004734
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-02 15:35:20 +0300

    feat: add openapi support and ajv to  ldap.testConnection route

--- apps/meteor/app/api/server/v1/ldap.ts
+++ apps/meteor/app/api/server/v1/ldap.ts
@@ -6,30 +6,77 @@ import { SystemLogger } from '../../../../server/lib/logger/system';
 import { settings } from '../../../settings/server';
 import { API } from '../api';
 
-API.v1.addRoute(
+API.v1.post(
 	'ldap.testConnection',
-	{ authRequired: true, permissionsRequired: ['test-admin-options'] },
 	{
-		async post() {
-			if (!this.userId) {
-				throw new Error('error-invalid-user');
-			}
-
-			if (settings.get<boolean>('LDAP_Enable') !== true) {
-				throw new Error('LDAP_disabled');
-			}
-
-			try {
-				await LDAP.testConnection();
-			} catch (error) {
-				SystemLogger.error(error);
-				throw new Error('Connection_failed');
-			}
-
-			return API.v1.success({
-				message: 'LDAP_Connection_successful' as const,
-			});
+		authRequired: true,
+		permissionsRequired: ['test-admin-options'],
+		response: {
+			200: ajv.compile({
+				type: 'object',
+				properties: {
+					message: {
+						type: 'string',
+						enum: ['LDAP_Connection_successful'],
+					},
+					success: {
+						type: 'boolean',
+						description: 'Whether the connection was successful or not',
+					},
+				},
+				required: ['success', 'message'],
+				additionalProperties: false,
+			}),
+			401: ajv.compile({
+				type: 'object',
+				properties: {
+					error: {
+						type: 'string',
+					},
+					success: {
+						type: 'boolean',
+						description: 'Indicates if the request was successful.',
+					},
+				},
+				required: ['success', 'error'],
+				additionalProperties: false,
+			}),
+			403: ajv.compile({
+				type: 'object',
+				properties: {
+					success: {
+						type: 'boolean',
+						description: 'Indicates if the request was successful.',
+					},
+					error: {
+						type: 'string',
+						description: 'Error message',
+					},
+				},
+				required: ['success', 'error'],
+				additionalProperties: false,
+			}),
 		},
+	},
+	async function () {
+		if (!this.userId) {
+			return API.v1.unauthorized({ error: 'error-invalid-user' });
+		}
+
+		if (settings.get<boolean>('LDAP_Enable') !== true) {
+			return API.v1.forbidden({ error: 'LDAP_disabled' });
+		}
+
+		try {
+			await LDAP.testConnection();
+		} catch (error) {
+			SystemLogger.error(error);
+			return API.v1.forbidden({ error: 'Connection_failed' });
+		}
+
+		return API.v1.success({
+			message: 'LDAP_Connection_successful' as const,
+		});
 	},
 );
 
