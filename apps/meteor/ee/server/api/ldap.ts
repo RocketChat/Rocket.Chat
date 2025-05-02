fb986919f68307c72fddc00a33d505ad0eb45a20
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-02 16:28:40 +0300

    feat: implement ldap.syncNow endpoint with openapi and ajv

--- apps/meteor/ee/server/api/ldap.ts
+++ apps/meteor/ee/server/api/ldap.ts
@@ -5,34 +5,155 @@ import { hasPermissionAsync } from '../../../app/authorization/server/functions/
 import { settings } from '../../../app/settings/server';
 import { LDAPEE } from '../sdk';
 
-API.v1.addRoute(
+API.v1.post(
 	'ldap.syncNow',
 	{
 		authRequired: true,
 		forceTwoFactorAuthenticationForNonEnterprise: true,
 		twoFactorRequired: true,
 		// license: ['ldap-enterprise'],
-	},
-	{
-		async post() {
-			if (!this.userId) {
-				throw new Error('error-invalid-user');
-			}
-
-			if (!(await hasPermissionAsync(this.userId, 'sync-auth-services-users'))) {
-				throw new Error('error-not-authorized');
-			}
-
-			if (settings.get('LDAP_Enable') !== true) {
-				throw new Error('LDAP_disabled');
-			}
-
-			await LDAPEE.sync();
-			await LDAPEE.syncAvatars();
-
-			return API.v1.success({
-				message: 'Sync_in_progress' as const,
-			});
+		response: {
+			200: ajv.compile({
+				type: 'object',
+				properties: {
+					message: {
+						type: 'string',
+						enum: ['LDAP_Connection_successful'],
+					},
+					success: {
+						type: 'boolean',
+						description: 'Whether the connection was successful or not',
+					},
+				},
+				required: ['success', 'message'],
+				additionalProperties: false,
+			}),
+			400: ajv.compile({
+				type: 'object',
+				properties: {
+					stack: {
+						type: 'string',
+						description: 'The stack trace of the error.',
+					},
+					error: {
+						type: 'string',
+						description: 'The error message.',
+					},
+					errorType: {
+						type: 'string',
+						description: 'The type of the error.',
+					},
+					details: {
+						type: 'object',
+						nullable: true,
+						properties: {
+							codeGenerated: { type: 'boolean' },
+							method: {
+								type: 'string',
+								description: 'The method that caused the error.',
+							},
+							availableMethods: {
+								type: 'array',
+								items: {
+									type: 'string',
+									description: 'The available methods.',
+								},
+							},
+						},
+					},
+					success: {
+						type: 'boolean',
+						description: 'Indicates if the request was successful.',
+					},
+				},
+				required: ['success', 'error', 'errorType', 'details', 'stack'],
+				additionalProperties: false,
+			}),
+			401: ajv.compile({
+				type: 'object',
+				properties: {
+					error: {
+						type: 'string',
+					},
+					success: {
+						type: 'boolean',
+						description: 'Indicates if the request was successful.',
+					},
+				},
+				required: ['success', 'error'],
+				additionalProperties: false,
+			}),
+			403: ajv.compile({
+				type: 'object',
+				properties: {
+					success: {
+						type: 'boolean',
+						description: 'Indicates if the request was successful.',
+					},
+					error: {
+						type: 'string',
+						description: 'Error message',
+					},
+				},
+				required: ['success', 'error'],
+				additionalProperties: false,
+			}),
 		},
 	},
+	async function () {
+		if (!this.userId) {
+			return API.v1.unauthorized({ error: 'error-invalid-user' });
+		}
+
+		if (!(await hasPermissionAsync(this.userId, 'sync-auth-services-users'))) {
+			return API.v1.unauthorized({ error: 'error-not-authorized' });
+		}
+
+		if (settings.get('LDAP_Enable') !== true) {
+			return API.v1.forbidden({ error: 'LDAP_disabled' });
+		}
+
+		try {
+			await LDAPEE.sync();
+			await LDAPEE.syncAvatars();
+		} catch (error) {
+			return API.v1.failure(error);
+		}
+
+		return API.v1.success({
+			message: 'Sync_in_progress' as const,
+		});
+	},
 );
+
+// API.v1.addRoute(
+// 	'ldap.syncNow',
+// 	{
+// 		authRequired: true,
+// 		forceTwoFactorAuthenticationForNonEnterprise: true,
+// 		twoFactorRequired: true,
+// 		// license: ['ldap-enterprise'],
+// 	},
+// 	{
+// 		async post() {
+// 			if (!this.userId) {
+// 				throw new Error('error-invalid-user');
+// 			}
+
+// 			if (!(await hasPermissionAsync(this.userId, 'sync-auth-services-users'))) {
+// 				throw new Error('error-not-authorized');
+// 			}
+
+// 			if (settings.get('LDAP_Enable') !== true) {
+// 				throw new Error('LDAP_disabled');
+// 			}
+
+// 			await LDAPEE.sync();
+// 			await LDAPEE.syncAvatars();
+
+// 			return API.v1.success({
+// 				message: 'Sync_in_progress' as const,
+// 			});
+// 		},
+// 	},
+// );
