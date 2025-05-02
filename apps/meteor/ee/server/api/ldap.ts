defd3e71fb169730d8ceabadd696530bf81576e0
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-02 16:29:05 +0300

    chore: remove commented-out ldap.syncNow route from ldap.ts

--- apps/meteor/ee/server/api/ldap.ts
+++ apps/meteor/ee/server/api/ldap.ts
@@ -125,35 +125,3 @@ API.v1.post(
 		});
 	},
 );
-
-// API.v1.addRoute(
-// 	'ldap.syncNow',
-// 	{
-// 		authRequired: true,
-// 		forceTwoFactorAuthenticationForNonEnterprise: true,
-// 		twoFactorRequired: true,
-// 		// license: ['ldap-enterprise'],
-// 	},
-// 	{
-// 		async post() {
-// 			if (!this.userId) {
-// 				throw new Error('error-invalid-user');
-// 			}
-
-// 			if (!(await hasPermissionAsync(this.userId, 'sync-auth-services-users'))) {
-// 				throw new Error('error-not-authorized');
-// 			}
-
-// 			if (settings.get('LDAP_Enable') !== true) {
-// 				throw new Error('LDAP_disabled');
-// 			}
-
-// 			await LDAPEE.sync();
-// 			await LDAPEE.syncAvatars();
-
-// 			return API.v1.success({
-// 				message: 'Sync_in_progress' as const,
-// 			});
-// 		},
-// 	},
-// );
