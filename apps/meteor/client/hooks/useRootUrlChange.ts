5318b58f4d719717694e53145dd9e0e78a82ee47
juliajforesti <juliajforesti@gmail.com>
2025-05-07 17:46:51 -0300

    chore: create `useRootUrlChange`

---
+++ apps/meteor/client/hooks/useRootUrlChange.ts
@@ -0,0 +1,69 @@
+import { useRole, useSetting, useSettingSetValue, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { Roles } from '../../app/models/client';
+import UrlChangeModal from '../components/UrlChangeModal';
+import { imperativeModal } from '../lib/imperativeModal';
+import { isSyncReady } from '../lib/userData';
+
+export const useRootUrlChange = (userId: string) => {
+	const { t } = useTranslation();
+	const dispatchToastMessage = useToastMessageDispatch();
+	const isAdmin = useRole('admin');
+
+	const currentUrl = location.origin + window.__meteor_runtime_config__.ROOT_URL_PATH_PREFIX;
+	const siteUrl = useSetting('Site_Url', '');
+	const documentDomain = useSetting('Document_Domain', '');
+	const setSiteUrl = useSettingSetValue('Site_Url');
+
+	const siteUrlMutation = useMutation({
+		mutationKey: ['settings', 'Site_Url'],
+		mutationFn: async (url: string) => {
+			await setSiteUrl(url);
+			return { url };
+		},
+		onSuccess: ({ url }) => dispatchToastMessage({ type: 'success', message: `${t('Saved')}, new site url is: ${url}` }),
+	});
+
+	useEffect(() => {
+		if (!Roles.ready.get() || !isSyncReady.get()) {
+			return;
+		}
+
+		if (!isAdmin) {
+			return;
+		}
+
+		if (!siteUrl) {
+			return;
+		}
+
+		if (window.__meteor_runtime_config__.ROOT_URL.replace(/\/$/, '') !== currentUrl) {
+			const confirm = (): void => {
+				imperativeModal.close();
+				siteUrlMutation.mutateAsync(currentUrl);
+			};
+			imperativeModal.open({
+				component: UrlChangeModal,
+				props: {
+					onConfirm: confirm,
+					siteUrl,
+					currentUrl,
+					onClose: imperativeModal.close,
+				},
+			});
+		}
+
+		if (documentDomain) {
+			window.document.domain = documentDomain;
+		}
+
+		return () => {
+			if (window.__meteor_runtime_config__.ROOT_URL.replace(/\/$/, '') !== currentUrl) {
+				imperativeModal.close();
+			}
+		};
+	}, [currentUrl, documentDomain, siteUrlMutation, siteUrl, userId, isAdmin]);
+};
