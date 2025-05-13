dc20ec44dbd562c1b2cb476ae9398ad53d8d1fbe
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-05-13 14:13:05 -0300

    chore: removed useWebRTC

--- apps/meteor/client/views/root/MainLayout/LoggedInArea.tsx
+++ apps/meteor/client/views/root/MainLayout/LoggedInArea.tsx
@@ -9,7 +9,6 @@ import { useForceLogout } from '../hooks/useForceLogout';
 import { useOTRMessaging } from '../hooks/useOTRMessaging';
 import { useStoreCookiesOnLogin } from '../hooks/useStoreCookiesOnLogin';
 import { useUpdateVideoConfUser } from '../hooks/useUpdateVideoConfUser';
-import { useWebRTC } from '../hooks/useWebRTC';
 
 const LoggedInArea = ({ children }: { children: ReactNode }) => {
 	const user = useUser();
@@ -20,7 +19,6 @@ const LoggedInArea = ({ children }: { children: ReactNode }) => {
 
 	useNotifyUser(user);
 	useUpdateVideoConfUser(user._id);
-	useWebRTC(user._id);
 	useOTRMessaging(user._id);
 	useNotificationUserCalendar(user);
 	useForceLogout(user._id);
