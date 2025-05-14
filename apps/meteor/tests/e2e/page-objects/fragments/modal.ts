12c9c4fc5c678ba9332729bd36d9e7ed6cca108a
Tasso <tasso.evangelista@rocket.chat>
2025-05-14 20:24:23 -0300

    Lint

--- apps/meteor/tests/e2e/page-objects/fragments/modal.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/modal.ts
@@ -1,10 +1,11 @@
 import type { Locator } from '@playwright/test';
+
 import { expect } from '../../utils/test';
 
 export abstract class Modal {
-  constructor(protected root: Locator) { }
+	constructor(protected root: Locator) {}
 
-  waitForDismissal() {
-    return expect(this.root).not.toBeVisible();
-  }
+	waitForDismissal() {
+		return expect(this.root).not.toBeVisible();
+	}
 }
