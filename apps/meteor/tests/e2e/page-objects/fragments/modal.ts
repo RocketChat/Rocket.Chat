a909d727578043376a5b2b2571a368313ec25a33
Tasso <tasso.evangelista@rocket.chat>
2025-05-14 18:26:30 -0300

    Add and use more page objects (and his fragments)

---
+++ apps/meteor/tests/e2e/page-objects/fragments/modal.ts
@@ -0,0 +1,10 @@
+import type { Locator } from '@playwright/test';
+import { expect } from '../../utils/test';
+
+export abstract class Modal {
+  constructor(protected root: Locator) { }
+
+  waitForDismissal() {
+    return expect(this.root).not.toBeVisible();
+  }
+}
