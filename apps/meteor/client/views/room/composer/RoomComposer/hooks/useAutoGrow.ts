eba44e4b711ec12803753421167fb2c30d4829f9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-05-15 14:39:46 -0300

    review

--- apps/meteor/client/views/room/composer/RoomComposer/hooks/useAutoGrow.ts
+++ apps/meteor/client/views/room/composer/RoomComposer/hooks/useAutoGrow.ts
@@ -1,3 +1,4 @@
+import { useSafeRefCallback } from '@rocket.chat/ui-client';
 import type { CSSProperties, MutableRefObject, RefCallback } from 'react';
 import { useCallback } from 'react';
 
@@ -8,26 +9,33 @@ export const useAutoGrow = (
 	textAreaStyle: CSSProperties;
 	autoGrowRef: RefCallback<HTMLTextAreaElement>;
 } => {
-	const autoGrowRef = useCallback(
-		(node: HTMLTextAreaElement) => {
-			if (!node) {
-				return;
-			}
-
-			ref.current = node;
-
-			node.addEventListener('input', (e) => {
-				const element = e.target as HTMLTextAreaElement;
-				element.style.height = '52px';
-				element.style.height = `${element.scrollHeight}px`;
-
-				// Keep cursor on the bottom when inserting a new line
-				if (element.scrollHeight > element.clientHeight) {
-					element.scrollTop = element.scrollHeight;
+	const autoGrowRef = useSafeRefCallback(
+		useCallback(
+			(node: HTMLTextAreaElement) => {
+				if (!node) {
+					return;
 				}
-			});
-		},
-		[ref],
+
+				ref.current = node;
+
+				const resize = () => {
+					node.style.height = '0';
+					node.style.height = `${node.scrollHeight}px`;
+				};
+
+				const resizeObserver = new ResizeObserver(resize);
+
+				resizeObserver.observe(node);
+
+				node.addEventListener('input', resize);
+
+				return () => {
+					resizeObserver.disconnect();
+					node.removeEventListener('input', resize);
+				};
+			},
+			[ref],
+		),
 	);
 
 	return {
