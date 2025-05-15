dcb116e8ef9fbc5edf57096c56c07119aef0ffde
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-05-15 15:05:10 -0300

    Refactor: Apply review suggestions to useUnread hook

--- apps/meteor/client/hooks/useUnread.ts
+++ apps/meteor/client/hooks/useUnread.ts
@@ -2,8 +2,8 @@ import { manageFavicon } from '@rocket.chat/favicon';
 import { useSession, useSessionDispatch, useUserPreference, useUserSubscriptions } from '@rocket.chat/ui-contexts';
 import { useEffect } from 'react';
 
+import { useFireGlobalEvent } from './useFireGlobalEvent';
 import { Rooms } from '../../app/models/client';
-import { fireGlobalEvent } from '../lib/utils/fireGlobalEvent';
 
 const query = { open: { $ne: false }, hideUnreadStatus: { $ne: true }, archived: { $ne: true } };
 const options = { fields: { unread: 1, alert: 1, rid: 1, t: 1, name: 1, ls: 1, unreadAlert: 1, fname: 1, prid: 1 } };
@@ -13,6 +13,9 @@ export const useUnread = () => {
 	const setUnread = useSessionDispatch('unread');
 	const unread = useSession('unread') as string | number | null | undefined;
 
+	const { mutate: fireEventUnreadChangedBySubscription } = useFireGlobalEvent('unread-changed-by-subscription');
+	const { mutate: fireEventUnreadChanged } = useFireGlobalEvent('unread-changed');
+
 	const subscriptions = useUserSubscriptions(query, options);
 
 	useEffect(() => {
@@ -20,7 +23,7 @@ export const useUnread = () => {
 
 		const unreadCount = subscriptions.reduce((ret, subscription) => {
 			const room = Rooms.findOne({ _id: subscription.rid }, { fields: { usersCount: 1 } });
-			fireGlobalEvent('unread-changed-by-subscription', {
+			fireEventUnreadChangedBySubscription({
 				...subscription,
 				usersCount: room?.usersCount,
 			});
@@ -48,11 +51,11 @@ export const useUnread = () => {
 		} else {
 			setUnread('');
 		}
-		fireGlobalEvent('unread-changed', unreadCount);
-	}, [setUnread, subscriptions, unread, unreadAlertEnabled]);
+		fireEventUnreadChanged(unreadCount);
+	}, [setUnread, unread, subscriptions, unreadAlertEnabled, fireEventUnreadChangedBySubscription, fireEventUnreadChanged]);
 
 	useEffect(() => {
 		const updateFavicon = manageFavicon();
 		updateFavicon(unread);
-	}, [unread, setUnread]);
+	}, [unread]);
 };
