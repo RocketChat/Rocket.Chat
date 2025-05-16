087cbc91acf376e8939eb537e58a937d5c4991eb
samir0607 <samir.gupta987@gmail.com>
2025-05-16 23:20:48 +0530

    Refactor: Use relative import

--- apps/meteor/client/views/room/composer/ComposerBoxPopup.tsx
+++ apps/meteor/client/views/room/composer/ComposerBoxPopup.tsx
@@ -4,7 +4,7 @@ import type { UseQueryResult } from '@tanstack/react-query';
 import type { ReactElement } from 'react';
 import { useEffect, memo, useMemo, useRef, useId } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CustomScrollbars } from '/client/components/CustomScrollbars';
+import { CustomScrollbars } from '../../../components/CustomScrollbars';
 
 export type ComposerBoxPopupProps<
 	T extends {
