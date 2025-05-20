51d2016a14bddc5dd89e645605e49cf42de3d837
MartinSchoeler <martinschoeler8@gmail.com>
2025-05-20 11:39:11 -0300

    review: update tests

--- apps/meteor/client/views/room/contextualBar/Threads/ThreadList.spec.tsx
+++ apps/meteor/client/views/room/contextualBar/Threads/ThreadList.spec.tsx
@@ -4,7 +4,6 @@ import { VirtuosoMockContext } from 'react-virtuoso';
 
 import ThreadList from './ThreadList';
 import { createFakeRoom } from '../../../../../tests/mocks/data';
-import { AsyncStatePhase } from '../../../../lib/asyncState';
 
 const fakeRoom = createFakeRoom({ t: 'c' });
 
@@ -19,22 +18,16 @@ jest.mock('../../../../../app/utils/lib/i18n', () => ({
 	t: (key: string) => key,
 }));
 
-jest.mock('../../../../hooks/lists/useRecordList', () => ({
-	useRecordList: () => ({
-		phase: AsyncStatePhase.REJECTED,
-		items: [],
-		itemCount: 0,
-		error: { success: false, error: 'error-not-allowed', errorType: 'error-not-allowed' },
-	}),
-}));
-
 describe('ThreadList Component', () => {
 	it('should display an error message when in error state', async () => {
 		render(<ThreadList />, {
 			wrapper: mockAppRoot()
 				.withRoom(fakeRoom)
-				// @ts-expect-error this is a mock
-				.withEndpoint('GET', '/v1/chat.getThreadsList', jest.fn)
+				.withEndpoint(
+					'GET',
+					'/v1/chat.getThreadsList',
+					jest.fn().mockRejectedValue({ success: false, error: 'error-not-allowed', errorType: 'error-not-allowed' }),
+				)
 				.wrap((children) => (
 					<VirtuosoMockContext.Provider value={{ viewportHeight: 300, itemHeight: 100 }}>{children}</VirtuosoMockContext.Provider>
 				))
