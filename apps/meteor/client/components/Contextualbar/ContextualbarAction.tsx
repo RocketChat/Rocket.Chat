dd7a46a4600d3479ee4a8c4b94bd5bd103eb080b
yash-rajpal <rajpal.yash03@gmail.com>
2025-05-21 21:36:18 +0530

    auto focus close contextual bar when navigating using keyboard

--- apps/meteor/client/components/Contextualbar/ContextualbarAction.tsx
+++ apps/meteor/client/components/Contextualbar/ContextualbarAction.tsx
@@ -1,17 +1,21 @@
 import { ContextualbarAction as ContextualbarActionComponent, ContextualbarV2Action } from '@rocket.chat/fuselage';
 import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '@rocket.chat/ui-client';
 import type { ComponentProps } from 'react';
-import { memo } from 'react';
+import { forwardRef, memo } from 'react';
 
-const ContextualbarAction = (props: ComponentProps<typeof ContextualbarActionComponent>) => (
-	<FeaturePreview feature='newNavigation'>
-		<FeaturePreviewOff>
-			<ContextualbarActionComponent {...props} />
-		</FeaturePreviewOff>
-		<FeaturePreviewOn>
-			<ContextualbarV2Action {...props} />
-		</FeaturePreviewOn>
-	</FeaturePreview>
+const ContextualbarAction = forwardRef<HTMLElement, ComponentProps<typeof ContextualbarActionComponent>>(
+	function ContextualbarAction(props, ref) {
+		return (
+			<FeaturePreview feature='newNavigation'>
+				<FeaturePreviewOff>
+					<ContextualbarActionComponent {...props} ref={ref} />
+				</FeaturePreviewOff>
+				<FeaturePreviewOn>
+					<ContextualbarV2Action {...props} ref={ref} />
+				</FeaturePreviewOn>
+			</FeaturePreview>
+		);
+	},
 );
 
 export default memo(ContextualbarAction);
--- apps/meteor/client/components/Contextualbar/ContextualbarClose.tsx
+++ apps/meteor/client/components/Contextualbar/ContextualbarClose.tsx
@@ -1,14 +1,19 @@
+import { useAutoFocus } from '@rocket.chat/fuselage-hooks';
 import type { ComponentProps, ReactElement } from 'react';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import ContextualbarAction from './ContextualbarAction';
+import { isKeyboardNavigationActive } from '../../lib/utils/isKeyboardNavigationActive';
 
 type ContextualbarCloseProps = Partial<ComponentProps<typeof ContextualbarAction>>;
 
 const ContextualbarClose = (props: ContextualbarCloseProps): ReactElement => {
 	const { t } = useTranslation();
-	return <ContextualbarAction data-qa='ContextualbarActionClose' {...props} aria-label={t('Close')} name='cross' />;
+	const shouldAutoFocus = useMemo(() => isKeyboardNavigationActive(), []);
+	const autoFocusRef = useAutoFocus<HTMLElement>(shouldAutoFocus);
+
+	return <ContextualbarAction data-qa='ContextualbarActionClose' {...props} aria-label={t('Close')} name='cross' ref={autoFocusRef} />;
 };
 
 export default memo(ContextualbarClose);
