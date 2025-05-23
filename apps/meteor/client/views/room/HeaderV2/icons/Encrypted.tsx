e3e37cf1cae0fd6408392551b8da41b33144462a
Sandra Nymark-Brand <sandra@nymark.nu>
2025-05-23 10:46:14 +0200

    fix: (WCAG 4.1.3) apply status-font-on-success to HeaderV2 Encrypted icon

--- apps/meteor/client/views/room/HeaderV2/icons/Encrypted.tsx
+++ apps/meteor/client/views/room/HeaderV2/icons/Encrypted.tsx
@@ -1,5 +1,4 @@
 import type { IRoom } from '@rocket.chat/core-typings';
-import colors from '@rocket.chat/fuselage-tokens/colors';
 import { useSetting } from '@rocket.chat/ui-contexts';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -9,7 +8,7 @@ import { HeaderState } from '../../../../components/Header';
 const Encrypted = ({ room }: { room: IRoom }) => {
 	const { t } = useTranslation();
 	const e2eEnabled = useSetting('E2E_Enable');
-	return e2eEnabled && room?.encrypted ? <HeaderState title={t('Encrypted')} icon='key' color={colors.g500} /> : null;
+	return e2eEnabled && room?.encrypted ? <HeaderState title={t('Encrypted')} icon='key' color='status-font-on-success' /> : null;
 };
 
 export default memo(Encrypted);
