dc09f168198da487aaeb154d13a9ca9ce830dd5d
Diego Sampaio <chinello@gmail.com>
2025-05-23 19:10:51 -0300

    fix: prometheus' REST API metrics (#36070)

--- apps/meteor/app/api/server/middlewares/metrics.ts
+++ apps/meteor/app/api/server/middlewares/metrics.ts
@@ -4,21 +4,37 @@ import type { Summary } from 'prom-client';
 import type { CachedSettings } from '../../../settings/server/CachedSettings';
 import type { APIClass } from '../api';
 
-export const metricsMiddleware =
-	(api: APIClass, settings: CachedSettings, summary: Summary) => async (req: Request, res: Response, next: NextFunction) => {
-		const { method, path } = req;
-
-		const rocketchatRestApiEnd = summary.startTimer({
-			method,
-			version: api.version,
-			...(settings.get('Prometheus_API_User_Agent') && { user_agent: req.headers['user-agent'] }),
-			entrypoint: path.startsWith('method.call') ? decodeURIComponent(req.url.slice(8)) : path,
-		});
+export const metricsMiddleware = ({
+	basePathRegex,
+	api,
+	settings,
+	summary,
+}: {
+	basePathRegex?: RegExp;
+	api: APIClass;
+	settings: CachedSettings;
+	summary: Summary;
+}) => {
+	return (req: Request, res: Response, next: NextFunction): void => {
+		const rocketchatRestApiEnd = summary.startTimer();
 
 		res.once('finish', () => {
+			const { method, path, route } = req;
+
+			const routePath = route?.path || path;
+
+			// get rid of the base path (i.e.: /api/v1/)
+			const entrypoint = basePathRegex ? routePath.replace(basePathRegex, '') : routePath;
+
 			rocketchatRestApiEnd({
 				status: res.statusCode,
+				method: method.toLowerCase(),
+				version: api.version,
+				...(settings.get('Prometheus_API_User_Agent') && { user_agent: req.headers['user-agent'] }),
+				entrypoint:
+					basePathRegex && entrypoint.startsWith('method.call') ? decodeURIComponent(path.replace(basePathRegex, '')) : entrypoint,
 			});
 		});
 		next();
 	};
+};
