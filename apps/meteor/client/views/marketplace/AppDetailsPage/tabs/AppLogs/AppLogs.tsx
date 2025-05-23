03f6327ab707986bb80fa86d700632413aca6224
MartinSchoeler <martinschoeler8@gmail.com>
2025-05-23 10:54:26 -0300

    feat: Add Pagination to App logs page

--- apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppLogs/AppLogs.tsx
+++ apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppLogs/AppLogs.tsx
@@ -1,8 +1,10 @@
-import { Accordion, Box } from '@rocket.chat/fuselage';
+import { Accordion, Box, Pagination } from '@rocket.chat/fuselage';
 import type { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AppLogsItem from './AppLogsItem';
+import { GenericTable } from '../../../../../components/GenericTable';
+import { usePagination } from '../../../../../components/GenericTable/hooks/usePagination';
 import { useFormatDateAndTime } from '../../../../../hooks/useFormatDateAndTime';
 import AccordionLoading from '../../../components/AccordionLoading';
 import { useLogs } from '../../../hooks/useLogs';
@@ -10,28 +12,40 @@ import { useLogs } from '../../../hooks/useLogs';
 const AppLogs = ({ id }: { id: string }): ReactElement => {
 	const { t } = useTranslation();
 	const formatDateAndTime = useFormatDateAndTime();
-	const { data, isSuccess, isError, isLoading } = useLogs(id);
+	const { current, itemsPerPage, setItemsPerPage: onSetItemsPerPage, setCurrent: onSetCurrent, ...paginationProps } = usePagination();
+	const { data, isSuccess, isError, isLoading } = useLogs({ appId: id, current, itemsPerPage });
 
 	return (
 		<>
-			{isLoading && <AccordionLoading />}
-			{isError && (
-				<Box maxWidth='x600' alignSelf='center'>
-					{t('App_not_found')}
-				</Box>
-			)}
-			{isSuccess && (
-				<Accordion width='100%' alignSelf='center'>
-					{data?.logs?.map((log) => (
-						<AppLogsItem
-							key={log._createdAt}
-							title={`${formatDateAndTime(log._createdAt)}: "${log.method}" (${log.totalTime}ms)`}
-							instanceId={log.instanceId}
-							entries={log.entries}
-						/>
-					))}
-				</Accordion>
-			)}
+			<GenericTable>
+				{isLoading && <AccordionLoading />}
+				{isError && (
+					<Box maxWidth='x600' alignSelf='center'>
+						{t('App_not_found')}
+					</Box>
+				)}
+				{isSuccess && (
+					<Accordion width='100%' alignSelf='center'>
+						{data?.logs?.map((log) => (
+							<AppLogsItem
+								key={log._createdAt}
+								title={`${formatDateAndTime(log._createdAt)}: "${log.method}" (${log.totalTime}ms)`}
+								instanceId={log.instanceId}
+								entries={log.entries}
+							/>
+						))}
+					</Accordion>
+				)}
+			</GenericTable>
+			<Pagination
+				divider
+				current={current}
+				itemsPerPage={itemsPerPage}
+				count={data?.total || 0}
+				onSetItemsPerPage={onSetItemsPerPage}
+				onSetCurrent={onSetCurrent}
+				{...paginationProps}
+			/>
 		</>
 	);
 };
