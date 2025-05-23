57a3aa2cc57bd554b02bf81d7492ae848c32dd0c
Diego Sampaio <chinello@gmail.com>
2025-05-23 15:27:54 -0300

    fix: prometheus' rest api metrics

--- apps/meteor/app/api/server/middlewares/metrics.ts
+++ apps/meteor/app/api/server/middlewares/metrics.ts
@@ -7,17 +7,20 @@ import type { APIClass } from '../api';
 export const metricsMiddleware =
 	(api: APIClass, settings: CachedSettings, summary: Summary): MiddlewareHandler =>
 	async (c, next) => {
-		const { method, path } = c.req;
-
-		const rocketchatRestApiEnd = summary.startTimer({
-			method,
-			version: api.version,
-			...(settings.get('Prometheus_API_User_Agent') && { user_agent: c.req.header('user-agent') }),
-			entrypoint: path.startsWith('method.call') ? decodeURIComponent(c.req.url.slice(8)) : path,
-		});
+		const rocketchatRestApiEnd = summary.startTimer();
 
 		await next();
+
+		const { method, path, routePath } = c.req;
+
+		// get rid of the base path (i.e.: /api/v1/)
+		const entrypoint = routePath.slice(8);
+
 		rocketchatRestApiEnd({
 			status: c.res.status,
+			method: method.toLowerCase(),
+			version: api.version,
+			...(settings.get('Prometheus_API_User_Agent') && { user_agent: c.req.header('user-agent') }),
+			entrypoint: entrypoint.startsWith('method.call') ? decodeURIComponent(path.slice(8)) : entrypoint,
 		});
 	};
