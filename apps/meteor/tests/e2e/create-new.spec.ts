40b6c3f9ba7cd8d24595735a9d3bdba4c128840d
Matheus Cardoso <matheus@cardo.so>
2025-05-27 16:56:21 -0300

    wip: dont refocus on PageHeaderNoShadow

--- apps/meteor/tests/e2e/create-new.spec.ts
+++ apps/meteor/tests/e2e/create-new.spec.ts
@@ -3,31 +3,48 @@ import type { Page } from '@playwright/test';
 import { Users } from './fixtures/userStates';
 import { test, expect } from './utils/test';
 
-test.use({ storageState: Users.admin.state, video: 'on' });
+test.use({ storageState: Users.admin.state, bypassCSP: true, headless: false, channel: 'chromium' });
 
 test.describe('menu-create-new', () => {
 	let page: Page;
 
 	test.beforeEach(async ({ browser }) => {
-		const context = await browser.newContext();
-		page = await context.newPage();
+		const reactDevTools = await fetch('http://localhost:8097');
+		if (reactDevTools.ok) {
+			const content = await reactDevTools.text();
+			// Create a new browser context with bypassCSP enabled
+			// This is necessary to allow the React DevTools to inject its script into the page
+			// and to avoid CSP issues that might prevent the script from loading.
+			const context = await browser.newContext({ bypassCSP: true });
+			page = await context.newPage();
 
-		await page.route('**/*', async (route) => {
-			await new Promise((f) => setTimeout(f, 100));
-			await route.continue();
+			await page.addInitScript({ content });
+		}
+
+		// Create a new page in the context
+
+		const client = await page.context().newCDPSession(page);
+		await client.send('Network.clearBrowserCache');
+		await client.send('Network.emulateNetworkConditions', {
+			connectionType: 'cellular4g',
+			offline: false,
+			latency: 50,
+			downloadThroughput: 200 * 1024, // 100 Kbps
+			uploadThroughput: 200 * 1024, // 100 Kbps
 		});
 
-		await page.goto('/home');
+		await page.goto('/home', { waitUntil: 'domcontentloaded' });
+
+		await page.getByRole('alert', { name: 'loading' }).waitFor({ state: 'hidden' });
 	});
 
 	test.afterEach(async () => {
 		await page.close();
 	});
 
-	test.fail('expect create a direct room', async () => {
+	test('expect create a direct room', async () => {
 		await page.getByRole('button', { name: 'Create new' }).click();
-		await expect(page.getByRole('menu', { name: 'Create new' })).toBeVisible({ timeout: 1000 });
-		await page.getByRole('menuitem', { name: 'Direct message' }).click({ timeout: 1000 });
-		await expect(page.getByRole('dialog')).toHaveText('New direct message');
+		await page.getByRole('menuitem', { name: 'Direct message' }).click({ timeout: 100 });
+		await expect(page.getByRole('dialog')).toContainText('New direct message');
 	});
 });
