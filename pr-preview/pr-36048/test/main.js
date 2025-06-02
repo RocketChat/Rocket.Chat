46fdd3eb51b9da1f6335b505054180c4862ec614
dougfabris <dougfabris@users.noreply.github.com>
2025-06-02 15:40:43 +0000

    Deploy preview for PR 36048 🛫

--- pr-preview/pr-36048/test/main.js
+++ pr-preview/pr-36048/test/main.js
@@ -1,13 +1,4 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-import { dirname, join, resolve } from 'path';
+import { dirname, join } from 'path';
 function getAbsolutePath(value) {
     return dirname(require.resolve(join(value, 'package.json')));
 }
@@ -27,13 +18,6 @@ export const config = {
     typescript: {
         reactDocgen: 'react-docgen',
     },
-    docs: {},
-    webpackFinal: (config) => __awaiter(void 0, void 0, void 0, function* () {
-        var _a, _b;
-        // This is only needed because of Fontello
-        config.resolve = Object.assign(Object.assign({}, config.resolve), { roots: [...((_b = (_a = config.resolve) === null || _a === void 0 ? void 0 : _a.roots) !== null && _b !== void 0 ? _b : []), resolve(__dirname, '../../../apps/meteor/public')] });
-        return config;
-    }),
 };
 export default config;
 //# sourceMappingURL=main.js.map
