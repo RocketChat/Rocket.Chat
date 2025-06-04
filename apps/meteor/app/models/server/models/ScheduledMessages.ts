f732a7a559074df4cd1b5b19a4534b8f6abed919
sem22-dev <thotjj22@gmail.com>
2025-06-04 11:18:30 +0530

    feat: implement scheduled_messages model with scheduledAt index

---
+++ apps/meteor/app/models/server/models/ScheduledMessages.ts
@@ -0,0 +1,22 @@
+import type {IUser, IRoom } from '@rocket.chat/core-typings';
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+
+export interface IScheduledMessage {
+  _id: string;
+  t: 'scheduled_message';
+  rid: IRoom['_id'];
+  msg: string;
+  u: Pick<IUser, '_id' | 'username' | 'name'>;
+  ts: Date;
+  scheduledAt: Date;
+  _updatedAt: Date;
+  threadId?: string;
+  notifyAt?: Date;
+}
+
+export const ScheduledMessages = new Mongo.Collection<IScheduledMessage>('scheduled_messages');
+
+Meteor.startup(() => {
+  ScheduledMessages.createIndex({ scheduledAt: 1 });
+});
