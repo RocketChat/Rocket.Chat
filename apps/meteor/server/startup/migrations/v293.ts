6156015a4e5fae7f1f4aa4451af4d1116b909095
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-06-05 16:16:06 -0300

    chore: Update migration files and versioning for server startup migrations, removing deprecated migrations and adjusting minimum version to 318

--- apps/meteor/server/startup/migrations/v293.ts
+++
@@ -1,9 +0,0 @@
-import { upsertPermissions } from '../../../app/authorization/server/functions/upsertPermissions';
-import { addMigration } from '../../lib/migrations';
-
-addMigration({
-	version: 293,
-	async up() {
-		await upsertPermissions();
-	},
-});
