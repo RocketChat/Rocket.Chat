9d98d65d6370cd60ed28a54e02c4c4a237d64898
juliajforesti <juliajforesti@gmail.com>
2025-06-10 11:30:33 -0300

    refactor: handle callbacks in `useLogoutCleanup` in `LoggedInArea`

---
+++ apps/meteor/client/views/root/hooks/useLogoutCleanup.ts
@@ -0,0 +1,20 @@
+import { UserContext, useSetting } from '@rocket.chat/ui-contexts';
+import { useContext, useEffect } from 'react';
+
+import { closeAllRooms } from './closeAllRooms';
+import { purgeAllDrafts } from './purgeAllDrafts';
+import { useFireGlobalEvent } from '../../../hooks/useFireGlobalEvent';
+
+export const useLogoutCleanup = () => {
+	const { onLogout } = useContext(UserContext);
+	const logoutCustomScript = useSetting('Custom_Script_On_Logout');
+	const { mutate: fireLogoutCustomScript } = useFireGlobalEvent('Custom_Script_On_Logout');
+
+	useEffect(() => {
+		return onLogout(() => {
+			purgeAllDrafts();
+			closeAllRooms();
+			fireLogoutCustomScript(logoutCustomScript);
+		});
+	}, [fireLogoutCustomScript, logoutCustomScript, onLogout]);
+};
