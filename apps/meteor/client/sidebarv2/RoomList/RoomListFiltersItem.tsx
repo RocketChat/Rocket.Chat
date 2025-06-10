28c92bf5a396343817c807fccbb4063bde49668b
dougfabris <devfabris@gmail.com>
2025-06-10 20:41:12 -0300

    feat: add badges in filters items

--- apps/meteor/client/sidebarv2/RoomList/RoomListFiltersItem.tsx
+++ apps/meteor/client/sidebarv2/RoomList/RoomListFiltersItem.tsx
@@ -1,11 +1,11 @@
-import { Icon, SidebarV2Item, SidebarV2ItemTitle } from '@rocket.chat/fuselage';
+import { Icon, SidebarV2Item, SidebarV2ItemBadge, SidebarV2ItemTitle } from '@rocket.chat/fuselage';
 import type { Keys as IconName } from '@rocket.chat/icons';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-// import { useUnreadDisplay } from './hooks/useUnreadDisplay';
 import type { SidePanelFiltersKeys } from '../../views/navigation/contexts/RoomsNavigationContext';
-// import { useUnreadGroupData } from '../views/navigation/contexts/RoomsNavigationContext';
+import { useUnreadGroupData } from '../../views/navigation/contexts/RoomsNavigationContext';
+import { useUnreadDisplay } from '../hooks/useUnreadDisplay';
 
 type SidebarFiltersItemProps = {
 	title: SidePanelFiltersKeys;
@@ -16,14 +16,14 @@ type SidebarFiltersItemProps = {
 
 const RoomListFiltersItem = ({ title, selected, icon, onClick }: SidebarFiltersItemProps) => {
 	const { t } = useTranslation();
-	// const unreadGroupCount = useUnreadGroupData(title);
-	// const { unreadTitle, unreadVariant, showUnread, unreadCount } = useUnreadDisplay(unreadGroupCount);
+	const unreadGroupCount = useUnreadGroupData(title);
+	const { unreadTitle, unreadVariant, showUnread, unreadCount, highlightUnread: highlighted } = useUnreadDisplay(unreadGroupCount);
 
 	return (
 		<SidebarV2Item tabIndex={0} role='tab' selected={selected} onClick={onClick}>
 			<Icon size='x20' name={icon} />
-			<SidebarV2ItemTitle>{t(title)}</SidebarV2ItemTitle>
-			{/* {showUnread && (
+			<SidebarV2ItemTitle unread={highlighted}>{t(title)}</SidebarV2ItemTitle>
+			{showUnread && (
 				<SidebarV2ItemBadge
 					variant={unreadVariant}
 					title={unreadTitle}
@@ -32,7 +32,7 @@ const RoomListFiltersItem = ({ title, selected, icon, onClick }: SidebarFiltersI
 				>
 					<span aria-hidden>{unreadCount.total}</span>
 				</SidebarV2ItemBadge>
-			)} */}
+			)}
 		</SidebarV2Item>
 	);
 };
