fac934e4fee549de2a4509b55d3f14123ed10dcf
Douglas Gubert <douglas.gubert@gmail.com>
2025-06-11 13:30:40 -0300

    Fix app logs export validation function

--- packages/rest-typings/src/apps/appLogsExportProps.ts
+++ packages/rest-typings/src/apps/appLogsExportProps.ts
@@ -2,22 +2,21 @@ import type { AppLogsProps } from './appLogsProps';
 import { ajv } from '../v1/Ajv';
 
 export type AppLogsExportProps = Omit<AppLogsProps, 'offset' | 'query'> & {
-	type: 'json' | 'plain_text';
+	type: 'json' | 'csv';
 };
 
 const AppLogsExportPropsSchema = {
 	type: 'object',
 	properties: {
-		appId: { type: 'string' },
 		logLevel: { type: 'string', enum: ['0', '1', '2'], nullable: true },
 		method: { type: 'string', nullable: true },
 		startDate: { type: 'string', format: 'date-time', nullable: true },
 		endDate: { type: 'string', format: 'date-time', nullable: true },
-		type: { type: 'string', enum: ['json', 'plain_text'] },
-		count: { type: 'number', minimum: 0, nullable: true },
+		type: { type: 'string', enum: ['json', 'csv'] },
+		count: { type: 'number', minimum: 1, nullable: true },
 		sort: { type: 'string', nullable: true },
 	},
-	required: ['appId', 'type'],
+	required: ['type'],
 	additionalProperties: false,
 };
 
