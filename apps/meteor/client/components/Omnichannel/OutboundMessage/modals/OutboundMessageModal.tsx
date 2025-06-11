c435acca9e39fee2e2fe4b7333c20768e716e2d5
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-11 19:09:43 -0300

    feat: OutboundMessageModal

---
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageModal.tsx
@@ -0,0 +1,21 @@
+import { Modal, ModalClose, ModalContent, ModalHeader, ModalTitle } from '@rocket.chat/fuselage';
+
+import { OutboundMessageWizard } from '../components/OutboundMessageStepWizard/OutboundMessageWizard';
+
+type OutboundMessageModalProps = {
+	onClose: () => void;
+};
+
+export const OutboundMessageModal = ({ onClose }: OutboundMessageModalProps) => {
+	return (
+		<Modal>
+			<ModalHeader>
+				<ModalTitle>Outbound Message</ModalTitle>
+				<ModalClose onClick={onClose} />
+			</ModalHeader>
+			<ModalContent pbe={16}>
+				<OutboundMessageWizard defaultValues={{}} />
+			</ModalContent>
+		</Modal>
+	);
+};
