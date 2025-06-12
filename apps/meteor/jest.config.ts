474e4f5bd8dca5a6a663eb251d90bcd593f00040
Matheus Cardoso <matheus@cardo.so>
2025-06-12 11:16:34 -0300

    chore: remove unused params from applyStatusChange

--- apps/meteor/jest.config.ts
+++ apps/meteor/jest.config.ts
@@ -41,6 +41,7 @@ export default {
 				'<rootDir>/server/cron/**.spec.ts',
 				'<rootDir>/app/api/server/**.spec.ts',
 				'<rootDir>/app/api/server/middlewares/**.spec.ts',
+				'<rootDir>/server/services/calendar/**/*.spec.ts',
 			],
 			coveragePathIgnorePatterns: ['/node_modules/'],
 		},
