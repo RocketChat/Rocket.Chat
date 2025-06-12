79d4ff5b7f54788bfc6b255346e99a651a3cfbac
MartinSchoeler <martinschoeler8@gmail.com>
2025-06-12 16:11:19 -0300

    chore: `style` -> `css-in-js`

--- apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppLogs/Components/CollapsibleRegion.tsx
+++ apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppLogs/Components/CollapsibleRegion.tsx
@@ -1,3 +1,4 @@
+import { css } from '@rocket.chat/css-in-js';
 import { Box } from '@rocket.chat/fuselage';
 import type { ComponentProps, ReactNode } from 'react';
 
@@ -8,7 +9,17 @@ type CollapsibleRegionProps = {
 
 export const CollapsibleRegion = ({ children, expanded, ...props }: CollapsibleRegionProps) => {
 	return (
-		<Box {...props} maxHeight={expanded ? 'fit-content' : 0} style={{ transition: 'all 0.18s ease', overflowY: 'hidden' }} is='dd'>
+		<Box
+			{...props}
+			maxHeight={expanded ? 'fit-content' : 0}
+			className={[
+				css`
+					transition: all 0.18s ease;
+				`,
+			]}
+			overflowY='hidden'
+			is='dd'
+		>
 			<Box role='region'>{children}</Box>
 		</Box>
 	);
