fc14a344cdc3b22611b536d4c1abc4335186a437
lucas-a-pelegrino <lucas-a-pelegrino@users.noreply.github.com>
2025-06-16 16:34:06 +0000

    Deploy preview for PR 36198 🛫

--- pr-preview/pr-36198/uikit-playground/assets/KatexBlock-BL6lQJ9L.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as o}from"./index-5u9uVt97.js";import{k as a}from"./katex-DvRmkopC.js";const m=({code:r})=>{const t=e.useMemo(()=>a.renderToString(r,{displayMode:!0,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[r]);return o.jsx("div",{role:"math",style:{overflowX:"auto"},"aria-label":r,dangerouslySetInnerHTML:{__html:t}})};export{m as default};
