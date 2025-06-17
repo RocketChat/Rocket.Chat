f4fe76cf5c4f1810cbf72d3b433c91e28eba684d
Jean Brito <jeanfbrito@gmail.com>
2025-06-17 19:37:07 -0300

    fix: email with whitespaces at end

--- packages/models/src/models/Users.ts
+++ packages/models/src/models/Users.ts
@@ -2859,7 +2859,7 @@ export class UsersRaw extends BaseRaw<IUser, DefaultFields<IUser>> implements IU
 			$set: {
 				emails: [
 					{
-						address: email,
+						address: email.trim(),
 						verified,
 					},
 				],
