5e43fd487fb96a067b1312c6e8ddae2c7f1be89e
Abhinav Kumar <abhinav@avitechlab.com>
2025-06-18 01:30:22 +0530

    fix: channels/groups/im counters endpoints includes deactivated users in members
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/model-typings/src/models/IUsersModel.ts
+++ packages/model-typings/src/models/IUsersModel.ts
@@ -442,4 +442,6 @@ export interface IUsersModel extends IBaseModel<IUser> {
 	countUsersInRoles(roles: IRole['_id'][]): Promise<number>;
 	countAllUsersWithPendingAvatar(): Promise<number>;
 	findOneByIdAndRole(userId: IUser['_id'], role: string, options: FindOptions<IUser>): Promise<IUser | null>;
+	countActiveUsersByNotDMRoomId(rid: string): Promise<number>;
+	countActiveUsersByDMRoomId(rid: string): Promise<number>;
 }
