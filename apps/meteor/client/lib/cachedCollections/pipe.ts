94e18eca9115e28a3518f817713b56f0eb947caa
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-06-18 10:51:34 -0300

    refactor: simplify `pipe` implementation by using a `PipeFunction` type and merging functions

--- apps/meteor/client/lib/cachedCollections/pipe.ts
+++ apps/meteor/client/lib/cachedCollections/pipe.ts
@@ -2,48 +2,48 @@ interface IPipeReturn<D> {
 	slice(skip: number, limit: number): IPipeReturn<D>;
 	sortByField(fieldName: keyof D, direction?: 1 | -1): IPipeReturn<D>;
 	apply(arg: D[]): D[];
-	readonly acc: Array<(arg: D[]) => D[]>;
+	readonly acc: PipeFunction<D>;
 	pipe(p: IPipeReturn<D>): IPipeReturn<D>;
 }
 
-interface IPipeFunction {
-	<D>(acc?: Array<(arg: D[]) => D[]>): IPipeReturn<D>;
-}
+type PipeFunction<D> = (arg: D[]) => D[];
 
-export const pipe: IPipeFunction = <D>(acc: Array<(arg: D[]) => D[]> = []) => {
+const merge =
+	<D>(fn: PipeFunction<D>, inner: PipeFunction<D>): PipeFunction<D> =>
+	(args) =>
+		fn(inner(args));
+
+export const pipe = <D>(acc: PipeFunction<D> = (arg) => [...arg]) => {
 	return {
 		slice(skip = 0, limit: number) {
-			return pipe<D>([...acc, (arr: D[]) => arr.slice(skip, skip + limit)]);
+			return pipe<D>(merge<D>((arr) => arr.slice(skip, skip + limit), acc));
 		},
 
 		sortByField(fieldName: keyof D, direction: 1 | -1 = 1) {
-			return pipe<D>([
-				...acc,
-				(arr: D[]) =>
-					arr.sort((a, b) => {
-						const aValue = a[fieldName];
-						const bValue = b[fieldName];
+			return pipe<D>(
+				merge(
+					(arr: D[]) =>
+						arr.sort((a, b) => {
+							const aValue = a[fieldName];
+							const bValue = b[fieldName];
 
-						if (aValue === undefined || bValue === undefined) {
+							if (typeof aValue === 'string' && typeof bValue === 'string') {
+								return direction * aValue.localeCompare(bValue);
+							}
+							if (typeof aValue === 'number' && typeof bValue === 'number') {
+								return direction * (aValue - bValue);
+							}
 							return 0;
-						}
-						if (typeof aValue === 'string' && typeof bValue === 'string') {
-							return direction * aValue.localeCompare(bValue);
-						}
-						if (typeof aValue === 'number' && typeof bValue === 'number') {
-							return direction * (aValue - bValue);
-						}
-						return 0;
-					}),
-			]);
+						}),
+					acc,
+				),
+			);
 		},
 
-		apply(arg: D[]): D[] {
-			return acc.reduce((acc, fn) => fn([...acc]), arg);
-		},
+		apply: acc,
 		acc,
 		pipe(p: IPipeReturn<D>) {
-			return pipe<D>([...acc, ...p.acc]);
+			return pipe<D>(merge(p.acc, acc));
 		},
 	};
 };
