468a1d2ae2577643bef71be8a7d90d089e8d805f
Douglas Fabris <devfabris@gmail.com>
2025-06-17 22:44:34 -0300

    feat: Responsive navigation filters (#36227)

---
+++ apps/meteor/client/portals/SidebarPortal/SidebarPortalV2.tsx
@@ -0,0 +1,37 @@
+import { Box, AnimatedVisibility } from '@rocket.chat/fuselage';
+import { useLayout } from '@rocket.chat/ui-contexts';
+import type { ReactNode } from 'react';
+import { memo, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+type SidebarPortalProps = { children?: ReactNode };
+
+const SidebarPortal = ({ children }: SidebarPortalProps) => {
+	const sidebarRoot = document.getElementById('navigation-region');
+	const { sidebar } = useLayout();
+
+	useEffect(() => {
+		if (sidebarRoot) {
+			sidebar.setOverlayed(true);
+		}
+
+		return () => sidebar.setOverlayed(false);
+	}, [sidebar, sidebarRoot]);
+
+	if (!sidebarRoot) {
+		return null;
+	}
+
+	return (
+		<>
+			{createPortal(
+				<AnimatedVisibility visibility={AnimatedVisibility.UNHIDING}>
+					<Box className='rcx-sidebar flex-nav'>{children}</Box>
+				</AnimatedVisibility>,
+				sidebarRoot,
+			)}
+		</>
+	);
+};
+
+export default memo(SidebarPortal);
