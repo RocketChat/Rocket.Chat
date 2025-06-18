313a76836e4038e310774020a1baff62b222dd07
Douglas Gubert <douglas.gubert@gmail.com>
2025-06-18 17:16:52 -0300

    Add test

--- apps/meteor/ee/server/apps/communication/endpoints/appLogsDistinctInstanceHandler.ts
+++ apps/meteor/ee/server/apps/communication/endpoints/appLogsDistinctInstanceHandler.ts
@@ -2,6 +2,30 @@ import { ajv } from '@rocket.chat/rest-typings/src/v1/Ajv';
 
 import type { AppsRestApi } from '../rest';
 
+// This might be a good candidate for a default validator function exported by @rocket.chat/rest-typings
+const errorResponse = ajv.compile({
+	additionalProperties: false,
+	type: 'object',
+	properties: {
+		error: {
+			type: 'string',
+		},
+		status: {
+			type: 'string',
+			nullable: true,
+		},
+		message: {
+			type: 'string',
+			nullable: true,
+		},
+		success: {
+			type: 'boolean',
+			description: 'Indicates if the request was successful.',
+		},
+	},
+	required: ['success', 'error'],
+});
+
 export const registerAppLogsDistinctInstanceHandler = ({ api, _orch }: AppsRestApi) =>
 	void api.get(
 		'logs/instanceIds',
@@ -11,10 +35,15 @@ export const registerAppLogsDistinctInstanceHandler = ({ api, _orch }: AppsRestA
 			response: {
 				200: ajv.compile({
 					type: 'object',
-					properties: { instanceIds: { type: 'array', items: { type: 'string' } } },
-					required: ['instanceIds'],
+					properties: {
+						instanceIds: { type: 'array', items: { type: 'string' } },
+						success: { type: 'boolean' },
+					},
+					required: ['instanceIds', 'success'],
 					additionalProperties: false,
 				}),
+				401: errorResponse,
+				403: errorResponse,
 			},
 		},
 		async () => {
