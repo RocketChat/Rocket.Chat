1c7c0b06e5a3cad284feebb0be14957c794fde5c
juliajforesti <juliajforesti@gmail.com>
2025-06-23 20:11:40 -0300

    chore: adapt `Users` store usage

--- apps/meteor/client/meteorOverrides/userAndUsers.ts
+++ apps/meteor/client/meteorOverrides/userAndUsers.ts
@@ -1,6 +1,6 @@
 import { Users } from '../../app/models/client/models/Users';
 
-Meteor.users = Users as typeof Meteor.users;
+Meteor.users = Users as unknown as typeof Meteor.users;
 
 // overwrite Meteor.users collection so records on it don't get erased whenever the client reconnects to websocket
 Meteor.user = function user(): Meteor.User | null {
@@ -10,5 +10,6 @@ Meteor.user = function user(): Meteor.User | null {
 		return null;
 	}
 
-	return (Users.findOne({ _id: uid }) ?? null) as Meteor.User | null;
+	const user = Users.state.get(uid);
+	return (user ?? null) as Meteor.User | null;
 };
