e158344dbde3a73265d034095f16bec77c6d487b
yash-rajpal <rajpal.yash03@gmail.com>
2025-06-23 20:09:06 +0530

    remove hide avatar preference

--- apps/meteor/client/sidebarv2/Sidebar.tsx
+++ apps/meteor/client/sidebarv2/Sidebar.tsx
@@ -1,24 +1,16 @@
 import { SidebarV2 } from '@rocket.chat/fuselage';
-import { useUserPreference } from '@rocket.chat/ui-contexts';
 import { memo } from 'react';
 
 import SidebarRoomList from './RoomList';
 import SidebarFooter from './footer';
 import BannerSection from './sections/BannerSection';
 
-const Sidebar = () => {
-	const sidebarHideAvatar = !useUserPreference('sidebarDisplayAvatar');
-
-	return (
-		<SidebarV2
-			aria-label='sidebar'
-			className={['rcx-sidebar--main', 'rcx-sidebar', sidebarHideAvatar && 'rcx-sidebar--hide-avatar'].filter(Boolean).join(' ')}
-		>
-			<BannerSection />
-			<SidebarRoomList />
-			<SidebarFooter />
-		</SidebarV2>
-	);
-};
+const Sidebar = () => (
+	<SidebarV2 aria-label='sidebar' className={['rcx-sidebar--main', 'rcx-sidebar'].filter(Boolean).join(' ')}>
+		<BannerSection />
+		<SidebarRoomList />
+		<SidebarFooter />
+	</SidebarV2>
+);
 
 export default memo(Sidebar);
