92158e9261df0d5f8dd5736b02a5562ab214c33a
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-24 12:51:15 -0300

    feat: Replies step

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RepliesStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RepliesStep.tsx
@@ -1,16 +1,47 @@
-import { WizardActions, WizardBackButton, WizardNextButton } from '../../../../../Wizard';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useMutation } from '@tanstack/react-query';
+import type { MouseEvent } from 'react';
+import { useRef } from 'react';
+
+import WizardActions from '../../../../../Wizard/WizardActions';
+import WizardBackButton from '../../../../../Wizard/WizardBackButton';
+import WizardNextButton from '../../../../../Wizard/WizardNextButton';
+import type { RepliesFormData, RepliesFormSubmitPayload, RepliesFormRef } from '../forms/RepliesForm';
+import RepliesForm from '../forms/RepliesForm';
+import { FormRefNotSetError } from '../utils/errors';
 
 type RepliesStepProps = {
-	onSubmit(values: Record<string, string>): void;
+	defaultValues?: Partial<RepliesFormData>;
+	onSubmit(values: RepliesFormSubmitPayload): void;
 };
 
-const RepliesStep = (_: RepliesStepProps) => {
+const RepliesStep = ({ defaultValues, onSubmit }: RepliesStepProps) => {
+	const formRef = useRef<RepliesFormRef>({
+		submit: () => Promise.reject(new FormRefNotSetError()),
+	});
+
+	const nextMutation = useMutation({
+		mutationKey: ['outbound-message', 'replies', 'submit'],
+		mutationFn: () => formRef.current.submit(),
+		throwOnError: false,
+	});
+
+	const handleNextClick = useEffectEvent(async (event: MouseEvent<HTMLButtonElement>) => {
+		try {
+			const values = await nextMutation.mutateAsync();
+			onSubmit(values);
+		} catch {
+			event.preventDefault();
+		}
+	});
+
 	return (
 		<div>
-			Replies Content
+			<RepliesForm ref={formRef} defaultValues={defaultValues} />
+
 			<WizardActions>
 				<WizardBackButton />
-				<WizardNextButton />
+				<WizardNextButton loading={nextMutation.isPending} onClick={handleNextClick} />
 			</WizardActions>
 		</div>
 	);
