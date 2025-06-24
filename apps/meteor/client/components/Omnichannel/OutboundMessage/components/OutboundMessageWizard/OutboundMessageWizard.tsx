2dff27754e141e06a091dbf7299eecea87b513b9
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-24 17:44:12 -0300

    feat: outbound message upsell modal

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/OutboundMessageWizard.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/OutboundMessageWizard.tsx
@@ -1,13 +1,17 @@
 import { Box } from '@rocket.chat/fuselage';
 import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 
 import type { SubmitPayload } from './forms';
 import { ReviewStep, MessageStep, RecipientStep, RepliesStep } from './steps';
+import { useHasLicenseModule } from '../../../../../hooks/useHasLicenseModule';
 import GenericError from '../../../../GenericError';
 import Wizard, { useWizard, WizardContent, WizardTabs } from '../../../../Wizard';
+import useOutboundProvidersList from '../../hooks/useOutboundProvidersList';
+import { useOutboundMessageUpsellModal } from '../../modals';
+import OutboubdMessageWizardSkeleton from './components/OutboundMessageWizardSkeleton';
 
 type OutboundMessageWizardProps = {
 	defaultValues?: Partial<Pick<SubmitPayload, 'contactId' | 'providerId'>>;
@@ -15,7 +19,11 @@ type OutboundMessageWizardProps = {
 
 const OutboundMessageWizard = ({ defaultValues = {} }: OutboundMessageWizardProps) => {
 	const { t } = useTranslation();
+	const upsellModal = useOutboundMessageUpsellModal();
+	const hasModule = useHasLicenseModule('outbound-message');
+	const isLoadingModule = hasModule === 'loading';
 	const [, setState] = useState<Partial<SubmitPayload>>(defaultValues);
+	const { data: hasProviders, isLoading: isLoadingProviders } = useOutboundProvidersList({ select: (providers) => providers.length > 0 });
 
 	const wizardApi = useWizard({
 		steps: [
@@ -26,9 +34,24 @@ const OutboundMessageWizard = ({ defaultValues = {} }: OutboundMessageWizardProp
 		],
 	});
 
+	useEffect(() => {
+		if (!isLoadingProviders && !isLoadingModule && !hasModule && !hasProviders) {
+			upsellModal.open();
+		}
+	}, [hasModule, hasProviders, isLoadingModule, isLoadingProviders, upsellModal]);
+
 	const handleSubmit = useEffectEvent((values: SubmitPayload) => {
+		if (!hasModule) {
+			upsellModal.open();
+			return;
+		}
+
 		setState((state) => ({ ...state, ...values }));
 	});
+
+	if (isLoadingModule || isLoadingProviders) {
+		return <OutboubdMessageWizardSkeleton />;
+	}
 
 	return (
 		<ErrorBoundary fallbackRender={() => <GenericError icon='circle-exclamation' />}>
