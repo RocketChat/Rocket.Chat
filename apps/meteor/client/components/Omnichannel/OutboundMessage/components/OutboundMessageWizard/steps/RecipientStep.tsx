61aa78f9fc22ff43738f5d5d155360e79dd17ec8
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-24 12:48:09 -0300

    feat: Recipient step

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RecipientStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RecipientStep.tsx
@@ -1,17 +1,45 @@
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { WizardActions, WizardNextButton } from '@rocket.chat/ui-client';
+import { useMutation } from '@tanstack/react-query';
+import type { MouseEvent } from 'react';
+import { useRef } from 'react';
 
-import type { RecipientFormSubmitPayload } from '../forms/RecipientForm';
+import type { RecipientFormData, RecipientFormRef, RecipientFormSubmitPayload } from '../forms/RecipientForm';
+import RecipientForm from '../forms/RecipientForm';
+import { FormRefNotSetError } from '../utils/errors';
 
 type RecipientStepProps = {
+	defaultValues?: Partial<RecipientFormData>;
+	onDirty?: () => void;
 	onSubmit(values: RecipientFormSubmitPayload): void;
 };
 
-const RecipientStep = (_: RecipientStepProps) => {
+const RecipientStep = ({ defaultValues, onDirty, onSubmit }: RecipientStepProps) => {
+	const formRef = useRef<RecipientFormRef>({
+		submit: () => Promise.reject(new FormRefNotSetError()),
+	});
+
+	const nextMutation = useMutation({
+		mutationKey: ['outbound-message', 'recipient', 'submit'],
+		mutationFn: () => formRef.current.submit(),
+		throwOnError: false,
+	});
+
+	const handleNextClick = useEffectEvent(async (event: MouseEvent<HTMLButtonElement>) => {
+		try {
+			const values = await nextMutation.mutateAsync();
+			onSubmit(values);
+		} catch (error) {
+			event.preventDefault();
+		}
+	});
+
 	return (
 		<div>
-			Recipient Content
+			<RecipientForm ref={formRef} defaultValues={defaultValues} onDirty={onDirty} />
+
 			<WizardActions>
-				<WizardNextButton />
+				<WizardNextButton loading={nextMutation.isPending} onClick={handleNextClick} />
 			</WizardActions>
 		</div>
 	);
