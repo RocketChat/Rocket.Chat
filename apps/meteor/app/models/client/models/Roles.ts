c39a7cbff4e964a8fe124944acbc1ac70c35ce2b
juliajforesti <juliajforesti@gmail.com>
2025-06-24 10:26:30 -0300

    chore: `createDocumentMapStore` in `Roles` model

--- apps/meteor/app/models/client/models/Roles.ts
+++ apps/meteor/app/models/client/models/Roles.ts
@@ -1,17 +1,19 @@
 import type { IRole, IUser } from '@rocket.chat/core-typings';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import { Subscriptions } from './Subscriptions';
 import { Users } from './Users';
-import { MinimongoCollection } from '../../../../client/lib/cachedCollections/MinimongoCollection';
+import { createDocumentMapStore } from '../../../../client/lib/cachedCollections/DocumentMapStore';
 
-class RolesCollection extends MinimongoCollection<IRole> {
-	ready = new ReactiveVar(false);
-
-	isUserInRoles(userId: IUser['_id'], roles: IRole['_id'][] | IRole['_id'], scope?: string, ignoreSubscriptions = false) {
+/** @deprecated prefer fetching data from the REST API, listening to changes via streamer events, and storing the state in a Tanstack Query */
+export const Roles = {
+	use: createDocumentMapStore<IRole>(),
+	get state() {
+		return Roles.use.getState();
+	},
+	isUserInRoles: (userId: IUser['_id'], roles: IRole['_id'][] | IRole['_id'], scope?: string, ignoreSubscriptions = false) => {
 		roles = Array.isArray(roles) ? roles : [roles];
 		return roles.some((roleId) => {
-			const role = this.state.get(roleId);
+			const role = Roles.state.get(roleId);
 			const roleScope = ignoreSubscriptions ? 'Users' : role?.scope || 'Users';
 
 			switch (roleScope) {
@@ -25,8 +27,5 @@ class RolesCollection extends MinimongoCollection<IRole> {
 					return false;
 			}
 		});
-	}
-}
-
-/** @deprecated new code refer to Minimongo collections like this one; prefer fetching data from the REST API, listening to changes via streamer events, and storing the state in a Tanstack Query */
-export const Roles = new RolesCollection();
+	},
+};
