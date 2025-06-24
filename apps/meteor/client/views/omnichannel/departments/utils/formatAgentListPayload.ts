f7a4329827c79894ed8206e1fe81b58dc345416e
Kevin Aleman <kaleman960@gmail.com>
2025-06-24 07:57:09 -0600

    ui

--- apps/meteor/client/views/omnichannel/departments/utils/formatAgentListPayload.ts
+++ apps/meteor/client/views/omnichannel/departments/utils/formatAgentListPayload.ts
@@ -2,7 +2,7 @@ import type { IDepartmentAgent } from '../definitions';
 
 export const formatAgentListPayload = (oldAgentList: IDepartmentAgent[], newAgentList: IDepartmentAgent[]) => {
 	const upsert: IDepartmentAgent[] = [];
-	const remove: IDepartmentAgent[] = [];
+	const remove: Pick<IDepartmentAgent, 'agentId' | 'username'>[] = [];
 
 	for (const agent of newAgentList) {
 		const initialAgent = agent._id ? oldAgentList.find((initialAgent) => initialAgent._id === agent._id) : undefined;
@@ -14,7 +14,7 @@ export const formatAgentListPayload = (oldAgentList: IDepartmentAgent[], newAgen
 
 	for (const initialAgent of oldAgentList) {
 		if (!newAgentList.some((agent) => initialAgent._id === agent._id)) {
-			remove.push(initialAgent);
+			remove.push({ agentId: initialAgent.agentId, username: initialAgent.username });
 		}
 	}
 
