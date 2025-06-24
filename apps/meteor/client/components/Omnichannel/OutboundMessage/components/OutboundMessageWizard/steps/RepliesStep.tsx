924d770fc111bb5b85525d57f1351336bee0f908
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-24 12:51:15 -0300

    feat: Replies step

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RepliesStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RepliesStep.tsx
@@ -1,16 +1,45 @@
 import { WizardActions, WizardBackButton, WizardNextButton } from '@rocket.chat/ui-client';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useMutation } from '@tanstack/react-query';
+import type { MouseEvent } from 'react';
+import { useRef } from 'react';
+
+import type { RepliesFormData, RepliesFormSubmitPayload, RepliesFormRef } from '../forms/RepliesForm';
+import RepliesForm from '../forms/RepliesForm';
+import { FormRefNotSetError } from '../utils/errors';
 
 type RepliesStepProps = {
-	onSubmit(values: Record<string, string>): void;
+	defaultValues?: Partial<RepliesFormData>;
+	onSubmit(values: RepliesFormSubmitPayload): void;
 };
 
-const RepliesStep = (_: RepliesStepProps) => {
+const RepliesStep = ({ defaultValues, onSubmit }: RepliesStepProps) => {
+	const formRef = useRef<RepliesFormRef>({
+		submit: () => Promise.reject(new FormRefNotSetError()),
+	});
+
+	const nextMutation = useMutation({
+		mutationKey: ['outbound-message', 'replies', 'submit'],
+		mutationFn: () => formRef.current.submit(),
+		throwOnError: false,
+	});
+
+	const handleNextClick = useEffectEvent(async (event: MouseEvent<HTMLButtonElement>) => {
+		try {
+			const values = await nextMutation.mutateAsync();
+			onSubmit(values);
+		} catch {
+			event.preventDefault();
+		}
+	});
+
 	return (
 		<div>
-			Replies Content
+			<RepliesForm ref={formRef} defaultValues={defaultValues} />
+
 			<WizardActions>
 				<WizardBackButton />
-				<WizardNextButton />
+				<WizardNextButton loading={nextMutation.isPending} onClick={handleNextClick} />
 			</WizardActions>
 		</div>
 	);
