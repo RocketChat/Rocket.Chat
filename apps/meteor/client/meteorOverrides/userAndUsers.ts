80950ed5028cc29434a9d20d3b93f8e1f2dd226f
juliajforesti <juliajforesti@gmail.com>
2025-06-24 20:26:26 -0300

    fix: reactivity

--- apps/meteor/client/meteorOverrides/userAndUsers.ts
+++ apps/meteor/client/meteorOverrides/userAndUsers.ts
@@ -1,15 +1,29 @@
+import { Tracker } from 'meteor/tracker';
+
 import { Users } from '../../app/models/client/models/Users';
 
 Meteor.users = Users as unknown as typeof Meteor.users;
 
+const dep = new Tracker.Dependency();
+
 // overwrite Meteor.users collection so records on it don't get erased whenever the client reconnects to websocket
-Meteor.user = function user(): Meteor.User | null {
-	const uid = Meteor.userId();
+Tracker.autorun(() => {
+	Meteor.user = function user(): Meteor.User | null {
+		const uid = Meteor.userId();
 
-	if (!uid) {
-		return null;
-	}
+		if (!uid) {
+			return null;
+		}
 
-	const user = Users.state.get(uid);
-	return (user ?? null) as Meteor.User | null;
-};
+		dep.depend();
+		Users.use.subscribe((state, prevState) => {
+			if (state.has(uid) && prevState.has(uid)) {
+				return;
+			}
+			dep.changed();
+		});
+
+		const user = Users.state.get(uid);
+		return (user ?? null) as Meteor.User | null;
+	};
+});
