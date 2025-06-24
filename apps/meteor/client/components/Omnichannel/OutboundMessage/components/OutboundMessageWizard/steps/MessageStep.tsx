a3829d218cadb803a424c0edbe85bf0415196608
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-06-24 12:50:03 -0300

    feat: Message step

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/MessageStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/MessageStep.tsx
@@ -1,16 +1,44 @@
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { WizardActions, WizardBackButton, WizardNextButton } from '@rocket.chat/ui-client';
+import { useMutation } from '@tanstack/react-query';
+import type { ComponentProps, MouseEvent } from 'react';
+import { useRef } from 'react';
 
-type MessageStepProps = {
-	onSubmit(values: Record<string, string>): void;
+import type { MessageFormRef, MessageFormSubmitPayload } from '../forms/MessageForm';
+import MessageForm from '../forms/MessageForm';
+import { FormRefNotSetError } from '../utils/errors';
+
+type MessageStepProps = Omit<ComponentProps<typeof MessageForm>, 'onSubmit'> & {
+	onSubmit(values: MessageFormSubmitPayload): void;
 };
 
-const MessageStep = (_: MessageStepProps) => {
+const MessageStep = ({ contact, templates, defaultValues, onSubmit }: MessageStepProps) => {
+	const formRef = useRef<MessageFormRef>({
+		submit: () => Promise.reject(new FormRefNotSetError()),
+	});
+
+	const nextMutation = useMutation({
+		mutationKey: ['outbound-message', 'message', 'submit'],
+		mutationFn: () => formRef.current.submit(),
+		throwOnError: false,
+	});
+
+	const handleNextClick = useEffectEvent(async (event: MouseEvent<HTMLButtonElement>) => {
+		try {
+			const values = await nextMutation.mutateAsync();
+			onSubmit(values);
+		} catch {
+			event.preventDefault();
+		}
+	});
+
 	return (
 		<div>
-			Message Content
+			<MessageForm ref={formRef} contact={contact} templates={templates} defaultValues={defaultValues} />
+
 			<WizardActions>
 				<WizardBackButton />
-				<WizardNextButton />
+				<WizardNextButton loading={nextMutation.isPending} onClick={handleNextClick} />
 			</WizardActions>
 		</div>
 	);
