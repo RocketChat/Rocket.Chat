b5a8d81a9c9c82c0d41e02a264fd6ba92f5536e7
juliajforesti <juliajforesti@gmail.com>
2025-06-25 11:36:01 -0300

    .

--- apps/meteor/client/meteorOverrides/userAndUsers.ts
+++ apps/meteor/client/meteorOverrides/userAndUsers.ts
@@ -6,24 +6,31 @@ Meteor.users = Users as unknown as typeof Meteor.users;
 
 const dep = new Tracker.Dependency();
 
-// overwrite Meteor.users collection so records on it don't get erased whenever the client reconnects to websocket
-Tracker.autorun(() => {
-	Meteor.user = function user(): Meteor.User | null {
-		const uid = Meteor.userId();
-
-		if (!uid) {
-			return null;
-		}
-
-		dep.depend();
-		Users.use.subscribe((state, prevState) => {
-			if (state.has(uid) && prevState.has(uid)) {
-				return;
+Tracker.autorun((computation) => {
+	const currentUid = Meteor.userId();
+	if (currentUid) {
+		const unsubscribe = Users.use.subscribe((state, prevState) => {
+			if (state.has(currentUid) && prevState.has(currentUid)) {
+				dep.changed();
 			}
-			dep.changed();
 		});
-
-		const user = Users.state.get(uid);
-		return (user ?? null) as Meteor.User | null;
-	};
+		computation.onInvalidate(() => {
+			unsubscribe?.();
+		});
+		return;
+	}
+	dep.changed();
 });
+
+// overwrite Meteor.users collection so records on it don't get erased whenever the client reconnects to websocket
+Meteor.user = function user(): Meteor.User | null {
+	dep.depend();
+	const uid = Meteor.userId();
+
+	if (!uid) {
+		return null;
+	}
+
+	const user = Users.state.get(uid);
+	return (user ?? null) as Meteor.User | null;
+};
