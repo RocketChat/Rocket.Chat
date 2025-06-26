302d56238e471db67f2bccc46f2c56d7c951d660
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-06-26 16:26:05 +0300

    test: add tests for ExtractRoutesFromAPI to validate POST response types

--- apps/meteor/app/api/server/ApiClass.spec.ts
+++ apps/meteor/app/api/server/ApiClass.spec.ts
@@ -6,36 +6,72 @@ import type { APIClass, ExtractRoutesFromAPI } from './ApiClass';
 type Expect<T extends true> = T;
 type ShallowEqual<X, Y> = X extends Y ? (Y extends X ? true : false) : false;
 
-type API = APIClass<
-	'/v1',
-	{
-		method: 'GET';
-		path: '/v1/endpoint.test';
-		response: {
-			200: ValidateFunction<
-				PaginatedResult<{
-					sounds: string[];
-				}>
-			>;
-		};
-		query: ValidateFunction<{
-			query: string;
-		}>;
-		authRequired: true;
-	}
->;
-
-it('Should return the expected type', () => {
-	type test = Expect<
-		ShallowEqual<
-			ReturnType<ExtractRoutesFromAPI<API>['/v1/endpoint.test']['GET']>,
+describe('ExtractRoutesFromAPI type utility', () => {
+	it('Should return the expected type for GET', () => {
+		type API = APIClass<
+			'/v1',
 			{
-				count: number;
-				offset: number;
-				total: number;
-				sounds: string[];
+				method: 'GET';
+				path: '/v1/endpoint.test';
+				response: {
+					200: ValidateFunction<
+						PaginatedResult<{
+							sounds: string[];
+						}>
+					>;
+				};
+				query: ValidateFunction<{
+					query: string;
+				}>;
+				authRequired: true;
 			}
-		>
-	>;
-	true as test;
+		>;
+		type test = Expect<
+			ShallowEqual<
+				ReturnType<ExtractRoutesFromAPI<API>['/v1/endpoint.test']['GET']>,
+				{
+					count: number;
+					offset: number;
+					total: number;
+					sounds: string[];
+				}
+			>
+		>;
+
+		true as test;
+	});
+
+	it('Should return the expected type for POST', () => {
+		type API = APIClass<
+			'/v1',
+			{
+				method: 'POST';
+				path: '/v1/endpoint.test';
+				response: {
+					200: ValidateFunction<
+						PaginatedResult<{
+							sounds: string[];
+						}>
+					>;
+				};
+				body: ValidateFunction<{
+					query: string;
+				}>;
+				authRequired: true;
+			}
+		>;
+		type test = Expect<
+			ShallowEqual<
+				ReturnType<ExtractRoutesFromAPI<API>['/v1/endpoint.test']['POST']>,
+				{
+					count: number;
+					offset: number;
+					total: number;
+					sounds: string[];
+				}
+			>
+		>;
+
+		true as test;
+	});
 });
