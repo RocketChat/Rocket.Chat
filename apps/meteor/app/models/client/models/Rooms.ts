ef880596de49b3653f877566aa11de6ca79b502f
juliajforesti <juliajforesti@gmail.com>
2025-06-27 20:02:32 -0300

    chore: `createDocumentMapStore` in `Rooms` model

--- apps/meteor/app/models/client/models/Rooms.ts
+++ apps/meteor/app/models/client/models/Rooms.ts
@@ -1,4 +1,13 @@
+import type { IRoom } from '@rocket.chat/core-typings';
+import type { StoreApi, UseBoundStore } from 'zustand';
+
 import { CachedChatRoom } from './CachedChatRoom';
+import type { IDocumentMapStore } from '../../../../client/lib/cachedCollections/DocumentMapStore';
+
+type RoomsStore = {
+	use: UseBoundStore<StoreApi<IDocumentMapStore<IRoom>>>;
+	readonly state: IDocumentMapStore<IRoom>;
+};
 
 /** @deprecated new code refer to Minimongo collections like this one; prefer fetching data from the REST API, listening to changes via streamer events, and storing the state in a Tanstack Query */
-export const Rooms = CachedChatRoom.collection;
+export const Rooms = CachedChatRoom.collection as RoomsStore;
