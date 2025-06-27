ef123144da3bb5021dc5af4f4b8a0706194c0ff1
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-06-27 11:18:16 -0300

    refactor: adds more minor improvements to listOutboundPRoviders logic

--- apps/meteor/ee/app/livechat-enterprise/server/api/lib/outbound.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/api/lib/outbound.ts
@@ -1,3 +1,4 @@
+import type { IOutboundMessageProviders } from '@rocket.chat/apps-engine/definition/outboundComunication';
 import type { IOutboundProvider } from '@rocket.chat/core-typings';
 
 import { OutboundMessageProvider } from '../../../../../../server/lib/OutboundMessageProvider';
@@ -9,11 +10,24 @@ export class OutboundMessageProviderService {
 		this.provider = new OutboundMessageProvider();
 	}
 
-	public listOutboundProviders(type?: string): IOutboundMessageProviders[] {
-		if (type !== undefined && !['phone', 'email'].includes(type)) {
+	private isProviderValid(type?: string): type is 'phone' | 'email' {
+		return type === 'phone' || type === 'email';
+	}
+
+	public listOutboundProviders(type?: string): IOutboundProvider[] {
+		if (type !== undefined && !this.isProviderValid(type)) {
 			throw new Error('Invalid type');
 		}
 
-		return this.provider.getOutboundMessageProviders(type);
+		const providers = this.provider.getOutboundMessageProviders(type);
+
+		return providers.map<IOutboundProvider>((provider: IOutboundMessageProviders) => {
+			return {
+				providerId: provider.appId,
+				providerName: provider.name,
+				supportsTemplates: true,
+				providerType: provider.type,
+			};
+		});
 	}
 }
