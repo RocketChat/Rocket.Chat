8eb155316d732c988a1870b62154e50ef5b7271d
Marcos Defendi <marcos.defendi@rocket.chat>
2025-07-01 16:08:17 -0300

    feat: basic accept invite

--- ee/packages/federation-matrix/src/events/index.ts
+++ ee/packages/federation-matrix/src/events/index.ts
@@ -1,9 +1,12 @@
 import type { Emitter } from '@rocket.chat/emitter';
 import type { HomeserverEventSignatures } from '@rocket.chat/homeserver';
-import { ping } from './ping';
+
+import { invite } from './invite';
 import { message } from './message';
+import { ping } from './ping';
 
 export function registerEvents(emitter: Emitter<HomeserverEventSignatures>) {
-    ping(emitter);
-    message(emitter);
+	ping(emitter);
+	message(emitter);
+	invite(emitter);
 }
