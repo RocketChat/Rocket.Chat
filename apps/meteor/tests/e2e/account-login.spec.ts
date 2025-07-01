bbf5fa29ff6c2a6e24dec14ed6c1ea31809257e6
Matheus Cardoso <matheus@cardo.so>
2025-07-01 12:57:31 -0300

    fix: stale data after login expired

---
+++ apps/meteor/tests/e2e/account-login.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from './utils/test';
+import { Users, restoreState } from './fixtures/userStates';
+
+test.use({ storageState: Users.userE2EE.state });
+
+test.describe('Account Login', () => {
+	test.beforeAll(async ({ api }) => {
+		await api.post('/settings/E2E_Enable', { value: true });
+		await api.post('/settings/E2E_Allow_Unencrypted_Messages', { value: true });
+	});
+
+	test.afterAll(async ({ api }) => {
+		await api.post('/settings/E2E_Enable', { value: false });
+		await api.post('/settings/E2E_Allow_Unencrypted_Messages', { value: false });
+	});
+
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/home');
+		await restoreState(page, Users.userE2EE);
+	});
+
+	test('should remove private_key and public_key from localStorage on logout', async ({ page }) => {
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.userId'))).not.toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.loginToken'))).not.toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.loginTokenExpires'))).not.toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('private_key'))).not.toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('public_key'))).not.toBeNull();
+
+		await page.evaluate(() => {
+			localStorage.setItem('Meteor.loginTokenExpires', new Date().toString());
+		});
+
+		await page.reload();
+
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.userId'))).toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.loginToken'))).toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('Meteor.loginTokenExpires'))).toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('private_key'))).toBeNull();
+		expect(await page.evaluate(() => localStorage.getItem('public_key'))).toBeNull();
+	});
+});
