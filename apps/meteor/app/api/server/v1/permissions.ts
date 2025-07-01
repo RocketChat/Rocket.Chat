2f1a6361e354ea7a24bd481565ad5767c9b6d64b
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-07-01 14:55:50 +0300

    feat: permissions API with validation and response schemas

--- apps/meteor/app/api/server/v1/permissions.ts
+++ apps/meteor/app/api/server/v1/permissions.ts
@@ -1,17 +1,128 @@
 import type { IPermission } from '@rocket.chat/core-typings';
 import { Permissions, Roles } from '@rocket.chat/models';
-import { isBodyParamsValidPermissionUpdate } from '@rocket.chat/rest-typings';
+import { isBodyParamsValidPermissionUpdate, isPermissionsListAll } from '@rocket.chat/rest-typings';
+import { ajv } from '@rocket.chat/rest-typings/src/v1/Ajv';
 import { Meteor } from 'meteor/meteor';
 
 import { permissionsGetMethod } from '../../../authorization/server/streamer/permissions';
 import { notifyOnPermissionChangedById } from '../../../lib/server/lib/notifyListener';
 import { API } from '../api';
 
-API.v1.addRoute(
-	'permissions.listAll',
-	{ authRequired: true },
-	{
-		async get() {
+API.v1
+	.get(
+		'permissions.listAll',
+		{
+			authRequired: true,
+			query: isPermissionsListAll,
+			response: {
+				200: ajv.compile({
+					type: 'object',
+					properties: {
+						update: {
+							type: 'array',
+							items: {
+								type: 'object',
+								properties: {
+									_id: { type: 'string' },
+									_updatedAt: { type: 'object' },
+									roles: {
+										type: 'array',
+										items: { type: 'string' },
+									},
+									group: { type: 'string', nullable: true },
+									groupPermissionId: { type: 'string', nullable: true },
+									level: { type: 'string', enum: ['settings'], nullable: true },
+									section: { type: 'string', nullable: true },
+									sectionPermissionId: { type: 'string', nullable: true },
+									settingId: { type: 'string', nullable: true },
+									sorter: { type: 'integer', nullable: true },
+								},
+								required: ['_id', '_updatedAt', 'roles'],
+								additionalProperties: false,
+							},
+						},
+						remove: {
+							type: 'array',
+							items: {
+								type: 'object',
+								properties: {
+									_id: { type: 'string' },
+									_updatedAt: { type: 'object' },
+									roles: {
+										type: 'array',
+										items: { type: 'string' },
+									},
+									group: { type: 'string', nullable: true },
+									groupPermissionId: { type: 'string', nullable: true },
+									level: { type: 'string', enum: ['settings'], nullable: true },
+									section: { type: 'string', nullable: true },
+									sectionPermissionId: { type: 'string', nullable: true },
+									settingId: { type: 'string', nullable: true },
+									sorter: { type: 'integer', nullable: true },
+								},
+								required: ['_id', '_updatedAt', 'roles'],
+								additionalProperties: false,
+							},
+						},
+						success: {
+							type: 'boolean',
+							description: 'Indicates if the request was successful.',
+						},
+					},
+					required: ['update', 'remove', 'success'],
+					additionalProperties: false,
+				}),
+				400: ajv.compile({
+					type: 'object',
+					properties: {
+						error: {
+							type: 'string',
+							description: 'The error message.',
+						},
+						errorType: {
+							type: 'string',
+							description: 'The type of the error.',
+						},
+						details: {
+							type: 'object',
+							nullable: true,
+							properties: {
+								rid: {
+									type: 'string',
+									description: 'The room ID.',
+								},
+								method: {
+									type: 'string',
+									description: 'The method that caused the error.',
+								},
+							},
+						},
+						success: {
+							type: 'boolean',
+							enum: [false],
+						},
+					},
+					required: ['success', 'error'],
+				}),
+				401: ajv.compile({
+					type: 'object',
+					properties: {
+						success: {
+							type: 'boolean',
+							enum: [false],
+						},
+						status: {
+							type: 'string',
+						},
+						message: {
+							type: 'string',
+						},
+					},
+					required: ['success'],
+				}),
+			},
+		},
+		async function () {
 			const { updatedSince } = this.queryParams;
 
 			let updatedSinceDate: Date | undefined;
@@ -36,14 +147,96 @@ API.v1.addRoute(
 
 			return API.v1.success(result);
 		},
-	},
-);
-
-API.v1.addRoute(
-	'permissions.update',
-	{ authRequired: true, permissionsRequired: ['access-permissions'] },
-	{
-		async post() {
+	)
+	.post(
+		'permissions.update',
+		{
+			authRequired: true,
+			permissionsRequired: ['access-permissions'],
+			body: isBodyParamsValidPermissionUpdate,
+			response: {
+				200: ajv.compile({
+					type: 'object',
+					properties: {
+						permissions: {
+							type: 'array',
+							items: {
+								type: 'object',
+								properties: {
+									_id: { type: 'string' },
+									_updatedAt: { type: 'object' },
+									roles: {
+										type: 'array',
+										items: { type: 'string' },
+									},
+									group: { type: 'string', nullable: true },
+									groupPermissionId: { type: 'string', nullable: true },
+									level: { type: 'string', enum: ['settings'], nullable: true },
+									section: { type: 'string', nullable: true },
+									sectionPermissionId: { type: 'string', nullable: true },
+									settingId: { type: 'string', nullable: true },
+									sorter: { type: 'integer', nullable: true },
+								},
+								required: ['_id', '_updatedAt', 'roles'],
+								additionalProperties: false,
+							},
+						},
+						success: {
+							type: 'boolean',
+							description: 'Indicates if the request was successful.',
+						},
+					},
+					required: ['permissions', 'success'],
+					additionalProperties: false,
+				}),
+				// TODO: add 400 error response
+				400: ajv.compile({
+					type: 'object',
+					properties: {
+						errorType: {
+							type: 'string',
+							description: 'The type of the error.',
+						},
+						success: {
+							type: 'boolean',
+							enum: [false],
+						},
+					},
+					required: ['success', 'errorType'],
+				}),
+				// TODO: add 401 error response
+				401: ajv.compile({
+					type: 'object',
+					properties: {
+						name: {
+							type: 'string',
+							description: 'The error message.',
+						},
+						errorType: {
+							type: 'string',
+							description: 'The type of the error.',
+						},
+						success: {
+							type: 'boolean',
+							enum: [false],
+						},
+					},
+					required: ['success', 'name', 'errorType'],
+				}),
+				// TODO: add 403 error response
+				403: ajv.compile({
+					type: 'object',
+					properties: {
+						success: {
+							type: 'boolean',
+							enum: [false],
+						},
+					},
+					required: ['success'],
+				}),
+			},
+		},
+		async function () {
 			const { bodyParams } = this;
 
 			if (!isBodyParamsValidPermissionUpdate(bodyParams)) {
@@ -76,5 +269,4 @@ API.v1.addRoute(
 				permissions: result,
 			});
 		},
-	},
-);
+	);
