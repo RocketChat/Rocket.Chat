5a2c097efe6aa229acab66d42a1fbf6385c635b9
Ricardo Garim <rswarovsky@gmail.com>
2025-06-30 22:11:31 -0300

    creates federation-matrix package

---
+++ ee/packages/federation-matrix/src/events/ping.ts
@@ -0,0 +1,8 @@
+import type { Emitter } from '@rocket.chat/emitter';
+import type { HomeserverEventSignatures } from '@rocket.chat/homeserver';
+
+export const ping = async (emitter: Emitter<HomeserverEventSignatures>) => {
+	emitter.on('homeserver.ping', async (data) => {
+		console.log('Message received from homeserver', data);
+	});
+};
