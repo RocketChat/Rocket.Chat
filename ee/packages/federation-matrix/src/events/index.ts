742070ddea30b4068b73d45bba374614c99192b2
Marcos Defendi <marcos.defendi@rocket.chat>
2025-07-01 16:19:50 -0300

    feat: basic support for accept invitation from remote

--- ee/packages/federation-matrix/src/events/index.ts
+++ ee/packages/federation-matrix/src/events/index.ts
@@ -1,10 +1,12 @@
 import type { Emitter } from '@rocket.chat/emitter';
 import type { HomeserverEventSignatures } from '@rocket.chat/homeserver';
 
+import { invite } from './invite';
 import { message } from './message';
 import { ping } from './ping';
 
 export function registerEvents(emitter: Emitter<HomeserverEventSignatures>) {
 	ping(emitter);
 	message(emitter);
+	invite(emitter);
 }
