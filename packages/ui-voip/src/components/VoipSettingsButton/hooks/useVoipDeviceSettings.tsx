b374ddc6e2496a963cc34fef63e62f6eb9b8d416
gabriellsh <henriques.gabriell@gmail.com>
2025-07-03 19:08:02 -0300

    WIP: fix unnamed devices

--- packages/ui-voip/src/components/VoipSettingsButton/hooks/useVoipDeviceSettings.tsx
+++ packages/ui-voip/src/components/VoipSettingsButton/hooks/useVoipDeviceSettings.tsx
@@ -7,6 +7,18 @@ import { useTranslation } from 'react-i18next';
 
 import { useVoipAPI } from '../../../hooks/useVoipAPI';
 
+// TODO: Ensure that there's never more than one default device item
+// if there's more than one, we need to change the label and id.
+const getDefaultDeviceItem = (label: string, type: 'input' | 'output') => ({
+	content: (
+		<Box is='span' title={label} fontSize={14}>
+			{label}
+		</Box>
+	),
+	addon: <RadioButton onChange={() => undefined} checked={true} disabled />,
+	id: `default-${type}`,
+});
+
 export const useVoipDeviceSettings = () => {
 	const { t } = useTranslation();
 	const dispatchToastMessage = useToastMessageDispatch();
@@ -28,44 +40,58 @@ export const useVoipDeviceSettings = () => {
 	});
 
 	const availableInputDevice =
-		availableDevices?.audioInput?.map<GenericMenuItemProps>((device) => ({
-			id: device.id,
-			content: (
-				<Box is='span' title={device.label} fontSize={14}>
-					{device.label}
-				</Box>
-			),
-			addon: <RadioButton onChange={() => changeInputDevice.mutate(device)} checked={device.id === selectedAudioDevices?.audioInput?.id} />,
-		})) || [];
+		availableDevices?.audioInput?.map<GenericMenuItemProps>((device) => {
+			if (!device.id || !device.label) {
+				return getDefaultDeviceItem(t('Default'), 'input');
+			}
+
+			return {
+				id: device.id,
+				content: (
+					<Box is='span' title={device.label} fontSize={14}>
+						{device.label}
+					</Box>
+				),
+				addon: (
+					<RadioButton onChange={() => changeInputDevice.mutate(device)} checked={device.id === selectedAudioDevices?.audioInput?.id} />
+				),
+			};
+		}) || [];
 
 	const availableOutputDevice =
-		availableDevices?.audioOutput?.map<GenericMenuItemProps>((device) => ({
-			id: device.id,
-			content: (
-				<Box is='span' title={device.label} fontSize={14}>
-					{device.label}
-				</Box>
-			),
-			addon: (
-				<RadioButton onChange={() => changeOutputDevice.mutate(device)} checked={device.id === selectedAudioDevices?.audioOutput?.id} />
-			),
-			onClick(e?: MouseEvent<HTMLElement>) {
-				e?.preventDefault();
-				e?.stopPropagation();
-			},
-		})) || [];
+		availableDevices?.audioOutput?.map<GenericMenuItemProps>((device) => {
+			if (!device.id || !device.label) {
+				return getDefaultDeviceItem(t('Default'), 'output');
+			}
+
+			return {
+				id: device.id,
+				content: (
+					<Box is='span' title={device.label} fontSize={14}>
+						{device.label}
+					</Box>
+				),
+				addon: (
+					<RadioButton onChange={() => changeOutputDevice.mutate(device)} checked={device.id === selectedAudioDevices?.audioOutput?.id} />
+				),
+				onClick(e?: MouseEvent<HTMLElement>) {
+					e?.preventDefault();
+					e?.stopPropagation();
+				},
+			};
+		}) || [];
 
 	const micSection = {
 		title: t('Microphone'),
-		items: availableInputDevice,
+		items: availableInputDevice.length > 0 ? availableInputDevice : getDefaultDeviceItem(t('Default'), 'input'),
 	};
 
 	const speakerSection = {
 		title: t('Speaker'),
-		items: availableOutputDevice,
+		items: availableOutputDevice.length > 0 ? availableOutputDevice : getDefaultDeviceItem(t('Default'), 'output'),
 	};
 
-	const disabled = !micSection.items.length || !speakerSection.items.length;
+	const disabled = availableOutputDevice.length === 0 && availableInputDevice.length === 0;
 
 	return {
 		disabled,
