cfed08433bf743c40b62b04234548ceaf15b169f
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-07-04 12:01:08 -0300

    chore: jsdocs for useWizard

--- apps/meteor/client/components/Wizard/useWizard.tsx
+++ apps/meteor/client/components/Wizard/useWizard.tsx
@@ -2,22 +2,39 @@ import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { useMemo, useState } from 'react';
 
 import type { WizardAPI } from './WizardContext';
-import type { StepMetadata, StepNode } from './lib/StepsLinkedList';
-import { StepsLinkedList } from './lib/StepsLinkedList';
+import type { StepNode, StepMetadata } from './lib/StepNode';
+import StepsLinkedList from './lib/StepsLinkedList';
 
 type UseWizardProps = {
 	steps: StepMetadata[];
 };
 
+/**
+ * Custom hook to manage the state and navigation of a wizard.
+ * It provides methods to register steps, navigate between them, and manage their state.
+ *
+ * @param {UseWizardProps} props - The properties for the wizard.
+ * @returns {WizardAPI} The API for managing the wizard state and navigation.
+ */
 export const useWizard = ({ steps: stepsMetadata }: UseWizardProps) => {
 	const [steps] = useState(new StepsLinkedList(stepsMetadata));
 	const [currentStep, setCurrentStep] = useState<StepNode | null>(steps.head);
 
+	/**
+	 * Registers a new step in the wizard.
+	 * If a step with the same ID already exists, it updates the existing step.
+	 */
 	const register = useEffectEvent((stepMetadata: StepMetadata) => {
 		const step = steps.append(stepMetadata);
 		return () => steps.remove(step.id);
 	});
 
+	/**
+	 * Navigates to a specific step in the wizard.
+	 * If the step is disabled, it does nothing.
+	 *
+	 * @param {StepNode} step - The step to navigate to.
+	 */
 	const goTo = useEffectEvent(async (step: StepNode) => {
 		if (step.disabled) {
 			return;
@@ -26,6 +43,10 @@ export const useWizard = ({ steps: stepsMetadata }: UseWizardProps) => {
 		setCurrentStep(step);
 	});
 
+	/**
+	 * Navigates to the next step in the wizard.
+	 * If there is no next step, it does nothing.
+	 */
 	const next = useEffectEvent(async () => {
 		if (!currentStep?.next) {
 			return;
@@ -35,6 +56,10 @@ export const useWizard = ({ steps: stepsMetadata }: UseWizardProps) => {
 		goTo(currentStep.next);
 	});
 
+	/**
+	 * Navigates to the previous step in the wizard.
+	 * If there is no previous step, it does nothing.
+	 */
 	const previous = useEffectEvent(async () => {
 		if (!currentStep?.prev) {
 			return;
@@ -44,6 +69,10 @@ export const useWizard = ({ steps: stepsMetadata }: UseWizardProps) => {
 		goTo(currentStep.prev);
 	});
 
+	/**
+	 * Resets the next steps in the wizard.
+	 * It disables all steps that come after the current step.
+	 */
 	const resetNextSteps = useEffectEvent(() => {
 		if (!currentStep) {
 			return;
