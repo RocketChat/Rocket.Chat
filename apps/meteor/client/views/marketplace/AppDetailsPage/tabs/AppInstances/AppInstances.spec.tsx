2b33af6c5ec200f2f0ea7dfc2d8bcfeda3b5b1ab
MartinSchoeler <martinschoeler8@gmail.com>
2025-07-04 17:01:40 -0300

    test: Adjust snapshot

--- apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppInstances/AppInstances.spec.tsx
+++ apps/meteor/client/views/marketplace/AppDetailsPage/tabs/AppInstances/AppInstances.spec.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { mockAppRoot } from '@rocket.chat/mock-providers';
 import { composeStories } from '@storybook/react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
 import * as stories from './AppInstances.stories';
@@ -10,6 +10,8 @@ const testCases = Object.values(composeStories(stories)).map((Story) => [Story.s
 
 test.each(testCases)(`renders AppInstances without crashing`, async (_storyname, Story) => {
 	const view = render(<Story />, { wrapper: mockAppRoot().build() });
+	expect(await screen.findByRole('table')).toBeInTheDocument();
+
 	expect(view.baseElement).toMatchSnapshot();
 });
 
@@ -18,5 +20,7 @@ test.each(testCases)('AppInstances should have no a11y violations', async (_stor
 
 	// Today we do not have exactly a pattern to handle menu cells that don't have a header
 	const results = await axe(container, { rules: { 'empty-table-header': { enabled: false } } });
+	expect(await screen.findByRole('table')).toBeInTheDocument();
+
 	expect(results).toHaveNoViolations();
 });
