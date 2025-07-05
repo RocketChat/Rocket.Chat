357ca0a549dc1f1f69eaccd97cf05184d106e919
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-07-05 12:38:45 -0300

    chore: RecipientSubmitPayload

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/forms/RecipientForm.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/forms/RecipientForm.tsx
@@ -1,1 +1,10 @@
-export type RecipientFormSubmitPayload = Record<string, string>;
+import type { IOutboundProviderMetadata, Serialized, ILivechatContact } from '@rocket.chat/core-typings';
+
+export type RecipientFormSubmitPayload = {
+	contactId: string;
+	contact: Serialized<ILivechatContact>;
+	providerId: string;
+	provider: IOutboundProviderMetadata;
+	recipient: string;
+	sender: string;
+};
