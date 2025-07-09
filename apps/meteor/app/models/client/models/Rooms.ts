58fea4aba88c675a55f0e34fc313b14d5e6c6bc5
Tasso <tasso.evangelista@rocket.chat>
2025-07-09 15:10:47 -0300

    Implement `CachedStore` as alternative for `CachedCollection`

--- apps/meteor/app/models/client/models/Rooms.ts
+++ apps/meteor/app/models/client/models/Rooms.ts
@@ -1,13 +1,9 @@
-import type { IRoom } from '@rocket.chat/core-typings';
-import type { StoreApi, UseBoundStore } from 'zustand';
-
 import { CachedChatRoom } from './CachedChatRoom';
-import type { IDocumentMapStore } from '../../../../client/lib/cachedCollections/DocumentMapStore';
-
-type RoomsStore = {
-	use: UseBoundStore<StoreApi<IDocumentMapStore<IRoom>>>;
-	readonly state: IDocumentMapStore<IRoom>;
-};
 
 /** @deprecated new code refer to Minimongo collections like this one; prefer fetching data from the REST API, listening to changes via streamer events, and storing the state in a Tanstack Query */
-export const Rooms = CachedChatRoom.collection as RoomsStore;
+export const Rooms = {
+	use: CachedChatRoom.store,
+	get state() {
+		return this.use.getState();
+	},
+} as const;
