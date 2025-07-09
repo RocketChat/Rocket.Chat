600ca064fead6dfbb356031375b457dc59c104a8
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-07-09 16:27:22 -0300

    refactor: use renderAction instead of formRef

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RecipientStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/RecipientStep.tsx
@@ -1,12 +1,8 @@
 import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
-import { WizardActions, WizardNextButton } from '@rocket.chat/ui-client';
-import { useMutation } from '@tanstack/react-query';
-import type { MouseEvent } from 'react';
-import { useRef } from 'react';
+import { useWizardContext, WizardActions, WizardNextButton } from '@rocket.chat/ui-client';
 
-import type { RecipientFormData, RecipientFormRef, RecipientFormSubmitPayload } from '../forms/RecipientForm';
+import type { RecipientFormData, RecipientFormSubmitPayload } from '../forms/RecipientForm';
 import RecipientForm from '../forms/RecipientForm';
-import { FormRefNotSetError } from '../utils/errors';
 
 type RecipientStepProps = {
 	defaultValues?: Partial<RecipientFormData>;
@@ -15,32 +11,25 @@ type RecipientStepProps = {
 };
 
 const RecipientStep = ({ defaultValues, onDirty, onSubmit }: RecipientStepProps) => {
-	const formRef = useRef<RecipientFormRef>({
-		submit: () => Promise.reject(new FormRefNotSetError()),
-	});
+	const { next } = useWizardContext();
 
-	const nextMutation = useMutation({
-		mutationKey: ['outbound-message', 'recipient', 'submit'],
-		mutationFn: () => formRef.current.submit(),
-		throwOnError: false,
-	});
-
-	const handleNextClick = useEffectEvent(async (event: MouseEvent<HTMLButtonElement>) => {
-		try {
-			const values = await nextMutation.mutateAsync();
-			onSubmit(values);
-		} catch (error) {
-			event.preventDefault();
-		}
+	const handleSubmit = useEffectEvent((values: RecipientFormSubmitPayload) => {
+		onSubmit(values);
+		next();
 	});
 
 	return (
 		<div>
-			<RecipientForm ref={formRef} defaultValues={defaultValues} onDirty={onDirty} />
-
-			<WizardActions>
-				<WizardNextButton loading={nextMutation.isPending} onClick={handleNextClick} />
-			</WizardActions>
+			<RecipientForm
+				defaultValues={defaultValues}
+				onDirty={onDirty}
+				onSubmit={handleSubmit}
+				renderActions={({ isSubmitting }) => (
+					<WizardActions>
+						<WizardNextButton type='submit' loading={isSubmitting} />
+					</WizardActions>
+				)}
+			/>
 		</div>
 	);
 };
