50f7f208d0d0ca928bb4e78af7735e3c039d6e82
yash-rajpal <rajpal.yash03@gmail.com>
2025-07-10 22:50:21 +0530

    fix sidepanel not showing rooms

--- apps/meteor/client/views/navigation/sidepanel/hooks/useChannelsChildrenList.ts
+++ apps/meteor/client/views/navigation/sidepanel/hooks/useChannelsChildrenList.ts
@@ -5,25 +5,20 @@ import { useShallow } from 'zustand/shallow';
 import { Subscriptions } from '../../../../../app/models/client';
 import { pipe } from '../../../../lib/cachedCollections';
 
-const isUnreadOnly = (subscription: ISubscription, unreadOnly: boolean) => unreadOnly && subscription.unread > 0;
+const isUnreadOnly = (subscription: ISubscription, unreadOnly: boolean) => (unreadOnly ? subscription.unread > 0 : true);
 
 export const useChannelsChildrenList = (parentRid: string, unreadOnly: boolean, teamId?: string) => {
 	return Subscriptions.use(
 		useShallow((state) => {
 			const records = state.filter((subscription) => {
-				if (subscription.prid !== parentRid && subscription.rid !== parentRid) {
-					if (!teamId) {
-						return false;
-					}
+				if (subscription.prid === parentRid || subscription.rid === parentRid) {
+					return isUnreadOnly(subscription, unreadOnly);
+				}
+				if (teamId && subscription.teamId === teamId) {
+					return isUnreadOnly(subscription, unreadOnly);
+				}
 
-					if (teamId && subscription.teamId !== teamId) {
-						return false;
-					}
-				}
-				if (!isUnreadOnly(subscription, unreadOnly)) {
-					return false;
-				}
-				return true;
+				return false;
 			});
 
 			const { apply: transform } = pipe<SubscriptionWithRoom>().sortByField('lm', 1);
