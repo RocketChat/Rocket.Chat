d036830745f24905756b84aeb9e95ca4ddb577da
Jean Brito <jeanfbrito@gmail.com>
2025-07-11 17:18:11 -0300

    fix: Font sizes and Ringtones translations not working

--- apps/meteor/client/views/account/accessibility/fontSizes.ts
+++ apps/meteor/client/views/account/accessibility/fontSizes.ts
@@ -1,9 +1,10 @@
 import type { SelectOption } from '@rocket.chat/fuselage';
+import type { TranslationKey } from '@rocket.chat/ui-contexts';
 
-export const fontSizes: SelectOption[] = [
-	[`${(14 / 16) * 100}%`, 'Small'],
-	['100%', 'Default'],
-	[`${(18 / 16) * 100}%`, 'Medium'],
-	[`${(20 / 16) * 100}%`, 'Large'],
-	[`${(24 / 16) * 100}%`, 'Extra large'],
+export const fontSizes = (t: (key: TranslationKey) => string): SelectOption[] => [
+	[`${(14 / 16) * 100}%`, t('Font_Small' as TranslationKey)],
+	['100%', t('Font_Default' as TranslationKey)],
+	[`${(18 / 16) * 100}%`, t('Font_Medium' as TranslationKey)],
+	[`${(20 / 16) * 100}%`, t('Font_Large' as TranslationKey)],
+	[`${(24 / 16) * 100}%`, t('Font_Extra_large' as TranslationKey)],
 ];
