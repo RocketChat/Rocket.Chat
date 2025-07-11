fb823d905c0ada2c3b1cdc30fbd1129e5b38a580
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-07-11 17:33:14 -0300

    feat: Add AJV schema support for invites and update related API endpoints

--- apps/meteor/app/api/server/v1/invites.ts
+++ apps/meteor/app/api/server/v1/invites.ts
@@ -13,53 +13,20 @@ import { removeInvite } from '../../../invites/server/functions/removeInvite';
 import { sendInvitationEmail } from '../../../invites/server/functions/sendInvitationEmail';
 import { useInviteToken } from '../../../invites/server/functions/useInviteToken';
 import { validateInviteToken } from '../../../invites/server/functions/validateInviteToken';
+import type { ExtractRoutesFromAPI } from '../ApiClass';
 import { API } from '../api';
 
-API.v1
+const invites = API.v1
 	.get(
 		'listInvites',
 		{
 			authRequired: true,
 			response: {
-				200: ajv.compile({
+				200: ajv.compile<IInvite[]>({
 					additionalProperties: false,
 					type: 'array',
 					items: {
-						type: 'object',
-						properties: {
-							_id: {
-								type: 'string',
-							},
-							days: {
-								type: 'number',
-							},
-							rid: {
-								type: 'string',
-							},
-							userId: {
-								type: 'string',
-							},
-							createdAt: {
-								type: 'object',
-							},
-							expires: {
-								type: 'object',
-								nullable: true,
-							},
-							url: {
-								type: 'string',
-							},
-							_updatedAt: {
-								type: 'object',
-							},
-							maxUses: {
-								type: 'number',
-							},
-							uses: {
-								type: 'number',
-							},
-						},
-						required: ['_id', 'days', 'rid', 'userId', 'createdAt', 'url', '_updatedAt', 'maxUses', 'uses'],
+						$ref: '#/components/schemas/IInvite',
 					},
 				}),
 				401: ajv.compile({
@@ -267,3 +234,10 @@ API.v1.addRoute(
 		},
 	},
 );
+
+type InvitesEndpoints = ExtractRoutesFromAPI<typeof invites>;
+
+declare module '@rocket.chat/rest-typings' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-empty-interface
+	interface Endpoints extends InvitesEndpoints {}
+}
--- packages/core-typings/src/Ajv.ts
+++ packages/core-typings/src/Ajv.ts
@@ -1,5 +1,6 @@
 import typia from 'typia';
 
+import type { IInvite } from './IInvite';
 import type { ISubscription } from './ISubscription';
 
-export const schemas = typia.json.schemas<[ISubscription], '3.0'>();
+export const schemas = typia.json.schemas<[ISubscription | IInvite], '3.0'>();
