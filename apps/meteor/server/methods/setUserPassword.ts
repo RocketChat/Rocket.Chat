7aa80b6120f5e075be53273ad2beea0b1a487c5e
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-07-11 16:03:57 -0300

    chore: remove useMethod saveUserProfile setUsername saveCustomFields setUserPassword

--- apps/meteor/server/methods/setUserPassword.ts
+++ apps/meteor/server/methods/setUserPassword.ts
@@ -7,6 +7,7 @@ import { Meteor } from 'meteor/meteor';
 import type { UpdateResult } from 'mongodb';
 
 import { passwordPolicy } from '../../app/lib/server';
+import { methodDeprecationLogger } from '../../app/lib/server/lib/deprecationWarningLogger';
 import { notifyOnUserChange } from '../../app/lib/server/lib/notifyListener';
 import { compareUserPassword } from '../lib/compareUserPassword';
 
@@ -49,6 +50,7 @@ export const executeResetPassword = async (user: IUser, password: string) => {
 
 Meteor.methods<ServerMethods>({
 	async setUserPassword(password) {
+		methodDeprecationLogger.method('setUserPassword', '8.0.0', 'Use the endpoint /v1/users.updateOwnBasicInfo instead');
 		check(password, String);
 
 		const userId = Meteor.userId();
