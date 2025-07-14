483730878726a53f0b2d5f628fe88e4ff9e5a25c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-07-14 13:13:28 -0300

    Refactor `handlePriorityChange` to use a local function within `useMemo`

--- apps/meteor/client/omnichannel/hooks/useOmnichannelPrioritiesMenu.tsx
+++ apps/meteor/client/omnichannel/hooks/useOmnichannelPrioritiesMenu.tsx
@@ -1,6 +1,5 @@
 import type { IRoom } from '@rocket.chat/core-typings';
 import { LivechatPriorityWeight } from '@rocket.chat/core-typings';
-import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { useEndpoint } from '@rocket.chat/ui-contexts';
 import { useQueryClient } from '@tanstack/react-query';
 import { useMemo } from 'react';
@@ -18,17 +17,17 @@ export const useOmnichannelPrioritiesMenu = (rid: IRoom['_id']) => {
 	const removeRoomPriority = useEndpoint('DELETE', '/v1/livechat/room/:rid/priority', { rid });
 	const { data: priorities } = useOmnichannelPriorities();
 
-	const handlePriorityChange = useEffectEvent((priorityId: string) => async () => {
-		try {
-			priorityId ? await updateRoomPriority({ priorityId }) : await removeRoomPriority();
-			queryClient.invalidateQueries({ queryKey: ['current-chats'] });
-			queryClient.invalidateQueries({ queryKey: roomsQueryKeys.info(rid) });
-		} catch (error) {
-			dispatchToastMessage({ type: 'error', message: error });
-		}
-	});
-
 	return useMemo(() => {
+		const handlePriorityChange = (priorityId: string) => async () => {
+			try {
+				priorityId ? await updateRoomPriority({ priorityId }) : await removeRoomPriority();
+				queryClient.invalidateQueries({ queryKey: ['current-chats'] });
+				queryClient.invalidateQueries({ queryKey: roomsQueryKeys.info(rid) });
+			} catch (error) {
+				dispatchToastMessage({ type: 'error', message: error });
+			}
+		};
+
 		const unprioritizedOption = {
 			id: 'unprioritized',
 			icon: PRIORITY_ICONS[LivechatPriorityWeight.NOT_SPECIFIED].iconName,
@@ -50,5 +49,5 @@ export const useOmnichannelPrioritiesMenu = (rid: IRoom['_id']) => {
 		});
 
 		return priorities.length ? [unprioritizedOption, ...options] : [];
-	}, [t, handlePriorityChange, priorities]);
+	}, [t, priorities, updateRoomPriority, removeRoomPriority, queryClient, rid]);
 };
