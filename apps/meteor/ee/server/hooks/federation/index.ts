59aff8d3e414773ffd216931213727e5a6f6af70
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2025-07-15 10:11:37 -0300

    chore: new federation callbacks listeners (#36421)

---
+++ apps/meteor/ee/server/hooks/federation/index.ts
@@ -0,0 +1,5 @@
+// import { FederationMatrix } from '@rocket.chat/core-services';
+
+// import { callbacks } from '../../../../lib/callbacks';
+
+// callbacks.add('federation-event-example', async () => FederationMatrix.handleExample(), callbacks.priority.MEDIUM, 'federation-event-example-handler');
