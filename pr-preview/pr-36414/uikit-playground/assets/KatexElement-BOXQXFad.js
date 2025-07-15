fbb1fb59f2f2f11317a0fcd2231cd4bc1e41ffa5
dougfabris <dougfabris@users.noreply.github.com>
2025-07-15 14:36:16 +0000

    Deploy preview for PR 36414 🛫

---
+++ pr-preview/pr-36414/uikit-playground/assets/KatexElement-BOXQXFad.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-CKj-mWCE.js";import{k as o}from"./katex-cNP_AtkC.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
