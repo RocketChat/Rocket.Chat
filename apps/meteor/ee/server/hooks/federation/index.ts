303631da3dd4a2716f86c3bdab9faa40d7536488
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2025-07-15 10:11:37 -0300

    chore: new federation callbacks listeners (#36421)

---
+++ apps/meteor/ee/server/hooks/federation/index.ts
@@ -0,0 +1,39 @@
+// import { FederationMatrix } from '@rocket.chat/core-services';
+
+import { FederationMatrix } from '@rocket.chat/core-services';
+
+import { callbacks } from '../../../../lib/callbacks';
+import { getFederationVersion } from '../../../../server/services/federation/utils';
+
+// callbacks.add('federation-event-example', async () => FederationMatrix.handleExample(), callbacks.priority.MEDIUM, 'federation-event-example-handler');
+
+callbacks.add('federation.afterCreateFederatedRoom', async (room, { owner, originalMemberList: members }) => {
+	const federationVersion = getFederationVersion();
+	if (federationVersion === 'matrix') {
+		await FederationMatrix.createRoom(room, owner, members);
+	}
+});
+
+callbacks.add(
+	'afterSaveMessage',
+	async (message, { room, user }) => {
+		const shouldBeHandledByFederation = room.federated === true || user.username?.includes(':');
+		const federationVersion = getFederationVersion();
+
+		if (shouldBeHandledByFederation && federationVersion === 'native') {
+			try {
+				// TODO: Check if message already exists in the database, if it does, don't send it to the federation to avoid loops
+				// If message is federated, it will save external_message_id like into the message object
+				// if this prop exists here it should not be sent to the federation to avoid loops
+				if (!message.federation?.eventId) {
+					await FederationMatrix.sendMessage(message, room, user);
+				}
+			} catch (error) {
+				// Log the error but don't prevent the message from being sent locally
+				console.error('[sendMessage] Failed to send message to Native Federation:', error);
+			}
+		}
+	},
+	callbacks.priority.HIGH,
+	'federation-v2-after-room-message-sent',
+);
