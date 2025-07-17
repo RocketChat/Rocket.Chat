301991560f93513b7bc1356eea818dc4fb8d894c
gabriellsh <henriques.gabriell@gmail.com>
2025-07-17 16:49:45 -0300

    Fix header action tooltip

--- apps/meteor/client/hooks/roomActions/useVoiceCallRoomAction.tsx
+++ apps/meteor/client/hooks/roomActions/useVoiceCallRoomAction.tsx
@@ -2,7 +2,6 @@ import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { useMediaDeviceMicrophonePermission, usePermission, useUserId } from '@rocket.chat/ui-contexts';
 import { useVoipAPI, useVoipState, useDevicePermissionPrompt } from '@rocket.chat/ui-voip';
 import { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import { useRoom } from '../../views/room/contexts/RoomContext';
 import type { RoomToolboxActionConfig } from '../../views/room/contexts/RoomToolboxContext';
@@ -10,7 +9,6 @@ import { useUserInfoQuery } from '../useUserInfoQuery';
 import { useVoipWarningModal } from '../useVoipWarningModal';
 
 export const useVoiceCallRoomAction = () => {
-	const { t } = useTranslation();
 	const { uids = [] } = useRoom();
 	const ownUserId = useUserId();
 	const canStartVoiceCall = usePermission('view-user-voip-extension');
@@ -37,15 +35,15 @@ export const useVoiceCallRoomAction = () => {
 
 	const tooltip = useMemo(() => {
 		if (isMicPermissionDenied) {
-			return t('Microphone_access_not_allowed');
+			return 'Microphone_access_not_allowed';
 		}
 
 		if (isInCall) {
-			return t('Unable_to_make_calls_while_another_is_ongoing');
+			return 'Unable_to_make_calls_while_another_is_ongoing';
 		}
 
-		return disabled ? t('Voice_calling_disabled') : '';
-	}, [disabled, isInCall, isMicPermissionDenied, t]);
+		return disabled ? 'Voice_calling_disabled' : '';
+	}, [disabled, isInCall, isMicPermissionDenied]);
 
 	const promptPermission = useDevicePermissionPrompt({
 		actionType: 'outgoing',
@@ -68,13 +66,12 @@ export const useVoiceCallRoomAction = () => {
 
 		return {
 			id: 'start-voice-call',
-			title: 'Voice_Call',
+			title: tooltip || 'Voice_Call',
 			icon: 'phone',
 			featured: true,
 			action: handleOnClick,
 			groups: ['direct'] as const,
 			disabled,
-			tooltip,
 		};
 	}, [allowed, disabled, handleOnClick, tooltip]);
 };
