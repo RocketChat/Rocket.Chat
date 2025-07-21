0765fdc44994ef09e296b2f91880a3bffc9115a9
Ricardo Garim <rswarovsky@gmail.com>
2025-07-21 12:51:58 -0300

    feat: adds license validation to federation packages and services (#36391)

--- apps/meteor/startRocketChat.ts
+++ apps/meteor/startRocketChat.ts
@@ -1,5 +1,6 @@
 import { startLicense } from './ee/app/license/server/startup';
 import { registerEEBroker } from './ee/server';
+import { startFederationService as startFederationMatrixService } from './ee/server/startup/federation';
 import { startFederationService } from './ee/server/startup/services';
 
 const loadBeforeLicense = async () => {
@@ -8,6 +9,7 @@ const loadBeforeLicense = async () => {
 
 const loadAfterLicense = async () => {
 	await startFederationService();
+	await startFederationMatrixService();
 };
 
 export const startRocketChat = async () => {
