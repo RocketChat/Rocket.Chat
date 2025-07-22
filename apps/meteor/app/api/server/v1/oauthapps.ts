2210ec4ffd088be25edfdcf961b278c51807a743
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-07-22 21:32:24 +0300

    feat: add openapi support to oauth-apps create api

--- apps/meteor/app/api/server/v1/oauthapps.ts
+++ apps/meteor/app/api/server/v1/oauthapps.ts
@@ -1,11 +1,13 @@
+import type { IOAuthApps } from '@rocket.chat/core-typings';
 import { OAuthApps } from '@rocket.chat/models';
-import { isUpdateOAuthAppParams, isOauthAppsGetParams, isOauthAppsAddParams, isDeleteOAuthAppParams } from '@rocket.chat/rest-typings';
+import { ajv, isUpdateOAuthAppParams, isOauthAppsGetParams, isDeleteOAuthAppParams } from '@rocket.chat/rest-typings';
 
 import { hasPermissionAsync } from '../../../authorization/server/functions/hasPermission';
 import { apiDeprecationLogger } from '../../../lib/server/lib/deprecationWarningLogger';
 import { addOAuthApp } from '../../../oauth2-server-config/server/admin/functions/addOAuthApp';
 import { deleteOAuthApp } from '../../../oauth2-server-config/server/admin/methods/deleteOAuthApp';
 import { updateOAuthApp } from '../../../oauth2-server-config/server/admin/methods/updateOAuthApp';
+import type { ExtractRoutesFromAPI } from '../ApiClass';
 import { API } from '../api';
 
 API.v1.addRoute(
@@ -83,18 +85,130 @@ API.v1.addRoute(
 	},
 );
 
-API.v1.addRoute(
+export type OauthAppsAddParams = {
+	name: string;
+	active: boolean;
+	redirectUri: string;
+};
+
+const OauthAppsAddParamsSchema = {
+	type: 'object',
+	properties: {
+		name: {
+			type: 'string',
+		},
+		active: {
+			type: 'boolean',
+		},
+		redirectUri: {
+			type: 'string',
+		},
+	},
+	required: ['name', 'active', 'redirectUri'],
+	additionalProperties: false,
+};
+
+export const isOauthAppsAddParams = ajv.compile<OauthAppsAddParams>(OauthAppsAddParamsSchema);
+
+const oauthAppsCreateEndpoints = API.v1.post(
 	'oauth-apps.create',
 	{
 		authRequired: true,
-		validateParams: isOauthAppsAddParams,
+		body: isOauthAppsAddParams,
 		permissionsRequired: ['manage-oauth-apps'],
-	},
-	{
-		async post() {
-			const application = await addOAuthApp(this.bodyParams, this.userId);
-
-			return API.v1.success({ application });
+		response: {
+			400: ajv.compile<{
+				error?: string;
+				errorType?: string;
+				stack?: string;
+				details?: object;
+			}>({
+				type: 'object',
+				properties: {
+					success: { type: 'boolean', enum: [false] },
+					stack: { type: 'string' },
+					error: { type: 'string' },
+					errorType: { type: 'string' },
+					details: { type: 'object' },
+				},
+				required: ['success'],
+				additionalProperties: false,
+			}),
+			401: ajv.compile({
+				type: 'object',
+				properties: {
+					success: { type: 'boolean', enum: [false] },
+					status: { type: 'string' },
+					message: { type: 'string' },
+					error: { type: 'string' },
+					errorType: { type: 'string' },
+				},
+				required: ['success'],
+				additionalProperties: false,
+			}),
+			403: ajv.compile({
+				type: 'object',
+				properties: {
+					success: { type: 'boolean', enum: [false] },
+					status: { type: 'string' },
+					message: { type: 'string' },
+					error: { type: 'string' },
+					errorType: { type: 'string' },
+				},
+				required: ['success'],
+				additionalProperties: false,
+			}),
+			200: ajv.compile<{ application: IOAuthApps }>({
+				type: 'object',
+				properties: {
+					application: {
+						type: 'object',
+						properties: {
+							_id: { type: 'string' },
+							name: { type: 'string' },
+							active: { type: 'boolean' },
+							clientId: { type: 'string' },
+							clientSecret: { type: 'string' },
+							redirectUri: { type: 'string' },
+							_createdAt: { type: 'string' },
+							_createdBy: {
+								type: 'object',
+								properties: {
+									_id: { type: 'string' },
+									username: { type: 'string' },
+								},
+								required: ['_id', 'username'],
+								additionalProperties: false,
+							},
+							_updatedAt: { type: 'string' },
+							appId: { type: 'string' },
+						},
+						required: ['_id', 'name', 'active', 'clientId', 'clientSecret', 'redirectUri', '_createdAt', '_createdBy', '_updatedAt'],
+						additionalProperties: false,
+					},
+					success: {
+						type: 'boolean',
+						enum: [true],
+					},
+				},
+				required: ['application', 'success'],
+				additionalProperties: false,
+			}),
 		},
 	},
+
+	async function action() {
+		const application = await addOAuthApp(this.bodyParams, this.userId);
+
+		return API.v1.success({ application });
+	},
 );
+
+type OauthAppsCreateEndpoints = ExtractRoutesFromAPI<typeof oauthAppsCreateEndpoints>;
+
+export type OAuthAppsEndpoints = OauthAppsCreateEndpoints;
+
+declare module '@rocket.chat/rest-typings' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-empty-interface
+	interface Endpoints extends OauthAppsCreateEndpoints {}
+}
