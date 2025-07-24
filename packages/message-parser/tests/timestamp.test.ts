85f227af7a660fd6fbcf57b9cdbae512f87435f0
Martin Schoeler <martin.schoeler@rocket.chat>
2025-07-24 11:57:06 -0300

    feat(message-parser): Make timestamp accept ISO time (#36498)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- packages/message-parser/tests/timestamp.test.ts
+++ packages/message-parser/tests/timestamp.test.ts
@@ -1,5 +1,12 @@
 import { parse } from '../src';
-import { bold, paragraph, plain, strike, timestamp } from '../src/utils';
+import {
+  bold,
+  paragraph,
+  plain,
+  strike,
+  timestamp,
+  timestampFromHours,
+} from '../src/utils';
 
 test.each([
   [`<t:1708551317>`, [paragraph([timestamp('1708551317')])]],
@@ -22,6 +29,40 @@ test.each([
 test.each([
   ['~<t:1708551317>~', [paragraph([strike([timestamp('1708551317')])])]],
   ['*<t:1708551317>*', [paragraph([bold([plain('<t:1708551317>')])])]],
+])('parses %p', (input, output) => {
+  expect(parse(input)).toMatchObject(output);
+});
+
+test.each([
+  [
+    '<t:2025-07-22T10:00:00.000Z:R>',
+    [paragraph([timestamp('1753178400000', 'R')])],
+  ],
+  [
+    '<t:2025-07-22T10:00:00.000Z:R>',
+    [paragraph([timestamp('1753178400000', 'R')])],
+  ],
+  [
+    '<t:2025-07-22T10:00:00.000:R>',
+    [paragraph([timestamp('1753178400000', 'R')])],
+  ],
+  ['<t:2025-07-22T10:00:00:R>', [paragraph([timestamp('1753178400000', 'R')])]],
+  [
+    '<t:10:00:00:R>',
+    [paragraph([timestamp(timestampFromHours('10', '00', '00'), 'R')])],
+  ],
+  [
+    '<t:10:00:R>',
+    [paragraph([timestamp(timestampFromHours('10', '00', '00'), 'R')])],
+  ],
+  [
+    '<t:10:00:00>',
+    [paragraph([timestamp(timestampFromHours('10', '00', '00'), 't')])],
+  ],
+  [
+    '<t:10:00>',
+    [paragraph([timestamp(timestampFromHours('10', '00', '00'), 't')])],
+  ],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
 });
