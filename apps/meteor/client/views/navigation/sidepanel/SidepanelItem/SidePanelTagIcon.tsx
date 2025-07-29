861ec164de4a2d7c3f91b0420d27bde3a4b0054d
dougfabris <devfabris@gmail.com>
2025-07-29 10:24:56 -0300

    chore: simplify `SidePanelTagIcon`

--- apps/meteor/client/views/navigation/sidepanel/SidepanelItem/SidePanelTagIcon.tsx
+++ apps/meteor/client/views/navigation/sidepanel/SidepanelItem/SidePanelTagIcon.tsx
@@ -1,13 +1,7 @@
-import { Box, Icon } from '@rocket.chat/fuselage';
-import type { ComponentProps, ReactElement } from 'react';
-import { isValidElement } from 'react';
+import { Icon } from '@rocket.chat/fuselage';
+import type { ComponentProps } from 'react';
 
-const SidePanelTagIcon = ({ icon }: { icon: ReactElement | Pick<ComponentProps<typeof Icon>, 'name' | 'color'> | null }) => {
-	if (isValidElement(icon)) {
-		return <Box>{icon}</Box>;
-	}
-
-	return icon && typeof icon === 'object' && 'name' in icon ? <Icon size='x12' mie={4} {...icon} /> : null;
-};
+const SidePanelTagIcon = ({ icon }: { icon: Pick<ComponentProps<typeof Icon>, 'name' | 'color'> | null }) =>
+	icon ? <Icon size='x12' mie={4} {...icon} /> : null;
 
 export default SidePanelTagIcon;
