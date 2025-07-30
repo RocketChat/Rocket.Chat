41bc6f605aebd1cdc1c3b82c227462d7b5e4995a
gabriellsh <henriques.gabriell@gmail.com>
2025-07-30 20:16:27 -0300

    fix exports

--- packages/ui-voip/src/v2/components/index.ts
+++ packages/ui-voip/src/v2/components/index.ts
@@ -1,3 +1,9 @@
 export * from './Widget';
 export * from './PeerInfo';
-export { default as ActionButton } from './ActionButton';
+export * from './PeerAutocomplete';
+
+export { default as ToggleButton } from './ToggleButton';
+export { default as Keypad } from './Keypad/Keypad';
+export { default as PeerAutocomplete } from './PeerAutocomplete';
+export { default as Timer } from './Timer';
+export { default as DevicePicker } from './DevicePicker';
