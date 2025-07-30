66643a47b3b6f8f99bce30bac65d81c778561c0d
gabriellsh <henriques.gabriell@gmail.com>
2025-07-30 20:16:09 -0300

    DevicePicker stub

---
+++ packages/ui-voip/src/v2/components/DevicePicker.tsx
@@ -0,0 +1,10 @@
+import { IconButton } from '@rocket.chat/fuselage';
+
+// type DevicePickerProps = {};
+
+// TODO: Implement
+const DevicePicker = () => {
+	return <IconButton name='customize' icon='customize' small flexGrow={0} />;
+};
+
+export default DevicePicker;
