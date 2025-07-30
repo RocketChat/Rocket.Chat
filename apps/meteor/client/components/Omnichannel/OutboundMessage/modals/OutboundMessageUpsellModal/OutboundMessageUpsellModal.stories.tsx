55de5c698e26be0e916335d703fe176b88d8bf0e
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-07-30 11:10:28 -0300

    chore: adjusted storybook env

--- apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/OutboundMessageUpsellModal.stories.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/OutboundMessageUpsellModal.stories.tsx
@@ -3,6 +3,11 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import OutboundMessageUpsellModal from './OutboundMessageUpsellModal';
 
+// @ts-expect-error __meteor_runtime_config__ is not defined in the type definitions
+global.__meteor_runtime_config__ = {
+	ROOT_URL_PATH_PREFIX: '',
+};
+
 export default {
 	title: 'Outbound Message/OutboundMessageUpsellModal',
 	component: OutboundMessageUpsellModal,
