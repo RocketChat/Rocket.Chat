334ab57ef97a19f8ed0f0d682dd20c0c426a36bf
Ricardo Garim <rswarovsky@gmail.com>
2025-07-31 19:15:43 -0300

    feat: federation message reactions (#36420)

--- apps/meteor/ee/server/index.ts
+++ apps/meteor/ee/server/index.ts
@@ -13,7 +13,7 @@ import './configuration/index';
 import './local-services/ldap/service';
 import './methods/getReadReceipts';
 import './patches';
-// import './hooks/federation';
+import './hooks/federation';
 
 export * from './apps/startup';
 export { registerEEBroker } from './startup';
