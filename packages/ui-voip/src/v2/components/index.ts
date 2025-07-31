cd117d31251d103cd621e9588084b08ba56347b2
gabriellsh <henriques.gabriell@gmail.com>
2025-07-31 17:46:28 -0300

    ActionButton

--- packages/ui-voip/src/v2/components/index.ts
+++ packages/ui-voip/src/v2/components/index.ts
@@ -3,6 +3,7 @@ export * from './PeerInfo';
 export * from './PeerAutocomplete';
 
 export { default as ToggleButton } from './ToggleButton';
+export { default as ActionButton } from './ActionButton';
 export { default as Keypad } from './Keypad/Keypad';
 export { default as PeerAutocomplete } from './PeerAutocomplete';
 export { default as Timer } from './Timer';
