11847d890ab258b374164ffe5f6e111936224789
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-07-31 16:20:53 -0300

    refactor: removed useOutsideClick

--- apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/OutboundMessageUpsellModal.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/OutboundMessageUpsellModal.tsx
@@ -1,5 +1,4 @@
-import { useOutsideClick } from '@rocket.chat/fuselage-hooks';
-import { useMemo, useRef } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { getURL } from '../../../../../../app/utils/client';
@@ -17,11 +16,8 @@ const CONTACT_SALES_LINK = 'https://go.rocket.chat/i/contact-sales';
 
 const OutboundMessageUpsellModal = ({ hasModule, isAdmin, onClose }: OutboundMessageUpsellModalProps) => {
 	const { t } = useTranslation();
-	const ref = useRef<HTMLDivElement>(null);
 
 	const openExternalLink = useExternalLink();
-
-	useOutsideClick([ref], onClose);
 
 	const props = useMemo(() => {
 		if (!hasModule) {
