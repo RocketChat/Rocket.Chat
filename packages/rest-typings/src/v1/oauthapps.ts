1d72a279d7054ba2435289fc30864823c12df56b
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-07-31 15:12:11 +0300

    refactor: remove UpdateOAuthAppParams type and related endpoint from rest-typing

--- packages/rest-typings/src/v1/oauthapps.ts
+++ packages/rest-typings/src/v1/oauthapps.ts
@@ -2,7 +2,6 @@ import type { IOAuthApps, IUser } from '@rocket.chat/core-typings';
 
 import type { DeleteOAuthAppParams } from './oauthapps/DeleteOAuthAppParamsDELETE';
 import type { OauthAppsGetParams } from './oauthapps/OAuthAppsGetParamsGET';
-import type { UpdateOAuthAppParams } from './oauthapps/UpdateOAuthAppParamsPOST';
 
 export type OAuthAppsEndpoint = {
 	'/v1/oauth-apps.list': {
@@ -15,10 +14,6 @@ export type OAuthAppsEndpoint = {
 		GET: (params: OauthAppsGetParams) => {
 			oauthApp: IOAuthApps;
 		};
-	};
-
-	'/v1/oauth-apps.update': {
-		POST: (params: UpdateOAuthAppParams) => IOAuthApps | null;
 	};
 
 	'/v1/oauth-apps.delete': {
