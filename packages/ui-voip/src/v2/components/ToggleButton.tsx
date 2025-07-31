1db4b14000de6d5d480ae5abc58624ea4bc549a4
gabriellsh <henriques.gabriell@gmail.com>
2025-07-31 17:46:19 -0300

    Update ToggleButton

--- packages/ui-voip/src/v2/components/ToggleButton.tsx
+++ packages/ui-voip/src/v2/components/ToggleButton.tsx
@@ -1,5 +1,6 @@
 import { Icon, IconButton } from '@rocket.chat/fuselage';
 import type { Keys } from '@rocket.chat/icons';
+import { ComponentProps } from 'react';
 
 type ToggleButtonProps = {
 	label: string;
@@ -7,14 +8,16 @@ type ToggleButtonProps = {
 	disabled?: boolean;
 	pressed?: boolean;
 	onToggle?: () => void;
-};
+} & Omit<ComponentProps<typeof IconButton>, 'icon' | 'title' | 'aria-label' | 'disabled' | 'onClick'>;
 
-const ToggleButton = ({ disabled, label, pressed, icons, onToggle }: ToggleButtonProps) => {
+const ToggleButton = ({ disabled, label, pressed, icons, onToggle, ...props }: ToggleButtonProps) => {
 	const iconName = icons[pressed ? 1 : 0];
 	const iconColor = pressed ? 'font-danger' : undefined;
 
 	return (
 		<IconButton
+			{...props}
+			label={label}
 			medium
 			secondary
 			icon={<Icon size={16} color={iconColor} name={iconName} />}
