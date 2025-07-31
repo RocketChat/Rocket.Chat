25f4e5e674a9365ee40d94d2118ca1cdd2f2cbcc
yash-rajpal <rajpal.yash03@gmail.com>
2025-07-31 22:43:20 +0530

    reset to all filter when filters don't match required conditions

--- apps/meteor/client/views/navigation/sidepanel/SidePanelRouter.tsx
+++ apps/meteor/client/views/navigation/sidepanel/SidePanelRouter.tsx
@@ -1,20 +1,30 @@
-import { useSetting } from '@rocket.chat/ui-contexts';
+import { usePermission, useSetting } from '@rocket.chat/ui-contexts';
 
 import SidePanelAll from './tabs/SidePanelAll';
 import { ALL_GROUPS, useRoomsListContext, useSidePanelFilter, useSwitchSidePanelTab } from '../contexts/RoomsNavigationContext';
 import SidePanelDiscussions from './tabs/SidePanelDiscussions';
 import SidePanelFavorites from './tabs/SidePanelFavorites';
-import SidepanelInProgress from './tabs/SidePanelInProgress';
+import SidePanelInProgress from './tabs/SidePanelInProgress';
 import SidePanelMentions from './tabs/SidePanelMentions';
 import SidePanelQueue from './tabs/SidePanelQueue';
 import SidePanelRooms from './tabs/SidePanelRooms';
 import SidePanelOnHold from './tabs/SidepanelOnHold';
+import { useHasLicenseModule } from '../../../hooks/useHasLicenseModule';
 
 const SidePanelRouter = () => {
 	const [currentTab] = useSidePanelFilter();
 	const { parentRid } = useRoomsListContext();
 	const switchSidePanelTab = useSwitchSidePanelTab();
+
 	const isDiscussionEnabled = useSetting('Discussion_enabled');
+	const hasEEModule = useHasLicenseModule('livechat-enterprise');
+	const canViewOmnichannelQueue = usePermission('view-livechat-queue');
+	const isQueueEnabled = useSetting('Livechat_waiting_queue');
+
+	const resetSidePanelTab = () => {
+		switchSidePanelTab(ALL_GROUPS.ALL);
+		return null;
+	};
 
 	switch (currentTab) {
 		case ALL_GROUPS.ALL:
@@ -27,19 +37,25 @@ const SidePanelRouter = () => {
 			if (isDiscussionEnabled) {
 				return <SidePanelDiscussions />;
 			}
-			return switchSidePanelTab(ALL_GROUPS.ALL);
+			return resetSidePanelTab();
 		case ALL_GROUPS.TEAMS:
 		case ALL_GROUPS.CHANNELS:
 		case ALL_GROUPS.DIRECT_MESSAGES:
 			return parentRid ? <SidePanelRooms parentRid={parentRid} /> : null;
 		case ALL_GROUPS.IN_PROGRESS:
-			return <SidepanelInProgress />;
+			return <SidePanelInProgress />;
 		case ALL_GROUPS.ON_HOLD:
-			return <SidePanelOnHold />;
+			if (hasEEModule) {
+				return <SidePanelOnHold />;
+			}
+			return resetSidePanelTab();
 		case ALL_GROUPS.QUEUE:
-			return <SidePanelQueue />;
+			if (hasEEModule && canViewOmnichannelQueue && isQueueEnabled) {
+				return <SidePanelQueue />;
+			}
+			return resetSidePanelTab();
 		default:
-			return null;
+			return resetSidePanelTab();
 	}
 };
 
