4d4b53c41b50e5d8ffd8bca619ac4bc04eca59df
Pierre Lehnen <pierre.lehnen@rocket.chat>
2025-07-31 15:35:30 -0300

    moving test code to a separate branch

--- packages/ui-voip/src/index.ts
+++ packages/ui-voip/src/index.ts
@@ -1,5 +1,4 @@
 export { default as VoipProvider } from './providers/VoipProvider';
-export { default as MediaCallsProvider } from './providers/MediaCallsProvider';
 export * from './definitions/VoipSession';
 export * from './hooks';
 export * from './components';
--- packages/ui-voip/src/lib/getUserMedia.ts
+++
@@ -1,7 +0,0 @@
-export async function getUserMedia(constraints: MediaStreamConstraints): Promise<MediaStream> {
-	if (navigator.mediaDevices === undefined) {
-		throw new Error('Media devices not available in insecure contexts.');
-	}
-
-	return navigator.mediaDevices.getUserMedia.call(navigator.mediaDevices, constraints);
-}
