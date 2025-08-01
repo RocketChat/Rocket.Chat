df79dc3d7131f48facc7ef94a18b956440204d43
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-01 11:32:58 -0300

    feat: Outbound Message: Message Step (#36370)

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/MessageStep.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/steps/MessageStep.tsx
@@ -1,17 +1,36 @@
-import { WizardActions, WizardBackButton, WizardNextButton } from '@rocket.chat/ui-client';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useWizardContext, WizardActions, WizardBackButton, WizardNextButton } from '@rocket.chat/ui-client';
+import type { ComponentProps } from 'react';
 
-type MessageStepProps = {
-	onSubmit(values: Record<string, string>): void;
+import type { MessageFormSubmitPayload } from '../forms/MessageForm';
+import MessageForm from '../forms/MessageForm';
+
+type MessageStepProps = Omit<ComponentProps<typeof MessageForm>, 'onSubmit'> & {
+	onSubmit(values: MessageFormSubmitPayload): void;
 };
 
-const MessageStep = (_: MessageStepProps) => {
+const MessageStep = ({ contact, templates, defaultValues, onSubmit }: MessageStepProps) => {
+	const { next } = useWizardContext();
+
+	const handleSubmit = useEffectEvent(async (values: MessageFormSubmitPayload) => {
+		onSubmit(values);
+		next();
+	});
+
 	return (
 		<div>
-			Message Content
-			<WizardActions>
-				<WizardBackButton />
-				<WizardNextButton />
-			</WizardActions>
+			<MessageForm
+				contact={contact}
+				templates={templates}
+				defaultValues={defaultValues}
+				onSubmit={handleSubmit}
+				renderActions={({ isSubmitting }) => (
+					<WizardActions>
+						<WizardBackButton />
+						<WizardNextButton manual type='submit' loading={isSubmitting} />
+					</WizardActions>
+				)}
+			/>
 		</div>
 	);
 };
