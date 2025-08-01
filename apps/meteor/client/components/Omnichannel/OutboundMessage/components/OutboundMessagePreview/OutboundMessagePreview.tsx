e267cfb844c333d0b7de84e5a5359501fc7489fc
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-01 15:59:32 -0300

    feat: Outbound Message: Review Step (#36372)

---
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessagePreview/OutboundMessagePreview.tsx
@@ -0,0 +1,101 @@
+import type {
+	ILivechatAgent,
+	ILivechatDepartment,
+	IOutboundProviderMetadata,
+	IOutboundProviderTemplate,
+	ILivechatContact,
+} from '@rocket.chat/core-typings';
+import { Box, Margins } from '@rocket.chat/fuselage';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import PreviewItem from './PreviewItem';
+import { formatPhoneNumber } from '../../../../../lib/formatPhoneNumber';
+import TemplatePreview from '../TemplatePreview';
+
+type OutboundMessagePreviewProps = {
+	template?: IOutboundProviderTemplate;
+	contactName?: ILivechatContact['name'];
+	providerName?: IOutboundProviderMetadata['providerName'];
+	providerType?: IOutboundProviderMetadata['providerType'];
+	departmentName?: ILivechatDepartment['name'];
+	agentName?: ILivechatAgent['name'];
+	agentUsername?: ILivechatAgent['username'];
+	sender?: string;
+	recipient?: string;
+	templateParameters?: Record<string, string[]>;
+};
+
+const formatContact = (rawValue?: string, providerType?: IOutboundProviderMetadata['providerType']) => {
+	if (!rawValue) {
+		return undefined;
+	}
+
+	if (providerType === 'phone') {
+		return formatPhoneNumber(rawValue);
+	}
+
+	return rawValue;
+};
+
+const OutboundMessagePreview = ({
+	template,
+	contactName,
+	providerName,
+	providerType,
+	departmentName,
+	agentName,
+	agentUsername,
+	sender: rawSender,
+	recipient: rawRecipient,
+	templateParameters,
+}: OutboundMessagePreviewProps) => {
+	const { t } = useTranslation();
+
+	const recipient = useMemo(() => formatContact(rawRecipient, providerType), [providerType, rawRecipient]);
+	const sender = useMemo(() => formatContact(rawSender, providerType), [providerType, rawSender]);
+	const replies = useMemo(() => {
+		if (agentName) {
+			return `${agentName || agentUsername} ${agentUsername ? `(${agentUsername})` : ''} ${departmentName ? `at ${departmentName}` : ''}`;
+		}
+
+		return departmentName;
+	}, [agentName, agentUsername, departmentName]);
+
+	return (
+		<div>
+			<ul>
+				<Margins blockStart={24}>
+					<Box is='li' mbs={0}>
+						<PreviewItem icon='doc' label={t('Template')}>
+							{template?.name}
+						</PreviewItem>
+					</Box>
+					<Box is='li'>
+						<PreviewItem icon='user' label={t('To')}>
+							{contactName && `${contactName} ${recipient ? `(${recipient})` : ''}`}
+						</PreviewItem>
+					</Box>
+					<Box is='li'>
+						<PreviewItem icon='send' label={t('From')}>
+							{providerName && `${providerName} ${sender ? `(${sender})` : ''}`}
+						</PreviewItem>
+					</Box>
+					<Box is='li'>
+						<PreviewItem icon='reply' label={t('Replies')}>
+							{replies}
+						</PreviewItem>
+					</Box>
+				</Margins>
+			</ul>
+
+			{template ? (
+				<Box mbs={30}>
+					<TemplatePreview template={template} parameters={templateParameters} />
+				</Box>
+			) : null}
+		</div>
+	);
+};
+
+export default OutboundMessagePreview;
