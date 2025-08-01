7e633c63602732c54430180485edb44e40475f5f
Diego Sampaio <chinello@gmail.com>
2025-08-01 11:18:32 -0300

    matcher

--- apps/meteor/app/livechat/imports/server/rest/integrations.ts
+++ apps/meteor/app/livechat/imports/server/rest/integrations.ts
@@ -1,5 +1,6 @@
 import { API } from '../../../../api/server';
 import { findIntegrationSettings } from '../../../server/api/lib/integrations';
+import { newIntegratorMessage } from '../../../server/lib/integrator';
 
 API.v1.addRoute(
 	'livechat/integrations.settings',
@@ -10,3 +11,37 @@ API.v1.addRoute(
 		},
 	},
 );
+
+API.v1.addRoute('livechat/integrations.send', {
+	async post() {
+		console.log((this as any).bodyParams);
+
+		const appId = 'get-app-id';
+		const appName = 'get-app-name';
+
+		const { visitorWaId, contactWaId } = (this as any).bodyParams;
+
+		const source = {
+			type: 'app',
+			id: appId,
+			alias: appName,
+			// ...(source &&
+			// 	source.type === 'app' && {
+			// 		sidebarIcon: source.sidebarIcon,
+			// 		defaultIcon: source.defaultIcon,
+			// 		label: source.label,
+			// 		destination: source.destination,
+			// 	}),
+		};
+
+		const result = await newIntegratorMessage({
+			...(this as any).bodyParams,
+			source,
+			roomCustomFields: {
+				whatsAppMessage: { visitorWaId, contactWaId },
+			},
+		});
+
+		return API.v1.success({ result });
+	},
+});
