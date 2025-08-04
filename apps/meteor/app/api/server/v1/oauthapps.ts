2f162a0dca79274d4458a5853afe64c506a7554f
Ahmed Nasser <ahmed.n.abdeltwab@gmail.com>
2025-08-04 16:28:09 +0300

    feat: Add OpenAPI Support to oauth-apps.delete API (#36606)

--- apps/meteor/app/api/server/v1/oauthapps.ts
+++ apps/meteor/app/api/server/v1/oauthapps.ts
@@ -4,7 +4,6 @@ import {
 	ajv,
 	isUpdateOAuthAppParams,
 	isOauthAppsGetParams,
-	isDeleteOAuthAppParams,
 	validateUnauthorizedErrorResponse,
 	validateBadRequestErrorResponse,
 	validateForbiddenErrorResponse,
@@ -18,50 +17,146 @@ import { updateOAuthApp } from '../../../oauth2-server-config/server/admin/metho
 import type { ExtractRoutesFromAPI } from '../ApiClass';
 import { API } from '../api';
 
-const oauthAppsListEndpoints = API.v1.get(
-	'oauth-apps.list',
-	{
-		authRequired: true,
-		query: ajv.compile<{ uid?: string }>({
-			type: 'object',
-			properties: {
-				uid: {
-					type: 'string',
-				},
-			},
-			additionalProperties: false,
-		}),
-		permissionsRequired: ['manage-oauth-apps'],
-		response: {
-			400: validateBadRequestErrorResponse,
-			401: validateUnauthorizedErrorResponse,
-			403: validateForbiddenErrorResponse,
-			200: ajv.compile<{ oauthApps: IOAuthApps[] }>({
-				type: 'object',
-				properties: {
-					oauthApps: {
-						type: 'array',
-						items: {
-							$ref: '#/components/schemas/IOAuthApps',
-						},
-					},
-					success: {
-						type: 'boolean',
-						enum: [true],
-					},
-				},
-				required: ['oauthApps', 'success'],
-				additionalProperties: false,
-			}),
+type DeleteOAuthAppParams = {
+	appId: string;
+};
+
+const DeleteOAuthAppParamsSchema = {
+	type: 'object',
+	properties: {
+		appId: {
+			type: 'string',
 		},
 	},
+	required: ['appId'],
+	additionalProperties: false,
+};
 
-	async function action() {
-		return API.v1.success({
-			oauthApps: await OAuthApps.find().toArray(),
-		});
+const isDeleteOAuthAppParams = ajv.compile<DeleteOAuthAppParams>(DeleteOAuthAppParamsSchema);
+
+export type OauthAppsAddParams = {
+	name: string;
+	active: boolean;
+	redirectUri: string;
+};
+
+const OauthAppsAddParamsSchema = {
+	type: 'object',
+	properties: {
+		name: {
+			type: 'string',
+		},
+		active: {
+			type: 'boolean',
+		},
+		redirectUri: {
+			type: 'string',
+		},
 	},
-);
+	required: ['name', 'active', 'redirectUri'],
+	additionalProperties: false,
+};
+
+const isOauthAppsAddParams = ajv.compile<OauthAppsAddParams>(OauthAppsAddParamsSchema);
+
+const oauthAppsEndpoints = API.v1
+	.get(
+		'oauth-apps.list',
+		{
+			authRequired: true,
+			query: ajv.compile<{ uid?: string }>({
+				type: 'object',
+				properties: {
+					uid: {
+						type: 'string',
+					},
+				},
+				additionalProperties: false,
+			}),
+			permissionsRequired: ['manage-oauth-apps'],
+			response: {
+				400: validateBadRequestErrorResponse,
+				401: validateUnauthorizedErrorResponse,
+				403: validateForbiddenErrorResponse,
+				200: ajv.compile<{ oauthApps: IOAuthApps[] }>({
+					type: 'object',
+					properties: {
+						oauthApps: {
+							type: 'array',
+							items: {
+								$ref: '#/components/schemas/IOAuthApps',
+							},
+						},
+						success: {
+							type: 'boolean',
+							enum: [true],
+						},
+					},
+					required: ['oauthApps', 'success'],
+					additionalProperties: false,
+				}),
+			},
+		},
+
+		async function action() {
+			return API.v1.success({
+				oauthApps: await OAuthApps.find().toArray(),
+			});
+		},
+	)
+	.post(
+		'oauth-apps.delete',
+		{
+			authRequired: true,
+			body: isDeleteOAuthAppParams,
+			permissionsRequired: ['manage-oauth-apps'],
+			response: {
+				400: validateBadRequestErrorResponse,
+				401: validateUnauthorizedErrorResponse,
+				403: validateForbiddenErrorResponse,
+				200: ajv.compile<boolean>({ type: 'boolean' }),
+			},
+		},
+
+		async function action() {
+			const { appId } = this.bodyParams;
+
+			const result = await deleteOAuthApp(this.userId, appId);
+
+			return API.v1.success(result);
+		},
+	)
+	.post(
+		'oauth-apps.create',
+		{
+			authRequired: true,
+			body: isOauthAppsAddParams,
+			permissionsRequired: ['manage-oauth-apps'],
+			response: {
+				400: validateBadRequestErrorResponse,
+				401: validateUnauthorizedErrorResponse,
+				403: validateForbiddenErrorResponse,
+				200: ajv.compile<{ application: IOAuthApps }>({
+					type: 'object',
+					properties: {
+						application: { $ref: '#/components/schemas/IOAuthApps' },
+						success: {
+							type: 'boolean',
+							enum: [true],
+						},
+					},
+					required: ['application', 'success'],
+					additionalProperties: false,
+				}),
+			},
+		},
+
+		async function action() {
+			const application = await addOAuthApp(this.bodyParams, this.userId);
+
+			return API.v1.success({ application });
+		},
+	);
 
 API.v1.addRoute(
 	'oauth-apps.get',
@@ -108,90 +203,9 @@ API.v1.addRoute(
 	},
 );
 
-API.v1.addRoute(
-	'oauth-apps.delete',
-	{
-		authRequired: true,
-		validateParams: isDeleteOAuthAppParams,
-		permissionsRequired: ['manage-oauth-apps'],
-	},
-	{
-		async post() {
-			const { appId } = this.bodyParams;
-
-			const result = await deleteOAuthApp(this.userId, appId);
-
-			return API.v1.success(result);
-		},
-	},
-);
-
-export type OauthAppsAddParams = {
-	name: string;
-	active: boolean;
-	redirectUri: string;
-};
-
-const OauthAppsAddParamsSchema = {
-	type: 'object',
-	properties: {
-		name: {
-			type: 'string',
-		},
-		active: {
-			type: 'boolean',
-		},
-		redirectUri: {
-			type: 'string',
-		},
-	},
-	required: ['name', 'active', 'redirectUri'],
-	additionalProperties: false,
-};
-
-const isOauthAppsAddParams = ajv.compile<OauthAppsAddParams>(OauthAppsAddParamsSchema);
-
-const oauthAppsCreateEndpoints = API.v1.post(
-	'oauth-apps.create',
-	{
-		authRequired: true,
-		body: isOauthAppsAddParams,
-		permissionsRequired: ['manage-oauth-apps'],
-		response: {
-			400: validateBadRequestErrorResponse,
-			401: validateUnauthorizedErrorResponse,
-			403: validateForbiddenErrorResponse,
-			200: ajv.compile<{ application: IOAuthApps }>({
-				type: 'object',
-				properties: {
-					application: { $ref: '#/components/schemas/IOAuthApps' },
-					success: {
-						type: 'boolean',
-						enum: [true],
-					},
-				},
-				required: ['application', 'success'],
-				additionalProperties: false,
-			}),
-		},
-	},
-
-	async function action() {
-		const application = await addOAuthApp(this.bodyParams, this.userId);
-
-		return API.v1.success({ application });
-	},
-);
-
-type OauthAppsCreateEndpoints = ExtractRoutesFromAPI<typeof oauthAppsCreateEndpoints>;
-
-type OauthAppsListEndpoints = ExtractRoutesFromAPI<typeof oauthAppsListEndpoints>;
-
-export type OAuthAppsEndpoints = OauthAppsCreateEndpoints | OauthAppsListEndpoints;
+export type OauthAppsEndpoints = ExtractRoutesFromAPI<typeof oauthAppsEndpoints>;
 
 declare module '@rocket.chat/rest-typings' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-empty-interface
-	interface Endpoints extends OauthAppsCreateEndpoints {}
-	// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-empty-interface
-	interface Endpoints extends OauthAppsListEndpoints {}
+	interface Endpoints extends OauthAppsEndpoints {}
 }
