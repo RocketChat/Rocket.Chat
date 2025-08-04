65291c05577099e8b563ecbfaa578ce2636aca54
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2025-08-04 15:50:40 -0300

    feat: support for redact message from RC to external (#36492)

--- apps/meteor/server/services/messages/hooks/BeforeFederationActions.ts
+++ apps/meteor/server/services/messages/hooks/BeforeFederationActions.ts
@@ -1,12 +1,12 @@
 import { isMessageFromMatrixFederation, isRoomFederated } from '@rocket.chat/core-typings';
 import type { AtLeast, IMessage, IRoom } from '@rocket.chat/core-typings';
 
-import { isFederationEnabled, isFederationReady } from '../../federation/utils';
+import { getFederationVersion, isFederationEnabled, isFederationReady } from '../../federation/utils';
 
 export class FederationActions {
 	public static shouldPerformAction(message: IMessage, room: AtLeast<IRoom, 'federated'>): boolean {
 		if (isMessageFromMatrixFederation(message) || isRoomFederated(room)) {
-			return isFederationEnabled() && isFederationReady();
+			return getFederationVersion() === 'native' || (isFederationEnabled() && isFederationReady());
 		}
 
 		return true;
