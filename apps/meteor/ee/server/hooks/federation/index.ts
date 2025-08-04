2e54712645afd45c39c32402d11e91cc6161dd91
Marcos Spessatto Defendi <marcos.defendi@rocket.chat>
2025-08-04 15:50:40 -0300

    feat: support for redact message from RC to external (#36492)

--- apps/meteor/ee/server/hooks/federation/index.ts
+++ apps/meteor/ee/server/hooks/federation/index.ts
@@ -1,9 +1,26 @@
 import { FederationMatrix } from '@rocket.chat/core-services';
 import type { IMessage, IRoom, IUser } from '@rocket.chat/core-typings';
+import { Messages } from '@rocket.chat/models';
 
 import { callbacks } from '../../../../lib/callbacks';
 import { afterLeaveRoomCallback } from '../../../../lib/callbacks/afterLeaveRoomCallback';
 import { afterRemoveFromRoomCallback } from '../../../../lib/callbacks/afterRemoveFromRoomCallback';
+
+callbacks.add(
+	'afterDeleteMessage',
+	async (message: IMessage) => {
+		if (!message.federation?.eventId) {
+			return;
+		}
+		const isEchoMessage = !(await Messages.findOneByFederationId(message.federation?.eventId));
+		if (isEchoMessage) {
+			return;
+		}
+		await FederationMatrix.deleteMessage(message);
+	},
+	callbacks.priority.MEDIUM,
+	'native-federation-after-delete-message',
+);
 
 callbacks.add(
 	'native-federation.onAddUsersToRoom',
