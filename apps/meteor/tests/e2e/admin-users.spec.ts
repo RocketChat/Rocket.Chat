b072aadd3bcfc725959716981962f84592d8699f
Harmeet221 <harmeetkaur568@gmail.com>
2025-08-04 18:28:06 +0530

    update tests to run serially

--- apps/meteor/tests/e2e/admin-users.spec.ts
+++ apps/meteor/tests/e2e/admin-users.spec.ts
@@ -1,193 +1,184 @@
-import { faker } from '@faker-js/faker';
-import type { IUser } from '@rocket.chat/core-typings';
+import type { BrowserContext, Page } from '@playwright/test';
 
+import { DEFAULT_USER_CREDENTIALS } from './config/constants';
 import { Users } from './fixtures/userStates';
 import { Admin, Registration, Utils } from './page-objects';
 import { test, expect } from './utils/test';
+import type { ITestUser } from './utils/user-helpers';
+import { createTestUser } from './utils/user-helpers';
 
-let user: IUser & { username: string };
-let password: string;
+let user: ITestUser;
 let admin: Admin;
 
-test.beforeAll('Create a new user', async ({ api }) => {
-	password = faker.internet.password();
-	const response = await api.post('/users.create', {
-		email: faker.internet.email(),
-		name: faker.person.fullName(),
-		password,
-		username: faker.internet.userName(),
-	});
-	expect(response.status()).toBe(200);
-	const json = await response.json();
-	user = json.user;
-});
-
-test.afterAll('Delete the new user', async ({ api }) => {
-	const response = await api.post('/users.delete', { userId: user._id });
-	expect(response.status()).toBe(200);
-});
-
-test.describe('Admin > Users', () => {
-	test.use({ storageState: Users.admin.state });
-
-	test.beforeEach('Go to /admin/users', async ({ page }) => {
-		admin = new Admin(page);
-		await page.goto('/admin/users');
+test.describe.serial('Admin > Users', () => {
+	test.beforeAll('Create a new user', async ({ api }) => {
+		user = await createTestUser(api);
 	});
 
-	test(
-		'New user shows in correct tabs when deactivated',
-		{
-			tag: '@admin',
-			annotation: {
-				type: 'issue',
-				description: 'https://rocketchat.atlassian.net/browse/SUP-775',
-			},
-		},
-		async ({ page }) => {
-			const { username } = user;
-			await admin.tabs.users.inputSearch.fill(username);
+	test.afterAll('Delete the new user', async () => {
+		await user.delete();
+	});
+
+	test.describe.serial('New user tab visibility on deactivation', () => {
+		test.use({ storageState: Users.admin.state });
+
+		test.beforeEach('Go to /admin/users', async ({ page }) => {
+			admin = new Admin(page);
+			await page.goto('/admin/users');
+		});
+		test('New user shows in correct tabs when deactivated', async ({ page }) => {
+			await admin.tabs.users.inputSearch.fill(user.data.username);
 
 			await test.step('is visible in the All tab', async () => {
 				await admin.tabs.users.tabAll.click();
-				await expect(admin.getUserRowByUsername(username)).toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
 			});
 
 			await test.step('is visible in the Pending tab', async () => {
 				await admin.tabs.users.tabPending.click();
-				await expect(admin.getUserRowByUsername(username)).toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
 			});
 
 			await test.step('is not visible in the Active tab', async () => {
 				await admin.tabs.users.tabActive.click();
-				await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
 			});
 
 			await test.step('is not visible in the Deactivated tab', async () => {
 				await admin.tabs.users.tabDeactivated.click();
-				await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
 			});
 
 			await test.step('moves from Pending to Deactivated tab', async () => {
 				await page.getByRole('tab', { name: 'Pending' }).click();
 				await admin.tabs.users.btnMoreActionsMenu.click();
 				await admin.tabs.users.menuItemDeactivated.click();
-				await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
 				await admin.tabs.users.tabDeactivated.click();
-				await expect(admin.getUserRowByUsername(username)).toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
 			});
 
 			await test.step('moves from Deactivated to Pending tab', async () => {
 				await admin.tabs.users.tabDeactivated.click();
 				await admin.tabs.users.btnMoreActionsMenu.click();
 				await admin.tabs.users.menuItemActivate.click();
-				await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
 				await admin.tabs.users.tabPending.click();
-				await expect(admin.getUserRowByUsername(username)).toBeVisible();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
 			});
-		},
-	);
-});
+		});
+	});
+	test.describe.serial('Login', () => {
+		let context: BrowserContext;
+		let page: Page;
+		let poRegistration: Registration;
+		let poUtils: Utils;
 
-test.describe('Login', () => {
-	let poRegistration: Registration;
-	let poUtils: Utils;
-	test('Login as a newly created user and verify its status is active', async ({ browser }) => {
-		const context = await browser.newContext();
-		const page = await context.newPage();
-		poRegistration = new Registration(page);
-		poUtils = new Utils(page);
-
-		await test.step('should log in as the newly created user', async () => {
-			await page.goto('/login');
-			await poRegistration.username.fill(user.username);
-			await poRegistration.inputPassword.fill(password);
-			await poRegistration.btnLogin.click();
+		test.beforeAll(async ({ browser }) => {
+			context = await browser.newContext();
+			page = await context.newPage();
+			poRegistration = new Registration(page);
+			poUtils = new Utils(page);
 		});
 
-		await test.step('Assert user is logged in', async () => {
-			await expect(poUtils.mainContent).toBeVisible();
-		});
-
-		await test.step('close browser', async () => {
+		test.afterAll(async () => {
 			await context.close();
 		});
-	});
-});
 
-test.describe('Login as a admin and verify registration status', () => {
-	test.use({ storageState: Users.admin.state });
+		test('Login as a newly created user and verify its status is active', async () => {
+			await page.goto('/login');
+			await test.step('should log in as the newly created user', async () => {
+				await poRegistration.username.fill(user.data.username);
+				await poRegistration.inputPassword.fill(DEFAULT_USER_CREDENTIALS.password);
+				await poRegistration.btnLogin.click();
+			});
 
-	test.beforeEach('Go to /admin/users', async ({ page }) => {
-		admin = new Admin(page);
-		await page.goto('/admin/users');
-	});
-
-	test('After the first login, the user gets listed under the Active tab', async () => {
-		const { username } = user;
-		await admin.tabs.users.inputSearch.fill(username);
-
-		await test.step('is visible in the All tab', async () => {
-			await admin.tabs.users.tabActive.click();
-			await expect(admin.getUserRowByUsername(username)).toBeVisible();
-		});
-
-		await test.step('is not visible in the Pending tab', async () => {
-			await admin.tabs.users.tabPending.click();
-			await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
-		});
-
-		await test.step('is visible in the Active tab', async () => {
-			await admin.tabs.users.tabActive.click();
-			await expect(admin.getUserRowByUsername(username)).toBeVisible();
-		});
-
-		await test.step('is not visible in the Deactivated tab', async () => {
-			await admin.tabs.users.tabDeactivated.click();
-			await expect(admin.getUserRowByUsername(username)).not.toBeVisible();
+			await test.step('Assert user is logged in', async () => {
+				await expect(poUtils.mainContent).toBeVisible();
+			});
 		});
 	});
 
-	test('Make a newly created user as admin', async () => {
-		const { username } = user;
-		await admin.tabs.users.inputSearch.fill(username);
+	test.describe('User Status Management', () => {
+		test.use({ storageState: Users.admin.state });
 
-		await test.step('User is visible in the All tab', async () => {
-			await admin.tabs.users.tabAll.click();
-			await expect(admin.getUserRowByUsername(username)).toBeVisible();
+		test.beforeEach('Go to /admin/users', async ({ page }) => {
+			admin = new Admin(page);
+			await page.goto('/admin/users');
 		});
 
-		await test.step('make a user admin', async () => {
-			await admin.tabs.users.openUserActionMenu(username);
-			await admin.tabs.users.menuItemMakeAdmin.click();
-			await expect(admin.tabs.users.toastMessage).toContainText('User is now an admin');
-		});
+		test('After the first login, the user gets listed under the Active tab', async () => {
+			await admin.tabs.users.inputSearch.fill(user.data.username);
 
-		await test.step('verify user is admin', async () => {
-			await admin.tabs.users.getUserRowByUsername(username).click();
-			await expect(admin.tabs.users.openDialog).toBeVisible();
-			await expect(admin.tabs.users.openDialog).toContainText('Admin');
+			await test.step('is visible in the All tab', async () => {
+				await admin.tabs.users.tabActive.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
+			});
+
+			await test.step('is not visible in the Pending tab', async () => {
+				await admin.tabs.users.tabPending.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
+			});
+
+			await test.step('is visible in the Active tab', async () => {
+				await admin.tabs.users.tabActive.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
+			});
+
+			await test.step('is not visible in the Deactivated tab', async () => {
+				await admin.tabs.users.tabDeactivated.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).not.toBeVisible();
+			});
 		});
 	});
 
-	test('Remove role as admin', async () => {
-		const { username } = user;
-		await admin.tabs.users.inputSearch.fill(username);
-		await test.step('User is visible in the All tab', async () => {
-			await admin.tabs.users.tabAll.click();
-			await expect(admin.getUserRowByUsername(username)).toBeVisible();
+	test.describe.serial('Admin Role Management', () => {
+		test.use({ storageState: Users.admin.state });
+
+		test.beforeEach('Go to /admin/users', async ({ page }) => {
+			admin = new Admin(page);
+			await page.goto('/admin/users');
 		});
 
-		await test.step('remove admin role', async () => {
-			await admin.tabs.users.openUserActionMenu(username);
-			await admin.tabs.users.menuItemRemoveAdmin.click();
-			await expect(admin.tabs.users.toastMessage).toHaveText('User is no longer an admin');
+		test('Make a newly created user as admin', async () => {
+			await admin.tabs.users.inputSearch.fill(user.data.username);
+
+			await test.step('User is visible in the All tab', async () => {
+				await admin.tabs.users.tabAll.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
+			});
+
+			await test.step('make a user admin', async () => {
+				await admin.tabs.users.openUserActionMenu(user.data.username);
+				await admin.tabs.users.menuItemMakeAdmin.click();
+				await expect(admin.tabs.users.toastMessage).toContainText('User is now an admin');
+			});
+
+			await test.step('verify user is admin', async () => {
+				await admin.tabs.users.getUserRowByUsername(user.data.username).click();
+				await expect(admin.tabs.users.openDialog).toBeVisible();
+				await expect(admin.tabs.users.openDialog).toContainText('Admin');
+			});
 		});
 
-		await test.step('verify user role as admin is removed', async () => {
-			await admin.tabs.users.getUserRowByUsername(username).click();
-			await expect(admin.tabs.users.openDialog).toBeVisible();
-			await expect(admin.tabs.users.openDialog).not.toHaveText('Admin');
+		test('Remove role as admin', async () => {
+			await admin.tabs.users.inputSearch.fill(user.data.username);
+			await test.step('User is visible in the All tab', async () => {
+				await admin.tabs.users.tabAll.click();
+				await expect(admin.getUserRowByUsername(user.data.username)).toBeVisible();
+			});
+
+			await test.step('remove admin role', async () => {
+				await admin.tabs.users.openUserActionMenu(user.data.username);
+				await admin.tabs.users.menuItemRemoveAdmin.click();
+				await expect(admin.tabs.users.toastMessage).toHaveText('User is no longer an admin');
+			});
+
+			await test.step('verify user role as admin is removed', async () => {
+				await admin.tabs.users.getUserRowByUsername(user.data.username).click();
+				await expect(admin.tabs.users.openDialog).toBeVisible();
+				await expect(admin.tabs.users.openDialog).not.toHaveText('Admin');
+			});
 		});
 	});
 });
