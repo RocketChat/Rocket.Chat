b33d9992ffdfc1d594810b6513c87c65433d4433
gabriellsh <henriques.gabriell@gmail.com>
2025-08-04 19:18:20 -0300

    Fix story

--- packages/ui-voip/src/v2/views/OngoingCall.stories.tsx
+++ packages/ui-voip/src/v2/views/OngoingCall.stories.tsx
@@ -1,12 +1,16 @@
+import { mockAppRoot } from '@rocket.chat/mock-providers';
 import type { Meta, StoryFn } from '@storybook/react';
 
 import OngoingCall from './OngoingCall';
 import MediaCallProviderMock from '../MockedMediaCallProvider';
 
+const mockedContexts = mockAppRoot().buildStoryDecorator();
+
 export default {
 	title: 'V2/Views/OngoingCall',
 	component: OngoingCall,
 	decorators: [
+		mockedContexts,
 		(Story) => (
 			<MediaCallProviderMock>
 				<Story />
