3fb55c5475e17d20f36640857b86446f1f3865c0
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-05 15:53:10 -0300

    feat: Outbound Message: Recipient Step (#36369)

--- apps/meteor/tests/mocks/data.ts
+++ apps/meteor/tests/mocks/data.ts
@@ -334,6 +334,20 @@ export function createFakeContactChannel(overrides?: Partial<Serialized<ILivecha
 	};
 }
 
+export function createFakeContact(overrides?: Partial<Serialized<ILivechatContact>>): Serialized<ILivechatContact> {
+	return {
+		_id: faker.string.uuid(),
+		_updatedAt: new Date().toISOString(),
+		name: pullNextVisitorName(),
+		phones: [{ phoneNumber: faker.phone.number() }],
+		emails: [{ address: faker.internet.email() }],
+		unknown: true,
+		channels: [createFakeContactChannel()],
+		createdAt: new Date().toISOString(),
+		...overrides,
+	};
+}
+
 export function createFakeContactWithManagerData(
 	overrides?: Partial<Serialized<ILivechatContactWithManagerData>>,
 ): Serialized<ILivechatContactWithManagerData> {
@@ -347,20 +361,6 @@ export function createFakeContactWithManagerData(
 			username: faker.internet.userName(),
 			...contactManagerOverwrites,
 		},
-	};
-}
-
-export function createFakeContact(overrides?: Partial<Serialized<ILivechatContact>>): Serialized<ILivechatContact> {
-	return {
-		_id: faker.string.uuid(),
-		_updatedAt: new Date().toISOString(),
-		name: pullNextVisitorName(),
-		phones: [{ phoneNumber: faker.phone.number() }],
-		emails: [{ address: faker.internet.email() }],
-		unknown: true,
-		channels: [createFakeContactChannel()],
-		createdAt: new Date().toISOString(),
-		...overrides,
 	};
 }
 
