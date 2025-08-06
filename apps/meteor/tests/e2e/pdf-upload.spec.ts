0a3b1f831f0df37651aedc576081ed82c9ac21f3
Matheus Cardoso <matheus@cardo.so>
2025-08-06 15:07:49 -0300

    test: pdf with embedded exif data

---
+++ apps/meteor/tests/e2e/pdf-upload.spec.ts
@@ -0,0 +1,58 @@
+import { Users } from './fixtures/userStates';
+import { HomeChannel } from './page-objects';
+import { createTargetChannel, getSettingValueById, setSettingValueById } from './utils';
+import { test, expect } from './utils/test';
+
+test.use({ storageState: Users.user1.state });
+
+test.describe('pdf-upload', () => {
+	let settingDefaultValue: unknown;
+	let poHomeChannel: HomeChannel;
+	let targetChannel: string;
+
+	test.beforeAll(async ({ api }) => {
+		settingDefaultValue = await getSettingValueById(api, 'Message_Attachments_Strip_Exif');
+		targetChannel = await createTargetChannel(api, { members: ['user1'] });
+	});
+
+	test.beforeEach(async ({ page }) => {
+		poHomeChannel = new HomeChannel(page);
+		await page.goto('/home');
+		await poHomeChannel.sidenav.openChat(targetChannel);
+	});
+
+	test.afterAll(async ({ api }) => {
+		await setSettingValueById(api, 'Message_Attachments_Strip_Exif', settingDefaultValue);
+		expect((await api.post('/channels.delete', { roomName: targetChannel })).status()).toBe(200);
+	});
+
+	test.describe('strip exif disabled', () => {
+		test.beforeAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Attachments_Strip_Exif', false);
+		});
+
+		test('should succeed upload of test_exif_embed.pdf', async () => {
+			await poHomeChannel.content.sendFileMessage('test_exif_embed.pdf');
+			await poHomeChannel.content.fileNameInput.fill('test_exif_embed.pdf');
+			await poHomeChannel.content.descriptionInput.fill('text_exif_embed_description');
+			await poHomeChannel.content.btnModalConfirm.click();
+
+			await expect(poHomeChannel.content.getFileDescription).toHaveText('text_exif_embed_description');
+		});
+	});
+
+	test.describe('strip exif enabled', () => {
+		test.beforeAll(async ({ api }) => {
+			await setSettingValueById(api, 'Message_Attachments_Strip_Exif', true);
+		});
+
+		test('should succeed upload of test_exif_embed.pdf', async () => {
+			await poHomeChannel.content.sendFileMessage('test_exif_embed.pdf');
+			await poHomeChannel.content.fileNameInput.fill('test_exif_embed.pdf');
+			await poHomeChannel.content.descriptionInput.fill('text_exif_embed_description');
+			await poHomeChannel.content.btnModalConfirm.click();
+
+			await expect(poHomeChannel.content.getFileDescription).toHaveText('text_exif_embed_description');
+		});
+	});
+});
