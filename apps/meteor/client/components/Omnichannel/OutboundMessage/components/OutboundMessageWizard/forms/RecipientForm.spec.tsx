628422e50fc655700a84b4ba5d094b3ffb213a8e
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-07 16:16:13 -0300

    refactor: Outbound Message Recipient Step Improvements (#36654)

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/forms/RecipientForm.spec.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/forms/RecipientForm.spec.tsx
@@ -2,6 +2,7 @@ import { mockAppRoot } from '@rocket.chat/mock-providers';
 import { screen, render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
+import { VirtuosoMockContext } from 'react-virtuoso';
 
 import RecipientForm from './RecipientForm';
 import { createFakeContactChannel, createFakeContactWithManagerData } from '../../../../../../../tests/mocks/data';
@@ -14,13 +15,15 @@ jest.mock('tinykeys', () => ({
 	default: jest.fn().mockReturnValue(() => () => undefined),
 }));
 
-const recipientPhoneNumber = '+12125554567';
-const senderPhoneNumber = '+12127774567';
+const recipientOnePhoneNumber = '+12125554567';
+const recipientTwoPhoneNumber = '+12125557788';
+const senderOnePhoneNumber = '+12127774567';
+const senderTwoPhoneNumber = '+12127778877';
 
-const contactMock = createFakeContactWithManagerData({
+const contactOneMock = createFakeContactWithManagerData({
 	_id: 'contact-1',
 	name: 'Contact 1',
-	phones: [{ phoneNumber: recipientPhoneNumber }],
+	phones: [{ phoneNumber: recipientOnePhoneNumber }],
 	channels: [
 		createFakeContactChannel({
 			name: 'provider-1',
@@ -29,29 +32,50 @@ const contactMock = createFakeContactWithManagerData({
 	],
 });
 
-const getContactMock = jest.fn().mockImplementation(() => ({ contact: contactMock }));
-const getContactMocksMock = jest.fn().mockImplementation(() => ({
-	contacts: [contactMock],
-	count: 1,
+const contactTwoMock = createFakeContactWithManagerData({
+	_id: 'contact-2',
+	name: 'Contact 2',
+	phones: [{ phoneNumber: recipientTwoPhoneNumber }],
+	channels: [
+		createFakeContactChannel({
+			name: 'provider-2',
+			lastChat: { _id: '', ts: new Date().toISOString() },
+		}),
+	],
+});
+
+const getContactMock = jest.fn().mockImplementation(() => ({ contact: contactOneMock }));
+const getContactsMock = jest.fn().mockImplementation(() => ({
+	contacts: [contactOneMock, contactTwoMock],
+	count: 2,
 	offset: 0,
-	total: 1,
+	total: 2,
 	success: true,
 }));
 
-const providerMock = createFakeProviderMetadata({
+const providerOneMock = createFakeProviderMetadata({
 	providerId: 'provider-1',
 	providerName: 'Provider 1',
 	templates: {
-		[senderPhoneNumber]: [createFakeOutboundTemplate({ phoneNumber: senderPhoneNumber })],
+		[senderOnePhoneNumber]: [createFakeOutboundTemplate({ phoneNumber: senderOnePhoneNumber })],
 	},
 });
-const getProvidersMock = jest.fn().mockImplementation(() => ({ providers: [providerMock] }));
-const getProviderMock = jest.fn().mockImplementation(() => ({ metadata: providerMock }));
+
+const providerTwoMock = createFakeProviderMetadata({
+	providerId: 'provider-2',
+	providerName: 'Provider 2',
+	templates: {
+		[senderTwoPhoneNumber]: [createFakeOutboundTemplate({ phoneNumber: senderTwoPhoneNumber })],
+	},
+});
+
+const getProvidersMock = jest.fn().mockImplementation(() => ({ providers: [providerOneMock, providerTwoMock] }));
+const getProviderMock = jest.fn().mockImplementation(() => ({ metadata: providerOneMock }));
 
 const appRoot = mockAppRoot()
 	.withJohnDoe()
 	.withEndpoint('GET', '/v1/omnichannel/contacts.get', () => getContactMock())
-	.withEndpoint('GET', '/v1/omnichannel/contacts.search', () => getContactMocksMock())
+	.withEndpoint('GET', '/v1/omnichannel/contacts.search', () => getContactsMock())
 	.withEndpoint('GET', '/v1/omnichannel/outbound/providers', () => getProvidersMock())
 	.withEndpoint('GET', '/v1/omnichannel/outbound/providers/:id/metadata', () => getProviderMock())
 	.withTranslations('en', 'core', {
@@ -60,7 +84,10 @@ const appRoot = mockAppRoot()
 		No_phone_number_yet_edit_contact: 'No phone number yet <1>Edit contact</1>',
 		No_phone_number_available_for_selected_channel: 'No phone number available for the selected channel',
 		Submit: 'Submit',
-	});
+	})
+	.wrap((children) => (
+		<VirtuosoMockContext.Provider value={{ viewportHeight: 300, itemHeight: 28 }}>{children}</VirtuosoMockContext.Provider>
+	));
 
 describe('RecipientForm', () => {
 	const defaultProps = {
@@ -80,8 +107,8 @@ describe('RecipientForm', () => {
 		expect(screen.getByLabelText('To*')).toBeInTheDocument();
 		expect(screen.getByLabelText('From*')).toBeInTheDocument();
 
-		expect(screen.getByLabelText('Contact*')).not.toHaveAttribute('aria-disabled');
-		expect(screen.getByLabelText('Channel*')).toHaveAttribute('aria-disabled', 'true');
+		await waitFor(() => expect(screen.getByLabelText('Contact*')).not.toHaveAttribute('aria-disabled'));
+		await waitFor(() => expect(screen.getByLabelText('Channel*')).toHaveAttribute('aria-disabled', 'true'));
 		expect(screen.getByLabelText('To*')).toHaveClass('disabled');
 		expect(screen.getByLabelText('From*')).toHaveClass('disabled');
 	});
@@ -96,8 +123,8 @@ describe('RecipientForm', () => {
 		const defaultValues = {
 			contactId: 'contact-1',
 			providerId: 'provider-1',
-			recipient: recipientPhoneNumber,
-			sender: senderPhoneNumber,
+			recipient: recipientOnePhoneNumber,
+			sender: senderOnePhoneNumber,
 		};
 
 		render(<RecipientForm defaultValues={defaultValues} {...defaultProps} />, { wrapper: appRoot.build() });
@@ -111,7 +138,7 @@ describe('RecipientForm', () => {
 	it('should disable provider selection when no contact is selected', async () => {
 		render(<RecipientForm {...defaultProps} />, { wrapper: appRoot.build() });
 
-		expect(screen.getByLabelText('Contact*')).not.toHaveAttribute('aria-disabled');
+		await waitFor(() => expect(screen.getByLabelText('Contact*')).not.toHaveAttribute('aria-disabled'));
 		expect(screen.getByLabelText('Channel*')).toHaveAttribute('aria-disabled', 'true');
 	});
 
@@ -119,7 +146,7 @@ describe('RecipientForm', () => {
 		const defaultValues = { contactId: 'contact-1' };
 		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} />, { wrapper: appRoot.build() });
 
-		expect(screen.getByLabelText('Channel*')).toHaveAttribute('aria-disabled', 'false');
+		await waitFor(() => expect(screen.getByLabelText('Channel*')).toHaveAttribute('aria-disabled', 'false'));
 		expect(screen.getByLabelText('To*')).toHaveClass('disabled');
 	});
 
@@ -128,20 +155,8 @@ describe('RecipientForm', () => {
 
 		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} />, { wrapper: appRoot.build() });
 
-		expect(screen.getByLabelText('To*')).not.toHaveClass('disabled');
+		await waitFor(() => expect(screen.getByLabelText('To*')).not.toHaveClass('disabled'));
 		expect(screen.getByLabelText('From*')).toHaveClass('disabled');
-	});
-
-	it('should enable sender selection when recipient is selected', async () => {
-		const defaultValues = {
-			contactId: 'contact-1',
-			providerId: 'provider-1',
-			recipient: recipientPhoneNumber,
-		};
-
-		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} />, { wrapper: appRoot.build() });
-
-		expect(screen.getByLabelText('From*')).not.toHaveClass('disabled');
 	});
 
 	it('should show retry button when contact fetch fails', async () => {
@@ -149,11 +164,23 @@ describe('RecipientForm', () => {
 
 		render(<RecipientForm defaultValues={{ contactId: 'contact-1' }} {...defaultProps} />, { wrapper: appRoot.build() });
 
-		const contactErrorMessage = await screen.findByText('Error loading contact information');
-		expect(contactErrorMessage).toBeInTheDocument();
+		await waitFor(() => expect(screen.getByLabelText('Contact*')).toHaveAccessibleDescription('Error loading contact information'));
 
 		const retryButton = screen.getByRole('button', { name: 'Retry' });
 		expect(retryButton).toBeInTheDocument();
+	});
+
+	it('should disable recipient field when contact fetch fails', async () => {
+		getContactMock.mockImplementationOnce(() => Promise.reject());
+		const defaultValues = {
+			contactId: 'contact-1',
+			providerId: 'provider-1',
+		};
+
+		render(<RecipientForm defaultValues={defaultValues} {...defaultProps} />, { wrapper: appRoot.build() });
+
+		await waitFor(() => expect(screen.getByLabelText('To*')).not.toHaveTextContent('Loading...'));
+		expect(screen.getByLabelText('To*')).toHaveClass('disabled');
 	});
 
 	it('should show retry button when provider fetch fails', async () => {
@@ -168,6 +195,17 @@ describe('RecipientForm', () => {
 
 		const retryButton = screen.getByRole('button', { name: 'Retry' });
 		expect(retryButton).toBeInTheDocument();
+	});
+
+	it('should disable sender when provider fetch fails', async () => {
+		getProviderMock.mockImplementationOnce(() => Promise.reject());
+
+		render(<RecipientForm defaultValues={{ contactId: 'contact-1', providerId: 'provider-1' }} {...defaultProps} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await waitFor(() => expect(screen.getByLabelText('From*')).not.toHaveTextContent('Loading...'));
+		expect(screen.getByLabelText('From*')).toHaveClass('disabled');
 	});
 
 	it('should call retry contact fetch when retry button is clicked', async () => {
@@ -205,8 +243,7 @@ describe('RecipientForm', () => {
 			wrapper: appRoot.build(),
 		});
 
-		const hintText = await screen.findByText(/Last contact/);
-		expect(hintText).toBeInTheDocument();
+		await waitFor(() => expect(screen.getByLabelText('Channel*')).toHaveAccessibleDescription('Last contact a few seconds ago'));
 	});
 
 	it('should call onSubmit with correct values when form is submitted', async () => {
@@ -214,8 +251,8 @@ describe('RecipientForm', () => {
 		const defaultValues = {
 			contactId: 'contact-1',
 			providerId: 'provider-1',
-			recipient: recipientPhoneNumber,
-			sender: senderPhoneNumber,
+			recipient: recipientOnePhoneNumber,
+			sender: senderOnePhoneNumber,
 		};
 
 		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={handleSubmit} />, {
@@ -228,18 +265,30 @@ describe('RecipientForm', () => {
 			expect(handleSubmit).toHaveBeenCalledWith({
 				contactId: 'contact-1',
 				providerId: 'provider-1',
-				recipient: recipientPhoneNumber,
-				sender: senderPhoneNumber,
-				contact: contactMock,
-				provider: providerMock,
+				recipient: recipientOnePhoneNumber,
+				sender: senderOnePhoneNumber,
+				contact: contactOneMock,
+				provider: providerOneMock,
 			}),
 		);
+	});
+
+	it('should not call onSubmit when form is invalid', async () => {
+		const handleSubmit = jest.fn();
+
+		render(<RecipientForm {...defaultProps} onSubmit={handleSubmit} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+		await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
 	});
 
 	it('should display no phone number error when contact has no phones', async () => {
 		const handleSubmit = jest.fn();
 		getContactMock.mockImplementationOnce(() => ({
-			contact: createFakeContactWithManagerData({ ...contactMock, phones: undefined }),
+			contact: createFakeContactWithManagerData({ ...contactOneMock, phones: undefined }),
 		}));
 
 		const defaultValues = {
@@ -265,7 +314,7 @@ describe('RecipientForm', () => {
 		const defaultValues = {
 			contactId: 'contact-1',
 			providerId: 'provider-1',
-			recipient: recipientPhoneNumber,
+			recipient: recipientOnePhoneNumber,
 		};
 
 		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onDirty={mockOnDirty} />, {
@@ -282,16 +331,39 @@ describe('RecipientForm', () => {
 		expect(mockOnDirty).toHaveBeenCalledTimes(1);
 	});
 
-	it('should show correct error when no phone numbers available for selected channel', async () => {
+	it('should clear recipient field when contact changes', async () => {
+		getContactMock.mockImplementationOnce(() => ({ contact: contactOneMock }));
+		getContactMock.mockImplementationOnce(() => ({ contact: contactTwoMock }));
+
+		const defaultValues = {
+			contactId: 'contact-1',
+			providerId: 'provider-1',
+			recipient: recipientOnePhoneNumber,
+		};
+
+		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={jest.fn()} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await waitFor(() => expect(screen.getByLabelText('To*')).toHaveTextContent(/\+1 212-555-4567/));
+
+		await userEvent.click(screen.getByLabelText('Contact*'));
+		await userEvent.click(await screen.findByRole('option', { name: /Contact 2/ }));
+
+		await waitFor(() => expect(screen.getByLabelText('To*')).toHaveTextContent('Contact_detail'));
+		expect(screen.getByLabelText('To*')).toHaveAccessibleDescription('Required_field');
+	});
+
+	it('should validate sender when no phone numbers available for selected channel', async () => {
 		const handleSubmit = jest.fn();
 		getProviderMock.mockImplementationOnce(() => ({
-			metadata: createFakeProviderMetadata({ ...providerMock, templates: {} }),
+			metadata: createFakeProviderMetadata({ ...providerOneMock, templates: {} }),
 		}));
 
 		const defaultValues = {
 			contactId: 'contact-1',
 			providerId: 'provider-1',
-			recipient: recipientPhoneNumber,
+			recipient: recipientOnePhoneNumber,
 		};
 
 		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={handleSubmit} />, {
@@ -300,9 +372,105 @@ describe('RecipientForm', () => {
 
 		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
 
-		const errorMessage = await screen.findByText('No phone number available for the selected channel');
-		expect(errorMessage).toBeInTheDocument();
+		expect(screen.getByLabelText('From*')).toHaveAccessibleDescription('No phone number available for the selected channel');
 	});
 
-	// TODO: it('should validate sender field when provider changes');
+	it('should clear sender field when provider changes', async () => {
+		getProviderMock.mockImplementationOnce(() => ({ metadata: providerOneMock }));
+		getProviderMock.mockImplementationOnce(() => ({ metadata: providerTwoMock }));
+
+		const defaultValues = {
+			contactId: 'contact-1',
+			providerId: 'provider-1',
+			recipient: recipientOnePhoneNumber,
+			sender: senderOnePhoneNumber,
+		};
+
+		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={jest.fn()} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await waitFor(() => expect(screen.getByLabelText('From*')).toHaveTextContent(/\+1 212-777-4567/));
+
+		await userEvent.click(screen.getByLabelText('Channel*'));
+		await userEvent.click(await screen.findByRole('option', { name: /Provider 2/ }));
+
+		await waitFor(() => expect(screen.getByLabelText('From*')).toHaveTextContent('Workspace_detail'));
+		expect(screen.getByLabelText('From*')).toHaveAccessibleDescription('Required_field');
+	});
+
+	it('should validate contact field is required', async () => {
+		const handleSubmit = jest.fn();
+		render(<RecipientForm {...defaultProps} onSubmit={handleSubmit} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+		expect(screen.getByLabelText('Contact*')).toHaveAccessibleDescription('Required_field');
+
+		await userEvent.click(screen.getByLabelText('Contact*'));
+		await userEvent.click(await screen.findByRole('option', { name: /Contact 1/ }));
+
+		expect(screen.getByLabelText('Contact*')).not.toHaveAccessibleDescription('Required_field');
+	});
+
+	it('should validate channel field is required', async () => {
+		const handleSubmit = jest.fn();
+		const defaultValues = {
+			contactId: 'contact-1',
+		};
+		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={handleSubmit} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+		expect(screen.getByLabelText('Channel*')).toHaveAccessibleDescription('Required_field');
+
+		await userEvent.click(screen.getByLabelText('Channel*'));
+		await userEvent.click(await screen.findByRole('option', { name: /Provider 1/ }));
+
+		expect(screen.getByLabelText('Channel*')).not.toHaveAccessibleDescription('Required_field');
+		await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
+	});
+
+	it('should validate recipient field is required', async () => {
+		const handleSubmit = jest.fn();
+		const defaultValues = {
+			contactId: 'contact-1',
+			providerId: 'provider-1',
+		};
+		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={handleSubmit} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+		expect(screen.getByLabelText('To*')).toHaveAccessibleDescription('Required_field');
+
+		await userEvent.click(screen.getByLabelText('To*'));
+		await userEvent.click(await screen.findByRole('option', { name: /\+1 212-555-4567/ }));
+
+		expect(screen.getByLabelText('To*')).not.toHaveAccessibleDescription('Required_field');
+		await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
+	});
+
+	it('should validate sender field is required', async () => {
+		const handleSubmit = jest.fn();
+		const defaultValues = {
+			contactId: 'contact-1',
+			providerId: 'provider-1',
+			recipient: recipientOnePhoneNumber,
+		};
+		render(<RecipientForm {...defaultProps} defaultValues={defaultValues} onSubmit={handleSubmit} />, {
+			wrapper: appRoot.build(),
+		});
+
+		await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+		expect(screen.getByLabelText('From*')).toHaveAccessibleDescription('Required_field');
+
+		await userEvent.click(screen.getByLabelText('From*'));
+		await userEvent.click(await screen.findByRole('option', { name: /\+1 212-777-4567/ }));
+
+		expect(screen.getByLabelText('From*')).not.toHaveAccessibleDescription('Required_field');
+		await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
+	});
 });
