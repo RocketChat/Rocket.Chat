5da003367bc4a4c22e312381e9574adcd1965988
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-08-07 12:59:32 +0300

    fix: the lint

--- packages/core-typings/src/Ajv.ts
+++ packages/core-typings/src/Ajv.ts
@@ -8,4 +8,7 @@ import type { IOAuthApps } from './IOAuthApps';
 import type { IPermission } from './IPermission';
 import type { ISubscription } from './ISubscription';
 
-export const schemas = typia.json.schemas<[ISubscription | IInvite | ICustomSound | IMessage | IOAuthApps | IIntegration | IPermission], '3.0'>();
+export const schemas = typia.json.schemas<
+	[ISubscription | IInvite | ICustomSound | IMessage | IOAuthApps | IIntegration | IPermission],
+	'3.0'
+>();
