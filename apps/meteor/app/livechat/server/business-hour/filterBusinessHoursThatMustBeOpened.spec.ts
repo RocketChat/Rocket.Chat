bd968048b1555770e78eb1f483a7dce0109de6f9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-08-07 22:55:54 -0300

    add on extra test reproducing an real error em prod

--- apps/meteor/app/livechat/server/business-hour/filterBusinessHoursThatMustBeOpened.spec.ts
+++ apps/meteor/app/livechat/server/business-hour/filterBusinessHoursThatMustBeOpened.spec.ts
@@ -261,3 +261,216 @@ describe('different timezones between server and business hours', () => {
 		expect(bh.length).toEqual(1);
 	});
 });
+
+describe('regular business hours', () => {
+	beforeEach(() => jest.useFakeTimers().setSystemTime(new Date('2025-08-07T22:02:11Z')));
+	afterEach(() => jest.useRealTimers());
+	it('should return a bh when the finish time resolves to a different day on server', async () => {
+		const bh = await filterBusinessHoursThatMustBeOpened([
+			{
+				_id: '68516f256ebb4bdceda2757e',
+				active: true,
+				type: LivechatBusinessHourTypes.DEFAULT,
+				ts: new Date(),
+				name: '',
+				workHours: [
+					{
+						day: 'Sunday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Sunday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Sunday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Sunday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Sunday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Monday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Monday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Monday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Monday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Monday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Tuesday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Tuesday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Tuesday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Tuesday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Tuesday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Wednesday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Wednesday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Wednesday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Wednesday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Wednesday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Thursday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Thursday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Thursday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Thursday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Thursday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Friday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Friday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Friday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Friday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Friday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+					{
+						day: 'Saturday',
+						start: {
+							time: '00:00',
+							utc: {
+								dayOfWeek: 'Saturday',
+								time: '03:00',
+							},
+							cron: {
+								dayOfWeek: 'Saturday',
+								time: '00:00',
+							},
+						},
+						finish: {
+							time: '00:01',
+							utc: {
+								dayOfWeek: 'Saturday',
+								time: '03:01',
+							},
+							cron: {
+								dayOfWeek: 'Saturday',
+								time: '00:01',
+							},
+						},
+						open: true,
+						code: '',
+					},
+				],
+				timezone: {
+					name: 'America/Sao_Paulo',
+					utc: '-3',
+				},
+			},
+		]);
+
+		expect(bh.length).toEqual(0);
+	});
+});
