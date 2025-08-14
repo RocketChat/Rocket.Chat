3a3afb1f3f53bf15f2074fadbf4cfab4d78a067d
MartinSchoeler <MartinSchoeler@users.noreply.github.com>
2025-08-14 14:54:48 +0000

    Deploy preview for PR 36375 🛫

--- pr-preview/pr-36375/uikit-playground/assets/KatexElement-CJdm4SB8.js
+++
@@ -1,1 +0,0 @@
-import{r,j as s}from"./index-DATWbQy7.js";import{k as o}from"./katex-cNP_AtkC.js";const m=({code:e})=>{const t=r.useMemo(()=>o.renderToString(e,{displayMode:!1,macros:{"\\href":"\\@secondoftwo"},maxSize:100}),[e]);return s.jsx("span",{dangerouslySetInnerHTML:{__html:t}})};export{m as default};
