de3638a8d4f95e47b162bfd01c69e38919cfb573
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-14 15:52:03 -0300

    test: added unit tests

---
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/utils/template.spec.ts
@@ -0,0 +1,48 @@
+import type { IOutboundProviderTemplate } from '@rocket.chat/apps-engine/definition/outboundComunication';
+import { capitalize } from '@rocket.chat/string-helpers';
+
+import { extractParameterMetadata, processTemplatePreviewText } from './template';
+import type { TemplateMediaParameter, TemplateComponent, TemplateParameterMetadata } from '../definitions/template';
+
+const createMockTextMetadata = (type: TemplateComponent['type'], index = 0): TemplateParameterMetadata => {
+	const placeholder = `{{${index + 1}}}`;
+	return {
+		id: `${type}.${placeholder}`,
+		type: 'text',
+		placeholder,
+		name: capitalize(type),
+		index,
+		componentType: type,
+		format: 'text',
+	};
+};
+
+const createMockMediaMetadata = (type: TemplateComponent['type'], format: TemplateMediaParameter['format']): TemplateParameterMetadata => ({
+	id: `${type}.mediaUrl`,
+	type: 'media',
+	placeholder: '',
+	name: 'Media_URL',
+	index: 0,
+	componentType: type,
+	format,
+});
+
+const variations: [IOutboundProviderTemplate['components'], TemplateParameterMetadata[]][] = [
+	[[{ type: 'header', text: 'Parameter {{1}}', format: 'image' }], [createMockMediaMetadata('header', 'image')]],
+	[[{ type: 'body', text: 'Parameter {{1}} and {{2}}' }], [createMockTextMetadata('body', 0), createMockTextMetadata('body', 1)]],
+	[[{ type: 'footer', text: 'Parameter {{1}}' }], [createMockTextMetadata('footer', 0)]],
+];
+
+describe('extractParameterMetadata', () => {
+	test.each(variations)('should return the parameters metadata for "%i" component type', (components, expected) => {
+		const parametersMetadata = extractParameterMetadata(components);
+
+		expect(parametersMetadata).toStrictEqual(expected);
+	});
+});
+
+describe('processTemplatePreviewText', () => {
+	const text = processTemplatePreviewText('Hello {{1}}', [{ type: 'text', value: 'World', format: 'text' }]);
+
+	expect(text).toBe('Hello World');
+});
