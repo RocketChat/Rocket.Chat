742bd941384c98e17dbaad1d11e3b77bc7e60f72
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-08-18 16:26:49 -0300

    chore: improves logic to handle contacts visitors as well

--- apps/meteor/app/livechat/server/lib/contacts/disableContact.ts
+++ apps/meteor/app/livechat/server/lib/contacts/disableContact.ts
@@ -1,8 +1,15 @@
-import { LivechatContacts } from '@rocket.chat/models';
+import type { ILivechatContact } from '@rocket.chat/core-typings';
+import { LivechatContacts, LivechatVisitors } from '@rocket.chat/models';
 
 export async function disableContactById(contactId: string): Promise<void> {
-	const updatedContact = await LivechatContacts.disableByContactId(contactId);
-	if (updatedContact.matchedCount === 0) {
+	const contact = await LivechatContacts.findOneById<Pick<ILivechatContact, '_id' | 'channels'>>(contactId);
+	if (!contact) {
 		throw new Error('error-contact-not-found');
 	}
+	// Cleaning up any visitor object for the contact
+	for await (const channel of contact.channels) {
+		await LivechatVisitors.disableById(channel.visitor.visitorId);
+	}
+
+	await LivechatContacts.disableByContactId(contactId);
 }
--- apps/meteor/tests/unit/app/livechat/server/lib/disableContact.spec.ts
+++ apps/meteor/tests/unit/app/livechat/server/lib/disableContact.spec.ts
@@ -8,30 +8,51 @@ const modelsMock = {
 		findOneById: sinon.stub(),
 		disableByContactId: sinon.stub(),
 	},
+	LivechatVisitors: {
+		disableById: sinon.stub(),
+	},
 };
 
 const { disableContactById } = proxyquire.noCallThru().load('../../../../../../app/livechat/server/lib/contacts/disableContact.ts', {
 	'@rocket.chat/models': modelsMock,
 });
 
+const contact = {
+	_id: 'contactId',
+	name: 'contactName',
+	channels: [
+		{
+			visitor: {
+				visitorId: 'visitorId',
+			},
+		},
+	],
+} as ILivechatContact;
+
 describe('disableContact', () => {
 	beforeEach(() => {
+		modelsMock.LivechatContacts.findOneById.reset();
+		modelsMock.LivechatVisitors.disableById.reset();
 		modelsMock.LivechatContacts.disableByContactId.reset();
 	});
 
 	it('should disable the contact', async () => {
-		modelsMock.LivechatContacts.disableByContactId.resolves({ _id: 'contactId' } as ILivechatContact);
+		modelsMock.LivechatContacts.findOneById.resolves(contact);
+		modelsMock.LivechatContacts.disableByContactId.resolves();
 
 		await disableContactById('contactId');
 
+		expect(modelsMock.LivechatVisitors.disableById.called).to.be.true;
 		expect(modelsMock.LivechatContacts.disableByContactId.getCall(0).args[0]).to.be.equal('contactId');
 	});
 
 	it('should throw error if contact is not found', async () => {
-		modelsMock.LivechatContacts.disableByContactId.resolves({ matchedCount: 0 });
+		modelsMock.LivechatContacts.findOneById.resolves(null);
 
 		await expect(disableContactById('contactId')).to.be.rejectedWith('error-contact-not-found');
 
-		expect(modelsMock.LivechatContacts.disableByContactId.getCall(0).args[0]).to.be.equal('contactId');
+		expect(modelsMock.LivechatContacts.findOneById.getCall(0).args[0]).to.be.equal('contactId');
+		expect(modelsMock.LivechatVisitors.disableById.called).to.be.false;
+		expect(modelsMock.LivechatContacts.disableByContactId.called).to.be.false;
 	});
 });
