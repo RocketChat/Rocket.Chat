9027468fb7e9c7099583be20737c6e96d20a7806
Kevin  Aleman <kaleman960@gmail.com>
2025-08-19 08:08:02 -0600

    fix

--- apps/meteor/ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/hooks/applyDepartmentRestrictions.ts
@@ -1,7 +1,6 @@
 import type { ILivechatDepartment } from '@rocket.chat/core-typings';
 import type { FilterOperators } from 'mongodb';
 
-import { hasRoleAsync } from '../../../../../app/authorization/server/functions/hasRole';
 import { callbacks } from '../../../../../lib/callbacks';
 import { cbLogger } from '../lib/logger';
 import { getUnitsFromUser } from '../methods/getUnitsFromUserRoles';
@@ -12,6 +11,7 @@ export const addQueryRestrictionsToDepartmentsModel = async (originalQuery: Filt
 	const units = await getUnitsFromUser(userId);
 	if (Array.isArray(units)) {
 		query.ancestors = { $in: units };
+		query._id = { $in: units };
 	}
 
 	cbLogger.debug({ msg: 'Applying department query restrictions', userId, units });
@@ -21,7 +21,7 @@ export const addQueryRestrictionsToDepartmentsModel = async (originalQuery: Filt
 callbacks.add(
 	'livechat.applyDepartmentRestrictions',
 	async (originalQuery: FilterOperators<ILivechatDepartment> = {}, { userId }: { userId?: string | null } = { userId: null }) => {
-		if (!userId || !(await hasRoleAsync(userId, 'livechat-monitor'))) {
+		if (!userId) {
 			return originalQuery;
 		}
 
