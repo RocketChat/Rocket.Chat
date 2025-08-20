1f17c40eedebd9c27c880142cc6b894dc6d7f757
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-20 14:13:02 -0300

    feat: added template language description to TemplateSelect

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/TemplateSelect.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/TemplateSelect.tsx
@@ -1,20 +1,52 @@
 import type { IOutboundProviderTemplate } from '@rocket.chat/core-typings';
-import { Select } from '@rocket.chat/fuselage';
+import type { SelectOption } from '@rocket.chat/fuselage';
+import { Option, OptionDescription, SelectFiltered } from '@rocket.chat/fuselage';
+import { useLanguages } from '@rocket.chat/ui-contexts';
 import { useMemo } from 'react';
 import type { Key, ComponentProps } from 'react';
 
-type TemplateSelectProps = Omit<ComponentProps<typeof Select>, 'value' | 'onChange' | 'options'> & {
+type TemplateSelectProps = Omit<ComponentProps<typeof SelectFiltered>, 'value' | 'onChange' | 'options'> & {
 	templates: IOutboundProviderTemplate[];
 	value: string;
 	onChange(value: Key): void;
 };
 
 const TemplateSelect = ({ templates, value, onChange, ...props }: TemplateSelectProps) => {
-	const options = useMemo<[string, string][]>(() => {
-		return templates.map((template) => [template.id, template.name]);
+	const languages = useLanguages();
+	console.log(languages);
+
+	const [options, templateMap] = useMemo(() => {
+		const templateMap = new Map<string, IOutboundProviderTemplate>();
+		const templateOptions: SelectOption[] = [];
+
+		for (const template of templates) {
+			templateMap.set(template.id, template);
+			templateOptions.push([template.id, template.name]);
+		}
+
+		return [templateOptions, templateMap];
 	}, [templates]);
 
-	return <Select {...props} value={value} options={options} onChange={onChange} />;
+	return (
+		<SelectFiltered
+			{...props}
+			value={value}
+			options={options}
+			onChange={onChange}
+			renderItem={({ label, value: templateId, ...props }) => {
+				const { language: templateLanguage = '' } = templateMap.get(templateId) || {};
+				const normalizedTemplateLanguage = templateLanguage.replace(/_/g, '-').replace(/en-US/g, 'en');
+				const language = languages.find((lang) => lang.key === normalizedTemplateLanguage);
+				console.log(normalizedTemplateLanguage, language);
+
+				return (
+					<Option {...props} label={label}>
+						{language ? <OptionDescription>{language.name}</OptionDescription> : null}
+					</Option>
+				);
+			}}
+		/>
+	);
 };
 
 export default TemplateSelect;
