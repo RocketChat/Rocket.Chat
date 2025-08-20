baf4a5023014a3b7f5adfc0cfe9005ff624e8202
Harmeet221 <harmeetkaur568@gmail.com>
2025-08-20 21:12:05 +0530

    enhance reusability

--- apps/meteor/tests/e2e/page-objects/omnichannel-room-info.ts
+++ apps/meteor/tests/e2e/page-objects/omnichannel-room-info.ts
@@ -1,10 +1,15 @@
 import type { Locator, Page } from '@playwright/test';
 
+import { HomeSidenav } from './fragments/home-sidenav';
+
 export class OmnichannelRoomInfo {
 	private readonly page: Page;
 
+	private readonly homeSidenav: HomeSidenav;
+
 	constructor(page: Page) {
 		this.page = page;
+		this.homeSidenav = new HomeSidenav(page);
 	}
 
 	get dialogRoomInfo(): Locator {
@@ -39,11 +44,11 @@ export class OmnichannelRoomInfo {
 		return this.page.getByRole('link', { name: `${name}` });
 	}
 
-	getSidebarItemByName(name: string): Locator {
-		return this.page.getByRole('link').filter({ has: this.page.getByText(name, { exact: true }) });
-	}
+	// getSidebarItemByName(name: string): Locator {
+	// 	return this.page.getByRole('link').filter({ has: this.page.getByText(name, { exact: true }) });
+	// }
 
 	getBadgeIndicator(name: string, title: string): Locator {
-		return this.getSidebarItemByName(name).getByTitle(`${title}`);
+		return this.homeSidenav.getSidebarItemByName(name).getByTitle(title);
 	}
 }
