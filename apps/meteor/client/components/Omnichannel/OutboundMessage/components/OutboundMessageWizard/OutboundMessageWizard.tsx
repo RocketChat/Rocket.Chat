b58a4f40d59dcf12beabf0b6d92a830555c106c3
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-20 15:36:14 -0300

    fix: Outbound message upsell modal not being displayed when there's no license module (#36744)

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/OutboundMessageWizard.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/OutboundMessageWizard.tsx
@@ -35,6 +35,7 @@ const OutboundMessageWizard = ({ defaultValues = {} }: OutboundMessageWizardProp
 		refetch: refetchProviders,
 	} = useOutboundProvidersList<boolean>({
 		select: ({ providers = [] }) => providers.length > 0,
+		enabled: hasModule === true,
 	});
 
 	const wizardApi = useWizard({
