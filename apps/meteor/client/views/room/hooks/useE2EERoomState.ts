0078ef81e2e92a110c9dda180f2f984018c11fe2
Matheus Cardoso <matheus@cardo.so>
2025-08-25 13:47:56 -0300

    remove dependency on e2eeroomstate

--- apps/meteor/client/views/room/hooks/useE2EERoomState.ts
+++ apps/meteor/client/views/room/hooks/useE2EERoomState.ts
@@ -1,7 +1,6 @@
 import { useMemo, useSyncExternalStore } from 'react';
 
 import { useE2EERoom } from './useE2EERoom';
-import type { E2ERoomState } from '../../../lib/e2ee/E2ERoomState';
 
 export const useE2EERoomState = (rid: string) => {
 	const e2eRoom = useE2EERoom(rid);
@@ -10,7 +9,7 @@ export const useE2EERoomState = (rid: string) => {
 		() =>
 			[
 				(callback: () => void): (() => void) => (e2eRoom ? e2eRoom.onStateChange(callback) : () => undefined),
-				(): E2ERoomState | undefined => (e2eRoom ? e2eRoom.getState() : undefined),
+				() => (e2eRoom ? e2eRoom.getState() : undefined),
 			] as const,
 		[e2eRoom],
 	);
