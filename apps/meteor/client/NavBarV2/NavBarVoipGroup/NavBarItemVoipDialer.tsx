16ac4544e49cc419c571d87cfaac33f172fcaaa5
gabriellsh <henriques.gabriell@gmail.com>
2025-08-25 12:53:12 -0300

    Update headerv2

--- apps/meteor/client/NavBarV2/NavBarVoipGroup/NavBarItemVoipDialer.tsx
+++ apps/meteor/client/NavBarV2/NavBarVoipGroup/NavBarItemVoipDialer.tsx
@@ -1,16 +1,16 @@
 import { NavBarItem } from '@rocket.chat/fuselage';
 import type { HTMLAttributes } from 'react';
 
-import { useVoipDialerAction } from './hooks/useVoipDialerAction';
+import { useMediaCallAction } from './hooks/useMediaCallAction';
 
 type NavBarItemVoipDialerProps = Omit<HTMLAttributes<HTMLElement>, 'is'> & {
 	primary?: boolean;
 };
 
 const NavBarItemVoipDialer = (props: NavBarItemVoipDialerProps) => {
-	const { title, handleToggleDialer, isPressed, isDisabled } = useVoipDialerAction();
+	const { action, ...mediaCallAction } = useMediaCallAction();
 
-	return <NavBarItem {...props} title={title} icon='dialpad' onClick={handleToggleDialer} pressed={isPressed} disabled={isDisabled} />;
+	return <NavBarItem {...props} {...mediaCallAction} onClick={action} />;
 };
 
 export default NavBarItemVoipDialer;
