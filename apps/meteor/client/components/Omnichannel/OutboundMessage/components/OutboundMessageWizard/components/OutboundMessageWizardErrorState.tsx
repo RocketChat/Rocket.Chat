1032c8c5dfa40bad899ad0ea23c1c595d29e7836
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-25 19:26:59 -0300

    feat: added description to OutboundMessageWizardErrorState

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/components/OutboundMessageWizardErrorState.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/OutboundMessageWizard/components/OutboundMessageWizardErrorState.tsx
@@ -1,17 +1,19 @@
-import { States, StatesIcon, StatesTitle, StatesActions, StatesAction } from '@rocket.chat/fuselage';
+import { States, StatesIcon, StatesTitle, StatesActions, StatesAction, StatesDescription } from '@rocket.chat/fuselage';
 import { useTranslation } from 'react-i18next';
 
 type Props = {
 	title?: string;
+	description?: string;
 	onRetry?(): void;
 };
 
-const OutboundMessageWizardErrorState = ({ title, onRetry }: Props) => {
+const OutboundMessageWizardErrorState = ({ title, description, onRetry }: Props) => {
 	const { t } = useTranslation();
 	return (
 		<States width='100%' height='100%'>
 			<StatesIcon name='circle-exclamation' />
 			<StatesTitle>{title ?? t('Something_went_wrong')}</StatesTitle>
+			{description ? <StatesDescription>{description}</StatesDescription> : null}
 			{onRetry ? (
 				<StatesActions>
 					<StatesAction onClick={onRetry}>{t('Retry')}</StatesAction>
