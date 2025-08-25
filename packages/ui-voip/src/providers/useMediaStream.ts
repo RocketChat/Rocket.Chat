5f0af482eaf18c86f5c7c92f5e74bcdbade2c612
gabriellsh <henriques.gabriell@gmail.com>
2025-08-25 11:45:30 -0300

    Fix stream not stopping

--- packages/ui-voip/src/providers/useMediaStream.ts
+++ packages/ui-voip/src/providers/useMediaStream.ts
@@ -3,25 +3,19 @@ import { useSafeRefCallback } from '@rocket.chat/ui-client';
 import { useCallback } from 'react';
 
 const useMediaStream = (instance?: MediaSignalingSession) => {
-	// TODO: Check if this actually works (infinite loop? stream does not update?)
-	// It could happen that the stream changes, but the instance does not update, causing the stream to not update
-	// Meaning the callback wont run.
-	// It's probably best to make this an externalStore and use the events.
 	const remoteStream = instance?.getMainCall()?.getRemoteMediaStream();
 
 	return useSafeRefCallback(
 		useCallback(
-			async (node: HTMLAudioElement | null) => {
-				if (!node || !remoteStream) {
+			(node: HTMLAudioElement) => {
+				if (!remoteStream) {
 					return;
 				}
 
-				try {
-					node.srcObject = remoteStream;
-					await node.play();
-				} catch (error) {
-					console.error('MediaCall: useMediaStream - Error getting/playing media stream', error);
-				}
+				node.srcObject = remoteStream;
+				node.play().catch((error) => {
+					console.error('MediaCall: useMediaStream - Error playing media stream', error);
+				});
 
 				return () => {
 					node.pause();
