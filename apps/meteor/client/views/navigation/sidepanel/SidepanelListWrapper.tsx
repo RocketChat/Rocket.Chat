3701230f1d21976a8a7e76facde075c1bbe866ad
dougfabris <devfabris@gmail.com>
2025-08-26 13:36:27 -0300

    fix: sidepanel list wrong gap

--- apps/meteor/client/views/navigation/sidepanel/SidepanelListWrapper.tsx
+++ apps/meteor/client/views/navigation/sidepanel/SidepanelListWrapper.tsx
@@ -1,19 +1,21 @@
 import { SidepanelList } from '@rocket.chat/fuselage';
-import { useMergedRefs } from '@rocket.chat/fuselage-hooks';
 import type { ForwardedRef, HTMLAttributes } from 'react';
 import { forwardRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { useSidebarListNavigation } from '../../../sidebar/RoomList/useSidebarListNavigation';
+import { useSidebarListNavigation } from '../sidebar/RoomList/useSidebarListNavigation';
 
 type SidepanelListWrapperProps = HTMLAttributes<HTMLDivElement>;
 
 const SidepanelListWrapper = forwardRef(function SidepanelListWrapper(props: SidepanelListWrapperProps, ref: ForwardedRef<HTMLDivElement>) {
 	const { t } = useTranslation();
 	const { sidebarListRef } = useSidebarListNavigation();
-	const mergedRefs = useMergedRefs(ref, sidebarListRef);
 
-	return <SidepanelList aria-label={t('Channels')} ref={mergedRefs} {...props} />;
+	return (
+		<SidepanelList aria-label={t('Channels')} ref={sidebarListRef}>
+			<div ref={ref} {...props} />
+		</SidepanelList>
+	);
 });
 
 export default SidepanelListWrapper;
