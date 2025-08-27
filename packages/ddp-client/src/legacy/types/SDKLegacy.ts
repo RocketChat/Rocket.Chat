c5d935b8fe3200f42a8e7d94fa114be7b5684958
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2025-08-27 09:03:17 -0300

    chore!: Remove MeetPage and webRTC (#36671)

--- packages/ddp-client/src/legacy/types/SDKLegacy.ts
+++ packages/ddp-client/src/legacy/types/SDKLegacy.ts
@@ -86,9 +86,7 @@ export type RocketchatSdkLegacyEvents = {
 	'Users:NameChanged': StreamerCallbackArgs<'notify-logged', 'Users:NameChanged'>;
 	'Users:Deleted': StreamerCallbackArgs<'notify-logged', 'Users:Deleted'>;
 	'updateAvatar': StreamerCallbackArgs<'notify-logged', 'updateAvatar'>;
-
 	'user-message': StreamerCallbackArgs<'notify-user', `${string}/message`>;
-	'webrtc': StreamerCallbackArgs<'notify-user', `${string}/webrtc`>;
 	'notification': StreamerCallbackArgs<'notify-user', `${string}/notification`>;
 	'rooms-changed': StreamerCallbackArgs<'notify-user', `${string}/rooms-changed`>;
 	'subscriptions-changed': StreamerCallbackArgs<'notify-user', `${string}/subscriptions-changed`>;
