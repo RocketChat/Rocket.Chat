ea8d7877844c5a1e631db6a5e6bc6b4be4aad3b6
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-08-28 14:22:14 -0300

    test: implemented e2e tests

--- apps/meteor/tests/e2e/page-objects/omnichannel-room-info.ts
+++ apps/meteor/tests/e2e/page-objects/omnichannel-room-info.ts
@@ -40,6 +40,19 @@ export class OmnichannelRoomInfo {
 		return this.page.locator(`div >> text="${label}"`);
 	}
 
+	getInfoByLabel(label: string): Locator {
+		return this.dialogRoomInfo.getByLabel(label);
+	}
+
+	get inputSLAPolicy(): Locator {
+		return this.dialogEditRoom.getByRole('button', { name: 'SLA Policy' });
+	}
+
+	async selectSLA(name: string): Promise<void> {
+		await this.inputSLAPolicy.click();
+		return this.page.getByRole('option', { name, exact: true }).click();
+	}
+
 	getBadgeIndicator(name: string, title: string): Locator {
 		return this.homeSidenav.getSidebarItemByName(name).getByTitle(title);
 	}
