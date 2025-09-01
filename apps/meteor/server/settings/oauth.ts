17addb5b3ca3833b81d6959728387683b6695499
Diego Sampaio <chinello@gmail.com>
2025-09-01 15:32:46 -0300

    chore!: Remove Tokenpass integration (#36620)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- apps/meteor/server/settings/oauth.ts
+++ apps/meteor/server/settings/oauth.ts
@@ -126,29 +126,6 @@ export const createOauthSettings = () =>
 			});
 		});
 
-		await this.section('Tokenpass', async function () {
-			const enableQuery = {
-				_id: 'Accounts_OAuth_Tokenpass',
-				value: true,
-			};
-
-			await this.add('Accounts_OAuth_Tokenpass', false, { type: 'boolean' });
-			await this.add('API_Tokenpass_URL', '', {
-				type: 'string',
-				public: true,
-				enableQuery,
-				i18nDescription: 'API_Tokenpass_URL_Description',
-			});
-			await this.add('Accounts_OAuth_Tokenpass_id', '', { type: 'string', enableQuery });
-			await this.add('Accounts_OAuth_Tokenpass_secret', '', { type: 'string', enableQuery });
-			await this.add('Accounts_OAuth_Tokenpass_callback_url', '_oauth/tokenpass', {
-				type: 'relativeUrl',
-				readonly: true,
-				// @ts-expect-error - force: true is not a valid option for this method
-				force: true,
-				enableQuery,
-			});
-		});
 		await this.section('WordPress', async function () {
 			const enableQuery = {
 				_id: 'Accounts_OAuth_Wordpress',
