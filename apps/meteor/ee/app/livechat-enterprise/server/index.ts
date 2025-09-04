712e2f328b48818e70518e7d4660dac131360d73
Martin Schoeler <martin.schoeler@rocket.chat>
2025-09-04 18:06:14 -0300

    chore!: Remove deprecated livechat methods & api endpoints (#36836)

--- apps/meteor/ee/app/livechat-enterprise/server/index.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/index.ts
@@ -3,7 +3,6 @@ import { patchOmniCore } from '@rocket.chat/omni-core-ee';
 import { Meteor } from 'meteor/meteor';
 
 import './methods/addMonitor';
-import './methods/getUnitsFromUserRoles';
 import './methods/removeMonitor';
 import './methods/removeTag';
 import './methods/saveTag';
