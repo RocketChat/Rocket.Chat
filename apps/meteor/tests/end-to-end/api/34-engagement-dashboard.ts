d429e492a9e3fb67aaa8e76c38b7f50301ed58ab
MartinSchoeler <martinschoeler8@gmail.com>
2025-09-09 14:31:15 -0300

    test: update and remove tests with old behavior

--- apps/meteor/tests/end-to-end/api/34-engagement-dashboard.ts
+++ apps/meteor/tests/end-to-end/api/34-engagement-dashboard.ts
@@ -164,7 +164,8 @@ describe('[Engagement Dashboard]', function () {
 				});
 		});
 
-		it('should correctly count messages in an empty room', async () => {
+		// Counting messages in rooms with no activity was deprecated and no longer a desired behavior
+		it('should not count messages in an empty room', async () => {
 			await request
 				.get(api('engagement-dashboard/channels/list'))
 				.set(credentials)
@@ -177,93 +178,10 @@ describe('[Engagement Dashboard]', function () {
 				.expect((res: Response) => {
 					expect(res.body).to.have.property('success', true);
 					expect(res.body).to.have.property('offset', 0);
-					expect(res.body).to.have.property('count');
-					expect(res.body).to.have.property('total');
+					expect(res.body).to.have.property('count', 0);
+					expect(res.body).to.have.property('total', 0);
 					expect(res.body).to.have.property('channels');
-					expect(res.body.channels).to.be.an('array').that.is.not.empty;
-
-					const channelRecord = res.body.channels.find(({ room }: { room: { _id: string } }) => room._id === testRoom._id);
-					expect(channelRecord).not.to.be.undefined;
-
-					expect(channelRecord).to.be.an('object').that.is.not.empty;
-					expect(channelRecord).to.have.property('messages', 0);
-					expect(channelRecord).to.have.property('lastWeekMessages', 0);
-					expect(channelRecord).to.have.property('diffFromLastWeek', 0);
-					expect(channelRecord.room).to.be.an('object').that.is.not.empty;
-
-					expect(channelRecord.room).to.have.property('_id', testRoom._id);
-					expect(channelRecord.room).to.have.property('name', testRoom.name);
-					expect(channelRecord.room).to.have.property('ts', testRoom.ts);
-					expect(channelRecord.room).to.have.property('t', testRoom.t);
-					expect(channelRecord.room).to.have.property('_updatedAt', testRoom._updatedAt);
-				});
-		});
-
-		it('should correctly count messages diff compared to last week when there are messages in a room', async () => {
-			await request
-				.get(api('engagement-dashboard/channels/list'))
-				.set(credentials)
-				.query({
-					end: new Date().toISOString(),
-					start: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
-				})
-				.expect('Content-Type', 'application/json')
-				.expect(200)
-				.expect((res: Response) => {
-					expect(res.body).to.have.property('success', true);
-					expect(res.body).to.have.property('offset', 0);
-					expect(res.body).to.have.property('count');
-					expect(res.body).to.have.property('total');
-					expect(res.body).to.have.property('channels');
-					expect(res.body.channels).to.be.an('array').that.is.not.empty;
-
-					const channelRecord = res.body.channels.find(({ room }: { room: { _id: string } }) => room._id === testRoom._id);
-					expect(channelRecord).not.to.be.undefined;
-
-					expect(channelRecord).to.be.an('object').that.is.not.empty;
-					expect(channelRecord).to.have.property('messages', 1);
-					expect(channelRecord).to.have.property('lastWeekMessages', 0);
-					expect(channelRecord).to.have.property('diffFromLastWeek', 1);
-					expect(channelRecord.room).to.be.an('object').that.is.not.empty;
-
-					expect(channelRecord.room).to.have.property('_id', testRoom._id);
-					expect(channelRecord.room).to.have.property('name', testRoom.name);
-					expect(channelRecord.room).to.have.property('ts', testRoom.ts);
-					expect(channelRecord.room).to.have.property('t', testRoom.t);
-				});
-		});
-
-		it('should correctly count messages from last week and diff when moving to the next week', async () => {
-			await request
-				.get(api('engagement-dashboard/channels/list'))
-				.set(credentials)
-				.query({
-					end: new Date(Date.now() + 8 * 24 * 60 * 60 * 1000).toISOString(),
-					start: new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString(),
-				})
-				.expect('Content-Type', 'application/json')
-				.expect(200)
-				.expect((res: Response) => {
-					expect(res.body).to.have.property('success', true);
-					expect(res.body).to.have.property('offset', 0);
-					expect(res.body).to.have.property('count');
-					expect(res.body).to.have.property('total');
-					expect(res.body).to.have.property('channels');
-					expect(res.body.channels).to.be.an('array').that.is.not.empty;
-
-					const channelRecord = res.body.channels.find(({ room }: { room: { _id: string } }) => room._id === testRoom._id);
-					expect(channelRecord).not.to.be.undefined;
-
-					expect(channelRecord).to.be.an('object').that.is.not.empty;
-					expect(channelRecord).to.have.property('messages', 0);
-					expect(channelRecord).to.have.property('lastWeekMessages', 1);
-					expect(channelRecord).to.have.property('diffFromLastWeek', -1);
-					expect(channelRecord.room).to.be.an('object').that.is.not.empty;
-
-					expect(channelRecord.room).to.have.property('_id', testRoom._id);
-					expect(channelRecord.room).to.have.property('name', testRoom.name);
-					expect(channelRecord.room).to.have.property('ts', testRoom.ts);
-					expect(channelRecord.room).to.have.property('t', testRoom.t);
+					expect(res.body.channels).to.be.an('array').that.is.empty;
 				});
 		});
 	});
