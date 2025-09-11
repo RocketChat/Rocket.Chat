3ce7075396a99886895ab05022747ad8ca483c1e
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-09-11 16:21:34 -0300

    fix(outbound): Placeholder input not being focused after menu is closed (#36910)

--- apps/meteor/client/components/Omnichannel/OutboundMessage/components/TemplatePlaceholderSelector/TemplatePlaceholderInput.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/components/TemplatePlaceholderSelector/TemplatePlaceholderInput.tsx
@@ -1,6 +1,7 @@
 import type { ILivechatContact, Serialized } from '@rocket.chat/core-typings';
 import { Box, Icon, TextInput } from '@rocket.chat/fuselage';
-import type { ComponentProps, FormEvent, FormEventHandler } from 'react';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useRef, type ComponentProps, type FormEvent, type FormEventHandler } from 'react';
 
 import PlaceholderSelector from './TemplatePlaceholderSelector';
 import type { TemplateParameter } from '../../definitions/template';
@@ -13,17 +14,23 @@ type TemplatePlaceholderInputProps = Omit<ComponentProps<typeof TextInput>, 'val
 };
 
 const TemplatePlaceholderInput = ({ contact, value = '', type, onChange, ...props }: TemplatePlaceholderInputProps) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	const handleChange = (event: FormEvent<HTMLInputElement> | string) => {
 		onChange(typeof event === 'string' ? event : event.currentTarget.value);
 	};
 
 	const addon = type === 'media' ? <Icon name='link' /> : undefined;
 
+	const handleOpenToggle = useEffectEvent((isOpen: boolean) => {
+		if (!isOpen) inputRef.current?.focus();
+	});
+
 	return (
 		<Box display='flex' width='100%'>
-			<TextInput {...props} value={value} addon={addon} onChange={handleChange as FormEventHandler<HTMLElement>} />
+			<TextInput {...props} ref={inputRef} value={value} addon={addon} onChange={handleChange as FormEventHandler<HTMLElement>} />
 
-			<PlaceholderSelector disabled={type !== 'text'} mis={12} contact={contact} onSelect={handleChange} />
+			<PlaceholderSelector disabled={type !== 'text'} mis={12} contact={contact} onSelect={handleChange} onOpenChange={handleOpenToggle} />
 		</Box>
 	);
 };
