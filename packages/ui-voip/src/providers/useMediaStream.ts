e22f9ec9b4b807fc754477567d7748e843092784
gabriellsh <henriques.gabriell@gmail.com>
2025-09-15 18:46:32 -0300

    Fix cbref type

--- packages/ui-voip/src/providers/useMediaStream.ts
+++ packages/ui-voip/src/providers/useMediaStream.ts
@@ -1,15 +1,17 @@
-import { MediaSignalingSession } from '@rocket.chat/media-signaling';
+import type { MediaSignalingSession } from '@rocket.chat/media-signaling';
 import { useSafeRefCallback } from '@rocket.chat/ui-client';
 import { useCallback, useRef } from 'react';
 
-const useMediaStream = (instance?: MediaSignalingSession): [(node: HTMLAudioElement) => void, { current: HTMLAudioElement | null }] => {
+const useMediaStream = (
+	instance?: MediaSignalingSession,
+): [(node: HTMLAudioElement | null) => void, { current: HTMLAudioElement | null }] => {
 	const remoteStream = instance?.getMainCall()?.getRemoteMediaStream();
 	const actualRef = useRef<HTMLAudioElement | null>(null);
 
 	return [
 		useSafeRefCallback(
 			useCallback(
-				(node: HTMLAudioElement) => {
+				(node) => {
 					// TODO remove node check when useSafeRefCallback is updated from fuselage.
 					if (!remoteStream || !node) {
 						return;
