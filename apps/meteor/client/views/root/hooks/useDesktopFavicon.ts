4cd768a755c6af82332b5ff5bb99fbb01f1d9d55
Tasso <tasso.evangelista@rocket.chat>
2025-09-15 13:37:32 -0300

    Accept CoderRabbit suggestion

--- apps/meteor/client/views/root/hooks/useDesktopFavicon.ts
+++ apps/meteor/client/views/root/hooks/useDesktopFavicon.ts
@@ -1,19 +1,19 @@
 import { useAbsoluteUrl, useAssetPath } from '@rocket.chat/ui-contexts';
 import { useEffect } from 'react';
 
-const { RocketChatDesktop } = window;
-
 export const useDesktopFavicon = () => {
 	const absoluteUrl = useAbsoluteUrl();
 
 	useEffect(() => {
-		RocketChatDesktop?.setUrlResolver((relativePath?: string) => absoluteUrl(relativePath ?? '/'));
+		if (typeof window === 'undefined') return;
+		window.RocketChatDesktop?.setUrlResolver((relativePath?: string) => absoluteUrl(relativePath ?? '/'));
 	}, [absoluteUrl]);
 
 	const faviconUrl = useAssetPath('favicon');
 
 	useEffect(() => {
+		if (typeof window === 'undefined') return;
 		if (!faviconUrl) return;
-		RocketChatDesktop?.setFavicon(faviconUrl);
+		window.RocketChatDesktop?.setFavicon(faviconUrl);
 	}, [faviconUrl]);
 };
--- apps/meteor/client/views/root/hooks/useDesktopTitle.ts
+++ apps/meteor/client/views/root/hooks/useDesktopTitle.ts
@@ -1,13 +1,12 @@
 import { useSetting } from '@rocket.chat/ui-contexts';
 import { useEffect } from 'react';
 
-const { RocketChatDesktop } = window;
-
 export const useDesktopTitle = () => {
 	const title = useSetting('Site_Name', 'Rocket.Chat');
 
 	useEffect(() => {
+		if (typeof window === 'undefined') return;
 		if (!title) return;
-		RocketChatDesktop?.setTitle(title);
+		window.RocketChatDesktop?.setTitle(title);
 	}, [title]);
 };
