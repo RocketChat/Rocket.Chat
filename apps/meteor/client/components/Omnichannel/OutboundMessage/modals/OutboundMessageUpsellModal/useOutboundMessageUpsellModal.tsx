c94a178968e1dcc9a2b5f79457f330ca94972c18
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-09-15 10:16:36 -0300

    fix(outbound): Upsell modal conditions (#36930)

--- apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/useOutboundMessageUpsellModal.tsx
+++ apps/meteor/client/components/Omnichannel/OutboundMessage/modals/OutboundMessageUpsellModal/useOutboundMessageUpsellModal.tsx
@@ -4,14 +4,18 @@ import { useMemo } from 'react';
 
 import OutboundMessageUpsellModal from './OutboundMessageUpsellModal';
 import { useHasLicenseModule } from '../../../../../hooks/useHasLicenseModule';
+import { useLicense } from '../../../../../hooks/useLicense';
 
 export const useOutboundMessageUpsellModal = () => {
 	const setModal = useSetModal();
 	const isAdmin = useRole('admin');
+	const license = useLicense();
 	const hasModule = useHasLicenseModule('outbound-messaging') === true;
 
 	const close = useEffectEvent(() => setModal(null));
-	const open = useEffectEvent(() => setModal(<OutboundMessageUpsellModal isAdmin={isAdmin} hasModule={hasModule} onClose={close} />));
+	const open = useEffectEvent(() =>
+		setModal(<OutboundMessageUpsellModal isCommunity={!license.data?.license} isAdmin={isAdmin} hasModule={hasModule} onClose={close} />),
+	);
 
 	return useMemo(() => ({ open, close }), [open, close]);
 };
