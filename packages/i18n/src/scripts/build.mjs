adfa563225139450cc03e1896eafbf060e3333a0
Tasso <tasso.evangelista@rocket.chat>
2025-09-16 18:13:44 -0300

    Change manifest exports

--- packages/i18n/src/scripts/build.mjs
+++ packages/i18n/src/scripts/build.mjs
@@ -105,34 +105,34 @@ exports.default = ${languagesSerialized};`,
 export default languages;`,
 	);
 
-	// ./index
-	await writeFile(join(distDirectory, 'index.mjs'), `export * from './esm/index.js';`);
+	// 	// ./index
+	// 	await writeFile(join(distDirectory, 'index.mjs'), `export * from './esm/index.js';`);
 
-	await writeFile(
-		join(distDirectory, 'index.js'),
-		`"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-		if (k2 === undefined) k2 = k;
-		var desc = Object.getOwnPropertyDescriptor(m, k);
-		if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-			desc = { enumerable: true, get: function() { return m[k]; } };
-		}
-		Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-		if (k2 === undefined) k2 = k;
-		o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-		for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-		return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./cjs/index"), exports);`,
-	);
+	// 	await writeFile(
+	// 		join(distDirectory, 'index.js'),
+	// 		`"use strict";
+	// var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+	// 		if (k2 === undefined) k2 = k;
+	// 		var desc = Object.getOwnPropertyDescriptor(m, k);
+	// 		if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+	// 			desc = { enumerable: true, get: function() { return m[k]; } };
+	// 		}
+	// 		Object.defineProperty(o, k2, desc);
+	// }) : (function(o, m, k, k2) {
+	// 		if (k2 === undefined) k2 = k;
+	// 		o[k2] = m[k];
+	// }));
+	// var __exportStar = (this && this.__exportStar) || function(m, exports) {
+	// 		for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+	// };
+	// var __importDefault = (this && this.__importDefault) || function (mod) {
+	// 		return (mod && mod.__esModule) ? mod : { "default": mod };
+	// };
+	// Object.defineProperty(exports, "__esModule", { value: true });
+	// __exportStar(require("./cjs/index"), exports);`,
+	// 	);
 
-	await writeFile(join(distDirectory, 'index.d.ts'), `export * from './esm/index';`);
+	// 	await writeFile(join(distDirectory, 'index.d.ts'), `export * from './esm/index';`);
 }
 
 if (import.meta.url.startsWith('file:')) {
