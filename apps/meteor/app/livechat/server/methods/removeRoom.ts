fa8c42ab3760543169ad4f88fe1a9c76c8a46e18
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-09-17 15:03:14 -0300

    fix: correct method on depracation warning

--- apps/meteor/app/livechat/server/methods/removeRoom.ts
+++ apps/meteor/app/livechat/server/methods/removeRoom.ts
@@ -16,7 +16,7 @@ declare module '@rocket.chat/ddp-client' {
 
 Meteor.methods<ServerMethods>({
 	async 'livechat:removeRoom'(rid) {
-		methodDeprecationLogger.method('livechat:removeCustomField', '8.0.0', '/v1/livechat/rooms.delete');
+		methodDeprecationLogger.method('livechat:removeRoom', '8.0.0', '/v1/livechat/rooms.delete');
 		const user = Meteor.userId();
 		if (!user || !(await hasPermissionAsync(user, 'remove-closed-livechat-room'))) {
 			throw new Meteor.Error('error-not-allowed', 'Not allowed', { method: 'livechat:removeRoom' });
