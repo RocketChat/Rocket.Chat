b86cae425e14ceea2c7ce20fc375083b4bc6af7b
Martin Schoeler <martin.schoeler@rocket.chat>
2025-09-18 15:52:59 -0300

    feat: Move Timestamp parser out of Feature Preview (#36976)

--- apps/meteor/client/views/room/composer/messageBox/MessageBoxActionsToolbar/hooks/useTimestampAction.tsx
+++ apps/meteor/client/views/room/composer/messageBox/MessageBoxActionsToolbar/hooks/useTimestampAction.tsx
@@ -1,5 +1,4 @@
 import type { GenericMenuItemProps } from '@rocket.chat/ui-client';
-import { useFeaturePreview } from '@rocket.chat/ui-client';
 import { useSetModal } from '@rocket.chat/ui-contexts';
 import { useTranslation } from 'react-i18next';
 
@@ -9,11 +8,6 @@ import type { ComposerAPI } from '../../../../../../lib/chats/ChatAPI';
 export const useTimestampAction = (composer: ComposerAPI | undefined): GenericMenuItemProps | undefined => {
 	const setModal = useSetModal();
 	const { t } = useTranslation();
-	const timestampFeatureEnabled = useFeaturePreview('enable-timestamp-message-parser');
-
-	if (!timestampFeatureEnabled) {
-		return;
-	}
 
 	const handleClick = () => {
 		if (!composer) {
