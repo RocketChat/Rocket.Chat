772a08686456c51bc08ebd75aa72ea122e3a501f
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-09-18 18:07:55 -0300

    fix: update type check for federation version in IUser interface to ensure it is a string

--- packages/core-typings/src/IUser.ts
+++ packages/core-typings/src/IUser.ts
@@ -268,7 +268,7 @@ export interface IUserNativeFederated extends IUser {
 }
 
 export const isUserNativeFederated = (user: Partial<IUser>): user is IUserNativeFederated =>
-	isUserFederated(user) && 'federation' in user && user.federation?.version === '0.0.0';
+	isUserFederated(user) && 'federation' in user && typeof user.federation?.version === 'string';
 
 export type IUserDataEvent = {
 	id: unknown;
