28c455121c4193754d8f23ed4143143e9b5f52ac
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-09-19 12:34:41 -0300

    feat: update ComposerFederationInvalidVersion to include external link and improved translation for invalid version message

--- apps/meteor/client/views/room/composer/ComposerFederation/ComposerFederationInvalidVersion.tsx
+++ apps/meteor/client/views/room/composer/ComposerFederation/ComposerFederationInvalidVersion.tsx
@@ -1,11 +1,21 @@
-import { MessageFooterCallout } from '@rocket.chat/ui-composer';
+import { ExternalLink } from '@rocket.chat/ui-client';
+import { MessageFooterCallout, MessageFooterCalloutContent } from '@rocket.chat/ui-composer';
 import type { ReactElement } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
 
 const ComposerFederationInvalidVersion = (): ReactElement => {
-	const { t } = useTranslation();
-
-	return <MessageFooterCallout>{t('Federation_Matrix_Federated_Description_invalid_version')}</MessageFooterCallout>;
+	return (
+		<MessageFooterCallout>
+			<MessageFooterCalloutContent>
+				<Trans
+					i18nKey='Federation_Matrix_Federated_Description_invalid_version'
+					components={{
+						1: <ExternalLink to='https://go.rocket.chat/i/matrix-federation' />,
+					}}
+				/>
+			</MessageFooterCalloutContent>
+		</MessageFooterCallout>
+	);
 };
 
 export default ComposerFederationInvalidVersion;
