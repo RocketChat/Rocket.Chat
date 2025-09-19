83bf4ce2d90b69d993619d34e8f95a722c1748bf
Ricardo Garim <rswarovsky@gmail.com>
2025-09-19 15:25:37 -0300

    feat: adds federation event ACL (#36913)

---
+++ ee/packages/federation-matrix/src/api/middlewares.ts
@@ -0,0 +1,33 @@
+import type { EventAuthorizationService } from '@hs/federation-sdk';
+import { errCodes } from '@hs/federation-sdk';
+import type { EventID } from '@hs/room';
+import type { Context, Next } from 'hono';
+
+export const canAccessEvent = (federationAuth: EventAuthorizationService) => async (c: Context, next: Next) => {
+	try {
+		const url = new URL(c.req.url);
+		const path = url.search ? `${c.req.path}${url.search}` : c.req.path;
+
+		const verificationResult = await federationAuth.canAccessEventFromAuthorizationHeader(
+			c.req.param('eventId') as EventID,
+			c.req.header('Authorization') || '',
+			c.req.method,
+			path,
+			undefined,
+		);
+
+		if (!verificationResult.authorized) {
+			return c.json(
+				{
+					errcode: errCodes[verificationResult.errorCode].errcode,
+					error: errCodes[verificationResult.errorCode].error,
+				},
+				errCodes[verificationResult.errorCode].status,
+			);
+		}
+
+		return next();
+	} catch (error) {
+		return c.json(errCodes.M_UNKNOWN, 500);
+	}
+};
