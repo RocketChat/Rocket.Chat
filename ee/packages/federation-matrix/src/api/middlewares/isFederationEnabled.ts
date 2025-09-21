06a16aebf5c9f8c9bbc9f2fd294c83d4a8d8997b
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2025-09-21 14:38:41 -0300

    Fix isFederationEnabledMiddleware inverted logic

--- ee/packages/federation-matrix/src/api/middlewares/isFederationEnabled.ts
+++ ee/packages/federation-matrix/src/api/middlewares/isFederationEnabled.ts
@@ -2,7 +2,7 @@ import { Settings } from '@rocket.chat/core-services';
 import { createMiddleware } from 'hono/factory';
 
 export const isFederationEnabledMiddleware = createMiddleware(async (c, next) => {
-	if (await Settings.get<boolean>('Federation_Enabled')) {
+	if (!(await Settings.get<boolean>('Federation_Service_Enabled'))) {
 		return c.json({ error: 'Federation is not enabled' }, 403);
 	}
 	return next();
