3b2905b476d62817e551ca639600195ebe614c40
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2025-09-24 12:22:01 -0300

    feat: Media Call Client (#36717)

--- packages/ui-voip/src/v2/index.ts
+++ packages/ui-voip/src/v2/index.ts
@@ -1,3 +1,4 @@
-export { useMediaCallContext } from './MediaCallContext';
+export { useMediaCallContext, useMediaCallExternalContext, default as MediaCallContext } from './MediaCallContext';
 export type { PeerInfo } from './MediaCallContext';
 export { default as MediaCallWidget } from './MediaCallWidget';
+export { useMediaCallAction } from './useMediaCallAction';
