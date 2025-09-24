63e4b89555ee53e7d33a677272e8cf24bc017e90
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-09-24 12:54:36 -0300

    chore: updates param type on method

--- apps/meteor/app/livechat/server/methods/saveBusinessHour.ts
+++ apps/meteor/app/livechat/server/methods/saveBusinessHour.ts
@@ -1,4 +1,4 @@
-import type { ILivechatBusinessHour } from '@rocket.chat/core-typings';
+import type { ISaveLivechatBusinessHour } from '@rocket.chat/core-typings';
 import type { ServerMethods } from '@rocket.chat/ddp-client';
 import { Meteor } from 'meteor/meteor';
 
@@ -8,7 +8,7 @@ import { businessHourManager } from '../business-hour';
 declare module '@rocket.chat/ddp-client' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	interface ServerMethods {
-		'livechat:saveBusinessHour'(businessHourData: ILivechatBusinessHour): void;
+		'livechat:saveBusinessHour'(businessHourData: ISaveLivechatBusinessHour): void;
 	}
 }
 
