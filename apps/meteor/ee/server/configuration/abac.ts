3ef70b0ee6a9de5d43a77e2ec37ec6934c05d942
Kevin  Aleman <kaleman960@gmail.com>
2025-09-29 13:54:17 -0600

    permission

--- apps/meteor/ee/server/configuration/abac.ts
+++ apps/meteor/ee/server/configuration/abac.ts
@@ -3,6 +3,9 @@ import { License } from '@rocket.chat/license';
 Meteor.startup(async () => {
 	await License.onLicense('abac', async () => {
 		const { addSettings } = await import('../settings/abac');
+		const { createPermissions } = await import('../lib/abac');
+
 		await addSettings();
+		await createPermissions();
 	});
 });
