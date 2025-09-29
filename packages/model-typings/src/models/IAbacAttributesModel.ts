7c622b2d018c7399b9ce374cdd6f28c89c325919
Kevin  Aleman <kaleman960@gmail.com>
2025-09-29 15:06:53 -0600

    lint

--- packages/model-typings/src/models/IAbacAttributesModel.ts
+++ packages/model-typings/src/models/IAbacAttributesModel.ts
@@ -1,4 +1,6 @@
 import type { IAbacAttribute } from '@rocket.chat/core-typings';
+
 import type { IBaseModel } from './IBaseModel';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IAbacAttributesModel extends IBaseModel<IAbacAttribute> {}
