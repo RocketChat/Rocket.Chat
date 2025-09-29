7384e9af4e9d783cf81ff37d78ef2cc0f14a0851
Kevin  Aleman <kaleman960@gmail.com>
2025-09-29 13:40:24 -0600

    abac model

---
+++ packages/core-typings/src/IAbacAttribute.ts
@@ -0,0 +1,3 @@
+import type { IRocketChatRecord } from './IRocketChatRecord';
+
+export interface IAbacAttribute extends IRocketChatRecord {}
---
+++ packages/model-typings/src/models/IAbacAttributesModel.ts
@@ -0,0 +1,4 @@
+import type { IAbacAttribute } from '@rocket.chat/core-typings';
+import type { IBaseModel } from './IBaseModel';
+
+export interface IAbacAttributesModel extends IBaseModel<IAbacAttribute> {}
