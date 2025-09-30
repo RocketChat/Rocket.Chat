eba9d14e05e50f68d4a2feec2060cab7c6dc0e81
Kevin  Aleman <kaleman960@gmail.com>
2025-09-30 14:07:20 -0600

    create an abac attribute

--- apps/meteor/ee/server/api/abac/schemas.ts
+++ apps/meteor/ee/server/api/abac/schemas.ts
@@ -4,7 +4,7 @@ const ajv = new Ajv({
 	coerceTypes: true,
 });
 
-const ABACToggleRoomStatusSuccess = {
+const GenericSuccess = {
 	type: 'object',
 	properties: {
 		success: { type: 'boolean', enum: [true] },
@@ -12,4 +12,24 @@ const ABACToggleRoomStatusSuccess = {
 	additionalProperties: false,
 };
 
-export const POSTAbacToggleRoomStatusSuccessSchema = ajv.compile<void>(ABACToggleRoomStatusSuccess);
+export const GenericSuccessSchema = ajv.compile<void>(GenericSuccess);
+
+// Create an abac attribute using the IAbacAttributeDefintion type, create the ajv schemas
+
+const AbacAttributeDefinition = {
+	type: 'object',
+	properties: {
+		key: { type: 'string', minLength: 1 },
+		values: {
+			type: 'array',
+			items: { type: 'string', minLength: 1 },
+			minItems: 1,
+			maxItems: 10,
+			uniqueItems: true,
+		},
+	},
+	required: ['key', 'values'],
+	additionalProperties: false,
+};
+
+export const POSTAbacAttributeDefinitionSchema = ajv.compile<void>(AbacAttributeDefinition);
--- packages/core-services/src/types/IAbacService.ts
+++ packages/core-services/src/types/IAbacService.ts
@@ -1,5 +1,6 @@
-import type { IRoom } from '@rocket.chat/core-typings';
+import type { IAbacAttributeDefinition, IRoom } from '@rocket.chat/core-typings';
 
 export interface IAbacService {
 	toggleAbacConfigurationForRoom(rid: IRoom['_id']): Promise<void>;
+	addAbacAttribute(attribute: IAbacAttributeDefinition): Promise<void>;
 }
