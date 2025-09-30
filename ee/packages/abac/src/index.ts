2af2b83f777ee29e9768af0f06bb7e7a3d542b2d
Kevin  Aleman <kaleman960@gmail.com>
2025-09-30 09:38:18 -0600

    abac flag

--- ee/packages/abac/src/index.ts
+++ ee/packages/abac/src/index.ts
@@ -1,3 +1,11 @@
-export function hello() {
-	return 'hello';
+import { Rooms } from '@rocket.chat/models';
+
+export async function toggleAbacConfigurationForRoom(rid: string) {
+	const room = await Rooms.findOneByIdAndType(rid, 'p');
+
+	if (!room) {
+		throw new Error('error-invalid-room');
+	}
+
+	await Rooms.updateAbacConfigurationById(rid, !room.abac);
 }
