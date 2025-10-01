b7f3cb72960b97f053e8e0dd8a4eb4a36be37739
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-10-01 15:51:36 -0300

    refactor(federation): remove deprecated callback for adding users to room and enhance username validation logic

--- apps/meteor/lib/callbacks.ts
+++ apps/meteor/lib/callbacks.ts
@@ -85,7 +85,6 @@ interface EventLikeCallbackSignatures {
 		message: IMessage,
 		params: { user: IUser; reaction: string; shouldReact: boolean; oldMessage: IMessage; room: IRoom },
 	) => void;
-	'federation.onAddUsersToRoom': (params: { invitees: IUser[] | Username[]; inviter: IUser }, room: IRoom) => void;
 	'onJoinVideoConference': (callId: VideoConference['_id'], userId?: IUser['_id']) => Promise<void>;
 	'usernameSet': () => void;
 	'beforeJoinRoom': (user: IUser, room: IRoom) => void;
