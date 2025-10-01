1f5f93b6d519f4598099348c3db8735b0813fdff
juliajforesti <juliajforesti@gmail.com>
2025-10-01 09:46:46 -0300

    fix: remove `playNewMessageLoop`

--- apps/meteor/client/providers/CustomSoundProvider/CustomSoundProvider.tsx
+++ apps/meteor/client/providers/CustomSoundProvider/CustomSoundProvider.tsx
@@ -79,7 +79,6 @@ const CustomSoundProvider = ({ children }: CustomSoundProviderProps) => {
 			playNewMessage: () => play(newMessageNotification, { loop: false, volume: formatVolume(notificationsSoundVolume) }),
 			playNewMessageCustom: (soundId: ICustomSound['_id']) =>
 				play(soundId, { loop: false, volume: formatVolume(notificationsSoundVolume) }),
-			playNewMessageLoop: () => play(newMessageNotification, { loop: true, volume: formatVolume(notificationsSoundVolume) }),
 			stopNewRoom: () => stop(newRoomNotification),
 			stopNewMessage: () => stop(newMessageNotification),
 		};
