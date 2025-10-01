7f8d42ec07dc7e5c45d65de7105e4621e429689a
juliajforesti <juliajforesti@gmail.com>
2025-10-01 15:30:58 -0300

    chore!: remove stdout logging functionality and related components

--- apps/meteor/client/views/admin/viewLogs/ViewLogsPage.tsx
+++ apps/meteor/client/views/admin/viewLogs/ViewLogsPage.tsx
@@ -4,27 +4,25 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AnalyticsReports from './AnalyticsReports';
-import ServerLogs from './ServerLogs';
 import { Page, PageHeader, PageContent } from '../../../components/Page';
 
 const ViewLogsPage = (): ReactElement => {
 	const { t } = useTranslation();
-	const [tab, setTab] = useState('Logs');
+	const [tab, setTab] = useState('Analytics');
 
 	return (
 		<Page background='tint'>
 			<PageHeader title={t('Reports')} />
 			<Margins blockEnd={24}>
 				<Tabs>
-					<Tabs.Item onClick={() => setTab('Logs')} selected={tab === 'Logs'}>
-						{t('Logs')}
-					</Tabs.Item>
 					<Tabs.Item onClick={() => setTab('Analytics')} selected={tab === 'Analytics'}>
 						{t('Analytic_reports')}
 					</Tabs.Item>
 				</Tabs>
 			</Margins>
-			<PageContent>{tab === 'Logs' ? <ServerLogs /> : <AnalyticsReports />}</PageContent>
+			<PageContent>
+				<AnalyticsReports />
+			</PageContent>
 		</Page>
 	);
 };
