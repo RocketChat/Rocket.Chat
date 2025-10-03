b8a45202634e36fc8d10c02ed112b91122d19e40
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-10-03 10:34:40 -0300

    tests: fixes createMonitor helper url

--- apps/meteor/tests/e2e/utils/omnichannel/monitors.ts
+++ apps/meteor/tests/e2e/utils/omnichannel/monitors.ts
@@ -6,7 +6,7 @@ const removeMonitor = async (api: BaseTest['api'], id: string) =>
 	});
 
 export const createMonitor = async (api: BaseTest['api'], username: string) => {
-	const response = await api.post('/v1/livechat/monitors.save', { username });
+	const response = await api.post('/livechat/monitors.save', { username });
 
 	if (response.status() !== 200) {
 		throw new Error(`Failed to create monitor [http status: ${response.status()}]`);
