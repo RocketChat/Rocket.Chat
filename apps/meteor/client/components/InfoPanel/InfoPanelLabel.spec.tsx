ab93ea42e36004f59222dd6790b6169c6c391667
MartinSchoeler <martinschoeler8@gmail.com>
2025-10-07 18:37:42 -0300

    fix: mistake on tests

--- apps/meteor/client/components/InfoPanel/InfoPanelLabel.spec.tsx
+++ apps/meteor/client/components/InfoPanel/InfoPanelLabel.spec.tsx
@@ -4,19 +4,13 @@ import InfoPanelLabel from './InfoPanelLabel';
 
 describe('InfoPanelLabel', () => {
 	it('should match snapshot for default label', () => {
-		const { baseElement } = render(
-			<InfoPanelLabel>
-				<span>Default label content</span>
-			</InfoPanelLabel>,
-		);
+		const { baseElement } = render(<InfoPanelLabel>Default label content</InfoPanelLabel>);
 		expect(baseElement).toMatchSnapshot();
 	});
 
 	it('should match snapshot for label with title', () => {
 		const { baseElement } = render(
-			<InfoPanelLabel title='This is helpful information about the label'>
-				<span>Label with info icon</span>
-			</InfoPanelLabel>,
+			<InfoPanelLabel title='This is helpful information about the label'>Label with info icon</InfoPanelLabel>,
 		);
 		expect(baseElement).toMatchSnapshot();
 	});
