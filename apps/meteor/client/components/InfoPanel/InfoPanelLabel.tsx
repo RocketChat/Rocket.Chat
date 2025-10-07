5bc5efddfeaf3d5d1bf3a9dd806dee47d82d6442
dougfabris <devfabris@gmail.com>
2025-10-07 19:08:31 -0300

    fix: review

--- apps/meteor/client/components/InfoPanel/InfoPanelLabel.tsx
+++ apps/meteor/client/components/InfoPanel/InfoPanelLabel.tsx
@@ -1,16 +1,13 @@
-import { Box, Icon, Palette } from '@rocket.chat/fuselage';
+import { Box, Icon } from '@rocket.chat/fuselage';
 import type { ComponentPropsWithoutRef } from 'react';
 
 type InfoPanelLabelProps = ComponentPropsWithoutRef<typeof Box>;
 
-const InfoPanelLabel = (props: InfoPanelLabelProps) => {
-	const { title, children, ...filteredProps } = props;
-	return (
-		<Box mb={8} fontScale='p2m' color='default' {...filteredProps}>
-			{children}
-			{title && <Icon name='info' color={Palette.statusColor['status-font-on-info']} mi={4} size='x16' title={title} />}
-		</Box>
-	);
-};
+const InfoPanelLabel = ({ title, children, ...props }: InfoPanelLabelProps) => (
+	<Box mb={8} fontScale='p2m' color='default' {...props}>
+		{children}
+		{title && <Icon name='info' color='info' mi={4} size='x16' title={title} />}
+	</Box>
+);
 
 export default InfoPanelLabel;
