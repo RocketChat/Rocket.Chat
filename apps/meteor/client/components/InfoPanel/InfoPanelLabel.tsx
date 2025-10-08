118e2f3d1aca208aaaa732d479dec92c9e883954
dougfabris <devfabris@gmail.com>
2025-10-08 10:24:27 -0300

    fix: icon color

--- apps/meteor/client/components/InfoPanel/InfoPanelLabel.tsx
+++ apps/meteor/client/components/InfoPanel/InfoPanelLabel.tsx
@@ -6,7 +6,7 @@ type InfoPanelLabelProps = ComponentPropsWithoutRef<typeof Box>;
 const InfoPanelLabel = ({ title, children, ...props }: InfoPanelLabelProps) => (
 	<Box mb={8} fontScale='p2m' color='default' {...props}>
 		{children}
-		{title && <Icon name='info' color='info' mi={4} size='x16' title={title} />}
+		{title && <Icon name='info' color='font-secondary-info' mi={4} size='x16' title={title} />}
 	</Box>
 );
 
