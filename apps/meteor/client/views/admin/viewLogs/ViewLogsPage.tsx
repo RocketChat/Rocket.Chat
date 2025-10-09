7f423be4721eed85932ede1db332f0fc3f5b8a98
juliajforesti <juliajforesti@gmail.com>
2025-10-09 11:39:45 -0300

    chore!: change `/reports` to `/analytic-reports`

--- apps/meteor/client/views/admin/viewLogs/ViewLogsPage.tsx
+++ apps/meteor/client/views/admin/viewLogs/ViewLogsPage.tsx
@@ -1,6 +1,4 @@
-import { Margins, Tabs } from '@rocket.chat/fuselage';
 import type { ReactElement } from 'react';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AnalyticsReports from './AnalyticsReports';
@@ -8,18 +6,10 @@ import { Page, PageHeader, PageContent } from '../../../components/Page';
 
 const ViewLogsPage = (): ReactElement => {
 	const { t } = useTranslation();
-	const [tab, setTab] = useState('Analytics');
 
 	return (
 		<Page background='tint'>
-			<PageHeader title={t('Reports')} />
-			<Margins blockEnd={24}>
-				<Tabs>
-					<Tabs.Item onClick={() => setTab('Analytics')} selected={tab === 'Analytics'}>
-						{t('Analytic_reports')}
-					</Tabs.Item>
-				</Tabs>
-			</Margins>
+			<PageHeader title={t('Analytic_reports')} />
 			<PageContent>
 				<AnalyticsReports />
 			</PageContent>
