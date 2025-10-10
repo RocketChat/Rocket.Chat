1186b5284ea6a2192bfbf1ccf4851bc169dab9e5
Kevin  Aleman <kaleman960@gmail.com>
2025-10-10 12:42:23 -0600

    fix message

--- apps/meteor/server/lib/dataExport/exportRoomMessagesToFile.ts
+++ apps/meteor/server/lib/dataExport/exportRoomMessagesToFile.ts
@@ -156,9 +156,7 @@ export const getMessageData = (
 			messageObject.msg = i18n.t('Chat_started');
 			break;
 		case 'abac-removed-user-from-room':
-			messageObject.msg = i18n.t('abac_removed__username__from_the_room', {
-				user_removed: hideUserName(msg.msg, userData, usersMap),
-			});
+			messageObject.msg = i18n.t('abac_removed_user_from_the_room');
 			break;
 	}
 
