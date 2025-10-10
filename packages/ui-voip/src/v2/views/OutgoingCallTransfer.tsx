e0a169d3bb8b748f99a243c396ff0e96da456860
gabriellsh <henriques.gabriell@gmail.com>
2025-10-10 13:16:43 -0300

    view components

---
+++ packages/ui-voip/src/v2/views/OutgoingCallTransfer.tsx
@@ -0,0 +1,40 @@
+import { Button, ButtonGroup } from '@rocket.chat/fuselage';
+import { useTranslation } from 'react-i18next';
+
+import { useMediaCallContext } from '../MediaCallContext';
+import { PeerInfo, Widget, WidgetFooter, WidgetHandle, WidgetHeader, WidgetContent, DevicePicker, WidgetInfo } from '../components';
+
+const OutgoingCall = () => {
+	const { t } = useTranslation();
+
+	const { onEndCall, peerInfo, connectionState /* , transferredBy */ } = useMediaCallContext();
+
+	// TODO: Figure out how to ensure this always exist before rendering the component
+	if (!peerInfo) {
+		throw new Error('Peer info is required');
+	}
+
+	const connecting = connectionState === 'CONNECTING';
+
+	return (
+		<Widget>
+			<WidgetHandle />
+			<WidgetHeader title={connecting ? t('meteor_status_connecting') : `${t('Transferring_call')}...`}>
+				<DevicePicker />
+			</WidgetHeader>
+			<WidgetInfo slots={[{ text: t('Transferred_call__from__to' /* , { from: transferredBy.displayName } */), type: 'info' }]} />
+			<WidgetContent>
+				<PeerInfo {...peerInfo} />
+			</WidgetContent>
+			<WidgetFooter>
+				<ButtonGroup stretch>
+					<Button medium name='phone' icon='phone-off' danger flexGrow={1} onClick={onEndCall}>
+						{t('Cancel')}
+					</Button>
+				</ButtonGroup>
+			</WidgetFooter>
+		</Widget>
+	);
+};
+
+export default OutgoingCall;
