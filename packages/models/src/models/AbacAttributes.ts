e53b21569bbdf704af6c77d046b823594f4acd9a
Kevin  Aleman <kaleman960@gmail.com>
2025-10-15 13:49:44 -0600

    fix: FindOneById used where key was provided

--- packages/models/src/models/AbacAttributes.ts
+++ packages/models/src/models/AbacAttributes.ts
@@ -1,5 +1,5 @@
 import type { IAbacAttribute } from '@rocket.chat/core-typings';
-import type { Db, IndexDescription } from 'mongodb';
+import type { Db, FindOptions, IndexDescription } from 'mongodb';
 
 import { BaseRaw } from './BaseRaw';
 
@@ -10,5 +10,9 @@ export class AbacAttributesRaw extends BaseRaw<IAbacAttribute> {
 
 	protected modelIndexes(): IndexDescription[] {
 		return [{ key: { key: 1 }, unique: true }, { key: { values: 1 } }];
+	}
+
+	findOneByKey(key: string, options: FindOptions<IAbacAttribute> = {}): Promise<IAbacAttribute | null> {
+		return this.findOne({ key }, options);
 	}
 }
