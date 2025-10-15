a7f1cb8d57efd5e1a13e5771027a38445d1f68a9
Kevin  Aleman <kaleman960@gmail.com>
2025-10-15 13:49:44 -0600

    fix: FindOneById used where key was provided

--- packages/model-typings/src/models/IAbacAttributesModel.ts
+++ packages/model-typings/src/models/IAbacAttributesModel.ts
@@ -1,6 +1,9 @@
 import type { IAbacAttribute } from '@rocket.chat/core-typings';
+import type { FindOptions } from 'mongodb';
 
 import type { IBaseModel } from './IBaseModel';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface IAbacAttributesModel extends IBaseModel<IAbacAttribute> {}
+export interface IAbacAttributesModel extends IBaseModel<IAbacAttribute> {
+	findOneByKey(key: string, options?: FindOptions<IAbacAttribute>): Promise<IAbacAttribute | null>;
+}
