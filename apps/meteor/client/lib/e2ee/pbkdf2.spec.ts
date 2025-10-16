f73043406d068ea26530625ad2d09674e9f81ed4
Matheus Cardoso <matheus@cardo.so>
2025-10-16 11:07:20 -0300

    test(e2ee): update `pbkdf2` tests for stricter type assertions

--- apps/meteor/client/lib/e2ee/pbkdf2.spec.ts
+++ apps/meteor/client/lib/e2ee/pbkdf2.spec.ts
@@ -8,11 +8,11 @@ describe('pbkdf2', () => {
 	it('should import a base key', async () => {
 		const keyData = new Uint8Array([1, 2, 3, 4, 5]);
 		const baseKey = await importBaseKey(keyData);
-		expect(baseKey).toBeDefined();
-		expect(baseKey.algorithm).toEqual({ name: 'PBKDF2' });
-		expect(baseKey.extractable).toBe(false);
-		expect(baseKey.type).toBe('secret');
-		expect(baseKey.usages).toEqual(['deriveBits']);
+		expect<'PBKDF2'>(baseKey.algorithm.name).toBe('PBKDF2');
+		expect<false>(baseKey.extractable).toBe(false);
+		expect<'secret'>(baseKey.type).toBe('secret');
+		expect<1>(baseKey.usages.length).toBe(1);
+		expect<'deriveBits'>(baseKey.usages[0]).toBe('deriveBits');
 	});
 
 	it('should derive bits from a base key', async () => {
@@ -21,31 +21,33 @@ describe('pbkdf2', () => {
 		const iterations = 1000;
 		const baseKey = await importBaseKey(keyData);
 		const derivedBits = await deriveBits(baseKey, { salt, iterations });
-		expect(derivedBits).toBeDefined();
-		expect(derivedBits.byteLength).toBe(32);
+		expect<false>(derivedBits.detached).toBe(false);
+		expect<false>(derivedBits.resizable).toBe(false);
+		expect<32>(derivedBits.byteLength).toBe(32);
+		expect<32>(derivedBits.maxByteLength).toBe(32);
+		expect<() => never>(() => derivedBits.resize(32)).toThrow();
 	});
 
 	it('should import a derived key for AES-CBC', async () => {
 		const baseKey = await importBaseKey(new Uint8Array([1, 2, 3, 4, 5]));
 		const derivedBits = await deriveBits(baseKey, { salt: new Uint8Array([5, 4, 3, 2, 1]), iterations: 1000 });
-		const algorithm = 'AES-CBC';
-		const derivedKey = await importKey(derivedBits, algorithm);
-		expect(derivedKey).toBeDefined();
-		expect(derivedKey.algorithm).toEqual({ name: 'AES-CBC', length: 256 });
-		expect(derivedKey.extractable).toBe(false);
-		expect(derivedKey.type).toBe('secret');
-		expect(derivedKey.usages).toEqual(['decrypt']);
+		const derivedKey = await importKey(derivedBits, 'AES-CBC');
+		expect<256>(derivedKey.algorithm.length).toBe(256);
+		expect<'AES-CBC'>(derivedKey.algorithm.name).toBe('AES-CBC');
+		expect<false>(derivedKey.extractable).toBe(false);
+		expect<'secret'>(derivedKey.type).toBe('secret');
+		expect<1>(derivedKey.usages.length).toBe(1);
+		expect<['decrypt']>(derivedKey.usages).toEqual(['decrypt']);
 	});
 
 	it('should import a derived key for AES-GCM', async () => {
 		const baseKey = await importBaseKey(new Uint8Array([1, 2, 3, 4, 5]));
 		const derivedBits = await deriveBits(baseKey, { salt: new Uint8Array([5, 4, 3, 2, 1]), iterations: 1000 });
-		const algorithm = 'AES-GCM';
-		const derivedKey = await importKey(derivedBits, algorithm);
-		expect(derivedKey).toBeDefined();
-		expect(derivedKey.algorithm).toEqual({ name: 'AES-GCM', length: 256 });
-		expect(derivedKey.extractable).toBe(false);
-		expect(derivedKey.type).toBe('secret');
-		expect(derivedKey.usages).toEqual(['encrypt', 'decrypt']);
+		const derivedKey = await importKey(derivedBits, 'AES-GCM');
+		expect<256>(derivedKey.algorithm.length).toBe(256);
+		expect<'AES-GCM'>(derivedKey.algorithm.name).toBe('AES-GCM');
+		expect<false>(derivedKey.extractable).toBe(false);
+		expect<'secret'>(derivedKey.type).toBe('secret');
+		expect<['encrypt', 'decrypt']>(derivedKey.usages).toEqual(['encrypt', 'decrypt']);
 	});
 });
