52163683a56e5fa48ac3a3977392367f2a7328c5
Kevin  Aleman <kaleman960@gmail.com>
2025-10-17 08:55:03 -0600

    restore

--- packages/http-router/src/Router.ts
+++ packages/http-router/src/Router.ts
@@ -264,6 +264,7 @@ export class Router<
 						method: req.method,
 						path: req.url,
 						error: responseValidatorFn.errors?.map((error: any) => error.message).join('\n '),
+						originalResponse: body,
 					});
 					return c.json(
 						{
