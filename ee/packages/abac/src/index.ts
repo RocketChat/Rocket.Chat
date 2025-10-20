4591df3565ed3335b8b57892c6c8e90a952a0eb7
Kevin Aleman <kaleman960@gmail.com>
2025-10-20 14:04:02 -0600

    feat: Remove users from room when new attributes are added to the room (#37172)

--- ee/packages/abac/src/index.ts
+++ ee/packages/abac/src/index.ts
@@ -170,13 +170,9 @@ export class AbacService extends ServiceClass implements IAbacService {
 	}
 
 	async setRoomAbacAttributes(rid: string, attributes: Record<string, string[]>): Promise<void> {
-		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain' | 'teamDefault' | 'default'>>(
-			rid,
-			'p',
-			{
-				projection: { abacAttributes: 1, t: 1, teamMain: 1, teamDefault: 1, default: 1 },
-			},
-		);
+		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain'>>(rid, 'p', {
+			projection: { abacAttributes: 1, t: 1, teamMain: 1 },
+		});
 		if (!room) {
 			throw new Error('error-room-not-found');
 		}
@@ -272,13 +268,9 @@ export class AbacService extends ServiceClass implements IAbacService {
 	}
 
 	async updateRoomAbacAttributeValues(rid: string, key: string, values: string[]): Promise<void> {
-		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain' | 'teamDefault' | 'default'>>(
-			rid,
-			'p',
-			{
-				projection: { abacAttributes: 1, t: 1, teamMain: 1, teamDefault: 1, default: 1 },
-			},
-		);
+		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain'>>(rid, 'p', {
+			projection: { abacAttributes: 1, t: 1, teamMain: 1 },
+		});
 		if (!room) {
 			throw new Error('error-room-not-found');
 		}
@@ -325,9 +317,7 @@ export class AbacService extends ServiceClass implements IAbacService {
 	}
 
 	async removeRoomAbacAttribute(rid: string, key: string): Promise<void> {
-		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 'teamDefault' | 'default'>>(rid, 'p', {
-			projection: { abacAttributes: 1, default: 1, teamDefault: 1 },
-		});
+		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes'>>(rid, 'p', { projection: { abacAttributes: 1 } });
 		if (!room) {
 			throw new Error('error-room-not-found');
 		}
@@ -342,12 +332,6 @@ export class AbacService extends ServiceClass implements IAbacService {
 			return;
 		}
 
-		// if is the last attribute, just remove all
-		if (previous.length === 1) {
-			await Rooms.unsetAbacAttributesById(rid);
-			return;
-		}
-
 		await Rooms.removeAbacAttributeByRoomIdAndKey(rid, key);
 		this.logger.debug({
 			msg: 'Room ABAC attribute removed',
@@ -359,13 +343,9 @@ export class AbacService extends ServiceClass implements IAbacService {
 	async addRoomAbacAttributeByKey(rid: string, key: string, values: string[]): Promise<void> {
 		await this.ensureAttributeDefinitionsExist([{ key, values }]);
 
-		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain' | 'default' | 'teamDefault'>>(
-			rid,
-			'p',
-			{
-				projection: { abacAttributes: 1, t: 1, teamMain: 1, teamDefault: 1, default: 1 },
-			},
-		);
+		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain'>>(rid, 'p', {
+			projection: { abacAttributes: 1, t: 1, teamMain: 1 },
+		});
 		if (!room) {
 			throw new Error('error-room-not-found');
 		}
@@ -392,13 +372,9 @@ export class AbacService extends ServiceClass implements IAbacService {
 	async replaceRoomAbacAttributeByKey(rid: string, key: string, values: string[]): Promise<void> {
 		await this.ensureAttributeDefinitionsExist([{ key, values }]);
 
-		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain' | 'default' | 'teamDefault'>>(
-			rid,
-			'p',
-			{
-				projection: { abacAttributes: 1, t: 1, teamMain: 1, teamDefault: 1, default: 1 },
-			},
-		);
+		const room = await Rooms.findOneByIdAndType<Pick<IRoom, '_id' | 'abacAttributes' | 't' | 'teamMain'>>(rid, 'p', {
+			projection: { abacAttributes: 1, t: 1, teamMain: 1 },
+		});
 		if (!room) {
 			throw new Error('error-room-not-found');
 		}
