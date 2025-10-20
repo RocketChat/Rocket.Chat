2c3063721ae127e5ff65bd4ee3c71a46caaa9950
Kevin Aleman <kaleman960@gmail.com>
2025-10-20 10:17:10 -0600

    Update apps/meteor/app/channel-settings/server/methods/saveRoomSettings.ts

--- apps/meteor/app/channel-settings/server/methods/saveRoomSettings.ts
+++ apps/meteor/app/channel-settings/server/methods/saveRoomSettings.ts
@@ -70,7 +70,7 @@ const validators: RoomSettingsValidators = {
 				action: 'Viewing_room_administration',
 			});
 		}
-		if (settings.get('Abac_Enabled') && value && room?.abacAttributes) {
+		if (settings.get('Abac_Enabled') && value && room?.abacAttributes?.length) {
 			throw new Meteor.Error('error-action-not-allowed', 'Setting an ABAC managed room as default is not allowed', {
 				method: 'saveRoomSettings',
 				action: 'Viewing_room_administration',
