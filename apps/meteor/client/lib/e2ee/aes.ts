d034005091d282c029634a6399e1a8af45346027
Matheus Cardoso <matheus@cardo.so>
2025-10-20 13:48:53 -0300

    chore: add aes tests

--- apps/meteor/client/lib/e2ee/aes.ts
+++ apps/meteor/client/lib/e2ee/aes.ts
@@ -10,8 +10,19 @@ interface IAesGcmKeyAlgorithm<TLength extends AesKeyAlgorithmLength> extends Aes
 	length: TLength;
 }
 
-export type Key = CryptoKey & {
-	algorithm: IAesCbcKeyAlgorithm<128> | IAesGcmKeyAlgorithm<256>;
+type SupportedAesKeyAlgorithm = IAesCbcKeyAlgorithm<128> | IAesGcmKeyAlgorithm<256>;
+
+type JwkAlg = 'A128CBC' | 'A256GCM';
+type Jwk<TAlg extends JwkAlg = JwkAlg> = {
+	kty: 'oct';
+	k: string;
+	key_ops: ['encrypt', 'decrypt'] | ['decrypt', 'encrypt'];
+	ext: true;
+	alg: TAlg;
+};
+
+export type Key<TAlgorithm extends SupportedAesKeyAlgorithm = SupportedAesKeyAlgorithm> = CryptoKey & {
+	algorithm: TAlgorithm;
 	extractable: true;
 	type: 'secret';
 	usages: ['encrypt', 'decrypt'] | ['decrypt', 'encrypt'];
@@ -22,12 +33,15 @@ type AesEncryptedContent = {
 	ciphertext: Uint8Array<ArrayBuffer>;
 };
 
+type ToJwkAlg<TAlgorithm extends SupportedAesKeyAlgorithm> =
+	TAlgorithm extends IAesGcmKeyAlgorithm<256> ? 'A256GCM' : TAlgorithm extends IAesCbcKeyAlgorithm<128> ? 'A128CBC' : never;
+
 const JWK_ALG_TO_AES_KEY_ALGORITHM = {
 	A256GCM: { name: 'AES-GCM', length: 256 },
 	A128CBC: { name: 'AES-CBC', length: 128 },
-} satisfies Record<string, Key['algorithm']>;
+} satisfies Record<JwkAlg, SupportedAesKeyAlgorithm>;
 
-export const importKey = async (jwk: JsonWebKey): Promise<Key> => {
+export const importKey = async <const TJwk extends Jwk>(jwk: TJwk): Promise<Key<(typeof JWK_ALG_TO_AES_KEY_ALGORITHM)[TJwk['alg']]>> => {
 	if (!('alg' in jwk)) {
 		throw new Error('JWK alg property is required to import the key');
 	}
@@ -40,17 +54,17 @@ export const importKey = async (jwk: JsonWebKey): Promise<Key> => {
 
 	const key = await crypto.subtle.importKey('jwk', jwk, algorithm, true, ['encrypt', 'decrypt']);
 
-	return key as Key;
+	return key as Key<(typeof JWK_ALG_TO_AES_KEY_ALGORITHM)[TJwk['alg']]>;
 };
 
-export const exportKey = (key: Key): Promise<JsonWebKey> => {
-	return crypto.subtle.exportKey('jwk', key);
+export const exportKey = async <TAlgorithm extends SupportedAesKeyAlgorithm>(key: Key<TAlgorithm>): Promise<Jwk<ToJwkAlg<TAlgorithm>>> => {
+	const jwk = await crypto.subtle.exportKey('jwk', key);
+	return jwk as Jwk<ToJwkAlg<TAlgorithm>>;
 };
 
-export const generateKey = async (): Promise<Key> => {
+export const generateKey = async (): Promise<Key<{ name: 'AES-GCM'; length: 256 }>> => {
 	const key = await crypto.subtle.generateKey({ name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']);
-
-	return key as Key;
+	return key as Key<{ name: 'AES-GCM'; length: 256 }>;
 };
 
 export const decrypt = async (key: Key, content: AesEncryptedContent): Promise<string> => {
