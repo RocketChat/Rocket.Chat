45fb4ab125d03e920ebfcf9b2cf1cc8261c6d41e
Kevin Aleman <kaleman960@gmail.com>
2025-10-20 14:04:02 -0600

    feat: Remove users from room when new attributes are added to the room (#37172)

--- apps/meteor/app/lib/server/functions/removeUserFromRoom.ts
+++ apps/meteor/app/lib/server/functions/removeUserFromRoom.ts
@@ -1,7 +1,7 @@
 import { Apps, AppEvents } from '@rocket.chat/apps';
 import { AppsEngineException } from '@rocket.chat/apps-engine/definition/exceptions';
 import { Message, Team, Room } from '@rocket.chat/core-services';
-import type { IRoom, IUser } from '@rocket.chat/core-typings';
+import type { IRoom, IUser, MessageTypesValues } from '@rocket.chat/core-typings';
 import { Subscriptions, Rooms } from '@rocket.chat/models';
 import { Meteor } from 'meteor/meteor';
 
@@ -72,26 +72,76 @@ export const performUserRemoval = async function (room: IRoom, user: IUser, opti
  * and triggering all standard callbacks. Used for local actions (UI or API)
  * that should propagate normally to federation and other subscribers.
  */
-export const removeUserFromRoom = async function (rid: string, user: IUser, options?: { byUser: IUser }): Promise<void> {
+export const removeUserFromRoom = async function (
+	rid: string,
+	user: IUser,
+	options?: { byUser?: IUser; skipAppPreEvents?: boolean; customSystemMessage?: MessageTypesValues },
+): Promise<void> {
 	const room = await Rooms.findOneById(rid);
 	if (!room) {
 		return;
 	}
 
-	try {
-		await Apps.self?.triggerEvent(AppEvents.IPreRoomUserLeave, room, user, options?.byUser);
-	} catch (error: any) {
-		if (error.name === AppsEngineException.name) {
-			throw new Meteor.Error('error-app-prevented', error.message);
-		}
+	// Rationale: for an abac room, we don't want apps to be able to prevent a user from leaving
+	if (!options?.skipAppPreEvents) {
+		try {
+			await Apps.self?.triggerEvent(AppEvents.IPreRoomUserLeave, room, user, options?.byUser);
+		} catch (error: any) {
+			if (error.name === AppsEngineException.name) {
+				throw new Meteor.Error('error-app-prevented', error.message);
+			}
 
-		throw error;
+			throw error;
+		}
 	}
 
 	await Room.beforeLeave(room);
 
 	await performUserRemoval(room, user, options);
 
+	const subscription = await Subscriptions.findOneByRoomIdAndUserId(rid, user._id, {
+		projection: { _id: 1 },
+	});
+
+	if (subscription) {
+		const removedUser = user;
+		if (options?.customSystemMessage) {
+			await Message.saveSystemMessage(options?.customSystemMessage, rid, user.username || '', user);
+		} else if (options?.byUser) {
+			const extraData = {
+				u: options.byUser,
+			};
+
+			if (room.teamMain) {
+				await Message.saveSystemMessage('removed-user-from-team', rid, user.username || '', user, extraData);
+			} else {
+				await Message.saveSystemMessage('ru', rid, user.username || '', user, extraData);
+			}
+		} else if (room.teamMain) {
+			await Message.saveSystemMessage('ult', rid, removedUser.username || '', removedUser);
+		} else {
+			await Message.saveSystemMessage('ul', rid, removedUser.username || '', removedUser);
+		}
+	}
+
+	if (room.t === 'l') {
+		await Message.saveSystemMessage('command', rid, 'survey', user);
+	}
+
+	const deletedSubscription = await Subscriptions.removeByRoomIdAndUserId(rid, user._id);
+	if (deletedSubscription) {
+		void notifyOnSubscriptionChanged(deletedSubscription, 'removed');
+	}
+
+	if (room.teamId && room.teamMain) {
+		await Team.removeMember(room.teamId, user._id);
+	}
+
+	if (room.encrypted && settings.get('E2E_Enable')) {
+		await Rooms.removeUsersFromE2EEQueueByRoomId(room._id, [user._id]);
+	}
+
+	// TODO: CACHE: maybe a queue?
 	await afterLeaveRoomCallback.run({ user, kicker: options?.byUser }, room);
 
 	await Apps.self?.triggerEvent(AppEvents.IPostRoomUserLeave, room, user, options?.byUser);
