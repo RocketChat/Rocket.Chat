a211163724947e98862c5912708cbe56e70caebe
Anton Månsson <antm@redpill-linpro.com>
2025-10-21 15:45:20 +0200

    fix: Add screen reader support for reading 'encrypted' status of channel.

--- apps/meteor/client/views/room/Header/icons/Encrypted.tsx
+++ apps/meteor/client/views/room/Header/icons/Encrypted.tsx
@@ -9,7 +9,16 @@ import { HeaderState } from '../../../../components/Header';
 const Encrypted = ({ room }: { room: IRoom }) => {
 	const { t } = useTranslation();
 	const e2eEnabled = useSetting('E2E_Enable');
-	return e2eEnabled && room?.encrypted ? <HeaderState title={t('Encrypted')} icon='key' color={colors.g500} /> : null;
+	return e2eEnabled && room?.encrypted ? (
+		<HeaderState
+			title={t('Encrypted')}
+			icon='key'
+			color={colors.g800}
+			aria-hidden={false}
+			aria-live="polite"
+			role="img"
+			tabindex={0}
+			aria-label={t('Encrypted')}/> : null;
 };
 
 export default memo(Encrypted);
