c586f3a36cec7d53c2db0ae2cb47d961e7d57805
Kevin Aleman <kaleman960@gmail.com>
2025-10-22 12:07:12 -0600

    chore: Prevent default room & team channels from becoming abac managed (and viceversa) (#37264)
    Co-authored-by: Tasso <tasso.evangelista@rocket.chat>

--- apps/meteor/app/channel-settings/server/methods/saveRoomSettings.ts
+++ apps/meteor/app/channel-settings/server/methods/saveRoomSettings.ts
@@ -11,6 +11,7 @@ import { roomCoordinator } from '../../../../server/lib/rooms/roomCoordinator';
 import { hasPermissionAsync } from '../../../authorization/server/functions/hasPermission';
 import { setRoomAvatar } from '../../../lib/server/functions/setRoomAvatar';
 import { notifyOnRoomChangedById } from '../../../lib/server/lib/notifyListener';
+import { settings } from '../../../settings/server';
 import { saveReactWhenReadOnly } from '../functions/saveReactWhenReadOnly';
 import { saveRoomAnnouncement } from '../functions/saveRoomAnnouncement';
 import { saveRoomCustomFields } from '../functions/saveRoomCustomFields';
@@ -62,9 +63,15 @@ const hasRetentionPolicy = (room: IRoom & { retention?: any }): room is IRoomWit
 	'retention' in room && room.retention !== undefined;
 
 const validators: RoomSettingsValidators = {
-	async default({ userId }) {
+	async default({ userId, room, value }) {
 		if (!(await hasPermissionAsync(userId, 'view-room-administration'))) {
 			throw new Meteor.Error('error-action-not-allowed', 'Viewing room administration is not allowed', {
+				method: 'saveRoomSettings',
+				action: 'Viewing_room_administration',
+			});
+		}
+		if (settings.get('ABAC_Enabled') && value && room?.abacAttributes?.length) {
+			throw new Meteor.Error('error-action-not-allowed', 'Setting an ABAC managed room as default is not allowed', {
 				method: 'saveRoomSettings',
 				action: 'Viewing_room_administration',
 			});
