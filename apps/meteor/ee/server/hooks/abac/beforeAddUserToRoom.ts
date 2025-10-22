66d4fa3d0f8d9624246c79b279f3d1b8efb2679c
Kevin  Aleman <kaleman960@gmail.com>
2025-10-22 15:17:21 -0600

    setting check

--- apps/meteor/ee/server/hooks/abac/beforeAddUserToRoom.ts
+++ apps/meteor/ee/server/hooks/abac/beforeAddUserToRoom.ts
@@ -2,11 +2,18 @@ import { Abac } from '@rocket.chat/core-services';
 import { License } from '@rocket.chat/license';
 
 import { beforeAddUserToRoom } from '../../../../app/lib/server/lib/beforeAddUserToRoom';
+import { settings } from '../../../../app/settings/server';
 
 beforeAddUserToRoom.patch(async (prev, users, room, actor) => {
 	await prev(users, room, actor);
 
-	if (!room?.abacAttributes?.length || !users.filter(Boolean).length || !License.hasModule('abac') || room.t !== 'p') {
+	if (
+		!room?.abacAttributes?.length ||
+		!users.filter(Boolean).length ||
+		!License.hasModule('abac') ||
+		room.t !== 'p' ||
+		!settings.get('ABAC_Enabled')
+	) {
 		return;
 	}
 
