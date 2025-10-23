1bd9195202d6efd35d85a8ba75c87a8b26058b33
Douglas Fabris <devfabris@gmail.com>
2025-10-22 21:31:14 -0300

    feat: remove feature preview toggler from contextualbar

--- packages/ui-client/src/components/Contextualbar/ContextualbarAction.tsx
+++ packages/ui-client/src/components/Contextualbar/ContextualbarAction.tsx
@@ -1,18 +1,7 @@
-import { ContextualbarAction as ContextualbarActionComponent, ContextualbarV2Action } from '@rocket.chat/fuselage';
+import { ContextualbarV2Action } from '@rocket.chat/fuselage';
 import type { ComponentProps } from 'react';
 import { memo } from 'react';
 
-import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from '../FeaturePreview';
-
-const ContextualbarAction = (props: ComponentProps<typeof ContextualbarActionComponent>) => (
-	<FeaturePreview feature='newNavigation'>
-		<FeaturePreviewOff>
-			<ContextualbarActionComponent {...props} />
-		</FeaturePreviewOff>
-		<FeaturePreviewOn>
-			<ContextualbarV2Action {...props} />
-		</FeaturePreviewOn>
-	</FeaturePreview>
-);
+const ContextualbarAction = (props: ComponentProps<typeof ContextualbarV2Action>) => <ContextualbarV2Action {...props} />;
 
 export default memo(ContextualbarAction);
