323039c1c4b550987b2d96214c96865577f169cf
Matheus Cardoso <matheus@cardo.so>
2025-11-03 17:41:12 -0300

    fix: inneffective memoization

--- packages/ui-client/src/hooks/useValidatePassword.ts
+++ packages/ui-client/src/hooks/useValidatePassword.ts
@@ -1,8 +1,6 @@
 import { useVerifyPassword } from '@rocket.chat/ui-contexts';
-import { useMemo } from 'react';
 
 export const useValidatePassword = (password: string): boolean => {
 	const passwordVerifications = useVerifyPassword(password);
-
-	return useMemo(() => passwordVerifications.valid, [passwordVerifications]);
+	return passwordVerifications.valid;
 };
