9173955a40a163e759059529cb749c5432ca034c
Kevin  Aleman <kaleman960@gmail.com>
2025-11-10 15:02:43 -0600

    test

--- apps/meteor/tests/end-to-end/api/livechat/00-rooms.ts
+++ apps/meteor/tests/end-to-end/api/livechat/00-rooms.ts
@@ -2626,8 +2626,8 @@ describe('LIVECHAT - rooms', () => {
 			after(async () => {
 				await updateSetting('Livechat_Routing_Method', 'Auto_Selection');
 
-				await deleteVisitor(visitor._id);
 				await closeOmnichannelRoom(room._id);
+				await deleteVisitor(visitor._id);
 			});
 			it('should not allow users to update room info without serving the chat or having "save-others-livechat-room-info" permission', async () => {
 				await request
