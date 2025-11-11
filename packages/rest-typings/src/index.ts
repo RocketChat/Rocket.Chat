ac999e2dcf02a5737bf242944a2a06bf6060096b
juliajforesti <juliajforesti@gmail.com>
2025-11-11 15:00:04 -0300

    chore: delete `e2eUpdateGroupKeyParamsPOST`

--- packages/rest-typings/src/index.ts
+++ packages/rest-typings/src/index.ts
@@ -248,7 +248,6 @@ export * from './v1/autotranslate/AutotranslateSaveSettingsParamsPOST';
 export * from './v1/autotranslate/AutotranslateTranslateMessageParamsPOST';
 export * from './v1/e2e/e2eGetUsersOfRoomWithoutKeyParamsGET';
 export * from './v1/e2e/e2eSetUserPublicAndPrivateKeysParamsPOST';
-export * from './v1/e2e/e2eUpdateGroupKeyParamsPOST';
 export * from './v1/e2e';
 export * from './v1/import';
 export * from './v1/voip';
