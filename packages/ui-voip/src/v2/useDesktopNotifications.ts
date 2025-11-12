fff262abc09a778a9af7610b3689c237eade27f8
gabriellsh <henriques.gabriell@gmail.com>
2025-11-12 17:53:40 -0300

    WIP dispatch

--- packages/ui-voip/src/v2/useDesktopNotifications.ts
+++ packages/ui-voip/src/v2/useDesktopNotifications.ts
@@ -1,12 +1,14 @@
-import type { CustomNotificationOptions } from '@rocket.chat/desktop-api';
-import { useDebouncedCallback } from '@rocket.chat/fuselage-hooks';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { PeerInfo } from './MediaCallContext';
 import { SessionInfo } from './useMediaSessionInstance';
 
-const getDisplayInfo = (peerInfo: PeerInfo) => {
+const getDisplayInfo = (peerInfo?: PeerInfo) => {
+	if (!peerInfo) {
+		return undefined;
+	}
+
 	if ('number' in peerInfo) {
 		return { title: peerInfo.number };
 	}
@@ -19,35 +21,29 @@ const getDisplayInfo = (peerInfo: PeerInfo) => {
 export const useDesktopNotifications = (sessionInfo: SessionInfo) => {
 	const { t } = useTranslation();
 
-	const dispatch = useDebouncedCallback(
-		(options: CustomNotificationOptions) => {
-			window.RocketChatDesktop?.dispatchCustomNotification(options);
-		},
-		1000,
-		[window.RocketChatDesktop?.dispatchCustomNotification],
-	);
+	const displayInfo = getDisplayInfo(sessionInfo.peerInfo);
 
 	useEffect(() => {
-		if (!window.RocketChatDesktop?.dispatchCustomNotification || sessionInfo.state !== 'ringing') {
+		if (sessionInfo.state !== 'ringing') {
 			return;
 		}
 
-		const displayInfo = getDisplayInfo(sessionInfo.peerInfo);
-
-		if (!displayInfo) {
+		if (!displayInfo?.title) {
 			return;
 		}
 
 		// TODO: Prevent duplicate notifications if peerInfo changes
 		// Probably save a reference to the call id and check if it's the same call
-		dispatch({
+		window.RocketChatDesktop?.dispatchCustomNotification({
 			type: 'voice',
+			// id: callId,
 			payload: {
 				title: displayInfo.title,
 				body: t('Incoming_call'),
 				avatar: displayInfo.avatar,
+				// requireInteraction: true,
 				// silent: userStatus !== 'online' TODO: get the user status to decide if the notification should be silent. We might need to check some preferences too.
 			},
 		});
-	}, [dispatch, sessionInfo.peerInfo, sessionInfo.state, t]);
+	}, [displayInfo?.avatar, displayInfo?.title, sessionInfo.state, t]);
 };
