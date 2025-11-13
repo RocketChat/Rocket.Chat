b547cbda24edb11e90ce987fb863f9ffcfe14309
MartinSchoeler <martinschoeler8@gmail.com>
2025-11-13 13:25:48 -0300

    review: improve useIsABACAvailable

--- apps/meteor/client/views/admin/ABAC/hooks/useIsABACAvailable.tsx
+++ apps/meteor/client/views/admin/ABAC/hooks/useIsABACAvailable.tsx
@@ -3,9 +3,9 @@ import { useSetting } from '@rocket.chat/ui-contexts';
 import { useHasLicenseModule } from '../../../../hooks/useHasLicenseModule';
 
 const useIsABACAvailable = () => {
-	const hasABAC = useHasLicenseModule('abac');
-	const isABACSettingEnabled = useSetting('ABAC_Enabled');
-	return Boolean(hasABAC && isABACSettingEnabled);
+	const hasABAC = useHasLicenseModule('abac') === true;
+	const isABACSettingEnabled = useSetting('ABAC_Enabled', false);
+	return hasABAC && isABACSettingEnabled;
 };
 
 export default useIsABACAvailable;
