99254e7bec647c92453b882a9b9b0d7ecaeab81d
juliajforesti <juliajforesti@gmail.com>
2025-11-14 09:52:11 -0300

    chore: remove deprecated `voip-freeswitch.extension.assign`

--- packages/rest-typings/src/v1/voip-freeswitch/index.ts
+++ packages/rest-typings/src/v1/voip-freeswitch/index.ts
@@ -1,11 +1,9 @@
 import type { FreeSwitchExtension } from '@rocket.chat/core-typings';
 
-import type { VoipFreeSwitchExtensionAssignProps } from './VoipFreeSwitchExtensionAssignProps';
 import type { VoipFreeSwitchExtensionGetDetailsProps } from './VoipFreeSwitchExtensionGetDetailsProps';
 import type { VoipFreeSwitchExtensionGetInfoProps } from './VoipFreeSwitchExtensionGetInfoProps';
 import type { VoipFreeSwitchExtensionListProps } from './VoipFreeSwitchExtensionListProps';
 
-export * from './VoipFreeSwitchExtensionAssignProps';
 export * from './VoipFreeSwitchExtensionGetDetailsProps';
 export * from './VoipFreeSwitchExtensionGetInfoProps';
 export * from './VoipFreeSwitchExtensionListProps';
@@ -16,9 +14,6 @@ export type VoipFreeSwitchEndpoints = {
 	};
 	'/v1/voip-freeswitch.extension.getDetails': {
 		GET: (params: VoipFreeSwitchExtensionGetDetailsProps) => FreeSwitchExtension & { userId?: string; username?: string; name?: string };
-	};
-	'/v1/voip-freeswitch.extension.assign': {
-		POST: (params: VoipFreeSwitchExtensionAssignProps) => void;
 	};
 	'/v1/voip-freeswitch.extension.getRegistrationInfoByUserId': {
 		GET: (params: VoipFreeSwitchExtensionGetInfoProps) => {
