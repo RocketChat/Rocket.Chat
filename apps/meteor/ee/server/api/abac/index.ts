3a16ee88e4914be85c2f540fcf1b61aecaf64377
Kevin  Aleman <kaleman960@gmail.com>
2025-11-17 13:48:37 -0600

    missing removal

--- apps/meteor/ee/server/api/abac/index.ts
+++ apps/meteor/ee/server/api/abac/index.ts
@@ -14,7 +14,6 @@ import {
 	POSTSingleRoomAbacAttributeBodySchema,
 	PUTRoomAbacAttributeValuesBodySchema,
 	POSTAbacUsersSyncBodySchema,
-	POSTAbacUsersSyncResponseSchema,
 	GenericErrorSchema,
 	GETAbacRoomsListQueryValidator,
 	GETAbacRoomsResponseValidator,
