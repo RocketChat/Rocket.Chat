0404bd31fe3bcd16a38fdf31c8fd243506c0b88c
Martin Schoeler <martin.schoeler@rocket.chat>
2025-11-17 14:53:53 -0300

    feat: ABAC Rooms attributes tab (#37408)

---
+++ apps/meteor/client/views/admin/ABAC/RoomAttributesContextualBarWithData.tsx
@@ -0,0 +1,28 @@
+import { useEndpoint } from '@rocket.chat/ui-contexts';
+import { useQuery } from '@tanstack/react-query';
+
+import RoomAttributesContextualBar from './RoomAttributesContextualBar';
+import { ContextualbarSkeletonBody } from '../../../components/Contextualbar';
+import { ABACQueryKeys } from '../../../lib/queryKeys';
+
+type RoomAttributesContextualBarWithDataProps = {
+	id: string;
+	onClose: () => void;
+};
+
+const RoomAttributesContextualBarWithData = ({ id, onClose }: RoomAttributesContextualBarWithDataProps) => {
+	const getAttributes = useEndpoint('GET', '/v1/abac/attributes/:_id', { _id: id });
+	const { data, isLoading, isFetching } = useQuery({
+		queryKey: ABACQueryKeys.roomAttributes.attribute(id),
+		queryFn: () => getAttributes(),
+		staleTime: 0,
+	});
+
+	if (isLoading || isFetching) {
+		return <ContextualbarSkeletonBody />;
+	}
+
+	return <RoomAttributesContextualBar attributeData={data} onClose={onClose} />;
+};
+
+export default RoomAttributesContextualBarWithData;
