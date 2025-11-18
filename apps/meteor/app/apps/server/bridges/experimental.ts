036e677463a8089b39b09854cf4e426a0b67697d
Dnouv <evan.shu.dev@gmail.com>
2025-11-19 00:30:03 +0530

    AppExperimentalBridge to ensure context is maintained in constructor

--- apps/meteor/app/apps/server/bridges/experimental.ts
+++ apps/meteor/app/apps/server/bridges/experimental.ts
@@ -2,8 +2,13 @@ import type { IAppServerOrchestrator } from '@rocket.chat/apps';
 import { ExperimentalBridge } from '@rocket.chat/apps-engine/server/bridges';
 
 export class AppExperimentalBridge extends ExperimentalBridge {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	constructor(private readonly orch: IAppServerOrchestrator) {
 		super();
+
+		this.keepContext();
+	}
+
+	private keepContext(): void {
+		void this.orch;
 	}
 }
