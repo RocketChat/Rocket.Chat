4e6bd516a5eb3dd8ea6d769d414d1ef6dd23901b
Dnouv <evan.shu.dev@gmail.com>
2025-11-19 00:04:07 +0530

    Ensure context is maintained in ExperimentalRead constructor

--- packages/apps-engine/src/server/accessors/ExperimentalRead.ts
+++ packages/apps-engine/src/server/accessors/ExperimentalRead.ts
@@ -3,9 +3,14 @@ import type { ExperimentalBridge } from '../bridges';
 
 export class ExperimentalRead implements IExperimentalRead {
 	constructor(
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		private readonly experimentalBridge: ExperimentalBridge,
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		private readonly appId: string,
-	) {}
+	) {
+		this.keepContext();
+	}
+
+	private keepContext(): void {
+		void this.experimentalBridge;
+		void this.appId;
+	}
 }
