68006b4937d2a3fda85da5341ac1f051b895fc29
Kevin  Aleman <kaleman960@gmail.com>
2025-11-19 14:26:31 -0600

    on setting change hook

--- apps/meteor/ee/server/configuration/abac.ts
+++ apps/meteor/ee/server/configuration/abac.ts
@@ -1,4 +1,26 @@
 import { License } from '@rocket.chat/license';
+import { Users } from '@rocket.chat/models';
+
+import { settings } from '../../../app/settings/server';
+import { LDAPEE } from '../sdk';
+
+function syncAbacAttributes() {
+	// Listen for setting changes
+	// question? does this fire when a setting was disabled on CE and then enabled because of EE upgrade?
+	settings.watch('ABAC_Enabled', async (value) => {
+		console.log('------------------------------------------------------------------------');
+		if (value) {
+			await LDAPEE.syncUsersAbacAttributes(Users.findLDAPUsers());
+		}
+	});
+
+	// Initial sync if enabled after license is applied
+	// This should only happen if the ws downgrades and then upgrades again
+	// Checking if needed
+	// if (settings.get('ABAC_Enabled')) {
+	// 	await LDAPEE.syncAbacAttributes();
+	// }
+}
 
 Meteor.startup(async () => {
 	await License.onLicense('abac', async () => {
@@ -9,5 +31,7 @@ Meteor.startup(async () => {
 		await createPermissions();
 
 		await import('../hooks/abac');
+
+		syncAbacAttributes();
 	});
 });
