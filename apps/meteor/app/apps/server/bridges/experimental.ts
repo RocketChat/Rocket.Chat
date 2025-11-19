bd241fe77a1656e5438ad6fd17146d791e1d3835
Dnouv <evan.shu.dev@gmail.com>
2025-11-19 10:48:02 +0530

    Relax experimental bridge visibility

--- apps/meteor/app/apps/server/bridges/experimental.ts
+++ apps/meteor/app/apps/server/bridges/experimental.ts
@@ -2,13 +2,7 @@ import type { IAppServerOrchestrator } from '@rocket.chat/apps';
 import { ExperimentalBridge } from '@rocket.chat/apps-engine/server/bridges';
 
 export class AppExperimentalBridge extends ExperimentalBridge {
-	constructor(private readonly orch: IAppServerOrchestrator) {
+	constructor(protected readonly orch: IAppServerOrchestrator) {
 		super();
-
-		this.keepContext();
-	}
-
-	private keepContext(): void {
-		void this.orch;
 	}
 }
--- packages/apps-engine/src/server/accessors/ExperimentalRead.ts
+++ packages/apps-engine/src/server/accessors/ExperimentalRead.ts
@@ -3,14 +3,7 @@ import type { ExperimentalBridge } from '../bridges';
 
 export class ExperimentalRead implements IExperimentalRead {
 	constructor(
-		private readonly experimentalBridge: ExperimentalBridge,
-		private readonly appId: string,
-	) {
-		this.keepContext();
-	}
-
-	private keepContext(): void {
-		void this.experimentalBridge;
-		void this.appId;
-	}
+		protected readonly experimentalBridge: ExperimentalBridge,
+		protected readonly appId: string,
+	) {}
 }
