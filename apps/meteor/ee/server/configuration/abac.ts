5c08901486f5aa53f63532bd7f77761df89ed6b5
Kevin Aleman <kaleman960@gmail.com>
2025-11-20 16:05:45 -0600

    chore: Run ABAC LDAP sync on license/setting changes (#37562)

--- apps/meteor/ee/server/configuration/abac.ts
+++ apps/meteor/ee/server/configuration/abac.ts
@@ -1,13 +1,29 @@
 import { License } from '@rocket.chat/license';
+import { Users } from '@rocket.chat/models';
+
+import { settings } from '../../../app/settings/server';
+import { LDAPEE } from '../sdk';
 
 Meteor.startup(async () => {
-	await License.onLicense('abac', async () => {
-		const { addSettings } = await import('../settings/abac');
-		const { createPermissions } = await import('../lib/abac');
+	let stopWatcher: () => void;
+	License.onToggledFeature('abac', {
+		up: async () => {
+			const { addSettings } = await import('../settings/abac');
+			const { createPermissions } = await import('../lib/abac');
 
-		await addSettings();
-		await createPermissions();
+			await addSettings();
+			await createPermissions();
 
-		await import('../hooks/abac');
+			await import('../hooks/abac');
+
+			stopWatcher = settings.watch('ABAC_Enabled', async (value) => {
+				if (value) {
+					await LDAPEE.syncUsersAbacAttributes(Users.findLDAPUsers());
+				}
+			});
+		},
+		down: () => {
+			stopWatcher?.();
+		},
 	});
 });
