aaebfd883354b85699311ec349e1daa9b18ec9e6
Kevin  Aleman <kaleman960@gmail.com>
2025-11-20 15:12:10 -0600

    ooops2

--- apps/meteor/ee/server/configuration/abac.ts
+++ apps/meteor/ee/server/configuration/abac.ts
@@ -4,25 +4,26 @@ import { Users } from '@rocket.chat/models';
 import { settings } from '../../../app/settings/server';
 import { LDAPEE } from '../sdk';
 
-function syncAbacAttributes() {
-	// Listen for setting changes
-	settings.watch('ABAC_Enabled', async (value) => {
-		if (value) {
-			await LDAPEE.syncUsersAbacAttributes(Users.findLDAPUsers());
-		}
-	});
-}
-
 Meteor.startup(async () => {
-	await License.onLicense('abac', async () => {
-		const { addSettings } = await import('../settings/abac');
-		const { createPermissions } = await import('../lib/abac');
+	let stopWatcher: () => void;
+	License.onToggledFeature('abac', {
+		up: async () => {
+			const { addSettings } = await import('../settings/abac');
+			const { createPermissions } = await import('../lib/abac');
 
-		await addSettings();
-		await createPermissions();
+			await addSettings();
+			await createPermissions();
 
-		await import('../hooks/abac');
+			await import('../hooks/abac');
 
-		syncAbacAttributes();
+			stopWatcher = settings.watch('ABAC_Enabled', async (value) => {
+				if (value) {
+					await LDAPEE.syncUsersAbacAttributes(Users.findLDAPUsers());
+				}
+			});
+		},
+		down: () => {
+			stopWatcher?.();
+		},
 	});
 });
