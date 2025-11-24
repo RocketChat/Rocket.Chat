901c83009f5fbee5e8f9c7a0d067ff523cbe6307
Douglas Gubert <douglas.gubert@gmail.com>
2025-11-24 11:08:30 -0300

    chore: start test conversion from Alsatian to NodeJS

---
+++ packages/apps-engine/tests/node-tests/server/accessors/ConfigurationModify.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, beforeEach } from 'node:test';
+import * as assert from 'node:assert';
+import type { ISchedulerModify, IServerSettingsModify, ISlashCommandsModify } from '../../../../src/definition/accessors';
+import { ConfigurationModify } from '../../../../src/server/accessors';
+
+describe('ConfigurationModify', () => {
+	let ssm: IServerSettingsModify;
+	let scm: ISlashCommandsModify;
+	let scheduler: ISchedulerModify;
+
+	beforeEach(() => {
+		ssm = {} as IServerSettingsModify;
+		scm = {} as ISlashCommandsModify;
+		scheduler = {} as ISchedulerModify;
+	});
+
+	it('useConfigurationModify', () => {
+		assert.doesNotThrow(() => new ConfigurationModify(ssm, scm, scheduler));
+
+		const sm = new ConfigurationModify(ssm, scm, scheduler);
+		assert.ok(sm.serverSettings);
+		assert.ok(sm.slashCommands);
+	});
+});
