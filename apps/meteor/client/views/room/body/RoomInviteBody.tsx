459248016612a5988ffc533b562d0cb03694b28b
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-11-25 18:12:09 -0300

    feat: RoomInviteBody

---
+++ apps/meteor/client/views/room/body/RoomInviteBody.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Chip, States, StatesActions, StatesIcon, StatesLink, StatesSubtitle, StatesTitle } from '@rocket.chat/fuselage';
+import { UserAvatar } from '@rocket.chat/ui-avatar';
+import { useTranslation } from 'react-i18next';
+
+type RoomInviteBodyProps = {
+	isLoading?: boolean;
+	inviterUsername: string;
+	onAccept: () => void;
+	onReject: () => void;
+};
+
+const RoomInviteBody = ({ inviterUsername, isLoading, onAccept, onReject }: RoomInviteBodyProps) => {
+	const { t } = useTranslation();
+
+	return (
+		<Box m='auto'>
+			<States>
+				<StatesIcon name='mail' />
+				<StatesTitle>{t('Message_request')}</StatesTitle>
+				<StatesSubtitle>
+					<Box mbe={8}>{t('You_have_been_invited_to_have_a_conversation_with')}</Box>
+					<Chip>
+						<UserAvatar username={inviterUsername} size='x16' /> {inviterUsername}
+					</Chip>
+				</StatesSubtitle>
+				<StatesActions>
+					<Button secondary danger loading={isLoading} onClick={onReject}>
+						{t('Reject')}
+					</Button>
+					<Button loading={isLoading} onClick={onAccept}>
+						{t('Accept')}
+					</Button>
+				</StatesActions>
+				<StatesLink href='#'>{t('Learn_more_about_Federation')}</StatesLink>
+			</States>
+		</Box>
+	);
+};
+
+export default RoomInviteBody;
