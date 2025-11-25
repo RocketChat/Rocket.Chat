0a287a861170ce3beb5196b98ffbc0059cd162b9
Ricardo Garim <rswarovsky@gmail.com>
2025-11-25 18:38:10 -0300

    fix: revert addUsersToRoom

--- apps/meteor/app/lib/server/methods/addUsersToRoom.ts
+++ apps/meteor/app/lib/server/methods/addUsersToRoom.ts
@@ -1,5 +1,5 @@
-import { api, FederationMatrix } from '@rocket.chat/core-services';
-import type { IUser, SubscriptionStatus } from '@rocket.chat/core-typings';
+import { api } from '@rocket.chat/core-services';
+import type { IUser } from '@rocket.chat/core-typings';
 import { isRoomNativeFederated } from '@rocket.chat/core-typings';
 import type { ServerMethods } from '@rocket.chat/ddp-client';
 import { validateFederatedUsername } from '@rocket.chat/federation-matrix';
@@ -107,30 +107,19 @@ export const addUsersToRoomMethod = async (userId: string, data: { rid: string; 
 
 			const subscription = await Subscriptions.findOneByRoomIdAndUserId(data.rid, newUser._id);
 			if (!subscription) {
-				let inviteOptions: { status?: SubscriptionStatus; inviterUsername?: string } = {};
-
-				if (isRoomNativeFederated(room) && user && newUser.username) {
-					await FederationMatrix.inviteUsersToRoom(room, [newUser.username], user);
-					inviteOptions = {
-						status: 'INVITED',
-						inviterUsername: user.username,
-					};
+				await addUserToRoom(data.rid, newUser, user);
+			} else {
+				if (!newUser.username) {
+					return;
 				}
-
-				return addUserToRoom(data.rid, newUser, user, inviteOptions);
+				void api.broadcast('notify.ephemeralMessage', userId, data.rid, {
+					msg: i18n.t('Username_is_already_in_here', {
+						postProcess: 'sprintf',
+						sprintf: [newUser.username],
+						lng: user?.language,
+					}),
+				});
 			}
-
-			if (!newUser.username) {
-				return;
-			}
-
-			void api.broadcast('notify.ephemeralMessage', userId, data.rid, {
-				msg: i18n.t('Username_is_already_in_here', {
-					postProcess: 'sprintf',
-					sprintf: [newUser.username],
-					lng: user?.language,
-				}),
-			});
 		}),
 	);
 
