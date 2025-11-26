73f0467487307b5454325f892a5f8515bd19b034
gabriellsh <henriques.gabriell@gmail.com>
2025-11-26 14:29:13 -0300

    refactor: move CustomScrollbars to ui-client package
    - Move CustomScrollbars, VirtualizedScrollbars, and BaseScrollbars to packages/ui-client/src/components/CustomScrollbars
    - Add overlayscrollbars and overlayscrollbars-react as peerDependencies and devDependencies
    - Export CustomScrollbars from ui-client components index
    - Build package successfully

---
+++ packages/ui-client/src/components/CustomScrollbars/index.ts
@@ -0,0 +1,6 @@
+import { OverlayScrollbars } from 'overlayscrollbars';
+
+export { OverlayScrollbars };
+export { default as CustomScrollbars } from './CustomScrollbars';
+export { default as VirtualizedScrollbars } from './VirtualizedScrollbars';
+
