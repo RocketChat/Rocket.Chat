9c1a2deb0010e89b0380082a0ec94ac0f089198d
Kevin Aleman <kaleman960@gmail.com>
2025-11-26 08:03:02 -0600

    chore: ABAC statistics (#37606)

--- packages/model-typings/src/models/IAbacAttributesModel.ts
+++ packages/model-typings/src/models/IAbacAttributesModel.ts
@@ -6,4 +6,5 @@ import type { IBaseModel } from './IBaseModel';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IAbacAttributesModel extends IBaseModel<IAbacAttribute> {
 	findOneByKey(key: string, options?: FindOptions<IAbacAttribute>): Promise<IAbacAttribute | null>;
+	countTotalValues(): Promise<number>;
 }
