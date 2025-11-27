4590230e7a1f789e322774ba03f618a01f74be42
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-11-27 18:33:27 -0300

    feat: added invitation badge to the sidebar

--- apps/meteor/client/sidebar/badges/SidebarItemBadges.tsx
+++ apps/meteor/client/sidebar/badges/SidebarItemBadges.tsx
@@ -1,7 +1,8 @@
-import { isOmnichannelRoom } from '@rocket.chat/core-typings';
+import { isOmnichannelRoom, isInviteSubscription } from '@rocket.chat/core-typings';
 import { Margins } from '@rocket.chat/fuselage';
 import type { SubscriptionWithRoom } from '@rocket.chat/ui-contexts';
 
+import InvitationBadge from './InvitationBadge';
 import UnreadBadge from './UnreadBadge';
 import OmnichannelBadges from '../../views/omnichannel/components/OmnichannelBadges';
 import { useUnreadDisplay } from '../hooks/useUnreadDisplay';
@@ -18,6 +19,7 @@ const SidebarItemBadges = ({ room, roomTitle }: SidebarItemBadgesProps) => {
 		<Margins inlineStart={8}>
 			{showUnread && <UnreadBadge title={unreadTitle} roomTitle={roomTitle} variant={unreadVariant} total={unreadCount.total} />}
 			{isOmnichannelRoom(room) && <OmnichannelBadges room={room} />}
+			{isInviteSubscription(room) && <InvitationBadge />}
 		</Margins>
 	);
 };
