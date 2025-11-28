7491b30a54c081cc163d5d7265a4ab5c9b177293
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-11-27 21:09:38 -0300

    refactor(sidebar-v1): SidebarItemBadges

---
+++ apps/meteor/client/sidebar/badges/SidebarItemBadges.tsx
@@ -0,0 +1,26 @@
+import type { IRoom, ISubscription } from '@rocket.chat/core-typings';
+import { isOmnichannelRoom } from '@rocket.chat/core-typings';
+import { Margins } from '@rocket.chat/fuselage';
+
+import OmnichannelBadges from './OmnichannelBadges';
+import UnreadBadge from './UnreadBadge';
+import { useUnreadDisplay } from '../hooks/useUnreadDisplay';
+
+type SidebarItemBadgesProps = {
+	room: ISubscription & IRoom;
+	roomTitle?: string;
+};
+
+const SidebarItemBadges = ({ room, roomTitle }: SidebarItemBadgesProps) => {
+	const { unreadCount, unreadTitle, unreadVariant, showUnread } = useUnreadDisplay(room);
+
+	return (
+		<Margins inlineStart={8}>
+			{showUnread && <UnreadBadge title={unreadTitle} roomTitle={roomTitle} variant={unreadVariant} total={unreadCount.total} />}
+
+			{isOmnichannelRoom(room) && <OmnichannelBadges room={room} />}
+		</Margins>
+	);
+};
+
+export default SidebarItemBadges;
