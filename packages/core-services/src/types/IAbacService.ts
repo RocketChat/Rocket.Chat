84e7662fb057f1f0c58a27a11d953a9fd8798042
Kevin  Aleman <kaleman960@gmail.com>
2025-11-28 08:13:52 -0600

    fix type

--- packages/core-services/src/types/IAbacService.ts
+++ packages/core-services/src/types/IAbacService.ts
@@ -11,7 +11,7 @@ import type {
 export type AbacActor = Pick<IUser, '_id' | 'username' | 'name'>;
 
 export interface IAbacService {
-	addAbacAttribute(attribute: IAbacAttributeDefinition, actor?: AbacActor): Promise<void>;
+	addAbacAttribute(attribute: IAbacAttributeDefinition, actor: AbacActor | undefined): Promise<void>;
 	listAbacAttributes(
 		filters?: {
 			key?: string;
@@ -30,21 +30,24 @@ export interface IAbacService {
 		},
 		actor?: AbacActor,
 	): Promise<{ rooms: IRoom[]; offset: number; count: number; total: number }>;
-	updateAbacAttributeById(_id: string, update: { key?: string; values?: string[] }, actor?: AbacActor): Promise<void>;
-	deleteAbacAttributeById(_id: string, actor?: AbacActor): Promise<void>;
+	updateAbacAttributeById(_id: string, update: { key?: string; values?: string[] }, actor: AbacActor | undefined): Promise<void>;
+	deleteAbacAttributeById(_id: string, actor: AbacActor | undefined): Promise<void>;
 	// Usage represents if the attribute values are in use or not. If no values are in use, the attribute is not in use.
-	getAbacAttributeById(_id: string, actor?: AbacActor): Promise<{ key: string; values: string[]; usage: Record<string, boolean> }>;
+	getAbacAttributeById(
+		_id: string,
+		actor: AbacActor | undefined,
+	): Promise<{ key: string; values: string[]; usage: Record<string, boolean> }>;
 	isAbacAttributeInUseByKey(key: string): Promise<boolean>;
-	setRoomAbacAttributes(rid: string, attributes: Record<string, string[]>, actor?: AbacActor): Promise<void>;
-	removeRoomAbacAttribute(rid: string, key: string, actor?: AbacActor): Promise<void>;
-	addRoomAbacAttributeByKey(rid: string, key: string, values: string[], actor?: AbacActor): Promise<void>;
-	replaceRoomAbacAttributeByKey(rid: string, key: string, values: string[], actor?: AbacActor): Promise<void>;
-	checkUsernamesMatchAttributes(usernames: string[], attributes: IAbacAttributeDefinition[], actor?: AbacActor): Promise<void>;
+	setRoomAbacAttributes(rid: string, attributes: Record<string, string[]>, actor: AbacActor | undefined): Promise<void>;
+	removeRoomAbacAttribute(rid: string, key: string, actor: AbacActor | undefined): Promise<void>;
+	addRoomAbacAttributeByKey(rid: string, key: string, values: string[], actor: AbacActor | undefined): Promise<void>;
+	replaceRoomAbacAttributeByKey(rid: string, key: string, values: string[], actor: AbacActor | undefined): Promise<void>;
+	checkUsernamesMatchAttributes(usernames: string[], attributes: IAbacAttributeDefinition[], actor: AbacActor | undefined): Promise<void>;
 	canAccessObject(
 		room: Pick<IRoom, '_id' | 't' | 'teamId' | 'prid' | 'abacAttributes'>,
 		user: Pick<IUser, '_id'>,
 		action: AbacAccessOperation,
 		objectType: AbacObjectType,
 	): Promise<boolean>;
-	addSubjectAttributes(user: IUser, ldapUser: ILDAPEntry, map: Record<string, string>, actor?: AbacActor): Promise<void>;
+	addSubjectAttributes(user: IUser, ldapUser: ILDAPEntry, map: Record<string, string>, actor: AbacActor | undefined): Promise<void>;
 }
