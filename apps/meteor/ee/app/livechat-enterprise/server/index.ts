4e40205995ecc3b23571558a0a60b97b30dc8484
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-12-01 19:00:32 -0300

    chore: removes deprecated method addMonitor

--- apps/meteor/ee/app/livechat-enterprise/server/index.ts
+++ apps/meteor/ee/app/livechat-enterprise/server/index.ts
@@ -2,7 +2,6 @@ import { License } from '@rocket.chat/license';
 import { patchOmniCore } from '@rocket.chat/omni-core-ee';
 import { Meteor } from 'meteor/meteor';
 
-import './methods/addMonitor';
 import './methods/removeMonitor';
 import './methods/saveTag';
 import './methods/removeBusinessHour';
