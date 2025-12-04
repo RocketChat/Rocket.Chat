2355bb7d6eeaad76ae2f5620ae5150e91c6c717f
Pierre Lehnen <pierre.lehnen@rocket.chat>
2025-12-04 18:25:57 -0300

    chore: add endpoints for retrieving call history data

--- packages/model-typings/src/models/ICallHistoryModel.ts
+++ packages/model-typings/src/models/ICallHistoryModel.ts
@@ -1,5 +1,18 @@
 import type { CallHistoryItem } from '@rocket.chat/core-typings';
+import type { FindOptions } from 'mongodb';
 
 import type { IBaseModel } from './IBaseModel';
 
-export type ICallHistoryModel = IBaseModel<CallHistoryItem>;
+export interface ICallHistoryModel extends IBaseModel<CallHistoryItem> {
+	findOneByIdAndUid(
+		_id: CallHistoryItem['_id'],
+		uid: CallHistoryItem['uid'],
+		options?: FindOptions<CallHistoryItem>,
+	): Promise<CallHistoryItem | null>;
+
+	findOneByCallIdAndUid(
+		callId: CallHistoryItem['callId'],
+		uid: CallHistoryItem['uid'],
+		options?: FindOptions<CallHistoryItem>,
+	): Promise<CallHistoryItem | null>;
+}
