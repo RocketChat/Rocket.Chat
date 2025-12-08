c3d4a20b10c22ff37e1220188d95f08e899ee994
gabriellsh <henriques.gabriell@gmail.com>
2025-12-08 15:13:05 -0300

    Add `framed` optional property for IconElement

--- packages/fuselage-ui-kit/src/elements/IconElement.tsx
+++ packages/fuselage-ui-kit/src/elements/IconElement.tsx
@@ -1,24 +1,47 @@
-import { Icon } from '@rocket.chat/fuselage';
+import { Icon, FramedIcon } from '@rocket.chat/fuselage';
 import type * as UiKit from '@rocket.chat/ui-kit';
-import type { ReactElement } from 'react';
+import type { ComponentProps, ReactElement } from 'react';
 
 import type { BlockProps } from '../utils/BlockProps';
 
-type IconElementProps = BlockProps<UiKit.IconElement>;
+type IconElementProps = BlockProps<UiKit.FrameableIconElement>;
 
-const getVariantColor = (variant: UiKit.IconElement['variant']): string => {
+const getVariantColor = (variant: UiKit.FrameableIconElement['variant']): string => {
 	switch (variant) {
-		case 'default':
-			return 'default';
 		case 'danger':
 			return 'danger';
 		case 'secondary':
 			return 'secondary-info';
+		case 'warning':
+			return 'status-font-on-warning';
+		case 'default':
+		default:
+			return 'default';
 	}
 };
 
-const IconElement = ({ block }: IconElementProps): ReactElement => (
-	<Icon size={20} name={block.icon} color={getVariantColor(block.variant)} />
-);
+const getFramedIconProps = (
+	variant: UiKit.FrameableIconElement['variant'],
+): Pick<ComponentProps<typeof FramedIcon>, 'warning' | 'danger' | 'neutral'> => {
+	switch (variant) {
+		case 'danger':
+			return { danger: true };
+		case 'warning':
+			return { warning: true };
+		case 'default':
+		case 'secondary':
+		default:
+			return { neutral: true };
+	}
+};
+
+const IconElement = ({ block }: IconElementProps): ReactElement => {
+	const { icon, variant, framed } = block;
+	console.log('block', block);
+	if (framed) {
+		return <FramedIcon size={20} icon={icon} {...getFramedIconProps(variant)} />;
+	}
+	return <Icon size={20} name={icon} color={getVariantColor(variant)} />;
+};
 
 export default IconElement;
