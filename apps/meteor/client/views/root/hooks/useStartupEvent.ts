157d407093d7c48552b88f6cfa5e63c636abdcdd
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2025-12-08 12:27:03 -0300

    fix: add scope to `fireStartupEvent`
    Co-authored-by: gabriellsh <40830821+gabriellsh@users.noreply.github.com>

--- apps/meteor/client/views/root/hooks/useStartupEvent.ts
+++ apps/meteor/client/views/root/hooks/useStartupEvent.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useFireGlobalEvent } from '../../../hooks/useFireGlobalEvent';
 
 export const useStartupEvent = () => {
-	const { mutate: fireStartupEvent } = useFireGlobalEvent('startup');
+	const { mutate: fireStartupEvent } = useFireGlobalEvent('startup', 'global');
 
 	useEffect(() => {
 		fireStartupEvent(true);
