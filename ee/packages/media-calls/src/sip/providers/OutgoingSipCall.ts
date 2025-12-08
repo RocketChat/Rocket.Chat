2fa527bf19160ffbb52ad8a634cc82397af7c593
sid2487 <sid454804@gmail.com>
2025-12-08 11:28:07 +0530

    chore(spelling): fix typos in comments and log messages

--- ee/packages/media-calls/src/sip/providers/OutgoingSipCall.ts
+++ ee/packages/media-calls/src/sip/providers/OutgoingSipCall.ts
@@ -225,7 +225,7 @@ export class OutgoingSipCall extends BaseSipCall {
 
 				logger.debug({ msg: 'modify', method: 'OutgoingSipCall.createDialog', req: this.session.stripDrachtioServerDetails(req) });
 			} catch (error) {
-				logger.error({ msg: 'An unexpected error occured while processing a modify event on an OutgoingSipCall dialog', error });
+				logger.error({ msg: 'An unexpected error occurred while processing a modify event on an OutgoingSipCall dialog', error });
 
 				try {
 					res.send(SipErrorCodes.INTERNAL_SERVER_ERROR);
