cb5b193bfd2a25f93e96aed63219c86bca90f7f5
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2025-12-09 09:57:09 -0300

    test: adjusted tests

--- apps/meteor/client/sidebar/badges/SidebarItemBadges.spec.tsx
+++ apps/meteor/client/sidebar/badges/SidebarItemBadges.spec.tsx
@@ -52,9 +52,18 @@ describe('SidebarItemBadges', () => {
 	});
 
 	it('should render InvitationBadge when subscription has status INVITED', () => {
-		render(<SidebarItemBadges room={createRoomWithSubscription({ status: 'INVITED', ts: new Date('2025-01-01T00:00:00.000Z') })} />, {
-			wrapper: appRoot,
-		});
+		render(
+			<SidebarItemBadges
+				room={createRoomWithSubscription({
+					status: 'INVITED',
+					inviter: { name: 'Rocket Cat', username: 'rocket.cat', _id: 'rocket.cat' },
+					ts: new Date('2025-01-01T00:00:00.000Z'),
+				})}
+			/>,
+			{
+				wrapper: appRoot,
+			},
+		);
 
 		expect(screen.getByRole('status', { name: 'Invited January 1, 2025' })).toBeInTheDocument();
 	});
