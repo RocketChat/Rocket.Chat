5a479fceb1c1abbb6cd4ccb8126b6da8b73a28e5
Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>
2025-12-10 18:27:19 +0530

    chore!: Remove `VoIP_TeamCollab_Enabled` setting  (#36941)
    Co-authored-by: Pierre Lehnen <pierre.lehnen@rocket.chat>

--- apps/meteor/client/views/admin/users/useShowVoipExtension.tsx
+++ apps/meteor/client/views/admin/users/useShowVoipExtension.tsx
@@ -1,7 +1,7 @@
 import { useSetting } from '@rocket.chat/ui-contexts';
 
 export const useShowVoipExtension = () => {
-	const isVoipSettingEnabled = useSetting('VoIP_TeamCollab_Enabled', false);
+	const isVoipSettingEnabled = useSetting('VoIP_TeamCollab_SIP_Integration_Enabled', false);
 
 	return isVoipSettingEnabled;
 };
