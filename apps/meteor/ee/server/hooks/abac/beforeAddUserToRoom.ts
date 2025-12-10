b81162865341eda1c27afce0b86b6daf1c626670
Kevin Aleman <kaleman960@gmail.com>
2025-12-10 08:02:17 -0600

    fix: Prevent invite when license/setting is disabled and room is abac managed (#37756)

--- apps/meteor/ee/server/hooks/abac/beforeAddUserToRoom.ts
+++ apps/meteor/ee/server/hooks/abac/beforeAddUserToRoom.ts
@@ -8,14 +8,14 @@ beforeAddUserToRoom.patch(async (prev, users, room, actor) => {
 	await prev(users, room, actor);
 
 	const validUsers = users.filter(Boolean);
-	if (
-		!room?.abacAttributes?.length ||
-		!validUsers.length ||
-		!License.hasModule('abac') ||
-		room.t !== 'p' ||
-		!settings.get('ABAC_Enabled')
-	) {
+	// No need to check ABAC when theres no users or when room is not private or when room is not ABAC managed
+	if (!validUsers.length || room.t !== 'p' || !room?.abacAttributes?.length) {
 		return;
+	}
+
+	// Throw error (prevent add) if ABAC is disabled (setting, license) but room is ABAC managed
+	if (!settings.get('ABAC_Enabled') || !License.hasModule('abac')) {
+		throw new Error('error-room-is-abac-managed');
 	}
 
 	await Abac.checkUsernamesMatchAttributes(validUsers as string[], room.abacAttributes);
