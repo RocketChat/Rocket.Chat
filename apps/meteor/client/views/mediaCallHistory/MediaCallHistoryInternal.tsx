eb0e61255a113170bf717baed9d17e8dfb5fa41f
gabriellsh <henriques.gabriell@gmail.com>
2025-12-12 17:08:46 -0300

    Split code for external/internal items

---
+++ apps/meteor/client/views/mediaCallHistory/MediaCallHistoryInternal.tsx
@@ -0,0 +1,52 @@
+import type { CallHistoryItem, IInternalMediaCallHistoryItem, IMediaCall, Serialized } from '@rocket.chat/core-typings';
+import { CallHistoryContextualBar } from '@rocket.chat/ui-voip';
+import { useMemo } from 'react';
+
+import { useMediaCallInternalHistoryActions } from './useMediaCallInternalHistoryActions';
+
+type InternalCallEndpointData = Serialized<{
+	item: IInternalMediaCallHistoryItem;
+	call: IMediaCall;
+}>;
+
+type MediaCallHistoryInternalProps = {
+	data: InternalCallEndpointData;
+	onClose: () => void;
+};
+
+export const isInternalCallHistoryItem = (data: {
+	item: Serialized<CallHistoryItem>;
+	call?: Serialized<IMediaCall>;
+}): data is InternalCallEndpointData => {
+	return 'external' in data.item && !data.item.external;
+};
+
+const getContact = (item: InternalCallEndpointData['item'], call: InternalCallEndpointData['call']) => {
+	const { caller, callee } = call ?? {};
+	const contact = caller?.id === item.contactId ? caller : callee;
+	const { id, sipExtension, username, ...rest } = contact;
+	return {
+		...rest,
+		_id: id,
+		username: username ?? '',
+		voiceCallExtension: sipExtension,
+	};
+};
+
+const MediaCallHistoryInternal = ({ data, onClose }: MediaCallHistoryInternalProps) => {
+	const contact = useMemo(() => getContact(data.item, data.call), [data]);
+	const historyData = useMemo(() => {
+		return {
+			callId: data.call._id,
+			direction: data.item.direction,
+			duration: data.item.duration,
+			startedAt: new Date(data.item.ts),
+			state: data.item.state,
+		};
+	}, [data]);
+	const actions = useMediaCallInternalHistoryActions(contact, data.item.messageId);
+
+	return <CallHistoryContextualBar onClose={onClose} actions={actions} contact={contact} data={historyData} />;
+};
+
+export default MediaCallHistoryInternal;
