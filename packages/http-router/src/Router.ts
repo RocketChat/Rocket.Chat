800d3056349e7b139040bae10270d75756bdcbf2
Jaysinh Patankar <patankararyan7@gmail.com>
2025-12-14 23:52:52 +0530

    fix(http-router): resolve Hono method typing for Node 22 (Closes #37801)

--- packages/http-router/src/Router.ts
+++ packages/http-router/src/Router.ts
@@ -194,7 +194,7 @@ export class Router<
 		const [middlewares, action] = splitArray<MiddlewareHandler, TActionCallback>(actions);
 		const convertedAction = this.convertActionToHandler(action);
 
-		this.innerRouter[method.toLowerCase() as Lowercase<Method>](`/${subpath}`.replace('//', '/'), ...middlewares, async (c) => {
+		(this.innerRouter[method.toLowerCase() as keyof typeof this.innerRouter] as Function)(`/${subpath}`.replace('//', '/'), ...middlewares, async (c: any) => {
 			const { req, res } = c;
 
 			const queryParams = this.parseQueryParams(req);
