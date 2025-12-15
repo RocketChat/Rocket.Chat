1e91c970b08f3bb654ef1741e6783d81f91fd0fb
Tasso Evangelista <tasso.evangelista@rocket.chat>
2025-12-15 13:23:41 -0300

    chore(message-parser): Review build configuration (#37737)

--- packages/message-parser/jest.config.ts
+++ packages/message-parser/jest.config.ts
@@ -4,9 +4,9 @@ import server from '@rocket.chat/jest-presets/server';
 import type { Config } from 'jest';
 
 export default {
-  preset: server.preset,
-  transform: {
-    '\\.pegjs$': resolve(__dirname, './loaders/pegtransform.js'),
-  },
-  moduleFileExtensions: ['js', 'ts', 'pegjs'],
+	preset: server.preset,
+	transform: {
+		'\\.pegjs$': resolve(__dirname, './loaders/pegtransform.js'),
+	},
+	moduleFileExtensions: ['js', 'ts', 'pegjs'],
 } satisfies Config;
--- packages/message-parser/src/index.ts
+++ packages/message-parser/src/index.ts
@@ -6,21 +6,20 @@ export * from './definitions';
 export { isNodeOfType } from './guards';
 
 export type Options = {
-  colors?: boolean;
-  emoticons?: boolean;
-  katex?: {
-    dollarSyntax?: boolean;
-    parenthesisSyntax?: boolean;
-  };
-  customDomains?: string[];
+	colors?: boolean;
+	emoticons?: boolean;
+	katex?: {
+		dollarSyntax?: boolean;
+		parenthesisSyntax?: boolean;
+	};
+	customDomains?: string[];
 };
 
-export const parse = (input: string, options?: Options): Root =>
-  grammar.parse(input, options);
+export const parse = (input: string, options?: Options): Root => grammar.parse(input, options);
 
 export {
-  /** @deprecated */
-  parse as parser,
-  /** @deprecated */
-  Root as MarkdownAST,
+	/** @deprecated */
+	parse as parser,
+	/** @deprecated */
+	Root as MarkdownAST,
 };
