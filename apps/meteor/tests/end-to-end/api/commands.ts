8ff0b231dc719ccba42c54dc9d0ea022e993f35a
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-12-15 10:11:39 -0300

    test: Add end-to-end tests for "leave" command in direct message rooms

--- apps/meteor/tests/end-to-end/api/commands.ts
+++ apps/meteor/tests/end-to-end/api/commands.ts
@@ -556,4 +556,41 @@ describe('[Commands]', () => {
 				.end(done);
 		});
 	});
+	describe('Command "leave"', function () {
+		let directMessageRoom: IRoom;
+		let user1: TestUser<IUser>;
+		let user2: TestUser<IUser>;
+		let user1Credentials: Credentials;
+		this.beforeAll(async () => {
+			user1 = await createUser();
+			user2 = await createUser();
+
+			[user1Credentials] = await Promise.all([login(user1.username, password)]);
+		});
+
+		this.beforeAll(async () => {
+			const [response1] = await Promise.all([
+				createRoom({ type: 'd', name: `room1-${Date.now()}.${Random.id()}`, username: user1.username }),
+			]);
+			directMessageRoom = response1.body.room;
+		});
+
+		this.afterAll(async () => {
+			await Promise.all([deleteRoom({ type: 'd', roomId: directMessageRoom._id })]);
+			await Promise.all([deleteUser(user1), deleteUser(user2)]);
+		});
+
+		it('should fail when trying to leave a direct message room', (done) => {
+			void request
+				.post(api('commands.run'))
+				.set(user1Credentials)
+				.send({ command: 'leave', roomId: directMessageRoom._id })
+				.expect(400)
+				.expect((res) => {
+					expect(res.body).to.have.property('success', false);
+					expect(res.body).to.have.property('error').that.is.a('string');
+				})
+				.end(done);
+		});
+	});
 });
