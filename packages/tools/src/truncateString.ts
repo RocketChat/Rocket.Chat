776e2f40b42cf8cc12f90fa74b7b83bd16eb3c4c
Lucas Pelegrino <lucas.pelegrino@icloud.com>
2025-12-18 16:58:34 -0300

    chore: adds improvements to truncateString

--- packages/tools/src/truncateString.ts
+++ packages/tools/src/truncateString.ts
@@ -1,8 +1,22 @@
+/**
+ * Truncates a string to a specified maximum length, optionally adding ellipses.
+ * @param str
+ * @param maxLength
+ * @param shouldAddEllipses
+ * @return {string}
+ */
 export function truncateString(str: string, maxLength: number, shouldAddEllipses = true): string {
 	const ellipsis = '...';
+	if (str.length <= maxLength) {
+		return str;
+	}
 
 	if (shouldAddEllipses && str.length > maxLength) {
-		return `${str.slice(0, maxLength - 3)}${ellipsis}`;
+		if (maxLength <= ellipsis.length) {
+			return str.slice(0, maxLength);
+		}
+
+		return `${str.slice(0, maxLength - ellipsis.length)}${ellipsis}`;
 	}
 
 	return str.slice(0, maxLength);
