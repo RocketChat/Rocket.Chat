b3b6726a31615991e94bba3eddd5a16f384cf139
Abhinav Kumar <abhinav@avitechlab.com>
2025-12-19 10:01:10 +0530

    fix: Global Search "Jump to message" failing due to incorrect URL generation (#37717)
    Co-authored-by: Douglas Fabris <27704687+dougfabris@users.noreply.github.com>

--- apps/meteor/client/lib/utils/legacyJumpToMessage.ts
+++ apps/meteor/client/lib/utils/legacyJumpToMessage.ts
@@ -4,7 +4,6 @@ import { isThreadMessage } from '@rocket.chat/core-typings';
 import { goToRoomById } from './goToRoomById';
 import { RoomHistoryManager } from '../../../app/ui-utils/client';
 import { router } from '../../providers/RouterProvider';
-import { Rooms } from '../../stores';
 import { RoomManager } from '../RoomManager';
 
 /** @deprecated */
@@ -15,24 +14,12 @@ export const legacyJumpToMessage = async (message: IMessage) => {
 		if (tab === 'thread' && (context === message.tmid || context === message._id)) {
 			return;
 		}
-		router.navigate(
-			{
-				name: router.getRouteName()!,
-				params: {
-					tab: 'thread',
-					context: message.tmid || message._id,
-					rid: message.rid,
-					name: Rooms.state.get(message.rid)?.name ?? '',
-				},
-				search: {
-					...router.getSearchParameters(),
-					msg: message._id,
-				},
-			},
-			{ replace: false },
-		);
-		await RoomHistoryManager.getSurroundingMessages(message);
 
+		await goToRoomById(message.rid, {
+			routeParamsOverrides: { tab: 'thread', context: message.tmid || message._id },
+			replace: RoomManager.opened === message.rid,
+		});
+		await RoomHistoryManager.getSurroundingMessages(message);
 		return;
 	}
 
