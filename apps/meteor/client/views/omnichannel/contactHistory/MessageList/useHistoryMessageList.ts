493cfc4aad0131827d7d0b0c839fb25297eb46e4
Tasso <tasso.evangelista@rocket.chat>
2025-12-26 13:42:08 -0300

    fix: handle item count inconsistencies

--- apps/meteor/client/views/omnichannel/contactHistory/MessageList/useHistoryMessageList.ts
+++ apps/meteor/client/views/omnichannel/contactHistory/MessageList/useHistoryMessageList.ts
@@ -12,12 +12,6 @@ type HistoryMessageListOptions = {
 	roomId: string;
 };
 
-const filter = (message: IMessage): message is IMessage => {
-	return message._hidden !== true;
-};
-
-const compare = (a: IMessage, b: IMessage): number => a.ts.getTime() - b.ts.getTime();
-
 export const useHistoryMessageList = ({ roomId, filter: searchTerm }: HistoryMessageListOptions) => {
 	const getMessages = useEndpoint('GET', '/v1/livechat/:rid/messages', { rid: roomId });
 
@@ -26,8 +20,11 @@ export const useHistoryMessageList = ({ roomId, filter: searchTerm }: HistoryMes
 	useInfiniteMessageQueryUpdates({
 		queryKey: omnichannelQueryKeys.contactMessages(roomId, { searchTerm }),
 		roomId,
-		filter,
-		compare,
+		// Replicates the filtering done server-side
+		filter: (message): message is IMessage =>
+			(!('t' in message) || message.t === 'livechat-close') && (!searchTerm || new RegExp(searchTerm, 'ig').test(message.msg)),
+		// Replicates the sorting forced on server-side
+		compare: (a, b) => a.ts.getTime() - b.ts.getTime(),
 	});
 
 	return useInfiniteQuery({
@@ -37,21 +34,22 @@ export const useHistoryMessageList = ({ roomId, filter: searchTerm }: HistoryMes
 				...(searchTerm && { searchTerm }),
 				offset,
 				count,
-				sort: `{ "ts": 1 }`,
+				sort: JSON.stringify({ ts: 1 }),
 			});
+
 			return {
-				items: messages.map(mapMessageFromApi).filter(filter).toSorted(compare),
+				items: messages.map(mapMessageFromApi),
 				itemCount: total,
 			};
 		},
 		initialPageParam: 0,
 		getNextPageParam: (lastPage, allPages) => {
+			// FIXME: This is an estimation, as messages can be created or removed while paginating
+			// Ideally, the server should return the next offset to use or the pagination should be done using "createdAt" or "updatedAt"
 			const loadedItemsCount = allPages.reduce((acc, page) => acc + page.items.length, 0);
 			return loadedItemsCount < lastPage.itemCount ? loadedItemsCount : undefined;
 		},
 		select: ({ pages }) => ({
-			// FIXME: This is an estimation, as messages can be created or removed while paginating
-			// Ideally, the server should return the next offset to use or the pagination should be done using "createdAt" or "updatedAt"
 			items: pages.flatMap((page) => page.items),
 			itemCount: pages.at(-1)?.itemCount ?? 0,
 		}),
