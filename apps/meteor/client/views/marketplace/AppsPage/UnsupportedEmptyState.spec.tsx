ed17b724ba9aef8449af6dd5a465113a3fc70d2b
Tasso <tasso.evangelista@rocket.chat>
2025-12-26 12:49:24 -0300

    refactor: simplify `AppsContext`

--- apps/meteor/client/views/marketplace/AppsPage/UnsupportedEmptyState.spec.tsx
+++ apps/meteor/client/views/marketplace/AppsPage/UnsupportedEmptyState.spec.tsx
@@ -2,14 +2,11 @@ import { mockAppRoot } from '@rocket.chat/mock-providers';
 import { render, screen } from '@testing-library/react';
 
 import UnsupportedEmptyState from './UnsupportedEmptyState';
-import { AppsContext } from '../../../contexts/AppsContext';
 
 describe('with private apps enabled', () => {
-	const appRoot = mockAppRoot()
-		.withTranslations('en', 'core', {
-			Marketplace_unavailable: 'Marketplace unavailable',
-		})
-		.wrap((children) => <AppsContext.Provider value={{ orchestrator: undefined }}>{children}</AppsContext.Provider>);
+	const appRoot = mockAppRoot().withTranslations('en', 'core', {
+		Marketplace_unavailable: 'Marketplace unavailable',
+	});
 
 	it('should inform that the marketplace is unavailable due unsupported version', () => {
 		render(<UnsupportedEmptyState />, { wrapper: appRoot.build() });
