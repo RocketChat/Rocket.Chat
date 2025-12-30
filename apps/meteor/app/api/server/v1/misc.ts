c82810854ecd4b2806bbed8738652294ea6830eb
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-12-30 11:34:06 -0300

    fix(api): return structured error responses instead of throwing exceptions in method calls

--- apps/meteor/app/api/server/v1/misc.ts
+++ apps/meteor/app/api/server/v1/misc.ts
@@ -526,7 +526,7 @@ API.v1.addRoute(
 					Meteor._debug(`Exception while invoking method ${method}`, err);
 				}
 
-				throw err;
+				return API.v1.failure(mountResult({ id, error: err }));
 			}
 		},
 	},
@@ -581,7 +581,7 @@ API.v1.addRoute(
 				if (settings.get('Log_Level') === '2') {
 					Meteor._debug(`Exception while invoking method ${method}`, err);
 				}
-				throw err;
+				return API.v1.failure(mountResult({ id, error: err }));
 			}
 		},
 	},
