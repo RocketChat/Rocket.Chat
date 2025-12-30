ec5f7274bdb4e2f531a0eb8c1428c10a956f568c
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2025-12-30 17:25:29 -0300

    test: remove homepage `data-qa` attributes (#38004)

--- apps/meteor/client/views/home/cards/DocumentationCard.tsx
+++ apps/meteor/client/views/home/cards/DocumentationCard.tsx
@@ -17,7 +17,6 @@ const DocumentationCard = (props: Omit<ComponentProps<typeof Card>, 'type'>): Re
 			title={t('Documentation')}
 			body={t('Learn_how_to_unlock_the_myriad_possibilities_of_rocket_chat')}
 			buttons={[<GenericCardButton key={1} onClick={() => handleOpenLink(DOCS_URL)} children={t('See_documentation')} role='link' />]}
-			data-qa-id='homepage-documentation-card'
 			width='x340'
 			{...props}
 		/>
