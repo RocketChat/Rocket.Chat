50524200c6782f09b13912ca1bae7e8842d70cc3
Douglas Gubert <douglas.gubert@gmail.com>
2025-12-29 23:18:37 -0300

    feat: add new upload event types

---
+++ packages/apps-engine/src/definition/uploads/IPreFileUploadStream.ts
@@ -0,0 +1,45 @@
+import type { IHttp, IModify, IPersistence, IRead } from '../accessors';
+import { AppMethod } from '../metadata';
+import type { IFileUploadStreamContext } from './IFileUploadContext';
+
+/**
+ * Event interface that allows an app to
+ * register as a handler of the `IPreFileUploadStream`
+ * event
+ *
+ * This event is triggered prior to an upload succesfully
+ * being saved to the database, with a ReadableStream to the
+ * contents of the file that is being uploaded.
+ *
+ * It is generally a recommended for the app NOT to load the entire
+ * file contents into memory, but rather consume the stream and act upon
+ * the chunks of data.
+ *
+ * To prevent the upload from completing, an app should throw a
+ * `FileUploadNotAllowedException` with a message specifying the
+ * reason for rejection.
+ */
+export interface IPreFileUploadStream {
+	/**
+	 * Event interface that allows an app to
+	 * register as a handler of the `IPreFileUpload`
+	 * event
+	 *
+	 * This event is triggered prior to an upload succesfully
+	 * being saved to the database, but *after* all its contents
+	 * have been retrieved by Rocket.Chat.
+	 *
+	 * To prevent the upload from completing, an app should throw a
+	 * `FileUploadNotAllowedException` with a message specifying the
+	 * reason for rejection.
+	 *
+	 * @deprecated as this method requires the entire file to be loaded into memory. Use IPreFileUploadStream instead.
+	 */
+	[AppMethod.EXECUTE_PRE_FILE_UPLOAD_STREAM](
+		context: IFileUploadStreamContext,
+		read: IRead,
+		http: IHttp,
+		persis: IPersistence,
+		modify: IModify,
+	): Promise<void>;
+}
