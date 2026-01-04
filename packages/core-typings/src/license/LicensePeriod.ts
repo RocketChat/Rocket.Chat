bb22f51d00762a593ced858ef98e984c4bea50c3
Tasso <tasso.evangelista@rocket.chat>
2026-01-04 15:23:29 -0300

    refactor: remove unused types

--- packages/core-typings/src/license/LicensePeriod.ts
+++ packages/core-typings/src/license/LicensePeriod.ts
@@ -4,10 +4,8 @@ import type { LicenseModule } from './LicenseModule';
 export type Timestamp = string;
 
 export type LicensePeriod = {
-	validFrom?: Timestamp;
-	validUntil?: Timestamp;
+	validFrom?: string;
+	validUntil?: string;
 	invalidBehavior: LicenseBehavior;
-} & ({ validFrom: Timestamp } | { validUntil: Timestamp }) &
+} & ({ validFrom: string } | { validUntil: string }) &
 	({ invalidBehavior: 'disable_modules'; modules: LicenseModule[] } | { invalidBehavior: Exclude<LicenseBehavior, 'disable_modules'> });
-
-export type LicensePeriodBehavior = Exclude<LicenseBehavior, 'prevent_action'>;
