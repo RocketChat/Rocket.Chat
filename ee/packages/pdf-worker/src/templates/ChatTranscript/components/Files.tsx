9edfff798cb3e2ff84be1003d254377e463dcae2
juliajforesti <juliajforesti@gmail.com>
2026-01-05 17:37:39 -0300

    fix: add message and file wrapping for multifiles scenario

--- ee/packages/pdf-worker/src/templates/ChatTranscript/components/Files.tsx
+++ ee/packages/pdf-worker/src/templates/ChatTranscript/components/Files.tsx
@@ -7,14 +7,22 @@ import { fontScales } from '@rocket.chat/fuselage-tokens/typography.json';
 import type { PDFFile } from '../../../types/ChatTranscriptData';
 
 const styles = StyleSheet.create({
+	container: {
+		marginTop: 8,
+		width: '100%',
+	},
 	file: {
 		color: colors.n700,
-		marginTop: 4,
+		marginBottom: 16,
 		flexDirection: 'column',
 		fontSize: fontScales.c1.fontSize,
+		width: '100%',
+	},
+	fileName: {
+		marginBottom: 6,
 	},
 	image: {
-		width: 400,
+		width: '100%',
 		maxHeight: 240,
 		objectFit: 'contain',
 		objectPosition: '0',
@@ -31,10 +39,10 @@ const styles = StyleSheet.create({
 });
 
 export const Files = ({ files, invalidMessage }: { files: PDFFile[]; invalidMessage: string }) => (
-	<View wrap={false}>
+	<View style={styles.container}>
 		{files?.map((file, index) => (
-			<View style={styles.file} key={index}>
-				<Text>{file.name}</Text>
+			<View style={styles.file} key={index} wrap>
+				<Text style={styles.fileName}>{file.name}</Text>
 				{file.buffer ? (
 					// Cache = false is required to avoid a bug in react-pdf
 					// Which causes the image to be duplicated when using buffers because of bad caching
