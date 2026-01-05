fc8909d9c0a5e2c17395d89b8e254f74adb3c1ca
Douglas Gubert <douglas.gubert@gmail.com>
2026-01-05 18:55:26 -0300

    fix: IPreFileUploadStream docstring and export

--- packages/apps-engine/src/definition/uploads/IPreFileUploadStream.ts
+++ packages/apps-engine/src/definition/uploads/IPreFileUploadStream.ts
@@ -22,18 +22,20 @@ import type { IFileUploadStreamContext } from './IFileUploadContext';
 export interface IPreFileUploadStream {
 	/**
 	 * Event interface that allows an app to
-	 * register as a handler of the `IPreFileUpload`
+	 * register as a handler of the `IPreFileUploadStream`
 	 * event
 	 *
 	 * This event is triggered prior to an upload succesfully
-	 * being saved to the database, but *after* all its contents
-	 * have been retrieved by Rocket.Chat.
+	 * being saved to the database, with a ReadableStream to the
+	 * contents of the file that is being uploaded.
+	 *
+	 * It is generally a recommended for the app NOT to load the entire
+	 * file contents into memory, but rather consume the stream and act upon
+	 * the chunks of data.
 	 *
 	 * To prevent the upload from completing, an app should throw a
 	 * `FileUploadNotAllowedException` with a message specifying the
 	 * reason for rejection.
-	 *
-	 * @deprecated as this method requires the entire file to be loaded into memory. Use IPreFileUploadStream instead.
 	 */
 	[AppMethod.EXECUTE_PRE_FILE_UPLOAD_STREAM](
 		context: IFileUploadStreamContext,
--- packages/apps-engine/src/definition/uploads/index.ts
+++ packages/apps-engine/src/definition/uploads/index.ts
@@ -1,6 +1,7 @@
 import { IFileUploadContext } from './IFileUploadContext';
 import { IPreFileUpload } from './IPreFileUpload';
+import type { IPreFileUploadStream } from './IPreFileUploadStream';
 import { IUpload } from './IUpload';
 import { StoreType } from './StoreType';
 
-export { IFileUploadContext, IPreFileUpload, IUpload, StoreType };
+export { IFileUploadContext, IPreFileUpload, IPreFileUploadStream, IUpload, StoreType };
