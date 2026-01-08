8d9c93de7cdf12846abb059e76bb5975fa46f3bd
juliajforesti <juliajforesti@gmail.com>
2026-01-08 11:33:18 -0300

    test: create `Composer` class

---
+++ apps/meteor/tests/e2e/page-objects/fragments/composer.ts
@@ -0,0 +1,43 @@
+import type { Locator, Page } from 'playwright-core';
+
+export abstract class Composer {
+	constructor(
+		protected root: Locator,
+		protected page?: Page,
+	) {
+		this.root = root;
+		this.page = page;
+	}
+
+	get inputMessage(): Locator {
+		return this.root.locator('[name="msg"]');
+	}
+
+	get toolbarPrimaryActions(): Locator {
+		return this.root.getByRole('toolbar', { name: 'Composer Primary Actions' });
+	}
+
+	get allPrimaryActions(): Locator {
+		return this.toolbarPrimaryActions.getByRole('button');
+	}
+
+	get btnComposerEmoji(): Locator {
+		return this.toolbarPrimaryActions.getByRole('button', { name: 'Emoji' });
+	}
+
+	get btnJoinRoom(): Locator {
+		return this.root.getByRole('button', { name: 'Join' });
+	}
+}
+
+export class MessageComposer extends Composer {
+	constructor(page: Page) {
+		super(page.getByRole('group', { name: 'Message composer' }));
+	}
+}
+
+export class ThreadMessageComposer extends Composer {
+	constructor(page: Page) {
+		super(page.getByRole('dialog', { name: 'thread' }).getByRole('group', { name: 'Message composer' }));
+	}
+}
