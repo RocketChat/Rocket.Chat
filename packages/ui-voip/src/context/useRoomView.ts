a1a8c92c3291768144d6b4dbb8e3c474519b5a52
gabriellsh <henriques.gabriell@gmail.com>
2026-01-08 17:18:25 -0300

    always hide widget

--- packages/ui-voip/src/context/useRoomView.ts
+++ packages/ui-voip/src/context/useRoomView.ts
@@ -6,14 +6,11 @@ const useRoomView = () => {
 	const { setInRoomView } = useMediaCallContext();
 
 	useLayoutEffect(() => {
+		setInRoomView(true);
 		return () => {
 			setInRoomView(false);
 		};
 	}, [setInRoomView]);
-
-	return () => {
-		setInRoomView(true);
-	};
 };
 
 export default useRoomView;
