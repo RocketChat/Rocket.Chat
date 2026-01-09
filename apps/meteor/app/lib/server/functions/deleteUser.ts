54deff7cfe7c4f71bfe26e49d789e993edaf8b4b
NAME-ASHWANIYADAV <22ashwaniyadav@gmail.com>
2026-01-09 08:49:41 +0000

    Fix: Add lastMessage refresh logic to Unlink case and format Rooms.ts

--- apps/meteor/app/lib/server/functions/deleteUser.ts
+++ apps/meteor/app/lib/server/functions/deleteUser.ts
@@ -114,6 +114,20 @@ export async function deleteUser(userId: string, confirmRelinquish = false, dele
                     break;
                 }
                 await Messages.unlinkUserId(userId, userToReplaceWhenUnlinking?._id, userToReplaceWhenUnlinking?.username, nameAlias);
+
+\                const roomsToUpdateUnlink = await Rooms.find({ 'lastMessage.u._id': userId }, { projection: { _id: 1 } }).toArray();
+                for await (const room of roomsToUpdateUnlink) {
+                    affectedRoomIds.push(room._id);
+
+\                    const [newLastMessage] = await Messages.find({ rid: room._id }, { sort: { ts: -1 }, limit: 1 }).toArray();
+
+                    if (newLastMessage) {
+                        await Rooms.updateOne({ _id: room._id }, { $set: { lastMessage: newLastMessage } });
+                    } else {
+                        await Rooms.updateOne({ _id: room._id }, { $unset: { lastMessage: 1 } });
+                    }
+                }
+
                 break;
         }
 
