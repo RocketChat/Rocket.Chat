0b68289280909df879899d135e5da4bbbac7a303
Matheus Cardoso <matheus@cardo.so>
2026-01-16 09:51:08 -0300

    chore: replace meteor/promises and meteor/fetchi [skip ci]

--- apps/meteor/app/utils/client/index.ts
+++ apps/meteor/app/utils/client/index.ts
@@ -1,4 +1,4 @@
-// export { Info } from '../rocketchat.info' with  { type };
+export { Info } from '../rocketchat.info';
 export { getUserPreference } from './lib/getUserPreference';
 export { fileUploadIsValidContentType } from './restrictions';
 export { getUserAvatarURL } from './getUserAvatarURL';
