47edc384ec89d4c0a295e34cfe864d041531d231
Chetan Agarwal <agarwalchetan1302@gmail.com>
2026-01-28 16:17:51 +0530

    fix(model-typings): resolve empty interface violations

--- packages/model-typings/src/models/ISmarshHistoryModel.ts
+++ packages/model-typings/src/models/ISmarshHistoryModel.ts
@@ -2,7 +2,4 @@ import type { ISmarshHistory } from '@rocket.chat/core-typings';
 
 import type { IBaseModel } from './IBaseModel';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ISmarshHistoryModel extends IBaseModel<ISmarshHistory> {
-	//
-}
+export type ISmarshHistoryModel = IBaseModel<ISmarshHistory>;
