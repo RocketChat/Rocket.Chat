71f204f2f97181eb1b4eef3daa1330af21ec3178
Tasso <tasso.evangelista@rocket.chat>
2026-01-28 22:22:05 -0300

    refactor: convert `apps/meteor/app/irc/server/irc-bridge/peerHandlers/disconnected.js` to TypeScript

--- apps/meteor/app/irc/server/irc-bridge/peerHandlers/disconnected.ts
+++ apps/meteor/app/irc/server/irc-bridge/peerHandlers/disconnected.ts
@@ -1,17 +1,26 @@
+import { UserStatus } from '@rocket.chat/core-typings';
 import { Users } from '@rocket.chat/models';
 
 import { notifyOnUserChange } from '../../../../lib/server/lib/notifyListener';
 
-export default async function handleQUIT(args) {
+type QuitArgs = {
+	nick: string;
+};
+
+export default async function handleQUIT(args: QuitArgs): Promise<void> {
 	const user = await Users.findOne({
 		'profile.irc.nick': args.nick,
 	});
+
+	if (!user) {
+		return;
+	}
 
 	await Users.updateOne(
 		{ _id: user._id },
 		{
 			$set: {
-				status: 'offline',
+				status: UserStatus.OFFLINE,
 			},
 		},
 	);
