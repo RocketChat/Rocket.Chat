0b1bea9954f8230a5178005084390429166ccb76
yush-1018 <light1ooo7710@gmail.com>
2026-01-29 15:47:48 +0530

    feat(App Engine): Implement deleteNotifyUser to remove ephemeral messages

--- packages/apps-engine/tests/test-data/bridges/messageBridge.ts
+++ packages/apps-engine/tests/test-data/bridges/messageBridge.ts
@@ -25,6 +25,10 @@ export class TestsMessageBridge extends MessageBridge {
 		throw new Error('Method not implemented.');
 	}
 
+	public deleteNotifyUser(user: IUser, message: IMessage, appId: string): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+
 	public delete(message: IMessage, user: IUser, appId: string): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
