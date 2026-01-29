4ff848d01a012b624107534b201021ed8c8f4ea1
Tasso <tasso.evangelista@rocket.chat>
2026-01-29 01:04:48 -0300

    refactor: convert `apps/meteor/app/lib/server/startup/robots.js` to TypeScript

--- apps/meteor/app/lib/server/startup/robots.ts
+++ apps/meteor/app/lib/server/startup/robots.ts
@@ -1,10 +1,12 @@
+import type { IncomingMessage, ServerResponse } from 'http';
+
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import { settings } from '../../../settings/server';
 
 Meteor.startup(() => {
-	return WebApp.connectHandlers.use('/robots.txt', (req, res /* , next*/) => {
+	return WebApp.connectHandlers.use('/robots.txt', (_req: IncomingMessage, res: ServerResponse /* , next*/): void => {
 		res.writeHead(200);
 		res.end(settings.get('Robot_Instructions_File_Content'));
 	});
