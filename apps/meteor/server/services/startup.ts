de0d131f1671a9140a51725865e46b3251a0cbfc
Dev Arora <aroradev1010@gmail.com>
2026-01-29 14:26:54 +0000

    chore(startup): add per-service diagnostics to service registration

--- apps/meteor/server/services/startup.ts
+++ apps/meteor/server/services/startup.ts
@@ -31,34 +31,43 @@ import { UserService } from './user/service';
 import { VideoConfService } from './video-conference/service';
 import { i18n } from '../lib/i18n';
 
+const registerWithDiagnostics = (name: string, factory: () => unknown): void => {
+	try {
+		api.registerService(factory());
+	} catch (error) {
+		Logger.error({ service: name, err: error }, 'Service registration failed');
+		throw error;
+	}
+};
+
 export const registerServices = async (): Promise<void> => {
 	const { db } = MongoInternals.defaultRemoteCollectionDriver().mongo;
 
-	api.registerService(new AppsEngineService());
-	api.registerService(new AnalyticsService());
-	api.registerService(new AuthorizationLivechat());
-	api.registerService(new BannerService());
-	api.registerService(new CalendarService());
-	api.registerService(new LDAPService());
-	api.registerService(new MediaService());
-	api.registerService(new MeteorService());
-	api.registerService(new NPSService());
-	api.registerService(new RoomService());
-	api.registerService(new SAUMonitorService());
-	api.registerService(new OmnichannelService());
-	api.registerService(new TeamService());
-	api.registerService(new UiKitCoreAppService());
-	api.registerService(new PushService());
-	api.registerService(new DeviceManagementService());
-	api.registerService(new VideoConfService());
-	api.registerService(new UploadService());
-	api.registerService(new MessageService());
-	api.registerService(new SettingsService());
-	api.registerService(new OmnichannelIntegrationService());
-	api.registerService(new ImportService());
-	api.registerService(new OmnichannelAnalyticsService());
-	api.registerService(new UserService());
-	api.registerService(new MediaCallService());
+	registerWithDiagnostics('AppsEngineService', () => new AppsEngineService());
+	registerWithDiagnostics('AnalyticsService', () => new AnalyticsService());
+	registerWithDiagnostics('AuthorizationLivechat', () => new AuthorizationLivechat());
+	registerWithDiagnostics('BannerService', () => new BannerService());
+	registerWithDiagnostics('CalendarService', () => new CalendarService());
+	registerWithDiagnostics('LDAPService', () => new LDAPService());
+	registerWithDiagnostics('MediaService', () => new MediaService());
+	registerWithDiagnostics('MeteorService', () => new MeteorService());
+	registerWithDiagnostics('NPSService', () => new NPSService());
+	registerWithDiagnostics('RoomService', () => new RoomService());
+	registerWithDiagnostics('SAUMonitorService', () => new SAUMonitorService());
+	registerWithDiagnostics('OmnichannelService', () => new OmnichannelService());
+	registerWithDiagnostics('TeamService', () => new TeamService());
+	registerWithDiagnostics('UiKitCoreAppService', () => new UiKitCoreAppService());
+	registerWithDiagnostics('PushService', () => new PushService());
+	registerWithDiagnostics('DeviceManagementService', () => new DeviceManagementService());
+	registerWithDiagnostics('VideoConfService', () => new VideoConfService());
+	registerWithDiagnostics('UploadService', () => new UploadService());
+	registerWithDiagnostics('MessageService', () => new MessageService());
+	registerWithDiagnostics('SettingsService', () => new SettingsService());
+	registerWithDiagnostics('OmnichannelIntegrationService', () => new OmnichannelIntegrationService());
+	registerWithDiagnostics('ImportService', () => new ImportService());
+	registerWithDiagnostics('OmnichannelAnalyticsService', () => new OmnichannelAnalyticsService());
+	registerWithDiagnostics('UserService', () => new UserService());
+	registerWithDiagnostics('MediaCallService', () => new MediaCallService());
 
 	// if the process is running in micro services mode we don't need to register services that will run separately
 	if (!isRunningMs()) {
@@ -66,13 +75,13 @@ export const registerServices = async (): Promise<void> => {
 
 		const { Authorization } = await import('./authorization/service');
 
-		api.registerService(new Presence());
-		api.registerService(new Authorization());
+		registerWithDiagnostics('Presence', () => new Presence());
+		registerWithDiagnostics('Authorization', () => new Authorization());
 
 		// Run EE services defined outside of the main repo
 		// Otherwise, monolith would ignore them :(
 		// Always register the service and manage licensing inside the service (tbd)
-		api.registerService(new QueueWorker(db, Logger));
-		api.registerService(new OmnichannelTranscript(Logger, i18n));
+		registerWithDiagnostics('QueueWorker', () => new QueueWorker(db, Logger));
+		registerWithDiagnostics('OmnichannelTranscript', () => new OmnichannelTranscript(Logger, i18n));
 	}
 };
