63cc7f531827a5a5165130ff1f6373a0627338f4
Tasso <tasso.evangelista@rocket.chat>
2026-01-28 22:17:03 -0300

    refactor: convert `apps/meteor/app/irc/server/irc-bridge/localHandlers/onSaveMessage.js` to TypeScript

--- apps/meteor/app/irc/server/irc-bridge/localHandlers/onSaveMessage.ts
+++ apps/meteor/app/irc/server/irc-bridge/localHandlers/onSaveMessage.ts
@@ -1,23 +1,24 @@
+import type { IMessage, IRoom } from '@rocket.chat/core-typings';
 import { Subscriptions, Users } from '@rocket.chat/models';
 
 import { SystemLogger } from '../../../../../server/lib/logger/system';
 
-export default async function handleOnSaveMessage(message, to) {
+export default async function handleOnSaveMessage(this: any, message: IMessage, to: IRoom): Promise<void> {
 	let toIdentification = '';
 	// Direct message
 	if (to.t === 'd') {
-		const subscriptions = Subscriptions.findByRoomId(to._id).toArray();
+		const subscriptions = await Subscriptions.findByRoomId(to._id).toArray();
 		for await (const subscription of subscriptions) {
 			if (subscription.u._id !== message.u._id) {
 				const userData = await Users.findOne({ username: subscription.u.username });
 				if (userData) {
-					if (userData.profile && userData.profile.irc && userData.profile.irc.nick) {
-						toIdentification = userData.profile.irc.nick;
+					if ((userData as any).profile && (userData as any).profile.irc && (userData as any).profile.irc.nick) {
+						toIdentification = (userData as any).profile.irc.nick;
 					} else {
-						toIdentification = userData.username;
+						toIdentification = userData.username as string;
 					}
 				} else {
-					toIdentification = subscription.u.username;
+					toIdentification = subscription.u.username as string;
 				}
 			}
 		}
