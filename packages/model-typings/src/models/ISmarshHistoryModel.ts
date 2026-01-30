c5cb9950d806fb1af8b460235c2de71a05181bf1
Chetan Agarwal <Agarwalchetan1302@gmail.com>
2026-01-31 03:07:40 +0530

    fix(model-typings): justify empty ISmarshHistoryModel interface
    Removed comments explaining the empty interface.

--- packages/model-typings/src/models/ISmarshHistoryModel.ts
+++ packages/model-typings/src/models/ISmarshHistoryModel.ts
@@ -2,8 +2,5 @@ import type { ISmarshHistory } from '@rocket.chat/core-typings';
 
 import type { IBaseModel } from './IBaseModel';
 
-/**
- * Intentionally empty interface.
- * Acts as an extension point for future or external augmentations.
- */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface -- intentional extension point
 export interface ISmarshHistoryModel extends IBaseModel<ISmarshHistory> {}
