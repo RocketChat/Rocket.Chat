fe3fed1469183fcb1af1ef83b97c833f0039491c
NAME-ASHWANIYADAV <22ashwaniyadav@gmail.com>
2026-01-30 05:09:07 +0000

    refactor(omnichannel): replace Modal with GenericModal in EnterpriseDepartmentsModal

--- apps/meteor/client/views/omnichannel/modals/EnterpriseDepartmentsModal.tsx
+++ apps/meteor/client/views/omnichannel/modals/EnterpriseDepartmentsModal.tsx
@@ -1,18 +1,6 @@
-import {
-	Button,
-	Modal,
-	Box,
-	ModalHeader,
-	ModalHeaderText,
-	ModalTagline,
-	ModalTitle,
-	ModalClose,
-	ModalContent,
-	ModalHeroImage,
-	ModalFooter,
-	ModalFooterControllers,
-} from '@rocket.chat/fuselage';
+import { ModalHeroImage, Box } from '@rocket.chat/fuselage';
 import { useOutsideClick } from '@rocket.chat/fuselage-hooks';
+import { GenericModal } from '@rocket.chat/ui-client';
 import { useRouter } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 import { useRef } from 'react';
@@ -21,12 +9,10 @@ import { useTranslation } from 'react-i18next';
 import { useExternalLink } from '../../../hooks/useExternalLink';
 import { useCheckoutUrl } from '../../admin/subscription/hooks/useCheckoutUrl';
 
-// TODO: use `GenericModal` instead of creating a new modal from scratch
-// This seems a upSell modal for enterprise feature
 const EnterpriseDepartmentsModal = ({ closeModal }: { closeModal: () => void }): ReactElement => {
 	const { t } = useTranslation();
 	const router = useRouter();
-	const ref = useRef<HTMLDivElement>(null);
+	const ref = useRef<HTMLElement>(null);
 
 	const openExternalLink = useExternalLink();
 	const manageSubscriptionUrl = useCheckoutUrl()({ target: 'new-departments-page', action: 'upgrade' });
@@ -44,30 +30,23 @@ const EnterpriseDepartmentsModal = ({ closeModal }: { closeModal: () => void }):
 	useOutsideClick([ref], onClose);
 
 	return (
-		<Modal aria-label={t('Departments')} ref={ref}>
-			<ModalHeader>
-				<ModalHeaderText>
-					<ModalTagline>{t('Premium_capability')}</ModalTagline>
-					<ModalTitle>{t('Departments')}</ModalTitle>
-				</ModalHeaderText>
-				<ModalClose onClick={onClose} />
-			</ModalHeader>
-			<ModalContent fontScale='p2'>
+		<Box ref={ref}>
+			<GenericModal
+				variant='upsell'
+				tagline={t('Premium_capability')}
+				title={t('Departments')}
+				cancelText={t('Cancel')}
+				confirmText={t('Upgrade')}
+				onCancel={onClose}
+				onConfirm={goToManageSubscriptionPage}
+			>
 				<ModalHeroImage src='/images/departments.svg' />
 				<Box fontScale='h3' mbe={28}>
 					{t('Premium_Departments_title')}
 				</Box>
 				{t('Premium_Departments_description_upgrade')}
-			</ModalContent>
-			<ModalFooter>
-				<ModalFooterControllers>
-					<Button onClick={onClose}>{t('Cancel')}</Button>
-					<Button onClick={goToManageSubscriptionPage} primary>
-						{t('Upgrade')}
-					</Button>
-				</ModalFooterControllers>
-			</ModalFooter>
-		</Modal>
+			</GenericModal>
+		</Box>
 	);
 };
 
