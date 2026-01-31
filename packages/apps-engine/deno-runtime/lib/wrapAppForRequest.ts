a2a76e1929f9e40f01875cda198d8a5e81f5cf32
Douglas Gubert <douglas.gubert@gmail.com>
2026-01-31 19:57:59 -0300

    refactor: improve detection of instances containing a reference to App

--- packages/apps-engine/deno-runtime/lib/wrapAppForRequest.ts
+++ packages/apps-engine/deno-runtime/lib/wrapAppForRequest.ts
@@ -1,7 +1,7 @@
 import type { App } from '@rocket.chat/apps-engine/definition/App.ts';
 
 import { RequestContext } from './requestContext.ts';
-import { isApp } from '../handlers/lib/assertions.ts';
+import { isApp, isRecord } from '../handlers/lib/assertions.ts';
 
 export function wrapAppForRequest(app: App, req: RequestContext): App {
 	return new Proxy(app, {
@@ -15,20 +15,42 @@ export function wrapAppForRequest(app: App, req: RequestContext): App {
 	});
 }
 
-function hasAppProperty(v: NonNullable<unknown>): v is { app: App } {
-	return isApp((v as unknown as { app?: App }).app);
+// Instances of objects that have a reference to an App instance won't change throughout the
+// lifetime of the runtime, so we can cache the results to avoid iterating the same object multiple times
+const composedCache = new WeakMap<NonNullable<unknown>, ReturnType<typeof findAppProperty>>();
+
+function findAppProperty(v: NonNullable<unknown>): [string, App] | undefined {
+	const cachedEntry = composedCache.get(v);
+
+	if (cachedEntry) {
+		return cachedEntry;
+	}
+
+	if (!isRecord(v)) {
+		// Enables us to avoid having to determine whether the value is a record again
+		composedCache.set(v, undefined);
+		return undefined;
+	}
+
+	const entry = Object.entries(v).find(([_,v]) => isApp(v)) as [string, App] | undefined;
+
+	composedCache.set(v, entry);
+
+	return entry;
 }
 
 export function wrapComposedApp<T extends NonNullable<unknown>>(composed: T, req: RequestContext): T {
-	if (!hasAppProperty(composed)) {
+	const prop = findAppProperty(composed);
+
+	if (!prop) {
 		return composed;
 	}
 
-	const proxy = wrapAppForRequest(composed.app, req);
+	const proxy = wrapAppForRequest(prop[1], req);
 
 	return new Proxy(composed, {
 		get(target, property, receiver) {
-			if (property === 'app') {
+			if (property === prop[0]) {
 				return proxy;
 			}
 
