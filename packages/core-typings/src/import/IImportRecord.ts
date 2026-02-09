071d7087a1189e29cd4f971fb8a260c4f17922bc
Tasso <tasso.evangelista@rocket.chat>
2026-02-09 08:52:04 -0300

    refactor(core-typings): update `IImportRecord` to extend `IRocketChatRecord`

--- packages/core-typings/src/import/IImportRecord.ts
+++ packages/core-typings/src/import/IImportRecord.ts
@@ -2,14 +2,14 @@ import type { IImportChannel } from './IImportChannel';
 import type { IImportContact } from './IImportContact';
 import type { IImportMessage } from './IImportMessage';
 import type { IImportUser } from './IImportUser';
+import type { IRocketChatRecord } from '../IRocketChatRecord';
 
 export type IImportRecordType = 'user' | 'channel' | 'message' | 'contact';
 export type IImportData = IImportUser | IImportChannel | IImportMessage | IImportContact;
 
-export interface IImportRecord {
+export interface IImportRecord extends IRocketChatRecord {
 	data: IImportData;
 	dataType: IImportRecordType;
-	_id: string;
 	options?: Record<string, any>;
 	errors?: Array<{
 		message: string;
