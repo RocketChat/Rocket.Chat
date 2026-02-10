13d3d3aa598d406d1e6b1ac86b55b2ddb1b24eaf
Purnendu Mishra <purnendumishra129th@gmail.com>
2026-02-10 19:48:05 +0000

    add as default parametre because it might constraints to another function use cases based on suggestion

--- apps/meteor/client/views/admin/import/PrepareImportPage.tsx
+++ apps/meteor/client/views/admin/import/PrepareImportPage.tsx
@@ -22,11 +22,9 @@ import {
 } from '../../../../app/importer/lib/ImporterProgressStep';
 import { numberFormat } from '../../../../lib/utils/stringUtils';
 
-const waitFor = <T, U extends T>(fn: () => Promise<T>, predicate: (arg: T) => arg is U) =>
+const waitFor = <T, U extends T>(fn: () => Promise<T>, predicate: (arg: T) => arg is U, maxRetries = 60) =>
 	new Promise<U>((resolve, reject) => {
 		let attempts = 0;
-		const maxRetries = 5;
-
 		const callPromise = () => {
 			fn().then((result) => {
 				if (predicate(result)) {
