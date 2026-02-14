6c93e89e0e7e8bcdbefd2ed79b320676907357d0
ScriptShah <ahmadshahdonishyar@gmail.com>
2026-02-14 13:11:24 +0430

    fix: avoid localized numerals in date input formatting

--- apps/meteor/client/views/omnichannel/analytics/DateRangePicker.tsx
+++ apps/meteor/client/views/omnichannel/analytics/DateRangePicker.tsx
@@ -13,7 +13,8 @@ type DateRangePickerProps = Omit<ComponentProps<typeof Box>, 'onChange'> & {
 	onChange(range: { start: string; end: string }): void;
 };
 
-const formatToDateInput = (date: Moment) => date.format('YYYY-MM-DD');
+const formatToDateInput = (date: Moment) =>
+	date.clone().locale('en').format('YYYY-MM-DD');
 
 const todayDate = formatToDateInput(moment());
 
