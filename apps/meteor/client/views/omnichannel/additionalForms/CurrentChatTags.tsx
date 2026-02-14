ea81bef1d0f188449275b94a4f318fedfbaded2d
NAME-ASHWANIYADAV <22ashwaniyadav@gmail.com>
2026-02-14 20:26:06 +0000

    style: fix prettier formatting in CurrentChatTags

--- apps/meteor/client/views/omnichannel/additionalForms/CurrentChatTags.tsx
+++ apps/meteor/client/views/omnichannel/additionalForms/CurrentChatTags.tsx
@@ -18,15 +18,7 @@ const CurrentChatTags = ({ value, handler, department, viewAll, ...props }: Curr
 		return null;
 	}
 
-	return (
-		<AutoCompleteTagsMultiple
-			{...props}
-			onChange={handler}
-			value={value}
-			department={department}
-			viewAll={viewAll}
-		/>
-	);
+	return <AutoCompleteTagsMultiple {...props} onChange={handler} value={value} department={department} viewAll={viewAll} />;
 };
 
 export default CurrentChatTags;
