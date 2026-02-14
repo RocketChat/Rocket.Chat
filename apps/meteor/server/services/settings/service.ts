f67dc0c87ef010de7a790515a0684e04671b5794
Kevin  Aleman <kaleman960@gmail.com>
2026-02-13 22:18:46 -0600

    fix presence and tests

--- apps/meteor/server/services/settings/service.ts
+++ apps/meteor/server/services/settings/service.ts
@@ -4,12 +4,21 @@ import type { SettingValue } from '@rocket.chat/core-typings';
 
 import { settings } from '../../../app/settings/server';
 import { verifyFingerPrint } from '../../settings/misc';
+import { Settings } from '@rocket.chat/models';
+import { notifyOnSettingChangedById } from '../../../app/lib/server/lib/notifyListener';
 
 export class SettingsService extends ServiceClassInternal implements ISettingsService {
 	protected name = 'settings';
 
 	async get<T extends SettingValue>(settingId: string): Promise<T> {
 		return settings.get<T>(settingId);
+	}
+
+	async set<T extends SettingValue>(settingId: string, value: T): Promise<void> {
+		const update = await Settings.updateValueById(settingId, value);
+		if (update.modifiedCount) {
+			void notifyOnSettingChangedById(settingId);
+		}
 	}
 
 	override async started() {
