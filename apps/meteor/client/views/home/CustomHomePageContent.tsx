5ca777535d8e4ebbd1e672b3332bde74dff5d572
Swarajreddy10 <swarajchandra22@gmail.com>
2026-02-15 19:13:00 +0530

    fix: sanitize Layout_Home_Body with DOMPurify to prevent stored XSS

--- apps/meteor/client/views/home/CustomHomePageContent.tsx
+++ apps/meteor/client/views/home/CustomHomePageContent.tsx
@@ -1,11 +1,16 @@
 import { Box } from '@rocket.chat/fuselage';
 import { useSetting } from '@rocket.chat/ui-contexts';
+import DOMPurify from 'dompurify';
 import type { ComponentProps, ReactElement } from 'react';
 
 const CustomHomePageContent = (props: ComponentProps<typeof Box>): ReactElement => {
 	const body = useSetting('Layout_Home_Body', '');
+	const sanitized = DOMPurify.sanitize(body, {
+		ADD_TAGS: ['iframe'],
+		ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'src'],
+	});
 
-	return <Box withRichContent dangerouslySetInnerHTML={{ __html: body }} {...props} />;
+	return <Box withRichContent dangerouslySetInnerHTML={{ __html: sanitized }} {...props} />;
 };
 
 export default CustomHomePageContent;
