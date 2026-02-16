e2ffa195491e4672c0145106dba880bf3358592f
yash-rajpal <rajpal.yash03@gmail.com>
2026-02-16 22:36:54 +0530

    add github with pkce

--- apps/meteor/server/passport.ts
+++ apps/meteor/server/passport.ts
@@ -5,13 +5,17 @@ import { Accounts } from 'meteor/accounts-base';
 import { WebApp } from 'meteor/webapp';
 import passport from 'passport';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 
 passport.use(
 	new FacebookStrategy(
 		{
-			clientID: 'process.env.FACEBOOK_CLIENT_ID',
-			clientSecret: 'process.env.FACEBOOK_APP_SECRET',
+			clientID: process.env.FACEBOOK_CLIENT_ID as string,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
 			callbackURL: 'http://localhost:3000/auth/facebook/callback',
+			pkce: true,
+			state: true,
+			graphAPIVersion: 'v22.0',
 		},
 		(accessToken, refreshToken, profile, cb) => {
 			console.log(
@@ -36,16 +40,50 @@ passport.use(
 	),
 );
 
+passport.use(
+	new GitHubStrategy(
+		{
+			clientID: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+			callbackURL: 'http://localhost:3000/auth/github/callback',
+			state: true,
+			pkce: true,
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log(
+				'Github strategy',
+				JSON.stringify(
+					{
+						accessToken,
+						refreshToken,
+						profile,
+					},
+					null,
+					2,
+				),
+			);
+			done(null, {
+				provider: 'github',
+				providerId: profile.id,
+				email: profile?.emails?.[0]?.value,
+				name: profile.displayName,
+			});
+		},
+	),
+);
+
 passport.serializeUser((user, done) => {
 	console.log('serializeUser', user);
 	// store the bare minimum
 	done(null, user.providerId);
 });
 
-passport.deserializeUser((id, done) => {
-	console.log('deserializeUser', id);
+passport.deserializeUser(async (id, done) => {
+	const user = Users.findOneById(id);
+
+	console.log('deserializeUser', user);
 	// we don’t actually use this user later
-	done(null, { providerId: id });
+	done(null, user);
 });
 
 const app = express();
@@ -57,7 +95,7 @@ app.use(
 		saveUninitialized: false,
 		cookie: {
 			httpOnly: true,
-			secure: true,
+			secure: process.env.NODE_ENV === 'production',
 			maxAge: 5 * 60 * 1000, // 5 minutes
 		},
 	}),
@@ -66,7 +104,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'], failureRedirect: '/login', failureFlash: true }));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), async (req, res) => {
 	console.log('facebook/callback', req.user);
@@ -75,6 +113,36 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRe
 	console.log('oAuthUser', oAuthUser);
 
 	const user = await Accounts.updateOrCreateUserFromExternalService('facebook', { ...oAuthUser, id: oAuthUser.providerId });
+	console.log('user', user);
+
+	if (!user?.userId) {
+		return res.redirect('/login');
+	}
+
+	const userFromDB = await Users.findOneById(user?.userId as string);
+	console.log('userFromDB', userFromDB);
+
+	const stampedToken = Accounts._generateStampedLoginToken();
+	Accounts._insertLoginToken(userFromDB?._id as string, stampedToken);
+
+	res.redirect(`http://localhost:3000/home?resumeToken=${stampedToken.token}`);
+
+	req.session.destroy((err) => {
+		if (err) {
+			console.error('Error destroying session', err);
+		}
+	});
+});
+
+app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), async (req, res) => {
+	console.log('github/callback', req.user);
+
+	const oAuthUser = req.user;
+	console.log('oAuthUser', oAuthUser);
+
+	const user = await Accounts.updateOrCreateUserFromExternalService('github', { ...oAuthUser, id: oAuthUser.providerId });
 	console.log('user', user);
 
 	if (!user?.userId) {
