49d1ad53c59c1243aa6698d7b89cbe6d949eb124
Julio Araujo <julio.araujo@rocket.chat>
2026-02-18 17:15:27 +0100

    Add guardrails

--- apps/meteor/app/file/server/functions/sanitizeFileName.ts
+++ apps/meteor/app/file/server/functions/sanitizeFileName.ts
@@ -7,6 +7,10 @@ export function sanitizeFileName(fileName: string) {
 		throw new Error('error-invalid-file-name');
 	}
 
+	if (base === '.' || base.startsWith('..')) {
+		throw new Error('error-invalid-file-name');
+	}
+
 	if (!/^[a-zA-Z0-9._-]+$/.test(base)) {
 		throw new Error('error-invalid-characters-in-file-name');
 	}
