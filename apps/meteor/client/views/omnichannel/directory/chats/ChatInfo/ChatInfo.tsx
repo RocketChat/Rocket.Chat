ac5b11fb2ab4a028f6f54b84afa9613daa94a53d
Chetan Agarwal <agarwalchetan1302@gmail.com>
2026-02-18 23:36:20 +0530

    refactor(omnichannel): replace moment.js with built-in formatters in ChatInfo
    - Replace moment(closedAt).from(moment(ts), true) with formatDuration for Chat Duration
    - Replace moment(responseBy.lastMessageTs).fromNow(true) with useFormattedRelativeTime for Inactivity Time
    - Remove moment import and TODO comment
    - useFormatDuration expects seconds, useFormattedRelativeTime expects milliseconds

--- apps/meteor/client/views/omnichannel/directory/chats/ChatInfo/ChatInfo.tsx
+++ apps/meteor/client/views/omnichannel/directory/chats/ChatInfo/ChatInfo.tsx
@@ -4,7 +4,7 @@ import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
 import { ContextualbarScrollableContent, ContextualbarFooter, InfoPanelField, InfoPanelLabel, InfoPanelText } from '@rocket.chat/ui-client';
 import type { IRouterPaths } from '@rocket.chat/ui-contexts';
 import { useToastMessageDispatch, useRoute, useUserSubscription, useTranslation, usePermission, useUserId } from '@rocket.chat/ui-contexts';
-import moment from 'moment';
+
 import { useMemo } from 'react';
 
 import DepartmentField from './DepartmentField';
@@ -12,6 +12,7 @@ import VisitorClientInfo from './VisitorClientInfo';
 import MarkdownText from '../../../../../components/MarkdownText';
 import { useFormatDateAndTime } from '../../../../../hooks/useFormatDateAndTime';
 import { useFormatDuration } from '../../../../../hooks/useFormatDuration';
+import { useFormattedRelativeTime } from '../../../../../hooks/useFormattedRelativeTime';
 import CustomField from '../../../components/CustomField';
 import { useValidCustomFields } from '../../../contactInfo/hooks/useValidCustomFields';
 import { AgentField, SlaField, ContactField, SourceField } from '../../components';
@@ -24,7 +25,6 @@ type ChatInfoProps = {
 	route: keyof IRouterPaths;
 };
 
-// TODO: Remove moment we are mixing moment and our own formatters :sadface:
 function ChatInfo({ id, route }: ChatInfoProps) {
 	const t = useTranslation();
 	const dispatchToastMessage = useToastMessageDispatch();
@@ -53,6 +53,10 @@ function ChatInfo({ id, route }: ChatInfoProps) {
 		queuedAt,
 	} = room ?? {};
 
+	const inactivityTime = useFormattedRelativeTime(
+		responseBy?.lastMessageTs ? Date.now() - new Date(responseBy.lastMessageTs).getTime() : 0,
+	);
+
 	const routePath = useRoute(route || 'omnichannel-directory');
 	const subscription = useUserSubscription(id);
 	const hasGlobalEditRoomPermission = usePermission('save-others-livechat-room-info');
@@ -73,15 +77,15 @@ function ChatInfo({ id, route }: ChatInfoProps) {
 		routePath.push(
 			route
 				? {
-						tab: 'room-info',
-						context: 'edit',
-						id,
-					}
+					tab: 'room-info',
+					context: 'edit',
+					id,
+				}
 				: {
-						page: 'chats',
-						id,
-						bar: 'edit',
-					},
+					page: 'chats',
+					id,
+					bar: 'edit',
+				},
 		);
 	});
 
@@ -127,7 +131,7 @@ function ChatInfo({ id, route }: ChatInfoProps) {
 					{closedAt && (
 						<InfoPanelField>
 							<InfoPanelLabel>{t('Chat_Duration')}</InfoPanelLabel>
-							<InfoPanelText>{moment(closedAt).from(moment(ts), true)}</InfoPanelText>
+							<InfoPanelText>{formatDuration((new Date(closedAt).getTime() - new Date(ts).getTime()) / 1000)}</InfoPanelText>
 						</InfoPanelField>
 					)}
 					{ts && (
@@ -157,7 +161,7 @@ function ChatInfo({ id, route }: ChatInfoProps) {
 					{!waitingResponse && responseBy?.lastMessageTs && (
 						<InfoPanelField>
 							<InfoPanelLabel>{t('Inactivity_Time')}</InfoPanelLabel>
-							<InfoPanelText>{moment(responseBy.lastMessageTs).fromNow(true)}</InfoPanelText>
+							<InfoPanelText>{inactivityTime}</InfoPanelText>
 						</InfoPanelField>
 					)}
 					{customFieldEntries?.length > 0 &&
