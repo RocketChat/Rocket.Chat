1f5be691072c2811b0ef0793e5e052bb126c6e4d
MartinSchoeler <martinschoeler8@gmail.com>
2026-02-20 16:22:32 -0300

    fix: Non latin characters not being rendered on exported messages pdf
    chore: update & patch react-pdf and yoga-layout
    
    ..
    
    ..
    
    fix hebrew characters
    
    ..
    
    remove symlink
    
    remove non related changes

--- apps/meteor/client/views/room/contextualBar/ExportMessages/useExportMessagesAsPDFMutation.tsx
+++ apps/meteor/client/views/room/contextualBar/ExportMessages/useExportMessagesAsPDFMutation.tsx
@@ -1,8 +1,8 @@
-import { Document, Image, Page, pdf, StyleSheet, Text, View } from '@react-pdf/renderer';
+import { Document, Font, Image, Page, pdf, StyleSheet, Text, View } from '@react-pdf/renderer';
 import type { IMessage, MessageAttachmentDefault } from '@rocket.chat/core-typings';
 import { MessageTypes } from '@rocket.chat/message-types';
 import { escapeHTML } from '@rocket.chat/string-helpers';
-import { useSetting, useToastMessageDispatch } from '@rocket.chat/ui-contexts';
+import { useSetting, useToastMessageDispatch, useAbsoluteUrl } from '@rocket.chat/ui-contexts';
 import { useMutation } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 
@@ -12,8 +12,28 @@ import { Messages } from '../../../../stores';
 const leftTab = {
 	marginLeft: 20,
 };
+const NOTO_SANS_FONTS: { name: string; fontSrc: string }[] = [
+	{ name: 'Noto Sans Hebrew', fontSrc: '/fonts/NotoSansHebrew-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans', fontSrc: '/fonts/NotoSans-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Arabic', fontSrc: '/fonts/NotoSansArabic-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Devanagari', fontSrc: '/fonts/NotoSansDevanagari-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Bengali', fontSrc: '/fonts/NotoSansBengali-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Tamil', fontSrc: '/fonts/NotoSansTamil-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Sinhala', fontSrc: '/fonts/NotoSansSinhala-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Thai', fontSrc: '/fonts/NotoSansThai-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Lao', fontSrc: '/fonts/NotoSansLao-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans Georgian', fontSrc: '/fonts/NotoSansGeorgian-VariableFont_wdth,wght.ttf' },
+	{ name: 'Noto Sans JP', fontSrc: '/fonts/NotoSansJP-VariableFont_wght.ttf' },
+	{ name: 'Noto Sans KR', fontSrc: '/fonts/NotoSansKR-VariableFont_wght.ttf' },
+	{ name: 'Noto Sans SC', fontSrc: '/fonts/NotoSansSC-VariableFont_wght.ttf' },
+	{ name: 'Noto Sans TC', fontSrc: '/fonts/NotoSansTC-VariableFont_wght.ttf' },
+	{ name: 'Noto Sans HK', fontSrc: '/fonts/NotoSansHK-VariableFont_wght.ttf' },
+];
 
 const pdfStyles = StyleSheet.create({
+	page: {
+		fontFamily: NOTO_SANS_FONTS.map((font) => font.name),
+	},
 	messageHeader: {
 		display: 'flex',
 		flexDirection: 'row',
@@ -48,6 +68,16 @@ export const useExportMessagesAsPDFMutation = () => {
 	const chatopsUsername = useSetting('Chatops_Username');
 	const formatDateAndTime = useFormatDateAndTime();
 	const dispatchToastMessage = useToastMessageDispatch();
+	const absoluteUrl = useAbsoluteUrl();
+
+	for (const font of NOTO_SANS_FONTS) {
+		Font.register({
+			family: font.name,
+			fonts: [{ src: absoluteUrl(font.fontSrc) }],
+		});
+	}
+
+	Font.registerHyphenationCallback((word) => [word]);
 
 	return useMutation({
 		mutationFn: async (messageIds: IMessage['_id'][]) => {
@@ -72,7 +102,7 @@ export const useExportMessagesAsPDFMutation = () => {
 			const jsx = (
 				<Document>
 					<Page size='A4'>
-						<View style={{ margin: 10 }}>
+						<View style={{ ...pdfStyles.page, margin: 10 }}>
 							{messages.map((message) => {
 								const dateTime = formatDateAndTime(message.ts);
 								return (
