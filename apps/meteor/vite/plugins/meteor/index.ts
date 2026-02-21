e3a4f909076d86b469d4ef7df1ac7af46dcdddf1
Matheus Cardoso <matheus@cardo.so>
2026-02-21 14:49:20 -0300

    chore: remove temporary code and dependencies

--- apps/meteor/vite/plugins/meteor/index.ts
+++ apps/meteor/vite/plugins/meteor/index.ts
@@ -3,20 +3,11 @@ import path from 'node:path';
 import type { PluginOption } from 'vite';
 
 import { globals } from './plugins/globals.ts';
-import { replace } from './plugins/replace.ts';
-import { resolve } from './plugins/resolve.ts';
 import type { PluginOptions, ResolvedPluginOptions } from './plugins/shared/config.ts';
-import { shim } from './plugins/shim.ts';
-import { treeshake } from './plugins/treeshake.ts';
 
 export default function meteorPlugin(options: PluginOptions = {}): PluginOption {
 	const resolvedConfig = resolveConfig(options);
-	// This order is important
-	// replace must come first to replace constants used in other plugins
-	// treeshake must come after replace to remove code based on replaced constants
-	// shim must come after treeshake to add imports to the final code
-	// resolve and globals can come last to handle any remaining imports
-	return [replace, treeshake, shim, resolve, globals].map((plugin) => plugin(resolvedConfig));
+	return [globals].map((plugin) => plugin(resolvedConfig));
 }
 
 function resolveConfig(options: PluginOptions): ResolvedPluginOptions {
