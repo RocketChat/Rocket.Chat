4a045833ed7696c00ca698117a2db07b4c23c62e
gabriellsh <henriques.gabriell@gmail.com>
2026-02-23 09:59:20 -0300

    refactor: Extract `getAutocompleteOptions` from `MediaCallProvider` to it's own hook

--- packages/ui-voip/src/context/MediaCallInstanceProvider.tsx
+++ packages/ui-voip/src/context/MediaCallInstanceProvider.tsx
@@ -1,10 +1,10 @@
 import { Emitter } from '@rocket.chat/emitter';
-import { useEndpoint, useSetting, useUser, useUserAvatarPath } from '@rocket.chat/ui-contexts';
-import { useCallback, useMemo, useState, type ReactNode } from 'react';
+import { useUser } from '@rocket.chat/ui-contexts';
+import { useMemo, useState, type ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 import { MediaCallInstanceContext, type Signals } from './MediaCallInstanceContext';
-import { getExtensionFromInstanceContact } from './useMediaSession';
+import { useGetAutocompleteOptions } from './useGetAutocompleteOptions';
 import { useMediaSessionInstance } from './useMediaSessionInstance';
 import useMediaStream from './useMediaStream';
 
@@ -20,56 +20,7 @@ const MediaCallInstanceProvider = ({ children }: MediaCallInstanceProviderProps)
 
 	const [remoteStreamRefCallback, audioElement] = useMediaStream(instance);
 
-	const forceSIPRouting = useSetting('VoIP_TeamCollab_SIP_Integration_For_Internal_Calls');
-
-	const usersAutoCompleteEndpoint = useEndpoint('GET', '/v1/users.autocomplete');
-
-	const getAvatarPath = useUserAvatarPath();
-
-	const getAutocompleteOptions = useCallback(
-		async (filter: string) => {
-			if (!instance) {
-				return [];
-			}
-
-			const contact = instance.getMainCall()?.contact;
-
-			const peerUsername = contact && 'username' in contact ? contact.username : undefined;
-			const peerExtension = contact ? getExtensionFromInstanceContact(contact) : undefined;
-
-			const conditions =
-				peerExtension || forceSIPRouting
-					? {
-							$and: [
-								forceSIPRouting && { freeSwitchExtension: { $exists: true } },
-								peerExtension && { freeSwitchExtension: { $ne: peerExtension } },
-							].filter(Boolean),
-						}
-					: undefined;
-
-			const exceptions = [user?.username, peerUsername].filter(Boolean);
-
-			const { items } = await usersAutoCompleteEndpoint({
-				selector: JSON.stringify({ term: filter, exceptions, ...(conditions && { conditions }) }),
-			});
-			return (
-				items.map((user) => {
-					const label = user.name || user.username;
-					// TODO: This endpoint does not provide the extension number, which is necessary to show in the UI.
-					const identifier = user.username !== label ? user.username : undefined;
-
-					return {
-						value: user._id,
-						label,
-						identifier,
-						status: user.status,
-						avatarUrl: getAvatarPath({ username: user.username, etag: user.avatarETag }),
-					};
-				}) || []
-			);
-		},
-		[instance, user, forceSIPRouting, usersAutoCompleteEndpoint, getAvatarPath],
-	);
+	const getAutocompleteOptions = useGetAutocompleteOptions(instance);
 
 	const value = useMemo(
 		() => ({ instance, signalEmitter, audioElement, openRoomId, setOpenRoomId, getAutocompleteOptions }),
