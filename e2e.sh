be2d46a11e0820c7b5c50a719872688bb52484c9
Matheus Cardoso <matheus@cardo.so>
2026-02-23 17:31:20 -0300

    chore: update scripts

--- e2e.sh
+++ e2e.sh
@@ -3,23 +3,23 @@ export MONGO_URL='mongodb://localhost:27017/rocketchat?replicaSet=rs0&directConn
 ./docker-vite-ci.sh reset
 cd apps/meteor
 yarn prepare
-yarn test:e2e --shard 1/4
+E2E_COVERAGE=true yarn test:e2e --shard 1/4
 cd ../..
 
 ./docker-vite-ci.sh reset
 cd apps/meteor
 yarn prepare
-yarn test:e2e --shard 2/4
+E2E_COVERAGE=true yarn test:e2e --shard 2/4
 cd ../..
 
 ./docker-vite-ci.sh reset
 cd apps/meteor
 yarn prepare
-yarn test:e2e --shard 3/4
+E2E_COVERAGE=true yarn test:e2e --shard 3/4
 cd ../..
 
 ./docker-vite-ci.sh reset
 cd apps/meteor
 yarn prepare
-yarn test:e2e --shard 4/4
+E2E_COVERAGE=true yarn test:e2e --shard 4/4
 cd ../..
