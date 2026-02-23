722df6f60bc86c51b204e28a39acb3dc8710bdeb
Ahmed Nasser <ahmed.n.abdeltwab@gmail.com>
2026-02-23 23:40:17 +0200

    chore: Add OpenAPI Support to custom-user-status.list API (#36916)
    Co-authored-by: Guilherme Gazzo <guilherme@gazzo.xyz>

--- packages/core-typings/src/Ajv.ts
+++ packages/core-typings/src/Ajv.ts
@@ -2,6 +2,7 @@ import typia from 'typia';
 
 import type { CallHistoryItem } from './ICallHistoryItem';
 import type { ICustomSound } from './ICustomSound';
+import type { ICustomUserStatus } from './ICustomUserStatus';
 import type { IInvite } from './IInvite';
 import type { IMessage } from './IMessage';
 import type { IOAuthApps } from './IOAuthApps';
@@ -11,6 +12,11 @@ import type { SlashCommand } from './SlashCommands';
 import type { IMediaCall } from './mediaCalls/IMediaCall';
 
 export const schemas = typia.json.schemas<
-	[ISubscription | IInvite | ICustomSound | IMessage | IOAuthApps | IPermission | IMediaCall, CallHistoryItem, SlashCommand],
+	[
+		ISubscription | IInvite | ICustomSound | IMessage | IOAuthApps | IPermission | IMediaCall,
+		CallHistoryItem,
+		ICustomUserStatus,
+		SlashCommand,
+	],
 	'3.0'
 >();
