b3cd8694baedb0157bf65d1bfbf24ea5ee84e485
Pratheek555 <pratheekravikumar55@gmail.com>
2026-02-24 18:55:29 +0530

    Fixing lint issues

--- apps/meteor/client/views/omnichannel/additionalForms/CurrentChatTags.tsx
+++ apps/meteor/client/views/omnichannel/additionalForms/CurrentChatTags.tsx
@@ -17,15 +17,7 @@ const CurrentChatTags = ({ value, handler, department, viewAll, ...props }: Curr
 		return null;
 	}
 
-	return (
-		<AutoCompleteTagsMultiple
-			{...props}
-			onChange={handler}
-			value={value}
-			department={department}
-			viewAll={viewAll}
-		/>
-	);
+	return <AutoCompleteTagsMultiple {...props} onChange={handler} value={value} department={department} viewAll={viewAll} />;
 };
 
 export default CurrentChatTags;
