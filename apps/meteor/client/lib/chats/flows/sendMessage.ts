f6dce5f22e1cdc1518d4c105b53e7f5db87bfd53
Diego Sampaio <chinello@gmail.com>
2026-02-25 01:53:37 -0300

    fix: message being marked as sent before the request completes (#39003)

--- apps/meteor/client/lib/chats/flows/sendMessage.ts
+++ apps/meteor/client/lib/chats/flows/sendMessage.ts
@@ -1,14 +1,15 @@
 import type { IMessage } from '@rocket.chat/core-typings';
 
-import { sdk } from '../../../../app/utils/client/lib/SDKClient';
-import { t } from '../../../../app/utils/lib/i18n';
-import { onClientBeforeSendMessage } from '../../onClientBeforeSendMessage';
-import { dispatchToastMessage } from '../../toast';
-import type { ChatAPI } from '../ChatAPI';
 import { processMessageEditing } from './processMessageEditing';
 import { processSetReaction } from './processSetReaction';
 import { processSlashCommand } from './processSlashCommand';
 import { processTooLongMessage } from './processTooLongMessage';
+import { sdk } from '../../../../app/utils/client/lib/SDKClient';
+import { t } from '../../../../app/utils/lib/i18n';
+import { Messages } from '../../../stores';
+import { onClientBeforeSendMessage } from '../../onClientBeforeSendMessage';
+import { dispatchToastMessage } from '../../toast';
+import type { ChatAPI } from '../ChatAPI';
 
 const process = async (chat: ChatAPI, message: IMessage, previewUrls?: string[], isSlashCommandAllowed?: boolean): Promise<void> => {
 	const mid = chat.currentEditingMessage.getMID();
@@ -36,6 +37,12 @@ const process = async (chat: ChatAPI, message: IMessage, previewUrls?: string[],
 	}
 
 	await sdk.call('sendMessage', message, previewUrls);
+
+	// after the request is complete we can go ahead and mark as sent
+	Messages.state.update(
+		(record) => record._id === message._id && record.temp === true,
+		({ temp: _, ...record }) => record,
+	);
 };
 
 export const sendMessage = async (
