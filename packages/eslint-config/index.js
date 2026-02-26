c0b146f5e4f64e6391d746ec2086033a846a0ec2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-02-26 01:17:44 -0300

    chore(eslint): attach recommended typescript configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,6 +1,4 @@
 import eslint from '@eslint/js';
-import typescriptPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
 import { defineConfig } from 'eslint/config';
 import antiTrojanSourcePlugin from 'eslint-plugin-anti-trojan-source';
 import importPlugin from 'eslint-plugin-import';
@@ -10,8 +8,8 @@ import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import storybookPlugin from 'eslint-plugin-storybook';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default defineConfig(
 	{
 		name: 'rocket.chat/linter',
@@ -24,6 +22,7 @@ export default defineConfig(
 		ignores: ['**/dist', '**/coverage', '**/storybook-static'],
 	},
 	eslint.configs.recommended,
+	tseslint.configs.recommendedTypeChecked,
 	importPlugin.flatConfigs.recommended,
 	importPlugin.flatConfigs.typescript,
 	jsxA11yPlugin.flatConfigs.recommended,
@@ -97,27 +96,45 @@ export default defineConfig(
 	},
 	prettierPluginRecommended,
 	{
-		name: 'rocket.chat/javascript-syntax',
+		name: 'rocket.chat/ecmascript',
 		languageOptions: {
 			ecmaVersion: 2024,
 			sourceType: 'module',
 		},
 	},
-	// typescript files
 	{
-		files: ['**/*.@(ts|tsx|cts|mts)'],
-		languageOptions: {
-			parser: typescriptParser,
-			parserOptions: {
-				warnOnUnsupportedTypeScriptVersion: false,
-			},
-		},
-		plugins: {
-			'@typescript-eslint': typescriptPlugin,
+		name: 'rocket.chat/disable-typescript-rules-for-js',
+		files: ['**/*.@(js|jsx|mjs|cjs)'],
+		rules: {
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'@typescript-eslint/no-array-constructor': 'off',
+			'@typescript-eslint/no-duplicate-enum-values': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-extra-non-null-assertion': 'off',
+			'@typescript-eslint/no-misused-new': 'off',
+			'@typescript-eslint/no-namespace': 'off',
+			'@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-this-alias': 'off',
+			'@typescript-eslint/no-unnecessary-type-constraint': 'off',
+			'@typescript-eslint/no-unsafe-declaration-merging': 'off',
+			'@typescript-eslint/no-unsafe-function-type': 'off',
+			'@typescript-eslint/no-unused-expressions': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-wrapper-object-types': 'off',
+			'@typescript-eslint/prefer-as-const': 'off',
+			'@typescript-eslint/prefer-namespace-keyword': 'off',
+			'@typescript-eslint/triple-slash-reference': 'off',
 		},
 	},
-	// best practices
 	{
+		name: 'rocket.chat/disable-type-checked-rules-for-js',
+		files: ['**/*.@(js|jsx|mjs|cjs)'],
+		...tseslint.configs.disableTypeChecked,
+	},
+	{
+		name: 'rocket.chat/best-practices',
 		rules: {
 			'array-callback-return': ['error', { allowImplicit: true }],
 			'block-scoped-var': 'error',
@@ -175,8 +192,8 @@ export default defineConfig(
 			'yoda': 'error',
 		},
 	},
-	// prevent common mistakes
 	{
+		name: 'rocket.chat/common-mistakes',
 		rules: {
 			'getter-return': ['error', { allowImplicit: true }],
 			'no-async-promise-executor': 'warn',
@@ -193,17 +210,17 @@ export default defineConfig(
 			'valid-typeof': ['error', { requireStringLiterals: true }],
 		},
 	},
-	// Node.js and CommonJS globals
 	// TODO: disable, as they are not available in all environments
 	{
+		name: 'rocket.chat/node-globals',
 		languageOptions: {
 			globals: {
 				...globals.node,
 			},
 		},
 	},
-	// stylistic issues
 	{
+		name: 'rocket.chat/stylistic',
 		rules: {
 			'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: false }],
 			'lines-around-directive': [
@@ -227,8 +244,8 @@ export default defineConfig(
 			'spaced-comment': 'error',
 		},
 	},
-	// variables related rules
 	{
+		name: 'rocket.chat/variables',
 		rules: {
 			'no-unused-vars': [
 				'error',
@@ -241,8 +258,8 @@ export default defineConfig(
 			'no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
 		},
 	},
-	// ES2015 related rules
 	{
+		name: 'rocket.chat/es2015',
 		rules: {
 			'no-duplicate-imports': 'off',
 			'no-useless-computed-key': 'error',
@@ -284,8 +301,8 @@ export default defineConfig(
 			'prefer-template': 'error',
 		},
 	},
-	// import related rules
 	{
+		name: 'rocket.chat/import',
 		settings: {
 			'import/resolver': {
 				node: true,
@@ -328,7 +345,7 @@ export default defineConfig(
 		},
 	},
 	{
-		files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+		files: ['**/*.@(ts|tsx|cts|mts)'],
 		rules: {
 			'@typescript-eslint/no-empty-object-type': 'warn',
 			'@typescript-eslint/no-unsafe-function-type': 'warn',
@@ -384,14 +401,24 @@ export default defineConfig(
 					},
 				},
 			],
+			'@typescript-eslint/await-thenable': 'warn',
+			'@typescript-eslint/no-array-delete': 'warn',
+			'@typescript-eslint/no-base-to-string': 'warn',
 			'@typescript-eslint/no-dupe-class-members': 'error',
+			'@typescript-eslint/no-duplicate-enum-values': 'warn',
+			'@typescript-eslint/no-duplicate-type-constituents': 'warn',
 			'@typescript-eslint/no-empty-function': 'error',
 			'@typescript-eslint/no-empty-interface': 'warn',
 			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-for-in-array': 'warn',
+			'@typescript-eslint/no-implied-eval': 'warn',
 			'@typescript-eslint/no-non-null-assertion': 'warn',
 			'@typescript-eslint/no-redeclare': 'error',
 			'@typescript-eslint/no-redundant-type-constituents': 'off',
+			'@typescript-eslint/no-require-imports': 'warn',
 			'@typescript-eslint/no-this-alias': 'error',
+			'@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+			'@typescript-eslint/no-unused-expressions': 'warn',
 			'@typescript-eslint/no-unused-vars': [
 				'error',
 				{
@@ -401,9 +428,18 @@ export default defineConfig(
 			],
 			'@typescript-eslint/no-unsafe-argument': 'off',
 			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/no-unsafe-call': 'warn',
+			'@typescript-eslint/no-unsafe-declaration-merging': 'warn',
+			'@typescript-eslint/no-unsafe-enum-comparison': 'warn',
 			'@typescript-eslint/no-unsafe-member-access': 'off',
+			'@typescript-eslint/no-unsafe-return': 'warn',
+			'@typescript-eslint/only-throw-error': 'warn',
+			'@typescript-eslint/prefer-promise-reject-errors': 'warn',
 			'@typescript-eslint/prefer-optional-chain': 'warn',
 			'@typescript-eslint/require-await': 'off',
+			'@typescript-eslint/restrict-plus-operands': 'warn',
+			'@typescript-eslint/restrict-template-expressions': 'warn',
+			'@typescript-eslint/unbound-method': 'off',
 			'no-dupe-class-members': 'off',
 			'no-empty-function': 'off',
 			'no-redeclare': 'off',
@@ -412,6 +448,29 @@ export default defineConfig(
 			'no-use-before-define': 'off',
 			'no-useless-constructor': 'off',
 		},
+	},
+	{
+		files: [
+			'**/*.d.ts',
+			'**/__tests__/**',
+			'**/*.spec.ts',
+			'**/*.spec.tsx',
+			'**/*.test.ts',
+			'**/*.test.tsx',
+			'**/tests/**',
+			'**/.storybook/**',
+			'**/jest.config.ts',
+			'**/jest.config.js',
+			'**/jest.config.*.ts',
+			'**/jest.config.*.js',
+			'**/webpack.config.ts',
+			'**/webpack.config.js',
+			'**/vite.config.ts',
+			'**/vite.config.js',
+			'**/rollup.config.ts',
+			'**/rollup.config.js',
+		],
+		...tseslint.configs.disableTypeChecked,
 	},
 	{
 		files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
