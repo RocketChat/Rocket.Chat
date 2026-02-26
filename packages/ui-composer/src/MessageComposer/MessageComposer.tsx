80ede024ff7adbceb1b4ad346da8802a5794ba53
Hetsavani <het07savani@gmail.com>
2026-02-26 23:54:41 +0530

    chore: fix import order as per lint rules

--- packages/ui-composer/src/MessageComposer/MessageComposer.tsx
+++ packages/ui-composer/src/MessageComposer/MessageComposer.tsx
@@ -1,5 +1,5 @@
-import { Box, Palette } from '@rocket.chat/fuselage';
 import { css } from '@rocket.chat/css-in-js';
+import { Box, Palette } from '@rocket.chat/fuselage';
 import type { ElementType, HTMLAttributes, ReactElement, ReactNode } from 'react';
 import { forwardRef } from 'react';
 
@@ -16,30 +16,28 @@ const subtleFocus = css`
 	}
 `;
 
-const MessageComposer = forwardRef<HTMLElement, MessageComposerProps>(
-	function MessageComposer(
-		{ variant, className, isBlur = false, children, ...props },
-		ref,
-	): ReactElement {
-		return (
-			<Box
-				rcx-input-box__wrapper
-				{...props}
-				className={[subtleFocus, className, isBlur && 'is-blur']}
-				mbs={2}
-				bg={variant === 'editing' ? 'status-background-warning-2' : undefined}
-				ref={ref}
-				role='group'
-				display='flex'
-				flexDirection='column'
-				overflow='hidden'
-				p={0}
-			>
-				{children}
-			</Box>
-		);
-	},
-);
+const MessageComposer = forwardRef<HTMLElement, MessageComposerProps>(function MessageComposer(
+	{ variant, className, isBlur = false, children, ...props },
+	ref,
+): ReactElement {
+	return (
+		<Box
+			rcx-input-box__wrapper
+			{...props}
+			className={[subtleFocus, className, isBlur && 'is-blur']}
+			mbs={2}
+			bg={variant === 'editing' ? 'status-background-warning-2' : undefined}
+			ref={ref}
+			role='group'
+			display='flex'
+			flexDirection='column'
+			overflow='hidden'
+			p={0}
+		>
+			{children}
+		</Box>
+	);
+});
 
 export default MessageComposer;
 // import { Box, Palette } from '@rocket.chat/fuselage';
