075af9c1600b0b449c906d1838a7b06c33095675
Harshit2405-2004 <virmaniharshit27@gmail.com>
2026-03-01 23:40:57 +0530

    feat(api): Enforce Schema Validation on groups.list and groups.listAll

--- packages/rest-typings/src/v1/groups/GroupsListProps.ts
+++ packages/rest-typings/src/v1/groups/GroupsListProps.ts
@@ -1,6 +1,34 @@
 import type { PaginatedRequest } from '../../helpers/PaginatedRequest';
 import { ajv } from '../Ajv';
 
-export type GroupsListProps = PaginatedRequest<null>;
-const groupsListPropsSchema = {};
+export type GroupsListProps = PaginatedRequest<{ name?: string }>;
+
+const groupsListPropsSchema = {
+    type: 'object',
+    properties: {
+        count: {
+            type: 'number',
+            nullable: true,
+        },
+        offset: {
+            type: 'number',
+            nullable: true,
+        },
+        sort: {
+            type: 'string',
+            nullable: true,
+        },
+        fields: {
+            type: 'string',
+            nullable: true,
+        },
+        query: {
+            type: 'string',
+            nullable: true,
+        },
+    },
+    required: [],
+    additionalProperties: false,
+};
+
 export const isGroupsListProps = ajv.compile<GroupsListProps>(groupsListPropsSchema);
