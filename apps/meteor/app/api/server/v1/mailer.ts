e42dc7698ac2f07925b2237d7cf54809148b3d27
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-03-02 19:10:30 -0300

    refactor: update API routes to use AJV for validation and improve response schemas

--- apps/meteor/app/api/server/v1/mailer.ts
+++ apps/meteor/app/api/server/v1/mailer.ts
@@ -1,41 +1,66 @@
-import { isMailerProps, isMailerUnsubscribeProps } from '@rocket.chat/rest-typings';
+import {
+	ajv,
+	isMailerProps,
+	isMailerUnsubscribeProps,
+	validateForbiddenErrorResponse,
+	validateUnauthorizedErrorResponse,
+} from '@rocket.chat/rest-typings';
 
 import { sendMail } from '../../../mail-messages/server/functions/sendMail';
 import { Mailer } from '../../../mail-messages/server/lib/Mailer';
 import { API } from '../api';
 
-API.v1.addRoute(
+API.v1.post(
 	'mailer',
 	{
 		authRequired: true,
-		validateParams: isMailerProps,
+		body: isMailerProps,
 		permissionsRequired: ['send-mail'],
-	},
-	{
-		async post() {
-			const { from, subject, body, dryrun, query } = this.bodyParams;
-
-			const result = await sendMail({ from, subject, body, dryrun: Boolean(dryrun), query });
-
-			return API.v1.success(result);
+		response: {
+			200: ajv.compile({
+				type: 'object',
+				properties: {
+					success: { type: 'boolean', enum: [true] },
+				},
+				required: ['success'],
+				additionalProperties: true,
+			}),
+			401: validateUnauthorizedErrorResponse,
+			403: validateForbiddenErrorResponse,
 		},
+	},
+	async function action() {
+		const { from, subject, body, dryrun, query } = this.bodyParams;
+
+		const result = await sendMail({ from, subject, body, dryrun: Boolean(dryrun), query });
+
+		return API.v1.success(result);
 	},
 );
 
-API.v1.addRoute(
+API.v1.post(
 	'mailer.unsubscribe',
 	{
 		authRequired: true,
-		validateParams: isMailerUnsubscribeProps,
+		body: isMailerUnsubscribeProps,
 		rateLimiterOptions: { intervalTimeInMS: 60000, numRequestsAllowed: 1 },
-	},
-	{
-		async post() {
-			const { _id, createdAt } = this.bodyParams;
-
-			await Mailer.unsubscribe(_id, createdAt);
-
-			return API.v1.success();
+		response: {
+			200: ajv.compile({
+				type: 'object',
+				properties: {
+					success: { type: 'boolean', enum: [true] },
+				},
+				required: ['success'],
+				additionalProperties: false,
+			}),
+			401: validateUnauthorizedErrorResponse,
 		},
+	},
+	async function action() {
+		const { _id, createdAt } = this.bodyParams;
+
+		await Mailer.unsubscribe(_id, createdAt);
+
+		return API.v1.success();
 	},
 );
