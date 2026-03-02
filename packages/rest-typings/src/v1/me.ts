9f3ec130e3211c767fc0aa5fc5c9ba77558c83ac
Yashika soni <soniyashika164@gmail.com>
2026-03-02 20:20:34 +0530

    Delete packages/rest-typings/src/v1/me.ts

--- packages/rest-typings/src/v1/me.ts
+++
@@ -1,8 +0,0 @@
-import type { MeEndpoints as MiscMeEndpoints } from '@rocket.chat/meteor/app/api/server/v1/misc';
-import { ExtractRoutesFromAPI } from '@rocket.chat/rest-typings';
-
-declare module '@rocket.chat/rest-typings' {
-  interface Endpoints extends ExtractRoutesFromAPI<MiscMeEndpoints> {}
-}
-
-export {};
