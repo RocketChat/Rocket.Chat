72cf7055b22c908fb7e344bff1efc815af6fc622
gabriellsh <henriques.gabriell@gmail.com>
2026-03-02 15:17:53 -0300

    Implement on widget

--- packages/ui-voip/src/views/MediaCallWidget/OngoingCall.tsx
+++ packages/ui-voip/src/views/MediaCallWidget/OngoingCall.tsx
@@ -16,15 +16,23 @@ import {
 	useKeypad,
 	useInfoSlots,
 } from '../../components';
+import StreamCard from '../../components/Cards/StreamCard';
 import { useMediaCallView } from '../../context/MediaCallViewContext';
+import { usePlayMediaStream } from '../../providers/usePlayMediaStream';
 
 const OngoingCall = () => {
 	const { t } = useTranslation();
 
-	const { sessionState, onMute, onHold, onForward, onEndCall, onTone, onClickDirectMessage } = useMediaCallView();
+	const { sessionState, onMute, onHold, onForward, onEndCall, onTone, onClickDirectMessage, streams, onToggleScreenSharing } =
+		useMediaCallView();
 	const { muted, held, remoteMuted, remoteHeld, peerInfo, connectionState } = sessionState;
 
+	const { localScreen, remoteScreen } = streams;
+
 	const { element: keypad, buttonProps: keypadButtonProps } = useKeypad(onTone);
+
+	const [remoteStreamRefCallback] = usePlayMediaStream(remoteScreen?.stream ?? null);
+	const [localStreamRefCallback] = usePlayMediaStream(localScreen?.stream ?? null);
 
 	const slots = useInfoSlots(muted, held, connectionState);
 	const remoteSlots = useInfoSlots(remoteMuted, remoteHeld);
@@ -48,13 +56,31 @@ const OngoingCall = () => {
 			</WidgetHeader>
 			<WidgetContent>
 				<PeerInfo {...peerInfo} slots={remoteSlots} remoteMuted={remoteMuted} />
+				{/* TODO: Cannot get user info right here, I'm avoiding to other contexts, so it has to come from VoipContext */}
+				{localScreen?.active && (
+					<StreamCard displayName={t('You')} own autoHeight maxHeight={120}>
+						<video preload='metadata' style={{ objectFit: 'contain', height: '100%', width: '100%' }} ref={localStreamRefCallback}>
+							<track kind='captions' />
+						</video>
+					</StreamCard>
+				)}
+				{remoteScreen?.active && (
+					<StreamCard displayName={'displayName' in peerInfo ? peerInfo.displayName : ''} autoHeight maxHeight={120}>
+						<video preload='metadata' style={{ objectFit: 'contain', height: '100%', width: '100%' }} ref={remoteStreamRefCallback}>
+							<track kind='captions' />
+						</video>
+					</StreamCard>
+				)}
 			</WidgetContent>
 			<WidgetInfo slots={slots} />
 			<WidgetFooter>
 				{keypad}
 				<ButtonGroup large>
-					<ActionButton disabled={connecting || reconnecting} icon='dialpad' label='Dialpad' {...keypadButtonProps} />
+					{'number' in peerInfo && (
+						<ActionButton disabled={connecting || reconnecting} icon='dialpad' label='Dialpad' {...keypadButtonProps} />
+					)}
 					<ToggleButton label={t('Mute')} icons={['mic', 'mic-off']} titles={[t('Mute'), t('Unmute')]} pressed={muted} onToggle={onMute} />
+
 					<ToggleButton
 						label={t('Hold')}
 						icons={['pause-shape-unfilled', 'pause-shape-unfilled']}
@@ -62,6 +88,15 @@ const OngoingCall = () => {
 						pressed={held}
 						onToggle={onHold}
 					/>
+					{'username' in peerInfo && (
+						<ToggleButton
+							label={t('Screen_sharing')}
+							icons={['computer', 'computer']}
+							titles={[t('Screen_sharing'), t('Screen_sharing_off')]}
+							pressed={localScreen?.active ?? false}
+							onToggle={onToggleScreenSharing}
+						/>
+					)}
 					<ActionButton disabled={connecting || reconnecting} label={t('Forward')} icon='arrow-forward' onClick={onForward} />
 					<ActionButton
 						label={t('Voice_call__user__hangup', { user: 'userId' in peerInfo ? peerInfo.displayName : peerInfo.number })}
