ce6d91331ea945f17d4ca620d6611e06538f4fbc
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-03-03 03:38:47 -0300

    refactor(ui-kit): use only erasable syntax

--- packages/ui-kit/src/rendering/BlockContext.ts
+++ packages/ui-kit/src/rendering/BlockContext.ts
@@ -1,8 +1,11 @@
-export enum BlockContext {
-	NONE = -1,
-	BLOCK,
-	SECTION,
-	ACTION,
-	FORM,
-	CONTEXT,
-}
+export const BlockContext = {
+	NONE: Symbol('none'),
+	BLOCK: Symbol('block'),
+	SECTION: Symbol('section'),
+	ACTION: Symbol('action'),
+	FORM: Symbol('form'),
+	CONTEXT: Symbol('context'),
+} as const;
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type BlockContext = (typeof BlockContext)[keyof typeof BlockContext];
