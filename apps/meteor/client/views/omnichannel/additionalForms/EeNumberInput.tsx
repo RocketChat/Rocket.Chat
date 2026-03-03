43296cf6b7a3d1e3e17ba28c264a69a630dd9854
CodeMatrix1 <codematrix1@users.noreply.github.com>
2026-03-04 00:44:43 +0530

    Restricting NumberInput to only values above 0

--- apps/meteor/client/views/omnichannel/additionalForms/EeNumberInput.tsx
+++ apps/meteor/client/views/omnichannel/additionalForms/EeNumberInput.tsx
@@ -1,20 +1,29 @@
 import { NumberInput, Field, FieldLabel, FieldRow } from '@rocket.chat/fuselage';
-import type { ComponentProps } from 'react';
+import type { ChangeEvent, ComponentProps } from 'react';
 
 import { useHasLicenseModule } from '../../../hooks/useHasLicenseModule';
 
 export const EeNumberInput = ({ label, ...props }: { label: string } & ComponentProps<typeof NumberInput>) => {
 	const { data: hasLicense = false } = useHasLicenseModule('livechat-enterprise');
+	const { onChange, min, ...rest } = props;
 
 	if (!hasLicense) {
 		return null;
 	}
 
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (Number(event.currentTarget.value) < 0) {
+			event.currentTarget.value = '0';
+		}
+
+		onChange?.(event);
+	};
+
 	return (
 		<Field>
 			<FieldLabel>{label}</FieldLabel>
 			<FieldRow>
-				<NumberInput {...props} flexGrow={1} />
+				<NumberInput {...rest} min={min ?? 0} onChange={handleChange} flexGrow={1} />
 			</FieldRow>
 		</Field>
 	);
