6cd8093eca48dac9461eee84ad637571316ad0a5
Matheus Cardoso <matheus@cardo.so>
2026-03-04 11:29:20 -0300

    feat: conditional fips mode

---
+++ ee/apps/ddp-streamer/src/fips.ts
@@ -0,0 +1,7 @@
+import crypto from 'crypto';
+
+crypto.setFips(true);
+
+console.log('=========================================');
+console.log('FIPS COMPLIANCE CHECK: YES');
+console.log('=========================================');
