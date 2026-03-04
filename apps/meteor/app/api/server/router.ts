8427af0e17ae48b69bf04cc6a0928606db0aefe9
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-03-04 17:05:20 -0300

    refactor(api): streamline authentication handling in APIClass
    - Removed commented-out code related to user authentication and authorization checks for clarity.
    - Updated the handling of the authentication token to improve readability and maintainability.
    - Cleaned up unused imports to enhance code quality.

--- apps/meteor/app/api/server/router.ts
+++ apps/meteor/app/api/server/router.ts
@@ -1,17 +1,20 @@
 import type { IncomingMessage } from 'node:http';
 
+import type { IUser } from '@rocket.chat/core-typings';
 import type { ResponseSchema } from '@rocket.chat/http-router';
 import { Router } from '@rocket.chat/http-router';
+import { type Logger } from '@rocket.chat/logger';
 import type { Context } from 'hono';
 
 import type { TypedOptions } from './definition';
 
-type HonoContext = Context<{
+export type HonoContext = Context<{
 	Bindings: { incoming: IncomingMessage };
 	Variables: {
 		remoteAddress: string;
 		bodyParams: Record<string, unknown>;
 		queryParams: Record<string, unknown>;
+		user?: IUser | null;
 	};
 }>;
 
@@ -25,6 +28,7 @@ export type APIActionContext = {
 	response: any;
 	route: string;
 	incoming: IncomingMessage;
+	logger: Logger;
 };
 
 export type APIActionHandler = (this: APIActionContext, request: Request) => Promise<ResponseSchema<TypedOptions>>;
@@ -35,28 +39,43 @@ export class RocketChatAPIRouter<
 		[x: string]: unknown;
 	} = NonNullable<unknown>,
 > extends Router<TBasePath, TOperations, APIActionHandler> {
-	protected override convertActionToHandler(action: APIActionHandler): (c: HonoContext) => Promise<ResponseSchema<TypedOptions>> {
+	protected override convertActionToHandler(
+		action: APIActionHandler,
+		logger: Logger,
+	): (c: HonoContext) => Promise<ResponseSchema<TypedOptions>> {
 		return async (c: HonoContext): Promise<ResponseSchema<TypedOptions>> => {
-			const { req, res } = c;
+			const { req } = c;
 
 			const request = req.raw.clone();
 
-			const context: APIActionContext = {
-				requestIp: c.get('remoteAddress'),
-				urlParams: req.param(),
-				queryParams: c.get('queryParams'),
-				bodyParams: c.get('bodyParams'),
-				request,
-				path: req.path,
-				response: res,
-				route: req.routePath,
-				incoming: c.env.incoming,
-			};
+			const context = convertHonoContextToApiActionContext(c, { logger });
 
 			return action.apply(context, [request]);
 		};
 	}
 }
+
+export const convertHonoContextToApiActionContext = (
+	c: HonoContext,
+	options: {
+		logger: Logger;
+	},
+): APIActionContext => {
+	const user = c.get('user');
+	return {
+		requestIp: c.get('remoteAddress'),
+		urlParams: c.req.param(),
+		queryParams: c.get('queryParams'),
+		bodyParams: c.get('bodyParams'),
+		request: c.req.raw.clone(),
+		path: c.req.path,
+		response: c.res,
+		route: c.req.routePath,
+		incoming: c.env.incoming,
+		logger: options.logger,
+		...(user && { user, userId: user._id }),
+	};
+};
 
 export type ExtractRouterEndpoints<TRoute extends RocketChatAPIRouter<any, any>> =
 	TRoute extends RocketChatAPIRouter<any, infer TOperations> ? TOperations : never;
