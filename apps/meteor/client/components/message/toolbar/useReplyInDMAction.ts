4686fecaa9afbc74d1bc0e078c4b1507fb5fda12
Kush Kumar <kushkumarking098765@gmail.com>
2026-03-07 11:29:18 +0000

    fix: restore 'Reply in DM' action in web client (fixes #39434)

--- apps/meteor/client/components/message/toolbar/useReplyInDMAction.ts
+++ apps/meteor/client/components/message/toolbar/useReplyInDMAction.ts
@@ -1,13 +1,11 @@
 import { type IMessage, type ISubscription, type IRoom, isE2EEMessage } from '@rocket.chat/core-typings';
 import { useEmbeddedLayout } from '@rocket.chat/ui-client';
 import { usePermission, useRouter, useUser } from '@rocket.chat/ui-contexts';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useShallow } from 'zustand/shallow';
 
 import type { MessageActionConfig } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { roomCoordinator } from '../../../lib/rooms/roomCoordinator';
-import { Rooms, Subscriptions } from '../../../stores';
 
 export const useReplyInDMAction = (
 	message: IMessage,
@@ -20,23 +18,6 @@ export const useReplyInDMAction = (
 	const canCreateDM = usePermission('create-d');
 	const isLayoutEmbedded = useEmbeddedLayout();
 	const { t } = useTranslation();
-
-	const roomPredicate = useCallback(
-		(record: IRoom): boolean => {
-			const ids = [user?._id, message.u._id].sort().join('');
-			return ids.includes(record._id);
-		},
-		[message.u._id, user],
-	);
-
-	const shouldFindRoom = useMemo(() => !!user && canCreateDM && user._id !== message.u._id, [canCreateDM, message.u._id, user]);
-	const dmRoom = Rooms.use(useShallow((state) => (shouldFindRoom ? state.find(roomPredicate) : undefined)));
-
-	const subsPredicate = useCallback(
-		(record: ISubscription) => record.rid === dmRoom?._id || record.u._id === user?._id,
-		[dmRoom, user?._id],
-	);
-	const dmSubs = Subscriptions.use(useShallow((state) => state.find(subsPredicate)));
 
 	const tooltip = useMemo(() => {
 		if (encrypted) {
@@ -52,13 +33,11 @@ export const useReplyInDMAction = (
 		if (!subscription || room.t === 'd' || room.t === 'l' || isLayoutEmbedded) {
 			return false;
 		}
-		if (!!user && user._id !== message.u._id && canCreateDM) {
-			if (!dmRoom || !dmSubs) {
-				return false;
-			}
+		if (!user || user._id === message.u._id || !canCreateDM) {
+			return false;
 		}
 		return true;
-	}, [canCreateDM, dmRoom, dmSubs, isLayoutEmbedded, message.u._id, room.t, subscription, user]);
+	}, [canCreateDM, isLayoutEmbedded, message.u._id, room.t, subscription, user]);
 
 	if (!canReplyInDM) {
 		return null;
