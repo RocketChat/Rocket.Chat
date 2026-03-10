9283c199d6fc93e3225078c65927439d01dcf734
gabriellsh <henriques.gabriell@gmail.com>
2026-03-10 17:32:14 -0300

    Fix lint

--- packages/media-signaling/src/definition/index.ts
+++ packages/media-signaling/src/definition/index.ts
@@ -1,6 +1,6 @@
 export * from './call';
-export * from './services';
-export * from './media';
+export type * from './services';
+export type * from './media';
 export * from './signals';
 export type * from './client';
 export type * from './logger';
