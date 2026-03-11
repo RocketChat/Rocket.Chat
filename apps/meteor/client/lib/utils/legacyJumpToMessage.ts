3342e6bdae8bb8dd3fadf247a03659df712397cb
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-03-11 13:22:25 -0300

    refactor: remove "out-of-React-cycle" helper function

--- apps/meteor/client/lib/utils/legacyJumpToMessage.ts
+++ apps/meteor/client/lib/utils/legacyJumpToMessage.ts
@@ -1,10 +1,34 @@
-import type { IMessage } from '@rocket.chat/core-typings';
 import { isThreadMessage } from '@rocket.chat/core-typings';
+import type { IMessage, IRoom } from '@rocket.chat/core-typings';
+import { memoize } from '@rocket.chat/memo';
 
-import { goToRoomById } from './goToRoomById';
+import { callWithErrorHandling } from './callWithErrorHandling';
 import { RoomHistoryManager } from '../../../app/ui-utils/client';
 import { router } from '../../providers/RouterProvider';
+import { Subscriptions } from '../../stores';
 import { RoomManager } from '../RoomManager';
+import { roomCoordinator } from '../rooms/roomCoordinator';
+
+const getRoomById = memoize((rid: IRoom['_id']) => callWithErrorHandling('getRoomById', rid));
+
+type GoToRoomByIdOptions = {
+	replace?: boolean;
+	routeParamsOverrides?: Record<string, string>;
+};
+
+const goToRoomById = async (roomId: IRoom['_id'], options: GoToRoomByIdOptions = {}): Promise<void> => {
+	if (!roomId) return;
+
+	const subscription = Subscriptions.state.find((record) => record.rid === roomId);
+
+	if (subscription) {
+		roomCoordinator.openRouteLink(subscription.t, subscription, router.getSearchParameters(), options);
+		return;
+	}
+
+	const room = await getRoomById(roomId);
+	roomCoordinator.openRouteLink(room.t, { rid: room._id, ...room }, router.getSearchParameters(), options);
+};
 
 /** @deprecated */
 export const legacyJumpToMessage = async (message: IMessage) => {
