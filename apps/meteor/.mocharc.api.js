00057827c9dfa7b65b6f95e339b3bc6a86ce2505
Guilherme Gazzo <guilhermegazzo@gmail.com>
2026-03-12 11:49:08 -0300

    chore: updade flags

--- apps/meteor/.mocharc.api.js
+++ apps/meteor/.mocharc.api.js
@@ -7,7 +7,7 @@
 module.exports = /** @satisfies {import('mocha').MochaOptions} */ ({
 	...require('./.mocharc.base.json'), // see https://github.com/mochajs/mocha/issues/3916
 	timeout: 10000,
-	bail: true,
+	bail: false,
 	retries: 0,
 	file: 'tests/end-to-end/teardown.ts',
 	spec: ['tests/end-to-end/api/*.ts', 'tests/end-to-end/api/helpers/**/*', 'tests/end-to-end/api/methods/**/*', 'tests/end-to-end/apps/*'],
