e25b51998d9df970c53b56a74093e4b51d67a369
Matheus Cardoso <matheus@cardo.so>
2026-03-16 15:43:34 -0300

    chore: reduce logs

--- ee/apps/authorization-service/src/fips.ts
+++ ee/apps/authorization-service/src/fips.ts
@@ -6,6 +6,4 @@ if (!crypto.getFips()) {
 	throw new Error('FIPS mode was not enabled after crypto.setFips(true)');
 }
 
-console.log('=========================================');
 console.log('FIPS COMPLIANCE CHECK: YES');
-console.log('=========================================');
