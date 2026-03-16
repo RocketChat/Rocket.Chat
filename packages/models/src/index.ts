35f9d6d444cb8cae75661d55773d3603947aeb56
Kevin  Aleman <kaleman960@gmail.com>
2026-03-16 15:12:23 -0600

    idle

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -118,6 +118,8 @@ export * from './modelClasses';
 
 export * from './dummy/ReadReceipts';
 
+export * from './lib/agentOnline';
+
 export { registerModel } from './proxify';
 export { type Updater, UpdaterImpl } from './updater';
 
