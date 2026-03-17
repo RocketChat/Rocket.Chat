2782705f18ab72c9c12f8d7aba1d1d2f1b122703
gabriellsh <henriques.gabriell@gmail.com>
2026-03-17 20:17:40 -0300

    fix: Limit CardListSection max height

--- packages/ui-voip/src/components/Cards/CardListSection.tsx
+++ packages/ui-voip/src/components/Cards/CardListSection.tsx
@@ -9,7 +9,7 @@ export const SECTION_MAX_HEIGHT = 50;
 
 const CardListSection = ({ children }: CardListSectionProps) => {
 	return (
-		<Box display='flex' flexGrow={1} flexShrink={1} overflow='auto' justifyContent='stretch' alignItems='center' p={2}>
+		<Box display='flex' flexGrow={1} flexShrink={1} overflow='auto' justifyContent='stretch' alignItems='center' p={2} maxHeight='full'>
 			{children}
 		</Box>
 	);
