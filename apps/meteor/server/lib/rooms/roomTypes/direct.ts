1ff36b0b243f0940dff4c14554096c7f8981b2b2
CodeMatrix1 <codematrix1@users.noreply.github.com>
2026-03-17 20:55:36 +0530

    refactor(server): rename useRealName to shouldUseRealName for clarity

--- apps/meteor/server/lib/rooms/roomTypes/direct.ts
+++ apps/meteor/server/lib/rooms/roomTypes/direct.ts
@@ -88,20 +88,20 @@ roomCoordinator.add(DirectMessageRoomType, {
 	},
 
 	async getNotificationDetails(room, sender, notificationMessage, userId) {
-		const useRealName = settings.get<boolean>('UI_Use_Real_Name');
+		const shouldUseRealName = settings.get<boolean>('UI_Use_Real_Name');
 
 		const displayRoomName = await this.roomName(room, userId);
 
 		if (this.isGroupChat(room)) {
 			return {
 				title: displayRoomName,
-				text: `${(useRealName && sender.name) || sender.username}: ${notificationMessage}`,
+				text: `${(shouldUseRealName && sender.name) || sender.username}: ${notificationMessage}`,
 				name: room.name || displayRoomName,
 			};
 		}
 
 		return {
-			title: (useRealName && sender.name) || sender.username,
+			title: (shouldUseRealName && sender.name) || sender.username,
 			text: notificationMessage,
 			name: room.name || displayRoomName,
 		};
