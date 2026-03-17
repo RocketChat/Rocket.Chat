c7878360919bc4fc23b7f4208498d973060d3d74
sezallagwal <sezal1470@gmail.com>
2026-03-17 02:44:28 +0000

    fix: move GetPinnedMessages schema to chat.ts

--- apps/meteor/app/api/server/v1/chat.ts
+++ apps/meteor/app/api/server/v1/chat.ts
@@ -274,6 +274,39 @@ const isChatPinMessageProps = ajv.compile<ChatPinMessage>(ChatPinMessageSchema);
 
 const isChatUnpinMessageProps = ajv.compile<ChatUnpinMessage>(ChatUnpinMessageSchema);
 
+type GetPinnedMessages = {
+	roomId: string;
+	count?: number;
+	offset?: number;
+	sort?: string;
+};
+
+const GetPinnedMessagesSchema = {
+	type: 'object',
+	properties: {
+		roomId: {
+			type: 'string',
+			minLength: 1,
+		},
+		count: {
+			type: 'number',
+			nullable: true,
+		},
+		offset: {
+			type: 'number',
+			nullable: true,
+		},
+		sort: {
+			type: 'string',
+			nullable: true,
+		},
+	},
+	required: ['roomId'],
+	additionalProperties: false,
+};
+
+const isChatGetPinnedMessagesProps = ajv.compile<GetPinnedMessages>(GetPinnedMessagesSchema);
+
 const chatEndpoints = API.v1
 	.post(
 		'chat.pinMessage',
@@ -562,29 +595,7 @@ const chatEndpoints = API.v1
 		'chat.getPinnedMessages',
 		{
 			authRequired: true,
-			query: ajv.compile<{ roomId: string; count?: number; offset?: number; sort?: string }>({
-				type: 'object',
-				properties: {
-					roomId: {
-						type: 'string',
-						minLength: 1,
-					},
-					count: {
-						type: 'number',
-						nullable: true,
-					},
-					offset: {
-						type: 'number',
-						nullable: true,
-					},
-					sort: {
-						type: 'string',
-						nullable: true,
-					},
-				},
-				required: ['roomId'],
-				additionalProperties: false,
-			}),
+			query: isChatGetPinnedMessagesProps,
 			response: {
 				200: ajv.compile<{ messages: IMessage[]; count: number; offset: number; total: number }>({
 					type: 'object',
