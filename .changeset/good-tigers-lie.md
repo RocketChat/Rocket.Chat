11e31515efbec87a3dd26b9d7dd778b82de70bca
Harxhit <harsxit04@gmail.com>
2026-03-20 14:17:48 +0530

    fix: Added changeset

---
+++ .changeset/good-tigers-lie.md
@@ -0,0 +1,6 @@
+---
+'@rocket.chat/rest-typings': minor
+'@rocket.chat/meteor': minor
+---
+
+Migrated chat.syncThreadMessages old Rest Api strucutre to new typed rest (openAPI) structure
--- packages/rest-typings/src/v1/chat.ts
+++ packages/rest-typings/src/v1/chat.ts
@@ -638,41 +638,6 @@ const ChatSyncMessagesSchema = {
 
 export const isChatSyncMessagesProps = ajvQuery.compile<ChatSyncMessages>(ChatSyncMessagesSchema);
 
-type ChatSyncThreadMessages = PaginatedRequest<{
-	tmid: string;
-	updatedSince: string;
-}>;
-
-const ChatSyncThreadMessagesSchema = {
-	type: 'object',
-	properties: {
-		tmid: {
-			type: 'string',
-			minLength: 1,
-		},
-		updatedSince: {
-			type: 'string',
-			format: 'iso-date-time',
-		},
-		count: {
-			type: 'number',
-			nullable: true,
-		},
-		offset: {
-			type: 'number',
-			nullable: true,
-		},
-		sort: {
-			type: 'string',
-			nullable: true,
-		},
-	},
-	required: ['tmid', 'updatedSince'],
-	additionalProperties: false,
-};
-
-export const isChatSyncThreadMessagesProps = ajvQuery.compile<ChatSyncThreadMessages>(ChatSyncThreadMessagesSchema);
-
 type ChatGetThreadMessages = PaginatedRequest<{
 	tmid: string;
 }>;
@@ -987,14 +952,6 @@ export type ChatEndpoints = {
 			ts: number;
 			channel: IRoom;
 			message: IMessage;
-		};
-	};
-	'/v1/chat.syncThreadMessages': {
-		GET: (params: ChatSyncThreadMessages) => {
-			messages: {
-				update: IMessage[];
-				remove: IMessage[];
-			};
 		};
 	};
 	'/v1/chat.getThreadMessages': {
