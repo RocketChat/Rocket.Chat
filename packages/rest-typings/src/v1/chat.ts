7e0e519997caa14dc9ab8359875b413ceb0e46f3
rkbansal <rohitkumarbansal23@gmail.com>
2026-03-21 15:36:11 +0530

    fix(REST): make roomId optional in chat.getMentionedMessages & chat.getStarredMessages

--- packages/rest-typings/src/v1/chat.ts
+++ packages/rest-typings/src/v1/chat.ts
@@ -497,7 +497,7 @@ const ChatGetMessageReadReceiptsSchema = {
 export const isChatGetMessageReadReceiptsProps = ajv.compile<ChatGetMessageReadReceipts>(ChatGetMessageReadReceiptsSchema);
 
 type GetStarredMessages = {
-	roomId: IRoom['_id'];
+	roomId?: IRoom['_id'];
 	count?: number;
 	offset?: number;
 	sort?: string;
@@ -523,7 +523,9 @@ const GetStarredMessagesSchema = {
 			nullable: true,
 		},
 	},
-	required: ['roomId'],
+	// roomId is optional to allow global fetch across user subscriptions
+	// (Activity Hub). Validation will accept requests without roomId.
+	required: [],
 	additionalProperties: false,
 };
 
@@ -563,7 +565,7 @@ const GetPinnedMessagesSchema = {
 export const isChatGetPinnedMessagesProps = ajv.compile<GetPinnedMessages>(GetPinnedMessagesSchema);
 
 type GetMentionedMessages = {
-	roomId: IRoom['_id'];
+	roomId?: IRoom['_id'];
 	count?: number;
 	offset?: number;
 	sort?: string;
@@ -589,7 +591,9 @@ const GetMentionedMessagesSchema = {
 			nullable: true,
 		},
 	},
-	required: ['roomId'],
+	// roomId is optional to allow global fetch across user subscriptions
+	// (Activity Hub). Validation will accept requests without roomId.
+	required: [],
 	additionalProperties: false,
 };
 
