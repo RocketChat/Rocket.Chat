dfca7a1ed4091f2d091b12d4fd4730ba6ff86caa
Kevin  Aleman <kaleman960@gmail.com>
2026-03-23 13:20:28 -0600

    smol

--- apps/meteor/ee/server/lib/ldap/Manager.ts
+++ apps/meteor/ee/server/lib/ldap/Manager.ts
@@ -109,7 +109,8 @@ export class LDAPEEManager extends LDAPManager {
 			!settings.get('LDAP_Enable') ||
 			!settings.get('LDAP_Background_Sync_ABAC_Attributes') ||
 			!License.hasModule('abac') ||
-			!settings.get('ABAC_Enabled')
+			!settings.get('ABAC_Enabled') ||
+			settings.get('ABAC_PDP_Type') === 'external'
 		) {
 			return;
 		}
@@ -129,7 +130,7 @@ export class LDAPEEManager extends LDAPManager {
 	}
 
 	public static async syncUsersAbacAttributes(users: FindCursor<IUser>): Promise<void> {
-		if (!settings.get('LDAP_Enable') || !License.hasModule('abac') || !settings.get('ABAC_Enabled')) {
+		if (!settings.get('LDAP_Enable') || !License.hasModule('abac') || !settings.get('ABAC_Enabled') || settings.get('ABAC_PDP_Type') === 'external') {
 			return;
 		}
 
