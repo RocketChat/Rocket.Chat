ed6ca9abbfad3e92cfb7cfa8476e9a0952866504
Jayesh Kathale <jayeshkathale24@gmail.com>
2026-03-24 10:38:11 +0530

    fix(agenda): correct off-by-one error in returnNextConcurrencyFreeJob

--- packages/agenda/src/JobProcessingQueue.ts
+++ packages/agenda/src/JobProcessingQueue.ts
@@ -60,7 +60,7 @@ export class JobProcessingQueue {
 	 */
 	returnNextConcurrencyFreeJob(agendaDefinitions: Record<string, JobDefinition>): Job {
 		let next;
-		for (next = this._queue.length - 1; next > 0; next -= 1) {
+		for (next = this._queue.length - 1; next >= 0; next -= 1) {
 			const def = agendaDefinitions[this._queue[next].attrs.name];
 			if (def.concurrency > def.running) {
 				break;
