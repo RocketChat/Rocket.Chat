82b3d456d7549703fbc9aaba346350fbf5cba2db
Kevin  Aleman <kaleman960@gmail.com>
2026-03-24 13:06:15 -0600

    clearing up

--- ee/packages/abac/src/pdp/index.ts
+++ ee/packages/abac/src/pdp/index.ts
@@ -1,4 +1,3 @@
 export { LocalPDP } from './LocalPDP';
 export { ExternalPDP } from './ExternalPDP';
-export type { IExternalPDPConfig as ExternalPDPConfig } from './ExternalPDP';
-export type { IPolicyDecisionPoint } from './types';
+export type { IExternalPDPConfig as ExternalPDPConfig, IPolicyDecisionPoint } from './types';
