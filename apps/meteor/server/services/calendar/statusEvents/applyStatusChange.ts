92299c3ad13798eba6f66c89c645eaeb30849255
Kevin  Aleman <kaleman960@gmail.com>
2026-03-26 15:51:08 -0600

    Revert "fix: Calendar status event status switch (#39491)"
    This reverts commit b29fb86f7c255353d09e29822ede132a6cf2c5c2.

--- apps/meteor/server/services/calendar/statusEvents/applyStatusChange.ts
+++ apps/meteor/server/services/calendar/statusEvents/applyStatusChange.ts
@@ -20,51 +20,33 @@ export async function applyStatusChange({
 	status?: UserStatus;
 	shouldScheduleRemoval?: boolean;
 }): Promise<void> {
-	const user = await Users.findOneById(uid, { projection: { roles: 1, username: 1, name: 1, statusDefault: 1 } });
+	logger.debug({
+		msg: 'Applying status change for event',
+		eventId,
+		uid,
+		startTime,
+		endTime,
+		status: status ?? UserStatus.BUSY,
+	});
 
-	if (!user || user.statusDefault === UserStatus.OFFLINE) {
-		logger.debug({
-			msg: 'Cannot apply status change for event, user is offline or does not exist',
-			eventId,
-			uid,
-		});
-
+	const user = await Users.findOneById(uid, { projection: { roles: 1, username: 1, name: 1, status: 1 } });
+	if (!user || user.status === UserStatus.OFFLINE) {
 		return;
 	}
 
 	const newStatus = status ?? UserStatus.BUSY;
-	const previousStatus = user.statusDefault;
+	const previousStatus = user.status;
 
-	logger.debug({
-		msg: 'Applying status change for event',
-		eventId,
-		user: { _id: uid, statusDefault: user?.statusDefault },
-		startTime,
-		endTime,
-		newStatus,
+	await Users.updateStatusAndStatusDefault(uid, newStatus, newStatus);
+
+	await api.broadcast('presence.status', {
+		user: {
+			status: newStatus,
+			_id: uid,
+			roles: user.roles,
+			username: user.username,
+			name: user.name,
+		},
 		previousStatus,
 	});
-
-	let statusChanged = false;
-
-	if (newStatus === UserStatus.BUSY) {
-		await Users.updateStatusAndStatusDefault(uid, newStatus, newStatus);
-		statusChanged = true;
-	} else if (user.statusDefault === UserStatus.BUSY) {
-		await Users.updateStatusAndStatusDefault(uid, newStatus, newStatus);
-		statusChanged = true;
-	}
-
-	if (statusChanged) {
-		await api.broadcast('presence.status', {
-			user: {
-				status: newStatus,
-				_id: uid,
-				roles: user.roles,
-				username: user.username,
-				name: user.name,
-			},
-			previousStatus,
-		});
-	}
 }
