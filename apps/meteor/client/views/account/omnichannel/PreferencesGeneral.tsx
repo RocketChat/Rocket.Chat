9a3c913e95677d25a9d5ba950acf09cc0804238c
juliajforesti <juliajforesti@gmail.com>
2026-03-27 16:03:47 -0300

    chore: apply fuselage-forms in `omnichannel` account preferences

--- apps/meteor/client/views/account/omnichannel/PreferencesGeneral.tsx
+++ apps/meteor/client/views/account/omnichannel/PreferencesGeneral.tsx
@@ -1,5 +1,4 @@
-import { Field, FieldGroup, FieldHint, FieldLabel, FieldRow, ToggleSwitch } from '@rocket.chat/fuselage';
-import { useId } from 'react';
+import { Field, FieldGroup, FieldHint, FieldLabel, FieldRow, ToggleSwitch } from '@rocket.chat/fuselage-forms';
 import type { ReactElement } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -7,14 +6,13 @@ import { useTranslation } from 'react-i18next';
 export const PreferencesGeneral = (): ReactElement => {
 	const { t } = useTranslation();
 	const { register } = useFormContext();
-	const omnichannelHideAfterClosing = useId();
 
 	return (
 		<FieldGroup marginBlockEnd='1.5rem' paddingInline='0.5rem'>
 			<Field>
 				<FieldRow>
-					<FieldLabel htmlFor={omnichannelHideAfterClosing}>{t('Omnichannel_hide_conversation_after_closing')}</FieldLabel>
-					<ToggleSwitch id={omnichannelHideAfterClosing} {...register('omnichannelHideConversationAfterClosing')} />
+					<FieldLabel>{t('Omnichannel_hide_conversation_after_closing')}</FieldLabel>
+					<ToggleSwitch {...register('omnichannelHideConversationAfterClosing')} />
 				</FieldRow>
 				<FieldHint>{t('Omnichannel_hide_conversation_after_closing_description')}</FieldHint>
 			</Field>
