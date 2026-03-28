7397a1ca6991eee473db2e7dcb2309f563cd2714
Kishan Patel <kishan.patel.tech.dev@gmail.com>
2026-03-28 17:10:14 +0530

    chore(message-parser): fix missing color token in attachment field description

--- apps/meteor/client/components/message/content/attachments/default/Field.tsx
+++ apps/meteor/client/components/message/content/attachments/default/Field.tsx
@@ -7,11 +7,10 @@ type FieldProps = {
 	value: ReactNode;
 } & Omit<ComponentProps<typeof Box>, 'title' | 'value'>;
 
-// TODO: description missing color token
 const Field = ({ title, value, ...props }: FieldProps) => (
 	<Box mb={4} pi={4} width='full' flexBasis={100} flexShrink={0} color='default' {...props}>
 		<Box fontScale='p2m'>{title}</Box>
-		{value}
+		<Box color='default'>{value}</Box>
 	</Box>
 );
 
