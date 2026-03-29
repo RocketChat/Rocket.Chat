974855aec53789569b81f21e4156be03268262fe
Sharanya Mahajan <sharanyamahajan9@gmail.com>
2026-03-29 16:32:54 +0530

    chore(message-parser): remove orphaned i18next pluralization TODO from Reaction component

--- apps/meteor/client/components/message/content/reactions/Reaction.tsx
+++ apps/meteor/client/components/message/content/reactions/Reaction.tsx
@@ -11,7 +11,6 @@ import { MessageListContext } from '../../list/MessageListContext';
 
 const normalizeUsernames = (names: string[]) => names.map((name) => (name.startsWith('@') ? name.slice(1) : name));
 
-// TODO: replace it with proper usage of i18next plurals
 type ReactionProps = {
 	hasReacted: (name: string) => boolean;
 	counter: number;
