f7bd5b25c5f47be02cb2d97eb54084762f16a5e6
Akash Gautam <akashogz@gmail.com>
2026-03-30 20:25:59 +0530

    fix(message-parser): remove TODO and apply color token in attachment Field

--- apps/meteor/client/components/message/content/attachments/default/Field.tsx
+++ apps/meteor/client/components/message/content/attachments/default/Field.tsx
@@ -7,11 +7,12 @@ type FieldProps = {
 	value: ReactNode;
 } & Omit<ComponentProps<typeof Box>, 'title' | 'value'>;
 
-// TODO: description missing color token
 const Field = ({ title, value, ...props }: FieldProps) => (
 	<Box mb={4} pi={4} width='full' flexBasis={100} flexShrink={0} color='default' {...props}>
-		<Box fontScale='p2m'>{title}</Box>
-		{value}
+		<Box fontScale='p2m' color='hint'>{title}</Box>
+		<Box fontScale='p1m' color='default'>
+			{value}
+		</Box>
 	</Box>
 );
 
