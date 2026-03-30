ae9097c591cd099dd1ba6947ee41b991bf53c375
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-03-30 18:24:19 -0300

    modify users.info validation schema for email filtering

--- packages/rest-typings/src/index.ts
+++ packages/rest-typings/src/index.ts
@@ -238,6 +238,7 @@ export * from './v1/users/UsersUpdateOwnBasicInfoParamsPOST';
 export * from './v1/users/UsersUpdateParamsPOST';
 export * from './v1/users/UsersCheckUsernameAvailabilityParamsGET';
 export * from './v1/users/UsersSendConfirmationEmailParamsPOST';
+export * from './v1/users/UsersListParamsGET';
 export * from './v1/moderation';
 export * from './v1/server-events';
 
