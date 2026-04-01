1a2c851cb888ec58265ff439a5c9a0ab1223175e
Douglas Fabris <devfabris@gmail.com>
2026-04-01 10:02:48 -0300

    fix: update form handling in PreferencesConversationTranscript to use Controller

--- apps/meteor/client/views/account/omnichannel/PreferencesConversationTranscript.tsx
+++ apps/meteor/client/views/account/omnichannel/PreferencesConversationTranscript.tsx
@@ -1,14 +1,13 @@
 import { AccordionItem, Tag } from '@rocket.chat/fuselage';
 import { Field, FieldGroup, FieldLabel, FieldRow, FieldHint, ToggleSwitch } from '@rocket.chat/fuselage-forms';
 import { useTranslation, usePermission, useSetting } from '@rocket.chat/ui-contexts';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 
 import { useHasLicenseModule } from '../../../hooks/useHasLicenseModule';
 
 const PreferencesConversationTranscript = () => {
 	const t = useTranslation();
-
-	const { register } = useFormContext();
+	const { control } = useFormContext();
 
 	const { data: hasLicense = false } = useHasLicenseModule('livechat-enterprise');
 	const alwaysSendEmailTranscript = useSetting('Livechat_transcript_send_always');
@@ -31,7 +30,11 @@ const PreferencesConversationTranscript = () => {
 							)}
 							{!canSendTranscriptPDF && hasLicense && <Tag mi={4}>{t('No_permission')}</Tag>}
 						</FieldLabel>
-						<ToggleSwitch disabled={cantSendTranscriptPDF} {...register('omnichannelTranscriptPDF')} />
+						<Controller
+							control={control}
+							name='omnichannelTranscriptPDF'
+							render={({ field: { value, ...field } }) => <ToggleSwitch disabled={cantSendTranscriptPDF} {...field} checked={value} />}
+						/>
 					</FieldRow>
 					<FieldHint>{t('Accounts_Default_User_Preferences_omnichannelTranscriptPDF_Description')}</FieldHint>
 				</Field>
@@ -41,7 +44,11 @@ const PreferencesConversationTranscript = () => {
 							{t('Omnichannel_transcript_email')}
 							{!canSendTranscriptEmailPermission && <Tag mi={4}>{t('No_permission')}</Tag>}
 						</FieldLabel>
-						<ToggleSwitch disabled={!canSendTranscriptEmail} {...register('omnichannelTranscriptEmail')} />
+						<Controller
+							control={control}
+							name='omnichannelTranscriptEmail'
+							render={({ field: { value, ...field } }) => <ToggleSwitch disabled={!canSendTranscriptEmail} {...field} checked={value} />}
+						/>
 					</FieldRow>
 					<FieldHint>{t('Accounts_Default_User_Preferences_omnichannelTranscriptEmail_Description')}</FieldHint>
 				</Field>
