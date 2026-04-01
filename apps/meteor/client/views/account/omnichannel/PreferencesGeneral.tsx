ad1cedf684bfc303bdefc800ca799d8574877234
Douglas Fabris <devfabris@gmail.com>
2026-04-01 10:03:57 -0300

    fix: update form handling in PreferencesGeneral to use Controller

--- apps/meteor/client/views/account/omnichannel/PreferencesGeneral.tsx
+++ apps/meteor/client/views/account/omnichannel/PreferencesGeneral.tsx
@@ -1,18 +1,22 @@
 import { Field, FieldGroup, FieldHint, FieldLabel, FieldRow, ToggleSwitch } from '@rocket.chat/fuselage-forms';
 import type { ReactElement } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 export const PreferencesGeneral = (): ReactElement => {
 	const { t } = useTranslation();
-	const { register } = useFormContext();
+	const { control } = useFormContext();
 
 	return (
 		<FieldGroup marginBlockEnd='1.5rem' paddingInline='0.5rem'>
 			<Field>
 				<FieldRow>
 					<FieldLabel>{t('Omnichannel_hide_conversation_after_closing')}</FieldLabel>
-					<ToggleSwitch {...register('omnichannelHideConversationAfterClosing')} />
+					<Controller
+						control={control}
+						name='omnichannelHideConversationAfterClosing'
+						render={({ field: { value, ...field } }) => <ToggleSwitch {...field} checked={value} />}
+					/>
 				</FieldRow>
 				<FieldHint>{t('Omnichannel_hide_conversation_after_closing_description')}</FieldHint>
 			</Field>
