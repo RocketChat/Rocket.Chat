f36419cdbac1b86f32ef012d03011595b2ef9097
Kevin  Aleman <kaleman960@gmail.com>
2026-04-08 11:15:42 -0600

    show ldap sync settings on abac page

--- apps/meteor/client/views/admin/ABAC/ABACSettingTab/SettingsPage.tsx
+++ apps/meteor/client/views/admin/ABAC/ABACSettingTab/SettingsPage.tsx
@@ -1,5 +1,5 @@
-import { Box, Callout, Margins } from '@rocket.chat/fuselage';
-import { Trans } from 'react-i18next';
+import { Accordion, AccordionItem, Box, Callout, Margins } from '@rocket.chat/fuselage';
+import { useTranslation, Trans } from 'react-i18next';
 
 import AbacEnabledToggle from './AbacEnabledToggle';
 import SettingField from './SettingField';
@@ -7,14 +7,25 @@ import { useHasLicenseModule } from '../../../../hooks/useHasLicenseModule';
 import { links } from '../../../../lib/links';
 
 const SettingsPage = () => {
+	const { t } = useTranslation();
 	const { data: hasABAC = false } = useHasLicenseModule('abac');
 	return (
-		<Box maxWidth='x600' w='full' alignSelf='center'>
+		<Box maxWidth='x600' w='full' alignSelf='center' overflow='auto'>
 			<Box>
 				<Margins block={24}>
 					<AbacEnabledToggle hasABAC={hasABAC} />
 					<SettingField settingId='ABAC_ShowAttributesInRooms' />
 					<SettingField settingId='Abac_Cache_Decision_Time_Seconds' />
+
+					<Accordion>
+						<AccordionItem title={t('LDAP_DataSync_ABAC')}>
+							<Margins block={16}>
+								<SettingField settingId='LDAP_Background_Sync_ABAC_Attributes' />
+								<SettingField settingId='LDAP_Background_Sync_ABAC_Attributes_Interval' />
+								<SettingField settingId='LDAP_ABAC_AttributeMap' />
+							</Margins>
+						</AccordionItem>
+					</Accordion>
 
 					<Callout>
 						<Trans i18nKey='ABAC_Enabled_callout'>
