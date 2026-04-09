26a14043516c88df22b8f22fb911498fbbf473f3
yash-rajpal <rajpal.yash03@gmail.com>
2026-04-10 04:27:50 +0530

    collection for two factor challenges

---
+++ packages/models/src/models/TwoFactorChallenges.ts
@@ -0,0 +1,23 @@
+import type { ITwoFactorChallenge } from '@rocket.chat/core-typings';
+import type { ITwoFactorChallengesModel } from '@rocket.chat/model-typings';
+import type { Db, FindOptions, IndexDescription } from 'mongodb';
+
+import { BaseRaw } from './BaseRaw';
+
+export class TwoFactorChallengesRaw extends BaseRaw<ITwoFactorChallenge> implements ITwoFactorChallengesModel {
+	constructor(db: Db) {
+		super(db, 'two_factor_challenges');
+	}
+
+	override modelIndexes(): IndexDescription[] {
+		return [{ key: { expireAt: 1 }, expireAfterSeconds: 0 }];
+	}
+
+	findOneByPendingChallengeId(pendingChallengeId: string, options?: FindOptions<ITwoFactorChallenge>) {
+		return this.findOne({ _id: pendingChallengeId }, options);
+	}
+
+	removeByPendingChallengeId(pendingChallengeId: string) {
+		return this.deleteOne({ _id: pendingChallengeId });
+	}
+}
