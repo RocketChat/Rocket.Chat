0140681d922c786e83a0ba8b4596edb24a43e5bd
Douglas Gubert <douglas.gubert@gmail.com>
2026-04-10 15:22:42 -0300

    refactor: change redaction to explicit paths instead of recursive
    refactor: improve paths on redactor

--- apps/meteor/ee/server/apps/lib/redactor.ts
+++ apps/meteor/ee/server/apps/lib/redactor.ts
@@ -1,36 +1,42 @@
 import fastRedact from 'fast-redact';
 
-export const redactionFields = {
-	'cookie': 'cookie',
-	'x-auth-token': '["x-auth-token"]',
-	'authorization': 'authorization',
-	'access_token': 'access_token',
-	'customFields': 'customFields.*',
-	'emails': 'emails[*].address',
-	'email': 'email',
-	'password': 'password',
-	'pass': 'pass',
-};
+const requestFields = [
+	'headers.Cookie',
+	'headers.cookie',
+	'headers["x-auth-token"]',
+	'headers["X-Auth-Token"]',
+	'headers.auth',
+	'headers.Auth',
+	'headers.authorization',
+	'headers.Authorization',
+	'headers.access_token',
+	'content.password',
+	'content.pass',
+	'data.password',
+	'data.pass',
+];
 
-const redactor = fastRedact({
-	paths: Object.values(redactionFields),
+const entityFields = ['password', 'pass', 'customFields.*', '_unmappedProperties_'];
+
+const roomFields = ['customFields.*', '_unmappedProperties_', ...entityFields.map((field) => `creator.${field}`)];
+
+export const redactionFieldPaths = [
+	// Incoming requests to the Apps API endpoints
+	...requestFields,
+	...entityFields.map((field) => `user.${field}`),
+	'query.access_token',
+	'query.query', // The deprecated `query` search param
+	// Outgoing requests from the Apps to the outter webs
+	...requestFields.map((field) => `request.${field}`),
+	`request.query`, // `query` here is a string, so we have to redact it all
+	// Slashcommands
+	...roomFields.map((field) => `params[0].room.${field}`),
+	...entityFields.map((field) => `params[0].sender.${field}`),
+];
+
+export const redact = fastRedact({
+	paths: redactionFieldPaths,
+	censor: '[Redacted]',
 	serialize: false,
 	strict: false,
 });
-
-export function redact(value: unknown): void {
-	if (!value || typeof value !== 'object') return;
-
-	if (Array.isArray(value)) {
-		return value.forEach(redact);
-	}
-
-	redactor(value);
-
-	Object.entries(value).forEach(([key, val]) => {
-		// Don't recurse into properties that have already been redacted
-		if (!(key in redactionFields)) {
-			redact(val);
-		}
-	});
-}
