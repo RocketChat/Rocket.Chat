22cfeeddaa0633e89a8806261054b930d94137ed
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-04-13 14:35:22 -0300

    do not send trying signals for active calls

--- ee/packages/media-calls/src/internal/SignalProcessor.ts
+++ ee/packages/media-calls/src/internal/SignalProcessor.ts
@@ -158,13 +158,15 @@ export class GlobalSignalProcessor {
 			await mediaCallDirector.renewCallId(call._id);
 		}
 
-		const otherActor = role === 'caller' ? call.callee : call.caller;
-		if (otherActor.type === 'user') {
-			this.sendSignal(otherActor.id, {
-				callId: call._id,
-				type: 'notification',
-				notification: 'trying',
-			});
+		if (call.state !== 'active') {
+			const otherActor = role === 'caller' ? call.callee : call.caller;
+			if (otherActor.type === 'user') {
+				this.sendSignal(otherActor.id, {
+					callId: call._id,
+					type: 'notification',
+					notification: 'trying',
+				});
+			}
 		}
 
 		if (!signal.requestSignals) {
