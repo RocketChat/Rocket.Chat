108f44fa73e5dcb9962b513748068cbd8914a538
yash-rajpal <rajpal.yash03@gmail.com>
2026-04-20 02:27:45 +0530

    add passport google oauth

--- apps/meteor/server/lib/oauth/oauthConfigs.ts
+++ apps/meteor/server/lib/oauth/oauthConfigs.ts
@@ -1,6 +1,7 @@
 import type { Strategy } from 'passport';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 import { Strategy as GitHubStrategy } from 'passport-github2';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 export type OAuthConfig = {
 	strategy: new (...args: any[]) => Strategy;
@@ -15,6 +16,10 @@ export const OAuthConfigs: Record<string, OAuthConfig> = {
 	facebook: {
 		strategy: FacebookStrategy,
 		scope: ['email'],
+	},
+	google: {
+		strategy: GoogleStrategy,
+		scope: ['email', 'profile'],
 	},
 } as const;
 
