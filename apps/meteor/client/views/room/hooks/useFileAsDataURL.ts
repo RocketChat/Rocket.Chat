8a2665ce0e32fb1e8f5bcf20a4e58cfff5a5d5c7
Douglas Fabris <devfabris@gmail.com>
2026-04-20 10:28:10 -0300

    refactor: streamline file reading logic in useFileAsDataURL hook

--- apps/meteor/client/views/room/hooks/useFileAsDataURL.ts
+++ apps/meteor/client/views/room/hooks/useFileAsDataURL.ts
@@ -1,24 +1,42 @@
 import { useEffect, useState } from 'react';
 
-type ReaderOnloadCallback = (url: FileReader['result']) => void;
-
-const readFileAsDataURL = (file: File, callback: ReaderOnloadCallback): void => {
-	const reader = new FileReader();
-	reader.onload = (e): void => callback(e?.target?.result || null);
-
-	return reader.readAsDataURL(file);
-};
-
 export const useFileAsDataURL = (file: File): [loaded: boolean, url: null | FileReader['result']] => {
 	const [loaded, setLoaded] = useState(false);
 	const [url, setUrl] = useState<FileReader['result']>(null);
 
 	useEffect(() => {
 		setLoaded(false);
-		readFileAsDataURL(file, (url) => {
-			setUrl(url);
+		setUrl(null);
+
+		let shouldIgnore = false;
+		const reader = new FileReader();
+
+		reader.onload = (e): void => {
+			if (shouldIgnore) {
+				return;
+			}
+
+			setUrl(e?.target?.result || null);
 			setLoaded(true);
-		});
+		};
+
+		reader.onerror = (): void => {
+			if (shouldIgnore) {
+				return;
+			}
+
+			setUrl(null);
+			setLoaded(true);
+		};
+
+		reader.readAsDataURL(file);
+
+		return () => {
+			shouldIgnore = true;
+			if (reader.readyState === FileReader.LOADING) {
+				reader.abort();
+			}
+		};
 	}, [file]);
 	return [loaded, url];
 };
