88c27a76f5abf1683fd287eee9b4d5b0898c0680
Ayush Sahu <2004ayushsahu@gmail.com>
2026-04-21 12:57:39 +0530

    feat: define schema for logs array items

--- apps/meteor/ee/server/apps/communication/endpoints/appLogsHandler.ts
+++ apps/meteor/ee/server/apps/communication/endpoints/appLogsHandler.ts
@@ -31,7 +31,7 @@ export const registerAppLogsHandler = ({ api, _manager, _orch }: AppsRestApi) =>
 					type: 'object',
 					properties: {
 						offset: { type: 'number' },
-						logs: { type: 'array' },
+						logs: { type: 'array' ,items:{type:'object',additionalProperties: true}},
 						count: { type: 'number' },
 						total: { type: 'number' },
 						success: { type: 'boolean' },
