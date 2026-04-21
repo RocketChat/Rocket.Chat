5e7cdc0ab9f1d86305cf6dd4f7e2badc979f3fd0
Ayush Sahu <2004ayushsahu@gmail.com>
2026-04-21 14:55:44 +0530

    feat: add migration to drop legacy read_receipts index

---
+++ apps/meteor/server/startup/migrations/v336.ts
@@ -0,0 +1,24 @@
+import { addMigration } from '../../lib/migrations';
+import { getRawCollection } from '@rocket.chat/models';
+
+addMigration({
+	version: 336,
+	name: 'Drop legacy unique index from read_receipts',
+	async up() {
+		const collection = getRawCollection('read_receipts');
+
+		const indexes = await collection.indexes();
+
+		const targetIndex = indexes.find((idx) => {
+			return (
+				idx.key?.roomId === 1 &&
+				idx.key?.userId === 1 &&
+				idx.key?.messageId === 1
+			);
+		});
+
+		if (targetIndex) {
+			await collection.dropIndex(targetIndex.name);
+		}
+	},
+});
