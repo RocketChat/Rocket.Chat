9b0c5337ff4d026ee339fad5a2fd8bc12095531c
DivyanshuVortex <divyanshuchandra9027@gmail.com>
2026-04-25 11:20:59 +0530

    chore(migration): drop unique index on Read Receipts collection to improve performance

--- apps/meteor/ee/server/models/raw/ReadReceipts.ts
+++ apps/meteor/ee/server/models/raw/ReadReceipts.ts
@@ -11,8 +11,6 @@ export class ReadReceiptsRaw extends BaseRaw<IReadReceipt> implements IReadRecei
 	protected override modelIndexes(): IndexDescription[] {
 		return [
 			// Unique index removed to increase performance. Uniqueness in now handled via composite value of _id
-			// TODO: Drop existent index on database migration of 9.0
-			// { key: { roomId: 1, userId: 1, messageId: 1 }, unique: true },
 			{ key: { messageId: 1 } },
 			{ key: { userId: 1 } },
 			{ key: { roomId: 1 } },
---
+++ apps/meteor/server/startup/migrations/v336.ts
@@ -0,0 +1,15 @@
+import { ReadReceipts } from '@rocket.chat/models';
+
+import { addMigration } from '../../lib/migrations';
+
+addMigration({
+	version: 336,
+	name: 'Drop "roomId_1_userId_1_messageId_1" index from Read Receipts collection',
+	async up() {
+		try {
+			await ReadReceipts.col.dropIndex('roomId_1_userId_1_messageId_1');
+		} catch {
+			console.warn('Error dropping index roomId_1_userId_1_messageId_1 from Read Receipts collection, it might have already been dropped.');
+		}
+	},
+});
