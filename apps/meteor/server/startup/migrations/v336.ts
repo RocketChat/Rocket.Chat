6f5462c70070ca4e00dd5525f3933e6edd600fbe
Ayush Sahu <2004ayushsahu@gmail.com>
2026-04-25 22:31:12 +0530

    refactor: use direct dropIndex with IndexNotFound handling

--- apps/meteor/server/startup/migrations/v336.ts
+++ apps/meteor/server/startup/migrations/v336.ts
@@ -1,29 +1,16 @@
-import { addMigration } from '../../lib/migrations';
 import { ReadReceipts } from '@rocket.chat/models';
+import { addMigration } from '../../lib/migrations';
 
 addMigration({
 	version: 336,
 	name: 'Drop legacy unique index from read_receipts',
 
 	async up() {
-		const collection = ReadReceipts.col;
-
-		const indexes = await collection.indexes();
-
-		const matchingIndexes = indexes.filter((idx) => {
-			const keys = idx.key || {};
-
-			return (
-				Object.keys(keys).length === 3 && 
-				keys.roomId === 1 &&
-				keys.userId === 1 &&
-				keys.messageId === 1
-			);
-		});
-
-		for (const idx of matchingIndexes) {
-			if (idx.name) {
-				await collection.dropIndex(idx.name);
+		try {
+			await ReadReceipts.col.dropIndex('roomId_1_userId_1_messageId_1');
+		} catch (e: any) {
+			if (e?.code !== 27 && e?.codeName !== 'IndexNotFound') {
+				throw e;
 			}
 		}
 	},
