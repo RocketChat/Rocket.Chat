9e03355ca3bcdf7a5c92a047818c6706f9dc222f
Douglas Gubert <douglas.gubert@gmail.com>
2026-04-28 12:25:04 -0300

    fix lint

--- packages/apps-engine/src/definition/accessors/IRoomRead.ts
+++ packages/apps-engine/src/definition/accessors/IRoomRead.ts
@@ -1,5 +1,5 @@
-import type { GetMessagesOptions, GetRoomsFilters, GetRoomsOptions } from '../rooms/IGetMessagesOptions';
 import type { IMessageRaw } from '../messages/index';
+import type { GetMessagesOptions, GetRoomsFilters, GetRoomsOptions } from '../rooms/IGetMessagesOptions';
 import type { IRoom, IRoomRaw } from '../rooms/index';
 import type { IUser } from '../users/index';
 
