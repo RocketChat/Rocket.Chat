e51e1d4c552247cb4e28e2b40f6ad3981a09f586
MartinSchoeler <martinschoeler8@gmail.com>
2026-05-11 15:04:49 -0300

    review: add guard

--- apps/meteor/client/lib/chats/flows/afterSendMessageCallback.ts
+++ apps/meteor/client/lib/chats/flows/afterSendMessageCallback.ts
@@ -7,6 +7,11 @@ import { getUser } from '../../user';
 export const afterSendMessageCallback = async (message: IMessage, rid: string) => {
 	const user = getUser();
 	const room = Rooms.state.get(rid);
+
+	if (!room || !user) {
+		return;
+	}
+
 	const processedMessage = {
 		...message,
 		u: {
