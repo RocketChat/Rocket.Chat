d0c4769cca68d459dc9344629c6e4500ea108fac
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-05-14 11:04:56 -0300

    add deprecation notice

--- apps/meteor/app/custom-sounds/server/methods/deleteCustomSound.ts
+++ apps/meteor/app/custom-sounds/server/methods/deleteCustomSound.ts
@@ -4,6 +4,7 @@ import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
 import { hasPermissionAsync } from '../../../authorization/server/functions/hasPermission';
+import { methodDeprecationLogger } from '../../../lib/server/lib/deprecationWarningLogger';
 
 declare module '@rocket.chat/ddp-client' {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
@@ -14,6 +15,7 @@ declare module '@rocket.chat/ddp-client' {
 
 Meteor.methods<ServerMethods>({
 	async deleteCustomSound(_id) {
+		methodDeprecationLogger.method('deleteCustomSound', '9.0.0', '/v1/custom-sounds.delete');
 		if (!this.userId || !(await hasPermissionAsync(this.userId, 'manage-sounds'))) {
 			throw new Meteor.Error('not_authorized');
 		}
