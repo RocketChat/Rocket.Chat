ddc8de110c066cba69f4e56869de0b9b226dc638
yash-rajpal <rajpal.yash03@gmail.com>
2026-05-18 22:09:40 +0530

    refactor

--- apps/meteor/app/linkedin/server/lib.ts
+++ apps/meteor/app/linkedin/server/lib.ts
@@ -1,8 +1,24 @@
+import type { OAuthConfiguration } from '@rocket.chat/core-typings';
 import { Meteor } from 'meteor/meteor';
 import passport from 'passport';
 
 import { addPassportCustomOAuth } from '../../../server/lib/oauth/addPassportCustomOAuth';
 import { settings } from '../../settings/server';
+
+const config: Partial<OAuthConfiguration & { clientSecret?: string }> = {
+	serverURL: 'https://www.linkedin.com',
+	authorizePath: '/oauth/v2/authorization',
+	tokenPath: '/oauth/v2/accessToken',
+	identityPath: 'https://api.linkedin.com/v2/userinfo',
+	scope: 'openid email profile',
+	tokenSentVia: 'header',
+	addAutopublishFields: {
+		forLoggedInUser: ['services.linkedin'],
+		forOtherUsers: ['services.linkedin.name'],
+	},
+	pkce: false,
+	emailField: 'email',
+};
 
 const serviceKey = 'linkedin';
 
@@ -21,22 +37,10 @@ const configureLinkedInOAuth = (): void => {
 		return;
 	}
 
-	addPassportCustomOAuth(serviceKey, {
-		serverURL: 'https://www.linkedin.com',
-		authorizePath: '/oauth/v2/authorization',
-		tokenPath: '/oauth/v2/accessToken',
-		identityPath: 'https://api.linkedin.com/v2/userinfo',
-		scope: 'openid email profile',
-		tokenSentVia: 'header',
-		clientSecret,
-		clientId,
-		addAutopublishFields: {
-			forLoggedInUser: ['services.linkedin'],
-			forOtherUsers: ['services.linkedin.name'],
-		},
-		pkce: false,
-		emailField: 'email',
-	});
+	config.clientId = clientId;
+	config.clientSecret = clientSecret;
+
+	addPassportCustomOAuth(serviceKey, config);
 };
 
 Meteor.startup(() => {
