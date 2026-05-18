1525fe36c8a6dd124c6e61458ad179496c91c483
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-05-18 13:14:51 -0300

    update tests

--- apps/meteor/tests/unit/server/services/messages/hooks/BeforeSaveMarkdownParser.tests.ts
+++ apps/meteor/tests/unit/server/services/messages/hooks/BeforeSaveMarkdownParser.tests.ts
@@ -1,17 +1,7 @@
 import { expect } from 'chai';
 import { beforeEach } from 'mocha';
-import proxyquire from 'proxyquire';
-import * as sinon from 'sinon';
 
-const settingsStub = { get: sinon.stub() };
-
-const { BeforeSaveMarkdownParser } = proxyquire
-	.noCallThru()
-	.load('../../../../../../server/services/messages/hooks/BeforeSaveMarkdownParser', {
-		'../../../../app/settings/server': {
-			settings: settingsStub,
-		},
-	});
+import { BeforeSaveMarkdownParser } from '../../../../../../server/services/messages/hooks/BeforeSaveMarkdownParser';
 
 const createMessage = (msg?: string, extra: any = {}) => ({
 	_id: 'random',
@@ -28,8 +18,11 @@ const createMessage = (msg?: string, extra: any = {}) => ({
 
 describe('Markdown parser', () => {
 	beforeEach(() => {
-		settingsStub.get.reset();
-		settingsStub.get.withArgs('Message_MaxMarkdownParseLength').returns(0); // disabled by default
+		delete process.env.MESSAGE_MAX_PARSE_LENGTH;
+	});
+
+	afterEach(() => {
+		delete process.env.MESSAGE_MAX_PARSE_LENGTH;
 	});
 
 	it('should do nothing if markdown parser is disabled', async () => {
@@ -54,8 +47,19 @@ describe('Markdown parser', () => {
 		expect(message).to.not.have.property('md');
 	});
 
-	it('should skip parsing when msg exceeds Message_MaxMarkdownParseLength', async () => {
-		settingsStub.get.withArgs('Message_MaxMarkdownParseLength').returns(10);
+	it('should parse markdown when the environment variable is not set, falling back to the default 30.000 limit', async () => {
+		const markdownParser = new BeforeSaveMarkdownParser(true);
+
+		const message = await markdownParser.parseMarkdown({
+			message: createMessage('a'.repeat(30_000)),
+			config: {},
+		});
+
+		expect(message).to.have.property('md');
+	});
+
+	it('should skip parsing when msg exceeds MESSAGE_MAX_PARSE_LENGTH', async () => {
+		process.env.MESSAGE_MAX_PARSE_LENGTH = '10';
 		const markdownParser = new BeforeSaveMarkdownParser(true);
 
 		const message = await markdownParser.parseMarkdown({
@@ -66,24 +70,12 @@ describe('Markdown parser', () => {
 		expect(message).to.not.have.property('md');
 	});
 
-	it('should parse normally when msg is within Message_MaxMarkdownParseLength', async () => {
-		settingsStub.get.withArgs('Message_MaxMarkdownParseLength').returns(100);
+	it('should parse normally when msg is within MESSAGE_MAX_PARSE_LENGTH', async () => {
+		process.env.MESSAGE_MAX_PARSE_LENGTH = '100';
 		const markdownParser = new BeforeSaveMarkdownParser(true);
 
 		const message = await markdownParser.parseMarkdown({
 			message: createMessage('short msg'),
-			config: {},
-		});
-
-		expect(message).to.have.property('md');
-	});
-
-	it('should parse normally when Message_MaxMarkdownParseLength is 0', async () => {
-		settingsStub.get.withArgs('Message_MaxMarkdownParseLength').returns(0);
-		const markdownParser = new BeforeSaveMarkdownParser(true);
-
-		const message = await markdownParser.parseMarkdown({
-			message: createMessage('a'.repeat(99999)),
 			config: {},
 		});
 
