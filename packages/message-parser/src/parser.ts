2d0705fa2b184730a1b7355e0c20aa25218a2ef9
amitkumarashutosh <amitkumarashutos@gmail.com>
2026-05-23 14:01:30 +0530

    feat: implement paragraph and line break parsing (144/634)

--- packages/message-parser/src/parser.ts
+++ packages/message-parser/src/parser.ts
@@ -1,5 +1,32 @@
-import { Options } from './index';
+import { Options, Root } from './index';
+import { lineBreak, paragraph, plain } from './utils';
 
 export const parse = (input: string, options?: Options) => {
-	return [];
+	const root: Root = [];
+	let i: number = 0;
+
+	while (i < input.length) {
+		const start = i;
+		while (i < input.length && input[i] !== '\n' && input[i] !== '\r') {
+			i++;
+		}
+
+		const text = input.slice(start, i);
+		const isLastPosition = i >= input.length;
+
+		if (text === '') {
+			if (!isLastPosition) {
+				root.push(lineBreak());
+			}
+		} else {
+			root.push(paragraph([plain(text)]));
+		}
+
+		if (i < input.length) {
+			if (input[i] === '\n' && input[i + 1] === '\r') i += 2;
+			else i += 1;
+		}
+	}
+
+	return root;
 };
