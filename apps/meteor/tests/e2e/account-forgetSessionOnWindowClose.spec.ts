6fa8d6c0688b5e3980004fa84138f90da9a4a9b6
Matheus Cardoso <matheus@cardo.so>
2026-05-25 13:00:00 -0300

    test: unskip Accounts_ForgetUserSessionOnWindowClose group

--- apps/meteor/tests/e2e/account-forgetSessionOnWindowClose.spec.ts
+++ apps/meteor/tests/e2e/account-forgetSessionOnWindowClose.spec.ts
@@ -28,8 +28,7 @@ test.describe.serial('Forget session on window close setting', () => {
 		});
 	});
 
-	// TODO: Fix this test
-	test.describe.skip('Setting on', async () => {
+	test.describe('Setting on', async () => {
 		test.beforeAll(async ({ api }) => {
 			await api.post('/settings/Accounts_ForgetUserSessionOnWindowClose', { value: true });
 		});
