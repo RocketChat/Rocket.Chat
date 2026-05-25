6055233ccbec8b22afc47fcee315f895e804d8db
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-05-25 13:06:51 -0300

    added timeout for external history request

--- apps/meteor/server/services/call-history/mitel/importHistoryForUser.ts
+++ apps/meteor/server/services/call-history/mitel/importHistoryForUser.ts
@@ -4,6 +4,7 @@ import { Users } from '@rocket.chat/models';
 import type { MitelConfig } from './definition';
 import { fetchMitelHistory } from './fetchMitelHistory';
 import { importHistoryItems } from './importHistoryItems';
+import { logger } from '../logger';
 import { parseMitelJSON } from './parse/parseMitelJSON';
 
 async function getUserDirectoryNumber(uid: IUser['_id']): Promise<string | null> {
@@ -11,13 +12,7 @@ async function getUserDirectoryNumber(uid: IUser['_id']): Promise<string | null>
 	return user?.freeSwitchExtension || null;
 }
 
-export async function importHistoryForUser(uid: IUser['_id'], config: MitelConfig): Promise<void> {
-	const directoryNumber = await getUserDirectoryNumber(uid);
-	if (!directoryNumber) {
-		return;
-	}
-
-	const result = await fetchMitelHistory(directoryNumber, config);
+async function processFetchedHistory(uid: IUser['_id'], result: string | null): Promise<void> {
 	if (!result) {
 		return;
 	}
@@ -28,4 +23,54 @@ export async function importHistoryForUser(uid: IUser['_id'], config: MitelConfi
 	}
 
 	await importHistoryItems(uid, callItems);
+}
+
+async function fetchAndImportHistory(user: { directoryNumber: string; uid: IUser['_id'] }, config: MitelConfig): Promise<void> {
+	const { timeout } = config;
+
+	if (!timeout) {
+		const result = await fetchMitelHistory(user.directoryNumber, config);
+		return processFetchedHistory(user.uid, result);
+	}
+
+	let promiseDecided = false;
+	return new Promise((resolve, reject) => {
+		fetchMitelHistory(user.directoryNumber, config)
+			.then((result) => {
+				const newPromise = processFetchedHistory(user.uid, result);
+
+				if (promiseDecided) {
+					return;
+				}
+
+				promiseDecided = true;
+				newPromise.then(resolve).catch(reject);
+			})
+			.catch((err) => {
+				if (promiseDecided) {
+					logger.error({ msg: 'Unexpected error on external call history processing', err });
+					return;
+				}
+				promiseDecided = true;
+				reject(err as Error);
+			});
+
+		setTimeout(() => {
+			if (promiseDecided) {
+				return;
+			}
+
+			promiseDecided = true;
+			resolve();
+		}, timeout);
+	});
+}
+
+export async function importHistoryForUser(uid: IUser['_id'], config: MitelConfig): Promise<void> {
+	const directoryNumber = await getUserDirectoryNumber(uid);
+	if (!directoryNumber) {
+		return;
+	}
+
+	return fetchAndImportHistory({ directoryNumber, uid }, config);
 }
