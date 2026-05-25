72907173ee5f2de01ef36bd736a38cc5a4bddc6e
gabriellsh <henriques.gabriell@gmail.com>
2026-05-25 11:40:19 -0300

    Undo

--- apps/meteor/client/meteor/login/oauth.ts
+++ apps/meteor/client/meteor/login/oauth.ts
@@ -1,4 +1,3 @@
-import { isTotpRequiredError } from '@rocket.chat/api-client';
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 import { OAuth } from 'meteor/oauth';
@@ -103,7 +102,7 @@ export const createOAuthTotpLoginMethod =
 Accounts.oauth.credentialRequestCompleteHandler = credentialRequestCompleteHandler;
 
 getDdpSdk().account.onPageLoadLogin(async (loginAttempt: any) => {
-	if (loginAttempt?.error && !isTotpRequiredError(loginAttempt.error)) {
+	if (loginAttempt?.error?.error !== 'totp-required') {
 		return;
 	}
 
@@ -116,27 +115,15 @@ getDdpSdk().account.onPageLoadLogin(async (loginAttempt: any) => {
 	const { credentialToken, credentialSecret } = oAuthArgs.oauth;
 	const cb = loginAttempt.userCallback;
 
-	const { challenge2fa } = await import('../../lib/2fa/challenge2fa');
+	const { process2faReturn } = await import('../../lib/2fa/process2faReturn');
 
-	const retryLogin = async (error: any) => {
-		const challenge = challenge2fa({
-			error,
-			errorHandler: cb,
-		});
-
-		if (!challenge) {
-			throw new Error('Unable to challenge 2fa');
-		}
-
-		const [code, resolveChallenge] = challenge;
-		const resolvedCode = await code;
-
-		try {
-			tryLoginAfterPopupClosed(credentialToken, cb, resolvedCode, credentialSecret);
-		} catch (error) {
-			resolveChallenge();
-		}
-	};
-
-	await retryLogin(loginAttempt.error);
+	await process2faReturn({
+		error: loginAttempt.error,
+		originalCallback: cb,
+		onCode: (code) => {
+			tryLoginAfterPopupClosed(credentialToken, cb, code, credentialSecret);
+		},
+		emailOrUsername: undefined,
+		result: undefined,
+	});
 });
