dc824d0ea7fefa66d3167ed651c0ca53b710c14e
gabriellsh <henriques.gabriell@gmail.com>
2026-06-02 10:47:42 -0300

    fix unused ref and element type

--- apps/meteor/client/views/room/MessageList/hooks/useKeepAtBottom.ts
+++ apps/meteor/client/views/room/MessageList/hooks/useKeepAtBottom.ts
@@ -2,9 +2,6 @@ import { useSafeRefCallback } from '@rocket.chat/fuselage-hooks';
 import type { MutableRefObject } from 'react';
 import { useCallback, useRef } from 'react';
 
-const sizeChanged = (a: { width: number; height: number } | null, b: { width: number; height: number } | null) =>
-	!a || a.width !== b?.width || a.height !== b.height;
-
 // This hook is responsible for keeping the message list at the bottom despite any size changes to the container.
 // Some examples of when this is needed are:
 // - When the user is at the bottom and a new message arrives, the message list will grow and we want to keep it at the bottom. (This one is already handled in another place, but this hook also does this job)
@@ -15,13 +12,12 @@ const sizeChanged = (a: { width: number; height: number } | null, b: { width: nu
 // - When the user is at the bottom and a video is loading, after loading the video element can change sizes and shift the list
 export const useKeepAtBottom = (isAtBottom: MutableRefObject<boolean | null>) => {
 	const handleRef = useRef<(() => void) | null>(null);
-	const contentRectRef = useRef<{ width: number; height: number } | null>(null);
 	const keepAtBottomRef = useSafeRefCallback(
 		useCallback(
-			(node: HTMLElement) => {
+			(node: HTMLDivElement) => {
 				const listWrapper = node.firstChild;
-				const observer = new ResizeObserver(([entry]) => {
-					if (sizeChanged(contentRectRef.current, entry.contentRect) && isAtBottom.current) {
+				const observer = new ResizeObserver(() => {
+					if (isAtBottom.current) {
 						if (handleRef.current) {
 							handleRef.current();
 						}
@@ -29,8 +25,8 @@ export const useKeepAtBottom = (isAtBottom: MutableRefObject<boolean | null>) =>
 				});
 
 				observer.observe(node);
-				if (listWrapper) {
-					observer.observe(node.firstChild as HTMLElement);
+				if (listWrapper instanceof HTMLElement) {
+					observer.observe(listWrapper);
 				}
 
 				return () => {
