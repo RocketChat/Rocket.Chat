0ce055dc646df50d0138080ecfa0f9e276f6ef43
gabriellsh <henriques.gabriell@gmail.com>
2026-06-03 14:43:18 -0300

    revert: useThreadMessagesQuery

--- apps/meteor/client/views/room/contextualBar/Threads/hooks/useThreadMessagesQuery.ts
+++ apps/meteor/client/views/room/contextualBar/Threads/hooks/useThreadMessagesQuery.ts
@@ -1,5 +1,5 @@
 import { isThreadMessage, type IMessage, type IRoom, type IThreadMainMessage, type IThreadMessage } from '@rocket.chat/core-typings';
-import { useMethod, useStream, useUserId } from '@rocket.chat/ui-contexts';
+import { useMethod, useStream } from '@rocket.chat/ui-contexts';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect, useRef } from 'react';
 
@@ -28,8 +28,6 @@ export const useThreadMessagesQuery = (tmid: IThreadMainMessage['_id'], rid?: IR
 
 	const subscribeToRoomMessages = useStream('room-messages');
 	const subscribeToNotifyRoom = useStream('notify-room');
-	const subscribeToNotifyUser = useStream('notify-user');
-	const uid = useUserId();
 
 	const unprocessedReadMessagesEvent = useRef<{ tmid: string; until: Date } | null>(null);
 
@@ -44,17 +42,6 @@ export const useThreadMessagesQuery = (tmid: IThreadMainMessage['_id'], rid?: IR
 			const processed = await onClientMessageReceived(event);
 			upsertThreadMessageInCache(processed, roomId, tmid, queryClient);
 		});
-
-		const unsubscribeFromUserMessages = uid
-			? subscribeToNotifyUser(`${uid}/message`, async (event) => {
-					if (event.rid !== roomId || event.tmid !== tmid || event._hidden === true) {
-						return;
-					}
-
-					const processed = await onClientMessageReceived(event);
-					upsertThreadMessageInCache(processed, roomId, tmid, queryClient);
-				})
-			: () => undefined;
 
 		const unsubscribeFromDeleteMessage = subscribeToNotifyRoom(`${roomId}/deleteMessage`, (event) => {
 			queryClient.setQueryData<IThreadMessage[]>(currentQueryKey, (old) => {
@@ -107,12 +94,11 @@ export const useThreadMessagesQuery = (tmid: IThreadMainMessage['_id'], rid?: IR
 
 		return () => {
 			unsubscribeFromRoomMessages();
-			unsubscribeFromUserMessages();
 			unsubscribeFromDeleteMessage();
 			unsubscribeFromDeleteMessageBulk();
 			unsubscribeFromMessagesRead();
 		};
-	}, [tmid, roomId, queryClient, subscribeToRoomMessages, subscribeToNotifyRoom, subscribeToNotifyUser, uid]);
+	}, [tmid, roomId, queryClient, subscribeToRoomMessages, subscribeToNotifyRoom]);
 
 	return useQuery({
 		queryKey,
