6c6fbb8778db8047a883e3534ccaddd14201495e
gabriellsh <henriques.gabriell@gmail.com>
2026-06-05 16:32:05 -0300

    chore: Update `usePopoutWindow` to open in landing page

--- packages/ui-voip/src/views/usePopoutWindow.ts
+++ packages/ui-voip/src/views/usePopoutWindow.ts
@@ -1,38 +1,50 @@
+import { useThemeMode } from '@rocket.chat/ui-client';
+import type { TFunction } from 'i18next';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-const createRootElement = (externalWindow: Window) => {
+const createRootElement = async (externalWindow: Window) => {
 	const newRoot = externalWindow.document.createElement('div');
 	newRoot.style.width = '100%';
 	newRoot.style.height = '100%';
-	externalWindow.document.body.appendChild(newRoot);
+
+	// await for page to load so that the root exists
+	await new Promise((resolve) => {
+		externalWindow.onload = resolve;
+	});
+
+	const landingPageRoot = externalWindow.document.getElementById('root');
+	if (!landingPageRoot) {
+		throw new Error('usePopoutWindow - createRootElement - landingPageRoot not found');
+	}
+
+	landingPageRoot.appendChild(newRoot);
+
 	return newRoot;
 };
 
-const copyStylesheets = (externalWindow: Window) => {
-	Array.from(document.styleSheets).forEach((stylesheet) => {
-		if (stylesheet.href) {
-			const link = document.createElement('link');
-			link.rel = 'stylesheet';
-			link.href = stylesheet.href;
-			externalWindow.document.head.appendChild(link);
-		} else if (stylesheet?.cssRules?.length > 0) {
-			const style = document.createElement('style');
-			Array.from(stylesheet.cssRules).forEach((rule) => {
-				style.appendChild(document.createTextNode(rule.cssText));
-			});
-			externalWindow.document.head.appendChild(style);
-		}
+const replaceWithTranslationString = (t: TFunction, externalDocument: Document) => {
+	externalDocument.querySelectorAll('[data-i18n]').forEach((el) => {
+		el.textContent = t(el.getAttribute('data-i18n') as string);
 	});
 };
 
-const openExternalWindow = (title: string) => {
+const changeTheme = (ownerDocument: Document, theme?: string) => {
+	if (theme) {
+		ownerDocument.documentElement.setAttribute('data-color-scheme', theme);
+	} else {
+		ownerDocument.documentElement.removeAttribute('data-color-scheme');
+	}
+};
+
+const openExternalWindow = async (title: string, theme: string) => {
 	try {
-		const externalWindow = window.open('', title, 'width=800,height=500,popup');
+		const externalWindow = window.open(`/voice-call-popup.html${theme ? `?theme=${theme}` : ''}`, title, 'width=800,height=500,popup');
 		if (!externalWindow) {
 			throw new Error('No window was opened');
 		}
-		copyStylesheets(externalWindow);
-		const root = createRootElement(externalWindow);
+
+		const root = await createRootElement(externalWindow);
 		return { root, externalWindow };
 	} catch (error) {
 		// This should maybe throw instead of returning null
@@ -43,7 +55,7 @@ const openExternalWindow = (title: string) => {
 
 export type PopoutContainer = { root: HTMLDivElement; ownerDocument: Document };
 type PopoutRef = { root: HTMLDivElement; externalWindow: Window; closing: boolean };
-type OpenPopoutWindow = (title: string) => void;
+type OpenPopoutWindow = (title: string) => Promise<void>;
 type ClosePopoutWindow = () => void;
 
 type UsePopoutWindowReturn = {
@@ -55,14 +67,17 @@ type UsePopoutWindowReturn = {
 export const usePopoutWindow = (onBeforeUnload: () => void): UsePopoutWindowReturn => {
 	const popoutRef = useRef<PopoutRef | null>(null);
 	const [container, setContainer] = useState<PopoutContainer | null>(null);
+	const { t } = useTranslation();
+
+	const [, , theme] = useThemeMode();
 
 	const openPopoutWindow = useCallback(
-		(title: string) => {
+		async (title: string) => {
 			if (!!popoutRef.current && popoutRef.current.externalWindow?.closed === false) {
 				return;
 			}
 
-			const result = openExternalWindow(title);
+			const result = await openExternalWindow(title, theme);
 
 			if (result) {
 				const { root, externalWindow } = result;
@@ -72,7 +87,7 @@ export const usePopoutWindow = (onBeforeUnload: () => void): UsePopoutWindowRetu
 			}
 			onBeforeUnload();
 		},
-		[onBeforeUnload],
+		[onBeforeUnload, theme],
 	);
 
 	const closePopoutWindow = useCallback(() => {
@@ -103,6 +118,20 @@ export const usePopoutWindow = (onBeforeUnload: () => void): UsePopoutWindowRetu
 			window.removeEventListener('beforeunload', closePopoutWindow);
 		};
 	}, [container, onBeforeUnload, closePopoutWindow]);
+
+	useEffect(() => {
+		if (!container) {
+			return;
+		}
+		replaceWithTranslationString(t, container.ownerDocument);
+	}, [container, t]);
+
+	useEffect(() => {
+		if (!container) {
+			return;
+		}
+		changeTheme(container.ownerDocument, theme);
+	}, [container, theme]);
 
 	return {
 		container,
