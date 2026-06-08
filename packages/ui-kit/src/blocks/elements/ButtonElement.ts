3350f3ce941c2b6eaa314d66aba092990ca731a8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-06-08 13:56:51 -0300

    chore: add todos

--- packages/ui-kit/src/blocks/elements/ButtonElement.ts
+++ packages/ui-kit/src/blocks/elements/ButtonElement.ts
@@ -4,7 +4,7 @@ import type { PlainText } from '../text/PlainText';
 export type ButtonElement = Actionable<{
 	type: 'button';
 	text: PlainText;
-	url?: string;
+	url?: string; // TODO: The url in ButtonElement should not be an arbitrary string, it lacks validation and can lead to security issues
 	value?: string;
 	style?: 'primary' | 'secondary' | 'danger' | 'warning' | 'success';
 	secondary?: boolean;
