4301b7c3574db66f48bc3d42d4471c3e5f0631aa
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-06-09 23:23:06 -0300

    refactor: fix variable typos

--- apps/meteor/client/components/message/hooks/useCollapse.ts
+++ apps/meteor/client/components/message/hooks/useCollapse.ts
@@ -3,7 +3,7 @@ import { useAttachmentIsCollapsedByDefault } from '@rocket.chat/ui-contexts';
 import { useCallback } from 'react';
 
 export const useCollapse = (attachmentCollapsed?: boolean) => {
-	const collpaseByDefault = useAttachmentIsCollapsedByDefault();
-	const [collapsed, toogleCollapsed] = useToggle(collpaseByDefault || attachmentCollapsed);
-	return [collapsed, useCallback(() => toogleCollapsed(), [toogleCollapsed])] as const;
+	const collapseByDefault = useAttachmentIsCollapsedByDefault();
+	const [collapsed, toggleCollapsed] = useToggle(collapseByDefault || attachmentCollapsed);
+	return [collapsed, useCallback(() => toggleCollapsed(), [toggleCollapsed])] as const;
 };
