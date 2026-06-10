e934f8f5ed727776fd70f6e90f02f4850bc0498e
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-06-10 18:02:07 -0300

    remove deleteCustomSound Meteor method

--- apps/meteor/app/custom-sounds/server/index.ts
+++ apps/meteor/app/custom-sounds/server/index.ts
@@ -1,5 +1,4 @@
 import './startup/custom-sounds';
-import './methods/deleteCustomSound';
 import './methods/insertOrUpdateSound';
 import './methods/listCustomSounds';
 import './methods/uploadCustomSound';
