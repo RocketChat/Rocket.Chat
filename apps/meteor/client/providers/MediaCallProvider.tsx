4c4b3c177c4be417ec5cefa61a0d344cfd7b9cbe
Douglas Gubert <douglas.gubert@gmail.com>
2026-06-10 16:23:09 -0300

    feat(meteor): adapt usage to support media call widget changes
    feat: hook to fetch action buttons and provide to media call widget
    
    feat: adapt meteor media call to get the action buttons
    
    fix handle interaction
    
    adapt meteor to new provider
    
    meteor ui changes
    
    refactor(meteor): adapt client views to ui-voip
    
    meteor ui changes
    
    meteor ui changes
    
    meteor changes
    
    refactor(meteor): refactor handleInteraction passed to media call widget
    
    meteor rebase
    
    refactor(ui-contexts): emitInteraction in ActionManager can return an
    object with the new update button response

--- apps/meteor/client/providers/MediaCallProvider.tsx
+++ apps/meteor/client/providers/MediaCallProvider.tsx
@@ -1,20 +1,27 @@
 import { usePermission } from '@rocket.chat/ui-contexts';
 import { MediaCallProvider as MediaCallProviderBase } from '@rocket.chat/ui-voip';
+import { MediaCallAppActionsProvider } from '@rocket.chat/ui-voip/dist/experimental/AppActionButtons';
 import type { ReactNode } from 'react';
 
 import { useHasLicenseModule } from '../hooks/useHasLicenseModule';
+import { useMediaCallWidgetAppsActionButtons } from '../hooks/useMediaCallWidgetAppsActionButtons';
 
 export type MediaCallProviderProps = { children: ReactNode };
 
 const MediaCallProvider = ({ children }: MediaCallProviderProps) => {
 	const canMakeInternalCall = usePermission('allow-internal-voice-calls');
 	const canMakeExternalCall = usePermission('allow-external-voice-calls');
+	const { actions, handleInteraction } = useMediaCallWidgetAppsActionButtons();
 
 	const { data: hasModule = false } = useHasLicenseModule('teams-voip');
 
 	const enabled = hasModule && (canMakeInternalCall || canMakeExternalCall);
 
-	return <MediaCallProviderBase enabled={enabled}>{children}</MediaCallProviderBase>;
+	return (
+		<MediaCallAppActionsProvider actions={actions} handleInteraction={handleInteraction}>
+			<MediaCallProviderBase enabled={enabled}>{children}</MediaCallProviderBase>
+		</MediaCallAppActionsProvider>
+	);
 };
 
 export default MediaCallProvider;
