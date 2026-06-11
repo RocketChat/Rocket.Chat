a3354073e03c1c190c48edbe68b71c3cec5ebc95
Julio Araujo <julio.araujo@rocket.chat>
2026-06-11 14:17:29 +0000

    fix: imported fixes 06-11-26 (#40889)
    Co-authored-by: Matheus Cardoso <matheus@cardo.so>

--- apps/meteor/app/apple/server/AppleCustomOAuth.ts
+++ apps/meteor/app/apple/server/AppleCustomOAuth.ts
@@ -2,6 +2,7 @@ import { MeteorError } from '@rocket.chat/core-services';
 import { Accounts } from 'meteor/accounts-base';
 
 import { CustomOAuth } from '../../custom-oauth/server/custom_oauth_server';
+import { settings } from '../../settings/server';
 import { handleIdentityToken } from '../lib/handleIdentityToken';
 
 export class AppleCustomOAuth extends CustomOAuth {
@@ -16,7 +17,9 @@ export class AppleCustomOAuth extends CustomOAuth {
 		}
 
 		try {
-			const serviceData = await handleIdentityToken(identityToken);
+			const clientId = settings.get<string>('Accounts_OAuth_Apple_id') || '';
+
+			const serviceData = await handleIdentityToken(identityToken, clientId);
 
 			if (usrObj?.name) {
 				serviceData.name = `${usrObj.name.firstName}${usrObj.name.middleName ? ` ${usrObj.name.middleName}` : ''}${
