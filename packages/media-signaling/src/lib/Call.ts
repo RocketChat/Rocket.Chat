9adbe25df27760561c54652dc8e9f0f31c6a3e25
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-06-15 18:33:57 -0300

    detect contact changes

--- packages/media-signaling/src/lib/Call.ts
+++ packages/media-signaling/src/lib/Call.ts
@@ -31,6 +31,7 @@ import type {
 	ServerMediaSignalNotification,
 	ServerMediaSignalRemoteSDP,
 	ServerMediaSignalRequestOffer,
+	ServerMediaSignalUpdateCall,
 } from '../definition/signals/server';
 
 export interface IClientMediaCallConfig {
@@ -661,6 +662,8 @@ export class ClientMediaCall implements IClientMediaCall {
 				return this.processOfferRequest(signal);
 			case 'notification':
 				return this.processNotification(signal);
+			case 'update':
+				return this.processCallUpdate(signal);
 		}
 	}
 
@@ -1179,6 +1182,18 @@ export class ClientMediaCall implements IClientMediaCall {
 
 			case 'hangup':
 				return this.flagAsEnded('remote', signal.hangupReason);
+		}
+	}
+
+	private async processCallUpdate(signal: ServerMediaSignalUpdateCall) {
+		this.config.logger?.debug('ClientMediaCall.processCallUpdate');
+
+		if (signal.features) {
+			this.enabledFeatures = signal.features;
+		}
+
+		if (signal.contact) {
+			this.changeContact(signal.contact);
 		}
 	}
 
