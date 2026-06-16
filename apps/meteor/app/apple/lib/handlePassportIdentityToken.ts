6d06a1e8a1979e19aa6dbc59c46b25bb9fb4de98
yash-rajpal <rajpal.yash03@gmail.com>
2026-06-17 02:21:29 +0530

    chore: move apple passport implementation

---
+++ apps/meteor/app/apple/lib/handlePassportIdentityToken.ts
@@ -0,0 +1,52 @@
+import { serverFetch as fetch } from '@rocket.chat/server-fetch';
+import { KJUR } from 'jsrsasign';
+import NodeRSA from 'node-rsa';
+
+async function isValidAppleJWT(identityToken: string, header: any): Promise<boolean> {
+	const request = await fetch('https://appleid.apple.com/auth/keys', {
+		method: 'GET',
+		// SECURITY: Hardcoded URL, no SSRF protection needed
+		ignoreSsrfValidation: true,
+	});
+	const applePublicKeys = ((await request.json()) as { keys: { kid: string; e: string; n: string }[] }).keys;
+	const { kid } = header;
+
+	const key = applePublicKeys.find((k: any) => k.kid === kid);
+	if (!key) {
+		return false;
+	}
+
+	const pubKey = new NodeRSA();
+	pubKey.importKey({ n: Buffer.from(key.n, 'base64'), e: Buffer.from(key.e, 'base64') }, 'components-public');
+	const userKey = pubKey.exportKey('public');
+
+	try {
+		return KJUR.jws.JWS.verify(identityToken, userKey, ['RS256']);
+	} catch {
+		return false;
+	}
+}
+
+export async function handlePassportIdentityToken(identityToken: string): Promise<Record<string, any>> {
+	const decodedToken = KJUR.jws.JWS.parse(identityToken);
+
+	if (!(await isValidAppleJWT(identityToken, decodedToken.headerObj))) {
+		throw new Error('identityToken is not a valid JWT');
+	}
+
+	if (!decodedToken.payloadObj) {
+		throw new Error('identityToken does not have a payload');
+	}
+
+	const { iss, sub } = decodedToken.payloadObj as any;
+	if (!iss) {
+		throw new Error('Insufficient data in auth response token');
+	}
+
+	const serviceData = {
+		id: sub,
+		...decodedToken.payloadObj,
+	};
+
+	return serviceData;
+}
---
+++ apps/meteor/app/apple/server/applePassportOAuth.ts
@@ -0,0 +1,152 @@
+import { MeteorError } from '@rocket.chat/core-services';
+import { Users } from '@rocket.chat/models';
+import express from 'express';
+import { Accounts } from 'meteor/accounts-base';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import passport from 'passport';
+import { Strategy as AppleStrategy } from 'passport-apple';
+import type { Profile } from 'passport-apple';
+
+import { AppleCustomOAuth } from './AppleCustomOAuth';
+import { oAuthRouter } from '../../../server/configuration/configurePassport';
+import { allowPassportOAuthMiddleware } from '../../../server/lib/oauth/allowPassportOAuthMiddleware';
+import { passportOAuthCallback } from '../../../server/lib/oauth/passportOAuthCallback';
+import { settings } from '../../settings/server';
+import { config } from '../lib/config';
+import { handlePassportIdentityToken } from '../lib/handlePassportIdentityToken';
+
+new AppleCustomOAuth('apple', config);
+
+settings.watchMultiple(
+	[
+		'Accounts_OAuth_Apple',
+		'Accounts_OAuth_Apple_id',
+		'Accounts_OAuth_Apple_secretKey',
+		'Accounts_OAuth_Apple_iss',
+		'Accounts_OAuth_Apple_kid',
+		'Accounts_OAuth_Use_Modern_Flow',
+	],
+	async ([enabled, clientId, serverSecret, iss, kid]) => {
+		passport.unuse('apple');
+
+		if (!enabled) {
+			return ServiceConfiguration.configurations.removeAsync({
+				service: 'apple',
+			});
+		}
+
+		// if everything is empty but Apple login is enabled, don't show the login button
+		if (!clientId && !serverSecret && !iss && !kid) {
+			await ServiceConfiguration.configurations.upsertAsync(
+				{
+					service: 'apple',
+				},
+				{
+					$set: {
+						showButton: false,
+						enabled: settings.get('Accounts_OAuth_Apple'),
+					},
+				},
+			);
+			return;
+		}
+
+		if (!clientId) {
+			return ServiceConfiguration.configurations.removeAsync({
+				service: 'apple',
+			});
+		}
+
+		passport.use(
+			'apple',
+
+			new AppleStrategy(
+				{
+					clientID: settings.get<string>('Accounts_OAuth_Apple_id'),
+					teamID: settings.get<string>('Accounts_OAuth_Apple_iss'),
+					keyID: settings.get<string>('Accounts_OAuth_Apple_kid'),
+					privateKeyString: settings.get<string>('Accounts_OAuth_Apple_secretKey').replace(/\\n/g, '\n'),
+					callbackURL: `${settings.get<string>('Site_Url')}/_oauth/apple`,
+					scope: ['name', 'email'],
+					passReqToCallback: false,
+					state: false,
+				},
+				async (accessToken: string, refreshToken: string, idToken: string, profile: Profile, done) => {
+					try {
+						const serviceData = await handlePassportIdentityToken(idToken);
+						if (profile?.name) {
+							serviceData.name = `${profile.name.firstName}${profile.name.middleName ? ` ${profile.name.middleName}` : ''}${
+								profile.name.lastName ? ` ${profile.name.lastName}` : ''
+							}`;
+						}
+
+						if (!serviceData.email && profile?.email) {
+							serviceData.email = profile.email;
+						}
+
+						const user = await Accounts.updateOrCreateUserFromExternalService(
+							'apple',
+							{
+								accessToken,
+								refreshToken,
+								...serviceData,
+							},
+							{},
+						);
+
+						if (!user?.userId || typeof user?.userId !== 'string') {
+							return done(new Error('User not found'));
+						}
+
+						const userFromDB = await Users.findOneById(user.userId);
+
+						if (!userFromDB) {
+							return done(new Error('User not found'));
+						}
+
+						return done(null, userFromDB);
+					} catch (error: any) {
+						done(error);
+						return {
+							type: 'apple',
+							error: new MeteorError(Accounts.LoginCancelledError.numericError, error.message),
+						};
+					}
+				},
+			),
+		);
+
+		const callbackHandler = [
+			allowPassportOAuthMiddleware('apple'),
+			express.urlencoded({ extended: true }),
+			passport.authenticate('apple', { failWithError: true, session: true, keepSessionInfo: true }),
+			passportOAuthCallback(settings.get<string>('Site_Url')),
+		];
+
+		oAuthRouter.get(
+			'/oauth/apple',
+			allowPassportOAuthMiddleware('apple'),
+			(req, _res, next) => {
+				const { loginClient } = req.query;
+				if (loginClient === 'mobile' || loginClient === 'desktop') {
+					req.session.loginClient = loginClient;
+					req.session.save(() => {
+						next();
+					});
+				} else {
+					//delete stale value from previous sessions if any
+					delete req.session.loginClient;
+					next();
+				}
+			},
+			passport.authenticate('apple', {
+				scope: ['name', 'email'],
+			}),
+		);
+
+		oAuthRouter
+			.route('/_oauth/apple')
+			.post(...callbackHandler)
+			.get(...callbackHandler);
+	},
+);
