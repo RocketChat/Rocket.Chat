50ab5c6cb5c9ad16cf38a74f4f9a2e202497faeb
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2026-06-16 11:01:06 -0300

    Use rid from conference record to open the room

---
+++ apps/meteor/client/views/conference/ConferenceRoom.tsx
@@ -0,0 +1,59 @@
+import type { ISubscription } from '@rocket.chat/core-typings';
+import { Box } from '@rocket.chat/fuselage';
+import { LayoutContext, useLayout, useStream, useUserId } from '@rocket.chat/ui-contexts';
+import type { ReactElement } from 'react';
+import { lazy, Suspense, useEffect, useMemo } from 'react';
+
+import { SubscriptionsCachedStore } from '../../cachedStores';
+import { NotAuthorizedError } from '../../lib/errors/NotAuthorizedError';
+import { useOpenRoomById } from '../room/hooks/useOpenRoom';
+
+const RoomProvider = lazy(() => import('../room/providers/RoomProvider'));
+const Room = lazy(() => import('../room/Room'));
+const RoomSkeleton = lazy(() => import('../room/RoomSkeleton'));
+const RoomNotFound = lazy(() => import('../room/RoomNotFound'));
+const NotAuthorizedPage = lazy(() => import('../notAuthorized/NotAuthorizedPage'));
+
+type ConferenceRoomProps = {
+	rid: string;
+};
+
+const ConferenceRoom = ({ rid }: ConferenceRoomProps): ReactElement => {
+	const { data, error, isSuccess, isError, isLoading } = useOpenRoomById(rid);
+	const uid = useUserId();
+	const subscribeToNotifyUser = useStream('notify-user');
+	const layoutContext = useLayout();
+	const layoutContextEmbedded = useMemo(() => ({ ...layoutContext, isEmbedded: true }), [layoutContext]);
+
+	useEffect(() => {
+		if (!uid || !data?.rid) {
+			return;
+		}
+
+		return subscribeToNotifyUser(`${uid}/subscriptions-changed`, (event, sub) => {
+			if (sub.rid !== data.rid || event === 'removed') {
+				return;
+			}
+
+			SubscriptionsCachedStore.upsertSubscription(sub as ISubscription);
+		});
+	}, [data?.rid, subscribeToNotifyUser, uid]);
+
+	return (
+		<LayoutContext.Provider value={layoutContextEmbedded}>
+			<Box display='flex' w='full' h='full'>
+				<Suspense fallback={<RoomSkeleton />}>
+					{isLoading && <RoomSkeleton />}
+					{isSuccess && (
+						<RoomProvider rid={data.rid}>
+							<Room />
+						</RoomProvider>
+					)}
+					{isError && (error instanceof NotAuthorizedError ? <NotAuthorizedPage /> : <RoomNotFound />)}
+				</Suspense>
+			</Box>
+		</LayoutContext.Provider>
+	);
+};
+
+export default ConferenceRoom;
