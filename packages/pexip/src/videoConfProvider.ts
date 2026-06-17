d2f553c4abd6c77620bd7c4b3f52e086fbf07adc
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-06-17 16:01:13 -0300

    disable old persistent chat

--- packages/pexip/src/videoConfProvider.ts
+++ packages/pexip/src/videoConfProvider.ts
@@ -1,6 +1,6 @@
 import type { IBlock } from '@rocket.chat/apps-engine/definition/uikit';
-import type { VideoConference, AtLeast, IRoom, IVideoConferenceUser, RequiredField } from '@rocket.chat/core-typings';
-import { MediaCalls, Rooms } from '@rocket.chat/models';
+import type { VideoConference, IVideoConferenceUser, RequiredField } from '@rocket.chat/core-typings';
+import { MediaCalls } from '@rocket.chat/models';
 
 import type { Pexip } from './Pexip';
 import { logger } from './logger';
@@ -43,73 +43,7 @@ export class PexipVideoConfProvider {
 
 		const relativeUrl = meetingUrl.replace('{callId}', call._id);
 
-		const meetingParams = {
-			rid: call.discussionRid && (await this.getDiscussionUrl(call.discussionRid)),
-		};
-
-		const encodedParams = {
-			...meetingParams,
-			rid: meetingParams.rid && encodeURIComponent(meetingParams.rid),
-		};
-
-		return this.joinUrlAndParams(`${baseUrl}${relativeUrl}`, encodedParams);
-	}
-
-	private joinUrlParams(params: Record<string, string | undefined>): string {
-		return Object.keys(params)
-			.filter((key) => params[key] !== undefined && params[key] !== null)
-			.map((key) => `${key}=${params[key]}`)
-			.join('&');
-	}
-
-	private joinUrlAndParams(baseUrl: string, params: Record<string, string | undefined>): string {
-		const joinedParams = this.joinUrlParams(params);
-		return `${baseUrl}${baseUrl.includes('?') ? '&' : '?'}${joinedParams}`;
-	}
-
-	private async getDiscussionUrl(rid: string): Promise<string | undefined> {
-		const room = await Rooms.findOneById<Pick<IRoom, '_id' | 't' | 'name'>>(rid, { projection: { t: 1, name: 1 } });
-		if (!room) {
-			return;
-		}
-
-		const roomRoute = this.getDiscussionRoute(room);
-		if (!roomRoute) {
-			return;
-		}
-
-		const baseUrl = await this.getBaseURLWithoutTrailingSlash();
-		const roomUrl = `${baseUrl}/${roomRoute}`;
-
-		const roomParams = {
-			layout: 'embedded',
-		};
-
-		const params = Object.keys(roomParams)
-			.map((key) => `${key}=${roomParams[key as keyof typeof roomParams]}`)
-			.join('&');
-
-		return `${roomUrl}${roomUrl.includes('?') ? '&' : '?'}${params}`;
-	}
-
-	private getDiscussionRoute(room: AtLeast<IRoom, 't' | 'name'>): string | undefined {
-		switch (room.t) {
-			case 'c':
-				return `channel/${room.name}`;
-			case 'p':
-				return `group/${room.name}`;
-			default:
-				return undefined;
-		}
-	}
-
-	private async getBaseURLWithoutTrailingSlash(): Promise<string> {
-		const url = this.pexip.settings.workspace.siteUrl;
-
-		if (url.endsWith('/')) {
-			return url.substr(0, url.length - 1);
-		}
-		return url;
+		return `${baseUrl}${relativeUrl}`;
 	}
 
 	public async customizeUrl(call: RequiredField<VideoConference, 'url'>, user: IVideoConferenceUser | undefined): Promise<string> {
