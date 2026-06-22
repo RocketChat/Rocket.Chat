01553df6579585efc61fa9ff0d89ee4e6eb45ba5
Ricardo Garim <rswarovsky@gmail.com>
2026-06-21 21:11:43 -0300

    chore: remove dead status validation in users.setStatus endpoint

--- apps/meteor/app/api/server/v1/users.ts
+++ apps/meteor/app/api/server/v1/users.ts
@@ -1942,7 +1942,7 @@ API.v1
 				intervalTimeInMS: 60000,
 			},
 			body: ajv.compile<{
-				status?: UserStatus;
+				status?: Exclude<UserStatus, UserStatus.DISABLED>;
 				message?: string;
 				expiresAt?: string;
 				userId?: string;
@@ -1999,13 +1999,6 @@ API.v1
 
 			if (!user) {
 				return API.v1.forbidden();
-			}
-
-			const validStatus = ['online', 'away', 'offline', 'busy'];
-			if (this.bodyParams.status && !validStatus.includes(this.bodyParams.status)) {
-				throw new Meteor.Error('error-invalid-status', 'Valid status types include online, away, offline, and busy.', {
-					method: 'users.setStatus',
-				});
 			}
 
 			const { status, message, expiresAt } = this.bodyParams;
