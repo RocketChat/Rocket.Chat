b6287454f3cdf38393d1998202641167cd3f56f6
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2026-06-22 10:45:29 -0300

    refactor: disallow guest mode only for embedded page

--- apps/meteor/client/views/conference/ConferenceRoute.tsx
+++ apps/meteor/client/views/conference/ConferenceRoute.tsx
@@ -1,12 +1,36 @@
-import ConferencePage from './ConferencePage';
+import { useRouteParameter } from '@rocket.chat/ui-contexts';
+
+import ConferenceEmbeddedPage from './ConferenceEmbeddedPage';
+import ConferencePageError from './ConferencePageError';
+import ConferenceRedirectPage from './ConferenceRedirectPage';
 import AuthenticationCheck from '../root/MainLayout/AuthenticationCheck';
 
+const getQueryParams = () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	return { callUrlParam: urlParams.get('callUrl') };
+};
+
 const ConferenceRoute = () => {
-	return (
-		<AuthenticationCheck guest={false}>
-			<ConferencePage />
-		</AuthenticationCheck>
-	);
+	const id = useRouteParameter('id');
+	const { callUrlParam } = getQueryParams();
+
+	if (callUrlParam) {
+		return (
+			<AuthenticationCheck>
+				<ConferenceRedirectPage callUrl={callUrlParam} />
+			</AuthenticationCheck>
+		);
+	}
+
+	if (id) {
+		return (
+			<AuthenticationCheck guest={false}>
+				<ConferenceEmbeddedPage callId={id} />
+			</AuthenticationCheck>
+		);
+	}
+
+	return <ConferencePageError />;
 };
 
 export default ConferenceRoute;
