7e7ff5e5b52ed03b3d9a932bdf3229569fc824c9
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-06-22 17:44:14 -0300

    chore: improve voip enums definitions

--- packages/media-signaling/src/definition/index.ts
+++ packages/media-signaling/src/definition/index.ts
@@ -2,5 +2,5 @@ export * from './call';
 export type * from './services';
 export type * from './media';
 export * from './signals';
-export type * from './client';
+export * from './client';
 export type * from './logger';
