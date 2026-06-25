b1c13c554a4a886af1c65fc0f8ae33ab83f0dca9
MartinSchoeler <martinschoeler8@gmail.com>
2026-06-25 11:40:58 -0300

    revert: emojitoolkit removal on pdf-worker

--- ee/packages/pdf-worker/src/templates/ChatTranscript/markup/blocks/BigEmojiBlock.tsx
+++ ee/packages/pdf-worker/src/templates/ChatTranscript/markup/blocks/BigEmojiBlock.tsx
@@ -1,14 +1,16 @@
 import { Text } from '@react-pdf/renderer';
 import type * as MessageParser from '@rocket.chat/message-parser';
+import emojione from 'emoji-toolkit';
+import type { ReactElement } from 'react';
 
 type BigEmojiBlockProps = {
 	emoji: MessageParser.Emoji[];
 };
 
-const BigEmojiBlock = ({ emoji }: BigEmojiBlockProps) => (
+const BigEmojiBlock = ({ emoji }: BigEmojiBlockProps): ReactElement => (
 	<Text>
 		{emoji.map((emoji, index) => (
-			<Text key={index}>{emoji.value ? `:${emoji.value?.value}:` : emoji.unicode}</Text>
+			<Text key={index}>{emoji.value ? `:${emoji.value?.value}:` : emojione.toShort(emoji.unicode)}</Text>
 		))}
 	</Text>
 );
