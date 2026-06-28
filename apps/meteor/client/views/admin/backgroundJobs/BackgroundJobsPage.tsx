c2268697b340c88fdd2a54aea1ce7210415f1dac
khizarshah01 <khizar.shah010@gmail.com>
2026-06-28 17:43:53 +0530

    feat: implement background jobs contextual bar and actions

--- apps/meteor/client/views/admin/backgroundJobs/BackgroundJobsPage.tsx
+++ apps/meteor/client/views/admin/backgroundJobs/BackgroundJobsPage.tsx
@@ -1,33 +1,65 @@
 import { Tabs, TabsItem } from '@rocket.chat/fuselage';
-import { Page, PageContent, PageHeader } from '@rocket.chat/ui-client';
-import { useState } from 'react';
+import {
+	ContextualbarClose,
+	ContextualbarDialog,
+	ContextualbarHeader,
+	ContextualbarTitle,
+	Page,
+	PageContent,
+	PageHeader,
+} from '@rocket.chat/ui-client';
+import { useRouter, useRouteParameter } from '@rocket.chat/ui-contexts';
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import BackgroundJobInfoContextualBar from './BackgroundJobInfoContextualBar';
 import BackgroundJobsTable from './BackgroundJobsTable';
 
 export type BackgroundJobsTab = 'system' | 'apps' | 'omnichannel';
 
 const BackgroundJobsPage = () => {
 	const { t } = useTranslation();
+	const router = useRouter();
+	const context = useRouteParameter('context');
+	const id = useRouteParameter('id');
 
 	const [tab, setTab] = useState<BackgroundJobsTab>('system');
+
+	const handleClose = useCallback(() => {
+		router.navigate({
+			name: 'admin-background-jobs',
+			params: {},
+		});
+	}, [router]);
+
 	return (
-		<Page>
-			<PageHeader title={t('Background_Jobs')} />
-			<Tabs>
-				<TabsItem selected={tab === 'system'} onClick={() => setTab('system')}>
-					{t('System')}
-				</TabsItem>
-				<TabsItem selected={tab === 'apps'} onClick={() => setTab('apps')}>
-					{t('Apps')}
-				</TabsItem>
-				<TabsItem selected={tab === 'omnichannel'} onClick={() => setTab('omnichannel')}>
-					{t('Omnichannel')}
-				</TabsItem>
-			</Tabs>
-			<PageContent>
-				<BackgroundJobsTable tab={tab} />
-			</PageContent>
+		<Page flexDirection='row'>
+			<Page name='admin-background-jobs'>
+				<PageHeader title={t('Background_Jobs')} />
+				<Tabs>
+					<TabsItem selected={tab === 'system'} onClick={() => setTab('system')}>
+						{t('System')}
+					</TabsItem>
+					<TabsItem selected={tab === 'apps'} onClick={() => setTab('apps')}>
+						{t('Apps')}
+					</TabsItem>
+					<TabsItem selected={tab === 'omnichannel'} onClick={() => setTab('omnichannel')}>
+						{t('Omnichannel')}
+					</TabsItem>
+				</Tabs>
+				<PageContent>
+					<BackgroundJobsTable tab={tab} />
+				</PageContent>
+			</Page>
+			{context && (
+				<ContextualbarDialog onClose={handleClose}>
+					<ContextualbarHeader>
+						<ContextualbarTitle>{t('Job_Info')}</ContextualbarTitle>
+						<ContextualbarClose onClick={handleClose} />
+					</ContextualbarHeader>
+					{context === 'info' && id && <BackgroundJobInfoContextualBar jobName={id} onClose={handleClose} />}
+				</ContextualbarDialog>
+			)}
 		</Page>
 	);
 };
