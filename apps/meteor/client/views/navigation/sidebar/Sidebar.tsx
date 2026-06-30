3f9518453903338e69dbd1adb92b935b30796bf7
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-06-30 15:39:11 -0300

    fix(audio): mount Now playing card in new navigation sidebar too

--- apps/meteor/client/views/navigation/sidebar/Sidebar.tsx
+++ apps/meteor/client/views/navigation/sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import SidebarRoomList from './RoomList';
 import SidebarFooter from '../../../sidebar/footer';
 import BannerSection from '../../../sidebar/sections/BannerSection';
+import NowPlayingSection from '../../../sidebar/sections/NowPlayingSection';
 
 const Sidebar = () => {
 	const { t } = useTranslation();
@@ -12,6 +13,7 @@ const Sidebar = () => {
 	return (
 		<SidebarV2 aria-label={t('Sidebar')} className='rcx-sidebar--main rcx-sidebar'>
 			<BannerSection />
+			<NowPlayingSection />
 			<SidebarRoomList />
 			<SidebarFooter />
 		</SidebarV2>
