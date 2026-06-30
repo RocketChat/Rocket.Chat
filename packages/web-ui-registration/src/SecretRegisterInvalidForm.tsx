937d5c59728184c03c7f6e43529d3324e629b669
Jessica Schelly Souza <jessica_schelly@hotmail.com>
2026-06-30 15:46:42 -0300

    refactor: remove unused import

--- packages/web-ui-registration/src/SecretRegisterInvalidForm.tsx
+++ packages/web-ui-registration/src/SecretRegisterInvalidForm.tsx
@@ -1,5 +1,4 @@
 import { Callout } from '@rocket.chat/fuselage';
-import type { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import VerticalTemplate from './template/VerticalTemplate';
