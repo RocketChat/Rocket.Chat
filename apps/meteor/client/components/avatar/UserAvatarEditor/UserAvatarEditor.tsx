bdd8d600af6a14d2ddf0da6f1bfa4a895fedeaf5
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-07-03 17:37:09 -0300

    refactor(apps/meteor/client/components/avatar/UserAvatarEditor): export component props types

--- apps/meteor/client/components/avatar/UserAvatarEditor/UserAvatarEditor.tsx
+++ apps/meteor/client/components/avatar/UserAvatarEditor/UserAvatarEditor.tsx
@@ -14,7 +14,7 @@ import { useSingleFileInput } from '../../../hooks/useSingleFileInput';
 import { isSafeAvatarUrl } from '../../../lib/utils/isSafeAvatarUrl';
 import { isValidImageFormat } from '../../../lib/utils/isValidImageFormat';
 
-type UserAvatarEditorProps = {
+export type UserAvatarEditorProps = {
 	currentUsername: IUser['username'];
 	username: IUser['username'];
 	setAvatarObj: (obj: AvatarObject) => void;
