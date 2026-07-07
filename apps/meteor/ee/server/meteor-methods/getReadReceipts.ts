ce8287314a04071914715e132e130676c7716781
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-07-07 18:55:51 -0300

    fix(lint): order deprecationWarningLogger import before canAccessRoom in getReadReceipts

--- apps/meteor/ee/server/meteor-methods/getReadReceipts.ts
+++ apps/meteor/ee/server/meteor-methods/getReadReceipts.ts
@@ -5,8 +5,8 @@ import { Messages } from '@rocket.chat/models';
 import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
-import { canAccessRoomIdAsync } from '../../../server/lib/authorization/canAccessRoom';
 import { methodDeprecationLogger } from '../../../app/lib/server/lib/deprecationWarningLogger';
+import { canAccessRoomIdAsync } from '../../../server/lib/authorization/canAccessRoom';
 import { ReadReceipt } from '../lib/message-read-receipt/ReadReceipt';
 
 declare module '@rocket.chat/ddp-client' {
