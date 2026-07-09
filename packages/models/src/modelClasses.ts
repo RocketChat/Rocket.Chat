8e9047c5f6861fe52765b25c1565309186cdd42c
Pierre Lehnen <55164754+pierre-lehnen-rc@users.noreply.github.com>
2026-07-09 02:33:49 -0300

    chore: remove media call channels model (#41194)

--- packages/models/src/modelClasses.ts
+++ packages/models/src/modelClasses.ts
@@ -69,7 +69,6 @@ export * from './models/CronHistoryModel';
 export * from './models/Migrations';
 export * from './models/ModerationReports';
 export * from './models/MediaCalls';
-export * from './models/MediaCallChannels';
 export * from './models/MediaCallNegotiations';
 export * from './models/WorkspaceCredentials';
 export * from './models/Trash';
