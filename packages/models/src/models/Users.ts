719e3db9734708e812ceb33483ffaa2e064b4d59
Abhinav Kumar <abhinav@avitechlab.com>
2026-07-11 10:53:19 +0530

    fix: LDAP users not merging by email during sync (#41279)
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/models/src/models/Users.ts
+++ packages/models/src/models/Users.ts
@@ -2264,7 +2264,7 @@ export class UsersRaw extends BaseRaw<IUser, DefaultFields<IUser>> implements IU
 
 	findOneWithoutLDAPByEmailAddress(emailAddress: string, options?: FindOptions<IUser>) {
 		const query = {
-			'email.address': emailAddress.trim().toLowerCase(),
+			'emails.address': emailAddress.trim().toLowerCase(),
 			'services.ldap': {
 				$exists: false,
 			},
