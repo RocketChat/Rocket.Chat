7cd0ff94f6c43635bb98780070239f6a1b3b91bd
Pierre Lehnen <55164754+pierre-lehnen-rc@users.noreply.github.com>
2026-07-14 17:45:30 -0300

    feat: External Call History (#40658 #41154)

---
+++ apps/meteor/server/services/call-history/mitel/definition.ts
@@ -0,0 +1,23 @@
+export type MitelConfig = {
+	host: string;
+	username: string;
+	password: string;
+	timeout?: number;
+};
+
+export type MitelCallItem = {
+	directoryNumber?: string;
+	name?: string;
+	callIdentity?: string;
+	dateTime: Date | null;
+	timeZone?: string;
+	duration: number;
+	typeOfCall?: 'incoming-answered' | 'incoming-missed' | 'outgoing' | 'outgoing-missed';
+	transferredCall: boolean;
+	divertedCall: boolean;
+	firstDialledNumber?: string;
+	remoteNumber?: string;
+	directoryNumber2?: string;
+	name2?: string;
+	infoText2?: string;
+};
