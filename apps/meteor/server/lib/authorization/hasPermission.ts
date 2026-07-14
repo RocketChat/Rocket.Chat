c4bede4d837fe67063572926319c73ca46516fb4
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-07-14 11:04:07 -0300

    refactor(authorization): accept a minimal user shape (UserWithRoles) in permission checks (#41351)

--- apps/meteor/server/lib/authorization/hasPermission.ts
+++ apps/meteor/server/lib/authorization/hasPermission.ts
@@ -1,18 +1,19 @@
 import { Authorization } from '@rocket.chat/core-services';
+import type { UserWithRoles } from '@rocket.chat/core-services';
 import type { IUser, IPermission, IRoom } from '@rocket.chat/core-typings';
 
 export const hasAllPermissionAsync = async (
-	user: IUser['_id'] | IUser,
+	user: IUser['_id'] | UserWithRoles,
 	permissions: IPermission['_id'][],
 	scope?: IRoom['_id'],
 ): Promise<boolean> => Authorization.hasAllPermission(user, permissions, scope);
 export const hasPermissionAsync = async (
-	user: IUser['_id'] | IUser,
+	user: IUser['_id'] | UserWithRoles,
 	permissionId: IPermission['_id'],
 	scope?: IRoom['_id'],
 ): Promise<boolean> => Authorization.hasPermission(user, permissionId, scope);
 export const hasAtLeastOnePermissionAsync = async (
-	user: IUser['_id'] | IUser,
+	user: IUser['_id'] | UserWithRoles,
 	permissions: IPermission['_id'][],
 	scope?: IRoom['_id'],
 ): Promise<boolean> => Authorization.hasAtLeastOnePermission(user, permissions, scope);
