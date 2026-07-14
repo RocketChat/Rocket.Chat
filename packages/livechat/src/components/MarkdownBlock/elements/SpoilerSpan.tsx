b39dea8ba1c992e21d132dec9ab8f1eece1e4db8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-07-14 11:56:56 -0300

    refactor(livechat): optimize content rendering in SpoilerSpan by using useMemo and improve structure

--- packages/livechat/src/components/MarkdownBlock/elements/SpoilerSpan.tsx
+++ packages/livechat/src/components/MarkdownBlock/elements/SpoilerSpan.tsx
@@ -1,5 +1,5 @@
 import type * as MessageParser from '@rocket.chat/message-parser';
-import { useCallback, useState } from 'preact/hooks';
+import { useCallback, useMemo, useState } from 'preact/hooks';
 import { useTranslation } from 'react-i18next';
 
 import BoldSpan from './BoldSpan';
@@ -63,67 +63,68 @@ const SpoilerSpan = ({ children }: SpoilerSpanProps) => {
 		[reveal],
 	);
 
-	const srText = t('Spoiler_hidden_activate_to_reveal', { defaultValue: 'Spoiler hidden. Activate to reveal.' });
+	const content = useMemo(
+		() =>
+			children.map((block, index) => {
+				switch (block.type) {
+					case 'EMOJI':
+						return <EmojiElement key={index} {...block} />;
+
+					case 'MENTION_USER':
+						return <UserMentionElement key={index} mention={block.value.value} />;
+
+					case 'MENTION_CHANNEL':
+						return <ChannelMentionElement key={index} mention={block.value.value} />;
+
+					case 'PLAIN_TEXT':
+						return <PlainSpan key={index} text={block.value} />;
+
+					case 'LINK':
+						return <LinkSpan key={index} href={block.value.src.value} label={block.value.label} />;
+
+					case 'STRIKE':
+						return <StrikeSpan key={index}>{block.value}</StrikeSpan>;
+
+					case 'ITALIC':
+						return <ItalicSpan key={index}>{block.value}</ItalicSpan>;
+
+					case 'BOLD':
+						return <BoldSpan key={index}>{block.value}</BoldSpan>;
+
+					case 'INLINE_CODE':
+						return <CodeElement key={index} code={block.value.value} />;
+
+					case 'TIMESTAMP':
+						return <Timestamp key={index}>{block}</Timestamp>;
+
+					case 'COLOR':
+						return <ColorElement key={index} {...block.value} />;
+
+					case 'IMAGE':
+						return <ImageElement key={index} src={block.value.src.value} alt={block.value.label} />;
+
+					case 'INLINE_KATEX':
+						return <PlainSpan key={index} text={block.value} />;
+
+					default:
+						return null;
+				}
+			}),
+		[children],
+	);
 
 	if (revealed) {
-		return <span style={revealedStyle}>{children.map((block, index) => renderBlockComponent(block, index))}</span>;
+		return <span style={revealedStyle}>{content}</span>;
 	}
+
+	const srText = t('Spoiler_hidden_activate_to_reveal', { defaultValue: 'Spoiler hidden. Activate to reveal.' });
 
 	return (
-		<span role='button' tabIndex={0} aria-expanded={false} aria-label={srText} onClick={reveal} onKeyDown={onKeyDown} style={spoilerStyle}>
+		<span role='button' tabIndex={0} aria-label={srText} onClick={reveal} onKeyDown={onKeyDown} style={spoilerStyle}>
 			<span style={srOnlyStyle}>{srText}</span>
-			<span aria-hidden>{children.map((block, index) => renderBlockComponent(block, index))}</span>
+			<span aria-hidden>{content}</span>
 		</span>
 	);
-};
-
-const renderBlockComponent = (block: MessageParser.Inlines, index: number) => {
-	switch (block.type) {
-		case 'EMOJI':
-			return <EmojiElement key={index} {...block} />;
-
-		case 'MENTION_USER':
-			return <UserMentionElement key={index} mention={block.value.value} />;
-
-		case 'MENTION_CHANNEL':
-			return <ChannelMentionElement key={index} mention={block.value.value} />;
-
-		case 'PLAIN_TEXT':
-			return <PlainSpan key={index} text={block.value} />;
-
-		case 'LINK':
-			return <LinkSpan key={index} href={block.value.src.value} label={block.value.label} />;
-
-		case 'STRIKE':
-			return <StrikeSpan key={index}>{block.value}</StrikeSpan>;
-
-		case 'ITALIC':
-			return <ItalicSpan key={index}>{block.value}</ItalicSpan>;
-
-		case 'BOLD':
-			return <BoldSpan key={index}>{block.value}</BoldSpan>;
-
-		case 'INLINE_CODE':
-			return <CodeElement key={index} code={block.value.value} />;
-
-		case 'SPOILER':
-			return <SpoilerSpan key={index}>{block.value}</SpoilerSpan>;
-
-		case 'TIMESTAMP':
-			return <Timestamp key={index}>{block}</Timestamp>;
-
-		case 'COLOR':
-			return <ColorElement key={index} {...block.value} />;
-
-		case 'IMAGE':
-			return <ImageElement key={index} src={block.value.src.value} alt={block.value.label} />;
-
-		case 'INLINE_KATEX':
-			return <PlainSpan key={index} text={block.value} />;
-
-		default:
-			return null;
-	}
 };
 
 export default SpoilerSpan;
