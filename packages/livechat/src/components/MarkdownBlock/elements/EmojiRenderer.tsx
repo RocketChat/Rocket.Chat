b077ab0d3632a81a7118384c72789255e696e768
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-07-14 11:14:46 -0300

    refactor(livechat): remove DOMPurify sanitization from fallback in EmojiRenderer

--- packages/livechat/src/components/MarkdownBlock/elements/EmojiRenderer.tsx
+++ packages/livechat/src/components/MarkdownBlock/elements/EmojiRenderer.tsx
@@ -1,5 +1,4 @@
 import type * as MessageParser from '@rocket.chat/message-parser';
-import DOMPurify from 'dompurify';
 
 type EmojiProps = MessageParser.Emoji & {
 	big?: boolean;
@@ -8,12 +7,10 @@ type EmojiProps = MessageParser.Emoji & {
 const EmojiRenderer = (props: EmojiProps) => {
 	const fallback = 'unicode' in props ? props.unicode : `:${props.shortCode ?? props.value.value}:`;
 
-	const sanitizedFallback = DOMPurify.sanitize(fallback);
-
 	return (
 		<>
-			<span title={sanitizedFallback} role='img' aria-label={sanitizedFallback.charAt(0) === ':' ? sanitizedFallback : undefined}>
-				{sanitizedFallback}
+			<span title={fallback} role='img' aria-label={fallback.charAt(0) === ':' ? fallback : undefined}>
+				{fallback}
 			</span>
 		</>
 	);
