14a4a6ce4dd19d1e9954d7dde642078adc85a3b6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-07-14 14:56:47 -0300

    refactor(livechat): remove unused highlight.js integration and simplify CodeBlock component

--- packages/livechat/src/components/MarkdownBlock/blocks/CodeBlock.tsx
+++ packages/livechat/src/components/MarkdownBlock/blocks/CodeBlock.tsx
@@ -1,6 +1,5 @@
 import type * as MessageParser from '@rocket.chat/message-parser';
-import hljs from 'highlight.js';
-import { useLayoutEffect, useMemo, useRef } from 'preact/hooks';
+import { useMemo } from 'preact/hooks';
 
 import styles from './CodeBlock.scss';
 
@@ -10,33 +9,12 @@ export type CodeBlockProps = {
 };
 
 const CodeBlock = ({ lines = [], language }: CodeBlockProps) => {
-	const ref = useRef<HTMLElement>(null);
-
 	const code = useMemo(() => lines.map((line) => line.value.value).join('\n'), [lines]);
-
-	useLayoutEffect(() => {
-		const element = ref.current;
-
-		if (!element) {
-			return;
-		}
-
-		hljs.highlightElement(element);
-		if (!element.classList.contains('hljs')) {
-			element.classList.add('hljs');
-		}
-	}, [language, code]);
 
 	return (
 		<pre className={styles.codeblock} role='region'>
 			<span className={styles.codeblock__copyonly}>```</span>
-			<code
-				key={language + code}
-				ref={ref}
-				className={((!language || language === 'none') && 'code-colors') || `code-colors language-${language}`}
-			>
-				{code}
-			</code>
+			<code className={((!language || language === 'none') && 'code-colors') || `code-colors language-${language}`}>{code}</code>
 			<span className={styles.codeblock__copyonly}>```</span>
 		</pre>
 	);
