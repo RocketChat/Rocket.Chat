88f4c2612bbb834c64a41a19cac1d5fdc90a9d28
Douglas Fabris <devfabris@gmail.com>
2026-07-21 17:42:54 -0300

    chore: escapeHTML messageType.text

--- apps/meteor/client/views/room/contextualBar/Threads/hooks/useNormalizedThreadTitleHtml.ts
+++ apps/meteor/client/views/room/contextualBar/Threads/hooks/useNormalizedThreadTitleHtml.ts
@@ -49,7 +49,7 @@ export const useNormalizedThreadTitleHtml = (mainMessage: IThreadMainMessage) =>
 		}
 
 		if (message.t && messageType) {
-			return messageType.text(t, message, { capitalize: true });
+			return escapeHTML(messageType.text(t, message, { capitalize: true }));
 		}
 
 		return '';
