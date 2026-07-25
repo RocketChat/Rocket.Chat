58dbc6caef0151770a2ab6404ea89efba5086960
manikantagoudgoud447 <goudmanikantagoud@gmail.com>
2026-07-25 18:41:59 +0530

    chore: address review feedback for protected roles fix

--- apps/meteor/server/lib/roles/createOrUpdateProtectedRole.ts
+++ apps/meteor/server/lib/roles/createOrUpdateProtectedRole.ts
@@ -18,8 +18,6 @@ export const createOrUpdateProtectedRoleAsync = async (
 			roleData.mandatory2fa || role.mandatory2fa,
 		);
 
-		await Roles.updateOne({ _id: roleId, protected: { $ne: true } }, { $set: { protected: true } });
-
 		return;
 	}
 
