c79cdba05b5670393c9a555f758c4927b5e2b12e
khizarshah01 <khizar.shah010@gmail.com>
2026-07-28 01:20:29 +0530

    feat: add server side search for background jobs

--- packages/core-services/src/types/ICronJobsService.ts
+++ packages/core-services/src/types/ICronJobsService.ts
@@ -4,8 +4,8 @@ import type { PaginatedResult } from '@rocket.chat/rest-typings';
 import type { IServiceClass } from './ServiceClass';
 
 export interface ICronJobsService extends IServiceClass {
-	getCoreJobs(pagination?: { offset?: number; count?: number }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
-	getAppJobs(pagination?: { offset?: number; count?: number }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
+	getCoreJobs(pagination?: { offset?: number; count?: number; searchTerm?: string }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
+	getAppJobs(pagination?: { offset?: number; count?: number; searchTerm?: string }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
 	getHistory(jobName: string, pagination?: { offset?: number; count?: number }): Promise<PaginatedResult<{ history: ICronHistoryItem[] }>>;
 	getJob(jobName: string): Promise<ICronJobItem | null>;
 
