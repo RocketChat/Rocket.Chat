d33457e53850b10f37d7726e534d5544a54af845
khizarshah01 <khizar.shah010@gmail.com>
2026-08-02 02:26:45 +0530

    feat: add status filter for jobs list

--- packages/core-services/src/types/ICronJobsService.ts
+++ packages/core-services/src/types/ICronJobsService.ts
@@ -1,11 +1,21 @@
-import type { ICronJobItem, ICronHistoryItem } from '@rocket.chat/core-typings';
+import type { CronJobStatus, ICronJobItem, ICronHistoryItem } from '@rocket.chat/core-typings';
 import type { PaginatedResult } from '@rocket.chat/rest-typings';
 
 import type { IServiceClass } from './ServiceClass';
 
 export interface ICronJobsService extends IServiceClass {
-	getCoreJobs(pagination?: { offset?: number; count?: number; searchTerm?: string }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
-	getAppJobs(pagination?: { offset?: number; count?: number; searchTerm?: string }): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
+	getCoreJobs(pagination?: {
+		offset?: number;
+		count?: number;
+		searchTerm?: string;
+		status?: CronJobStatus;
+	}): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
+	getAppJobs(pagination?: {
+		offset?: number;
+		count?: number;
+		searchTerm?: string;
+		status?: CronJobStatus;
+	}): Promise<PaginatedResult<{ jobs: ICronJobItem[] }>>;
 	getHistory(jobName: string, pagination?: { offset?: number; count?: number }): Promise<PaginatedResult<{ history: ICronHistoryItem[] }>>;
 	getJob(jobName: string): Promise<ICronJobItem | null>;
 
