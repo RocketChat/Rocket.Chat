6f61569b1e1623f64c69ea8c7f493befb490c714
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-08-03 23:56:18 -0300

    change isRecord in favor of isObject

--- packages/tools/src/mongoErrors.ts
+++ packages/tools/src/mongoErrors.ts
@@ -1,4 +1,4 @@
-import { isRecord } from './isRecord';
+import { isObject } from './isObject';
 
 export const MongoErrorCode = {
 	DuplicateKey: 11000,
@@ -18,6 +18,6 @@ type MongoBulkWriteErrorLike = {
 };
 
 export const isMongoServerError = (error: unknown): error is MongoServerErrorLike =>
-	isRecord(error) && ('code' in error || 'codeName' in error);
+	isObject(error) && ('code' in error || 'codeName' in error);
 
-export const isMongoBulkWriteError = (error: unknown): error is MongoBulkWriteErrorLike => isRecord(error) && 'writeErrors' in error;
+export const isMongoBulkWriteError = (error: unknown): error is MongoBulkWriteErrorLike => isObject(error) && 'writeErrors' in error;
