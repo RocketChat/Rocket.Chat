17e033ecc2f4638779f1f31baad16c9cca7e972a
Rahul Burman <rahulburman669@gmail.com>
2026-08-12 15:34:36 +0530

    fix(mp3-encoder): log failed assertions

--- packages/mp3-encoder/src/lame/assert.ts
+++ packages/mp3-encoder/src/lame/assert.ts
@@ -1,5 +1,6 @@
-export function assert(condition: boolean, _message?: string): asserts condition {
+export function assert(condition: boolean, message?: string): asserts condition {
 	if (!condition) {
+		console.assert(condition, message);
 		// TODO: There is a condition generating multiple NaN values that was never
 		//       addressed in the original code and is not clear how to handle it.
 		//       Originally this assertion was commented out, probably because of it.
