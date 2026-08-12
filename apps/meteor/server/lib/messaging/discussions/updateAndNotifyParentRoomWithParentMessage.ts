afbdedc7ce705ca0a902b343c47d6fcc8faa2f28
Diego Sampaio <chinello@gmail.com>
2026-08-12 16:01:27 -0300

    tighten IRoom projections

--- apps/meteor/server/lib/messaging/discussions/updateAndNotifyParentRoomWithParentMessage.ts
+++ apps/meteor/server/lib/messaging/discussions/updateAndNotifyParentRoomWithParentMessage.ts
@@ -3,7 +3,7 @@ import { Messages } from '@rocket.chat/models';
 
 import { notifyOnMessageChange } from '../../notifyListener';
 
-export const updateAndNotifyParentRoomWithParentMessage = async (room: IRoom): Promise<void> => {
+export const updateAndNotifyParentRoomWithParentMessage = async (room: Pick<IRoom, '_id' | 'msgs' | 'lm'>): Promise<void> => {
 	const parentMessage = await Messages.refreshDiscussionMetadata(room);
 	if (!parentMessage) {
 		return;
--- apps/meteor/server/services/team/service.ts
+++ apps/meteor/server/services/team/service.ts
@@ -575,7 +575,7 @@ export class TeamService extends ServiceClassInternal implements ITeamService {
 			throw new Error('user-not-on-private-team');
 		}
 
-		const teamRooms: (IRoom & {
+		const teamRooms: (Pick<IRoom, '_id' | 't'> & {
 			userCanDelete?: boolean;
 		})[] = await Rooms.findByTeamId(teamId, {
 			projection: { _id: 1, t: 1 },
