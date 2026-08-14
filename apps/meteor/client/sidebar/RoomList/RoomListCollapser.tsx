52fda8760171ae4e1186159a41b4c1bef44a2359
Douglas Fabris <devfabris@gmail.com>
2026-08-13 22:24:07 -0300

    feat: enhance RoomListCollapser with deferred menu and improved interaction handling

--- apps/meteor/client/sidebar/RoomList/RoomListCollapser.tsx
+++ apps/meteor/client/sidebar/RoomList/RoomListCollapser.tsx
@@ -1,8 +1,10 @@
-import { Badge, SidebarV2CollapseGroup } from '@rocket.chat/fuselage';
+import { Badge, IconButton, SidebarV2CollapseGroup, SidebarV2CollapseGroupMenu } from '@rocket.chat/fuselage';
 import type { TranslationKey } from '@rocket.chat/ui-contexts';
 import type { HTMLAttributes, KeyboardEvent, MouseEventHandler } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { usePreventPropagation } from '../../hooks/usePreventPropagation';
+import { useDeferredMenuMount } from '../Item/useDeferredMenuMount';
 import CategoryMenu from '../categories/CategoryMenu';
 import type { SidebarRoomListGroup } from '../hooks/useRoomList';
 import { useUnreadDisplay } from '../hooks/useUnreadDisplay';
@@ -19,7 +21,8 @@ type RoomListCollapserProps = {
 
 const RoomListCollapser = ({ group, canMoveUp, canMoveDown, onMoveUp, onMoveDown, ...props }: RoomListCollapserProps) => {
 	const { t } = useTranslation();
-
+	const preventPropagation = usePreventPropagation();
+	const { mounted: menuVisibility, requestMount, mountNow } = useDeferredMenuMount();
 	const { unreadTitle, unreadVariant, showUnread, unreadCount } = useUnreadDisplay(group.unreadInfo);
 
 	const title = group.translateTitle ? t(group.title as TranslationKey) : group.title;
@@ -35,17 +38,25 @@ const RoomListCollapser = ({ group, canMoveUp, canMoveDown, onMoveUp, onMoveDown
 					</Badge>
 				) : undefined
 			}
+			onFocus={mountNow}
+			onPointerEnter={requestMount}
 			menu={
-				<CategoryMenu
-					category={group.category}
-					groupKey={group.key}
-					showUnreads={group.showUnreads}
-					keepUnreadsOnTop={group.keepUnreadsOnTop}
-					canMoveUp={canMoveUp}
-					canMoveDown={canMoveDown}
-					onMoveUp={onMoveUp}
-					onMoveDown={onMoveDown}
-				/>
+				<SidebarV2CollapseGroupMenu onClick={preventPropagation}>
+					{menuVisibility ? (
+						<CategoryMenu
+							category={group.category}
+							groupKey={group.key}
+							showUnreads={group.showUnreads}
+							keepUnreadsOnTop={group.keepUnreadsOnTop}
+							canMoveUp={canMoveUp}
+							canMoveDown={canMoveDown}
+							onMoveUp={onMoveUp}
+							onMoveDown={onMoveDown}
+						/>
+					) : (
+						<IconButton tabIndex={-1} aria-hidden mini icon='kebab' onPointerDown={mountNow} />
+					)}
+				</SidebarV2CollapseGroupMenu>
 			}
 			aria-label={group.collapsed ? t('Expand_group', { group: title }) : t('Collapse_group', { group: title })}
 			{...props}
