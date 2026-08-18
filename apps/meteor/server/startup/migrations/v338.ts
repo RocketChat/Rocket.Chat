55f10a2b62ff0c832725c82194e781fe84b238e8
Kevin  Aleman <kaleman960@gmail.com>
2026-08-18 10:00:28 -0600

    chore: deactivate existing anonymous users on upgrade

--- apps/meteor/server/startup/migrations/v338.ts
+++ apps/meteor/server/startup/migrations/v338.ts
@@ -1,11 +1,18 @@
-import { Settings } from '@rocket.chat/models';
+import { Settings, Users } from '@rocket.chat/models';
 
 import { addMigration } from '../../lib/migrations';
 
 addMigration({
 	version: 338,
-	name: 'Remove Accounts_AllowAnonymousWrite setting',
+	name: 'Remove Accounts_AllowAnonymousWrite setting and deactivate anonymous users',
 	async up() {
 		await Settings.deleteOne({ _id: 'Accounts_AllowAnonymousWrite' });
+		await Users.updateMany(
+			{ roles: 'anonymous' },
+			{
+				$set: { active: false },
+				$unset: { 'services.resume.loginTokens': 1 },
+			},
+		);
 	},
 });
