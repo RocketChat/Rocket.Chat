2b359ff430b84d4f9a6485d35a8bf26b5ea3bfd6
Kevin Aleman <kaleman960@gmail.com>
2026-08-26 10:07:43 -0600

    chore!: remove dead legacy federation bridge plumbing (#41940)

--- apps/meteor/client/startup/slashCommands/federation.ts
+++ apps/meteor/client/startup/slashCommands/federation.ts
@@ -1,24 +1,5 @@
 import { slashCommands } from '../../../app/utils/client/slashCommand';
 
-const callback = undefined;
-const result = undefined;
-const providesPreview = false;
-const previewer = undefined;
-const previewCallback = undefined;
-
-slashCommands.add({
-	command: 'federation',
-	callback,
-	options: {
-		description: 'Federation_slash_commands',
-		params: '#command (dm) #user',
-	},
-	result,
-	providesPreview,
-	previewer,
-	previewCallback,
-});
-
 slashCommands.add({
 	command: 'xmpp-join',
 	options: {
