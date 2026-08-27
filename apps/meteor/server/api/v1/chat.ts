01d81c2c8e82cf695a732f5366a3433b4dbb8e7b
Ricardo Garim <rswarovsky@gmail.com>
2026-08-27 17:40:54 -0300

    regression: add chat.sendMessage rate limiter (#41966)

--- apps/meteor/server/api/v1/chat.ts
+++ apps/meteor/server/api/v1/chat.ts
@@ -895,6 +895,7 @@ const chatEndpoints = API.v1
 		'chat.sendMessage',
 		{
 			authRequired: true,
+			rateLimiterOptions: { numRequestsAllowed: 5, intervalTimeInMS: 1000, bypassPermissions: ['send-many-messages'] },
 			body: isChatSendMessageProps,
 			response: {
 				200: ajv.compile<{ message: IMessage }>({
