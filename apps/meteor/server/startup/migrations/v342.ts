3dd42d8cb77bbd3119e549588a5498f597a75fff
Kevin  Aleman <kaleman960@gmail.com>
2026-08-27 09:17:48 -0600

    chore: drop visitor index in separate try/catch

--- apps/meteor/server/startup/migrations/v342.ts
+++ apps/meteor/server/startup/migrations/v342.ts
@@ -7,16 +7,17 @@ addMigration({
 	name: 'Drop unused indexes from rocketchat_livechat_visitor and rocketchat_subscription',
 	async up() {
 		try {
+			await LivechatVisitors.col.dropIndex('activity_1');
+		} catch {
+			// ignore
+		}
+
+		try {
 			await Subscriptions.col.dropIndex('desktopNotifications_1');
 			await Subscriptions.col.dropIndex('mobilePushNotifications_1');
 			await Subscriptions.col.dropIndex('emailNotifications_1');
-			// activity_1 stopped being created in 7.3 while the subscription indexes lasted until 8.7,
-			// so it must be dropped last: an IndexNotFound here aborts the try block
-			await LivechatVisitors.col.dropIndex('activity_1');
-		} catch (e: any) {
-			if (e?.code !== 27 && e?.codeName !== 'IndexNotFound') {
-				throw e;
-			}
+		} catch {
+			// ignore
 		}
 	},
 });
