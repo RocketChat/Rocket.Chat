eddda3302956f096ccc3f06328cd91e1bfd253e4
Ricardo Garim <rswarovsky@gmail.com>
2026-09-07 19:21:12 -0300

    fix: name the status-disabled hint icons for screen readers

--- apps/meteor/client/views/account/profile/UserStatusDisabledInfo.tsx
+++ apps/meteor/client/views/account/profile/UserStatusDisabledInfo.tsx
@@ -6,8 +6,10 @@ type UserStatusDisabledInfoProps = { workspace: boolean };
 const UserStatusDisabledInfo = ({ workspace }: UserStatusDisabledInfoProps) => {
 	const { t } = useTranslation();
 
+	const reason = workspace ? t('User_status_disabled_on_this_workspace') : t('User_status_disabled_by_an_admin');
+
 	return (
-		<Box display='flex' title={workspace ? t('User_status_disabled_on_this_workspace') : t('User_status_disabled_by_an_admin')}>
+		<Box display='flex' role='img' aria-label={reason} title={reason}>
 			<Icon name='info-circled' size='x20' color='info' />
 		</Box>
 	);
