21b8d1c6796ee4913cd7414fa19a182bc936655f
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-09-08 14:12:41 -0300

    fix: voice call fails to connect if a re-invite is received before the first answer is saved

--- ee/packages/media-calls/src/sip/providers/OutgoingSipCall.ts
+++ ee/packages/media-calls/src/sip/providers/OutgoingSipCall.ts
@@ -203,6 +203,7 @@ export class OutgoingSipCall extends BaseSipCall {
 			webrtcAnswer: { type: 'answer', sdp: this.sipDialog.remote.sdp },
 			supportedFeatures: SIP_CALL_FEATURES,
 			sipCallId: this.sipDialog.sip?.callId,
+			negotiationId: negotiation._id,
 		});
 	}
 
