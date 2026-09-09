decc5eb1645207f6f4947c8be6c340e9362fbd17
Ricardo Garim <rswarovsky@gmail.com>
2026-09-09 17:10:26 +0000

    test: add coverage for AutoTranslate client logic (#42024)

--- apps/meteor/tests/e2e/page-objects/home-channel.ts
+++ apps/meteor/tests/e2e/page-objects/home-channel.ts
@@ -11,6 +11,7 @@ import {
 	MembersFlexTab,
 	ChannelsFlexTab,
 	NotificationPreferencesFlexTab,
+	AutoTranslateFlexTab,
 	ExportMessagesFlexTab,
 	PruneMessagesFlexTab,
 	SearchMessagesFlexTab,
@@ -45,6 +46,7 @@ export class HomeChannel extends RoutedPage {
 		editRoom: EditRoomFlexTab;
 		channels: ChannelsFlexTab;
 		notificationPreferences: NotificationPreferencesFlexTab;
+		autoTranslate: AutoTranslateFlexTab;
 		exportMessages: ExportMessagesFlexTab;
 		pruneMessages: PruneMessagesFlexTab;
 		searchMessages: SearchMessagesFlexTab;
@@ -76,6 +78,7 @@ export class HomeChannel extends RoutedPage {
 			editRoom: new EditRoomFlexTab(page.getByRole('dialog', { name: 'Edit channel' })),
 			channels: new ChannelsFlexTab(page),
 			notificationPreferences: new NotificationPreferencesFlexTab(page),
+			autoTranslate: new AutoTranslateFlexTab(page),
 			exportMessages: new ExportMessagesFlexTab(page),
 			pruneMessages: new PruneMessagesFlexTab(page),
 			searchMessages: new SearchMessagesFlexTab(page),
