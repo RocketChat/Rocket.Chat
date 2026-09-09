f2d4b433caed00a55c2b1906ff5b3fef14b0c97d
Ricardo Garim <rswarovsky@gmail.com>
2026-09-09 09:57:12 -0300

    feat: filter users.list by custom fields

--- packages/rest-typings/src/v1/users/UsersListParamsGET.ts
+++ packages/rest-typings/src/v1/users/UsersListParamsGET.ts
@@ -5,6 +5,8 @@ export type UsersListParamsGET = PaginatedRequest<{
 	fields?: string;
 	query?: string;
 	email?: string;
+	customFields?: string;
+	includeCustomFields?: 'true' | 'false';
 }>;
 
 const UsersListParamsGetSchema = {
@@ -16,6 +18,8 @@ const UsersListParamsGetSchema = {
 		offset: { type: 'number', nullable: true },
 		sort: { type: 'string', nullable: true },
 		email: { type: 'string', minLength: 1, nullable: true },
+		customFields: { type: 'string', nullable: true },
+		includeCustomFields: { type: 'string', enum: ['true', 'false'], nullable: true },
 	},
 	additionalProperties: false,
 };
