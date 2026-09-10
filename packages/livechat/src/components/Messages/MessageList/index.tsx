c5752710318c97271cde43a95724324c87545621
Kevin  Aleman <kaleman960@gmail.com>
2026-09-10 12:37:52 -0600

    chore: revert unrelated optional-chaining autofix in livechat MessageList

--- packages/livechat/src/components/Messages/MessageList/index.tsx
+++ packages/livechat/src/components/Messages/MessageList/index.tsx
@@ -1,5 +1,5 @@
-import { isSameDay } from 'date-fns/isSameDay';
 import { parseISO } from 'date-fns/parseISO';
+import { isSameDay } from 'date-fns/isSameDay';
 import i18next from 'i18next';
 import type { VNode } from 'preact';
 import type { CSSProperties } from 'preact/compat';
@@ -81,7 +81,7 @@ export class MessageList extends MemoizedComponent<MessageListProps, unknown> {
 		if (this.scrollPosition !== scrollPosition) {
 			this.scrollPosition = scrollPosition;
 			const { onScrollTo } = this.props;
-			onScrollTo?.(scrollPosition);
+			onScrollTo && onScrollTo(scrollPosition);
 		}
 
 		const { dispatch } = this.props;
@@ -104,13 +104,13 @@ export class MessageList extends MemoizedComponent<MessageListProps, unknown> {
 		if (base.scrollHeight <= base.clientHeight) {
 			const { onScrollTo } = this.props;
 			this.scrollPosition = MessageList.SCROLL_AT_BOTTOM;
-			onScrollTo?.(MessageList.SCROLL_AT_BOTTOM);
+			onScrollTo && onScrollTo(MessageList.SCROLL_AT_BOTTOM);
 		}
 	};
 
 	handleClick = () => {
 		const { handleEmojiClick } = this.props;
-		handleEmojiClick?.();
+		handleEmojiClick && handleEmojiClick();
 	};
 
 	override componentWillUpdate() {
@@ -208,7 +208,7 @@ export class MessageList extends MemoizedComponent<MessageListProps, unknown> {
 			}
 		}
 
-		if (typingUsernames?.length) {
+		if (typingUsernames && typingUsernames.length) {
 			const indicatorLabel = `${typingUsernames.join(', ')} ${typingUsernames.length > 1 ? i18next.t('are_typing') : i18next.t('is_typing')}`;
 			items.push(
 				<TypingIndicator
