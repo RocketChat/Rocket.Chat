1fbbadb969e6acabde718543d322e3b60bf7b6c6
Yash Rajpal <58601732+yash-rajpal@users.noreply.github.com>
2026-09-11 15:24:23 +0000

    fix: Ignored thread messages not working (#42019)

--- apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
@@ -119,6 +119,14 @@ export class HomeContent {
 		return this.lastUserMessageBody.locator('role=button[name="This message was ignored"]');
 	}
 
+	get lastIgnoredThreadMessage(): Locator {
+		return this.lastUserThreadMessage.getByRole('button', { name: 'This message was ignored' });
+	}
+
+	get ignoredThreadMessages(): Locator {
+		return this.threadMessageListItems.getByRole('button', { name: 'This message was ignored' });
+	}
+
 	async joinRoomIfNeeded(): Promise<void> {
 		if (await this.composer.inputMessage.isEnabled()) {
 			return;
