015e4dcc027ed3604e7f14b7c31fd91c83da03ab
dougfabris <devfabris@gmail.com>
2026-09-14 19:45:00 +0000

    test: attach batches in a single selection (#42126)

--- apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
@@ -432,6 +432,19 @@ export class HomeContent {
 		}
 	}
 
+	async sendMultipleFilesMessage(fileNames: string[], { waitForResponse = true }: { waitForResponse?: boolean } = {}): Promise<void> {
+		await this.page
+			.getByLabel('Room composer')
+			.locator('input[type=file]')
+			.setInputFiles(fileNames.map((name) => getFilePath(name)));
+
+		if (!waitForResponse) {
+			return;
+		}
+
+		await expect(this.composer.btnSend).toBeEnabled();
+	}
+
 	async sendFileMessage(fileName: string, { waitForResponse = true }: { waitForResponse?: boolean } = {}): Promise<void> {
 		const responsePromise = waitForResponse ? createMediaResponsePromise(this.page) : null;
 		await this.page.getByLabel('Room composer').locator('input[type=file]').setInputFiles(getFilePath(fileName));
