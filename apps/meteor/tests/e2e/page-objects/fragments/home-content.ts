65e69cbff388f0838c448795bb8419f107e8a420
Douglas Fabris <devfabris@gmail.com>
2026-09-14 12:55:14 -0300

    test: add support for sending multiple files in file upload tests

--- apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
+++ apps/meteor/tests/e2e/page-objects/fragments/home-content.ts
@@ -432,6 +432,17 @@ export class HomeContent {
 		}
 	}
 
+	async sendMultipleFilesMessage(fileNames: string[], { waitForResponse = true }: { waitForResponse?: boolean } = {}): Promise<void> {
+		const responsePromise = waitForResponse ? createMediaResponsePromise(this.page) : null;
+		await this.page
+			.getByLabel('Room composer')
+			.locator('input[type=file]')
+			.setInputFiles(fileNames.map((name) => getFilePath(name)));
+		if (responsePromise) {
+			await responsePromise;
+		}
+	}
+
 	async sendFileMessage(fileName: string, { waitForResponse = true }: { waitForResponse?: boolean } = {}): Promise<void> {
 		const responsePromise = waitForResponse ? createMediaResponsePromise(this.page) : null;
 		await this.page.getByLabel('Room composer').locator('input[type=file]').setInputFiles(getFilePath(fileName));
