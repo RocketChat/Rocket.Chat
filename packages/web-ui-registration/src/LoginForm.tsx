b999b9ef7e14c5c2b79ed877c8affeea93c4adfb
Anil <anilgorade8@gmail.com>
2026-09-17 21:02:37 +0530

    fix: use existing password placeholder translation

--- packages/web-ui-registration/src/LoginForm.tsx
+++ packages/web-ui-registration/src/LoginForm.tsx
@@ -204,11 +204,13 @@ export const LoginForm = ({ setLoginRoute }: LoginFormProps) => {
 								<FieldRow>
 									<PasswordInput
 										{...register('password', {
-											required: t('Required_field', { field: t('registration.component.form.password') }),
+											required: t('Required_field', {
+												field: t('registration.component.form.password'),
+											}),
 										})}
 										placeholder={
 											passwordPlaceholder ||
-											t('registration.component.form.passwordPlaceholder')
+											t('Create_a_password')
 										}
 										error={errors.password?.message}
 										aria-invalid={errors.password || errorOnSubmit ? 'true' : 'false'}
