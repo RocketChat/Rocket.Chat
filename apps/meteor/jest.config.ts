f57fff3e15d08333fbc55ef21bde382189568140
Ricardo Garim <rswarovsky@gmail.com>
2026-09-18 16:26:17 -0300

    test: cover outgoing integration trigger handler (#42119)

--- apps/meteor/jest.config.ts
+++ apps/meteor/jest.config.ts
@@ -52,6 +52,7 @@ export default {
 				'<rootDir>/server/api/v1/middlewares/*.spec.ts',
 				'<rootDir>/server/lib/cloud/version-check/**/*.spec.ts',
 				'<rootDir>/server/lib/auth-providers/apple/**.spec.ts',
+				'<rootDir>/server/lib/integrations/**/*.spec.ts',
 				'<rootDir>/server/lib/statusVisibility/*.spec.ts',
 				'<rootDir>/server/services/statusVisibility/*.spec.ts',
 			],
