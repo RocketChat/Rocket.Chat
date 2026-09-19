3ff12bfb5812cf229ee26a395d64d0f8789cb603
Ashish-Minj1290 <aasishtoppo1290@gmail.com>
2026-09-19 16:15:41 +0530

    fix(composer): resolve channel mentions in composer markup

--- packages/gazzodown-alt/src/ComposerMentionChannel.tsx
+++ packages/gazzodown-alt/src/ComposerMentionChannel.tsx
@@ -1,5 +1,8 @@
 import type { ReactElement } from 'react';
+import { useContext } from 'react';
 import { memo } from 'react';
+
+import { ComposerMarkupContext } from './ComposerMarkupContext';
 
 type ComposerMentionChannelProps = {
 	mention: string;
@@ -7,6 +10,11 @@ type ComposerMentionChannelProps = {
 
 const className = 'rcx-message__highlight rcx-message__highlight--link';
 
-const ComposerMentionChannel = ({ mention }: ComposerMentionChannelProps): ReactElement => <span className={className}>#{mention}</span>;
+const ComposerMentionChannel = ({ mention }: ComposerMentionChannelProps): ReactElement => {
+	const { resolveChannelMention } = useContext(ComposerMarkupContext);
+	const resolved = resolveChannelMention?.(mention);
+
+	return <span className={className}>#{resolved?.fname ?? resolved?.name ?? mention}</span>;
+};
 
 export default memo(ComposerMentionChannel);
