0f6394aa0009e8e4eb2946c65dd3d7243f71db23
Ujjwal Kar <55041104+ujjwalkar0@users.noreply.github.com>
2026-09-24 02:50:04 +0530

    fix: prevent UI crash in channel directory when lastMessage  timestamp is missing (#40584)
    Co-authored-by: Ricardo Garim <rswarovsky@gmail.com>

--- apps/meteor/client/views/directory/tabs/channels/ChannelsTable/ChannelsTableRow.tsx
+++ apps/meteor/client/views/directory/tabs/channels/ChannelsTable/ChannelsTableRow.tsx
@@ -19,6 +19,7 @@ const ChannelsTableRow = ({ onClick, room, mediaQuery }: ChannelsTableRowProps) 
 	const formatDate = useFormatDate();
 	const { _id, ts, t, name, fname, usersCount, lastMessage, topic, belongsTo } = room;
 	const avatarUrl = roomCoordinator.getRoomDirectives(t).getAvatarPath(room);
+	const lastMessageTs = lastMessage?.ts;
 
 	return (
 		<GenericTableRow key={_id} onKeyDown={onClick(name, t)} onClick={onClick(name, t)} tabIndex={0} role='link' action>
@@ -47,7 +48,7 @@ const ChannelsTableRow = ({ onClick, room, mediaQuery }: ChannelsTableRowProps) 
 			)}
 			{mediaQuery && (
 				<GenericTableCell fontScale='p2' color='hint' withTruncatedText>
-					{lastMessage && formatDate(lastMessage.ts)}
+					{lastMessageTs && formatDate(lastMessageTs)}
 				</GenericTableCell>
 			)}
 			{mediaQuery && (
